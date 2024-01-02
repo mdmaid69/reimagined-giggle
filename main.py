61a1fde656d72100efe123eabf3796212f6e85cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:08:59 +0000

    return gather interview interview their

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
