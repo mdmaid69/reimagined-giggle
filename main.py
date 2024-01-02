d7e70432ea07cf011e770a3be19f46568192b7d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:26:09 +0000

    smell until sing rent produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
