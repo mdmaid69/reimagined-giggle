3836c3ac0da2b14af3b1cae9722bafd37cb74098
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:00:35 +0000

    whom warning rid ago same

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
