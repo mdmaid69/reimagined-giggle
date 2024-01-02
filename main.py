5053198c63afbcba0799325bf4493850b56ba9ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:04:10 +0000

    dance afraid there exactly reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
