8237055103c693e720f14325894770d2ab599ec7
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:23:19 +0000

    plan expensive impression demonstrate ground

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
