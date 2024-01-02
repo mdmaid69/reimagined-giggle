c748861a3a5cc428a72af914d49e80c1edc6dd59
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:59:01 +0000

    place necessary dream last patience

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
+def find_min(numbers):
+        return min(numbers)
