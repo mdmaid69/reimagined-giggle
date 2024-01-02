8892474c4a011325c7b88d16bfcd20edd7c98651
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:33 +0000

    estimate welcome wonder attempt rush

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-def subtract_numbers(x, y):
-        return x - y
-for i in range(10): print(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_floor(x):
+        return math.floor(x)
