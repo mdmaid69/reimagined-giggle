3e3a29d59706d77044da8580166f68114237332f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:39:38 +0000

    especially clear room mine hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
