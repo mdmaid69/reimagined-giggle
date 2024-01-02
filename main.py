ea29d4cc90587274c1989b763bd271259aefb863
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:17:04 +0000

    used woman measure discover tune

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import sys
+print(sys.version)
