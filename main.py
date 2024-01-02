56d798b996976e365cab83124355e7772c033224
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:43:08 +0000

    copy speak freedom probably fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
