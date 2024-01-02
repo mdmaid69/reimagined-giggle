c8063f3e852571ba41d804d47c1369f4c13e9916
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:00:43 +0000

    willing file environment shake crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
