79f0c92443374b80c20fcb9d27220d1d3d5fabce
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:30:27 +0000

    hold level survive hope previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_typecode(array):
-        return array.typecode
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
