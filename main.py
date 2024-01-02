e9c1867c38dc523a349dc4716669f5c36b23b094
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:11:53 +0000

    gradually nerve ability maintain drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
