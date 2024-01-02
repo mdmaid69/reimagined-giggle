b6a38310e4c5a263f2fd11e438195a1de1d0cb83
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:36:59 +0000

    towards passage this threaten sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
