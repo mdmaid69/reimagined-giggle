e90c44ec61cbf2fb45f46f4414b970536704cdc9
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:58:19 +0000

    pleasant pen weekend stretch scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
