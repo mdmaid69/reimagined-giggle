6241c709567ce13b80c4770c3f5749fff7694c72
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:17:43 +0000

    fierce schedule breakinto hole almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
