2424fdbcb5117b619007e244592c974690555cc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:43:30 +0000

    typical generous appeal workupto repair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
