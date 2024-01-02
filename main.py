c6fd5aaa2224f6e88f567de3e4f9a51b4dbb24ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:34:05 +0000

    wire big period apologizeapologise exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
