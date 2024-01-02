1505530b952883bdc10a5e65ad495f04eb673044
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:34:20 +0000

    deaf rise suck last slight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
