8198f1cda508579ac3e22c05426c8775df7b0be1
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:48:32 +0000

    describe usually port heart rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
