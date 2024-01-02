3c8ba21a83b26aad44a8eaa16ff246c511fb6fd7
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:14:38 +0000

    themselves sea neither communication damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
