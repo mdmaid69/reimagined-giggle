6fe80552a527a52b99eae26c9d77e31825754582
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:48:01 +0000

    considering surface sleep glad uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
