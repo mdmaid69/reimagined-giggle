ea2f63b237deec8e00e3a8aaa2ff6d123221b926
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:29:49 +0000

    gradually suckme like shoot laugh

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
