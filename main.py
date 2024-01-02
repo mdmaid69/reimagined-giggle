846a1a27d392e902fc593d8afbe54a64c26a048c
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:08:29 +0000

    goout child lesson run off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
