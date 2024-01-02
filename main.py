817dbdfd07444c33eb4f46fc1de89cf598b20995
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:25:39 +0000

    belong urgent winter ask fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_set(array):
+        return set(array)
