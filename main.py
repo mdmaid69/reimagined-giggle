823358bcdc046d5b70676d6b9c42ad3c6e396d48
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:20:02 +0000

    since control exciting fire hit

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
