5d5eaaa430c853c8aa603bcd25f9d42ff930f98d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:14:05 +0000

    stage talk each sleep amount

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
