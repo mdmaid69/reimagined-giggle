edb3842ac8cb4539dd96d85b0298af6664af1f39
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:22:48 +0000

    result clothes dear turnback form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def find_max(numbers):
-        return max(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
