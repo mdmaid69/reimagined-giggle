a6b1cdab8395b25374a6493622b48d61a286fb22
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:54:49 +0000

    comeout effect ourselves judge recommend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+print(datetime.datetime.now())
