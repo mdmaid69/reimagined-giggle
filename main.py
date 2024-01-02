3bb56a504721e24e992c3c56eab026e1edc6e955
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:18:58 +0000

    afterwards excuse us proof certainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
