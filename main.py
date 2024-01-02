86fae4af06312965c6cabc0a746de23864a7166c
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:11:55 +0000

    welcome whom use idea tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
