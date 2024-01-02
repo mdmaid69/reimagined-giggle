bfede76dda2881754797a4606a899bd15a69c392
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:55:10 +0000

    environment straight born base fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
+def get_array_length(array):
+        return len(array)
+import array
 def get_array_as_repr(array):
         return repr(array)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
