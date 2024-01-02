f818fe8295af4ed90f99534bbd6f1b97e7619f4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:06:17 +0000

    news allow none high around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
