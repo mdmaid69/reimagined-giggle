74fb8c9eb67322a88d5cef6db55ad8312d222849
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:35:33 +0000

    escape slide line have passenger

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_power(work, time):
+        return work / time
