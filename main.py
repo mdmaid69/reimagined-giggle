2a0e6dd230a533ca36e4dafcf3bfafeb38d3beab
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:28:01 +0000

    ball plenty treatment insist murder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_area_circle(r):
+        return 3.14 * r**2
