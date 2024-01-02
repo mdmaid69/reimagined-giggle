5dd01e5b84d1af5438416442db963fd9805286c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:42:59 +0000

    anywayanyhow tooth head old deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
