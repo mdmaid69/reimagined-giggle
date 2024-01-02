30a5d667500347b0ac56cc13ade844ed659eaac5
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:30:19 +0000

    early home roof test paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
