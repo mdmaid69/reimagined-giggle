d50401f3954aaee4d5222a9327d2287b93b5d101
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:41:26 +0000

    crime beautiful badly book giveout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
