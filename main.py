711ca02ae2e1b44907af5306bd5bbfd639bad66b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:37:14 +0000

    journey Zz regular small zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
