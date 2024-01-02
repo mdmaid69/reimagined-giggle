45b2811599429257e0d0e243aeb6933339c959ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:12:23 +0000

    grateful mess smell mend arrange

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
