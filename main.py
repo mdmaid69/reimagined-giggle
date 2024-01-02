5797c84fac2e4964bfbe2e49952ef4d987429d84
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:53:13 +0000

    anywhere object talkback confuse past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
