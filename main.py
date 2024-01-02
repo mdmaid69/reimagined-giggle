3db67d7c5575c0f6a318e4712c59caed35837c72
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:35:12 +0000

    reasonable opinion anything start neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
