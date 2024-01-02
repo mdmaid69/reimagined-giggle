b6315ba6426824a4f6b58b13cd90824015ca87b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:36:46 +0000

    house characteristic advantage sail ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
