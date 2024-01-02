1e9343958e8145b5d4b2315e37cb1d38bc554bd5
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:08:38 +0000

    married less above gradually war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
