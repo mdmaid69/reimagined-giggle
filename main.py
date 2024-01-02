ef8c65778834601cb4fdf03974b92144c679f1cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:48:03 +0000

    front seriously permission gift apart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
