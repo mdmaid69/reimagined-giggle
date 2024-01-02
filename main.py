03c32fd02909c408278ea150eabfdf3bd5195c2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:19:19 +0000

    stiff somebody film pass clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_hex(n):
+        return hex(n)
