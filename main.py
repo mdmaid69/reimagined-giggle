9f958659a54c84dfd12c49b07367a6c359da0f49
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:41:12 +0000

    love telephone majority prefer correct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
