852040e8f25aceaf635aa2437bc9572691f4b10d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:19:16 +0000

    566end extremely like loss lookafter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
