3c254d35e38634b0b2faa6f054deeb8a543541d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:40:39 +0000

    even soak mark getup noone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
