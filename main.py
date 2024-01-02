09307b3c5093948498c68a201eaa056912c4fd74
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:33:00 +0000

    such workon Hh fast frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
