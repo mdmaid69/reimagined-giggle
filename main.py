32fa774096651615d886c244f7c8a4a32582d196
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:00:23 +0000

    regional wealth reach answer expression

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def multiply_numbers(x, y):
+        return x * y
