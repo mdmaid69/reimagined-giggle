735434bccbd51bb781bc137a7694faacffd4b16a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:16:39 +0000

    store identify relax centre objection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
