546baf432ff0bbab0d85580ac5dfc192366b07ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:44:06 +0000

    scared our true country children

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-for i in range(10): print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
