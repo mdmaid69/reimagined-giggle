fd6076ed45334e923303beff97912f1a0fb8afd4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:17:52 +0000

    genuine large criticize bye concerned

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def find_unique_words(sentence):
-        return set(sentence.split())
