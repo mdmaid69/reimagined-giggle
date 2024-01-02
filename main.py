f7d6e7e11c343af133e90add73751c224efcbb7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:16:50 +0000

    curious amount your conversation again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
