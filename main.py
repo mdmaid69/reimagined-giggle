6784c5dabd310afda20c6ec3243f8cf2f1a644f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:27:13 +0000

    formal difference noise shame rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
