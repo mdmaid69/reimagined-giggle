2ae27a3c85f7febc6d176b7fe0eb1616fbd02e4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:38:00 +0000

    calloff rain before sad public

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
