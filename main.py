363ce702c46711da7a1b8d311c4a77b13ffccc9f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:16:44 +0000

    surface out butter above punishment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def convert_to_octal(n):
+        return oct(n)
