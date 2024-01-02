fa0b6e6a9d539d5c5b79b760c3fb368bcd633c04
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:10:48 +0000

    wish notice amount past him

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def count_characters(sentence):
+        return len(sentence)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
