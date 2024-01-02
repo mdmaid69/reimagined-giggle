3adc64f52e133484439f93853f135ab78efbfae7
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:56:59 +0000

    free command rob meaning regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
