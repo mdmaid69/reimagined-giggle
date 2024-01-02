0b290967ffbacb11262ae264f98b39c0f7e5b712
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:00:26 +0000

    best sore firm mysterious popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
