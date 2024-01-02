2e1cc83ef4bf35e8136724133c26b28166e679fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:21:39 +0000

    before height callout however so

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def sort_list(lst):
-        return sorted(lst)
+import re
+print(re.match("h.*o", "hello world"))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
