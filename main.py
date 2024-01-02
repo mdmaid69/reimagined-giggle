bedbb3bf95aa15b91ec2bf4b564360934f58ad48
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:35:23 +0000

    earn round round contribute light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
