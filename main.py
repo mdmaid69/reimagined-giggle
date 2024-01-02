c2a2a7051bb0cb333acd62af7503dff623f36b48
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:49:37 +0000

    ordinary holdon pair hide bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
