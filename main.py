f57856c8c330651a3f871f6d3aad34586e626780
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:45:48 +0000

    breakup hour decide story container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import datetime
+def get_today_date():
+        return datetime.date.today()
