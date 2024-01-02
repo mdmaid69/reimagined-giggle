8f37e030d6adbd1c9b6a0abe82f62ea7a6b1f03d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:40:37 +0000

    minute slight sink extra after

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
