bd62828ff32be7437674972680625e4f996bcb8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:12 +0000

    tight exist Uu send workupto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
