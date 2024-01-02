d61f07864f5478f01f45dcdd67a2ba2bcb656acb
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:20:59 +0000

    frame yet middle fairly ring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
