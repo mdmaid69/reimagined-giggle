5089dd4547b68ad71d519042344bc97a8d5db264
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:37:26 +0000

    earn next glad paper attract

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
