066f7e2e30d219b53734c87c9517f7e33ce54434
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:07:09 +0000

    eager job guess receive stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def count_elements(lst):
-        return len(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
