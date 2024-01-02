8b15bf4ed74fd58a0a51c3b56eefae25d1a55322
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:14:49 +0000

    but maintain rather precise brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def is_even(n):
+        return n % 2 == 0
