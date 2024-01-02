95ed46697fdde6d3a66539155b94d4cf251b9052
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:20:10 +0000

    inside sorry to fresh bringforward

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_volume(length, width, height):
+        return length * width * height
