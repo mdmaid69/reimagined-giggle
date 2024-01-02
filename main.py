0dacffb27d59bf93bb0567c6244000587c2e9daf
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:35:54 +0000

    cheat tax lock behaviour evidence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
