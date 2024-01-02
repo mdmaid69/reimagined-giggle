2fe3043c449eb30717b59e6ebb137beb3fd4b6fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:59:38 +0000

    fairly offer right argue common

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
