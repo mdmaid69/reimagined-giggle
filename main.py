326636d2d41e1a1eba456025afa64105efebb91c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:34:14 +0000

    classroom dig pass sink each

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
