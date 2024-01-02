e807e00ee89c4db45c57ba52f407025c7214216f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:06:39 +0000

    cruel study back negative before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
