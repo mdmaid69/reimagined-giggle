b44cc6f2fb64f4940d06e905cefc71834c7ea3b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:16:00 +0000

    scale just waste contents certain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+print([x**2 for x in range(10)])
