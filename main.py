60a0e6e281f0bb45ee19900675870194b8db0d6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:39 +0000

    to advantage promise talkoutof stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def count_elements(lst):
+        return len(lst)
