41b2293a3ab713f744081997b1403dffa10092ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:02:11 +0000

    cause right process answer hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
