beaa8df7c666823b8e8708eb91486af902e495bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:00:07 +0000

    plan deep answer size habit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
