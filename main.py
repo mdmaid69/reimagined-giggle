e7924a4c5b60563488676d564f7f88b4532f3dbc
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:39:56 +0000

    face exhibition rain wave something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
