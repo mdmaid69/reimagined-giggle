5305bb96c2bda85568413f1802c62dd0d933f37a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:36:17 +0000

    safe recently cook report shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
