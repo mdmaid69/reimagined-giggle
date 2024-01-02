318db50fe20f3314e95b3d7ea476c422b69ae9b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:40:29 +0000

    hers belief offend proof colour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def sort_list(lst):
+        return sorted(lst)
