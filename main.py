1709ffc5bf350bfcf8277dadae41309b3ac52418
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:53:36 +0000

    manage car especially might step

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
