e8388f76c336fe971e9f519be7768b77c9940933
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:23:20 +0000

    both store catch unkind law

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def reverse_list(lst):
-        return lst[::-1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_average(lst):
+        return sum(lst) / len(lst)
