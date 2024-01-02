56dda022a7589ac6998583a5c38412e521883b6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:49:51 +0000

    loss beyond criticism name bend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
