1bf391e5130c32024d68f6130c9d805773143f9a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:15:59 +0000

    violent report always trust spare

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
