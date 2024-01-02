5cc886a69af719331c3569af1236dae636661dbb
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:18:31 +0000

    pull fact pay takeoff trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
