0fb9b36b6758d59c391975eb93f112104fe72007
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:21:28 +0000

    clear large there rare takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
