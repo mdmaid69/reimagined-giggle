d3a28b5b2efbb370c287088abad66ad3d78a29c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:52:39 +0000

    best physical distance dangerous frighten

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+text = "Hello, world!"
+print("Characters:", len(text))
