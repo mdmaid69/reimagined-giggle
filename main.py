5a97830069e4de4ac555e426db36907febaeb97d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:33:31 +0000

    familiar somehow takeout sensitive replace

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_density(mass, volume):
+        return mass / volume
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
