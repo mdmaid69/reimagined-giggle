6dac4d6d44720de22f9781dcb41fa0e7e6f9ca02
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:52:36 +0000

    contact hole what Pp church

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
