811cc9ed246c4563ec81e15a45e1a6b24b8b2798
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:38:20 +0000

    idea brief land listenfor healthy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
