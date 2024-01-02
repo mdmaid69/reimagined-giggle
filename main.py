d9681fbc8133e1cd991812c5428984b321753f00
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:49:41 +0000

    properly practise trial afford relative

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def add_numbers(a, b):
+        return a + b
