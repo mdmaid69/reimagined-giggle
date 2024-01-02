d9cd33af66bb4896beca26b89048e08aa42aa3fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:41:42 +0000

    comfortable consider degree choose certainly

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
