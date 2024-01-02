e318185200a19d84487848a8d05bbc346f57a079
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:07:48 +0000

    use respect best think ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
