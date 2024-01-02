aacaf8f71f665a75bfd3dcc7d310e97ba40ed52c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:41:56 +0000

    afternoon what sure destroy could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import datetime
+print(datetime.datetime.now())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
