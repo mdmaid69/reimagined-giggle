3abdd2c713cb42fd4a7567addf677f999b582300
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:09:07 +0000

    carry professional fight error plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
