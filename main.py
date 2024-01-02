80426df8a1430c77b3206dff1bbeef702e7ae0b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:34:21 +0000

    near pass climb time combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
