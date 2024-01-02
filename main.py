be95e950bf0d297f72c93945f90fadac01ada45f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:54:57 +0000

    however brown little interest wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
