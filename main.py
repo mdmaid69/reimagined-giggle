0a844c37dc42fb060f4bed3724d11272e1b9913e
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:45:44 +0000

    stretch holdup people wide suffer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
