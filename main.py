b68bf7d834e5a20b93d40d9744648cc2889c20c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:51 +0000

    emotion port people trap that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
