33e1fc554cd6a11a04a3e2634624056ba73e9cbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:27:39 +0000

    back holdon already cause buy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import collections
-def create_user_list():
-        return collections.UserList()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
