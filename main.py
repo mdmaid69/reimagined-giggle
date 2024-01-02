9d75888349bc3f733d3c2431444c30e32b34efe3
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:31:51 +0000

    sweet exciting anything design advice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
