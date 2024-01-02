dcb315d1fcb3f573654f74cdacb5471041bb06fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:47:03 +0000

    exactly speech number include thorough

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def convert_array_to_string(array):
+        return array.tostring()
+import getpass
+def get_username():
+        return getpass.getuser()
