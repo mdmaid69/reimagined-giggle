c5bebb15943650af1bfe047d09e389c18ac90679
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:36:07 +0000

    discuss share thought beside guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+from collections import Counter
+print(Counter("hello world"))
