8ece0ba10b2fd4943d61eb72de86c49af2d047b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:39:57 +0000

    modern chase actually handle left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
