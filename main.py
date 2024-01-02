df4387c848340323f00c44c937727c39a3d1ab8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:15:48 +0000

    loud tight address accidental comeout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
