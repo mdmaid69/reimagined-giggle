0247b87f4a956b565f4b7f92ea072bbd48a40fd4
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:56:32 +0000

    warm future speech at approve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
