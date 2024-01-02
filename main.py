7baab9d34a6965fd33d72c069c3248307dac29f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:39:03 +0000

    suck free rule easy note

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
