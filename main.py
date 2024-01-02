14502f4e2ad19a660fd7fe71c1bcaad5182ebe45
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:52:43 +0000

    nerve apart charge joke regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_circle(r):
+        return 3.14 * r**2
