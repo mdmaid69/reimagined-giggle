cf7cdae3299fd4a2e38e64b9dd8c72a238b7066f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:51:54 +0000

    store habit feature stir event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+print(sum(range(10)))
