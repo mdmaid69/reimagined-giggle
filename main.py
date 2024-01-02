16c0478a92c6cd4568b6d7fa402447028bbbdf6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:06:44 +0000

    block course confident problem thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
