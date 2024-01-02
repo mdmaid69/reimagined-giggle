e3a6476c85d5438f8ffec4cdf07eccd2edeb6722
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:51:09 +0000

    comment cold fly any dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
