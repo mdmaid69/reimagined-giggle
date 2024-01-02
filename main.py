b4a8af48d4de3f191dd21359eb547afae678776d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:37:29 +0000

    team bet sometimes about yesterday

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import os
+print(os.getcwd())
