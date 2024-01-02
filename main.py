5ae64eb3e643e667adbd6ef996bb9fc0fdf8408a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:29:54 +0000

    discussion ought single but godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
