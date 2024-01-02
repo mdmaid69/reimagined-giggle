27484552e21e8835d8d574b127d091f7640ddfc2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:58 +0000

    steal modern too suppose achievement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
