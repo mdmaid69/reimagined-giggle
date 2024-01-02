383dd560cd2a3b14a9853d57750f3fb474b20843
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:47:14 +0000

    learn deaf loud mind season

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
