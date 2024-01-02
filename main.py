afce6d71ee4d9109be17df34c03755d17d7655bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:34:03 +0000

    talk into married serious save

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_density(mass, volume):
+        return mass / volume
