b9b3b23fb5cba2f2807ddd22c06845c08b767e58
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:57:45 +0000

    unlikely achieve cap quantity hurry

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import collections
+def create_queue():
+        return collections.deque()
