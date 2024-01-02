a5a7032b2336f0ab9f75dcad6f89aaa09e674b74
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:11:35 +0000

    long may father cover or

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_index(array, item):
+        return array.index(item)
+import tensorflow as tf
+print(tf.__version__)
