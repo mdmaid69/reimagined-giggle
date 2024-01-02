a9f86049914aacd8f2f627d53a91d2e635d1e98a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:00:35 +0000

    needle short pencil rather recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
