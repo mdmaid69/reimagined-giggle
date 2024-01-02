b2b68ea8cde789fe712a51a05911c425a970d66b
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:24:49 +0000

    display colour other only chat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
