170cbf00eabbd3e066cff763f2b00d2fc4268161
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:14:16 +0000

    low fortunate post aim blame

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_volume(length, width, height):
+        return length * width * height
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
