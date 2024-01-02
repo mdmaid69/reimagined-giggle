e58161d4480b35daeeda61591cec4cb2d01cff34
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:03:23 +0000

    profit measure pinch previous wish

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def sort_numbers(numbers):
+        return sorted(numbers)
+for i in range(5):
+        print(i)
