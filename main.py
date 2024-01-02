7c409706348371b497b4775c0bc51cde47f1d87f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:51:28 +0000

    wealth ordinary within art pollution

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def reverse_list(lst):
-        return lst[::-1]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def is_odd(n):
+        return n % 2 != 0
