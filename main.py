04c7e1f9fac6dfa1f39c1dd46e530e2355547c3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:04:05 +0000

    collection Jj back compete make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def add_numbers(a, b):
-        return a + b
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
