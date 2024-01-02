852f0c1e4fcfe15c121cac55ece53cf019cbea70
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:12:17 +0000

    birthday grateful Gg guilty rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
