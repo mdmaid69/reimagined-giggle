3160f5b96470c8a68a0efa3fb03f877bd2cf5c70
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:08:29 +0000

    nerve tell quantity equal crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
