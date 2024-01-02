79d0948c19c97790d147da94fcf94376cb9ecf49
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:01:47 +0000

    march talk help attempt secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
 def group_by(iterable, key_func):
         return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_itemsize(array):
+        return array.itemsize
