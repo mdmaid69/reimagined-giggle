2d4aa8aff7f4acfbb06c8999bc714bf4f3c99899
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:26:42 +0000

    live lose whenever goodbye stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
