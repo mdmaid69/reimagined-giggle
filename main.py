b544bbb26ca316ef7923d3ec027326808d7a4e9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:21:47 +0000

    achievement impressive limit empty whether

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
