ce21d0c9b451e96e8f125c9761461551aa7a2113
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:24:27 +0000

    sharp earn exam should confuse

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
