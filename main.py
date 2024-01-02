957a505d4edf483f5849a4da1449c38c757c652e
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:17:02 +0000

    surprised autumn issue picture blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
