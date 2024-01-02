f25d934892df06500dd2e1c14c65200226ebd381
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:43:42 +0000

    round tomorrow giveaway dress beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
