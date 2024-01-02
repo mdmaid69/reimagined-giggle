2dd2ee01b9c52af3b58720b3aeb11f8b7a44cd51
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:04:31 +0000

    excuse burn acceptable Nn violence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-from collections import Counter
-print(Counter("hello world"))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
