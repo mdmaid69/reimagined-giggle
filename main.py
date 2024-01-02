aebf3098dd193ae86103a696660e651602abf8ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:10:37 +0000

    sleep machine open close lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
