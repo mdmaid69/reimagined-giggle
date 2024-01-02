2d04feaa7d24100aead3ba975b292826974810ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:06:06 +0000

    priority scream heavy pretty burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
