bc652c80910f7e2e5bb0d8ecd2d88284fd39e62f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:12:01 +0000

    beautiful practical believe climb suit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
