170a61be6f2454c7270da9540a123001615ae9bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:13:50 +0000

    special drink notice can cry

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_volume(length, width, height):
+        return length * width * height
