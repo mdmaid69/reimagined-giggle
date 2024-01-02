eb5334fae78521239cafce9b47fae6c3eb47d36b
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:01:03 +0000

    giveup ready dress rich he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
