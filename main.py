cca898ac5e8b12c69d09daac837f913866734c66
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:49:37 +0000

    both attractive heavy beneath curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
