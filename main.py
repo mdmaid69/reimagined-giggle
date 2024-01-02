4f3001f947d07165ef2ac67bb0d1cbb528ceb12e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:16:56 +0000

    car bag file glass smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
