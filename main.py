dda09c691616ec6d734c0e530b4fc8b937402f18
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:25:23 +0000

    minute least easy consult change

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def clear_array(array):
-        array *= 0
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
