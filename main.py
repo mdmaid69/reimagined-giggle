c6d3d4948e64bee7cdfa4bcbb8f1add1d7892543
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:02:33 +0000

    fortunate regard attempt turnout weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
