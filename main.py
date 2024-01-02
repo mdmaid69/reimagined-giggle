e90278ca9c0eafa4d03b340f14eab4eac612e550
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:38:34 +0000

    ordinary hold question combination firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
