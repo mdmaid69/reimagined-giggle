d2ab25ac02977ade69ea8a8b43ec6f43d676094c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:04:36 +0000

    average lie better study late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_octal(n):
-        return oct(n)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
