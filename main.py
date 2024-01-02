7771fa676aa6e05c85f1b09018f6f29de60e6b02
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:11:21 +0000

    pleased sad chin bet surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_int(array):
+        return int(array[0])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
