cc9ac055cdae123b296d8fc8244edb10445b9edf
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:19:52 +0000

    gap bother whistle count listen

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def is_odd(n):
+        return n % 2 != 0
