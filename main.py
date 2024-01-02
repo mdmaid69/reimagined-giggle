7149c337f35543762af4cf0b21b677861bfd1c29
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:47:32 +0000

    573entertainment impatient speech read fail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
