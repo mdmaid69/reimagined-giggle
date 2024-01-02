bb8e672ba49ce6d1107a70fb0fe31355d187989c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:24:39 +0000

    may scheme wheel say problem

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def add_numbers(x, y):
+        return x + y
