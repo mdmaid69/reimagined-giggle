2cabc5ca16deb731a90693d066ff713831452ab8
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:55:31 +0000

    cost finish instrument every pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
