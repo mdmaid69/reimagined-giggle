ee6a4bfed708d4eee0d13ead6c68debf216a03f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:15:29 +0000

    hurry comeback regard type friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
