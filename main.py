b0e284bd65bde52cc1e7b758f235f5c2c76e420a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:47:22 +0000

    bit from about speech wind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import tensorflow as tf
-print(tf.__version__)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
