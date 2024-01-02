c97dce0943b62c6cf51b3720f1f8fab21e22e7de
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:06:40 +0000

    meaning simple sad original believe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
