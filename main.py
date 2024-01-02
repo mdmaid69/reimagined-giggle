3538d4f7ebd0045a18fe770cb29310db15b04b62
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:20:48 +0000

    earn refuse better evening tradition

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
