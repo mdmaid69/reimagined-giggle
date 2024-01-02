635b8e450c3e67053b27aab64d43c58f0d4d7e8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:01:48 +0000

    another hurt rent process shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
