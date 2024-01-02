ddedac27d7e81caf3209b9abecfd64811b59f134
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:28:12 +0000

    need pleased off rarely gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
