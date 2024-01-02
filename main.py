0f4c62441eba5b8c5a11c068b85a077ffc17645d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:15:07 +0000

    character same repeat among quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def add_numbers(a, b):
+        return a + b
