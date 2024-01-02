aa998d2f938adc0771751ce8ce0a2f996ab1a0ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:28:23 +0000

    although gap storm turnback movement

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import datetime
-print(datetime.datetime.now())
+for i in range(10): print(i)
+  def convert_to_binary(n):
+        return bin(n)
