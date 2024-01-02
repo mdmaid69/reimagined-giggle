c29d8bf25d054c4dd89f691af4d1527a01906f98
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:05:12 +0000

    undo considering anyone strange distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def square_number(x):
-        return x**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
