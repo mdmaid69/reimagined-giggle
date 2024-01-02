dfebaecb43f5b8281e899d8fe850ede65d67055b
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:05:39 +0000

    popular seriously goodbye deliberately bet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
