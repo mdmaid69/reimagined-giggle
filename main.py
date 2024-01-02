14d438a0141a16057f8b2b803355787f88ec1d2d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:42:25 +0000

    treat spin plate limit refer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
