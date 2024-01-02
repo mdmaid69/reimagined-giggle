96b040e09cfecf6cc99e3de375a06ff94232a68e
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:22:23 +0000

    not each actually stir will

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import datetime
 def get_current_datetime():
         return datetime.datetime.now()
