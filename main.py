417247c154ee72ae22f228f393c28bf45118b50c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:56:09 +0000

    moral godown desperate consider shallow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
