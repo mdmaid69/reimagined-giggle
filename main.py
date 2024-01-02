148e7572aa27de925b1dac2334c9b96db1d5e0b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:39:04 +0000

    limit increase use able skirt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def reverse_list(lst):
-        return lst[::-1]
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
