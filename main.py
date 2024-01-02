e278a3efd2e2dee8b711a52174beb9c848d6390e
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:23:44 +0000

    produce past thing message ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
