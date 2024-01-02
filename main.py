7ebe8892a1c5c309ee8eee6b466e1600f9e3442c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:54:06 +0000

    much badly 567enemy phone row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
