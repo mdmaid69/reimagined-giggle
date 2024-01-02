302f3a6f8f47b47e2991cc1da3a5f5d1c236e265
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:07:22 +0000

    example understand same condition well

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
