9ad6498a0b231449ec7953f95e312768274fa8b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:28:06 +0000

    fond shoulder whatever ago couple

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+print([x**2 for x in range(10)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
