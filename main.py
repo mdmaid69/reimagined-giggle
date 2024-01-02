8cd101785f5d0f4566ff47891856adcceec07787
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:07:48 +0000

    benefit train hungry send I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
