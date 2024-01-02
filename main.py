e46a77c7630b1f034883cab2da517dedce8a59d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:30:42 +0000

    claim prize in Ii judge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
