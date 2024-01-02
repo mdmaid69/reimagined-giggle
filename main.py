447f1fcae99f2c1348fb4b52b22ebee88a90b897
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:12:42 +0000

    edge pump character nigga date

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_current_directory():
+        return os.getcwd()
