f8a9b8b5e44b3268069ea0504428715818cafb43
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:22:46 +0000

    ground arrange Zz occasional classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
