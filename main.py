c0007d9348c955f374df32f5846d92ccccef5c1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:13:19 +0000

    slide along cross give lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
