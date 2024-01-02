071fd807530fb27b99a6933277115363a21216da
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:49:52 +0000

    unkind or study watch exit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def cube_number(x):
+        return x**3
+  def add_numbers(x, y):
+        return x + y
