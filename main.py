34ecf37cce6368ccc6b67048752b38823ec6c880
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:44:05 +0000

    ride relationship takeout far front

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def count_elements(lst):
+        return len(lst)
