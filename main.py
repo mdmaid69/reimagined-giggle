fb7a93258ecb54d301ecd5ba0b69faea52960e2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:46:03 +0000

    appearance repeat through already smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
