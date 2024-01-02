51a8f2356ada4540733621a85fe75ef87b98880b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:02:58 +0000

    concentration stupid considering forgive uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+print(sum(range(10)))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
