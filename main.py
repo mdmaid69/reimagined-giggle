94ed4315382e2ba47094003e95e8830e01ad0780
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:05:32 +0000

    expression yours strong like off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
