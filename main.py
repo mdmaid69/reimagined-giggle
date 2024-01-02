6b74d1361cf00c60113f5deefb5043b93c0d15d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:16:13 +0000

    thumb Ll round born precise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
