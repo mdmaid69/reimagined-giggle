4a8ee4189e575d130b119d52a9303c9fc80922fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:39:51 +0000

    purpose concentration care fresh lookafter

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_power(work, time):
-        return work / time
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def subtract_numbers(x, y):
+        return x - y
