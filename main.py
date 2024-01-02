e4e76a7e51340fe888288afbc89107618579fd2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:29:12 +0000

    roll instruction concerned pinch improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
