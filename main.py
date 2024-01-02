e48429be63d97b1d676bca30f34cc09a1cda9923
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:49:59 +0000

    cheap error disagree fight sensitive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
