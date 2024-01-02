ac5b0c4dbad5177dfab5c47022e2622146bbe995
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:17:56 +0000

    bush risk depend challenge exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import tensorflow as tf
-print(tf.__version__)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
