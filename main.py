4a84ef14ba8eddba72fc1b741f24d94823de3da0
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:06:05 +0000

    disapprove catch disgusting number you

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
