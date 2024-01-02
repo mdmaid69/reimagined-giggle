c83cbf7da5975f764299434151cbc48b6af90a31
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:44:12 +0000

    fat turninto history children occasional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
