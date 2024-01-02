231a73381893bb107c5fadedcfb53b5959050b38
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:53:52 +0000

    art course confidence description include

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
