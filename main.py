ac36f2276fee6c879fe3303a1777d86e43dc72f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:50:58 +0000

    pause approval refer go pick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
