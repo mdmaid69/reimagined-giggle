6cb0701231df6eaff490d1893e142c0fa7f59681
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:17:23 +0000

    point also flight life thinkof

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_work(force, distance):
+        return force * distance
