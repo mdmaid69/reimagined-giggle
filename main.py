ac677f17469250788235edcceb8c9990d2c4f704
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:59:12 +0000

    news near around Rr sad

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_as_str(array):
+        return str(array)
+name = "Python"
+print("Hello,", name)
