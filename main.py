c980a32b18adfebd170053a08ca824f58bd2d886
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:39 +0000

    late perform time many long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
