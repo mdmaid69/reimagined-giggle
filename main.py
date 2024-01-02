db09995dda6b8aef698da16b537a298d70b9cf39
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:56:38 +0000

    begin spoil explain thick far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
