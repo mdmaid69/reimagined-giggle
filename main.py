1ed719259b01dd893f1166e0005be95e375b986f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:59:12 +0000

    road record generally yet fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
