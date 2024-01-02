5631623a94815b62c0b8914152ea0edafebc4d81
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:22:20 +0000

    play bridge callout suffer previous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
