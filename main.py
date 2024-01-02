901bd90f389e698ea3ecae8ebb254c07f50bb0dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:26 +0000

    petrol correct respect below tread

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print(sum(range(10)))
-def is_odd(n):
-        return n % 2 != 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
