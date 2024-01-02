dc2c108e5780707429643ea074e1c88547b05d76
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:34:41 +0000

    coat manage trust interrupt block

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def reverse_string(s):
-        return s[::-1]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
