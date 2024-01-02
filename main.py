b84db7f82ab0de38f026984858fb2ed4c6ca08f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:51:31 +0000

    character frame scheme wonder total

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
