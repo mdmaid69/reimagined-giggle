dd85fbc6f09e59d970b69fb1c8e0202af3cc9433
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:37:43 +0000

    bother trip pay cool container

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import random
+def roll_die():
+        return random.randint(1, 6)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
