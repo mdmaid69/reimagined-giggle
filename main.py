f040cc4389d2b4792c51cf7479d30171fa3c5e1f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:35:53 +0000

    attempt meal factory summer swimming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
