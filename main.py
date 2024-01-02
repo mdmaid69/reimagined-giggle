d883e753042df140423396583f0da331e2c24031
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:12:51 +0000

    attractive acceptable means ticket press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
