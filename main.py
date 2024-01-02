70aaffa60c3493db6dbd2f2525d444d819d33ba1
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:03:17 +0000

    temper field service dare nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import json
+def convert_to_json(data):
+        return json.dumps(data)
