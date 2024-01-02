697aee88fdf31cec0d5b07687771a4223d764f07
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:20:31 +0000

    limit criticize accident after takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_list_from_array(array):
+        return array.tolist()
