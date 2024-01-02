bec1fee5c693de2da5fb51d1b813decf28b4d68e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:20:47 +0000

    dream side comment prefer determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
