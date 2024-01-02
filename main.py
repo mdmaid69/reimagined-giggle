63ebefebdd125abaac401b2cdbb37aab7f55f1ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:34:13 +0000

    hardly accidentally control turnaway performance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def subtract_numbers(x, y):
-        return x - y
+def is_palindrome(s):
+        return s == s[::-1]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
