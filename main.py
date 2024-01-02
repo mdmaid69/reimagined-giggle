a2f27c157a089c8bf935036fb64bd34737190f41
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:58:36 +0000

    draw lookup stick tap squash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
