5608198f3af52ba6cd8d5a21f87881081ecbbf39
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:21:27 +0000

    direction though announce safety my

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def add_numbers(x, y):
+        return x + y
