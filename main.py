f259ba12fc3787ceca604a8520a255676cdcecb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:59:54 +0000

    rest generous environment talkinto change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
