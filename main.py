8cc14ccd35261ee7544568b93c47672550d42f80
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:22:27 +0000

    lock upset sort abroad string

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
