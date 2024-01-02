6d253326126a2d95f6a26a5ba994c684b67e729a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:40:19 +0000

    odd freedom advice yourself Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_list(array):
+        return list(array)
