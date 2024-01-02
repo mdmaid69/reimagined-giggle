023483a8e10f5c7575ee5fc8d3442699186c9186
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:11:07 +0000

    smell fit grand someone relax

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
