2d41856ece3f048daa0db7dbdbfa137f005e301f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:43:41 +0000

    style fold war some advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
