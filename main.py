33c161ac2fbacf72a202c21d751e9b92c62cb2c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:40 +0000

    turnback bored raw report what

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-def greet(name):
-        print(f"Hello, {name}!")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
