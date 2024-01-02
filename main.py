ba6e7511aaefd65f3ca48c83b6d42563725da91c
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:49:44 +0000

    buy middle move disturbing popular

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
