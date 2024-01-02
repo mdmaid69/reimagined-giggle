4783d117255d227c11e06de827e7fd187012048e
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:15:01 +0000

    private effort leave fork taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_force(mass, acceleration):
+        return mass * acceleration
