eca68a2629e3fdeda9dfc635a82651920b6ee065
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:04:37 +0000

    mail miss mess talkinto whatever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
