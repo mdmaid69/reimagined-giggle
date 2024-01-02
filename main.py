dba4ffbd063c62c27b14a527972449a5d042c3ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:27:24 +0000

    keen sport touch change minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sys
-print(sys.version)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
