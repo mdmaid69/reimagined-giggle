f7e5727a77dfef812ba4bfa98fb8a1e6e2b64d3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:26:12 +0000

    situation failure work degree popular

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
