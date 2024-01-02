ffa8ff0920c84a6691c8efe4b21e1c8cfaa5c3df
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:56:27 +0000

    either boil attempt favourite no

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(10): print(i)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import random
+print(random.randint(0, 100))
