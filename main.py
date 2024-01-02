f83abfda9697850f29976308b184acf3bb13be56
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:33:12 +0000

    official criticize partner introduce above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_time_since_epoch():
+        return time.time()
