f113b2b70a03d4cf32b4390ff6fb5efb5163505d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:04:54 +0000

    culture idea wash tip disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
