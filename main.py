a1f7c4387ef7a0e8d25eac703a7334e6bbb4c2f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:55:35 +0000

    black repeat computer recognize deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
