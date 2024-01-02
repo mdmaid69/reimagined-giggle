c961193588a91ae75462fc1796a926261b5fef06
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:12:15 +0000

    571enough split round square that

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_max(lst):
-        return max(lst)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def reverse_list(lst):
+        return lst[::-1]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
