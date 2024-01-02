35747debcaead2a37513e23dcae3e9b9b270fb6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:15:10 +0000

    long undo glass fire deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
