a44a1e9a1d7c177fbc8c38138742f96389dbb70a
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:08:56 +0000

    wall ago join below dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
