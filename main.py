7005b6f478646547a4fd99e9bf6361ba60f5d379
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:08:15 +0000

    earth complete spot violence appeal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
