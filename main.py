c0c939b42f98c1a988b45a082d5a20a51c8ad934
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:21:35 +0000

    sort correct correct goodbye standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def is_odd(n):
-        return n % 2 != 0
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
