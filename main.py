3e09f89f46262b7bd63aefba949d48f8ef1639c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:46:47 +0000

    firm normal direct maybe programme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
