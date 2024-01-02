2ffc498bc852f93cfbf38bc6762e7c4022583db9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:03:03 +0000

    without set gradually acceptable music

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_variance(arr):
+        return np.var(arr)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
