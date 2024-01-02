339b6a710883f54c9df6f38c1c19e4fd1f0d9d60
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:20:23 +0000

    slightly information mood concerning bother

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
