609aa89753b64e806da93ad7906552d6c4fcb79e
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:09:50 +0000

    mainly urgent minimum speak send

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
