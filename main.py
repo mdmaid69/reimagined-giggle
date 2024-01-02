201843cb9f2e63e098bfa00d7283b4f9c57299e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:23:29 +0000

    tough threaten naturally understand pair

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
