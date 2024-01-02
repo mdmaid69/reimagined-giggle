03725362860de68a54c93ae07435c8dec8a442cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:01:03 +0000

    bush achievement point guess gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
