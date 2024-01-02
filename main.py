6b10fd109b210e5fbb5b5bca84fa63a04b3b8b10
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:32:39 +0000

    flow journey house total dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
