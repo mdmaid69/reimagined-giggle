6551669a10266893d27421c7465c5d939d293582
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:39 +0000

    unite section heart Cc chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
