e1fb4e6931da0ea66be75ed11ec8faee926c29d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:28:36 +0000

    happy high lay burst accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_even(n):
-        return n % 2 == 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
