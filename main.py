afe9510b7115d4bf0ca93691b5dc4cdc0fdbca1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:17:10 +0000

    hear official past beginning commit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
