599047a1857f87b65a0e0b8237086589d10ab712
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:50 +0000

    crime message feel interruption maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
