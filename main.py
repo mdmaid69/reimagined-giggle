ddee4620fd3a43fa12cd498b50cd629368f86df3
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:27:56 +0000

    sport introduction shrink passenger something

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
