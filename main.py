bd94be6b7231188e43f1d81de447fc04f3f429b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:43:16 +0000

    cheap computer start press to

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
