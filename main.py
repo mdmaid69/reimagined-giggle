f8e197b4787cacbe1f4ddbfdf2471fc98127071f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:24:52 +0000

    recognize claim excellent free runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
