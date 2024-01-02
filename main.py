36500dc2a2e3c23490f12b766c96d65e67a539d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:23:27 +0000

    concentrate patience win warm better

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def is_even(n):
+        return n % 2 == 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
