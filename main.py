f1ecdcdb8f1fd4216b8498c87c815c5579f8e39d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:16:09 +0000

    trial this owe hole shut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def is_even(n):
+        return n % 2 == 0
