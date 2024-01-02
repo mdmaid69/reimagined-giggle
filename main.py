a53f79c83e6402468e6529fcb0b0acb26680ae3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:00:39 +0000

    wing wheel chief shut children

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def multiply_numbers(x, y):
+        return x * y
