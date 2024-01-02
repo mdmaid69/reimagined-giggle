fd6cad090ef6176e64b54577e1341321ddac2032
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:38:00 +0000

    direct separate alive sensitive school

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def divide_numbers(x, y):
-        return x / y
+def convert_array_to_string(array):
+        return array.tostring()
