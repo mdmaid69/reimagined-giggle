f773d9ebd16fce5b9d88acd12bb6e5db7b086210
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:37:20 +0000

    mean fat wander finger scene

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
