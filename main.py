422704c240cfaa5816a659dc0fb901c875d76e8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:51:31 +0000

    tap reject available lonely cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
