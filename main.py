86c174cd635890b7ada3381a2d1dd6eeaea22f67
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:32:49 +0000

    search threaten set blind communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
