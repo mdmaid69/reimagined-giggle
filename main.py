f4047cda35faddd07ed338ee05948eb00d581393
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:25:06 +0000

    famous smooth connected only value

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
