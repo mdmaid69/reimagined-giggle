4ef84e475e6858fa8ee7faa440395c812d70d2ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:44:30 +0000

    rob need niggaacademy shrink wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
