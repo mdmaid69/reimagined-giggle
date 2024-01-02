24de09dd478762d3350b69f2a1d8e43ea6e4a818
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:24 +0000

    house contribute simple race family

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
