9ca30aad6ac02f0de17ccfa4b1dc5c2eb3a03f97
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:04:34 +0000

    side off keen tense summer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
