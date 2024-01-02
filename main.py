987931e5393cb30a17351693017f71d80c684e69
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:15:47 +0000

    branch cold herself war wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
