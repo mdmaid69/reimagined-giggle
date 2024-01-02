4741d2454f3329a04abb1d79ec04b51d295d5e57
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:29:18 +0000

    drive far mend sink closed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
