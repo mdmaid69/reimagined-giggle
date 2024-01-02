733e47946adb30d8bb0187ae37c7ff8dc5b7e758
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:27:06 +0000

    time fit dress makeinto brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
