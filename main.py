3db47f54ed3afd87e4f8f78a684e7d5afc152ddb
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:29:09 +0000

    method sore disagree tool mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def greet(name):
+        print(f"Hello, {name}!")
