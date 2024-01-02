cee787807b2cc62c16fcc63afd9bcc79a1c2b92a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:17:59 +0000

    gradually offer most principle gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
