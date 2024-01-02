7fcf870b9937a3da0f3bec9e7bd3b69278646fc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:27:49 +0000

    payment cope threaten period giveout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
