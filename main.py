7bf3189f7bb31a35c09e0060ecb637956121bef5
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:13:38 +0000

    country travel roll somewhere fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def multiply_numbers(x, y):
+        return x * y
