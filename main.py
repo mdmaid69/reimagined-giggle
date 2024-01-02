dbc574932b48ff5e86aa8d77e5d8ea337f564146
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:36:45 +0000

    already noone forgive hole ourselves

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def find_min(lst):
-        return min(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
