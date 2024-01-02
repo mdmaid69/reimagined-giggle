b20b69bcb568a28666c690cf8c09231ecb354337
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:52:16 +0000

    afraid duty season rain worried

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
