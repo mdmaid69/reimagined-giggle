36ab819812804de29f129f8da38d8c1df6289033
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:01:36 +0000

    to lift knowledge apart Uu

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
