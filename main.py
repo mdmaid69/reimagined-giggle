677608729044965ce6aaa9f128f9530eea08de69
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:08:11 +0000

    new dream tidy back month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def add_numbers(a, b):
+        return a + b
