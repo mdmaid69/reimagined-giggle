28509d3a39166017b9b37b9f47ae9f18fd616a48
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:28:58 +0000

    cook moral must worry blame

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def add_numbers(a, b):
+        return a + b
