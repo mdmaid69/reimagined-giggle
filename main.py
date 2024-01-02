9f10db04cd4990c17d03fa845d36f9d44adb553b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:47 +0000

    wind sit tax born bang

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
