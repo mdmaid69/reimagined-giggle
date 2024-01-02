a21c98466a636d6078bd607c820a7645e2d0d085
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:36:29 +0000

    distance traffic production mistake lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
