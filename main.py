ffb3ad47f899abee835ca2936579b593e7bf61a6
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:46:54 +0000

    across towards underneath lot left

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import collections
+def create_queue():
+        return collections.deque()
