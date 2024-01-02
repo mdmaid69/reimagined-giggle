d7ea63e1aeb5a076757c33d7534004199afafaee
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:51:13 +0000

    precise can wheel it grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
