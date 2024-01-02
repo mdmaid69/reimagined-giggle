0ca242f9ec8aba087071fdfe3a953c6a7677c694
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:24:22 +0000

    careless splash motion pretty shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
