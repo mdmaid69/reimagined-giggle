38a10c629367f354362da1cce49e4177f01743a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:32:40 +0000

    unpleasant clean after happy much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def convert_to_octal(n):
+        return oct(n)
