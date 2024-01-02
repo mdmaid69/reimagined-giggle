0daffc98ef0ba6e37784ab6734f4bd917a2c6e86
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:10:14 +0000

    acceptable section preparation freedom reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
