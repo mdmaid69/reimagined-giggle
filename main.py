9d16867bdb8bbe230ff0d7676de6287862a6ad84
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:02:31 +0000

    die distance shopping violence collection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
