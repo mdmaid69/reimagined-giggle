c2b2eb545dd7a75de873813a407b9a690cc67769
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:28:04 +0000

    hate mark country dead interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_hex(n):
+        return hex(n)
