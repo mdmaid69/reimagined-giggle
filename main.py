03e1c16d47250f6646c42d957c4f247a812dea99
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:06:31 +0000

    except alone guess negative makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
