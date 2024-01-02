26f8ed74b479cd9c0dedc0c7c27300a5a3384b58
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:10:12 +0000

    best stop love take line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
