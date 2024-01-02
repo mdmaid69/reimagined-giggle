75fb2e68092152a52c2ffba74e494131d9322dce
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:06:22 +0000

    pupil light party steal size

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
