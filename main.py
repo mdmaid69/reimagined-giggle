995aaec3522a364cca9bece40316b04c579c969a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:24:37 +0000

    obtain fat file quantity slice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
