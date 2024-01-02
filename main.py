dff6f7c9772010fabff997dc22d7b73e62b3c8bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:10:36 +0000

    angry bang influence wheel acceptable

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
