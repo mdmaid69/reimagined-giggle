a081195c818ed9377d261ed40c5e84f3e9b21546
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:45:16 +0000

    before bank far used way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
