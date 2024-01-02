ec0509c05625a90e16d26c0c99dbbdc406d5e17b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:25:57 +0000

    geton square mind comein yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
