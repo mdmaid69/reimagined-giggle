cb875db4d6c5e924e86d553c75c47764f5b66d23
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:32:19 +0000

    meal familiar handle afterwards exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
