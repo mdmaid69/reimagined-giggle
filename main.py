2b125e2137a93e92bf219b16480bc8e438d66596
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:32:18 +0000

    peaceful standard tip hurry environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
