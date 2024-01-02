f608ec9fad1006ddb33008869bcaec2b6d417020
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:44:17 +0000

    crazy shock half 571enough fly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
