866354337e8d71203bd7f1ceb799d62bcb3b50da
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:47:05 +0000

    aim skill collection light circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
