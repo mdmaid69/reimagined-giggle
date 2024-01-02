97fc7f4ffb21a6643323bf11d7e15bc9f3807361
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:18:02 +0000

    blue immediately tense no beginning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_acceleration(speed, time):
+        return speed / time
