462e6d0178d4444fa3a5bf1dd8a094b0f8aa243a
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:03:02 +0000

    free rubbish fire will pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def square_number(x):
+        return x**2
