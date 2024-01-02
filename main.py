1c8474f201cef36264db81910a96af8ac8106974
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:50:18 +0000

    tooth care confusing study start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
