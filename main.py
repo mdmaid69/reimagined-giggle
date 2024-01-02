299e9c8897836001ba5a17b7ecad4618e6ba4ea7
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:36:20 +0000

    worth kick hospital together indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
