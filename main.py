d69b1ad2400558be992d9271ff5d6570884327ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:23:30 +0000

    safety turnover poison flight lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
