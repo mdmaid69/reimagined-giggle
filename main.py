308b395b4e84b50b8174f5b78e90f8a7c107ca3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:16:04 +0000

    why odd lookaround happy government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_list(lst):
-        return lst[::-1]
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_as_list(array):
+        return list(array)
