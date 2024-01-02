5c7b6cc19203583c6628d92c2c11f5b6aca9e238
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:46:48 +0000

    callout plenty small close at

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def find_max(numbers):
+        return max(numbers)
