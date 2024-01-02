136115951699a5d7af429252e038c174b72f2a5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:21:07 +0000

    loan polite Pp push respect

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
+text = "Hello, world!"
+print("Characters:", len(text))
