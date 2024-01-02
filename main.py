9192121acd5bf942dcb8bcbab1e81c09a324f025
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:28:09 +0000

    squash stop charge comfortable charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
