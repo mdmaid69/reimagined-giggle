25a2665dff627ef18196a0c6c80c2144873ac714
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:23:04 +0000

    wave method low handle play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def subtract_numbers(x, y):
+        return x - y
