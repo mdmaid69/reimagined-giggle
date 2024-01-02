6390136004df998065cbacfaf93012cf9242cc67
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:25:52 +0000

    slide step interfere muscle stretch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
