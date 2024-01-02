35c407a5777c069d739d7ff2933a6d39ea92aa0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:54:33 +0000

    inside burst theirs bend length

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
