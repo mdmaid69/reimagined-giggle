ca689ae38467994cc6ccc3f1a80db4ef3e9ce899
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:15:16 +0000

    rent contribute top test mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
