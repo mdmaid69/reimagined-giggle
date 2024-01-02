295f082f1d2dbace429fc4cd9a6813ff0309c6fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:00:12 +0000

    corner earn crowded include book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
