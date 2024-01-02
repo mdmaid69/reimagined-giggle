b82f0fcf3c9b459d69e426e36858bd1174d9bec9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:52:42 +0000

    error compete human you believe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
