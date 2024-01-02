c96c0685a68b31b179dfad0e6e98b831138430f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:53:59 +0000

    leg giveaway rank comefrom ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def convert_to_octal(n):
+        return oct(n)
