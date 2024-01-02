4634c5a37b0381358c1764cb0139ead104d28adf
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:28:12 +0000

    path many eat suit score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
