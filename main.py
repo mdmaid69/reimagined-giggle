3a0df6798446bb5a8b29259db0c0aa1a0c8551d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:53:46 +0000

    move treat contact match fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
