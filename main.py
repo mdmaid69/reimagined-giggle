a3840dc5261e2f36e295f25902ca838106f8a4cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:59:09 +0000

    confused squeeze laugh fun ill

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def split_path(path):
+        return os.path.split(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
