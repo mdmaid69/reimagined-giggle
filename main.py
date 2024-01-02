c7753cd87beb09ed1160aaeb32d562d5a0a780ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:47:46 +0000

    best fashion respect which black

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import numpy as np
+print(np.array([1, 2, 3]))
