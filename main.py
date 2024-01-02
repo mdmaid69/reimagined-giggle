a5ca3b29495263ef9efd59aecd1a96a103c03bbb
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:44:14 +0000

    mainly protection read offer film

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def square_number(x):
+        return x**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
