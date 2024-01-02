997c9e7a5e976952e55c1ca6830ffb963ea00749
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:26 +0000

    keen laugh sound friendly upset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import time
-print(time.time())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
