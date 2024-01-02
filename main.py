14254f11841d0db9aff7b01e4b93d614ab734316
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:55:25 +0000

    agreement union blow shut state

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def square_number(x):
-        return x**2
-  def sort_list(lst):
-        return sorted(lst)
+for i in range(10): print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
