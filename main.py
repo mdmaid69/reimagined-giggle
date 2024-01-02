d965fcdd94247a0dd3d7b7eef41dc71a5798b7db
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:03:41 +0000

    limit play class message increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
