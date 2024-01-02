1e34b067250f33a837f0d71d78d3ed85567cb9c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:21:05 +0000

    thinkthrough fly copy sit plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
