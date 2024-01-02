0ef15b6148ebeb0c6e04a5e9a2db3b5e234aac16
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:24:18 +0000

    sign bush pretty search learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(numbers):
-        return min(numbers)
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
