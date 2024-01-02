e951835e808c392b2d94d133c041a5379dd72494
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:01:19 +0000

    tradition happen relief date characteristic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
