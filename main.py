2a2ba59e44fa499796774db48ed1c0cbed136baa
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:52:14 +0000

    mark fix rest or warn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import sys
+print(sys.version)
