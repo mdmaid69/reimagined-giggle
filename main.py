d80ae3d8413c830b30811e36a506ea2dfce863d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:15:28 +0000

    ask serve broad immediate have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
