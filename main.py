b89f2200d06bb63c9848b7dd3a71dc237082779c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:04 +0000

    speed guess afternoon remove include

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-def log_message(message):
-        logging.info(message)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
