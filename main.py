cef5d2440afce2a38edc9c8efa794514af946183
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:50:23 +0000

    funny cruel glass refer Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
