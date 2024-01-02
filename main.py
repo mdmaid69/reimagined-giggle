e5471ce017eccd555ee8b157c80dc3a04bf60ba4
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:46:51 +0000

    entrance each large transparent otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
