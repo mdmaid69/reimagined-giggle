4b5314a2efbe8f36608ce51c1b377a57cd2f2d33
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:27:38 +0000

    bunch song giveout noone collection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
