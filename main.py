55933c1dafe9ecc14bc4e3e6a88ae3072c67681a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:31:33 +0000

    active public course rush club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
