5c4d02ea6101673c905b5af232a5a0898b632027
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:05:20 +0000

    drop art 570enjoyable cheque rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
