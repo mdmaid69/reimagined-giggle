276b9f4c37976d64b29422079b8dde7a12d61284
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:32:18 +0000

    need hollow home board speech

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
