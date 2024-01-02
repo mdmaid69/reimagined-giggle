2b2bdb266a4922da40d7d73377917706a3de2950
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:13:52 +0000

    fold onto talkback definite Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
