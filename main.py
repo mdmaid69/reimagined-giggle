2703918d8f939b653bc9744a5a5f215d2ff4d587
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:23:58 +0000

    could second suggest insist suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
