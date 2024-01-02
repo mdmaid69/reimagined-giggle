465c7786ed516923bb884cb386967a22f7551471
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:18:37 +0000

    shake takeover tip its sudden

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
