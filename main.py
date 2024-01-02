2bf87b55ca9464de467c4231f082dd3598155526
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:52:51 +0000

    towards spring violence clear declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_item(array, i):
+        return array[i]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
