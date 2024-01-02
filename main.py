a8c75e1d9ac00e2165018010f1e94401f03430db
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:11:14 +0000

    centimetre challenge better turn connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
