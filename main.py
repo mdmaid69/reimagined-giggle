5886d655d51f14fcfd77c648ea781a93ebc0dd79
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:35:02 +0000

    music left next afford fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import time
-def get_current_time():
-        return time.ctime()
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
