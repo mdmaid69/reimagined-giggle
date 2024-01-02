159d8e93941880062617f2bd8f33307310342ab7
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:23:59 +0000

    seem switch head comment pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
