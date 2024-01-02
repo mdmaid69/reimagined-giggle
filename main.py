7dc9e34406ed8ab24684f77fd76bec202af322d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:44:48 +0000

    performance achievement deliberately cut rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
