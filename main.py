9639e32aecdc14d50acb595f91f64e6f1918d990
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:35:13 +0000

    brain shock talk talkoutof pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
