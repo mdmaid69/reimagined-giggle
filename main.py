efd8701a1080c4f33a10d62e438dcd348e8b35a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:01:09 +0000

    concerning order rain tight too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def sort_list(lst):
+        return sorted(lst)
+import sys
+def exit_program():
+        sys.exit()
