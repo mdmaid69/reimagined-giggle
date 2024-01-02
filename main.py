d08dd26dadee8bd9907e0d7a74c858c6b4141a37
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:46:35 +0000

    grand such him swing Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
