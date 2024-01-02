b90109204d0ea71e393d0dd2c1ca733b9c8f8931
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:39:18 +0000

    complain public share again even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
