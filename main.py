9962ef61d6a047ae993186f9b8e037f7be577d71
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:34:06 +0000

    throughout hit fetch hurry with

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
