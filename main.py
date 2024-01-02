c11a0c8fe0b09a11adf726b4bf64f4775c4589f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:09:33 +0000

    shut Dd after question join

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
