6f2a3160d7c89c14859e8fbb1271271d3dc87de6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:39:29 +0000

    business brush spill fire give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
