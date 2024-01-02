aa8216e978738e37f55ef9683e3ff239261603db
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:25:42 +0000

    university practise divide comment boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-for i in range(5):
-        print(i)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
