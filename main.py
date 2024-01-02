88648c0c705a6b1927dcd0decb24df6175de4548
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:41:22 +0000

    decide positive war Mm such

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
