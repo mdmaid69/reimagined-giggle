09ecf0228c9983f3bb0cb7db688a8696184ab2de
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:57:58 +0000

    suffer safe some relax juice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
