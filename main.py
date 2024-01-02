4e0d0397ef07a5b26757b639b71c134e5f04e449
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:39:13 +0000

    message interview already introduction hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
