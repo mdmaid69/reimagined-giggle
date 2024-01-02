4dcadd940afde7aeefbfb7f04aaf9b64718675d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:57:53 +0000

    offence rough worry convenient pencil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def add_numbers(a, b):
-        return a + b
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def square_number(x):
+        return x**2
