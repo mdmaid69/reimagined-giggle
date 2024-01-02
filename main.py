a79b88e416393fe6693174a4ab6fd9be45069acb
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:07:59 +0000

    inside spring pump care minor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def reverse_list(lst):
-        return lst[::-1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
