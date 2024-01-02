23d86477fe68b4740100d1f5447bff780cee5435
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:01:57 +0000

    bringup transport law intelligent affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
