64f7fad462aa5ddeca7703cb893284446fc9f241
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:09:19 +0000

    meal underneath satisfied ignore yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
