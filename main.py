58e695db230037b1775404464d046784cc7a7159
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:10:22 +0000

    treat performance circumstance chin book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
