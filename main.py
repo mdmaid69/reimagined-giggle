9f97502225a76bdc3f06cd0c3b9ab1c07eba399d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:36 +0000

    record roof worse drive cross

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
