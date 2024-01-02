192c3b38e13d57cc307e87c3588ae6e7f61492da
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:14:22 +0000

    wear spring present trust goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
