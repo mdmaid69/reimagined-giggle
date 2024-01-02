5ebfc3a8d192d874df203198be61dbb44843034d
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:47:12 +0000

    exchange crash book split thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_item_count(array, item):
+        return array.count(item)
+def is_palindrome(s):
+        return s == s[::-1]
