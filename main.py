a3acabab182744a5f5f34126d7d49204100fc57a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:38:11 +0000

    sure forever concerned getup tap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_directory():
+        return os.getcwd()
