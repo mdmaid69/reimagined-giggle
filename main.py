252e6f1cb23b8d8e4f8ed06a3e83cbf306249642
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:21:22 +0000

    bunch please border judge blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_stack():
-        return collections.deque()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
