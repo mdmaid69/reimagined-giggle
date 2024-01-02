947a590a347d4a9bb733e0f098569a8d766e5800
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:24:40 +0000

    himself frame gas introduce continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
