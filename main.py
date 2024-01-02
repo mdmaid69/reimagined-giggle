12b6438be5ae8cedf7c2d72170d60d29179c1888
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:33:37 +0000

    rid border Ff rid spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
