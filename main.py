5e5f9613da585c47fd0a648e57a4fd5e485049f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:29:50 +0000

    shame finally hurt influence world

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
