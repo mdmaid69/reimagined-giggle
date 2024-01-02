e60d0c06dc522225fa4f4452a87f4f1dd9d390f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:20:21 +0000

    face trouble go reduction permission

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
