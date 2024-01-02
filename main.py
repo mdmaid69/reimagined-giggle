e3db9bea5ed35ec9cf5b7d8890dd5ebb2b856bd6
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:42:11 +0000

    object kick some Jj earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
