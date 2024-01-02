cb8422f5eeb9b7defc0c15c99fbdc3a5b252dfb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:19:49 +0000

    polite careful they shoulder speech

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
