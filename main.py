bd3851bc97843230c331e4040a25db2ccc3eff5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:33:47 +0000

    goaway worth matter plenty organization

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_acceleration(speed, time):
-        return speed / time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
