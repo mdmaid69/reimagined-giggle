5e689fc278337bb9f0a36d860a3df9639f82079d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:51:17 +0000

    bang official blame carry official

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
