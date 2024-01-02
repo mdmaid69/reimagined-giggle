03a66a27ac7cb605512f612866b80b1e64cc4f0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:30:03 +0000

    to recover fasten relationship sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+for i in range(10): print(i)
