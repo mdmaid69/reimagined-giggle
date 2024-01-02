6f92979a0732b77d5820c64b9365481b05eaa9b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:06:16 +0000

    bath use early morning meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
