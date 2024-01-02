d7e93c970e9e047ffa7fac00089527996e9b00a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:26:26 +0000

    favourite trick workout address problem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import time
-def get_current_time():
-        return time.ctime()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
