3b64dc7be8150d77c66da3d064e684c77695986d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:55:10 +0000

    fix reserve contain dead Hh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
