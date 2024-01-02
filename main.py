3caf276ff343a26e13a3d3c8caa9a7c30be63f5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:18:49 +0000

    voice still bring behind sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
