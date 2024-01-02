41b304efe27b25619c0fe3fc998a1498f29a6e39
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:40:41 +0000

    recently table form turnup affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_work(force, distance):
+        return force * distance
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
