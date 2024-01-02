62f741b05849eb6aa83cbd5d841a84b0dc8ca346
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:26:14 +0000

    touch wish along announce board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
