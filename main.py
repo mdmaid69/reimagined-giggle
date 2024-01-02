709c8740ced272d781dcd7d3881b5ca2f4794792
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:11:33 +0000

    little minor scheme fortunate broad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
