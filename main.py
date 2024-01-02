d1686afb1738b192cd5f7c02dc8c67ffdfa2233f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:15:53 +0000

    stamp confused future ought good

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
