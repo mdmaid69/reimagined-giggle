9dc5da6cc11838cb35e9f9b641a62dcb99a5852c
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:58:22 +0000

    big tape OKorokay early apply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
