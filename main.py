731516438d581ff6f65121c65dbbcbd2ebe1a27e
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:08:14 +0000

    getaway switch blame brain environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import sys
-def exit_program():
-        sys.exit()
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
