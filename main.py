5374d05a8a9b69b836c431976ddd7fc7a7234031
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:15:40 +0000

    class play square comedown punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
