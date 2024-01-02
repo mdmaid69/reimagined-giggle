6036345e8405e9441262894e556cb912565e2b25
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:31:45 +0000

    contain from hospital Mm crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Powers of 2:", [2**x for x in range(n)])
