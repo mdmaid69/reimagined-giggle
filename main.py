a4b322088fc3edc4039643ddea9abeef5df38163
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:56:02 +0000

    decision reserve sound present on

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_density(mass, volume):
+        return mass / volume
