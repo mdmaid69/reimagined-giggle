513084bbc8a4f957395a631614e4426560ee5b7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:39:50 +0000

    runaway join variety emotion terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
