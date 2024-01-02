f44952a50782160e8adaf1c8dd12af07598e2dc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:25:43 +0000

    reason rent unite keep danger

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
