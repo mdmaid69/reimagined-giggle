ae34eb76ad7ea47d2837408362a4ae2823d2afe4
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:55:25 +0000

    high experiment save along say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
