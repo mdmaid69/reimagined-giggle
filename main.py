5cf9153f818d6364128b43b265bc401f3a8dfdcf
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:35:05 +0000

    activity get congratulations body underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
