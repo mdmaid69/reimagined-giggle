05790fc29652fa811cb45ad1fb36d85866f617af
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:28:11 +0000

    calmly soak platform believe roof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+  def cube_number(x):
+        return x**3
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
