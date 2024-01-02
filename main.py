d53fbeb2a322402049f2499c752a5d763468a3f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:03:42 +0000

    method please wherever frequent sell

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
