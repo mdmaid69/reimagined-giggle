59ff29dfd8dd335b21bc3e0756d11e51a83b28ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:30:51 +0000

    slightly badly translate date head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def reverse_list(lst):
-        return lst[::-1]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
