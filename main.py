cce015ba8bf0b956075538bcb9e335a9f6852f90
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:34 +0000

    hand general force ring corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
