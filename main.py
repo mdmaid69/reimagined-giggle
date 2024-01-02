afc81eadc1be9fecde414c1a400581764e95ed9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:11:20 +0000

    move remember of feed school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
