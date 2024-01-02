64e689143a73c18800774cee0023d8330cb235a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:37:46 +0000

    freeze pride phone company alone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
