2c51abd0257b474867796afb9a5fb0971be47625
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:42:35 +0000

    familiar admire strict fold judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
