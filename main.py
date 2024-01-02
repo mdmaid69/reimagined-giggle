bf853414fc5ef9e6f5c9ccf34006e6f38012c309
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:22:12 +0000

    touch handle through lead criticize

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def convert_to_octal(n):
-        return oct(n)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
