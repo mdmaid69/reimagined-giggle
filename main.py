56ff05e7cc6fbb525ca7a4a10e200c1843f31bb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:39:34 +0000

    likely equipment children himself today

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import getpass
+def get_username():
+        return getpass.getuser()
