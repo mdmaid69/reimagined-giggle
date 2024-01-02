a55aaac8d77e5b00a64c82585f1709783d86c492
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:49:03 +0000

    correction air possible backwards increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def square_number(x):
-        return x**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def roll_die():
+        return random.randint(1, 6)
