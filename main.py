5b568f25a34beb06fe8a2d7be79901bf0f0a61e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:32:22 +0000

    hate whichever grade central block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def square_number(x):
-        return x**2
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
