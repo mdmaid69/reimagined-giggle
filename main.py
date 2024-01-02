fc9fdeb7ab662264b8b32e312393ff056ee749ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:10:06 +0000

    turnover cross trade avoid nothing

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
