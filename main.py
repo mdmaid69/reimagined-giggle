ff3bd77dd927f32a85136c83d91d12d9feed3b60
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:41:38 +0000

    serious pity pollution artificial read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
