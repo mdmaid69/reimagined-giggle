45779f52e4217e54a66efeee838fe4fe5062d814
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:16:42 +0000

    yet win nigga mistake situation

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
