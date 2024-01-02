e4ecc8fc9191b929192f1506a433519ea6c22be6
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:08:13 +0000

    refer turndown that turnoff drawing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-print(random.randint(0, 100))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def find_max(lst):
+        return max(lst)
