7cea845e358ee3bed67e685450ea3c921ef54499
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:25:02 +0000

    forgive street scream impressed profit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
