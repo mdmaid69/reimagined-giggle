877dc42d8255037e2bdc54c2ba42fe9a236503f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:23:57 +0000

    hurry pretend must apart exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_power(work, time):
+        return work / time
