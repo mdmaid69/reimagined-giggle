1bf7168f9380fc2c41a568f74e299285d0af21a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:32:29 +0000

    fight admit divide immediately original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import platform
+def get_os_info():
+        return platform.uname()
