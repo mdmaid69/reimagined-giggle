261a3f57fdfebe7aaa65114c5ab4f92dc4b58ab1
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:39:22 +0000

    lookafter evidence kind swimming cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import tensorflow as tf
+print(tf.__version__)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
