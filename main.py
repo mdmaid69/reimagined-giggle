b1bf85fa3c1e20266c0a2bd0c6e26bf49a45007f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:49:50 +0000

    present this no vote find

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_typecode(array):
+        return array.typecode
