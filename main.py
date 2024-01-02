659a116c9647ef163dffc04a7b16a3b9c158ebc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:50:20 +0000

    support tour frightened clever Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
