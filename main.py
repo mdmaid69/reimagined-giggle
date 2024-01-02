58596ac35e0195e2b75fe1406d6fb3c19aa16cdc
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:36:49 +0000

    because callup expression dress house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def convert_to_binary(n):
+        return bin(n)
