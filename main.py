bf47c8ee835a295cc39721e5aa7ca5d04b652f42
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:58:59 +0000

    depend nerve room loud scream

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
