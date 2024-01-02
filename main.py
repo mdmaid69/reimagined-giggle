403eeeceec0c8c777d126a4ca9dc1ee23caf91dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:31:43 +0000

    contain free mention bury find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
