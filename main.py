94661dbf4469f6f2ec672cf18e289b99cd8c9057
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:14:26 +0000

    teach television frighten promise priority

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
