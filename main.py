2d64200f8f09069fd55359a0e915147f8b40e963
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:44:25 +0000

    obvious giveaway discussion behind side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
