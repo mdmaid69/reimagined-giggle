5526c5f5d67dccf453e221b3a3cfd8a9396a5c67
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:13:59 +0000

    kick other takeoff look dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
