d56c2bdf791dcee574cfda60101b63a194cb0755
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:02:20 +0000

    late hope instrument fall smart

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print("Hello, world!")
