8762d22ea7a92165317fadf1afbed5b8a4151e96
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:14:13 +0000

    take circumstance design spin circle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
