b6b114b935a6f83c1c62a3eeaf4ba33f333d2eb3
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:47:36 +0000

    occasion immediate advanced information wild

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import numpy as np
-print(np.array([1, 2, 3]))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
