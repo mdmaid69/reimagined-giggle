6b77c3febb7daf4b810cfc5ff5dcd1ab90557959
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:01:53 +0000

    wander through science message happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
