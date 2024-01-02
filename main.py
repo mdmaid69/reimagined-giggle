ad0e6b8c1b1f5d1c208cb50bb97eedd121119317
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:55:24 +0000

    person shine control respect finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
