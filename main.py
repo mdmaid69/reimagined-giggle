4b23a071ddda96cc49412440e7d83cdb9bd19247
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:59:01 +0000

    twist pure fetch pen base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
