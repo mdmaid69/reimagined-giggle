fb09b2e42b0981605c954a232cb6d6eaaa6b87a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:41:45 +0000

    music used solve tonight Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
