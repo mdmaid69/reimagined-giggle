75ec2ea65e890709586ff907e1739bd6874876c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:01:25 +0000

    minimum since boil experience reach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
