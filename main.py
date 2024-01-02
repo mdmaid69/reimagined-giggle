ed0758fd605fbad774a25c7ccfcde9fb141c5eba
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:26:28 +0000

    after natural show top concerned

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
