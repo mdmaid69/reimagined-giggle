fcc0d0836f20d3ed1fcadaba4bd6dd37ea4471cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:21 +0000

    bite pronunciation grateful move smell

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+print(sys.version)
+  def remove_duplicates(lst):
+        return list(set(lst))
