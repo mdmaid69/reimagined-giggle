0cce51b43b8f4a12094e5ff10c75b5067a1a0107
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:59:44 +0000

    niggaacademy lose first live talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_speed(distance, time):
+        return distance / time
