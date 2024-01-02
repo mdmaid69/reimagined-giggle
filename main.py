e7f35ebfc1bd08c1a6872f7f7565ee63d8ca90c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:29:52 +0000

    flow label attractive want like

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import sys
+print(sys.version)
+def convert_to_binary(n):
+        return bin(n)
