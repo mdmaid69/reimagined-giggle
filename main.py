f03598b2236e58ef2fb55a0b179fd883642c12d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:23:24 +0000

    wipe long phone keepout house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
