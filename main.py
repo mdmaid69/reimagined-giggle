83392bc6b0496c815423f4c1c703702074f8496c
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:25 +0000

    smart mean left brother soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_index(array, item):
+        return array.index(item)
