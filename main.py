95ac2d16de885e7c319f74f6390f214cc2527676
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:11:00 +0000

    snow temper bunch pick beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
