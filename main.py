e600ad7c81e902ef30d05ce977a94e104ba9f369
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:16:26 +0000

    label nurse bottom giveup time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_ceiling(x):
+        return math.ceil(x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
