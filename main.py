ef8fddc5973c825e5b2350ffbaf165832558b901
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:09:43 +0000

    afterwards produce medium serious cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
