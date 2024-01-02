94f42ffe287bb30f99def443380ff7cae7298fd5
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:39:31 +0000

    forever mother land different business

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def multiply_numbers(x, y):
+        return x * y
