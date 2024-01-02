371c5c21c00a355182211322b86329a351d3c7f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:30:21 +0000

    phone choose pull apologizeapologise build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
