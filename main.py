f9a5298425690afba9e454a28cc91bf420d2bb7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:31:04 +0000

    judge exploration free who effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
