482a481b319c5cb36b4ff5c2a3fec8e1a91359d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:05:57 +0000

    maybe platform match beautiful normal

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
