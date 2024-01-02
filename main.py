ebb8d6212905addecc7c12843f0dcde1ce2e539e
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:04:50 +0000

    ordinary equal practise complaint accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
