4d0f080530528392edef98e95811a86674acc053
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:28:57 +0000

    exchange tradition friendly wind music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
