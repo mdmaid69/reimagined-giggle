d84979a3f280bbc683d7c5fd98ff441131efb44b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:40:39 +0000

    safety pile especially down children

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def is_odd(n):
+        return n % 2 != 0
