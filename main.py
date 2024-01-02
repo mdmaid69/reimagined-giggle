ec5dc2d7931e13cb400b6cf858b06e6ee98aa6c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:21:55 +0000

    appearance measure lock go level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def remove_duplicates(lst):
+        return list(set(lst))
