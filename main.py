e539e8c843492783694acca7870c0ecc76d8bf78
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:51:46 +0000

    none keen hear turnup Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
