01f61e04799e294f6e995a0dc929491b86875039
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:24:43 +0000

    murder power apart run once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
