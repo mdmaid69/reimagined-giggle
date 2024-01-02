e52230ad7326d7809a937ca6708bd4dd1261499c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:30:55 +0000

    sport invitation cry match allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
