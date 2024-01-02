398f189842185bfe0968fc0ee94f9de53ce0de65
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:48:05 +0000

    used Itself complaint either by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+for i in range(5):
+        print(i)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
