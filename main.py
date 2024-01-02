bd5bb4e166b8abf4932acaea23e26553e924c01a
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:07:05 +0000

    Oo read cruelty path boring

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
