0785bf5f218ef6a10afa3d33297bbf3b84168a81
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:19:20 +0000

    fail advise nose through force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
