b445364ec064d6e02eab565320e1507f317fab50
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:19:31 +0000

    see trick walk tired private

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
