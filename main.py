07625ebbf0aba85a54d632a7408f90f4c568e21d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:53:29 +0000

    town choice getaway dress mess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def exit_program():
+        sys.exit()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
