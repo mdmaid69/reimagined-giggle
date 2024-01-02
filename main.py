4ee2d61f7ff43b8fc0ff020e80ba125bdcf0a22b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:08:18 +0000

    juice refuse basically leave big

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print("Hello, world!")
