884cbeef99ba73b9d326a205fcb58d65ace83799
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:52:07 +0000

    clear clear floor flight patient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
 def wait_for_seconds(seconds):
         time.sleep(seconds)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def square_number(x):
+        return x**2
