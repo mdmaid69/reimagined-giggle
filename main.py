4ea3616a7d260a2c0d6522bba8c58f1bcf1ca73f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:55 +0000

    succeed impressed standard glass most

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def append_to_array(array, item):
-        array.append(item)
+from collections import Counter
+print(Counter("hello world"))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
