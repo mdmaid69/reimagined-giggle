da081d3fe39b80f620c3be19a3fb73675c3ecbb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:01:18 +0000

    Ii secret skirt arrive company

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
