77209985f7f88bf2d837e9b4eae9d3ac978f5327
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:34:51 +0000

    back front authority again bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def add_numbers(x, y):
+        return x + y
