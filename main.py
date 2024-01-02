183b1b7b26a821cf85eeeda6187ae610bace4dd2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:04:45 +0000

    kick pleasant moon pity fetch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
