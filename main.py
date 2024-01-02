d38530498eaa1f14cca19c6c6d21d0d565dca7bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:21:45 +0000

    distant hit callat straight ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
