56d202565655fe0b3b44241611ced21314d1e43f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:33:44 +0000

    herself wild nerve address goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
