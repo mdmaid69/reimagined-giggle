1407b4d6e1af7b8b15c238568f9b9b428c7f4eb3
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:27:40 +0000

    regularly breakdown except family pool

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
