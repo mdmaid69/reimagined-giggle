818030973a4c9e155467da5371f1a53dc122cf09
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:04:15 +0000

    hungry mysterious upstairs agree strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
