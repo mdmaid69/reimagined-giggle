ffa2d18ec30a7c49eee32a7a6196e3182a6e2ebf
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:45:48 +0000

    report react content spin 569enjoy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
