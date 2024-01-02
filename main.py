657a197995f9ac7a3104943fb85a43cb0e4686e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:24:05 +0000

    pity disturbing off block here

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
