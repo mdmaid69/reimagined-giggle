624e006e732117ac9469d892b0a0c9347788cd73
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:59:38 +0000

    few ring police cover unlucky

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def reverse_array(array):
+        array.reverse()
