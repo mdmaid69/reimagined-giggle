df9de45149f7254762729425daf64202389a21d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:16 +0000

    out rather exchange run address

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
