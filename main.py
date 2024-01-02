23018c25230d5a10ac6be3274d0fcb67a5d23849
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:44:06 +0000

    wire tone relationship brief regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
