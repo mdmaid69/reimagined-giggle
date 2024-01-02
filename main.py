c2ba8f16c13189ea56ad86a1fba32340effff607
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:17:02 +0000

    combine simple I polite reserve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
