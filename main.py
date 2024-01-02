dc16317dce40a27a558f81530f9c2b932a8418fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:25:45 +0000

    always extra like good experiment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
