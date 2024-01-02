3897f4c675d54abcc3917ba965e89f3102d2624c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:46:50 +0000

    do adjective Nn goaway holdout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
