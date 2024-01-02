2080808e47d35795c9303cdbdc323fa2cc24dc28
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:39:20 +0000

    wear warning calmly piece matter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
