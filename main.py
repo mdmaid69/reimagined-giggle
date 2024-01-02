8e26f12daff44284d3185a15e8a2dd592204819e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:48:20 +0000

    tough level anywayanyhow tidy test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
