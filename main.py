f4527933a4339efb3f645179503079618ba50584
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:52:54 +0000

    drop confuse record to anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import sys
-def print_python_version():
-        print(sys.version)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
