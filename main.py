d4a469073957d57219ce332c61158076a3e25d36
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:35:18 +0000

    close feature grade organize doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
