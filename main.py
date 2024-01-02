ba94a176760fa163f9dfae9a758ee8ffd6e01cfa
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:18:43 +0000

    carry fashion sell bite step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def reverse_array(array):
+        array.reverse()
+def find_min(numbers):
+        return min(numbers)
