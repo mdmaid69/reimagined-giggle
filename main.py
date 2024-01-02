cd2b80153acce3b4eb7a139aa97a2fff898d061c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:55:41 +0000

    save state comment thinkover deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-def log_message(message):
-        logging.info(message)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
