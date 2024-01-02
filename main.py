8c5146bd8405ba0f0fc4cf3950cc685bc713c38b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:50:43 +0000

    can music fence central sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def convert_to_hex(n):
-        return hex(n)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
