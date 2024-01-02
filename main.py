7269023cf31e8d6bf51f856837c60cd7515f819e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:17:47 +0000

    injury future explain turn set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
