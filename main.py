b4474c6380ec487d8acbd1c23b34f4d7847039f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:12:02 +0000

    lay sink hope move lonely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_item(array, i):
+        return array[i]
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
