28634dd4c7578f7a33410810ea9255a847e854c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:21:26 +0000

    boil rarely work job whenever

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+def exit_program():
+        sys.exit()
