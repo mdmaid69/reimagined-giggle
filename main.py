df205df1cd2092416b7a8d3f5c2f99461ba2d07f
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:03:10 +0000

    often measure left pleasant furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
