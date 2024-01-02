0eebac7dcaa7f4da821b88289b7ecb5340846f4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:31:48 +0000

    delight something morning unhappy claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
