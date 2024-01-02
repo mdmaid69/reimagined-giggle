3dd469876c4d2e1973d47e7b593a41635e0f412e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:50:12 +0000

    identify onto reaction plant excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
