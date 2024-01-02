282697884cdbab803986528f2fd89e957192eff0
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:06:33 +0000

    worst return advantage pass upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
