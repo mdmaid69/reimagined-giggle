492b46021d20b49cdcbb32c637a44f2ee102c327
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:08:28 +0000

    poor occasion finger actual dull

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
