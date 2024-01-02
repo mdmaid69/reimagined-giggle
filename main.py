98fd505b336e8a505b0f964463c2c8687620f5f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:03:51 +0000

    question colour fun lead press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
