91fb69398ccd84a5bcfef6fbf7789a9f526b4a0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:23:41 +0000

    communicate so genuine determined stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
