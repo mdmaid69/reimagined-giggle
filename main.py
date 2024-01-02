790e849620a209f309e42e601db581cc954a7a5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:37:58 +0000

    upset must shrink mad comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
