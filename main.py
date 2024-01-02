eb589c5693276a368fae12efef633b6d145ed17b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:50:35 +0000

    fortunately experiment open separate learn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  def is_even(n):
-        return n % 2 == 0
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
