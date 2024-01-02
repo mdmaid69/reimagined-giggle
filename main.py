80725e38a7ec1072ef05b138929e2ccbd58827a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:46:54 +0000

    centimetre thank try Kk together

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(numbers):
-        return max(numbers)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
