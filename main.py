c16ff6667169671c0bacb04d25e9cce5c94e8674
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:50:28 +0000

    remove school task before put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_sign(x):
+        return math.copysign(1, x)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
