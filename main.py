f05f6008d32863202a3d615a481951a814bd848d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:25:50 +0000

    exact borrow file mysterious child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import sys
-def exit_program():
-        sys.exit()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
