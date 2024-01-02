63918127072cbd96a4259b38d00862cafb5317d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:40:46 +0000

    fall backwards sport complain takeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
