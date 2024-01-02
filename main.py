8924e2120dbdf92971fc1bf3e6a0fee65333636b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:12:34 +0000

    him issue scrap fire battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
