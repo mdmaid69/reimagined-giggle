68316bafb0e7c2039aca566114581076a9b82976
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:49:44 +0000

    wrap godown workupto plain memory

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-  def count_elements(lst):
-        return len(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
