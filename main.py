0b86cf0c7dab94b27ac4d305175e057c93e706c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:26:20 +0000

    no offence smart thinkover secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
