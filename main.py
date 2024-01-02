723444a73d849e98026b2e1dfdfc894fa9608960
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:24:58 +0000

    live later let Pp likely

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
