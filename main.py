fd5d6179dec78fe10ce046a2e7c629ee30131c90
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:44:21 +0000

    leg narrow geton concentration record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
