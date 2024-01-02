73a8bda6c05eeaee9390e453e949f8ab5296fc4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:28:04 +0000

    claim trade meanwhile neat so

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
