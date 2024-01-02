30119587711c8ead92695c44e4d51a942fce9ab0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:56:12 +0000

    shine moment sleep complete offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import sys
+print(sys.version)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
