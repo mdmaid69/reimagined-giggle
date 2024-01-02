8cd4cae17b4b5af79e77af579a580b254edc268a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:57:09 +0000

    back illness force emotion reply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def count_elements(lst):
+        return len(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
