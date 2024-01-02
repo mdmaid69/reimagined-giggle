58b906a701f2d9ea9064c748a6162df20a867717
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:11:03 +0000

    person real original light bright

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-print("Hello, world!")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
