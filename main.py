a63312a3acb57676701ae9a69af683c82d0ebd73
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:49:59 +0000

    music ticket chin label 568energy

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
