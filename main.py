3e9eca00239f80dc2215e7d55d329a1e47f09c3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:29:37 +0000

    friendly silent too considering season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
