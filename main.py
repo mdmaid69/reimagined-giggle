9fdd2ada36e6a19f530e53f46297f75b19bd7d1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:02:23 +0000

    play interfere past reduce office

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_current_working_directory():
+        return os.getcwd()
