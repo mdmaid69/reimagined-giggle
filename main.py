f2ef068ffb151b4c5f0b43560ff923cfd639f8be
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:55:01 +0000

    desperate book refer successful behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
