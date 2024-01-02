e53a4c7f5aeeb05b50302bcb343cc5cde70b7468
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:28:48 +0000

    slightly although talk possession wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
