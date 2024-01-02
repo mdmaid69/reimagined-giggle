1f15cb77de1d7bb39b57590e092521d882d89533
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:48:13 +0000

    lot worried address space organize

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def find_max(lst):
+        return max(lst)
