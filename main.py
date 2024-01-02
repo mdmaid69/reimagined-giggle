d7eb2fbad155612ca1fe1576afcbcb5ad65184d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:32:57 +0000

    question injury careless error shop

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import sys
-def exit_program():
-        sys.exit()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
