a8eccd15f9d2c45b0e5b9d47634eaa9bf4e165df
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:53:15 +0000

    woman male tread plant Jj

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
