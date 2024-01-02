78d9df37a9a30e6bb8ff3653942e121d5ca47fc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:45:41 +0000

    instrument exist shallow children hand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item(array, i):
+        return array[i]
