895906d88f18c0b0d7df54c2b5b11bcf18dd8a7f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:52:39 +0000

    extremely rest war business drink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
