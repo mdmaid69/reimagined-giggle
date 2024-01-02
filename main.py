cec6c2b87dc8b32ff124ce825c3735075866781b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:35:22 +0000

    proper getaway thick spite left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_base_name(path):
+        return os.path.basename(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
