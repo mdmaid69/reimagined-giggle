cba771bf46e58e1ef0ce01161b69d7f7e3a58e4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:50:57 +0000

    machine year acceptable lower course

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
