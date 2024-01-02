6d790729be5285c9f2272471441ca1a0fa4848e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:22:16 +0000

    mass tend another post limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
