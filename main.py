e75184464faaff8208842c29e1d5b34b57ca1fb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:24:42 +0000

    fall callup prepare attempt keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
