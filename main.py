a64061356c0dcc31ce920c89029e773b132c2080
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:04:40 +0000

    happy prevent fairly cry cheat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+print(random.randint(0, 100))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
