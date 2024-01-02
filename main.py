1c0364d84c10f82cf3d563204b79a6bf5722f037
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:09:22 +0000

    serious from touch explanation regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
