4b423364fc0630414d3be22e6d80bec7aa1613b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:46:21 +0000

    similar match pin guide expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+import math
+print(math.pi)
