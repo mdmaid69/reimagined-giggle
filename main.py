5ef1b47f567d58828b42b5cbf50433270bf6e91b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:00:29 +0000

    tell prove place play herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
