516f891624fe97fd8a3eafdca8d23e2b0cc60e08
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:06:25 +0000

    reject shame really forgive shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def clear_array(array):
-        array *= 0
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
