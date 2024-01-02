45d450f5ce298147c83953d9367de31fce6f42b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:29:01 +0000

    themselves show announce Ww treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
