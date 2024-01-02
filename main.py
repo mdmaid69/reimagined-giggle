11b0310ca510811862bc6a84a2631a8ae61b1a6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:22:01 +0000

    instrument feeling taste fast proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
