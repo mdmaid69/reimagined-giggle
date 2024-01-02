f1ae8eb722c0eacde8d6a79bbbf37618f6314750
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:29:21 +0000

    eat tomorrow strong different science

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import random
-def generate_random_number(start, end):
+  import random
+  def generate_random_number(start, end):
         return random.randint(start, end)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
