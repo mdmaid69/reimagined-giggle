9ab029ad546870b038d42631a9c997a79788f32d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:15:48 +0000

    statement stop accept steady scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
