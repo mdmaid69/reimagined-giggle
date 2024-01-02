e5add16c89a1d3de35c9a0d24a566e74236aff3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:31:01 +0000

    tight soak bunch into attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
