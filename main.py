94eec30554dca21b6530b3a1c12a183b7f84463e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:31:12 +0000

    organize matter direct callat breakinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
