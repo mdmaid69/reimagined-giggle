e60f15e9441044eb2cc9319489c2ce6ebcab3f7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:16:29 +0000

    opponent learn discover burst every

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
