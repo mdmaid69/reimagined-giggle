2da6f2f73d3ce5d9945d0e3680f358e42a7a2fd2
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:18:19 +0000

    left respect light careful valuable

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import random
-print(random.randint(0, 100))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
