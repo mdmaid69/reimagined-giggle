acb8b93263ed7c44ed46206d34c00aef6cc5abae
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:22:19 +0000

    keepoff narrow wild eat afraid

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
