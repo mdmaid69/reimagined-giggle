6e2786a5c13bf30bd299d96f71dfe5481976c2b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:33:24 +0000

    delicate failure write plain deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
