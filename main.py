572d2e9c81efb9ea8bea656c19009a9ec519cc42
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:57:21 +0000

    violent whoever environment rare between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
