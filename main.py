26ecbf3ac1e17ff6cc7822a15b9f800815e3bfbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:00:16 +0000

    film used post yes at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_exponential(x):
+        return math.exp(x)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
