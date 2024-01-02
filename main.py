6abbb0d117f6b895b6a262a9d5db9192065181ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:07:00 +0000

    responsible exam relation which argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
