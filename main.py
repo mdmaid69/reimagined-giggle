89ea7781a3e8aa1ee0c8f4f14ae6a95efaaae1ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:14:01 +0000

    Oo discussion danger plenty sign

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import sys
+def exit_program():
+        sys.exit()
