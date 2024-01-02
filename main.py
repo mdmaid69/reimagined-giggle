59c58c2be44fceca813b3c60d258236524867a1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:00:02 +0000

    doubt niggaacademy key project drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
