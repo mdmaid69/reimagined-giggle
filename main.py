4d5ac2d3f101b496dda433235c513b2fcc7be37c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:27:58 +0000

    near whatever boring example hollow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def multiply_numbers(x, y):
+        return x * y
