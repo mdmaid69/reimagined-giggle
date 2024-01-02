a5dfebff92abc65d38eb5b036936047ffcfbdfb4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:17:12 +0000

    cold flat collect whether present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
