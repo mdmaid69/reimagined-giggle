d0b9d503d2fcd3b8491ecdeca2107c80707fc836
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:20:34 +0000

    straight future because giveaway thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
