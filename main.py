950ad64e6269f419618dfe8e229f8573ca769b88
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:24:34 +0000

    single control laugh flight alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
