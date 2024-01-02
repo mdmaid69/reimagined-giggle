71a6dedbc377e2ffd6db0536d7ce161ab21bdc9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:05:37 +0000

    month keepup judge without maximum

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
