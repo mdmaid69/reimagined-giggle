91c8c5910d159ccc6f6db02198b2739c1e9986eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:37:11 +0000

    today perfect commit visit car

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
