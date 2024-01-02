a540099ba2148a563f91cdb16ee269e3560bafe1
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:34:19 +0000

    relief busy nigga exchange please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
