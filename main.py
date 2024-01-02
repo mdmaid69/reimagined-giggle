d14c927b46a09b69d2953f43c39ed608b87ea7ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:09:51 +0000

    big congratulate bright 573entertainment bread

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 def find_min(lst):
         return min(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
