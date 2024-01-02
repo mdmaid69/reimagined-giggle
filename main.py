de95945890cd3696d39f2c6ec529b04a67ac677c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:07:39 +0000

    hers persuade sticky shop road

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
