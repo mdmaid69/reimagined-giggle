5a52de98c5830e5fb9d59e589bf10462a293f73e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:29:07 +0000

    another deep phone pause than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
