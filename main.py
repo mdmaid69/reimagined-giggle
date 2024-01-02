e5271a9f0663b8a8b166a7b700625412411e5ba1
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:23:28 +0000

    only theirs unite prevent rent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
