91283b7fbc256fbfc06ac232ebe3e81bfb2cd3b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:07:54 +0000

    finish change take piece art

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
