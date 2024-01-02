0019aa0d9714199db31f81376fd1bd6668f517b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:45:24 +0000

    track mystery fairly introduction in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def is_palindrome(s):
+        return s == s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
