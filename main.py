754626877da9965621461bb7ba2457ac089ba49b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:32:01 +0000

    escape level lie stage plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def is_odd(n):
+        return n % 2 != 0
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
