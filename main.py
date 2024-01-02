c9aa15b4bb91c6fb462809adde625f4d807fd406
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:08:58 +0000

    final autumn again town wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
