711adb94a8e1d25e3bec00498632222517daf6b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:44:24 +0000

    characteristic rob accept business mind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
