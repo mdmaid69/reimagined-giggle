32c807e47032fe8cdbbfe3fb1ba5010d2edecaa4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:19:25 +0000

    party long serve lay design

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def count_characters(sentence):
+        return len(sentence)
