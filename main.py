04d06d1519d7b5edb8fffe1f2a482db4a7d31574
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:47:17 +0000

    new price fact gas judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
