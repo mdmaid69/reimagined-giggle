493004ce24a5fd0924ba2b56cade9cc0830cef42
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:22 +0000

    upstairs over bank steady place

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-name = "Python"
-print("Hello,", name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
