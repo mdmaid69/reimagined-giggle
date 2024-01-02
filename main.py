6e91930491e174fc7a8b928a81026e8442a5e0e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:28:02 +0000

    pass mess container crime deliberate

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
