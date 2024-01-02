513cbc92fbd93c1d7c944f545193c2c551e8b6a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:21:30 +0000

    lot often address bright prove

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
