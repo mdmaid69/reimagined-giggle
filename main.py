98a164cf117cb9f35748e34cab6469bcded4f4f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:32:49 +0000

    forever favour time sound bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
