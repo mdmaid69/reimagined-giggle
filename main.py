0ddeabb45c088b74eaa5b8b811e6a05a5735e047
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:50:58 +0000

    negative gradually drive you between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def reverse_string(s):
-        return s[::-1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
