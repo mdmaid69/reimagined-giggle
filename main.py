30a8f047878fa3837cdd5e085bcf964c4a7faacd
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:30:08 +0000

    today say sort familiar cry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
