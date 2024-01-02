51dd2a4d4f286782957d2ad9b0021f6c06797109
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:35:13 +0000

    centre skill wealth imagine name

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
