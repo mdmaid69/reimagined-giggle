5a507e189f8d4a7d14e8c2b5c123929a4b341f0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:29:26 +0000

    practice bet feel tax simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
