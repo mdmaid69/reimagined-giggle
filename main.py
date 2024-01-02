765f4c01127b67afeadba9489c61ee1ccf459d1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:52:57 +0000

    freeze contrast job and store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
