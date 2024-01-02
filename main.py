2dc9c7eca45241223dc3f58facef1579a9abb305
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:59:22 +0000

    back prefer destroy strange insist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def convert_to_binary(n):
+        return bin(n)
+def is_even(n):
+        return n % 2 == 0
