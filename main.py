5b98dc8230667f6448f64c309df7f770467baaf4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:05:15 +0000

    performance real by speech impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
