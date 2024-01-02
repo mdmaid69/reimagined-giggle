251168aba3feda498608b755f896b8af969dc6fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:52:59 +0000

    song keep circumstance wall surprise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
