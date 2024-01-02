2b428d7710b23b1e01e8e82142649054b649ab34
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:37:53 +0000

    head they sure no include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import array
+def get_array_index(array, item):
+        return array.index(item)
