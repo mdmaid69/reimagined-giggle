38dc6ac722e63578e0f6f4207107c2fa3b5503cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:44:36 +0000

    present necessary broad order forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
