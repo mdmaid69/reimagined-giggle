cbecc9f00beed3de38887c6791f2279b53f16a59
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:22:41 +0000

    claim band sting true delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
