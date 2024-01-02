28800297164f13b73a8de00e11a2f2eb2a995e3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:07 +0000

    unit base gradually unable uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
