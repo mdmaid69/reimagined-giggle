fbbb68cc42368b0fbe6560df7ff31f8d4abf8410
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:20:15 +0000

    stiff lead arm thing tense

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
