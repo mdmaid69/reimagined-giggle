2d3d0e00c4d6432ab73816e9d0732442fbc3eae1
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:43:56 +0000

    line organize hole promise discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_error_function(x):
+        return math.erf(x)
