6b7624e4db7a8beb94dad761f63942d4a5e279fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:11:13 +0000

    embarrassing exist uncomfortable furniture not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
