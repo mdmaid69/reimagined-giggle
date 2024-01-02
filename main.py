993f7268da02795de86001aea64d059edfde0649
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:45:42 +0000

    effort any estimate heart stay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
