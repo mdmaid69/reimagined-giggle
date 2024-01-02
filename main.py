0eaaff2f644a342e82da33892907ba0d016328a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:22 +0000

    pain free hit rank plastic

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def divide_numbers(x, y):
-        return x / y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
