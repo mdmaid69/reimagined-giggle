c518cc085274d7f7989e4f13143f58fedd898658
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:28:17 +0000

    fond disappear than guide feed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
