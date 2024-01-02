d6bb43fa8b47ef5a3f7417df23f95afb9cd4f990
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:46:43 +0000

    score politics complain entrance right

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def subtract_numbers(x, y):
+        return x - y
+def find_max(lst):
+        return max(lst)
