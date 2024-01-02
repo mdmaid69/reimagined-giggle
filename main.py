23f643d0167cd3c13e6306fbfcbe2a945e70db9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:40:14 +0000

    minimum previous level slow discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
