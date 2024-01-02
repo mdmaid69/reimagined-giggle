83c1e950d2c7b22cc33209c01589a9217f2f72ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:42:43 +0000

    fast talk week shape behind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
