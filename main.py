ac2ab83ef38731761c707b76b91d6303b9894e39
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:00:53 +0000

    566end 566end breakin set confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def convert_to_hex(n):
+        return hex(n)
