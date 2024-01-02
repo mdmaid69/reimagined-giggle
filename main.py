79c80e2a71281e5cbfed456f4339c32d52cd864b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:18:10 +0000

    want violent walk bright beyond

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import re
-print(re.match("h.*o", "hello world"))
+def get_time_since_epoch():
+        return time.time()
