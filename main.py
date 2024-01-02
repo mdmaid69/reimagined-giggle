f7f0d2934c52420943f78ff36cdaf7fd00a55eaf
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:59:25 +0000

    even exam thinkover forward search

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def convert_array_to_unicode(array):
+        return array.tounicode()
