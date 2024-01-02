9ca331d5500babfd982ca32cd510cf9742b9b725
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:46:08 +0000

    type steady awake bet reasonable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
