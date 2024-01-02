92946224ddf50a4d7afa04d5ff1ca19214ecfed2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:44:20 +0000

    Yy young song keepout cheat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
