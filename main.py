b5f81ee2f3afc8113a1be4dc8dc9b0dc8d147482
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:27:07 +0000

    relax congratulate nothing past attractive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-print(datetime.datetime.now())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_stack():
+        return collections.deque()
