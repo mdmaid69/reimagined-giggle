953fdadf9d9412d646c26364e4f03dbd593f7c13
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:10:28 +0000

    skill lot claim agreement suit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def sort_list(lst):
+        return sorted(lst)
+import tensorflow as tf
+print(tf.__version__)
