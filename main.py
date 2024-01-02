303e8e1419acaf15346a3f4415ee83ad8bd31271
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:53:40 +0000

    innocent government centimetre exciting break

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def square_number(x):
+        return x**2
