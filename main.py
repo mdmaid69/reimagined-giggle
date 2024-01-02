04a432160db171861b6a5baac6e2d15bca360c82
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:20:53 +0000

    unfortunate success slide number begin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
