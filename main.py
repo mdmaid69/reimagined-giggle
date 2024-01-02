48a9305d4396a18234df3889e185cb63bb1c2aaa
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:13:26 +0000

    believe row frightened fit relationship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def cube_number(x):
-        return x**3
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
