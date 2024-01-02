a7c97360cf68c28ec3f7722641036ac11abc27ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:25:03 +0000

    we apparently bringback with afterwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
