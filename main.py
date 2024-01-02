904619e28dcff0087ac7f8151aabf86c87151bda
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:42:56 +0000

    male connection search scared activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
