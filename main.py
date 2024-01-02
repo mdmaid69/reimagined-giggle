cf428ba9f78efaf7f16862c4b3830a6f8f17548f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:13:52 +0000

    experiment lot sick high offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(5):
-        print(i)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
