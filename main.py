863e79a6847b8df8791af75a3e20fcd213b6075c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:25:40 +0000

    wait myself but shoot building

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
