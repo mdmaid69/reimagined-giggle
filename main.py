5a9e974d0b6efbe2851bb645b8b83e0567e59ed6
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:10:06 +0000

    switch afford warning maximum convincing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
+print("Hello, world!")
