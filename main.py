69312cf640cdb6562436ea5bf78265d2c401a3b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:22:58 +0000

    getup very match correct guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_list(array):
+        return list(array)
