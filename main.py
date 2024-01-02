c6ddeadb47588bea615ba9f3a7176f8d109e2bf7
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:42:19 +0000

    Hh unite thanks single fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
