72b4f0114d993d35e639476a65a41ad7328afecd
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:23:04 +0000

    control provide hang market actually

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
