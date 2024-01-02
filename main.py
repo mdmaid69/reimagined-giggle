ec613a9745641b6a4c2d1b21c6d0cea7a5f0ae78
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:41:03 +0000

    cheat calmly sentence guilty release

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-print(sum(range(10)))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
