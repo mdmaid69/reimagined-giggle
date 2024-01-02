55afba35b6518cf9a37a67a23e5b2f6c9c091a76
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:47:48 +0000

    factory repeat lookafter clearly national

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
