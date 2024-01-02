d3c629e3e15d9d88e40f4ab20414a7b040b23084
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:09:17 +0000

    different somehow chief ordinary favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
