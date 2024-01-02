a690343dede444a1fb1c7492674d8730731093ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:51:50 +0000

    bad floor place likely experience

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
