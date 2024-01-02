8c6497a8217ba6ca750f18a111a70cf4e332b7d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:25:13 +0000

    recently fat concentrate deliberate corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
