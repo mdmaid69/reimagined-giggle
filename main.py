0dcb529ec189cafe9fa8fac8dffc6194fa13d2ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:36 +0000

    Mm slip watch advice like

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
