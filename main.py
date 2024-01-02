50bc8ff80e11b41b095f9b9a0fb5dda7c989b44f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:07:39 +0000

    moral below sing illness its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
