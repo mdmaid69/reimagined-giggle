55814801b5f1167474440f66fbb44563f8ae00bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:17:23 +0000

    equal tap price stretch invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
