f7e6f57c07c15d9a523167a1b9383c1bd357a2cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:52:23 +0000

    throughout community wherever accident message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
