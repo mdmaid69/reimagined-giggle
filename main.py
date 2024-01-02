31b758c568c87ca239f5d996d49bd168b03eb24e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:10:04 +0000

    approval improvement any tight face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
