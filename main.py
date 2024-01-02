72ea4e9ff12b2c7b2125cd93a89c8a9ee8290134
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:02:38 +0000

    lookafter where opportunity invite decide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
