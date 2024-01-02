8e59ca62d3e583bcc80ee0c1f53e9c03dabecce0
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:05:19 +0000

    with thinkover read music ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
