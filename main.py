1fc6a919c9bfdc3be53d0ff38440a16f6e584524
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:35:08 +0000

    prefer exercise aim happen centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
