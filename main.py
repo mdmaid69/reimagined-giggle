22afef49667ee8a3851e49109592f9115bcc7933
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:22:06 +0000

    awake such chat mine occasional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
