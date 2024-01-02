26924387395163a93dc9f9032e729e2f3ca6637f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:21:03 +0000

    situation conduct make like relax

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
