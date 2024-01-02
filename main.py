a2459f61ca3a22eca04d89846b2396d96ecaa58b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:51:29 +0000

    probably century embarrassing speech giveback

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+text = "Hello, world!"
+print("Characters:", len(text))
