0a41680a27ee5ad3099116dd0d55c1aaee087cd5
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:40:42 +0000

    comeround save glad mad pressure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
