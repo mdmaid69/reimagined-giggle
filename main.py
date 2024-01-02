d1efb28f8564cb07de318308eb7296be6e8fe962
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:34:18 +0000

    shopping destroy reserve ashamed lookafter

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-print([x**2 for x in range(10)])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def is_even(n):
+        return n % 2 == 0
