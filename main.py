e10e840b4e1ad4aed6c02ade593e7e1352400eb4
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:37:22 +0000

    spot crowded visit generous ear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_min(lst):
-        return min(lst)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
