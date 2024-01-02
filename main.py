40079a60c2c6886d164a1d4b70df406eb20d526c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:03:43 +0000

    everyone flat 568energy wall against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
