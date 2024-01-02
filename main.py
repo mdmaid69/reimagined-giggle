8fa4fbb922c4f359d8c2e0936806035470aaffd1
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:57:25 +0000

    file warm punishment or poor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
