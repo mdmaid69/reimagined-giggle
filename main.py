0a630b9888f5f8d8cea4a29f21d2d705a782704a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:29:01 +0000

    school nonsense follow circle scene

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
