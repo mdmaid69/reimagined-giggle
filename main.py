75da9e5337f2d18883e5116517aac217c81c2335
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:07:35 +0000

    high pile copy exercise ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
