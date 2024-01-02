ace4b823cc37ca03a05c63ebc4b469e209a137c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:48:31 +0000

    hour need storm Cc push

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
