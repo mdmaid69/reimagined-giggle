7447e38eac61f41d41fcf615e1fe1502a8a56b56
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:24:23 +0000

    past near clever base funny

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
