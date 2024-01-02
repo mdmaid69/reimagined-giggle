4c880ba5bad88a0e7dc8f1e9c1e2f645e9a57fea
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:16:28 +0000

    wide apply consider children just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def get_current_time():
+        return time.ctime()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
