b4ac8277736e48db7cd81f9388433251aceb13a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:31:42 +0000

    common flight on get dry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def cube_number(x):
+        return x**3
