8a7e81ee34744e94f0d31835a35c714113e003b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:52 +0000

    probably tragedy lesson wealth crowd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
