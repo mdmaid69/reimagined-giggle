b47327ff9b8b6e8d031e9ad3aef25e279f853ccf
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:07:55 +0000

    dare swim hot buy correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
