edaa727bf2344845ff8e86084daccc6e7698107b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:24:09 +0000

    hurry we makeinto polite pronounce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
