65d11afa97e5aa9722439d5a9424541b27e3c499
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:33:20 +0000

    wear meanwhile else concentrate absolutely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
