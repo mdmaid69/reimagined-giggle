555b0729d9d8cc6a37e0922bff03b206c3330940
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:50:22 +0000

    climb by often scrape refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_bytes(array):
+        return bytes(array)
