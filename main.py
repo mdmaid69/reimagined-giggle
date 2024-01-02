7a2f42a616ae875cb5d9ceb3413b4acb25b39cc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:54:37 +0000

    exactly fall support broken place

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import collections
+def create_user_dict():
+        return collections.UserDict()
