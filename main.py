755ec23b100e3c24dd2bfea39ca43f6d07263617
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:37:39 +0000

    compete profit course operation throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
