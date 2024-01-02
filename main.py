9600e35c706bcb6b8cc68cf33fe08e09505eecbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:21:17 +0000

    sweet theirs instead guess correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
