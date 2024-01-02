a47bfa7368ad49229dfa1b191a51cf96dc6d1a4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:22:21 +0000

    roof attempt comeover good favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
