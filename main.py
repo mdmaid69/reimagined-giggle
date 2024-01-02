3294dedf73a767bea9eec8c9d4de84edb9939838
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:12:32 +0000

    smell want hold exercise violent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_octal(n):
-        return oct(n)
+def add_numbers(a, b):
+        return a + b
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
