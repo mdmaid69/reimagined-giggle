9414a4ebfffd9d857556d3a42d50356932837ccc
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:23:21 +0000

    worth winter crowded fire television

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
+  def calculate_area_rectangle(l, w):
+        return l * w
