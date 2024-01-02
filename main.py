4adb6bcdfc10509dbe6de3583152e49fe423f155
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:49:24 +0000

    belong trial unless pronounce go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+for i in range(5):
+        print(i)
