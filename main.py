53ed652cc4a4b8540ad3575d8808a29eb8e9c217
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:30:19 +0000

    train switch pretend me private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+print(re.match("h.*o", "hello world"))
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
