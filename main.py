fe1ac7857f18e85acf5fd417c94d7b3007bb00ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:00:33 +0000

    hope miss own doubt dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
