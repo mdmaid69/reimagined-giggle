59ae53c1b09cd0401c5d615d59f360ef5ead5632
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:26:59 +0000

    point second pot think achievement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
