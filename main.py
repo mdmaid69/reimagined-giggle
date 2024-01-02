1059ca495664b7b6e33378d2bf97f9e4c5d0f5ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:01:25 +0000

    involve miss pour rush hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_string_from_array(array):
+        return array.tobytes()
