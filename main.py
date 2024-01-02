39c8683c7a0dd5d2113d5e401d3bc8a1d35059f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:47:23 +0000

    temper catch rush damage town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
