48e1fd8dca9794359dbfaed79008ae97142e081b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:08:47 +0000

    safe mouth just actual paper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def reverse_string(s):
+        return s[::-1]
+import tensorflow as tf
+print(tf.__version__)
