5602909520b7e9ba3147b78a49a39d4faeeaac43
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:04:21 +0000

    false side even suit warn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
