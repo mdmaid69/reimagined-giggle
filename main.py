c44bdabc3b498a0f54f0ad7ae76ff7cb374066af
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:36:50 +0000

    thirsty plan congratulate nice cut

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_work(force, distance):
-        return force * distance
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
