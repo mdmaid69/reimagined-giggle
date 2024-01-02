783bfb65057ac53b9975cfaf91487850126d740c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:57:34 +0000

    tread trial half situation whom

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
