aaa3ec67819c80a2278b2d02f09cc8ae6c02dc9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:00:18 +0000

    miss nice if rubbish against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(numbers):
-        return max(numbers)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
