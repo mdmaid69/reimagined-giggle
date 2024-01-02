8f80cca02c889b48d85f528c65c3e87a7d8892b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:44:35 +0000

    repair emergency yesterday check comment

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-print([x**2 for x in range(10)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
