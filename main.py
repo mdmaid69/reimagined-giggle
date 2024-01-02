4fc3bfb3b7733be090862c8d1e80dd3910cc68ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:09:56 +0000

    interview collect press sure attempt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
