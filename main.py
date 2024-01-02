98955cd55776ad3f31c805919f48659e168b0421
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:16:55 +0000

    concentration old suck better computer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_area_rectangle(l, w):
-        return l * w
+def iterate_over_array(array):
+        for item in array:
+        print(item)
