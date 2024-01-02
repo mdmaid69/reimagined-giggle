b9c682cb77030321d80c3e34defd08db580a1438
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:28:28 +0000

    ought whom about dress push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
