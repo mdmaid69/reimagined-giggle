ac45b167d10be6ab8e5bc0f277148c6bd4043650
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:45:36 +0000

    sort profit refuse worst convincing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def reverse_list(lst):
+        return lst[::-1]
+import math
+print(math.pi)
