0767184b342731c398acd7914cd4b6261e1df399
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:13:52 +0000

    lot unhappy century cook water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_list(array):
+        return list(array)
+  def add_numbers(x, y):
+        return x + y
