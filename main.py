6c319f859cf9635a54699356858c6f687487e7ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:23:38 +0000

    colour feel vision cost scale

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
