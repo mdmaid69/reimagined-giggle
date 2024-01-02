16e4617c0cdbf07c08f8fbea5ad1f3235ad13ef2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:30:01 +0000

    and giveout through law plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_area_rectangle(l, w):
+        return l * w
