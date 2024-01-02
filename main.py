97b08353d30998cb0e9ff1e7dfa6be0f725e724e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:35:44 +0000

    funny refer deliver match sky

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
