323879e7b7c4b24aabca66b100d76ca6013c7e0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:34:32 +0000

    squeeze drive beginning connected custom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
