d96e85820f34df8e0d913411b5b536aafccb6ec9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:37:06 +0000

    wise flavour weekend avoid why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_string_from_array(array):
+        return array.tobytes()
