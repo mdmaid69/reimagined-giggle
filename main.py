1f7a867752728f20b95f056b03db7f4f2c67fabe
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:26:57 +0000

    fun turnup treat lock bringdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
