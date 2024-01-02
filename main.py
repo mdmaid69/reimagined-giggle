a7d973a52c03c2b79b9f2ffc38b8ad1bb510a696
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:26:24 +0000

    proud head cum band violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
