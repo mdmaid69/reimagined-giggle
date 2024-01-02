682a8d60efdc8eaa7647ab500de398811fd75823
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:47:57 +0000

    switch reasonable gift direction various

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
