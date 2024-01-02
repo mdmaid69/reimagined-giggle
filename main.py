50d1217a11c5889f05a92598b939a435a2e42747
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:33:05 +0000

    disgusting behaviour neither keepup shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
