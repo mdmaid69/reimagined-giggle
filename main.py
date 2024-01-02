56df59cafb620346e047adab313bc942ef712ba0
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:29:50 +0000

    inside book definite transparent fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_hex(n):
-        return hex(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
