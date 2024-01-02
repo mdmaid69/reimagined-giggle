84f6024ad1637160bf70b24625e74a4148e05ff4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:33:21 +0000

    usually now grateful lookback(on) woman

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
