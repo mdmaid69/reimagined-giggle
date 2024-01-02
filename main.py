49be97fe5e44dad3d8df456ecb68f2673e17a39e
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:57:32 +0000

    decision slowly spring stop market

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def is_odd(n):
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_odd(n):
         return n % 2 != 0
