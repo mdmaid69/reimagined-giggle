0e6a8887013813b1845c1e24b6e60a19939d54b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:05:22 +0000

    near team swallow rare noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
