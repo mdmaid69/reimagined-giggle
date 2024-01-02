73fb8d1246d4a89435dce730cd82f9b41b5a7975
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:10:40 +0000

    suddenly write load fat allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
