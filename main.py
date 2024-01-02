663d70aaff36a194faa9c11394c681739be077f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:25:59 +0000

    film information please rate spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
