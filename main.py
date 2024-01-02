822cf78bcd2b70023fddcb66aa1d7fa223643590
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:08:36 +0000

    uncomfortable shock about old separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
