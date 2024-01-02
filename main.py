e692583902fb0860ab35ceb95264541ff1d9dd6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:13:35 +0000

    on estimate bit squash interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
