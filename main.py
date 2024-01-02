bc3ed2de585d7a64ade94e3d9f2c11aeabe17022
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:47:46 +0000

    generous neither tip during arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
