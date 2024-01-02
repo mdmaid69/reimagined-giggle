9a5f13998c260422a40ba27ff3f2cb64a0e1a699
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:10:51 +0000

    eat violence match company pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
