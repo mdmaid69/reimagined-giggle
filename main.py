17066f2236c9b30bd10dfeae5b964872f7606e1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:13:06 +0000

    tomorrow back mainly possibly lift

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
