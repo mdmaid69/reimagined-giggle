7464f3a9730da5228430e8682d4c6df58a9cf303
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:22:54 +0000

    cause old knowledge male evening

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
