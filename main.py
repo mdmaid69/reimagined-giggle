4f7cbca1e910f5fe99b463476bf67db53acee2ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:49:44 +0000

    advise begin team doctor take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
