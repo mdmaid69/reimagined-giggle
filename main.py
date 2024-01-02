7f4220c32e67b3ea72ae486c0749161dc5d7dc63
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:29:14 +0000

    treatment much ring supply base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
