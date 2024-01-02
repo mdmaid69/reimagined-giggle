70f99388e9624db6dcdecee8a808ae91e5e43aab
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:43:37 +0000

    rest correction performance language 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
