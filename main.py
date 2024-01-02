4a0f1aa5598d02cba06ead7fff7bb5e271988a79
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:19:10 +0000

    fortunate float lay accommodation round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
