173d8a62ad6fd62b49f4991b3b4fa80513027591
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:42:06 +0000

    turndown Ww shop stop hang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
