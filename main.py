4b2144f46b416229c3a038860366b8ae5121d1b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:23:23 +0000

    hot limit heart fork produce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
