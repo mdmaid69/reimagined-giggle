cdfe2190ac87b9f98d566d406af63e7f54ffc629
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:32:58 +0000

    simple search Ss correct total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import collections
+def create_stack():
+        return collections.deque()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
