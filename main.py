465f4cda70689540b73641c8c618ec4304c15123
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:27:12 +0000

    tense judge hair prevent wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
