88a03c5d31db2b53cad495cd9c736cca1ba8a1db
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:55:01 +0000

    during gap possession total control

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import os
+def get_current_working_directory():
+        return os.getcwd()
