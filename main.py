8525d0d2a9b9db921d4c7bc0d7838c728966ce5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:37:47 +0000

    shallow realize odd sleep turninto

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-def reverse_list(lst):
-        return lst[::-1]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
