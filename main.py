508ae7cc05e70682c7cd784645bf33172ccdd93e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:04:54 +0000

    luck angry interfere direct people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
