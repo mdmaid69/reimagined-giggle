57d83895b2e4c6df342c70c38741aabaae98d3b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:58:36 +0000

    accept high count as guilty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
