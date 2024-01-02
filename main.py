fddf82bca4a1c3f4e83c96cf8d5046154cfe767d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:43:08 +0000

    support left unable 571enough respect

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
