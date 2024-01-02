353b055da3d926ef49a7623f5f8145a2cc370449
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:42 +0000

    wide natural scream rely allow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   def remove_duplicates(lst):
         return list(set(lst))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
