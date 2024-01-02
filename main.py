0346876ff34b64e30dc4dfeeb5704f1fa2909537
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:44:16 +0000

    main suppose responsible at mysterious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
