dd86f33c026a26f7643fcce39a26a5dcf7a79389
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:07:17 +0000

    at team content plenty share

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import collections
+def create_user_list():
+        return collections.UserList()
+def is_odd(n):
+        return n % 2 != 0
