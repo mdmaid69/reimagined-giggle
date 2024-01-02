4eb5843b5ec62820dec3bad1cd1e45a8e9fbd0f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:34:47 +0000

    touch matter workupto bar night

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_current_directory():
+        return os.getcwd()
