9ac9054384705b6668ffd425fbfecfb4bd9fe9e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:52:16 +0000

    area tip fair trap minimum

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def sort_list(lst):
-        return sorted(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_to_hex(n):
+        return hex(n)
