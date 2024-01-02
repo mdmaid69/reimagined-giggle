b55bb2bdef62f2832772127da9012a9747fcf87f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:34:25 +0000

    its flow together angry quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
