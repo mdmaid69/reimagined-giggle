de31a32f7e6abbbb2cdae1055550de59098e9ac3
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:43:32 +0000

    round point march steer welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
