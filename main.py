063e05aae623a5ba729a2f050be875bcb22d5f46
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:42:45 +0000

    house hear fight brown equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
