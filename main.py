e6f9d945f275a8676ad89557c004ea8cb9ff78bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:40 +0000

    fresh spring power exchange affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
