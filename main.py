40d9bedd0970f6e708abc42a8df8fd3fa85c0a7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:15:54 +0000

    experiment share main makeup mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
