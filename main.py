9e7c48229e608f86c0ceb13efd711fbceda0ba07
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:32:14 +0000

    hollow book own minor through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
