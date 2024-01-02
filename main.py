3e161094a20372f7c814c0f404a83a00fe4feb22
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:27:01 +0000

    mystery compete complain risk healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
