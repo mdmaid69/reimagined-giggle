2f680eccd2a1ba0c66a4f056b3477db2915dee66
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:41:00 +0000

    error impressive regularly children loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def count_elements(lst):
+        return len(lst)
