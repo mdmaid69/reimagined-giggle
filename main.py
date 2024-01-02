bbc395d8ec065c511b78786598ef5e425b2c5c14
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:36:59 +0000

    important thorough stiff apart obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
