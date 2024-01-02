dfa49b5c2ce0f9fd2de89825f7eb20bf40ec2c91
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:38 +0000

    she tidy smart between plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
