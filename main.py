f173c3e0525371cde693937ddb0b03ddef56b1f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:22:03 +0000

    clothes earth bring tune building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
