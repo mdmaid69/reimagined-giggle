f46a324d0980a7629a9d83e7ea0601320f856789
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:40:23 +0000

    classroom ago protest kill silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
