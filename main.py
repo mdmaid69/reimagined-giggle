5fb26a1aa06390a3ebe29b5e56b34479c1455d9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:46:37 +0000

    depend report freedom noisy organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
