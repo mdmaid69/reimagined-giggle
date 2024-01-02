dfaef5487a109e4ae461a3b9eea1f1537c25b639
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:46:12 +0000

    trip pollution brush unlikely excellent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
