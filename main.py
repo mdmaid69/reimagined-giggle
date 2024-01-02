ebd30293fc6f351682c9fc7c182d93cef88ae896
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:49:23 +0000

    couple breakin method one disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
