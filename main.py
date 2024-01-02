f5e9f44dfd4a11e315735b4a5b0745846955db61
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:40:28 +0000

    main load disapprove later limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
