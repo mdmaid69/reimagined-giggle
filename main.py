c537b866c5255456ec73502f90b8a478b0e0187d
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:25:56 +0000

    boring border temperature dress Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
