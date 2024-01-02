5b6fe7725c64078f137bede10ee02d7154af92c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:46:22 +0000

    neither tap challenge light drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
