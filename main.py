9a004300a5284decdc23a7e42280350c4028a2be
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:41:21 +0000

    plan juice professional grateful morning

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import math
 def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
 import array
-def get_array_as_set(array):
-        return set(array)
+def append_to_array(array, item):
+        array.append(item)
