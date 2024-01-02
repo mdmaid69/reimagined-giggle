fe3bbbcb94a531f5df8dd33e64fffaff657d53e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:19:46 +0000

    getback control comein manner fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
 import datetime
 def get_days_until_next_year():
         next_year = datetime.date.today().year + 1
