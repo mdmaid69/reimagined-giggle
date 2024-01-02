cf1bd95b86646123bebf4ae6f1fcfb27f3dfb171
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:05:17 +0000

    patience bill comeback minimum choose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
