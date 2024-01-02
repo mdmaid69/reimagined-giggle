08eddd1629a2f3f7baceb2a57a6231b7f48032bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:01:58 +0000

    demonstrate inform show father onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
