6db8a3040d31f3c5ee016c449a5d602e80b32974
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:12:52 +0000

    motion common arrange godown marry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_elements(lst):
-        return len(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
