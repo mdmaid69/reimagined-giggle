9d2b0934746e15fc79d5b9ce6904ba3e5f4e3cf7
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:35:54 +0000

    convincing another name front fat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
