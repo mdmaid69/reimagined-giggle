dd29ce0f64d53a41d17071b43455ae3579426cd1
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:50:29 +0000

    uncomfortable arrive detail sensible constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
