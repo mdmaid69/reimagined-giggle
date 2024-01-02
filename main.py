61d52949dd5aea80c4967c1fc6aa177361322510
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:39:06 +0000

    unfortunate run acceptable because know

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def convert_to_hex(n):
+        return hex(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
