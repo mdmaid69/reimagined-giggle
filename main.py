77b1c3d6cd63f48e137d28a8fa2b00fa43c6f846
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:46:48 +0000

    anybody join reserve speed succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
