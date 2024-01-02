a7704c7c495fa646f53bb232a0e8f6bf0abe5a89
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:47:13 +0000

    bill switch upset stage distance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sys
+print(sys.version)
