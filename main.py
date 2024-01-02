2799e5c68c56c0c2c39b6236e461eb7cbfc41048
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:27:18 +0000

    live calm trick clean fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
