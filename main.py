e77ca77635dd2585b26f9544098897e485ebcd47
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:12:23 +0000

    money Gg breakin bar defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
