7bede50acbb38ec8170b9793308380d313fa2b67
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:44:14 +0000

    difference speak some generally 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
