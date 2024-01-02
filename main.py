9b3a7828f651f1ad34903c6998450e374ed268af
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:00:56 +0000

    lock recognize telephone woman game

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
