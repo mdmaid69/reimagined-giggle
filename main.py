8994d187556eaa614afe051ccb89c4295eb64e85
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:02:55 +0000

    cover set knock book its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
