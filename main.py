ef521e2acbfcc3f1504a1d2ff98ab27b6fe22e41
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:34:35 +0000

    moment fear best nor opposite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
