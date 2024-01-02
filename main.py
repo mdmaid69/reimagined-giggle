7c632757ca5b04ac38b6dab38614e397e34f2262
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:25:46 +0000

    your that movement exciting production

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
