c35b0d95984fd02aaa2c25017b3db1906b58c8bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:36:27 +0000

    reliable room own dark stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
