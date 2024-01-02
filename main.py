4f4dcb760e51ff9f0a3785bf4265d41064f42203
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:45:28 +0000

    square ground worry needle position

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
