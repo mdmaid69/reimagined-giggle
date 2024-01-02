51d7518892ac09f77a2e77c6af4bdae18c7b3d44
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:24:43 +0000

    mystery scrap complain pause breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
