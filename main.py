ced77478c63e6cf51170feb9321d7774c3db8647
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:25 +0000

    complicated ticket proper careless apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
