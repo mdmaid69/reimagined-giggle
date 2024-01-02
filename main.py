0896e8499885798da6905286c86eaa085185fe98
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:09:23 +0000

    poor whatever cross across search

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
