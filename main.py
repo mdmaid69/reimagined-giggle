97047d9798800d9ed3959bf6a238916cba34528e
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:14:43 +0000

    physical trip while exactly speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
