0da45793945e7f2b6c58e7ede4ef101aa5f189a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:44:18 +0000

    condition number concerning old both

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
