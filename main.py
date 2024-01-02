aad5e4be7caa06763cee3995fa543cc6d6af1df0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:28:15 +0000

    tough callup heat draw bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_int(array):
+        return int(array[0])
