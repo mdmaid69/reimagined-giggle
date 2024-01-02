3f2ca90d0d968a43fa33d2586f9cad1f38d0b660
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:56:34 +0000

    unable language minor means wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def square_number(x):
+        return x**2
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
