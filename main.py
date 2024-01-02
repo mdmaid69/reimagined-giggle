882a90e85b4aeaef780533ae2f35802f990d6d79
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:29:12 +0000

    dead steep really display Ll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
