4bee443d60d3cafb895fa092a68921f1fc9be2f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:34:23 +0000

    exploration seriously build anything suddenly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
