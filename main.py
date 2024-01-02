8cbf139c23909618886e04582226234e1f38382d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:28:27 +0000

    soft talkoutof hollow nature hand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
