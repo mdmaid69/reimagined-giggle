907f529d75218ccecb2c3cc1bef014ebd69f5f1e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:10:43 +0000

    absolutely message argue comedown patience

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def convert_to_hex(n):
+        return hex(n)
