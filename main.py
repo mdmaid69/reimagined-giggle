9c9e84cba93e8c56e6b4bac6bffc9156f82fbf6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:14:37 +0000

    boil occur read play that

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
