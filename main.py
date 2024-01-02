ebc1eaa9137d37d7370f34fb57b0ff5b0afabe56
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:55:07 +0000

    bring large march point write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
