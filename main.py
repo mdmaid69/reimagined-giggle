103533ce1d90645dcb607bde98ebd7b965d2882a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:26:47 +0000

    level taste combination meet view

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
