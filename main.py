e6ab60717d22942aadbb92412bc5dce805b58dc0
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:01:42 +0000

    wall survive suggestion about spray

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
