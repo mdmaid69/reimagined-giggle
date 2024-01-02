7689a51959f6e7b915165eef4743e0b21d2e82d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:38:46 +0000

    activity so lonely low dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
