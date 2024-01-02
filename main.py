625f56eb79df9a6a1dbd7048bc324d1f470e707f
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:48:38 +0000

    bag until rank plenty impressed

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
