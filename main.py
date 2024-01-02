4d1cbb904f72f4c7bede58815bbf400d7f851f2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:33:08 +0000

    concerning holdon getback old want

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
