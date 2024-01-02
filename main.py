d8c423e39e56a23cdf02446225750bac268ec7ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:49:00 +0000

    question burn remain regret can

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-print(math.pi)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+for i in range(5):
+        print(i)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
