6f456c3850d8d7eca509e605dff73c4cf4895da1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:02:10 +0000

    cough hour creature interview alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
