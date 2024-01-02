566f36661fa5d515a201fced399e3e1ce726504c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:50:19 +0000

    cry minor direct drive wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
