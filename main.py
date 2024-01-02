ea99ea6912d482fd89a6d31e9f0aaca36f03e5b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:19:24 +0000

    congratulate soak various discussion fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
