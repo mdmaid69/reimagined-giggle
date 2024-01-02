b1dfd80aac6228f4c4f3a7c1d28a4d312cb7f601
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:53:52 +0000

    really modern see weekend separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def greet(name):
+        print(f"Hello, {name}!")
