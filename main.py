79f61870abf0ce1e5b04eb381804277540934286
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:09:49 +0000

    179bite level sticky finally eye

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
