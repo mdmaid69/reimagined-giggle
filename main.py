d6a13643a740d0eff52e0b2afccddbc90f0e799d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:11:00 +0000

    peace street pull under she

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
