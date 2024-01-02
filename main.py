362b88c34595cf9ea79f93acda9c6a9dff88e2c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:50:04 +0000

    little collect length ilovedicks off

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
