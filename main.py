e94bf1d85b5354ec6212c7e8a5000b2bba634a07
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:29:21 +0000

    desperate long length direct hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
