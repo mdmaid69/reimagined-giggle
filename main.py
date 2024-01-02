225d4c480324c2d28b54d7972fae213f0c6966bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:29:06 +0000

    drink building hang consist wind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def multiply_numbers(x, y):
-        return x * y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
