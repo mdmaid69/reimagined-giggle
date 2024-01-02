ae3ecad93c81e2626570c41e89003b0ccc868d79
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:32:54 +0000

    make count on speech dream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
