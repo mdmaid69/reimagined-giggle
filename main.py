e9fb1b9e7499c25e1b35fe6e423a736428ebaa54
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:12:36 +0000

    miss design piece over chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_as_repr(array):
+        return repr(array)
