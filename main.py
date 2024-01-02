1e47c6d7ced268a5d61b212094e43dadcca252ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:32:45 +0000

    seat rob under Cc both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def divide_numbers(x, y):
-        return x / y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
