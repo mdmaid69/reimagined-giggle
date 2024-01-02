02c767cccf84b703bff4d6b993be3cbb61751773
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:40:37 +0000

    pass occasional book getoff takeover

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def subtract_numbers(x, y):
-        return x - y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
