0cd8b082bccffc6e2d9060497302e48a63909880
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:20:12 +0000

    them confusing either any spring

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import datetime
-def get_current_date():
-        return datetime.date.today()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
