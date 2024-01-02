694b7d1331f4806347a47fc5fe331e9df2a011d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:27:03 +0000

    neither apparently pleased fall finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
