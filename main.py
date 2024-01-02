fb7b2f8ec253906e5b938b36d80902573081d5f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:07:33 +0000

    organization tongue disappointed even godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def cube_number(x):
+        return x**3
