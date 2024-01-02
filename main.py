c5b3c879f3819a4f0462e2c02153854708745509
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:05:39 +0000

    careful towards close bet actual

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
