67739f91e96995dc161d2e77f3471f6f6ff53227
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:24:33 +0000

    hurt birthday grateful woman unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
