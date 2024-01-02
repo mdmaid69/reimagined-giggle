85745b078d9ae39952916b04e304d138f29d3029
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:06:25 +0000

    somewhere table means better flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
