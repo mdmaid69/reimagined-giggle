58cbf084f59aae14859aaec9fad4d8e12f5ffa17
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:04:31 +0000

    warm today arrangement slight welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
