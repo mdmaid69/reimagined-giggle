e1dee54b1a4b2e535e34e48fa97420ce56524c20
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:43:55 +0000

    direct minor hospital lock upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import os
+def change_working_directory(path):
+        os.chdir(path)
