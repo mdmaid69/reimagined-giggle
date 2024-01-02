3a558e5e17d1269d9c8662f4647d08ab41091f5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:42:34 +0000

    surprising secret suppose embarrassed rob

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
