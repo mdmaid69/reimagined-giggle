40c3d432504ef612c5ea9b84ff1c0c0ccef86dec
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:41:29 +0000

    activity cope stupid vision threaten

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
