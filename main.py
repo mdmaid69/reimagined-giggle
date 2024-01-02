0f04041f6767e88b7027dafade2043dca5360a0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:50:16 +0000

    keepout wonder nerve pronounce operate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
