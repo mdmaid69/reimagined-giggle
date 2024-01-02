f22c2e8b047cc1cbec9b620262b65115c91df83f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:40:16 +0000

    fork border understand spoil giveup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
