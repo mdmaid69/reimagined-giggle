051b376441c14d6d32949cf143f765a4ae2af73f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:43:18 +0000

    bringback sale noone care what

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
