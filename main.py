7dad26d7087be2a7017ca4e1efa5a3255c8bc846
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:10:16 +0000

    past secret thinkover promise string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
