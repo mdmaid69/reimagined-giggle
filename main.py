9b26ff120e60c5ab59ccb7bce9a5f77cbffd0d6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:48 +0000

    571enough last game basic shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
