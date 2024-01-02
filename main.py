7feb1157a01a8943320ba6186611852061dc1096
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:52:10 +0000

    convenient cook turnoff have rule

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_max(lst):
-        return max(lst)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
