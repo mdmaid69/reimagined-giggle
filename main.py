6f70089dd26376e565d1f324bb87003f49470155
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:08:41 +0000

    lead after aim urgent of

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def sort_list(lst):
+        return sorted(lst)
