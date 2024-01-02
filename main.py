80096ae81ceb768307badc72aed52ca29ed62bd9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:41:40 +0000

    pleasure respect trade bottom wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
