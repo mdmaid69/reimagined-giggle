9a1ea8380d5d00cd5f9dc0ea3818f8a8365c8aea
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:13:25 +0000

    follow leg fairly especially how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
