671e080a45e03e6898c97eb2ade21c28e636a314
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:40:16 +0000

    treat environment some grow corner

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import random
+print(random.randint(0, 100))
