7af5b27dc5e3994fe89d6bc867079f020782bbd8
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:20:25 +0000

    street unpleasant why pipe improvement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
