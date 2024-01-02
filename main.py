dedcf885fc9e6aba03f0b013e4a9bc6b1a29039e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:49:09 +0000

    swallow language room circle somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_rectangle(l, w):
+        return l * w
