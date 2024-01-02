b13a4392973f5f8a0ee776e54aaee5283f42d8b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:27:21 +0000

    thick listenfor fact flat respect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_speed(distance, time):
+        return distance / time
