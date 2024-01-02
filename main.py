3f7dd0abe9ee1ff7bcc6888d5b7a7fbbcab90116
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:53:33 +0000

    still trip chance grade adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
