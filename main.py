e061048ebf09862a51046e53f43aabf52dc809b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:35:33 +0000

    some male most talk OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def create_user_dict():
+        return collections.UserDict()
