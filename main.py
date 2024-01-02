a5d8960cb63e11b80670cfd16d1cb0e43c66ac78
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:15:38 +0000

    alive cry song before patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
