286bcfbfd75355406a08ae63a8697aab0eb29a04
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:54:24 +0000

    home manner connection roll somebody

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def count_elements(lst):
+        return len(lst)
