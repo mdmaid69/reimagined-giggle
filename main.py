5ecd52982b6004017df9d2b16b949ae0309cedce
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:11:04 +0000

    chin lean Itself shelter attention

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def square_number(x):
+        return x**2
