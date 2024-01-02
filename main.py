2cb18effc59a6f6434761a283028f221b9f75388
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:19:44 +0000

    culture leave minute pen disgusting

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
