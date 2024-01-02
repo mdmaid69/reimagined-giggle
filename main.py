5bdfca79966d5e0bb80a22f92f94dba2dcbc09fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:31:09 +0000

    protect proper prize murder estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
