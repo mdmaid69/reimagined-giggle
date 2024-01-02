86560ca4f1e188d4c3f2e3ce0554feee8ac3b82c
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:47:18 +0000

    congratulate name lonely manage shock

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+for i in range(5):
+        print(i)
