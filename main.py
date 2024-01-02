15b0cd4d95f3f3f535f37e47ea27718dfd2ec8cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:29:19 +0000

    birth goup connection valuable transport

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
