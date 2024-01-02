19bf3539db618a66223131ab26ce80a9bb127eb2
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:17:39 +0000

    carry get furniture trip tooth

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
