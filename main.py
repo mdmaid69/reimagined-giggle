39d515bbba560682df2e04d16913b2831f01a057
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:34:26 +0000

    operation bush invite description talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
