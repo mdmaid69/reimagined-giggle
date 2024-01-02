8dee89403c89a4533cae497ce2a12aa1c76908bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:28:06 +0000

    combine important method hang service

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
