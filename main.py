9c2178aa340c21178d3f588c5e5a7bdc83a44441
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:42:33 +0000

    exchange owe tip finally tend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
