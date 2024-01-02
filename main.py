5b2263393bf08e75315f8cdb83fbba664e9eb6f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:18:54 +0000

    Pp understand might come legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
