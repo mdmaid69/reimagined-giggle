ac069b043ee4fe462a9d956a8d9710c6c7d5ec54
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:01:44 +0000

    regard wide ear risk describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
