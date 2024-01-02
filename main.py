f1f8e55c6f07ac407f3ecbe839b23fb306c1860a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:55:14 +0000

    stupid correct car responsibility wind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
