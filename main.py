bc0761c2d4db454d0fe8bcd6425074d8750b11c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:05:50 +0000

    afraid naturally serious start discover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
