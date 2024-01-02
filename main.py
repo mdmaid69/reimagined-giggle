53d39b6ca07c89f666e978c1083ecd0880d06327
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:55:33 +0000

    legal space forward guard apparently

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
