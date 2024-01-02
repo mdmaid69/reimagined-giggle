fc44741d4947d9e6b8b23e81bc38239e06ea946f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:13:38 +0000

    arrange across rush certain safety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+def is_even(n):
+        return n % 2 == 0
   import os
   def get_file_name_without_extension(file_name):
         return os.path.splitext(file_name)[0]
