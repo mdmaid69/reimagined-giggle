5660634f3d5ca313ca1bbbaa32caac349653ba5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:35:51 +0000

    mess nature hard below generous

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
