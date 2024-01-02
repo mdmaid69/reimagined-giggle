a4af1706a2d85850f470910f3424affecd47f69b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:27:32 +0000

    across worth regret start before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
