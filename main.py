31ede1ea98da3fc4a201b3144122f465af93cf7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:06:11 +0000

    smile embarrassed thinkof happy else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
