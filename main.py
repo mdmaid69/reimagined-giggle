20b417b392b6ec9f549cc91e876f308b00eac365
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:18:55 +0000

    send cause wealth fail anxious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
