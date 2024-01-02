62af372c6f32c3ab86d5282f11a5bc95d9df9e1f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:16:25 +0000

    above hear band embarrassed press

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_power(work, time):
-        return work / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
