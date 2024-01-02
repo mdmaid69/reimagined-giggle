2415b62ec8810533a61fba5fc8a7b1345494364a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:34:52 +0000

    open save persuade rest family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import platform
+def get_os_info():
+        return platform.uname()
