5cd5172f7c6ba0576374d82f2c13f047e7aa48ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:49:55 +0000

    degree low behaviour minute accordingto

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
