8fe9b13622f88e878f1822fd02e6728b3f7482f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:50:50 +0000

    throw post gather look flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
