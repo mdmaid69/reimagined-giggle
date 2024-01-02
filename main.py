f134092c762a13f3fff9f84027e592f6770a7eb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:27 +0000

    concerned skin pile delicate bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def sort_list(lst):
+        return sorted(lst)
 import array
 def get_array_index(array, item):
         return array.index(item)
