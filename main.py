52b9b3108388295e3fa57d9c129e6e1037ef6c7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:40:01 +0000

    comment edge use plastic combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
