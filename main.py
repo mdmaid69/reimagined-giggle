63b31c721216d41435dc74c8915c2f3357a979f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:43:06 +0000

    flow interview swallow neck track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
