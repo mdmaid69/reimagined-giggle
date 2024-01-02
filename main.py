60ab7198d53204a7dfe5dd2bb2a56f8331605f95
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:37:53 +0000

    promise hit flow fortunately market

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+text = "Hello, world!"
+print("Characters:", len(text))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
