0583e8b34953f6099e5c3a0142e0d9b0671fb390
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:30:15 +0000

    birth giveaway giveup spray occasion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_current_working_directory():
+        return os.getcwd()
