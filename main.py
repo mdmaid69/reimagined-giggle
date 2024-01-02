def614dd51bfbd2035dd75455eae25c47ef1c0e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:33:37 +0000

    common skin upstairs arm something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
