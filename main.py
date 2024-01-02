ba2196cdc346980446d951b8dc7b0098e416be97
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:57:37 +0000

    recovery spare nowhere sink so

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_item(array, i):
-        return array[i]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def is_even(n):
+        return n % 2 == 0
