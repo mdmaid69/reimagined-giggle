96f29967ecd67ec6f2c2ff837216af9181c43c9a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:07:20 +0000

    family welcome store refer neither

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_work(force, distance):
+        return force * distance
+def find_max(lst):
+        return max(lst)
