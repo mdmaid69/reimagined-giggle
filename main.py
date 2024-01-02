b48c6e1fdbf23d0b458e156c445d13b062d5689f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:48:28 +0000

    thumb turnon reason ball under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
