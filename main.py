051a844af15e1d7f6e8118607f75e25fb292d2d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:37:27 +0000

    themselves why sense eye mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
