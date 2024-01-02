a34bbd64349ce97d8f2abf1db06dae9b5772a4e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:48:19 +0000

    twist shoot cheat declaration clearly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
