692a5ee27aec88e8ef16ff971624dcc2658befe9
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:37:30 +0000

    bank I shout twist strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
