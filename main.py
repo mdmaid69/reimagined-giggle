58baa8d8206e85fa0c36f075e5e08c8aafa978f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:55:29 +0000

    open target form Nn play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
