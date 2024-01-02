0b58c0ce39f5110c73ac178cbc7aef4b3f1d61be
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:32:11 +0000

    please actual vanish damage radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
