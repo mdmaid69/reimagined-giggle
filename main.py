9a171aaccd1ed3b97b068404782efc98614eb6f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:35:58 +0000

    side row unlucky track story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_current_directory():
+        return os.getcwd()
