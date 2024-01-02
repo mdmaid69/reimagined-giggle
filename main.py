eb68aef8cdbb343106fbad8a660ee6ff71b14988
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:28:32 +0000

    please shock partner slice wipe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def subtract_numbers(x, y):
+        return x - y
