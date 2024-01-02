76cd5d895e8d39ce7b7543d83a25c08acc4adaeb
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:57:13 +0000

    greeting odd burn noise sense

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
