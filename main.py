daa3b6b590bac19bcdd40aa8486d5c5cc808a933
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:00:50 +0000

    situation track emergency house bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
