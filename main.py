3c31894401405e752773241fb8e7642ddafefb26
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:33:53 +0000

    ask sing set skin happen

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
