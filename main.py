5f0392b2aa934b763988cfbb2d9e41ebe0c40235
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:44:49 +0000

    cheat judge ability book kind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
