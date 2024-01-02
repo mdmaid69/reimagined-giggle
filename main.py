80b2b910a7cd1c16c511c6ca376bdd41f186a6f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:23:33 +0000

    tough breakin better just argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
