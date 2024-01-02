29a7e79f5420764200205cb882703d3182ac0460
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:43:53 +0000

    holdon whole list cut use

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
