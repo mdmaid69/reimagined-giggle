986fcdbd6fa29e975629778e47a022950c625cf5
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:48:05 +0000

    stand before even turn spot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import time
+print(time.time())
+import getpass
+def get_username():
+        return getpass.getuser()
