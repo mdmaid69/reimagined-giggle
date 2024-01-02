a5b15a3a350c5ffc5afd66ec0b6510fad27fe9ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:42:58 +0000

    and platform cut damage tend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def square_number(x):
+        return x**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
