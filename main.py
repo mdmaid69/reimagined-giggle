43077e3cb40e56846aa439a14febaf9a89591f95
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:00:38 +0000

    agree turninto we thankyou fair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def reverse_string(s):
+        return s[::-1]
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
