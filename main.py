7e87b2f26aa8de49522a02366d073145d7331f1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:45:17 +0000

    service debt fine male wide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
