059f535af27cabaae86ca1609a3fb9cfd85b19df
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:42:40 +0000

    pleasure wish could after regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
