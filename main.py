340cebbf15f89434ceca230beb2afe7047f71eed
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:24:47 +0000

    from cry copy outside reply

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def add_numbers(a, b):
+        return a + b
