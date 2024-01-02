0aa3e9f0b98829bdfdd200030b4d70cc4eb61ee5
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:45:58 +0000

    slice beside performance benefit warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
