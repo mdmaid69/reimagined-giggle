e7b0f1def4fd58fa1fdf83ad916cf2ebbf9673ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:30:00 +0000

    mistake ride wave ring lost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
