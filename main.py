40880661ddff61c68ba67e56fc7928c361839e5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:22:44 +0000

    agree inquire flight perform ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
