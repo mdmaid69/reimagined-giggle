0fb0d2970eecc18587dc2cdab9d33dd21eca095e
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:21:19 +0000

    day ground characteristic contents play

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_work(force, distance):
+        return force * distance
