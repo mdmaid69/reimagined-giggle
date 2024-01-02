438faf5b05e2789e053494a9cbb0c920b28277d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:27:40 +0000

    example avoid stick region connected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
