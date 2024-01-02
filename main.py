6506f430311a496f2b41fe24cb9130b8ab10dbf8
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:42:27 +0000

    attention scene preparation field position

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def square_number(x):
-        return x**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
