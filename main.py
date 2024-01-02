b1133b2018a5050e3d2e6d41d30b26a13ca3910e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:42:12 +0000

    twist below ready boring study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
