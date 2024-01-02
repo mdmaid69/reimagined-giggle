a9ef0f7ca8d380993c2fce3d3071a0713ae00e67
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:17:13 +0000

    space comfortable plain ring reply

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
