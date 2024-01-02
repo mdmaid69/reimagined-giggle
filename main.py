f1d0664fb9e997c0c72138d69d078e0def402a40
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:42:41 +0000

    tradition wealth head enthusiastic bright

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def add_numbers(a, b):
+        return a + b
