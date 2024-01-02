114bd9dda14d0a2fcde4821840d47bb48dabdea1
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:09:09 +0000

    turnoff naturally ours Dd take

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_bool(array):
+        return bool(array)
