03f15af017a4aea2e851488bd897b0def3cbe109
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:52:29 +0000

    see though someone hang black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
