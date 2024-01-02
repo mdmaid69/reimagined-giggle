6924126b9a287253e0a6766f92dd5f561d3836c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:17:36 +0000

    ignore light passenger recently concerning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def find_max(lst):
-        return max(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
