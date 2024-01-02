c13001ca17ec0b7110ea890d990a5fb85b7b29ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:48:19 +0000

    please everything listenfor health smoke

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
