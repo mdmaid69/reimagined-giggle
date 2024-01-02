40599c5d0465f32da5e322e45db7700f557a80f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:37:15 +0000

    wing fortunate identify street throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_binary(n):
+        return bin(n)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
