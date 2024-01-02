b049165b678fe1817b9f7606640431708b5f263e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:24:32 +0000

    also independent process tip tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
