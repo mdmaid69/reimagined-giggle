1865a0ce23a7f50e4915f861e64aba10f5b7e237
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:14:03 +0000

    honest workon interfere runaway fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_list(array):
+        return list(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
