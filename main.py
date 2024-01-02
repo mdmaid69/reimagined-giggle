1bffc7b95406eb4876f3f6e582b552b9b8d52d18
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:21:01 +0000

    least approve interview bear schedule

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def reverse_list(lst):
+        return lst[::-1]
