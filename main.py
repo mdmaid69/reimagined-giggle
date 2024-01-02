263a38b2e74c87cc7321c1c66d2f6e44218bbaca
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:45:59 +0000

    track year flight fast suggestion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
