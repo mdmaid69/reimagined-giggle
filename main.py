11b714f4991be8194db94aecb1276d3568f9f8ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:07 +0000

    wake very quarter search hide

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
