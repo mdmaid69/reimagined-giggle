8869d0cfa0eae5905dd080391d3a664fd75fdbd2
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:03:09 +0000

    train aim somebody related fortunately

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
