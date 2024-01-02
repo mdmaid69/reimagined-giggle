4f4c851955b05fa37de891c02499c36224fa5375
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:49:03 +0000

    hungry choice treat contact cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
