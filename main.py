d864fd3cdc3e68eb33b612cc738fc9e47bca00a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:31:51 +0000

    creature adjective high leave shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import collections
+def create_user_list():
+        return collections.UserList()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
