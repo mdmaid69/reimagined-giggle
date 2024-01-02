ab53ca8cb31002b901bf197700612b4f28a62c70
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:28:42 +0000

    exercise comeup protection spin proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
