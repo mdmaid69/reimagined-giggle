c0eab2c54443c9f04bdff8616b8d84ccc9b304e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:32:58 +0000

    onto on image well difference

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
