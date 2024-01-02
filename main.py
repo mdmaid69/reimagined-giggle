09d9ade376b5e0c8960dab26960dde062e4edd0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:45:49 +0000

    mouth measure nothing whichever interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
