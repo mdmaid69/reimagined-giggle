2e620e71bf3892da09b660105e711d0d21d5780c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:35:17 +0000

    organization picture board steer bag

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
