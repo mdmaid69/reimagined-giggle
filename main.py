552532c2de4204d26a7cf4546a9d13f7cceaa1f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:49:43 +0000

    watch neck flat hide road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
