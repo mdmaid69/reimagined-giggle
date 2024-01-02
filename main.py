e19ed2652f2566a331721bc4535fb8bcae6dea81
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:13:55 +0000

    temper ready concentrate image mystery

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
