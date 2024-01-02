c762144e3c944b6e2cf4b22435637f6b9f1dfd8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:23:14 +0000

    occasion twist list yourself than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
