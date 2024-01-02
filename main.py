a445d1329fc7752acec0525df8c7c397de967bad
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:28:37 +0000

    tip loss settle order yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import platform
+def get_os_info():
+        return platform.uname()
