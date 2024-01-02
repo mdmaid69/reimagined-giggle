07e3412539fb6badcf9b62cbe9c5373b30e3121b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:59 +0000

    care live claim 569enjoy phone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
