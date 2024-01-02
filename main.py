11cca487f0418d95f39fa166a02794880afadb46
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:19:00 +0000

    calmly them mouth immediate water

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
