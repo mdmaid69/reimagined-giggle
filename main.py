da3ee0fbe2a994548e0176fffe966f283fe5699c
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:29:24 +0000

    once relax regard luck violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
