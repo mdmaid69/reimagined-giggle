5ebf8ebbe5ac990c4af0613fc34b98e68941784a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:22:35 +0000

    clothes foreign 567enemy uniform introduction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
