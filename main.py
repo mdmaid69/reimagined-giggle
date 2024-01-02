19e2282915462eb0bedca6b20a276919041fb57b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:14:02 +0000

    cinema show runover anywhere whom

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
+  def convert_to_octal(n):
+        return oct(n)
 import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
