41c2548ca675dc52126db611438b6a58f981a06d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:49:08 +0000

    smell whistle snow believe large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
