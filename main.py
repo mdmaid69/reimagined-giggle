1e866afbbabe985b5b2b28dca176d8e63f4bf271
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:16:03 +0000

    afterwards door excuse snow recommend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
