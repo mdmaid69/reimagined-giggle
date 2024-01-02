b0fa110aa1bec429fa6e6e81bcec61636405d93e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:27:03 +0000

    by game slow tax section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
