f32d64097186a8d65edf4cb14db16d1aae9f02d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:42:33 +0000

    difference copy famous complete dick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
