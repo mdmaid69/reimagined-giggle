4ae8009657c14df4b459c0010c3ab130bf6a9aad
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:00:27 +0000

    slightly interruption scared number silent

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import array
+def get_array_as_list(array):
+        return list(array)
 import datetime
 def get_days_until_next_year():
         next_year = datetime.date.today().year + 1
         next_new_year = datetime.date(next_year, 1, 1)
         return (next_new_year - datetime.date.today()).days
-def add_numbers(x, y):
-        return x + y
