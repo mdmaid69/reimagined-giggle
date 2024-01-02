fcf55be7c75a4dc18dd467ced80ce2a8ad23e57d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:38:38 +0000

    squash recovery tax game act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
