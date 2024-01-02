8c07fdc1ecfba7bdcaba35b9da3a0bc77659fe79
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:40:46 +0000

    damage theirs home food protest

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sys
-print(sys.version)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
