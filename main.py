008ab41a08f39572494a8a4c2f9ca4ce7eb9bcaa
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:13:14 +0000

    declaration protect weak broad crowded

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_string_from_array(array):
+        return array.tobytes()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
