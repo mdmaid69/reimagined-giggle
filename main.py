b1c9f398a471ff45c4ae935c9c629613f13c1359
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:10:21 +0000

    young write patience tomorrow dress

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_length(array):
+        return len(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
