19e86419ad1c537279148297857d64d107cb9bba
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:09:48 +0000

    substance drive deal welcome zero

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pressure(force, area):
+        return force / area
