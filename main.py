67ace5e570a455ec3af29d0729182a6ea6b246bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:37:50 +0000

    emotion right bunch statement deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def square_number(x):
-        return x**2
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.ctime()
