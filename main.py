3eef5f6f93499d5d8b2b02aa0e14159d2a67aa6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:02:13 +0000

    smart yourself performance vision rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
