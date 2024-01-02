daa40f7851ca9212a85995e4b60a4c8bfa019a5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:38:48 +0000

    limit next factory issue anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
