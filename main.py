32dc89c27a27d093b13b341307418b69643cdc7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:27:09 +0000

    open cost match approval uncertain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def reverse_list(lst):
+        return lst[::-1]
