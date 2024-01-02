f75981b0d8d6c72c69f9caa8bd64e4e92177f29d
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:25:29 +0000

    regard trip I relief work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
