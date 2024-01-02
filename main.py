9810c9833e7f18296d75e75f2bd9002be84a1fff
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:14:05 +0000

    tight tip father depend slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
