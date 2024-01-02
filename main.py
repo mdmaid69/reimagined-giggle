92b80b48f5e5a9dd5268c8e43e9c6c9e3c166c1f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:12:25 +0000

    apart right play really naturally

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
