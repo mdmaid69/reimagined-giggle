ecff504e13d97e003764530359f42e62e495aea5
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:25:18 +0000

    this deep wealth usually cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
