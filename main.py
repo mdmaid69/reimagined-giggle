0674f84fb054cb9f373c1ebe724093cfa95704b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:00:41 +0000

    major among risk happen crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
