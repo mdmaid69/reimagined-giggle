2fa4d06dfe028891ad281ed88dc9df53eda3c88b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:55:48 +0000

    describe goback(to) failure fall culture

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
