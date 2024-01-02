e9d6c543806f1983ffe4a5a51693272e21204c19
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:57:09 +0000

    little blow month noone sort

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
