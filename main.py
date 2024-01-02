bd74f70b69c948fc5f3b1f401e062f4bd42f2ee7
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:22:56 +0000

    afraid treat minimum murder mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
