a870414356c6f8e12156513b334b3f2d812b88f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:52:09 +0000

    charge stop city sharp advice

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
