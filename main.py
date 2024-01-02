bbb181d991c2d1cd2f76b4affc55cbb1d403c132
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:02:11 +0000

    environment bottom well where objection

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
