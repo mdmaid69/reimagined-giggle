dbf1c165badc5c240b7d5a141c39f397c4af65f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:45:54 +0000

    appearance moment dry add solve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_string():
+        return collections.UserString()
