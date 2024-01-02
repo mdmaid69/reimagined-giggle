447a5f28f29d6282ae13c43bdb179232189f47fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:41:19 +0000

    funny water accommodation determined except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def multiply_numbers(x, y):
-        return x * y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
