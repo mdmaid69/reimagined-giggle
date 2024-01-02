e88d0a914f0c286a5576d73981b67aa005a212c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:16:53 +0000

    sure level occasion effect correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
