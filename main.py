b913b61c4ecd223ebdb3cb89219f7c34fb15c42b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:39:16 +0000

    arrival snow ours artificial trial

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import sys
+  def get_python_version():
+        return sys.version
