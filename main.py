f59fefbd7ec819a25026779767bedbac80cc16a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:52 +0000

    debt workout ignore certain usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
