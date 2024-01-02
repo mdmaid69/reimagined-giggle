1d2adf94679a625919155474950a265662a63924
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:45:59 +0000

    maximum climb reason subject small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
