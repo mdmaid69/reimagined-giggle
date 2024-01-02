e9edc85a0de7fe425763ce6bf3fb78978c29150b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:17:21 +0000

    clothes special achievement convenient crisis

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
