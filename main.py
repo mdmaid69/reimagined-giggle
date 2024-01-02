69a9315c03ba9fa97727454e20e2a0412628a98f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:22:42 +0000

    foot whistle them tone software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
