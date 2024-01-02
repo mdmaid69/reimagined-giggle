3298d622c3020b01cacde93cf07da67ab1f8afd4
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:04:54 +0000

    increase let about gas about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
