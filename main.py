8d3bd72a3a1af30dacf870cc7334d22e3f33cc35
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:35:25 +0000

    state along unusual rough information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
