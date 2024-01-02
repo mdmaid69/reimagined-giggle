adcb65e5aa143c7382ec1bcae43fb81b867dab1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:51:16 +0000

    preparation petrol scrape lock satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print(sum(range(10)))
