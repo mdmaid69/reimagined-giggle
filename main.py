dc49c59d23a18cfc5f8db494111352c9482fc716
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:07:14 +0000

    still callback season touch among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
