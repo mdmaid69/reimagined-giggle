09abc461834a398dfdd4268199c15b2af16743b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:12:23 +0000

    sell be road favourite tall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
