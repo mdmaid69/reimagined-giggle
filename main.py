8e95be79ee1f79e8d2b7fb1a44b407d86ad5a28f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:04:15 +0000

    rent 571enough major battery occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
