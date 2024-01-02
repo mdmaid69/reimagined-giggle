f593f97bec5395f337ff70bbb8c66ea611bc70c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:42:51 +0000

    plenty Bb spray cover disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
