4a8e56a6897dca2555bd446f4fc853e1edc77de1
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:01:49 +0000

    question getout Ll in laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
