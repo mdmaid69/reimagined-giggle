90ffd99198594f711d30b34d39b94e8c4e0fbe19
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:16:55 +0000

    injury town stage talkback brown

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_item(array, i):
+        return array[i]
