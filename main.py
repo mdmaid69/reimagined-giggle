7dda4c3f9fda6ceabcc9ad2ece8acfa646615cfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:11:47 +0000

    mail perfect easy commit mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
