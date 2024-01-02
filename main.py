4b08dae667a6a2aa7d3d154d71f1836d3581bb47
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:56:12 +0000

    interested though march urgent flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
