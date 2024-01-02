ea3d8127310e86e7dec631cb96aa4775452ba2fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:22:00 +0000

    scene story pleasure short charge

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
