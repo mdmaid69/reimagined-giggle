2a57edf8e069097cf39cc17b85872f94a3e28159
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:15:45 +0000

    length uponon plant count cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
