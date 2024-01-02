3248b35e5040bcf5d121467a14bb7f3899e92b79
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:14:10 +0000

    market accept lucky however think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
