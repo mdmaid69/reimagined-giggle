1019baa6438fdb8acb4a38a75fe57aa5c5fedb93
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:07:43 +0000

    tight whistle pressure pity pay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
