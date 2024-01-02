0366cd6936b6644a19423592970693bd3a4f9bf9
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:23:51 +0000

    middle principle computer cheat recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
