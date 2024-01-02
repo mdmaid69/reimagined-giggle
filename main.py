4cf3e08c02231345ce37f4b77f253ada4c653f25
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:06:09 +0000

    challenge comeout pair callup giveup

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
