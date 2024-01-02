b8f19ed16e9b49a2d68ed8bc0adf3b325c09acfc
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:29:22 +0000

    night many actually stay hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+print(math.pi)
