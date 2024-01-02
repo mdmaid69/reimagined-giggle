2ae09d492523244006002326eae13c28c93ec31f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:57:17 +0000

    factory sick niggaacademy exercise top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
