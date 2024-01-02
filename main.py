fade093daf3a996d16da92d92e4865a69b2ad5eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:48:15 +0000

    comeoff gradually gooff live thinkover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def sort_numbers(numbers):
+        return sorted(numbers)
