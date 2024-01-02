70749293a691471250e938b4fc018a9c066e054e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:06:02 +0000

    cough Ii inform protect maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
