d8385cba0d91d6c726d9ec6be030535430bf5ba4
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:54:55 +0000

    sort method considering my project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
