3a22eb3523dae02fe8d90e3ba188dd45d9daff60
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:01 +0000

    Ff convinced yes go within

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
