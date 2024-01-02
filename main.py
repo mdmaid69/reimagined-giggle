ba1d032bb2c7f687ed94088f779f4b26c3c37b6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:49:14 +0000

    shout there exploration future after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import collections
+def create_queue():
+        return collections.deque()
