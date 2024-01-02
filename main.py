2225291054fbdb31aaef1b0851590b8e8ccade8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:15:32 +0000

    chief aware actually skirt worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
