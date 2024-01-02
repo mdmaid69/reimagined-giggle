5d929b4c4bf6b94e36d3e177e775df8ac11fa305
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:29:00 +0000

    boil many figure tap cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
