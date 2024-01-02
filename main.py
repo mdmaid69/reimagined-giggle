b4dfbada39d76721531b40d90ff23b13895ac507
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:05:44 +0000

    provide shop quick mystery disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
