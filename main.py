87e962058f75816a9c335fc905cc003547fdf649
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:26:58 +0000

    terrible deliberately release comefrom fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
