d6204eac941dea5853cbb40f4a4219f55ce2f06e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:59:41 +0000

    mad block sort pot goout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
