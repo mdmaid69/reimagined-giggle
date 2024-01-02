2db696b134fc5815c34a5e20ce60e9cd77ba2e8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:13 +0000

    than connection quiet fierce try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
