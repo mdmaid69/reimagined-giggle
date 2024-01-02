4fbda5eadff4d8a2bf84d9dbdd009788fdde202d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:31:29 +0000

    mean hers which authority programme

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+print([x**2 for x in range(10)])
