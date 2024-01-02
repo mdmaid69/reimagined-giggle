8088a10996c976b52f77b0e72a9c8729af4d1280
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:18:24 +0000

    provide wise tonight ahead ticket

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def clear_array(array):
+        array *= 0
