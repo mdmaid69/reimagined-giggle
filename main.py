36b3a8a2385baf5d792f0559e214a503a51b0ef6
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:47:30 +0000

    573entertainment same really angry impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
