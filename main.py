ff06074f6e48460600c69065a81a76b2bf46f5f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:29:42 +0000

    final transparent official square clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import datetime
+def get_today_date():
+        return datetime.date.today()
