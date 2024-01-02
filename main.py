dfa5846276364a9a90ffe3d45f869adcf7a77d53
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:59:56 +0000

    eager through thank sit empty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
