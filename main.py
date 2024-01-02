08cbdea18348a4ce7105747509c2e05f64547aff
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:37:33 +0000

    age goup right mine house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
