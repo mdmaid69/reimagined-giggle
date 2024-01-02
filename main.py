b9dacbf02b9845c2ef9a398d002626cf69147806
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:27:05 +0000

    quality attractive unless any middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import time
-def get_time_since_epoch():
-        return time.time()
+import collections
+def create_stack():
+        return collections.deque()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
