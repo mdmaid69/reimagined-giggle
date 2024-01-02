2a1ad2e59df6c0b9e115447b5cc7feede438d538
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:39:25 +0000

    collection voice conduct unite turn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
