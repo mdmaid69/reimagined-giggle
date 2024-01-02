13be449d7bd7c273583e41f3279bbd4d072cbf7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:14:39 +0000

    miss attach Mm Ff judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
