6eb32101a3bbc57003051315e3b9ad5726f9964b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:15:31 +0000

    need see some basis around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
