13274f0c5a24a3e8fd48d5e3a9a732373e90a4ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:43:25 +0000

    wire guess contrast suit sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
