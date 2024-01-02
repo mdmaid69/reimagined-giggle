2349dbda72dfa0b4949034131eaff059e2125bf0
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:36:40 +0000

    dinner 179bite during cover bar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
