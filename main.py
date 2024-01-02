0567d86a32ee3f356667ec8889da7cf43a755e6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:23:30 +0000

    workon food smooth gap dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
