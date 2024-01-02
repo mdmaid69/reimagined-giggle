084e92bc2730f2c60d5f17174199780261608db3
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:13:28 +0000

    represent declare wise press appearance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
