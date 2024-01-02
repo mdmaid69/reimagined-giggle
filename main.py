204350559fa51324026569d4fabbe25e51131bae
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:10:19 +0000

    police whisper belief opportunity whistle

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
