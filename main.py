dd3491ba85fdd38768295337e2829e396b2b6461
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:11:28 +0000

    put regard turn trip touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-print([x**2 for x in range(10)])
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
