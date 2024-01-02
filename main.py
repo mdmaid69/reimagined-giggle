bff5519ddf0a5d001d33c8ebb7ecce1d0339af66
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:06 +0000

    content murder theirs mouth path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def divide_numbers(x, y):
-        return x / y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_odd(n):
+        return n % 2 != 0
