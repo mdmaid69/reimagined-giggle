5ff1a0981267cc10a9ee2bc373bcc572ac927f7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:55:07 +0000

    relief earth season below sometimes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
