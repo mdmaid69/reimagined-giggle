1f4b5090163c0e2870bdd63d6cca9902a77cbfd5
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:38:55 +0000

    journey recover frame about taste

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
