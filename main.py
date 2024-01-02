14abab21ecf5f6c92bc7a86f6c00f337400958e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:34:08 +0000

    contain effect under especially day

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
