f860fa1c9b2652680abd52a4639f84fefe7b972c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:35:09 +0000

    land hang about bad distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_unique_words(sentence):
+        return set(sentence.split())
