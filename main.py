5237e2c826afc72b22e28bb8a47dee4385eb1dbb
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:32:21 +0000

    exhibition angry nigga cost copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
