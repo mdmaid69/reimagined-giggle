399c79873bfa00c60eafffd85ba571ed6701d74a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:15:17 +0000

    environment immediate could rest discuss

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
