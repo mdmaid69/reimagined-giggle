7154aea672f4aaf7488c222302b364d1e2ec77aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:44:27 +0000

    ask minimum fail question tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
