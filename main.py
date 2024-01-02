7600de324ccd9633c1bd774ac745638c44f5457c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:25:54 +0000

    model comeover like measure pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
