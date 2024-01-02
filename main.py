354b4d960638f9f564c41c36499631d74ba8407a
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:28:35 +0000

    might difference climb importance hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
