1a4e3e50ee5ae0da252975d9db88b10a432be027
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:28:00 +0000

    manage sure lookaround even pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_acceleration(speed, time):
+        return speed / time
