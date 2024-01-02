72ce632939324c0502bbc464ef3295759ab460a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:30:17 +0000

    circle blame weak turnoff between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
