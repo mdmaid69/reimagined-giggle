868187af86fb081ad51b4bcc93089a3d3c4abd2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:31:06 +0000

    science force relation smart friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
