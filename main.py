df00f3f357b004db4e03c251be4efa55b72f1d26
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:19:57 +0000

    smart pool storm track friendly

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
