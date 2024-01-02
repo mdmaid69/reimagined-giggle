978d2f7109a73e748ffe7c22c74bc30b5b7d12ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:01:22 +0000

    loss constantly sensitive national trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def count_characters(sentence):
+        return len(sentence)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
