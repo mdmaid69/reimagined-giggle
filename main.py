8230a52bbd8284203e09d5a8e031d71fd90a5786
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:22:44 +0000

    exhibition tie pinch turnon convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
