ec5b438494b4e699f6bf0d0a188a788e328e8885
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:18:04 +0000

    welcome next through shop Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
