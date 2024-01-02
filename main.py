dc9fd1731d01bf08c646b65510a9ab7138fb41c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:06:35 +0000

    muscle force cheque notice least

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
