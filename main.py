c071de1c10f47e818db1fe7c5b279ff334b13b04
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:03:27 +0000

    post family choice my talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import time
+print(time.time())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
