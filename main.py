b422a7ee224ea6737df9377a6a30c523b13847fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:24:55 +0000

    cross save nice complete negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_queue():
+        return collections.deque()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
