a1e125e238bc579814da4448d5b69b0b6a2c38bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:03:27 +0000

    even sign pause splash dream

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
