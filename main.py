f74ed9a7ce26f223418705af50905c04825859b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:42:49 +0000

    getout tongue desperate create far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
