7fc4c3b99a93c48090b5e55893c006899d422f4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:25:22 +0000

    seem miss determined vision guess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+print(math.pi)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
