8e0c5c6a00afd432985ea71748a1962bed327d4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:22:11 +0000

    contain intelligent visit lift Kk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
