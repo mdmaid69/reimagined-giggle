65b42189e5fda8fb60222679232fd8a7d2c9877b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:45:07 +0000

    label far blame strike still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
