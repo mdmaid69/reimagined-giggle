8bd08c0f51f98cf1b8cfbadc4554423c7b05492f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:24:01 +0000

    note breathe test anxious spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
