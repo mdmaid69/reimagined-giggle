fc574809e1fa002dfa74aa1b555aaff384e44155
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:59:44 +0000

    funny powerful service float patient

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def sort_list(lst):
+        return sorted(lst)
