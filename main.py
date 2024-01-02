c6f5e15e4c15e1c73e905baf9720fba5b33569b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:37:41 +0000

    sink regularly example everywhere star

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
