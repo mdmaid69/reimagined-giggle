f5f678bd82c8d021a530fbc9d1de19268bb21276
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:29:11 +0000

    slow spoil feed deaf start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
