66c921be4fced17ed81a4591569288bc94340a60
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:33:51 +0000

    sore slow moment wind represent

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
