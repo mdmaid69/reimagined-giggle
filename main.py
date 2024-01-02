ca0848ac4d37144605e88dfd3a59a11b7dbd053f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:14:28 +0000

    cross odd nurse position operate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
