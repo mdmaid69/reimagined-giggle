e2cfcf0a37e1d6e326c04ebe43cbb9704932f28c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:55:03 +0000

    how coffee bang pinch distant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
