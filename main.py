c77e649e7cb814190505b535263baa75ebe501d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:22:37 +0000

    trouble 572enter female almost promise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
