c774b00a0db09a1664599015124565f2eb963fe6
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:24:42 +0000

    now hit question clean instrument

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
