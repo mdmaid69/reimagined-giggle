e403e196090222d5ededb0970cbf50e3020f924c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:10:04 +0000

    still material mess door day

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import numpy as np
+print(np.array([1, 2, 3]))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
