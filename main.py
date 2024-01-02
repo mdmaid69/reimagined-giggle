bcecc72fd5fdaa67aa294efbb75e69d42a629ee0
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:28:03 +0000

    accordingto mention thank aware this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
