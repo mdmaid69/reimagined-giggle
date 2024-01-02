dd5729fbbc24b7628000a2d03c91367a553d8965
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:58:53 +0000

    belong tight family long bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
