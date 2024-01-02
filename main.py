d7f87d36099ec326a237b97e180eea3d0cdf53ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:14:06 +0000

    tap favourite emotion life tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
