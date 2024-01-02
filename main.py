01aa445917ed9c0e1fcbbb90f695242d900e11af
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:52:38 +0000

    apparently phone custom difficulty mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
