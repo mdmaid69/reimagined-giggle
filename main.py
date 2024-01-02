7fd212a8e2514fa5dfe6c8ef1c7829bfef2c13cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:13:01 +0000

    old hold secret control corner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_power(work, time):
+        return work / time
