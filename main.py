4c897c8c31cb44937468d4a3744ba10c62f8d355
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:52:40 +0000

    ring crime future breakin complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_repr(array):
+        return repr(array)
