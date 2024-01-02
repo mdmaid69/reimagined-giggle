cac7b692e1b7fc4e6d78f57cfaf88de0f697a80a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:28:10 +0000

    medium shoulder downstairs book shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
