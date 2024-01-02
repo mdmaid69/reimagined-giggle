a3d4c43c8d6269a4ae38eae79db6efe7e1d062cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:00:11 +0000

    strict rest edge guard main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
