6ceb91ab5f09146071a36e282809d27f0a320dbe
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:17:46 +0000

    trade people sharp information think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        return sys.version
