7a622edc15699b25194956b719e8ff805280170c
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:35:02 +0000

    machine Ee bin murder geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
