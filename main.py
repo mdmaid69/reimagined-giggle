511cf6cc380980c393d5d4013ce40a418d7894e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:55 +0000

    club shape down mouth limit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
