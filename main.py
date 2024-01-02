a486f45afdd616345d85ceb5e09ed2b3aceb1809
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:00:46 +0000

    appeal compete message legal string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
