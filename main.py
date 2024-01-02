1e1e18745f47659c71def40b859c5135dd9a76ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:43:58 +0000

    in deep bored space drink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sys
-def exit_program():
-        sys.exit()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
