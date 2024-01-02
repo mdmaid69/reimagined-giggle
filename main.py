a3d5617c5460a0b4111a852017d22df3340f0345
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:44:49 +0000

    thinkthrough bored plain interview introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
