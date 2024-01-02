92397baa71f5e599618b3c1e9954bb69be03afec
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:50:44 +0000

    available laugh top turninto song

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def count_words(sentence):
+        return len(sentence.split())
