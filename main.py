a8c9a7f50702bc041397eb1bd826276dce049276
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:53:38 +0000

    cross patience uponon gather moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def count_elements(lst):
+        return len(lst)
