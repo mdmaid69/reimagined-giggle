348140e8703191df3190a7c428a374e5821dbd6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:56:38 +0000

    walk class bury force reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_area_circle(r):
+        return 3.14 * r**2
