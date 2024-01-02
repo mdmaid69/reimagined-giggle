c67fd40a91396dc5b39818614e009f3e39a906e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:41 +0000

    wish welcome order hungry world

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
