65246b8b492d4389ff1895de7f2485a6dc06dd6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:09:49 +0000

    house people blame remove hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_directory_name(path):
+        return os.path.dirname(path)
