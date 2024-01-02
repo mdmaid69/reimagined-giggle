866c3c9f7f38863bdcd5956e0fa1880ae30985fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:41:21 +0000

    please entrance tough storm there

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
