831c69358d901e3fb96a5c9f99eaab1f10c9a03c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:05:24 +0000

    children walk to middle entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
