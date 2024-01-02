84a260c5cb61361b710699c20c076e3b05bde714
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:31:11 +0000

    slowly more somebody land reduction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
