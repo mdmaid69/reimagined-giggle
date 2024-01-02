58df11c9ba5cd3ae1751a72d6980f5ac4a29e070
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:22:37 +0000

    boat Kk recommend avoid zero

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
