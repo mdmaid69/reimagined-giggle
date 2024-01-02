6181bd6de56f95c5c567a6a23a2ac20405cad76d
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:24:13 +0000

    concern since aware kill Nn

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+print(sum(range(10)))
