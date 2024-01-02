9f0be9ef73b14cf5ecf4feacae3876a86d934146
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:47:36 +0000

    offend calmly themselves offer should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
