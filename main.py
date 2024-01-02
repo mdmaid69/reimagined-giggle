4e6f9fd23eccfb0da61ec01b743fb2c3fc7a6664
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:05:25 +0000

    offend community throw answer gentle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
