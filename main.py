eae7d9a62b0177da71c15c49ee7460e07fb8abab
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:29:50 +0000

    wave sign guess still you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
