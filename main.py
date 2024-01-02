74bd5c9eb5526df6c886f19577c173ff5edd2752
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:02:57 +0000

    division mysterious branch instruction family

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import sys
+print(sys.version)
