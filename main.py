7a57eb20e4f1d8c03c01db62633b132846db27ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:49:48 +0000

    warn quarter calmly hers appeal

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
