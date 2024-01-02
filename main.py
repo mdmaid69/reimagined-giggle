c3d92bf1f33461a1444a13d02435e41d791ce45d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:03:31 +0000

    favour sight point task breakout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
