a96ba8da7d1387341318e9f0e6dc9f8700214aaa
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:11:35 +0000

    abroad snow protest her last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
