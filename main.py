20b6d60f5f2083af0672deb2f4cccd59919be08e
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:07:42 +0000

    previous out attempt health tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
