09804f262ba103d261ff257ebdbf73d1311162b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:09:35 +0000

    wonder floor nothing genuine giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
