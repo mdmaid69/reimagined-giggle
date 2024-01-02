d86f18b8438f88537d949cba567a9e59c1b871f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:58:03 +0000

    surprise sure dance I flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
