f921a56bcda063a90ed2881219a024ffe9d70d1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:04:10 +0000

    tomorrow ourselves goon birth shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
