bc299c3d9f8eb847993f0c335384ff7c8476d62b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:08:59 +0000

    maximum completely change contain squash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
