6164418a2ba960229e4e3585f490278e24a695e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:24:39 +0000

    exit afraid go add also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def add_numbers(x, y):
+        return x + y
