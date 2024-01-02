2bda4536d9cf1f4081ab1de00ff4008f89787109
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:35:01 +0000

    ability nearly lock manage constantly

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
