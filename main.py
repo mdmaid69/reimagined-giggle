2d20543fc326c2bf629bf939617424d32633baec
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:37:31 +0000

    unusual drop however how workout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_repr(array):
+        return repr(array)
