2ac97daea7e585c90d8f5435c75d4fbff4362c54
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:57:28 +0000

    perform this Qq content clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
