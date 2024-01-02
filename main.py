aedcda75f1f56ed674f9d2a073043626815ebf19
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:47:16 +0000

    authority plate somebody sign under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
