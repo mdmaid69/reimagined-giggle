b86292c4998cfde749e92aaf933d3e83d484bbaa
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:07:24 +0000

    recommend artificial feel poison turn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import numpy as np
+print(np.array([1, 2, 3]))
