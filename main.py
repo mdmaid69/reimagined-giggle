d2682cc6e2178b1a39fc8cb521e3db3afc14f8eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:54:28 +0000

    stand legal something bath secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import tensorflow as tf
+print(tf.__version__)
