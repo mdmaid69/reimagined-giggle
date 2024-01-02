264f377ecfe208004e3eb6711dc90d12385fbeeb
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:44:50 +0000

    healthy battery border willing thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
