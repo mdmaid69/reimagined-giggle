7a94a12573afb5a7a7fc1c9a966443832e15b85f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:06:56 +0000

    war match trip scrape with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
