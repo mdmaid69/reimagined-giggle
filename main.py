9269ce7ee786fed8731235543e9fa2c33f53874b
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:12 +0000

    repeat arrive equipment wealth thing

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
