5990ef86cff7e5ef99f2cb16d59a86752129a029
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:03:53 +0000

    file however comment especially contact

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def square_number(x):
-        return x**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
