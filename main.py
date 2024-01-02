57881dd0cc27d2ec8da0c994ca9669bee40ff4f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:50:15 +0000

    behaviour goup pretty opponent bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
