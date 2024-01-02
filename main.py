742270a876148b401e4ee2bb5df5735754672ea0
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:08:53 +0000

    appear certain proud stop partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def is_odd(n):
+        return n % 2 != 0
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
