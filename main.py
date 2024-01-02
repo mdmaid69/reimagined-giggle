44f24c80a5758765e5e9698d5772c8d689fe13a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:16:33 +0000

    funny excuse string seem chief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
