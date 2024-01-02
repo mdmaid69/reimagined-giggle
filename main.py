2d08f45fa541cd71e5341a1f124d3785b23e204b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:08:35 +0000

    gift memory programme foot rescue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_max(lst):
-        return max(lst)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
