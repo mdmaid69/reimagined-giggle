28019a6f9c66d4030b8edb891da0c4b01972ff61
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:57:47 +0000

    manage like positive flow bus

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import array
-def reverse_array(array):
-        array.reverse()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
