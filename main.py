6594aeedeecdf2fae436b41b09e9c1e2d6d6f3f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:50 +0000

    between soil below unusual teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_work(force, distance):
+        return force * distance
