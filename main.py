791859c5e3ea9f7d85444573147f167c733cc988
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:32:26 +0000

    also disagree holdout difficulty violence

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def multiply_numbers(x, y):
+        return x * y
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
