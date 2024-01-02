aade2c85c6a8ea7a728218b3ae16fc6726f61073
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:44:36 +0000

    apply by boat tip apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def multiply_numbers(x, y):
-        return x * y
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
