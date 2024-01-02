c7b7b25e5ab184367e611c533f91b7db817df0fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:23:55 +0000

    preparation environment real left translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+print(os.getcwd())
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
