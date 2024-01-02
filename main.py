bf492c582217237149a1f1e717b4aa9f62e635ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:30:13 +0000

    fire his open summer yours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_density(mass, volume):
-        return mass / volume
+  def delete_file(file_name):
+        os.remove(file_name)
