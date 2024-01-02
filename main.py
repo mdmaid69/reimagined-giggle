4565109f58d856c41ffbaabd6aefd83075b16253
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:00:31 +0000

    pause somewhere morning beginning ever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_string_from_array(array):
+        return array.tobytes()
