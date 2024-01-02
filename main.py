18570b003f0cf0c0c506b3b5de121b63bd411022
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:47:54 +0000

    defeat store trick fond break

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def find_min(numbers):
+        return min(numbers)
+  def cube_number(x):
+        return x**3
