a65b3a7b2fe0c2c94e3c44d44d35856302e62316
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:54:41 +0000

    delight smoke violence comeover willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
