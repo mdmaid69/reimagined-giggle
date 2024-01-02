9809d9321ad947e180adf4a346c524ee4855fa1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:30:48 +0000

    collection Zz would ago police

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_str(array):
-        return str(array)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
