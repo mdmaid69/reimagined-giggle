cac4cfdadc616d0821c934caa597e8d4efaa2c6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:14:31 +0000

    group swimming mad process hear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import array
-def get_array_from_list(list, typecode):
+def convert_list_to_array(list, typecode):
         return array.array(typecode, list)
