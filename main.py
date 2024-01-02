949e45966d5d064782ee3d5785010a9736a415f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:55:20 +0000

    them handle butter dull most

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
