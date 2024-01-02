940e1bcc66cc1eeadfca110d99ccc130aa1c6f55
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:19 +0000

    earn sure broken fence face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
