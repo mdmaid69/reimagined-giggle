f0e832fda4bdff87129b44a9fd026ca322a4e25f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:02:26 +0000

    turnout length wrong rest create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import tensorflow as tf
+print(tf.__version__)
