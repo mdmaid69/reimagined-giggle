69f4aa30ee642b0605cc6d58033e11fec494d24e
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:54:06 +0000

    centimetre escape cum variety pour

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def add_numbers(a, b):
+        return a + b
