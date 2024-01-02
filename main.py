e4cee77620108a0d22ed998ff497cd99c03afc9a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:23 +0000

    spin backwards clearly until letter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_length(array):
+        return len(array)
