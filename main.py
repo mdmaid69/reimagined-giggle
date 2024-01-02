510c31a9c95471577b52c1937dad23fe9fc56a22
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:33:22 +0000

    experience term than paper practise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
+import math
+def calculate_floor(x):
+        return math.floor(x)
