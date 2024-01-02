9519cbbde993e4c7d8212765aa9fd7c58c5d86b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:52:37 +0000

    cope equal national joke motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
