8280b0d2ab4c5af175b14b889040f0ef2438f420
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:58:08 +0000

    clear drink maybe further entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
