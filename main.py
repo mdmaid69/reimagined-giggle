7b0c85ec26becf87c2275b8e14b3c8a2051ae1f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:13:46 +0000

    mark feature interview post cost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
