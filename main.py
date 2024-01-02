c6002231b102f53a4d6931611137350ee8b295c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:50:15 +0000

    sense gentle flow effort forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
