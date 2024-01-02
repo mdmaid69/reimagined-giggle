ba8661f491e8db2edc9be716dd827d042d9529ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:52:14 +0000

    clothes neat emotion rub across

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
