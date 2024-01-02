9e091a36ae3c2b0f0ef3b65b247c47be45deb780
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:13:46 +0000

    mix dry thinkover narrow also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def log_message(message):
-        logging.info(message)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
