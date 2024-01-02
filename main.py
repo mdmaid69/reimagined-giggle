7668838511f1ab6a0208ede6f14eb34b6302dbf1
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:33:12 +0000

    year character unpleasant general shoot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+print(os.getcwd())
