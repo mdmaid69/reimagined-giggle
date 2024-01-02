ab74528d94bddfaef1d74a9b36906e77d7eb0887
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:53:07 +0000

    term key impossible nowhere near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import getpass
+def get_username():
+        return getpass.getuser()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
