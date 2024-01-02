b051053b1f4d94edd2164725fac853cd24662172
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:29:02 +0000

    result list amount further connection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
