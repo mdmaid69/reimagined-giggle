d66071be5242bb5f59ca2366876ffbba022350ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:13:51 +0000

    congratulate stick freedom noisy protection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
