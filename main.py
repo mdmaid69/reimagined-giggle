a3f893af20652203e1e81041a8ec79b44e231228
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:49:59 +0000

    system autumn wrong man shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
