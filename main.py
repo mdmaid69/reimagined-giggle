719c3cf5e29ddd591b39b1aeb9f3e4699cc36b31
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:08:23 +0000

    foreign left steady emphasize keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
