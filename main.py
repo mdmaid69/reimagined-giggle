d9e18ccbb4811ad26579a76a637a7d41d96381fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:19:10 +0000

    hope pain begin apologizeapologise leave

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
