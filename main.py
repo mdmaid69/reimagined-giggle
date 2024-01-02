233b271f0f1785f17cf3830bc2ed46462d961983
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:55:44 +0000

    stretch separate reason perfect petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_error_function(x):
+        return math.erf(x)
