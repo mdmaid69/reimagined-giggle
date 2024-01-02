1daf9d0d1a89dffd4ca2e8dfb1e61f6b81ff41ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:39:17 +0000

    method guess coat meeting connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
