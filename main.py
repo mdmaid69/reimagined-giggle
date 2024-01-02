f62f0426223de69139a6e0a1b165c234a040c3a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:22:36 +0000

    return influence Uu mad tough

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import time
-print(time.time())
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
