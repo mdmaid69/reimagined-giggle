82113caab22ae0605f0c86c01863f857e28f21a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:28:26 +0000

    count dance than bend dish

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-from collections import Counter
-print(Counter("hello world"))
+  def convert_to_octal(n):
+        return oct(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
