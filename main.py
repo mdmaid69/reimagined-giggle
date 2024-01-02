de07b6e472c94362a3bb1b5bb64d056e6cc376b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:57:26 +0000

    admit recover blame me central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
