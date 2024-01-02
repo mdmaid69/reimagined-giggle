364e01e2cb1a90c74a38f60f879ad0612709b090
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:49:37 +0000

    impression operate injury estimate mood

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
