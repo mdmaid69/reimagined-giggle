09dce1bd3c237012efe038c07c018ca237c4fada
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:36:00 +0000

    store suggestion near plant cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
