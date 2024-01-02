af80b72153a469b2f45f1baa8fcecfe6d2ab3d8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:43:36 +0000

    advise thing priority disturbing spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_arc_cosine(x):
+        return math.acos(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
