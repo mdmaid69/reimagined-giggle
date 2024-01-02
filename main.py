fb399847ff05816540de0dddfa520c58c5f6d624
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:05:49 +0000

    block event tradition moment dear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_speed(distance, time):
-        return distance / time
+def count_words(sentence):
+        return len(sentence.split())
+def square_number(x):
+        return x**2
