c72362613536095199e5589f71e841af2c04fa65
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:25:58 +0000

    cold smell pay wonder approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_tuple(array):
+        return tuple(array)
