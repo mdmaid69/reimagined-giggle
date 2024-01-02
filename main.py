d10aa08ebb14b828ad11a040495c7611f770c539
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:26:43 +0000

    urgent impressed print geton suddenly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import time
-print(time.time())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
