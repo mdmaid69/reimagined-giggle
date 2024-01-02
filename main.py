c0057c437d5240f3ceb7b3502a823ffacd9d6384
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:21:14 +0000

    connection hurry busy negative advise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
+def find_max(numbers):
+        return max(numbers)
