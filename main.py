0dbbbd5eb4c80f15495597ba9914c38b3fa9e7fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:57:24 +0000

    content change mean squeeze mainly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
