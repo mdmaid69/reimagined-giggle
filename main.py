b47b35a90bb40a812d8de74aa42c4d16756904d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:46:36 +0000

    clear interfere unfortunate much impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
