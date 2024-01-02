87e0bf0a2f71b3af269fe6ca628e5035f66f9c26
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:42:18 +0000

    anywayanyhow lock bring glass turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_index(array, item):
+        return array.index(item)
