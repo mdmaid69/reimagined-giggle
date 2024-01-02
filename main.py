fc2f66567e82e514b3592cbed392b80eaeae432c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:39:52 +0000

    move turnon callup conversation shake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+print(math.pi)
