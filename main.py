f0b21024596afbd769eb8bfc585dc81db6fae1b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:27:53 +0000

    so object human during appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_user_string():
+        return collections.UserString()
