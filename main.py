eb1cf30cc6d0736d79b11946c55c2260bec03dfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:07:06 +0000

    callup wander certain front natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Words:", len(text.split()))
