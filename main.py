53e6a79b4fd15c3f8ae39ad293473370abdf88a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:49:06 +0000

    naturally protect raise today holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
