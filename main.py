861039e699210993b7780058c7e3b373c4a7d286
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:03:57 +0000

    today gap innocent need lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_hex(n):
-        return hex(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
