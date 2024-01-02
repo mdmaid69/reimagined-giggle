35da580efcde83fdee7542e01e10162204f8072d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:03:33 +0000

    badly strike fun soon season

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
