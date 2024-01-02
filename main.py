633c27b2415d4cb80e1e01d9b67c2e60d90897c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:57:31 +0000

    also strange clear win lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_current_directory():
+        return os.getcwd()
