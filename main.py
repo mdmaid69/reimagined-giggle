4979a6e438a05d799e208a2ab080c9ecc21d2739
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:10:44 +0000

    stir tooth board take board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def split_path(path):
+        return os.path.split(path)
