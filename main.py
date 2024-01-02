308f474086a88d6245e2b14932a457f697018b77
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:42:55 +0000

    ball here wear Rr hair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
