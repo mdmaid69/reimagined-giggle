6f8a64a3eb862a95a759069730924c2fd36cf49f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:35:19 +0000

    surprise mix fact familiar centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def get_array_buffer_info(array):
+        return array.buffer_info()
