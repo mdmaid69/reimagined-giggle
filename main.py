5ce3c5b4a978d050bd09eab5dc5afbcdd362551d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:12:43 +0000

    state emphasize average call pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
