c2f006154a224b3d5aeeadc74949d301001c1808
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:19:26 +0000

    stop performance section cap error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
