2e87b8bc090010772c59114f5af726dd32b156e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:24:31 +0000

    spin objection secret majority agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_base_name(path):
+        return os.path.basename(path)
