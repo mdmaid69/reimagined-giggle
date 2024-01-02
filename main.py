b19465cbe7c7e14a113f65efe475c4f060b03a4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:33:56 +0000

    think shock paper waste weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
