02a59b545b3bd0ce71ddcf27b9a4184c1f3a308f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:45:09 +0000

    keepaway walk use workupto convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
