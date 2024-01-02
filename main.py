b53537cb46e68a2a82de7c0af8d45af9b3a56474
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:56:17 +0000

    excellent cut well somebody feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
