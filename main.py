aa50bc1c99abf6738fce528e0800b7af4528c616
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:05:02 +0000

    table suddenly delight answer drop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
