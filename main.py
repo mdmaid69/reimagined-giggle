d779f45af569247c2903eeff65907560ce11831e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:58:49 +0000

    last corner wander giveout turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
