4d6e29a620f3a5ae54b2fbd8a7267c29cea69cfc
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:12:43 +0000

    soft emotion themselves crime exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
