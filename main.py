956f980e45bc33387033603b43b9db841fc8046c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:14:48 +0000

    equal together discover squeeze gas

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
