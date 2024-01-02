090893b32a7c752743a926e4428ffc7678d8543d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:09:12 +0000

    fix various alone destroy promise

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
