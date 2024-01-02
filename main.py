8e21e9ea7e45a5551ba287166e1ce178a3b17f99
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:04:34 +0000

    comefrom straight basis walk hate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+print(math.pi)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
