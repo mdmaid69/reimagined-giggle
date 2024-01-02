2546b3ba76a9136022643eddf10b4e82b32dadad
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:47:20 +0000

    keep direct correct pair way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
