280bc4bea8e39a175d1b787c19da905694e4b477
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:45:38 +0000

    dance there his empty plate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
