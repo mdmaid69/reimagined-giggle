9d0d14d423861a2412640bfc0c03257a637cc205
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:38:27 +0000

    less comein 568energy time tool

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-print(sum(range(10)))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
