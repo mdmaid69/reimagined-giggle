f5e7dd3c3a661d1330dc88f195b9067d693f1acd
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:01:00 +0000

    towards drink future adjective fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
