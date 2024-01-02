1e882b4650069b05fcef3ae16f0d9ff24c1e040f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:41:53 +0000

    furniture just couple block modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def reverse_array(array):
+        array.reverse()
