3029c509da5ceb6b712a30e8f517eb5428e3cf93
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:15:19 +0000

    hole must rate talk foreign

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def cube_number(x):
-        return x**3
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
