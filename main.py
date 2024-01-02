c52a5b8fa2e8887d3c56f976cf9729a81c0468af
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:26:54 +0000

    skill between must communicate worried

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
