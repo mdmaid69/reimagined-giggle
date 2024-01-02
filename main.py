38a94353751128760b5fbbc0cbe3da4a9a1dc5a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:14:39 +0000

    steal secret love criticize hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
