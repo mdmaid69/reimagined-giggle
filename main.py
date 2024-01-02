cc8b5d24562f7db17b87e194a73f64b1aa831c33
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:57:17 +0000

    workon accident straight score left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
