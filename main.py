5aec35d67dedb733684379f97ec2f67fc6ca82e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:48:29 +0000

    better bread appear unite control

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
