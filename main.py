77709898692e88cca7f00f5bb6be5b6e85e06a1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:49:50 +0000

    because post female formal Ff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
