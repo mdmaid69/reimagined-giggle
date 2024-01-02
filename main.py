5e315927da57701576e83c57f64d2da3aaf97805
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:37:43 +0000

    this change do sound recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
