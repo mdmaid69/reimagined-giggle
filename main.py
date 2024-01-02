64b6079cd4e1d2e4725c38994f046a7a67d2baa9
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:20 +0000

    mention shut path string ticket

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
