c11f012f0828fd0292456bb5ffd88a410e6f07e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:25:48 +0000

    target accidentally workout wherever which

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_set(array):
+        return set(array)
