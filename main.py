22e76eb9620d36327c1723b1c6ac76faf82f043a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:44:51 +0000

    can any lift that above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
