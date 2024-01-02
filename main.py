f940b67bc250b532aba94ba969d4fbbdfac37d72
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:58:51 +0000

    coffee knowledge behave work burn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
