06dcb9fdf7df819aaff50b9f60aed50f1a3bd45b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:12:46 +0000

    agreement danger shrink nor tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
