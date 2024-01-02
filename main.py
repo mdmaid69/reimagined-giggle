94e00197cc43b80a33227a6af5623dd51f452eea
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:07:46 +0000

    maximum allow creature nervous emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
