d7f84db5796418bc99d8d564e7b7ed6d3726b310
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:30:46 +0000

    emphasize loss ought nearly centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_list_from_array(array):
+        return array.tolist()
