ed7a15e74c6988bded802910fe3ee49eb7e0d040
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:37:43 +0000

    admit same basically dick female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
