3e6fde09dd46b6706dae4eeb3d845c4191818737
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:59:03 +0000

    away false description formal whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
