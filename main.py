6c9d2625b12ec7698d9f55d2ae452d51ca601c20
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:40:14 +0000

    lock star attempt hungry perhaps

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
