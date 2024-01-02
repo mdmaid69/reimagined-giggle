4abed876b9b9b54997f0cb465621bd4917c3b12e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:18:53 +0000

    card OKorokay comeout play drop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def count_elements(lst):
+        return len(lst)
+def is_even(n):
+        return n % 2 == 0
