d9001041bc47447211e8663ef838707edfc9899f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:02:28 +0000

    convince plain accident error characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
