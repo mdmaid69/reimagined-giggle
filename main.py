fb0f32fd66d5a0b483013a50da3ddbe8bcaa6585
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:19:41 +0000

    refuse mend keep trouble sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
