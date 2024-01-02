3add0d30fb7f3c2556bd3a1e6983ab80a8398d76
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:38:09 +0000

    whoever concern ever formal confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import logging
-def log_message(message):
-        logging.info(message)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_bool(array):
+        return bool(array)
