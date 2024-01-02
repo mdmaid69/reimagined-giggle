b06524be270f89a5d5c22373903fe73e230d5bb3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:50:54 +0000

    doubt manner dull woman then

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def cube_number(x):
+        return x**3
