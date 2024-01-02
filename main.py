752f9a5643100166ac4843b8840b05cb84885392
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:25:24 +0000

    amount rather collect earn ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
