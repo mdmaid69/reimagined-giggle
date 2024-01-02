d551c93c37a05ea563e0a8e8bb83c8dc85577357
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:51:06 +0000

    share car somewhere study risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_max(lst):
-        return max(lst)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
