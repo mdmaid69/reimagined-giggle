2e1c89c3fa762563d850aa53b34da71f3d10e1a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:19:02 +0000

    possible which relationship division necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
