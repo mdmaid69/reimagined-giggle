fe98be183d2b413ebb86803d86775e3101167d49
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:41:06 +0000

    interested takeover weather achievement tool

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
