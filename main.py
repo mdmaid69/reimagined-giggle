ed9724a757d41540fe6ac13bb1b23da0148ae4ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:04:00 +0000

    figure happy fresh release fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
