ec27fb1060e51d50828e4796e0a78955d61d2866
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:32:32 +0000

    towards arrival target please back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import os
+print(os.getcwd())
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
