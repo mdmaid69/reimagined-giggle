69d547b50c1f4a2764075e681efcad516f306c50
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:35:57 +0000

    trap convincing sore flat love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
