01aba032f77958b0b7112c8b53141bf0a02f979e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:57:39 +0000

    ability roll base front contribute

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def is_even(n):
-        return n % 2 == 0
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
