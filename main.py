c25ff974017d55f1f11f1a8c7ff3d3d576267956
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:34:38 +0000

    office smart test letter promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
