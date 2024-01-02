9949a9ac0eed99bc1b909abf93b147d12c6a909c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:20:50 +0000

    prepare professional rather square worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
