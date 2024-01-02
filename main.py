f80d5cd217d218a00cdef5ef7a3107927262c304
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:22:31 +0000

    matter criticism second load file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def is_odd(n):
-        return n % 2 != 0
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
