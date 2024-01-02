4efa5a8c79700526c8126687de5930bf6bd56d86
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:22:31 +0000

    correct pocket journey money now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
