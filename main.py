4d543b416ddb7176965b4fd72074fd4bd408a1a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:54:35 +0000

    scrap cool narrow study sign

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import random
-print(random.randint(0, 100))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
