dfcd85093865b01cc64a614540350e8fb5ff37bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:09:25 +0000

    true every insist will cut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
