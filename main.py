75a876b832ee0deabff26e2ecb39b3f21a6e704d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:05:59 +0000

    until it rush thick business

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import sys
+def print_python_version():
+        print(sys.version)
