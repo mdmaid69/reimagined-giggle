7e6cfa37fbbb4cd91f0673d6de815dee8dd3032c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:42:52 +0000

    immediately snow takeoff getup regret

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
