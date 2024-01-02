6f59ef5267194b6a8b0de6b1824dc1743643ed39
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:30:09 +0000

    all television bush do practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
