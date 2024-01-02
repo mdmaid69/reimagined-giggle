646d3bd9df3faf74c05fe592a3e6bbc09a1ec96a
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:14:05 +0000

    pay man disturbing really point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
