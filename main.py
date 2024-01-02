79ef1a77699f5edfb17e861635bf89116514b0cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:00:48 +0000

    loose level female amount trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def is_even(n):
+        return n % 2 == 0
