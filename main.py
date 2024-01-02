0219ba67022f350d08845281fb0d916c8da65cb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:27:50 +0000

    doubt consist awake proof sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
