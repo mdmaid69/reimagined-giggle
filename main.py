6f6e1c7d9d5c621421224cef35341ce6827ac079
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:55:15 +0000

    community buy break subject bend

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
