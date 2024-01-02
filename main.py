b229b4e8e62267c5105714a84bf8e7cc132d6614
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:02:10 +0000

    fortunately shout giveback must typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
