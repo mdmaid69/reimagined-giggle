b55b64637d7cbd6808985c122290f6de34f2ca3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:36:42 +0000

    back thing few runinto pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
