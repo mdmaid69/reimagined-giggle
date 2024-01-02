419596e5276e4172617d6c830a4a5cc43f3eefe0
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:17:03 +0000

    later open club period brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
