8ca03c0a0e234739ce6b1b19db504d1717535061
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:50:07 +0000

    whether trap wake terrible tax

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-from collections import Counter
-print(Counter("hello world"))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_as_str(array):
+        return str(array)
