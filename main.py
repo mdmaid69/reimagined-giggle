8f58b5b9df81c3d3b17bf7fe7d6cb950c2eb10d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:47:08 +0000

    bright through progress punish answer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+print(os.getcwd())
