c8b5c29d60cda0c6fc9f304976d2b485151e20ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:37:56 +0000

    what slow start loan dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+print("Powers of 2:", [2**x for x in range(n)])
