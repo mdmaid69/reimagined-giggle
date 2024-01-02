61038edaf0abf3dfaafa20981a2cc9a38c385d0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:12:38 +0000

    party sure rush declare measure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
