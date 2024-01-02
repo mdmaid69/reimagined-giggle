2c27de84c7cf4e668396b49133319c332bca45e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:39:09 +0000

    present soil period post small

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_queue():
+        return collections.deque()
+def reverse_list(lst):
+        return lst[::-1]
