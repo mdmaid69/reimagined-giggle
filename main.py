bfb4d14e5c3b59b0a1a58f21801599b0d5fdeebd
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:30:38 +0000

    move evidence except none weak

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def convert_to_hex(n):
-        return hex(n)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
