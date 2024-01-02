74e8c98df1ab0c8ed8ff63a51d1be59ab88c3619
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:28:06 +0000

    normal object admire light step

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import sys
+  def get_python_version():
+        return sys.version
