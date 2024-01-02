6f470c816257e7f3a84beb069972e09e29dc77ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:43:25 +0000

    callat office benefit deserve catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
