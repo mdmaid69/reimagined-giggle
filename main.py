8b15775ac080f54a96f040f9591dfc5a83217a6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:26:37 +0000

    let rank wall business information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
