b00e0f28c3137844a598b7680c3c6c0465ebd250
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:42:04 +0000

    normal paper children authority stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
