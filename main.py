7a399c43d2ff7a40a84f5bc4875c18a56c81cc0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:02:01 +0000

    mistake damage impossible regular complain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_stack():
+        return collections.deque()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
