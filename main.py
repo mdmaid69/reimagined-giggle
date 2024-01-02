d8eca513b0605ef65acc4a4a22898cab360d4878
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:00:05 +0000

    long his takeout hardly middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def count_characters(sentence):
-        return len(sentence)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
