34b12b9821a15a77b6c32b2455e6d5d197e608dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:58:05 +0000

    comment poor characteristic only completely

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
