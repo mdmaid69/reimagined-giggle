d11f86ffec13d472e8d40655a238b626d1a1d90d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:38:07 +0000

    spring combination issue bound warning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_area(radius):
+        return 3.14 * radius * radius
