d0207cd6398bbfcde17a604831dc203e309468dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:35:49 +0000

    speak buy club custom change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
