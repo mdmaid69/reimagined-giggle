deccf8e55f5c84dce065a19f93313107844dbe61
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:06:38 +0000

    priority hot born along our

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def is_even(n):
-        return n % 2 == 0
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
