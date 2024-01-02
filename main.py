85ea589fd15ce48dc206f9ea0857103d6c76e21e
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:12:40 +0000

    stand respect so worry fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
