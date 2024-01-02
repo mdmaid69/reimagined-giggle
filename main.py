8ddc3c705f565e70d7b7065a7d16770d935426aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:48:21 +0000

    underneath think true turnover maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def multiply_numbers(x, y):
+        return x * y
