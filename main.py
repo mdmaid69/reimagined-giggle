e358ecfbc172695323deadd1d4d4da612098b1cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:47:42 +0000

    sell explain name smell Gg

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def get_list_from_array(array):
+        return array.tolist()
