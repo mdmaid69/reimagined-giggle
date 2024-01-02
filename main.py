e7de16e2b41f850d60b384521b11d729a5c18965
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:40:31 +0000

    above division light possibly you

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
