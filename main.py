b5d2ba580dbfe1962225438d4a236fb333d7112e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:22:27 +0000

    in thin mood speed see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
