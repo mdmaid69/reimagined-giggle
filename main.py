7bdb007693b868242fca9bf9a8e64aa7f94c32ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:35:47 +0000

    declaration equal nigga violent whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
