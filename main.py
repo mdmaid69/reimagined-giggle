3a9563002ac1de930487d68f69a553ff5ba13773
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:32:02 +0000

    lock copy to find congratulate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
