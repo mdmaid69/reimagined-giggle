2ab54cb56d39fef07afffbb7e565afd72c103f59
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:07:59 +0000

    trousers sorry quiet criticism famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import numpy as np
-print(np.array([1, 2, 3]))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
