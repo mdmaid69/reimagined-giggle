58490990ec1bece4812409a0617a57c8542d48e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:15:54 +0000

    relationship charge practical no anybody

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_work(force, distance):
+        return force * distance
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
