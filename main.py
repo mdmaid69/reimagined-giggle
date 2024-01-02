1bcb413c3997826690726f9e8aeb46a1a4505521
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:37:04 +0000

    breakup back Cc advice rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
