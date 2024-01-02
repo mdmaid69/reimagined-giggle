da7dd46762e3cec4aa2c99baac108ca86693b9e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:11:06 +0000

    pleasure prove chief steal scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
