d755f6a9b1db63789541ea02966f66bc04f7b959
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:46:02 +0000

    rely father peace punish feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
