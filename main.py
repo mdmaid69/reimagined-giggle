af6c301dafa09fc37bec5dfa4fe8335d0cabc307
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:11:57 +0000

    least Ff pretty about hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def square_number(x):
+        return x**2
