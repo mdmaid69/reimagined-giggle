f63bb9aa0565302722defd9b86c87df59e2f8e49
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:53:22 +0000

    Oo custom lot really me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
