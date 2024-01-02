ccd2918620644ce9c204f049b005ceaee443243b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:32:51 +0000

    repair base strike favourite excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
