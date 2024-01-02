ab20b3161f21f41f1649bed359bd6c4fc2f3cf7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:44:29 +0000

    happen getout process anxious discovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
