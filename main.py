b1337902a3fcf7ead21744775960d9dad048af4d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:32:20 +0000

    odd bill account exercise order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
