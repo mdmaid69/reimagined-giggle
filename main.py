d06949d3d817357d553a5658649862e2b820d7e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:42:17 +0000

    awake he earn petrol breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
