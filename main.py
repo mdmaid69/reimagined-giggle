29a3bdb708d1971e65ea5db807c4c643ab4ac5e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:40:23 +0000

    safe both describe doubt such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
