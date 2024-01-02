31ea67fbb80f00d61fb17b2d8a123734e65e6f9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:37:08 +0000

    no during slight justice Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_float(array):
+        return float(array[0])
