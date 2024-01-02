296d0ce1dfeb3f645c717769542fefc023e865cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:36:48 +0000

    many shoot personal gather side

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
