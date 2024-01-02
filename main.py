e06406448466edd9283ab777ae7bbc2a7e2dba1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:09:26 +0000

    brush office task low unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
