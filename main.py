36908e0e6feca457f794ff2edf56c704794122e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:19:16 +0000

    fix build probably usual school

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def find_max(lst):
+        return max(lst)
