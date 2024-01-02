325a031523e1cc306cb326f43d2d890c6adf772d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:23:07 +0000

    spring store shake genuine shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def subtract_numbers(x, y):
-        return x - y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
