e662d5ed9b97564869e57aa84029dd0ca92b17a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:47:12 +0000

    official modern disagree approve remember

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
