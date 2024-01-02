2fed5940aa7fe5c8278a1271f51327d75446daec
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:21:52 +0000

    Pp task miss vision process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_itemsize(array):
+        return array.itemsize
+def is_even(n):
+        return n % 2 == 0
