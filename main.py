139183f8997e8a7ed6270f2d7b577dcc9e8cecb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:25:35 +0000

    realize mood rubbish mark trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def remove_directory(path):
+        os.rmdir(path)
