eacd4e9a62b27a908c695c92a4ed42a2e438ec94
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:20:30 +0000

    actual take completely absolutely interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
