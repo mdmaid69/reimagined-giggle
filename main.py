7aa114905aab1c57f387f14f9311df0d4eaceb0a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:12:40 +0000

    flat pair include quantity charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def find_max(lst):
-        return max(lst)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
