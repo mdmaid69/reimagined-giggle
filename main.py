a49af727a5e7652811025a7be79668dc9f32c7c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:51:11 +0000

    pool worse away refer boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
