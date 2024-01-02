fb717eb6a61d46dcd2f5f76c946a41f85d0af89e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:08:46 +0000

    gradually whatever prevent negative Ff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def convert_to_binary(n):
-        return bin(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
