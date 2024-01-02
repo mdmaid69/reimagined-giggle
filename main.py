5c95217223dad7f84149ec09dd4f4a115d14ffb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:20:01 +0000

    tough journey partner show getback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
