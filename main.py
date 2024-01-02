16cc5d6d8fcebb016737244c8f82129f84dc9f7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:05:09 +0000

    point ring want declaration song

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
