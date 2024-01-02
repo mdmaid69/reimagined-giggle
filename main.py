a7723ecb092cb8d879b1511027d2ded44227f1c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:36:34 +0000

    we part close not man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
