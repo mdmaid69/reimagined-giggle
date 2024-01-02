04f6b2edce208a76e48f836ddb20fe87e844d273
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:48:40 +0000

    confuse Zz 565end deserve boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
