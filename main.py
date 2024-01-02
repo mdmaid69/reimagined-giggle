2b43e8ce5f82e838dece604c2d302b40b45af567
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:53:07 +0000

    makeinto on when relationship pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
