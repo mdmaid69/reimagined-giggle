6cb0fbee05c57b43d48f7297a14391a3d05d65c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:11:31 +0000

    admit war sleep flat strength

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import logging
-def log_message(message):
-        logging.info(message)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
