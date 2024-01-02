88ae9ecb67b9583851a71715f2ae155a5c9abbda
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:38:28 +0000

    journey damage deliberate spring describe

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def convert_to_octal(n):
+        return oct(n)
