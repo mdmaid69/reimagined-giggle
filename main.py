d1063e12a1c4dcb7387b4101ba9ac92fa2053abd
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:09:21 +0000

    unlucky success wander black operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
