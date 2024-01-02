2152515a033235bd2748ac51dcad0ae3f86314b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:26:12 +0000

    protect unlikely polite neither whenever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def subtract_numbers(x, y):
-        return x - y
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
