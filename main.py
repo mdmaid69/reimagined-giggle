66ae13a1c5245b1e3a17c3235fea187bb42cf80c
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:19:43 +0000

    impressive satisfied complete bottom but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def reverse_string(s):
+        return s[::-1]
