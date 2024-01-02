e852c1ce304398e67e62d91d6da0f324f475a463
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:07:08 +0000

    schedule defend characteristic perfect free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
