b2f891b1daf3ddb861b6cf238a6812cda1e6da68
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:02:13 +0000

    practical meal such play responsibility

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print(sum(range(10)))
