a669ebd021fefa435052f350b7ba67d88c864aa6
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:44:12 +0000

    matter makeup ignore bright convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
