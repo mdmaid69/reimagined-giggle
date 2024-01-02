94877ab9e0351b66f8a0c93956978a29299423f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:35:43 +0000

    division keepaway beneath complaint grand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_set(array):
+        return set(array)
