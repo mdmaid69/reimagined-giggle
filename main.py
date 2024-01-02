714c8a2d3132b7b673a3c7bd8d43511f40aa70c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:09:20 +0000

    uniform chance nearly buy sleep

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
