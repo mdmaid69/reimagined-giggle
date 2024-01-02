6e25e551c172526a181f17bbf6d4cef30dfca8e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:19:39 +0000

    choose cope serious perform trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
