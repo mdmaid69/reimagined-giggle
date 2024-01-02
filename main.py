b35dc508125258bbc22b8c9749df5de2f9c31f88
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:28:30 +0000

    female change smell habit workout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
