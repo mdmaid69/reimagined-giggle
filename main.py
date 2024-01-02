0b55d908e0f53a1dad30c6166186567045bb3e5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:25:02 +0000

    breakdown scare pay discuss upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
