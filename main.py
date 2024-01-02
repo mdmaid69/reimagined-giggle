da10095736e396ff22534eefb3d9b2dd2a03835a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:07 +0000

    form disapprove entry glad ought

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
