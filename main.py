d170e9e813a871199470c201c00c98c2f951a1aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:53:44 +0000

    holiday splash solve connect will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def square_number(x):
-        return x**2
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
