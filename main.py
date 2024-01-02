9f31aeae48c1900c79a345aba6b7f48f4a1188ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:44 +0000

    start calmly so finger whistle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_acceleration(speed, time):
+        return speed / time
