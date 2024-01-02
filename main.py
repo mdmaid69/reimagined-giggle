cdff03ebbf77885325c0a1abfe3c693c33911c97
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:33:20 +0000

    smile keepout promise meet modern

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
