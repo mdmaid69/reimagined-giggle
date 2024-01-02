691bc92a0529855933bf9d5637dd4f7119a46858
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:36:49 +0000

    regard collect clean capital treat

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
