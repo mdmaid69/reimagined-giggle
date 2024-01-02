b2fd077a1eb860a99e1f72b203b3715313c7f611
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:45:59 +0000

    interest promise goback(to) sort tired

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
