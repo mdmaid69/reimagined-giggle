d6d6fc4092968d908a27cc23ef07c864a070c528
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:30:07 +0000

    butter tone break capital regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
