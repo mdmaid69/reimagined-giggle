76406fe9d971fcefe6c5eb0ad3cbfc1ca991102f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:13:14 +0000

    pleasant tired rain fun minor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
