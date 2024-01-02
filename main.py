c80ce6164436da8e3d111e2efdedb8ef1f83c290
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:31:29 +0000

    report consult physical count rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import time
+def get_current_time():
+        return time.time()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
