ad7583214ef019cb73b3c855cf1526db01259266
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:45:12 +0000

    dry comefrom steer allow failure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
