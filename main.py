4dd1c5b63a9c27fa4d95a333b6e0e0a71a93e291
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:38:26 +0000

    science tap steep dance exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def convert_to_octal(n):
+        return oct(n)
