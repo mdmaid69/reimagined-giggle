363b6ee457fe92cfad90bb8dd2f4d7e62f7452e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:32:57 +0000

    wash meaning classroom total special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
