f6e57584985cc185edef51613142571c4f26f520
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:48:51 +0000

    return path lower hot nervous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
