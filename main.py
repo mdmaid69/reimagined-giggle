fd7ad43577655e14b7e364e730464a94fda5bef7
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:29:32 +0000

    eat cap under steer lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_error_function(x):
+        return math.erf(x)
