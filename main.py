522e8a69ede4550017de9e24f5384deaf8acda03
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:18:37 +0000

    art Bb sleep plate bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
