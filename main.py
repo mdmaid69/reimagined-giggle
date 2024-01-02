d159f4f73de10042b53e12f54eb23ca19319164a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:04:08 +0000

    conclusion emotion instead like minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
