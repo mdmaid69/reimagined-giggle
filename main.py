0929af232970fcbbc643d84c649e35d9d168e784
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:34:18 +0000

    traffic wonder interrupt apart alive

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
