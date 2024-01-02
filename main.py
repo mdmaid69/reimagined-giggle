82230688b55585a292e0a33b13980d0b020ba509
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:50:25 +0000

    scare pipe home lock runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_speed(distance, time):
+        return distance / time
