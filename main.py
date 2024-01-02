4c4aac0a2ac2116dd641350e780d9aa37acba1d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:14:49 +0000

    lean compare neck worry information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
