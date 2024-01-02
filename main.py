bf791004c1d40a98429719d1470426ffabb5eb90
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:40:32 +0000

    talk too towards present shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
