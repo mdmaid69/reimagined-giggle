9505a8bdb9388c1eaffdf0a4a3be7246211f9ca7
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:03:14 +0000

    respect wire sign direct keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def clear_array(array):
-        array *= 0
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
