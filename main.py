dfb18eb54cd32a43e10daedd88cee8921dd8e150
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:23:09 +0000

    getout everybody best equal cheap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import numpy as np
+print(np.array([1, 2, 3]))
