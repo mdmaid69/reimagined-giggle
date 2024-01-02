eb43c0105e959ad7c493252363a005980d31b9d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:37:06 +0000

    excuse comein nearly combine rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
