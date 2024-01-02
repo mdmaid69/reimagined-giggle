4163a294397c61c1f814a00c3bb6aace1c5d6e1e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:26:50 +0000

    matter key often 570enjoyable wild

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_perpetuity(payment, rate):
+        return payment / rate
