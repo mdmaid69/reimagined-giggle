f1bdfbf173c770bc6977a6ae1bb5675fe6a3cdb3
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:39:59 +0000

    favourite bath damage week love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
