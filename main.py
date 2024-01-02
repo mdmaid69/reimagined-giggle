d6cde2e25fefdd8566a0a72746b2db7bcba29e68
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:05:39 +0000

    charge properly if surface letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+text = "Hello, world!"
+print("Words:", len(text.split()))
