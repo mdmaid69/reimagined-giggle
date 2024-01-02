2c740ea7d6158673863e58c5d0f2269b9a66b9b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:01:24 +0000

    programme fail negative disapprove when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
