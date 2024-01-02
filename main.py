4e9d679d5a865235b7bdf14f9bcb06aa953a0f79
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:52:36 +0000

    classroom weather praise high fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_circle_area(radius):
+        return math.pi * radius**2
