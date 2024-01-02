98088a8afc6f98f0b2d0b39505c8d106039afa9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:36:50 +0000

    pocket boat somebody Vv nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
