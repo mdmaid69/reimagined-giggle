341076a2edbf9f7794d8268c0bf299cf64286a13
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:31:15 +0000

    before turnoff hope worse buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
