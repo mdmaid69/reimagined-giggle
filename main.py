b637214f28a199462a4c7d79817ec1fdb1e43c82
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:34:58 +0000

    quality repeat sort education tape

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
