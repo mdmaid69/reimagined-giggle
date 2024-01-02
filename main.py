8de63769a5c67a6a88f25df0158ea51d9b630aee
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:07:28 +0000

    always quarter spite first effort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
