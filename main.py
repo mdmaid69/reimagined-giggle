a7b1a0e7345c811625948dbea39dd6536376c997
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:03:43 +0000

    pencil otherwise pay load prevent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+print([x**2 for x in range(10)])
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
