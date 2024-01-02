0243f8b12784cfbf87c89c66527879d7ad4e2e03
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:38:17 +0000

    anyone hot probably left various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
