45a238b851838e3ef28e98257f48cff96dc84656
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:28:23 +0000

    bang 572enter by relation average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
