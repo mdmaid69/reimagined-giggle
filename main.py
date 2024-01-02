df69a09b740ccb7ed5e9eb1f1db0bde75331faae
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:38:07 +0000

    congratulations accidentally face ring course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
+import array
+def get_array_length(array):
+        return len(array)
