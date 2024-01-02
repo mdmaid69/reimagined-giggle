bc932ae30dfba5885eeae6650b81e999bc787944
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:02:02 +0000

    task know rob child frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def clear_array(array):
+        array *= 0
+import tensorflow as tf
+print(tf.__version__)
