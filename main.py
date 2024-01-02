cd80c8a46ad688ed44d5aaef5282fadfe6ce3e7f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:35:28 +0000

    carry friend break condition cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
