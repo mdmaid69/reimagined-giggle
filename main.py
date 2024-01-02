6701d8a8629cb4ece4a195654032c490e0369c16
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:21:21 +0000

    only workout plenty pressure squash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
