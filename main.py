e7b2bd8332ffff66b381bb37b5180a2576d13b42
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:33:45 +0000

    visit breakinto straight ear actually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
