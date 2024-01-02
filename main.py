ce35b5a833fd91e7551565690447c16fbfa434d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:11:36 +0000

    total still whisper flat beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
