e546652c31ca4fc123b28ddddf9c1a72173616f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:59:28 +0000

    fair stage frame pair lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
