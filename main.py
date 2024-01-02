bb01fe39a30046c1f10c4bf0ff80ca1e4ff6aab1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:39:54 +0000

    attempt interrupt dance maximum happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
