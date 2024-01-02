a0f3ae476f7671dae906c5b9ef72d4eb8ead2d43
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:22:48 +0000

    call demonstrate tear all path

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def convert_to_octal(n):
+        return oct(n)
+def is_even(n):
+        return n % 2 == 0
