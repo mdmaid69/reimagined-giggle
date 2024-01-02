02547496bbea3ab91d41f852bf26027b22cfdd57
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:00:23 +0000

    run splash closely impression share

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_repr(array):
+        return repr(array)
