0a2e3582cc8147deec9812be93d802dff7bc94e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:11 +0000

    soft mark key knowledge allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def subtract_numbers(x, y):
+        return x - y
