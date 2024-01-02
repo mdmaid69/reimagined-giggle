26e3fb23c4788d953ddc2d49a9f7e495b57b4256
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:16:37 +0000

    study actually face healthy right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
