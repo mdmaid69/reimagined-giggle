22ba8416d14aefde9b2f769bddab2aa83bd64b55
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:28:28 +0000

    takeout disappointed change joke order

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
