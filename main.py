c9e4057a4f997144085b3e48b083ed7c835b7d2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:33:52 +0000

    lean everything space part hold

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
