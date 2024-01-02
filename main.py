4c29f195e51289ae2852a746df2e07a58b5197e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:13:27 +0000

    comeup later apparently square safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
