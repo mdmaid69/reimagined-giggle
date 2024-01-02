1891db9c6a4a4d5957c2e019c4f0c7e313fc59a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:38:55 +0000

    far where always opposite price

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
