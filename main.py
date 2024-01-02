ca2384c824dd9a281d8126e363cf92296678a43a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:53:59 +0000

    hold I trip blow most

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def add_numbers(a, b):
+        return a + b
