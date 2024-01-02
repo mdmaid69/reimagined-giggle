504db550b00d9ec8090bf7ec724a400b12bc4274
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:35:39 +0000

    innocent comeout sticky example satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
