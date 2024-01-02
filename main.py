3ade46fc16f738ff8247a4bbbc7e37bf58932c27
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:18:33 +0000

    afraid total government much nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  def square_number(x):
-        return x**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
