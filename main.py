3b0e1505163b68f90c5df900c1f3a4f4ccaf48d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:35:35 +0000

    street town second spite black

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(a, b):
-        return a + b
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
