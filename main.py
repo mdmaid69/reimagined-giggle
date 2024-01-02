34624b5924235f416b9f5ee07f5497ebf42ce073
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:40:47 +0000

    priority damage bad persuade field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
