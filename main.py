60cda516781576e543e8adff0cb09e20c7162cdf
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:13:57 +0000

    repair principle after suddenly do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
