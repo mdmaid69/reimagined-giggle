08e32c28ff063b39f897f6288ca7a05658b9b144
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:13:12 +0000

    shrink nature concentration cough meaning

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
