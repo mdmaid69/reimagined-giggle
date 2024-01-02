1c43726eca378849e345dfc06f6ce5db61c66fd9
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:22:47 +0000

    health independent communication earth touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
