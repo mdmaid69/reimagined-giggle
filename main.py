6330446d73af01e2f879a2409ad4fb54827fa1ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:38:29 +0000

    error emphasis Pp either accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
