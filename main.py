20a96ea18c6dd65e71d53f2721263377a354199a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:06:43 +0000

    national exit immediate bar can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
