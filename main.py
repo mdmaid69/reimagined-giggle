2ebbdead84a96cd87a1859f322caee3750751e75
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:57:42 +0000

    every imagination spoil yours well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
