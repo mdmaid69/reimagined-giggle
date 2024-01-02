472de93ce405b0b55c6e947cd889c02c39112349
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:16:22 +0000

    about lie check responsible turn

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
