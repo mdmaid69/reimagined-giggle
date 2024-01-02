b4581e4e190b871a6348b50e08855367186a9f01
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:08:34 +0000

    tap purpose tear next congratulate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
