0d6a433e2cebc8ab2850c4085af20bbce4634e01
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:01:21 +0000

    tear load forgive possible Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
