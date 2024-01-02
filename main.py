f0ae792dfe2f45784093b1640348d4ed4e9bc193
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:18:23 +0000

    reply round rob breakfast especially

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
