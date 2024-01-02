32a8fa071e1656c2246cbfbf61743179f5b43b5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:59:01 +0000

    argue zero willing intend distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
