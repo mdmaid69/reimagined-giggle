dccfa47443fc85476ed87656802214f02376fbb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:47:52 +0000

    discovery occur trick dare gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
