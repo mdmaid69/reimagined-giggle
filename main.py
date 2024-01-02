0c3335d8f7a63c10940aa5814ae12470df4aa8bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:37:58 +0000

    can room especially string that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
