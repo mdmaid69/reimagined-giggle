22c5c76dbf6ec05f1af4e8f958a087305611dc77
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:20:10 +0000

    part before music act drive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
