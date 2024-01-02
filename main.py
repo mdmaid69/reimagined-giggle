bcadb8c246670b4f6be4ba68016b399e5b5331aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:07:59 +0000

    building body Qq lot minimum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
