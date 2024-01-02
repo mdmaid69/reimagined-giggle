beb81c954ffdf0c8df8e4ff659af35616baf0eeb
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:23:26 +0000

    represent holdon about coat Oo

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
