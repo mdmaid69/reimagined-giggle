23d1a30706ac9eb04b39952cb0abe572078d5cd1
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:35:17 +0000

    authority concentrate nothing feel entry

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def multiply_numbers(x, y):
+        return x * y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
