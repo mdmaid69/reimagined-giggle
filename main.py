d14e4f44395a75d5945065ef63a337f86f0160ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:35:13 +0000

    board appearance contribute imagination possession

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
