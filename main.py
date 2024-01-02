68d3cc2c7ab324a9dcae4df40ec02eb64b8db2c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:15:59 +0000

    improve fun somewhere branch include

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
