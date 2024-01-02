708102e2e9279f232c53f14acccf736260f343a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:04:44 +0000

    arrange concerning row innocent bridge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
