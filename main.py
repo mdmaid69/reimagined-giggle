f4578805a39a5304b4486b78289db70f9e36db48
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:23:22 +0000

    closely attempt spread note collection

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_int(array):
+        return int(array[0])
