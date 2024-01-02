74513b4dad09b92e9fe20a06d5ea707bc53f0362
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:42:43 +0000

    morning run talk new nerve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
+  def remove_duplicates(lst):
+        return list(set(lst))
