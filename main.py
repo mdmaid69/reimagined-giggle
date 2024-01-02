9f5908817305b28a2392f635b136a2c5c30b0fef
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:30:59 +0000

    right no report tour weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_item(array, i):
+        return array[i]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
