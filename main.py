2f7226e6299c653e40b2963c9d68c88f0e19eb41
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:06:04 +0000

    rich talkback demand turnup knock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
