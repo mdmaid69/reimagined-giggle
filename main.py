f1d35df6264938f77ddf34f176beb07b17bc9864
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:39:05 +0000

    rescue fail invitation female extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
