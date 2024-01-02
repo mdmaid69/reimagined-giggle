cc3c9ab84389920eb80c902af8b159927e2ba6c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:32:55 +0000

    steam emphasize moon Cc announce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
