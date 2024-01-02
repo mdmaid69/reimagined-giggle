9b9444e74d6c3081272f41c1bda3cff5dc48a98b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:56:07 +0000

    poison practice suckme deep about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
