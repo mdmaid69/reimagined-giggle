27a72752c62665f73e620894595501861a48b20b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:56:10 +0000

    takeback touch plastic note difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
