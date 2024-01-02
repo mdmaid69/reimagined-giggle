b702475435b77b5354ed0d60d353091c413bd315
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:47:22 +0000

    party clean last turninto pipe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
