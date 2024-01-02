c65690653316aeba9eba5c15e198863f3046de14
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:30:38 +0000

    prize phone pretty over nose

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
