90c94980f30819b900703e1dd85e8c249f04f786
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:07:12 +0000

    more whether translate hot coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
