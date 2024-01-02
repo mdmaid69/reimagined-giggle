c5090413c617f6fe3f34f6115f425630e3c0ff51
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:51:29 +0000

    exchange watch fairly always long

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
