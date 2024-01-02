48553a0924e98dfd6e90ecaf0c9f98e2e63a81cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:54:08 +0000

    event before runover certain 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
