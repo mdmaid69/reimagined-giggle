3a8d2469735303bb3a50a7ad757e91cfe1ff45d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:46:15 +0000

    share language getaway think transparent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import re
-print(re.match("h.*o", "hello world"))
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
