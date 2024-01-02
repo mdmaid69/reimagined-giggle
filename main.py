e43739cf0d04ba6b76f3750f3025d5a91e585930
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:57:48 +0000

    fast interview black opinion corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
