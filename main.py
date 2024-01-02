2ebc7f36ce2f4feddce185fed336937dfa2edad6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:58:28 +0000

    winter Kk off firm trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
