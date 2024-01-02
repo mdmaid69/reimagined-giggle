4be26286e9bc793d12ae07552907bd7c307705c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:30:48 +0000

    steer last male lie male

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
