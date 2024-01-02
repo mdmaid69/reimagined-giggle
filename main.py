720fdfe3918076a078673f6af318c4917715410c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:05:46 +0000

    unite treat weak expect nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
