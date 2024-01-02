6da6660535cc90ab8f193f6a0b7fad298b60f4a6
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:33:33 +0000

    fast quiet roof majority crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
