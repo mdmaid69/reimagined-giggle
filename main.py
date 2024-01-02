ac05a31c761cc0dce174788e6666fea0095a8b80
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:57:11 +0000

    material who trouble emphasis summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
