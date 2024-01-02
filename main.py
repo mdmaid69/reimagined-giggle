4f6410967b143d9ef005fa3d8e4f1417794f9d3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:56:22 +0000

    strike have organization turnup notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
