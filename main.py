e503db8b85642dc3876f1a47e8b62e86d9ec1818
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:35:28 +0000

    trap announce busy emphasis thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
