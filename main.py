b73f4d300fe1bc9db57d7671bac51825334ffd37
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:59:24 +0000

    wash brown splash furniture block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def convert_to_hex(n):
-        return hex(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
