6e02488ae1f743167beccc167bc28240ca13b0f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:01:21 +0000

    report move pocket terrible near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def split_path(path):
+        return os.path.split(path)
