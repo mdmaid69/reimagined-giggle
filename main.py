d2346ce4493d639d6e9db0a27ec0d5a82885b08e
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:47:27 +0000

    produce commit spare disappear discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
