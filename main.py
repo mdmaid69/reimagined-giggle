d92caea0ec2a001413662911f7bb51cfe2d958e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:00:55 +0000

    congratulations interesting tread makeup container

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
