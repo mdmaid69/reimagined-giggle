d14eb1db93452a0aa4e497aba72152e351b30793
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:39:50 +0000

    attract image opposite broken ordinary

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def cube_number(x):
-        return x**3
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_stack():
+        return collections.deque()
