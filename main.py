b7ce7d9b5bf52e7caa1ab7facfe5d36069cd1d46
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:09:50 +0000

    spoil nowhere study content love

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+  def get_base_name(path):
+        return os.path.basename(path)
