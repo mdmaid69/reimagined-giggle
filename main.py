429bca858789c830092d3da40daebfc813cabf92
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:55:41 +0000

    be possession complaint stage act

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
