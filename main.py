cc17aea173f0378c2c34daeaa7a39c7f5c50b9a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:39:40 +0000

    commit cut bet while slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
