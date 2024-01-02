288aef29ad0f535195faf450378e342ca320a697
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:04 +0000

    jump danger comment previous trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
