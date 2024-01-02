d5893e21e5f48dc917376de9ec6ddc5c2b90d8e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:40:03 +0000

    crash of idea do suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def calculate_mean(arr):
+        return np.mean(arr)
