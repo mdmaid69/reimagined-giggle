8c378652ba8365b538cef337469340e7cd9e50c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:19:18 +0000

    fun pay above broad protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
