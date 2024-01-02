c74d5068b646944e57d91d92d5130769feabb965
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:34:49 +0000

    succeed burn man arm regularly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
