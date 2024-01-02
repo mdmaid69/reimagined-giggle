ed638e7d031902acc2344f9862f41afd3a17edf2
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:34:19 +0000

    reasonable drop punishment character sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
