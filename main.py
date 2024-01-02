3a58b0212c48fa3ce05e6460d9c6b44361e66916
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:06:51 +0000

    tragedy late imagination willing respect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import platform
+def get_python_version():
+        return platform.python_version()
