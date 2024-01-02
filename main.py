482ff2f9af3c532d08185818ba6b49fec7c3a1b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:40:01 +0000

    himself sort survive size supply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_gamma_function(x):
+        return math.gamma(x)
