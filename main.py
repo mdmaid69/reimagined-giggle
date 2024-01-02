e6d11646476dba4fe687b618c2cd6e1a6db3146b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:08:06 +0000

    solve scream way Qq knowledge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
