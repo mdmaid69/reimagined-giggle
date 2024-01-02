b25c0b83dfed911927e9431c49d2f9d8b8145069
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:35:35 +0000

    address broken drive area finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
