103c084c2d95813c24fa89b4214d54fcbd716d32
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:24:04 +0000

    represent bring sensitive mistake black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
