ec585f0180824c8545d3d67c5a111bc007eb5398
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:50:31 +0000

    separate occasional apply organization material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
