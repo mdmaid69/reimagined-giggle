833520e4f45c9c70d67960a8b14206f139492a8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:29:26 +0000

    guide bring surprising object shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
