b02ff56dbd16849b5acb77c62d66a80c8012e552
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:29:48 +0000

    appeal them contact complaint appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
