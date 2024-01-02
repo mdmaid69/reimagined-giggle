b7393fa41e1e8b3c5298dab9c9759927edf6e86d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:14:15 +0000

    appear she succeed fresh never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
