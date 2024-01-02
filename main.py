f2baf72b76086a0de4e5bc14fe8988208353584f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:27:41 +0000

    press arrive match phone reasonable

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
