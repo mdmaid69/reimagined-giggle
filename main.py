d420bf0a04db13f280f2507093b5194c2d55449f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:33:44 +0000

    past straight create just price

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
