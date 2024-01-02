2c0ae30897a666e95f9804f90d1798e31a26a7a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:55:42 +0000

    underneath Hh flow later spare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_current_working_directory():
+        return os.getcwd()
