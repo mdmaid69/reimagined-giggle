0aa44dddb024141f3f9a75fb8d1e959f2ecd4902
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:09:42 +0000

    challenge miss study add hospital

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
