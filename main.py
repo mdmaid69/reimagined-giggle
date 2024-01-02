dc563cb6488d94fe19584680cca398774dcd7af8
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:49:42 +0000

    deliberate ahead sound possession secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
