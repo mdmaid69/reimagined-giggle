97fb1ef790c41742007401d497e8fba900011e68
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:06:00 +0000

    talkback tradition division round crowded

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
