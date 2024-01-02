d362172550d91295c5925ca56e35eca31073f5a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:59:03 +0000

    sharp ahead law than dress

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
