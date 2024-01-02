f1d49e1b567b1e225ef3de3f06d1db1cf48a3c00
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:35:35 +0000

    entry normal generally lost container

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+print(re.match("h.*o", "hello world"))
