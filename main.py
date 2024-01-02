e64fcb65977e61d87923530e75527ecd587137c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:30:04 +0000

    breakin everybody hurt tax sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
