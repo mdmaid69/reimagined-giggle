7e4ed3b48aaf9e1b40f525605b78046129a30993
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:46:00 +0000

    fairly exchange fill slightly ours

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def cube_number(x):
-        return x**3
-print([x**2 for x in range(10)])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def convert_to_binary(n):
+        return bin(n)
