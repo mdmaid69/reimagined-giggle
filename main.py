205fb0bffa2277f0616b197d3a1c91f7951a0795
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:40:59 +0000

    card education university pride port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def clear_array(array):
-        array *= 0
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
