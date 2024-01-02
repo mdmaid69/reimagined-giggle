c4569e00652410800d81b4b1d697f4966292a9b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:24:29 +0000

    profit load love dream performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
