ac5eb08c20a21f3ab072ba766ac212ff10b00b6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:08:40 +0000

    achieve store part just fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
