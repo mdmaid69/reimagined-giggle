9e60e92883fbe05a924f239e1e6ba50bdbfcb52f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:15:39 +0000

    slice breakin classroom at breathe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
