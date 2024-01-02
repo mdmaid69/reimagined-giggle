465235dff9fa66fe732ac2f841698216b739926c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:42:15 +0000

    pretty fail further take exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
