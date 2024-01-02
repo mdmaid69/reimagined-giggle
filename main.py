741b35bf2f6ae2bb62a6ed8fc40bd958173ab3a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:10:23 +0000

    account outside fast brush shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
