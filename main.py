07f14914351478332ea16fdbc9d47a07eda378db
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:29:53 +0000

    spill partner nose relative hide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
