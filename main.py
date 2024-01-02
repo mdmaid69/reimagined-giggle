63eaaf9796ec0e92eaf4fdfec3fe700bd09cfea5
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:44:13 +0000

    order exist wrap smile wheel

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
