d771ace4eb82129139e65b52b23bb43164334a9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:49 +0000

    use holdout between view hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
