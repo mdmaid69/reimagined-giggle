cdd21bca4cbdc8f7559db59b1f55bc4152670cb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:44:58 +0000

    takeback principle down produce pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
