48e4d8a2fa5a93958d7ddf60c3e7e16dbf53278f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:56:57 +0000

    identify judge his centimetre block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
