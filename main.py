4e3542c5b23fc6e262e17bebae7db05c7303a203
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:26:11 +0000

    rubbish earn thorough greeting much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
