4628fba5354e7c8a8284c03934d9cd707d7b3dd3
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:34:01 +0000

    accommodation turn know Hh collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
