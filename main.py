0feb0f719c7af3e4176284865d923424ec2130f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:01:16 +0000

    influence takeaway father fire medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
