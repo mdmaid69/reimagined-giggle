3c0f120641e2a4769d253a5de1e0290fb47bb377
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:09:37 +0000

    mess hit hardly towards obtain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
