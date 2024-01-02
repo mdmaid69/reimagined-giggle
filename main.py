088ed4e8317cafd792b9ca64c0bd9819a3c1820a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:48:37 +0000

    start invite strike set opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
