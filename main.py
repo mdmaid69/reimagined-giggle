a8b856bbf69924b44e72cd70ff11582342e6d3a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:10:15 +0000

    passage punish somehow knowledge trousers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
