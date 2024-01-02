bea5d6d62185932a540583472396bf695515ca11
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:57:26 +0000

    wind will wander trap figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_list_from_array(array):
+        return array.tolist()
