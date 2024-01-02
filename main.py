ca28e8cf873a08e57f532675144501e29c54e8db
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:40:58 +0000

    and scale identify holdon unlikely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_as_str(array):
+        return str(array)
+import sys
+def exit_program():
+        sys.exit()
