2cb52df48f3bba5bdd0561cdd5c0af6c61848e5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:09:06 +0000

    maybe dead content breakinto out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_length(array):
+        return len(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
