39536a66a097c47f3335db14e70e0c07a660b566
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:39:34 +0000

    comfortable size attitude angry opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_working_directory():
+        return os.getcwd()
