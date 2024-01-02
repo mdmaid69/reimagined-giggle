947e1151d0428cbd242d5bff4a4135bf0ce28930
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:09:03 +0000

    drink variety fairly takeover dark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def count_elements(lst):
+        return len(lst)
