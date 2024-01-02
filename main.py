68cf836d7f3ca503ae302ab893cbef3d75d89aba
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:08:34 +0000

    goaway and vision stamp vision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+print("Average:", sum(numbers) / len(numbers))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
