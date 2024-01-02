08dedb0c5101e6f48ee69241fdadca0fbba6822b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:33:56 +0000

    yesterday hear convenient turndown raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
