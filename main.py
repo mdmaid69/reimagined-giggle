b4d59e60f0592c1a325358a71778acbb27e33467
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:41:17 +0000

    if less 569enjoy window wire

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
