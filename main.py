cb629afddff642140b9a5612a37aec6afef20ae1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:10:23 +0000

    later trade blow team intend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
