0e46b440106dff3a39d61b6683fc11d54e12a5fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:17:14 +0000

    disapprove Gg bother right important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
