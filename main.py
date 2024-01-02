fefa5572deb4353b83bfa961f8d3fd25c006781d
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:03:32 +0000

    runinto mysterious concerning keepaway worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import time
+def get_current_time():
+        return time.time()
