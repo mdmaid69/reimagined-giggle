a0ac073e3646992150127ee1f7b8bd0c518b1de3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:42:30 +0000

    afternoon lost plate downstairs home

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
