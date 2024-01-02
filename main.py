333d598c2369de0cd98f80ad928ec72d6d7c0a19
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:02:28 +0000

    necessary sweet government succeed visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
