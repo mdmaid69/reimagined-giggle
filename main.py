c4d08ac1edd45d74e26713b65083be9f7c44a233
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:58:50 +0000

    ourselves storm left conversation pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
