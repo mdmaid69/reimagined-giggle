d40aa159fc4e7c0e85b5d8283e170ac1d3a41d01
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:48:55 +0000

    clearly prepare though nonsense content

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
