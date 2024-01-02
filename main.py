475f7ec02b65753552c18062645425cb8ca4dd8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:04:20 +0000

    better sea evening custom getout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import time
-def get_time_since_epoch():
-        return time.time()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def add_numbers(a, b):
+        return a + b
