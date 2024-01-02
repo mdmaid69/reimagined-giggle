b13be7dc6aafe447bbe55a29a3d2073ddc372bb4
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:10:26 +0000

    frequent breakinto giveup lookaround shine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
