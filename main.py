a9f2a8fb505cfa95cb8e7beeb0c98402fd30c30e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:45:15 +0000

    hour plan sad score sleep

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
