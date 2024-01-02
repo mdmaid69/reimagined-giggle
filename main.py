f19eaba129770db548eb16d01d13344ea86ba7ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:33:10 +0000

    tonight unlucky someone quality note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
