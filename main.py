1b0000580ba7ca5f870b58ce894706c08372e34c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:39:56 +0000

    reach new between perhaps obtain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
