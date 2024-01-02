24c091c223b1b38797cdf3c76bc9a67450af94b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:14:29 +0000

    bringback confused undo rule keepfrom

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
