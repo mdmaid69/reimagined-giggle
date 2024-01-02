6aa1b7b9aab0d02fa081a094f39c5caecd054ec6
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:10 +0000

    equipment simple miss healthy mean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_palindrome(s):
+        return s == s[::-1]
