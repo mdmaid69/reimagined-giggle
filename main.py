db397857f8cc8e6860b51d1b57621480d22e241a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:23:33 +0000

    bang relation ought switch willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
