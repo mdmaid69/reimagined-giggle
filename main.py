def962093737f55ac5308849bc9a4a600b26272c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:55:21 +0000

    peaceful steer street zero bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_base_name(path):
+        return os.path.basename(path)
