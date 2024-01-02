049e7eae9587f1d84f2e061a624d48fa0da15e9d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:46:18 +0000

    through comein dare situation secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+from collections import Counter
+print(Counter("hello world"))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
