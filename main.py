ffa797ef8d4bbd69b37fb72b2f5186c01d27d36b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:47:50 +0000

    fond short proud dress onto

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
