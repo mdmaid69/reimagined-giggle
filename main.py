2f59240c17b78efca6dea9c0318adc236422e3e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:00:08 +0000

    whoever advanced allow keen fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
