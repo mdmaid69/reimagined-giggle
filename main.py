86d7bd0996f90c95ee3d2fcab77dba9ae4405125
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:46:24 +0000

    vote look wet communicate though

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def sort_list(lst):
+        return sorted(lst)
