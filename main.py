546a1bc0629ad3efc39903c781ee3e0e10102a86
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:18 +0000

    why last owe scale until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
