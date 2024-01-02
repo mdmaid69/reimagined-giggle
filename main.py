ea4855683fa1b435f86b214e3a9811387c25bb82
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:20:24 +0000

    believe command judge would yesterday

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def exit_program():
-        sys.exit()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
