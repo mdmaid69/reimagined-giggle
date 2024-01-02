dd5ac268cc104f24548b56cbc5871010551fe316
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:11:29 +0000

    majority might goaway transparent name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
