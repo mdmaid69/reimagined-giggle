fbc74b4ed63974c8ef68410ddf27f5253c9b8dcf
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:41:19 +0000

    miss interview defend cause born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_set(array):
+        return set(array)
