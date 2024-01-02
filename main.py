ee0ba4b66cac10221c6d84c553cd774a440f3d78
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:12:06 +0000

    broken art although nose no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
