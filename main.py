d3a7b370f78f2a862462d2e64746297007330af4
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:25:55 +0000

    attract successful game interesting stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_list():
+        return collections.UserList()
