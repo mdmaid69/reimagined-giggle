097520ec3531ae7fa0f767d80fe422d9282d5362
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:03:24 +0000

    sale press fall to trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import shutil
 def delete_directory(path):
         shutil.rmtree(path)
