606cdc1b9a9c146f5e13ddfe256a1fd66a639927
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:27:38 +0000

    expensive breakinto crime bang admit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_list(array):
+        return list(array)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
