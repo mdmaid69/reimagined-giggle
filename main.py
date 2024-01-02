29a8f605fa98e0c8540e0beb5b4ea603069e699c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:05:02 +0000

    should bread language modern mood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
