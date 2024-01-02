598422e6411c21d14475b6e6f9f67404ce2d195b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:40:40 +0000

    fasten pollution direct distance drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
