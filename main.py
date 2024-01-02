cb09d3cc2796db5bbcf52e63b6dec7804d7992c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:36:50 +0000

    demonstrate children space why loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
