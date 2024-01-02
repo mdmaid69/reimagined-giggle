cac66c8105d4618460c0d2ad65ac73b115f0a6e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:44:49 +0000

    trap meeting compete real climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
