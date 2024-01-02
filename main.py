2f75edb66de912d018928a24c4dcb4906da71725
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:45:27 +0000

    bright tip entry comefrom shout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
