5922ad0b9962d8c1c1646bb75f4a9f8d5bbcf491
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:43:56 +0000

    hurt address spend lost react

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sys
+def exit_program():
+        sys.exit()
