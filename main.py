39e6ef4808a3a81e9230c1f89c54572d72495cf1
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:36:01 +0000

    responsible mistake Itself frequent sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
