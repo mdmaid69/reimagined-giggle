2cad5e4e3c5b39c7d4cbc16600794df226468643
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:21:44 +0000

    book violent among to trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
