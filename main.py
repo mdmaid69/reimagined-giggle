4cda13ec57e892ea7c5f20e854aff9a94d0b201e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:01:56 +0000

    involve attitude round travel beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
