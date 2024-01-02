c58e822dd1376c27b368cb99966e5dee1f10ad06
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:14:14 +0000

    man previous convince too defend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
