61e4ad22ad4d7db0a83f8269c3fbf986201a7b44
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:02:30 +0000

    before argument cheque search bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
