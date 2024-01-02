0600a34c7fae678a1b8b922941770cded8a2bf73
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:34:51 +0000

    careless listen worried for calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
