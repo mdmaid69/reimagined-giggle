0e15e92b4773bc5613797402e1ee798e6913141b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:13:11 +0000

    emphasize realize father effort near

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
