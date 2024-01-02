647f3cb2af0c546f2373c0653a5fccfadd7a7016
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:32:36 +0000

    throw small although stamp nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
