0c91c9b7ae50a004c740941d392f27919ba027f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:07:15 +0000

    sleep command turndown loose thirsty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
