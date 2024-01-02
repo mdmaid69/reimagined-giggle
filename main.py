fd2e16361776381e6add026241c7ae653f5889dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:22:16 +0000

    burn thick note friendly criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
