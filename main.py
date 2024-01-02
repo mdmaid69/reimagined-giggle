40fa8b640336eb4b5a8f3b5bea9285c2ff88c973
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:14:35 +0000

    like comment within blow film

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
