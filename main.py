23d25eb0aba47f6aa1f91768c059ca0a337ff5d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:28:07 +0000

    equipment money always cost reach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
