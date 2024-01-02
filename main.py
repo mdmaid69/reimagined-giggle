07692623579177adaf09e7d107b3eddf6c224cfe
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:25:32 +0000

    coat better gentle plenty importance

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def calculate_pressure(force, area):
-        return force / area
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
