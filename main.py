8de81fda8adf8a44d7b3a52df4f737eb92c52028
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:19:49 +0000

    yours Ww chief better unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+text = "Hello, world!"
+print("Characters:", len(text))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
