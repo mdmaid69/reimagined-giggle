e42ff630476c30aebd9ab8e2384b10872d018f91
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:25:22 +0000

    report could measure correct fit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
