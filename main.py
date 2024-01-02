2721890af037501857b4d2d4c6a366ad30ac2d30
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:51:01 +0000

    mood clean wrong disappointed tool

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
