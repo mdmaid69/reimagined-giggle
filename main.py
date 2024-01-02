f3d55acd004adf2af44eb7994b10934c1b9a1d83
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:14:12 +0000

    call state getaway repair without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
