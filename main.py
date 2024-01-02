addcd7f1f99b6e6c422c8017efa545008bcaa770
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:49:09 +0000

    long everybody interrupt proper task

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def split_path(path):
+        return os.path.split(path)
