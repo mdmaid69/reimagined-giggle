27b7cd7af47d2114229bac558b8a6b666b39ceda
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:23:13 +0000

    teach project bang tip share

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_queue():
+        return collections.deque()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
