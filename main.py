e675c017951558aa9e5a63b26e3f37388c55256d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:10:41 +0000

    far respect near branch rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
