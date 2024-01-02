cacc962005425ddc86c751cd654f8cc653d1c326
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:11:33 +0000

    make among gentle hollow responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
