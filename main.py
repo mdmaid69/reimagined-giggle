be39ffc159f756ddfebe625ded5fbdb651705439
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:54:34 +0000

    dull ill book tear terrible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import platform
-def get_os_info():
-        return platform.uname()
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
