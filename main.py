2e76615951243348f538f2d1b5b66890a35c9388
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:26:42 +0000

    occasional rest telephone pay abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def is_even(n):
+        return n % 2 == 0
