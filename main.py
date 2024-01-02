bac84b3904fcb01d11222f2cb055e5ebd539158f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:34:27 +0000

    interest common to secret kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
