ec15b80229285aa934c9c278d226a02cbe0d01d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:42:38 +0000

    about calmly preparation stir close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_words(sentence):
-        return len(sentence.split())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
