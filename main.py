90e991f6383a8dfc215e6744750a55b8cb063c7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:28:52 +0000

    pretend desperate bye big past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
