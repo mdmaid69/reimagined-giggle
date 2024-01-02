f4528fe893692493ecda187175fdf644cb69f83c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:12:29 +0000

    activity size ilovedicks continue health

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
