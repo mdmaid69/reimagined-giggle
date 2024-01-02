6e48e65c9c22bd58868e5aca32aad06dcbbfb22d
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:54:02 +0000

    uniform valuable ashamed list offend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
