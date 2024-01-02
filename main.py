a66b080ad5d77ea7fb576b44db9353de7e1627a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:14:05 +0000

    moral intelligent search back mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
