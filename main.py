7839f1c6c9c7842a4654d8ea68aa2c5cb44bfec9
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:32 +0000

    date appear belong leave honest

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+print([x**2 for x in range(10)])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
