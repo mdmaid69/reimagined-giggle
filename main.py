c886612ac9f2846eda32057fd2b601b35cf92733
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:54:16 +0000

    poor bye discuss speak trick

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
