6a361f342672095d9e062a99a2033df848311960
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:49:01 +0000

    travel stand sorry question goback(to)

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+from collections import Counter
+print(Counter("hello world"))
+def calculate_acceleration(speed, time):
+        return speed / time
