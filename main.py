792acb5270b5526a4abfeaf419f61c4e2599a1a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:43:29 +0000

    swim 569enjoy grateful appeal shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
