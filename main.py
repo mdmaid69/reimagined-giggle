3d6c07ce6a87f22bbfb4cdded94955062af4e54d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:58:03 +0000

    only zero challenge offence delicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_current_directory():
-        return os.getcwd()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
