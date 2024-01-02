2e0ce8c2c0e2c4c14864e77a12c18c5e2dae5ed6
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:17:35 +0000

    bet trial character makeinto slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
