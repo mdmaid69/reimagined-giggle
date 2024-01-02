185cf549fbd8c6a7fcbc3a341462d1ebefa476f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:10:34 +0000

    patience between anywhere change limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_pressure(force, area):
-        return force / area
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
