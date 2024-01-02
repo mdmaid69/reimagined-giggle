7b67d6f42e031be8dccf08f5fe4416c3baae39e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:36:26 +0000

    television 573entertainment building disappointed naturally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
