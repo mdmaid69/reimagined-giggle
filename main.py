ad09e2b758db5704a0d33dab5a56059b57f7c932
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:57:19 +0000

    water politics voice music impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
