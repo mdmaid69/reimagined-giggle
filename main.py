c55d869423938340f4590476fbd8bdc6be2fe376
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:32:57 +0000

    university port pause train help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
