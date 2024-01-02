96d40f2f6f4b056c513d07e1fd917647b9bbfa6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:03:00 +0000

    come exercise move tall fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_max(lst):
-        return max(lst)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
