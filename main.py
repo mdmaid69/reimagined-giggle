3b2f2c606c201dc67e46d4dcbee36dc105110222
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:25:20 +0000

    holdout transport value crowded shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
