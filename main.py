704884fd6664773d3e2f063ab39783f7f1842c7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:12:46 +0000

    bringdown colour honest at stick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
