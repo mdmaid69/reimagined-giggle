0cf7e990bbf4ae1b035f382ecc5df62b70c20841
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:37:57 +0000

    behind that area wrap Kk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
