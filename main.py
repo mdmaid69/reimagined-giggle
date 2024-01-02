8247c0cd442055a20937d439bc135d3f7839a8d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:29:19 +0000

    head treat giveback turnup finish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
