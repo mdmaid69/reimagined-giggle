0042c05a84ed312d97ff119af6ada0c72d697bc2
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:02:31 +0000

    past desperate protest so calloff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print("Hello, world!")
