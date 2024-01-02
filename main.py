54d78355052a7c6242e1b2632a664ccd20e009e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:28:48 +0000

    twist shine undo address give

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
+import array
+def get_array_as_float(array):
+        return float(array[0])
