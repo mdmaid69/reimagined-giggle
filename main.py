6683b9533e4ac5e78e6d6ef40133c45aa1950529
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:45:27 +0000

    goon science shock problem complicated

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_density(mass, volume):
-        return mass / volume
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
