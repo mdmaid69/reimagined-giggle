112a24a4f6828d650ca1c5d87f962cee1fcbc592
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:41:29 +0000

    off roll over black under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
