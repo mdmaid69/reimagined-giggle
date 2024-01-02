21a9bdfb4d2c2f6f830a1524dee9faa3a586f099
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:12:33 +0000

    life both naturally game sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
