0710a2d07491a8ef5bdd84d04f1c69184b198a03
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:53:28 +0000

    welcome fond sound lot just

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
