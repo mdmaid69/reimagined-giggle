0e44c6bf8a9088059c51e000fe3d71339c6a6be6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:45:46 +0000

    away book proper buy dress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
