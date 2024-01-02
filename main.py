816bb6ed2c771077182bd37d44777ad5efd43824
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:42:02 +0000

    bite mixture within Yy block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
