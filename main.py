e04c28a0940ace3afa0f162ed16a13a7f6784ae4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:49:42 +0000

    wide combine trap safe spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
