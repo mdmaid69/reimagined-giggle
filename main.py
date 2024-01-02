ea1c99af9cac249aadd300d835215e3dc166ce6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:29:05 +0000

    lean give choice supply some

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-print(sum(range(10)))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+x = 10
+y = 20
+print("Sum:", x + y)
