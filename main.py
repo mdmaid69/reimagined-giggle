ea65d68e64eb02eaa58453e379d9adabaa2952fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:34:33 +0000

    almost dinner now goback(to) benefit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
