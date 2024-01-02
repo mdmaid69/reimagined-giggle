a20c09ce13a6b5251da3a176efdfdf886a3f3c61
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:00 +0000

    fork blame exist agreement rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
