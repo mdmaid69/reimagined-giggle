2e050cc3285aed8969f95bc68206b3abcb5b521f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:43:24 +0000

    character far either takeout ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
