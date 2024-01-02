8fc34955d4e27ecba99733c37ab8c82ec4e1855b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:17:46 +0000

    provide fork contents limit whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_bytes(array):
+        return bytes(array)
