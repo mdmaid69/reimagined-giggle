19a897525b33141838363b972c583bb4ebe864b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:59:46 +0000

    weather tradition wander balance either

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
