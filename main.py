1bb140f4b9bb2613eb13b07a204436163ce97388
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:08:41 +0000

    final crash region murder do

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def count_elements(lst):
+        return len(lst)
