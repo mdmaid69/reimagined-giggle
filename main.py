0abd4f10fcec8a8d7e66eef6e80e46692fa7bb9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:42:18 +0000

    ring petrol beside comein clothes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
