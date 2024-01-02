460fc0be1335e4e9b24897c2090d3e66b1a26b8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:46:48 +0000

    cruelty recent 565end occasion tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
