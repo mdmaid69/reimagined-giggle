408e97728b5a05c0d46fe3bdb041d078ff1c0e06
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:27:12 +0000

    now blind arm wet pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        return sys.version
