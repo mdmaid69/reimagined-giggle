6c00d3af3b8fb47cb6277895d05b7d960697db66
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:18:23 +0000

    turnaway below we taste children

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
