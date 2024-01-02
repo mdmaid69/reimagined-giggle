1e3acb37b1a68547ee56ad8b7fffac72907fb3eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:01:49 +0000

    power turnup purpose lookback(on) sure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
