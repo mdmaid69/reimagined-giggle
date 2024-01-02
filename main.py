b33eb17a8ce9d6e340ecda8b49d7801f9ff34365
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:47:56 +0000

    dangerous confusing unhappy threaten reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
