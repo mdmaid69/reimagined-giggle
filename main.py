c164c15353c45c2abc815d7157b3dd740b46c637
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:15:42 +0000

    female towards anything authority compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_area(radius):
+        return 3.14 * radius * radius
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
