faab7b5d53e1adeb5494b698e643a976d755b176
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:47:05 +0000

    power turnon food tense better

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
