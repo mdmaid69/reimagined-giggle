abe59cd15f5f9d82a3f96552fb35dabec9d4bdcf
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:34:39 +0000

    century along themselves thinkof why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
