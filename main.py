2700d532b57898dd14da23f3ca639a9c7788798e
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:23:13 +0000

    description post keepup knowledge its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
