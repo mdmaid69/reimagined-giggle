aa546bf8cd01b3e3575f3a8bf72437ec17f039ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:39:27 +0000

    government ours comfortable congratulations winter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
