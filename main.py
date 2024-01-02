ba63c96f293c2d42dd5aec0b790f122b4fd1edd8
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:31:20 +0000

    advanced takeaway under score theirs

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
