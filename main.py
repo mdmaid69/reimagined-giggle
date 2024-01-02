9b8b9fbb4eda9e748abf50204ab4fc631bf6bae3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:17:26 +0000

    brush effect pattern road 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
