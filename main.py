13ec0e8e71079c2a948048ca1f93f53b3f296f06
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:33:11 +0000

    wide return noisy set shelter

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
