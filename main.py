3fc80247e253f562797c5f43b7a2bb19e5b724ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:25:28 +0000

    turn piece interfere available transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
