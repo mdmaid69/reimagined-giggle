709876d3bb33852122eb8daad0d8bb0f526eb956
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:48:42 +0000

    form bread small address row

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+name = "Python"
+print("Hello,", name)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
