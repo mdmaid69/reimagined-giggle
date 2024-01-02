5e4ae443c1f426851d87c69756b06a062428364b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:43:34 +0000

    discover wet trade enthusiastic die

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
