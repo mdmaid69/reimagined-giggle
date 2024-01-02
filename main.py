f7c548e7ef76963b43ab886db8473c2a3aab4c21
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:47 +0000

    succeed before word pride every

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def square_number(x):
+        return x**2
