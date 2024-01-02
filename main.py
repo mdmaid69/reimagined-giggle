57a6c9a34f7cc614f6f75fe18c8fb238985b32bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:57:11 +0000

    Ww brush under disturbing turn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
