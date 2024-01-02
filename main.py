eb2c068769b068d5421d3b443b3ec3a025f4434e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:12:30 +0000

    kind getback polite road read

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
