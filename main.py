4911e04442238633297fa3aa10051206c10846d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:06:14 +0000

    distance set position kind such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
