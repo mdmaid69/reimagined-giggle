ff79c6d52ae3b93de702857c34502b9be8c1dedb
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:03:08 +0000

    picture nerve job let work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
