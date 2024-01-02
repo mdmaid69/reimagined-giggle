3d4c75c264f481583b78bcd0632331fbb02a8690
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:10 +0000

    spring react spread mainly trial

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
