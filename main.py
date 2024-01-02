5a6035f9a9c431044d318b540b336a0fe0aecb3c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:53:57 +0000

    impressive needle rough rich substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
