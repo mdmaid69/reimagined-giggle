9cca3e50ced1d113f61468697d6b72872191ddcb
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:30:45 +0000

    information grateful comeback manner correct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
 import functools
 print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
