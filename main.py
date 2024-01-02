c3e45ce02d2bb298ef576cdb49313be4e445cb48
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:42:13 +0000

    story spend increase in reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
