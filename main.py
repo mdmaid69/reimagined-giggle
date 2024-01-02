c8bba397ff0a74e6b4d1b76a37dc33592b6c48e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:48:24 +0000

    accidental surface week within rain

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
