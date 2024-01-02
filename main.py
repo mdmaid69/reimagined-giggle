f2f2958df084dc8ca6488e875bc5599ebc4e22db
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:40:14 +0000

    lower yes move achieve trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
