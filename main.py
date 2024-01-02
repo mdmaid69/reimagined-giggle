e0c062557765d7d77f997da6a82db0801bb26cfe
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:14:07 +0000

    car glass deserve joke Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
