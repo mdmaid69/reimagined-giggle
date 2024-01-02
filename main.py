db7d081f9bb7076cc81528827e5361c8d8062a85
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:20:57 +0000

    score relation lunch colour report

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_min(lst):
-        return min(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import tensorflow as tf
+print(tf.__version__)
