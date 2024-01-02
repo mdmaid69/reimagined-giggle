b2c1659de708814f7650e1f6da9189ad0f520297
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:56:35 +0000

    story future call think later

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import re
+print(re.match("h.*o", "hello world"))
