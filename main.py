d5316e56b704270d75a8e581067329c20bd6a40e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:42:11 +0000

    getaway share sound ability throughout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
