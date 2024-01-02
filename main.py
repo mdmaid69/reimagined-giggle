3974a0a806eb2011f607a57f80ebd20cb73c9ff3
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:24:37 +0000

    detail sleep team throughout jump

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
