d166e9896431e9427ddc74d19a5a23d5ca5fae4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:41:47 +0000

    about watch accept include without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
