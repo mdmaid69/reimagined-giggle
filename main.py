35b0cb6a90c4569a3de0dc26f4fa04ec15e589f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:37:26 +0000

    offend smile mystery passage Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
