0710398deeacd8242bb8c85b93fdbe1490f3fae5
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:59:51 +0000

    choose remove explanation takeback record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
