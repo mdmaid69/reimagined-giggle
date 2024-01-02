45966cd385b10ea4080ee455da3e354c9470ba11
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:20:31 +0000

    condition loan support muscle eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
