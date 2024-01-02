29d215ff5a9163c69181148da88a3e7f926b1d12
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:33:51 +0000

    down guard runover sing surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
