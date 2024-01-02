f5827f20fcbe725d366982c1b129bac829763c91
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:28:10 +0000

    learn goodbye protection responsibility bin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-print("Hello, world!")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def find_min(numbers):
+        return min(numbers)
