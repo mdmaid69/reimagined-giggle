9f841011f8cdab2bddc2408d574f722446232915
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:19:37 +0000

    lot declare depend back roof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def convert_to_octal(n):
+        return oct(n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
