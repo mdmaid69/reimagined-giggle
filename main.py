a0bfd0483bae7fc2dcb2fc0a15174b23f078ea7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:52:33 +0000

    there crime limit jump estimate

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
