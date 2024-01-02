a0d49a867b553e4b8b1d7d136d76fc9ce040e23e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:11:52 +0000

    section simple dress butter birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def convert_to_octal(n):
-        return oct(n)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
