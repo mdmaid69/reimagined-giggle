4ecc3a8a332d39676507aca5993087352e78df79
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:37:48 +0000

    bringdown cheap hospital live 568energy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
