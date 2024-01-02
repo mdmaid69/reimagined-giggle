c438f52749a20cdbe378a08a8cdb9b0f0d8e433d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:41:06 +0000

    preparation steer convinced interrupt phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
