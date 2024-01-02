6d7443debcf65d3f435626a5caea562d03754052
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:09:20 +0000

    suffer wait address morning debt

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
