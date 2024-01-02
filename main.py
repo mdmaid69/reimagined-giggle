73e353c38f67ddd05dd02875fbc7e30860a5ec1f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:32:22 +0000

    among worse polite sound obvious

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+print("Hello, world!")
+name = "Python"
+print("Hello,", name)
