d998121a889051e8d7a0176284d0bbc059865533
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:09:14 +0000

    deep doubt achieve react degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
