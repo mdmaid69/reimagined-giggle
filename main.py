825d4eb94905631b215a75259876b711cd64fb68
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:53:38 +0000

    neat early basically join anybody

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def sort_list(lst):
+        return sorted(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
