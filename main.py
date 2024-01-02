e5e2827bd3ef09aef134c1904f82460d40dc22bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:31:24 +0000

    discussion show sort consult holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
