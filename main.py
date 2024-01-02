f46bec0c06a2848b22520c9ab1878f8fc700165b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:38:44 +0000

    uniform act spoil change direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
