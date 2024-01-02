1483ff7de3c6caec9edee33ee37cc06b72964f78
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:01:14 +0000

    squash still anxious worry wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
