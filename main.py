2d746e39518affeea70143a04f08c74b6b924532
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:22:52 +0000

    grow sudden combine holiday around

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import tensorflow as tf
-print(tf.__version__)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
