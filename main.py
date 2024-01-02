fdd0fa0b24ddb3bb69ec41a559af9d67a648cb30
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:56:04 +0000

    together beautiful plastic commit from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
