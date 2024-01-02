785a4f5910d7c24b4fe86623fb1c81260be302eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:53:14 +0000

    transport object proof study whether

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_length(array):
+        return len(array)
+def reverse_list(lst):
+        return lst[::-1]
