23ebdd2bd3202000187602a33d9ea150fea7787a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:46:05 +0000

    person usually that comment whistle

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_item(array, i):
+        return array[i]
