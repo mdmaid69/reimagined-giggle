09675e4cd8aa9c272d429f2b58ec3e490e9de767
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:57:01 +0000

    common sticky platform dead tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
