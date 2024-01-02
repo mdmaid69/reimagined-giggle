e25df49ab232d41c77b2bf5a17712aea82e09502
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:12:29 +0000

    busy disapprove fair desperate spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import sys
+def exit_program():
+        sys.exit()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
