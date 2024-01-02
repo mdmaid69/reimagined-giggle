ef243b3b61109459ec4377528a38c1e1ceeee2b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:01:24 +0000

    beyond children shoulder excited cut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
+import sys
+def print_python_version():
+        return sys.version
