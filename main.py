aa0aaea91e3c6d83de83cb9ff01894005ca8527c
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:37:24 +0000

    hurt sort underneath hospital regional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
