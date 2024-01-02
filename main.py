faabd5dd20dbbdea12089df9278ce167635d1a72
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:19:18 +0000

    advantage they get show neither

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
