b1c0d46fb03a1c323fc698dd54746b850bbca694
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:05:53 +0000

    keepup sister walk cheat similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
