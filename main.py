c302928b038b970efb609938bc7f429fdf111fb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:57:09 +0000

    seriously replace university collect disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_user_string():
+        return collections.UserString()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
