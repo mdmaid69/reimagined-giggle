306bcc08086d4eacfd4e3377b0de0fc299da8fba
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:06:15 +0000

    trip example concern live punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
