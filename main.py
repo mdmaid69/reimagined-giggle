773f74904ee4a63ef1eda02bbcf2bb35a8e6bdf5
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:59:49 +0000

    test equipment scream match size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
