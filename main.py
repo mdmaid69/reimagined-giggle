da0c0f7d32b4e491a3040a6cdda804108f17cbb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:41:17 +0000

    attract know herself live across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
