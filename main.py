85e1cd54a3800a6faae2f3dde5d27b97420db743
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:18:57 +0000

    authority himself valuable float high

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def clear_array(array):
-        array *= 0
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
