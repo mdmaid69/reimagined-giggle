57cf35ef3f3ed128427eb13a564ed4dd80f8ca96
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:39:30 +0000

    plenty no dare certainly join

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
