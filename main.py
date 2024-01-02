6952f4111523c7b3aa906b6298947283c45687fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:25 +0000

    show takeoff defeat sure as

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-from collections import Counter
-print(Counter("hello world"))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
