ca39b9ed5fe83430b0b4eb46fa69b83987668be7
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:44:35 +0000

    knock accident exercise star report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
