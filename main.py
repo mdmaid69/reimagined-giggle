8dcd8c331ef2a640cca0c6c7313c337afe85edef
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:00:50 +0000

    exist address impressive sensitive pleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
