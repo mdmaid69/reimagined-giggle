8773ff5d701e8e7f87f9256db39f8ccc9e95f84a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:24:06 +0000

    reach responsibility curious guard where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
