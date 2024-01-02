7e0083a7d34289310b0344684f4f8279b5c1e408
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:32:54 +0000

    also rob shrink collection careless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_rectangle(l, w):
+        return l * w
