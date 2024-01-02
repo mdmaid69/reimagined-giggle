a3f01070900c025fca21b4c51e47e73d3d9adde1
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:17:49 +0000

    remark cap cinema process comeback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
