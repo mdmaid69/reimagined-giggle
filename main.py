caa305d81a74f14b20ecbaa5592888e2ab8fa1e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:17:04 +0000

    circumstance getback bin lucky roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_words(sentence):
+        return len(sentence.split())
