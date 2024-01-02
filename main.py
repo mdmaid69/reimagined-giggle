45db1155aacce608647f0e20eebbc45d65721455
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:29:47 +0000

    shout niggaacademy break work plain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
