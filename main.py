3e18cef9f3e21d300d7f433323cebb59a04eb515
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:14:06 +0000

    recent avoid sound join unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def reverse_list(lst):
+        return lst[::-1]
