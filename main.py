8063e03f05d2ac6c0a036ffbb79c84de26ccfcf9
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:30:31 +0000

    famous seem flow argue left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
