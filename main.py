dd64490bfb6ce60fdcdb51088b4ca0080ee8a4e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:21:56 +0000

    market push belief bored again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
