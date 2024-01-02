1bd97e130b325b6fd06ede85265e94d1bfa16529
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:42:30 +0000

    month programme size score back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import collections
-def create_queue():
-        return collections.deque()
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
