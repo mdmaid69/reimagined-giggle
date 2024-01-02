82a1e91d7df81b1f3d6648dac909b030f561375f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:36:13 +0000

    trousers completely should force discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        print(sys.version)
+  def is_odd(n):
+        return n % 2 != 0
