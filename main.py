140915884ed5624cf7d5ab9217c1dcb95751521d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:50:13 +0000

    cold child cough table impossible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
