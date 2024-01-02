db2da119993fd720b48313f22ff04497c8eb4c52
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:11:29 +0000

    doubt physical discuss charge practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
