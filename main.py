4b4a2e21b229fcd46e2a498159dc25a185e0d3e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:31:07 +0000

    yes flat room refuse survive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import time
-def get_current_time():
-        return time.time()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
