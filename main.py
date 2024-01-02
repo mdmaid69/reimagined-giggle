204604ff04fd6652cd53d976b8b804060defa15e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:32:34 +0000

    generous right violence wherever throughout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def find_max(lst):
+        return max(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
