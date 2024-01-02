fb55381864c85e7c3ea6c735a2eab754c96fde81
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:38:35 +0000

    one achieve record side provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
