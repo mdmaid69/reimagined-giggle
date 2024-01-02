bc87cd61afe9bb31abf6e1ac50aca53aa7056f06
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:00:12 +0000

    handle scrape second around shall

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-print("Hello, world!")
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
