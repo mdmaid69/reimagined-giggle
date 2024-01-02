bea8be215fd088c95e634f582220d83e2b245527
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:35:12 +0000

    ability people clean complicated straight

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
