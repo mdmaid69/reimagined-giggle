d93eb23eef83bc3b0429e4a5d69bf2c68677c21e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:31:30 +0000

    increase extra some cough office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
