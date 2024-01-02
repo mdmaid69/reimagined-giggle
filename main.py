78074eca87a004f66d79a025d306a57e602b3951
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:05:32 +0000

    piece must week size get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import time
-def get_current_time():
-        return time.time()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
