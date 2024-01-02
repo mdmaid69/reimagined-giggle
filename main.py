537b2fe4cc9c151e7eb3d18d493409dc94033cd4
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:06 +0000

    confusing care wheel dry may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
