0c97796848445702729667f6aaacd24d522e186c
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:18:22 +0000

    get affect will square underneath

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
