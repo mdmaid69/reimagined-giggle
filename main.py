3ff136d3403bbab19fe8a5a83983c612202b7da7
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:15:24 +0000

    minority earth disturbing when next

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_power(work, time):
-        return work / time
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
