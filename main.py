9c46e03c1fffc743cbf50dd8b3e7204eeba4aa9a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:53:55 +0000

    win explain theirs list language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
