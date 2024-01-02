ac539f943c8fa0daa2aa0744545d813bb491abe7
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:45:58 +0000

    next second cry point boring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
