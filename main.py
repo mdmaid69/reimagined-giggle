a8cbe901b3e981538ae16a0d485a89c87c724d80
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:03:40 +0000

    tap everybody holiday height murder

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def count_words(sentence):
+        return len(sentence.split())
