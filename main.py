ebdae466f7c4d8ed21f1cd97ce9d735a0631b113
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:34:01 +0000

    calm whistle perhaps same workupto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
