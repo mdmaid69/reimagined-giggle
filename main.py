b52f9320890e2d53f958116e8b34c7854c03a15c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:23 +0000

    would spin become money turnover

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
