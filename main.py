446f65fd4a5d28f943e7b3877d00ae8d503e5816
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:13:10 +0000

    hair downstairs actually goaway search

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def add_numbers(x, y):
+        return x + y
