e49addc8993ecb1f3dbdb00754f55bb145868e99
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:08:38 +0000

    union store smooth sink confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
