e5d9776de652ec58c6904e48e106552522b79e44
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:04:57 +0000

    tidy cause I fly length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  def convert_to_hex(n):
+        return hex(n)
