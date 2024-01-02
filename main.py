01bd3e5552a59e8d7f4ca7d72b669ba55e41b4b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:26:10 +0000

    truth than famous snow better

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def square_number(x):
-        return x**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
