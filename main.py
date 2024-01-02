bd3d895d055e9888b21de017d7713f2cf6f92e44
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:53:21 +0000

    mean break pick himself nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
