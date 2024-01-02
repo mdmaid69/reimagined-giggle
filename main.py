12af0159a43af190711e99ed051df17a7348dbc3
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:20:30 +0000

    thumb shadow cost more year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
