2f698567eea237b91dec35f9ff061f8a339f09f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:59:55 +0000

    complicated as wish flood favour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
