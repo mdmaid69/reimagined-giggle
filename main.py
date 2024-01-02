0cd6267f71ee76ede8c9fcc6ac864197e40c0b64
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:25:46 +0000

    further idea close union arrangement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def add_numbers(x, y):
+        return x + y
