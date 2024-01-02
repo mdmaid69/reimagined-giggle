a1f02d65c719a670c1a67f77305b6fe0dcc2de74
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:01:00 +0000

    port lower spare shelter proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
