47671a38f15c622b065b08bff18dbcfadca2cea2
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:33:48 +0000

    print concerning correct hardly previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import sys
+def print_python_version():
+        print(sys.version)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
