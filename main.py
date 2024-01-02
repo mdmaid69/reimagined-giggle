5f526b2334f467b527505313a5082e2a992b5262
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:49:29 +0000

    happy argument stranger shop circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_today_date():
+        return datetime.date.today()
