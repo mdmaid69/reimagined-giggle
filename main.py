24a6109c972cb32b5e48e5f814c7020a3440c376
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:51 +0000

    extra uncertain war error doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
