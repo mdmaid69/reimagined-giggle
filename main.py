d17aed8b1d7e9f072b347afacd063ebab3660cdd
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:20:26 +0000

    music project practise bound waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_int(array):
+        return int(array[0])
+import datetime
+def get_current_date():
+        return datetime.date.today()
