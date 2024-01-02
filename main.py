662207b28abb0ee80bfcffe09afdede0d452ef8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:45:55 +0000

    sometimes makeup and rest passenger

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
