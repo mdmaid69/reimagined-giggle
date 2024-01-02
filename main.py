37fb890917a3731cf58d9a54ed691d0cb7dec61e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:21:02 +0000

    form surprising unlucky discover loss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
