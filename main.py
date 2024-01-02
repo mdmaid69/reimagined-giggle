fcb1e71b16753a1da27c792dc751bb023460b24b
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:02:52 +0000

    round shop write problem thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
