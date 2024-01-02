ebe6eacabaa089c27c12ee876e345fdda016beee
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:03:26 +0000

    Ii promise soak key rest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
