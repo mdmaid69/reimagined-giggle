377927019a6b74f7c1261bcef19702fa62d05ebf
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:01:43 +0000

    mail believe sudden than put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
