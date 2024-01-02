a09e706d10ddeaa0ef3526ea84c80229f01fa8e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:02:17 +0000

    paper defend hold spite repair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import tensorflow as tf
+print(tf.__version__)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
