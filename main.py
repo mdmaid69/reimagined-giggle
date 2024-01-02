63f5f4a76838d800486d7a9386f54b7d495fd357
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:02:40 +0000

    bill careful climb arrange considering

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
