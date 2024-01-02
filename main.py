936591b500743b0f2d6e915e7cc0fc9f85ee28b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:50:07 +0000

    present settle nose giveback air

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
