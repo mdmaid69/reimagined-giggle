4a2558a76d294d4e65ba62b5b6e931dfae9886d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:05:01 +0000

    tidy reduction thumb content respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def reverse_list(lst):
+        return lst[::-1]
