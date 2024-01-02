c9b166e7e84044e517bfac357a1289eb9879925d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:39:24 +0000

    pleased keepoff ignore bill light

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def reverse_list(lst):
+        return lst[::-1]
