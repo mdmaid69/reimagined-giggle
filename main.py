bba473754245edc2982fa29c5562f84c5caf132b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:43:28 +0000

    consult emphasis fight now delicate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
