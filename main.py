c3bae34e938a67fc1d4bab29d9c5693e1f332e30
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:45:34 +0000

    past dead embarrassing deliver bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
