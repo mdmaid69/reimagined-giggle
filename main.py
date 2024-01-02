10925b0b13dd99a1bcdbc161e8f21ca156aa0ff4
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:23:35 +0000

    share ignore pause congratulate dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
