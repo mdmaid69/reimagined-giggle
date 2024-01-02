5f2e5d1b2c93ba90d03640942ba540693ae873bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:19:47 +0000

    chat collection shop relation trip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+print("Hello, world!")
