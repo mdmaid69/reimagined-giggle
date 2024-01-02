119623c00c52963c87a7a5ddc8415024a1a16ab5
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:01:33 +0000

    pen it example across sky

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
