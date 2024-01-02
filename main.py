012a0dc3016b14402db5b4b26d4ca312b189f62f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:45:03 +0000

    war runinto though extraordinary production

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def is_odd(n):
+        return n % 2 != 0
