dadeaf2b2669382fc772b86f4f143ce2e638ed74
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:04:25 +0000

    reach door university quarter wide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
