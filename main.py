5661d57af0bf0d041537b9b7ecc53c63f8cf6a2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:39:42 +0000

    clean silent before else impressed

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
