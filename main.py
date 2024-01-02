67bdd02a7f5249becb92b0c5953541da2a84ea51
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:47:08 +0000

    tool successful boring talk wall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
