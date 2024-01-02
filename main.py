2482cbff69b8cee8a5b2595afb581b7896ef87a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:36:22 +0000

    cold trap activity development nurse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
