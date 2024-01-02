e89a4ea68428408a019aa1d2ff3bb2e48a03b815
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:54:01 +0000

    market with also steal describe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
