77e20b1e02507d52a1c359fb911871e3657e89a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:16:29 +0000

    office trial building their influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def find_min(numbers):
-        return min(numbers)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
