0a6f061e313d0ed4cc635bdd1e5ba30be6e03ffc
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:03:33 +0000

    workout fence open bottom rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
