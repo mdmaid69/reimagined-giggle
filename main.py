b916d6d6e184d5a73809ad5cbae9183f16b8b4de
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:42:29 +0000

    contain very tread corner second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
