d46bdc32dedbfbb2bd8771f288599f8331968783
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:42:15 +0000

    collection aim young new contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
