07f197ccaf8f88039a6d464a62370179ea661d96
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:37:34 +0000

    top lower cut confused middle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
