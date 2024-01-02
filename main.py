ff03d862bdc237056a4bd29862f417fa1cef3469
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:18:13 +0000

    scare effort until touch like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
