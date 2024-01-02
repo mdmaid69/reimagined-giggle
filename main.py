2546cf8f71f167f555955d7eb6c14acfe7f9c5bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:07:40 +0000

    weather food shut disapprove report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def convert_to_hex(n):
-        return hex(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
