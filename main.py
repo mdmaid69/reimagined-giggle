48a7e8af136e023bfac2a4bcb704b38e4fd55423
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:39 +0000

    prize without deep above agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
