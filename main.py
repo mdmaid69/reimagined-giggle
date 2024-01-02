9b227c93a5099b74c5fc2503ccc0d0cd28b06913
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:12:50 +0000

    takeoff little fix throw extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
