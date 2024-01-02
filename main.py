d98f5e8beb6a8b1c54a7ded1da7813f8c768a815
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:29 +0000

    minute brown connected shame often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
