e436530984f29e14ca21e82436510919363812e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:45:59 +0000

    person everyone himself much always

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import datetime
-print(datetime.datetime.now())
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
