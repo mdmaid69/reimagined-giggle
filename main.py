7109d5d405af7f845913c82d4faa7fbbeabe87e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:12:52 +0000

    otherwise roof roof behave style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import math
+def calculate_floor(x):
+        return math.floor(x)
