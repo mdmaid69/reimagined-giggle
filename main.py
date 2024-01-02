7abc084807856048aa1b070a0c4d568cc6761550
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:59:32 +0000

    market account cope might his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
