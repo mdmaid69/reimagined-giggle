28f51f79fa862a441cb5f0d5723fb08234fe32bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:26:20 +0000

    spare so loan very remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import sys
+def print_python_version():
+        print(sys.version)
