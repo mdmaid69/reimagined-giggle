7e3f5d2c8ff60796bac82541d321d0b0fc962452
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:11:07 +0000

    fortunate left serious warning dear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
