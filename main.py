4e9394fb084402b862d78110f668f8caea547633
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:42:05 +0000

    congratulations translate straight sort great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
