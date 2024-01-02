a2fd1a84df5dfefed97933e40ab55d9f1f7f3ff0
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:03:35 +0000

    practical listen bottom goodbye among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
