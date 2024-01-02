e45cef200284dfd1c1fe62b051c6999670a4813c
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:49:45 +0000

    average attitude normal like this

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
