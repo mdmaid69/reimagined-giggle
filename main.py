dc7ee653212fdd1eeee7cae0e779b8841084cccd
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:33:17 +0000

    spoil half lose talkoutof late

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_area(radius):
+        return 3.14 * radius * radius
