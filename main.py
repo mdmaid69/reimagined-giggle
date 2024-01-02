8836f2e3c951ce0910474090a92b07a9d61ad7d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:52:39 +0000

    thankyou announce necessary none port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
