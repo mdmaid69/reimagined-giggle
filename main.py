bcaf8ddafef1403d9ecf0c788ebafb35d6032504
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:43:10 +0000

    spend sting sensitive muscle exploration

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def add_numbers(x, y):
+        return x + y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
