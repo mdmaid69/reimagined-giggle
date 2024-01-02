cb739356783f75cffcb6fa30e5d970f06eec4e1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:01:52 +0000

    fork shout progress swallow sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
