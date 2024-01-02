5cef04d0aaa1646ab07e9903c31ea7b2a7df8263
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:03:03 +0000

    surprising before while welcome consider

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
