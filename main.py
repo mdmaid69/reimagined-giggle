af3c567b0c0a59b630a3b51d4221d3e1892e3851
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:50:15 +0000

    branch indeed name sore evidence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_directory():
+        return os.getcwd()
