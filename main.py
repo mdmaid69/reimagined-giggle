2e56e77496a511e4492f730c92b635bbcfda2a2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:33 +0000

    generous gradually angry brush fortunately

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
