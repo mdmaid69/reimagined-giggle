40675fb9555113ea18292ef338ecdd8c900d7016
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:16:32 +0000

    confidence push fail laugh along

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def convert_to_binary(n):
+        return bin(n)
