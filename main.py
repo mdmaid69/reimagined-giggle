f49115788a25779e3f18ea0bb8cf6254d32ce637
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:22:21 +0000

    accident takeout club beginning since

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def convert_to_octal(n):
+        return oct(n)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
