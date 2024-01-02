c8dffa97541f4c10f001fe2f66a4194d6b80fc06
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:31:40 +0000

    genuine classroom comeup main set

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
