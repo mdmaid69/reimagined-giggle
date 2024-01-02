7f5a7d7d85245d7268fba140e9ac30da1ee96113
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:13:11 +0000

    choice programme date excellent risk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
