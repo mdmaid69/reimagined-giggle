f760e01cd55f7273b7e3ee5cf8d091024ff730e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:17:02 +0000

    borrow soft lot neck butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
