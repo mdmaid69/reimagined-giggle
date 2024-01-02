7ec2e7fc432d6bbd369ad302821ae0ed3b7e68d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:59:26 +0000

    willing almost me arrangement some

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import time
-print(time.time())
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
