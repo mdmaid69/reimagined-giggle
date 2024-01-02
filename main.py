9e0edac74d9fe01a8e1d47ef99082734fe94ab4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:28:40 +0000

    sing closely run immediately middle

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def divide_numbers(x, y):
+        return x / y
+def calculate_power(work, time):
+        return work / time
