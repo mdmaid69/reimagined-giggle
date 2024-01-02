40b492beea5094647e4851ee980ea7cba7bb8780
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:21:48 +0000

    extremely myself cry rubbish print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
