5a80355f3e44bd5818a3e0042b8102bbc654e841
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:37 +0000

    bring survive upstairs turnaway afterwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
