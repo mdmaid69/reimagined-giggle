2d6ff2a95e56d1ff2d823f2ba97742cc091dd71a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:54:07 +0000

    tired deliver mistake area excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
