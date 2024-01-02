e771eaa759256ac4dab8b4a72abe6a888a15d25d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:30:55 +0000

    pile completely flat case tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
