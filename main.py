4cd3a27f081d0a701404107d9ac22770c6cbfa93
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:01:47 +0000

    tell base address lose young

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def remove_duplicates(lst):
+        return list(set(lst))
