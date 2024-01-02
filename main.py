32b4f20241f16b686bac79ee8208fed5f65d49bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:07:00 +0000

    many quarter turnoff them fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import platform
+def get_os_info():
+        return platform.uname()
