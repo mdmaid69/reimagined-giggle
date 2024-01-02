4b96a377f08d9bac66f10afab4bed79259afbda4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:22:57 +0000

    shut cum treat patient equal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
+import time
+def get_time_since_epoch():
+        return time.time()
