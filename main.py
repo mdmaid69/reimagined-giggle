e0cce5f6d381dd33a3d767bceddaf1014c896108
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:59:39 +0000

    environment capital failure clear partner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_max(numbers):
+        return max(numbers)
