f109d22c8b4f6ade6a717efe87ee1d0974167a2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:28:56 +0000

    legal brain accommodation otherwise get

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
