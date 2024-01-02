5d05e166a728605c85d321ab81e5f2e2993aefd6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:58:44 +0000

    method large proud performance trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
