b4af61d4ab490ef389b209086d5019ddedd76d26
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:48:49 +0000

    drawing squeeze nice often intelligent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
