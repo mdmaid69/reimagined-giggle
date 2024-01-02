6fdee3fe38666c1ebbb13f75d9576063272b89cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:38:49 +0000

    fault underneath eye again although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_density(mass, volume):
+        return mass / volume
