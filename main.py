1e74bc24023ef0a11f98b6dae758c002845bf560
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:05:38 +0000

    follow file must connect past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
