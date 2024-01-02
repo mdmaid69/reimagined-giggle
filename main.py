ea4f904b14ca33ff874d860d7a167b14a72d0e9d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:10:59 +0000

    allow priority would height border

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
