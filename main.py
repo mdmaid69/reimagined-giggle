240517e8760bbe2d2960944db1a91c1d4413472c
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:54:08 +0000

    deep confusing Ff calm street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
