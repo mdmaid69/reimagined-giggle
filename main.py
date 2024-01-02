80e883f30ac7d15364e4efdfb42c360d80abeae3
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:56:21 +0000

    shelter over moral tie strength

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
