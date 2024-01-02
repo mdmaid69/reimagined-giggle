fcf8bc2f8a276547f1148728679b61642372a70a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:12:51 +0000

    often if child report joke

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def sort_list(lst):
+        return sorted(lst)
