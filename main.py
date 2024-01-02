5d7efac8d4d2de2f4d374285a85f60db4c1a4ff8
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:20:59 +0000

    fact actual space concern stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
