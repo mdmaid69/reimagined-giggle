7d3e582eba58f11a00a43c6308ccefbf8fd7a0c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:01:25 +0000

    hollow dark why guess sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
 import array
-def clear_array(array):
-        array *= 0
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
