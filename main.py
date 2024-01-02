17d66f929169d17008598aa0c076f5412a850299
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:55:37 +0000

    woman neither damp 566end reply

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
