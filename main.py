92016d8e1a793f8e32aa005ccf625cdeb13f92eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:55:38 +0000

    petrol few affect under may

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
