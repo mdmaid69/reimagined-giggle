f00bd5467a384a6e8e4dd8813499b02f056d5061
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:11:04 +0000

    standard official ill fast especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
