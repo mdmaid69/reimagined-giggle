e696b12641f620da2e434cccda087f16e081ca98
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:49:35 +0000

    child getup exactly back and

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
