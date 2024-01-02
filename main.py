cf8d968077532df4b1b984e368beac2f439ac6c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:43:08 +0000

    realize away culture stranger confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(lst):
+        return max(lst)
