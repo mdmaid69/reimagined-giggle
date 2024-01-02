16a53fc0bf7134b65631950f1f208466bb4fcd02
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:01:17 +0000

    danger food mother obtain what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
