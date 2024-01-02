4629b2eda44c0cc893a376100af9d479168986c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:02:00 +0000

    strength respect pick possibility beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import time
-print(time.time())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
