1549cbe5f8b674d8068b88415fbd437ed3b5cf54
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:10:36 +0000

    sick callout case protect reject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
