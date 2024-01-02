10023f7c17b6cf84b43ee9b475ae452c332fc601
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:52:39 +0000

    practical grand smile drop congratulations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_debt_ratio(total_debt, total_assets):
         return total_debt / total_assets
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
