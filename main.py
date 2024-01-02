35450c7f8a0badb84decc0157599fc4c211e1f7f
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:19:36 +0000

    certainly error tough reply port

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_list(lst):
+        return lst[::-1]
