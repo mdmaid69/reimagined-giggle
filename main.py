244c055882c0b78479afb65ec359f4410d781985
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:50:10 +0000

    field usual left trade message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
