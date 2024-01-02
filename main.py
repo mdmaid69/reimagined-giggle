6225549f21aca70bc8f17a56d0c96c845b2a5045
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:42:11 +0000

    one think lonely heavy closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
