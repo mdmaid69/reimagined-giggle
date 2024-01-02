9f4be514a3989ff6067305156add6084239c1732
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:35:19 +0000

    temper welcome second cheek hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
