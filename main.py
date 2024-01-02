5693276d7510eed0d5a9441ab044c15e4d42b712
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:28:56 +0000

    keepfrom limit angry container deep

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
