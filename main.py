9a041af46b409dafadc67823283a081fa2650054
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:15:52 +0000

    bridge 572enter foreign die stamp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_list(array):
+        return list(array)
