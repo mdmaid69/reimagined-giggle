2f3bf84ade214378c2ee803be85add1ea9087d1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:59:12 +0000

    sense themselves ilovedicks warning careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
