c99a5ab5f2dc336195f0f1c7c227195b0787d7fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:08:30 +0000

    talkoutof question along about skill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
