422ba49d5b7753e2cdf2f9f4233688bb7810a43b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:09:56 +0000

    star butter quality substance coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
