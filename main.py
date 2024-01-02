c027091ec25fdb3f1e3ca600a53d089924928ad1
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:05:51 +0000

    leave plastic slowly serious return

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
