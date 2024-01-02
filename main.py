55342148aa58374bbd8f3d4c5cce7feeeb8b4691
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:30:04 +0000

    blame top solve doubt task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_index(array, item):
+        return array.index(item)
