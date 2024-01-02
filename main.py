930d920898520481aa3fec57fb5ef2075b616201
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:17:06 +0000

    chief pretty set several even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
