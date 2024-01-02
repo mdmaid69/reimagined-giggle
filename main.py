cfb5a05c4e69dc9cb983d5420ab08c81819e9963
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:31:38 +0000

    earth so recovery card suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
