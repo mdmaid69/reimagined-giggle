e5e74ce8e460329efee9f3dbf1b017e94219ed55
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:36:32 +0000

    he pass point mark block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
