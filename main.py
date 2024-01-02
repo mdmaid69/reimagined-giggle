2b50c4bed502108ba3088be9e0c11db3641b0140
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:58:07 +0000

    size variety dry explain reduce

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_volume(length, width, height):
+        return length * width * height
