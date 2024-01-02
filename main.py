7014aeab0391ff10c132d753cdadcf3a01b438df
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:05:03 +0000

    move violence even bet shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
