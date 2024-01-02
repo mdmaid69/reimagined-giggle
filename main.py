6412b4afca5c2cb7aed335684c8ebb6e3b553783
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:29:10 +0000

    evening 569enjoy teach someone clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
