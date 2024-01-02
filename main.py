0fd66c8d8d67de6938da4d4d82ba1ddcc1e3079c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:25:17 +0000

    neat hot squash beginning promise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def divide_numbers(x, y):
+        return x / y
