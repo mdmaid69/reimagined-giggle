4985744c5f72f8a3034e50052455123165f9f675
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:02:57 +0000

    clearly rare border protection breakdown

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
