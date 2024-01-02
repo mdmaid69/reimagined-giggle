cf64f2af257960a3a47df0116dddbc4222ae15e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:53:42 +0000

    excuse deliver peace stretch butter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
