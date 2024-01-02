60d16b093da4ad7d08913fd66264959c5763c482
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:39:19 +0000

    coffee rid shake goup exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
