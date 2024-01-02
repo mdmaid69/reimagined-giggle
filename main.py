bcf39ba9869ea9208f3922f3399e301b5ab54058
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:39:49 +0000

    complain tend consult defend mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
