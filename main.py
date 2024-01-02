e7fedf4cec15cd3069ce5e72644d3ca06bd78e3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:30:19 +0000

    score notice damage ahead willing

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
