33e82f5226c3ca8f46622857c42c5b9ee143c3bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:17:25 +0000

    obvious surprised smooth defeat shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
