d5260958f7abc38d3faad8b86a36ecf1439aa0cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:17:11 +0000

    continue arrangement light wind official

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_speed(distance, time):
+        return distance / time
 import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
