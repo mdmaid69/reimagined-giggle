a757c787af6cfbe08478c59d28c4069460248d14
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:27:17 +0000

    comeover minimum stir nice mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
