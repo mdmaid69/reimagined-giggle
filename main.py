2480f4645cb47ce1731e8fe598d6b936f774f92a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:23:05 +0000

    brush majority rubbish fly light

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def is_even(n):
+        return n % 2 == 0
