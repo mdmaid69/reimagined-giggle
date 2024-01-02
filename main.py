43a41d352024c81e0cd257aada9e3bb6883aa352
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:16:56 +0000

    fun fold how otherwise travel

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
