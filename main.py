0deda6fdeb8aeb100a51b352d649124eee65ddbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:43:13 +0000

    cheque rubbish determined fit avoid

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def multiply_numbers(x, y):
+        return x * y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
