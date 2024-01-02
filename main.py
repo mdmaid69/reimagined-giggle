f0fe236823b7af5731ce56a3963ceb96cccba465
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:52:14 +0000

    even hollow comeoff comment maintain

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Uppercase:", text.upper())
