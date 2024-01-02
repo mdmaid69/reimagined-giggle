06ea313b494e9c8424db40050af4d40e14d71605
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:10:45 +0000

    behind part spray behind embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
