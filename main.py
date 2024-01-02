aa8113f424be4b166fcdc2fb890dca7d7ec5404c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:04:27 +0000

    eager crash attach illness keepup

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_max(lst):
-        return max(lst)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
