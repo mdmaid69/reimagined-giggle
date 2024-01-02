820ae61d559e5422a8a39b88f61a356639e1ebbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:43:10 +0000

    beyond hope sensible dress objection

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+print("Hello, world!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
