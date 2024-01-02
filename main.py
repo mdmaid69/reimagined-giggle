3ea5289be304bdf07cc84a8091d3f64dba36b463
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:05:12 +0000

    help cool conduct worried human

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+x = 10
+y = 20
+print("Sum:", x + y)
