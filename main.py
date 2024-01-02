fe18b8c9a3d13edf8ff60e95c96c3e8762d50190
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:25:33 +0000

    sea improve minority store hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
