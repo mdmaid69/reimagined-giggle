0828b2e8bdb11b0eddecc78ba5f905e8e3da8ac0
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:37:00 +0000

    regard national course Bb pattern

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_work(force, distance):
+        return force * distance
