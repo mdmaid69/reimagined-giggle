5b30c910b3ea3cf19171761a3793c85067031011
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:46:34 +0000

    down special holdon wire main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_force(mass, acceleration):
+        return mass * acceleration
