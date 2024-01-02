0ebaf2dd21b466ae196f8c9632210590a252d007
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:34:19 +0000

    someone slice goup claim warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
