a0ea9ee1340a5ccce56a55415d9ec3f7b2a82b38
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:39:02 +0000

    meet increase rub collect stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
