539ab4e45462dd81ba54bd429b379e310b8e6906
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:31:03 +0000

    scheme study argue slow open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
