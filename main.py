a3bd83fe3f2567f02793754f84e6c1e4e5e30cd7
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:25:08 +0000

    choice neat age be combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def find_max(lst):
+        return max(lst)
