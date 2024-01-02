cbe1a5abca7d8b31712c6e1d6904fc4e7bcbfd97
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:37:34 +0000

    away issue appear burn aim

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_power(work, time):
-        return work / time
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
