1a55c2a7fabfbd57d6fcbf2a7d42f219d7c45769
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:21:48 +0000

    politics eye yours favour direct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
