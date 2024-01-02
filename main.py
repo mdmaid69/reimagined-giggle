c98829e69dca53ddbc4ef807dc42aa47e8d1e099
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:33:21 +0000

    explain recent care urgent Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
