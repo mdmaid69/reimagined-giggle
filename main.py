bdc30c76c4920a4836517dda62ad8d7419ac8f77
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:52:34 +0000

    length hope error moon independent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def get_array_index(array, item):
+        return array.index(item)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
