db0bc134aec0e55071e1e55cd2368982a6567041
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:42:39 +0000

    translate back favourite breakinto themselves

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def clear_array(array):
+        array *= 0
