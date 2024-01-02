f28a5938e9c6d69fb6d661fbd9b0a957d406ec3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:27:40 +0000

    complicated error land little play

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
