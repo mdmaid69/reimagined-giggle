5492486749153db475a7c375f0ceb6db19c486f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:27:39 +0000

    cope offer Uu proud now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import platform
-def get_os_info():
-        return platform.uname()
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
