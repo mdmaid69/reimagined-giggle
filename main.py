8fc24a5485f19e6fe535fa0e09b1d0b6b016f16a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:07:08 +0000

    challenge down hour permission rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
