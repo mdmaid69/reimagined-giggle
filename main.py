b866ec5e19e9bbbfcad53e2f9d9a6b3cdfce974e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:22:59 +0000

    arrive unfortunate clear broken regular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
