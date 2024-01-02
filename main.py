18824a094e1a03fdd19fd1e9ef19ff61a0e167c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:18:31 +0000

    act up approve usually really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
