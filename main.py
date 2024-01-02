643491e9b2793b9529b638697b74f4b564af2d73
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:41:49 +0000

    load public accept surface goon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_as_list(array):
+        return list(array)
