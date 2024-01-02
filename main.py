55c431361ad058a50b257459e6be78e53ae1ed72
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:34:19 +0000

    recently task after guess still

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def multiply_numbers(x, y):
+        return x * y
