5b3948dcb58eb0811f6b5d24969eff56e75c7263
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:25:22 +0000

    build conduct out tough tongue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
