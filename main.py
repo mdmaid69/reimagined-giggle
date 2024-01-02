4c5f8f14559f592d7f0590cfb22050e5aa93f665
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:55:10 +0000

    behave want public dear suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
