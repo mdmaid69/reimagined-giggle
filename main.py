6723ff9aeff2594305134ae4228371d08c93cdd3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:07:24 +0000

    remove coat bringback action shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
