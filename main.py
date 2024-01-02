b87ab0942b2a2a249aea9d1432615d82a760b91a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:29:51 +0000

    eye least pleasant divide speed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import getpass
+def get_username():
+        return getpass.getuser()
