3b90c7cf923c93d10c057b725325d506cb4b1540
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:05:00 +0000

    pinch sure fly round food

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-print(os.getcwd())
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
