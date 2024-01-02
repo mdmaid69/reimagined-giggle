9057203ab22eba1f02e2022c8ccd2a07579b8aad
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:35:05 +0000

    attempt scream wealth effort difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import random
-print(random.randint(0, 100))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
