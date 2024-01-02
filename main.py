6a0d6b2b192ae2bdf21a05c04a8483b4679af01c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:59:35 +0000

    pocket alone dead mind convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
