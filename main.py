68451088fd7811356661b97376c64682eb12c367
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:26:43 +0000

    another total bored getaway tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
