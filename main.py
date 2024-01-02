889792b656e9377dee8a605dcc3cb12a59620585
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:45:32 +0000

    pleased damage thinkover injury software

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import sys
-def exit_program():
-        sys.exit()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
