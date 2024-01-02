a2dbecbb1429100d819fbc9db575038af611cb21
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:14:13 +0000

    autumn brain little across getback

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_counter():
-        return collections.Counter()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+print("Hello, world!")
