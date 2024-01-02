dbf98822fe3702617292ed6871a3581947657514
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:47:15 +0000

    practical shop repeat reach peaceful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def subtract_numbers(x, y):
+def subtract_numbers(x, y):
         return x - y
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
