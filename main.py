37447acc3d98f26efe106e1bd52f197d02c027a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:53:09 +0000

    wrap sit emphasis Oo getback

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
