4cb14e5d25402865e02680fa25bcf3cf5cb482c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:15:27 +0000

    place whenever mother false food

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
