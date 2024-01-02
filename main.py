b683e505462e657c21bc64eeca375c5fe193ea46
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:55:03 +0000

    shoot Nn plastic forward refer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
 text = "Hello, world!"
-print("Words:", len(text.split()))
+print("Uppercase:", text.upper())
+import time
+print(time.time())
