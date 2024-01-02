45fda14c923f4a7c3232c57ac8a1d9ef96e54cbc
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:18:25 +0000

    later post performance bread wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
