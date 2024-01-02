9caf5bf61e0a280309f5263cdab1c2a27f3be2bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:24:54 +0000

    impatient early there calloff coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_repr(array):
+        return repr(array)
