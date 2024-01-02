5f626a56ddff1238c2fe112eee57a957443a0091
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:52:54 +0000

    holdout visit window exist bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
