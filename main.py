5f46c9db8d32f392bec3a643027160abc3bf2612
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:44:56 +0000

    off play 571enough mystery buy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
