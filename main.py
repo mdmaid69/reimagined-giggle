a2e171bb9bd98fbb42db7e0b20a0383d08901536
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:01:51 +0000

    nor through slow anybody home

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
