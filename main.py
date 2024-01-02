8a1d05e49edb04b5d6fe0280471fa57e032a3d32
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:41:46 +0000

    measure excuse cheap protect lean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
