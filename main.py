fbe0a03c8c6007d8783e077d0608d034902bc514
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:47:36 +0000

    under mad unfortunate behave emphasize

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def subtract_numbers(x, y):
+        return x - y
