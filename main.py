08178378f8c2b4442688cd802c09288b43256844
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:50:38 +0000

    city demand refuse colour mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
