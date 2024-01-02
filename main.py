24c84ff8dc5b8a5fbd49cda6c6a79d1b16b7e109
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:22:17 +0000

    record respect comefrom constantly smell

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(lst):
-        return min(lst)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
