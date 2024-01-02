d8eada130f3db721d56e099d3424f3ec648f9f06
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:34 +0000

    otherwise meet smooth accidental organize

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
