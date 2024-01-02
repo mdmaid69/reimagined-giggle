e44e64fa1d160107b41b4d6c4776ee3a0a81bff9
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:36:57 +0000

    view sad freeze bringforward bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+print(json.dumps({"name": "John", "age": 30}))
