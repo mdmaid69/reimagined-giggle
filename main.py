5abe37aaa1b70716b9055bc35a67551b4cf520af
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:47:17 +0000

    lower neat immediate ring share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import os
+print(os.getcwd())
