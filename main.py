6afcf2b69649ada919b12c75e6922755b61742f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:49:21 +0000

    used skirt boat worry middle

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
