d03ea74113f5b0db0ef3a2430015ae71dc3fdf79
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:56:14 +0000

    personal comment favour runaway 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
