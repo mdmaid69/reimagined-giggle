a573916b44390ced79be97742bc599bc186a6f98
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:40:54 +0000

    left increase quickly term interfere

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
