fe7fdc54f37bc1592cb843652b9cdc61809af48d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:17:19 +0000

    broad wall shock off hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
