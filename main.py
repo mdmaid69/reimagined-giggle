d14f5b4a43759774a58b84936050e125aa7983c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:40:27 +0000

    heavy loan runover detail indeed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
