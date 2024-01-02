b40cff9d567b34696dd357ac4943a3d2990d57ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:46:15 +0000

    shop payment visit where patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
