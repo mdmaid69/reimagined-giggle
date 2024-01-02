fe7e1199f77f1b6f1ac8a300e8b47d7b9cbd4b95
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:00:11 +0000

    sore quiet perhaps tired talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
