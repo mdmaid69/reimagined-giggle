4f2ff2802e4dd78633ff3eba8ffd26490b64d96b
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:52:47 +0000

    to accident should ear react

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+i = 0
+while i < 5:
+        print(i)
+        i += 1
