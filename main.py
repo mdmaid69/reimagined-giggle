a1191d0e6171cf319c8996aa2723b1839aabbd08
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:14 +0000

    wide capital region persuade replace

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ for i in range(5):
 
 while not q.empty():
         print(q.get())
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
