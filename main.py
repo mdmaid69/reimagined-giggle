df77fe5476e5e8c661d34c748c2f5ea0ecc95c4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:49:33 +0000

    recovery nice difficulty private forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def reverse_list(lst):
+        return lst[::-1]
