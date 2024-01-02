e0bcd802e3b5e331e7e34384bd3151e16bc873e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:18:08 +0000

    564encourage flood pause think estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
