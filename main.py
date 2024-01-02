3f11eb41eaf78dc4260c4b42822c766cf255ac28
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:09:47 +0000

    side single let confusing movement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
