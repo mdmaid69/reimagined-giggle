73f18a19b0c5b760c1093e3955f5c8ee7c711c38
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:33:46 +0000

    suggestion trick beat real previous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
