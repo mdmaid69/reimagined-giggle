b989b9892e2ac3a3299d9bdfa7a8ec7aef914252
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:53:32 +0000

    chance look delicate glass earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
