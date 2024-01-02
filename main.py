103dd2519e8a6db85a87d46d7a14143149ff8d84
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:43:44 +0000

    squash man air count foreign

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
