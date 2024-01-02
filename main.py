59668ece6703c97e7e13eb40666a7862e82d1601
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:37:01 +0000

    suppose dangerous head please alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
