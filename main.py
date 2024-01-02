195443965bf03273311976246eb9d9ebe1b9058e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:19:16 +0000

    wide especially giveaway scared murder

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
