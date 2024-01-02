29429f1180ae42fe3027a4f9200433788a8f36a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:26:49 +0000

    suggest grow can string fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
