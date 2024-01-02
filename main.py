6bee53108821245985656cf7181ba51961b8477e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:48:28 +0000

    protection fast black progress black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
