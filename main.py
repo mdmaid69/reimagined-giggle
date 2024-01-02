72818e97f6a2605fb2f66460a8ad9de4b53c3871
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:59:24 +0000

    shout threaten one tense street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
