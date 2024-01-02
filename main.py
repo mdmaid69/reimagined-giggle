737e87ea20d92eeb2118f9a75e09899381242cc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:29:43 +0000

    spoil return Ee there thick

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
