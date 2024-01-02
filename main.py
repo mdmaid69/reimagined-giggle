c4d8b515df3c730571e8a78d37d7c864569f7d66
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:21 +0000

    lonely happen little your takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
