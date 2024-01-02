145aeca9c39ff9aba96b30fb57500a80348b28a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:41:46 +0000

    smile nurse regional without image

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
