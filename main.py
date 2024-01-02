b0ddb791ed2476a4e268836b7beaa1263c15dacf
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:43:34 +0000

    happen obvious fasten machine mistake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
