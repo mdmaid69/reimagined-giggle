78c58a1a1628ac65748da1548a89cfe4c07ad64f
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:27:41 +0000

    combination gate stir face disappointed

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
