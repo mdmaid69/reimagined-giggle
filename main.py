b3d67db5aa94a1d81a86d9d4d222b36e30259517
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:24:01 +0000

    equal result sick firm workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_tangent(x):
+        return math.tan(x)
