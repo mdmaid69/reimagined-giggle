9dec1a665e6f2c18753d540836a0c49f2fc94b73
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:37:39 +0000

    various our deep still offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
