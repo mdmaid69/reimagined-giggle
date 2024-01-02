3f4540d0a654b1ae55155b3b23248125a5eb71ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:38:45 +0000

    deserve silent we influence sensible

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
