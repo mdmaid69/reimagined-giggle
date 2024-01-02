da085eda631f2b104755a94f4453e9362c8a50d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:20:57 +0000

    back clean generally opinion downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_min(lst):
-        return min(lst)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
