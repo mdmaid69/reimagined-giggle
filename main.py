184e27dff286d61839b44ec95070c9c06d2495c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:10:43 +0000

    none brain passenger regular bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def clear_array(array):
+        array *= 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
