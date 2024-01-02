6a3434574caab5d9009ee05beadea15061fcb200
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:53:03 +0000

    scene run whisper previous last

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import os
-print(os.getcwd())
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
