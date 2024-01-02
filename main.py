23e16f8b87f9910753094d8ac026dc9e24d59437
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:58:53 +0000

    neat chance best woman pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_binary(n):
+        return bin(n)
