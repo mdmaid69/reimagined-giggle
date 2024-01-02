f0f5dbda710cc713390a6a97114ab9ad7144b68d
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:04:21 +0000

    upset about unless square store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
+def get_bytes_from_array(array):
         return array.tobytes()
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
