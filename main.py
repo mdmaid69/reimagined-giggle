c0af2f16aa8ee7ad1327507b8a7cdc45599b5d91
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:07:04 +0000

    noisy remove word explanation start

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
