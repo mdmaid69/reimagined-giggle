f7d750322513b9a253fa698fa6f3382b350b9f31
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:34:53 +0000

    neither somewhere glad lean private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
