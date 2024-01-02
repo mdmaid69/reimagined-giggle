9ec5f387c2db2afba81bbcaa4e9f5c214739b0ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:03:51 +0000

    own mess comeround warning importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_hex(n):
+        return hex(n)
