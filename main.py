bd31da7271e7d6188e7b4f9c6971a77eb00c2e06
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:03 +0000

    opponent single realize discover admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def exit_program():
+        sys.exit()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
