0478c6d4f117b6c077874f914eab0c8701ced975
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:26:44 +0000

    waste now charge keepup keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
