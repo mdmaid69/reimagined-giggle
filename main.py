9fe09ecd6467b1c5f6b9b76c49f268e24f7e2a6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:36:02 +0000

    already mine identify mass guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
