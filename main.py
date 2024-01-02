89daa4f4f7bed95022e28612b4688f7f7b85d53a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:07:36 +0000

    climb used wild no state

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import os
+print(os.getcwd())
