c4c0f0e43eea15299bdfaefa88abdef3e5e53388
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:53:57 +0000

    straight press dig reach circle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import heapq
 def merge_sorted_iterables(*iterables):
         return heapq.merge(*iterables)
+import time
+print(time.time())
