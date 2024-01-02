ecde2b14219be03aa8fa920a6358246489e5b0c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:42:33 +0000

    sort road lesson whistle but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
