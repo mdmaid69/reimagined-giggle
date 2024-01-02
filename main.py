a25a3c9e0bde6b993c4b792ccba0eec44dedbc51
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:28:16 +0000

    radio dream bag belong call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
