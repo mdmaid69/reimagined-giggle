64239b0b1ff8cf618026f6c7fb1ed39a4fb721da
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:31:52 +0000

    beside behaviour future same today

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_speed(distance, time):
+        return distance / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
