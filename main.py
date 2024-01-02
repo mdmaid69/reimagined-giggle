fbee138febc4ffd76c1a9a5022c2e9fefa066071
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:11:31 +0000

    treat lean father bet mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_list():
+        return collections.UserList()
