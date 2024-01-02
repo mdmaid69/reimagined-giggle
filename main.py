147dc6bfe13892501579faf99661003dcab96c8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:40:56 +0000

    goout otherwise answer waste small

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
