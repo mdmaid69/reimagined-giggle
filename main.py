c7b2a7cdfc21c7715984837e564190f40d4bfe07
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:51:06 +0000

    excuse deliberate create declare workupto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def sort_list(lst):
+        return sorted(lst)
