d4271b06a32380f2e9dd22c037253ad56d661620
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:16:56 +0000

    our quiet cause ask related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
