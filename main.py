42ac7096151809f344e35b0301416c7e91800b45
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:59:51 +0000

    often stretch Ll fail tidy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
