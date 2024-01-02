b2c460f7c2a428c2b1e5501c0f404fc9c554fd04
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:58:03 +0000

    organize congratulations stupid tax extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
