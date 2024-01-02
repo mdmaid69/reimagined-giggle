c1e5cde79635a08bf994dc373aaa44367aa38e02
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:49:16 +0000

    makeup image shock original calmly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
