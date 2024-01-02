2638f705f0b2c0c07295369360d66cc6fb445f6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:51:00 +0000

    afford emergency exit mess image

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-name = "Python"
-print("Hello,", name)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_density(mass, volume):
+        return mass / volume
