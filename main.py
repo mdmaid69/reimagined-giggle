ce3902ef2dc4fb6b78ec816bb01f7d70f10f9be0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:07:08 +0000

    Cc awake shame weak dry

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-print(sum(range(10)))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
