4c3d4b9ab050e798d1dca90dbf9b7bc0aa6f2528
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:59:27 +0000

    laugh interesting money plenty taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
