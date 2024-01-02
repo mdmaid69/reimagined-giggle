5348bbccff82cf2ea14f387d98b99b918e26e0c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:00:26 +0000

    depend hide my spread confuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def greet(name):
-        print(f"Hello, {name}!")
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
