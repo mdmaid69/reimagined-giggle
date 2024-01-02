795e30bc09198727521bdbcaf2886719ee72dd37
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:10:53 +0000

    afternoon description attention effect consist

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def sort_numbers(numbers):
+        return sorted(numbers)
