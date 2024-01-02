2ba865e69df0fd816d8e66831940d4fe2e7f937d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:22:23 +0000

    woman skill factory travel cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+for i in range(5):
+        print(i)
