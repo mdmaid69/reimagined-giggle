4058dadd70047eb0710004e7eb4f94b55d3f3c22
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:05:17 +0000

    star voice call beat priority

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
