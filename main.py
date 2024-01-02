77d020f4d77b54e4023fecfa15a19bd9c345e646
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:45:24 +0000

    character pollution travel between rate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
