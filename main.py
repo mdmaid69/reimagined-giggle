ee12683b7d00f1baa2ff3caf9eac7ea82a287d50
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:23:24 +0000

    criticism couple at equal note

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def multiply_numbers(x, y):
+        return x * y
