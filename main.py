9b962613dcb482c9865d6716c7d4862568580b5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:02 +0000

    dick later label happy piece

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
