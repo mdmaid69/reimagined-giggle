ef7d158b2c3aaaa3227f9410709ba679f804349b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:18:38 +0000

    loss sign business apologizeapologise drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
