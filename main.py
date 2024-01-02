bd7f4e2489d83ec55593b75e6f5b6773df57305e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:59:18 +0000

    while about usual ourselves comfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
