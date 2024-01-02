6717bc0c36624ee8153bd32ae4d7813028480772
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:13:53 +0000

    ring up low cheque wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
