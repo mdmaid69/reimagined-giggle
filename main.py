625429c98a67a30a9271a350a8bd200683661ee1
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:47:52 +0000

    official show flat chance stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+name = "Python"
+print("Hello,", name)
