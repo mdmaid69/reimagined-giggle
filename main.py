1e16fe1ce5d9985e80473db3ced9bb8249faefc1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:52:03 +0000

    lunch concerning spill gap fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
