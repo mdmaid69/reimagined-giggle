4d921ccd416f7b8bbf4112247f6787f553785936
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:02:39 +0000

    weekend through fill curious lookup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
