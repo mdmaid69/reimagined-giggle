f165940012c9aaf78762e8e5ec8aeedb800a9b42
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:27:13 +0000

    formal clothes direct hour once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
