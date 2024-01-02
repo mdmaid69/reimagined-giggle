1a253b28c0d18e22b1437c0fedc21581af8d882f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:18:34 +0000

    notice regularly reach little mine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
