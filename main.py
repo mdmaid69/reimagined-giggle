f50cc2f7f5b3b99840ac4f0c91d8a10e0325f10c
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:36:56 +0000

    late noone crisis several determined

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
