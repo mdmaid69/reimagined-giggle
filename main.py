a74507aaec03d71a7461f2da59cedff2cca8f41f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:50:46 +0000

    exciting roll workon regret cover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def convert_to_binary(n):
+        return bin(n)
