88ae0dfacddcb36b1e446110b05f1d86347d802c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:42:34 +0000

    otherwise vanish blow together although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
