e63aed8b806ea18f330118558eb8ea1cd827eca0
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:56:30 +0000

    dry friend store reply odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
