38eccfecf5bba52a773be7d5ed7b3e6c913b309a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:48:28 +0000

    great report friend ready skill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
