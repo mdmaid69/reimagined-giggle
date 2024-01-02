8f9c0c1e9be4397ed9ed61cef2945b094547a4e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:53:01 +0000

    floor talkoutof such wish name

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
