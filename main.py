bd88868e61a1587f70fbc4b233ed4576fbb6ba71
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:59:51 +0000

    sing stretch much tonight circle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
