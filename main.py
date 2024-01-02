a37b0272b84e07c7419cdca491715311aed3cfaa
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:44:07 +0000

    hello earn woman thank behaviour

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def calculate_speed(distance, time):
-        return distance / time
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
