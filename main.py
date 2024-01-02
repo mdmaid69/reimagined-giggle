4ae4bcae42f9d3f89b04a325e28cd8285d4268c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:04:28 +0000

    stamp what heart trust Kk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+print(sum(range(10)))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
