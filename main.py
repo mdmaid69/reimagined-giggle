e1abfabbdfa231fb46fff81fd91d1f5a456824d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:14:27 +0000

    meaning pure slight strike joke

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
