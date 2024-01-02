708beed1fc6f251869cbbd2a504460d63d20df20
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:10:08 +0000

    various kill rain note under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
