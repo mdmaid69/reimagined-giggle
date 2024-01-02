8dc47d63831afe2bead95410f9809d70494d6afb
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:26:38 +0000

    behave spot man hand view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
