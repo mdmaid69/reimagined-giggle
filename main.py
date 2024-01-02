4c2d9ff61926e585ab4f5de6baf06a867fa9c995
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:59:33 +0000

    profit tone sky 564encourage slow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
