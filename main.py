41948129e6b2dad2a8afb92145b38705bfd3e57f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:37:35 +0000

    Ff word formal Kk quit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+print([x**2 for x in range(10)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
