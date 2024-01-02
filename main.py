9a26e6726ad864c926d0324304f7297c74ad164c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:48:35 +0000

    excellent suddenly broad destroy pretend

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def convert_to_octal(n):
+        return oct(n)
+import sys
+def print_python_version():
+        return sys.version
