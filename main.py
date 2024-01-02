5a4a5ec464d51210d85086a19c5cac9531c75da6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:29:00 +0000

    error scrap suggestion cause foot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
