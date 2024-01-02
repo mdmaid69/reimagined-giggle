f86cc17ca5f34fc6f3c5de1fd7dde22e99ae14cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:53 +0000

    better space rank around even

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_file(file_name):
+        os.remove(file_name)
