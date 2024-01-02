aaf83125b85db52270e0284b6e7ae03d8f37cc17
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:55:27 +0000

    false computer ahead disappointed lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
