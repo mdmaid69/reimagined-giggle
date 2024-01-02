e9e900f217ca929c66c81018819e939d8d1472fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:38:35 +0000

    service dull heat talk take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
