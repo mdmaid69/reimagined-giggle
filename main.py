3f0568257d409aaa77298cd6dd3bd849acc8d60f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:48:14 +0000

    woman Ll circumstance safe to

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
