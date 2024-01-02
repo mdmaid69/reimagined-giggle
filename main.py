7b3c7066db4d958618ca16e9006790f8e67e2a01
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:20:16 +0000

    violent dry shout entrance refer

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
