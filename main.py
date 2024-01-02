95b2902d1f155f0ba50cb2ee0b1525f7a2438016
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:27:01 +0000

    situation while amount recover count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
