b49e07a93ac8dca23e0906b45490cc2bb7df4133
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:23:03 +0000

    present interrupt possibly famous not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
