b0513c8061b3ffeb9c3008b2f0ad87deac7b5f61
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:13:47 +0000

    kill middle rare jump Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
