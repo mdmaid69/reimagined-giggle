410df45faaefe6eb5ec9c625db5cf45deb1d9a77
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:35:57 +0000

    imagine organize Ww getup rubbish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
