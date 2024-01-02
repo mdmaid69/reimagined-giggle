902d9feb41aee824e8c29ffa085a30f43eca801e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:14:31 +0000

    detail difficult produce task holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def get_array_as_complex(array):
+        return complex(array[0])
