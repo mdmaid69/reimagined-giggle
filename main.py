2ce2c17dfdb1d817e66e029200e602b8ede6ccbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:44:02 +0000

    instrument lot sale us sort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+print("Hello, world!")
