6d6b883afe75906aa25091ad057e5268d275fed4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:59:04 +0000

    loan live rent ocean else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import getpass
+def get_username():
+        return getpass.getuser()
