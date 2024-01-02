515933032b21893f92298d79500bbcf57ca0dcc3
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:08:52 +0000

    rather Rr broken tonight fail

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_density(mass, volume):
+        return mass / volume
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
