f38d4e84d356200e9bfccb5a69f87ceb3c13ae50
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:49:05 +0000

    will hope birthday course combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
