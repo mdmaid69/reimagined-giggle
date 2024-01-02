afee6fc5aa2aaf7dafa6f8d248fe5479254b357e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:09:45 +0000

    environment bad her unable damage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
