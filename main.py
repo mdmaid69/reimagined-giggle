f310c5863d172351fdeb76438041e2c7644c9e4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:16:45 +0000

    instrument bread mark shall full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
