dddadfbfde5cef548c4d5313313a0bba949567d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:33:35 +0000

    window dry respect loss minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
