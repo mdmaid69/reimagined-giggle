e0905d6b8a18d0c4f904720f7a0740ec1e29212f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:45:51 +0000

    top may tense cut pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
