d57913d3549e63cf81d0e1517b20bb2a8ffc43a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:31:50 +0000

    everybody great excited complicated mainly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  def square_number(x):
+        return x**2
