082aa611e38a1db0bb0cc2d7038856c8409f8f56
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:35:32 +0000

    ready direction mend pretty bank

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
