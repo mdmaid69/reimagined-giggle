0cb8e1883f7e161825e25ed59c01cc8747e560ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:59:11 +0000

    suit cheat division boil hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_max(lst):
+        return max(lst)
