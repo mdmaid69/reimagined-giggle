7c1c87e802f4f181ba48274974296f1d27aca5ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:31:39 +0000

    scene day flight foreign sad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
