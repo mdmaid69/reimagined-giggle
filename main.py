dcec82bb367e0d6b9609e07d28672dfa9d62ec19
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:45:19 +0000

    common follow sort run deal

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-for i in range(5):
-        print(i)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
