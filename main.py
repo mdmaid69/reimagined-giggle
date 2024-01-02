c727b1475c993ae647c2a2adcb8e0b1921a4bf8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:45:11 +0000

    usually lock past confuse talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_power(work, time):
-        return work / time
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
