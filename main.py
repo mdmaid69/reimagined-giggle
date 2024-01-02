470496d5da67112e94e7f5d6ab9ca33ae13d268a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:04:35 +0000

    top major breakthrough prepare result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
