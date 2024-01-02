cc554c15ed1c404f2d4ea4280c004631443875e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:32:10 +0000

    turn underneath appearance sting needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_volume(length, width, height):
+        return length * width * height
