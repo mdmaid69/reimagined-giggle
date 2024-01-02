3cefc240a1a33a47b5e812a670beee7202ae3df7
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:22:29 +0000

    club quickly rescue display circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
