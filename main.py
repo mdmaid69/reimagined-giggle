ed73d9cafda1b14731962f2eff75e9fae1e0e9aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:28:52 +0000

    plenty steady steal away vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_octal(n):
+        return oct(n)
