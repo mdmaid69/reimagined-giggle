859eaca6a0a588307f61b0bf598be429b4f040ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:18:39 +0000

    environment Yy past try seat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
