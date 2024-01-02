0512f394f3d6826c0f3c605b54e993e5d684c69e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:22:46 +0000

    transport share month near luck

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
