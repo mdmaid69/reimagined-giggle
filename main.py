53eef5b0db2813359da1a8fc49c94091e4656db9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:43:08 +0000

    thing steam give fasten power

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
