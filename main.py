972919acd0ce8d505923a230dea8d12d969d0a83
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:06:43 +0000

    remain notice by case point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
