f9407fd0bbdd8f23331496e64d7d874d8033213d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:59:53 +0000

    care stop 570enjoyable first instant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
