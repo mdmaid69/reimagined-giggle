e28406c7dbc4c90b45e423cd652196f258834803
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:57:36 +0000

    breakdown run share usually whistle

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
