923d2142470e03f60cc78b4261d08a70d2b70351
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:52:04 +0000

    fact go always any platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
