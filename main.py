5e3b6fb5ade4aaedf786518e2f7b14027baa7e56
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:53:50 +0000

    bringforward talk platform variety birth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
