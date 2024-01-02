0ba92d315b6eae5d40f542834fc8e7e4d197c42e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:30:30 +0000

    knowledge breakout die getout exercise

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
