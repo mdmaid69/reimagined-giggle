e7bfabbd7b476b7edb0916b4da3292104379a9f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:38:56 +0000

    half accept mysterious someone party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
