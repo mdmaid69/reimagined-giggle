aaa57b05dac8eea0351f40f2398d753539fafd07
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:10:35 +0000

    seat party place pile obtain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import random
-def roll_die():
-        return random.randint(1, 6)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import datetime
+print(datetime.datetime.now())
