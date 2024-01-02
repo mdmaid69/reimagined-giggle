57ee3a5736c17cd1343db4ba57eb5a0cb3876cf5
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:20:36 +0000

    later trip lunch central point

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def clear_array(array):
+        array *= 0
