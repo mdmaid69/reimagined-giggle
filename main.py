d1f58290b1bfb3f6057258adc9f870b0a0b037a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:10 +0000

    war meanwhile show your criticism

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
+  def calculate_area_triangle(b, h):
         return 0.5 * b * h
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
