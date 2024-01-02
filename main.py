bbae1e8b756d76873c0cd1518e2bf49eb5987f69
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:23:36 +0000

    place number cap your rent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
