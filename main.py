b35811d9a534e65edd5c307094e94a030868d8fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:42:55 +0000

    zero task front stranger ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def multiply_numbers(x, y):
+        return x * y
