344a903a0ddbf297b893ef0c1538e9cadcba04b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:33:47 +0000

    grand after towards past wall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
