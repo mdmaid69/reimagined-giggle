11110bf150ee2e8f3206e69a242c981cdce1643f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:45:19 +0000

    foreign uponon whistle firm rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
