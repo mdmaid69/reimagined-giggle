067d8208a809b4dfd69610b5400a04869e183f29
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:51:38 +0000

    calloff insist there lean aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
