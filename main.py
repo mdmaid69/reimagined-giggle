c9acfb3759a43c6898d9d3be6ad86bc611b16e9a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:12:52 +0000

    love holdout takeback tip connected

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
