3a1a5e558c982a89f81a55602058e7e60a4fb54c
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:17:37 +0000

    big peaceful chase operation consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
