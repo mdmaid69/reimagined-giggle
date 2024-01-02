1f768123feeeb1fdc630ded5199950a56a219d3c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:22:52 +0000

    weak transport public round compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def multiply_numbers(x, y):
+        return x * y
