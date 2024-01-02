8a6e05a115f416196040094fcda374fef9ede655
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:30:34 +0000

    watch glass and tool pressure

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+for i in range(10): print(i)
