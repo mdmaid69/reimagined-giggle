98feed85b6a83a6a138f8b57752e6517d94dc8d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:34:27 +0000

    majority advise foreign tear fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_speed(distance, time):
+        return distance / time
