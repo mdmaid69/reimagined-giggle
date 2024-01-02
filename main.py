db5e3ca731bb7089b876759fdd5ce24075ad3feb
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:18:37 +0000

    square false offence dance than

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
