2b27778da18a143876e9b024cfbd22c2ead1bca2
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:46:44 +0000

    yesterday guard development convenient real

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
