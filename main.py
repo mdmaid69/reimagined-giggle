4138d4949aa940bf393cf23204ef254165cae1fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:46:45 +0000

    with dry influence rather wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
