bc63563db62f4b29d73a07dd235d3aeb9178a76e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:28:19 +0000

    your second thank human wipe

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
