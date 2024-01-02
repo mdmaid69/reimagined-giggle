669894091e61227e982675613856d41d47dfac06
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:27:12 +0000

    base hour admire bin check

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import os
+print(os.getcwd())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
