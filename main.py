646ee8e808f2611a660fb65f5681451246c67f2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:52:33 +0000

    limit beautiful Ee unfair corner

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print(sum(range(10)))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
