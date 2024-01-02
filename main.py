20cae8a0cac787e30fb87d75e00563e59696b2ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:56:23 +0000

    light just turninto tone pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
