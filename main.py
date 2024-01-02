3db3faddf11ffa3a5d8e50c776c067ae72e76321
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:16:45 +0000

    admire everything fix delight see

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import collections
-def create_queue():
+def create_priority_queue():
         return collections.deque()
