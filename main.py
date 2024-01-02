89a7a0cd26846258a74547a28f2b9aa85fa0580a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:51 +0000

    sentence receive upwards whichever you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def is_odd(n):
-        return n % 2 != 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
