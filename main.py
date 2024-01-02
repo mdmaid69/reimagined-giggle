dd7e6141db57cde89ad063ca8b1212bd0e8d6b23
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:31:54 +0000

    shine appearance age nonsense can

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import re
 def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
