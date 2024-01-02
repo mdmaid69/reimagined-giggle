edc42c28bc4c4bb41c28827f9a9ae46a661b989d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:46 +0000

    tool pattern please field remove

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
