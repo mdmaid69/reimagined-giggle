3b8ebaa7a271b211fbcaab949672feca96abbc3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:10:51 +0000

    concentration though could pleasant imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
