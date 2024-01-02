6ca256845b8735379191d7b9f59354e2cfab852c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:45:33 +0000

    suck respect music estimate commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
