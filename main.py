b910701dd871bf227062403bb4765377e73020f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:40:07 +0000

    brave hardly breakin fold set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
