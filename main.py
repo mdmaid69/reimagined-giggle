437b3239ebe80a4eec0d33ec8edb45d653cf2fed
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:30:35 +0000

    sell sure just loan burn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
