f0d8792e1897f911a3a99808aeef5d997b9d259c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:50 +0000

    flood regret head close sign

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
