817502e61e487209153ff0e1c64cc3f50c3260a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:58:26 +0000

    join whom hurry allow degree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import random
+print(random.randint(0, 100))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
