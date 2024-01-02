f340dd72f0106994d2887cc8a57e0bb054f0db7f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:16:31 +0000

    reach when walk row light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
