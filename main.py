950215329bb6b709e579db3351e521fc06b2f353
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:16:18 +0000

    one rid land broad serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
