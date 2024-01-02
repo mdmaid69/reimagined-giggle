cfa197a661b679bf737f17813dd89243edbf14e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:10:29 +0000

    breakin ring room whether father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
