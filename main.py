ac53a879facc6a0ad69516ab792ef29410cce720
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:34:25 +0000

    apart spring loss lift hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
