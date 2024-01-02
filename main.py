e07b07977836cd31efe267bab8e7c80890b4e995
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:32:47 +0000

    roof middle claim greeting board

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
