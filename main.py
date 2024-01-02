7fecde1cc5fde28b3ac5cbe471ecde6151fd0b4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:14:23 +0000

    law dare 570enjoyable practice pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
