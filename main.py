427d21133f8aa8f3f290775fdf5bdc8b5da3e81c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:28:54 +0000

    arrange previous apart effort yesterday

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+def roll_die():
+        return random.randint(1, 6)
