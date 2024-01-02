0aead40cdcab3336bd9adedc3cd404cd6567ee04
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:41:42 +0000

    emotion basically attention pollution declare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
