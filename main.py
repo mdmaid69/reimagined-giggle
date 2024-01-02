f5da26e7284354714e64ca5a897b53a902bd7a1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:04:19 +0000

    179bite brother judge announce uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
