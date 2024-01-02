2262f09598b91807871c499ad56d2d9390351629
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:36:47 +0000

    tell spring unusual lookafter shallow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def count_characters(sentence):
+        return len(sentence)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
