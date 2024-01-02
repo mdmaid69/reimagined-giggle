8c73e4bb2e89e0b13dc6a028dd911a5c7f5ee6e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:57:58 +0000

    interested breakthrough please leave hear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
