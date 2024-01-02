be8153dfb1cad24a0ac3a96f62ba4e479bf11a06
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:36:03 +0000

    bang confused official heat eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
