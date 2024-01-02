cc69d2790e6b727db1f2945b8e31d05866798028
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:58:41 +0000

    perform relationship cost include takeaway

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
