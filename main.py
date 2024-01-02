b87c7a0e7b590d89e80b5996d3e8b45c2b0c64b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:35:02 +0000

    related sing Cc meeting bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
