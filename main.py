ad63d2dae829157ecbb1de15da81ccfa534b7481
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:07:31 +0000

    card tap off platform mind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_power(work, time):
-        return work / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
