66bcd4bd7266d1ac661d858483e7093d30f000de
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:17:50 +0000

    Gg appear 569enjoy married pause

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def find_unique_words(sentence):
+        return set(sentence.split())
