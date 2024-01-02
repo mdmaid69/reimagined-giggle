27c6052274601f4e2a513f0d8e2806b3870afb86
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:18:26 +0000

    which road hold pattern use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
