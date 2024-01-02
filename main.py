94fedfa8ee2aee51d8647b24f135cad54e74cea1
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:14 +0000

    guess simple light cheek expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def cube_number(x):
+        return x**3
