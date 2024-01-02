44d43be80402f96abfeb2d32d9b02d976d2ed979
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:25:34 +0000

    leave scream wealth any sleep

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
