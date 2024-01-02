53ee383176cd490ae842632fdc82ae2bb130958d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:55:36 +0000

    prize direct juice remain sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
