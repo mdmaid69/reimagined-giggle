ae652d781a3037be3725e0b06955a749e7ffff2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:40:32 +0000

    offer road boring yet cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
