0e8fae3749fc68ab4b3a01d5070c2dd3675e0469
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:31:34 +0000

    friend go sort male store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_bool(array):
+        return bool(array)
