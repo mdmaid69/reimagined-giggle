2a476d32a8f7566866f25697bd5369126e6a4a8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:12:45 +0000

    score admire stick left attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def remove_directory(path):
+        os.rmdir(path)
