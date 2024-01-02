39ce5c9cfa55b1614b59d0cc2e429b98622b52b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:29:04 +0000

    cheque turnback frequent repeat trip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def multiply_numbers(x, y):
+        return x * y
