074060eb01a48f119b3911947e1741c1d0507241
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:16:02 +0000

    consist factory swim stay emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
