4f5ecf68a71c9a4daa163bf9f2f52d5c68fa21b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:01:49 +0000

    bother stare rest seat ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
