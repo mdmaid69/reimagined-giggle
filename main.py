b7fe062bc29d8ece1f93ab59193d6b6c776005c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:47 +0000

    apparently spring relief experience until

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def convert_to_binary(n):
+        return bin(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
