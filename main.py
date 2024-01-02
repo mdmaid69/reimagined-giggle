d2f068c34409bbbed5ea8675f431d121c72987ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:14:02 +0000

    habit whistle urgent trap sky

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+from collections import Counter
+print(Counter("hello world"))
