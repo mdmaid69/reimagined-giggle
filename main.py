0f3b45af7388cb8015a5ca425c4c4ea81ab9fa0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:18:32 +0000

    back disgusting or exhibition rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
