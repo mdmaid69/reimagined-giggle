09b017ae398e42e3af0ee7e9c8c9f49f4b25cf99
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:07:16 +0000

    happy brief remain mean Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import getpass
+def get_username():
+        return getpass.getuser()
