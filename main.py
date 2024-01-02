219edfb56eaaa2a8952b4c6fc39711ecc3be64e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:34:53 +0000

    wave meeting interview load crash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
