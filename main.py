42aa7d86dc386a2ea4764a53d6e1124eb8dea881
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:44:33 +0000

    along few Mm show sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-print([x**2 for x in range(10)])
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
