422c818c1f5f1604758157385a90d7580d574ab6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:16:05 +0000

    table bus cough brother squeeze

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
