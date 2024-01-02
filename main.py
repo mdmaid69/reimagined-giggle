3430b945e288d5be6f5bf68dabec1a4f3c3eb9d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:42:00 +0000

    gentle honest shopping no shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
