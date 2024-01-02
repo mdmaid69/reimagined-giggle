2338fa5627fa868b57188af9db07058d040ea220
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:18:48 +0000

    dance crisis estimate board fine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
