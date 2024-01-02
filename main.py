981faf553a66d1b18e6acad0429e29ee1d8b7e0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:43:08 +0000

    storm pronunciation objection store say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
