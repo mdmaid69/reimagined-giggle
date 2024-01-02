e1420c784905ded094854cbaa3a83fa46c8ca1de
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:38:38 +0000

    leave convinced plan trade total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def log_message(message):
+        logging.info(message)
