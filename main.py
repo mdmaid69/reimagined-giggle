7573ada64116ff8031289bd6b325cb586126e335
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:26:00 +0000

    under suggest also listen value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import collections
-def create_user_list():
-        return collections.UserList()
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
