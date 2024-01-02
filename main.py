006919ec2fb41b4804eda537034e0fded27c64ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:53:34 +0000

    instrument accept formal except rush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_acceleration(speed, time):
+        return speed / time
