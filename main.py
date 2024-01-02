5aebbc0c4647dd65b8c1cb14512e352541243432
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:15:23 +0000

    deep the flat later hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
