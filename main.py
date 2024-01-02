0c3b80091e3e8ee64e4a4850efe46d8dd0240827
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:38:39 +0000

    foot keep better public abroad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
