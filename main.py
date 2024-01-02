fa259362d1afab26925d60ca4a470f9782729a0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:02:11 +0000

    turninto pass sticky approval nice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
