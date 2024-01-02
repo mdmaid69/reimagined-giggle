6c9034b516aabac014431c445aa362b036e5ad06
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:33:10 +0000

    bang like all teach change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
