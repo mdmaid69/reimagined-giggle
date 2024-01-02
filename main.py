d7890f73830e8afba6da51200e431762ff9b8c56
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:24:45 +0000

    phone name deserve table skin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_unique_words(sentence):
+        return set(sentence.split())
