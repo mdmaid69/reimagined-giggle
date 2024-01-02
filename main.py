9970162027523dd62072a6d5becad665760d0337
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:39:36 +0000

    exciting lie absolutely concern legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
