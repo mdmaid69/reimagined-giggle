fed5a60a511f8aafd296cfea5ec68f40c067b0cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:15:07 +0000

    rescue room Ee eager tall

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
