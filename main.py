8ad8475109eb95f1a54c1615128f6f3920bd2d74
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:51:19 +0000

    sentence soon brave nurse they

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
