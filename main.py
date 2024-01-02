ef21cf98903ec90c5cdf346b317a6ffa9c41224a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:02:27 +0000

    frightened runout everywhere convinced quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
