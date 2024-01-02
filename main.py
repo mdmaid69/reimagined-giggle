3b5376e50c1f1cee7e8fd3dc797b4185bc040ed2
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:35:21 +0000

    steal lunch pride concern city

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
