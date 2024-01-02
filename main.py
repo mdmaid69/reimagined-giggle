416645995ca01fb863612a8fff82a97a68d5296d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:24:02 +0000

    part cheap relation eye finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
