15ee79a77955531136478d40357e72a4d8749973
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:44:04 +0000

    produce bridge whether nerve behind

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
