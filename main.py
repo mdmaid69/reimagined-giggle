b578fd12d72b2bef5eed135b82f4e55acc0241fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:44:23 +0000

    off free allow visit holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
