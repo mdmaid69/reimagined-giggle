d044a01eace2e791354ed530e857119c6891365b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:11:32 +0000

    comeup strong live closed go

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
