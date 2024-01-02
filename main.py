009c52024f84d9bef0a33165f8091ca9fe74fb76
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:24:38 +0000

    busy rest intend check priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_max(lst):
+        return max(lst)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
