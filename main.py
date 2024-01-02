8adbc5f09f8ac105aec7a364478696adee9b0cdc
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:39:24 +0000

    charge turnoff door push violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
+def convert_to_octal(n):
+        return oct(n)
