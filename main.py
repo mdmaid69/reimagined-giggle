e2b521c91270f5c4311070f406dc9c50a1fc8b47
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:54:02 +0000

    demand drawing break bang development

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-for i in range(5):
-        print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
