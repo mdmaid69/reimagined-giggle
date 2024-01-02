2d9cbbc82cf7f1f95feff5202b4683b6c8731e3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:18:34 +0000

    whistle interrupt old occasional touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import platform
+def get_os_info():
+        return platform.uname()
