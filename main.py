718fe178a460ae83762013d5314839f41df53a4a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:47:41 +0000

    uncomfortable coat another thing promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
