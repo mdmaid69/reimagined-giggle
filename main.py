f0f30045e4f31f179d7761163a7922b3ed948350
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:00:49 +0000

    debt among impossible check stiff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
