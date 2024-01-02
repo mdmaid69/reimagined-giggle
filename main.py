e42b65edab6182d460decd4c34f624eddac2d28a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:08:57 +0000

    responsibility respect cope development design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
