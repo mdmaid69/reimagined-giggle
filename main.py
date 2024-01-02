22920c55c56ebf919134ace6ff2791e9cd60b923
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:27:46 +0000

    neither nose feed burn foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
