cd540ab72027839921df8166ade16741fed126cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:23:14 +0000

    include treatment exchange wave wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def reverse_list(lst):
-        return lst[::-1]
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
