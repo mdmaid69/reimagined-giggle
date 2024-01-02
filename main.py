341132f75de9061859a0d67be81fc090b3b793e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:08:32 +0000

    offence short sell goup minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def sort_numbers(numbers):
-        return sorted(numbers)
+def get_bytes_from_array(array):
+        return array.tobytes()
