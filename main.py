4496ddee0b6fd36662325bdb69b98c329619ddd9
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:38:07 +0000

    close safe bet seriously violence

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
