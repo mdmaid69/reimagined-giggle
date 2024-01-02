3935cc74c521c04c2aa09b94445e4ee57f194f33
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:47:02 +0000

    either reliable manage intend expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_odd(n):
-        return n % 2 != 0
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
