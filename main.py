2592331f55504787219bbb256934a07993e473cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:57:09 +0000

    scale eager favour authority nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
