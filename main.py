d1a7bec27be77d7b1eac20494906e36488647121
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:08:39 +0000

    tip lucky band complicated last

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
