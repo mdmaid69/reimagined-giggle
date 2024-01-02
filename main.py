5860f6b5fc28bf1d5c28e1d2c4ab50b08384c422
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:28:25 +0000

    without opponent criticism slip include

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
