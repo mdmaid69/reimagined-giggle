d31f3ca77e1d3452c49baabf234e146239092724
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:44:49 +0000

    platform land explain trust professional

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import numpy as np
+print(np.array([1, 2, 3]))
