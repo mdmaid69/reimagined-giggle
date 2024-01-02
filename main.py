058e84dac33631d1f9119d27ceb98c7b30a507bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:58:17 +0000

    health major light bend practise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def multiply_numbers(x, y):
+        return x * y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
