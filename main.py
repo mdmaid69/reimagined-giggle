5ef0d70b6ef979d15a7e83fb70643184e5ef46de
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:13:05 +0000

    for care once callout culture

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import sys
+print(sys.version)
