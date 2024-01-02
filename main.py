7a65ddb8bf720b079845438dfb164f525246c1a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:05:45 +0000

    tooth tight cut discuss argue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
