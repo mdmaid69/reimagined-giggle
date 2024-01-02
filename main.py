adfe6407cc23eab6f083553006ae53b41d7aedef
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:02:37 +0000

    reserve lock jump ours replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
