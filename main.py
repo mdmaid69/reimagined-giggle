a373e1d58a36323af669be717444fba442b31010
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:35:07 +0000

    regret they account all store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
