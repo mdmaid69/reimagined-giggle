0dcb685e2af3aca91d4fa47c02ffcfc71c1e30f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:40:37 +0000

    admire turnback male Yy cheap

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
