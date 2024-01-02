7b830f91afac1838e91fe43fb8c3e3a79c93b8b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:36:21 +0000

    train some scale born government

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import platform
+def get_os_info():
+        return platform.uname()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
