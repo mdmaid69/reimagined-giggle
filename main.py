29ad52f2eb5e7a7fd15d3f9613d57c8ba9ad86fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:34:37 +0000

    pocket evening pump level swim

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def add_numbers(a, b):
+        return a + b
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
