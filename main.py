cec82daf9c8443b15d1e42926bf101dbff179a85
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:25:42 +0000

    mixture justice speech guess takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
