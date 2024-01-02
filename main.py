5681a1c83b9473cc1e92340ec0f1f7ead336a958
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:47:38 +0000

    mind hardly clear reliable sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
