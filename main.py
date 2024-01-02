b62195479670a56fc1f4b626c0c41b3736e9495a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:33:54 +0000

    identify I 567enemy band past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
