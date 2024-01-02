013ebb5fdc9fa0e4e055683920634fc29c79ce28
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:35:37 +0000

    trouble after frequent early part

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(lst):
-        return max(lst)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def reverse_list(lst):
+        return lst[::-1]
