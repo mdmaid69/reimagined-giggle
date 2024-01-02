83017a95ad0d5f0aee0920273767d9dc85d8329f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:37:13 +0000

    definite exit around shut somehow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
