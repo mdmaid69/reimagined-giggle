35ad929adcf8b8564754596ec82db98aeaac5ee7
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:30:25 +0000

    Ii during reserve system keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
