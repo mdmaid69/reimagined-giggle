1b0db5167825cd965152fc1a2d5bdfbad4a299e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:25:52 +0000

    bottom practise image have custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
