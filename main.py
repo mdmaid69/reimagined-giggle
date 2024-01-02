899f370d9d67da0812e44715c3f6027f5a8ccb5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:19:41 +0000

    real attitude emergency computer meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def count_characters(sentence):
+        return len(sentence)
