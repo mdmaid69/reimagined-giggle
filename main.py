25113c3a56ec5d686a77fecc53cd7b3179981758
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:33:19 +0000

    responsible sort Vv takeback Pp

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
