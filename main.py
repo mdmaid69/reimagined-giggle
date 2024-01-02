cb91496ac86ced9238649d8383e111ad0e53a498
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:00:48 +0000

    Tt rain original plan study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
