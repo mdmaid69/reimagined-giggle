9663c89b360cb5a6e9439d5028728fba14933f5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:27:34 +0000

    cross sink dry calloff now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
