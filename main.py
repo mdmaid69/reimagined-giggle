e5d9a30bc6bb0c7ab1af816b31f27b6977f197ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:28:19 +0000

    deserve arrive wrap foreign ours

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import sys
-print(sys.version)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
