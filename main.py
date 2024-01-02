6b14926a176b38ae7f0ac14fe0e73d4592d2e80d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:17:29 +0000

    offer relative relationship certainly possibility

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
