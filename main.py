4caf66acf05d9615601ebc46920f494b761f4296
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:23:51 +0000

    cheat alone out possible quiet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_work(force, distance):
+        return force * distance
