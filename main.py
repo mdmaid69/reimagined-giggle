abba8355008db6c206925bbe6e7c5c4ee10df6c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:54:53 +0000

    excuse rate neck kind company

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
