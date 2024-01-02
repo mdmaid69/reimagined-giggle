8878edbad894198e6f5449ad9be99cc58a61aa67
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:22:42 +0000

    remove favour opposite nice normal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
