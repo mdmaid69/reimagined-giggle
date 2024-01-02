2541581afac7a77b2475e339896b282dd7efa74d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:09:59 +0000

    start pen belong attach shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def square_number(x):
+        return x**2
