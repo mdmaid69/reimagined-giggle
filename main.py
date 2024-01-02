85865dbc64630ac9c89843edd4c36e30328a1715
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:22:43 +0000

    float apart exercise steer mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
