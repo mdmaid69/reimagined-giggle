2f1abc3d75b74549d655b5d1308c6a1b108f454b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:57:46 +0000

    throughout pile count satisfactory train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
