429c879fff00ba079a814126257ff53d3a53bc3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:55:28 +0000

    system light instant taste office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
