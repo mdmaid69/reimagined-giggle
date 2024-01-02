608c94202a690ffe8e7d5acf109a549bea12c2c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:35:22 +0000

    field carry get concerning meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+print(datetime.datetime.now())
