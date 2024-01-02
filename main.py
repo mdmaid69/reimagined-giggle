6fbd23b1c7b4aaa2a3bcf8c7b3847b1795a26170
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:51:19 +0000

    good give single chat steep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
