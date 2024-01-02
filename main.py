e6895ad610b4b8b838aa8dc9d6871f8733f0e3e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:00:38 +0000

    perfect sink that breakin makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import time
-def get_current_time():
-        return time.ctime()
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
