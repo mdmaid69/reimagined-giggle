8e95b7023ef3d71e80882e39d929cb6f8bcc1b9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:24:33 +0000

    worse excited bound just whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import sys
-def exit_program():
-        sys.exit()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
