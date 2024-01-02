a02a5c8ab7c3912d1e4f549e9866ac841f08fb85
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:13:03 +0000

    practice divide handle pot surprised

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def sort_list(lst):
-        return sorted(lst)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
