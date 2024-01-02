f1e061c3c3394ba76605c32c4701938d326944a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:36:00 +0000

    traffic board ought during mine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
