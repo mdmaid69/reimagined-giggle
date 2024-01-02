55b0b0e17b08a9f48809df125b2fa446f06d6938
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:06:49 +0000

    meaning means sharp stiff design

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
