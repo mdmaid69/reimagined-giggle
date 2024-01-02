ff61ab6589b1d84f66d88c4776ef118e36ba5063
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:13:35 +0000

    yet ago workon plenty wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
