02e039ad54bacba23d41c698001a9313d55bea69
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:30:47 +0000

    below phone wide shallow loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+x = 10
+y = 20
+print("Sum:", x + y)
