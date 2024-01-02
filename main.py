47c6b155df39501bfbdda1966ec7a916b995429f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:00:27 +0000

    getback pour protest interrupt division

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def is_odd(n):
+        return n % 2 != 0
+def remove_duplicates(lst):
+        return list(set(lst))
