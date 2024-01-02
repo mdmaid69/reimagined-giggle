e79398008989b06249e190082a5ee05ce2888fce
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:33:05 +0000

    often shade previous deaf skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
