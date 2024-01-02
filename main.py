7c5b5b2f5ae439adcbe12bc4e77c69f3902ebe48
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:25:38 +0000

    wave dull attract mess glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
