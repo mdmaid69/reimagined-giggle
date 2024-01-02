0e0f88202ad641ab5ae1b56a5b6e5b40665dd2c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:20:20 +0000

    similar round gather beneath keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
