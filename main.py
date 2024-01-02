d012ef9ce75f69756251174bec3cc8b592b02221
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:42:53 +0000

    never desperate interest form dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
