4b10eb2df06d803215127188eb30bb3a898d1a8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:10 +0000

    train clean suit arrange advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_str(array):
+        return str(array)
