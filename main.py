a7c4bfaf406ce048687d824b87e58d27d106b8b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:12:31 +0000

    study follow hurt own activity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import time
+def get_current_time():
+        return time.time()
