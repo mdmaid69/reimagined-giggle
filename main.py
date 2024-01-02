bfdccc97e3c00efbb337033a7b70d3acbe806978
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:33:34 +0000

    example disturb beautiful influence maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+print(datetime.datetime.now())
