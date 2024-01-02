acdd32948684d18cfb7654165b483da0d30129e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:38:53 +0000

    accordingto comeover coffee behave peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def clear_array(array):
-        array *= 0
+  def get_current_working_directory():
+        return os.getcwd()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
