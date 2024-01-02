521c603b7303d2f98232eeecc35fa93fee63db19
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:38:55 +0000

    turninto scale gradually top importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sys
+def exit_program():
+        sys.exit()
