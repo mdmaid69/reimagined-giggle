f1fedc5c99864352f240710064c115d89ed56764
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:38:40 +0000

    further dare runout many confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
