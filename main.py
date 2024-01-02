dc08f020abbe50268ab127fb486c4bfe0d21aa07
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:03:37 +0000

    impossible getback Uu bath tap

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
