3ebbe8f04656085bae23fd7bcb4daccad787b3da
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:01:30 +0000

    there place music club load

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
