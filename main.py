88d57480af5259f4ea9e6a99de009c502d7b8105
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:49:15 +0000

    keepup treatment lot us report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_bool(array):
+        return bool(array)
