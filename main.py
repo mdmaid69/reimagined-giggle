942d9808138239b97204f4508fc583f226f0dc6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:34:45 +0000

    recommend actual receive improve respect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
