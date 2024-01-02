73ad47c031ba24f76f8425e5ca222ea1f714b6ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:26:54 +0000

    fault holdup understand twist turnback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_speed(distance, time):
+        return distance / time
