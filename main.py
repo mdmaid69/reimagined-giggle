da4a754345ce2dcb5fb8d2822764cfe97a3322db
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:13:57 +0000

    travel mind fall occur breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
