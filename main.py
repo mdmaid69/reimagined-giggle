47493fe4da07aedf32d6472b2f1c23615f58694b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:03:10 +0000

    unit level teach prize story

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
