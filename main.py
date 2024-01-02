4e744df9dd8c2e65843323deb79546eb69311a1d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:04:41 +0000

    forward wrong health very react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
