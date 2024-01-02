f680fd352dcf9fb02f9e515fcccd4daf16d64a21
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:12:31 +0000

    across wait connect call unkind

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
