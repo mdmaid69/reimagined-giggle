a6e23f4f65a0345a6bd1e5cfa94984a7909f0a21
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:48 +0000

    cut communicate squash government sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-print(math.pi)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
