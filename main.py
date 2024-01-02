acfe730d8cec66c8abd53fcec24843aff21c5d91
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:14:28 +0000

    takeover confidence each ball include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
