1170e089ccbf99341e4d440031aab2110db7401b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:27:00 +0000

    apologizeapologise address seem remark operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_item(array, i):
+        return array[i]
