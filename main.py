a04fd8bbb9fffa9de50e84258182f930564f8939
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:20:26 +0000

    store cheap downstairs time give

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_area_rectangle(l, w):
+        return l * w
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
