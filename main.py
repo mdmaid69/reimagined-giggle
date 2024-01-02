a5f731187bec28c10ed838ad27cc1ee5532c2704
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:39:38 +0000

    cold provide mistake right activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_acceleration(speed, time):
+        return speed / time
