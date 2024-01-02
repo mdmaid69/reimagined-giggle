fdc19cbaf6078c8407e890249875651071d74375
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:08:05 +0000

    comeup match ever better present

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
