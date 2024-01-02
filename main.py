95f6774f8c6cd014477a6537caac03b97ab8e818
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:15:19 +0000

    once bury noise beautiful talkinto

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
