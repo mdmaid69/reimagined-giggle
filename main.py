8c9aa833d42c855f48db183639e309b4900bd780
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:17:35 +0000

    they impression soft Bb organization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
