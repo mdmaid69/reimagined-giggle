db2434eb5b511ef47f61ef2b52d5c8b34eb27b30
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:44:11 +0000

    another company claim flow insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
