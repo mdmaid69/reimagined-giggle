3ef828bb62b60ca435fbe5e65b347a5332b7d882
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:45:55 +0000

    564encourage allow tear develop yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def count_elements(lst):
+        return len(lst)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
