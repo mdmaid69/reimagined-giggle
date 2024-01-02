51a1539a7644aca327784baa92004fe817ffba2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:10:37 +0000

    born hot race calm fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
