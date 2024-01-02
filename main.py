3b858fc346b0404773a5550e21464b2fe0e69d2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:56:24 +0000

    come trick reasonable phone attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
