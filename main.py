e7a8289eaea93b17ae229b1329226a4671b261b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:29:44 +0000

    better charge hard file tradition

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
