a9292d0cf151f7d8540f8eae5a44a3f1a3b06ada
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:09:56 +0000

    married strong part offer callat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
