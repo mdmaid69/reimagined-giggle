63221f04002b705b18535c2318a888fc1f598d02
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:52:41 +0000

    getout himself wind card correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
