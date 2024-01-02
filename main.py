62c19dd1645e5cfb344330a8daf1062c3aa3eb06
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:58:18 +0000

    even probably pipe land better

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
