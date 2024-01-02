82ceb86f74cdfd39fbc18dbe4825e4bb7a7b348f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:24:40 +0000

    damage proper cry work noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
