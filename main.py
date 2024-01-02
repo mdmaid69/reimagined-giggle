88a844944115f4e4a8f9fe36a902f8c3c814125e
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:10:48 +0000

    keepfrom finish handle combine well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
