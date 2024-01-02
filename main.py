7aa608308f35a35685d4f3032ed233c33c3bbc41
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:47:19 +0000

    press pool smell mass display

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def convert_to_octal(n):
+        return oct(n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
