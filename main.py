172559afb6a4817ed4873ad5c807aa5c3af68a31
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:50:03 +0000

    traffic possession equipment idea uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def clear_array(array):
+        array *= 0
