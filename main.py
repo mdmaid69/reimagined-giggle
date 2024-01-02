c4916e7bfd160896bb2396fa1b0611a3db7fdc43
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:35:00 +0000

    although twist shape himself exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
