9aba050fe39e09d4866435e7c3554abe837d06b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:42:10 +0000

    temperature centre drink tape giveaway

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import time
+print(time.time())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
