fc85b5c00765561a9167a5e2d40d78c07b8ebae2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:57:17 +0000

    height fun education rush remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_sine(x):
+        return math.sin(x)
