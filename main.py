60475d7184923b1707a85e20b783b1c5c15a26a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:59:41 +0000

    snow for feeling until mood

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
