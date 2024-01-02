01b47aeb65a97fb55c02128540dbb88edec80d03
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:14:33 +0000

    die carry stir exit complicated

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def append_to_array(array, item):
+        array.append(item)
