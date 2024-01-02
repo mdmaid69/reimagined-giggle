135a7b1666620515e6520a9e81a275d07b68579c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:29:10 +0000

    square out coffee value discovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
