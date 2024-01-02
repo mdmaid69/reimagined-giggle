38715f64a6041fa2b689171bfa28ba4b54e827ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:17:08 +0000

    guide Gg nor bin exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
