3d494e7db34e14e8bf454f9e18bca55f216eba93
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:19:54 +0000

    off post reaction dark ought

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import time
+print(time.time())
