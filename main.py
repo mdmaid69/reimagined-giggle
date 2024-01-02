d289c21cd47483d064873f787f8d8c5dcf4a428b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:30:07 +0000

    central minor tend crowded out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
