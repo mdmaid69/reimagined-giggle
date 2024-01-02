61d33c1f3adbea9f216fdfb2e5aee3a6a93b5fbd
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:16:50 +0000

    afternoon loan takeaway distance major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_floor(x):
+        return math.floor(x)
