b1879d160238d5796a97a5552a40f5d70a6b05be
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:57:07 +0000

    this Vv sensible turnout hardly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
