da752f867db159f815ab35e0dcef49af7e9e6908
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:34:06 +0000

    straight search mind quality strong

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
