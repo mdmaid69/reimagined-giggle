12235a220e5c69e998a1571886a447f98c8f6c0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:42:23 +0000

    clean may hour game trick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def create_stack():
-        return collections.deque()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
