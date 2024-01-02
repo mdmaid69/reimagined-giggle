c19b5dc2c5cd552c58e6ff71114c69589bf85e83
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:58:47 +0000

    runaway advantage achieve address record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
