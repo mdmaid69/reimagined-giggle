92270f8b5c298bd906d47842d01e8aac4dfc48ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:15:57 +0000

    clean own breakinto rest first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
