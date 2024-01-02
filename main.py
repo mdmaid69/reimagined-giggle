b526e64af81da5597f7231c5ab12ab46c0822f3a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:12:14 +0000

    callout food thinkof slight represent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
