3b72a354553aa721d421fa13a5f0dbdbdd69ee58
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:50:48 +0000

    goon bang close Pp breath

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
