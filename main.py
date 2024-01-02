e28cf22cd71e671873dcbdfc0490b45f23f29d7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:31:15 +0000

    somewhere anywayanyhow continue great actual

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
