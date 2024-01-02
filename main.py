dc9f1c3f7bd49b91d3eef5661a1bb5b5734f49b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:44:06 +0000

    well split save after fact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def square_number(x):
+        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
