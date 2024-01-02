c1887c71f0291f2a40deb837030b021c592f93e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:45:53 +0000

    down place terrible bury up

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
