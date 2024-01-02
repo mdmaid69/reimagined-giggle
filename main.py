97c0ebdea7141ce92e34af269559e78fb236bff4
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:46:42 +0000

    offend pleasure office brush face

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
