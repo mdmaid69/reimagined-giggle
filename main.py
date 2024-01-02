21f091c4caf97371df55138494d8c67359cc690d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:14:46 +0000

    task sound spread talkback finally

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
