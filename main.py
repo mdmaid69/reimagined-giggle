a86b6ef33b836e15e11c65331f710ee772c70817
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:27:59 +0000

    form under wide weekend attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
