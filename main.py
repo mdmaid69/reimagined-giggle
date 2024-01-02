d5421bb5efa166396b70d93950e9ba546034ad8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:57 +0000

    culture ring heavy instrument oil

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def remove_from_array(array, item):
+        array.remove(item)
