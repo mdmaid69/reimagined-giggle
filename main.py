ef7967f658c60f3e5e63198ce38493448e733a8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:17:24 +0000

    bottom represent somehow content form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_force(mass, acceleration):
+        return mass * acceleration
