2eeb56e08702b828bb22ee80c1f691ba91a06c6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:48:39 +0000

    visit sight disagree home truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
