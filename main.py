565caa16f8a2eb7c397bc07a79f79280ef631cbe
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:21:12 +0000

    whoever angry century copy refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
