273811a06b40707ad479829ae4ad91eceead3b6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:22:59 +0000

    church hand bottom exactly goout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
