7f6bcebb8916eb3274898df4910b1feb893325e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:05:16 +0000

    development actual recommend special thankyou

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import logging
-def log_message(message):
-        logging.info(message)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
