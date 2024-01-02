871f011a7c5ec4fa780f0c8fbe5f9888d751ed71
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:13:51 +0000

    achieve walk gather substance pile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def is_even(n):
-        return n % 2 == 0
+  def cube_number(x):
+        return x**3
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
