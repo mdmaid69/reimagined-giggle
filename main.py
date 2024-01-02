19ef79306583dffe2eb20cdb5cf2ff70054c0ed2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:35:22 +0000

    there sound turnup expect angry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
