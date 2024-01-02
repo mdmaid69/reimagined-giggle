2812de72208a2c913da1b333b9f432812e002b7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:45:56 +0000

    relax fortunately guilty should unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_string():
-        return collections.UserString()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
