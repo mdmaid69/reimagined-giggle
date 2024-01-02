ef2544204b0f20a7ce4b517c2c61146efb6343d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:49:27 +0000

    vision burst bang personal aware

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def reverse_list(lst):
+        return lst[::-1]
