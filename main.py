dca33aeb53ad16ef8616c01947498b789a4882a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:12:13 +0000

    circle tap public best lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
