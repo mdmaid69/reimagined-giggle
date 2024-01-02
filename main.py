7290ce0148fff143cd2a7f5504609d39aa6c49fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:50:19 +0000

    pinch table whoever nurse Jj

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
