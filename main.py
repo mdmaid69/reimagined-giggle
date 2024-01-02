6221ab88c976cede484309a6bc5381ee42c3e85d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:00:07 +0000

    stop owe matter left calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
