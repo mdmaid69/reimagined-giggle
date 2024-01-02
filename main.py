e7d5da3be37d1803ffcdb705cac304bcefbd0bb4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:20:44 +0000

    appeal except steady thank long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
