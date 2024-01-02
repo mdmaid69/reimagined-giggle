8365ca77d8c5ba780dc2ee9989de0087219935d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:29:19 +0000

    young pinch tip clean art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
