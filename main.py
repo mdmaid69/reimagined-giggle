d98fb6ec4c3f4f01e7b160a27ddcff905d97bf9a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:49:00 +0000

    concentrate worst treat stop from

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
