ec9bc412f1597a31234bcc6fa5f8e55790b69345
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:48:53 +0000

    drink out means creature criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+text = "Hello, world!"
+print("Words:", len(text.split()))
