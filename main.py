06f6911ea9247a1d80f2ce7d982e76191820ad99
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:34:00 +0000

    involve bar unless building pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
