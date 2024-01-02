9c33378d76248ca44b9ec83c4fae559c9d0ea631
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:13:44 +0000

    flight tidy cause head thank

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+for i in range(10): print(i)
