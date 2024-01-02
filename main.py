622484b7c62818ef054e1e2fb87ffbd2cd0469d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:12:35 +0000

    should careful computer repeat room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def count_words(sentence):
+        return len(sentence.split())
