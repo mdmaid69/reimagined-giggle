dd0ab8f464b3fc7f86516b557784b6e10f7e0edc
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:52 +0000

    stone thankyou equal test mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
