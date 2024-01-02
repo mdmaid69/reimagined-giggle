d3c1fdf76ea49e03ca793e2bbb8d6b3dbd3bbc2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:57:05 +0000

    back worth sweep fair excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
