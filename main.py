fc5253c13ca81afcfc87a9270f273e87a3cc417e
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:47:18 +0000

    block mystery interview exercise call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
 import math
 def calculate_root(x, n):
         return math.pow(x, 1/n)
