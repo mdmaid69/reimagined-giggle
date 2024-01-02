fd2606dc051d54f710b52aedb6aed5e20f055775
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:33:00 +0000

    control smooth door any welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
