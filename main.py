8215dd5c49db477047bccf5feb5b8d5623b2e8b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:43:35 +0000

    safe read light get home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
