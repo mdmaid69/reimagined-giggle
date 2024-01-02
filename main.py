2e7d487b28ccb9e50a64457824b8679ec44d0a68
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:39:08 +0000

    bath runover kick emphasize brush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
