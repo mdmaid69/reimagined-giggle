5d8b66aafb856da0c4c67e19ab28c6ee88c54d8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:54:53 +0000

    trip pinch lose thorough high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
