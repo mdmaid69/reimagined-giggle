e17b3d35e7ae8a00d24b101f11b351dd6c6033ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:43:36 +0000

    equal body himself smell platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
