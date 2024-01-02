f726783ef9c3188d1441a2f7fded9b23640e65a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:29:26 +0000

    clear rely within describe close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
