88cdba7d0c8b985087669a94d5c64dcdd54793d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:24:04 +0000

    contact order long still break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
