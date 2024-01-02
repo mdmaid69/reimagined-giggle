e3b18a1d863782b1ac59612b71e3a55494035658
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:41:47 +0000

    challenge about among wherever over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
