c1796eb286386c0e219b37e6b009f3d1abe90ed8
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:38:22 +0000

    through single getaway report also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def cube_number(x):
+        return x**3
