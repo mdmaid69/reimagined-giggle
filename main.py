1a1639826e78275e276990eb167c500e2082becf
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:52:07 +0000

    sensible apparently stir little refuse

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
