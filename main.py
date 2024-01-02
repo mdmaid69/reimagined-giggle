e11b19e6543877a766d8d51d47cbfb312174f0fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:01:50 +0000

    main breakthrough introduction storm ready

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-for i in range(5):
-        print(i)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_tangent(x):
+        return math.tan(x)
