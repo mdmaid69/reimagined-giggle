7966c0a52fbc76ce7ec08873bb7f0a98307e60a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:47:11 +0000

    star hot scheme pinch shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
