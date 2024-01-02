c309857afc4b2c21aa1fe18bd31aeee647091576
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:20:39 +0000

    clear computer good occasional introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def convert_to_binary(n):
+        return bin(n)
