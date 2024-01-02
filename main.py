f50f424c81ab42da176c3b9956b6debc85ededb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:19:51 +0000

    centre bridge rate content lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
