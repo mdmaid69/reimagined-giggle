3ff3440ee51a6204eb21deaa1df1b42a7aaf1c69
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:54:43 +0000

    comment all brief charge announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_length(array):
-        return len(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def find_max(lst):
+        return max(lst)
