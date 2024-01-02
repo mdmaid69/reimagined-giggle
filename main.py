9cf8cf38dd8b3e4509428d6b8571044c57fe5116
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:33:25 +0000

    sweet slice eager pity measure

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
