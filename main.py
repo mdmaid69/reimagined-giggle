bdb677835a2b4e41482b7a0f1ccc17c3b28def68
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:53:14 +0000

    artificial safety wish getoff tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_max(numbers):
+        return max(numbers)
