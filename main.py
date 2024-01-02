5fd22e954db349851e98a0872c3d9ee77bbf2cdc
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:38:36 +0000

    excellent shop leave cinema wonder

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
