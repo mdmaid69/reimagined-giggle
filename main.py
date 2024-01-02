e187f54e2f1726bea08073ffd337c9bfdee3075e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:24:34 +0000

    top perfect on he concern

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
