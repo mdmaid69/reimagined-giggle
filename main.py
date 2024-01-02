97cc68fa49a26c978b5a978ff17cd71256ac65e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:00:16 +0000

    phone Yy takeaway twist keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def reverse_list(lst):
+        return lst[::-1]
