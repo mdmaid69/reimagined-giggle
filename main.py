eabe788986df240b7d2fa4ab1f21580c3a563e2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:46:23 +0000

    turnup sea stamp have necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
