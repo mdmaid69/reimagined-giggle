2a1bc750522db2c6f5fe0c07496d2762e7df4d71
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:35:15 +0000

    discuss plate explanation history office

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
