23ea30066998d19628c9ab83dc6d90b0d55f2048
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:06:42 +0000

    display save immediately behaviour uniform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
