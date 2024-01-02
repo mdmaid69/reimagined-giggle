110755cd595ebeb5107fa1e7a53353eae6d339ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:49:56 +0000

    professional term flavour stare Ff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
