854627fb2bb94bba4e8a7a8a2fad83c350f4f07c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:31:01 +0000

    weak any summer raise concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
