8b10d0f15fbe283b126a8fd627715af99c72ea35
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:46:54 +0000

    project brush ourselves bear emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
