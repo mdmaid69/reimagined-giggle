7fe4b1aac2c732afb34f0259faaa7b2c5303c0ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:53:06 +0000

    relief substance risk on breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
