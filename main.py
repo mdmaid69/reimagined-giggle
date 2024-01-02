c4520c78c17770ada01b17a411d04dcef9da91fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:49:29 +0000

    sleep aim young wing floor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def convert_to_hex(n):
+        return hex(n)
