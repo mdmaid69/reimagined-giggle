6126feef0f9db8b16a20563837ccd5c2dfae1880
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:29:15 +0000

    whenever murder genuine his after

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        print(sys.version)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
