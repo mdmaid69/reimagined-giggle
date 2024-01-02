559c228c3743ef71d012dcfbc5c44303598bf37b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:36:03 +0000

    drink ago clear bottom tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
