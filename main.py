47fca128f041aa5c72f80384b28b57ec7ac4b1d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:55 +0000

    worry seriously invite like let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
