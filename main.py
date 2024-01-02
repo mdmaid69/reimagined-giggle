8e6fb81fc88a8e2779a764a76cbeafda32df2a25
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:53:49 +0000

    plan art friendly swing crash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_power(work, time):
+        return work / time
