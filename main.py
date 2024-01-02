e9da8db6ff8307077f5f6b65a310e8892603e8d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:14:25 +0000

    combination maintain tie list suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
