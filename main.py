d8441e0338598d999ccad8c7ea2902ccd3376728
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:08:12 +0000

    already round still attention set

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
