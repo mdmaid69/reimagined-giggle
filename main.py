2e7a811eb367def722be82bfcb88b641258768fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:52:22 +0000

    wing so repair rarely listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
