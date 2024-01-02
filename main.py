73fa441701fec85dbc307fc222b7a5b4a4fe1764
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:36:37 +0000

    sort interrupt experiment smell formal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
