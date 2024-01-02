080cdd57607e2d6d2dcf13377b1852174e9eac51
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:41:00 +0000

    until better bring skill fact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def sort_list(lst):
+        return sorted(lst)
