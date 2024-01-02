02b3736f41c0733959877b05341654a2aebc2021
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:58:46 +0000

    less demonstrate best tap scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
