a3667653984092d55e7c50166c5dbe3e69ad04af
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:20:51 +0000

    level go pile bottom sink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
