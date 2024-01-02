52c3b396fa706a3bd59bc3015c5179ab52ebad7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:36:33 +0000

    general bury fine first communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def convert_to_binary(n):
+        return bin(n)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
