f0e0161e55956318d1f0b465065d6288adf72ad7
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:12:19 +0000

    shade cum affect fortunate depend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
