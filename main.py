2bf5095fc6b386e852dc4fab6ab6a53dc9a37e48
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:15:42 +0000

    Ii honest approve raw score

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_list_from_array(array):
+        return array.tolist()
