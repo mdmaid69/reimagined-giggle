876826f602636cfe40d4c5c0e05b11e8743c3539
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:53:00 +0000

    takeaway succeed entrance month card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_bool(array):
+        return bool(array)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def check_if_array_contains_item(array, item):
+        return item in array
