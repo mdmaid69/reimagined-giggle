225b6ed3b422456e92609bc54ddb2c693c4d7f31
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:08:55 +0000

    afterwards instant correction unhappy flood

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
