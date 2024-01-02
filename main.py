011246ff290d7b2e3dd8a150627dbc8405834352
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:09:50 +0000

    no cough chase dance only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
