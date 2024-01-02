0207a34f4b2aaa484ca10f1830ef99f1aad26b0a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:45:31 +0000

    getoff badly meaning on soon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_index(array, item):
+        return array.index(item)
