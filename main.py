524985706b958833549612e0712d018dd72359fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:42:26 +0000

    shine considering pride workout progress

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
