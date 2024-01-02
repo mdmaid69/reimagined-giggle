45cb3408b6a7bc69d0b122396c3b4ddd486785d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:41:52 +0000

    apart radio when save strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+from collections import Counter
+print(Counter("hello world"))
