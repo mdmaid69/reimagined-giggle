bb278c6a951a4850688496cdf33cbe271aa94169
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:16 +0000

    thirsty shop cause sore treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
