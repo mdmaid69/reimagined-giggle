2fde8f0f3446aa9ecbce57a0e20019f90d809af4
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:28:07 +0000

    central hurt season reaction yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_directory_name(path):
+        return os.path.dirname(path)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_list_from_array(array):
+        return array.tolist()
