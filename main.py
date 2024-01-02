ba28442eccaed6a6641b6a334e5828ee7a67a2c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:31:53 +0000

    scream gate father strength sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import sys
+def print_python_version():
+        return sys.version
+import os
+def get_current_working_directory():
+        return os.getcwd()
