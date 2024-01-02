914a10ebb18662c16de2dca8bf558ddc543b44ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:51:44 +0000

    risk thank confused special recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import array
+def reverse_array(array):
+        array.reverse()
