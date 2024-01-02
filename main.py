57b6888edb3adafc1512acd3e8ca36e7181becee
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:52:45 +0000

    lookback(on) waste sport twist friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
