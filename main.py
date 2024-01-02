f28f49563d7950160e6264a013095cbcb3825706
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:03:53 +0000

    task charge makeinto 566end music

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
