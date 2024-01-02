c92487ec223efe9b89d8cd83d19f6387fa922775
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:09:23 +0000

    unite bite ask advanced otherwise

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
