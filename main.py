6a1496496b540f6deb8c52f5fb00a37d18f712de
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:01:46 +0000

    intend union reduce fast offer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+print("Hello, world!")
