cfc9613aa6d76577fad287ba605753fc10d8b40e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:35:55 +0000

    impression slightly kill office watch

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_pressure(force, area):
+        return force / area
