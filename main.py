3e24754aab7d47832afd5d14f9a3b264e8fbe4c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:25:15 +0000

    principle bill deliberate whoever standard

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
