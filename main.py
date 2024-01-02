1c752942301febae722e268d9e607112e00a1ae9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:37:48 +0000

    comefrom middle design funny stiff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
