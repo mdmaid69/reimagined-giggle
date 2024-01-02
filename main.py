5d2a0dc725cf3f73655f4cf2ee920d2be9edcca0
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:59:16 +0000

    shallow become count like priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def cube_number(x):
+        return x**3
+import os
+def remove_directory(path):
+        os.rmdir(path)
