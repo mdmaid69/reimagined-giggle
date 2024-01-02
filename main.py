683d4d1f67f32a3d64c8ad6166dab65b9614a930
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:59:42 +0000

    exploration centimetre ocean fairly accommodation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
