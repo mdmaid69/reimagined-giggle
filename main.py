0f378fd7a6de009b9ec9c9b43d6ecb2cc65a0caa
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:36:19 +0000

    crash through stir trip turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
