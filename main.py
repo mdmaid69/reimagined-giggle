6d48ba46ade7e15403075cb3a2dc85dd040a1cfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:43:12 +0000

    would raw no ring sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
