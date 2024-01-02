0dbb1d7be7b1960bb9b666c6cbce3a247b7f21b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:20:41 +0000

    tape underneath no fortunately allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
