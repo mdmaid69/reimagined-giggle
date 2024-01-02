e0fb6f267c8e8f86cf1c422ac55b76149943e85b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:18 +0000

    fair stupid splash drop lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
