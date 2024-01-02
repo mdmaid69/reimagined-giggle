e88ed574e1af6ffa617aa1f6a070f219c0bdff01
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:15:18 +0000

    important stamp form represent pause

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
