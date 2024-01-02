46e1378c1232efe5426415d979dc83d4f4b08047
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:27:55 +0000

    calm stranger dance production impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
