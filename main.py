f3fb5260900eb903da99a7dc3e5b0d3480db5f81
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:34:59 +0000

    date friendly goback(to) below concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
