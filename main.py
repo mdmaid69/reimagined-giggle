4df58a37054717c32697d43785b0a346f9470cc8
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:06:08 +0000

    tense sudden free better slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_index(array, item):
+        return array.index(item)
