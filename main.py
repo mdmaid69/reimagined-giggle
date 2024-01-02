68610b7fff5bd9935541169016fd74ccc376b346
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:38:24 +0000

    clue whom him takeover thank

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
