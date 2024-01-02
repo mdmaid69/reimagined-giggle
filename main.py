345d0d5e993aae01be4e6f04a80d32392c7b6e85
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:31:05 +0000

    hit woman address correct bring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def convert_to_hex(n):
+        return hex(n)
