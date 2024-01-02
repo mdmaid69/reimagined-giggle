c92376d727ed3f9cae4d6072307435a16b06bba4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:31:05 +0000

    concentrate angry convince thin hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
