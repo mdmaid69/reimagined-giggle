f79dcdf2df104dfd3f7e66c2ee09f92bfbda94ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:50:46 +0000

    can genuine plan deep guess

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
