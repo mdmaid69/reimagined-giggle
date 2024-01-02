cff181e0398d0d5b5964877df9fa2d81861e5bb4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:25:24 +0000

    chat forget soak include tour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
