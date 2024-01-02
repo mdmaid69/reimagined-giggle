16609a39cd5a484c576d86079c0801d25ee1fe24
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:14:50 +0000

    point favour frighten please demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_power(work, time):
+        return work / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
