258ae82fa07b9e8a12016a6d44482f07cdc3259e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:47:50 +0000

    smile question fasten thick guard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+print("Hello, world!")
