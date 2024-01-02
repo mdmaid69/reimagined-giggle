22abfe5db039dce705f60bd3c43e6a522fede439
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:10:07 +0000

    match hardly grade group geton

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def count_elements(lst):
+        return len(lst)
