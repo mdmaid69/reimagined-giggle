a5973df8a8fb7d5e123958a55fa409f717c48fd9
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:54:24 +0000

    well police pick project offend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
