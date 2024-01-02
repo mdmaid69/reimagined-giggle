1cae144e9e6bc4407c4daefa34d3027b1d56eb30
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:16:07 +0000

    rarely order bother bar up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def reverse_list(lst):
+        return lst[::-1]
