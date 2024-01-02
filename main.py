91140d97869c2cf9977ec00b1c74a53298779869
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:28:10 +0000

    occasional change person past silent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
