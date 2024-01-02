48468ea994dd1b9e82ea6ac334763713f574de07
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:33:37 +0000

    alone set sound information change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def is_even(n):
+        return n % 2 == 0
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
