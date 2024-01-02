2ca9841438ae6b7dbb9eb3feb4ed4615eb031ac0
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:03:38 +0000

    female sail threaten convenient on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
