e018d5b47571688e8dd08a7c5b5bc03456858563
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:09:13 +0000

    century public objection mainly supply

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_speed(distance, time):
+        return distance / time
