d4b73ae6cb4b5924b6ac7f4c15f0678b3a8babb2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:12:00 +0000

    sick handle escape pollution drawing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
