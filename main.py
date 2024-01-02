0bb6664438d0a1a418f4579dc8c165843b545d60
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:12:55 +0000

    congratulate pattern force fit possible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
