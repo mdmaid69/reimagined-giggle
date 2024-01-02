66aa2049efa49146d9125c7205216bb4ec4c5523
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:55:06 +0000

    narrow although upset hit flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
