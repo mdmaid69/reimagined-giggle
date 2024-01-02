8576ca380a577ac420b52f3f072619538f9246d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:23:01 +0000

    your sleep boat subject congratulations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
