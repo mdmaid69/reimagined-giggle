23d5d6611976e6cc146a9ab53e14fac7f6de3921
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:57:53 +0000

    well suit stupid gap trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_power(work, time):
-        return work / time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
