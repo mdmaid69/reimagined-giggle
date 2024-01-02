32ee7539d56b9fc01bdfd20ae9e79dd852cb971c
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:14:45 +0000

    present turnback cry state pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import collections
+def create_counter():
+        return collections.Counter()
