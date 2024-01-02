6d68010befb968a9363a684f172f30a9ae6f6bdd
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:17:38 +0000

    present if eager back share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
