e1563ae6eb32ada78f6d1343f0dace3a85a61709
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:18:19 +0000

    task almost nose interview prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
