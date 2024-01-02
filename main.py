7a08f77952783ddd36873610cfa9e6a654979658
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:58 +0000

    estimate shock matter workupto object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import datetime
+def get_current_date():
+        return datetime.date.today()
