0aa3d24d82126923f2918aac956554b2cd1e1a5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:22:46 +0000

    fix excuse pair experiment round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
