2c11f2818d81c891a3d710d196f60455cf0ae709
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:47:17 +0000

    air intend rate back wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
