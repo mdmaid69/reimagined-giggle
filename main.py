9a02eec3f212ec86eed00790c200ecc61e97eca0
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:36:17 +0000

    fail Ll down refuse angry

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
