cabba3007c0a0dde646b42e677a09340a01d56cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:15:44 +0000

    music sick sail drink keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
