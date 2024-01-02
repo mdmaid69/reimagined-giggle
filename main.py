29e53aba0b24a97fb6b4c0cabc9220a38ff08bca
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:24:30 +0000

    black deal talk be material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import sys
+def exit_program():
+        sys.exit()
