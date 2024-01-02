46a5aa74e5cd30c17bce69ef37d0e25a2726b267
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:50 +0000

    pattern smooth admire office watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
