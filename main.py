6ccef0917a73d843dbb7a082500c24e5cb8e349d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:17 +0000

    aim spot along week Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_max(lst):
+        return max(lst)
