65257b61aa6a3323d8b6602909451431b35cd266
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:38:43 +0000

    plastic cook squeeze sell say

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
