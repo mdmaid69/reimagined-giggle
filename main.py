302124e1915e9efa6f3fa1c21928690abebfe9ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:34:25 +0000

    grand breakfast laugh issue pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_list(array):
+        return list(array)
