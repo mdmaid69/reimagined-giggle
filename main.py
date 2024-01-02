e961c3b829b66877fb2bde532e95c84a6cfb6dc8
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:56:35 +0000

    else occur law object path

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def remove_duplicates(lst):
+        return list(set(lst))
