7d2f0ae752becbfc3abf0ed4a2d9c5cb559f6228
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:30:46 +0000

    embarrassed him search environment dull

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
