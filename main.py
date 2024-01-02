42d0c952ae6719b949fce7145f99c33c61bc4bdb
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:34:00 +0000

    exciting towards satisfactory turnoff suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
