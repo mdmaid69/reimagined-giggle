7b31b62e9b86d05b9820b371a226027e6085e002
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:01 +0000

    pool fear basically callback comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
