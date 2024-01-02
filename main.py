6f8e6bd715d0d605bde1a500c4159691b2922ad9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:44:20 +0000

    breathe label path attach roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
