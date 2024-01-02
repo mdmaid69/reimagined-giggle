5c65c97f5d06887035e774a0271a67065e5d26bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:42:03 +0000

    rely chase bin Kk welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import tensorflow as tf
-print(tf.__version__)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
