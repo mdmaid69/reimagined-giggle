f800b23bbadbf0ecfcc6fedc59c04107a82ae85b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:42:08 +0000

    scrape patience pause show 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
