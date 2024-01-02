3429e0c9afa235265f79eb0dca728a474a430683
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:29:49 +0000

    strange coat pretty sister quality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
