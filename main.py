65970a5f7e5edfa4dec8b1b498cb9975f584cf6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:04:00 +0000

    path reject bringback place free

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
