12088132e472ad95a9a7904fb2d5e2c1058ea8b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:20:19 +0000

    in bringdown calloff correction march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
