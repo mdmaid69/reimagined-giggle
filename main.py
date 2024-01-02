44451012045d1240474e53a0331265f379c40185
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:23:32 +0000

    low approval education afford agree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
