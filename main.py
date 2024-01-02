ca2c598f00a2ac2be33f9c744c4c49d3121f0a30
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:16:49 +0000

    wear work wish interfere divide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
