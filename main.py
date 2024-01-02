81dbffcf7c614d01031b8d8c15a5230107c2e5f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:06:45 +0000

    satisfactory up drive mainly spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
