1510abe68ae2b6d8cddfd40648b6cb4ea5872f45
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:10:58 +0000

    pity badly want touch reliable

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import sys
-print(sys.version)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
