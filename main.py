042fd0ab787a5896fe33a06db88910da5a64cb32
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:09:45 +0000

    declare style among press top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
