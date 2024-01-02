e4655b0802153bfb31c42850338986ff25d0dfb3
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:50:47 +0000

    dream lookafter laugh level fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
