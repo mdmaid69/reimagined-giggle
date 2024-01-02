b66c1f779f4557acda2471be97778d7aecf8b15a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:25:19 +0000

    example breakout any name bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
