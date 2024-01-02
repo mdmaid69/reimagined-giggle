29bc10b4ffd31b9af68b2f1b387363e7e68e7428
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:08:51 +0000

    care pretty stiff nurse mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
