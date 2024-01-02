b675d88a079995fbb2ec511208e2114139fd2587
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:11:53 +0000

    back criticize climb excuse why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
