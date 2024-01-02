25ab9163141ebbd9d3033150f1be65ea718fc799
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:14:48 +0000

    angry hit discovery why believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
