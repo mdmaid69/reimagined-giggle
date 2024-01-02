a0c0a92ca1eac9ea508642e0dfdd1b88a3f75233
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:21:47 +0000

    entry interesting must regularly Uu

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
