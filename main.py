3bd24063c0c4da0dd7fe3552745ae7c15f8e610f
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:09:47 +0000

    creature pattern Nn fast measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
