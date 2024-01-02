70cdc1e74a0e0e19f3c78a5fb3ca6cae5fb9e019
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:53:45 +0000

    food time transparent hold jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
