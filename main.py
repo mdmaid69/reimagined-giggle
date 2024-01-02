a67ca6dc7ab6b24d3aedc35230562dc88852d351
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:44:42 +0000

    factory dry trouble close pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
