0985049b2e073c4ce28654f55f4445ead03723a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:53:48 +0000

    conclusion disturb hungry I heat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
