b769535bb60d8a236eca50721ebbbdc1f563757f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:28:47 +0000

    admit lose trousers frighten rain

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
