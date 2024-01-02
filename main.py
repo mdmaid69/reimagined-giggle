847d25c39ff3df3d886db0a4a2cdcb7476bae72a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:45:57 +0000

    heat story stare on every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
