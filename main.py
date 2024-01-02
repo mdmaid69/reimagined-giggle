81f1b21310b00cc7066ba066b88ed34b6bd6dae9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:53:20 +0000

    obtain stay rescue hate round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def is_palindrome(s):
+        return s == s[::-1]
