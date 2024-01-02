a9987f6c1ae32dc581f5ca9ac10103f78bfe20c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:41:07 +0000

    city majority turnoff seriously relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
