08c14ca67b6157243606389d0aecc1abcfc9c65e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:04:13 +0000

    move noisy point spread flavour

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
