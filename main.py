88ee1cdf42de2096946b1b67c68e5873959cb352
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:02:06 +0000

    no convincing bored about hide

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def count_elements(lst):
+        return len(lst)
