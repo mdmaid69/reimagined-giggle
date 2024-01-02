a43e747c8c0dae46ff74e7ad549a060a6ca48ccb
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:51:57 +0000

    want desperate issue word night

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
