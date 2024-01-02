f9b2cca992a8ec7275b83b84a0b3608c05872bbe
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:19:55 +0000

    murder light road agree here

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
