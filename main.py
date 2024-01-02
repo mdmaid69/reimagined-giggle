73ac97acf277505e35cdcb0c865d4ab45b8642df
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:58:43 +0000

    treat whom explain taste expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def add_numbers(a, b):
-        return a + b
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
