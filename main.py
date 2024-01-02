384ee62d5305f8e0c8cfa86b464f52a2697c5976
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:02:06 +0000

    perform rest order sell any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
