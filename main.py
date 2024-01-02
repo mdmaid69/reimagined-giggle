060e9c2331ec562a2d3e60999a344e26675e9790
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:05:09 +0000

    friendly course guilty sensitive back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
