5aac18afd3db2e86870c065125d358811df13dd0
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:45:27 +0000

    shadow comedown innocent earn behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
