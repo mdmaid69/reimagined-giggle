db50cd752b6e8b118bee4f4bcc184492f3a56aba
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:16:56 +0000

    pot among tense hang ordinary

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def clear_array(array):
+        array *= 0
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
