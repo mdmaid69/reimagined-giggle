c3706c566d1688c9b6f49ed44861345fc82ca99c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:34:08 +0000

    convenient divide outside full obvious

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sys
+print(sys.version)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
