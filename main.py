f03ddc4e668495a0c4df0890e3fa57ec6e6dcdbd
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:03:44 +0000

    other rush importance target wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def convert_to_hex(n):
+        return hex(n)
