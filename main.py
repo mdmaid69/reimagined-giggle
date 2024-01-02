5b054040ddfc0d7442d57d1374ad0014e245626c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:04:27 +0000

    question partner off each pleased

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
