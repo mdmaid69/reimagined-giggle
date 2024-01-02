006fbcae82d4383d90ba6ad344f9d9a194ab4879
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:10:20 +0000

    team argument body disapprove trouble

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
