2b6a6289afbfe484ef6c83b9f2235f9164fdeaa5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:40:13 +0000

    supply reduction could trust effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
