ac30754df127a0e7eb8b5a8d8d3f6306f616f29f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:34:17 +0000

    escape embarrassed base nice mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
