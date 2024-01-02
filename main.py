399eba071088251875b7986b7285dc895d5f7b5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:29:44 +0000

    unusual forget bye occasional skirt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
