68d24a605d797dbe556f456353acd405d6d3a558
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:07:33 +0000

    container crash ours feed software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
