05675fa02da2cd36bf171961ec3ce42575362869
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:30:55 +0000

    indeed ask experience wherever yours

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
