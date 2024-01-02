1f44b3af65de0ed21342484aee59dec3d21c8aa8
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:09:23 +0000

    about hour however maybe contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
