fc1d80e55fd87c15f6bf6176ec3c014848bfc5f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:21:15 +0000

    work unhappy standard scrape giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
