bc60967df2883ed06802998b04183d1281cc9bca
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:07:56 +0000

    laugh complete mean punishment start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
