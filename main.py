e3e84be3bf4f104f102ce92bc04b0490cea9ef56
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:38:43 +0000

    behave lose suit Nn confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def convert_to_octal(n):
+        return oct(n)
