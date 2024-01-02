715f7c99418f7246bc0b063fa6364d984d6a71ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:23:00 +0000

    unfortunate keep straight behaviour step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
