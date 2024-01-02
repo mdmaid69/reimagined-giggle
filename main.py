812b6cdaa3e923a88edbdb23e6060a025d191415
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:40:33 +0000

    experiment punish wind thankyou godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
