074a19e95a8fdea36b919f6b2788ab7eba076f91
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:11:14 +0000

    alive force nice fire evening

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
