6b36c8854dff18d31d386f142ba7a302ce0044bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:41:45 +0000

    impressive when hold anxious warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
