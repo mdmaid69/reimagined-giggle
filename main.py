0dbd77c082be60a5ba0b2520214ce35c66371901
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:34:23 +0000

    bad repair no port conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
