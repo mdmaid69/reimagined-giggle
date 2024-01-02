2a83fa34ad6db6e9061d29c2deaadba42913ba98
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:47:51 +0000

    dream house actually recent hang

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def is_even(n):
+        return n % 2 == 0
