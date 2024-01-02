d244193f04325688cccfb1dc3bd60ccbe5a6914e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:34:47 +0000

    impressed care cold warn clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
