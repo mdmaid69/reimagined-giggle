f90e4703851ef0fffc2369fdd45e11340676b2f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:19:23 +0000

    miss total sometimes upstairs famous

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
