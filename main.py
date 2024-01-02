2a0f2f490359c540f17d70903608bb4955c4d024
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:23:47 +0000

    turnoff sentence suit believe breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
