b1a3df1491ba5790c7563963a88f8efa2ff13ec6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:34:17 +0000

    attitude whichever everything point runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
