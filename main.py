0fdc41521c96a1fd661e6d1b51ccda9759d2e1f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:28:40 +0000

    could refer safety people later

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_list():
+        return collections.UserList()
