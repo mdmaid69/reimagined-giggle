d2ff627e7483af87b2b9f83be26b26492a4b4210
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:41:46 +0000

    scale arm accidental paper his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
