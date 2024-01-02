42ed65a7854bc7abbe0bedaf7b27e0977a532f2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:38:13 +0000

    hers phone fight lunch happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
