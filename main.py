febc02443934469ce4071246d7669a801483e26e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:02:47 +0000

    usual general exhibition natural bother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
