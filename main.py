cc7be64e5b8c707bdbc1d96f98f8a1789ac3e194
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:25:55 +0000

    earn final let control connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-print(random.randint(0, 100))
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
