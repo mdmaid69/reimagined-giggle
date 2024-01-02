0884e50762f188d2496f1ab99643348e410e3f8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:06:40 +0000

    clue degree themselves throughout amount

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
