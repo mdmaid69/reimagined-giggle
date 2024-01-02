e5e02ff7cd12c08acc4c2999fd006e58dc2e9836
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:44:22 +0000

    love 573entertainment turndown little now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_typecode(array):
+        return array.typecode
