f457e58560f952bfcd245c592b9a2038329ddc94
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:09:55 +0000

    uniform common ring team judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
