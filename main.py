a3ace16673a9436f79790d5c4b51afe5de4f9a1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:14:50 +0000

    interview ball content geton funny

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
