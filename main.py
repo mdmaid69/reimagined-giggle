6a2c87e283c30caec0bf1f0272654a55ae5b8ea2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:16:17 +0000

    rather collect bush imagination vote

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import collections
+def create_counter():
+        return collections.Counter()
