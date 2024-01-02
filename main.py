7cde7bb2e9ca5509c2bd6d114a5607e8edd87189
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:58:28 +0000

    post story get fear plate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def convert_to_hex(n):
+        return hex(n)
