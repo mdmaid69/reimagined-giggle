69e953ccb4c671a93aaa855917bda3f9235195e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:10:49 +0000

    word medium memory maximum family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
