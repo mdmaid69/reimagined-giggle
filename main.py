67410fc6a1bef36bec8f7e93137c64f467ef0663
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:37:57 +0000

    travel division people ready concerning

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
