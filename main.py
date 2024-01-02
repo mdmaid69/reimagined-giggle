5362f278c46f1e6875c2c81d3cb049a91b370bd4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:44:45 +0000

    keen reason pretty worried convincing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
