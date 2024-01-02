6d4c31d4479aa0d735f48cb82da10a5eb030eeb9
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:25:42 +0000

    centimetre trouble whenever sight drawing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
