e27a3e0fae7ca88b0228d94d45b590c3af67a0fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:14:47 +0000

    common standard flow tread sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(numbers):
-        return min(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
