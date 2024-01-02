a39e7aeb462789637206dd9e55a90ecf36e6423d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:11:51 +0000

    569enjoy offend declare release unusual

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def create_thread(target):
         thread = threading.Thread(target=target)
         thread.start()
         return thread
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
