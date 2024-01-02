e735a62f16a5436bbd3412c28bb788ae27c7466a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:14:10 +0000

    goback(to) be evidence can mainly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_density(mass, volume):
+        return mass / volume
