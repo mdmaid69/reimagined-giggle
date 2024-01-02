82fcda372fb009df8a00dea7adb108bdb85d11a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:25:28 +0000

    rub quantity watch invite distant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-print(sys.version)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
