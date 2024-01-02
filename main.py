c8c7c3e3f8ec2a451602dfd734687388536b7eb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:03:04 +0000

    partner middle backwards colour sink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
