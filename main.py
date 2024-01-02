338e7519a8807786c5dbc2e3b4dbd66a403ce883
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:09:43 +0000

    near Uu deep form mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def subtract_numbers(x, y):
+        return x - y
