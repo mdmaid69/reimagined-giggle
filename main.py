62207d37d702383c28ce53c060091e428fab053f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:10:11 +0000

    stick operation size fall summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
