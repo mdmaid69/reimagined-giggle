94053a29cb15e9e88357d4f5dbb6fd4f16b4a8b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:23:15 +0000

    rest raw sentence runinto reject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_speed(distance, time):
+        return distance / time
