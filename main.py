8d03a60110f5a6fa4f46ebfc659df8f277a1609b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:54:22 +0000

    all holiday whether nurse try

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_power(work, time):
-        return work / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_bool(array):
+        return bool(array)
