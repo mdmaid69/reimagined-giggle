5bafa904678134d3de7aaf1692d1d7e6a007a659
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:27:43 +0000

    disappear effort new strange principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import collections
-def create_counter():
-        return collections.Counter()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
