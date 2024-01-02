6eec1515964bd4b8f3866049fbfa390d97abc9b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:47:19 +0000

    variety famous fix collection come

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
