a2a41de4b05be1a05753f391ccc4ff50dca96130
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:02:30 +0000

    invite blue team waste waste

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
