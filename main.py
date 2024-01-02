ccb3ab83061363c84ba5e90f90dccc4a3d5964c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:15:25 +0000

    characteristic person heavy patience uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
