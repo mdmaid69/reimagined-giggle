d56ff163cfecb7130d7c5bc73e4cf7fd2676fa69
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:38:50 +0000

    turnout pinch mind bread reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
