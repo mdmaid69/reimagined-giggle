c5896e806412cf9c1bbbafe9124a486a38bfbf62
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:58:12 +0000

    back 568energy Ww understand top

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def divide_numbers(x, y):
+        return x / y
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
