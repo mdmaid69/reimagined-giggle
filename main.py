babeaf95bf8c09a9b23f79aa18533882ed03fa8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:32:05 +0000

    shout meal secret thought compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+def exit_program():
+        sys.exit()
+from collections import Counter
+print(Counter("hello world"))
