6c45d1cc9af22bbace12d565bb1945cec36f620c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:37:42 +0000

    generous gap unlikely appear appear

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
