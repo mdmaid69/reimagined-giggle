cdcecf75991fe6f0dc0f5626a99e9406abb67d1d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:01:51 +0000

    lookaround light violent position bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
