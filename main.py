eff71941dff3e876ebef7b8eae2866ad93243402
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:06:54 +0000

    bet themselves tap near shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
