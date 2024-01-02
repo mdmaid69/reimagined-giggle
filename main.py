83571f34ce05d547af90503e80c5f1f43ff08411
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:28:10 +0000

    jump woman fix goback(to) remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
