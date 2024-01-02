60bd59319f52daa7b34feb7359303d3c4c497e83
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:20:03 +0000

    nature thanks getoff reasonable hair

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-for i in range(5):
-        print(i)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import sys
+print(sys.version)
