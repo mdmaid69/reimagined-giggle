44870ac16b6585116f635db535fe751795dd16b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:47:29 +0000

    add giveaway lend threaten music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print([x**2 for x in range(10)])
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
