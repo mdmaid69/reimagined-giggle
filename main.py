22be49bcbc853418ca4617115f11ebca41f62cdc
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:11:16 +0000

    appeal type answer afford information

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        return sys.version
