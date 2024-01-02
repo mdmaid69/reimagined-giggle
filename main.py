7912f0ead540dfd48daebc278fd219477fab76c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:24:32 +0000

    tragedy night unhappy unhappy swimming

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_max(numbers):
-        return max(numbers)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
