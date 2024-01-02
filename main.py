a1220ad97a14504e3bea1e8af1e6767177359519
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:13:03 +0000

    substance face order crash jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
