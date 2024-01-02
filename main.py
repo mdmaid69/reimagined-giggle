2776b43002834cb5879024eb1896db7521a9d3f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:42:43 +0000

    past doctor detail neither subject

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def add_numbers(a, b):
-        return a + b
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
