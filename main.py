e843ac478effa4cbaf1c3f37fab96a25004f897e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:34:12 +0000

    ground sick bridge city plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
