ee439ca7aeda586a585df171971cff9469049439
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:55:20 +0000

    lie left unless too chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-x = 10
-y = 20
-print("Sum:", x + y)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
