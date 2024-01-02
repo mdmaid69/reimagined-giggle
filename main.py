cf05a53f62c73d3825fe8ce934fe7710ff7a86fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:33:43 +0000

    runinto condition behave answer own

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
