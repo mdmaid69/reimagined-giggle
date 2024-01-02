f6d9595c1a5d688f9a7b5f4df963b68c8f615824
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:27:02 +0000

    advice keen wild wish party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def reverse_list(lst):
+        return lst[::-1]
