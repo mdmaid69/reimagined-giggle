6bfed62aa93bdd5783b52ebcd396b7d261835c80
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:43:12 +0000

    telephone bit answer still run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_sign(x):
         return math.copysign(1, x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_array_as_repr(array):
+        return repr(array)
