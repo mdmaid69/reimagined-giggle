56ec17be4270744158d3092260fb3703d70f73b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:00:54 +0000

    test impressed before success discovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def add_numbers(x, y):
+        return x + y
