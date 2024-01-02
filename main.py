1e9367900474f5206a450f66b61323e46d979888
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:31:52 +0000

    afterwards schedule ought challenge share

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def cube_number(x):
+        return x**3
