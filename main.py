821c5a80529c64d6bad29c6d4a05aeca75e6c692
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:56:18 +0000

    increase book this study false

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_area_circle(r):
+        return 3.14 * r**2
