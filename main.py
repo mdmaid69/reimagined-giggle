43f7b5910b4eea075ad3481708e631d28b09c5d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:24:00 +0000

    welcome way communication tie typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
