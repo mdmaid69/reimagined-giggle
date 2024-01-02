0e3ab8ff20ad1f0fcdad67f79c6be823cdbeec77
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:45:14 +0000

    wear basically last line concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
