ae01039bdba5d90af86daf27fd9e1b4b7afb2ce5
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:26:23 +0000

    grow silent school kind like

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def find_max(lst):
+        return max(lst)
