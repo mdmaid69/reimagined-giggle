6d69ea9cd33d167f99dedb01e50b7d7f56b14b74
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:48:53 +0000

    desperate direct sometimes clear difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import time
-print(time.time())
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
