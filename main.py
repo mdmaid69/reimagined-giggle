8dd21d47998b0f469a05005d1ef92cf6f4605fd7
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:31:23 +0000

    plate fight below false wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_index(array, item):
+        return array.index(item)
