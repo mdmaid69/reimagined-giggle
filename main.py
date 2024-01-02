9809e69211be4ad671689e2c0e24e25302e448c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:43:34 +0000

    fear fair mine match difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
