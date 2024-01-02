e30ae2f7171977c78277bbb79ba17ada8572e129
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:34:16 +0000

    570enjoyable destroy mention tongue scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
