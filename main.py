5aefb8d8e82b54c839768b7fd7146123a37c7db2
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:03:48 +0000

    claim voice crash famous nervous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
