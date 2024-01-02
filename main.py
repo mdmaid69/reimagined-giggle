37f1964a1c5c57340d9388f38cd1ff2956757f08
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:21:53 +0000

    sudden open stupid out getup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_area_rectangle(l, w):
+        return l * w
