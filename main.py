7dce62686fefd71325ded9ee3b3595f69ccca20c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:16:39 +0000

    wash blame everything runinto anyone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
