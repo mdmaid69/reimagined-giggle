f935a26c6182304e3740886d81ff79a6cba5c24f
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:07:39 +0000

    raise race female cope sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
