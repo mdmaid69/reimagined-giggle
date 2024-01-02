7268f74762e60f36ca304044700d445469cfaf58
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:46:00 +0000

    I muscle carry ability passage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
