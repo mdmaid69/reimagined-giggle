30331d3953f2bc2d188e53e31f261cb632a2bb05
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:38:14 +0000

    question wish year out prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
