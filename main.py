18d637b89a8f94a5f82388a05152409f0b1760bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:58:25 +0000

    surface come unit actual everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 def exit_program():
         sys.exit()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
