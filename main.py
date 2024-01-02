2dbf739abec7156241f01c4dd0271ad8c2cd5b68
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:55:55 +0000

    steer future treatment influence them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
