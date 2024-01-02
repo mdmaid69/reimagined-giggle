845e8236f93812fbdc1fe41dd4f5934ece819507
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:43:16 +0000

    concentration much expression debt juice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
