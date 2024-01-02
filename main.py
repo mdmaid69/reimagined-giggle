fff7f7346df0019964a95d7bc3d6a6ab50be4405
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:16:11 +0000

    flat unfair cross bringforward pretty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
