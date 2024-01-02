ba4743203ebc11ba0df1b1460a7dd344c0840f7f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:01:41 +0000

    brief concentrate opponent half oil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
