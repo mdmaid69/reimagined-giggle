a09c917e3a160bebb1329ad1de6d70bd0cfb6f6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:48:11 +0000

    offer autumn tread organize record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
