e214becb7146137256ccbbb1e2a9b4d65060bc0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:05:12 +0000

    boat criticize guess on thought

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
