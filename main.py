e9014cbc7c5311888b67f6930850ba527d0c7aec
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:27:02 +0000

    argue heat wave violent clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
