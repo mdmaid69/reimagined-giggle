742658e96ae9e26f0782c08f0a3bb6222500f77e
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:10:29 +0000

    show habit odd hard account

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
