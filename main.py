85721377d650b3dde26e9c969a7872fddd7a6752
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:28:18 +0000

    book main unkind come water

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
