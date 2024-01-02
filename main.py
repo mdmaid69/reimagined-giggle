49fcfe1f71b42c29000fdff31098186c5da7c1a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:03:41 +0000

    negative past become hate neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
