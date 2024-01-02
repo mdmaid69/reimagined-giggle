15421e64bf739be58a56958994941b46c1118a04
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:12:01 +0000

    store 179bite tread sing along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
