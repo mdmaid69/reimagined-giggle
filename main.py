b38ff069516ed7feba85dfc12c30784193f4c5f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:28:00 +0000

    watch part another find exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def is_even(n):
+        return n % 2 == 0
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
