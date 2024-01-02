43f412490469f243216dd1eef4f0d725eac986ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:05:23 +0000

    house slice fasten treatment place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
