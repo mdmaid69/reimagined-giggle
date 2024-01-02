af1ee917f14debbed65e3524c1a540f946d4ad08
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:29:32 +0000

    whole occur type closed each

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import sys
+  def get_python_version():
+        return sys.version
