14dffa41a9834fd56fb8e999dc38adacb0ea71f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:58:55 +0000

    pump factory direction discovery understand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def cube_number(x):
+        return x**3
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
