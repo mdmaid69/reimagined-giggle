a6227b5653ca77693ceaec146fd02bf7b504c797
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:36:17 +0000

    battery keen run someone meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
