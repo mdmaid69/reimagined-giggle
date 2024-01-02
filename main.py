742e0ad0ff9907127fffca168a694893e087c8b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:01:32 +0000

    mail clean able connection while

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
