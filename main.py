a2af125249b7625ae43ffd944d9605e043f376f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:07:03 +0000

    getoff they organization scene shine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def reverse_string(s):
+        return s[::-1]
+  def subtract_numbers(x, y):
+        return x - y
