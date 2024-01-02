b5c31201e5f35381c65535493aa570301597cb19
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:26:54 +0000

    hollow error wise lonely dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
