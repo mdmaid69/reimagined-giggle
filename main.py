a989b8aa354502d46c650584f4c29e1a4974d848
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:29:44 +0000

    example Mm hour swim sweet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def find_min(lst):
+        return min(lst)
