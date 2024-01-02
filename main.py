bda1f6eb4b1f093b2f551ea2940115f59ff73967
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:17:30 +0000

    fine bringup clue sometimes hour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def count_characters(sentence):
-        return len(sentence)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
