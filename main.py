33273926eeb6c2731127fb849ad156dbae4bc7e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:44:59 +0000

    still 565end another fill punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+x = 10
+y = 20
+print("Sum:", x + y)
