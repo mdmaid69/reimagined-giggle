dc49809953680c98832281f8dcc29e8f56596922
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:37:07 +0000

    accidental possible bath rescue appeal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bool(array):
+        return bool(array)
