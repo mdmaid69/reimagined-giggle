2a44a2373b6856eb67efc393bef3834a48b60028
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:29:59 +0000

    same set unfair empty Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
