bd1ddb301400d447ac25eb55e0e633167a49d1a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:24:43 +0000

    stare replace shade pause chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
