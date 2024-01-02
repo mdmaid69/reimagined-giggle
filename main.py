52fdd1bcf0339805916c9fe03dd4d671580ccd61
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:38:57 +0000

    equipment band use careless method

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
