07cb431f96dea8b78aba51fde0310b8937e31a7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:22:08 +0000

    dream land fat lift unite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
