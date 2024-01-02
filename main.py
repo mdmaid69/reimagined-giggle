a07d0c5ba831390834dee530dec3fbec024481de
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:30:21 +0000

    afterwards mark punishment about permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
