f1c663417e5f002ec81d25d310ca07b1cb179811
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:54:17 +0000

    about disagree border telephone opposite

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_power(work, time):
+        return work / time
