e8facb697f337765b40d86c13eca38285f184e8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:47:02 +0000

    rank knowledge trip comment beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print("Hello, world!")
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
