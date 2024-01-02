a4f4fbdd8a4077a8c96f1047dfd1f99c512cb8b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:26:27 +0000

    tread behind weigh mix tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
