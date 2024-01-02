aee5dd2c88a18e0369005663d0b7f5977f43a86e
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:56:47 +0000

    trousers empty emotion question reject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import datetime
+print(datetime.datetime.now())
