abf86fa272b637df4aa51243adc7057ecdb1892c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:51:50 +0000

    murder strong nearly public argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
