6de1486cdcdafe586d3356a57abd8b6716069f02
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:00:50 +0000

    especially unhappy any goon rate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
