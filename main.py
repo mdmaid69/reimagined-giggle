599c103634e456c7ee65a9bfffb50e4ab9aaa3bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:20:18 +0000

    keepout laugh butter Gg hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
