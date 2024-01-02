1f2af5775a680a5437f391b05e97e8bc68139b1e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:48:45 +0000

    worry wheel constantly concentration sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
