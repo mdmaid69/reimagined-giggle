3c60b6eb30ee5b84e4dce02c7c024daa8192d3a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:23:16 +0000

    apparently field escape shoot family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
