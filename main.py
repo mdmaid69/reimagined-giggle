410b3100745a42b52bba0fffab36216903d46653
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:06:53 +0000

    pronounce willing unit child unless

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import platform
-def get_python_version():
-        return platform.python_version()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
