65a2c99344cd90bf08755333a2f4c1168ee842d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:39:14 +0000

    lower sleep base Itself reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
