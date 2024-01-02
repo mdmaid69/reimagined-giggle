9c5a4972206ac97722d1eb82d68a439fa8f09cfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:34:02 +0000

    later letter learn live of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_pressure(force, area):
+        return force / area
