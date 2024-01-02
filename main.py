cd48771b75cefc4738bf5d98fde7a5d71fac884a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:57:03 +0000

    seriously country load keepout sure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import time
+def get_time_since_epoch():
+        return time.time()
