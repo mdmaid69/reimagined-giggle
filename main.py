076f6f7a3f02cf6fd5b8130255b6233cc9d922a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:38:47 +0000

    pity special absolutely pot game

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import json
+print(json.dumps({"name": "John", "age": 30}))
