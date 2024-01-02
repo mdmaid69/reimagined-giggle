1e17f252a022baacc510a2e8a5880770aaa6bffe
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:04:17 +0000

    tidy soft wheel top part

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
