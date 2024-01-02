eb99a2cd1ae3e1be9ebc7c0e616d500d7f00bdcd
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:18:54 +0000

    paper spot appeal Uu connect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
