727087028c4291fdcf8e985cea088d440ed30ce9
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:00:08 +0000

    steep beautiful Qq nearly long

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def get_array_as_set(array):
+        return set(array)
+text = "Hello, world!"
+print("Words:", len(text.split()))
