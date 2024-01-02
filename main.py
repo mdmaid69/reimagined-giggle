818eecb4ebe539232d0024c6ff1746781f69e933
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:06:47 +0000

    female sharp frequent average tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
