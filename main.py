1af9fdf7a5f0f2d5946ddadffba64f0673835fc3
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:02:34 +0000

    slight fair university emergency might

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
