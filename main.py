e67cc69e6b89a40f3d72e0396e05498c0157fd4d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:47:55 +0000

    talkoutof answer embarrassing grade blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
