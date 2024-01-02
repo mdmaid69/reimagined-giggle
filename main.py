0b8b4b37c71fde23c9007db0e8216f4db5edff93
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:00:26 +0000

    broken tonight count claim pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
