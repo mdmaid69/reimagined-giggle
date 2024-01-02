555722cc80a8d19dfe7b89c2470a4108ce4cedf6
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:52:02 +0000

    full actually 564encourage little patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bool(array):
+        return bool(array)
