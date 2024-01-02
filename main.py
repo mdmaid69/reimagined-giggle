ba4bcbe6b57e4fdcfadb41277c1680b3bf04481a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:23:41 +0000

    theirs file keepout machine rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
