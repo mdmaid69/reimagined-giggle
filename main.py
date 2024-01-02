0d091e1c3d698588423994554ac84da149177e9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:01:09 +0000

    shame exact last paper form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
