4eadb910b92ec6a65dc64cbc2476bfc948ebdfa6
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:23:45 +0000

    indeed Bb even pity government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
