c97a32da6816eb2330a61eaae74172e696cb8d78
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:50:23 +0000

    boat peaceful him suit development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
