f6d6222527345510a7223698c83672862d2b925a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:03:35 +0000

    train now ask degree comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
