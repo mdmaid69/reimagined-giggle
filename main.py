665903ba9ea260b3de6d1f5026f3590aa5788cb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:56:25 +0000

    fall laugh transport keepoff fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def add_numbers(x, y):
+        return x + y
