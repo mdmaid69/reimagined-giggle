706214d43984c03a2a25a75cfd54b8ed99806236
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:54:03 +0000

    design rough damage method port

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import sqlite3
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def reverse_list(lst):
+        return lst[::-1]
