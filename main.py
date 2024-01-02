d18a15e038ebbe13602115b60464135fa1b3ce9b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:19:52 +0000

    he land instrument recognize state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
