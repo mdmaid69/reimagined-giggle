e3e392e1037f80b76df453616989491a1cbea74d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:25:40 +0000

    trousers later survive convince recent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
