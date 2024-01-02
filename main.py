e87d477888b7dc2c5e6f71ff40b3b6f7839794a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:49:56 +0000

    home surprising smell week official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
