8d4e4924a3f7d8dd7dda2521f1c1252ba972f605
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:57:50 +0000

    guide lookup ashamed holdup meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
