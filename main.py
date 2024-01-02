7ebace4e503a631a92ea6d538aa1cbf0cfe0e1ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:59:50 +0000

    pause before recovery whether maximum

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
