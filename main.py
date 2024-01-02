0fc0b6c024c17f7f35c450398908daa4554d5f75
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:15:38 +0000

    proud meaning noise dig buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
