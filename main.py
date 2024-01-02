b95e92554e259bbf4f57d113fbbe2d478a66e1c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:24:20 +0000

    wealth hurt command yet square

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
