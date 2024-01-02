9e0e0d35db0ae341cb32d94d8e5fe8edd2d3d896
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:42:41 +0000

    place near comment Kk year

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
