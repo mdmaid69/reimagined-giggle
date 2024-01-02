411369f4165ca632588acb80903d6d58693ee379
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:54:26 +0000

    correct only underneath catch rank

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def sort_list(lst):
+        return sorted(lst)
