220cde8d73578ca6c07476ff1262ccdce7cc47e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:43:18 +0000

    prefer reserve laugh worst for

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_area(radius):
+        return 3.14 * radius * radius
