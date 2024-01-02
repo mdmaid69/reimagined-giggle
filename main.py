239aa9d8447472fadbe17bfa59bfa54a7d2771bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:31:44 +0000

    there sky luck race minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
