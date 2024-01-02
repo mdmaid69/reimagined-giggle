be46b0f366a67a47589e098e0396637248cfd9cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:01:43 +0000

    level write describe herself content

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(10): print(i)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
