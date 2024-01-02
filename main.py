7233d966644227ca32bacb419b383697450c98c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:09:38 +0000

    bridge unfair spoil cool deep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import datetime
+def get_today_date():
+        return datetime.date.today()
