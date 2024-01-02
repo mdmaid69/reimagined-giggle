8bae011729d46ac4a2c4ff87bc57f197351b5875
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:34:42 +0000

    healthy traffic opportunity dark dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_circle(r):
+        return 3.14 * r**2
