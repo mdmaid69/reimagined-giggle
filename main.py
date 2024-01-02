7f1b5beab367ea1736060da7315eccbac977b92f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:38:50 +0000

    game maximum lock comeup sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def clear_array(array):
+        array *= 0
