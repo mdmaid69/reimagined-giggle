0f02885f42aefa3ba48aedfdc2c49317338f2833
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:02:59 +0000

    artificial beneath recently vote slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
