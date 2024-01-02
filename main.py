fd8bb1f7a4d94b712ed7ddc72a9667aa4c9320b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:22:09 +0000

    steal ours little worst unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
