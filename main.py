e47e2644a4b913f6cfad772be27a7eafc4ba8a44
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:52:08 +0000

    honest risk upwards sensible lookafter

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
