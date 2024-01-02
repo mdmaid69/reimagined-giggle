b9da6668730a5717e0722a22ab9467ccf7df8b92
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:38:29 +0000

    formal lend fail injury flight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print("Hello, world!")
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
