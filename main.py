2f21dd2ada4ce3b022e5501bb9ead93703efb527
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:18:26 +0000

    572enter transport hers makeup lucky

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def sort_numbers(numbers):
+        return sorted(numbers)
