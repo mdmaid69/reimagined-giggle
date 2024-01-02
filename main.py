76444de7298d97b8903909cf1c92b8df3f291eff
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:18:54 +0000

    yesterday surprising tradition smile politics

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
