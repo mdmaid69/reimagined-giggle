60c3d23629b2ca86ee74f5609cc0aa25f89240e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:29:38 +0000

    destroy classroom strange action regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def subtract_numbers(x, y):
+        return x - y
