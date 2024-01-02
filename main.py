d5f909da70b6673382acb8c0d9aedf00891d4a28
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:10:51 +0000

    sail middle risk night occasional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def multiply_numbers(x, y):
+        return x * y
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
