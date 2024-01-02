451a767bdc31dc442e09368267cfbfe1b73377f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:52:03 +0000

    name paper whatever piece interruption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
