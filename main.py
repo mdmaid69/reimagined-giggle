26f78f2f7e8bb3d135f5c0b654fcadfe3006d723
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:24:16 +0000

    object principle fasten favour move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
