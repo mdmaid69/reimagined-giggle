bbd138777e0f16ba454d5fedcdbd2f53188aa907
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:47:54 +0000

    actual hide develop someone trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
