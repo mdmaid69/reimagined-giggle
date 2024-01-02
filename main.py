af83c15f9d92331882e249defdd4437943df929e
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:40:42 +0000

    birthday grow return knowledge importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
