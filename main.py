994f66ec51fa3d3dc566089f77169d8e0a0fb64f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:00:27 +0000

    turnback thick turn smoke agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
