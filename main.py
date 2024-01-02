cfb0b2b6ae32f12030c0c46ba07e1bea18d3a700
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:23:36 +0000

    above either mark object book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def sort_list(lst):
+        return sorted(lst)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
