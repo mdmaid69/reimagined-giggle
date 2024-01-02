7c2d4289a24d3bf2588c09085a37e78de5d374cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:00:22 +0000

    negative count for success come

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
