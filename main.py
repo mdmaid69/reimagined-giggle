95f0460b46de86aec81313ff3f56077a37f13dc8
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:52:53 +0000

    refer will turnup bring winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_item(array, i):
-        return array[i]
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def get_array_buffer_info(array):
+        return array.buffer_info()
