450580a53c6d2f71d1bcffdf2a0087bbd60baff7
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:41:15 +0000

    related type position favour distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
