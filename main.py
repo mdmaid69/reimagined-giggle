c1838f436781b4e293edf830001055b0cb192880
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:25:07 +0000

    mixture tool chance downstairs round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
