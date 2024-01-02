7320508b44af98ae12f88c2ebbc59e25571d90e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:40 +0000

    instead shade record loan awake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
