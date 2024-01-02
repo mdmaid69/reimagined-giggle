c43c90bc93d8f9c5c320dc3f0d96980e2f956956
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:00:24 +0000

    available obvious post building organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
