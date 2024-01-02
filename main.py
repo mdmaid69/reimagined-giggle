dcf8faa80bfb6738ffe69adde1cc9a52ee624f4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:01:12 +0000

    glad arm afterwards decision clean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_length(array):
+        return len(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
