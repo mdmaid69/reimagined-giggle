3cd76dc3bb331c20489d31dea207c602831eac4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:34:15 +0000

    relief travel shrink improve period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
