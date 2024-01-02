ba6dd0fa3363e026e0be48f23a273bf3a12aee5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:00:55 +0000

    rest pot clue success skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
