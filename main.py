90b7247dbce1b6b08dc688c909ba3ace179d2eb8
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:54:51 +0000

    shelter we work but describe

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
