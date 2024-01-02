61c6b2b063137d8b2641ae3b7aaa33fdf1d1e388
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:14 +0000

    proper marry Ss be unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
