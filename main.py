e8f1c0967930d82d9e15d467b15d56f6fbbb58e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:19:25 +0000

    balance story central load trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def is_even(n):
+        return n % 2 == 0
