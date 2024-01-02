70059092e14695c0946336d2005fca1002ea8b6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:33:49 +0000

    unit foreign effort makeup hide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
