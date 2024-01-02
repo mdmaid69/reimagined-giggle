38dfd2dbb6d79c4f66e051fabfebd9715a7cf94e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:34:41 +0000

    aware freeze air point band

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
