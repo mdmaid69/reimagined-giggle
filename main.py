31608c02c07e103e067ede9e69d14110a0e123b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:06:10 +0000

    from play reserve purpose coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_memoryview(array):
+        return memoryview(array)
