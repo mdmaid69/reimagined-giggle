26b6b95b3810233bd59533cdb379ac3152d2ccc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:00:39 +0000

    unit combination excuse dear dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_even(n):
+        return n % 2 == 0
