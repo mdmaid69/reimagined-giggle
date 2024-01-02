a2cc57f02e04526088c717d49487f119cb877ae8
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:28:37 +0000

    work might passenger below unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-x = 10
-y = 20
-print("Sum:", x + y)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
