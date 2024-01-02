ea76580144e26169ed2b22f7ff8455ff1195414a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:11:52 +0000

    delicate deserve plant yesterday workout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
