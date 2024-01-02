f7a60a9ee8c280f96a780f54c6051ba3db5a7202
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:17:02 +0000

    increase estimate certain borrow reliable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
