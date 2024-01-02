8e4f675799320f7535d1dfe790a47563a6007642
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:12:56 +0000

    confuse cover soft fear heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
