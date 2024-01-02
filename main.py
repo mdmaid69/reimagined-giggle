f3a2b2d1b297703818b6de9271e0d6b7cb3e806a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:27:45 +0000

    quarter belong light think comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
