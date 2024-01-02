a68f331c0ab6752d3a8cf12ab2a5a968ec5be6f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:15:43 +0000

    herself watch should sure half

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
