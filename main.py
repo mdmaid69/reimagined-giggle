051408864e1d3c4dc1e54d79b7bb075930e10583
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:12:58 +0000

    Kk stupid book which type

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_str(array):
+        return str(array)
