24bac08b758b1f27f124df755112ceb57326823b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:12:54 +0000

    march destroy consult knock broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
