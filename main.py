cc57fe2e231d2a49204676a3f05dc89e1d6263c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:37:28 +0000

    would first little popular schedule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
