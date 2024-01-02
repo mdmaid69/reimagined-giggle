87a4bc8f3a74170182043590f366ffb2f57d1bd8
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:41:27 +0000

    about congratulate thinkof final star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
