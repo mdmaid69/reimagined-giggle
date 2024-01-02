4f28e5b158dbf27ce5582f1f74c60abf5735f7fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:08:29 +0000

    breakfast dull dress pot grade

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def convert_string_to_array(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
