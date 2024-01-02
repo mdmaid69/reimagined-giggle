f83d0a9b693842a913b55d681f4af2403daa97d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:36:17 +0000

    view within loan away hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def reverse_string(s):
-        return s[::-1]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
