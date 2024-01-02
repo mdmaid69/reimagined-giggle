9edd496104f82a953cd958c7601842a96cb5d58e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:06:15 +0000

    major morning branch land interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_list(array):
+        return list(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
