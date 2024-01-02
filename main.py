e7ad936f1dc900be57af784a9422bcbd05d77269
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:26:17 +0000

    walk beginning add keepfrom destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
