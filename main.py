cf8694d9193163073ec33bee047353b7ea500afd
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:10:27 +0000

    public rub them start offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
