7e9a6ad1d6e7bd33635003aa3b5c8a459d7122c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:41:31 +0000

    honest operation stop tooth take

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
