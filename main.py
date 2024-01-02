3f5cda93b3a171764d977e208919bca8bb4fa505
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:35:54 +0000

    dance deep whether second more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+from collections import Counter
+print(Counter("hello world"))
