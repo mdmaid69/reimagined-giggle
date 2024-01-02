4bb67ff48d01d124b8767684bb1d93a56bd664cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:11:13 +0000

    fine long block illness door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
