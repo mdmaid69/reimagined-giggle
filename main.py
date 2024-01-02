f658524af3ee1a1a81ded048c1a62dd68f35209e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:14:39 +0000

    comeover block temper release mystery

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
