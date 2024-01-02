369bb4fbdfafb1f3dbe6b5fb54400a3ff9476862
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:56:34 +0000

    return imagination comeout Ii thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
