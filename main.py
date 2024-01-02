14b7be4cefe39411e3ba298430c2932c1716917d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:07:52 +0000

    sweet mainly OKorokay shadow congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def check_if_array_contains_item(array, item):
+        return item in array
