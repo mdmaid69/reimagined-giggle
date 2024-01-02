3bd5808ea649242226762849ef11083c97242f1d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:18:30 +0000

    dance noise Ll instead if

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
