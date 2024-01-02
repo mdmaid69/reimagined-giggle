ee0ec8e9fcb73a057d954ed0eea95828b567d519
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:20:51 +0000

    copy get business holdon complete

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
