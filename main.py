5ce207f8813b1517312f2f862b90fa8f6db3c8de
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:52:25 +0000

    pleasure provide form square by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
