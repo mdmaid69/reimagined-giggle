7acc05ac98221dcc5be2e1b7840941951bc9eb99
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:35 +0000

    motion effort getup hide will

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
