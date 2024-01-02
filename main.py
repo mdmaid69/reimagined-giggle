205f20af725d093618ea9673e610f6ac133f62ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:35:16 +0000

    emphasize he like spare total

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def clear_array(array):
+        array *= 0
