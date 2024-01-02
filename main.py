6b7ca7fba6a0f075856d0f406ec8b9d1949c5ff7
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:34:44 +0000

    there grand light reliable exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
