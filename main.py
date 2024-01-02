4046f92d2d0c8e5ba939be880234634e975e9481
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:18:26 +0000

    somewhere carry minimum Ll legal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+print(sum(range(10)))
