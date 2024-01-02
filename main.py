0f7e2f66a3d233f105768dc888a0096ab7bc626d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:47:44 +0000

    used closed takeout wave conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
