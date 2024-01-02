850d79fa9a2ad9d9e493b9311b47528c77a08bee
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:22:54 +0000

    hear Cc second young separate

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
