87111f889899580b209e5afc4b4eab2b0b601a87
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:57:02 +0000

    through turndown issue comein never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
