630742389c82f274467149d1eacc7f1aa0956898
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:06:25 +0000

    except region breakdown deaf little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
