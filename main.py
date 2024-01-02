620fba60a6a2256cbcb8d88ddd7f7e5e7bddd85f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:38:41 +0000

    miss worth string spin past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
