1a4dc7682ec3997ffab3e9da83accdd1ccb3746f
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:57:59 +0000

    properly turnback fond bother Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
