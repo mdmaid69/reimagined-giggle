564bba31685a18fd09e9deb319ae6a2a3e83654b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:09:41 +0000

    favourite they false chin strike

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
