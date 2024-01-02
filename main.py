8c5e62f375dd5618d1c211eca0fc50dd4079dfe3
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:23:36 +0000

    friend stranger round value state

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import getpass
-def get_username():
-        return getpass.getuser()
+  def square_number(x):
+        return x**2
+def calculate_work(force, distance):
+        return force * distance
