309cc753c17512f0813224ed3e398cbce56a98e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:09:14 +0000

    anyone us pure fetch tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
