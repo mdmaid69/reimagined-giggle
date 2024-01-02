8e0154a9ad27cb9d6a001a8c9daf648a3a552d59
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:06:08 +0000

    destroy group unhappy female team

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
