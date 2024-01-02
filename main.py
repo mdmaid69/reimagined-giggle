337123fc6cebce33d6aa42e0e238f537578879e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:27:48 +0000

    accidentally immediately neither field round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
