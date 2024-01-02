3f0c482a218b5e0c48ae44c4ce6491977bd14040
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:13:05 +0000

    muscle development cruelty sing continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
