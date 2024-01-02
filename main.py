1285f8863c9bfdd0d7105610c26006553461206a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:11:25 +0000

    warning drop breath far frighten

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
