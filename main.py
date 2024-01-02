6bf54aae99cebdf9537ed07d55b7b7bca202884b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:04:32 +0000

    precise cinema brave thing roll

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
