82eef45eebcc093fa6e0d624d815da569d89ee3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:14:26 +0000

    them fly represent attach form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
