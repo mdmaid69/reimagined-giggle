9973ed95a2af288630c7944d6808806f0081aa67
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:15:27 +0000

    old pretty morning gas another

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
