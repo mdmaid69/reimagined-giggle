a0e2066bb3a5cf560a3e20e82bcb52003088e316
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:53:26 +0000

    club personal mark please fine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
