25890487a8c249e2e2c9ea8dc8e5bda07b354bbc
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:20:49 +0000

    moment satisfactory try dish break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
