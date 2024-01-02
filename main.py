966311a691214ecf3d36bcb0354b132b67ae505a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:07:38 +0000

    keepoff mystery available development everything

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
