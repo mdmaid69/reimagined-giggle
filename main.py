240438bc31a804bfbcbeafd66161e36e610f5f59
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:24:57 +0000

    perfect address delight typical solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
