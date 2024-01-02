a8a3db21daf7725aa659cb3d50b0e9395f5c2114
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:50:00 +0000

    breakthrough improve war Itself flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
