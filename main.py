94b0b54fb1d9bb34700a56d1544003df42219721
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:30 +0000

    cope suffer equal shout invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
