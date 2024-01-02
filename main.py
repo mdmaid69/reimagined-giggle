9000b5da314dce354c130eeca6bbec4a163594d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:28:07 +0000

    congratulations shall introduction serve communicate

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
