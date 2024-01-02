61cbcdf109169709fa9fca2d9f12ab85a7a9e4c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:55:29 +0000

    empty remember weekend damage push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import sys
+def print_python_version():
+        print(sys.version)
