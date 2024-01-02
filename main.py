d75268196a27339a94a894a8cbf391c91d3177cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:16:20 +0000

    shoot keep sail taste hurt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
