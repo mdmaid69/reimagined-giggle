f235bd5651f54a6b6f07cc1f45846a23a2758c88
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:49:53 +0000

    firm television roll afraid experience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
