b45b3788529fdf3f15331f6b51c46a8b7ad2455d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:24:04 +0000

    consult none some pump emotion

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
