dbca099f3124d2b32fd761b79856cfb561babd41
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:03:20 +0000

    improvement Gg welcome pupil report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
