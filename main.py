bd31f47f61bd9a638be605f96d23e75db81a86be
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:46:10 +0000

    shout suck plant conversation swing

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def add_numbers(a, b):
-        return a + b
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
