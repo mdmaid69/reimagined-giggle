8a77b73107f01794d88b9b6e8dd200799f1808f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:43 +0000

    base tooth lost warn rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import sys
+print(sys.version)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
