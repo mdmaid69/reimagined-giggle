46cd2042f054b44a55d06d5adc1fa0cf2281ce5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:31:53 +0000

    again female force OKorokay loose

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_item(array, i):
+        return array[i]
