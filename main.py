505d5a0a6518ca131db2d900ec2b8852568ffda3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:02:47 +0000

    pure tone also outside crash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
