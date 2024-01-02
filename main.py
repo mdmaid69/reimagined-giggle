bb9ff51295e48c31aa00f3d10f35ca11496f7b7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:31:56 +0000

    indeed cruelty thick benefit field

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
