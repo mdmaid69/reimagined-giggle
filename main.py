90ea30b3e03b7f0269625337af70eb2effa06711
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:59:59 +0000

    moon system limit guess less

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
