3dec8ad8f02521add627467c04d32596b8a6e1d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:45:56 +0000

    receive fire admire physical notice

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
