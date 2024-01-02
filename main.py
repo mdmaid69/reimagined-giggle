54b350f86517fce49c8dce1873cb932deb35059d
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:37:44 +0000

    clean month attention fairly storm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
