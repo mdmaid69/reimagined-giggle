6f6318dd972ff4e2c5ab950387d140199af95c19
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:15:08 +0000

    bored possibility concentration variety approval

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
