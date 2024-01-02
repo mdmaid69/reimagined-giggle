7990ea2f9a7842908ae0fb0ae02809eb37b34ab1
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:56:30 +0000

    indeed rise fill state pool

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
