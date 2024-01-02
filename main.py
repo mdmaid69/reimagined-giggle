9f287194b5084e567856d45bad7f332ae012fd81
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:49:36 +0000

    goup getup convincing absolutely base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
