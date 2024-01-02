1f1718d93c2168e2c7a1d2e042c1f2212f2a055d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:23 +0000

    anybody influence freedom forgive about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
