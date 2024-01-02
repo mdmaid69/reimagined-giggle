a6cf98507d9b64845bc32ed85a1ea558b4260933
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:56:43 +0000

    mind accept yours sign Dd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def cube_number(x):
-        return x**3
+def get_string_from_array(array):
+        return array.tobytes()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
