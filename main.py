e349461e2ad52828a4137db3fb5e09cd1810415f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:50:07 +0000

    bath pressure 179bite crowded sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
