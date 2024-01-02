1af53ab3920d8e8a7006d578c94dc711662ef3f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:24:45 +0000

    polite similar Ii heavy mainly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
