4aa45e1e4df0812f619da0c9094374dbe906c846
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:27:06 +0000

    put below fun instruction profit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def add_numbers(x, y):
+        return x + y
