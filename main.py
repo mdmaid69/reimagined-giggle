a0dd655728ecbf55981fb9884418437ce65d3033
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:02:27 +0000

    forever beyond exactly danger along

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
