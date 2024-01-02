1fe1a2271d41a8cd9a0e805e09c8e994878bfdf8
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:26:47 +0000

    introduce patient hardly and exciting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def remove_duplicates(lst):
+        return list(set(lst))
