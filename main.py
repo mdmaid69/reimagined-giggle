33f0331acf3f8a3ef5fdf8b466972638ba3376e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:22:26 +0000

    couple discovery neck pleasant language

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
