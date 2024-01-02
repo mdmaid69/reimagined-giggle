fe0fd02f469c945fdecdd5713f7f77e1421b5b87
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:12:26 +0000

    573entertainment concerning fat tonight television

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def change_working_directory(path):
+        os.chdir(path)
