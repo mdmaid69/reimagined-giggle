c39ee6dfb6547172e799f33d4f5ed707c25a229a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:29:38 +0000

    suppose twist load base since

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_area_circle(r):
+        return 3.14 * r**2
