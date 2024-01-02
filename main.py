6c56802b56459b92b889747e7a1bb7a720b78020
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:10 +0000

    shock safe send hungry drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import time
+print(time.time())
+import array
+def get_array_as_bool(array):
+        return bool(array)
