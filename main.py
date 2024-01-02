9976b6254e6c5a20a996f88702cc58ff507e9b01
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:29:58 +0000

    partner party whether satisfied possibly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+for i in range(5):
+        print(i)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
