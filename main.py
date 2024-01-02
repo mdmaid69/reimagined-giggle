11559ac30628efcbcf932967706e1ca7970bde0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:18:34 +0000

    control hear lift night calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import random
+print(random.randint(0, 100))
