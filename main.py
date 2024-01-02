b3e32873877476eb3c677a99ccdb463f5f804377
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:13:19 +0000

    match smart dick value breakfast

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-for i in range(5):
+i = 0
+while i < 5:
         print(i)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+        i += 1
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
