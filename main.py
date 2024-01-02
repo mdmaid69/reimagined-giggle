d0cc3661d50ac9d22a75fa558e676f0ad5265002
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:55:40 +0000

    house intelligent contain downstairs large

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+print(sum(range(10)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
