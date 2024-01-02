29a226d0caa06bbb5f6c23c00e02e6c155597e89
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:15:32 +0000

    comeround book appearance colour live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
