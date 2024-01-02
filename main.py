685cf6ef7defe8aeb85872082aae0c03df92773e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:13:25 +0000

    hit must concentrate ago damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
