062dd2262166260605cfd55f6888ca4680e795c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:10:45 +0000

    Tt balance swing tone much

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
