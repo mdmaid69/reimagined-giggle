3b0da465510e28576ccf921674bdbac29f625d4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:19 +0000

    cruelty promise influence maybe treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_max(lst):
-        return max(lst)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
