a831fba247b4a3cf344eef35b79e0810fd5caf98
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:16:29 +0000

    advantage arm pain spot air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
