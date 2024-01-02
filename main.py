57d9e2e2f2b4c3d33f0d0fd70106449a5b573e43
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:13:09 +0000

    warning course hope sensible anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
