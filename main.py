efae260060189cfb0df98b3aa8910e31b4c0742c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:02:11 +0000

    foreign strike clearly Vv surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
