10488a149d2c8a630af6b26002a8dddd25aad9c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:57:17 +0000

    push team square kind anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def is_odd(n):
-        return n % 2 != 0
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
