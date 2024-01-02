a1090846e1dd98f605050f5a2a584a4554a780f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:11:25 +0000

    matter arrange nervous guess basic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import sys
-print(sys.version)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
