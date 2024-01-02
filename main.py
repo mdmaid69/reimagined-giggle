1e65cd14f6e0a661b237e3b73707c37cebca638c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:10:55 +0000

    corner attract tragedy bear sharp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def divide_numbers(x, y):
+        return x / y
