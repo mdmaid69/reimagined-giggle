d491773848ad6529c2f806b4be9248325551e32a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:04:17 +0000

    reduction tired raw extraordinary someone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
