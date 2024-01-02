9b8376fe0a897c612256f6e71ba6039f3eb427a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:59:31 +0000

    unusual course immediate and turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
