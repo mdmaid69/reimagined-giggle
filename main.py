11f94de5b71d92724db1ce168236b9b950ebf880
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:01:32 +0000

    callout poor partner breathe invitation

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
