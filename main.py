c1d54594a51946947f4a6d0acd15390b11d76f58
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:43:24 +0000

    up hour combine concern high

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import random
-print(random.randint(0, 100))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
