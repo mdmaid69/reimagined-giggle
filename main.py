26c2b48cd5adf6d6e1c0da8f08d93cb4bd6e1046
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:09:13 +0000

    drink promise cause keepaway constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
