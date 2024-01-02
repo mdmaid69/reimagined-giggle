61087482a48062e88575f86af3c1e74149de4869
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:24:39 +0000

    experiment show apply aim female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(a, b):
+        return a + b
