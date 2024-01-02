bc16c37ed6b76f105deb5594bc81cd2c64071671
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:49:15 +0000

    limit tune father guess shopping

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
