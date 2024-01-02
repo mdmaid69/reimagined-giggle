f79c67e50262a523a9c5f0bc5e2d92bf352df5b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:03:59 +0000

    perform while sweep within their

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
