e01583f911f4e0bebb1eb232a609d435d6683a05
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:54:22 +0000

    correct meanwhile match trick concerned

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
