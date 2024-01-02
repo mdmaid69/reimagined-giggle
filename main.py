f769a9b05b09c462687b0e74e824c122bdc78f6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:44:43 +0000

    across light especially spray turnup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
