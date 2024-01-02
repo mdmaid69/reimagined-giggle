637dc4a35d7504120754ae19e32edb84cd506096
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:12:56 +0000

    getoff anything proper cheek mainly

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
