fcc4835c7d2ec0a94fe6640c4d4ed0128ad47376
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:05:44 +0000

    neither song breakin exit soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
