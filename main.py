46c873d3fcdbf568b84ef1f862e17b14a4eb5efa
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:39:27 +0000

    until education usually summer one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
