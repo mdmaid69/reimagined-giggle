6268a1aeb3175a856f79232cf7a7c0fbecc193a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:31:18 +0000

    steady Gg artificial bother account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
