fcad8d5910932363c9d577ea86cf332364972cea
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:22:03 +0000

    mean permission pain ashamed new

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-print([x**2 for x in range(10)])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_typecode(array):
+        return array.typecode
