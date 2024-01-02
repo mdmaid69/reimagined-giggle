062b10f3fe85e58e461632470fd7700576b40cb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:16:24 +0000

    cap direct point system major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
