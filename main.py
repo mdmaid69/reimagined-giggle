e9e32efd08bb636cc16c97af87090f5337169a10
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:22:12 +0000

    region rise poor friend hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
