6ca68515a2b2f68445110807d14bb2c3c9494482
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:33:27 +0000

    train maximum urgent upwards check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_repr(array):
+        return repr(array)
