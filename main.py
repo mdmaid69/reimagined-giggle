d9b94decce120a83c32d575f2115693f0d1afad6
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:26:09 +0000

    drink next flow switch spread

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
