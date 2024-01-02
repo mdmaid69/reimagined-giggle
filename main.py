bd488c800904a8f63ebcff32f7ba54169fba39ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:34:16 +0000

    change remove legal sound amount

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+print([x**2 for x in range(10)])
