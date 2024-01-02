8f6bd7cb3c1ed1803a8f40eac808d39bf0dc1acc
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:16:12 +0000

    steep perhaps concern battery stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_item(array, i):
+        return array[i]
