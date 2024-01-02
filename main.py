d828c7d700f4a91679e1a679bcdafd289bcc0278
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:08:28 +0000

    habit impatient cross out area

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
