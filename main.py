e7ec1f1769c0b8603bdb11e2e4072a8c47abdaab
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:42:33 +0000

    fork recover joke geton away

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
