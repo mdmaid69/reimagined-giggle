6d8ecd8e3ea1e89d41c871cc760c2749f0fe0fe9
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:03:06 +0000

    university age realize giveup ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import collections
-def create_user_list():
-        return collections.UserList()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
