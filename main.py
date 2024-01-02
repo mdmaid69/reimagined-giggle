7be1feaa4dc4f634f033f61699f2ab96ea9d05aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:00:59 +0000

    anywayanyhow happen succeed habit outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
