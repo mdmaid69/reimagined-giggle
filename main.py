cd57b6c088a04a8181ac3c5371a02e332e3ecdea
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:46:35 +0000

    false reduce though characteristic giveup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+for i in range(10): print(i)
