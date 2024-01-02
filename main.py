63f9810e852fb98e4410cc0655dc5bd9099ed547
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:28:22 +0000

    piece activity head amount cinema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
