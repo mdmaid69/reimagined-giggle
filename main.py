3dc1c135be9d26826ffac66373e1f896424e6858
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:41:02 +0000

    skirt crazy sticky naturally bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def remove_duplicates(lst):
+        return list(set(lst))
