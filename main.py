b03ca3c529ab6ca9883fa5a42ac746060f01630d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:51:20 +0000

    interesting trial tear fail instead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
