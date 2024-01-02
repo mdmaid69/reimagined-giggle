ee3d79de6c08e97e2acc6cf6be3addb37101e54e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:20:13 +0000

    hope comment consist touch discover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
   def calculate_area_rectangle(l, w):
         return l * w
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
