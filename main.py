a9557cbe58f6a378a9c88bbe582d65c6efb9f863
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:25:33 +0000

    petrol Qq waste look clear

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
