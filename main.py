72cc4b6de39cd29395eaa866c5362bd98e2a9781
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:27:40 +0000

    heat stop spin nigga artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
