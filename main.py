37ca2c5ca1206f201ea6d1df860ae6b04c0209d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:15:14 +0000

    imagination like thorough ago so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
