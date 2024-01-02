38be21a79b51b70d36bd3b6e94e30e5426b1d85a
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:32:16 +0000

    narrow unpleasant politics be still

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
