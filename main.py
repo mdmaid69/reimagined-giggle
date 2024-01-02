b2aa18c5650f7586cabe7153e8be7b2837b87541
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:30:04 +0000

    at character region law give

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-print(re.match("h.*o", "hello world"))
+  def is_odd(n):
+        return n % 2 != 0
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
