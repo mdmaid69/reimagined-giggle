08e9fbeac31ef45bb9671c319acd3986908d8b47
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:51:23 +0000

    entry act remove undo concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
