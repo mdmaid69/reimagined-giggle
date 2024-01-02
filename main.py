052fa2ff225245400ece5e8da1dbc926271b735e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:04:11 +0000

    holdon method show cut congratulate

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
