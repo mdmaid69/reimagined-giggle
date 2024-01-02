6972a3a300d8b214889bec3fcb73c4cf2b7d40a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:11:33 +0000

    promise round train mark hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
