d51b5353e3a4cd463ab59e92c233c7a8f18b4926
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:39:33 +0000

    from anywayanyhow glass grade book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
