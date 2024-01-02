fd46e4e44f325f68d505daa3b63ee5702fa9ff6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:47:59 +0000

    cause bread sensitive height protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
