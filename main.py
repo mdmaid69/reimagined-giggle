6320f33e3ae65377d87b0b8ae6756fd83de9fd65
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:26:52 +0000

    tie character gift visit share

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
