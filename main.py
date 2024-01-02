aa51519729f20c3922244f1f52b82316c20525fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:48:16 +0000

    nose turn guide ocean thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
