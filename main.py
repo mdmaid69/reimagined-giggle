b9b7e4e2914b17db8e1bff66003498ab44f5d078
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:06:13 +0000

    mad delight compare smile continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
