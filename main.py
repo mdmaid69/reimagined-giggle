8ac864de81680f0b0a1a339e3356fb7c5dbcfee2
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:55:00 +0000

    discussion by if conduct score

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
