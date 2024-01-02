6e0f27aad63e35e18519336e522b01a9ea66dd3c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:57:06 +0000

    although depend Yy correct unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
