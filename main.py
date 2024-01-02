896ea0b3f10a1b58875c494079363cee6cb2509f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:43:25 +0000

    detail suckme mind physical square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_ceiling(x):
+        return math.ceil(x)
