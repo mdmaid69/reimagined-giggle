f49b11fe7b8f5944f29ac490f3b171885baded75
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:00:02 +0000

    trip fasten myself successful deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
 import re
 def find_pattern(pattern, string):
         return re.findall(pattern, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
