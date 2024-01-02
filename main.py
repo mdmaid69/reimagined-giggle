d871ddfc8e4db2ee2a7d4873cd0e149c1a05676f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:18:02 +0000

    song post no delicate debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
