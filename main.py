66503fec0643327aac0c54ea5372c8c7acce462e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:25:13 +0000

    everyone pencil centre tap arrival

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import getpass
+def get_username():
+        return getpass.getuser()
