efd1e16488a7047edec0a2d6daa169881f5e6930
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:05:09 +0000

    accommodation debt recovery joke free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def count_elements(lst):
+        return len(lst)
