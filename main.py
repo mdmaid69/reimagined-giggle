782b3b4edc691a2f5e7e30163bd99b678d15d820
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:52:25 +0000

    public talk mess whether light

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def add_numbers(x, y):
-        return x + y
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
