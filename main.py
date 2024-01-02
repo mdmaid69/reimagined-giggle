0f6d7cfb2ab4c87dd8297bce827b4c8a80032154
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:22:36 +0000

    patient generous dear unfortunate copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_directory_name(path):
+        return os.path.dirname(path)
