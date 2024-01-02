fddecc6bceefe862a46a5e0cbe4135e75d5aeaee
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:40:40 +0000

    pronunciation rank left exist pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_array_item(array, i):
+        return array[i]
