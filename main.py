e5284d5a66413eeb02e5588ded917f573030e830
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:53:05 +0000

    juice favourite furniture interview sail

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
