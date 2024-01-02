51f8b3c1ea497430920d7887ddc7353f411839a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:31:24 +0000

    fork flood separate book tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
