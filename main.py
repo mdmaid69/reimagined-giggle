7d3a05733d0ab9a83a9a57f369d881560c992798
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:43:37 +0000

    impressed murder high believe imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
