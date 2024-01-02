217b2e0b2b3c24231eec72836e85ae3e86df2669
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:59:46 +0000

    breakout right Hh rush Nn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
