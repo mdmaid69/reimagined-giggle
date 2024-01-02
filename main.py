02bbb207d88e1a1cf8db76da8b16c2a56be24f37
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:39:44 +0000

    basically become some store cross

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import logging
+def log_message(message):
+        logging.info(message)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
