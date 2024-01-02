bdf5cd41291f3fbccd79d770700b6b1512e8a732
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:42:13 +0000

    mystery thick tragedy listenfor turndown

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-import sys
-print(sys.version)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
