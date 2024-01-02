c60f529906cb5c7dbf061fe34c3aab26449e8a57
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:04:11 +0000

    find positive declaration getaway reply

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
