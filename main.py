7ca5734ba8b127b53fb76fc2ac428ce01479eea4
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:06 +0000

    race pressure flavour 568energy ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
