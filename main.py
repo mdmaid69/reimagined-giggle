59e17221c99167a37a7a310052144283b3a5e440
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:22:42 +0000

    band 566end dead possible health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def find_min(lst):
+        return min(lst)
