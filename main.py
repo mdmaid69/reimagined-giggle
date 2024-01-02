835db1b48c2dd252a92e06597e7416d132425e67
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:48:30 +0000

    maybe lookback(on) will record occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
