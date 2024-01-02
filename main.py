b46f3a79f84cbee70cf34b0764d68018fdcedac8
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:50:37 +0000

    hang label dance set autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
   def delete_file(file_name):
         os.remove(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
