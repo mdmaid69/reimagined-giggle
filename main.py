f555eb57920c30fd5a5c90ec5d58c38fbe49bbf0
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:58:24 +0000

    crazy handle arrangement thanks window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def multiply_numbers(x, y):
+        return x * y
