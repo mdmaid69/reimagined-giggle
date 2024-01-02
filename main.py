91353a88ab0d9cd45e9ae62286b9c3d8ea959578
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:39:35 +0000

    passage private smart mistake insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
