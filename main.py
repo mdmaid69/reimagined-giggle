fb69b2e1acaa2d17018bcee43e9045ad5dcfe60c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:52:53 +0000

    moral climb her tune popular

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
