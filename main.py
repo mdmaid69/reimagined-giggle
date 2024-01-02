87d70fa248971bc103cda2403ef06f6ccfc00408
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:48:20 +0000

    clearly knock tune surprised piece

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
