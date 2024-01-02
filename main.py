e1c468ff7a94e5c146ef794f2b8caeee8db73153
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:36:54 +0000

    scrape record me release top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_error_function(x):
+        return math.erf(x)
