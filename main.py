b7fe373559bf0b00ed7ff2236bd56cc67fc851f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:02:46 +0000

    handle use smile allow hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def is_odd(n):
-        return n % 2 != 0
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
