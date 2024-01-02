edd6bc54012c1fce78d03c3e828591cc3be74880
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:37:00 +0000

    official sensitive unkind into region

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
