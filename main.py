0a824b4d404100d006b0fe93c0c0257f3fef6008
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:11:49 +0000

    kill slide threaten detail history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
