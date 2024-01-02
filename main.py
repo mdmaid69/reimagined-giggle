66cc8bf6bfb6c524b09d7ff09ea3c118b51ae016
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:50:43 +0000

    long excuse formal expect could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
