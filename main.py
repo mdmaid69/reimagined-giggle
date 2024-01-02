c9ab7df41b1b1d95f9d14284c5fb6c8cbc55c12d
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:40:03 +0000

    last expression accordingto see face

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import os
-print(os.getcwd())
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
