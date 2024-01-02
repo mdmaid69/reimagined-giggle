8abe6b51bdd7b6bebd2d922c00dc11aa8fe1c454
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:42:00 +0000

    reasonable store confused light Ss

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
