d240a31f5e40fb860f6f800f2f08e117e97a62d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:23:45 +0000

    grade straight tomorrow Dd pain

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import getpass
+def get_username():
+        return getpass.getuser()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
