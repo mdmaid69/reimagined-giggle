6ce35abbca2e0e1dfb7313fc2248a356f797f477
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:42 +0000

    thing equipment Mm just blind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
