22cb208811177fb983d9a80ca895de43a32834ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:10:39 +0000

    meet throw treat disgusting satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
