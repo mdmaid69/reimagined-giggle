2b7d03fa1a0fc52a582d649d622e69c6ef2a203d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:34:29 +0000

    attempt disappointed walk capital other

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def is_even(n):
-        return n % 2 == 0
+  def convert_to_octal(n):
+        return oct(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
