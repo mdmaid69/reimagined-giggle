ae40c83a544bd4ff145abfde927a8f87498e278d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:35:54 +0000

    brief knowledge independent unfortunate her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
