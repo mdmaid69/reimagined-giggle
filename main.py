a28eaed0d4287c3ce0e67391fc5feadba99b27d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:28:36 +0000

    offend callat however enthusiastic nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
