b336973fd64572aee1c9b379e60fb06f7163ef37
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:59:00 +0000

    phone original if nigga Mm

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def read_from_json(json_string):
+        return json.loads(json_string)
