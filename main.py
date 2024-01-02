00553b64c1dad9d843c233cb0cd0e540b382bbd6
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:34:19 +0000

    unless discover hour nor rely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
