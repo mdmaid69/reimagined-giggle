244f0355528972a8e16d7952eef9bc0c4ed70f59
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:49:17 +0000

    experience there responsibility relax open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
