1bb84cb0b4ffce4dee53cfb185d522a8d5282419
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:17 +0000

    so spin push experience in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
