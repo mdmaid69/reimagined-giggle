c35682808bdbf1c9a45315858e6565b58eb8731b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:41:42 +0000

    accommodation buy danger there birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
