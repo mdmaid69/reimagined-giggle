0bdc2112943ee669351eab4c4c5331638702fe16
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:25:00 +0000

    error even back recently repair

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
