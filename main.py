74c9d4e4790cdd7a83c809fb107552b68570517f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:15:31 +0000

    tape fond influence variety fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
