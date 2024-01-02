be33074b4bad987c7d43a04265a804046edd03cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:59:40 +0000

    help Itself such emphasize impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import heapq
 def pop_push_heap(heap, item):
         return heapq.heapreplace(heap, item)
-import array
-def reverse_array(array):
-        array.reverse()
