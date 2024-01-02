f631f3410eebd035a4aec9abc93498299e54cdda
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:37:32 +0000

    pretty ride anywayanyhow tooth fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_file(file_name):
+        os.remove(file_name)
