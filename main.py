5af0553d1ecd198785fef54637bf8669839061b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:19:29 +0000

    to example object art alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  def sort_list(lst):
-        return sorted(lst)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
