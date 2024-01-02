f2ec5c07b4b1020e92f524b404e25f28df3e6d83
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:28:50 +0000

    cost spite often wish software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import array
+def get_array_as_list(array):
+        return list(array)
