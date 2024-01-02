7577b891b6f73f585d4a1177ed52565dbe3e9870
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:12:25 +0000

    present conversation life bar tax

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
