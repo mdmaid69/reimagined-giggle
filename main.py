6438ba6267bed4e89ab52afb63882a1728d2c510
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:54:49 +0000

    block relative emphasize willing scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
