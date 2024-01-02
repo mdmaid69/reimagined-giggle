cb5266a91ea5c7760ed170fda88ae77c0025dee6
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:54:08 +0000

    lookaround last spring sweep question

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def add_numbers(a, b):
-        return a + b
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
