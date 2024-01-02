fc2f5a973f29f623e24333c4dbd0e2e84ac3d928
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:35:26 +0000

    dead certain neck hers disappear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
