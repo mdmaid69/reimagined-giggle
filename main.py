ea08e225227be05866593a6e6adb9fb45ee1390f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:59:03 +0000

    runover sorry chat deliberately hour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def find_min(numbers):
-        return min(numbers)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
