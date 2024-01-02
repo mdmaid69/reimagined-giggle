d528a5f4917280cf32358a9ff748f78566c4c915
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:27:55 +0000

    typical train immediate Dd formal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
