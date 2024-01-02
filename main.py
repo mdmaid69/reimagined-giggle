72db2f9a42fb114369469ccf89c8392db9d7361f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:03:00 +0000

    this 179bite permission follow between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
