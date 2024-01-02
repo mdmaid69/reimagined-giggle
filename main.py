4cce74e0ef31d5fcf9ef6f7b3177c056fff73f5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:51:45 +0000

    makeup hollow well since information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
