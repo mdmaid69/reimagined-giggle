ede3c584fe6dde289efb327f6f11bc0e62e28308
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:34:39 +0000

    own basically support mess cap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+for i in range(10): print(i)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
