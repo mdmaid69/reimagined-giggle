26132045fbd09166d125de8c6bd37a5fdf3f199a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:41:45 +0000

    forgive estimate Ee television everywhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
