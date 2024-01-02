1b6ff219a4cd793cc44bc18f5cd336698222966f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:12:41 +0000

    contrast tool trade long slice

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
