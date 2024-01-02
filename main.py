28c1879809b69622ae487f48f52c1505f7da6251
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:04:24 +0000

    blame crazy free some typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_work(force, distance):
+        return force * distance
