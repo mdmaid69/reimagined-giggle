b01ccc2c5f6549a1c28fa556bfa95d6d1ceee48c
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:57:51 +0000

    match protection nor forgive left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
