2d4123078e77667873ed9657c6c48edca9aa5efa
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:45:26 +0000

    bringup system yesterday taste identify

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
