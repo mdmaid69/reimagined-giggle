c6b0bea9b426cd339eff0a32c4620c3e597cabd9
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:09:17 +0000

    nonsense wise coffee life throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
