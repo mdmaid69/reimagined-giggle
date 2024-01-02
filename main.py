a6445deab958ea5cdeb5a3b00b2182ef296d15ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:09:36 +0000

    television path no to regular

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
