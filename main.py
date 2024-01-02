6ace415ac154b1c8a3aac39bbcf46c98b6825895
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:12:32 +0000

    tradition shade thin object move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
