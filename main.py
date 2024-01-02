3a7f0149cfc432317b9e8d548b182cfd28c5e8e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:04:59 +0000

    harm unhappy board come damage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
