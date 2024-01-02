2d9249e925b5497f2bbc8ec44405f1de4e80bec7
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:59:08 +0000

    stretch limit lesson fork worth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
