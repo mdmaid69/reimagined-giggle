ef8aece5e50522a059853f69c11d1144b38cdec3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:27:49 +0000

    father bother flood store more

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def append_to_array(array, item):
+        array.append(item)
