e6be4f38990c6e1f2a3412f5f936a7bb8d580404
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:47:56 +0000

    convince track rough put copy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
