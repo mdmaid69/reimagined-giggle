35e5be770d751b7c4c714b3ab03e91fb65025797
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:46:52 +0000

    nature score love grateful clear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
