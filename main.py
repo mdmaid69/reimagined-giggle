7387661645d842b4a653fffcfafb9ecfb81d3453
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:42:20 +0000

    comfort pity tone must tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
