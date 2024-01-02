85288cf2ee23662df675a014242b579ea779f475
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:41:33 +0000

    raw party disappointed combination permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
