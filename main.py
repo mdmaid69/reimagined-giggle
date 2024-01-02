30dd9392840261d6c2e983f554ecf4b3db469982
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:22:41 +0000

    benefit wild its breakup bringup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
