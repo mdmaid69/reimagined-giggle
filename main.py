7f7aeae5e0a4f01c39148ba03141f492f54c02d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:10:08 +0000

    drop finish weigh wish far

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-print("Hello, world!")
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
