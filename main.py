ce8e8c1c401d68d7f0123d0b5ca9243c9fb18c8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:19:40 +0000

    division thumb power rubbish rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+for i in range(5):
+        print(i)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
