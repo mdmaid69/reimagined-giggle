9249a6a1e004f6224e2d6400f3d3d17be2426577
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:53:19 +0000

    copy ignore front close land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_power(work, time):
+        return work / time
