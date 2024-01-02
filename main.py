f1b8278362eb910d8f51bd2652a2a01a8015723d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:26:43 +0000

    attract situation trial Ii giveout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_max(lst):
-        return max(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
