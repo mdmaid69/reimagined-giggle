c1d2ebb32c8aa53b24dbd25d037e80c775fffd0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:26:21 +0000

    although dry brother ear mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
