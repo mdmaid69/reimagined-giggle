cb7d526fe42d6f9d75f5b911f822767fd003d265
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:44:57 +0000

    no both television age embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
