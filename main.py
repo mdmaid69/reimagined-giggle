0076eb9a7ffb2245331e6158b554257f9868c8ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:04:39 +0000

    plastic dark building damp conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_item(array, i):
+        return array[i]
