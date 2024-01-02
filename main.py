17ff50e876e01200dfcfab39219d93c49cabe2cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:15:15 +0000

    petrol hang congratulations sweep behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_ceiling(x):
+        return math.ceil(x)
