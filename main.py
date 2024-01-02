934453ee220f75ce2f1b88e330d08884751324ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:18:53 +0000

    criticism introduce string winter formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_current_working_directory():
+        return os.getcwd()
