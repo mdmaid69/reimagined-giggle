0a13b1fe6cf120f989b89dc9bbdfe528908b9386
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:19:45 +0000

    burst some balance look march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
