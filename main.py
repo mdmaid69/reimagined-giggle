4cc2e53a9daf4eabecaf833c5b358f8b946a8540
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:07:49 +0000

    wise 567enemy street obtain Itself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
