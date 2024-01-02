60336d3960fe4c53f6b66c634717947e99073433
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:43:04 +0000

    bother official music bottom hospital

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import time
-print(time.time())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
