53201e6b6c390f50dc5f2acd5def8011285719a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:47:06 +0000

    beside pinch actually still light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
