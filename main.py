f71df3c8603f02bbd0820d4ded0f4d8c003583f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:34:17 +0000

    space cry town modern comefrom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
