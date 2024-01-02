4a1c5bbd698fee2e2ba1c121faca9db722a51b94
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:51:32 +0000

    early 571enough communication successful mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
