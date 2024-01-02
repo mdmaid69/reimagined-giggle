d8ed655eff159c1a1c9ce8b8d66844a078517137
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:11:51 +0000

    dry means fault shock please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_min(numbers):
-        return min(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
