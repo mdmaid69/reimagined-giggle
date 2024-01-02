af3deb8b561450b9ab8e737706166b22d614549f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:35:36 +0000

    hurt once down closely immediate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_cosine(x):
+        return math.cos(x)
