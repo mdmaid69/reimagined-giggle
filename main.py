fd82a2abc150d4f6320f7ab87bbc3481d8cc86f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:29:32 +0000

    tongue blame book method cut

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
