7932d5b6369855570b06ed75a263281648804d0a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:49:38 +0000

    press arrange refuse myself instrument

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_list(array):
+        return array.tolist()
