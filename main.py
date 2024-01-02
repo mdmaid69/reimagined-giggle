4dde5ed414c3e23f0e818a8353509748f2e4b21f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:15 +0000

    least best class head unlucky

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def set_array_item(array, i, item):
+        array[i] = item
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
