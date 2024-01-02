36a9144e8afdf8930e167bf32115cf2f77bb9ce0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:30:57 +0000

    else train track man burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def count_words(sentence):
+        return len(sentence.split())
