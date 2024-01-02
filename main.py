0609dd88752227bfa86eff0215f37e95f396f466
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:44:40 +0000

    mood pretty stupid neither still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
