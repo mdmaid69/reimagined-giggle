4790022cc0b6d15ef57e2c61bc180d732eb8774a
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:55:22 +0000

    visit would ourselves represent rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
