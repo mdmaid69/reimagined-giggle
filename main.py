d1b39350aec4a16ed68f7d2ec9d5d05e0fd3826f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:16:08 +0000

    year early broad thinkover basis

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def multiply_numbers(x, y):
+        return x * y
