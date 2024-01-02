59c106ebdf5a2757de320561c73efb63441bc50a
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:36:35 +0000

    179bite learn safe feed life

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
