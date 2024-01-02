538eff24e272700496bdfc2fad02d841cdd43606
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:32:42 +0000

    safe wind direction objection slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def convert_to_octal(n):
-        return oct(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
