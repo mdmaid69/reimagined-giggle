513630f9b8b7186f5dd491f1f1f33c8f6dabd80f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:31:15 +0000

    mass home completely I phone

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def is_odd(n):
+        return n % 2 != 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
