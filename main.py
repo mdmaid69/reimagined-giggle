2461087180ab36eb5fd857be579dab83e126298f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:53:38 +0000

    proud throw guess correct rob

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
