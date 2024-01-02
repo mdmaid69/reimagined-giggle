b858ee407f0308c3dd5237b5a0168129b2a1aa3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:08:57 +0000

    hold total deep turnup cinema

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def convert_to_binary(n):
+        return bin(n)
