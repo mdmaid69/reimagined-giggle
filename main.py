903cdab9e36a0b26116485fc7f5edc569f9d95ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:07:50 +0000

    year warm extra stiff low

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
