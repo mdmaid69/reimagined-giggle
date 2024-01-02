a7c978870cd94fbbfce0c83aec3672fe68dab4ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:03:22 +0000

    would soft shame judge save

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
