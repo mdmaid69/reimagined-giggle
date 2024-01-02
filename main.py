0693c59c6014794cac333c62b6f71813988a2db3
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:12:00 +0000

    yourself convenient tap often introduction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_area(radius):
+        return 3.14 * radius * radius
