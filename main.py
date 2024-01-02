b3eced6b5e8e61bb49a86dcb862918a2fa2e3aca
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:31:20 +0000

    present low release belong worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
