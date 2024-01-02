f2df2c9369e07424cee7bc3063428900c097f507
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:26:00 +0000

    back another bank comeout old

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
