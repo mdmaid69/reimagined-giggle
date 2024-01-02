ff7919914e4ad9d4d953f4f22a8317521a4b323e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:44:00 +0000

    obtain rely chance habit powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
