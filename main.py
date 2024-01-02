be9bd72bec76d1b1a1e82637ec51f8e921c07d81
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:34:31 +0000

    loss proper live offer replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def find_unique_words(sentence):
-        return set(sentence.split())
+from collections import Counter
+print(Counter("hello world"))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
