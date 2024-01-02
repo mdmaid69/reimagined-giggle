f515fa7ff88d4c6bcb20e313ec46e119b6268586
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:42:51 +0000

    wave Kk disturbing pretend by

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
