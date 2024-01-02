47c864a0e12b355bd64c189c77a0ed8fbe139850
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:36:09 +0000

    slice plain office blow flood

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
