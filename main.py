afcf2f9792a30cf72444ef04eca3877ad657fae7
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:55:30 +0000

    fasten pretty guess write ilovedicks

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-for i in range(10): print(i)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
