160196af584d32d21ab7204060ba210a81c53f37
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:50:25 +0000

    hardly square wide shut wire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
