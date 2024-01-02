f4e502e53f73de923f8b467bb1cd0aa31b66fdeb
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:33:25 +0000

    Ii lay similar downstairs nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_repr(array):
+        return repr(array)
