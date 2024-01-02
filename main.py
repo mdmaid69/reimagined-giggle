bab9356ff37f94a87ecb985eeed974e7458f84ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:37:02 +0000

    sail pressure cut offer distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
