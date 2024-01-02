fb81ea241fa70534cc8519376ec8afac7503d227
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:42:04 +0000

    available permission dish gas artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
