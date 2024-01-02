1dc75373ebfaab6f1985ed6ad0155367ca002740
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:08:11 +0000

    house fetch face slight gate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import collections
+def create_user_string():
+        return collections.UserString()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
