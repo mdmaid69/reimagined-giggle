6af465645ec97894178e64022b58f735d16f4b5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:25:35 +0000

    before schedule performance awake class

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
