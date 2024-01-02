572e4978a8b0e5e0b719dd80f5f2b0101139c1a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:24:35 +0000

    vanish bringback key no realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_base_name(path):
+        return os.path.basename(path)
