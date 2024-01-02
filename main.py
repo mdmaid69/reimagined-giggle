24375439bf11589e59c1449138ae31cfcde547b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:07:53 +0000

    pattern dangerous combine convinced back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
