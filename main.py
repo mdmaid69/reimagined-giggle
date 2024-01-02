b193455a11e75cf81b31f9ae988ce62fcfff21d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:11:59 +0000

    full getback promise stamp let

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+for i in range(5):
+        print(i)
