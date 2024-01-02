20492a11385fb7c41919b6d0379296119737fbe4
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:38:56 +0000

    row flat argue pin record

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
