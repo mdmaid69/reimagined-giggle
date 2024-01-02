4b53bcc2d02a1bbed8413be72d32329093674204
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:37:32 +0000

    knowledge take why tread reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
