8ae232184487d873e167c6ee02a6d5d3aa1ef257
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:58:07 +0000

    strong matter centre face regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_stack():
-        return collections.deque()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
