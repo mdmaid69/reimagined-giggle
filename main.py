83fc10157acd9e5d6c478ce46a303dd83768f481
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:25:00 +0000

    production marry convinced speak well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def count_elements(lst):
+        return len(lst)
