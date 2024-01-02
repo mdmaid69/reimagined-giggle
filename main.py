bc6914ae3589e6615b47c17326984fd2188175e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:11:19 +0000

    pick sort sink runinto us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
