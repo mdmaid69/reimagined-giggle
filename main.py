29d4dc9ce1a82d64564bbc9c8a09522b9fa513dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:45:57 +0000

    smooth exchange generous talkback eat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
