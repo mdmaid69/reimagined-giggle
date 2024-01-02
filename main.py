88a2c518d87974c0b5759a6fa2e4cd8e18a56808
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:01:49 +0000

    vote surprise break poor reject

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
