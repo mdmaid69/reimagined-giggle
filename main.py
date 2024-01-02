2dfac00e7a11cca943816fc9d596bf9e6311e973
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:05 +0000

    challenge hit different remember thumb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def subtract_numbers(x, y):
-        return x - y
+import sys
+print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
