902d052fbed4183cd1eb1723696460b037e96628
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:16:53 +0000

    tight storm violence face block

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def cube_number(x):
+        return x**3
