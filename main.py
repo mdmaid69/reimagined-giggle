069be36d097bcd95f5181cb09f21cb854aab133a
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:09:30 +0000

    outside hello negative giveback poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
