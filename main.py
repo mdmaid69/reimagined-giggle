e4b53dafabcf8d026303ac28ecf32ecf1637850c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:30:31 +0000

    turnaway late punish word travel

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
+  def subtract_numbers(x, y):
+        return x - y
