1f3a57a9c1745cc39ec4ec58c1e6b4bd9f58ddb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:27:43 +0000

    make unpleasant wild allow talkoutof

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
+def calculate_acceleration(speed, time):
+        return speed / time
