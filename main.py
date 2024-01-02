3d1baf2ec568d5091a247866b03433cf8789419a
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:43:17 +0000

    criticism benefit pity pipe worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
