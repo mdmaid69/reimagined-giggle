b7efe7db34e326a8a9081ec748e0508f26cd40ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:21:18 +0000

    565end practise swim for exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import datetime
+def get_today_date():
+        return datetime.date.today()
