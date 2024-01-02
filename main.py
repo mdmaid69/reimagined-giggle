4393b7306238d9b20805f088a5c51f425a16526c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:50:24 +0000

    hot fortunate invite fast increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
