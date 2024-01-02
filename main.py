2a7790cb86873f7733fa8cd120c56b900bee8f8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:30:02 +0000

    accidental properly patient 570enjoyable off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
