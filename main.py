6cb0f0d52912c8c086d8c2086eac977bbd19ede3
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:26:29 +0000

    pay bear snow protest birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
