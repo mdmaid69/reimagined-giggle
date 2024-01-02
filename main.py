0fc033e01a1bd23aa25e194ca50693ada56fe6ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:07:38 +0000

    general worst burn 569enjoy communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
