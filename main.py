1762497e9a55140ec3c17c66e46dfac9267b98f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:44:54 +0000

    stage bottom both arrange disgusting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_floor(x):
+        return math.floor(x)
