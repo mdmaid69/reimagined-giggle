d1e8281a57bf4ab1585d3f94c53e0df9b8c67231
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:58:06 +0000

    prevent wide happen much flavour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def is_palindrome(s):
+        return s == s[::-1]
+import sys
+def print_python_version():
+        print(sys.version)
