a621035c930923c847f67c5146976ca3b57e40e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:58:18 +0000

    thumb close really deaf godown

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
