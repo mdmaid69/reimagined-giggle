00b97ece88c66eefea9674cf7e5a70bc99160187
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:55:12 +0000

    floor very consider belief walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
