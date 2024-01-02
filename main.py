8a3439dd6453ddf1fe18d981f7cb526d74e0ab22
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:20:53 +0000

    cheat service neat approval crime

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_string(s):
-        return s[::-1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
