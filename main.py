ff798824b114db063eb56b13072ad77e39e408db
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:10:57 +0000

    card breakout early hear yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
