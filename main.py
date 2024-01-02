ec45a91c8c4272dee68371a4afaa1ddbe9eea887
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:33:20 +0000

    trap action tooth thankyou body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
