01379e3a1270bf2a0c8bcb50056b40249329da7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:54:59 +0000

    corner cheat write free ear

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
