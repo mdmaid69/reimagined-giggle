e9954a7f60f36751190705d3c557b4805ec4da09
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:59:20 +0000

    container rank trip squeeze mistake

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
