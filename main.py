0799cea4be675c31ff929519d31806bf5af0f353
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:21 +0000

    write direction mood spare light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
