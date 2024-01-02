03d65edafb0eef48f540eee3df60eefb5fd36c5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:08:24 +0000

    talk complete wrong backwards usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
