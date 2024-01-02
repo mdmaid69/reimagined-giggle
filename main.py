840b13e7bc489c22012a31bc5c93b9c3fc2e6dbe
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:24:13 +0000

    price care lookup language limit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
