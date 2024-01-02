1ad8a9308156a1f3b77ca58114902d45cc75907f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:05:53 +0000

    after properly bottom exam track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
