e9512e5ee3e4e4fecd0a9f0b9e4c80d942c2f732
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:34:56 +0000

    dance afternoon release autumn hour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
