9ffa41765dde33d7f285db6a2647c897d794c186
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:29:53 +0000

    both limit introduction my young

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_power(work, time):
-        return work / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
