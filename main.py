697113eaee430027129d5a114465034cc7aab88f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:29:31 +0000

    may mark add bound stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
