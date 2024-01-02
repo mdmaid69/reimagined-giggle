0ce839cc552cfb84c177ac964bad90cf76d93da0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:01:46 +0000

    blue charge full accidental rough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+print(os.getcwd())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
