947654b0e78ba5c41d0ae8a579b8fe8a0baa1e52
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:17:20 +0000

    understand complain count behaviour fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
