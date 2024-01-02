2b08605a19cc7fb3ef1f4dcb69a45fc99ec0c924
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:31:17 +0000

    pump hold let crazy uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
