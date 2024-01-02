8a9ff354a75621809102d5277d21409f87abd3b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:37:00 +0000

    leg whistle interview pull off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
