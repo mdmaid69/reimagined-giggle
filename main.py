8bf7a20f57c8d72759bdd5113c83c8ed879ebbd7
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:19:48 +0000

    sign underneath scream once sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
