cbc46d215df19143e6c930a4b70598a82daaed2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:00:20 +0000

    know completely considering holdon comeout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import time
-def get_time_since_epoch():
-        return time.time()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def is_odd(n):
+        return n % 2 != 0
