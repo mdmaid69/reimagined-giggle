b96e88c25e1ca72fe436fffda4128aa467fb0f42
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:21:24 +0000

    desperate anybody free obvious type

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
