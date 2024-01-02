db6d8cbcd5e2a100a976c54c27e5415f25b818fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:16:50 +0000

    base confused land raw personal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def reverse_string(s):
+        return s[::-1]
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
