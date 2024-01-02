3ca40dda8879b3c455933a6a8542f54d78a75eae
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:45:45 +0000

    laugh of fill impressive related

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
