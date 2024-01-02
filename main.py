7039968f4572b1664ca58e8dfedd9e2283d4c903
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:32:13 +0000

    tap Ff know piece mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def sort_numbers(numbers):
+        return sorted(numbers)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
