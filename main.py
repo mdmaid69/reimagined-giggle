592220da88cc67507d4ca40e26234254dba84389
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:09:29 +0000

    feel slightly for still raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
