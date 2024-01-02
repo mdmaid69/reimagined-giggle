8d619ffac708334ebd8a68283c33d7a385af09ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:27:09 +0000

    account high maximum absolutely bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def append_to_array(array, item):
+        array.append(item)
