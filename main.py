ace7dc678b831371a750ce3cfd90c25ca16ea78f
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:39:57 +0000

    advise past little step send

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
