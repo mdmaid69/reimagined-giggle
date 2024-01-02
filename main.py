90d9f997522657d1544dc0af52f94a338065e3e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:21:21 +0000

    slice claim trade prepare size

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_work(force, distance):
-        return force * distance
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
