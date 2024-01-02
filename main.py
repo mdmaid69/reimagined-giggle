c27d512b49b8bad6dc68790ea2c11b477e96b1bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:22:02 +0000

    scrap hour scare wealth advise

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
