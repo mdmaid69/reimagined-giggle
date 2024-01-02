725383c820ef7b19e8d77062703d4b6c0ab41d36
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:04:30 +0000

    material reduce light alone error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
