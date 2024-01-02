4964f10582d98d2a9341c76ed1375de14f1d30cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:10:38 +0000

    567enemy weather fly constantly belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
