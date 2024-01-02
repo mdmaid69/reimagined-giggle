ee54327a2c0ae07fcee5145f6bd5741ba2f64ee7
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:09:08 +0000

    neither change sport blow polite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def sort_list(lst):
-        return sorted(lst)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
