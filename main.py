4d672725bdd8eb9d04bb61899628589453c35c02
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:28:04 +0000

    loud health last later wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def square_number(x):
+        return x**2
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
