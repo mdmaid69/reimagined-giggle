dd9f0212de997a7b3aa2a753bb68e4d4f4263ab2
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:41:23 +0000

    allow taste trap fortunate produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
