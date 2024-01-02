cbf6c2ce6a6bce72d45a9c04a66e23b33d6b688d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:24:27 +0000

    lost mark mass related undo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_itemsize(array):
+        return array.itemsize
