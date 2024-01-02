ea6e8f8722fb8cf63bd5bd00a65ad5cc17bf0b37
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:50:52 +0000

    study laugh dark light we

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
