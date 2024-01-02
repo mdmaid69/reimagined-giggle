7201f83ff2f2a00c98f4f9e7b70d874d1aa78dfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:52:28 +0000

    practice flat weekend broken mean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
