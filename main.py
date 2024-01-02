aa60b2a1786523948bcfb7259da0a165ea39cd3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:46:40 +0000

    bored earn height turnon involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
