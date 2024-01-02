5e56d579d47a0ce5061ccd0eab999ba4d8c498d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:33:52 +0000

    present border wake beautiful goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
