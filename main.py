5b39fa86c922fa6db424402dd1d2f1579e12d3b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:22:16 +0000

    bath one payment level force

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-def find_min(numbers):
-        return min(numbers)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
