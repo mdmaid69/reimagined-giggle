c687ccddfd383d33c867d0bc2da8d874ccd1ae41
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:04:40 +0000

    behind save behaviour direction cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
