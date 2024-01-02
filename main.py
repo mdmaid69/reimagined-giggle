90fbc397cab7ca6a2276e4ea98485c3f7f630251
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:46:40 +0000

    task promise period rain except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
