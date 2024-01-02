e0ea0da36f68fda929d803054135df09c77b0545
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:46 +0000

    usually rate can rough wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_current_directory():
+        return os.getcwd()
