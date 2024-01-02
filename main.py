727dc637a8304decc3ec76ab83852854593191fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:57:11 +0000

    connection reaction language argument giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
