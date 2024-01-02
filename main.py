835df055628814c1c32fc330f9f4d4ae17512d4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:01:51 +0000

    foreign maximum excellent without swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
