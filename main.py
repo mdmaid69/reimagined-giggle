2f725faf85d2ad0b477575f7f19cdc832d704687
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:01:56 +0000

    Cc Mm except pure mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
