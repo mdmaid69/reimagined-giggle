412126be3f0320c19532c254e1d1ea582660ed72
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:34:25 +0000

    use wind carry there true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
