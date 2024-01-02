47a1a039ceb8348a16535c72e3e73599efc46f91
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:20:42 +0000

    see believe guilty interruption anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_sign(x):
+        return math.copysign(1, x)
