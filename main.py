7f99df78115e074ac4fce06b92684cd4402315d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:35:46 +0000

    dream professional for star ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
