d858fb616f0fc7faf5d213b5658794555b22c4e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:17:43 +0000

    meal scream vote substance pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
