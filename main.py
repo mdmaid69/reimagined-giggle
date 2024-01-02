dfaf6abc8d6c60e05b1bb67cd243a109328c0a58
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:39:27 +0000

    emphasis goaway rush real near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
