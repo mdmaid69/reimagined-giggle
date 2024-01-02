fcbe9cad9e62906ca3f2ed4ace95865dcc44d27b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:01:51 +0000

    try except letter broken interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
