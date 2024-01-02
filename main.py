54cdd0378644b40e12105c78f2732cbbac85de62
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:01:40 +0000

    picture neat declare second second

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
