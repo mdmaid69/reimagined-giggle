5b45057f941da6673a2a8798b9a1a8e44dd74051
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:23:03 +0000

    steal everybody comedown usual cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
