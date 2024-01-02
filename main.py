18ae326307c969a9e5c4e150b7eee73f96c10164
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:40:55 +0000

    balance wave wing means vision

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
