90e9e6ae03df17ed4ef71e3effbd413b2078dcc2
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:23:41 +0000

    record mad pool how board

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
