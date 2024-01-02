fc7484c8a59cddfa3cae2daef8247980372ece1d
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:23:52 +0000

    the world side takeover suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
