2f2d804460196acc45d9fb5b6f6111161a15468f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:53:03 +0000

    hope goodbye total listenfor stare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print([x**2 for x in range(10)])
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
