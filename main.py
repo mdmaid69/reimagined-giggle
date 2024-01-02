31566bd59c680c5870143397ba8d8d1a89c8e8cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:13:36 +0000

    priority question convince square law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
