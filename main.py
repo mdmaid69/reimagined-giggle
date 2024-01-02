aa0e6bec4669d8a3c2efec2895593c10caa22c06
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:05:28 +0000

    cross apologizeapologise height best autumn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
