0eec76bf7e25f69c69708c74d71f440d0a459ac0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:13:01 +0000

    join base disapprove tie call

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_str(array):
-        return str(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def sort_list(lst):
+        return sorted(lst)
