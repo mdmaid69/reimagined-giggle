e24fe56171704fd09cffeb3bf92508e2b1050f5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:48:01 +0000

    turn listenfor murder concerned sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
