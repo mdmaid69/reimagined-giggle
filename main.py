659ea616549fee3b073f9c7a68b41c67611e7281
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:09:41 +0000

    shape clear child unit press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
