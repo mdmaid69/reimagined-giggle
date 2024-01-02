78d29edb842ae769d5b5fea6567feebf1bbebc06
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:59:59 +0000

    so pick recently interview unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def find_max(numbers):
+        return max(numbers)
