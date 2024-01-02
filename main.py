11a639ac4454aa99772d3013a6dde2c6e8f0d0f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:28:55 +0000

    sleep closed consider clearly thirsty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
