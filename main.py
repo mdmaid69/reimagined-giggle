e5056cea760da627f7eb70d676034a1f61bb5524
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:07:58 +0000

    strike dick thinkthrough section responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_power(work, time):
+        return work / time
