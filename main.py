b29ee5f89472050b2d951a8d0d695d4bfab11516
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:39:46 +0000

    connected low congratulations drawing factory

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def find_unique_words(sentence):
-        return set(sentence.split())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
