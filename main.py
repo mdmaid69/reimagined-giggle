539d34c0400b7b61968ba23d18f2410c2e69e2e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:18:39 +0000

    short television sink vote say

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
