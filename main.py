5f2f5eabfaf5ad96b8b28cabda36b727ed6a6dcf
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:13:18 +0000

    afford continue now exit inform

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
