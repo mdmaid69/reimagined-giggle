28a2f7fa512805a6318423cb8e62228775550089
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:13:54 +0000

    doctor upstairs famous shout embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import time
-print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
