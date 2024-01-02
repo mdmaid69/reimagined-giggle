b393685e843517fa699124ed148c9b61382e1d98
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:20:21 +0000

    maintain boat world sentence test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
