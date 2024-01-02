3dd9b70ffc0bd21ae721aff57fee4f9f52a00106
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:30 +0000

    crash constantly climb all fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_str(array):
+        return str(array)
