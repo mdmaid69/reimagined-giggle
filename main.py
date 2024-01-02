cf40b4367fdf531a6c423c255920bea7095be237
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:01:01 +0000

    book somehow pronounce wait recognize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
