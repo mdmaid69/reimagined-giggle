568f220acbdfa47177f3956dcbfffb1dfcc4819c
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:02 +0000

    man chase interruption list wait

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
