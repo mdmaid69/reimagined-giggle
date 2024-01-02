46754edec905768cc9d59868f1bd4ba97febfca4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:23:59 +0000

    throw plenty surprise search comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
