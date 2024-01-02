fda750434d5951169bfac8c2eaee2d6eb1c56d05
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:47:29 +0000

    rich odd several hungry wander

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
