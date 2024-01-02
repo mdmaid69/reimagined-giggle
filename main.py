7c18b8bce4288f76c18b536d6aa713355fe19f5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:26:56 +0000

    tread communicate address interview uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
