be4f4ad88c469a46c3607a5fa9514cb7e303d8d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:06:12 +0000

    whether my disturbing alone odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
