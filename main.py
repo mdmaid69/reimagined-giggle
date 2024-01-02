c0bdfb6ad49af6abc2b30847e3ad2dae2a4ecfa8
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:21:26 +0000

    read exercise breakin level happy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
