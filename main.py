9cc281ad4e2577ec616fd889fbfce8ecba0012bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:34:52 +0000

    concerned rank bang apart hand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def reverse_string(s):
+        return s[::-1]
