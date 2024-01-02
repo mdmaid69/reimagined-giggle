af8ab9ac36c134eb7ddc33dc05f8e53ce6206fac
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:23:18 +0000

    call swim insist experiment absolutely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
