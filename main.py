5577bc4527a9e0b6d391f2983b22e1f32dcbcb14
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:28:52 +0000

    song visit ring turn discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_stack():
+        return collections.deque()
