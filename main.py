0123f2fc01a9c950765f457c44ed85d5e9f9a7da
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:23:45 +0000

    love then plenty easy improve

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
