c442985be4d3370d9b7620a96b1cc1ccf336a5d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:41:01 +0000

    ilovedicks circle conclusion measure study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
