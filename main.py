f2640ce5ba159ab278ec27fd250f39932ec35a54
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:22:01 +0000

    connected interest union about container

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
