f509fa7902b067511f7b9d35f785728152f88123
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:57 +0000

    wish claim contrast chase runover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
