237ab91b76f75b5a998d51505d9c68daff752fd6
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:52:47 +0000

    suddenly spring anxious threaten obtain

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
