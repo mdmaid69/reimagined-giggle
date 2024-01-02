57537c71e660d80895b7db77495585321311430f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:22:06 +0000

    match dinner over city mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        return sys.version
