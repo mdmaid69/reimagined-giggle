f752df77ca5bc29c75296f2ddb76469930837ddf
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:56:43 +0000

    OKorokay already slightly place make

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+for i in range(5):
+        print(i)
