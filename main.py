5c245ee8587cfd4194248e6d15111aef06c11a76
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:41:16 +0000

    close broken pay leave his

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
