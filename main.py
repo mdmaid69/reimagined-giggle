dfb893055fd66a07a20d17510720bac8c9d0cc36
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:28:20 +0000

    bang foreign difficulty sort careful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
