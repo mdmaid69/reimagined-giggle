7ae6a7f3dd73e79a27cb8ff90c5da8a3427c1d64
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:26:34 +0000

    tradition priority trip pain geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def count_elements(lst):
+        return len(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
