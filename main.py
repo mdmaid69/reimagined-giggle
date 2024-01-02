545a041c0ea56cf319b9fa720a6f93ad2925c842
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:52:12 +0000

    sink eager dare fashion stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
