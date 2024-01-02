929a3dcc944989eb5f787a6b1449723170585652
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:46:29 +0000

    myself suggestion runinto temper principle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
