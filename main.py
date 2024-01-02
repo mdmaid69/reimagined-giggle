d499e2e972c217e9a24cd0f131a19ccae9a77c7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:49:04 +0000

    chance involve in winter live

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
