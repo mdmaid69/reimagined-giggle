8d52c358df3c6f2729a67b658d606c5ef196b586
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:46:37 +0000

    city correction keep here contact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_area(radius):
+        return 3.14 * radius * radius
