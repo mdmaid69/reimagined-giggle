50350ee54f464ba46a6f5f9dbcc6158eba2bebb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:58:52 +0000

    whoever keepfrom mine present left

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print([x**2 for x in range(10)])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
