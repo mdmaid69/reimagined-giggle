93d079b55fa85bf2b18c1727ecca7e30e1b0ded2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:46:27 +0000

    innocent land phone shame increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def multiply_numbers(x, y):
+        return x * y
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
