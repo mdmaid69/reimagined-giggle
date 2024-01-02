66ef30b226f4c45b9e310c01c80e9e424c876a24
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:08:40 +0000

    its tooth office include lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
