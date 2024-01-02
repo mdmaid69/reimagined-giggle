d583de6e8f46273e330e2f8c4ab6155641c63d58
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:58:02 +0000

    result tour impressed dig only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def sort_numbers(numbers):
+        return sorted(numbers)
