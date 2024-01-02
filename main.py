03de6e5fc6795179ec9523593a51d49d94333eed
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:31:27 +0000

    minimum tidy effort steal exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
