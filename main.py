575bd67726f8315fd008dae0c14264ab25f56930
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:52:22 +0000

    past frighten software environment live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
