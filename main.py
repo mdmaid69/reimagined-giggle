8cd8e089c5291491b4b3a4fe4be1f127fbb427b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:55:03 +0000

    thing spare unusual lead exact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
