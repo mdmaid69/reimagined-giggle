12fc96449e58b09e3427a9acbc08e6e5f756ea02
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:45:44 +0000

    communication entrance pass slip learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_index(array, item):
+        return array.index(item)
