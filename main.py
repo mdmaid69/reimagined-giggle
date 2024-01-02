e201c0ba2ebcb7f70f4a5fbaf7ebe3c93e46acca
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:36:38 +0000

    friend clean frightened mad autumn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
