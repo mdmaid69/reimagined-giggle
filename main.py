176ce27d2b521ca62f133aff338e8c3d33c6cf4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:12:50 +0000

    direct roll concentration software because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
