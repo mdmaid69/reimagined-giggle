38036d64203ce830f2d9e522e62c106aff11cf02
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:26:03 +0000

    whom nearly search modern software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sys
-def print_python_version():
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import sys
+  def get_python_version():
         return sys.version
