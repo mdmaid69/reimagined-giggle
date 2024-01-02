228c5289996ea4c96c68014cf3428b96bb07cb6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:24:25 +0000

    surprised guess compete dig confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
