f6caa8fc64232ffd04d2dad72a6dc91d9b0ba378
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:31:54 +0000

    majority earn block objection traffic

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import math
-print(math.pi)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+print(sum(range(10)))
