cd00ad8cea7aee7938c0c6ed0f196b6f6d88e02e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:58:12 +0000

    warning art clean summer fold

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
