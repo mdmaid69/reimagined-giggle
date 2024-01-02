34dbad7ae59e25d030e005f3fcb333d0e18f0e26
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:24:47 +0000

    fierce taste yours whether flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
