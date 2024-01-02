3b6ca2601c8b3d6b689367e9e89dad6bb56897f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:18:11 +0000

    available shop wrong ordinary fault

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
