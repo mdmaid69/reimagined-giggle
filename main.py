756516b281d9acdfe7ceb0af90be960b03a83151
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:36:17 +0000

    involve plan share belong unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
