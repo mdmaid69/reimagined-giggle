55ab3b226eb41a9db13a19e21dcff2d5f3cd493f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:50:00 +0000

    concerning ride beat Nn comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
