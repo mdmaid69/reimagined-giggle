da99d3053de713e9ae22700b5762a51e36a269ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:07:34 +0000

    terrible sight bringup 564encourage lose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import datetime
+print(datetime.datetime.now())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
