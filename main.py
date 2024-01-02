02ea328304f9b0aea8921553f2ccf866b7c6b0b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:26:48 +0000

    however train little butter report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def calculate_variance(arr):
+        return np.var(arr)
