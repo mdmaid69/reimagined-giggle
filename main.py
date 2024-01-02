ebebf5df9a8a05fb7b0adf67714cf897e48ecdc1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:26:54 +0000

    exist charge lesson workout occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
