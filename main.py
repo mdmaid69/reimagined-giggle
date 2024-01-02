d15b6fe85853dfd8bb40e779dfd5ba25a7e82d5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:09:06 +0000

    shut mixture thumb slight far

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
