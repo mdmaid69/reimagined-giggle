1c29cd0d408c5f40c90f1e85198c5cc2fc139f20
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:45:21 +0000

    spring bury sound middle education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
