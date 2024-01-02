2cd8a879d53ae50296f379e8d5fec6ece3744c93
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:19:48 +0000

    shame cool chin vote police

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
