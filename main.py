2e2572ca2303dcac9a4d3a61358da932fb571c4a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:30:51 +0000

    schedule they minimum already it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
