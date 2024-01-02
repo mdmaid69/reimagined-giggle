8c4f1bb0e46e22eb346c6765cea28592bf11fca6
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:32:40 +0000

    section count call half Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def split_path(path):
-        return os.path.split(path)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
