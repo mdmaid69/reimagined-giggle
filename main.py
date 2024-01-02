66edd5dcfd12906233d05788374e17b835d4b69f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:28:09 +0000

    dare Ii trust evidence expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
