77bfed280bb4d251adc8820ccfbcf315c19815d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:55:24 +0000

    disapprove unkind whom treat art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
