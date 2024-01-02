fc3c9a7e7a9636b059d749d41cd358a456ac5f09
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:05:01 +0000

    formal whoever right interest evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
