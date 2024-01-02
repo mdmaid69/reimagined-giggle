1bad8762c2136a1455debe755e2f03500a6c2f8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:21:34 +0000

    brush this visit wipe really

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def find_min(numbers):
-        return min(numbers)
+def calculate_speed(distance, time):
+        return distance / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
