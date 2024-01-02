13de45ebb2254b9fea049ea679c9dbe770a24dd5
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:59:20 +0000

    meaning dry fire he giveaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
