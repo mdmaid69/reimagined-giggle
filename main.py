8357aed8586c2ffef30b57a39fa09a37ce04c0d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:26:57 +0000

    recommend word minor dream high

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def square_number(x):
+        return x**2
