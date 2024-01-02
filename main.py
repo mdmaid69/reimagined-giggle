c08938e4387d7a1d5422af785cbd0295d6b3adbd
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:47:23 +0000

    attractive hide ashamed upstairs 570enjoyable

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-import os
-print(os.getcwd())
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
