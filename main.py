7c90c3e185969f7ba3a7cd5de0da50d7ab489afe
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:13:50 +0000

    centre fold happy whom surprising

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+for i in range(10): print(i)
