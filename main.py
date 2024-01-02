bb83a320226a3b19dc70a938c82e44f3fb622934
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:43:40 +0000

    empty defeat variety jump material

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
