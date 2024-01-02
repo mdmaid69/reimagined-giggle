db56694fe22801f8468b64ea635e7bdcb8d13a0a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:57:04 +0000

    eat somehow dry deserve turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
