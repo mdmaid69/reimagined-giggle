503c07cd7ddeb281ebe7570325aeec9a88732142
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:02:08 +0000

    police race drop lend shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
