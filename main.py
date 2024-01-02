8a2b6527104895a48cdf80f95cf16f2673aafb1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:24:48 +0000

    needle software manner rise noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
