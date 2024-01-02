8e5d3a46a93fbf0e5a7337fbf26ee3de0fb62418
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:45:57 +0000

    sensible available abroad cause spare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+print([x**2 for x in range(10)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
