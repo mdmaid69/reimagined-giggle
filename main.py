16bd69769dd0d905e402d86ceb9def0fac34d26d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:03:48 +0000

    scrap do fault persuade communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
