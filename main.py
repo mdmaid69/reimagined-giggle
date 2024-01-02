dc28abbbb1daa403070ac56043a7e4f4a9a5e6ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:50:40 +0000

    petrol personal anything tongue smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
