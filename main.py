dca2bb5370c40fe92b679b5b298fe661d4027d59
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:24:22 +0000

    pipe keen original definite open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
