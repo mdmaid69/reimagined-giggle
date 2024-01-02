478127e394a6d4e2e8f15546e177ebbd879e81dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:54:14 +0000

    pocket old part lead expensive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
