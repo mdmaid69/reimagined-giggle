befc5f4ca45636d30da97c54419e4303755ac535
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:59:20 +0000

    yet command give right common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
