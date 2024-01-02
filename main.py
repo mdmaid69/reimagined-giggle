e825a81fa567bd9895c7d1dcd673177a84112a17
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:12:11 +0000

    usual hold offer conduct dry

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
