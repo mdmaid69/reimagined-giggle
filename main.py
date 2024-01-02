fd7d40162fc0eefffe676f07b415fb1f2be991b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:31 +0000

    general fierce meanwhile total content

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def subtract_numbers(x, y):
+        return x - y
+for i in range(10): print(i)
