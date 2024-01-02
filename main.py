920ca1db193c7843db2d015d0b98e8fe2eaf44fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:55:09 +0000

    teach increase quantity floor so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
