596dafb7e186f5d3879f4769cea2456fd5b56065
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:55:40 +0000

    involve rent lower hurry speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
