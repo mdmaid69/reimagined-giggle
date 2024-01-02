ccaf3e9e1bddce501e06acc521327e99f973075a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:49:34 +0000

    lookafter your person evidence guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
