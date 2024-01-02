7f628366fe3aee9c24b2d235729ab457ac79a6e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:01:45 +0000

    breakthrough and knock straight beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_area_rectangle(l, w):
+        return l * w
