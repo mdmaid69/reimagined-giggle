a2dfcd3dd11185b644b20d96dc690f391ca9f690
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:34 +0000

    lucky brief emotion course raise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
+def count_words(sentence):
+        return len(sentence.split())
