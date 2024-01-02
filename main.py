26571e2788ee45d9a782e3efcadcd3a310668271
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:06:24 +0000

    result space bang wall famous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print(sum(range(10)))
