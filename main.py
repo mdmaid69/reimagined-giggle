2f60ebfc82bdd0ea02c53abacc72d62c83cdb707
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:11:02 +0000

    translate drawing calmly touch entrance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
