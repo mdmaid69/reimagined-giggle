3059875604b261f66bc324e6188b98546e61b03f
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:55:23 +0000

    dress somebody lookafter breakout hear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
+def convert_to_octal(n):
+        return oct(n)
