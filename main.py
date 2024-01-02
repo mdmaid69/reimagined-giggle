a8a3afa6a6314e5c32bee839c9bb709698ebb495
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:24:16 +0000

    great tonight passage believe takeout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
