2eed4f6e74af1f388e051713a75dd50b2392f931
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:56:26 +0000

    label shock knowledge peaceful education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
