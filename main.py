cacc5aaefb3bc0823fff71554718bc3829e10a18
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:07:05 +0000

    destroy inside pretty proper enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_volume(length, width, height):
+        return length * width * height
