2a43be4a9c879dc6e1cc368aa38e5e52e27e23f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:17:01 +0000

    often rarely condition drawing whistle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
