3d02dffed044190064c6ec4b24b2d169739f90aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:00:49 +0000

    organize tread support form police

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
