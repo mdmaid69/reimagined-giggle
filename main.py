ccb4b9b0e3643062d1c838cfda43f60341e61cae
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:11:34 +0000

    press Ll field offer warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
