9a1ff182e7c7ecea57bda993af0495fee2745c1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:15:15 +0000

    warning disappear home hard think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
