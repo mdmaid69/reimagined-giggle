23b49ce277c193f0849c42010b74ea1ca83b9238
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:38:34 +0000

    yours man suggest either print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_string_from_array(array):
+        return array.tobytes()
