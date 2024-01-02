5d93c1a880b101b369ddda84bf1a6642e7e4bc61
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:23:35 +0000

    wing lock regularly else private

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
