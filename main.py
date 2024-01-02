bb3267b80e52206cfb15163e4feaa7bd8a1e3fc2
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:36:04 +0000

    could message that makeinto cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
