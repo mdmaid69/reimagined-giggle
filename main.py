66d9b24494f92539bdfd41a228ae978a3749a554
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:02:48 +0000

    beside loss together comeback think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
 def set_array_slice(array, i, j, iterable):
         array[i:j] = iterable
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def count_elements(lst):
+        return len(lst)
