9a5e75fa3c7f3209b21a2a5e2827dd5a961d8e83
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:49:48 +0000

    giveback trial ill typical prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_length(array):
+        return len(array)
