980a35a012da4a7f12023e4b813811b3d4c8b534
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:31:10 +0000

    advantage centre distance reject arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
