f05efbbf6dd45fabdbb56722044a2b5169f6ee01
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:36:57 +0000

    might conduct float similar stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_username():
-        return getpass.getuser()
+import sys
+print(sys.version)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
