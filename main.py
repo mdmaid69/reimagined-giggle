1677851cfa81e51d8c9d4c83b9af8e4fefdf9183
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:24:17 +0000

    correction little rush combine wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def is_palindrome(s):
+        return s == s[::-1]
