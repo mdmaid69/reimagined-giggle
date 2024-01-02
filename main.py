64f5d8c9717a658ef6bb7062de2da5825885758b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:59 +0000

    wander dry calm my muscle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
