8f8629e49c83c65116da0220d8066a9274a68550
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:38:54 +0000

    unlucky lock fond direction win

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
