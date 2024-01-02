e9f492a6c149b738db4ae6ee191dcf2c056dec4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:59:03 +0000

    brave spread use last petrol

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
