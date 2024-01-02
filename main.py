aa76bfc9692da84d4b23bfd88bd44756fbd9cf66
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:18:06 +0000

    list range Jj guide correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_area_circle(r):
+        return 3.14 * r**2
