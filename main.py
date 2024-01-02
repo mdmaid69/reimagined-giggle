6688e9b89c8d13b0bb1f6ba71496cd8b99a0cb2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:35:23 +0000

    then convince turn inform notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
