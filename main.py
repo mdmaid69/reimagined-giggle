3a3fd4e3493f6685df578404f89dc39e385a8a77
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:22:22 +0000

    glad government rid share foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
