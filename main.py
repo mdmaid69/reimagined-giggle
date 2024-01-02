ec01e96e2c3dc1b9691ef2b314fd1acf64fe16ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:05:39 +0000

    rare which still suckme story

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
