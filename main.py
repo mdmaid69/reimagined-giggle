9eea49aa88a56439ffbab4a811bb131d1ca1ff21
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:46:08 +0000

    murder plenty direct tomorrow direct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
