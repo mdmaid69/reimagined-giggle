500cf08ec3b08de218bd6467c4a24fbb2c6fbc5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:49:13 +0000

    fill him reliable Mm hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
