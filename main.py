12b888217e28090432645af92a87061aa20265ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:45:29 +0000

    exam sea care honest stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
