3ed1711a1c343438d87a9bff1043703bdd19b265
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:13:27 +0000

    loud maximum statement makeup furniture

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_repr(array):
+        return repr(array)
