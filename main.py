dde9796907d32a0d501a68397b9c6ef44893b326
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:41:18 +0000

    OKorokay send order know create

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
