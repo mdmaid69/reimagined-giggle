15bb6284ec93886425a2f7c5a5ab81992be4c13c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:15:38 +0000

    new Cc formal buy direction

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
