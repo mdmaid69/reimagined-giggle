bc8ab1792164d1d17d14446a5388883c50b714c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:14:27 +0000

    doubt passage as smart good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
