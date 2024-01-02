300bec28eb830db09278933bfafe5b370249626e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:08:13 +0000

    dead pride pretend thick apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_exponential(x):
+        return math.exp(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
