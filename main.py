640840d7feb05a4face70a0be4394ca6d859efd6
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:43:54 +0000

    sorry air gather violent shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
