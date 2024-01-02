aa959ada7fb4cccce8fc9dab02f321a35e68e237
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:47:38 +0000

    comfortable Ss direction grade note

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def subtract_numbers(x, y):
-        return x - y
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def square_number(x):
+        return x**2
