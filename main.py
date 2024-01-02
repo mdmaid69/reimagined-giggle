fc24865347b1698f3c0a4a4fda3520974b5aa0f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:28:30 +0000

    workout who actually visit mention

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
