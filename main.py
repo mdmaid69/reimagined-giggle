1d7694104cdf253fb09c0eb3d308ca75d0c99f93
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:45:39 +0000

    ahead wild claim be set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
