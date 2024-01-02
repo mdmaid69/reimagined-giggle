dce14b232b97b3cb9038233e11afe7a0c5c244f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:09:18 +0000

    learn condition this comefrom move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(x, y):
-        return x + y
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
