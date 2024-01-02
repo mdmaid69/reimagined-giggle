dedc409d6bb7518974fe467ae63e6a2c62a83e26
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:52:07 +0000

    wire turnover state than sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
