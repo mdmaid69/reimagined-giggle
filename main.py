f1529943a9f649d7d829d529b8a1b90a520db4ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:26:26 +0000

    up except detail treat pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
