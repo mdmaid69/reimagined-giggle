c181d329a5ee7332a603fe8bf91aa830f4e9fa9f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:10:20 +0000

    near right apparently statement flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import os
+print(os.getcwd())
+import logging
+def log_message(message):
+        logging.info(message)
