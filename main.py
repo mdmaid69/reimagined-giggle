f02db6310ecfa405d2804795ab784f9ac7c956e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:21:14 +0000

    average battery organization bank sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
