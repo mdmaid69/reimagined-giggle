538383d83aa6d896c2a003b37fa1bcd6d74bf32f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:44:57 +0000

    borrow main hit any respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_acceleration(speed, time):
+        return speed / time
