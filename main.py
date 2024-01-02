1d7cd89940d6da1fddcaf90d36aa681a8db8ec53
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:17:25 +0000

    progress brush actual furniture sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
