487604da4b632e6b76102e359a385855df49e639
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:09:14 +0000

    Nn work free artificial deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
