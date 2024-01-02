c42c20bc184a62ae5080ae8df9be5c7233cace06
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:37:23 +0000

    race fire patient roll situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cosine(x):
+        return math.cos(x)
