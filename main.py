f3e91f6a1de0fbbaa2a97c5e57b35213b236306b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:37:07 +0000

    think avoid piece shallow plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_array_to_unicode(array):
+        return array.tounicode()
