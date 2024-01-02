f2764d26756b8773ce79a16521b198c016b954df
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:03:51 +0000

    argue yesterday estimate slice arrival

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
