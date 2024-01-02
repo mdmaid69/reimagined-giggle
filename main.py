1ba59da61cd6710a9b2a29415675cf4a936beefd
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:55:17 +0000

    someone taste major wheel concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
