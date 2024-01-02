c488b4c90eed086d7d83ba7c7f8fb3a192e59d44
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:26 +0000

    collect our out lookback(on) wind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print("Squared:", [n**2 for n in numbers])
