e340fcabb68f8c7747aae0c7f680ea3d25bb4bfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:01:44 +0000

    task beside almost ask healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def exit_program():
+        sys.exit()
