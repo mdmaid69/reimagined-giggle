480dedbcf665d293ad9bb7d98e05ecc0ac376a69
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:08:52 +0000

    566end distant connect only rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
