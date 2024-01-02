54e95a1c4a2af31fd70e905e13ecc6db9f56d749
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:12:18 +0000

    probably bridge strange find rank

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
