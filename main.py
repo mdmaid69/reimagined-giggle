8c9248f93eabf3e614d83c010aa8b81ca3c59cc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:57:12 +0000

    evening risk moon spin thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
