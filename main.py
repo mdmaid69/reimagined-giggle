3862f4c0cea7cc1e0b3919bf9bd0bc0409bd648a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:39:42 +0000

    please foreign exact loose train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
 def append_to_array(array, item):
         array.append(item)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def reverse_string(s):
+        return s[::-1]
