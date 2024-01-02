4d9989289dbbd30308cff072134b1a2950561e81
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:58:36 +0000

    might thinkof escape wish comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(numbers):
-        return min(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
