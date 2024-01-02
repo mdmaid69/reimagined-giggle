e063c14b648cbd8c1b0c2ae32a370e844f204d43
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:45:27 +0000

    compete disagree kick strike start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import sys
-def exit_program():
-        sys.exit()
+def remove_directory(path):
+        os.rmdir(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
