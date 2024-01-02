a8c6c232776b57f960aad110f4781417c6e2f5ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:26:26 +0000

    stop call command add thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
