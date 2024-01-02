43a5ddd16e36e3d5772b1d2e89a28251dbde1acd
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:50:31 +0000

    crash feel flow thinkof serve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
