41f66618435bc78f947fb54d60168cb76ddba925
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:21:35 +0000

    say insist which trip small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
