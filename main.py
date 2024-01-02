42e7805f64063a7e92641942672d37a2ca5f33fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:21:19 +0000

    cheque upset slide usual separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
