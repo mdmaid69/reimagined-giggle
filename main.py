85c5e2d77febaaf602fb0c70b10d458e04b3de0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:27:52 +0000

    legal pride rid prevent string

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
