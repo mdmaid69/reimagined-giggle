fdecd80c73137ad5635c805885c631841380a743
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:00:18 +0000

    moon sad around worst nurse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
