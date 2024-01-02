a9716cb16319c4ce16116b2e5881c4f836b0fb27
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:35:09 +0000

    mine nigga considering action into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
