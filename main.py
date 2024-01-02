cd6f4ed9492dbe3f8d75f57c579b60782637f86a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:40:10 +0000

    border size problem urgent record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import tensorflow as tf
+print(tf.__version__)
