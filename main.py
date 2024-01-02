410937cdf524e4f73a858b779fc05fcc7b62338b
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:22:07 +0000

    person winter control sale little

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import random
-print(random.randint(0, 100))
+def subtract_numbers(x, y):
+        return x - y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
