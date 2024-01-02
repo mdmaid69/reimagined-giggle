10f5b158714dfaa5532f207f86c3bdaea2a1186b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:23:24 +0000

    wave pot chase absolutely hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
