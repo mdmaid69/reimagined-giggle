b17894ef23ad3f3c7f66d3ac3ff10e0633da6d74
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:47:04 +0000

    sea shade prepare ourselves whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
