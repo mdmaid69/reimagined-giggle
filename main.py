3108ab9945ae4e1e02ca51dd94639775192cab6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:18:19 +0000

    issue breakdown available passenger want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
