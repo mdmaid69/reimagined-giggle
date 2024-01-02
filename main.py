b7126974efde7d87069ee38c80a5014d8b865115
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:48:42 +0000

    number save move calloff turnover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
