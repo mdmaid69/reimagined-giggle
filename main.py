4ff554d345324141ca145bab0b2d4230e1ac4694
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:47:54 +0000

    schedule load past open port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
