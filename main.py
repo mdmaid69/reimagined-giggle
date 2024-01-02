bc91e1a5e1add6d677ed995d9376abf7a517a31e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:51:37 +0000

    near central famous bear rest

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
