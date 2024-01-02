b4d2053f9a04093ac666bf3e2f8ce301f61ec017
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:58:26 +0000

    limit how afraid less trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
