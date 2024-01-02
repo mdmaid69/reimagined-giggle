c98a301526aa4430047f7ba9989cdbc0c0f2dfa9
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:56:23 +0000

    boat excuse bar peaceful affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
