a696dcda178410ad97fabee0be340dcf0dfa39d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:05:13 +0000

    born partner rather battery favour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
