ea1af228a2a5448e02209f98f23cd34af1d678f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:59:57 +0000

    taste question debt figure train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def remove_duplicates(lst):
+        return list(set(lst))
