6a4fc05c605256030c4e3fe0fcb981f5e5673db1
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:44:54 +0000

    recover fresh everybody culture window

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
