b1a82b6a18d737d3319f07b84e48cd81c88653ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:04:28 +0000

    invite convince town mark pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
