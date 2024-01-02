8fc24871405e876190d679f5b96cd9639198a1ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:41:05 +0000

    usually stir entrance clearly just

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
