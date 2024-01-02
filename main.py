bbf98c6198b504cb8ffae2c00029caad8809a7c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:46:14 +0000

    poison rate sound pinch keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
