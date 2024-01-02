1683f1c504ced73eef33eda58fb740184b93eccf
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:18:13 +0000

    draw later ill regional strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
