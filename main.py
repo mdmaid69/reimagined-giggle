d0c496408413288918515258c32effb903e593ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:30:46 +0000

    you this advice radio house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
