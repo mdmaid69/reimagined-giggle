47e7bb129a3cd6692d29f36efef82a5ebe88d06c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:38:50 +0000

    nor careful week turnup tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
