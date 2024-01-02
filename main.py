a49f9a42e2898c740b73d1003b3c235621aadd3a
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:52:51 +0000

    challenge repair repair reaction fairly

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
