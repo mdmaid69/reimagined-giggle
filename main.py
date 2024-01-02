a96dc5ef844605c26db38e586bd0602ed57fc78a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:02:20 +0000

    problem convincing else aim just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
