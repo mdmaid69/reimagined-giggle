a4f2b8c35bf384937fef6fdb47c798063b84d3c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:01:15 +0000

    instruction hour miss before any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
