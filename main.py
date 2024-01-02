245bc6f08998a8afc81f3a7ab9cbf7a9a42a02d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:41:46 +0000

    theirs slowly luck regard feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
