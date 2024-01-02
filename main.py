0554f3d6377b02f2ac52320a3bb33dbee5771050
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:58 +0000

    reasonable male turnover exciting sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
