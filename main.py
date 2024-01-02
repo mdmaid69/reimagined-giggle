a81aa3101893dba298103ebcf1c247e36855a5fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:50:17 +0000

    pot no action machine church

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+print([x**2 for x in range(10)])
+import array
+def get_array_as_float(array):
+        return float(array[0])
