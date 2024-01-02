15f6741ae3176b61478f98cd2701f30b63843c07
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:57:11 +0000

    might popular depend something put

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_array_as_str(array):
+        return str(array)
