8475a723044e0883f31f966b11bcd2c554f2a4e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:28:49 +0000

    reliable expression least backwards hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        print(sys.version)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
