76a5694e7b766ffe5181de08fed6831fbc770c15
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:18:51 +0000

    owe kind attractive spare light

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
