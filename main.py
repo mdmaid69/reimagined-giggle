7a89fa7b55762d76b4714fbea1a50aa62b8551eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:49 +0000

    file leg tax hot accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
