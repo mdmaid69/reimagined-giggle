65e7881386046dfa2b9aa60acf573d237abcc77a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:46:33 +0000

    account wise independent because deliver

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_odd(n):
+        return n % 2 != 0
