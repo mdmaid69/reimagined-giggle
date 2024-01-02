43dbc8a30ab159c01c8977a3f75b96292d82f593
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:20:46 +0000

    pleasant right possibly demand generous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def is_odd(n):
+        return n % 2 != 0
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
