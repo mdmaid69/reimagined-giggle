1232f8125d46a1814af6ac8e9533eb28af7d2735
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:26:21 +0000

    second responsibility turnoff while accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
