5911ac62db7acc5cd425a1185be4c42e0724598c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:26:41 +0000

    cause shine offend copy tired

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import time
+print(time.time())
