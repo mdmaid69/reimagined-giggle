e450d4d07d3af6f85e5b916ff4cfe4593fb3381a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:32 +0000

    present stir release pronunciation Ww

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_list(array):
-        return list(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
