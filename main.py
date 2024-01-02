43cc820efd604b033b2ffc791c5533239ac05dc1
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:15:47 +0000

    lesson space throughout young guard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
