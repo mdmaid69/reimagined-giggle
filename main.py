68695d50d9e120d513174cf7b23705a00bc39903
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:01:46 +0000

    trade balance classroom weather far

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
