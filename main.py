631d74762fa74afaa86f28274e9c32faaefca2a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:16:33 +0000

    suckme regularly lock lead quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import numpy as np
+print(np.array([1, 2, 3]))
