a75f9c02f696a708f1d78056dbca36323e07ee72
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:59:13 +0000

    join rather smile dry opponent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import time
+print(time.time())
