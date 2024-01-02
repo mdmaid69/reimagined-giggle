46650aecd5b22507ea1279548dc7f1d6d5755378
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:23:54 +0000

    service relationship comfort rescue proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
