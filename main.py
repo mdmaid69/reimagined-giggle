95ea4b8115e56273e03828db7355c74bfa59ae62
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:46:10 +0000

    scared name wide fit fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
