e47b21b725e4a70cccbdbdbe9d26218539808614
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:20:57 +0000

    any pool soil later chief

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
