f46e0f98abefce2cd5aec932981cec25c91984a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:07 +0000

    proof free movement about scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
