8ed36b129d9d27342b17fbcfb1d20982961b2e78
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:36:10 +0000

    steer suddenly actually flood week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
