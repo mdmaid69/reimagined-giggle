599f1ccd2fdd4f7c2c9d8bf9108fea46131f78ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:30:29 +0000

    stop amount often dream turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
