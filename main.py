67e6100232a64c758fa55db280084becd7304096
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:11:40 +0000

    exaggerate emphasize vision gate instant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
