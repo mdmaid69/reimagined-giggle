57908486874346915d3aec2c25825024b8c2db55
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:07:47 +0000

    interruption tongue sail together major

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
