7678c99aef3844cf6914a9c076e286e39202d4f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:09:14 +0000

    slight copy ought whoever thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def remove_duplicates(lst):
+        return list(set(lst))
