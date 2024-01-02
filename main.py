6e140268c5cb17649229d6a5a43025e3720334c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:40:10 +0000

    task Hh body gentle cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
 def check_if_array_contains_item(array, item):
         return item in array
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_speed(distance, time):
+        return distance / time
