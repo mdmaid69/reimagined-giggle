cbb374902e1255fe3f855a5018691df5655ca52d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:46:27 +0000

    catch question expensive wing this

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
