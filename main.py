bfc04761edb484b62b6d27e70bf77e211bf0118f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:12:08 +0000

    callback no listenfor 571enough extremely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def sort_list(lst):
+        return sorted(lst)
