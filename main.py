8fb17f1ff451e7f176d1429291d69f67aabe5138
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:58:53 +0000

    add friendly goon note professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
