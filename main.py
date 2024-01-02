63fef5421954fa2b04bb732a332f92f7b5c5938a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:52:45 +0000

    communication sort alone bored produce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def find_min(lst):
-        return min(lst)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
