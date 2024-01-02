1c6de415160c74fb7e326df6dccb2546d6b499fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:19:58 +0000

    which capital apply poor dare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_work(force, distance):
-        return force * distance
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
