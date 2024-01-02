95f37ac3556f17679d89b2c08baa5759c744b4fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:17:20 +0000

    favourite suppose dry aware foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
