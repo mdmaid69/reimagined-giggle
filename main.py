3bf049b5d02b4abc935c4f4b8600bb7243935cd6
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:26:16 +0000

    education load main what again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
