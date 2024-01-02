7cd53ffbe0cbefa6f6e710492eda81941aa5d0bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:19:08 +0000

    choose attempt container debt chin

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import tensorflow as tf
+print(tf.__version__)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
