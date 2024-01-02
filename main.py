1134d36e9eb8afd5378159558f062148dc63eed9
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:11:19 +0000

    split circle taste prevent free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+print("Hello, world!")
