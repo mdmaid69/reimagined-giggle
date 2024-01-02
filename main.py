ad8a79a4fbb3e48068b2f349a8175e87bdb374b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:51:29 +0000

    convince appearance cold welcome charge

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
