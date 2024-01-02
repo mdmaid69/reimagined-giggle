71109c687cfe743838aa76d96f39096d8c5562ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:33:26 +0000

    there equal thumb before paper

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
