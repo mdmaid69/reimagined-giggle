66a09b2375cb5077cb06b68b21c935e509ebb558
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:43:26 +0000

    concentrate drop city band pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def reverse_array(array):
+        array.reverse()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
