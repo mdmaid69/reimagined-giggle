ea381ebd4bd928929363740914d15982c43372c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:11:21 +0000

    improvement everybody depend spray quality

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
