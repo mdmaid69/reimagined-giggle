fdfc63ce05c340528f7ccd8a9fecdc3ba9b01529
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:12 +0000

    joke break imagination sister damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
