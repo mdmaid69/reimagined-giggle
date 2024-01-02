b73fa5527a981b55ff36738751e51ff3989942f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:53:10 +0000

    display taste beneath stare getaway

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def count_words(sentence):
-        return len(sentence.split())
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
