c9a809480ab1eea26d3608d4df65deb9cc8830f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:24:20 +0000

    runinto late mine though wire

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
