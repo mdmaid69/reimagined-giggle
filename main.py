0f8eae0ca77c06add5c855b371f28a9b0f503ea2
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:40:54 +0000

    try comeround introduction each part

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
