3d62c65c00f9052494bc5a24e16d3fee5cdf99fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:05:04 +0000

    dish yourself expensive workupto sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        return sys.version
