29637ce07e7a732badf2012abbba9201f0a08a9f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:02:52 +0000

    phone all blame feeling brave

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
