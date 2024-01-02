2e9abc5e3956c1a0b69b2f5963299af59564915e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:53:49 +0000

    rank switch spin escape scale

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
