3a0db963d3a9e10056795c8b15adece4efd7509b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:15:25 +0000

    why bottom turninto giveback label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
