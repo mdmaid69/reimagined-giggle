6085ce352f023362f92e0aa9b08ebe19413334ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:09:54 +0000

    talk priority willing steam blame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_set(array):
+        return set(array)
+for i in range(10): print(i)
