3fdd9fb05e9109dd335a54e33c385eeba5f32fff
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:29:13 +0000

    between pleasure address well Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
