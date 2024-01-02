1fa1dae038ac7558cd4cb64ffbdcb118163a8f49
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:52:13 +0000

    dig concern journey clean far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
