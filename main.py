a2d44a8be46c5f7c3b8e2f95b03452d770eae835
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:22:45 +0000

    description address someone effect crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
