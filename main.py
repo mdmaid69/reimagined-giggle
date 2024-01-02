4c9b6a0444fce5b5dd8af2ad0d1bc36ba3a6924f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:34:38 +0000

    instant eager emotion tone uncomfortable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
