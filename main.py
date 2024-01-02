a2b1f782d834b1d0a4feb84e58b06a8df6f22161
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:57:17 +0000

    basis different that factory chase

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
