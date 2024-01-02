1bc6c47b9d21fd6f5824f7ef3fbf9424f69bbeb3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:46:07 +0000

    rain duty flat but giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
