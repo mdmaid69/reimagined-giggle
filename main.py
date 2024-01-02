480500b7f44191f1ec005df2cfa6b310a1a3f7a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:47:57 +0000

    field needle lesson suggestion government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
