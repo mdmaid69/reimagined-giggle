27b5b7530fb64183bdd3b3ef321981cdb6f9ed43
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:59:48 +0000

    introduce how reserve now away

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_force(mass, acceleration):
+        return mass * acceleration
