366495f4c5ae8e66bfec46321825e5b4a9e46e1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:17:19 +0000

    frighten room without exercise pour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
