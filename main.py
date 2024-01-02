90e44da0d38f0f52f45dd9455cbf1fe1f154f868
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:53:33 +0000

    ride best instrument nothing unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def count_words(sentence):
+        return len(sentence.split())
