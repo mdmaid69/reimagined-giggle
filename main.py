051fd604266562637a9c92ed212ac845b2763bbc
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:12:18 +0000

    word introduction almost difficult mood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
