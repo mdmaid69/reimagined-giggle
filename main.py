6cb8d1402c1f19d97ac5ee51aa87f3a972d85071
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:01:15 +0000

    wing available weekend early whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-print(math.pi)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def subtract_numbers(x, y):
+        return x - y
