2bc2c5d0d45573a514ea6f1cc78b80f83fec4c2d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:20:18 +0000

    steal owe region possibility emergency

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
