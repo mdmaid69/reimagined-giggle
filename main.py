762bf582a4fa7a223bce786766af54beac0edd12
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:26:47 +0000

    wrong tear moral minor achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
