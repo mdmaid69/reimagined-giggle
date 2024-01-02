3b81b5406ec90db5b26bd8e599ccad8b9fd698be
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:35:01 +0000

    touch tall present follow anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import time
+print(time.time())
