a70c65d745a6a5afa7fbf2ffe7599084038b925a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:25:07 +0000

    equal it usually pot bury

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
