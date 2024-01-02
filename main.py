83aa9cd6e89817137291a89f259195bd5f8de50a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:04 +0000

    circle present indeed trade oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
