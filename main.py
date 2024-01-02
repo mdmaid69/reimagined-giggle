9ff19c55e087ee5bb5a497ea6d325fe8852e9ba4
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:47:20 +0000

    frame possibility cruel move acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
