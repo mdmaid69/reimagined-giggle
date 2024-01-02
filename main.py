eea78e124d6b568f7313c1f923780389b4d65ddc
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:37:06 +0000

    crowded whisper goout tool meet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
