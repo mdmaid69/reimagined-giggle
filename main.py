862ad678be359557d0301050580a1ef322b159bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:11:34 +0000

    balance benefit comefrom solve match

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
