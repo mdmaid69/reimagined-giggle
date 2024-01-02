0064b18ae01e110c52180f6394745474bb56f861
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:17:59 +0000

    keepup married prove ring satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
