f85d66cd7b905febe8133a21921ca9ffb6fb596c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:35:10 +0000

    deliver party turninto takeout book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
