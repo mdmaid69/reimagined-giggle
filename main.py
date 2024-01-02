cf2104c11e92ccf1a823555e2dfb92efc0ca5d27
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:29:19 +0000

    aim suggest ready free disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def split_path(path):
+        return os.path.split(path)
+import sys
+def exit_program():
+        sys.exit()
