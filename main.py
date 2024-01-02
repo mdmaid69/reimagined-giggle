9f7b8ba32ab0d81c8c2e5806316d33d2ee60a70c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:01:34 +0000

    shout fit hurt small furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
