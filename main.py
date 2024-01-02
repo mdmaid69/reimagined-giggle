612d2d2de205ad15f9d0af9ab702b5f4355b0dc8
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:43:18 +0000

    importance none deep common check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def cube_number(x):
+        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
