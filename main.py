4c36bf1bea22e531155ba372f6f44c7cb1d636b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:28:54 +0000

    past equal disturbing tomorrow basically

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
