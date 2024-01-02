0f93e6c197b3c86dce6ee59c83e84ca03581a868
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:21:54 +0000

    scream somebody scream quit determined

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
