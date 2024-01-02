b3138f5e0d6b57b6a504e7ab77426c1bacbeebf2
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:08:51 +0000

    cruel middle own gooff terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def get_array_as_int(array):
+        return int(array[0])
