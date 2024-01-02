29f28bc120039cb1f73ea8d3f4f855a1f705aeb9
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:22:50 +0000

    language one bye hospital however

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
