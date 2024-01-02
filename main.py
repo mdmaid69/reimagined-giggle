a707f42e992ae7610431fb0586a3b15e63ab901e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:55:29 +0000

    advise as be impression after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
