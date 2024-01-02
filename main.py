938e354f0e33de5a2b9f77e25b09a4824c70d682
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:06:51 +0000

    equal major risk anything sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_repr(array):
+        return repr(array)
