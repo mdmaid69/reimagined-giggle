598b732e29585d8f79853177bcae4e6672bce5f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:10:28 +0000

    Oo month basically runout this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
