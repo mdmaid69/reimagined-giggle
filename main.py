f75ebb76e0f0a3aae9d0790c92eb8c4a2cdb3540
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:20:58 +0000

    argue closely traffic culture fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
