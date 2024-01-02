76b46c9e0e20246023a4f1df537d08eae72404c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:45:15 +0000

    day catch respect skill approval

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
