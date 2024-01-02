1e96917b1441da668ea475294e69bb29d8d8462f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:20:31 +0000

    perfect pool underneath guard fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
