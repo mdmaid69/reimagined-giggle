dbfd610d184a7105a9ffceb66eca498e92623c02
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:56:15 +0000

    definite left along your call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_numbers(numbers):
+        return sorted(numbers)
