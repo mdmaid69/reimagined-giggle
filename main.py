7be2d479964266a79c27f75a50d1fbebdcc0e2f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:41:56 +0000

    mind easy Jj town nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
