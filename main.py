0ccd74cf0e645f410a119fc3b4f40a3c25b02411
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:42:09 +0000

    turnout Qq position fashion there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
