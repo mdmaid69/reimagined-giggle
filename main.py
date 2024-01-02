ed9ec02a94b4bb8e3069f5121b0700b137ac3f50
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:13:10 +0000

    shallow nose damage oil original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
