bcb8fdd77406330658d9d8818fe3a962c28db82a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:30:07 +0000

    team of type trousers force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
