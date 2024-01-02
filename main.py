01963c251ecc0e106cd254936ce0459060ea57d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:11:17 +0000

    left hurry moral deliver please

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_array_to_unicode(array):
+        return array.tounicode()
