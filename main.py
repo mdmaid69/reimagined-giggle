24c23627bd9d3d01ffbf9c4b269e03d0330bec67
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:50:41 +0000

    left that callback case strange

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
