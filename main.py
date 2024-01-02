1bfb87e89abcc9b323b12c82e8afefa5c9a8652d
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:37:22 +0000

    height near steam still excellent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
