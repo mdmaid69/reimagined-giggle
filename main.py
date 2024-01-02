3632b4d66fda521303e24ac50ac9b02eeb419e79
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:04:06 +0000

    far independent plenty statement school

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
