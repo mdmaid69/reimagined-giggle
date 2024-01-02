92d4b6b3cd5a8a856c113344c25247f726a0182d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:56:15 +0000

    round pleasant runout workout this

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
