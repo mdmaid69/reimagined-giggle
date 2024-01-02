20e4476328e64659bf0305508def2ca674ac3910
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:46:19 +0000

    consider school although fly Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
