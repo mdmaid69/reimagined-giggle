4fc34d98b6532a110935294ed479b4f36ea79119
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:14:16 +0000

    quantity degree such wipe capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
