bba78c36980d13f99981ecf46cbbe3811ed56ab2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:56:02 +0000

    wherever study back wait plan

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
