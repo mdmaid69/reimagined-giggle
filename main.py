d8f3ef1110033ff6fc7a2e5cfba53b10ff10cc2e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:06:43 +0000

    question angry clever scheme receive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
