697d7eec1dba632497caa1a2abc5c0e822cdb1f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:45:34 +0000

    angry describe material poison attractive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
