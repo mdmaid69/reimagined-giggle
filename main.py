c7cbaa8375a7a3c5b8eea30c11473b488c31c10d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:21:55 +0000

    check sink equipment gift or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
