8ecffb92c9fe903aa400982d850fb0e100360025
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:08:45 +0000

    spin spend land match target

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
