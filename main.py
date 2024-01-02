8543dfbfbba0be9222aeaa83e2655b15fb5be7d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:12:58 +0000

    same team seem sky goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
