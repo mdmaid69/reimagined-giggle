7ff8e7b1b8378d47fa24c79779a066b6cef5b38c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:24:58 +0000

    angry necessary surprised statement depend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
