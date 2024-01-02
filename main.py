1f8276d502f2d46cc97da4c8ec84f695481261de
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:37:16 +0000

    surface sea callback shout equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
