26e4c9d82897729c293acce74c51f3b76b14fe73
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:25:53 +0000

    excited cum mine strength ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
