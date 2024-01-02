0567f0b3f23569ba83c1f876b6d23ff8ca2fb0fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:42:15 +0000

    equal sweep lookback(on) meeting passenger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
