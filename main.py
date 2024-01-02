4346625d522855078943bb3c644601000d83b518
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:50:12 +0000

    prevent detail lookafter rush wealth

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
