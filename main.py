b4c291d8b3a677a50f71b5a703f0440c5fd7f01b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:00:13 +0000

    game female here could match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
