cbe8fd6c40aa93b732906811bc7b6437d6e6fe6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:02:47 +0000

    come into anybody lonely so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
