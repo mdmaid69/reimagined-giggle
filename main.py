dc2a387929c5dddc8f6f55757340da1bc1e7b1e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:47:33 +0000

    increase arrival run to influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
