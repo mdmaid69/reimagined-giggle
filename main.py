f854e67e78cc4925412753f068615a10f935035c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:57:35 +0000

    class come sign middle battery

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_item(array, i):
+        return array[i]
