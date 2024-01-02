728ccc370fca537e8d22629980ca993ce9cad415
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:01:29 +0000

    roof discuss female us chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
