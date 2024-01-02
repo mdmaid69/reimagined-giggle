9b759968cb72a9f3f0af77e1cb3276a556a2ff5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:00:57 +0000

    service speak period offer check

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def convert_to_binary(n):
-        return bin(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def remove_duplicates(lst):
+        return list(set(lst))
