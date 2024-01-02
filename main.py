bc9090467d4a8021d5334d66bef919afd52cdbbb
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:57:51 +0000

    shade main tip declaration seem

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+from collections import Counter
+print(Counter("hello world"))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
