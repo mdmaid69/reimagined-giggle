2f60a549dd4c489166bb8862cfad0b7f8e24c572
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:06:26 +0000

    right bunch Bb accept from

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print(sum(range(10)))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
