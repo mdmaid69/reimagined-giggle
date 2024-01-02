ab2ee8505fbb564d0c271bd75cc6aa1dc831e069
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:06:56 +0000

    test habit much sky waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
