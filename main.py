0bb0d0b3e0012026713f4468cf6d84a875d2356c
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:49:42 +0000

    getup most workout eat Bb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def cube_number(x):
+        return x**3
