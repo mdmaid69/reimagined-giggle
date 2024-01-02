e18d4eb15b00a7a3dacd158d2afcb1d1076d777d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:37:41 +0000

    difference entry somewhere crash confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
