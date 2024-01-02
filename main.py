05356640ad1b5a1ee5f1660f4462109783d32a91
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:05:01 +0000

    sea march lean out pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
