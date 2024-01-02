259df89c38c4928cb8545d2048f5873d9f6d2551
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:38:14 +0000

    over exactly rough represent sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_as_list(array):
+        return list(array)
