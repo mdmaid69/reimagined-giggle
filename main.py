94fef4f254c50d6841baf37c03c7c381576d61e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:25:18 +0000

    free get dark reason interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_sine(x):
+        return math.sin(x)
