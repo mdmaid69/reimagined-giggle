1781f884b2da8322bb0fdcef0386450164fd3158
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:40 +0000

    rest sense hospital vote door

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
