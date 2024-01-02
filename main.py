0959f24401c3396c68bc71871db32b1449460a71
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:29:04 +0000

    ago still definite towards result

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def convert_to_octal(n):
+        return oct(n)
