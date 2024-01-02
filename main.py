ef3eeca6ca5154799de297dd469b5701fc25a395
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:59:48 +0000

    delicate passage news coffee cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
