6dd8c0fd232141416a1b13d99d7772cb91257906
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:23:43 +0000

    minimum alone influence family workout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
