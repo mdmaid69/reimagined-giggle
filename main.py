ede8b9c3e762d11a62d764e1a82e7d2386caa5e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:00:15 +0000

    recovery unlucky close Itself way

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
