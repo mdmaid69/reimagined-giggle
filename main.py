b94a7ff38805b330a1c8d9717ffa53c343f826e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:23:31 +0000

    left at possibility hair crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
