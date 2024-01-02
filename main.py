eb30f431fa2c191eca01caff2bb06d2a4d247875
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:29:07 +0000

    flat free somebody order nose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def count_words(sentence):
-        return len(sentence.split())
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
