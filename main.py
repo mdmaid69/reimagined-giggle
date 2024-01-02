b5603d8d90ca488dc7855f8e69e309574d28a811
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:48:58 +0000

    temper independent wish season eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def subtract_numbers(x, y):
-        return x - y
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
