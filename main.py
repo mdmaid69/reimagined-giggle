40c29970107999877967be7f98c14a3caab3e140
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:46:07 +0000

    real spend punishment imagination relation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
 import random
 def shuffle_list(my_list):
         random.shuffle(my_list)
         return my_list
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
