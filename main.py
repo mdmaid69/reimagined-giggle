d1aae378e6b29d1d4ae37f67ad6dc2220c602cbe
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:09:11 +0000

    block upstairs each cheat uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
