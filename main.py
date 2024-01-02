9b88076abcc3b5d42bd6602703476500e936e694
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:02:41 +0000

    light recommend on short next

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
