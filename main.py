92358b39dea3cf83aae26728800c44696a13ef2e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:44 +0000

    otherwise difference law past care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+print(math.pi)
