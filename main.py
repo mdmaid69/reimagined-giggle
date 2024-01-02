0c39f1a36c7f0661ec534006e1dda91b01123029
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:37 +0000

    careless Ff leave goon obvious

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
