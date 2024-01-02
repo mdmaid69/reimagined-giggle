2bdbd900c880aca766d8d1c55224432f0202ffe8
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:01:47 +0000

    exchange damage success valuable pretend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
