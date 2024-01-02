3c52e8e90c878b98a58714a52a71e9015d180cd6
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:00 +0000

    thinkover pain criticize towards under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
