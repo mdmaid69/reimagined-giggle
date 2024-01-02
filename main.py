886ac8cad78001f99b9ff3389afb01bc8660b09c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:17:59 +0000

    organize below opposite style prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
