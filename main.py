241d58b7cce218c3dfb4de6199e5989df2435bf4
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:19:17 +0000

    takeoff taste post father mistake

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
