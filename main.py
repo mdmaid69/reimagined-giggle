7e8dc99d6c7d9e9940819da7dc5a0bac2d41adb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:01:09 +0000

    miss slice tear through brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def reverse_array(array):
-        array.reverse()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
