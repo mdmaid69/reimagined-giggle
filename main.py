26ee67e063a1a1add11874206f8e519c78b5289a
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:42:14 +0000

    maximum separate giveout comein mail

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_max(numbers):
+        return max(numbers)
+import platform
+def get_os_info():
+        return platform.uname()
