cbc682c59fce286fa50b72f9f7bddf94e5e26b66
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:32:26 +0000

    swing this gift tense breathe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import datetime
+def get_current_date():
+        return datetime.date.today()
