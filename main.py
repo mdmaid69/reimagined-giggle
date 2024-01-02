ddc8144520a00fa7a4db6f7739f9a4873dd67f14
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:52:10 +0000

    safe bend emergency lay bottom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_speed(distance, time):
+        return distance / time
