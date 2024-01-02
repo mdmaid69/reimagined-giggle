2555d7ab1b34a91d92112329b5e805eeef0b94d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:02:47 +0000

    exciting quality thank giveaway man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
