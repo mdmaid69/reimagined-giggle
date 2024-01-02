ed8dc9add587deb254164c95518660a946f963a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:24:30 +0000

    peace phone Uu feed popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import sys
+def print_python_version():
+        print(sys.version)
+def convert_to_binary(n):
+        return bin(n)
