4dd4798d8afa3ce64f7828eaeb015134b7b5ef94
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:41:44 +0000

    journey then child upset wonder

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_float(array):
+        return float(array[0])
