4cde4d30c5c0b154e9bc65795c561a7aa7efa7c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:08:22 +0000

    false equal write image dare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
