0c635ef241e5e00255b1975a118d4a9c97ab407b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:37:26 +0000

    border introduce hear confident detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
