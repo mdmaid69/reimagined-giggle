3f8a6706332199cb08afe52f558ee03e4a35b981
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:55:53 +0000

    spill car control love watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def square_number(x):
-        return x**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
