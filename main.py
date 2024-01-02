3c2a86c905cd19888361c47a5f1b84088d626204
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:01:26 +0000

    use form solve admit connection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def find_min(numbers):
+        return min(numbers)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
