7e6d8b5606ae799cca7155659dc3c641c462b48f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:54:23 +0000

    direct label instrument turnback entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
