5bc9935b109019b418b9ddb5cdba3a31ea4923ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:23:35 +0000

    impression oil vote on score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
