637b85722d7563ed37097b667286431824cbd1d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:51:35 +0000

    tax recently fall occasional flood

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
