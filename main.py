477c13f6f5e979afdf0dccaceaa5343264302ed7
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:28:59 +0000

    secret ocean mine the judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
