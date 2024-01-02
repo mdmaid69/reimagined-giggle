d28e39ed82d671e4345137fbb936f22d780cd979
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:46:58 +0000

    myself excuse play remove 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_even(n):
-        return n % 2 == 0
+  def split_path(path):
+        return os.path.split(path)
