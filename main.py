2b9ce33932b46bc4f6b347b70ff7e46541fa5b3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:28:47 +0000

    immediate protection comeoff sing satisfied

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-from collections import Counter
-print(Counter("hello world"))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
