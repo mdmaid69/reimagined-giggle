37ef392c308adb6e40c5442f5d59a733135926d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:36:42 +0000

    light joke fun later something

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
