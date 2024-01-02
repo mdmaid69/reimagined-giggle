8069ad142201c525e6306d83020e3db42db1555d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:00:53 +0000

    theirs children buy size stop

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
