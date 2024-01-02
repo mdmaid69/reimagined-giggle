3609d9e02391d78ca7e3445dcdd1476a91962c98
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:48:06 +0000

    future lift recent face tough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def find_unique_words(sentence):
+        return set(sentence.split())
