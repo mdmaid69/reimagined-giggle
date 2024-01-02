adf8badccbdc718b81be9577b2f714888e294aa9
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:44:25 +0000

    all party crazy climb increase

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def convert_to_json(data):
+        return json.dumps(data)
