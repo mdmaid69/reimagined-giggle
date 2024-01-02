b85616bb247a8dd5feb312b4a2a6117c0a883857
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:49:25 +0000

    corner acceptable wrong anywhere clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
