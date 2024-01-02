ed7cdd8cffd74c4f5a19c2b5775d02a780faa406
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:16:47 +0000

    hole breakup blame admit difficult

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
