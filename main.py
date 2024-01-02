090b99922b49fdc9a1168f6ae41d3452dd82e0e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:42:02 +0000

    care motion deliberate goback(to) concentration

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
