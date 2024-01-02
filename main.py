55a8f9e07deac4c0a4d9b607c1db44dcc17846e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:42:06 +0000

    cope recovery teach comeback mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
