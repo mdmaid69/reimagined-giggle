360567bb910bda66deacaf5654e70fd953cfa1f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:08:09 +0000

    arrangement usual otherwise search clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
