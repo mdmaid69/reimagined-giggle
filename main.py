165bbb5e1f7acf18de1bde2dfddd949ed077da71
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:08:09 +0000

    understand now exchange rather freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
