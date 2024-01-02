3e2277fa7cde011a36950ea4c0fb3e6d347ff009
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:34 +0000

    very excuse hope and disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import datetime
+def get_current_date():
+        return datetime.date.today()
