57a7a7b8bd7c43f12dc5c311ed20f5e1b6843873
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:42:51 +0000

    present insist wind turnup cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
