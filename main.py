bd9a60e2c9da434279d5dae6149deb4595b194e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:17:49 +0000

    dull work whom blow lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import collections
-def create_queue():
-        return collections.deque()
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
