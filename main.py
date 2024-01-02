1977c7bbe772b7646742998ba31eecfb07adff2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:44:08 +0000

    name stir waste music opportunity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def reverse_list(lst):
+        return lst[::-1]
