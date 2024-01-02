e996fd39c2b4c02d635bc95623370bb35d69e726
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:55:50 +0000

    glass word water narrow polite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def is_odd(n):
+        return n % 2 != 0
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
