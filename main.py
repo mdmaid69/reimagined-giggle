6fd77fc5e4d2141b59a594d519fee0295efb09dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:25:58 +0000

    present service calmly pronunciation getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
