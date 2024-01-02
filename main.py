1c9bcb6eed19c318d7e0d2f2162503bcb87c8d5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:28:27 +0000

    challenge keepup section OKorokay yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
