2e1e809a7fa2c87c15701d1aa93e3483535fbb9f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:44:27 +0000

    favour shape realize study announce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-print(random.randint(0, 100))
-import collections
-def create_stack():
-        return collections.deque()
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
