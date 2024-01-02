3597402a38362cc758e8f549d9552f3a26725fea
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:33:38 +0000

    bottom sensitive direct tool provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_base_name(path):
+        return os.path.basename(path)
