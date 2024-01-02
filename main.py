3c25fa4d0ae0a9fa3823a28dc0528b978bff42f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:15:09 +0000

    enthusiastic grateful reply explain lock

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
