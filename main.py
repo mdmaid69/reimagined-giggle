ba36759e8149d62e982de6e09059c95dad5eab55
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:52:24 +0000

    as dig ring his age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
