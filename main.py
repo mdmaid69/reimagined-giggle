efc2b3db235c78f90b0f2e545b602a89af6d4685
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:49:04 +0000

    high everyone go birthday opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
