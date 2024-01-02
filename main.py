f6b8fd0c75a9dc58c87cfcf42be904cbc7985b8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:59:10 +0000

    tragedy right impressed good rest

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
