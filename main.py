0dc9a3d6ecc8cee66fe9fc865bef76e41fb27a2e
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:19:59 +0000

    rent trick profit shrink know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
