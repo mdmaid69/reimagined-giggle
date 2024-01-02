43f83e1fe70ee680b88a8f19ce3a658e81e09ce6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:44:33 +0000

    so increase minimum dull level

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
