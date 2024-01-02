4f3ed9817d766a91471e71ed93c97472ce735fb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:33:06 +0000

    slowly wave use long giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import array
 def append_to_array(array, item):
         array.append(item)
