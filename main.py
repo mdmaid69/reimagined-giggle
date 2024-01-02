da6d1dfe16986689ac1263d7d56fa34f1599ddb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:41:54 +0000

    attractive land area fly match

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
