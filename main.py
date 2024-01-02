744e575a274d6847291e5c6745af8aa3d8f2c144
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:54:24 +0000

    open connect question post average

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
