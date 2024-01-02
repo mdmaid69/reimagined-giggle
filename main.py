3df650bac2c5d02361921c662e65912c9c793e7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:38:35 +0000

    rent offer cook marry offend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
