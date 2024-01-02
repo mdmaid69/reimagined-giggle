24dfeae93bc4afdbaca376aac64aebe8166453a6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:13:42 +0000

    other delicate over connect polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import collections
-def create_user_string():
-        return collections.UserString()
+def find_max(lst):
+        return max(lst)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
