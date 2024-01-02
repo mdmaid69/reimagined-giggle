114dda620e5f86d8e4126409ebe3aa7489eebd8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:27:31 +0000

    snow identify welcome strike draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
