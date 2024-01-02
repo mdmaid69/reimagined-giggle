01b5ae2d00912bf4258db37973f9c0dca9e5d63f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:45:25 +0000

    happy stop pocket walk top

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
