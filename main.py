54fe56944e57f829fab61850e71c025ce67bbcd5
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:47:08 +0000

    dead understand scene free brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
