00d94747193a319b6e7ace23dd67b5d3fe8c24ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:54:28 +0000

    effect information entrance abroad stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
