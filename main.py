b41a6bf38f8fcb44c775944f3908a21cc35e3874
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:15:48 +0000

    smile payment responsible pencil other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
