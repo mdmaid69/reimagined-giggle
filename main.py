f79f376046d46c4f438c47e0bf4ae45dac72d11d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:31:02 +0000

    turnup glass bringup couple ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
