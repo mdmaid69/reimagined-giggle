0e700cd993976549e72750fc0fa5046b3e53f573
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:49:40 +0000

    lonely pipe cheap allow advice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
