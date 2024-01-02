66ab1f366af73574f45073d29d1141cdb764c571
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:05:25 +0000

    sweep stone eat spare size

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_power(work, time):
+        return work / time
