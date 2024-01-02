181c7ccf959680b54ade1e4982540b1fc9cccae1
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:45:02 +0000

    we serious suckme tap charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import sys
+def print_python_version():
+        return sys.version
