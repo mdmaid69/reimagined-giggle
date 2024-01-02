2b4bcdb80564dd3cb20f4155c0de98e45c0384ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:32:28 +0000

    treatment union inquire open 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
