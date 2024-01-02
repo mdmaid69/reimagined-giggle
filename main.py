fad01fb5aadb28d6725fae3a58ee654269160953
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:20:02 +0000

    fine trap callat unkind shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
