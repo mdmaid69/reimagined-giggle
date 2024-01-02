38860a430a9955ed8a458814982218f27d973c26
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:49:10 +0000

    defend transport lost dream slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
