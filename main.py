d51fb961c8ce5de64e65bc3defe458114ef3f01c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:52:22 +0000

    seem have answer trick education

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
