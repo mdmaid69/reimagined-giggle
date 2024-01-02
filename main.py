942c67ee15e86ee9e48f02921b7d79420fe1add3
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:41:20 +0000

    uponon finish solve friendly frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
