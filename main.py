5b9c6ff569fb96692c1cbf05b9a9c04b8ce0a381
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:34:45 +0000

    of on club Dd occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
