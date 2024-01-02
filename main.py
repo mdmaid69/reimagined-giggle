bf2bd65de351e6c1c1351b6d55fab77dcf1a62d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:27:37 +0000

    speech vanish far shape generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
