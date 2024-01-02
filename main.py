c65f7c9e5bd088f45c57afbc235cabe3f32332af
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:07:53 +0000

    season table tool world branch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
