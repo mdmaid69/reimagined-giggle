1117887130712d05c88c5eec152bd0add773ead4
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:12:49 +0000

    hole tidy practice holdup him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
