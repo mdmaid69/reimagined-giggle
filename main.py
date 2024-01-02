1b6c9a2832818df984fc9323e4c3e6e9994ead1d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:48:37 +0000

    so arrange wet train before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
