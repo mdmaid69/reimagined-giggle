bee23dbeba593fe73568ba860462b1cef06e7a65
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:48:45 +0000

    talk relative hospital how past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
