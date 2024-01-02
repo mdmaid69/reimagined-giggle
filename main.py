fbb9eb92657984fbcd0713c56bdda179bc014b43
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:15:35 +0000

    second matter face medium care

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
