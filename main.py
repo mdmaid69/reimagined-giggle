7aa9500e0d30b68b824cb6a02c63e0d357616723
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:57:11 +0000

    talkinto from interruption sometimes pause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
