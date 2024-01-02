9b8845a56ead21b9faf14ae41a61694e8109f6ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:59:47 +0000

    support read complete vanish public

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
