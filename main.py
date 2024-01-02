6bd7adc8fc3ff8e81035056333434667a9cfcf63
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:22:38 +0000

    tradition survive busy equipment say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
