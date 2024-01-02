87b689972a22472266fcd441ac6508c6ea67c5c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:36:09 +0000

    demand careful except Ee temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+for i in range(5):
+        print(i)
