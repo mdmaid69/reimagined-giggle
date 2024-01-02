2b855f4277ab71ccc6be11b86b563cee1b9c516f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:47:38 +0000

    small murder doubt brief experiment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
