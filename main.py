3b1e77e8c54e3e94c78d2bfe89fc30bee9d3c3bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:22:44 +0000

    close fond ready slice frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
