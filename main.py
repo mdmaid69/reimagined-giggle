414caa640c6bbb1687f2de31847fa1c7629e3aeb
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:33:59 +0000

    attitude thick class makeinto attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def clear_array(array):
+        array *= 0
+import platform
+def get_python_version():
+        return platform.python_version()
