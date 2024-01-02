a782310820e810f4f9697ecf2ea08a3cda6490a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:33:01 +0000

    bury rich thin consult human

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_work(force, distance):
+        return force * distance
+  def convert_to_binary(n):
+        return bin(n)
