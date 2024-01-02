f4bdb62a6adc1c14d718b3dfb54e75729d21a120
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:18:33 +0000

    still grade turn nothing worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
 import array
 def get_array_itemsize(array):
         return array.itemsize
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
