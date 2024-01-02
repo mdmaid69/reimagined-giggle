244f9d3e8cfdb1ffa4620cf73e1690addd30a76e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:34:15 +0000

    music gas laugh search scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
