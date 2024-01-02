7eab718d10e79551efa5b977836b7e2285d9b9bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:27:45 +0000

    send factory call blame goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_bytes_from_array(array):
+        return array.tobytes()
