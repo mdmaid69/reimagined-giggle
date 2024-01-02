a4d02959552261fc9fd1472cd4054610e072b8b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:20:18 +0000

    thorough blame spray brave handle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import re
+print(re.match("h.*o", "hello world"))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
