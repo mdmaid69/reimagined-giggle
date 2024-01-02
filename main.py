df6bc59f681fa94db8899387ec23faf06667a46a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:53:29 +0000

    instrument translate form pleasant chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
