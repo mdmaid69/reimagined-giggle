89999972a4939e3a22c463aa0d22e3dd7f3125c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:47:13 +0000

    forgive store trip protest result

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+for i in range(10): print(i)
+  def is_odd(n):
+        return n % 2 != 0
