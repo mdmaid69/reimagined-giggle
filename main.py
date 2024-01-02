acf4cb1580d5cc3bcce56ea0af433b5049e9b0a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:27:30 +0000

    whistle moment interest getaway doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
