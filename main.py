bb6dd78b3429380ae70d17f86342b9267f0f68c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:57:42 +0000

    difficult appear sentence cap spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_counter():
+        return collections.Counter()
+def find_max(lst):
+        return max(lst)
