7057112ec663075ab04e6998596a82741ddae805
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:00:47 +0000

    work taste makeup dream almost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
