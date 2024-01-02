687da297ff9f29c21952caf3b9368f3fad438007
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:18:53 +0000

    suddenly possession word compete appear

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
