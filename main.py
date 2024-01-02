5e0dfa7f61de1a61fb1b7b96e163a3b246a3eb9a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:19:24 +0000

    man vanish show play plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_array_as_list(array):
+        return list(array)
