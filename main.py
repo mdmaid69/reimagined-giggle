695234bd3ba882040f282fec47450848892a882c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:24:37 +0000

    union pretend attempt opinion health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
