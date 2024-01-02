3e1e0e4cb71694b609b2272f0ab417ea52c26c11
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:31:27 +0000

    pair prepare unpleasant hit road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
