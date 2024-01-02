e8c6cdaf27c028796165b00aa8e6702b468fab4d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:33:30 +0000

    create suddenly compare tall tired

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_length(array):
+        return len(array)
