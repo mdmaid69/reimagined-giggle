1ca6839ea6fc56e7f576a78617bb7bdd993b6bad
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:07:03 +0000

    right concerned job understand through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
