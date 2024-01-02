02268aeacab777164e473fd7e8d34d285cc7ba17
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:13:54 +0000

    number backwards willing nigga sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
