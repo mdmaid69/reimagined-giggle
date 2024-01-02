f803cf3492df893bfdddef784c89ff787670a708
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:52:08 +0000

    your persuade scale grateful 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_float(array):
+        return float(array[0])
+import os
+print(os.getcwd())
