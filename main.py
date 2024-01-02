733947d35e9935d38008babce0a8d5bb1caded46
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:09:28 +0000

    clean idea coat disappointed hurry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
