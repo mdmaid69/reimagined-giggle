2f5f4dcb6e294bd7af30c45cbf5cc59b16763ce4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:12:37 +0000

    angry godown each character bush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
