4c85e6003d38deb412ec0054901e1a5599a9787e
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:41:21 +0000

    giveback forward upstairs play familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
