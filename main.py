8372b6049a840a7e0a9e62ec96aa5e1ad2786c24
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:59:55 +0000

    importance yes treatment ignore convincing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def reverse_array(array):
-        array.reverse()
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
