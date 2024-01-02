d5fa3d201eb09f6498657f0a2b9a43bbd8f514fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:58:11 +0000

    pretty whistle chat injury follow

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_pressure(force, area):
+        return force / area
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
