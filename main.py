cfdda176d46ec1c855528a52d93041fb73f69492
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:40:55 +0000

    reliable turnback react alive bring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
