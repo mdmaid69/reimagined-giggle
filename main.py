31869d7b5c25d0f120c9b918149ff85a316c7d91
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:09:05 +0000

    complain less father almost before

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import time
+def get_time_since_epoch():
+        return time.time()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
