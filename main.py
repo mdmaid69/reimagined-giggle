4510d42eee68b75b1f7af61a90e7a43ae27299a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:06:31 +0000

    us fill address correction count

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
