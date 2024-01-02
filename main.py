3c3bae44f6717bddaa7be178224ea61c0effcd0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:57:56 +0000

    plenty downstairs them party sharp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def reverse_array(array):
+        array.reverse()
+def convert_to_octal(n):
+        return oct(n)
