99478eb6c956f71c06f3a5e6fb278965fa264322
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:49:03 +0000

    eager Jj ear remove burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_speed(distance, time):
-        return distance / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
