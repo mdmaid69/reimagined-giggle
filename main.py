4b1a804e00d9338e6279fa5284f568ba41b34a53
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:42:14 +0000

    imagine run considering match quantity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
