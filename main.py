19c46764a4844e86b6652bd84e75f0763e76e148
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:57:38 +0000

    except tense through inform under

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def reverse_list(lst):
+        return lst[::-1]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
