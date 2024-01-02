d3d3b517177c49bbc038ef06f4b20725c1ced7dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:28:45 +0000

    holdup Cc reject honest body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
