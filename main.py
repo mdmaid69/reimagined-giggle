03016b1813a6f7a6ccca0fe9808408eea4c76421
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:29:27 +0000

    mass flat possible offer contents

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
