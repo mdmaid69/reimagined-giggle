46892ed30744d91d2c83a0182e1b2b4931d580d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:27:50 +0000

    bang impressed relation vanish equal

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_to_hex(n):
+        return hex(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
