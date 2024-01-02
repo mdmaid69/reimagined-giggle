4e930baf546219b6e704f275df90fb11fa79b03a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:48:05 +0000

    prevent pause past bill odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_min(lst):
-        return min(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
