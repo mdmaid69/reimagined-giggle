fb4297eb6d9cdf8318e2700b06202fc415e75055
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:11:09 +0000

    walk separate moon next worth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
+  def convert_to_hex(n):
+        return hex(n)
