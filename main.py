3dabc0dd573134445dba806c503cc0570ace31c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:20:50 +0000

    shade speed design mad comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
