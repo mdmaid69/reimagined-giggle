6d278c0eac1cc7415dfa68348cbe9751d109ce0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:39:30 +0000

    real nature change direct drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
