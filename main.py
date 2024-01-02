e34a1447387e8708a90b67c85f64f4fa4bd2266b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:37:20 +0000

    deliver past government estimate argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
