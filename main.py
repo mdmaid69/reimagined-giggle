696455a09731a3d1cd2ea023dd553c77b158afcc
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:23:51 +0000

    Hh deep loan warm mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_area_rectangle(l, w):
+        return l * w
