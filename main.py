fc7af88bc99bc918f222182e2e57830dbbad1d0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:43:08 +0000

    risk close record stamp Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
