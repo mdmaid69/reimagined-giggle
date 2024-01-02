0ca25dd19c3a8ed2aac26f2306f06da822269ffd
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:37:46 +0000

    slice sound mark offer pain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
