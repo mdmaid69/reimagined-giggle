8bb54173b81eb16b9a18381177a6229922aad83c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:30:43 +0000

    favour blame frightened tread react

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def remove_duplicates(lst):
+        return list(set(lst))
