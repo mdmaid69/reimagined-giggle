819550a9ab5c638c884deaa778e15c69200fd3bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:32:55 +0000

    takeoff before rule pupil without

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-print(sum(range(10)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
