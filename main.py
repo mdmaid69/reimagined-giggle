11424ff65ce81115df45df7cc65c0ec429518b46
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:04:05 +0000

    drawing forgive splash estimate insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
