b1c648d397f4e3cbb3fbac79b1880313b4da03fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:14:06 +0000

    turnback note again thank name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def append_to_array(array, item):
+        array.append(item)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
