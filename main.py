a1ed01e7db21a82c1bc5ebfa667ddd3e3c16085c
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:43:38 +0000

    neck week foreign Gg speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
