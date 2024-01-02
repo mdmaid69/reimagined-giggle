5cb4107a4a811c2bfd0e8be2f7e4d0e7948acc1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:42:25 +0000

    the storm dangerous pocket whichever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
