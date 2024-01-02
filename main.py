f3c18d4ca9272f089736e38faab9fbd760b1e37f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:07:36 +0000

    Ff country start into possibility

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-for i in range(10): print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
