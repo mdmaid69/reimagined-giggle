f1ccc951f48911076bc69717422f8f61587a1f17
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:00:37 +0000

    natural identify Ll usual also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_circle(r):
+        return 3.14 * r**2
