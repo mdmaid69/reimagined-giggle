59d3e3e4f9ab959932c8e3a17699e7f0b9a6bd75
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:52:57 +0000

    notice return discover war man

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def subtract_numbers(x, y):
-        return x - y
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
