5433a18591b28435dd72aa02d80f052c7b0234af
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:28:42 +0000

    healthy ride attempt wish drive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print(sum(range(10)))
