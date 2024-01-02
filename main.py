0e88018a65d3fa6d9498e2082b590e2e0b88facb
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:13:57 +0000

    perhaps imagine money blind natural

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def cube_number(x):
+        return x**3
