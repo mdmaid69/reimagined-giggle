b297f7efdd54dec8465f99821737c5c5a4ac0cb9
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:11:37 +0000

    worst separate not when concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
