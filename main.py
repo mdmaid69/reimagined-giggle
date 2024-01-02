abc9ab5d00710831601ba92aee3f017419d23783
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:06:11 +0000

    stretch yet compare expensive calm

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+print("Hello, world!")
+import platform
+def get_python_version():
+        return platform.python_version()
