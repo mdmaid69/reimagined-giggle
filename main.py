b702b59f9c6bbf12d336297b7c0bde03b98e46fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:49:02 +0000

    dance achieve grade absolutely own

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
