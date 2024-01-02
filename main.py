22b504cc2133eef0f1ed21929ab61ac181dbd8a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:18:21 +0000

    aim their quick peace dark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
