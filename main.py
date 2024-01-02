b74807a5f2741714819005ab51c2ef2fa129cb98
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:49:04 +0000

    continue brown height serve club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def check_if_array_contains_item(array, item):
+        return item in array
