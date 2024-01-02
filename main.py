a9f3fc93581d64580bcea395003fe65b2d669dbc
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:52:10 +0000

    consist key strict target black

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(10): print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
