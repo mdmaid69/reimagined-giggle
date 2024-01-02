0a5e9330a69878601e93500b668f8de173fd3f4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:02:28 +0000

    regional luck wide push off

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
