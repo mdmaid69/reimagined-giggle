cb83524faad808bc148e28be937763aae3de666e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:23:33 +0000

    spend rush luck anybody window

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
