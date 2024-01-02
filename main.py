d0c88cdc49d9439b961bbe3776825876f6239f5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:19:08 +0000

    share possibly believe shame eat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_power(work, time):
-        return work / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
