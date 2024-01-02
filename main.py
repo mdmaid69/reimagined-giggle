3979c4212e5e6de490aa84f65b8aa54ba2045bb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:04:43 +0000

    chin wealth aim press sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
