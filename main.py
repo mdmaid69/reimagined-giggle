ea50d66bc080305da6747192c59fccf1ff9500d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:07:25 +0000

    point certainly clear breakinto remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_exponential(x):
+        return math.exp(x)
