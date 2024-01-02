d90c18a8066a26514d620bb053a2c590584ab1e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:07:01 +0000

    left round straight feel basic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_density(mass, volume):
+        return mass / volume
