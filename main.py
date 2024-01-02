9b1091580037c65a6d0fc81c7eac762c3b13db4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:14:36 +0000

    variety knowledge collect style makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
