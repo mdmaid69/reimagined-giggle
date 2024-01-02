13b298bb8cc505b332f323e35940b5e07ec5fb73
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:33:39 +0000

    tall where chin increase issue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_binary(n):
+        return bin(n)
