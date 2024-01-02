e12d1c992c74ab4d196a2afc3b44e63823b262b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:50:05 +0000

    mass lay sport fast inside

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+from collections import Counter
+print(Counter("hello world"))
