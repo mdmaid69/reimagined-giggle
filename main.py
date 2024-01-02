4a04100917ba38e49dd32e91dff8038aad0816f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:21:07 +0000

    work attractive him acceptable pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
