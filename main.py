f21fe20b21a0e4da2f815f7a86d6b020fc855046
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:43:47 +0000

    entrance depend connect exploration would

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
