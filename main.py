ff3967f51d589577f219230c33e60e0ee78ec2a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:29:18 +0000

    agree risk full allow tidy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def count_elements(lst):
+        return len(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
