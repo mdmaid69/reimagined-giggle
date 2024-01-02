4ab4c2eda2e5871f8026414c456dac284d571dfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:57:53 +0000

    care charge rescue describe mark

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def subtract_numbers(x, y):
+        return x - y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
