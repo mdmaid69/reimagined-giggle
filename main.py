ffc5f64ea363be7ccdbf22a34fcad2a6ff9f6a16
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:33:22 +0000

    offence doubt minimum go below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
