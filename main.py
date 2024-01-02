70bafbdf5eebbd306c518509246e56395f362122
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:46:46 +0000

    border comedown who praise large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
