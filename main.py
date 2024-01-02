5c04005cbc8c6b66e6ad671de14259bf0b928e19
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:06:41 +0000

    language anywhere tour winter along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
