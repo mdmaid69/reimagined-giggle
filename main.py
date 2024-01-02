908c9ec13b20694b748a1fcce9ccaa9501de1d5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:59:23 +0000

    crisis hit towards level wave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
