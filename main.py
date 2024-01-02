e23e42b998ba1c90a21a7d22fe5e748f261faf09
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:20:25 +0000

    nothing strike actually distant comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
