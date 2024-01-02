69c8e20236a2c3f9237719f9cccb832e7116dec3
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:55:28 +0000

    promise present watch breakup suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
