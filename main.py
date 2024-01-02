d39d1f8c3b59eb29ea8c58996b8ff94a841fd54c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:13:49 +0000

    adjective path connect violent mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
