95ba60fd54bf98bec12aa8027cfe642529ad7bd7
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:19:46 +0000

    priority post seem opponent mouth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_max(lst):
+        return max(lst)
