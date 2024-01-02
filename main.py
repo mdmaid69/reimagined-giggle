4cc19a2987a501233198a2261bec0b63522c02d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:35:44 +0000

    that anything plan contents pen

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
