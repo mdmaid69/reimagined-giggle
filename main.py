dd4f6c3d426aa82476494da791db31ffb5eb6bbe
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:58:36 +0000

    cook intelligent chief maintain furniture

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
