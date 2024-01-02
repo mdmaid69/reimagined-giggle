ff98dfad5dc22aa872cd4397f053efacda009daf
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:14:33 +0000

    introduction race pinch for Qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
