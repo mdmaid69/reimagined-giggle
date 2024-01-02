58cec5bc9fa0ccdb8050624d64feb0790a140ab8
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:21:59 +0000

    worst forget so emotion goup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
