367ffe4755bc29a3f4695dcba67869af249eee72
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:29:58 +0000

    impatient building flow tape simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
