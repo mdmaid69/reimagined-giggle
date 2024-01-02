388d8b2632e49c9cd37d774ae612c63d2b561f55
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:29:48 +0000

    project collection treat scrap stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
