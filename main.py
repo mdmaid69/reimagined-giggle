711c1884f0ae9f2b014c239303413db18caa26e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:13:58 +0000

    broken ought proof listenfor bottom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
