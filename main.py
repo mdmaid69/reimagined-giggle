4819c41631de8a902021475f8995a353c0a3e0ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:00:15 +0000

    minority somewhere uponon deserve cheap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
