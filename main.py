6f5b5faca2f8ac37c1790f2fc05737706fdd36ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:01:24 +0000

    responsible long often game professional

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
