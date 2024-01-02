bb78fff03e4434b60d2ae210d76ba665710735fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:05:54 +0000

    emergency reasonable recently alive complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+print(re.match("h.*o", "hello world"))
