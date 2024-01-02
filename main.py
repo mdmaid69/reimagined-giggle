da7c8694de38d1c35d2e06dabd7e1e3141b2469a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:44:03 +0000

    note steep equipment paper frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
