83a0ef8edfe55fb7cbb592c7b4866ed53dc0bfdf
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:24:10 +0000

    everything street extraordinary work guard

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_work(force, distance):
+        return force * distance
+def find_min(lst):
+        return min(lst)
