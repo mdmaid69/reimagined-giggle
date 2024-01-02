750b3d11b2f1437d7a93ec3c80cb83a7d2a92eeb
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:09:33 +0000

    offend exchange purpose careful unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
