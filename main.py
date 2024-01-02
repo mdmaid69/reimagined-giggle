5c488bfd136ae8c1e511b913967995764f454705
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:18:57 +0000

    rain bill over unkind card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
