e127d11dc023ff719239762e15ced6d1c72ab861
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:41:26 +0000

    murder music Uu time lead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+i = 0
+while i < 5:
+        print(i)
+        i += 1
