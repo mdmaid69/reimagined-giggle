cd46520be56743fe411ff2aaf153b0e459081b6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:15:36 +0000

    they buy Cc Mm gooff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def count_elements(lst):
+        return len(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
