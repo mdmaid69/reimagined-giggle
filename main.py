46862c179c6d974acaf7e6e73ec5cbe7926cc0b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:09:35 +0000

    thankyou separate worth avoid behave

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import time
-def get_current_time():
-        return time.time()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
