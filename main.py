3dc8f7bf7ec697c7b1072eb14a987e4bd4efb637
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:22:26 +0000

    importance afternoon mess black thing

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
