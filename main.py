5a72b6e0cbd402a0622f1cc79ec20877664f5a79
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:21:46 +0000

    flow pattern behaviour male very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
