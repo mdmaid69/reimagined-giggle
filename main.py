55eb2dad1d64d96188db127ec50a2add2074de83
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:48:24 +0000

    afraid software judge store sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+for i in range(5):
+        print(i)
