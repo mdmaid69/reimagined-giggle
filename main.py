1984597f22ec9e28e6659575952727132c6fd977
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:54:23 +0000

    card lead frame back instrument

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
