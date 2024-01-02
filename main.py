440b857c2f6a8027536a1f2b7978cf493207b7ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:31:40 +0000

    cause desperate confusing now Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
