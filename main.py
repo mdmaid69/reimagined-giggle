e2aee0691b911f9ae4ef8c15ac25db1b828f1494
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:48:33 +0000

    complete lay plant or snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
