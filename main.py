da671a48a0bcef77650ff93b623fd45057c9adb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:51:26 +0000

    uncomfortable feel complete pronounce announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
