ecef780f20e829cf46c3608e324cd14068a30ab5
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:42:07 +0000

    load bridge my solve appearance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
