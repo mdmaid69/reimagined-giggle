ab28e81859f42b758374e2b3ac7aa34fce561b79
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:16:58 +0000

    advise drawing train acceptable concern

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
