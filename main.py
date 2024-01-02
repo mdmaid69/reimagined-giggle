c6da4fc149939289b1068534bb85192e6aff7ca8
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:29:46 +0000

    hang choose nigga concerned seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
