e3b1b36ec3e65f68fc28bdba397be5d81934d758
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:53:15 +0000

    loud oil standard interfere fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
