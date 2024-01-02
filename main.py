df8281759e1de031c38aaf7bd8d85fe6273f7269
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:47:16 +0000

    unpleasant part wake realize finger

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
