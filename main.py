b0c58a21d22ef1010491037f5c45a6bf93cca3b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:38:10 +0000

    moon because production equal project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
