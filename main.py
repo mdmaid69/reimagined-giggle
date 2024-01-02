cab85654d8d8b45ac488c8708cb66bc1ff073550
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:25:35 +0000

    check minor straight radio feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
