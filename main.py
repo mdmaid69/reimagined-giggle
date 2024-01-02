24726fc0955421b2404098384d066fbf5eeac316
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:57:51 +0000

    foot beat callup frame wash

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+print("Hello, world!")
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
