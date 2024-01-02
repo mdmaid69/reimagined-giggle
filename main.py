39f30c251008d5c20d9b2928534a650b18cb98bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:47:26 +0000

    please release listenfor actually exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
