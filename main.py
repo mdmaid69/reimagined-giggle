aa82a548575a7b74225132079302438f7976e888
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:15:28 +0000

    spin hide just grateful my

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
