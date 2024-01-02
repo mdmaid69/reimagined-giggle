f8bc65783605ea73ec743c5b43f81c1e0a37026d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:58:51 +0000

    offer free wall home will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def reverse_array(array):
+        array.reverse()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
