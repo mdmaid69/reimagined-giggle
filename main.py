7a1bd96e38d5472aa8b5798654f5649a873ef4af
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:04:11 +0000

    system interview promise tune watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
