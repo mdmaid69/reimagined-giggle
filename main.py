0a48c911fa5e939a9fbd4adcd5e60ca2dc11ad6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:41:29 +0000

    takeoff skirt wave do the

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
