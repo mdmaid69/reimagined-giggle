13cb5e98fb7f72f14b587c861edbf27f33d1f0eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:21:39 +0000

    blame guess adjective join fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
