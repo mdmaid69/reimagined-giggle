e8292d334c61bf63da23a9d972dcd5700d2e05e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:04:36 +0000

    stupid tip medium learn whom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def square_number(x):
-        return x**2
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def reverse_array(array):
+        array.reverse()
