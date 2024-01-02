4712dd86e9fdecff431d92561c5ae702f0a782fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:25:01 +0000

    natural my scream opportunity recover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
