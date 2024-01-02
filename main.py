cfaeba6f3dee41557bcdd0609cfabd6f1ef240d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:11:22 +0000

    idea fact music shop case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import time
+def get_current_time():
+        return time.ctime()
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def clear_array(array):
+        array *= 0
