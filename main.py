c85f617b4f5786d5d4028cfada2f9ba3cb57327f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:46:58 +0000

    squash plate owe wrap react

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_density(mass, volume):
+        return mass / volume
