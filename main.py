14924f27b0bc20123846f4afffba8363c5164d6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:28:56 +0000

    promise let right disturbing earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
