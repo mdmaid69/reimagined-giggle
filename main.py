f9282c13aa0b8b977d506c20c337bbe01a9e0049
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:30:40 +0000

    language never work corner telephone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
