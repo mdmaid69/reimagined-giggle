f4aa0ed684a65c1a416bdffc060aee708ad02b38
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:03:54 +0000

    send confidence train joke steep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
