2d97b61498e5c2778d3720fbd8c05e7d0594579e
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:42:51 +0000

    which regular fun argument explain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
