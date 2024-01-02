278f9d46d7df770171086407d0392546a3947134
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:48:43 +0000

    unless this task comeout live

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
