ff11dbf6fec24bb3210421c9335003b7ae38fa53
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:55:04 +0000

    priority touch connected deliver strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def delete_file(file_name):
+        os.remove(file_name)
