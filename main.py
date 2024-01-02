ae3d58be76f0b7a8eae5740cc50d92ff381aa4d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:35:04 +0000

    blue post board declare clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
