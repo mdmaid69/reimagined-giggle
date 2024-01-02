77f854ff096533c49c20986bfa46e613ab10340d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:53:53 +0000

    split benefit embarrassing her fold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def convert_to_octal(n):
+        return oct(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
