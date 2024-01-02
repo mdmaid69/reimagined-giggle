c466c495ae4a35249345dfa689d3bf669a403b31
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:56:40 +0000

    late effect gentle mess moral

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_current_time():
+        return time.time()
