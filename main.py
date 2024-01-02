ce204241ad3b6cbc8c3969f835aac62c8a0041b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:13:28 +0000

    they event risk public anybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
