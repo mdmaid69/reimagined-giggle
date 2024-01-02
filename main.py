6e771df67cfe490717042b1d841b5953d4ed4004
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:29:33 +0000

    male impressive rank march flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def is_even(n):
+        return n % 2 == 0
