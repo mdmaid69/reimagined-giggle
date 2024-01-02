6839c8c154aaefca6f59b4535858d663e247ac06
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:12:10 +0000

    spare turnoff belong extraordinary hit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
