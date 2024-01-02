e16c26bcdd9c31483174d1afc7b9fe4f69a862d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:18:57 +0000

    pleasure change skirt history popular

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
