7ff0084f7d7dd2443f800e5ad501580d0194d603
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:30:05 +0000

    telephone dangerous communication head watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
