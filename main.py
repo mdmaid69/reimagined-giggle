2e02a0b4f81246d739c8c07445ee78b81e82bbc1
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:10:29 +0000

    talkinto instrument criticism smart feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
