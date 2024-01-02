02a81c5c5141b349b7fb011154e89238c219f3f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:55:23 +0000

    cope car Vv flight comein

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
