1cfb31795692e35755815610aad46215e8e4df77
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:08:58 +0000

    improve normal bringforward cough arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import os
+print(os.getcwd())
