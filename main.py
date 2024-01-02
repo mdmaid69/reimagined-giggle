63c5ee55b9990993d7e8d6436deefde45ba7a35f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:10:10 +0000

    previous speed old anywhere imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
