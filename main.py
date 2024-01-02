a03963155d3fd2fc483ce43db367de34ca2a9539
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:54:38 +0000

    style proof outside care present

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
