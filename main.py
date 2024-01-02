589e5d6ea4f8df32bfe81175e525d8baee77079a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:07:51 +0000

    neck meanwhile giveup suck fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
