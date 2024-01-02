b545ac9ea04c6f844738f13f4a84bf6ae0a45d84
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:33:20 +0000

    near state match rule tax

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
