5564ad17dad8d639aa3850bee4a7a87220746ac4
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:45:59 +0000

    571enough along no always image

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
