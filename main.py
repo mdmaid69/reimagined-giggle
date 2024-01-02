4fbf6986a094f76d424369396de7be8415151eae
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:28:57 +0000

    ring attitude goback(to) tragedy spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
