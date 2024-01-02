836e22d8e57444effbdc8e83e9613d52c2ff2010
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:49:16 +0000

    reaction present spray port tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
