29c64322ccddd02b48e9d793d2043a57c972eb3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:27:01 +0000

    fold keepout ago clothes ocean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
