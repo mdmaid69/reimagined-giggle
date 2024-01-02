607fab6688c2cf3ed96d1b49491c89d4d9bb45d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:10:33 +0000

    put coat under Rr another

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
