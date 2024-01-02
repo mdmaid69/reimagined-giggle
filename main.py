dbe93ebc584cb233703c3d0e1fda7f49c770a98c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:39:48 +0000

    classroom slide ago conduct fortunately

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_power(work, time):
+        return work / time
