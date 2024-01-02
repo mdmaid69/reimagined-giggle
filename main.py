bf5fff74b730077271f8207cf4922efac2463a63
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:16:35 +0000

    frighten complete for accept difference

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import time
+def get_current_time():
+        return time.time()
