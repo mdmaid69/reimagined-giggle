0dc5112223b8a0eec983b567a398af0e9bdc759a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:08:53 +0000

    none label better university dry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
