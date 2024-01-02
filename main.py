a8c260bb63d2b5f116b8d48599c51d2768e71538
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:19:12 +0000

    freeze sit street sign final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
