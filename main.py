1ef044064bf69a41c66599b75429f878efffb8c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:44:25 +0000

    flat software style takeout rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
