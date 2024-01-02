32dede6e21510b31873c03f8f409fa6848ee0e15
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:14:29 +0000

    active cum recently comeback path

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
