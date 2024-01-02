a1abaad4a55ba82288a2ed311fca92f083d5089d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:47:23 +0000

    value child field different force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
