dc0c6bd22755a3f44e0b61e4da730d147117fb5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:24:41 +0000

    great high form shopping hate

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
