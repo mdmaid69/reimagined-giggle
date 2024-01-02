de0506a9e984b6876e4bf57359efabada64df89b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:55:18 +0000

    tonight more aim passage flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
