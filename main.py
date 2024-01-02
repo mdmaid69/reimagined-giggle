bb1a44f6b5f18aeda6a0ad0823f0dcb191995e27
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:35:43 +0000

    cause interview course top move

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
