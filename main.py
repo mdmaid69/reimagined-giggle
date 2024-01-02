ee027921088c824c86f733fdde9be5371a021e07
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:04:16 +0000

    remember floor theirs which cause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_bool(array):
+        return bool(array)
