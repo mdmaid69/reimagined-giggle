0003abf8332050dad7cdab7cf9a2587c04fe09c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:06:45 +0000

    organize better square danger ourselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
