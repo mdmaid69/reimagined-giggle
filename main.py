6429f400af25b2a56c07735c31ec9a7f9ffb108a
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:25:32 +0000

    flat crazy check noise behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
