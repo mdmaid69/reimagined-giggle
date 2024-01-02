fb23a71d269df75ee2644387ed5ed9aa8490dd7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:32:11 +0000

    trouble upstairs swim involve apologizeapologise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
