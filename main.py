43cded5b2da3f0d75bda422786ea3df593da2905
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:33:14 +0000

    bar pour moral reply slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_bool(array):
+        return bool(array)
