5d4ce04a03941f9128e647c86ac445aead233334
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:01:03 +0000

    hers I legal dig professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
