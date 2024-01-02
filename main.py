14cafcf7e12b70b3bb845212af37ea1cce8f676a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:10:16 +0000

    lead path themselves undo getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
