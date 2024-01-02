8d8bb3b50838216a62238329047420e45d23fa7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:11:20 +0000

    contain fashion letter ignore either

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
