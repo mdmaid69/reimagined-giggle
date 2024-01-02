6153abad66f6c7c6b76cbb37e00448140788203d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:37:20 +0000

    schedule hope thirsty poor calm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
