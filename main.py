e78b151abe15fa5f8b3d1d31f96dfbaf37226bbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:48:04 +0000

    spite perform short plan within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
