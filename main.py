069cec8626168352cbafc5b8b339683ae6534050
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:52:38 +0000

    story before laugh well although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
