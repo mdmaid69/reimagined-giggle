b905830cbd19afb06353438140f6c70356aa8484
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:32:30 +0000

    serve answer front comeoff wrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
