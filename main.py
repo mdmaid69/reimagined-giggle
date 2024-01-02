dbd39770504c9ad320cba3992f7a65bfbef07955
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:11:14 +0000

    power platform mix release read

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
