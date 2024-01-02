bd182ecc624335123daa1d1afc1c31a70850278d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:34:33 +0000

    complete activity circumstance strength confused

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
