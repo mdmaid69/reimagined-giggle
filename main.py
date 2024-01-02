6ba36408beccfb064e8f84b8248f3603d2b99887
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:37 +0000

    rubbish wipe confuse container suddenly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+for i in range(5):
+        print(i)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
