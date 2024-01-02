d896cbd80b069f5c64eec20f43fecade6bf83e2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:31:43 +0000

    tread crowded centimetre test mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
