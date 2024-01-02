bd3a434edb8eea09a7c4f1650f2722f5dcd1123e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:15:44 +0000

    nor twist life smoke speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
