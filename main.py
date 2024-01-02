c69f951618a56fa72232ae98c917b811d60510f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:46:22 +0000

    regional belong word behind hollow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def convert_to_octal(n):
+        return oct(n)
