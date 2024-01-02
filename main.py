fa6603789e5f260bb416ebae6d68d604b62debad
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:04:10 +0000

    right interrupt machine evidence Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_typecode(array):
+        return array.typecode
+import datetime
+def get_today_date():
+        return datetime.date.today()
