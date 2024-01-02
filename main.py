d3da56a54de2abb67c723bf2a5c2b96c5b310eab
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:14:28 +0000

    back country joke swimming language

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
