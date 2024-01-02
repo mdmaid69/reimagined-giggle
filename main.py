31dd477afb2c57169145357eda9c84933952db8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:25:56 +0000

    decide crowd shoulder advantage surface

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
