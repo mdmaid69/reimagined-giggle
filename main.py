5ede6a398f79cd016b64fdccc5ed0e027b7b1927
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:38:59 +0000

    gradually city swim situation figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_density(mass, volume):
+        return mass / volume
