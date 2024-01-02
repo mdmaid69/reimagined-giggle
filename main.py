9dfc987ae3f42d6be87182e2afe24a761344f907
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:35:48 +0000

    tradition practice lower behaviour boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
