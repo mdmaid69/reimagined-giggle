c331692e327489d5bd1ae371c9913ca467b62ea5
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:05:41 +0000

    progress father about runout attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import collections
+def create_user_list():
+        return collections.UserList()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
