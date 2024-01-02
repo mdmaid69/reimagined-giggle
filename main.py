d14368104858a8c046f19eafae50159993eafea9
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:24:21 +0000

    grand name than break creature

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_length(array):
-        return len(array)
+def add_numbers(x, y):
+        return x + y
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
