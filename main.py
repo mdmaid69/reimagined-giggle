61fd160c61797fa59da35e07f0edbe4b0ea369bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:04:43 +0000

    yourself narrow objection exhibition sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
