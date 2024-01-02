80a5c419ce03da6144b277d50159cc70aa445a1d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:10:58 +0000

    actual reply stare glad wing

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
