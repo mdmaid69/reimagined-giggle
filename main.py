2eb12a9518fbfb0b20f9c999802d95b2f088bd98
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:52:17 +0000

    dream deliberate exam butter respect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
