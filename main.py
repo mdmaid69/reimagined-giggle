c04cecef7cc037727c8b70150f9fe7bf5bddaae6
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:13:39 +0000

    swallow wave wash necessary tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
