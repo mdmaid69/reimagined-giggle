7b774ce579a685106902a8a074f7d97043912754
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:18:39 +0000

    fast mark experience surprised lean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
