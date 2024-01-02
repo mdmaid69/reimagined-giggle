480a4618553d0766912eb274f30470515a87f01d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:58:59 +0000

    towards sleep part charge home

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
