ffd2510c58e13ddc5b0d036d0ddacab9dfeba29b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:44:01 +0000

    ago children off fight achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_work(force, distance):
+        return force * distance
