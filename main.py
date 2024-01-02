8e93dcba83e2620441f03cfaa216a47a6887985f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:40:06 +0000

    debt table part read keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
