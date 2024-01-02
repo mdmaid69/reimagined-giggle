08f068eb8feb8c7e7a1d9e4da3a49c2e02b56ad1
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:06:41 +0000

    mark charge bus by flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from collections import Counter
-print(Counter("hello world"))
+def remove_duplicates(lst):
+        return list(set(lst))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
