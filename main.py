c6a715c9dd5c93a9b6f87e6454852cd50b3a19a6
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:09:13 +0000

    flight emphasize gradually subject vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
