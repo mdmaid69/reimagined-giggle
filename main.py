06c9604d7f5241a288e9a46e245262b582abce53
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:44:08 +0000

    heart curious paper boring physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
