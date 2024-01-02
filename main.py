9d0b7792b4e330fa11cb80c62c5831cf6f4f4b9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:09:21 +0000

    tongue advanced laugh friend age

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
