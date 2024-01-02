3a622d23b3e85f455669253bd3ff235bb29bc29a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:08:34 +0000

    much bet comeout science deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
