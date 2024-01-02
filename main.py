738a713f7f0c5db894bd095a3e9359371ad7973e
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:03:33 +0000

    everything drop declaration she university

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def multiply_numbers(x, y):
-        return x * y
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
