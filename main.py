d4f700521f3b47c956eb808586d051e675f7120a
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:45:05 +0000

    subject mind float wall reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_cosine(x):
+        return math.cos(x)
