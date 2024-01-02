dfc4f51b377cfeb00c4ac4f17dbc32156ee56820
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:57:27 +0000

    workout fast frequent brush concerning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
