d30974a0fe66e6eb9e46aafd9ba0f193bcbca270
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:21 +0000

    check admire bus move recover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+def count_words(sentence):
+        return len(sentence.split())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
