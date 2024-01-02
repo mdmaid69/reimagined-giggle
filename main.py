4e2304efac7a885fdb85f401a3649d20bc8ef360
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:40:52 +0000

    false recent mistake mind lookback(on)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
