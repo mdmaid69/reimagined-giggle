609738fa07a332ea10fbeeb5976e7ba1ba32e52a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:24:32 +0000

    tragedy emotion rid correct remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
