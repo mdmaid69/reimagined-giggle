95db560491ff56896fcc8f8c0753ec4292d9a523
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:09:45 +0000

    vanish influence breakin somehow generally

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
