95aa0068184b008eefaf751a84a5d7eae9dcde09
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:40:04 +0000

    side towards majority period base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
