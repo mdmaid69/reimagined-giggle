8bb9ae86cb529d8d0c9711434872ff037b544c4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:38:42 +0000

    important copy unit separate disturbing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def convert_to_octal(n):
+        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
