68236e92b51c76fd5103f1f38b44756ab936ae54
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:38:23 +0000

    that cover before emergency science

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(5):
+        print(i)
