1b272242a7059d5268d65fd896002b8dc8afebd3
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:51:13 +0000

    tap lean nowhere possibility correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
