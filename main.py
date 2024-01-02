ad7b84583dee346afe13593f50921ef7a8791fa8
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:40:06 +0000

    pretty thirsty people better bringdown

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
