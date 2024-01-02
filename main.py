fe6343dfdc851c2a841359c3bd861f337d3f81c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:04:20 +0000

    before pretty steam big create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
