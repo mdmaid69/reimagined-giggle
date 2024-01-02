972224391934c11d67be4b5c728946cab4553f91
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:08:53 +0000

    train base yourself point full

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_pressure(force, area):
+        return force / area
+import tensorflow as tf
+print(tf.__version__)
