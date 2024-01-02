56161eafda472b889ceab9e02d087319350d008f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:07:16 +0000

    spin direct strange public pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
