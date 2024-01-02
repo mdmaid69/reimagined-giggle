e0d1b6e68cc6921fa42c37bbb1af2c6b8f51e00a
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:06:06 +0000

    wish deaf steam hardly earth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_numbers(numbers):
+        return sorted(numbers)
