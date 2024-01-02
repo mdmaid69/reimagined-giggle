ef34d77efbb47d5f08495be6608873777f6d9070
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:35:00 +0000

    568energy makeinto convinced work ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_to_binary(n):
+        return bin(n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
