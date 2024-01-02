65eb679acc53b651bd6d4ced21ec963d7da17985
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:38:16 +0000

    left comeup welcome hurt shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
