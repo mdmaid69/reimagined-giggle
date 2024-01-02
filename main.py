986df43caf484d65dc89a7e8af2cf46c5190b0ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:39:08 +0000

    famous accordingto muscle speed tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
