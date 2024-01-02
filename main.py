2eab8df25dc4beaf878be132b031efc822a67377
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:11:08 +0000

    majority fast shop open formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
