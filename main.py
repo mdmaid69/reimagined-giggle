0acfc6010aca91a5a0f4f3e91afcfeff3477d80c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:12 +0000

    stupid takeoff hard waste violent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
