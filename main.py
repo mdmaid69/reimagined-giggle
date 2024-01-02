0c4dd679f4e3bda6059a50d967ea9a522e0927ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:11:54 +0000

    union Mm apart may spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
