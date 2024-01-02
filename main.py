ace12637257834619afba1f0a54246bdffd00686
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:16:03 +0000

    intelligent consist not instant easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
