80ee35c26648450c061feccb6307850ff7ea7ce8
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:43:53 +0000

    pause keep pump concentration none

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
