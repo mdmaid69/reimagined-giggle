918a4b41e0ecfda2c9e042a3abb3b5d7e30fbdb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:24:25 +0000

    turnon various bringforward hear they

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-print(sum(range(10)))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
