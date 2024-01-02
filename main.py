a0212c949e8754ac44610d356e83dd0e57f9f225
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:49:35 +0000

    argument entry begin opposite reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
