b21cc88da5e571c7adae23001c513a6927e8aa33
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:45:19 +0000

    personal Gg whenever string aware

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+name = "Python"
+print("Hello,", name)
