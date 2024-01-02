4aeacca07cfd30e1a98e0397f851966947b645a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:53:13 +0000

    blame precise base case port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-name = "Python"
-print("Hello,", name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
