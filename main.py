ec671f4f780f4f0024032af9e8d4208ea2f721fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:35 +0000

    equal bury hollow surface peace

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
