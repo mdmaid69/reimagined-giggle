ac376c0b421110e3461b9af0a48e62a9ebc11488
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:43:10 +0000

    solve sleep anybody bend any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
