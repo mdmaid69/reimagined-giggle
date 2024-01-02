d0fb171c04f2c9898a22cd991a87cf7b330a9a42
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:39:32 +0000

    quantity able like nothing deal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def greet(name):
+        print(f"Hello, {name}!")
