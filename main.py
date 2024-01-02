f38bed83b10cd23009256c45f39771697e80ebd3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:12:39 +0000

    pick free delicate left far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
