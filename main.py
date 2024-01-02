a986a2300b817d93e6171e03177024906f5d6f44
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:05:02 +0000

    worse commit check dress door

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def multiply_numbers(x, y):
+        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
