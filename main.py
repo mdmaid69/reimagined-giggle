4e6cf60328c3132df21f665d5c2fb5cbb8ba7245
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:50:53 +0000

    takeout emergency community tape report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
