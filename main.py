715e6e51bcd241f20a95a7438c2a18927d5b1dbb
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:18:32 +0000

    after neck mother expect pretty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
