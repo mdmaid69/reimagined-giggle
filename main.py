b979424484823c984ed16f7432f879fd29c00e58
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:35:09 +0000

    beside careful gas offer fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
