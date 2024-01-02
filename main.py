b74aa7cd1297b7ba81c33b15aae92687e8ddff80
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:24:58 +0000

    each tap react science piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
