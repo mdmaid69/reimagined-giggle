0b6c7b9d7821ed3645b13f04a99386431b5bfacd
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:35:33 +0000

    class truth uponon sticky and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
