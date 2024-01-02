c0ca5c4ca63c9cb906f1a13e4afa4082787373ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:40:46 +0000

    callup personal on brush hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import re
+print(re.match("h.*o", "hello world"))
