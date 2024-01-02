a2ea4ea91b25f9c855829556be46877a65889d68
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:59:17 +0000

    damage flat sea square commit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
