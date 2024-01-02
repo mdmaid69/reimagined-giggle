99e172a31c1669c359ddee27c25c883dd26cd383
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:22:46 +0000

    label discovery talkoutof switch hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
