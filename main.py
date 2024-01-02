5288e60c3c6beee3c8791cee645f8e8b46166d85
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:48:08 +0000

    tidy appearance present object hurt

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import time
+print(time.time())
