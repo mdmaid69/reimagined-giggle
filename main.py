cd77f9b4443b22f3eff3a335720859aeef20fc50
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:17:04 +0000

    holiday combination closed close reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
