a5f2db3b1daaa6d9ae8ef63b1f8a3ad551d2d11c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:17:07 +0000

    wrap service closed man principle

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
