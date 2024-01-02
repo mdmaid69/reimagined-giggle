0a739a318a7c8def16f27938e4f139b67413446f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:57:26 +0000

    talk appearance however track attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
