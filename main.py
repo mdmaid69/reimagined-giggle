e5e0af1a3470fc40d5c756716d5f56644d326505
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:37:04 +0000

    finish near certain finish indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
