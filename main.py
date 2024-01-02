4211e2a5617e731ec9fe1b7cb55b5d1ccb59ccb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:15:44 +0000

    beat plenty several ocean properly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
