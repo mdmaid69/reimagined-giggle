f6cf0cb8b4e91d16ff7ba17122f705c5ac225b0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:54:41 +0000

    think recommend false list bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
