60130de12f8eae984dbe12e0bb0d187fb6f567f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:02 +0000

    active image wash strict present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
