d2d6a9effb5a5a81d4ecca4a9b34b9479bfd2644
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:49:32 +0000

    below alive pretty drop press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
