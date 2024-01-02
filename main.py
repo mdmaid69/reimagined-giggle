7cd868cecc5d085b98ceb9146463848227bcc0e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:09:08 +0000

    recovery trick insist fail shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_bytearray(array):
+        return bytearray(array)
