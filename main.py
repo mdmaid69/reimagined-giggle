851cff97d1831d403cbccfbc3bcbfa806cf993da
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:39:29 +0000

    whenever passage hit police learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
