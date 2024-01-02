aa1d9987c219b20f38e7e18a926aff3bd1f1ba08
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:28:48 +0000

    form face meal father smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def convert_array_to_list(array):
+        return array.tolist()
