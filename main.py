183feaa4664d08fa4588098df734585fd6869ee6
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:01:35 +0000

    people achievement inform press inform

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_acceleration(speed, time):
+        return speed / time
