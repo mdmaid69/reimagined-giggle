0ab409a92dbd5a8ae593c5641e4f3af17f080c2e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:15:36 +0000

    necessary picture hurry blow hurt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import time
-def get_current_time():
-        return time.ctime()
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_item(array, i):
+        return array[i]
