68da8c4121a4a159629b422ad7b16b3a7179cde3
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:17:02 +0000

    argument place wide relief shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
