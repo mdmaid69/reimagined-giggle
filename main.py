48d7803c03bfb746708ab297b9e64e38482e28d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:51:34 +0000

    limit past towards familiar tie

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
