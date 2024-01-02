971e247be7a0ce4c895066989a279767d302c2d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:05:58 +0000

    common dead turnaway life gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_length(array):
+        return len(array)
