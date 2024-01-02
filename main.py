3e8abc0366b28fa152fa56eb7ed9c5306763c863
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:55:24 +0000

    whoever weigh concerning eager child

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
