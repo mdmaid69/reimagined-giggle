2c66251f4f1780bdb178b5e507ab2d831048209f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:18:19 +0000

    pinch but reduce 573entertainment heavy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def reverse_list(lst):
+        return lst[::-1]
