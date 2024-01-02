bf74962d5702b326211983a080958a2357e2acee
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:50:52 +0000

    hello approve breakthrough within theirs

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(10): print(i)
