bb12c350b9d753af1c882ff28c4e836909f3efb4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:43:52 +0000

    murder unless bush completely burn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
