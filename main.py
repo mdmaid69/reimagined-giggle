9ecc9ed404af0d64d620a2231784fae5c0dedf98
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:03:52 +0000

    ring willing whistle live glad

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+print(datetime.datetime.now())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
