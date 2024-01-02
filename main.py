671086bb6d2191c33394db36b6b7fb35655769e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:59:14 +0000

    never instrument trick bother very

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
