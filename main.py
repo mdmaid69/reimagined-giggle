d106c48092af78efa3e9f11b6b2992a4826d6c96
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:21:34 +0000

    bang female real love job

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
