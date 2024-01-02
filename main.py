b88cd864be55001f5364695b30c58e6849bfbbc1
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:46 +0000

    confident exciting sink but and

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
