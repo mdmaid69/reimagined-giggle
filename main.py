f38f20011ccca97a220acb2cd7104e6c6797df81
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:56:04 +0000

    rare birthday Bb quantity search

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-print(sum(range(10)))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
