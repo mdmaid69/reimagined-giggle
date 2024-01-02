0fcbbb41bbf9b09d83632456f815dda7d7a51b80
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:54:56 +0000

    advanced inside well urgent fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
