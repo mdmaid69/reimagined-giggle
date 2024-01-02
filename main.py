73911b8ff529fbf3269ef57d287fe7a0c3751a2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:13:27 +0000

    after ought loss anywayanyhow rescue

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
