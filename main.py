ddf095b60cb638faa4b442795ea9e39e6c7aa8e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:09:04 +0000

    advanced less different demonstrate claim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import platform
+def get_python_version():
+        return platform.python_version()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
