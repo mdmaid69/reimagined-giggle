62ad25568abae52278e5a3211553663c77279f74
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:29:14 +0000

    claim surprising increase save poor

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print("Hello, world!")
-def find_unique_words(sentence):
-        return set(sentence.split())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
