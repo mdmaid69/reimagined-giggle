592578a4cf1fa593e73cf6000ad7a898e6be7bdd
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:01:14 +0000

    breakinto way lie generally much

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def calculate_area_circle(r):
+        return 3.14 * r**2
