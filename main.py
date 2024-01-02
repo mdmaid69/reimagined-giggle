86d529805ac08c674a71d5d42da743ff18d9e963
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:07:38 +0000

    build pleasant direct harm acceptable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def convert_to_hex(n):
+        return hex(n)
