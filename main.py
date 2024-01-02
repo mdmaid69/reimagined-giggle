3d3a8616183d90e67dbfd3701b3c761fe95eaf74
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:15:07 +0000

    furniture science letter fine play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
