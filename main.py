06280bd38b73a49c054402bad026f89c2933f732
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:04:07 +0000

    aim much steady joke glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
