07212872437f94303cc750d648a42ddafdcb2e97
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:55:45 +0000

    year gift cost lookback(on) attractive

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
