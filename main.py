7b9bc95c709542b4df6c44466cf1c90e314deb13
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:16:30 +0000

    shallow shop urgent produce suggestion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
