00a23284a97e8ba130fe78590088827d5b4bb6f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:20:01 +0000

    watch disappear come person team

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
