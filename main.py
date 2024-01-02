bbdfb4e7f6d0558532f4f7d92c74b9f94d6c347b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:22:58 +0000

    debt definite decide object race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_length(array):
+        return len(array)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
