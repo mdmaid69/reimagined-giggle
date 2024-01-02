0375e8ac4d3efee5ae8e98623ffebf9bd1342262
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:31:35 +0000

    concerned afraid then rubbish vision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 def calculate_simple_interest(principal, rate, time):
         return principal * rate * time
-import math
-def calculate_cosine(x):
-        return math.cos(x)
