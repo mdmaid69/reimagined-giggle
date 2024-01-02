6983f326d53086f19926d88b7db517f2ce758a43
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:31:43 +0000

    back to thankyou in woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
