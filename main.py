ee76f1522f101ec744db6d75c628ceb635051f98
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:46:50 +0000

    considering thick cry care variety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
