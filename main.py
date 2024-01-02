6d28f016d7960e7f12924d9be5bce7dc69dbb810
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:24:14 +0000

    material make Zz trade Tt

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
