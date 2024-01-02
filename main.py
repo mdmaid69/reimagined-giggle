3975222925e61db2a7f8fae881d1e4e9d9ff1add
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:17:05 +0000

    well tie light strict invite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
+def calculate_area_circle(r):
+        return 3.14 * r**2
