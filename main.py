8c3b45c1a8ab62b8f8e7000182445deee8bd9acd
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:57:57 +0000

    near miss watch promise challenge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
