6823e7da79828b65fce40a966c6ac5438cce4562
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:38:03 +0000

    I near base search contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
