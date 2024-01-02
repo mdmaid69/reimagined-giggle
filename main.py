f9ac2c11c177bc93810010ec79146b211b8a1b7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:52:40 +0000

    force towards lunch lesson pretend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
