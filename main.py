162a4b7265467b489d14840d77b018e4b3b9a078
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:41:09 +0000

    team hair when course sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def count_characters(sentence):
+        return len(sentence)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
