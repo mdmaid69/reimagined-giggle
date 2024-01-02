dc58c89bf61590ac87652b351bd10184cb889b69
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:21:01 +0000

    reduction before last rely mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
