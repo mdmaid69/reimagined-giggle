53b18f4c5e8d917eca2762f9584ceaa7f1b7ca0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:11:14 +0000

    show performance board fit weekend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-  def convert_to_hex(n):
-        return hex(n)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
