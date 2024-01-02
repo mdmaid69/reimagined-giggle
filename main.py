57da966ab25cb05cfb5113acc163ac900d5c7e06
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:10:56 +0000

    issue thought used suggest show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
