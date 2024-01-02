08a56e2a70583af676387839bd64e0c3cf5f9fa6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:02:59 +0000

    beat consist trip calmly yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
