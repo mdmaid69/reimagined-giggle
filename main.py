f452916cc7b12312457e1acf197488792b280287
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:31:25 +0000

    neat post his past hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
