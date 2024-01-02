f7b5444b15898d5f9a5924a7c23698356725e5b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:48:30 +0000

    give turnaway coffee ask film

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
