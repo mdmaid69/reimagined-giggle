776dd7871011be2f4bc29ac4b4193202e88c67a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:57:44 +0000

    insist exaggerate in somehow wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def divide_numbers(x, y):
+        return x / y
