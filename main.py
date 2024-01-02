4a253c45b3e39727be8a87900220bc02dbdc2a5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:55:21 +0000

    whisper turn fasten independent definite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def convert_to_binary(n):
+        return bin(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
