1c9090bc3dbcd721a64120e7640dce0b69e81b3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:22:11 +0000

    thankyou place cruelty float offence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import random
+def roll_die():
+        return random.randint(1, 6)
