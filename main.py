71f404ecfd4f8ffe83997c851dd10a667304cd11
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:52:54 +0000

    remark run cost succeed fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
