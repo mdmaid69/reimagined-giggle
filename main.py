82a811cafa6a3f60e594ddbfbe11636e7e0fd326
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:25 +0000

    busy correction from rain gather

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_elements(lst):
-        return len(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
