be28e2c2fbaa375975de2a9613184d55aa540672
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:14:09 +0000

    recover recommend material complete official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def reverse_list(lst):
-        return lst[::-1]
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
