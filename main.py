64bca76b8dd51d2318ce027c664682620c3fe0c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:23:30 +0000

    pain concerning like drink fresh

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
