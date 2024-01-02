fa2e194fb8980622ec3c732c3b17cf436fc926b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:33:51 +0000

    gas television comeup goup recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
