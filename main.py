a2e3757650bfd72c31fde5968fb8d6a52434d82f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:14:42 +0000

    change negative natural noone view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
