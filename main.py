003da4bd007f673833dd399f0dd6a50de9d1b514
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:43:58 +0000

    age indeed official convince once

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+print(time.time())
+import os
+print(os.getcwd())
