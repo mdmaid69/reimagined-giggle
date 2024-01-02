b49a6b3feac70e3567b6a2b006862a4db5e2152a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:02:07 +0000

    hard fairly guess cut everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
