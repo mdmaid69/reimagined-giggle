58f4db9377958eb4a3efcae4d51c83353ad97e9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:27:20 +0000

    touch reliable fall surface climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
