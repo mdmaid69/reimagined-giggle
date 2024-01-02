32c8d634e6788a0547dc27fabc57da5bca08c5e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:08:06 +0000

    period to choose whisper remember

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
