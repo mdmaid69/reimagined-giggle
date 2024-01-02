324f7173d4ceb0bef908e020d47c96aaec8ffc48
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:38:14 +0000

    backwards pencil low need advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(10): print(i)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
