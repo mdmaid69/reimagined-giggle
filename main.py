daa4e8dd1985feda02042df514e60d32d6840405
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:30:27 +0000

    beautiful avoid sharp tape on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def get_array_as_str(array):
+        return str(array)
+def divide_numbers(x, y):
+        return x / y
