2ad32f34fb2ac6de9d8ab184a80435e27f5b87c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:53:03 +0000

    target dry true separate ask

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+for i in range(5):
+        print(i)
+def calculate_density(mass, volume):
+        return mass / volume
