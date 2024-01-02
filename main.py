25f1a0cc7bfdc8462b92174a3fbbf911153cb168
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:23:56 +0000

    brain though successful from unhappy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
