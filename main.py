0420ce243ac38475fa7e246ba9f9a13c7029b8d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:16:07 +0000

    exercise convincing near now bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
