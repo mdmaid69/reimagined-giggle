58f5da69f1f75f440f37193d6aa2fcb2e91a158b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:36:23 +0000

    container physical add slip ago

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
