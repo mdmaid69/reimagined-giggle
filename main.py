0616cabd217b5c1b65b3858d06afa0e66555c35a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:59 +0000

    whoever write hot reduction report

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
