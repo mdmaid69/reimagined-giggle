bc6214f6fa586b85c9229d17aafc986265792c34
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:15:57 +0000

    win sentence bend listen content

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
