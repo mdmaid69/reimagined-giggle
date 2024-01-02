cef071a03479dc170a861d0c85a1cf7bdc584e95
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:24:29 +0000

    shrink unkind centimetre cruelty example

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
