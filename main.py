b3f52842435c309460b0539a1e568b2eb396e769
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:34:17 +0000

    threaten centre concern left upwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
