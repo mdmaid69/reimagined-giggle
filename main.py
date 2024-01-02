3f6f63d195011b822c793db926266e78e5ae932e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:45:59 +0000

    shout sink spare better modern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
