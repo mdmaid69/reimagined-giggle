f75e3bd8b2e1dc363de3f549efca5f24b195c5dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:30:15 +0000

    Ff father include in attempt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
