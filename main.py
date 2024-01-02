97fe20f1a743ffb29830e8d241f0c3aee6600462
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:21 +0000

    shrink pull be pot wrap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
