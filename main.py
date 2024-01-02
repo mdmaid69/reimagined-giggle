09bbac5715ede89b5a49009d7931f3d591473895
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:19:00 +0000

    decide usually turnout danger nearly

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
