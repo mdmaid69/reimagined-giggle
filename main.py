f6707687070a0e749689ec3b842d4a78d9a8a6e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:20:46 +0000

    smile twist snow demonstrate blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
