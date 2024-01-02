5cb38d9a17453e5afda19c0c5661a0dc6a9bfd76
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:45:05 +0000

    clever odd child behind pure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
