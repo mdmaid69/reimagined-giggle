a48a86d4a9f30d3c32bd5465256227f4fe4a766f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:31:24 +0000

    special goback(to) perhaps pretty neither

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
