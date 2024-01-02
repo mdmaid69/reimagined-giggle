10356bb492f3b1e0a1dc06f7b10adaa7e775a9f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:45:39 +0000

    rain will colour used afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
