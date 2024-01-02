ca452d4205aa52cb0b81e24af93012c128291049
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:05 +0000

    petrol moment about pick bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_length(array):
+        return len(array)
