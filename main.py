64bb53ab4231d0535b4debc61662c08c4918a672
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:16:15 +0000

    pour favourite convenient unit properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
