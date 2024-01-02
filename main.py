bedd8851b8b31b04800869408946a1b4d5344d7f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:03:54 +0000

    repeat comedown future convincing pause

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
