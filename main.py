2870caf78937b03feaa192dd1a8079af80e18a28
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:33 +0000

    muscle capital thank genuine company

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
