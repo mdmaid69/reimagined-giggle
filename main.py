f93a8d08ac87aabacd5f8dd81c93a4dce9522eb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:20:39 +0000

    season develop mention sink maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
