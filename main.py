4c34d9fab8d91b92d18eec6e767a6e6e61b9f6d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:07:00 +0000

    middle Mm out path afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
