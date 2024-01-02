8b476bc25a6ad5869090aca009deef5545eb59bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:12:16 +0000

    appearance long perform hungry previous

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
