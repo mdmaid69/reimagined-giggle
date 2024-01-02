d97492dae0f0fed5c38ec58901456d37e30d2a95
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:46:25 +0000

    pretty proper yesterday grateful responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
