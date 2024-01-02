76bd556e4942b013d639b766dabd717d13ae3893
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:18:45 +0000

    discover tight injury murder safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
