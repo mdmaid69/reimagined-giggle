0a1e1d7769647ea89b1aa8f19b1ee1c72adb5b02
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:50:38 +0000

    description downstairs able square money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
