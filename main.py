30991b470831422eb779809cbd378097fe70756f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:52:59 +0000

    memory past often offer record

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
