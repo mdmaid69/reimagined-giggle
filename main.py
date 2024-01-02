a68a0bae5f49d059016b030e5cd1e7b988433320
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:07:48 +0000

    talkinto rubbish divide answer sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
