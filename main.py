daacf32224485c8c7ddcc240f958fcb037630a01
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:58:01 +0000

    level late whenever hurry off

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_work(force, distance):
-        return force * distance
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def reverse_list(lst):
+        return lst[::-1]
