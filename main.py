cfbac2e77de00d82c40f411a394041fcceafa6a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:11:18 +0000

    relax slight Bb rest drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_index(array, item):
+        return array.index(item)
