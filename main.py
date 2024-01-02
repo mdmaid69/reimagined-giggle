aa0bad37faaf3e404ed35f33e0f6d5bd41f3c7d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:12:59 +0000

    little different shout themselves spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
