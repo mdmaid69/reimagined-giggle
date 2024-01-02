28e4fdf6bf2fe9e2b11608126e37b67930ad3fbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:02:07 +0000

    deep thought over morning dish

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def square_number(x):
+        return x**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
