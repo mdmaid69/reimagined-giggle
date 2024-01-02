f568774439cfc54addb111e401442b41eea35952
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:09:02 +0000

    however foot public believe indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def is_palindrome(s):
+        return s == s[::-1]
