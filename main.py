cdae66feab8b02e6b6a832116e0d1766b1c2b4e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:53:47 +0000

    fight vote factory appearance tie

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
