7ee98a792a294c18a2029aca584f18efeb1aed63
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:54:56 +0000

    necessary care tour tour breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_current_time():
-        return time.time()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
