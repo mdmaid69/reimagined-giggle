5c4795f57b16738be17cfd6494509d561f0e536b
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:40:15 +0000

    trousers agree pay mean take

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def divide_numbers(x, y):
-        return x / y
+def convert_to_hex(n):
+        return hex(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
