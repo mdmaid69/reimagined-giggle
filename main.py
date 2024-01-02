1ba89ea8bd88e651b15c81769af65bd868d215fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:11 +0000

    exchange address afternoon cold crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
