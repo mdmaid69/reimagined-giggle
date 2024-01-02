410b07f5c8e954af06c9d0e8afe6cb159a6e0e2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:03:01 +0000

    practise attention mouth admit rule

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
