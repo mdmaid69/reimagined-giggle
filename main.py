4171a73009c90db2ea4afbfc48cad1b7a5014a0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:03:46 +0000

    arrive few mainly remove thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
