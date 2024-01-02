4029a44ca31abf3e0a47e69b19396aa93a771f33
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:59:33 +0000

    this degree conclusion Qq cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+print(random.randint(0, 100))
