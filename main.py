5bba5e1df48a21d514d1874d181906e13d1a1c8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:04:55 +0000

    anything flow thought peace drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_array_as_set(array):
+        return set(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
