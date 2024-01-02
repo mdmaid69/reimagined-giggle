1c71956fb8086973f644ba39dc8be62c5bd57828
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:25:52 +0000

    office hollow quit think sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import collections
+def create_priority_queue():
+        return collections.deque()
