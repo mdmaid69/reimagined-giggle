5268458e7d6ce70be5c151b334547287698f2de7
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:00:53 +0000

    drive breakout direction embarrassing bringback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
