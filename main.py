99b41e98fc4f7d62e1925c9d43e4c5eb7545ff93
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:51:25 +0000

    happy Zz pencil along day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
