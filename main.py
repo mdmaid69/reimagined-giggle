c37ec43f088158fb18c18c74057be3d75f271876
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:39:44 +0000

    statement keepout spin repeat Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
