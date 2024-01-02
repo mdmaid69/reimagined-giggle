05b12df3e9f53c237df353152d49c6ed0e3c1030
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:29 +0000

    develop major move train rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
