a2313556b5edea8cfaebf5f138d961b13952c9b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:49:33 +0000

    minimum piece hard translate wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
