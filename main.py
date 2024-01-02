5acb8d710eafd74f4f58b40260ba567ee46d7c4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:45:53 +0000

    might comein board right limit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+print([x**2 for x in range(10)])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
