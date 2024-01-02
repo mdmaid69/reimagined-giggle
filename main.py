f45be37e597b76cc77bc03fb88850dc84ae82a39
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:49:18 +0000

    wherever free necessary getout Mm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_list_from_array(array):
+        return array.tolist()
