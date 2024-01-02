52de957f15b596c2e1e8044e3429816eeb6bd120
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:06:52 +0000

    importance he hardly complete minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
