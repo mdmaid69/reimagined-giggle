f38388779531983fc5eda9e74a8c4251239caa60
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:45:21 +0000

    pleasant rain workout address glass

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
