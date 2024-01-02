416c376bee20e4598bd6795a4c47fc00051fa2cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:26:40 +0000

    refer perform suck few by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
