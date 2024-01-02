c0348a6f247702f30c0b92e8fab37b06e76f6aeb
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:37:01 +0000

    stick common season mad brain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+for i in range(10): print(i)
