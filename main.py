0ebaaf75377b0403ffdd7c06df6fab34c7ab510b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:56:32 +0000

    next should discussion allow ocean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_index(array, item):
+        return array.index(item)
