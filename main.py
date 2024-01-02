3efa386fa2e3ead759fea945e183ffb688591af6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:05:21 +0000

    muscle uponon usually below punish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
