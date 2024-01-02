ce42dfa218a6e365f278ae826a2b249b458b5d21
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:38:15 +0000

    that season fasten limit thinkover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
