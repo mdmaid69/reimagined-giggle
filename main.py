29ff8c61bcf8f5009dd21f182fdd16d6fb984202
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:07:53 +0000

    play cheat opposite safe divide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
