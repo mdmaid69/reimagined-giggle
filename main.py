d3b7f7674e649c2eee103a60594aa19d34b22103
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:42:12 +0000

    turnback left thought pile awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
