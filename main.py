663d2b79a1bcd198e2647dfc5fb57908fe443264
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:06:24 +0000

    majority number tax her motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_max(lst):
-        return max(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
