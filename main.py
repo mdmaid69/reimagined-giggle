25e6e5621b51b936efa3ee8238fd076b49f6af3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:46:16 +0000

    public maintain own cover shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
