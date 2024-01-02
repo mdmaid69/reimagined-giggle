ebe2c00600c6f0a401e821ffe09643a6f76d0782
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:20:51 +0000

    begin agreement light path date

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
