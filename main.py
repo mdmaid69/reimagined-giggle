1f7460e68bef20282f60c47f9ece09f8d7956e11
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:44:19 +0000

    whatever talk country house load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
