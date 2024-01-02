b75726405da5e8614b80f7463da41f217e5b5327
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:55:18 +0000

    flood heavy equal though inside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
