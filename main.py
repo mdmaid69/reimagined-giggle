6a8235372da800ff4fe2fd5ef917d9906384b9ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:30:19 +0000

    you separate project thinkover uponon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
