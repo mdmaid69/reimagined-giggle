c1f198f3c4d0014982ec2e205b8e5ac49c151c4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:17:56 +0000

    genuine sink Ww let careful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
