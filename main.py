73716b0724b6f898a7b8a993ccc2019b358be0f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:30:31 +0000

    happen practise mind charge use

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def reverse_string(s):
+        return s[::-1]
