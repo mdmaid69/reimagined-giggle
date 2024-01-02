6bdb76acf9632eee50fe88759687e87b01ddee0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:34:52 +0000

    stop suit famous listen design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
