bea08b6e161273ef7a027f8bd11eaeeade9153b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:10:23 +0000

    difficult cheat usually comein performance

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
