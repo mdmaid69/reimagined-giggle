d5051fa97cdee786f2517ba337faa43b8021daca
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:00:55 +0000

    hope swim part 571enough heart

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def convert_to_hex(n):
-        return hex(n)
+print(sum(range(10)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
