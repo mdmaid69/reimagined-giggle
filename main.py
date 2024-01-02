07a2572177ababca991768547e34a86dfd3590d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:55:38 +0000

    easy notice wave argument dark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
