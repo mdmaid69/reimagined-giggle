d7919dbe9d51ceb79bdec7d9dd538e50635f493e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:16:05 +0000

    today quit spite turnback protect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def count_elements(lst):
+        return len(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
