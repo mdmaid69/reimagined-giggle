e275e83d27078e5826e16556dcd7581e9fa0e761
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:26:43 +0000

    professional without start temper thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+print(time.time())
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
