2225599a51267a2f0458b901b23f2a42f0ed26bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:27:37 +0000

    love flat risk during steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def add_numbers(x, y):
-        return x + y
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
