3c8ff4dde56df5d91e439b0479034c6c3eb172a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:37:58 +0000

    statement keep hang be left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
