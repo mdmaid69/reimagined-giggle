1c8cfb894b7cb2baa57c13a05f63b0cc5e04ca30
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:29:39 +0000

    poor ordinary speech scene influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
