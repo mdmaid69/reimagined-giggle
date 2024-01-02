610fce6176e5bfc5bd9d90c8c761d67578044274
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:30:16 +0000

    lay mystery next skirt powerful

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_item(array, i):
+        return array[i]
