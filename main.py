c50492f0580eb6ec2a00b47704f8b280cb7a0467
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:34:31 +0000

    protect contain dangerous refuse unable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
