7e8d5ae39822dde800f214a5f9cd8dc6707fd918
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:13 +0000

    by stranger door should lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
