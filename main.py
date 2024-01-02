5fa528705dd416b249ab2f10985bb9714f17c530
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:27:13 +0000

    aware gap past nigga hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
