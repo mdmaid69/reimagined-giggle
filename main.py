c0876875314c082dbcdeee1c100a0281f8c1ec02
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:03:52 +0000

    mind street pity interruption church

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
