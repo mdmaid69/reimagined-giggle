c7201f919fcfcd9cb2763854dcc69cf8af8a411d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:03:01 +0000

    custom hang attract sure pencil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
