a8149a42b69cbbc1c1af88bcb486d14022bcb058
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:44:09 +0000

    limit medium small visit maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
