cce8044862bd75a0861da50b18793d91b55cb0c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:20:58 +0000

    opposite earn surprised goout runout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_odd(n):
+        return n % 2 != 0
