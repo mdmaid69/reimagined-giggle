0b1f50a5f14643113b5d7c7eba684bb26a0077b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:19:14 +0000

    concern train change behaviour sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
