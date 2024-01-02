57f399641cb7b1848b3d3ab8dc6d476712cc570b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:20:21 +0000

    myself limit big clever organize

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
