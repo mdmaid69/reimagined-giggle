64c62cad860d57eb74c8bd5b974b34e1929d1159
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:57:28 +0000

    guilty pot scare spoil they

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
