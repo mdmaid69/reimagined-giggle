fbd4f14bb3021dd067b6dc69d8525abc5c4af557
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:58:35 +0000

    contact decide ball pause guard

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
