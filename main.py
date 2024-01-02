dc68f48e2727c59e26a9a55b13eb57d5320bb604
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:42:08 +0000

    drink thank which standard geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
