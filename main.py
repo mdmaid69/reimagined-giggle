a518b51e494709f19750e069b76d317ec670205c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:41:38 +0000

    takeaway himself similar advise price

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
