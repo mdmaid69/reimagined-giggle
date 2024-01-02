e8a4d72eb03d44c790dc9af5f687bf8b0a607bc3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:40:40 +0000

    sign light answer justice lift

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
