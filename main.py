1ef76019cfa4f997e971b054431b88ad65dfab4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:07:02 +0000

    otherwise brother touch getback aim

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
