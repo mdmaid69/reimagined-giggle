ffe3ffebf36e16a11b014f9956af4b15b51123cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:37:51 +0000

    total expression worry still community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
