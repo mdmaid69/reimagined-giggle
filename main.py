8d2bd79077792b4bbbb11f939719a511e886fea0
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:10:18 +0000

    terrible long fortunate main tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
