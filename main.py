084d1319f0fa0b5d73eab94df215f4f1b05b0d88
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:34:37 +0000

    narrow juice sure pause upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
