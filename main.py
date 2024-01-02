74841200b682ba6feb9025476a9d26bac9707284
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:44:04 +0000

    difficulty mainly fight Ff join

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
