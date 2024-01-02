59038d385f7bb35589badb1903b99cbcbef04867
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:09:07 +0000

    secret safe public harm key

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
