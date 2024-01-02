e42c11d2163b228d8f0db4b08a28f11a27a88615
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:46:40 +0000

    before start market join whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_complex(array):
+        return complex(array[0])
