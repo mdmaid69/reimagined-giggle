018e86f9e49f2117f69ba53b3f01692015035119
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:43:06 +0000

    similar live shock rain scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
