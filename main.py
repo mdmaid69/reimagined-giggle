fc9573320cba4994348b6bc29bdfbb5667cb2335
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:07:18 +0000

    sport behave brown be day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def get_array_as_tuple(array):
+        return tuple(array)
