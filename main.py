a1a58a7b8a810c4a9e11b97131d578df60c72baa
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:15 +0000

    hard to scared phone present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def cube_number(x):
-        return x**3
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_int(array):
+        return int(array[0])
