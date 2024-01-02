048df5658492dd08373b1b4d8374acdf2ceec064
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:17 +0000

    back only pure crash lost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_logarithm_base_e(x):
+        return math.log(x)
