7765bb34526d59e8e97b81865d7568a778824898
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:55:33 +0000

    fast admit bear hospital shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
