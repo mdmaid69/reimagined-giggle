94df1cd49e382993ec87fbdafc58c330bcaf9905
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:51:23 +0000

    building talkback record godown discuss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import collections
+def create_stack():
+        return collections.deque()
