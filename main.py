5d86bc6d9013409a84b27a5493520df75dfe4a76
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:23:26 +0000

    comeout card lookafter programme early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
