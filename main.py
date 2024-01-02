4a0aa1969a4fe94180b8d66f68ccec66823e52a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:12:54 +0000

    giveaway style seem congratulations 565end

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
