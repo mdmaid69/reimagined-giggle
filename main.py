a114a7727dc9584016b7e2524ea2bfd2ae692615
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:44:38 +0000

    imagine wild good contribute declaration

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
