59edb113cadca40cc42eab77c57a07f8b9288a5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:00:17 +0000

    squeeze beat takeaway collect giveup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
