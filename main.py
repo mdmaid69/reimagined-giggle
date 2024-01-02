a101ef851bb0cf2b2d70cc6ef1560211c049d037
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:03:29 +0000

    shallow pleasure goaway shine interest

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
