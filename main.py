963a95970b0fffe7b8ee19ecf8b546dbfaf1f0ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:56:01 +0000

    her cough tip opportunity visit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
