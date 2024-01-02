6404143bb94e0f554dd543202f80cf57e35440a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:28:40 +0000

    brown dangerous dinner share better

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def change_working_directory(path):
+        os.chdir(path)
