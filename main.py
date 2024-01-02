b6cf5776312edbf8dea19e48c14fb00f84ed57e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:28:56 +0000

    accommodation market drop kill snow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import random
-def roll_die():
-        return random.randint(1, 6)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import os
+print(os.getcwd())
