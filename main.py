4ed95d2159c7f32d1e1c36ff9b76509da7424acd
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:59:11 +0000

    comein recently lesson exchange underneath

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def greet(name):
+        print(f"Hello, {name}!")
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
