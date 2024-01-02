430b483e9070c1d45f5b0262ae45512aff1d8de5
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:32:40 +0000

    week usual worth doubt difficulty

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_work(force, distance):
+        return force * distance
