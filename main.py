657ef755ea13d85713f3c4f5579c92f63a5ac419
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:29:56 +0000

    division if exciting should meaning

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
