fcd9eeb3f82366787dbdf8a7374fa120aaf04c5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:11:39 +0000

    pay interview clearly early cheat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
