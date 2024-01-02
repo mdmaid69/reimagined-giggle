4c17befa16fc2385631dea525b10ff84964191a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:26:50 +0000

    only such behind formal shall

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
