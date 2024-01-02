317b82f372340f2b5a00b5a09b93494b7cd561bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:23:07 +0000

    cut control coat lost impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
