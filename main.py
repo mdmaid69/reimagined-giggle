9c8fbc89b5f8eb682aec233778290040b49bd489
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:02:56 +0000

    book dream fine through transport

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  def subtract_numbers(x, y):
-        return x - y
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
