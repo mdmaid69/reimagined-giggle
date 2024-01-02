2642702ad42d151130c88cdc23cf302963ec94db
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:34:14 +0000

    shopping several performance gap please

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_work(force, distance):
+        return force * distance
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
