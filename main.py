2c5fb34867c920b24037b628cf93002a11751223
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:58:48 +0000

    week whatever over always interfere

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_bool(array):
+        return bool(array)
