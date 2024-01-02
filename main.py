4454a9f18f62afd1531c4e10f551b31bc575c345
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:03:18 +0000

    interfere urgent cause prevent comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
