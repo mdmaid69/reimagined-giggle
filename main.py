bef8b56453190c81fc6f6041c1d74aaba006517d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:54:08 +0000

    violence hard snow explanation false

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
