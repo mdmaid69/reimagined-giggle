5340ab4c944dd460649d2ea784af8da363dbf78a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:22 +0000

    purpose back matter getup hurry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
