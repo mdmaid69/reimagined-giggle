70b6ce7d124cbddd85f20be9f078153d3b5cf5ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:13:30 +0000

    shut underneath beside law activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
