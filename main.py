a95e4b822d20565fdb78eacfaf348c3930fafe25
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:00:19 +0000

    doubt comeup violent female anybody

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
