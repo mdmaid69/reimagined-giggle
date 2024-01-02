e317d32fe8cf383f7999c91486a524dd0d6ef024
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:56:19 +0000

    special guess plan appeal talk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
