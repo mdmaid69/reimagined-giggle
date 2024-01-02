995a346e755f585f65e0210735f8645f6224c67f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:07:07 +0000

    broken teach comeround him ignore

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
