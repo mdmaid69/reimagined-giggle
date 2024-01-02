410eb36e292568c6fb71860568fe3a41b6d3eece
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:43:26 +0000

    stare interrupt sea set lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
