13806faa62fa0ef1b0ab36f684edc019c9e8eba1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:23:20 +0000

    dark strange pause mood very

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_typecode(array):
+        return array.typecode
