16eb96a0302423633f9badd635a17edf673093b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:25:30 +0000

    substance hear yesterday neat grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
