73b94d04aa4011af14faf8d759dee94cb7f792d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:13:34 +0000

    undo blame return plain command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
