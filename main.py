88bd0408960e0ca5315ddcaa6cefa66abb70d657
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:31:01 +0000

    figure develop call responsible mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def find_min(numbers):
+        return min(numbers)
