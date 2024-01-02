f236ae2c028eee6078ea50856cb887b7702ec67e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:04:15 +0000

    recognize learn life say bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
