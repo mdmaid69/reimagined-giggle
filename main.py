7817e15af4b5ab5af41be287f626868a0796a08f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:21:18 +0000

    almost float past partner memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+print(time.time())
