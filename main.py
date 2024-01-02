6ab000c4d42750978de64a03abadf2aae5010703
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:01:53 +0000

    powerful out trust trap ball

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
