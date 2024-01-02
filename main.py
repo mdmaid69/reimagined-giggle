8bdb85354f95225af2bf9ad1f145a769316c5c47
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:38:04 +0000

    holdup kick date later and

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
