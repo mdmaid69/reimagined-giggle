54924aadd1eccc89e30484150d687f4c87a9b1b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:39:36 +0000

    this wind hang disapprove risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
