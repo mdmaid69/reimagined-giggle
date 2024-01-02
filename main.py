e796137a2a4aa21aff3ce8a1961ad7f8aa157771
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:21:38 +0000

    to tidy ours increase whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def add_numbers(a, b):
-        return a + b
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
