4ba615ed20b00fabf4761af3755cd6df99e20132
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:06:32 +0000

    provide race breakin trick earn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
