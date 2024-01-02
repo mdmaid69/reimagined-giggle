3bd4d8b4944b65f48cd75ce1b2c01f94671d2b00
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:07:00 +0000

    fly past argue past tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
