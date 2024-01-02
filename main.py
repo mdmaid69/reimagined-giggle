3f46d53800aceae764117335832dc74b1bd11884
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:39:56 +0000

    lend spoil pleased similar artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
