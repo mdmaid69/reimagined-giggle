e4fbac41b23a5fb8907d6f6b95f353732da96c08
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:45:51 +0000

    statement I dangerous finish knowledge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
