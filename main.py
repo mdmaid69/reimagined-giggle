1f19bcec203b5cafd702fcf717f92821b63b809a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:40:19 +0000

    always praise instant corner wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  def remove_duplicates(lst):
+        return list(set(lst))
