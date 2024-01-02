28a38c1e91134c779cd1fd48ef2238635e55e675
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:15:56 +0000

    recent emphasis considering contain hard

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
