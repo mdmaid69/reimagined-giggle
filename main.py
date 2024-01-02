09902329d909ad57a0d21202bdf4c87f6b419463
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:12:26 +0000

    degree instruction straight control order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
