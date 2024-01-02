ad39fda45a56c7301b7ae03c566c6c4109d8a1bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:17:19 +0000

    callup anywayanyhow shut wander goup

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
