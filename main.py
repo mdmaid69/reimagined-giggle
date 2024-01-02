2726eb8d20b25323c1aed9dab4fc30f34175d77b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:41:02 +0000

    issue meal measure raw guess

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
+import re
+print(re.match("h.*o", "hello world"))
