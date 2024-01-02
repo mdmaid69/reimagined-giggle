7148b22bb56b059789f2be70b22cd58fe7681ce5
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:34:21 +0000

    must ear walk weekend main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
