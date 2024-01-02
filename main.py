ca395a2ff5598aa4c8de049e92672f461f590f3a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:48:30 +0000

    scream exciting couple trade everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
