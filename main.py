4ff4ba79a2ab5198e6b34eeea21e3374dab9847b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:43:33 +0000

    yours sign lookafter knowledge form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def sort_list(lst):
+        return sorted(lst)
