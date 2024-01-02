6dde642d2b9aec10698a7d8c4ffc208394593bf4
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:36:49 +0000

    cinema dead length kick test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
