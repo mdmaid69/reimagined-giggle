0bcec511d30207f4310acace102715aefb0da816
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:32:50 +0000

    may myself complaint objection soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
