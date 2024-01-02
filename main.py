07ae7013a717bc43f063d0d51260809628d5974b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:20:32 +0000

    statement juice right rise private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
