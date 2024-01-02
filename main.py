581734e1afe20756112bfe22fa0aa966691c9a8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:38:14 +0000

    right loss class phone invite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-print([x**2 for x in range(10)])
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
