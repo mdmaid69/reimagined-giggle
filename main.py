889e3347e29a9205f52b67e526869cc5b4414240
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:04:13 +0000

    street always shock little through

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def get_array_as_tuple(array):
+        return tuple(array)
