70b3f4d493d1af36ab553405f3c6b171c2ecc8e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:26:49 +0000

    search shine 566end song clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
