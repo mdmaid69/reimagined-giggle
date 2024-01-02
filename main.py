c9a1060d2dab42cd78d09e99571878012f0662f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:18:15 +0000

    goout become piece decision correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_speed(distance, time):
+        return distance / time
