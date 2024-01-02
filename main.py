de966c6dd99cfe03d0f164a41236306edd45bbfd
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:26:45 +0000

    Ww raise sweep decision walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def cube_number(x):
+        return x**3
+i = 0
+while i < 5:
+        print(i)
+        i += 1
