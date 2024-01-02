ad9b87abf28b0422800a7f1891aaa7e341c963cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:58:55 +0000

    sort ahead greeting tear actually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
