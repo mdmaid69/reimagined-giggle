c9c1fe80a0a8e03b820e693a02f1cb84d006f46a
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:53:39 +0000

    human criticize holdup pair public

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
