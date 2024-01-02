aae75c44f1be3e79906f1c4bed66e593274a9582
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:13:40 +0000

    stare relationship fit answer material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
