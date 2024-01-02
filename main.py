329421362d0833ac7d6194155946b37f37889400
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:51:05 +0000

    complain task journey education comeoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
