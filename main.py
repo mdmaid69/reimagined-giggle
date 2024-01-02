2ef78f7d46dd44e8cd56127be464800be01e278e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:33:56 +0000

    maybe load consult Tt effort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
