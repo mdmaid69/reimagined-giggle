18c42a8c8a194f7438e6e8f41090568f987530f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:22:24 +0000

    guess where motion conversation best

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
