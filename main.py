a8af5a89d7f18433d998a2e80d8e62620b075202
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:44:36 +0000

    quarter height bottom use contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+def is_odd(n):
+        return n % 2 != 0
