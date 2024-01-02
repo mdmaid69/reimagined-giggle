fcac74d68cb16677e63613171beea90ee5a91d51
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:44:47 +0000

    run pour giveup keepout embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def check_if_array_contains_item(array, item):
+        return item in array
