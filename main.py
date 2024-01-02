e356bd1256dbdfde6d84052d0dbffa0dbc0ea96e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:15:39 +0000

    gift minor surprise standard whistle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
