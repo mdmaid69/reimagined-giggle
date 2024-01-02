3d76185d30cba23494f6a1af4fc4f22d45dcd023
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:03:07 +0000

    ever model black concentration drop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
