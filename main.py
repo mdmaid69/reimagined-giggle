55a0deaaef52653d03ad161d21665e23f9ce9edb
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:09:21 +0000

    song piece evidence afraid national

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
