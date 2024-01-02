cb4255b5d6ceb6fbe46d9cafaf293c7ab7ee11db
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:50:43 +0000

    home sleep left conversation organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_even(n):
-        return n % 2 == 0
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
