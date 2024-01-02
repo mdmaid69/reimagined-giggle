dd8dbd6be0b39ea00cefc30a09546e8f6649a9c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:53:40 +0000

    ahead from history add excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_tangent(x):
+        return math.tan(x)
