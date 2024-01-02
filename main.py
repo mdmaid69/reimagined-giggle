c14e9910656d15bdb7b134fce5f7e34c30148847
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:22:45 +0000

    mark responsibility without flow lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
