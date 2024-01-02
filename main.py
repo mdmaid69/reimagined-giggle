3b46032862975def84c0c57536af4569400e0104
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:10 +0000

    direction injury raw injury turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
