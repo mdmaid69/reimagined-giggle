fd6631245f21e810231c417002a3509be5e38cc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:29:35 +0000

    towards circle lot tooth relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def is_even(n):
-        return n % 2 == 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
