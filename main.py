ccdfeb558d04b0bf1ea5fd25ca00a773f846e296
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:51:09 +0000

    no wish fork polite shadow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
