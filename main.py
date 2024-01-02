192b288db632832c94057bdad10cf3688551fab4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:09:11 +0000

    impressed table point will stranger

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
