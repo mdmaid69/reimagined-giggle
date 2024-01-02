ef5317526b15d37d3224413e167a65a2e953b6f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:46:27 +0000

    by completely complicated the ball

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def set_array_item(array, i, item):
+        array[i] = item
