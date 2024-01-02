cd8cd57565036b334bc97c2aa59396557dd92c7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:28:21 +0000

    disagree illness whom plenty guess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+print(datetime.datetime.now())
