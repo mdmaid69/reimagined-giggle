fa2a01d8a43555e2ad2302da6471619dc0eb4f75
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:16:39 +0000

    bear giveback arrive getout negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def reverse_array(array):
+        array.reverse()
