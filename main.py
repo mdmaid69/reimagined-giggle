4b3a34a302211aa678d6eb0932d3991ee5aa1501
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:06:54 +0000

    popular Hh accordingto priority healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
