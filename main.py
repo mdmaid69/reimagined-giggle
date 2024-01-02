d917dbfcf04b9663bc400a1ab38da03c090fc229
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:55:36 +0000

    dream instruction mend everything switch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import sys
-print(sys.version)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
