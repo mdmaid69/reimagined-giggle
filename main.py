488ef87f94b94194a80acf5580acf102da5c0742
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:27:35 +0000

    mail trust shake remove tone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def add_numbers(x, y):
+        return x + y
