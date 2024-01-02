19b062c41ad9e1ab72614465b0468767ebdf2417
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:42:29 +0000

    violent unite sorry pass female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
