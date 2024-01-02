5e13916a2242dc38595704050fa56793a57b6e2e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:34:58 +0000

    uncertain study mistake fat command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import time
+def get_current_time():
+        return time.ctime()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
