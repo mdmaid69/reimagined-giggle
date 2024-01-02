59bb7ed531d675c0ac73b4edff6f5af1a117cca4
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:38:57 +0000

    combination fix holiday develop slightly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
