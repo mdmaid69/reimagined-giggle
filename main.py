060e0a4469eb1b5171bb1014414c1b0bb7781a51
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:36:13 +0000

    busy whether another keepup violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
