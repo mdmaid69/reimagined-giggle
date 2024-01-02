328f5ba140001ba494c4f2e00c66a6a7ee577e6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:34:25 +0000

    nothing typical write past dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
