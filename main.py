9f2c189e4b81630c32270a10d8cfc39b2c209ddf
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:55:15 +0000

    wear rent chief rain scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
