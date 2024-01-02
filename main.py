05b4e74a7bc6baaafda90c380a1fa4b28c84ba83
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:32:59 +0000

    workon hour transport black vanish

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
