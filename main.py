ab56cb2c57ce23b4b2108d9cb2e09318a47e83e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:19:05 +0000

    carry little minor crazy play

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+print(sys.version)
