fdda93a2b0ccaddf336293f89cc390ac7136ebf1
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:23:54 +0000

    street getaway correction comeover dear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
