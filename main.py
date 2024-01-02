a67c741cd2ff02cd2f6789774373e3604388cc62
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:14:21 +0000

    drink peace want correct activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
