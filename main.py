d1e39f3a2ce0475d1439ff2c6df13bb7922e4503
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:39:20 +0000

    term fat here lookup flood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_volume(length, width, height):
+        return length * width * height
