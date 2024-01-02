fbf405a195c227431266f307b36c538397a2cf9d
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:59:24 +0000

    road nice bit legal definite

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_area_rectangle(l, w):
+        return l * w
+def find_max(numbers):
+        return max(numbers)
