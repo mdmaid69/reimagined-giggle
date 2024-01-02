3b4cc7e747cecf7d074da81f2c9e5473d37c8b74
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:10:02 +0000

    event where let motion shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
