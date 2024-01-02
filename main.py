289c29dc705df449da0cce844c67d5b8ec76e5c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:06:50 +0000

    exaggerate push aim wrap pause

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
