a9f128eadfaf4edae2409c22cda52fe3c2e59532
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:32:07 +0000

    learn sweep hide snow preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
