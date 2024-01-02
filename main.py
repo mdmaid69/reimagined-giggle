f51718c5e289b8cc868f1a3d7cc5dc0d95dd765d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:25:34 +0000

    recommend wake about it change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
