f91617c10090db950769501fa60727145ba17399
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:13:44 +0000

    bye picture damage friend sale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_numbers(numbers):
+        return sorted(numbers)
