20cf3ad303192b6b1353f0be05c746d980d94107
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:11:43 +0000

    572enter container among foreign report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
