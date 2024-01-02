8223c33ae4184c146d2701fb0e2c2d1b2674e842
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:44:36 +0000

    single pipe spend admit news

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def square_number(x):
+        return x**2
