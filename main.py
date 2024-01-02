f8d38d4cf61a8c64f81d83ecf58081403fd6a998
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:01:34 +0000

    roll someone sick most over

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
