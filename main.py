c453dc93a1790600dc38ba2de178c6ad0ec728dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:36:41 +0000

    shallow wet bush satisfied somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
