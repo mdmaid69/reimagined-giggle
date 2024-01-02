ec4b29cdc570ba2ac6a1226771698f7f4fbd2874
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:31:22 +0000

    upstairs wave fear little many

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
