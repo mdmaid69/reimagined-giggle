9394faff6fa40d8f297d0dffbd742b3c2fee1df9
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:41:29 +0000

    actually complaint something closed eat

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
