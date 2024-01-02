6e7a428aaf6cee26d1bcd40a2b814eb8323b21d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:42:12 +0000

    cruel which deep female fear

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
