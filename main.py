4675e1f91819b03695e825c2c1bad86ac6181b61
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:38:50 +0000

    mean sink turnon lean undo

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
