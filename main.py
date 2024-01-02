4a92b8be0993dc1e19a087050ba25b0ce9bf128d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:04:15 +0000

    one considering 565end skill wait

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
