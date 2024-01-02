3ce33e020b4463df8264a7c683ace46f62702571
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:09:29 +0000

    appeal extra closely smell turnout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
