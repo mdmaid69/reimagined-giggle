d7edf4324d56ca12ee494067e9e1389c25bc95a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:13:41 +0000

    flat possibility quarter instruction pride

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
