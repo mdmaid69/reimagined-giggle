997ccb4dbf04dc088041b6d74791aaf7328860d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:12:19 +0000

    answer they typical takeout talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
