ff40f1807d5ffde225cb584907d7d8af88904afc
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:30:38 +0000

    female fierce turnover illness watch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
