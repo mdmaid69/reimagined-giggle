db1a64bf4b3203aac2332935ed0744503658be23
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:37:13 +0000

    home personal Yy pin boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
