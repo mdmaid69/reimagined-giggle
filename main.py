ca62a810fe45a53434553ab34d17bdebd3c86b44
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:08:30 +0000

    shop getback take chin be

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import os
+print(os.getcwd())
+def convert_to_octal(n):
+        return oct(n)
