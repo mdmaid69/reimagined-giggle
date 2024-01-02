ca35cc9baa917db6b35658668a20ac9c14d95ab8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:32:13 +0000

    interrupt smell lunch unit backwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def is_odd(n):
+        return n % 2 != 0
