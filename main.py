4d85f2463e61af11900a347f398ff6a78a0a99c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:48:31 +0000

    either freeze hard trap official

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_pressure(force, area):
+        return force / area
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
