b066a9f830edfd1752fd50e170093b0b094a7f72
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:37:01 +0000

    such thin contact commit still

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sys
+def exit_program():
+        sys.exit()
