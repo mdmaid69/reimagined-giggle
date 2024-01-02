a6ddf5590b4d576a4c521ba2071dde48751ed8b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:19:59 +0000

    ago down whistle everywhere land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
