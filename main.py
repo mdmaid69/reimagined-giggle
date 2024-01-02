ea730b09bff0cfe036a06214f2e841b9933cec6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:50 +0000

    tradition that mean persuade behaviour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def cube_number(x):
-        return x**3
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
