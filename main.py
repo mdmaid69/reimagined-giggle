0d18a8d201fecf3ada742e4dbc63fa94772b651f
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:48:46 +0000

    offer future steady straight to

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def square_number(x):
-        return x**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
