f5a28187e727cb92544e9cbee95dfa772816c7f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:14:48 +0000

    exhibition before some important almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
