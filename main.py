4170b4940a9b2b9baef57c2be551c5644a3666c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:12:42 +0000

    somewhere entry far my close

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
