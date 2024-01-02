140382b5b86956bb58cc60b3eaebe19053c50fd2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:15:43 +0000

    discovery whatever discovery open proud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
