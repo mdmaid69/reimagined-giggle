36c457d0e3bf568c085d8747bb81eabf25810de1
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:17:54 +0000

    ahead unite stay else legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
