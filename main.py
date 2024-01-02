64ec0e6662a8c14fc765a88e4d2708bf8cdf0a8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:10:54 +0000

    right colour peace seat recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
