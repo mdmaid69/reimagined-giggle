c6e809f76fb2ec482d9f20d857a7451671780785
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:50:37 +0000

    though address row us father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
