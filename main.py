5d9030307a9942bb8fec9464cd5df464742ded91
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:42:16 +0000

    valuable match service spill cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def cube_number(x):
+        return x**3
+import math
+def calculate_error_function(x):
+        return math.erf(x)
