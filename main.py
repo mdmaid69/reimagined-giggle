4e0e2244e51cefde454d2a81c6cc9b78f007e4a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:53:20 +0000

    mine bottom scale bush fear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
