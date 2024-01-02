de5a441f1f04a65c60463e9c0bc40b14eb1ac17c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:17:57 +0000

    stare careful squeeze confidence probably

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
