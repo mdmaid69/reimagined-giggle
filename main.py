fd2ad175dcd2f32925157a96521c0bfe0f6ce2e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:15:46 +0000

    comeover aware sure value address

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
