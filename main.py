ad2673409f5d884f6d294ff96ddaddd6e6621c10
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:41:19 +0000

    comment time clothes visit careful

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
