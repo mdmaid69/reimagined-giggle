3b572c7376f75b26f1c303cc0604fd8e2ac2da58
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:10:45 +0000

    motion risk male branch custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+name = "Python"
+print("Hello,", name)
