b5c89d9935489ada3a27e102fc77800815f2e6bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:43:51 +0000

    nothing regional success breakinto near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def sort_list(lst):
-        return sorted(lst)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_pressure(force, area):
+        return force / area
