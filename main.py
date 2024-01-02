4280f16f94d1742e078fdc95b8ce6b67e2cebc33
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:20:02 +0000

    bottom authority later recovery storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import platform
+def get_python_version():
+        return platform.python_version()
+def count_elements(lst):
+        return len(lst)
