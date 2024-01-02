856022669a17fc722179831be1b79f3f4a453a73
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:01:28 +0000

    tired should dream transparent treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
