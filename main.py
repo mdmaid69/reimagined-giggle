8b8e2d9a61b334b4b11c229c8dcc20f253671fa1
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:47:36 +0000

    muscle object paper uponon care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
