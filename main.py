5b40116ce15177b0b97180ae98d96ad3b53e8269
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:41:58 +0000

    gradually instead splash advise tape

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
