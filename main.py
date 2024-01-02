84803b9929fca3d6b08218c5f5a9f07804ed36da
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:40:45 +0000

    handle dangerous quantity fit this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_odd(n):
+        return n % 2 != 0
