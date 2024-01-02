39e2bca67070e9bc565a8dcea92f4fe750033a5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:28:12 +0000

    tradition final smell shop combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
