cf73ae453ff7b6021bf3f8ec04b0a25f59f78a2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:34:25 +0000

    group usually education wave slowly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_binary(n):
-        return bin(n)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
