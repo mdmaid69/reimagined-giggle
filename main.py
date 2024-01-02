a299187c75f32ebd73322fd3bd60e4325e2727fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:28:02 +0000

    unhappy season university protection sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
