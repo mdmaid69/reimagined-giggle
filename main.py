37b2ac42168558862500d976ed1e1f61157ce670
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:55:45 +0000

    office test evidence nigga happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
