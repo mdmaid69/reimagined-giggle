5012cf3549bd05bfb1007f400d101088013536a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:30:10 +0000

    window pride temper take delicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_volume(length, width, height):
+        return length * width * height
