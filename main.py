a609eaa0b6e66aae376a5e413ae5e59f68209a0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:04:12 +0000

    passage reaction illness to load

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def is_odd(n):
+        return n % 2 != 0
