93a9c044fdd2df3bbb3ec2dfca18f4621f976a95
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:38:50 +0000

    software confuse sudden onto turnover

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_power(work, time):
+        return work / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
