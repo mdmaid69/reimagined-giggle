ea37e6821d0c9410ff60aa279a6a8891ef7d7fb2
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:55:30 +0000

    matter arrangement excuse carry abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
