62e38db941b0cf3aee248a0d1cfd6c9e8fa1dd06
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:42:56 +0000

    getout scare approve to live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
