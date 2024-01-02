4cf6cb43a58fb8cc9a24b93b20e2a5e59d7256e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:03:19 +0000

    workon born sport proof full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
