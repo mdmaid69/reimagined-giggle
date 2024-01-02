a694201c404716d316b280ca3bda51017e8925ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:21:09 +0000

    tread sometimes excuse from ride

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
