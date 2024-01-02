73307a50efed4440d79cfa7f77225aaf8c8afa6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:53:55 +0000

    trick prepare make arm centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def is_even(n):
+        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
