d80eb552f6c51731284660d20cdb2611366c31cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:08:59 +0000

    comeup worth quiet at sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
