b795026ccbd16579411579e1c6cf21d9a6ad5fd6
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:49:34 +0000

    waste form flood because course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
