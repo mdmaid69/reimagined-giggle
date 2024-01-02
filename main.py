75d8527591ce57545f718e359336ea90f0520cb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:55:01 +0000

    worry tap proper raw public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_base_name(path):
+        return os.path.basename(path)
+import getpass
+def get_username():
+        return getpass.getuser()
