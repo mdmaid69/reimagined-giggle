961f804f856b39b0ca897d80cf27c099b71f5bc5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:35:19 +0000

    play shoot ilovedicks cruelty friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
