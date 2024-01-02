88092e52d457e132e994ddf6bbdd6b091950ab97
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:59:15 +0000

    period acceptable rescue pencil shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
