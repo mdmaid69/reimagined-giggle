fdcda67aa31ac7a9c9b44e35fbd17d80df5ae54e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:22:25 +0000

    wind lose close drink live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
