7ae2b17ad6d69469820e8246970e0ac24beb64cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:19:42 +0000

    second cover appeal equal height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
