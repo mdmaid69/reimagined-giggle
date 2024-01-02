76c59fbf51551e403e66d80399b5392afb29d007
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:51:36 +0000

    ticket some breakfast deliver drink

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
