aa059c08de4d9dd2d97867591a04763330d5f22f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:25:50 +0000

    gooff home drink for display

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
