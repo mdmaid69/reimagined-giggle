76caddbf5f39712ecb636fe66feee6e3999d77fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:48:49 +0000

    godown moon bridge scheme reply

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
