3705eed31d766a0bcdd75d35613a44d0029a7791
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:11:23 +0000

    food report fast quarter both

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
