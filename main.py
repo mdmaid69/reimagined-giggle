2976b446fd4d97a162776ede876ff22135ef24c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:50:23 +0000

    dress stone move burn smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
