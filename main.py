05f126b075f6647903d7f6e7ee372aa3d2394e97
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:21:40 +0000

    sting eat takeout spot back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
