3f1172560d5ac2b36203c5bd62ea27312b503740
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:39:25 +0000

    justice transport look heat ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
