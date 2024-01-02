1e5f5b32ee748394b90e5db860c83d1e249693b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:38:02 +0000

    result fat accordingto light open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_area_rectangle(l, w):
+        return l * w
