ba7e82578d3ded2d1804d1841132bafe4cac94f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:18:31 +0000

    decide smell switch truth dream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
