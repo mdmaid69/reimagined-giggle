6309953cf41001d49a392c1361991cb71adfac2d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:31:41 +0000

    story between earth quantity popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
