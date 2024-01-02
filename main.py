672cbebd98b89ac1465e185ffa198e2cc9ee056c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:13:57 +0000

    material eat special party worried

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
