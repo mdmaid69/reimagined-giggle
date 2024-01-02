2665edc04f54a9a0ee945f7805bbe14722bb7dcd
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:05:26 +0000

    juice confidence help proof once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
