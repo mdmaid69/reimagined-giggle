319e6298fa9676acc273057d05b0e4b8761239fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:30:55 +0000

    slightly hurry peaceful better match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def add_numbers(a, b):
-        return a + b
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
