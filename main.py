35857f1ca3650f73a7f9c122e693fd1c028d879e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:40:20 +0000

    unusual broad in likely conduct

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
