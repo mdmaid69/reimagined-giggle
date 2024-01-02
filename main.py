bf1c1292ad382c382c4ae4685bdb7c3a4556211c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:21:44 +0000

    kill try thinkover backwards pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
