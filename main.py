9a78d440749cf5231c29fd1c5bc5d56d1415a04c
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:13:42 +0000

    past workupto check speak fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
