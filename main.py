cbb51ae7208e6b4b09b66844b8ce29e1e39141d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:26:19 +0000

    offence deliver average line case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
