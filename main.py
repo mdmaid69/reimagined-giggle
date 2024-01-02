947fc39944e5ba087db6e682e9c1cf80497e570f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:18:18 +0000

    secret television introduce tie drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def multiply_numbers(x, y):
-        return x * y
+import getpass
+def get_username():
+        return getpass.getuser()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
