a4430f5a187ebebf89aa48a79fb0d58ae497d560
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:52:44 +0000

    ourselves time unhappy concentration shop

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
