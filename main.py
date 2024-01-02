719c9af26b83e204829ff7c103fb4f77d6e971e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:37:44 +0000

    persuade when good relative completely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
