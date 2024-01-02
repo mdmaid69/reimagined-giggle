37e21524ba9277611e878606893a48b2af09fe2d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:50:37 +0000

    alone Ii protection impressive land

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
