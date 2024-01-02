4ca8b17b86467020cdc1ed85e6ab02769902949e
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:23:23 +0000

    sleep belong hold convenient stiff

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
