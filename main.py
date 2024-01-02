49097d41067ebfc498fd01bdff314adec2ee62df
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:00:36 +0000

    fork hide declaration nerve godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
