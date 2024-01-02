11a52393b985b2b841ade10a174beafd302bde50
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:02:32 +0000

    mother big lean because zero

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
 import array
-def convert_string_to_array(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
