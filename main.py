d266e86165eccf5c855b1c105eab7c5250475bc2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:29:23 +0000

    freeze exactly confused show journey

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def is_palindrome(s):
+        return s == s[::-1]
