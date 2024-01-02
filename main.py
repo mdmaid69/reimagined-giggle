0e74f93d084bcf640f1e43d12fd70da424b8b967
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:55:53 +0000

    thing university describe lot not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+text = "Hello, world!"
+print("Words:", len(text.split()))
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
