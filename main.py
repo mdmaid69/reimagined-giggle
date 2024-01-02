6c8e7f8376b0fc4510bbb86c7e285fe1d0646a41
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:24:24 +0000

    other trap shout dare persuade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-for i in range(10): print(i)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
