341f13b7505c7b6d0b59f77f81f61decff98660f
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:28:45 +0000

    angry keepfrom event noise law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def reverse_array(array):
+        array.reverse()
