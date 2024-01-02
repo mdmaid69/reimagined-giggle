bc514d989c9654f91124c199cc9e3cdb504f65a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:15:06 +0000

    before rich false fairly art

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+for i in range(10): print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
