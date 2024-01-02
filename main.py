2c6b8b6c05707e559a2f2727cef719dc6ff9cd79
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:43:39 +0000

    depend hold about refuse fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
