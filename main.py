a0cbeb16d34ffe7777aaac49db57b6b30a611e08
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:38:39 +0000

    habit tense fortunately level event

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print(sum(range(10)))
