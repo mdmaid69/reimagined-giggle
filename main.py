c195557a06e39af664897f49b1ff9f01fd36dc7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:06:19 +0000

    dead wire permission turnon until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
