41c51d4988a626c6b83e223f7d2f3881e03af808
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:44:48 +0000

    man underneath shop rich song

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
