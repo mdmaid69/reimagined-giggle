ea7b0deea3b75f67bc92342a0956678b72f93c2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:13:27 +0000

    mysterious tonight contact wing war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
