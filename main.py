bcf34ac5a2ea61999bd78b0439292a9e6c7c59d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:27:47 +0000

    approval section twist raw consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
