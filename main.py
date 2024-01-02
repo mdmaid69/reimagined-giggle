6bf14b221baffe58ff6ab71f174e7f1594a17ade
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:42:51 +0000

    answer extra sick scrap smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
