a46681e491eaacb5770105a8329cf94ef2e5c090
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:03:06 +0000

    cross finish important only party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_python_version():
+        return platform.python_version()
