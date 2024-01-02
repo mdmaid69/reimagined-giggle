3ab191addc9623e1401dd3a9aefd33692667f098
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:51:24 +0000

    mad tread involve direct beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def cube_number(x):
-        return x**3
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
