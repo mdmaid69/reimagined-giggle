021da3744a18bc2ce3570c2584e38a0fd0080d73
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:30:15 +0000

    mix issue whichever spring unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
