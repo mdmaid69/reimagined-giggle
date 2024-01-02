69e62524a5d762eb6f690ab422fd5827bcba48ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:08:51 +0000

    shopping glad unlikely basic row

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
