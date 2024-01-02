eb3fe200c21bb0abbcf80c7cd7784305f96b5cfc
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:56 +0000

    prefer unkind roof trip patience

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_even(n):
-        return n % 2 == 0
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
