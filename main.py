56ec6a4343c5c23e18f9ebc223e1442177b95d3a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:38:49 +0000

    uncomfortable tax combine brown describe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
