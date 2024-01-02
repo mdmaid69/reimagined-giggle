de18652c3858f38e1acd728621bb54ac6e90cb21
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:51:32 +0000

    yesterday turnback mass considering exam

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
