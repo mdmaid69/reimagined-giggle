490cc50a5a305d21a524f3f18822a7d16816f1cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:26:18 +0000

    small judge than turnover study

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
