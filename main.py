c106ab970894800515431eb72b761eb1eec17c43
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:16:27 +0000

    arm there old influence ourselves

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
