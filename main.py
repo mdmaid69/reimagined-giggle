4eeaa0873bb6828b7ef5967142b4e74590b09270
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:06:10 +0000

    minimum damage practice slightly hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import sys
-print(sys.version)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
