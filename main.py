e39b200600e2e4f919004f54e24b7002890a6357
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:14:35 +0000

    search lost contrast thanks war

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
