9ac73136440112bf3b803048f0edf79c07ad3ccc
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:26:41 +0000

    word sport near contents exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
