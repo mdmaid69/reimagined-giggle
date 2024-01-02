691ec559b7727013e0e0ca9c279d44e9eecd435a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:34:32 +0000

    soft play ashamed moment listen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def find_max(lst):
-        return max(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
