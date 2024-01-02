b1bb4ecf7e8afa0928a1900d8b252458f5b5ca67
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:43:25 +0000

    choice release thin available welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_unique_words(sentence):
+        return set(sentence.split())
