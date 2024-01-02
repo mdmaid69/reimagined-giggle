58cee7f36cfa60ba174ba547aac7b57bc49f0afa
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:23:26 +0000

    I matter appear until spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import sys
-def print_python_version():
-        print(sys.version)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
