ef522aee9a3fc4fcb0d9f6f0e3da8f4147a69000
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:45:13 +0000

    accept stamp Ff relative argue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import time
-print(time.time())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
