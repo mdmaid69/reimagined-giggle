6527ffe782335ea7bb61efbaf5825dea50de561f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:06:36 +0000

    please suffer long understand workout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_pressure(force, area):
+        return force / area
