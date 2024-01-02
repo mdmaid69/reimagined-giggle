60e3fdc1028408ea5f5cc4e298f0bf3fe30d6082
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:35:36 +0000

    fat stare uncertain squash repeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_min(lst):
+        return min(lst)
