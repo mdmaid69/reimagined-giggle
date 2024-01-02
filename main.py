c83af17a31e44386c39c8175222736fd18b03b49
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:54:05 +0000

    or aim stop embarrassed bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
