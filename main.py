4f8f15cf60748feeafc9a290452dc11c821e6067
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:09:47 +0000

    freedom manage first keepoff offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
