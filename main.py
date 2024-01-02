958a0fee3c39ec039fd6b69e6ee7eefa67f1a71e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:14:01 +0000

    another area cause still anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
