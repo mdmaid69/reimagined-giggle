310f528774f9b0beef05c0c8f9bf53052af06704
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:21:40 +0000

    half trouble become runinto food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
