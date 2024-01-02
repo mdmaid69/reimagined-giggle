39e1e7c630b990672701d884e66d98e173a11525
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:10:12 +0000

    common closed comefrom major bush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def find_min(lst):
-        return min(lst)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
