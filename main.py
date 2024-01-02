8a8c84ba01fa5210f6a4ddd3872dbcbb6db00121
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:10:38 +0000

    responsible joke course attach public

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
