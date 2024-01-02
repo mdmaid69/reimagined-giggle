be16673d4536a21a7b9521cb7f48dcb39eb12c65
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:57:33 +0000

    interesting human down production him

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def reverse_list(lst):
+        return lst[::-1]
+def square_number(x):
+        return x**2
