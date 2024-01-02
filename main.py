e78c2bdd2a352822b075993cbfba579f1c03aac1
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:12:22 +0000

    branch spring book exhibition collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
