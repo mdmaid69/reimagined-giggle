e436cbe681f36203dfbb28920a6a20fd72c84c21
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:45:50 +0000

    confuse sea calloff run path

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
