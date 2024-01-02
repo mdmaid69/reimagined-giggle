7b9f6b2e669cd03aead8e1bbad8277bb19c79d09
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:05:07 +0000

    minimum innocent cinema guilty except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
