c37c4b579733784616059d74ba4a515299859a8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:47:03 +0000

    clear exchange imagine disappear collection

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_power(work, time):
+        return work / time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
