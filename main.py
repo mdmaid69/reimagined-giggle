a1f58764c4a50790205781323bc30ebc94977663
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:22:17 +0000

    table reasonable keep since trip

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
