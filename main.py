71078ec38604ae701a5cbd1959f570df4059bb81
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:29:02 +0000

    rate welcome question copy row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
