5f76edc03890975a10fd5635242ee5bca77bd8bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:21:45 +0000

    contrast strict fairly bar recommend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
