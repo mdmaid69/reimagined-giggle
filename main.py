5b355b2edf0adb0901c9bf7625618150dd5ba21f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:57:01 +0000

    pinch process table against maybe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print(sum(range(10)))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def cube_number(x):
+        return x**3
