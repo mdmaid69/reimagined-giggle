19b82fd9898de2320ae00471351cf4cd9d2e36c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:21:59 +0000

    check off confusing talkinto spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def reverse_array(array):
+        array.reverse()
