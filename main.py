d25541bf6101411373f9eb1a2d62bbe1680b88ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:17:58 +0000

    lead proud frighten hers meanwhile

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
