978441191893cacab81483875d1e31dbbacb863d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:23:50 +0000

    early soak sticky effect weather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
