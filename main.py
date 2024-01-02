6dd23b255df2463cd1b731505727ad7ba6695a5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:30:40 +0000

    beneath future body rough turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
