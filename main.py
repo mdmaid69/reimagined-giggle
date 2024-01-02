0ea7f86109c877b61c1d07a745313f94f5e76325
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:42:06 +0000

    concentrate against which meal hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
