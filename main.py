adc03d51c524fcdebd297312cf92e9aa59f43a25
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:36:04 +0000

    themselves cover convenient profit pride

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_exponential(x):
+        return math.exp(x)
