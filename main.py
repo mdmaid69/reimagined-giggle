8185581462b6f2af1a46c79f760d51445c578d60
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:40:21 +0000

    want surprising cost fall noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
