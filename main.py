fee916cd87912bc55cf104db76cf30830e7c82a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:32:26 +0000

    persuade oil fashion unable recovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
