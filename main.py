e3dd6e6f65fa4093877df556de85e3d7a8ec0c3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:56:38 +0000

    more offence cook stupid represent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
