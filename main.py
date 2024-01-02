cdce8c7c4988e6b00dba00b2e08ea5360cf8ffa7
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:53:57 +0000

    cover last simple police report

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
