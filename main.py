90540b0e072344839f7e483a7c2c57005311bd22
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:57:29 +0000

    strict hurry sweet ring underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  def cube_number(x):
+        return x**3
