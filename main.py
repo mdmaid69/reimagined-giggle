ec8419c6ec0facc11637780ed9fa4052e5a9d59f
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:20:37 +0000

    goaway fly walk store curious

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
