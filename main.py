7625616d513b82755e6102e4b74fe38649582a37
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:09:58 +0000

    573entertainment guess crowd stop strike

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-print([x**2 for x in range(10)])
+for i in range(5):
+        print(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
