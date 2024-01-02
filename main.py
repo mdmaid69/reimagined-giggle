ecec45fb41da0b9db7cd782cc25912dad06e9371
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:38:28 +0000

    offence mine wherever lock soon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def convert_to_hex(n):
+        return hex(n)
+  def cube_number(x):
+        return x**3
