70773c97029eae576a1ea99953c9a8896c0f3c27
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:09:43 +0000

    571enough increase spring sticky grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import numpy as np
+print(np.array([1, 2, 3]))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
