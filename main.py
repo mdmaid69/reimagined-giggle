face44e3e9d66f948e4612096f2f2f315c99353d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:41:47 +0000

    hate definite run include fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
