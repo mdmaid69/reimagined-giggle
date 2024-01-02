0a8cb48a8ed78c6a02408b9ffc2641c534820e9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:34:01 +0000

    mine learn balance serve weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_priority_queue():
+        return collections.deque()
