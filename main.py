cee1ad75fdcd8bf7233a8dcec4eb40b8e874486d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:35:49 +0000

    regional circle live crowd about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
