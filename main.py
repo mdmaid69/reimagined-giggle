d7314886c06d34b4ce58c8e4b89c45835db14628
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:33:13 +0000

    not worse scream opinion least

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_bool(array):
+        return bool(array)
