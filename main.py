723cfee29a3dbb9086da34dc7459cf6c46eef6be
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:43:22 +0000

    hope shade to mix bunch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def remove_duplicates(lst):
+        return list(set(lst))
