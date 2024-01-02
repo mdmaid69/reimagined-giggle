93d1ba87d8d339f251160904dad6d02d0159c69f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:02:09 +0000

    wide suppose insist pass by

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
