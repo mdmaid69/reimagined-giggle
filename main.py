da29f6f5c0435ae725206f41677d28c510c31fa9
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:06:48 +0000

    grow job off operate tend

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
