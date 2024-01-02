9916fcc2fc8a1fc070f8544224b27d2c71d3b8ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:01:45 +0000

    Jj straight polite unkind emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
