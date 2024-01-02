51f5ee0a78ce09d1316ade9e92ef4dcd99b600fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:35:45 +0000

    miss good strike original control

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
