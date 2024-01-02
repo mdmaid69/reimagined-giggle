5bc4d10807098034b9cd9c77c24838417fdcd074
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:32:20 +0000

    priority rise change wise 572enter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def cube_number(x):
+        return x**3
+import array
+def convert_array_to_list(array):
+        return array.tolist()
