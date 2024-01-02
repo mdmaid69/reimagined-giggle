1214dd081de17c03fc3c9a2ea2cb25f30e7e14ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:04:26 +0000

    generally heart half face show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
