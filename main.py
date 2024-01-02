0cc7323ec49ee62875db2027895b669ea122041d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:35:28 +0000

    moment number grateful sort match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import logging
+def log_message(message):
+        logging.info(message)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
