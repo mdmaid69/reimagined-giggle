a25e22ad21f37e454954cc4e9b92c8f84f83293d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:16:41 +0000

    share towards contrast high classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
