026373aa811bd7356dc6275acdf634f3a1d4e2a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:05:31 +0000

    return press since phone improvement

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
