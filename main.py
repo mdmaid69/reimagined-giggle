8c4383d163fc52469d59447b12a43fd23043ed6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:51:54 +0000

    often whoever Kk deliver look

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
