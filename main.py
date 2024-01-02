cde9552ae166a76783460de4c7c685dbe5aba539
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:26 +0000

    connection curious world completely content

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_length(array):
-        return len(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
