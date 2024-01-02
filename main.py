4142168c8045a631ffb96b25217f2aaf6d4d70c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:08:51 +0000

    win declare reaction picture centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
