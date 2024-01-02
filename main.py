ef166ddba47edb33fd1fcbd6760605116a62af98
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:37:51 +0000

    moral valuable absolutely generally trade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def subtract_numbers(x, y):
+        return x - y
