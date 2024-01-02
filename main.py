a1ef96bb3bb79524972d861a42850b990fc39afc
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:23:50 +0000

    recovery regularly friend anything beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_list_from_array(array):
+        return array.tolist()
