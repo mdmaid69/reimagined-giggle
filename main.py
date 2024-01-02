a7b44c4703a75637cdf0007639215cd6c73c75db
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:38:01 +0000

    public finally rate dinner arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
