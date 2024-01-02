0f0b3c0261d0b8ddd2d304cdf9c315a5037ee353
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:06:51 +0000

    black worse lead Cc public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
