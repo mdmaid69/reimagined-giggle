f8cf93b2d03c7fc04e6f31b282352992d51254c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:13:45 +0000

    weak top film minority uncomfortable

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import re
 def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
