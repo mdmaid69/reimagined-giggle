4706b6382459f6e9806b8ee2d6e8779c53e4f9b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:42:54 +0000

    form insist land whichever shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
