2c995498dec9d158e01d0cd27ddcd1e66d1ca63b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:57:45 +0000

    temper avoid mine come certainly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def reverse_array(array):
+        array.reverse()
