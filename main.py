9dd30801961fd563cdddb70bd282bbee6edad7b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:46 +0000

    test adjective respect mark fairly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
