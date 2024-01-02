0c22fc83e612ccbffd8e5a7c6fc05546c55a4b38
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:19:40 +0000

    nerve stare match distant born

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
