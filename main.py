e9cba1c73390ada69bb30b470a393bec5318866b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:40:29 +0000

    cheque getup keepfrom 570enjoyable herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_error_function(x):
+        return math.erf(x)
