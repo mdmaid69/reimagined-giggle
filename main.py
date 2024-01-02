8b3216172aab74f01af2de8509782bf1283bd5cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:41 +0000

    insist respect gas although geton

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
