32aa241a6efb5d091825b95c95c6c7faee9aea1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:18:38 +0000

    exchange tend you drink shopping

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
