e9f34e3a0651b2e63b0eee3663272efc5cc9fb33
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:42:02 +0000

    whole produce runinto little message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
