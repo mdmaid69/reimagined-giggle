725dc00b02e959caf96bdc10c0dae8bc7cdf5577
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:27:09 +0000

    right present common value long

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
