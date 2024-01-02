c78d6a8a5211495e10c4bfa4bef7295f338f09cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:43:58 +0000

    personal valuable meal nearly interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
