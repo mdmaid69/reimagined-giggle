2be9b3c704f2404e60bdecf4c0467bf5a5b37b24
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:58:04 +0000

    press dance whole coat lookafter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
