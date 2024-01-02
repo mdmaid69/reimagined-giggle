32720a6290b93183937a026fa6b53642294c55ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:40:20 +0000

    statement slightly takeaway sleep team

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-print([x**2 for x in range(10)])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
