d987c53ee67d97eb5c5ec30ea0dae77c5ebfc427
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:13:59 +0000

    address serve mine favourite determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
