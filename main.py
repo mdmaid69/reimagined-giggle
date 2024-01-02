48c441817656813185489489d33f17e794f05b0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:28:01 +0000

    mine exit equipment excuse century

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
