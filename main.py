0f69c0ef16591ed9f5be72b4b8938c8a16b66572
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:39:50 +0000

    professional order hello stick and

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_min(lst):
+        return min(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
