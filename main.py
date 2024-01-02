7277d886a99cba45349fb36859acf4ba95f43131
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:48:37 +0000

    comment book deliberately above Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
