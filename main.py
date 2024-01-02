26fc7463f6376cade719678b8ee018106e741a0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:22:24 +0000

    lie bush open drive notice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
