20f652707cd2a1b4e66638e62fa54733bbcb523c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:43 +0000

    round rush father feeling fast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+print(random.randint(0, 100))
