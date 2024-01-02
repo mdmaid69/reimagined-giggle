60a1845a7dc80a1830b85401f5f26e84a64f9f21
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:22:59 +0000

    breakin month whichever manage you

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
