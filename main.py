3eff6be477e0a9c19034d0f54f14571dee0563f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:44:48 +0000

    answer mine there finally rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import numpy as np
+print(np.array([1, 2, 3]))
