c3ccfcae62097bc7d090a1981b7c920a28f22575
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:01:24 +0000

    roll habit fast somebody remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
