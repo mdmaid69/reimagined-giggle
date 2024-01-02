0b002f9eb2e5a28c15ba70607acacfc7c40bf963
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:23:30 +0000

    space visit cost petrol profit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def greet(name):
+        print(f"Hello, {name}!")
