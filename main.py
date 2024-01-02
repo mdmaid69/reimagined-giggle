8c0f3bff7d8722289346aac150c306c39c6ccd57
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:18:03 +0000

    shut giveout react dig schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
