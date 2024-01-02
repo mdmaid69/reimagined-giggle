398ef8bdaf63d9f89ff55ed3a71d0fe9d5c153dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:55:05 +0000

    line fit steam workon noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
