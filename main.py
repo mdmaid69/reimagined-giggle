f7425428ec0dae3ff636be086d23057f4b5e950a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:04:36 +0000

    calloff operate advantage professional birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
