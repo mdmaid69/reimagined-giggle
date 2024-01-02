d77ed64624bede8bef45afe0cb5be904ddb32a57
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:19:12 +0000

    it insist talk face flow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
