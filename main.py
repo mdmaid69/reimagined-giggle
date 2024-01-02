e4840a98ee48fca88860fc8ccda4c8c8bc30484d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:20:59 +0000

    advise water branch need lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import os
+print(os.getcwd())
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
