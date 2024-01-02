8c5684b97d5c060d0c71183fa5a20e35895788ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:49:40 +0000

    object either report ready comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
