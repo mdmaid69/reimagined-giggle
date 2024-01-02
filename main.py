9774e5367230c5f22478a7be14a601b42028f18b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:36:44 +0000

    lunch sail finally out stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
