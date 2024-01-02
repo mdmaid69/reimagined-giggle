7e89acd647cf61dede5dfc4bc26af227b129735a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:41:57 +0000

    almost trap manner 570enjoyable damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
