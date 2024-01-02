6defb252e7b698aea3ee54edec5d43f235dc41a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:43:14 +0000

    cool speak steal thin succeed

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def cube_number(x):
+        return x**3
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
