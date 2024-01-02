770f355b940359164171ad2dd5b4c5276aa73d5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:27:44 +0000

    pride special few material match

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
