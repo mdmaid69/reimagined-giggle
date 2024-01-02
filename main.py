e214909b5a48e8301c4169398210337d5d7f60b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:18:43 +0000

    his later statement crowd Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
