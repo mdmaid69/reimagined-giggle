4e1a46719c060ff7976aa77b14ad7ddbccdd9046
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:11:48 +0000

    tired cap mark stamp brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
