caad4cb25ce8c8cc8da6ef39ec386687ad1407d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:57:35 +0000

    destroy another uniform workupto responsibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def square_number(x):
-        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
