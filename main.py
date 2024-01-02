7d1e9fc75915c1b13ca1bddb0fa45ee70fc44db2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:33:10 +0000

    history not getaway comeover awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
