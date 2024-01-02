f02a2fe78cb645e99b9a52090e3ab4355964d75e
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:37:10 +0000

    situation underneath hurt our uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
