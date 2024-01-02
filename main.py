d2e625c40593013dd3fd987a6163e12893466768
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:40:01 +0000

    lead mean public on colour

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
