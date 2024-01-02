cfab360c84882fbae3a6f7749638a64c6d6b8428
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:58:17 +0000

    understand learn colour search stupid

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-print(random.randint(0, 100))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
