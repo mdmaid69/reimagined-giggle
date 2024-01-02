3b0f66a6f4c2716042b27a374c4faa9874bc8e41
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:38:05 +0000

    Ii again sense Gg walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import random
+def roll_die():
+        return random.randint(1, 6)
