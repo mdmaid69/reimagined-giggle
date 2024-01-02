2baad554e6cccb90b8487e9dc10a1113684cf558
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:45:54 +0000

    broad clue this state however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
