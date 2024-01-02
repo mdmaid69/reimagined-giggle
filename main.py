c20ae3c08047ec4805652f5d5a187b93bd7e2d00
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:08:02 +0000

    pronounce floor flight maybe distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
