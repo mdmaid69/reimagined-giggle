0c28177143e9366c35cb40a5fcf5c85d0ff546d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:13:19 +0000

    platform off wide deep write

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
