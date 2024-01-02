3342ddcda6bd7a11b3ef504546ed0bb18fd267aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:23:36 +0000

    cut straight occasional laugh wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
