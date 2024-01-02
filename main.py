b050d115a82fda26c49afb13d2447dcfb47ad9a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:26:55 +0000

    custom cause Dd less tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
