5542bc45d7c0b4ace258d28e347aefaf45c26044
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:06:14 +0000

    calm shade length suggestion takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_typecode(array):
-        return array.typecode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
