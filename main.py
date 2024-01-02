35f9b11ecc2a2f66d99c9aa4ececf5a467508457
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:07:39 +0000

    store trap afford shelter force

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import collections
+def create_counter():
+        return collections.Counter()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
