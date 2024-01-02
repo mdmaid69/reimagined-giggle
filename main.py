b5267f5ce24e697093d0498821ebfa7c2073d287
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:23:31 +0000

    greeting such and manner share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
