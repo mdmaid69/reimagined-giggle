c5d2bd0a73caa4e373687a5d996534447ba6327a
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:51:41 +0000

    music keen live uponon close

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
