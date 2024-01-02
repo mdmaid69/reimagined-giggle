5db3947c43c77122ad16e0cb0572cc66a5a13f71
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:59:17 +0000

    car store cum little port

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
