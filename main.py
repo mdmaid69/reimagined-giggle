b9c924ce71efeb9d2fa5adf0dfcd65c452f7f431
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:10:05 +0000

    hit image communication estimate murder

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
