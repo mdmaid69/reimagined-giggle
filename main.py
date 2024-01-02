bb60a5aefa199e95ca8af64371a1cdbc0dca0efb
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:24:54 +0000

    feature understand interesting group sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def convert_to_binary(n):
+        return bin(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
