a41d304bfdbbfb9b539b787ab3e848b854f2b448
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:31:34 +0000

    display thinkover prepare forever meaning

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  def square_number(x):
-        return x**2
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import time
+print(time.time())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
