cc9a6c0c01553b9fc956411d3c3bb0d84f5f9389
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:33:22 +0000

    cut period afford breakup art

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def reverse_list(lst):
+        return lst[::-1]
+import platform
+def get_python_version():
+        return platform.python_version()
