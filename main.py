63ff45706bf62cda184cb60dee8ffad7ef10c596
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:56:25 +0000

    back protect whistle rest opponent

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
