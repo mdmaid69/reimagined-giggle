1b974c5606bf14fa236f059f66be2c5104213a83
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:17:43 +0000

    hit crash circle dry moon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_int(array):
+        return int(array[0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
