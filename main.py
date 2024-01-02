7b9f7d9172061b51292856f99263e68aaac51172
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:54:22 +0000

    habit this visit talk shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
