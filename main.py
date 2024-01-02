7e5d28eeed58fb472b34b83a3d1f81fbcdfd8e98
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:23:01 +0000

    demonstrate top like bound sweet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
