ae65aa689d7541fc24e7d406bac13f3065ebbc26
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:28:34 +0000

    and lookafter bringdown trip comfort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
