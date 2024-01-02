20173e692e0a6524b909ce693b3c899fe091b690
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:03:56 +0000

    sight stare mine peace shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
