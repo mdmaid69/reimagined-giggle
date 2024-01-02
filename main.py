1c79c1fa8e0ef0c61a9699b9de5b2879e8aeeced
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:19:34 +0000

    attract survive wind my formal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import tensorflow as tf
+print(tf.__version__)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
