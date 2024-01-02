3362e10c7ed7a321771e1c22f4c167fe7dff4753
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:58:26 +0000

    emphasize confusing protection mine shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
