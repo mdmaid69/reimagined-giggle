9a26fbfe158a20fda23f662b21af03ced031aee1
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:32:47 +0000

    fast declare game immediately play

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_power(work, time):
+        return work / time
