8fa96fa2b5e4a19e4b3e857735fe971bfaadcc57
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:11:13 +0000

    knowledge rush touch pressure piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
