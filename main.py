c18d977be103730ddd25e810851b6465d86800e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:45 +0000

    turn recognize land alive contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_current_directory():
+        return os.getcwd()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
