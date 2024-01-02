306b106a431045424ac1ddf7be964feb4cd7805e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:05:41 +0000

    speech feed breakfast low behind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
