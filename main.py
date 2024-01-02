f204089fd2c6ba74bde8f3b1e57f0adff7bea40f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:53:54 +0000

    Oo powerful block butter trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
