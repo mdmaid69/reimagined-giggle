df9304b31e4ac9826c81568e66dde539b0addee7
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:08:32 +0000

    mistake major embarrassed sticky same

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
