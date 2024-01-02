2306b0a83c345942b4730be57a4e17be49ffe355
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:15:45 +0000

    offer ago build runinto attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
