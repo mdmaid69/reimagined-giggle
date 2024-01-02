0b684b980d3471ce405c955911ab83c6c2aab84e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:52:41 +0000

    more friend development whatever art

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
