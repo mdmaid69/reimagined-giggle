c142a6a8c4cff9fba9d421a31130481ec7a2cf72
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:35:27 +0000

    intend who watch doubt description

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def convert_to_binary(n):
+        return bin(n)
