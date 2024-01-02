ef0c46e14b08dcfa58fd31d6eef1fe3214543c69
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:52:10 +0000

    subject past sorry enthusiastic fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-print(os.getcwd())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
