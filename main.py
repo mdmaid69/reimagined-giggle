521e8542e696aacf544210d4b6a3e148034b9402
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:34:59 +0000

    bottom bath keep hello union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
