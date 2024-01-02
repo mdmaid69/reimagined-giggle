d2c568a91ac7683c94eb7ae8e6681bae880c6257
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:09:16 +0000

    score right 573entertainment fresh pencil

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_speed(distance, time):
-        return distance / time
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
