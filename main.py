c055cdfaac7220078ef015ebfb8b8491487d6217
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:51:41 +0000

    telephone slice cause insist level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
