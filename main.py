92fdf177d131f9c05b0b10b48e1da4e3d03b78b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:46:41 +0000

    us but keepaway air schedule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_even(n):
+        return n % 2 == 0
