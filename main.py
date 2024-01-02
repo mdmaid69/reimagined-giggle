9aa4753770dc99177b0d9a26dc0e47788f5702f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:15:05 +0000

    aim tall unfortunate place smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
