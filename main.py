af4cf0d1f2f92c6c6195b5846beeceff45797edf
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:44:24 +0000

    run central information whistle estimate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
