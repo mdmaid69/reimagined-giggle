8df29f6b8a94554199289ef9d4250df06dfc545c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:47:08 +0000

    frightened exchange failure about defend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_volume(length, width, height):
-        return length * width * height
+import getpass
+def get_username():
+        return getpass.getuser()
+def subtract_numbers(x, y):
+        return x - y
