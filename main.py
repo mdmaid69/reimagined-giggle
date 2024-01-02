3d74c85bfb969bd354526177022c6596482edda7
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:02:47 +0000

    its breath regard failure invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
