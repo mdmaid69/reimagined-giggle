7901ca626b52e72fa0a1e529fd20a21ea28d8d47
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:20:44 +0000

    keepup state warm large natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_as_list(array):
+        return list(array)
