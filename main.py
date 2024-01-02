85590f7366c9235cd89065abcce028a1160995c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:39:59 +0000

    vote believe classroom connect middle

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-def calculate_power(work, time):
-        return work / time
