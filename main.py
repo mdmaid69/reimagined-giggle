56c5b294db437b3f0e1698d206fccb818ab73d89
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:21:26 +0000

    pain uncertain pipe instrument view

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
