6ced708dc038b4459e501477ae7f5e051116cbc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:01:36 +0000

    kill future model various concerning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import datetime
+def get_current_date():
+        return datetime.date.today()
