49439a612ca591a942530560adbf6ef094e5ea6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:58:44 +0000

    division receive partner welcome occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
