6cc3bf2e3ad115eac3b0e2dc5383529785b4e0ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:54:04 +0000

    whole rather entry for test

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def subtract_numbers(x, y):
+        return x - y
