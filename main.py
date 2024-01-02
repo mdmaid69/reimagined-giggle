5c2aaf86f8af48f9e24d39426315d4752d6cf206
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:19:28 +0000

    goon deliver only score goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
