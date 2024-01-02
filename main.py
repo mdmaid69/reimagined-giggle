ed8461c8bb082a05a7093996f76d818548c5afc2
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:46:47 +0000

    closed gap my gap right

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def sort_list(lst):
-        return sorted(lst)
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
