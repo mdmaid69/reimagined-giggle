a876d57c1bf0e8722ce079e7205729a1f4cd436a
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:12:00 +0000

    will freedom right with special

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-print(math.pi)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_item(array, i):
+        return array[i]
