152178b2beacacf38c151c053d4843253a208846
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:38:11 +0000

    perfect keepaway autumn illness far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_file(file_name):
+        os.remove(file_name)
