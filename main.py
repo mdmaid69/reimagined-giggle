c3d53fb28e36620f7ace47c67d4d53bd861e2ea9
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:45:48 +0000

    reduce live dream pot state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
