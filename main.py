94732bf08f1a912d59227e014c2601890f6e9d30
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:27:35 +0000

    chin attitude secret could theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import os
+print(os.getcwd())
