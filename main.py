422d338f1637fc03785f32fbf18b52ef4a374a12
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:05:23 +0000

    stir talk similar criticism chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
