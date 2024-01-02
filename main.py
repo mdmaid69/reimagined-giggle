56d2a5d64286b3ac8d57f87799f96717e7f5e977
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:36:14 +0000

    cut turndown beneath recover reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
