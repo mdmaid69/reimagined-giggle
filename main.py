2b50cea25cda78a20d1103bd2e3ed6d8f6930969
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:57:41 +0000

    trial lonely once feeling tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import os
-print(os.getcwd())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
