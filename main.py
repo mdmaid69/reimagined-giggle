302761b60882fa65c8f7e5e95a55930c314dc321
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:39:52 +0000

    away deep Tt long attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
