c3c13fd50f0a54a63878a54249b41c6876715021
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:35:04 +0000

    takeoff smell tune mean all

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(5):
+        print(i)
