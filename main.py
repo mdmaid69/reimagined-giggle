11bd1f6968817ea21ab79b291a15c41162a676b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:51:57 +0000

    whom disapprove forever flavour performance

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import datetime
+print(datetime.datetime.now())
