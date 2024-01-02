0301b510244c95aae5b8e177ac03a06d4668637d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:34:47 +0000

    hungry bringdown round own never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
