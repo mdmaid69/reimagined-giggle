8d42897bb176236302b36fad2dabd5d5d0c2a50c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:33:33 +0000

    advise talkoutof activity first active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_itemsize(array):
+        return array.itemsize
