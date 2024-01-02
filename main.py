ef5dbb292a113265d4f8a766b87c96a78a5eb2d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:51 +0000

    task who wake still word

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
