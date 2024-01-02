6578ab677a6f0bc23805adb5e57bf1da0e2032f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:56:36 +0000

    572enter report start takeback getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
