d3e77b3075808a02227bab1e4b055cf9f6df07c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:26:22 +0000

    feed colour best emergency search

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
