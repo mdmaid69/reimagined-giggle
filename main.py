a2dea2ee8e0d28c265ae9883986d499206fd9bb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:09:41 +0000

    likely why rise trick stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
