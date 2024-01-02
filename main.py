6bf8eeed1e06a80025026793f2b0cc9533bf0ded
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:32:54 +0000

    wealth rent sweet ability nonsense

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
