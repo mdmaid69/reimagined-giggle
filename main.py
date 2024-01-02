d31a1b55ad94e277b3a48c689f3ca666095bbe07
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:51:33 +0000

    price contact face class rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
