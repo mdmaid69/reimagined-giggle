bb06d4f105cf2fb909b62e9cd989840620b66d93
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:37:09 +0000

    blind embarrassed reduce unkind consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
