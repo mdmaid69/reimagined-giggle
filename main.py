bb092bc5a4837db98a7f83e3e9eac1df4a4280da
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:52:52 +0000

    good admit comeout morning fine

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
