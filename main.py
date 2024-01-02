4317bfb110f38ae3485df6bec94a4aedd409179a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:02:32 +0000

    mean enthusiastic tradition takeback of

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def reverse_list(lst):
+        return lst[::-1]
