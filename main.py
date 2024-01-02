9317fc1982eff58fd13c7aa87a6025b1277a6e0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:32:35 +0000

    talkback by shopping coffee off

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def divide_numbers(x, y):
-        return x / y
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_pressure(force, area):
+        return force / area
