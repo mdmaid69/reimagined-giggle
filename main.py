75f9d0a7c092675d0aa271e5507442c52f9c9e9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:05:22 +0000

    hope large pronunciation suffer expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
