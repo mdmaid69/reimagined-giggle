90e93e37d61cdaa9554f9a8505775e749915b829
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:04:59 +0000

    country final profit flow child

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
