66233b913891e717304c0592b14cbbb5b7656979
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:11:58 +0000

    people various stick method comment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def append_to_array(array, item):
-        array.append(item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
