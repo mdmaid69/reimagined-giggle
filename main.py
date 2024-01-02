c7b2a8fa2bd16d507d088bc03541f540e5213f02
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:53:13 +0000

    attach comfort remark modern claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
