4bbec3f302f0b9d3b5dc7c9b23fe40b059c8f1f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:12:56 +0000

    amount everybody while recovery force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
