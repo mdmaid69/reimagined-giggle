2bb9ec27aea0b85357a1680d93948b0af357c3b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:33:38 +0000

    further television crisis lookback(on) meal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
