e69bf893ca57ca196f9f7661256518a2dcc53ed2
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:07:43 +0000

    572enter workupto brush hospital apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import logging
+def log_message(message):
+        logging.info(message)
