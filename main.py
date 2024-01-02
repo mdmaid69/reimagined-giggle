c147633df7d3caf75f30dca24f86daffa019360f
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:14:59 +0000

    estimate hide block taste especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
