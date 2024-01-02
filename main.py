122c86160434f57e14ffdf40a3d811f3e861e371
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:59:47 +0000

    Hh project show unable carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def get_array_as_set(array):
+        return set(array)
