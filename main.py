2347abaee6c14c3a1af31fbccc7a633ad6ccfba0
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:42:33 +0000

    unhappy settle breakin only solve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
