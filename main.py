5f499402402ce940ec99eefc643fcfb44c93f489
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:39:18 +0000

    shut shame you declaration hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
   def get_file_owner(file_name):
         return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
