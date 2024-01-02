3564ca02e1260a150b2ff438d6b23a4ee0bb9717
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:31:32 +0000

    sensitive safety goaway possibility yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+for i in range(5):
+        print(i)
