3aeb5cdc5d35dd77bc976ffcbf846ff46c629b71
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:00:35 +0000

    scare love fall practise burst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def reverse_string(s):
-        return s[::-1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
