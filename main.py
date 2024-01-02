1b04b1936129c9656a71c31e24a86c43a2c87496
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:52:01 +0000

    phone obtain pour bend doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
