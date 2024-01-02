2db956014ac76ca544e79f8bd53beb1ec21e2082
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:13:16 +0000

    deliver announce depend worried pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
