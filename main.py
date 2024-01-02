d06b3d01961156c242c33c56263fc9e05ec3b941
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:29:26 +0000

    mark whatever clear judge borrow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
