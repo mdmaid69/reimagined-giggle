e2a6c46980d17898b33cccd9b198b28fd8cec22d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:18:20 +0000

    treat track concentration manage excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_speed(distance, time):
+        return distance / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
