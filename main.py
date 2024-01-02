dd6a824373121ace3ffd3cef53d71a8dc903f18f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:11:53 +0000

    walk please brain pressure once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_itemsize(array):
+        return array.itemsize
