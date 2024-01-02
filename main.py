963e84f99bb0f19f2ea8dbb7fd1daca6fd20820e
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:35:23 +0000

    instrument comeout feeling tap save

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
