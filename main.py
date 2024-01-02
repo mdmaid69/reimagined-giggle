bed68b8d384e1050fc4547a64b1566ffd67ed370
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:08:57 +0000

    add so by hold concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import time
+def get_time_since_epoch():
+        return time.time()
