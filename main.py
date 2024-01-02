b9987626924e9c303d5ac576e061a5c377e600ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:14:17 +0000

    demonstrate march silent Bb bad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_length(array):
-        return len(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_max(lst):
+        return max(lst)
