6696c5b441e82ea10b5dad9fb5127c6b7afb771e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:50:00 +0000

    closely branch practise set goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
