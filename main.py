15b3128bf276f577c226c7f1e3e1117e3582cf49
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:20:54 +0000

    meanwhile 571enough wear cheap keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
