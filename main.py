6a9794563f0ba453604f5d31d483241df417e27c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:52:09 +0000

    rule stranger rate list breakin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
