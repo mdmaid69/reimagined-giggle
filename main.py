f6f19f73ee2063523900321dcf52f34223eefd46
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:22:39 +0000

    most comeback roll lookback(on) judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def subtract_numbers(x, y):
+        return x - y
