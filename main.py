4b900f891b9512734719064daac497acd6ec5b50
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:26:35 +0000

    along compete dangerous serve slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
