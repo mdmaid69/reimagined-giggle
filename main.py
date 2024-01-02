aed9e145f1f00658b2cc141e3329b0e64eda8dc5
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:36:15 +0000

    take pour discussion to season

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
