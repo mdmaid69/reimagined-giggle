b4bc3851a0875fc44e154171734a38c17dbbb391
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:57:57 +0000

    awake fashion soil surprised circle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def is_even(n):
+        return n % 2 == 0
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
