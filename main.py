d6cb2f301f8f8623ec0db86a3956d97914c7a139
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:52:30 +0000

    little move check ignore busy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
