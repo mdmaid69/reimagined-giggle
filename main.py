94aa48e465061e26fc4b83e7f44c7e3e95c96cd2
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:32:51 +0000

    why present knowledge wise obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_typecode(array):
+        return array.typecode
