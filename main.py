4cef1bb6ddb0f959fc2cf784f4e4156ffe5bc3b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:50:58 +0000

    feeling stamp whom interest badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
