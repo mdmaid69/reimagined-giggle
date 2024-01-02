04b669d3d0d7feb12e1d9fb9c38f7f438c8f63f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:42 +0000

    pick post badly concerning wind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
