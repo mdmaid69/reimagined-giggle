2280d489696014526f89ed1b706f984931464c86
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:39:47 +0000

    live kick emphasize other rain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_power(work, time):
-        return work / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
