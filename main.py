dd5094a02c187b4f326772969e398c4ebf008f56
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:49:00 +0000

    flight love transport drive occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
