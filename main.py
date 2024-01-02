0d948ce0d2099b582c46a636d7957f5f093b389d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:27:30 +0000

    become flow scene top wave

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
