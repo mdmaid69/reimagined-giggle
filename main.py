ba564e6f9052805dbc09e42b1047312dcfa5ec6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:19:24 +0000

    wing please fresh neither result

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def get_array_as_float(array):
+        return float(array[0])
