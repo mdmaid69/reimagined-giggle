c082e53cd9866a5f1ab462c99666cb9a8b994f8e
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:07:58 +0000

    class relief my promise thorough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def subtract_numbers(x, y):
-        return x - y
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
