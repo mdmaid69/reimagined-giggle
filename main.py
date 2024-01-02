9f4f239425691a939f4bfd39ab42ede89ceb5bdf
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:27:07 +0000

    over without trip sudden narrow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
