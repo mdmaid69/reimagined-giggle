7514d7ce544f8b9f6357b6299216a3c0ce2dd6f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:43:27 +0000

    mind bury roll happen pile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def is_palindrome(s):
+        return s == s[::-1]
