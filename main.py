2d01dd800c8cc065939b5c4f6a08e8b3029b0b5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:23:13 +0000

    frighten chat shut would nonsense

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
