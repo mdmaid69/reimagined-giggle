5ee6357d669d04f29950ae24c793f5d167a19c8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:31:39 +0000

    feel terrible ask develop walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
