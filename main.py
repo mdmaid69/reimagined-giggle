18d5b4aac6493b89f1caaba51c4fde14eb309d20
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:01:07 +0000

    lesson swallow lonely every holdout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
