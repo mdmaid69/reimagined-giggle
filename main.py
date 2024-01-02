cf8bb1adeb377f7b566eacbb6b5a691c7b116000
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:10:17 +0000

    delicate spring world else final

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
