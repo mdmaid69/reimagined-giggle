480696db9353c618a1b13fe9764b048987ba43a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:19:16 +0000

    later furniture in cheat excited

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def cube_number(x):
-        return x**3
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
