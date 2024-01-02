ac340eccefd04b354851b076b2ed65cfeb7a991a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:13:00 +0000

    bringback ocean course swimming use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
