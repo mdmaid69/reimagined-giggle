4bf05552492c386f9dfecd51387ad08b71babaa0
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:23:29 +0000

    himself important blind everything interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
