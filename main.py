6c22a654f1cc1b3c37f77c68639ad2af61a88b04
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:16:18 +0000

    welcome single influence wave fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
