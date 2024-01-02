bc7bc2b3e72e915bf4b9431a057496bd59a6b016
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:12:21 +0000

    star discovery holdup organize dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
