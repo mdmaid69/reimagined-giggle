6d3fba4f29eaed6376f6b401fab20c56c717e673
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:36:42 +0000

    guess cover steal area Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
