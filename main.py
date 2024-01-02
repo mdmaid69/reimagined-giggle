009c9fab3f0c0179b8ce90123e584ad6a193c1d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:20:01 +0000

    habit respect score beautiful oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
