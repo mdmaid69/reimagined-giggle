7bace09aa6d6bdbb148bd9878f85fd24368f3045
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:40:48 +0000

    clean persuade account progress pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
