6169e79e49114dd24e073d03b4b390184e82b208
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:37:23 +0000

    load little another declaration yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
