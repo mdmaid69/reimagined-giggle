5d63fea945398b862e3e1af829e11afa00c4273a
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:18:55 +0000

    speak height getoff eager neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_float(array):
+        return float(array[0])
