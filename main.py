0fd01625abb68d4fbe83d9b416aca6f612a4b34e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:05:57 +0000

    way patient concerned lend genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import time
+print(time.time())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
