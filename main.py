2fddf4a464f0b142818cf9e48183cd1132d0aded
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:36:05 +0000

    active shoot high number base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
