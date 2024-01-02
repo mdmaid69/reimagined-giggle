942ae9c8fb2dcb5b404e3488d161ba07eeff4564
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:46:31 +0000

    replace repair improve train arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
