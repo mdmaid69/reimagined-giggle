eb8f1102c4f2776d313b80c5f159ab5b94e4e23b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:25:15 +0000

    scream church fork radio catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
