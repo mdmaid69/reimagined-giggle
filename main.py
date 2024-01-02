1be55e9995da8d8fceacf2dfc686be6cc36412c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:47:16 +0000

    maximum hurry public accommodation odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
