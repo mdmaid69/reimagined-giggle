ed8aa711044e1674ad054238f0fe758f6e564ae6
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:55:31 +0000

    question sure possession wing culture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def is_even(n):
+        return n % 2 == 0
