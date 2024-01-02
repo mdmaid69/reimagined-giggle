25916bc62a73dd7bb180e07cde3c8fb755dc34c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:42:21 +0000

    achieve calm dance unusual backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
