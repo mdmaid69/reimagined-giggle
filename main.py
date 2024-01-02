f820e780eddeffcf17c4f5cc1380726351889bc3
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:24:33 +0000

    unhappy challenge short chase seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
