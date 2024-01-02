1d61003b766300acb6c40a7e590da58ed5b25c40
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:14:54 +0000

    rubbish tap official normal influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def sort_list(lst):
+        return sorted(lst)
