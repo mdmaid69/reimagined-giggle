f13b787576045f38df276f3a6e03195b3d18eb20
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:59:30 +0000

    frighten offend clean play dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import json
+def convert_to_json(data):
+        return json.dumps(data)
