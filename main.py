0470d0e86ae4a1464361c6cd49d3d007a3370a19
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:01:54 +0000

    smell need unlikely mend takeover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
