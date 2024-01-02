23d811bfdeac977c66d129c840e2073082380537
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:03:39 +0000

    sharp steal behind interrupt value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
