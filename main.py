ad92b510f8e420217ec4e068b7d123f034b6f425
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:17:56 +0000

    Ww cost hardly odd string

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
