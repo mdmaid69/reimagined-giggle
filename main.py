c20ab18b7498a53c75c94b9287a5aad2ff457aa7
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:40:15 +0000

    breakthrough opponent among little unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
