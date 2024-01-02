8d4a46f430d5f4feef2958fc6c65566bae5b2c4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:46:30 +0000

    whisper lookaround force small mine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def clear_array(array):
+        array *= 0
