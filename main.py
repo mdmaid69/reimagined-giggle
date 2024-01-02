b7536a3f29e153de7d1988ce469577dc9188e490
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:31:58 +0000

    true unless emergency pocket under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_queue():
+        return collections.deque()
