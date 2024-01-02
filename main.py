4f5833b1c96a9a0ba1bc8097f0259e8d9cfc94fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:35:26 +0000

    pupil vanish bank heat brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
