e2c16dc29d6491f2ceee84a95f442a6e369e9af2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:32:47 +0000

    father ear branch sensible yet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
