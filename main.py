e991daaa2ac26009e4cd4a508de8732113a61daf
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:37:25 +0000

    buy temper immediately realize trip

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
