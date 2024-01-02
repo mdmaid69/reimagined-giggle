3443855248e06e35113ae6a5d0ba0b8a634becb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:56:26 +0000

    contribute mix move spite poor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
