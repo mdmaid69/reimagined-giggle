e5ec079d2c1ae8406736c61d6b1acae5d75a7e47
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:38:59 +0000

    anywhere chat error score spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
