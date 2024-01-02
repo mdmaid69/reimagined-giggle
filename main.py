0c2ebdfca498e14a6922d995290577edfedebb5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:45:38 +0000

    shut hate software burn spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+print(math.pi)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
