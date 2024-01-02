700b529ffd7a72d97ec30eafa4da52b2ac44d85a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:06:27 +0000

    care glass area true slip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
