c8d217ba1210970549fe4756c7559d9b4d58b714
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:58:40 +0000

    ability plan range represent direct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def is_even(n):
+        return n % 2 == 0
+for i in range(5):
+        print(i)
