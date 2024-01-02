e11031ec00114f2731272926105a66ac5c1a3ae3
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:19:18 +0000

    radio tread makeup city bright

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
