96ceaa8fe04a10843de07418a8bb8661cb323493
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:10:48 +0000

    shine key relief down sink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
