9c84e11fdffc2045864b8076daf4cd39debe7149
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:23:18 +0000

    study increase store police life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
