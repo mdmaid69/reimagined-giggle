5d216a00a253eb7ac185db94a3a448574e06065e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:04:25 +0000

    OKorokay object gather shoulder cap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def convert_to_hex(n):
-        return hex(n)
+def find_max(lst):
+        return max(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
