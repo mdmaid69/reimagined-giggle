1aa513e190221f95c14211907701d9a5c6605848
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:38:22 +0000

    backwards year play machine picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import time
-print(time.time())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
