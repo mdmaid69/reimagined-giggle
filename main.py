fcf331d92424ec9caeb0500ca65f3288741a146d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:22:42 +0000

    combination present unable waste them

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
