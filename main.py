bfcfbde378c37a96aae432074de563598e818950
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:54:54 +0000

    disagree common road she interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
