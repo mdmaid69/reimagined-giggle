4ec16d170ed7aa82c2a059eac981b5028be9f2a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:38:16 +0000

    check press dance die pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
