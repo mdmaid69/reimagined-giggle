f07ded94e5de9a1dc703ab6bb6f5d0c356976fd5
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:41:29 +0000

    over runover inquire passenger confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
