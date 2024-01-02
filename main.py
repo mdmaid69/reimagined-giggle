8786baa2c260f19292b96d0fe0c0ef7e6a8abef5
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:18:22 +0000

    steady benefit meanwhile drop lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
