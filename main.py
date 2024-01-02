ced1bc9c449e07662350f8479d4779383538d220
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:13:25 +0000

    environment direction immediate invite as

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
