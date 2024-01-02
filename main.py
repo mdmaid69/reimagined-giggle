550ed155cc8cccd78ab15087943b04bd3ebd800a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:59:06 +0000

    hurry offer closely opposite concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
