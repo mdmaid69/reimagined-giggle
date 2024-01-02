0a3505dcf7a03f1d585272d79df98517d5d682bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:39:16 +0000

    rent off rush spare ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import collections
-def create_queue():
-        return collections.deque()
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
