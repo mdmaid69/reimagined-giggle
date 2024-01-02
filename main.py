96fa51078b83dbceb0098c1d46124e6798ed9187
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:47:59 +0000

    pump represent intelligent education straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
