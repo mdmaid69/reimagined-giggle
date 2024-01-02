42bc2283d87f22a1cc6c46ad450afc17d55b7f5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:20:26 +0000

    possibility swing pretend anyone comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def get_array_as_bytearray(array):
+        return bytearray(array)
