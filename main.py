53f85e7fab842446a10581d5197b0b3ecfbda111
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:04:29 +0000

    bringforward suckme have history tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
