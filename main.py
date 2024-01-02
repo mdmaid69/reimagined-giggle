40d2c506f7c68bccc7f9cdf8df8c8c767d5109de
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:02:53 +0000

    bottom block hardly round music

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
