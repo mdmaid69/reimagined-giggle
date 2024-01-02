d33cca7a01b39cab8f32daa727f67e4efc9d18d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:47:17 +0000

    gather Ss contribute steady aim

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import os
+print(os.getcwd())
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
