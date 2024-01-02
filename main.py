1cb5cd65bdb14475383cdb69f1e805b2b7eb63ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:31:20 +0000

    limit feel takeoff entrance tall

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
