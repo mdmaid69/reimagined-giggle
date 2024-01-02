7eb02b7ab4f1a195d86e32351d6a79785b52023b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:28:58 +0000

    creature concentration fresh dress drink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def remove_duplicates(lst):
+        return list(set(lst))
+  def convert_to_octal(n):
+        return oct(n)
