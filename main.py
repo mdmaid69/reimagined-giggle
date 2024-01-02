3879996944ad6573856e71c95d3c26313d8f7305
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:13:53 +0000

    chat alive above ago negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
