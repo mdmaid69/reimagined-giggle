e21e242bda1d8605f2534f628ab843ad7a43d4b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:40:38 +0000

    grand shrink close label cheap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_as_set(array):
+        return set(array)
