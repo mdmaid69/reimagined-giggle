50fbde0c6b34634a1ceef35c78c3cde5f80e4642
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:48:30 +0000

    in pinch radio damage film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
