74e520f6ea69d8fef27c2e84dc240815a83636db
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:44:18 +0000

    could imagine contact kind carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
