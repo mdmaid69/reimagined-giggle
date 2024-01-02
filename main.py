2352814995a269e664148c0b57ee290b041d7b9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:24:14 +0000

    burst second hold shadow lonely

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
