8d4e1efc84a1f1bc7072907208be69c1aa17311c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:13:58 +0000

    wind cover present bag exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
