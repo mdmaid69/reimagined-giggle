5798e81b8deebaf4520bed3bb23aa369593d854e
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:02:30 +0000

    than unpleasant passenger narrow so

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
