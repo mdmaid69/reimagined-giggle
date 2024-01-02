7410580bb66bf77c71bad818508430b988f3a0d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:42:54 +0000

    unkind general skirt your put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
