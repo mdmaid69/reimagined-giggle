85ba8f47f3b2e394ff52b77cd17e0677e67acf79
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:04:47 +0000

    brief study cheque something surprise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def remove_duplicates(lst):
+        return list(set(lst))
