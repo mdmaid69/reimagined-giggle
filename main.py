4ca5bfffaeeca20aa5de351951e0a9b679d3b116
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:23:48 +0000

    shop side appeal wipe battery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def reverse_string(s):
-        return s[::-1]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
