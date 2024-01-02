379cad84bb0d93629b2f70768031660ae18c15c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:47:17 +0000

    surface record gentle interested country

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
