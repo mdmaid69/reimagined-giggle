3d74d06bbdf45e875997e333077cafcadda32d9f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:16:51 +0000

    crisis awake will while gentle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
