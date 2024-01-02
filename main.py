cd56ed8fbb823197ee06d5d19f284578eb5acc08
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:19:14 +0000

    different translate through advise doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
