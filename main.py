7cff4d58b4ff196a289e6f265577aaf6d741e0cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:15 +0000

    trip extra bunch cough build

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_directory_name(path):
+        return os.path.dirname(path)
