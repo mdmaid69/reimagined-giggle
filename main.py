5462ec353ace7cc13c7aab96313364c8d053ac65
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:31 +0000

    education anxious makeinto shut increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
