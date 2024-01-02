abb79b5ca0702a1767d08befe1e59c9aaf093e4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:51:31 +0000

    discuss change cook flow cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import tensorflow as tf
+print(tf.__version__)
