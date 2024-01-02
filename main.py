8b32ec182ed80c371892eb6d8a1f522db122015a
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:27:05 +0000

    back switch prove attractive create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
