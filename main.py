6720d7231e3fade9d4682bc965894a4b52903a06
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:11:12 +0000

    understand think question neck to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
