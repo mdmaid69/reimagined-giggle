d1cd158c0b52465b529bfc02a19f767505215ac2
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:22:23 +0000

    reduce bath meal section point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
