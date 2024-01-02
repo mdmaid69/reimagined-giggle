26b2ac0dfc4c5898837cb95b22116cbdf580210a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:30:28 +0000

    hold curious old tap stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
