137ef011fe31a8b7b2495a022b874dd4e986d690
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:10:20 +0000

    relax onto roll build odd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
