b0f8cea35c2a758336f77586bf1754b2efc31567
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:25:25 +0000

    juice noisy coat region fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
