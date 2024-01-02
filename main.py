b552cc000cb72b449e73179754684e58f80d6e92
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:00:06 +0000

    drop raise information ready remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
