680ac10d82fd1a06ad7b94cdb2652a998c0e6385
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:03 +0000

    sort choose low next any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
