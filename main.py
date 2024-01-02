0f12bbfc374b9c81e20e21e33a114ff0ddf5b7d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:22:55 +0000

    dare flat recent scared dry

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
