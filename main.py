8a48625f303bddd952c8aef3c6323310e8d5a702
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:06:28 +0000

    work believe cook sky hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
