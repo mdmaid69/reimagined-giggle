50bed8bd41565e2bfaa7b95e252bd06637ba84b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:52:11 +0000

    increase draw bad equal typical

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+name = "Python"
+print("Hello,", name)
+import random
+print(random.randint(0, 100))
