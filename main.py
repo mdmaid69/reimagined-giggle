14506cb999fe1f4114f91a2dbc8c24fb2f754f73
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:51:34 +0000

    order murder suggestion account intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
