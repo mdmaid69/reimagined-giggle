72593551bca5f79de169a25f389464a90f63247f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:49:48 +0000

    search impressive wire black awake

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
