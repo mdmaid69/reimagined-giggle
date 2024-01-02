5c721e1a4587d68abbfe472f4a6f506df106a1fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:26:12 +0000

    whistle say chin open lean

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_acceleration(speed, time):
+        return speed / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
