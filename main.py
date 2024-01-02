deb5a221d5092113aade8a3b1f6e54aed387b505
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:14:41 +0000

    suit Kk block getup stop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
