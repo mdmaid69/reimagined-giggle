69b9f8bd7a8e00da1444e7ad1055c22f948beda4
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:16:26 +0000

    ground favourite long cut short

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
