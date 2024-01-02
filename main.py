e0586c2798709ecd8a9f191ea327ef547b17c75e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:58:27 +0000

    search mark key failure clever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
