f25bdb45ba20192009da645c8f95cf3c0f78ca6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:07:16 +0000

    wrap union necessary before force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
