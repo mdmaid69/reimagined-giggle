a500d26afaa5736a2004b532f380076c533b408c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:09:40 +0000

    shade sticky suggest complete closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
