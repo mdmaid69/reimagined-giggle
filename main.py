73ff5aa4ecf6003c717460d709125df9597a5d8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:39:14 +0000

    ashamed tip top well explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
