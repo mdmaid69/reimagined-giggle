1baf8f37587a79e449ad17b562b8007e382003ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:15:22 +0000

    guess seat occasional total create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
