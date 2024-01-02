4850e6a8e96ad744493014d01e3a9bfc9f575555
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:29:53 +0000

    basic there Ll begin beside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
