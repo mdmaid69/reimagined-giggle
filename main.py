fc9c2532aa1016c8a6c6a851be86fd4dd2033ad7
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:17:28 +0000

    stupid dish how seriously 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def convert_to_hex(n):
-        return hex(n)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
