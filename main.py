b6fe70d523a1ac5371c2a5d7797f57d46d98ed7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:57:28 +0000

    shut manner handle score dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
