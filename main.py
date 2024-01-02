8909432ee1a8e5fa6412fb075bb2f88f2bffae2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:33:40 +0000

    check cross laugh because tall

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
