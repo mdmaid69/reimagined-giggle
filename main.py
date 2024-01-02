de7faa5fcbb8af74e405bf9ad21bc0cc7c84422d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:56:31 +0000

    lower giveback software actually attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
