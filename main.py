b2f8c7b84eaf139b6288518b2f195caa5a7fccf7
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:48:57 +0000

    manner wander early anybody as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
