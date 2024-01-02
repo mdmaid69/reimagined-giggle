c2eb562e360610a2d17ab1512639086a6f67ffa7
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:40:02 +0000

    me proper sweet owe easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_sign(x):
+        return math.copysign(1, x)
