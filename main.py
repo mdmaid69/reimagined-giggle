98386ba337bef4135689f364670ed0cc6abf212b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:30:11 +0000

    pollution shade before find radio

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def square_number(x):
-        return x**2
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
