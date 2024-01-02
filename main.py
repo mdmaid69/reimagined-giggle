160a2307ced9802be413e70dab63005f55715a35
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:29:08 +0000

    one machine nature charge perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import time
-def get_current_time():
-        return time.ctime()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
