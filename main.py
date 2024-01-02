4762d50c8992ad97f8e78d1db653af2c7090c5d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:21:26 +0000

    furniture short dream hit floor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(lst):
+        return sum(lst) / len(lst)
