dc2a50b0b8bf6a059e5e56721958cad786f3488c
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:53 +0000

    path mind quit influence scale

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_stack():
-        return collections.deque()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_as_repr(array):
+        return repr(array)
