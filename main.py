878cf98899a0129d25976b8cff595a047d4a0435
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:04:28 +0000

    cause takeout desperate bar she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
