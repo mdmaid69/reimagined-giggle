4af3c919d2c36f6a58c26085e5ca0abec914b209
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:35:30 +0000

    related must line average impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
