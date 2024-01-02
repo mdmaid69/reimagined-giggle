7b2461b772658df9e56723fc2324a9b04f537fdb
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:13:38 +0000

    apparently string face moon dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
