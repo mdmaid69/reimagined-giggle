4e012e80efef0d591b873472004ffc9cd94f495f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:31:46 +0000

    whichever rarely chase event exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def clear_array(array):
+        array *= 0
