7ad37d1dbf6a2b3face617bdfd893eaadd8c700f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:12:53 +0000

    repeat tone explanation communication considering

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+print(re.match("h.*o", "hello world"))
