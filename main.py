bfa2f1e08e2146bbc1920384c69dc0c15d426420
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:07:37 +0000

    report education comeround anywayanyhow achieve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
