a4e5f56cd0efa64ee1dd3173645fa3e1e69abdaa
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:46:39 +0000

    move fact wonder tape dig

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+print(sys.version)
