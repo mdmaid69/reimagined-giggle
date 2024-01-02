aa1ab2a8db6cf0348a52c4cdb02e2c47440048b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:50:17 +0000

    hit partner inside hour activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sys
+  def get_python_version():
+        return sys.version
