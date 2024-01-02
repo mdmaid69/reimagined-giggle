9a558b4dc84dbc491005f1063acd627e2aaa2aa2
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:20:41 +0000

    issue Zz calm feed ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
