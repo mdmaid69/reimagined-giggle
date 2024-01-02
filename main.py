fed6e1a7926b4c0c759dfc8fa9b7696985da487f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:54:14 +0000

    fill simple distant while temper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
