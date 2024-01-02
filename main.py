24a6375d246d0a00ef799a9bc78727f17ddd17de
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:44:22 +0000

    major say interested tired than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
