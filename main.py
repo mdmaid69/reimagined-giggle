2f519f3f576d0b7b10fe7e4a03506da20c38c2de
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:42:01 +0000

    concerning present punishment water stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
