660eba78b32408545bdabb19a8dc28b461fbdb4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:09 +0000

    flavour rare quantity chin organization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
