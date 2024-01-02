ee3a527345e1826b0382ceedea31c933c1c3ed70
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:19:58 +0000

    object explain spot excited offer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
