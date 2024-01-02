9134605b447397a2ef3e47181319bb6ecbaa5480
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:25:07 +0000

    consider now collect after pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import time
+def get_current_time():
+        return time.time()
