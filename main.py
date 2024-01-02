0098f7842bde1623b0f26bbfa140ae084393344c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:18:26 +0000

    quarter through warning breakinto delicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import platform
+def get_python_version():
+        return platform.python_version()
