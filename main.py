89f8a8feccecd719dc64048d7c105d9118fc9886
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:39:15 +0000

    sentence act every ignore house

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
