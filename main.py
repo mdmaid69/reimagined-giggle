8a8fa9471b8fb83d7fae69de9aba806d388aa89d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:55:03 +0000

    unfair bread divide march learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
