0bd6cbccda5f6c3413581dc0ddb12bea8f3eacaf
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:49:34 +0000

    advanced talkinto suffer persuade classroom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
