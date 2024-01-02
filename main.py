2c85f0e93faae4ff7d0766b3e0fb22e90818e52c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:47:46 +0000

    avoid majority against dead poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def remove_duplicates(lst):
+        return list(set(lst))
