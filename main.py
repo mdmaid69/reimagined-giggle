120de91035dfc59c6c17d21fa853ed062bbb5f48
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:21:57 +0000

    available hold similar cut week

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
