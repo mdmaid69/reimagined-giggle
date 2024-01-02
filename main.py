f5db97f6b153ed07e21fe3035eefbb1e24d917c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:01:08 +0000

    shop please play explain sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
