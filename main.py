3cbd31a0290ab82545c1f212e6934f2847ffd9c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:20:55 +0000

    bridge bite bin thinkover ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_min(lst):
+        return min(lst)
