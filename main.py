75559ba0acf6acf7b7eb0fc5f634ca3bdef11ef1
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:27:05 +0000

    want show stupid danger drawing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
