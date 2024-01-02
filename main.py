214c4e3274485db6923abd31ddb89f9e158eb9f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:33:51 +0000

    unfair wheel air practice suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import time
+def get_current_time():
+        return time.time()
