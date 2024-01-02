3a54b0802f3cf10f40eea110a6d357097e22053c
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:39:16 +0000

    cut snow convenient interfere turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+print("Squared:", [n**2 for n in numbers])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
