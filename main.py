2850180910690a8638c93432b14df9b65e3f5c5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:48:57 +0000

    achievement match interfere shall convenient

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
