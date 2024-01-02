19787e32c39750fdfda228c3ad636f8bc294a923
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:55:42 +0000

    task quantity scheme quick age

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
