83940d2b8b52d3e507b56c8fa1e8dc7df7a487bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:35:29 +0000

    label ours interest call getaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
