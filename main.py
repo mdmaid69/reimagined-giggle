d70fb952ca049c0255431eb8f792fb996487b08e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:28:46 +0000

    violence profit mess honest workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
