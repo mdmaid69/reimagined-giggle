287ea8c4fdc07bbb7500c3ef30e18fe974d200c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:16:23 +0000

    live lend pupil schedule describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
