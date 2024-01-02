d54a015b85793d598d36c0c777d3e551ec4d49e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:11:10 +0000

    follow feature bite story brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
