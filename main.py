379f4469fd1a84d870f2be1e394f363a7628ff4d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:57:06 +0000

    basis progress why town not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  def cube_number(x):
+        return x**3
