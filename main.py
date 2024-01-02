aeb8b1700cf0d29f4fedacaa0c6d019062aab611
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:20:59 +0000

    reliable unusual Hh load guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
