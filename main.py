b318062ac694594e5d352e0008e4fb097f1801ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:00:26 +0000

    interview disturb confidence group skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_typecode(array):
+        return array.typecode
