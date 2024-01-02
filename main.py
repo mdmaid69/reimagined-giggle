04f13e0c7843f3c063abf7cc40f6deddf4cead12
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:22:32 +0000

    human doctor suckme rare value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
