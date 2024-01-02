e625f0ac238888dbbab819e91fac286031c1b66e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:22:50 +0000

    Ff report blame degree involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-name = "Python"
-print("Hello,", name)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
