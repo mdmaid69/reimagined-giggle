57285d9297c2dbaf16e003468aa6d21b7647325c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:50:00 +0000

    air plant hollow detail sharp

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def clear_array(array):
-        array *= 0
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
