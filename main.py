5278745cb7d14a27df470f8f67dd8caa054eeae4
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:38:08 +0000

    need fire mess fast Ss

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
