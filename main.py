be3564cc77fbd8d7bdfb99c89fa0e475b0f769ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:05:13 +0000

    possibly sweet possibility flood memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
