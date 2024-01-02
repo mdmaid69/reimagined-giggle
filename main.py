b7d7ac0a8d6389d484dd6da080cf57a21b00bb35
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:40:58 +0000

    ocean worse night wish stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
