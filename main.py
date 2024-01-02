eb0598c25816b26eb9fa182fb2e300721d96be91
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:14:09 +0000

    dark but score act good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
