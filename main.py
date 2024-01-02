720d6b203840ec44be92c8cd897bb1694067e687
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:31:53 +0000

    lunch hit turn fine car

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
