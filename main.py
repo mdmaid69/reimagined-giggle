f0e30f93bd53a6c1c689d0bbc91adf4ecc61558a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:11:37 +0000

    lookup attractive rank attractive fast

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
