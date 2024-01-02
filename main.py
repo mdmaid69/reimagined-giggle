a0b71504f5ded459a9344c03f87524553d4a4427
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:09:35 +0000

    justice trap dance spread maybe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
