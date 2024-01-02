e478849280c267791f9a90c87b44d88801a5a549
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:47:43 +0000

    gather could goback(to) chance off

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
