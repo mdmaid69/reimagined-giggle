cc15a3c66fc42af2e51410ea7a9f1756387978c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:13:25 +0000

    closed sad freedom responsibility formal

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
