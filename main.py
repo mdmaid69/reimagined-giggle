ba165b1f836d27456859bfa05193b64c831d78e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:00:41 +0000

    so catch poison suffer peaceful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
