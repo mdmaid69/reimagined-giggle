0a880bddb50cf20c44ea9346c39da47648608b37
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:08:30 +0000

    official radio needle top persuade

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_min(lst):
+        return min(lst)
