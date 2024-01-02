1363a4155c1c954651c28db763a55a8970f67aab
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:15:06 +0000

    year coat clear find breakinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
