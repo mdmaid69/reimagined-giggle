6ec665cbaa14342c82f6e1527c03007a74d96048
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:49:24 +0000

    conclusion clue brown organize floor

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+print("Hello, world!")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
