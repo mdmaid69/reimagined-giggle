1dd8351b71e5e12624f9d620573b3424e384d624
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:36:37 +0000

    comeoff public order owe ought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
