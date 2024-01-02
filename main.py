60f449fb02d77309d150eceee09aa3d664bb9d54
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:33:58 +0000

    desperate perform otherwise object far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
