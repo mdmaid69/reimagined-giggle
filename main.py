b167cf9ef316b29784f1a6f0970bf42b299c0139
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:45:10 +0000

    fortunate dress young it married

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
