2cc0167556c5e05f81e32c5876959aa4d11521aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:24:47 +0000

    language rest loan experiment dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_length(array):
+        return len(array)
