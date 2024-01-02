4fa22a07f2eecc5cd7b76028a308a2442c78bf0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:17:37 +0000

    motion tight praise weather shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
