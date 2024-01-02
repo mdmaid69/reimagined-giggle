58b0aaa00466726fe49982eb2c79f9e69c4a3855
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:53:51 +0000

    practise independent century view interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
