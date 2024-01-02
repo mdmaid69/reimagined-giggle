259130f09ca27e9ebe57b9fbe1632c60cf51c13f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:12:00 +0000

    formal continue keepaway match exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def sort_numbers(numbers):
+        return sorted(numbers)
