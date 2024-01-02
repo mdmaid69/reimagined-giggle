734ed7c4c3357277672aa4674f4766f13bfe1be9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:53:28 +0000

    behind may act street cheat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def convert_array_to_bytes(array):
+        return array.tobytes()
