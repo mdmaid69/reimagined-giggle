589bec9b55eed9cdf4dfd40c668cd4e2dc5d52ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:17:55 +0000

    clearly crazy hole lose taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_stack():
-        return collections.deque()
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
