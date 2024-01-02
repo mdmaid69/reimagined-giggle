3fcfb236d912e4925d461bc07486592b34e57a0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:43:43 +0000

    takeback tap determined deep shade

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
