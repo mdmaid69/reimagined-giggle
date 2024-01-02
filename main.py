e9c3d36512d6063c595b6e372befbd0003db81a6
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:35:10 +0000

    activity idea post organize beneath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import sys
+print(sys.version)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
