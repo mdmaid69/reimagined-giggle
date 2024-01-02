19fdd4a0673ba5a6d5b7186b5960bb251c33e1ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:53:18 +0000

    lift normal extra gather movement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
