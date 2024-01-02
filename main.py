36f79bdb67627d6c95fcbe3a2306557d70a12b5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:20:00 +0000

    charge glad love computer truth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
