7be85fd3232ad4d19debf389fe447262533f2c5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:32:23 +0000

    back test exchange anyone previous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
