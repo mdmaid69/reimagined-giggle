61a2c401c04b4451b2d6e7ff8175e822593fc516
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:26:49 +0000

    past embarrassing gift Mm post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
