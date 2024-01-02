ebfb7cc8c1e686b6ca54aa7bad0f797e9e2de60e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:22:21 +0000

    loud search may expression agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
