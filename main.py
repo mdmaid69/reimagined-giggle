e9d60c4527cc56e2702c564ff81b25edc929378e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:32:53 +0000

    crime glad unless go organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
