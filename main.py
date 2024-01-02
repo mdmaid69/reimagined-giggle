92da73e03d46689711e9cdc5d51d1aebffc5aaa4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:48 +0000

    interest size drink greeting clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
