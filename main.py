e207ee096d82a45cffa86c565bd2bb1ed8463c6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:53:09 +0000

    reply good kick ahead shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
