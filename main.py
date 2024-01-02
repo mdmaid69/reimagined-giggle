7c408e2d4ac47612f5869ef2bee472dab9a2c0ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:05:27 +0000

    Nn slip serious activity type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
