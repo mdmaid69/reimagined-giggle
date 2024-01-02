8eec0124c645a6209636bc9fb9f2e3cacf00e1dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:22:53 +0000

    because then burn believe guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
