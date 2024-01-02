e34a8623a59dcdf69480613e41b72c1ff458f527
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:40:58 +0000

    why ever goback(to) air score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def find_min(lst):
-        return min(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
