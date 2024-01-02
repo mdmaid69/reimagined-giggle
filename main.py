fc70fc9e1e3585cdbc5d93bb2b359559c4b65b3a
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:23:22 +0000

    fear room wrong worry before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
