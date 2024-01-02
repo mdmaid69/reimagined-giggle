fb4b33c370e21253d01b851db997b4f41248e1ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:37:55 +0000

    out concerned control load 568energy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
