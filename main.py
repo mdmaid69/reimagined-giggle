8f7afa2391cee45419b72efbcd92eae787ce49ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:07:39 +0000

    share task description environment bath

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_length(array):
+        return len(array)
