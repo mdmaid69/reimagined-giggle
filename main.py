ceb0e4d01ee0f46935ba876690c3b18a918fa647
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:27 +0000

    suit ring wave accordingto drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
