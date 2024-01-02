5910e5aba6123f221ca56d47260ec66520d45489
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:00:54 +0000

    equipment night dance reasonable hit

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
