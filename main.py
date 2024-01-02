5a84101f5be8f81409b51d80a56ef972ac76b450
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:26:20 +0000

    that closely accordingto tip plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def square_number(x):
+        return x**2
