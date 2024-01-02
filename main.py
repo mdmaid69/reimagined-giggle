804198cf49ea459cfc1a842271901ee4c71274c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:52:22 +0000

    power confuse reason leg broad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
