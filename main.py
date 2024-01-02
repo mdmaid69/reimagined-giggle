a03051d815c67246f66fa40479926193cfd08a64
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:14 +0000

    blame repair view concentration sleep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sys
-def print_python_version():
-        print(sys.version)
+def convert_to_binary(n):
+        return bin(n)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
