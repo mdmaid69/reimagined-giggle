a85deffc1317d7dbeddc2769fa2767509d67f345
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:33:32 +0000

    sentence situation allow guilty advice

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def clear_array(array):
+        array *= 0
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
