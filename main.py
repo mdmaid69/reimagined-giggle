c0241a6d66b5c475ab50200a453c01c44d34528a
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:47:53 +0000

    pull platform calmly recently though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
