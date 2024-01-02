c7430a2c02e87129a83654b840a28814bcac9bd2
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:11:08 +0000

    bottom recently watch report interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
