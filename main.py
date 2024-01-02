a83669d3b1834447994ce5598934ee69658d73e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:54:19 +0000

    judge seriously success precise destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+def is_even(n):
+        return n % 2 == 0
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def check_if_array_contains_item(array, item):
+        return item in array
