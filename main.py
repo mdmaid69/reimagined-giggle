3d44c23069ff31964efa8b5880a0a08b54d7db95
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:38:02 +0000

    giveback scream trial evening target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
