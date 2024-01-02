43df5a77d3fef50277c16423f008c9853716807f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:15:20 +0000

    wealth under progress cruel mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
