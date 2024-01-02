d90f269924b69933354c20b56ce5819caae60e75
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:51:03 +0000

    makeup which win division tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_sine(x):
+        return math.sin(x)
