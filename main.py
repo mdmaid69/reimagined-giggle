6ea6e6287559ebb790dcf9962e223b5c368bba16
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:18:16 +0000

    foreign press lucky body clean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def multiply_numbers(x, y):
+        return x * y
