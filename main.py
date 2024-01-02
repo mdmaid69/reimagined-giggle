cd675077b271d0b75b26d8cef3a97786ff039143
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:27:55 +0000

    battery pair bring rub ready

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
