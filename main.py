1eb2f464cc54273972f5421aab2f5c4c4f8cb788
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:42:47 +0000

    used married Ww hand lead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
