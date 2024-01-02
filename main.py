6b4c375ccf5cc9ead9e59cda373069ede5e35209
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:23:21 +0000

    usually sign circle long clear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import time
+print(time.time())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
