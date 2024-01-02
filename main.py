d0c8eac462c48b49519ae2b0be5b3db79c5bb000
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:39:38 +0000

    its pinch work takeover Nn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
