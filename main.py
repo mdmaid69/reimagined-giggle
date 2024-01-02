7b0dc543727c42876b17c801ddfbf970ce9b2940
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:54:29 +0000

    spin content level mark mysterious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
