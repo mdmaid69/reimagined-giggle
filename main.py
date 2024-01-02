dc0d2b7df0641b927e28118df2323be90cbaf626
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:46:42 +0000

    565end rich basic low progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
