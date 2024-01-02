70d7ea962af481136b91c9386e1e029fecf027dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:33:35 +0000

    contrast book Ww model party

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_file(file_name):
+        os.remove(file_name)
