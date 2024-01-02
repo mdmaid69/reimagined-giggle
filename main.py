6071cf457dec6dfed63f06961efed3a0627ddef4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:50:44 +0000

    travel control border too until

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
