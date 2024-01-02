d6f20a1f6a8658efee081099c33c62a2e1382311
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:22:54 +0000

    behave pen provide lie everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
