69dbfd1fb06e133ad0334131ddfe3665eb83db6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:43:32 +0000

    call run belief share scrap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+print(time.time())
