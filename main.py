29ce086fcb6ab226529c713918727e22028f0772
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:54:09 +0000

    shout frame tragedy this deliver

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
