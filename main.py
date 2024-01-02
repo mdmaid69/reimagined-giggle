ef8e9ebe829d727ea886635a086f2b2ded0706e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:32:53 +0000

    exist born steal young then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
