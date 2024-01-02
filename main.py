6008a06552d353630c5629880fa27aa92559d859
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:29:39 +0000

    school although shake bus will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
