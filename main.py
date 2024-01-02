e3edcdf2f47e2e22b1fd20d1e129e3688d325c65
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:17:23 +0000

    seat back city thank child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  def convert_to_hex(n):
+        return hex(n)
