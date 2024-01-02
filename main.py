e0233af1080e7db143a857f3556429bb82ecaaf1
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:48:49 +0000

    strike cut active apart various

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
