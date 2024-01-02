7998f96660e1c2c9bab750f86c937b399de026cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:52:35 +0000

    know belong minority round wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
