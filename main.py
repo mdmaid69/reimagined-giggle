9452bdf9f3964356309c9d153a6ebd57092a065f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:07:30 +0000

    along decision nothing reserve plan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+print(sys.version)
