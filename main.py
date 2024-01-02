fa18dc153ef0a6b77e5dc81bcc90c514261c9c39
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:08:30 +0000

    sudden food introduce speech time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
