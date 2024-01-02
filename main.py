db7fe187536a1c16e6c2020c104c96c3984eef65
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:45:03 +0000

    tap exhibition future everybody increase

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def add_numbers(a, b):
+        return a + b
