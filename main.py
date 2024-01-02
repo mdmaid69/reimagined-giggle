2283b765a42f3d6255dc791928f1e96b3c40e01a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:21:54 +0000

    unusual will custom front exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_even(n):
-        return n % 2 == 0
+for i in range(5):
+        print(i)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
