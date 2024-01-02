bdf9c719fa745394f297002246408f6e042718ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:36:59 +0000

    final over doctor breakdown live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
