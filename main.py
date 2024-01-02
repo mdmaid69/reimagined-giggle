212b310f57b1318d059253ec10b9482a1139a4a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:59:15 +0000

    ilovedicks unusual clean seat claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
