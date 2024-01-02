505676d2074242919da37c2cfe690cf982b0bdcb
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:28:21 +0000

    along comeover actual steady lend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
