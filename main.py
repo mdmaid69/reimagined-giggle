f02bd1439afba238cc594e09ed74ad8d709f37ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:42:19 +0000

    either total exist peace spot

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+for i in range(10): print(i)
+import array
+def get_array_typecode(array):
+        return array.typecode
