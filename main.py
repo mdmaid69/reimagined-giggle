aa647a8f0111fd7480472c40ba406db7c371943c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:23:13 +0000

    mean lock sensible have year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
