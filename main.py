47f5bb7f51bcf9239204e65bcd61e85aca6e16b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:59:47 +0000

    takeoff eager this aim bringdown

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
