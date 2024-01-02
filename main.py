87d0ab36545fb062c0e9df7d5051a03faee75ee2
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:04:50 +0000

    justice which tonight communicate free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
