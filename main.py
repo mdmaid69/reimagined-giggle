f6739a9c5d58c210dcb104c3f68eab96a285b164
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:39:17 +0000

    guide godown pay top how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
