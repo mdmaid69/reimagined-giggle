74f8054c2f0d60a59c36d52d614c66ad93b53e9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:27:58 +0000

    taste leave Ee roof it

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
