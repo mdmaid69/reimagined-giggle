b9d780e8a75981801fd5f283cda364e43bf5ed59
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:48:40 +0000

    bringup bright someone building passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
