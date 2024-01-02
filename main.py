46a9eb31dde0194795858e24790b9728e08f72ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:20:39 +0000

    possibly many year occur scare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
