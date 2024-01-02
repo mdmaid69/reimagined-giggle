a02e498617a05687fccc41cdbc4c076ab5776297
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:06:38 +0000

    sensitive mark light loose risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
