a04fe75c96b733556cb9867be7a202309217380e
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:54:42 +0000

    step world transparent Ff share

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area(radius):
+        return 3.14 * radius * radius
