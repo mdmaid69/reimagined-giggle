3fb27e88dad6855da409ba71976485bfaa4d2f38
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:51:59 +0000

    part broad preparation pay common

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
