c6a38df5e8bf813980293349e54e92401671eea9
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:22:48 +0000

    image turnback breath wander appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
