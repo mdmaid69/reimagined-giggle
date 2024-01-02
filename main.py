eb405ad82a9caa746ca53ade506cc68336461b99
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:17:12 +0000

    mother level above man tidy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def is_even(n):
+        return n % 2 == 0
