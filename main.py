acb8a931fce2bcf41c36c7e94733aa25e6f2a637
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:38 +0000

    sink spring uponon peaceful nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_list(array):
-        return list(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def convert_to_hex(n):
+        return hex(n)
