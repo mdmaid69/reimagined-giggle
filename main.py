20afd07ac8565325ed4c984ff87fe1062af7787f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:59:07 +0000

    design contact I chin accommodation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
