6924eb41adaecbdb8e08dec177592594f398804c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:39:40 +0000

    convince 566end confuse read side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
