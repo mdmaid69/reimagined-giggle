885502048d55a7cab9a70c840c4bd3aa9c4dfb74
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:06:56 +0000

    rid skin pretty prize delicate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def cube_number(x):
+        return x**3
