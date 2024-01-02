15ed0868752a176f77c50bf4d85dc5e768b2084d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:10:35 +0000

    birth afraid clear later comeback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
