29443bd80b4c84744c602f0467c7a0bb8ad5f9c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:54:13 +0000

    true sensitive pipe mean uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
