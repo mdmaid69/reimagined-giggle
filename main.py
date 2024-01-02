56a360acf64b8ff475402c93d917e1c11161c61b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:30:55 +0000

    rubbish load drawing relative agree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_power(work, time):
+        return work / time
+def is_odd(n):
+        return n % 2 != 0
