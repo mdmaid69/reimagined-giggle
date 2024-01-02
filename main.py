90775d13b59f597a9543d49003694137a3cba937
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:35:14 +0000

    design guess money walk difficulty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def cube_number(x):
+        return x**3
+def multiply_numbers(x, y):
+        return x * y
