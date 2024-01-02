aa330100bafca7759fd3e9dfa731ccc08ba6f797
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:08:20 +0000

    fit worth raw unable soft

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print("Hello, world!")
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
