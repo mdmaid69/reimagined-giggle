c220e05bda5403c9ac9ce4c1016882276e5fcb03
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:30:21 +0000

    would plastic plenty breakthrough blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
