d28851409b9347b3f43e71e5bd22f186104ccfca
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:07:44 +0000

    object swimming finger cold 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
