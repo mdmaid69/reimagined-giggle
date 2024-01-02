315640c1d5aa3fa33dc915a50498fce04bf81547
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:52 +0000

    offence release word serious crisis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 def remove_duplicates(lst):
         return list(set(lst))
-import array
-def get_array_as_float(array):
-        return float(array[0])
