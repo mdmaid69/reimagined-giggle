5945422d3d0e853ac9367fd71cc4a3a1315d487e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:16 +0000

    sink lunch generous group guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_item_count(array, item):
+        return array.count(item)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
