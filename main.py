80e38278ffb3eaa43f8a0f5235996b50b2fda643
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:04:02 +0000

    hope occasional still 573entertainment terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
