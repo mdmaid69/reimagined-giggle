372f4acca799804ee4ba2cd159bd0ec49f097231
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:40:45 +0000

    valuable display steady juice close

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def square_number(x):
+        return x**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
