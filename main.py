51e75b2da2e59f210172ae1eb6cb980d8b178c2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:23:45 +0000

    evidence company write raw form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def remove_duplicates(lst):
-        return list(set(lst))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
