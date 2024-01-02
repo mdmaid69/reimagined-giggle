1b039640192c372ad3fc767a17180e198b57d802
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:04:54 +0000

    cover star mistake praise gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
