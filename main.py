85f430a6c8bd40c588580b11e37d3d8c8e04b684
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:19:58 +0000

    imagination alive love as survive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
