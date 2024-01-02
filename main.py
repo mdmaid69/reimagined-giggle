35df24666396b00917f5dce10e914d9764fa6614
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:24:41 +0000

    plenty man survive develop water

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_hex(n):
+        return hex(n)
