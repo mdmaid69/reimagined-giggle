abe76f653f2f634d0032ffd534df338d0a1a4272
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:36:15 +0000

    pretty generally return approval niggaacademy

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
