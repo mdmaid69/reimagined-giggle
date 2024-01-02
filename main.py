5628a6bbff437e0ee9bc4a13689fd80bdc089b76
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:43:55 +0000

    convince judge travel short scheme

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_str(array):
+        return str(array)
