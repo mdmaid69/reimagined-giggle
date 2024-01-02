608e1092549f22895aa41f6bb731d0399847bb11
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:46:51 +0000

    pronunciation basis past better upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
