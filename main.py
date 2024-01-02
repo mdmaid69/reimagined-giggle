b4f03e3d95c4ca76ada38eae88e527f649928744
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:58 +0000

    attempt sticky city regularly design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
