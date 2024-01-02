9c6da1d1e1f6f0749112362535d30894ce02c3f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:35:39 +0000

    wait negative correct small her

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
