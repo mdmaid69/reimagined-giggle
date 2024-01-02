00498775eb61b63013d5ab97b9b21ed689e7d71d
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:52:37 +0000

    middle violence zero excited lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
