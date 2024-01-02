d8027e0bb99ebe25ff991b25d98eeef22b759597
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:55:36 +0000

    nor crash large cap change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
