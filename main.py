c43780e430f85150e82600f7ce9be15c0034c8d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:41:15 +0000

    suffer anywayanyhow convincing shock spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+print(os.getcwd())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
