596aeb8926e2d41e74d78844b34aac13299598b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:06:53 +0000

    minor stamp corner gas top

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
