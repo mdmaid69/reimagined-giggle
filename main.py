f739c3ad8f1d6671e161d2b868d26797ea805309
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:49:46 +0000

    separate deliver Dd mixture play

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
