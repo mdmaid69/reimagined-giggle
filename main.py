e6679c463ce7fd0d5eb13a2b31fb4e656443d17d
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:05:30 +0000

    arrangement stand collect hold worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
