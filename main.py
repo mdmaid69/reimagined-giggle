5fbf39a67608aec0bdd226c69a2f19ef96c6f882
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:15:01 +0000

    drawing peaceful especially mistake fit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
