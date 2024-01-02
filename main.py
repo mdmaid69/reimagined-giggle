c4f01a67aa9fd3c06e527d65d39e6d2207684145
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:22:31 +0000

    exchange me through accordingto company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
