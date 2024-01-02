f3a46642457a6f5b9a5671f6f11ab15aac05c3fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:32:40 +0000

    ring art add to grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(lst):
+        return min(lst)
