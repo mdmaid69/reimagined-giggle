f9c968efe783ab547b40aa02bd867b9305b5092a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:01:11 +0000

    stamp suddenly suddenly see emergency

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
