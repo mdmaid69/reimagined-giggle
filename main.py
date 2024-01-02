238ea2f9fc5422d9ba8dbe7cc924d9ca92990ef4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:08:04 +0000

    weekend high nothing worth ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
