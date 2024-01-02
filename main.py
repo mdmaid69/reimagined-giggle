b4cbabe64a9704fe11c4b0ce74788515f277c5af
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:44:41 +0000

    to doubt straight bury you

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
