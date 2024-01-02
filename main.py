9264543a9bdebcfd6c31d0bc77e4c25323bd4332
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:54:01 +0000

    deserve definite become nature formal

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
