f26e3ba385555bcf4fe4270fb91578a376222b60
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:39:01 +0000

    traffic mess getaway upwards office

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
