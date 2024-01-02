42a18a43ec6169f282c47cec629ce02a01e33bd3
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:16:17 +0000

    sweet occasional change table relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_queue():
-        return collections.deque()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def convert_to_binary(n):
+        return bin(n)
