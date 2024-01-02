a6d787e4bfe84b72470c9182e7f14e1ed5bd1ed9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:23:21 +0000

    office gift shallow task gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
