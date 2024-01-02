95d45ed4e86326c58fa64e1e9f213628e9df3440
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:13:16 +0000

    rather rob command minority apart

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
