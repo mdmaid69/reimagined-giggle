2e84cc6daa1c74a8b9f94ecca247fac2985aa0c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:50:29 +0000

    law debt present circumstance second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
