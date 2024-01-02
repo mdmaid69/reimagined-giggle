9042c5af001da498ecdc0e77f0fe1cfb4e529fc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:33:28 +0000

    any pour block bet use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
