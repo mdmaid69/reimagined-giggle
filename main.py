1b45cd0aacc56db2e2dca4ad2f0d929d2292296d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:03 +0000

    sound proof break apart possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_array_index(array, item):
+        return array.index(item)
