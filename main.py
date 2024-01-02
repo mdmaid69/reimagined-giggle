8063bcb043b71eda263fef5f2a62a507e4327f03
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:48:40 +0000

    used intend pair trap occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
