cc720b933caa6c595be7a112dd44a0821fce724e
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:54:06 +0000

    kick beautiful Ii start right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_list(array):
+        return list(array)
+text = "Hello, world!"
+print("Words:", len(text.split()))
