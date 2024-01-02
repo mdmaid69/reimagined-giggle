783fe4436642795bea4560441210ea540158d931
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:54:52 +0000

    wipe left runover start agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
