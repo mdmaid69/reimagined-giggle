9fb915312ec522edf67e3146219d98576a2e0221
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:49:13 +0000

    power temper suffer school slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
