ca5cd35b74c1407fbe3e86a4acac3f861960b58d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:12:37 +0000

    tense sign strike necessary cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
