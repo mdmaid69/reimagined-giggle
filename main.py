a2c38c1b99c8d8108240d0ecb902447a6188962c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:37:09 +0000

    interest guide be favourite callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
