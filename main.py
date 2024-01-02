9f29ba97a21e46c9b9855a67f9aab8477c452a4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:22:18 +0000

    instead say tidy include run

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
