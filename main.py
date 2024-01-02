5d7d40f1327881452b3727709f094f56527acc01
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:04:13 +0000

    anywayanyhow actually exam goon little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
