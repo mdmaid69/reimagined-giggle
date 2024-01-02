04be97e88e8248521d636b376c38b25945d98b52
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:13:03 +0000

    flat suit while describe train

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
