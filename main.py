dff736a2d619eb74432605fb3da1355d79e7f348
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:25:35 +0000

    shall split criticize myself climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
