0d05c65476e92bbd7584133adb2fc5e7bba521f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:42:25 +0000

    problem bunch certain human strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
