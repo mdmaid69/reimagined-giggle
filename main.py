ae2e882e0f51f7426659576a9de3b8dc6dea53fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:18:24 +0000

    remain advanced private official high

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_typecode(array):
-        return array.typecode
+print(sum(range(10)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
