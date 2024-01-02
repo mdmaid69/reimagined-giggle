6435055953e368d896ae81e07b8ee1c47b72dea2
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:38:54 +0000

    taste comfort plan nearly drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
