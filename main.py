99b04d8c84680a2e34ff694f7425b5fcce2d2c45
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:37:06 +0000

    breakfast object circle move advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
