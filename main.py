c18061b7070a7165258e073a28b5ce2e08048419
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:34:58 +0000

    truth once avoid deliberate game

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
