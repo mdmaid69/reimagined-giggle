cd514ac5954a6e697727e51d13d5d66ed41d8054
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:18:43 +0000

    row sit professional because prepare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
