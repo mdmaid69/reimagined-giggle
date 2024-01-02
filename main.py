1497e306b4ba8659326e69f4ec13e0580f5e80fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:56:45 +0000

    fly cry mind plate pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
