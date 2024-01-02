e47503530f626c24d767d815d5f7913812ed0c0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:20:33 +0000

    upstairs pocket recover invite pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def is_even(n):
+        return n % 2 == 0
