3486f2a363c550a44787344186fda21e02232365
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:31:55 +0000

    perfect eager interrupt letter want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
