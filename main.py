766a4342baf71e02ecadc08495661d9429dc6ae3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:40 +0000

    sure wide keepout owe finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
