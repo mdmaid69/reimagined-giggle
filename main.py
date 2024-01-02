17c2ea4a28954981aa11c3f9fc5eddf1a280b58c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:34:12 +0000

    mad equal improvement play apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
