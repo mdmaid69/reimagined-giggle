5cd323b17168d2ff26bee7cd55778eafa868ca1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:17:17 +0000

    by pin fear flat here

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
