6424d4706eabada18721edf4dc0f97ca38378279
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:02 +0000

    direct weak thin bet busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
