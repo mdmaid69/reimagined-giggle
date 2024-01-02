334ec8b284e0b3e3bf0b27ee44299a758beea057
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:03:40 +0000

    hurry trouble tend male takeover

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
