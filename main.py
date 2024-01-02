1635a307698812014d929fc10bf2abc268a6cf35
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:48:05 +0000

    cold field manner bright roof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def sort_list(lst):
+        return sorted(lst)
