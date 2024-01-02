ac2192a6e15807d80c2b338c22cc8cbe0e4d95b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:09:10 +0000

    564encourage Ee content shock introduce

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
