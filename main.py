dea88898baadc6fa45e499371123d4357e570aae
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:33:49 +0000

    fold proud fill mysterious lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
