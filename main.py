373899aea25b804fd80ddb405c4c17fed06f026a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:39:56 +0000

    near wave computer skill hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
