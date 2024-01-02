045cb2d7f1229f1d4b020a3b48c6d945bc4f394f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:05:03 +0000

    hurt arrive turnoff hit replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
