a9ad9660ac0ae0d11679a7cf20754626f8d0c2f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:47:16 +0000

    valuable congratulate opposite almost recover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
