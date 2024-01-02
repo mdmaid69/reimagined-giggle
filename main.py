14422ea8561f2ffc60d3edc2869336b10db5a21a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:56:27 +0000

    punishment luck deaf if create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_index(array, item):
+        return array.index(item)
