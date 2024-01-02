6db37dd26c3ce76f9b24448a81a152eef76e5194
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:19:51 +0000

    careless anywhere far creature large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
