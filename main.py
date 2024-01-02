572b57152102ebf826e9e320a3391194e7930622
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:34:50 +0000

    across stage voice beat wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
