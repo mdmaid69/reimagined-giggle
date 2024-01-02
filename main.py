5d6380e9f828f8894ce8b1dc6c19c29ec229a2d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:34:56 +0000

    turnover mystery comeround idea middle

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
