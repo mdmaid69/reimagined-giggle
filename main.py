8baafeb617b2d2244a7bc5f5cf2be7393cfdc9b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:58:12 +0000

    lead payment add mark difference

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import getpass
+def get_username():
+        return getpass.getuser()
