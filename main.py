1ee047380a38307b5834e60cf66e239fb1e81411
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:06:02 +0000

    practise closed bound gooff around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def add_numbers(x, y):
+        return x + y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
