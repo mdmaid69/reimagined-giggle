7183a162516a340da3aeb2ebf7659348680961c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:41:13 +0000

    term independent answer sharp operation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
