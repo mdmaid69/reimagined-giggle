00f613bdd793f479397991aeaf57122a7c5f410a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:35:49 +0000

    often majority excuse interest downstairs

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+print(math.pi)
