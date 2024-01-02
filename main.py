edccca983469b2717224f7f58a78d67a23864aca
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:10:02 +0000

    comeout promise trick behaviour pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
