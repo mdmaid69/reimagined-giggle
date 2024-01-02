308bd46ec501696ceaa064bc49b2fa480f04bac9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:07:39 +0000

    pattern invitation country distant vote

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import sys
+def exit_program():
+        sys.exit()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
