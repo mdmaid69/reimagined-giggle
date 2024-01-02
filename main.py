5b8d5d61bbc0bed03997611458d1364d73f0f4c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:58:22 +0000

    young petrol very owe strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
