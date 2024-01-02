f2d58c473313a38be4c9a29f29e727b026d3e38e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:48:55 +0000

    lost concern taste cross deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
