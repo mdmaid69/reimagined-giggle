31aafffa1d50b9cb0358be35642d8b9faf8d4a16
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:34:54 +0000

    back estimate able below summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
