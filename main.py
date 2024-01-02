545a459419209507cfa08d9481019d25bfe41694
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:09 +0000

    unfair visit service club stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
