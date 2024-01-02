b88a48140dd8327c9d724fcad162ef6746eb8252
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:06:18 +0000

    see owe female attention convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
   def join_paths(path1, path2):
         return os.path.join(path1, path2)
