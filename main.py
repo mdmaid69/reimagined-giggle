48dc4a74b1c1326c5e89e02ffe70efd4a1ef203e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:03:56 +0000

    impatient read in concern small

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_area_rectangle(l, w):
+        return l * w
