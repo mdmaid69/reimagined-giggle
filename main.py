207c6ca2a161a3011da3157afb49b733b02ef171
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:42:21 +0000

    field knock mean spite judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
