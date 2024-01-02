5f80bc8cdd2b43270cc7a25736e90b11894787c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:40:42 +0000

    guide health answer teach born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
