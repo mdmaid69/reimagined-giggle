75d52ca0687f1040b28328c06a0f22d679369c1f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:21:55 +0000

    measure forget deaf afraid cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
