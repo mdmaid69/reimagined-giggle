8f627666021d4536dba244bab397faa882a167f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:45:10 +0000

    runout callout zero spin congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
