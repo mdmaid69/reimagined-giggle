adf4976242d8912fe60b048472e05c157078711e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:15:53 +0000

    must just whichever remark then

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def append_to_array(array, item):
+        array.append(item)
