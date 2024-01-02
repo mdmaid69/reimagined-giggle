0c3c6406f52b04474929868656a5761dd6f93a54
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:14:03 +0000

    forward declaration person scale lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
