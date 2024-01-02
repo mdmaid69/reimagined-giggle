7d2d263e07a908fa7ab89e4908778cef857e1219
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:01:15 +0000

    curious fasten afterwards callback more

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def is_even(n):
-        return n % 2 == 0
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import time
+print(time.time())
