3287af7aee7dd081eb31817bc6449e256ad071e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:06:42 +0000

    him string congratulations zero body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
