af968907320e3fe1bacaadb265e935f7400dccb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:50:50 +0000

    through create mess finish argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
