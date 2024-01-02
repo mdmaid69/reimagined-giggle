622e2283c0b10f40bb7651d7953ff2d96e3f377c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:33:21 +0000

    strange impressive instead activity spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
