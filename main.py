ac70c807e11cd3f9cdef2c2f9c281461f788c375
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:08:04 +0000

    town go skill order before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
