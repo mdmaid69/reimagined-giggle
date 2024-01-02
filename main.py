663a6c14af5bc94bba062cf5874cbb6468e4aad1
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:29:30 +0000

    feature feature breakin odd loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
