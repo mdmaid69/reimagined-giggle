3c5e185f21f66daa7ac3a618cda2a5beaf554018
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:37:45 +0000

    safety unlikely she claim stretch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def is_odd(n):
+        return n % 2 != 0
+def reverse_list(lst):
+        return lst[::-1]
