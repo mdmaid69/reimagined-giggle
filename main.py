615b5d8fb079d14567f566fda927cfa43adaf989
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:14:11 +0000

    breakup effect talk decide university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
