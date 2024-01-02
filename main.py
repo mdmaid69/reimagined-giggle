bc6614cbd786f3e619ee3220c213bf26834150e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:28:15 +0000

    runout transport here hang doctor

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
