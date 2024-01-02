3390e44729ba95af734d8c9107546fc64107ca91
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:04:23 +0000

    yet flow sore improvement material

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
