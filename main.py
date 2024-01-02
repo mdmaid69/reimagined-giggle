0e7161aa4545e1ed9ef08d61a064593e7967ae28
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:08:16 +0000

    bang die service giveup pattern

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import datetime
+print(datetime.datetime.now())
