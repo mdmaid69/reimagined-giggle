87dad5af200f5c96f3cdd5a027b3a7e403a5f292
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:30:32 +0000

    discussion study call turnout hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
