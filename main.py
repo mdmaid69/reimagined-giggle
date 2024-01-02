fe1f727430d153161a52974350c8470d8c74a4b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:46:06 +0000

    note disappear practice suddenly 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
