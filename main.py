9e355a8b6de4e3d70208b79107a729dc785e5afa
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:31:41 +0000

    hard kind swing crash breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
