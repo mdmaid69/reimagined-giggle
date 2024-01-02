11a62987f7c85e9a58be319de813e13f577ee1a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:58:45 +0000

    contribute dance dance common avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
