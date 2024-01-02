787c9a3a5ebe139b53694db08ca78c3f4f52c95a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:06:20 +0000

    upwards wire wide tired bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def convert_to_binary(n):
+        return bin(n)
