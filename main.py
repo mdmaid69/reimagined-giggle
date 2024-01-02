4590a5ca5c451c6da720fd6855e8ffc04c373d47
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:18:25 +0000

    emotion unfair please smart capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
