a1d9e6af41432dfe42b9d270413963910280032e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:58:20 +0000

    classroom search Ww nerve development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
