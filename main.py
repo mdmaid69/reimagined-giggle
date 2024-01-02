f6dbe0fafa5d0c182f7063c696aab9ad5b5b8527
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:51:58 +0000

    gas scene exhibition watch another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
