9cac80953463b5c7005995dd0c43e55cfc9056b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:36:46 +0000

    circumstance break field project measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
