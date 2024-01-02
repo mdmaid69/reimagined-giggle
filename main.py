45ce6c0a582203ca8281e91c9691b6db69cd6355
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:15:35 +0000

    father fact be sail here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
