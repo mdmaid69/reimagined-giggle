63726a79e6073472025cdbb4a93dec0fece6ebae
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:47:48 +0000

    before try previous concentrate side

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
-print(time.time())
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(10): print(i)
