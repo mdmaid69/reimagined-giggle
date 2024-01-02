dfd08d1cc81939fbc3a1418c5fabdbb36541ffe0
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:13:28 +0000

    system spring damage excuse late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
