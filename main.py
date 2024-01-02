e55165100f35f7f5600382cce1d4f5189b631ac2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:47:46 +0000

    rush spill remember first vote

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_speed(distance, time):
+        return distance / time
