d830e12f339fad6bd3a340def7140456b38b8b06
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:29:52 +0000

    however height carry Nn say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import time
-print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
