a3411ea85e915b2ae4ef98e16c3eab2b59ededdd
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:52:47 +0000

    so criticism hand vision go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
