51beff3f133b3c783c0d722d7b66398f538feb40
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:17:41 +0000

    confidence pump bringdown Jj favourite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def count_elements(lst):
+        return len(lst)
