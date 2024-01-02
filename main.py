909a377bcd619fdf245d8c39574a6b7812d7839f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:17:00 +0000

    extra tap government describe trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
