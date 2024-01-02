265f8cbfe5315d85667b0e15975320f79ada355c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:39:21 +0000

    talkback pure bang money distant

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
