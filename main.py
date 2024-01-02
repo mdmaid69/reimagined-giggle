09dd58ce76f224dfe759e56e932c16f2efdcb0e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:35:23 +0000

    jump course smell occur appearance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def reverse_array(array):
+        array.reverse()
