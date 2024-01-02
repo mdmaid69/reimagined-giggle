428b95119ec761dd053b976af38f8ca4ae638235
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:53:31 +0000

    everyone curious voice lean work

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_list(array):
+        return list(array)
