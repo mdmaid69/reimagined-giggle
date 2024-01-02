6e8bceed6d71d728d0dd3f0a9f886e50288e4282
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:08 +0000

    music survive myself inform bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
