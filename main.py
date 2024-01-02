032807e240b479026e91903e44420147e704644b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:35:55 +0000

    protest rely night trade greeting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+def calculate_power(work, time):
+        return work / time
   import csv
   def read_csv_file(file_name):
         with open(file_name, "r") as file:
           return list(csv.reader(file))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
