c62542379c5622d4a101d1e8aea2a45b335b10c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:19:10 +0000

    tall official run thinkover knock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def subtract_numbers(x, y):
+        return x - y
