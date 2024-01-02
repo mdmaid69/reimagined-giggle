a513a52562ae2f8bf0c2622ecc8bcef74f71dc7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:49:19 +0000

    decision object why 571enough sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
