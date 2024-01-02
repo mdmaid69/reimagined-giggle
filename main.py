d6f1e68a9a5e2cf4e0f94980c26360841fd379e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:17:00 +0000

    bread giveback giveup nor feeling

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
