70746d1b904574ca04c90e41c1b91ce77233979e
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:10:07 +0000

    work plastic keepoff interview exam

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def convert_to_hex(n):
-        return hex(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
