64392458584084d437c026c17a8c2f68523ff16c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:18:32 +0000

    bring tape holdon instant firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
