a79c45d464246f6689264484b5fe5c22822d8e54
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:23:43 +0000

    free pretty whole load slide

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def convert_to_binary(n):
+        return bin(n)
