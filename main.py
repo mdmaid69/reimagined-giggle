6d7cb41a2116a80c54293dc28395468ca44f96b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:05:56 +0000

    nature bringforward storm watch term

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
