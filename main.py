5c4a0eac07ad55c9efae05866f55dd98a1cc91ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:52:11 +0000

    nor speak sing whom simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
