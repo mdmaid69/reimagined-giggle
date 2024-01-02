2162b2fcd5d9a39dc19b2043e08692608b00f6cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:49:25 +0000

    negative arrangement plan desperate listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
