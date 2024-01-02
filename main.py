fb4700d4561b79a155f58b261bfb7ae397368a31
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:58:18 +0000

    smooth cool bit neither yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_as_float(array):
+        return float(array[0])
