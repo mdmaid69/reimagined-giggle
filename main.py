700e4a631c11d9d79064e896a02e674de1185e59
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:20:25 +0000

    change shut practical decision talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
