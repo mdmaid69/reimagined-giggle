18d1ce4f4985e60eda4d073091fca138c9289840
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:19:02 +0000

    life chief image badly immediately

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
