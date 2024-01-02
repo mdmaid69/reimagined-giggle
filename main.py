08f18232f37cd0af153840cbfab77b6ef88fb299
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:56:00 +0000

    fetch raise bet head active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def reverse_string(s):
-        return s[::-1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
