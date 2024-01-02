a176b447996b32f4aedb9dbec0e698665573b92e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:07:05 +0000

    feature special earn side sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_user_list():
+        return collections.UserList()
