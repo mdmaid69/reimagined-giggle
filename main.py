4aad9e8254c23c78a2f4108de8096841c8239b83
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:37:55 +0000

    design pool live cost while

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def count_characters(sentence):
+        return len(sentence)
