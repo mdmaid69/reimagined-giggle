7180ddefed2318ba0a0d5aee4f4960d3ab3db7c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:28:38 +0000

    use late suffer gentle slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import os
+def remove_directory(path):
+        os.rmdir(path)
