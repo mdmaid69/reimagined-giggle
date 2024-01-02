1f8df9c163a1a49be7de961ddde07cabcb670363
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:19:04 +0000

    himself hello relative upstairs pour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
