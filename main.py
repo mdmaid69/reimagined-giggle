d4d9711eb09b2adadb48cb6f41098feb57bfdd01
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:04:04 +0000

    frequent unfair gate bend telephone

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def subtract_numbers(x, y):
+        return x - y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
