7d3bc2d48b96386a99c28993cd390d4887098562
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:42:24 +0000

    attempt each spoil natural building

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
