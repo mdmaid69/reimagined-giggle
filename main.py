4a7516e62c5acef7360b0377af909f1ddc6821cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:58:30 +0000

    Itself head false dull smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
