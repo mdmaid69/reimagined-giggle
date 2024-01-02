a53466563f4a068c9163e7d182633cd8333ab47e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:11:51 +0000

    tidy awake favourite fire slightly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_array_to_bytes(array):
+        return array.tobytes()
+text = "Hello, world!"
+print("Words:", len(text.split()))
