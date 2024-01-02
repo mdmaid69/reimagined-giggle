385bbd49531f3ac10b241b375232bc2e2ef1c5e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:10:02 +0000

    slight comefrom accident surprising give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
