7a9e42cb0e585bab5ded5c020315b6bd3e9c6170
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:00:41 +0000

    opposite concentrate refuse story skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
