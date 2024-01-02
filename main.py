636db13c99fb71cab36b4779dc32d3f389558520
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:42:32 +0000

    space relationship bad dress stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
