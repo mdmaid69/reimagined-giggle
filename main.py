3efd9cf9d79ce2c0a8fe926c4a96ed9ef713155a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:57:56 +0000

    transparent swim borrow past leg

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
