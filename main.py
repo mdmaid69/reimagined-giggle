e97888b2ad3ab06cfc61e4e69b137fc47492483e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:28:31 +0000

    lock shall future morning line

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
