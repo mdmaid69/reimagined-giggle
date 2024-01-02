d95e46c63c4d16bbd68a1bc666159df5f76896db
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:15:02 +0000

    sign underneath OKorokay read pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
