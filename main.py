27d4744232eb45bcc44599b5c0c0a3a07a86d899
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:14:42 +0000

    sweet move alone rise exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
