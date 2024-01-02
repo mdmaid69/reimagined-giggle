003892bb9562ce69a14c9476331bce81657dcdc2
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:26:52 +0000

    arrange breakinto turnover official blind

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
