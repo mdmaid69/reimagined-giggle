a80499bf2f17bfb8b524c81eefce7427109447da
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:22 +0000

    effort group convinced there race

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
