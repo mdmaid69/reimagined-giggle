c8051f61e96d963b1fe80a2f45a2106614048476
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:24:12 +0000

    inquire make crowded work judge

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
