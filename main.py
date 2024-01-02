2d571824afd011c0e6606991dab546a251ea82dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:33:06 +0000

    somewhere mix leave inquire gradually

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
