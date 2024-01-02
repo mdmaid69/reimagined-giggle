b41ad8149eda693e479bd1b4e6e75a6816eafa59
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:22:39 +0000

    congratulations large rarely position shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
