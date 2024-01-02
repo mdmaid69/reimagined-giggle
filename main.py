3d976204e9bf76e411629ebcaa2106ec9c687a03
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:27:33 +0000

    block declare store receive shadow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
