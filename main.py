3b1b8b315f8f2d59cba8b5717ce173e82ead9014
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:50:34 +0000

    belong will afternoon read positive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
