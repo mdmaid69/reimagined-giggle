704370a51f06797484283d80fb6f752d806e9b77
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:03:33 +0000

    hit home style murder television

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
