962809fdc4a0bff396bebe00f31c098b7c69ca81
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:12:16 +0000

    poison ourselves pocket disturbing price

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
