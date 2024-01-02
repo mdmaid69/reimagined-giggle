a94fb0cd46c38275457aba889769cbaf2b32f772
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:37:38 +0000

    effort roll ought mother rarely

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
