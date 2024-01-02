d63791ca29e0531f501804d50235f8a6ebabeaff
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:10:36 +0000

    charge have identify definite excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
