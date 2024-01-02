1aa601fa670d0628ce00b9788c39ff83ab124fed
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:00:52 +0000

    second greeting some reply attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
