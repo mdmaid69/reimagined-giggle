d2b7d8961721b9e278380523a7f7aede97b1f044
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:00:54 +0000

    crowded opposite means shout major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
