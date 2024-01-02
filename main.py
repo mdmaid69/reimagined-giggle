5d6e0037ec51b0ccbbb478ee2959f3175a63d677
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:32:03 +0000

    total intend sensible splash building

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
