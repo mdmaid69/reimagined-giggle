3f23e4aa5a0863dcbe03e1852058b039f9785b30
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:18:07 +0000

    carry bringup clean slow really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
