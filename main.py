e72013e08f9af78891368ab1dbc492ecfdb4203d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:23:41 +0000

    there loan open complicated tense

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_volume(length, width, height):
+        return length * width * height
