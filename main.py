bcef5b4db3c9d9d6177abdab1a46f8d5817314f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:12:45 +0000

    zero colour about best mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
