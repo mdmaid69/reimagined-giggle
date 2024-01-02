ddcb5225c2a61bbf6f773220dfa6400486b9b303
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:03:51 +0000

    calm increase story ability proud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
