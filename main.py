11c6d7458285e726e507a0e185df56dd088677f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:22:15 +0000

    compare round rely mistake summer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
