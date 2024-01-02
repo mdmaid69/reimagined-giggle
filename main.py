0eb1a841487edcf5274e672f4dc5a099de52d7ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:30:19 +0000

    Kk picture sale shock 571enough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import os
+print(os.getcwd())
