0718e0a528fa22b2d5ea79b97f89b334a6b94ff8
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:25:09 +0000

    event behave suit 569enjoy comeover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
