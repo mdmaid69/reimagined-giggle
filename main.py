35028b5839aeacad7ef16478cf06afd4f0b57401
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:58:53 +0000

    entry number turnaway explain independent

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
