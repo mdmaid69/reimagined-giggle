9611a2761b92a726bfbbcd000e95d3891b6a0bb2
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:08:24 +0000

    rescue art repeat nose try

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def square_number(x):
-        return x**2
-import collections
-def create_stack():
-        return collections.deque()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_repr(array):
+        return repr(array)
