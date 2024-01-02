1c56daa1c630c9dbf6014d4a33d2fa6966a7d3d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:31:37 +0000

    edge programme sport stranger operation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
