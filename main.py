cd454bd5bf0e8d00e14af8523317fd7ba25e5aed
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:36:19 +0000

    sit only work leave wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_stack():
+        return collections.deque()
