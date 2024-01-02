fcf50d95598d39dd3b1bda9ecf3b228a81aafdfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:56:05 +0000

    rob meaning modern shop discover

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
