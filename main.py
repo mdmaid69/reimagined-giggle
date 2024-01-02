17af6fc6c007db0637afc262aa8a6f768ec42355
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:48:46 +0000

    freeze post increase sensible private

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
