a949105be44c8668be5ae5144e65a00b19bfe46f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:49:10 +0000

    getup ahead stick hard wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
