39c20a6ea659b4b519ace99a170ed956459dff6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:40:53 +0000

    indeed talkoutof pump ticket material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
