bf314909d39c790a13973cb69294b0bf361c50d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:47:27 +0000

    think broad wide ignore achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
 def calculate_volume(length, width, height):
         return length * width * height
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
