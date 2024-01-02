4c1fe58baec4a2984624177b104c3f7e0f6c38d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:04 +0000

    sense refer phone understand path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def cube_number(x):
+        return x**3
