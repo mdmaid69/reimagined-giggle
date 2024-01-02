271bc8b7e0632ad7780fa4f1170ac48f39c2ed41
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:13:24 +0000

    frequent calm fasten properly how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
