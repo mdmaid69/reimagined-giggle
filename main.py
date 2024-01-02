5f55e41a26f416b03e4b3788cb5ce28486e5a2ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:02:26 +0000

    purpose drawing government boat complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+for i in range(5):
+        print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_directory_name(path):
+        return os.path.dirname(path)
