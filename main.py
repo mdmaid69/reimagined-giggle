4c44d5d35bc14c17f50fdf7ce88bb1a8c658f02a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:25 +0000

    steady unlikely over mistake floor

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def cube_number(x):
-        return x**3
+import os
+print(os.getcwd())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
