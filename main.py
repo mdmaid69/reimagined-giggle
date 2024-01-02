13834a2d1765a2417860915aa976d1649d43fc7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:22:42 +0000

    Gg almost go above land

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sys
+print(sys.version)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
