479e572218b69d90309dfa23aa26b33d7f49e188
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:25:46 +0000

    rescue clean get system trade

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
