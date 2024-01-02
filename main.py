1d7e571fdc4843c1770ce393c654a0162da13967
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:16:24 +0000

    argument cut answer science wind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
