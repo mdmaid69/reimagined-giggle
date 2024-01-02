5868fa669928020caddc89be2b5198f117011d19
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:47:24 +0000

    his ilovedicks bother emphasis bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
