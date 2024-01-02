65e88f4b477422ecedabd176046e96cd389f7cde
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:09:24 +0000

    danger minimum butter doubt mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def square_number(x):
+        return x**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
