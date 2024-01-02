2ae858247155e63e2af4989e0939f063ab2dc1f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:16:00 +0000

    several powerful image satisfied tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import tensorflow as tf
+print(tf.__version__)
