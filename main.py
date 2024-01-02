92b19de6fc181c76f834113230b0416c291ad7bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:10:34 +0000

    use born remove blow force

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
