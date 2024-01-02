8a7406a2ba9e0a260568ff9cec15a076201d8c87
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:46:25 +0000

    coffee order value consist Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
