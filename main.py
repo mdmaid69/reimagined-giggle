9656cd61351396b1d036e2049c2b7b0f1b5aa1f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:40:18 +0000

    serve brave pause form solve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_sine(x):
+        return math.sin(x)
