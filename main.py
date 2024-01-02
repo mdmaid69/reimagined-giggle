0d6485f80d847ca6e4c8792f88d7dfabd1403fb3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:19:51 +0000

    press morning crowded lead everywhere

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
