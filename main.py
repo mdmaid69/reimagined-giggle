96d74c8296ae61d033f335c5fc76f12e7bde45b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:01 +0000

    find cheek behind embarrassed noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def subtract_numbers(x, y):
+        return x - y
