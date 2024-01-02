3b0b5ef0cefef8faeb56a28ab496c9de87764e05
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:09:38 +0000

    trap conversation take but through

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
