30b8f1225073339ba2c75b52a9b9c156c2b8f7f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:16:09 +0000

    arrangement there second custom report

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def get_array_as_set(array):
-        return set(array)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
