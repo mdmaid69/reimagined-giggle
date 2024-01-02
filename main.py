754e1d1fed80051cded5c231374f96ec8c62752e
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:03:14 +0000

    themselves trip relax innocent supply

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def reverse_array(array):
-        array.reverse()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
