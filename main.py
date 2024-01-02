238ab26f2ded978910a77ff06085ba0acb17d5c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:11:49 +0000

    impressive sign live switch to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import os
+print(os.getcwd())
+import array
+def set_array_item(array, i, item):
+        array[i] = item
