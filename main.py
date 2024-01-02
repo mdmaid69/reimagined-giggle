8a7d90ee5e9d7becebfc72801133ee26ade440a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:34:56 +0000

    ticket practical record form affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
