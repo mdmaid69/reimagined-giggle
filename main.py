fd0879b222d81ad71b1e7faba732f575cdc717a6
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:49:31 +0000

    so wet dish handle letter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
