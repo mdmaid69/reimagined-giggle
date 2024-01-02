b4e63219d5f320d52eb2fc2fddd677ecee61bb15
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:15:37 +0000

    agree wash number sky performance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
