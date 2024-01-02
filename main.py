86c69848033c873827033ee87584cfac642aa43f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:21:17 +0000

    hit slip every to personal

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print([x**2 for x in range(10)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
