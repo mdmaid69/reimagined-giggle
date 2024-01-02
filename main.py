3f947158da28dd4bceb315780ba1156d3d2286ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:56:25 +0000

    conduct identify majority team unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bool(array):
+        return bool(array)
