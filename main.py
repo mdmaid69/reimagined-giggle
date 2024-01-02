cc2c8cd3f764c084946c5dca57c5f9bf8fe25399
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:53:16 +0000

    cum cry blue swimming Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
