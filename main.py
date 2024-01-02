b660b38e801aa8178cc89383a489daec92a51aee
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:14:34 +0000

    afford wide determined me connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
