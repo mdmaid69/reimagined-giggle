55b53649c4f90ac39ed09e88383872b97a404f27
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:45:08 +0000

    dress impossible always Oo report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
 import glob
 def find_files(pattern):
         return glob.glob(pattern)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
