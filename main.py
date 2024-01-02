d9c0d4296a005283b5697385d98b4e061fe56fe9
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:55:52 +0000

    warning recognize first something comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
