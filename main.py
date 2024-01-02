4ec4d9eb9f92350f24d0f91c8f218e0a584167ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:44:44 +0000

    phone chance pinch take sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
