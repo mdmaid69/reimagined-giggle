139f67de83badb17edc8101af19a4bad2b355509
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:48:14 +0000

    burn stare Ee official lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import os
+def change_working_directory(path):
+        os.chdir(path)
