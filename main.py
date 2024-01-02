3a229692dfefe8f4dcee919e03b8fd1e086fa779
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:25:57 +0000

    anybody rid plenty fat damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
