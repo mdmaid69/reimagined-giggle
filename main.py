cd38f07fe8e1dd5d2336e3968a7bd140e5395667
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:49:52 +0000

    art personal nonsense point badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
