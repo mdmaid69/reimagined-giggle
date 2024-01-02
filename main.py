55a65d02bd7021a5ad80e6850b4b2fc184ec47a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:09:29 +0000

    somewhere disappointed hurry what opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
