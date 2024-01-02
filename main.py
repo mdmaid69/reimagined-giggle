80d2079ec81af6d72ec77b0fc6e0748f70ff0f22
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:40:53 +0000

    moon shut opposite shout discover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
