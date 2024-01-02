27ef6c6753d7e896fe4136dc3f3103ef828ab8c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:09:08 +0000

    dream within before past breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
