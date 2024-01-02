c2bc01e88d8b16412da8ea838ac5e075e33a6260
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:25:05 +0000

    Ss arrangement related circle switch

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
