8e886886dc6c2e06bd6e0b2a94115718a8f32211
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:11:05 +0000

    sale shoot join violence strike

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
