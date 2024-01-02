b0f58e288580de177f13410299e83ed1d780b0a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:27:08 +0000

    live thankyou size record anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import time
+print(time.time())
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
