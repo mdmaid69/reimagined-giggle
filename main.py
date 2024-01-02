4f3090b378e9ba0c1b197b9d1488b9819d15aaf7
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:17:00 +0000

    occasion past thankyou average keepaway

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
