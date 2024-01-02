a17da65ba793ec40d928796d8faff82961a8456a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:31:34 +0000

    mess sink room to behaviour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_sine(x):
+        return math.sin(x)
