6997963b01198deee56da3a6b5fd08cfdc27f271
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:27:16 +0000

    either speed possible show read

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
