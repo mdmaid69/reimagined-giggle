5bc4ae41105bf4dcba7886a465ba7d897d6b4daf
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:55:11 +0000

    they progress wind quit command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
