325cc6b796acc9e4e122f2a32f34b16f5b20c008
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:17:49 +0000

    like picture suddenly climb fierce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
