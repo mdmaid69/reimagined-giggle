5eeebc4e89179bd51e3302d6a202fd3044e25ab4
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:49:51 +0000

    replace hear considering sleep value

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
