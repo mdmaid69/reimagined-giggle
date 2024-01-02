3ad565a3d882791b848e70a2ff391179ad8b4fd7
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:23:11 +0000

    shop adjective sound tall wash

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_work(force, distance):
+        return force * distance
