a3fc1e72cee06281686f869692d055621473c5cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:13:00 +0000

    refer course anywhere separate mysterious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
