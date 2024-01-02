09282f80573c4d1734444399eaf0610e323a4a16
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:19:51 +0000

    fine mysterious cheek completely cinema

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
