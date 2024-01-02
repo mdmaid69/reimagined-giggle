f54024fb617f2b626296d3cd691705c579803b7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:18:55 +0000

    education introduction alone someone public

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
