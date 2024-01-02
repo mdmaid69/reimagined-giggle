574b6f37a6f9936c2ff58c5cedc42f8989a506bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:27:48 +0000

    hour peaceful circumstance whom previous

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
