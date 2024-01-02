ba3b8b9901632c4da0dd3fb82583394575d75636
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:44:06 +0000

    569enjoy attempt bury both opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
