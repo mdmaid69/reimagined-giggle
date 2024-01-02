cb1a62bef73c1bf2a68b0669621522df724bda4d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:13:07 +0000

    another workout return face drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   def cube_number(x):
         return x**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
