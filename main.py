a21447996b0a4af54f3b48315dbb3cdea2700570
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:11:48 +0000

    right completely breakout circle narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
