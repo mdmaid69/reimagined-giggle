7ab1f11a9a046c1195f050984306df626056c533
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:16:40 +0000

    activity tense moon professional tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
