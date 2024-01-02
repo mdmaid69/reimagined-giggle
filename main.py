4c54e1ffce9fa4dce18913c50df11daf42933a17
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:36:47 +0000

    basically takeaway swim necessary problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
