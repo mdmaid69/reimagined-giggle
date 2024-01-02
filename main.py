89f104e1a5ebab69d22d24c1ac70adc975e68181
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:01:55 +0000

    wipe excited then considering talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
