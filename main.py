8360993cc6fc6f71362b7b9e8fedcf07848e5526
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:58:40 +0000

    write platform will remember show

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
