91cb0a7c6d60b9f80b79a34cae4d5ee4c9483b53
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:41:35 +0000

    tap bright turnup group pupil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import array
-def get_array_as_list(array):
-        return list(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
