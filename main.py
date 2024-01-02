22f315844206397293f19b0a400f4a05ff30bf7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:17:08 +0000

    generous circle die advantage nearly

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
