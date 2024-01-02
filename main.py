e09be90875ce39963ddb50b6dde498fee93e66c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:35:41 +0000

    considering ago match maybe regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
