e65a0277dccb9bff952954ba58899baa8c556f82
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:39:56 +0000

    characteristic skirt correct somehow Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
