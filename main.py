b2e70a0313cd85bcdb225dc181b4943f999cdea4
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:32:30 +0000

    dress touch month sentence post

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def count_elements(lst):
+        return len(lst)
+  def count_elements(lst):
+        return len(lst)
