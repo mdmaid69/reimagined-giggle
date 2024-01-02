494e6977251b4091aa392b949e6cfdb6432143cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:12:21 +0000

    black next calloff wash replace

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-for i in range(5):
-        print(i)
+def square_number(x):
+        return x**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
