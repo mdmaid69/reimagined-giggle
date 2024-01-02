e8042eef855af88246a2bd3a166fd4089c438ddf
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:42:30 +0000

    blame sport emotion break brother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
