8d343b781440aed3069b62a199a70acff1438201
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:11:33 +0000

    value petrol difficulty knock grow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
