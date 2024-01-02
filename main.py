f96c0a7a057e4ddbc7e5cb575d66b13d24aceb02
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:27:19 +0000

    confidence fall news blue tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
