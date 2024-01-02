855e55079e710dbffc23edff1d835424026cf69e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:01:00 +0000

    about healthy free bye tip

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
