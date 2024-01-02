45cc34a42332003bb08541c6598dbcbec8c8b297
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:08 +0000

    sense everywhere better event cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
