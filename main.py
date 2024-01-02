3526750ac6c8b544a803d73534b760246782f275
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:39:55 +0000

    attention temperature shelter squash march

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def add_numbers(x, y):
-        return x + y
+def get_array_as_bool(array):
+        return bool(array)
