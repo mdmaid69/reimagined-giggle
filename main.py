4d4c0cb4a2d254394dde037d79adabcf3ae44add
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:27:08 +0000

    pupil not perfect about along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_user_string():
+        return collections.UserString()
