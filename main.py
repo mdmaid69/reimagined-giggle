7f0797f51efa95883e53ed186244795ad6d830ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:20:24 +0000

    equipment entrance edge considering vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
