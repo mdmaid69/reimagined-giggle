37a7262d90d8d0bb46ffc1d0dcd28b53a03c67f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:11:27 +0000

    private organization spend swimming meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
