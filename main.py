41a48b805b63c6d2d62dfcd5dc2ae99c4aae797c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:05:59 +0000

    welcome children suit congratulations instead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
