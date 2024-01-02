38b3dd81bad2f4a2959b9d5491f7a51db5140472
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:41:10 +0000

    lunch science transport teach rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
