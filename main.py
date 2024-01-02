3c950de7b2713afa1eadfaa7d7bbd831547a2d08
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:43:40 +0000

    successful apparently bar tough ring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
