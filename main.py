ca468b05562ca75d465772f3a0f3e2784ff8a7d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:48:43 +0000

    friend beat borrow share neither

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
