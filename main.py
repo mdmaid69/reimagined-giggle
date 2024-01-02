2f25746017c2f4fa4d696afdaadbc66c9dc0a5e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:54 +0000

    gather rubbish together Hh quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_even(n):
+        return n % 2 == 0
