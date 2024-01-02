60a205798c5249b76fc1182acf621e83fff9f813
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:08:09 +0000

    maximum finger pleasure cross press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
