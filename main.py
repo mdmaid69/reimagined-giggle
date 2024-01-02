890492665349151354951a2e3d2a84e95845df1d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:12:07 +0000

    neither act anywayanyhow before do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_int(array):
+        return int(array[0])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
