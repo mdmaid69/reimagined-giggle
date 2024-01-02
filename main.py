305a5efa39539adc5c1d1c3a27b7d764f7048b64
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:07:13 +0000

    friendly profit emphasize stretch rob

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
