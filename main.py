46d33db2c1378dfea7a3b0479ba77194e4f9d60e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:56:06 +0000

    announce good gas replace copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def square_number(x):
+        return x**2
+import random
+def roll_die():
+        return random.randint(1, 6)
