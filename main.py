452d45b936a4b99de9d49eb4fa46caeb4aede4e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:10:47 +0000

    judge very blue heavy post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
