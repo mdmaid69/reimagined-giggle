f7180a90e63ca271ca1e4c6c58672417e28173ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:01:19 +0000

    deep trust afternoon above bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
