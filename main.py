19fbd1ae3522a21eae85bcf8608dbcb0eb7e6710
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:09:07 +0000

    contents season emergency worry shoulder

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import tensorflow as tf
-print(tf.__version__)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
