483f5e72d72f9f6adf535faa15e9779054588bf1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:20:43 +0000

    comeoff share might clothes partner

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
