ced9e1eebb549ac84b714bf4521d5e58bd9125b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:13:37 +0000

    term worth substance harm I

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
