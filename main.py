c12eb78c9e486bae457c3e4440eb17bf4cba117f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:38:26 +0000

    object Pp below plenty main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
