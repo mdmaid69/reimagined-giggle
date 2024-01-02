45933c293a33c5c001b0ec16c9f3d155e74c3b91
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:30:19 +0000

    judge rent weak trap contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
