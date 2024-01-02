44367535011c502af5195fab272418a8dec43d76
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:21:35 +0000

    must press frighten solve yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
