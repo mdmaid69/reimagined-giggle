135789bbfce0565477399e57e33e85f152d0e9d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:29:51 +0000

    noisy big argument manner hear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def subtract_numbers(x, y):
-        return x - y
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_power(work, time):
+        return work / time
