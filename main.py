33dbb1559c61132716c3fd27b54b4a0bc052fb3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:43:27 +0000

    press none lie surprise name

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_logarithm_base_e(x):
+        return math.log(x)
