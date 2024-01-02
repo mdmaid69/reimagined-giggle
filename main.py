a7746ccb12bc7dd88ae70780cfaf34d51a50a980
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:54:57 +0000

    range divide ground bad press

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import os
-print(os.getcwd())
+def get_array_as_memoryview(array):
+        return memoryview(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
