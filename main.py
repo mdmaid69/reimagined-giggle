c57052d4ce6aa6e214c664fa75d6d36b2e695b53
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:15:36 +0000

    sting doubt anybody year threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_item(array, i):
+        return array[i]
