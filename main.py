18af2002d7d3793f72182b0cdff2e3d36dfb3957
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:24:41 +0000

    book cinema female eager breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def add_numbers(a, b):
-        return a + b
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_length(array):
+        return len(array)
