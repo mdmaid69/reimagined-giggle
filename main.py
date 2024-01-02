a46a8739a42761eb1caad653f97e32bc4088fdb4
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:01:59 +0000

    worry tune succeed people unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import math
 def calculate_square_root(x):
         return math.sqrt(x)
