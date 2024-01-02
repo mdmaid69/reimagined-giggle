3113f7ed1968dc6a3c41a36c050624af781b73ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:19:19 +0000

    immediately offence dear teach behaviour

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
