dc1216ddf5dac2b10f31984a8f8317b5ffb34a2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:05:24 +0000

    picture fall description sink note

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
