dba772c48073bbaf229841d18400bd94aa8c8e9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:46:25 +0000

    regional bright rain under trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
