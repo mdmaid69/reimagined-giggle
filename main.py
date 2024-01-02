1568b82a68a013d3c0d60cacd64d5619d5afc575
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:31:13 +0000

    doubt summer office intend translate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
