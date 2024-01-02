67d3a48174f228ffa31fb7893d1c2a044ab374c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:40:04 +0000

    Ff content closed Ll reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
