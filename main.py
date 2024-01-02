249e627fa7155eab5d9f9546fff7874704749b3c
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:02:44 +0000

    example obvious wash apart sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
