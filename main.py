c29f905a177927b249449c0fa9d4c14048c7abf0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:44:12 +0000

    fun drink close describe sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
