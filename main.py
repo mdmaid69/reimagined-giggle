f54685e61c4471f6c63f8628ea1013448968ea3d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:30:05 +0000

    road concern special window operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
