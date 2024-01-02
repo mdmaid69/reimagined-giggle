5a9660d0e917141032620b9f3c8056ebe3230de9
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:12:41 +0000

    turnout fair support fast identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
