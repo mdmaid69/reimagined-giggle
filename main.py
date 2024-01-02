c2c148aa4a209dbf92e5ec081f68ef593d0f335a
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:56:50 +0000

    walk knock confident fear play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+print(os.getcwd())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
