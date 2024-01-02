ba3cec5a0742a3588a18c2fa442a1147f3e535c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:31:05 +0000

    somehow calmly unfair waste communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
