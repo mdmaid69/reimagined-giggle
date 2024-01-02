85676eece0f0eb6e93531f64a9dc96d7c6b9d9da
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:25:11 +0000

    deliberate whatever agree bet spot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
