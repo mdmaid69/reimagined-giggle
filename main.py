af0ec67e5a2725383dfdfc8fd6ed579cd4b85dec
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:14:08 +0000

    what do wide land later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-for i in range(5):
-        print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
