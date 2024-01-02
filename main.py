c0ca8c74aca279d8dfdb4ec1c559ded2cea56581
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:48:55 +0000

    minute march shopping comeback clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
