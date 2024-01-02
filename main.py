81ad40e4187465bab23dacaf0708fef4c16f45ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:52:59 +0000

    towards board hand lonely enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
