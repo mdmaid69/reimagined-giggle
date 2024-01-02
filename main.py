248622ebe6c03c0a50df86a8b0de9ede8a077a4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:05:06 +0000

    round cost nearly Ff top

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
