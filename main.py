3aa40dcea8d9fbbf80335a2a87221db8f6c085ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:08:15 +0000

    guess along swing wise shut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-def log_message(message):
-        logging.info(message)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
