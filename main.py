62bc3dcd2f85ebe5e78ff2a78d47f6d3b5c77481
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:44 +0000

    broad behind deal question develop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
