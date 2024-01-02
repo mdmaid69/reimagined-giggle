a89e61f8c1eb95c9884260305e1597adf7fda4da
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:45:55 +0000

    become fight estimate hard dance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
