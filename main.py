2b7f953ba7a5104824a54ea127166fb7306022ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:01:57 +0000

    profit demand proof Kk touch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_area_rectangle(l, w):
+        return l * w
