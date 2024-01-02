27fd53c23fd8e38aa83d419eca375371fd03b24b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:00:31 +0000

    pocket punishment close shock suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def convert_to_binary(n):
+        return bin(n)
