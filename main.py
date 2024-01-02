51eb4430155cee83c3f79cde4ebfd10748ebf5ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:00:18 +0000

    free its especially row goout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+print(sys.version)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
