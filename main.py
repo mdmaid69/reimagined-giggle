bc543758a00d3626f4a44fd1f866ff130bc164a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:17:58 +0000

    they pride dance distance smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
