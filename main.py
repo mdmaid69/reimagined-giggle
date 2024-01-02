d621c945a2f815719db918888758077d9fa86671
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:58:45 +0000

    mystery ride anybody survive thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_list_from_array(array):
+        return array.tolist()
