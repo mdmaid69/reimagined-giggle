f1165a5256e173463bb90bbcd819db03b236c082
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:40:56 +0000

    far freeze late mend do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
