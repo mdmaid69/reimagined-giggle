2e8303071dfc55ac6bf6b0dbc4643ad2bf5e3d08
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:41:47 +0000

    term special shop begin height

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def convert_to_hex(n):
-        return hex(n)
+import random
+print(random.randint(0, 100))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
