718619ab16eaefc04518f4318192b6d10004d6fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:49:49 +0000

    obtain cut note effort nose

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
