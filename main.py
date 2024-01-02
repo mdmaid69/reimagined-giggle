d6c12d6737dc569755ba04f1e81f76fba859bd5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:43:47 +0000

    certainly tidy various getout education

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def convert_to_binary(n):
+        return bin(n)
