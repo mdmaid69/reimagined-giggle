6a3b03cc8baa9dff0bd736d37db4d64acbe71537
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:59:24 +0000

    weekend mend straight ignore extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
