2a4da85d0148ffd47e5e50386acb51fec17bd37c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:30:05 +0000

    seriously truth trip legal consist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
