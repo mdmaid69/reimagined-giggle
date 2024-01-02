3ee08d33771df23a93067c39c558f429304aa8a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:39:12 +0000

    brother list comeout wide praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
