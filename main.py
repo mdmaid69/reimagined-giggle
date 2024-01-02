dc2cacddf26b2b4671639c97fec52bddecec04ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:52:04 +0000

    pause nerve failure clean recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
