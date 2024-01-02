6020b7b2291f5b4d6f7f322a3327a4c49d4525d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:07:16 +0000

    guilty pay grateful smell health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        return sys.version
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
