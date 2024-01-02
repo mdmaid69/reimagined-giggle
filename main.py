e8afeb65afab5d22db18008fc2eaaca00370fff6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:16:20 +0000

    calloff holdon late pay independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
