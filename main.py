fe65ff3bc2c2a78cf6ae978bae139688c51e7c80
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:38:08 +0000

    might personal respect proud exploration

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
