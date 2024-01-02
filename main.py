b66f875f164693cde96496b7f090dfb3c68728c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:38 +0000

    ought takeaway keepaway protect embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
