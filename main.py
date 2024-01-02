830ed6adde5f00ccfdd1befc3546f24ceb30ab16
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:46:14 +0000

    owe steep impatient involve meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_time_since_epoch():
+        return time.time()
