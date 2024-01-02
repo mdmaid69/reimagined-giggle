e46ec8163c445b8a8ac2165cc285794769964dfc
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:37:49 +0000

    present test match confusing hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
+def convert_to_binary(n):
         return bin(n)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
