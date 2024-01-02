98224396dcc0e0005d5be00113cdc89af9b446be
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:38:33 +0000

    form advantage mixture average official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
