d59e9d112d3fc4025e7bc799d5e4cc3b2b9e9cb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:15:12 +0000

    principle boat invitation stiff beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
