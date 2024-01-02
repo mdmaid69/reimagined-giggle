8113a7639b7f7d006e248f31596de7d19874d5cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:53:39 +0000

    comeround appeal mend work cope

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
