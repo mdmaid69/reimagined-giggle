f787802531bc48074170083af1b60acfc8659361
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:40:38 +0000

    getoff Rr getoff earn grand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_power(work, time):
-        return work / time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
