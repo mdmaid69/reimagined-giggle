48d83793199a690191ed82a8224d9d58c8b1b914
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:58:32 +0000

    holdon vanish support disturb now

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
