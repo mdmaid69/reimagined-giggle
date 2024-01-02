e304d75ac7e6a1e82c901969ce8b2583935343c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:20:08 +0000

    judge joke keep violent string

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def is_odd(n):
+        return n % 2 != 0
+print(sum(range(10)))
