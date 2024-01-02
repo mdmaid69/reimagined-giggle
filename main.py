c5febca2c5a74c2f44e598c8f6d1f30b19ddc496
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:40:12 +0000

    whether judge murder discovery modern

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
