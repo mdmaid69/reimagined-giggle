53cf8ff4d483084367a98b926bef27098dfa9006
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:22:13 +0000

    sort contact during sink curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
