fe6832fdd1943b89ce4e2237c6d99d6bb9a7a9c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:19:29 +0000

    belong your high bus train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
