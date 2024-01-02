e744609dde967e2b7b98799d18912052609ca80a
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:54:12 +0000

    definite connect usual unfair congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
