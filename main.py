7ff3306d8711f5f9b21ac88f9e9367de7e9232cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:04:28 +0000

    bury patient meanwhile deliver shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
