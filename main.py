84a060c84c15e6a7f988493ab90a3f2d614e047e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:26:47 +0000

    card action pair ill classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
