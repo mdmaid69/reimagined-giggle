326b3fdf69ab6ac71dbda66810abc5a506ff0598
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:33:33 +0000

    to feature float name catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
