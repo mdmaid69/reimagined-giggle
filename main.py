210f852e8352f50be630f53c9ca3fabe375af8b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:03:09 +0000

    trousers protect in open bet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
