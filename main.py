a31276aa8be8552434177024e2cbbc2a379ff73e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:47:48 +0000

    OKorokay pour team thorough offer

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
