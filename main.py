2bcbb1f5065e3c96a8686f652b1d3eb148f5e34b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:10:57 +0000

    star forward back more process

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_area_circle(r):
+        return 3.14 * r**2
