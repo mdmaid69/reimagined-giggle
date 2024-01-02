710bf949d11e2a2f6e2db4fc206f4913c97a4087
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:24:39 +0000

    force today exchange although praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def find_min(lst):
-        return min(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
