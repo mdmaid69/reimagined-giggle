5d43df4f97b9149ccf17b79b0e824e96dad2ea87
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:15:26 +0000

    feel false boil sure hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
