8df7fb0c62bea68d5661f072a19e09e012561e1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:16:20 +0000

    land secret certainly consider question

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+x = 10
+y = 20
+print("Sum:", x + y)
+name = "Python"
+print("Hello,", name)
