99b1198621e1844ec3c3d5c7e719d01feceaf278
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:29:33 +0000

    project harm share constantly after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
