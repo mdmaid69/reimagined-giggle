7f0ae61be47d6213c9ae01bde4047c5a28861f42
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:39:44 +0000

    thought blow direct discovery previous

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
