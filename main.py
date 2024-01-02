5bf00e9d79a16a2677c96b363630bc7bdcf1d0ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:23:35 +0000

    fine minority just pleasant also

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
