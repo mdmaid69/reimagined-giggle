084eac0c238538c28fadbb9d388c54fb786907f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:14:16 +0000

    score rise opportunity post public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
