a822a0ecbd09539e2e7174e585dc76df11602072
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:12:06 +0000

    anxious dry yes sound congratulate

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def sort_list(lst):
-        return sorted(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
