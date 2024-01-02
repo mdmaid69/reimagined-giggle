9189160fdb06d5a0dbf6afbca05e5c2434b96428
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:20:39 +0000

    makeup case importance exact 564encourage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
