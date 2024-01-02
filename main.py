4b6914947fa8fb839cfd133fe4b1d87d3d58781a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:24:49 +0000

    charge building open nor promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
