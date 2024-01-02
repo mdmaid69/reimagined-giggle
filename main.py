44ac57b66e582452b7189a90b0441f947eb5c332
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:05:32 +0000

    custom share care pleasant after

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
