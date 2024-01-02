a9b456053b0af1a94737bc1ec52c1947f1ab27b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:42:23 +0000

    goout burn broken fire steal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-print(sys.version)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
