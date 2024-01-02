6c60dae8f0261f485dfab6729703c0e65ed19cfd
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:21:52 +0000

    occasional pretty either mouth blow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import collections
+def create_stack():
+        return collections.deque()
