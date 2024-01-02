fe75e083f9973f43a1bf1ed3793126baf3457659
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:32:08 +0000

    swimming foot duty war Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
