11079a2f2c2ebb11eec550dad2b961eb67546444
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:22:29 +0000

    terrible your appeal wealth clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
