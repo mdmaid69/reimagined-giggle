ec2ec580eb46962c182586105d0631d9de591567
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:31:57 +0000

    exhibition appeal bury best great

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def calculate_area_rectangle(l, w):
+        return l * w
