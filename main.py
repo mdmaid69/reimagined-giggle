13c9baa142ce6d980b0fbeb2323f77778b35d12d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:29:14 +0000

    invitation any would commit last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
