9c1fe3b100cb417eeec4c0f55aa4fe495cdef79d
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:49:57 +0000

    through stop pretty check flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_repr(array):
+        return repr(array)
