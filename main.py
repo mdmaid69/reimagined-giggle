2c59e345ba8f2a04b341496141b9e2f5b475eb84
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:14:44 +0000

    exercise sense song game keepoff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def clear_array(array):
+        array *= 0
+def calculate_acceleration(speed, time):
+        return speed / time
