165f60b8a704baa41b98e470232e947c1a9f6662
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:44:55 +0000

    upset wide error should view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_sine(x):
+        return math.sin(x)
