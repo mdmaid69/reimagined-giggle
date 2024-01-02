2e5396134303ad7ddbdf4fb692f8b1f978ce577f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:11:49 +0000

    send my behind spill fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def convert_to_octal(n):
+        return oct(n)
