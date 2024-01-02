eccec3e93bb49c25e71eb96dd0ee205a657455af
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:37:02 +0000

    anybody official expression best deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_blocks_allocated(file_name):
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
