763ce2879847b17988bc82c11b01720264deb967
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:58:11 +0000

    attach relief minimum hope share

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
