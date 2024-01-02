72ce6de6d54b9df4cf7a502d433c6bc4d2313b28
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:28:09 +0000

    rush border hide Ww lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
