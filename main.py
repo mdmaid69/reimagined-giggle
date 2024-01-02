d1d8f4f773b85563eebbe3b75c10b1c34eac0d86
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:51:44 +0000

    task safe defend anxious extremely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Characters:", len(text))
