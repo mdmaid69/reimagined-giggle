59e39ccc668f01a4f354ea6b95ec42f9efdcdc8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:04:54 +0000

    birthday damp sail proof sale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
