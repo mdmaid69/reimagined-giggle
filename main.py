4b75348b82c5b81db901873de78698f858fd74e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:22:15 +0000

    mine simple juice next prove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import time
+def get_current_time():
+        return time.time()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
