f180728a9269dfe81e6e645c3f2122f116b7b713
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:44:33 +0000

    tough expect stamp whom card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
