ddfbab94d900fd6de0fc82b3dad6a598f21860f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:17:51 +0000

    computer suppose past freedom join

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
