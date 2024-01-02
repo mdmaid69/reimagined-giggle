fa6e429eff5a55dc962c0769bbeb2ec45692cf4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:34:59 +0000

    complete call ocean me private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
