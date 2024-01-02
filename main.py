9351c03b4df24d33596e806c1b9a717c31bf6bc1
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:27:18 +0000

    disagree not city beginning contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-print(os.getcwd())
+def calculate_pressure(force, area):
+        return force / area
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
