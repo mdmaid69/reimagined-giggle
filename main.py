54b6139bdccc23616303618e8dbc01497349eb19
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:31:49 +0000

    scrap nearly way surprised promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
