782f7d880bed9e8b55d2c1070b78b1b8e3473f02
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:48:48 +0000

    book rest unhappy 565end quantity

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
