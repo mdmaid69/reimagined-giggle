f24ba27d9f2ae0a3eda2133995b904e85fd7ddc0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:31 +0000

    influence cry clean second attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
