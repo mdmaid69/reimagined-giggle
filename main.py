c825707ae1f5b4cc392f965abf5cfd8a34af8be2
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:33:41 +0000

    Dd kind regret comedown quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
