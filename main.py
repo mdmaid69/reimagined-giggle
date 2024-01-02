1d3643c32039c4b236ac462014db3b4956a02978
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:38:01 +0000

    block leg travel square copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import platform
+def get_python_version():
+        return platform.python_version()
