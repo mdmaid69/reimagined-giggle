7c2633f3ed4ac25e5c7c63bd9844fe4687577b57
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:20:06 +0000

    treat there hurry main character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
