d64098816aa630869cb42e62528fb636994e7fbd
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:01:30 +0000

    spare estimate moment soil abroad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
