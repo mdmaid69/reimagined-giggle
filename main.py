f4c656fa6532009a233a85bf918b7b0c8faeb3b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:24:49 +0000

    bring war agreement sport mix

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
