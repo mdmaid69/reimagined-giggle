d0ec8b623546243cdae19435d952602737c4f936
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:33:54 +0000

    remove private key different getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
