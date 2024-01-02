f616d86cde031e91ee9ad89fa55e6e4c5f7d37c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:32:19 +0000

    pretend high interview dull identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def count_elements(lst):
+        return len(lst)
