7405347db8d09117443bbef4a74bcb26e0a112fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:04:06 +0000

    family nature object advise burn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_repr(array):
+        return repr(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
