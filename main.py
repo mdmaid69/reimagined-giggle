029cca57e12a7d33cbfdb6cb486b1e146d261b23
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:15:25 +0000

    although none show warning pupil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_pressure(force, area):
+        return force / area
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
