3232e90a48883d183cbc43480aa71dea10666b86
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:01:11 +0000

    turnback fix goup scream wave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
