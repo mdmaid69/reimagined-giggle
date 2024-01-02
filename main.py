18d2973ad60c86e5d0fef23f9576c9bae6e282d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:16:24 +0000

    suit theirs hour drawing equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
