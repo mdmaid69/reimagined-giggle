b5081ee5cd792c43a4759ac30014dbfc7f3880ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:56 +0000

    edge tone main soon needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
