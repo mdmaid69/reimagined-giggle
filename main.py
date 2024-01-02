de2c097cc233829f5fb14ff2e8700a8a64275925
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:05:20 +0000

    stranger wealth rid language declare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import sys
+def print_python_version():
+        return sys.version
