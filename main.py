55f1d91cb50feb92d6e7cc575a71250d7481cf53
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:08:55 +0000

    platform base body divide glad

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
