ab4204abe8acc0f96ee9685b93c7418ce453fbde
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:02:53 +0000

    ride about race chase pencil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def split_path(path):
+        return os.path.split(path)
