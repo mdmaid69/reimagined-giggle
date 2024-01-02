9ee70f058750fe53f9008a16c6d42bef45496bc1
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:39:55 +0000

    regularly name reply admit deaf

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
