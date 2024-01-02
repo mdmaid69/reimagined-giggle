14b8ce899d4ce1114218f685b703d12998fc0672
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:11:11 +0000

    doctor sing ask head design

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
