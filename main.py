8af34c3f066840a6a965d82aac635d9179dd4d07
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:35:39 +0000

    die ticket land available average

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
