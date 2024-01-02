73629c090d73e30a8be1722adcf5ca195f04c7cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:44:22 +0000

    begin recent past introduce future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_working_directory():
+        return os.getcwd()
