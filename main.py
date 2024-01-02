8c19f0a1febbdc81345e136e231ad270d431aeb4
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:37:04 +0000

    equal catch lead ever terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
