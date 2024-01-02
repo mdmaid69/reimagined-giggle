67e605899b347865e2d7c0912bf8a5583fb7b6f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:38:41 +0000

    advanced breakup sorry worth public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
