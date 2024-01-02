73c88264fd971850e38d6c33815535e6cfabad9d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:11:12 +0000

    blind tradition run example which

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
