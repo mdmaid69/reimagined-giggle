d9e87f61733f70b8ebf989df394cabd2574df246
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:07:19 +0000

    storm substance intend weekend pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
