3312c8ff5cf939d81b1cd867ad8fafa1fa169f98
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:36:33 +0000

    board report curious generous upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
