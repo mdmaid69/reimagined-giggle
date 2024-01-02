69746d0aaaac65e9418eae590a45d43616b56e22
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:59:49 +0000

    train roll piece official bang

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
