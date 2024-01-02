29cfcce000e640dd7b2d5ca0d2280c278faba95f
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:09:17 +0000

    be copy how hole fence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_length(array):
-        return len(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
