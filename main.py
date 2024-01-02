e396108daf2b6e3acbbebeb5a690a1be5143e496
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:13:14 +0000

    some science land sound say

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+def change_working_directory(path):
+        os.chdir(path)
