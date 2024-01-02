0d796437c9815e61f7d6e1631883d374b9b4af2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:56:11 +0000

    everybody sign pair scrape defeat

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
