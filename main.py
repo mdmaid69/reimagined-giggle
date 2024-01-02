0064745fe004f43babde35e601bae3d394e901a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:21:09 +0000

    call wide over completely study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
