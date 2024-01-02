fbb5ff74490a97f65f547ca666db0adec664c3ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:13:47 +0000

    home makeinto model calloff pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
