ee6c9eb03ca4d3bc73394a333e13bc27f136c24b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:35:59 +0000

    computer mark proof house now

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
