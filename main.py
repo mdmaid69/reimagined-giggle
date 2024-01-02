bbd32a9ab6bf924fb97c478b045dddb7993e7845
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:25:29 +0000

    probably lonely and heat fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
