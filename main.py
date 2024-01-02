c676dd5619d1d243b0693bdd6c73e9695330dc11
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:37:30 +0000

    tape expensive manner telephone lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
