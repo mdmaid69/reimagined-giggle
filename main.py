a071b391ca781b6b23f41b203ecf33409497e563
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:52:28 +0000

    turnup measure sting centre doctor

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def convert_to_binary(n):
+        return bin(n)
