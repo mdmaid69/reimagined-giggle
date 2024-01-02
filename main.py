7f620d4d9deb2bd7020c30fbce12edac6e16194f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:59:32 +0000

    say understand late judge smoke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
