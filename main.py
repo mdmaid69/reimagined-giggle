beb580128e2d69f38bbca6735625ac106c1b355a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:37:36 +0000

    house polite react water seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
+import getpass
+def get_username():
+        return getpass.getuser()
