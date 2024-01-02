e6e706bb603166a4b32fc42798abf1b632279d86
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:38:26 +0000

    good spray public upwards large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def convert_to_json(data):
+        return json.dumps(data)
