f2ac50256c71c7890a2f48960aea44bc7cbaf1b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:32:16 +0000

    owe storm afraid spot order

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def cube_number(x):
-        return x**3
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
