b6ca8254432e591a0394f95349dfe3b4b258ef47
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:56:41 +0000

    produce sense pupil money half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
