1410d1cc458f6a118cc7ff96b1b4cf82e60d8908
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:49:53 +0000

    sight really freeze healthy breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
