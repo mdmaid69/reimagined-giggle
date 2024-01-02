29599bc4a63da05c044eecc81c8f46b664155aeb
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:41:30 +0000

    building after smart consider wheel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def sort_list(lst):
+        return sorted(lst)
