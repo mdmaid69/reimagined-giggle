be7237f9a72707cda4610f5773891c657670e7a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:43:05 +0000

    basis keepup bin suit visit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
