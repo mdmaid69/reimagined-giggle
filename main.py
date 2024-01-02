d7ee41f4e1e751fa0bc7162c94aee5accdbbd63b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:08:47 +0000

    brain plate rare listenfor guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
