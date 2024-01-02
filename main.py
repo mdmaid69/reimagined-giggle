d4b02f9625ab7216fbaec84000227e64d116fb0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:13:37 +0000

    basic opinion stone mess second

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import time
-print(time.time())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
