a3f9d6c8ccc27791aa768952d75f376607c60183
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:27:52 +0000

    greeting file operate sport appear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
