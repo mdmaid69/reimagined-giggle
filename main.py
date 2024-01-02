33872207a282475c9a31384edbb3df049f574ef5
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:18:17 +0000

    somehow able honest anywhere train

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_speed(distance, time):
-        return distance / time
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
