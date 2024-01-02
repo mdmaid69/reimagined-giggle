9fdceef17af907936e22debfee5cdddc41659703
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:13:48 +0000

    go serious badly count little

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
