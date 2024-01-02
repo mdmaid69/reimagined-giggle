52e73c8c19528a0e1f8cac323ba1100012caffe2
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:03:50 +0000

    science by walk dig city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
