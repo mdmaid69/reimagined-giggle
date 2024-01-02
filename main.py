f37c2044a613ff66d56ba432c14b1562e817358c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:58:07 +0000

    peace region confusing flight secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
