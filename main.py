1ea82fa0118fdb586ef8f201d4a8f7468b968125
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:56:05 +0000

    believe bottom turnaway hungry feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
