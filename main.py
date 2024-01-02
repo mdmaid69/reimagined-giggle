58af06bc7bbc0b94cc0a3d2427952ae4f43cc187
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:08:00 +0000

    sink neither change Zz spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
