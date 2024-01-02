7aef87fd2cb183e2bb49e0a4b6f7b15e2da972e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:39:04 +0000

    neat whoever crowd crime arrangement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
