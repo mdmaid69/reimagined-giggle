aa8a1e04175126a866bb32eb0718d8f3ac3b7d03
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:49 +0000

    under control sweet lie spot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_str(array):
+        return str(array)
+  def is_odd(n):
+        return n % 2 != 0
