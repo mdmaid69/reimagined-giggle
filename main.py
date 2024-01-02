e52be2e871a1b493419da2a111bd27bb0588e6db
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:26:30 +0000

    brother birthday different term test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def is_even(n):
+        return n % 2 == 0
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
