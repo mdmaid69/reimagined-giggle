6c9edcb2f5f59f0c2cc5694c39fe5fc91c30e885
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:24:27 +0000

    bite create side complete famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
