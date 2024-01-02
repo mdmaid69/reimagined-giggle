c63705f90c2a9d8aa44e4744262d7298f6d1c14e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:31:05 +0000

    regular draw ourselves tip excellent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import time
+def get_current_time():
+        return time.time()
