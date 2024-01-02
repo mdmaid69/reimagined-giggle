5a61f91f67d68cbb6a3aab55a08ccbc72d553ffb
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:20:16 +0000

    record offer write squeeze hollow

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
