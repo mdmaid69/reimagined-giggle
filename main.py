5163b57899207f0cd739fd57df6712bf92544819
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:49:29 +0000

    building date order later person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_set(array):
-        return set(array)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def find_min(numbers):
+        return min(numbers)
