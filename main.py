f23f921e7fb667b59a3ea09dffc7373a78b9e1c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:36:34 +0000

    sad calloff Jj lonely to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
