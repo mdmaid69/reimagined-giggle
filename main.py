d5bc317ec74f69e8f0d9a2790f2f917a47afa06e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:31:33 +0000

    pronounce base crazy mystery fortunately

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_length(array):
+        return len(array)
