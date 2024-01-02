f2dc8d1fc826aa1aeb41fdbfb636bde53728e374
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:30:44 +0000

    project weak emergency someone connected

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_density(mass, volume):
-        return mass / volume
+def find_min(numbers):
+        return min(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
