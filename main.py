f86c24d75d7b6be0a0d9e10becd465ca0a119c96
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:12 +0000

    hardly their bye turnout goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
