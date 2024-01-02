254828eb4c295873368265b01962f50ff5a3c62b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:06:54 +0000

    even exaggerate effect produce decide

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
