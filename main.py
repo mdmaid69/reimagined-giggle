95a2a94e08d122fbdb4954566fcb927abf0bdcdf
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:30:05 +0000

    style will there interruption anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
