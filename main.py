b6aebf0d967037c4ab4f8d2650e21a100985e102
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:33:12 +0000

    road breakfast smart congratulate destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
