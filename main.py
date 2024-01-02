3d6cfb7ccbcedd26f8e848195ab18744c6f007aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:03 +0000

    bang wild whisper earth shout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def subtract_numbers(x, y):
+        return x - y
