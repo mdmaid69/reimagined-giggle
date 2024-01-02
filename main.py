368412d1e31a565ea37df7bdc10a5ed2ff4a7aba
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:51:58 +0000

    brush border wrap frame fix

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
