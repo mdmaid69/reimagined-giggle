33dfa44f3ab23b946e498d870deaa126e987ca0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:31:26 +0000

    culture scheme sort hope lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
