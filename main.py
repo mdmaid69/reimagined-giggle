8487dccedc2b30b5ba5fa97ec690aff4ac0b5d90
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:40:04 +0000

    quality country here dare match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
