912d5b30bfce792a1b5e0182d18d7a90aec64c66
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:18:59 +0000

    brush repeat warning knock far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
