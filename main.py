85deae38ec78d3290fc84549e59c9688f4ede411
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:02:01 +0000

    regard watch condition moment combine

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import collections
+def create_user_string():
+        return collections.UserString()
