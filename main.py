60a94f13157d380efbe1340baa801894a6b6e1e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:44:05 +0000

    ground exactly punishment patience before

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
