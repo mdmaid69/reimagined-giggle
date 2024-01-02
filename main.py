a596f6cdf983b2a4cfbc51d6579f35d2c08c4706
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:06:29 +0000

    far pick figure space absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
