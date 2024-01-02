26af276b8cba7ba91416fac70647392cdd6ebc42
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:02:23 +0000

    brush ought exchange custom university

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sys
+  def get_python_version():
+        return sys.version
