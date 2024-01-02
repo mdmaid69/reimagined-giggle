7d2713fc19ab15b8b0654203124fe35eeec27ea4
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:59:10 +0000

    suck objection since move appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
