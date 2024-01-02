602f7e480e64e29df7ba11ea1996e4a616df981b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:40:27 +0000

    important slice birthday serve weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
