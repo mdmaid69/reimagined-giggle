aa45d723559e508d2557540268257e83ecaabca8
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:17:46 +0000

    only by tooth transparent frighten

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
