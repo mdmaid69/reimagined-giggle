e241b741d952df28ca9106801f3c1959469fc1fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:52:52 +0000

    deserve suddenly justice getout against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
