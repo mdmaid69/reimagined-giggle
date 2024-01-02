2c2587e0e67ca13b62ae1e4698ce5a5fad81bf06
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:11:06 +0000

    disappear exit morning manage whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
