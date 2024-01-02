6a10b56249841aa470db9b59c25cccd317fb7412
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:08:23 +0000

    film environment among question correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def divide_numbers(x, y):
-        return x / y
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
