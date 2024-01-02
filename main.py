e404e410942b82dc868a30405e42153c4ebab908
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:21:22 +0000

    myself valuable at exam improve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def count_words(sentence):
-        return len(sentence.split())
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
