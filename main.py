489e25409cf3a170236e51d8134e34400c6d9c68
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:26:07 +0000

    and related seat load hurry

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
