5faa86190bd3eb11d04aae73b120d56d527087d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:20:05 +0000

    include friend slowly sky child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
