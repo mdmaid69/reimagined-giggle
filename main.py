bbcec2c8b9fa08ac327f12218a75ec63a3ba9ce1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:44:09 +0000

    open main equal believe admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
 import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
