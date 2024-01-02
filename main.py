fe2b5fad358cb7d9208fb553775e58652f08a089
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:06:50 +0000

    kick brain post love hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_pressure(force, area):
+        return force / area
+import platform
+def get_os_info():
+        return platform.uname()
