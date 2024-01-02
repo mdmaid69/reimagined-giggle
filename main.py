1edd4a71e7428e5f2981b8aec046eb910230b465
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:41:46 +0000

    avoid profit disturb clean freedom

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
