dd3610c27f1504f9fe2a97c22dd0d3b540e9f9be
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:23:27 +0000

    interview concern smile clue modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_repr(array):
+        return repr(array)
