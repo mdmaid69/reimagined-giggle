4a707643a634634fefee88518296a557cfac7f55
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:29:06 +0000

    lookaround bury whether ask provide

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
