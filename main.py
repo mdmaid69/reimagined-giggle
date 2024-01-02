d6c7ea1a2aef568e20652b7d54ccef64a8707d31
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:24:30 +0000

    back match job through to

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def split_path(path):
-        return os.path.split(path)
+text = "Hello, world!"
+print("Characters:", len(text))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
