859969dffb67f20968a42309d1d456266ac24438
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:34:10 +0000

    impossible himself cause harm passenger

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
         plt.show()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
