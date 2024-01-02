9712ae0b8833e4a39760948803cbc959280b66cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:36:16 +0000

    live all continue obvious tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
