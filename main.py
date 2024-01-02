c7d25eda7f563eedc831a62c2ba2719c5d7a6fda
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:56:55 +0000

    approval copy compare for comfort

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_set(array):
+        return set(array)
