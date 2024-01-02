ad933d8d757a46a53dc11dd3b77545c76c17d4a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:59:10 +0000

    beside concerned production software section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
