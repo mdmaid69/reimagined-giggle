bbd27aaecf179eaac805f6fd0243bb08b96d1b18
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:25:50 +0000

    interruption many 571enough idea shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
