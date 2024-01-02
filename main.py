6ca21c9fd27d1821507a9be1f16ea4649281d9d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:43:51 +0000

    paper summer sign season recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
