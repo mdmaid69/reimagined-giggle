0973c02f69b15eb36bd04019d4784b631f646862
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:38:44 +0000

    pocket say trust 564encourage habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
