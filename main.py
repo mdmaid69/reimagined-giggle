9540d111707807cf1b525668bc4d5c91d0582b90
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:21:36 +0000

    minor dark steep neck about

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
