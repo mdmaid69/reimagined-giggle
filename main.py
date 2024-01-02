67880bc4632c9d4c912150662ed80750e7bc8b58
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:04:39 +0000

    fetch pull glad history collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
