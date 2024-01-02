f3252f0a24d547ee0414deb198fa7988e8a74aeb
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:14:54 +0000

    arm guard interview shallow crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def clear_array(array):
+        array *= 0
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_factorial(n):
+        return math.factorial(n)
