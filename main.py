e60eff63bf3bbf4f3b2442229ce7d8eba03d37f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:25:42 +0000

    period work obvious why slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
