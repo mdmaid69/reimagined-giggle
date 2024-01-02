ca519498729390c8bd3b6b36325591c0f1149a03
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:55:25 +0000

    well contact belong passenger expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
