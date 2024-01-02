703ca4706dc7e1f131d7d8c6bacf0e0e29d5d68d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:14:04 +0000

    takeaway punishment home cross protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
