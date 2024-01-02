fe11120b0c5f21f64e8e5564688ed68731b0e4a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:25:15 +0000

    exact nerve hide split inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
