80613943663f89a568e8e5579df0afa0cb8aa406
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:27:13 +0000

    comment minority cut again marry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
