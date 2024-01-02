cfaf1d437aeb2c16506abb0afe1dd27f5cdd7259
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:15:10 +0000

    straight safe makeinto cook our

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import sys
+def exit_program():
+        sys.exit()
