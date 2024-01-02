5e8c464e2896867440c112b6e5b08478311aade9
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:20:26 +0000

    herself turnup action so middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def is_even(n):
+        return n % 2 == 0
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
