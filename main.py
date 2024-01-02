27611f5951b7378440491e67bd76b6009c6b1a81
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:45:40 +0000

    wise dick rank us decide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
