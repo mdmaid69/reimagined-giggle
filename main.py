102a5a2a882c66da422e0d8d4120e8fc09cb4141
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:32:16 +0000

    punishment expensive sort transport control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def is_odd(n):
-        return n % 2 != 0
+for i in range(5):
+        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
