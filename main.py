e6e750f34e15f91d82b32e16f009627262c52bd0
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:44:13 +0000

    beside freedom Tt strike once

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
