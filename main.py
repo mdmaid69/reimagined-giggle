81ab2b090e08c2c819f6074a54129ca8a99e27e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:18:35 +0000

    tone opposite recently convincing opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def find_max(lst):
+        return max(lst)
