12c8548350de10fd6f8f1958c2b18dace6c2488c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:53:56 +0000

    swing comeoff arrange trust worth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
