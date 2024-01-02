87cce20ba8b9fdc0321a1dd15714624a651f1775
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:33:29 +0000

    no error strange seat delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def set_array_item(array, i, item):
+        array[i] = item
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
