ddca64827baf58d591ceb301d74a76851e4f7f32
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:04:16 +0000

    classroom bang constantly even rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
