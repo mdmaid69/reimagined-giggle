fc87c1161cee11f5f6e5626846317619b93aa0e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:41:09 +0000

    star mine neat excuse close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import sys
-print(sys.version)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
