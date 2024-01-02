0b9197a74f574839987852f797261051ac14a58e
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:08:21 +0000

    key comedown consult possible silent

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
