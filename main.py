6644fa88bed10298d1c0d2f3c021ee0d3fc84838
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:25:04 +0000

    tight snow dear lift embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
