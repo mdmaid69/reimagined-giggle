e0847e34c1ef502fedafffe0221c25552bf3e000
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:23:18 +0000

    finally long train exact many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
