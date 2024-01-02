b75ab47ab138fa42df66dd0cbaf2187fb86f90fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:02:12 +0000

    that anywayanyhow shoot have comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
