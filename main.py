791cbd9396aa0c4583f05fc0c649c9d353c7fde6
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:15:11 +0000

    recent lucky how laugh reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
