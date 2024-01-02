5e93a605a776d98c2c15accdde25b023c1686a2e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:18:28 +0000

    in place right odd corner

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
