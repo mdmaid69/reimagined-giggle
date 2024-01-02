a4816d204da61af3095f9faf1e812b0f6e5d5051
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:27:22 +0000

    market worst holdout accidental decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
