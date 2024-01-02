02fa6b6e1c38a5c51bf4a2ddaf46889e062c5061
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:23:36 +0000

    goout still tough Dd separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
