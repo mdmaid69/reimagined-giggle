bd80a26bd548a080e104a8f24c1ca4a2432cd754
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:38:20 +0000

    public disturbing release foreign agree

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
