859d48bb240bf2cdc726bb522b0248e7227b2b0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:01:16 +0000

    callout sink class obvious through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
