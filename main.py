713e650c1cefa82dc9ccccb60471d30d4018d662
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:43:49 +0000

    situation during identify win open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_arc_tangent(x):
+        return math.atan(x)
