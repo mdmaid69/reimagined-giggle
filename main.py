2295387026cd9ddf09b1df5cf96645c6581c6c9b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:42:04 +0000

    moon lucky education about convincing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def square_number(x):
+        return x**2
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
