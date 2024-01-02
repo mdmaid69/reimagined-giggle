7dd1a9aeff48e288b8be7ae34d91190dee32214c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:25:08 +0000

    butter concern container beginning snow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
