5c45834e5ab7d58e1f8481cda9fc29ceac90dfb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:04:26 +0000

    deaf skill dangerous physical store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def add_numbers(a, b):
-        return a + b
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
