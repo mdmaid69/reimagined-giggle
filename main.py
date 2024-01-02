e3e2c153883ae5be9dd1ec6b644f00ffb670061d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:02:50 +0000

    workupto little payment dress breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
