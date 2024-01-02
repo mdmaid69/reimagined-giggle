064eb173093c4060a62fafc698ae752f4cb1c6cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:43:43 +0000

    giveback belong like love as

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import time
+def get_current_time():
+        return time.time()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
