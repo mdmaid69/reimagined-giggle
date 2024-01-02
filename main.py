b22a6b7a44c6d008d94649df0a37d9ebbcace73d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:06 +0000

    offence better mention cap direct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
