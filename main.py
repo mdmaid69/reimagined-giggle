cf4a8616c67235caae3003663291fa1b834e3fa9
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:21:19 +0000

    pot angry rest loan bright

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import os
+print(os.getcwd())
