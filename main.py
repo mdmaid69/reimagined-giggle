819599e3e82965b53b9ed5e318255eeb8a415d23
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:39:07 +0000

    speed honest over impressed near

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def count_elements(lst):
-        return len(lst)
+def find_max(lst):
+        return max(lst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
