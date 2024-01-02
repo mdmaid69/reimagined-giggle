466543c7c87fc470d2c8bcf386233033d85e7e8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:25:45 +0000

    floor closed offer money she

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def convert_to_hex(n):
+        return hex(n)
