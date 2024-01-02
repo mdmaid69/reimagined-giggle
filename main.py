e44417865cf58e80aacb78df2498c17caf144ebf
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:15:52 +0000

    muscle Tt maximum runout receive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print([x**2 for x in range(10)])
