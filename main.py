df837dc8467721e871ed75e01b822ecf2c3daf32
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:50:06 +0000

    undo now few among start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
