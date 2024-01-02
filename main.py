331f61a4b2dfbbe52838094e05134c86409d4135
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:10:27 +0000

    ever during correction whatever sometimes

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def append_to_array(array, item):
+        array.append(item)
