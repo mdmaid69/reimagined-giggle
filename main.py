47406a9d95056e5d5800762b0bd36f3f9b0e1168
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:06:38 +0000

    world stupid until effect although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
