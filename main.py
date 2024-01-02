4eee37aa94f615b585099937efc550451504cde0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:19:32 +0000

    store getback field bar equal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_as_repr(array):
+        return repr(array)
