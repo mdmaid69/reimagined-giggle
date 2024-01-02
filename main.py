8d31e0c47f02a8f97546d03268d615b401b788fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:00:04 +0000

    flow sick treat quiet runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
