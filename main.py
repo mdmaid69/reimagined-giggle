04093cf1e014e3c7d2421b21eea18951cee0dcf3
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:48:56 +0000

    level ashamed pinch cost control

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
