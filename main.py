4f47165065176eea7698bc3a6fa9a4658e7ccbc3
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:31:47 +0000

    again turnoff platform level glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_counter():
+        return collections.Counter()
