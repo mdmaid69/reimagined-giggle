f946601a7664425d40cf668edf3d5693597c31d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:42 +0000

    stiff injury authority rain wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_as_set(array):
+        return set(array)
