bd5ba59d34ff8c62bfc73fb2e2b6c15197ea270a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:06:06 +0000

    during smile number sell influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def count_elements(lst):
+        return len(lst)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
