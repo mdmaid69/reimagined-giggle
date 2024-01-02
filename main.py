6413ea9b940e537dd123fd96d292744226e3922e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:38:21 +0000

    emotion home raise same Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
