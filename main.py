970e32daee114f268fa2d9b1f80c9df8c5bf7333
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:57:43 +0000

    tend considering passenger unable contain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
