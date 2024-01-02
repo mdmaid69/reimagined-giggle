186c8532ea39c4d3c27bec818e926a0be9df0b07
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:13:35 +0000

    vision same university Qq careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-def calculate_work(force, distance):
-        return force * distance
