b26091712fff7c23728f2cbb98a722ab63473c54
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:53:55 +0000

    mind sign variety thought Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def greet(name):
-        print(f"Hello, {name}!")
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
