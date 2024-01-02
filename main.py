2729e184a395518bc6daa6e944c699800cf34ac0
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:36:52 +0000

    mine feed today violent order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
