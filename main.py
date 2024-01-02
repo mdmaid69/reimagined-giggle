8b23ea3636d8d2afe75dc6a41fc6f7e37b13761c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:49:05 +0000

    point male manner discussion history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
