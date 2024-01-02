6717fecf8ded13790e352ad42624a961495e9cde
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:34:05 +0000

    money wave discovery bill opportunity

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
