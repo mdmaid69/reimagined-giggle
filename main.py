69a83d87cc8d4a81ef6353d8569028a6a3506666
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:39:42 +0000

    possession report I relax congratulate

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import os
+def get_current_working_directory():
+        return os.getcwd()
