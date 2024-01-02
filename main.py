113bdb07e78a6694628b88b28a49e46b3b6872ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:16:43 +0000

    number close high never now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
