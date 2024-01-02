fba1e18ef94a311e657975841d5eb42d067c556b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:47 +0000

    at continue break look blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
