b3825824ae89f80073a141eafdc910de9cf1444e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:58:34 +0000

    lock comeround class window force

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_directory_name(path):
+        return os.path.dirname(path)
