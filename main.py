cc8bbceaec688c31e0e4c14855cd6adbb9a54348
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:09:52 +0000

    talk tool rain Dd sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
