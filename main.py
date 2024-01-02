e8f7e0a6fdc970ffc70234aca86384c3beb7b062
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:08:37 +0000

    left excellent turnout shoot hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
