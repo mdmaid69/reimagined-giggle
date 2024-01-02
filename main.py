670fb3bc18802981733d5f40b219c6a338bbe39c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:47:02 +0000

    handle bath hope point relax

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
