9f696fd1052f64c4fff027c803f7f83b6712d35d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:36:52 +0000

    sound boat take clearly whisper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_acceleration(speed, time):
+        return speed / time
