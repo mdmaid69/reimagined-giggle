cf5623f60461de4a6f2899525d936a64bac08288
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:16:39 +0000

    few sorry sky break move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
