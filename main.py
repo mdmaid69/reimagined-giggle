8e5e1ea6c1dc1f2cda7f1bca23049f1bceab0dd0
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:47:44 +0000

    best upwards earth tough shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_itemsize(array):
+        return array.itemsize
