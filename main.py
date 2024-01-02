3e73cb089ace9efe80706b3d2fd516932177e2ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:36:32 +0000

    turnout grade cut direction us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
