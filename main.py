b67dab405b2833aebd519541b12dfcaf7eff4ad7
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:49:17 +0000

    pot likely port before place

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
