82514c32e39813cb3beeda1190e91c6e549bb96c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:33:23 +0000

    interest unlucky illness form sensible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 n = 10
 print("Square numbers:", [x**2 for x in range(n)])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
