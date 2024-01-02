1e64db732759734356bc635d7d36bb3ec7b559fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:09:19 +0000

    however by most relative offence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_str(array):
-        return str(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
