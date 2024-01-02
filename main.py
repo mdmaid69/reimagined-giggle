1e73ab16de71f25953a83b505e0c06141da83619
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:21:11 +0000

    final difficulty pile add base

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def convert_to_hex(n):
-        return hex(n)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
