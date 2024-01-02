c3972c3055b80c1fd6ef87bdf1eb127470fc1e33
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:25:21 +0000

    patient quiet enthusiastic murder people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
