cf12254b42955a7623f24c7c985e69aae4a8c15a
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:20:22 +0000

    comeback information school rubbish height

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
