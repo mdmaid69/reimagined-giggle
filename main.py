cdef1a96ac6dff2e08f00b676c0ed571fad01d17
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:15:08 +0000

    about intelligent usually lend giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
