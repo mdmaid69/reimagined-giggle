7274d8ea273d526294a017727fd07f1b15316e16
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:37:55 +0000

    lay hurt bus wash old

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
