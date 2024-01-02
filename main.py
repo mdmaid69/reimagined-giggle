ecbc13a09a98e32fe6e9095640e5c55180a8422f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:33:04 +0000

    anywhere account clever train obtain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
