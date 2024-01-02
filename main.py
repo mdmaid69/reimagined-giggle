6c99c9d546526977691c729d024e1ca1d80af847
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:08 +0000

    figure goup stay frighten heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
