3f294e1116c9dbc22247cca05cae6ee19bfece24
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:28:56 +0000

    pretty defend takeaway bridge after

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
