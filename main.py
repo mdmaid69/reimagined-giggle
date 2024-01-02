55533787a3f9c82730d19ad2478d646787ef70bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:24:39 +0000

    complicated shallow goback(to) lock famous

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
