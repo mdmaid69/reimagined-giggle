af70e3458a3b29154642f0ed1bd14bc12c418b35
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:15 +0000

    splash find search rain thanks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
