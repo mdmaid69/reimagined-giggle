4d03faa6d1ab19f05fe0018eddb862324c7140c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:17:14 +0000

    board value violence on only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
