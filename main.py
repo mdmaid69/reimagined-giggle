aa873b82eb5902f8ee58d9d1593fbe1f4221f46c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:26:54 +0000

    communication separate female destroy Ee

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
