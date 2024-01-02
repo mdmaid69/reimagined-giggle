a697007e419989ce4691cd5530625c282ffbbf0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:20:55 +0000

    city plant address mood tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
