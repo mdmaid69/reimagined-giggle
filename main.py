1daa89855b06599942147525f794b8433fb56704
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:07:46 +0000

    reach form preparation hold scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
