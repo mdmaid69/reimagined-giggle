c70a9badc2c3a504e27d30ca223c057ab4caaa01
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:30:53 +0000

    join suddenly day cough just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def is_odd(n):
-        return n % 2 != 0
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
