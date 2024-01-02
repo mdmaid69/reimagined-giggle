0f98e2d7ada804ba34197d955d0952c6f3d9730b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:31:24 +0000

    tidy act dinner squash rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
