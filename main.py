9642f7765779d18e64a9abd6521f8cbcb98e8b37
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:46:19 +0000

    clearly round chance breathe Kk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+for i in range(5):
+        print(i)
