1a8d2861b08977b1d0a539d9fe8377180408103b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:14:56 +0000

    waste pleasant hurt at market

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def find_max(numbers):
+        return max(numbers)
