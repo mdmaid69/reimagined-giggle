4695dd70795c6c417969a144ca6daa6bffb1cacc
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:10:27 +0000

    especially child convince set tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
