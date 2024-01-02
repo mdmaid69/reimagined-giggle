e7bdae3783a1508490f613acd7b89a08112a9adb
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:59:10 +0000

    recovery protect Jj organization top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def find_unique_words(sentence):
+        return set(sentence.split())
