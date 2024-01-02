6c47ef79cc3ae4533043b097ed36ad9d04b7e846
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:49:21 +0000

    sore sport rubbish substance right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def cube_number(x):
-        return x**3
+def find_min(numbers):
+        return min(numbers)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
