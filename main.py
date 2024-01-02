a4cb73b01fa4a837671fd898ec3bcf9f6df7a5bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:44:46 +0000

    attract surprising advanced cover my

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
