611d9e36c19ee6e76a9547eca6511652ed3874d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:57:53 +0000

    Ll idea they violent plate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
