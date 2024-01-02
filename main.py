e75780b0fd3da769310efe7620e471e01a9adf6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:44:04 +0000

    relative approval pronunciation exam activity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_area_rectangle(l, w):
+        return l * w
