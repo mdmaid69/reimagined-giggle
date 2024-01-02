b6559e9e96194c88a1f1ec488bff309bba445bd2
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:13:43 +0000

    space computer tidy respect petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
