61f110de407b8608fbb315746c9a4bfdac241e66
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:53:26 +0000

    arrival violent dear black shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_octal(n):
-        return oct(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import os
+print(os.getcwd())
