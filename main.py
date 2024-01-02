c24b721b3fa50b0e45fab34e1453e5981a66c82a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:48:24 +0000

    but we short region rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
