2b4593655fd14a4f078ecbcf7292a45dfa1a6a3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:56:48 +0000

    lucky control disgusting possible tall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
