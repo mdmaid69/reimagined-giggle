29ae6f50d04ed220b19885bd80335ffc907a1895
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:08:41 +0000

    point acceptable awake apparently breakinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
