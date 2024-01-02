a75bfc61650783ecdaa67e366d314cefc4899099
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:24:14 +0000

    plastic list ever dull morning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def convert_to_binary(n):
+        return bin(n)
