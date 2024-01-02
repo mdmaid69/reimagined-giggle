db033b1364276b86a8485a38f2c9dee6237d632f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:56:40 +0000

    watch side bored raw Ff

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
