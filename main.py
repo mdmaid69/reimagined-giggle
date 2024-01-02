be3bde5d08f4735433ab8ecd565f588c566084be
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:04:08 +0000

    experience sleep anxious usual Vv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
 def get_array_as_repr(array):
         return repr(array)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
