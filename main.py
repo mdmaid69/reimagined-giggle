735776c7251600e5239fbacb415a007ba1c267c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:57:09 +0000

    command his joke prove plant

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  def cube_number(x):
-        return x**3
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
