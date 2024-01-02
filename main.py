ac326ae2f6c7ab5c7541f48cafa6046c53c5fd97
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:14:59 +0000

    breakin shoot punishment degree business

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_stack():
-        return collections.deque()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_index(array, item):
+        return array.index(item)
