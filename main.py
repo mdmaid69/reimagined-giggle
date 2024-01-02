f633cf1bd36eeca00f25f5073ff16240fcf64678
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:35:42 +0000

    some follow hurry giveout gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
