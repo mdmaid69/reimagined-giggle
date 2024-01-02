5bfc34a4b08887426cefcb48574f815b7c06b010
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:23:19 +0000

    average deep stranger since cheat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import json
+print(json.dumps({"name": "John", "age": 30}))
