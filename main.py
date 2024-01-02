d32322f05cbda32900d1244e365a0fd5070b1f03
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:46:01 +0000

    he mess why ill pump

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import datetime
-print(datetime.datetime.now())
+def calculate_acceleration(speed, time):
+        return speed / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
