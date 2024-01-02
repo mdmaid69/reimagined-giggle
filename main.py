fec4688a76b9dcf34cb9e948c17d2a7351851ba0
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:14:13 +0000

    gate concerned forget he nor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return math.factorial(n)
