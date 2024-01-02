6e741e4d12cb9bba9a1c42517546c45551a31b40
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:51:51 +0000

    harm rare apart continue spread

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import getpass
+def get_username():
+        return getpass.getuser()
