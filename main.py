ee7ed1b90dbf80f3281dae5e3a4ce2dc6842efa0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:03:04 +0000

    roof act trade geton there

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
