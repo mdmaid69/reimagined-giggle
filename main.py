84f1c8eb6eb3375de319c6fde4fd6c765ad3bbd0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:37:15 +0000

    walk declare sometimes complete begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
