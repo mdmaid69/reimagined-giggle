015c086d5296cd7c13970b523f5e98031ec33d4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:02:00 +0000

    fast reasonable tax action how

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
