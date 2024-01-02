f967c2e40cea05cf1e707878096ea525c1b878a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:26:09 +0000

    secret through smile within neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def count_characters(sentence):
-        return len(sentence)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
