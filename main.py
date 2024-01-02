6e4ba9dae98717ce6c7cdf7ef363fbe9261be4cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:20:14 +0000

    Bb clean increase hit moment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
