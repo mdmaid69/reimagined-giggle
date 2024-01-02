4e10b66d43fb12169a4510cd02fc131aa62a953a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:42:58 +0000

    their apparently blame invite only

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_acceleration(speed, time):
+        return speed / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
