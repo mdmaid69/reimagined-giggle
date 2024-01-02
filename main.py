30f52e87e45e1702d9266917b844dd693b76a2d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:22:42 +0000

    hot invite admit congratulate wild

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
