4c22964b5b42d7921d03fe3efa0f3390dfd65cc0
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:07:22 +0000

    unpleasant spill until offend power

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_combinations(n, k):
+        return math.comb(n, k)
