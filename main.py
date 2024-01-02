e9322174c7694c76f11cff4e7585ae19c6453e54
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:30:24 +0000

    should dear speech various appearance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_power(work, time):
+        return work / time
