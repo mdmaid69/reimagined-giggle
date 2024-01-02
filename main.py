a0477a7318104166abf0d063efbf00a6d3462e7f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:57:00 +0000

    beginning fold kind innocent quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_power(work, time):
+        return work / time
