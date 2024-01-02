7edf7ad6fcde8e79aeacce5c7055ae00d1367e91
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:19:29 +0000

    record centre involve ask Mm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_user_string():
+        return collections.UserString()
+def find_max(lst):
+        return max(lst)
