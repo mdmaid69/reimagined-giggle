f8bf503fbe0b8e523850131406b989b23b60b50f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:30:29 +0000

    land murder 564encourage equipment blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
