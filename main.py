8e5aa90df25246075b19e41dafccafe239336f13
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:16:43 +0000

    centre keen natural whether niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
