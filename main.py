377f9ce1f2671478cc7290608905babf4c18a13d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:34:07 +0000

    steam limit should unless rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
