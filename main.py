3531abfe913ae41a4113878fba0c9b6d86869ad7
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:22:31 +0000

    cope opposite complain shock will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
