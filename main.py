d1414c181213a9eec9ae8fdcd497522e9a732b79
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:57:40 +0000

    strange instrument snow listenfor related

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def remove_directory(path):
+        os.rmdir(path)
