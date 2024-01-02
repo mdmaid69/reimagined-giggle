9c2cf6842498ffd406afe9f80b67bf9ea37294f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:38:06 +0000

    town trouble poor apologizeapologise turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
