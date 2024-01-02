decc0f6f977d09c2aa4b0df5088206a1c0251057
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:14:44 +0000

    short advise politics cause radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
