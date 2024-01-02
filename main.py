f85b6014b8bea362104c032c9df47ba3d60f7084
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:46:26 +0000

    try damp wire replace extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
