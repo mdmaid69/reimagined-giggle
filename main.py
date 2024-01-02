73a3f85f411df464716f7b960d51ff5f34c856a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:17:45 +0000

    limit clear previous pure under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_python_version():
+        return platform.python_version()
