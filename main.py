41362b8f088b629d2cd32b7e5c7d34bbd9a95b14
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:38:44 +0000

    direct underneath couple makeinto damage

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import sys
-def print_python_version():
-        print(sys.version)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
