d11407c27f6bf070b6212e3bf455494543b3e720
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:36:48 +0000

    fold unlikely welcome show stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
