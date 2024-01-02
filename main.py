22f249537a142fe11e6123525a2195743f132002
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:17:54 +0000

    strength relative concerned party neither

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def is_prime(n):
+        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
+        if n % i == 0:
                 return False
         return True
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_circle(r):
+        return 3.14 * r**2
