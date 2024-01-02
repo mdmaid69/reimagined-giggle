e94ff1ac95e00325a0890231b05b66b9edb9da91
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:50 +0000

    vote think especially owe affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
