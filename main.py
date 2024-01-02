3eb1fc3116ea838fe797652930b805016cb3617f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:07:42 +0000

    shame accordingto mend mainly everything

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def count_elements(lst):
-        return len(lst)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
