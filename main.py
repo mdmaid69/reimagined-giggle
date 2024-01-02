7bd03313c845a4c467c7b7a75abb84ec01e77a0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:35:43 +0000

    cause or score bit else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
