671e350e1e657d2d2e674cd289b51597220443a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:49:08 +0000

    turnout cum argue wish statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
