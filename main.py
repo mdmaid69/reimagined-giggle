444e3c1e6fed87b04c74897f7e553a17650b9cf5
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:56:47 +0000

    makeinto prefer eye previous doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
