19c33cefd1ef2d38d02bc22f292e61c12d8d1048
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:55:51 +0000

    you kind equal concerned concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bool(array):
+        return bool(array)
