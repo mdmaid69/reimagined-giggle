cc5fa4644d80871c893987f4b921de72b53d99d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:17:42 +0000

    call break reason around friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
