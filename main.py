b44006631e5dbc8ae4df4bfac595675b564a35a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:51:05 +0000

    anywayanyhow study getback recover pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
