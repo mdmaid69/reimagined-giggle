eb3f08a7b17bed3cf4c778072600b4285fc41fac
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:42:26 +0000

    back slice large estimate sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
