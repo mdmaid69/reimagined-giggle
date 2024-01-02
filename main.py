846bf1c833d4c8b9a23a9fc75ff6355bf8b0e7cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:21:10 +0000

    operation avoid quality manner fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def reverse_array(array):
+        array.reverse()
