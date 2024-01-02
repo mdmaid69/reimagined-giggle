a2c8c9440fd518b22c846f67756577b640eb9825
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:09:39 +0000

    search walk stiff they coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
