9db1c384008ad41f63fd47d6b2343147b369ed19
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:04:47 +0000

    section old finish house comeback

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
