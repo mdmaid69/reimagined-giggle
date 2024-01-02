6ed094af0bf508968f28702c0275328fdd814aee
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:49:06 +0000

    himself not skill back friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
