0a15a3a53ccd25cff7fd9282b2583dd62630ebcc
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:37:17 +0000

    ear bill see dish lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
