2a9dcac054a6f79b1da2490e830a2da436bccedf
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:17:42 +0000

    serve claim extra why satisfied

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_complex(array):
+        return complex(array[0])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
