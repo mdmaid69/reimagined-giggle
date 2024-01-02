c97abccc656404ddbb9de8c598946e2ba83ffd8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:58:38 +0000

    its late generally autumn century

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
