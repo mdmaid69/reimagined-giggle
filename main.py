920d7a98caf8712b9fd7cd2011ae6b227daf227a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:05:10 +0000

    sweep approval broad condition thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import datetime
+def get_today_date():
+        return datetime.date.today()
