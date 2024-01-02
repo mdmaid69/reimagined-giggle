1d37a241bb818a0a741e69cde8413ed0a8f0f0dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:46:51 +0000

    port sink content area frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
