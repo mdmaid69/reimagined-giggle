7825a538e816b574ef71b610196fc015db938c80
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:21:32 +0000

    ought talk worried only wealth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-print(time.time())
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
