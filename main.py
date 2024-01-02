dd3baf7b96918f293f4f5c7ddfc398ecfade93b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:56:14 +0000

    compete crowded declare load talkoutof

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
