7624dee02e2d25181685c9d6cdc4872b638a0b8e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:25:28 +0000

    print thinkthrough proper wet true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_max(lst):
-        return max(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
