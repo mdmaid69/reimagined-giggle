182b479cac849c582b69e40c2804deda134c0733
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:37:15 +0000

    absolutely own Kk well special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  def is_odd(n):
+        return n % 2 != 0
