37e195ed95da3afe4c0696de5275da89a1c9c60c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:20:25 +0000

    moral keen large state do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
