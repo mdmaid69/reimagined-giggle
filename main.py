c34f4d4f4d861d7dec434d45880d79ac1dca68a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:11:06 +0000

    level whichever reasonable manner bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
