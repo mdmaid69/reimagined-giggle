d00b97e4c636b6dd188c70ae540123789927c481
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:13:09 +0000

    off base design in goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(5):
-        print(i)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
