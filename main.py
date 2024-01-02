fc5da57f1771bcae58447b83869ac88e0b96b695
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:05:12 +0000

    ready answer deserve meanwhile takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
