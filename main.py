c67432ab48e87464a62c1929989d31cfd1ad5674
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:33:02 +0000

    office really look welcome seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_stack():
+        return collections.deque()
