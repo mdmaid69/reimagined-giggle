564afca7ddd098a5f46c5d43a8d65bf0587c7209
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:26:29 +0000

    responsible search unfair voice ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
