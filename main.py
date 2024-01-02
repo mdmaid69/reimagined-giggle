7502892deb0939d8034ffcafd8f73b5d14c7752c
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:52:36 +0000

    tool it welcome muscle callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
