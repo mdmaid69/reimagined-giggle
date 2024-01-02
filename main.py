df666a1be9d0e0fcf070d103b6fe7c1cfe5a534e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:07:51 +0000

    swimming failure motion question average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-for i in range(10): print(i)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
