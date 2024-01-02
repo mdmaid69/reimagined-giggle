84cffbdc1902b70d069743279b44c9be87bac5fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:39:33 +0000

    about young search worst strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_index(array, item):
+        return array.index(item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
