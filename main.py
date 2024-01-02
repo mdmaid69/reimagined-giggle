424c1ed3635d40894f6f86f26c58fcebe9e2a4c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:56:16 +0000

    she shade anxious proper help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
