21d6b7725b1936d03efeb212ffd5a0f2254c0c7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:35:21 +0000

    step share further office content

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def square_number(x):
-        return x**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
