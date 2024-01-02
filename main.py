0d98ca2026ec7060c9794ae4e303d39f59df1e94
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:22:54 +0000

    greeting sea print find marry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def find_min(lst):
+        return min(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
