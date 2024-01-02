607444ae4b534fc4bff36bf0998cfa0102245d2d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:15:48 +0000

    slice tax how wind supply

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
