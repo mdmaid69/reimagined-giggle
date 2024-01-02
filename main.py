bd74edff90ed03fa985c52f2039103e821aeac56
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:26:00 +0000

    nose measure fun suffer ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_density(mass, volume):
+        return mass / volume
