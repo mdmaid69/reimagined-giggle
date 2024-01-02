05be83aa677f92825711347fde05c2d0cfe6621f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:15:01 +0000

    or angry willing low repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def convert_to_octal(n):
+        return oct(n)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
