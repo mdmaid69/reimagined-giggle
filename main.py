5c937567af4a2938cdedf368bcd99db6c81ee0d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:30:25 +0000

    careless exact declare mean consist

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import os
-print(os.getcwd())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def reverse_array(array):
+        array.reverse()
