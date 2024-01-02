08d04a1eadda9330728f184c8fab0c7c9430d296
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:06:44 +0000

    pin closed mistake minute affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
