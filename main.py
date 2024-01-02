491d6c3b30436f907b8e301fdbebbd1e52c9548f
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:54:15 +0000

    affect release obvious offend yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
