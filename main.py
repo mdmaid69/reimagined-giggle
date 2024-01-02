2bf4445e12ef7982ecab538e6f9d31a206415f3c
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:23:13 +0000

    tall undo thinkthrough confused nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
