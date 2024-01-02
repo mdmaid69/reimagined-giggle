9dfc44ddba13015183eb5afdb22a8341baaff3a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:20:18 +0000

    paper react charge drink fit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import random
+print(random.randint(0, 100))
+  def reverse_list(lst):
+        return lst[::-1]
