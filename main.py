b6fbe7e9989489db346c303ff0066787fcb4eedd
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:41:08 +0000

    convinced tough feeling share government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
