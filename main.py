fea4a9327e9689463bba8473ca14f0059374de79
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:25:05 +0000

    concerned keepoff cross sign burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
