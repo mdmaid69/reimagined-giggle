f58e689dbf3e7dc3da118bacab8f0d7738686081
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:51:12 +0000

    reply contribute day opportunity shame

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
