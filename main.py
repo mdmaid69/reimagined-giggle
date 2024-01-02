8470347eba62759e034790b44e501800a243c12b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:58:08 +0000

    out prevent finally entry company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
