fd7aa9a84854cd7706c211ba1b9b3f5584f7012b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:35 +0000

    ought even communicate course table

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def reverse_list(lst):
+        return lst[::-1]
