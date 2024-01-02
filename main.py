0ca416490077b8e2d2cc940fe009d124c16b11e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:16:36 +0000

    carry push hard tone unable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
 import array
 def clear_array(array):
         array *= 0
