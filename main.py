cfe9523d26d565b5033c8e52ce872d7836ae7188
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:12:35 +0000

    often divide begin discovery to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
