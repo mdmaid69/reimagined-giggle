b5d41ed1cda64573c643b147c14cf8c64e473a07
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:24:20 +0000

    believe private unfortunate afford invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def reverse_string(s):
-        return s[::-1]
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
