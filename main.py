dd5fb5e69d9403518189b80af6ee494d32f9c5f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:17:58 +0000

    light marry bringup lie effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
