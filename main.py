dfce77b487334ee4c4c6cc6cc6bc13177f9ab66a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:15:31 +0000

    pretty rely shame press weather

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
