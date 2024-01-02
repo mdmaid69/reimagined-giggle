2f46ef5690596a57d2a0e9363a3fd3e2efe17e37
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:57:02 +0000

    clearly weak live afraid divide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def reverse_list(lst):
+        return lst[::-1]
