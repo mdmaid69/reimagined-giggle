1dde9bf07d7a2df0707e91c15615afa1544aab6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:44 +0000

    become bang worth repair emphasis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
