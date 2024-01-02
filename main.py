fe8f4a6718a3e329a161e8353aa419a1773fad6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:50:06 +0000

    someone key spill thin new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
