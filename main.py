db6fef804e52a3cca68db132a8a14ef7921010b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:56:23 +0000

    any squash exciting excited all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
