26e1d467567f7002b810dca7baa64ff764eb3011
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:58:40 +0000

    order able understand police ordinary

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
