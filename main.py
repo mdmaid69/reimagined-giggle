b966e0e7c3ddc8d51f9df2e62423e800c9b5695b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:10:49 +0000

    agreement partner steer comeround plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
