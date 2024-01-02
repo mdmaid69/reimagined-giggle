8cb89afcfce0ddc1a523d672db49d71e07e08ce7
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:00:54 +0000

    unable owe giveout relative there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def clear_array(array):
+        array *= 0
