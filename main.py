cb0fbaded8399096f23933ef6318061b7eed1b40
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:39:31 +0000

    recently close failure spring artificial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
