8b1ab6655d072110fde3632416ddbad4f85e9992
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:48:51 +0000

    react top exploration better charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
