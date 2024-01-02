37b5b4959b977e0f032c73918029b66585d96ba0
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:17:32 +0000

    deliver around careless stage bottom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
