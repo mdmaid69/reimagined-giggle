3e52aa82df31a56ab1ffaf084e9b32eb3d9f3b11
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:06:54 +0000

    vanish memory complain likely simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_repr(array):
+        return repr(array)
