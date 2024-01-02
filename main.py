5104aa02d00a84d940f817e7481cfb64feb166b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:37:38 +0000

    declare town bang practical copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
