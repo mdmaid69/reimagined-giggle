5c96837d691cdf23966845727dfcf074ede38a95
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:09:38 +0000

    check trip press sticky full

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
