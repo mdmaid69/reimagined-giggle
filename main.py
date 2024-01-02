60676d689246608b7f315435fb6b1aeaf5718189
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:07:42 +0000

    emphasis give relation material me

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
