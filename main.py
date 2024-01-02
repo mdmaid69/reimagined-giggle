fed855df92b7270d82cf10e8b2c3bf7816586bd9
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:51:03 +0000

    tap light rush public step

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def cube_number(x):
-        return x**3
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
