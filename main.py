0a3679d4b051b4f2d5ea5555d282d349f09274eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:22:13 +0000

    Gg exam advantage turnout nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
