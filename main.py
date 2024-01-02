4a17572402e47f167f82e489212af4c0216518aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:17:24 +0000

    blind thorough favourite lookup harm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
