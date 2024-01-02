be957f993335b600606eea45ae64a0521eb01b41
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:35:38 +0000

    glass substance low fond conversation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_odd(n):
+        return n % 2 != 0
+def calculate_area_rectangle(l, w):
+        return l * w
