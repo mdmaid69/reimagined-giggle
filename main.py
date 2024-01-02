489395632a1c0e07110e10a54da3c3579afcf28e
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:16:19 +0000

    bored hurry woman tired scrape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import random
+print(random.randint(0, 100))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
