46e88ebb3e84c3fd9770e4a63b41148484142e8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:01:01 +0000

    pass appear lend tongue represent

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
