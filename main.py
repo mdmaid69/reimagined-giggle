769f41d79a29648d789baaaf5dfd5856b622710f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:05:37 +0000

    turnaway light lookback(on) conclusion interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
