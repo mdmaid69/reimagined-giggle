95962bac6402e13a07d133ec1bd0057942cfab13
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:23:48 +0000

    worry substance second set ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
