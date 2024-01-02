536cb5af694fdec93152d8a3f5c29e7d915db405
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:35:40 +0000

    cause before whenever reserve means

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
