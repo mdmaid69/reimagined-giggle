231c7fbf517699efe5831860cb8fcd901d65cf86
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:12:04 +0000

    at seriously with switch twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
