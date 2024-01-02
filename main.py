35661d351fdb6e5bf6c42d4398b559519e252dab
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:19:04 +0000

    account offer promise square dark

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
