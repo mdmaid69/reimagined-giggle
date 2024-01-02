346eace8ca86440285e28079c1787e250df37941
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:39:00 +0000

    nothing production mixture meal makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
