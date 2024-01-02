6e33a69c3d37bc261be61b1df13019b20625bb17
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:56:15 +0000

    in basic contact belong getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
