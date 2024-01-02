fae8d2ed3c5a2eb66168b604550c0d9051d39b01
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:59:34 +0000

    already level really fasten increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
