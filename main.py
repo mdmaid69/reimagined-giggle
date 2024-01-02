d7f11e2bdf39ed601d1c57af9889396a9c0da845
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:39:28 +0000

    hour complicated right hollow most

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
