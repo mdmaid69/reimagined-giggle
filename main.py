5aa791f7a807392967f1f65af081ecbeabf2b032
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:03 +0000

    shade hold pity treat lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
