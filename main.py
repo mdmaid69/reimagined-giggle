13ac117b93d7dd93246c0a63998c30b71ebffe3a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:02:35 +0000

    clean swing possession flat forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
