75baa42f2e75b152f55c57a9fed40d4eba1fdbbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:12:32 +0000

    dare swim wide thank small

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
