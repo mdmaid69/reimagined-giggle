69f9adf3b64a111d6d2b426db1553bc7bebf3952
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:44:52 +0000

    cost honest general poison fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
