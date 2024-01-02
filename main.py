4c3e7537036258566ed8da84467131375b309b59
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:15:55 +0000

    creature sometimes suffer part because

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print([x**2 for x in range(10)])
+def calculate_pressure(force, area):
+        return force / area
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
