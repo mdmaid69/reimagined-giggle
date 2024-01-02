58ecba0794e2952a351dc31d4b92ffe94bc4110b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:44:57 +0000

    money temper outside mood today

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
