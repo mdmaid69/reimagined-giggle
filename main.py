fea888920b9299c97a9c8f695ae438e465085d4d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:38:18 +0000

    slightly ours stand balance upwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_acceleration(speed, time):
+        return speed / time
