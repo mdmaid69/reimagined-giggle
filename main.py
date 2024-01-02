0a755289acaf33b6a718d4a04a074d732b7f578e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:32:47 +0000

    comedown period love release thing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
