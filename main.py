0670278aaade85ac4c6a7b076525d4ce71975f81
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:41:40 +0000

    mainly object friend see best

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
