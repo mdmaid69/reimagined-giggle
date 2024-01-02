e13610ccec678a3df81b4c538327e4f8a4cd3175
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:21:44 +0000

    block flavour callup character drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
