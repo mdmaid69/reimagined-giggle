276f698f0c211162f6c22fd78ccfbf2a5b5da461
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:47:46 +0000

    free again under talkback present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
