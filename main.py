124264ecd50a330748d4f552d8982eb3482caf8e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:17:54 +0000

    harm ocean holiday factory detail

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import sys
 def add_to_python_path(path):
         sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
