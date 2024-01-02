eb3c5ea7f9ae72ebe3087c4e9d26a8dc7bd17947
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:19:13 +0000

    cry mystery error accident mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def square_number(x):
-        return x**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
