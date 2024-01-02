f36563c8cb8230c761c4fd88686071dc76c6ea74
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:58:53 +0000

    when reach consult enthusiastic criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
