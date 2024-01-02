81f8dbb4ddd753edbbfe218252bd3e7c8c153772
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:03:44 +0000

    prevent rid divide family splash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_string_from_array(array):
+        return array.tobytes()
