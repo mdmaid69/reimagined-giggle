802421c2d11f7a2d4d45b739d879801c7c7c64ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:13:39 +0000

    tend law tone 570enjoyable false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
