e51d4679d01667626035b95c10bbe199923fe25e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:26:14 +0000

    lay breakdown side turnover load

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_area_rectangle(l, w):
+        return l * w
