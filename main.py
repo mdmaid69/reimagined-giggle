52ba335c4402caf4a333c28ab36341fcb07585b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:00:41 +0000

    instruction nurse reason sight permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
