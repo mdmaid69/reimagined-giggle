e39990de75a62233c04d9272a94044bb44a5c919
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:27:52 +0000

    into wash brush responsibility interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
