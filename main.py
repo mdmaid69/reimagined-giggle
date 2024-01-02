46f3f59bb81b5484489e5fd2e213d38c38b8c616
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:46:22 +0000

    rough polite label soil base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
