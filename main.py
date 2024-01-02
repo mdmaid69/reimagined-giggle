17e09ef751d24c3b45c152f9690a1d9186e7c9a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:32:41 +0000

    fortunately capital target uncertain comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
