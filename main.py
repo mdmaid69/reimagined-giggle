5f179560e50c1f361d0143b2e1483108f9d7d417
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:42:54 +0000

    discover extra her exact comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
