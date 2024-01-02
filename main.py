9d1cfb3957c9bb87a43a8095d8a2b6abf2c559a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:34:34 +0000

    underneath 564encourage eat it everyone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
