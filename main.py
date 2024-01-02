92e313d980e494d59708cfd1804d1a6e48911819
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:07:44 +0000

    copy snow moment hurt die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
