c73b539312b7611a3f3bcc9fbe9aaddb59688262
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:24:59 +0000

    consist judge wet organization wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
