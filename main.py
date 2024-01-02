7f8061db8425cd6792e918e60416ab84dd6a1009
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:58:51 +0000

    approval calloff twist fair rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
