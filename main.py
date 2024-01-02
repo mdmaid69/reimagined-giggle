ca734f5ea4e38f35b3f9447d4c0d19f02cd5c0cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:22:48 +0000

    contrast couple close suddenly dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_list_from_array(array):
+        return array.tolist()
