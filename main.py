a4ce05efd004b0d88ffdb332ebb5730e359b29ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:05:17 +0000

    regular speak build find prevent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import sys
+print(sys.version)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
