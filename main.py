46c1349fc55716dd072aa3b0edc24e81cec8e7a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:19 +0000

    bang cheap underneath fat means

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
