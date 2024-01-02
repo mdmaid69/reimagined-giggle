ec93994a4fe78afd6c6d15ca477c7671b6b9985e
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:46:34 +0000

    offer fit gate escape perform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_words(sentence):
+        return len(sentence.split())
