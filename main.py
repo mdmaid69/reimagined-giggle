f78cf1a951263ed9d175e9168ecb74c93fba69aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:19 +0000

    fork mouth turn fold trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
