3bc429ebe7d88c78e2853cdaef2bedc10696619f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:17:28 +0000

    runaway wise direct least bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_max(lst):
-        return max(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
