658b29dbf17d63a23dd0060e8a4dff3d3ad6b8f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:49:12 +0000

    lunch coffee last pay quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def greet(name):
-        print(f"Hello, {name}!")
+def get_array_as_bool(array):
+        return bool(array)
