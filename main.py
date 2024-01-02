fdb805ffc93ba0e869203078b9dec44687ffc550
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:33:03 +0000

    commit effort Kk delight snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
