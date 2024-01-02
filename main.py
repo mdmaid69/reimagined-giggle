40fcddf100c9adaf2c4c5bdd10b797d6091fda0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:00:40 +0000

    risk over woman frightened sentence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
