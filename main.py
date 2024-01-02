fc21105e980fe8316c3495ff0b52fd1f1a666672
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:55:40 +0000

    certainly under clear trick name

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
