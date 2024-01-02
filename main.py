c1d15350ab0797a5bea223433fcd614edea92c18
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:14:18 +0000

    pump mind pupil 573entertainment ticket

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
