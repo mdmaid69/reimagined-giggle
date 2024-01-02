1749c0d0e6ef7ef3291d068fbcc073d156b83d78
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:11:23 +0000

    change definite square friend seat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
