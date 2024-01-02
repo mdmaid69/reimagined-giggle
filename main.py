37ee09c3a9bd5f2bf9459aa4185865290ee5bd95
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:37:39 +0000

    unable adjective fast division mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
