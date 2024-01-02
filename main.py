9a9029c821a28331972d89393658b4793aa11faa
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:07:24 +0000

    contrast knowledge fine belong table

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
