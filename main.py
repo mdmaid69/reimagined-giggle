d0ed3835801e80ca732df9caa9d935667406e869
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:02:19 +0000

    turnup unkind soil argue deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
