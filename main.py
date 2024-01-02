a286659e4ff2813542e00ac6af4280b6df88cee9
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:13:23 +0000

    error word present lost valuable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
