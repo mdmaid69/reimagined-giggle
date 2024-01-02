0493b8c939e3ac677b72c785a8227796368c0827
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:31:25 +0000

    dare judge cope exactly suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_priority_queue():
+        return collections.deque()
