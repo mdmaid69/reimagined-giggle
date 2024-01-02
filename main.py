d3ca95d719a4995c9e008c61aee8eafc96c11dc5
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:46:38 +0000

    fast force necessary total conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
