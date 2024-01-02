e1d7beac2a78ed9b27b51f383de33408f3ee8fd2
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:11:35 +0000

    part breakinto taste circle fact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
