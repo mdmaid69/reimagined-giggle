13a3f3d2233e977eb663893324a48b02afa2ea7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:39 +0000

    brush communicate keepout desperate desperate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_sign(x):
+        return math.copysign(1, x)
