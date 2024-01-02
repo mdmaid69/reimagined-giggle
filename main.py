ce21a444b07d08c6814e7bbf68b9675f2a001490
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:17:13 +0000

    region peace reply dick row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_current_time():
+        return time.time()
