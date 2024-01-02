85c4e454e94f7c9d68a54ffd96e89de72aa5ba9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:34:50 +0000

    expression upstairs wild present watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
