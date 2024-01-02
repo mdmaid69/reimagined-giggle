adc49cfae25e995204a6fe850e9de831ab9d1b56
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:01:47 +0000

    think against meanwhile fear reserve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
