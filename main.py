2d42ebb4ce674eef364daa6bbeb24832d3569f4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:06:18 +0000

    future prevent close substance culture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
