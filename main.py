9a9c8b63264c247da3859f0aed303ee8aa18f0c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:44:16 +0000

    thick snow fall chin door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
