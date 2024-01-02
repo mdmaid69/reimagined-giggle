ef5e26290ccd86bfe515bcdfd34dd2b5906214d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:48:31 +0000

    show runover exchange also rich

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
