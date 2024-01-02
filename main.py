02f5e89b7182666ed70f6067ad77642756a09b06
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:08:14 +0000

    fast lower search offence around

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
