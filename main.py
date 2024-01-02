9b395e3c99608dbc620f36f8ab65e389474d93c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:27:30 +0000

    tooth special coffee along shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def clear_array(array):
+        array *= 0
+import platform
+def get_os_info():
+        return platform.uname()
