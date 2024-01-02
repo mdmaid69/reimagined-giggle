ac7a4a0760993e8b89985c622e0960b0dee0e73b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:54:19 +0000

    act basically turnover grateful top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
