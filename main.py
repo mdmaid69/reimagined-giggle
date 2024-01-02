366181e3272433681ddba903d841ee76d244bcd2
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:11:16 +0000

    coffee disgusting right onto target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def split_path(path):
+        return os.path.split(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
