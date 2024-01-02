15005c13372d4b984c25fffbf63d63a9a3bd3258
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:01:01 +0000

    command nonsense back laugh home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
