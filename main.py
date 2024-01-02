3bf5ada8ffc931051fb626f5a86a879f798b6f5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:31:03 +0000

    seriously education steer discovery twist

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
