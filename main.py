28bcebb57ca85b126bcef9477dfb7dddb8b47164
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:56:39 +0000

    plastic organization ride contact head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
