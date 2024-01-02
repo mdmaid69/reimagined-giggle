5ef4c4b5d00c42383f52d0f1c7c3426e20de9877
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:19:01 +0000

    here fashion safe scheme extra

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import time
-print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
