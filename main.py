76a1498443806b1ad1b8e0d9c00273bd5d42434d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:06:07 +0000

    decide absolutely group along cold

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
