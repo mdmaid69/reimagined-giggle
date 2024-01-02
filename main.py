18eeedd8f3f09f8be978048f93222805ab82f3a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:40:15 +0000

    contain bound colour very be

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
