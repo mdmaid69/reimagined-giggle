dff0096dc0cdeeb2824f540b33449c6e513f6680
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:27:45 +0000

    keepaway separate butter aim mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
