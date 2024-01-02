40187a36ddb035c6c9df7e35d05aa0b8fee76437
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:55:56 +0000

    cut stiff gooff serve line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
