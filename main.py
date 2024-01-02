f94c443380e00735c70ae2b7b03428fd9926019c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:55:06 +0000

    forgive mark responsibility tip ago

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def find_unique_words(sentence):
+        return set(sentence.split())
