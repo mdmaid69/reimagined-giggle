c0b35eb095492a6d5817f1f8a95a09600636f9bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:56:02 +0000

    eager basically shop hear Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
