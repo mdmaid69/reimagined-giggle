c3c8d6c5055cfbd0f92dca1bd6cdd1859cdf9e67
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:33:19 +0000

    separate type classroom smart always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_floor(x):
+        return math.floor(x)
