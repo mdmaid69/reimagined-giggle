e1219df8e0bc34d7d2818b5ac82e9c7ea10ec146
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:32:11 +0000

    bin buy spread split cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
