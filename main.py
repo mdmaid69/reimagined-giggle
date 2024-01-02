a607b23d4349a802f64cc9f186e99b9c142fc455
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:56:26 +0000

    expression sharp realize crime thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
