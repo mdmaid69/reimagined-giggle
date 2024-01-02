92b8c6cf35f7a52c17d653f323c60e5b4449a910
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:59:46 +0000

    machine stage actual side makeup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def find_min(lst):
-        return min(lst)
+def convert_to_binary(n):
+        return bin(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
