2e5248c480fd6e1a6ad1fdb82d87ca37753b3512
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:26:40 +0000

    takeover polite nigga post perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
