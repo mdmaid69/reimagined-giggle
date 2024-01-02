b891f135832d7d2442c5199f1635b0f5cc556b6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:58:37 +0000

    twist partner regular avoid picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
