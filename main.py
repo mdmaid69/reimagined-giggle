84973b8cdc635d399ec945459cbe0c7a36a8bdd9
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:23:24 +0000

    challenge clear rare number fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
