a61b26114ccddc410a3efd55ee12450c0220c6fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:12:50 +0000

    week film wrap matter decision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_binary(n):
-        return bin(n)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
