c572318ecb53ac6fb881257802ae3409a5dded56
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:54:46 +0000

    frightened unlikely deep obvious praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
