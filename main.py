95602bcfa0100f4fbc745f642ab3479091bdb635
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:57:17 +0000

    past able stare considering pay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
