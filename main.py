c9305c3214c803be68a1c5971e726193b1b8c28c
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:46:34 +0000

    task all waste constantly cook

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import time
+print(time.time())
+def calculate_density(mass, volume):
+        return mass / volume
