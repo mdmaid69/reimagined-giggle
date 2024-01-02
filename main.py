5d71cbaff4c9e9bdf998ce7823248915393d3e31
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:14:37 +0000

    bread force treat tidy appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def reverse_array(array):
-        array.reverse()
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
