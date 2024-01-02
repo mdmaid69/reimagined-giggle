acdf939364dc8d646423f9589043bad4b3d36230
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:09:57 +0000

    ring finger especially middle independent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_area_rectangle(l, w):
-        return l * w
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
