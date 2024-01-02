bd75b0f111df5456b1469cd41add6bee346fe1d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:57:33 +0000

    snow switch Rr impressed problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def split_path(path):
+        return os.path.split(path)
