51065910895c70b09cdbb0b0c17c13ff0ee802cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:24:46 +0000

    another Yy act paper push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
