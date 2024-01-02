3fc7bd33d2ed962f948a4a7ae134c870dd17385c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:52:33 +0000

    raise cry usual peaceful unite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
