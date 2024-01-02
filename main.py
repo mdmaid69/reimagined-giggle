0daea0e1bba8ef17dcb4add28fd3001ba5a4a493
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:37:09 +0000

    someone burn sign trade brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
