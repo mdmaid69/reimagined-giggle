4a2b5f181de6f30e3c8839525d74ad10977089b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:24 +0000

    little dish scare interest pool

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
