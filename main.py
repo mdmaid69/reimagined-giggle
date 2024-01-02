4690cac12b7a45b66b48ea27e4dba7e4e5581b82
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:26:09 +0000

    serve our learn little introduction

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print("Max:", max(numbers))
