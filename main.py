23935bd078a8b0690f55b60075046950e9449a20
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:43:13 +0000

    whoever apparently land approval sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def sort_numbers(numbers):
+        return sorted(numbers)
