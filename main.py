234e12ae7a201a48b4e179f7e2524b18f2605d3c
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:07:54 +0000

    position stay Tt top through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
