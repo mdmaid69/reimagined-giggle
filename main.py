6efaae4bb3f0f670e8d26f0f3cc9f59ebeae3c75
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:59:41 +0000

    shoot please recover mixture minor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
