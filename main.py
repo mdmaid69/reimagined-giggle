bf3c90cde536e1cd7f09288f247562a1d0de1cc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:55:39 +0000

    bag natural appear opposite plan

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def square_number(x):
+        return x**2
+def count_words(sentence):
+        return len(sentence.split())
