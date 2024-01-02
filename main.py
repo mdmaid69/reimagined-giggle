e85d86f3e3c66fe112d4be9e2cbd86de048e9c55
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:26:33 +0000

    thought will smooth Ee whistle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
