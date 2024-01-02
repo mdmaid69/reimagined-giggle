0b7ca1485549126d55337bbd69c7b39466376e11
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:58:10 +0000

    excuse medium contribute public smart

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
