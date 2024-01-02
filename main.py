597d280306afbe991958c4b0e6b57cedce34960f
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:17:59 +0000

    tradition size unlikely gift connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
