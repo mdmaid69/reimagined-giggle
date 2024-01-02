7035f01d38d65a427d3b76c6f6251fc59b2fad0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:54:49 +0000

    entry keepaway glad apart confuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
