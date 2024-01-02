b81ab7c255cf4211e3d32b336c9e4fbf4ec21712
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:54:16 +0000

    smooth quantity difference meeting best

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
