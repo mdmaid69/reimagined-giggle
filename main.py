6080699044dc9913f58ffcc0a05bbd00f6f41bae
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:46:31 +0000

    tell wide interview total confusing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
