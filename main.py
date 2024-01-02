dd15f4c68c1a1fdaf64fbf15f7bb2101b1ca7334
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:41:06 +0000

    transport honest earth pleasant really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
