61ac1e3fff0d28fd86f569522680684c9f3f6e54
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:30:17 +0000

    prefer ordinary might admire expensive

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def count_elements(lst):
+        return len(lst)
