d5212cb483adadf46698f3ec7a1a14fc1b5f5c94
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:33:54 +0000

    fear failure manner tight runinto

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
