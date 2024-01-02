750c52109a68fbec6827ab158b5d85acb89c8897
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:19:38 +0000

    excellent pronounce us heat shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
