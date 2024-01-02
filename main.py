3b5d9ec68543c23e81df3c54e8408dcecfbc01bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:03:52 +0000

    direct block attention change false

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def multiply_numbers(x, y):
-        return x * y
+def get_array_item(array, i):
+        return array[i]
