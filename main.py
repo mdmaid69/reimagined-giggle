ee893a5da4b19e571eb62b72727a1847f04d40fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:22:15 +0000

    freedom burst rarely reduction circle

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-print("Hello, world!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(lst):
+        return min(lst)
