5a23b670573ae7ab8dc7b0737189243b377d55ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:41:37 +0000

    different language record smell back

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+print("Hello, world!")
