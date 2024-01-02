834c0396697913dc2d512652741151655e47dc11
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:48:41 +0000

    train yourself makeup boring make

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
