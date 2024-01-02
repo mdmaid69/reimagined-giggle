fd5348599f7fa3a3c5d3edb1bf86c46a0fec39c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:11:07 +0000

    count blue danger period tonight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_hex(n):
+        return hex(n)
+from collections import Counter
+print(Counter("hello world"))
