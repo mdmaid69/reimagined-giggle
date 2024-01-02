3366a620443492b5f3269ccac8509d1959918a6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:12:23 +0000

    nerve behaviour copy explanation pity

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
