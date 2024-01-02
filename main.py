08f552d773e95c2b16cbe80307ba99c173e60b08
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:43:25 +0000

    crowd house test tear manage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
