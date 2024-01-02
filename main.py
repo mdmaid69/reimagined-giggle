5931d411ee1b2c112ac6e031dd4538e42886e72c
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:50:17 +0000

    wait difference generous degree pretend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
