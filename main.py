ee103f84b1470f4bf3b473deaf0e71b36d4cb6f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:46:00 +0000

    cool short event nice busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
