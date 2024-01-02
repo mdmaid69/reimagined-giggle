75c99adbcb342f1fac1ed75298ee583cb363e8ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:14:58 +0000

    original scream 179bite shoulder otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
