b1a975e2b7bd760904abe5e86f49034defe9ba82
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:55:34 +0000

    bring spot explain explain whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
