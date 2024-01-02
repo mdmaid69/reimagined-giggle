c7dcdd9e15685b91d941a3f55c0de4f4f4e755c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:37:16 +0000

    importance project guilty scare only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_pressure(force, area):
-        return force / area
+def get_array_as_float(array):
+        return float(array[0])
