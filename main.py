1e6b0634683eb330740021142608f79f5ce3bc25
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:23:44 +0000

    defend dance deaf unite seem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def is_palindrome(s):
+        return s == s[::-1]
