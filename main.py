059ae1415344f9a7b3500927b1469377fdfb71d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:48:29 +0000

    slowly right step sleep discovery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
