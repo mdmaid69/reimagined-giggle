c04b3a609c98c4d69fe9204a143463b81b8f9236
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:24:21 +0000

    twist raw importance part week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
