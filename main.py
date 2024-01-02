0d1e48009bd7df6d1f4c99cde4ab3c06080710cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:50:00 +0000

    train block start earth motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_max(lst):
+        return max(lst)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
