a5148f2960c53ca8b7cba826125732e4a86f22b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:07:37 +0000

    invite below issue measure release

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
