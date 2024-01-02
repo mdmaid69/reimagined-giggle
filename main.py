9e84690ac468ad18eb9b83326ab8621b6307b81f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:28:41 +0000

    hard book always other raw

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
 def calculate_force(mass, acceleration):
         return mass * acceleration
