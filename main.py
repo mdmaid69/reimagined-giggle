ad67e0d654b248bcb77f8492ed38874c59896a88
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:38:51 +0000

    aim hold cost adjective like

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_as_str(array):
-        return str(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import os
+print(os.getcwd())
