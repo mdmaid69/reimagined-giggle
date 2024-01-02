5931aa0115cbcdd3cb7747aea2a3e6f504f46b37
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:14:42 +0000

    wait distant rain warn light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_repr(array):
+        return repr(array)
