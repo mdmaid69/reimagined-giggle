74f0724b7d6f3d8bd7808c2351c93ed6a67a8cfc
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:46:01 +0000

    instant nerve plan plenty prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
