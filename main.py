b60a5ef26a124371d97e54010983a296e7da8d18
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:24:27 +0000

    closely cost health almost description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def multiply_numbers(x, y):
+        return x * y
