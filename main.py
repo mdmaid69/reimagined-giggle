099e5834082dee20596a762da5c517699283a966
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:34:41 +0000

    crash hurry conversation consider branch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print([x**2 for x in range(10)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_even(n):
+        return n % 2 == 0
