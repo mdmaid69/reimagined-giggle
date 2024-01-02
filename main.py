ac0b26c0b8623c6e12df87adafa3c60ab2ea21eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:07:45 +0000

    learn violent wherever expression please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
