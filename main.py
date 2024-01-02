cd5d87f4f953c1a3e41d382c910f08c877603107
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:31:29 +0000

    table mark smoke modern usually

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print([x**2 for x in range(10)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
