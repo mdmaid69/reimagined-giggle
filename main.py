abbbfa29584d7456ed81db1efdf4f00f13af2610
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:56:38 +0000

    ticket enthusiastic reach shoulder callback

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
