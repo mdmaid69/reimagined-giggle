deb792b7184da991ac714caf014c653276149b5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:12:14 +0000

    please equal worry during kill

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def cube_number(x):
-        return x**3
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
