1d3acab5173c0c7cb8a19810d673c2387df70984
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:59 +0000

    pot present Itself scared hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
