b9b05bb9bf43ab93110dd77bbb86030737605a72
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:14:46 +0000

    busy friendly address bend declaration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
