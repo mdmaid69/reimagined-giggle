c9ea5152dc8017ce617f05e88711f3463871eafd
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:19:53 +0000

    first turn guard about else

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_max(numbers):
+        return max(numbers)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
