5f0234c23b5c8c2b5e812fa823cd68de0414230f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:15:09 +0000

    meet teach ability reply choice

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_stack():
-        return collections.deque()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
