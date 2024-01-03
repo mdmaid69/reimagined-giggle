43c18cc1a3d4af37a5b974fabf61dc0b7138edb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:06:04 +0000

    less convinced declaration public delicate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_octal(n):
-        return oct(n)
+def find_max(numbers):
+        return max(numbers)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
