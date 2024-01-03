9e0a0603e29a6efe0ee7128d5914c9335d2198ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:06:17 +0000

    previous train blind fast work

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def is_even(n):
-        return n % 2 == 0
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
