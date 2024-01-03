4b1a38ad23dea40c971aa11c6a44e48fc2a12833
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:21:31 +0000

    past test trap exam ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
