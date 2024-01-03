755422a50c91521c4c5526884c685846595a8cf8
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:55:19 +0000

    responsible related beneath sleep bring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
