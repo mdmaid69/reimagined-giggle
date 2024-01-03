cdc9083c78861f7f5e36499fee607119b6a2cf3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:18:23 +0000

    earth anyone statement catch surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
