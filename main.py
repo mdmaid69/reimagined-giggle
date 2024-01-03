cb4ff0a5dde63cd0230bff212d708ae49685666f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:45:11 +0000

    side machine shut hollow proud

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
