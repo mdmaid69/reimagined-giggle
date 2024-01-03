780726af0f078f2bc324b3af0a94c9a3dbd5d570
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:54:01 +0000

    of work 566end plenty further

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
