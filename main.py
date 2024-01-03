9dd9185b8c06a08afc98fa1fc0af7d8181175e0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:15:09 +0000

    fire completely through strike get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
