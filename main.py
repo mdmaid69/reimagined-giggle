81218e8b53cf8f5eda8507eec2909ae6a068f5ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:42:10 +0000

    invite brother main swim work

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
