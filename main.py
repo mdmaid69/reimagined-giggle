9cbf67b2b6670bf2d5cefb84fef7e2dc1270fb04
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:07:06 +0000

    seem pupil behind sleep sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+def find_min(numbers):
+        return min(numbers)
