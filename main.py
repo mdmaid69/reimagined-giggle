e43e33f77fc446598484562faf23ec5a2b25a36f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:40:16 +0000

    number coat switch strike swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
