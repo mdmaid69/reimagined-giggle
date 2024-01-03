a27f020ca445efefa04e38aa974377ebdd44116d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:12:49 +0000

    contain scheme breakfast cheat question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
