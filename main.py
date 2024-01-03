5947eed160bee64f7c82c6a7208c7e000eb7e6b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:25:51 +0000

    brain table door start task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
