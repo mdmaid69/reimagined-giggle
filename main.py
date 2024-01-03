6b57c23a7692bd8d6187754d4872c388b4ec7086
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:48:57 +0000

    talk damp benefit your mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
