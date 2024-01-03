f7f4443888b63324b4b83dbefb9ea5c6a7f42340
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:49:33 +0000

    cut hear bar usual scene

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import time
+def get_current_time():
+        return time.ctime()
