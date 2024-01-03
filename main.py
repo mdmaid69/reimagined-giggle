b3bf84d122ef999823046a394c04c728232f033e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:59 +0000

    sleep unfortunate what punish produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
