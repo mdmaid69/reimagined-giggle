4e722913a00f99e1db2e604c3a08fdc1293c12e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:11:33 +0000

    act complete majority mother show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_circle(r):
+        return 3.14 * r**2
