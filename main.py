6260f640400df190ad622cc1f4def57f61cce52f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:13:00 +0000

    sudden lift burn wind once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
