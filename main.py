a168e7f9f89ce947c5d97bd6cb6e06fb43cacf63
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:58:36 +0000

    suggestion blame should beginning clearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
