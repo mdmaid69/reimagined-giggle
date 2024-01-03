84cab4017f171337c97df66f54c619c672cda5e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:20:42 +0000

    squash constantly voice direct level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
