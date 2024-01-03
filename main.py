2fe3714240a6e17c0c24068c99f2949214df5501
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:16 +0000

    typical swing wish Ll past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
