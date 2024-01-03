6c649f9c3bec9f62e8010bda1698b76314bb0125
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:47:45 +0000

    rest boil agreement immediately list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_priority_queue():
+        return collections.deque()
