e27b8773ba50b3df77b84138efcef2763104e08c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:05:31 +0000

    convince advantage show furniture suckme

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
