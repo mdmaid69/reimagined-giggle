ee5744300a63b0d728534fc8fadadfb595248809
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:49:10 +0000

    slight maintain surprise task within

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def square_number(x):
+        return x**2
