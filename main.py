abab27203d1aab8ec990ad8b092559b2ae48927b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:02:31 +0000

    consider holdon strike behave count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def count_words(sentence):
+        return len(sentence.split())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
