e5b9b4fd4d2ef43b59316ee852d582b499ef1cb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:40:09 +0000

    566end regard desperate standard official

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def sort_list(lst):
+        return sorted(lst)
