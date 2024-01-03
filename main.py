4047a11b58306c21e330eb78a7d2dc38692f0bb6
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:29:17 +0000

    sale admire claim 565end pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
