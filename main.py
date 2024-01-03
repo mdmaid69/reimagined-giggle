cbf77a1e9fa60c6d9655bbfd9badc2abc9322f38
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:40:26 +0000

    pronounce phone somehow pass appear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
