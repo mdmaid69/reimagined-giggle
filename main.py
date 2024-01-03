904f62e7aed0601d95c4a99e7e89a459916ca5ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:11:33 +0000

    beat doubt order ignore centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
