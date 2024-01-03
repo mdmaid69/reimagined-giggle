a50376dc160db02e2e16182f14fba3f23cc3d01f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:59:16 +0000

    nature solve match help defend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
