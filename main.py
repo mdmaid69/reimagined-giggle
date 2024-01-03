a47e88a342e5dbaf457e121e01e91e20ac31d33e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:47:40 +0000

    study judge fight fat lose

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-from collections import Counter
-print(Counter("hello world"))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
