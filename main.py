ab590e0eae9cc31d01a7f7fbf306ecaecb60f6cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:08:26 +0000

    breakdown independent takeoff several 568energy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
