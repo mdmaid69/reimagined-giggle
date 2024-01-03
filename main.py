9a867ee2eb2eb654267ab9a2065d527c5e995c40
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:22:28 +0000

    rob before giveout below exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
