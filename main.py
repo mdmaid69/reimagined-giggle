535e3e46fc3f9e597c3a780446d288c5ad7984cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:58:36 +0000

    hit punish adjective rarely fork

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
