5815bb91c97270d9c0d8e3a6c89790ebc34f2550
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:26:27 +0000

    once welcome public lookup tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
