ed6617b57734845e3563b5a81d6e433cdb5e00b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:45:32 +0000

    against shoulder although trick if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
