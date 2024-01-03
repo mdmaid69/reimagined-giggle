d2f9d21675acac6d16ba1ee632287a017b5d0f58
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:58:39 +0000

    owe scene bury fresh less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
