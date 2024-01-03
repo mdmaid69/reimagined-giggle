73b6964953304cc6cd1709d82e9673cc157c77bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:11:08 +0000

    slightly sudden thorough rank period

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
