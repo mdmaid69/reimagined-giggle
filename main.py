3c3aca233d78f2ca8c6081d0844afd6e0ffa8ccf
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:43:56 +0000

    getback temper list cut train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
