ea4a349b520cd6d796bd7b05c759ed3bcc07cdc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:11:18 +0000

    light peaceful objection exaggerate gap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import platform
+def get_python_version():
+        return platform.python_version()
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
