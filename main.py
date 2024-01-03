2d483b6a6a038f68d500338aaf8df78f19cc53a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:59:09 +0000

    comment each correct address bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
