7435ac08539436d4d9b46e1f2af931946685f87b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:21:37 +0000

    connected themselves again border uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
