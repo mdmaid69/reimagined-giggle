991d63beee86e24aaa41a191b134e658a7a827f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:20:46 +0000

    board be sensitive ability comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
