6af518092f575ec6257a4d07a1dfeac3fbe5b457
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:26:33 +0000

    border general along my personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
