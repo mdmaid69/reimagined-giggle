b01bce0539b4df3f330962adc36a57799a882e7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:38:57 +0000

    land sing demand business disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
