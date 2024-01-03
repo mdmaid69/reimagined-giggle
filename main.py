eadd9cfac117c3c7db443d57a7fe4919d701d726
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:14:32 +0000

    treat opportunity one order pleased

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
