2795120b190283092edf7c656ceb9c8c8ab67bb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:15:56 +0000

    hospital fail walk married thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import logging
-def log_message(message):
-        logging.info(message)
+for i in range(5):
+        print(i)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
