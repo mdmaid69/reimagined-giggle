8a94e30f140d77434d722a8956e5a0e86f9acf49
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:32:09 +0000

    little yours consult nor major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_volume(length, width, height):
-        return length * width * height
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
