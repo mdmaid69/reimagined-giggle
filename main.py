422d1d8cf40ca0be9b54388b9b92fa4e436ee66b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:18:20 +0000

    mystery join sentence completely friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
