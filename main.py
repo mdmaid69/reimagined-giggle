4e93b6e324c3f1edd512f9f74417eb21484cd9d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:27:00 +0000

    somehow sweet inside waste geton

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
