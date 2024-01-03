6a506037ea081a5b8cbb772100d7d662b93afdea
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:32:49 +0000

    interrupt 569enjoy turninto Cc makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
