1081371b736c5d98832bf4a7e8c84b33db56f5e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:40:46 +0000

    tear male back pick move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
