6836314558f63b94ab4fcbef1643a7b4f50ee938
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:50:30 +0000

    early funny advise platform crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def append_to_array(array, item):
+        array.append(item)
