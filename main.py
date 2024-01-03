3cf86ae9180047a0f5cc7f24b8aa9008cd76af7e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:10:26 +0000

    relief face hello such disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
