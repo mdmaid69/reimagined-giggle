7c140716a8570482768a6d44e7d1d8c6d889b07c
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:19:05 +0000

    trick violence better must pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
