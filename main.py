f49346557ed62d7c3f8463aff68a68e7955c878b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:00:35 +0000

    small keepaway dangerous confidence production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
