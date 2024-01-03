d696997d5bfc7f68aad490dab90cfbc2e3eeaeba
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:48:34 +0000

    movement shopping for along but

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_set(array):
+        return set(array)
