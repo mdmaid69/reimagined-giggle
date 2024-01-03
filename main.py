14f41a0426d6e510a27dd68e1a2f1a577dcc758b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:13:20 +0000

    best involve study mine pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
