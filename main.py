d54aeaaefd361c51ab88d743b601c40cbd1f3226
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:44:39 +0000

    average laugh swallow study keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def find_min(lst):
-        return min(lst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
