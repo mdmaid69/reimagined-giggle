e5a5293e169b4f8a45bdf0a0a86ea0a4076a0d0a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:30:15 +0000

    profit something violence official early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
