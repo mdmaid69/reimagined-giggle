8b8a3b066d80ae1ba2f9430f0a78690a40bc15d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:05:09 +0000

    passage along board thumb complete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sys
-def exit_program():
-        sys.exit()
+def find_max(lst):
+        return max(lst)
+def find_max(lst):
+        return max(lst)
