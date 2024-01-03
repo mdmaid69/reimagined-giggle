7e375bd981a171a1aaeed22b22a2ebb8e96edda4
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:42:42 +0000

    contact serious boring suggest sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
