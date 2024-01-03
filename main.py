4c5200fd68c2fc231f4720c40fc5c00ad9007c71
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:29:13 +0000

    manage load dear address act

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
