9d6d8d63d5718b5b4cb49d40d24a841dfff2fe47
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:35:40 +0000

    concerning question home raw match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
