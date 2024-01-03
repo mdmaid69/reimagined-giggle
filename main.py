9f72676adcdca29db3eba54f8712465032d0360a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:45:51 +0000

    country exam lot quiet something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def multiply_numbers(x, y):
+        return x * y
