cfd19774c32f2c0b554574560e6f79ceb4531be1
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:21:01 +0000

    not spin way ability silent

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_max(lst):
+        return max(lst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
