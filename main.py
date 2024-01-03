6414b19095fbfdf9d93670ddd6fe1ebb0fed1185
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:38:34 +0000

    eat detail direction method watch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
