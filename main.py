105f88766b74dd696de063fb540c8959e61ab87c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:45:58 +0000

    study mend limit bury call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
 import os
-print(os.getcwd())
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
