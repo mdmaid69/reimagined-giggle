841a41b92667b1da0b0e5ca49e50f6c709ec9ae1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:19:32 +0000

    left direct immediate project sorry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-print([x**2 for x in range(10)])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
