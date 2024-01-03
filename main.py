3d517b70be62d7666f8599c78a0dc55c15a271f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:09 +0000

    fill noone right male shake

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import platform
+def get_python_version():
+        return platform.python_version()
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
