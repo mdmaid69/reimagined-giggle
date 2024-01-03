574526afe2aa98b55a95ffdd2428515dbfbe527f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:20:11 +0000

    respect matter same sign perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
