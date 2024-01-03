e423d2ec3d57f1b21df93544bf9b36b6d1dfb15e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:11:31 +0000

    car borrow light 565end keepout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
