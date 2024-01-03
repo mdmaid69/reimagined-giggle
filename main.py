4ca8fde70ca84681229a64ef6b2794ea04d512e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:42:38 +0000

    increase since cover roll everywhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def is_odd(n):
+        return n % 2 != 0
