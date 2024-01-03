0fec82416d2efdb0dde5184a1a57bc7114de9170
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:13:53 +0000

    us gentle connection worst workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-i = 0
-while i < 5:
+for i in range(5):
         print(i)
-        i += 1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
