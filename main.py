ad167b1afd8b6110a5b4e2cfba6d56235222f24a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:13:36 +0000

    do chase promise escape reduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
