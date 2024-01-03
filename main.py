b0202add4bafedea71a8f4d861fe4f04a49c490d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:56:25 +0000

    at run classroom either figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
