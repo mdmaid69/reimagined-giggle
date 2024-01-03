4b71539808edff0b5c397f60f4b5b8c3a8a3925a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:02:01 +0000

    fold exist false insist off

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
