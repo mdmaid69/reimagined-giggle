feccea41c40a3cbd6352768d5522285c788941f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:06:45 +0000

    sing them file record steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
