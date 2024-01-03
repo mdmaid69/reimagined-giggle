5d8a39e59b2dddf9d8eee443c455b57b4399423d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:24:48 +0000

    big within repeat understand emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def insert_into_array(array, i, item):
+        array.insert(i, item)
