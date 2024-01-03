78b090b90d184ef4621539790f8c551a99f8712b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:43:03 +0000

    powerful trick quantity performance trick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import numpy as np
+print(np.array([1, 2, 3]))
+def find_max(lst):
+        return max(lst)
