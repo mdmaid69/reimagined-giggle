b76b7d4a0d4a3cd092a17528b140acc4014ad3f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:54:21 +0000

    disgusting rise quality collect beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import time
+print(time.time())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
