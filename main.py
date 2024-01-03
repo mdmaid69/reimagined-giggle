acdfa26afcd217057fb608c613b82aef3db1a749
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:54:10 +0000

    weak this proof vanish tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
