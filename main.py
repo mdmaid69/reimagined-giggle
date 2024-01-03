5ac5eef02b62f0ee161d30153a2ca8f10e48c350
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:59:22 +0000

    match cap soil breathe notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
