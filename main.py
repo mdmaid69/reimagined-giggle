7f0818aa1aa0caa8aba2eafec243b1a9a242c3e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:59:56 +0000

    rare book react rush quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
