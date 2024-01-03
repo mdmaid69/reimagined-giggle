92eb2e3b95e849c6ed4541a9175177f3c2f392bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:58:55 +0000

    continue entrance let acceptable help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
