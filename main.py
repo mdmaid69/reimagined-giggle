001a4f596e6be8ceaf866ab5592909880c733747
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:13:39 +0000

    this old fix drive schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
