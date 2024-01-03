bb55d4a393003d990d811afe36753ef8f7a7cfd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:33:24 +0000

    duty 179bite giveup sing freeze

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+print([x**2 for x in range(10)])
+import math
+def calculate_floor(x):
+        return math.floor(x)
