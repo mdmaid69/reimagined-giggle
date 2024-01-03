e6c173e313f7004c6856208ab2ed1f945b99df2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:57:32 +0000

    crowd drawing permission trick connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_length(array):
-        return len(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
