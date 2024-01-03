43c336a0480b334da14300f7666701f820559de5
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:59:36 +0000

    attitude event concern boil central

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
