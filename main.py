63a369dbab93d7fd793f7dcaa9fee27e4517de8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:08 +0000

    tell similar yesterday lean authority

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import datetime
-def get_current_date():
-        return datetime.date.today()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def count_elements(lst):
+        return len(lst)
