c0495e0ce9cbb2226929e964fa5839702503684f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:42:11 +0000

    fortunately cheque appear suggestion rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
