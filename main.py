36e3a753b4ce6bd6c848b2395a65259373397f0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:57:39 +0000

    lookup cry buy hide search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
