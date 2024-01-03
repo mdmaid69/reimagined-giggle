95704517e22401083ed832d2ac4008b0e6281b85
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:55:01 +0000

    find failure realize right answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
