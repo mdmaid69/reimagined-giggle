e32cc3da81cd6ed44c7801907991ae1a17525743
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:30:11 +0000

    radio blind reason classroom matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_binary(n):
+        return bin(n)
