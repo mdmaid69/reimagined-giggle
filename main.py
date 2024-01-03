2196dfda47ef80b3f3b2c1015be03577afe68d66
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:04:57 +0000

    allow trap we squeeze soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+print(math.pi)
