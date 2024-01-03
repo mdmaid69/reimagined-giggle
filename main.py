39d8f25e0d187b79c9f16301556b34897a73384b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:38:18 +0000

    backwards mass uncomfortable attention back

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
