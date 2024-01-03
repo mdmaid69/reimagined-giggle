bc91a3e5783aced52b1d5394e2cce41030b540bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:53:49 +0000

    pocket across disappear keep fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
