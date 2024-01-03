5c8cffc072f44b92d038ecad2d3e1b58cd1645dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:06:32 +0000

    serious benefit contribute alone justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
