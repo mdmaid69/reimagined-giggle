6e70154d12ebb3d48f5ba58b76a6877b3384223f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:05:26 +0000

    pot bunch behind force power

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
