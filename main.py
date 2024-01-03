48fab96569a2bf72453b37a81187cb94ca22907a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:50:24 +0000

    genuine runover invitation forward healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
