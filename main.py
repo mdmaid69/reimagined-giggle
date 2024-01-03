cf78d93e675b8e0582c914ba72e4f1ccc2ea5a52
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:24:57 +0000

    defend compete position real onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
