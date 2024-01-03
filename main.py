0239c87e80e33a60962021fd62fa8f62f1dd49e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:48:45 +0000

    region follow through protest along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
