952b5319c1dccd728210534ab74c23d0366d9069
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:37 +0000

    flat treat pretend terrible fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
