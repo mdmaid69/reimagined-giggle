b6a206b6e15a3871feadaa4964201efa127e0097
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:42:02 +0000

    direction fail receive do smile

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
