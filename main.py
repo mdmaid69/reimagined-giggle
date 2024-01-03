1439b45e785a78ea722cc9e8aa692604ba9cf9c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:11:38 +0000

    mouth even at satisfied curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
