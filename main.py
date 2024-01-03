b5be2ecbf4cb50009fed771d8bc7197986021f9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:30:35 +0000

    watch plastic ground petrol smell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def find_union(list1, list2):
+        return set(list1) | set(list2)
