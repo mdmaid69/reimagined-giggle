200438fdf6024deef97d002dd352f65cc71d1b2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:21:19 +0000

    fear tough lost meanwhile laugh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import platform
+def get_os_info():
+        return platform.uname()
 text = "Hello, world!"
-print("Reversed:", text[::-1])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+print("Uppercase:", text.upper())
