5f62175c06515c708a7a952e297ef516bfc66449
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:20:31 +0000

    violence rent command really couple

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
