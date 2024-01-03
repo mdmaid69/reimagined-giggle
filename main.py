3accedcbba997bfe2a5039d072ba234422ad5bce
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:16 +0000

    pause cut anyone sale passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import collections
-def create_counter():
-        return collections.Counter()
+def get_array_as_bool(array):
+        return bool(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
