e488ae22edcb12dcbf3f58ab4aeae2626ace59ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:26:16 +0000

    across system breakdown burst or

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
