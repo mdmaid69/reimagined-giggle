65ca1e5785281eb7e6e13add0805fd84f1aedd00
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:43:08 +0000

    excuse over find unpleasant niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
