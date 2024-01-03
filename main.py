5f78339c9465e2470ce20e5a21cb70b78cb7c4e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:56:50 +0000

    tap church I everyone see

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_acceleration(speed, time):
+        return speed / time
