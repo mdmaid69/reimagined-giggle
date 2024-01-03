09b910a07115067bcfcf9192f6745494a891078a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:04:20 +0000

    up left congratulations oil refer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
