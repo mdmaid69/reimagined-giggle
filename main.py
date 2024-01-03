7737ecd146b12c331ac4bb8073a3564cdcccd91c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:51:01 +0000

    grateful tidy story above card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_as_bool(array):
+        return bool(array)
