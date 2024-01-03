3b04a713b3cb4e381871a6c1cdd4eb03d935fc58
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:52:55 +0000

    minority only direct spot climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
