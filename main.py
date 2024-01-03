8ab4950797609201fcd173eabf42e829e1402ea1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:38:24 +0000

    patience agree quarter arrival company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
