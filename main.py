c69a66838b811b79bc2ddfd50cd81f1088136c02
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:00:53 +0000

    present do cut Mm change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_acceleration(speed, time):
+        return speed / time
