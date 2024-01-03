73c099c07ab70ecbb61a395a63279862aacd1ee0
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:52:36 +0000

    exam loud follow direct slice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
