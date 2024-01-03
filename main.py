193f7a47bbdebd64bee1e98e53c88237ecd67f9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:53:05 +0000

    hurry really number edge remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import sys
+def print_python_version():
+        print(sys.version)
