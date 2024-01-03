495da2b4b42a975d472e5d186c2e5f33999e4da2
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:56:33 +0000

    suffer speech more polite Oo

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+print(math.pi)
+def calculate_power(work, time):
+        return work / time
