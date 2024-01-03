7bfffa41fae6464a05c41d62ae4b13f9d2ead359
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:27 +0000

    across judge change special smile

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
