74ee9951f20c461f9fb75eaa27a8002a292b9b4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:38:54 +0000

    equal medium trial decide phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
