e758581a504d005b779c0559bae41be8759dbf9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:11:21 +0000

    pool impression unlucky direct already

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
+  def cube_number(x):
+        return x**3
