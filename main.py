c6f1395f695ab44c1f5e749e909f533334d5f725
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:39:36 +0000

    pause moment communicate question seriously

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import sys
+def exit_program():
+        sys.exit()
+import sys
+def print_python_version():
+        print(sys.version)
