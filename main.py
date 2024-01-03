1741cb5eb44dd03643c9b1160f72289e156a51bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:09:20 +0000

    tear show disappointed demand central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+name = "Python"
+print("Hello,", name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
