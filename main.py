4f1646a9c3bbc5550581f8cb75229e45f65339a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:36:05 +0000

    owe free pause impression passenger

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
