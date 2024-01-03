8905c638fdb48a4cc273961e2bb0c73ce51846d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:41:55 +0000

    further influence emphasize stiff female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
