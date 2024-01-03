5d21bfd718e9b53f31b36055874950cdcc1bffcc
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:00:07 +0000

    hollow swing much now fair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+print(sys.version)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
