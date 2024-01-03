3d8116e09454937b086b1549c6c52b48e5b1c18a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:22:55 +0000

    together impatient combine damage generally

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
