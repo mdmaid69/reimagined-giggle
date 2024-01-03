6c470d9b5f6a6896e1e5ba3493347bd214e9f3be
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:25:50 +0000

    advantage unhappy sink boil responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import time
+def get_current_time():
+        return time.ctime()
