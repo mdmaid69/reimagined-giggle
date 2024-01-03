15b79341d10c78c9ca5e50a45bf125feeefbbc9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:47:15 +0000

    classroom charge next same hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
