f6286c592adebcf0b94fabe7638ab70e5255a1a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:38:33 +0000

    turnout beat lot enthusiastic apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
