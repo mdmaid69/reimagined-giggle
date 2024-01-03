2e109550311b17a838ae901e1aa1ec2d89f6a280
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:01:13 +0000

    bye century rely lead bang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
