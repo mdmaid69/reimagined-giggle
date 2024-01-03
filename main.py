1b9f012ca13c5ca9ec1c02ad182d9b7e8ef71099
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:23:51 +0000

    direction previous centimetre classroom explain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
