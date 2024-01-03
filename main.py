e4850e62ac7947b0a9bf668afa0d2be3afed3a42
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:30:49 +0000

    occur legal month combine task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+print(math.pi)
