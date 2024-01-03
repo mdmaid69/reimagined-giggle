a2bc663e26df6602659ccfa112d16e4d8c44522d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:16:37 +0000

    top strike for convince lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
