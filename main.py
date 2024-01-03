7080a2a390dd06e46a969c48875beaecb22dcaac
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:41:41 +0000

    accidentally begin moment clean immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
