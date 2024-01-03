149752d5141c84e66bfe78def499718511482771
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:14:25 +0000

    accidental close start return shock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
