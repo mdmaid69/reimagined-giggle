441dd302a46768db5a871613d0e2fba9f2ccfc9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:20:46 +0000

    come address win neither front

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
