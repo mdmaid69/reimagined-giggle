8b89758aef4a3754f1a6a571c479d7a9cee59809
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:03:13 +0000

    one Uu ashamed its calm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def subtract_numbers(x, y):
+        return x - y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
