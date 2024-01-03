8a8a887b497e9209fee7598e90e34a0cb53d86f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:58:17 +0000

    convinced emphasis copy glad change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
