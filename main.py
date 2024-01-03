ea97d8530e7d58df9160e6f64fe7e003d76b9153
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:04:21 +0000

    like duty measure record judge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
