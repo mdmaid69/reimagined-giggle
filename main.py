d03c8709e761d64cfbab23a021078bd603078122
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:46:59 +0000

    zero provide return long autumn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
