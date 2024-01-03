7eb803d71b388768e5bfda2be9df25d1c34c7367
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:23:50 +0000

    lay cough persuade small sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
