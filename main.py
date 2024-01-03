ccaead44f390993808ef8af111bff998c17d4fb6
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:12:45 +0000

    clothes classroom safety still welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
