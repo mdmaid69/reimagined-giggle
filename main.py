fd0bb9cabd0a66464a4be675a998433f2e02ba3e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:18:27 +0000

    pass sail off base quiet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
