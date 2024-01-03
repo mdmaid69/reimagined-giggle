fc0ba3fb695808398eec6391baac06479d18f47f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:56:51 +0000

    shame passage her bottom where

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from collections import Counter
-print(Counter("hello world"))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
