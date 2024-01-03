5cdd5bed1a82cb1b7c1edfc834399c4fbd564b6d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:58:43 +0000

    best quarter advice friendly general

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
