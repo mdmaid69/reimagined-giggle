75b51f9e0062b7d256959b86fd8ef22035a8632b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:04:20 +0000

    duty bother rain hide again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def convert_to_binary(n):
+        return bin(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
