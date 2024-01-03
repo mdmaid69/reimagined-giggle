25cdb9293ea91df5a7a497d7f40ad7c415cddf23
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:35:39 +0000

    but any fun fear warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
