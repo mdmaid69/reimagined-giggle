c52c45b67420557faee8ea918bf67eaa82e442a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:59:52 +0000

    glass play month popular arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
