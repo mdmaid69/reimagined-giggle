04558a78a2ce41a642994ba410a9b8443d6852e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:55:10 +0000

    either concerning follow glass emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
