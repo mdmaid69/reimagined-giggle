0f71d864e1d871013e98fa58f3837ea4424cf93d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:35:09 +0000

    whistle now careful taste computer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
