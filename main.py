65fb8e0b01e815c7efdbb64529c8b63b24e2b92b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:01:37 +0000

    please add scare everything unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
