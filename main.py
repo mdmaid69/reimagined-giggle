ff069acb3b5ff03d3fe38463cf4d33cdbe558976
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:53:11 +0000

    dish prize character dark wave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def square_number(x):
+        return x**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
