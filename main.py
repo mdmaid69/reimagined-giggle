07f08af4b0dde10191f762eafcb952bc690ce8aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:46:00 +0000

    press crowd waste wear steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
