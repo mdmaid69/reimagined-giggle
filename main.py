f322d2f78828acb0d4216dedeede93627ac3cc86
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:37 +0000

    talkback opportunity safe offend disgusting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def find_max(numbers):
+        return max(numbers)
