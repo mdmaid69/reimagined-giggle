64ebb9ced2b51ff51900964b2ecf7a9a6106430e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:50:31 +0000

    shock nose tip equal spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
