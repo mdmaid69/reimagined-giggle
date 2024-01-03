c422f39432df115562027f1595851eb4349beabc
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:08:33 +0000

    intend forgive than appear storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
