850def9dbc70bff6e6b17bd150eccee075420bc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:45:09 +0000

    free properly start term answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
