af32d30be5d60d290a6d28de7e0f33b3bf95e60f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:20:23 +0000

    flat score risk recover line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
