b21f7f954ec94525f156d679ae899bab35ff54b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:34 +0000

    deliberate between mark himself cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
