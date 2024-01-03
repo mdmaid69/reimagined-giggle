92c42834064ae70ead3024da616853601e478343
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:40:11 +0000

    bite copy second lesson arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def convert_to_hex(n):
+        return hex(n)
