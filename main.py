07b963a083cb4f5c150a86fdb3afbd6a3a7fe735
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:00 +0000

    taste kill total on may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
