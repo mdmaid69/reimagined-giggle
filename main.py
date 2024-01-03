3a2265c4d2db3cc81bcf03a35febe00d271620cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:41:44 +0000

    appear image complete relative seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
