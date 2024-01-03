8dcab820a3060454af5edf8dc68e343c9c342f2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:06:24 +0000

    average position fear measure ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
