b5c4302b414ebb7396b8a396c677135bc3b80ac6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:42:59 +0000

    Ii number appeal lookup post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_user_list():
+        return collections.UserList()
