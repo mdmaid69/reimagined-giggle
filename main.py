715ef9b1b87794bf4f5d89066aea44450f16d6df
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:20:07 +0000

    related may opportunity swing can

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_error_function(x):
+        return math.erf(x)
