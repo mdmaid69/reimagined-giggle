1b960d02d1ae883fa34751ff3ef640c894f053ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:01:49 +0000

    upwards poor however pronounce live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
