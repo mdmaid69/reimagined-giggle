298c443cfba27dcf58c7908f5703ef4418a98a9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:12:38 +0000

    fact respect easy hello generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
