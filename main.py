63fc9ba88f52a82cadfbd7c45462e77d0bd521c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:45:18 +0000

    kind gate regular wheel regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
