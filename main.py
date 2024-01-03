ce7a28b9dddc9bc65fe2f3259846997912259334
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:23:33 +0000

    warn would hers tense thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
