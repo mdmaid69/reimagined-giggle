2dc1268909c69048bb9dc17ff4166b1a40fb1733
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:04:38 +0000

    late contain pocket easy feed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
