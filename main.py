65375caba022aa309b14fdf8bbaca04fbf84a1eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:21:17 +0000

    practise solve secret powerful exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
