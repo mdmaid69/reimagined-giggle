f4e40b4aecd0b4dfb94b6a293678011130bc6651
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:28:09 +0000

    remember meanwhile influence judge stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
