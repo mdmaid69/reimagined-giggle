9cde44fedaef2b3ad9934f84d7a3ae238a01e985
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:08:46 +0000

    action whether comeoff lonely rid

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
