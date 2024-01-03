8478053582d068dee14b579098ca7e55364073f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:01:45 +0000

    choice sure steal general confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+text = "Hello, world!"
+print("Characters:", len(text))
