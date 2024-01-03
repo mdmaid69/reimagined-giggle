2d5479d8a2423114c3411783456388a2dc70eeea
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:59:07 +0000

    better to cope body near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_list(array):
+        return list(array)
