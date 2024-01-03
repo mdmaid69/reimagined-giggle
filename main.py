71da04b9338dec87d59d8a0e2e2a98c36b4f85c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:35:45 +0000

    effect feed lay science will

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-print([x**2 for x in range(10)])
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
