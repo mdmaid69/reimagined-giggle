2a4ccaabd2132788757f5b236431b6a06fa92fb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:58:54 +0000

    actually test mysterious separate but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
