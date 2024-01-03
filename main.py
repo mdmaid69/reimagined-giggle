522e1ff05b53d01527ef84c39548990b37c30ba2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:15:30 +0000

    force hope distance certainly ask

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
