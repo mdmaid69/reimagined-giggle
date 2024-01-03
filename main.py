efda4e043650987241d58fc43e4b6d8b38bd8ba3
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:38:02 +0000

    deep platform attempt willing lock

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
