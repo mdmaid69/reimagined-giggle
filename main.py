3cddff190549f3b4b99467073c3b9f8cb8a065b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:43:04 +0000

    gift luck deserve thankyou software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
