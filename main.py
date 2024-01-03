20648d9daf9b68285e6ece2330c0f8ce8bec82f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:44:57 +0000

    complaint pity talkinto sharp appeal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def subtract_numbers(x, y):
+        return x - y
