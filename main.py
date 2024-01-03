79b3cdc3b880e24bc47f2e50eeed8aa49f0a6ec3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:17:38 +0000

    advantage contrast naturally woman OKorokay

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
