386a31c1565bdc274ebbfe79b66089040654abde
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:37:06 +0000

    shopping forever work legal drink

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
