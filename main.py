ce895196567cfab101762d5c8c846a467de47d67
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:50:45 +0000

    view rid grade flow 568energy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_words(sentence):
-        return len(sentence.split())
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
