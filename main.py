7f0b0623ff45d6c33d19f83b780d5a3a1c7f8932
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:23:58 +0000

    work far important shelter measure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_length(array):
-        return len(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
