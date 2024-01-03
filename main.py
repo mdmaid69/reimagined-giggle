e88b9f3c5d69a52a5dd74180f75c1657c6ffe8c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:41:50 +0000

    tap home last tap shout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+print(sum(range(10)))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
