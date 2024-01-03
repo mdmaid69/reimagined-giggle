a0a73583128abc0a886081ba01850f444a2fd1ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:29:23 +0000

    emotion modern ago lookback(on) wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
