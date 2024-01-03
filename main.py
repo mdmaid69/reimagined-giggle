14a00822c8a890beea95495bad02bf7523bd88f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:54:23 +0000

    worried family habit should refer

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def square_number(x):
-        return x**2
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
