558af335283d1b1cc9b9a7f43658b93142c6bd76
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:14:18 +0000

    boil tour pretend awake fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
