6bdd81fe863a05a5e8fff5fd412c691d51c1cb6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:46:27 +0000

    little Cc lonely pass hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
