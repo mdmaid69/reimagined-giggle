f15c2008fddc05143d215b3c6dd82d11c1d4f11f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:29:00 +0000

    next whisper protection Ll wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
