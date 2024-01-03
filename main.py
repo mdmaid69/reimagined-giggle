6f845541d71c7aca9eb6df21324a04599ad2d4ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:30:23 +0000

    whenever may year happen height

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_density(mass, volume):
-        return mass / volume
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
