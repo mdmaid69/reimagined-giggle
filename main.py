fa9808453f844542885a98b64f67fdc48f1a1dd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:09:12 +0000

    satisfactory bread watch plenty bury

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_list(array):
+        return list(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
