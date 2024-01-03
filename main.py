506a171b0369fca435b36ae2240b47bee0b94311
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:39:22 +0000

    everywhere account suddenly face damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
