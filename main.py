318ba7df9c26cd367e340d43719a66e0add8dec6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:25:33 +0000

    stay pretty loan spring block

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def cube_number(x):
+        return x**3
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
