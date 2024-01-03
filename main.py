67c1247e18fc149c2449f44c40a6f398c81e84d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:18 +0000

    dry holiday check get stop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import array
-def clear_array(array):
-        array *= 0
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
