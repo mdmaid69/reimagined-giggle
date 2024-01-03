66dbd340801616fd51ecd8fcfe207bec46da1580
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:57:09 +0000

    cough callup modern sense reply

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def is_odd(n):
+        return n % 2 != 0
