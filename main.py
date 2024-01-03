62bf36b74c71257620c1e84bd74d674d9b12f909
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:20:25 +0000

    Zz wind scare character within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
