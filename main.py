3a980408a7d2994db582f28905d9c16809f5a0d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:54:17 +0000

    minute message high instruction drive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_list(lst):
-        return sorted(lst)
+def find_max(numbers):
+        return max(numbers)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
