a4ecd2f1f78b7fca3a7cf22195eb0831cac4df5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:19:13 +0000

    unpleasant switch there miss side

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
