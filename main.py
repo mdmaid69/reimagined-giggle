7554cf2a624c8db2f373b89046e7464aa2ad0784
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:10:37 +0000

    considering rain save walk sign

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def sort_numbers(numbers):
+        return sorted(numbers)
