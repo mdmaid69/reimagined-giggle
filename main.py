ea9bb5412bb9e9132700f5c119358b7b641c5066
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:39:56 +0000

    frighten move ordinary bad along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
