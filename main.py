f7c37147456b037ab0ebdad4c8ecc0b1ea5d581a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:12:01 +0000

    promise error suck ask unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
