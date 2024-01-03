2d8d2f5d2f64c1f6a681d17a757697f2cea3d96f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:29:31 +0000

    excuse minor sail roll yes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
