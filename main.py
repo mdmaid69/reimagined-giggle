6012dde89178a526f90ce896ad7296bc9939184d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:11:34 +0000

    whistle injury anywayanyhow keep earn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
