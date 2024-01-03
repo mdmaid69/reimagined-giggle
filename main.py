2e279146322228b470d7eaf5a5fbab87c354212c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:49 +0000

    recognize hard search correct provide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
