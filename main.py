731798757b667a3e111fe3d2aec9f9eaad998cf9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:41:37 +0000

    afford city hit change miss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
