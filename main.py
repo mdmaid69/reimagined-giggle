33a46e59e624c056bfa6815b307506fca26958e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:21:49 +0000

    hospital deliberately happen reject under

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
