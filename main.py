f7aace41193ffcbf1334b3a460899e25c56f9dfa
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:03:41 +0000

    such file place crazy bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
