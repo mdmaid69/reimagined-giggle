03e20264524931108dfd00c45b4cd2660bf7b538
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:38:31 +0000

    possibly glad lonely trap fortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
