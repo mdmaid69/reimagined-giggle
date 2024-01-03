00b8a51c88c922b76eb46c240dd17b3de9d3ad23
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:12:09 +0000

    pause part separate declare burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
