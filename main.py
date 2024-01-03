e3542d23d1abd272ab9ef8b39715288147dc9685
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:07:56 +0000

    easy deep certainly flow be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
