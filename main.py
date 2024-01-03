3cd941fcd4cfa5bd946ea5461e721d8f58aafc5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:12 +0000

    happen system freeze cold imagination

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
