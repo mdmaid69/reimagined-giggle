dbea0f0b91d956fee6e68c10389faa2305e590cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:55:07 +0000

    narrow mix divide habit door

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def remove_duplicates(lst):
+        return list(set(lst))
