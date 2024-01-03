6568f37d7cf691459ae47061900c3316e098e4a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:57 +0000

    impossible actual alive turnoff see

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
