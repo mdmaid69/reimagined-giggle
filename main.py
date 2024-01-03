9ec738ab3308dbff2fa7b9ae8b71986a607f90d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:54:58 +0000

    well miss record fight no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
