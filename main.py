7431e1f1087f10c444d2e4728851e025cac8b797
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:48:25 +0000

    cheat breakinto head trip chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_work(force, distance):
-        return force * distance
+for i in range(5):
+        print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
