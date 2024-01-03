e2bf5084bdc40d85676eb81848eff2bd52aff0d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:01:22 +0000

    disturbing exit makeup shrink satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_as_set(array):
-        return set(array)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
