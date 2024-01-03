d0a7fdd1f3ed95698ad94fcf6b31297b209d8f3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:48 +0000

    manage lower lunch stiff mistake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+print(datetime.datetime.now())
