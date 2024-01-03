f769612ff9ae513df4faa506bb7a5a0698cd9556
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:49:04 +0000

    watch best in usually invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def square_number(x):
+        return x**2
