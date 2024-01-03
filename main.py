08fb08a68a57dd701dd68128b567864f736a05e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:14:54 +0000

    company type majority prize wander

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
