e060f78341989403b0d6a0d987c231798edc5ab4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:29:29 +0000

    describe otherwise balance takeover occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_queue():
-        return collections.deque()
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
