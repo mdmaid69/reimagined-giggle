c25e14f5f68f21d75f3abf2043d5c11965762718
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:31:54 +0000

    walk question would at sweet

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_list(array):
+        return list(array)
