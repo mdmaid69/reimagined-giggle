4b7ed19d7f1d8de8e60b0de441e6950e7d9faf11
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:07:36 +0000

    suit listenfor suck around most

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_typecode(array):
+        return array.typecode
+import collections
+def create_counter():
+        return collections.Counter()
