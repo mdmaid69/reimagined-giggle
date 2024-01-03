9ba3561bbebae2c2cd75786639a1d7e96b83dc8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:09:43 +0000

    travel club difficulty colour pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
