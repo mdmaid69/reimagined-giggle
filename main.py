e2139388ff1c8adeea53004e020a9e9855dcc2d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:03:50 +0000

    tear point tour left spoil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
