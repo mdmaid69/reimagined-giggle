83d0302e03e6477c62050655b908fe6e98abcc1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:57:47 +0000

    sky corner watch mind development

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
