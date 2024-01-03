91eb8c0209e3b4f90bbc9d28b200d2129ae8b2c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:17:23 +0000

    sail petrol grow play preparation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
