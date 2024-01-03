fcca7f57287233e8a82bf16e3099a815031ab664
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:30:26 +0000

    class opponent weak disappear turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import datetime
-print(datetime.datetime.now())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
