9aa851d12c73f7441b7e1fa570cc55e478cd4804
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:10:34 +0000

    close read bush exercise tape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
