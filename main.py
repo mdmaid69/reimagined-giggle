6f8c5bd18449bc4cad52b8ae310f9d63a9d5c238
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:36:41 +0000

    crash of disappointed past callout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def sort_numbers(numbers):
+        return sorted(numbers)
