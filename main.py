3e3d18a9578e4b708f2ac4c42764786aa1345dab
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:44:48 +0000

    keepaway mine father matter especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
