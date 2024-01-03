cca79062b5d492641a99fe5c90ad09d44085b10b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:14 +0000

    comfortable especially walk mine use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
