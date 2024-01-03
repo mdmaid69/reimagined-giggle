ab168241226dff08ea4221da448e4dcfe46c025f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:47:25 +0000

    crowded keep recently gentle getout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
