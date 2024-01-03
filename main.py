1ed528e9cbca7608acad2ac417c5fbdba7bcd48e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:38:53 +0000

    science reject getout blue hate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_current_directory():
+        return os.getcwd()
