cdba81d76aa4a4b94eb4e13603e5eb95d7111c04
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:11:08 +0000

    command floor responsible plant sister

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
