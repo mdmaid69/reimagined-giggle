7770ba6644baf8f01c9858c04417d54449550227
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:31:31 +0000

    between keepup present for hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
