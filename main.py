f20cecce825f21b949d4bebd41bab0fa58dba683
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:15:58 +0000

    fast lesson test mine sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
