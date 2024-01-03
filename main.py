f0eedf78076e0f380dfe2a20cc49978bf47fa8a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:07:00 +0000

    drop morning fat sister brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
