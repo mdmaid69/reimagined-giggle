5ac271c0cf8639943bc2f2b556ae9bbf55f654c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:40:37 +0000

    spoil evidence nonsense arrive would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
