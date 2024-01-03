8e9a841e66c300b90943cc9a121245dc2dba81a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:20:57 +0000

    be extremely typical touch treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
