72332d7bb9a75fc93cbeddd6636de64c3e409164
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:18:27 +0000

    throw want date job base

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def append_to_array(array, item):
+        array.append(item)
