941efea21852172c6206b174069b345243285cf7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:10 +0000

    expect invitation instrument method acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
