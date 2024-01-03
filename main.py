b82e1dbb8a115a9a09ba98e1fca6427b582dd158
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:55:19 +0000

    exit school guess trip strike

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def calculate_area_rectangle(l, w):
+        return l * w
