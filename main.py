af401e58ba06d3216a40de8c418409d95c1f07b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:10:43 +0000

    key tap getoff famous home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def cube_number(x):
-        return x**3
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
