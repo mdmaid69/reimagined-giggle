d30192125c90a7dd56caf4e7960c4b7213d32bdd
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:09 +0000

    turnon nonsense wipe ticket minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
