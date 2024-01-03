b6ee6ed350237bd58f5f0e84a6d99687b00496b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:55:11 +0000

    press blind strong personal talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
