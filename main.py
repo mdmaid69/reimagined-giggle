c23df6c1894332270c0ab46bf71baf54f043a4b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:01:36 +0000

    them hurry hit emphasis issue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
