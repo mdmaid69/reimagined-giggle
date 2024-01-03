c905facd5d5bfc7d2142d1c4533a1909d5374239
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:49 +0000

    soft night quantity again turninto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
