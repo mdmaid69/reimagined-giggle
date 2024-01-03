d2464201f8ccca6c63e289311ecc45215348b190
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:29:46 +0000

    aware wash important schedule spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
