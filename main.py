948e4a853de7cbe06e4d3020c094d01bec8ac537
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:47:16 +0000

    Ii near accommodation ourselves sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def find_max(lst):
+        return max(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
