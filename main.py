ef241020f3775f7b0d36c987684b19649275bb4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:09:58 +0000

    direction declaration 569enjoy though carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
