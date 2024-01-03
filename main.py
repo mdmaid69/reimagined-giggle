b28a3d6134b7917427fdfca2d7884adacb33a09d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:32:23 +0000

    connect stand welcome basis spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
