0f8f51f770aa41b17b554b2b4fe21ce81a7c3eed
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:54 +0000

    reasonable rate certain both hope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
