cc46e6e582583986125d9170e5afdba3b2eb2044
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:41:10 +0000

    since opponent explain sad journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
