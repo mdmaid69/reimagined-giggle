9c8cd265ff031409350244c8763ca971e633359d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:44:30 +0000

    cinema congratulate quarter talk modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_environment_variable(var):
+        return os.getenv(var)
