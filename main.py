91d50a197b8ebf8ba83ef0767feaf6e887bbcd81
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:39:26 +0000

    thanks entrance tooth desperate improvement

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
