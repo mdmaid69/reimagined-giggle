d068d6ad81fdb67e53b2feea8ce30c36c4344e5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:08:23 +0000

    history statement wish Ss pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
