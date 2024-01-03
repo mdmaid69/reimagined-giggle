75a73711d58b95580c27de8b90d84d03f5889cd1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:59:54 +0000

    important keepaway strike nearly claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
