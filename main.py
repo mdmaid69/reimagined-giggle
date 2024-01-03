f53e9778cc9f7f6d64a69ef60f74cc006def81bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:53:49 +0000

    fresh upwards crash negative impossible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
