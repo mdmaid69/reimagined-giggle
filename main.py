d6ff143e109b10a7016b2af75c6721cc552779d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:22:57 +0000

    decide dangerous rain test long

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
