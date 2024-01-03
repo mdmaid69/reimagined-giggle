92b32a187e598ad4b643ee72d53281cfcf80197e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:09:46 +0000

    sister great future war mix

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
