a353d2990788877659375f45725e0f9e37cf1c12
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:26:36 +0000

    contrast quantity sink rely hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
