7fd7a1f97404e615308f37a27209d1ed23984971
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:06:41 +0000

    block damage fight phone know

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_max(lst):
+        return max(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
