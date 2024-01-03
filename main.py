1890c9ccb56d51d51294227cc13644dba8068936
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:52:24 +0000

    swallow distance still explanation recover

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_error_function(x):
+        return math.erf(x)
