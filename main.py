bcc2c418f32d672b25f7b3e143c1a74727da651a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:44:26 +0000

    order weak flood comment then

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_power(work, time):
-        return work / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
