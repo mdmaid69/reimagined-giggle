66b4c28c804c7413c1e2f18bd8d8161f0bb40297
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:05:09 +0000

    person pile fit challenge day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
