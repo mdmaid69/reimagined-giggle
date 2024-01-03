0e4b20158237ac2b2d3ed811cfb03af8c4d12952
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:50:41 +0000

    hurry question left reject ought

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def count_elements(lst):
+        return len(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
