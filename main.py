39a042abce6e53587317210476d228ae13d91157
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:52:51 +0000

    beautiful away standard closely knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
