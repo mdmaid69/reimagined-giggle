e253110b7821375a31e4220d846fedd97c3f5098
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:51:34 +0000

    different dry makeinto involve understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
