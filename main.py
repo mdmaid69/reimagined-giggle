bbed2a8cfae1d818eea5f18c7872fce9cc51ec92
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:37:52 +0000

    comedown evidence danger independent dead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
