18fb213b1ab7738d3cfe042a246259e060a8da91
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:27:28 +0000

    unlucky what support fence disturb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+for i in range(5):
+        print(i)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
