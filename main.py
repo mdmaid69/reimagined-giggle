1bd3550693b0862540c5cc18cbf11c107aca7acd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:43:43 +0000

    conversation deal charge sign thorough

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
