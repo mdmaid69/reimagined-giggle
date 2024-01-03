cf742c320cfbf25ee550fceb46d896dabd9fcce9
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:20:10 +0000

    kill discussion comment front pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
