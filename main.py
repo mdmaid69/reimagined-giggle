722065ad951142f143200ca291bc93075fb53836
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:26:49 +0000

    makeinto blame wide original father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+def find_unique_words(sentence):
+        return set(sentence.split())
