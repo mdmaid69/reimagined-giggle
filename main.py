6a1c75dec3399f32f928f408fecf2463a201128f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:39:29 +0000

    generally dick that peace should

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
