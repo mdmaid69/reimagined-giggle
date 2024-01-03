f989ac26a70b40e9719f584e508d4213df6de18c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:26:55 +0000

    city apply city watch general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def is_odd(n):
-        return n % 2 != 0
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
