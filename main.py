7d8911ef4b8e28818ef8928b5fb98afaf1263568
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:02:52 +0000

    giveout swimming punishment both right

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def reverse_array(array):
+        array.reverse()
