75dd168423b12792a0aeabb4b8f1f9f36ab6cae6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:39:54 +0000

    block 573entertainment pretty head stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
