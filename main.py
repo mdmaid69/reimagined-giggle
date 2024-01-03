a998ea36a13caae3cc6bd7852a99c760b45adb09
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:45:55 +0000

    ready sharp until correct rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
