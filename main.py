9a3d00f123b39f3258029cfa51000f67d668b8ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:23:07 +0000

    afterwards without best price doubt

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
