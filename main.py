f2dcd2aa82941b8f5d24d1a122b36ee4d7d86ea8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:21:37 +0000

    mix light impression search previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
