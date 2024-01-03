16ac1570ec31fc3f855f170196f97767247d6bd1
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:29:54 +0000

    keepoff certainly happen finish leg

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def count_words(sentence):
+        return len(sentence.split())
