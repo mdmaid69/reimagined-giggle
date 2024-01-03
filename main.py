a005206cc920f4194e8827b3a697732ec1230981
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:54:16 +0000

    perhaps bus choice work breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def remove_from_array(array, item):
+        array.remove(item)
