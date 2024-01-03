a0b700d0da4e5d0fec4bc557e4af685f2608c1a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:05:50 +0000

    workon dream inquire pause step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
