3ad1df367cd7d5742a753d73268b1fb886fee885
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:06:37 +0000

    fairly dick mind feel murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
