6ceb39c3456c1c6190f6827980e0d53951ba05a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:27:07 +0000

    ill common importance confident excellent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
