5981b3981784d5216c4edbef6c0611fcbb22d63d
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:11:19 +0000

    holdon group tall him intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
