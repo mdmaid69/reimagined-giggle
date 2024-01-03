48b82daafc0bb4ea9c62e6021730d20f7c508bbe
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:51:54 +0000

    floor call beat exercise sink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def convert_to_binary(n):
+        return bin(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
