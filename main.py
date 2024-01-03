f1e37fd0135940a30889e19f75b557712e8b3f1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:33:57 +0000

    experiment tap interview upwards workon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
