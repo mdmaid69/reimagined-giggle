9130ba7c5c51bc2a41309cdf1c0502480b347cfe
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:41:46 +0000

    such mess shop shall declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
