8ae7664277c6239d1d06617e4e89df354b662ccb
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:06:08 +0000

    runover break Nn check port

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
