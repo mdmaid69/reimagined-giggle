cf4dc8bf535eb6ac925240154597f7f4972421cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:15 +0000

    situation must interest smile public

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import time
-print(time.time())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def print_python_version():
+        return sys.version
