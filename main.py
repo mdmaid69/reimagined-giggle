333e2edb33a1c88eeb6da5c576004d81fe21a9c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:33:17 +0000

    exciting OKorokay before shopping government

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
