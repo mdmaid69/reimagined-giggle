b251d9ffdfde6b1ecb25fdee3c206e9da75869d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:11 +0000

    wipe ago mystery treatment result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_base_name(path):
+        return os.path.basename(path)
+  def convert_to_octal(n):
+        return oct(n)
