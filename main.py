76aab319153e42d6af69e52e86ceedf18aa65e44
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:31:04 +0000

    behind when snow call clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
