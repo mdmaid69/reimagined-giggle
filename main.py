2f441cd05be7bf82974a4617c44b4a8d2efe9896
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:42:09 +0000

    distant taste answer criticism experience

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def get_array_as_memoryview(array):
+        return memoryview(array)
