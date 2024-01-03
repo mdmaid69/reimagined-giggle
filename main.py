5d6ac6827c797a8f3811c9045fc47d779792473a
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:40:33 +0000

    recognize scrap lean the visit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
