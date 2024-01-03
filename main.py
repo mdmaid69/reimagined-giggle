1ce6f2d84ef4a9d0562d3814b19e52a1a787e753
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:43 +0000

    book tour substance professional remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
