bc40e8bc42140cf4cb5b07fa9a67b916acbb3dbb
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:04:38 +0000

    someone nurse roll never getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
