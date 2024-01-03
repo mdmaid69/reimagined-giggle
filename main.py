61db7939ba99642a824065ffbea596954a14f1d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:47:27 +0000

    vanish swallow reject meanwhile understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
