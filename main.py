27a7e6364796e2cee755b13e8ba867bc442f564e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:07:04 +0000

    law unusual regular rid each

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
