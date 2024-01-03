747e6e882d05f138983281c31ad6e51d4b7e2f77
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:41:56 +0000

    slide communicate stupid sweep whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def split_path(path):
+        return os.path.split(path)
