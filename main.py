bf436a069fcad8280ab3cb7ceda356a65458318a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:03:08 +0000

    often pronunciation edge rest project

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
