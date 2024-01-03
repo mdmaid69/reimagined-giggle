afca79e0ff4ce6dcf0cdd22f5da1bc45766ee452
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:13:23 +0000

    once interested invitation Ss sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def square_number(x):
+        return x**2
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
