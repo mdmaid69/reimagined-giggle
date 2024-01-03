60ec6056f209cd7f26d1fb221fbe1c434a4869c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:02:30 +0000

    contribute area home surface care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(10): print(i)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
