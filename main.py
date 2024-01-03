8ad27f3ac2e982b65ad6dfcb17f562b88a721a9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:22:47 +0000

    pour address grade nonsense quickly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
