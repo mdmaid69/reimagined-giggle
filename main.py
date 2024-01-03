ffbd02d26ea24fc1e7f3ee2b376cd25957bcb4bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:58:51 +0000

    display beat yesterday lock movement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
