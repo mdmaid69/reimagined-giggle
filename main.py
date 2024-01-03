7f9c6db1724429f359bd12fafba0127e07d28252
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:17:55 +0000

    command reasonable soak content read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
