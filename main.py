5a03b1ac938b5875434f7be78e73cc7c1483bf22
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:22:18 +0000

    possession stamp pile load turnover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_octal(n):
+        return oct(n)
