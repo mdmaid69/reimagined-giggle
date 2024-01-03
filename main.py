eb67173fd06a2080221d1b7a587c28927b9f552b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:25:38 +0000

    harm call great press left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
