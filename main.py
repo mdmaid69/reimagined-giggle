5199ec4d6ae9c1894095ef3df63ed8abc5855fb4
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:59:03 +0000

    excuse insist bath funny perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
