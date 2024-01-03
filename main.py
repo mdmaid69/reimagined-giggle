bad6e83b6d16437d95764ba5601bd7e93f42fc46
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:40:01 +0000

    tread fast tax attention land

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
