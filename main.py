b70bd514407d79a5b98f82209e46bd81cb7c897c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:48:57 +0000

    table guilty belief line wave

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
