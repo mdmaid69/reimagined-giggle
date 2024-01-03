4f1a3f9be47abe2c7ff5c8d3de10c420b5649be7
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:24:40 +0000

    everywhere thanks criticize comfort brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
 def check_if_array_does_not_contain_item(array, item):
         return item not in array
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def reverse_list(lst):
+        return lst[::-1]
