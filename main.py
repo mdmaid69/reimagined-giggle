98cea40ef7a5bf8ba631ae218473ea098b21ac75
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:53:39 +0000

    cheque brave let help reserve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-  def count_elements(lst):
-        return len(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def convert_to_octal(n):
+        return oct(n)
