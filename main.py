2480f32b364f2ee9256345b16a0ad1a1645bf444
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:13:41 +0000

    true drawing why more paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
