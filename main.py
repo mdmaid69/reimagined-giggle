2526b359dfa4443f9c235b223bcfb6d400f9ee41
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:56:46 +0000

    picture bring mess used vote

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
