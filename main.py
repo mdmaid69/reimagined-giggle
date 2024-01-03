554c255b342df189d7e16dab10c252951bd9df5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:51:12 +0000

    direction dress either substance obtain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
