0c16537aeca2575a37c47e3c387a6dce0eb3b59a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:41:04 +0000

    narrow wash walk never lookafter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def get_array_as_set(array):
+        return set(array)
