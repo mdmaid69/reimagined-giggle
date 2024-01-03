c640a28c4b7711450fb317e8d686b4c72f2dd6bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:26:56 +0000

    degree sorry stop especially sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
