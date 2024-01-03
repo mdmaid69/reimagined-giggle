f77148fdd62e24760531008b39a487a871245bf2
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:11:23 +0000

    discover sweep whistle print maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
