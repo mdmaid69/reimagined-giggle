3012aca1a35da9c3009e7a05f9c12425c531836e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:06:00 +0000

    runaway length detail afford surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
