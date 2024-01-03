b5453b5c6fd6eec523dd623537ad47e6d0c7b157
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:39:34 +0000

    favour if joke sink connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
