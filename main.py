efd2d9329ab65ca0342347ea8afe213cc2dda9b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:25:47 +0000

    mail factory basically strict far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
