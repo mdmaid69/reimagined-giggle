758da1e4aae2d06fa04744951b9f6b279aa153df
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:54:41 +0000

    room achieve silent unfair shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
