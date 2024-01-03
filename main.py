9615e12372d452481e697992cc507ce91269744b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:34:10 +0000

    congratulate giveup hold nerve twist

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
