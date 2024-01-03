3e9bd1f6d17acb792dca58f763d019b5764daed0
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:08:21 +0000

    list contribute lesson limit fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
