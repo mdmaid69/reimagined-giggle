6e1ea9077c44c2804ba60d1e8469e60a650f35ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:24:29 +0000

    truth just cause wash abroad

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
