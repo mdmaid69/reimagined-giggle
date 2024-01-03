0e9e3da29edb20a6bf664b5937bd7372f4115426
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:11:06 +0000

    listen clean afraid print book

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
