786464a7587528b4258f8fef52992720e6a29729
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:08:12 +0000

    second direction smart since matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
