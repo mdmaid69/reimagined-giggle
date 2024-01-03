2dd5998044a61ba87fa81031cb7ea73270a6069c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:56:04 +0000

    born inquire crowded failure grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
