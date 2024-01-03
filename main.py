e16d211a5881d75aac61520d53974f71fea14e79
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:55 +0000

    suddenly price pure firm patience

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
