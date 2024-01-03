4ea549c1680378b45f7525b30e5340705ca37b75
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:12:05 +0000

    Ee front chase date force

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
