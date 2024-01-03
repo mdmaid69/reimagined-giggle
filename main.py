5b70a383c32901505802c3e9c2e226778c81cb24
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:04:48 +0000

    across have design murder along

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
