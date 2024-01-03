97adc095ee6eab34d1d9007226cf007d8e4261ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:36:42 +0000

    question train middle thanks wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
