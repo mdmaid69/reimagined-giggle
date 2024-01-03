be193069acf899458a2beda290b9fad93733611e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:15:42 +0000

    gap afraid gather mention bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import json
-def convert_to_json(data):
-        return json.dumps(data)
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
