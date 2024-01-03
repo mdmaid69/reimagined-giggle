69cbc2087b302fe8fb6bf7c29911c87d54033c12
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:49:55 +0000

    narrow cough wrong quiet not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
