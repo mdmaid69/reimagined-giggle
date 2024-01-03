e71760bd5ad1a2ff41e51e60458d91f2a1a97fda
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:45:08 +0000

    people exploration broad combination ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import platform
+def get_python_version():
+        return platform.python_version()
