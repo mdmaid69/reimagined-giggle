027951e779e66cf1fc0636175b3c8c90e0adaac8
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:51:50 +0000

    attempt report fix effect demonstrate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_array_to_string(array):
+        return array.tostring()
