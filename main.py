85edff1692521d2aa06a3023aff679b456d7386d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:39:07 +0000

    unfortunate giveback treat meaning quick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
