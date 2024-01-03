0b465eecabadac5e35946ad8832f816fba16a57c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:11:02 +0000

    excuse style tragedy other cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
