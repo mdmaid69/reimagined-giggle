aac3825d7c05ea0ad879b3200fea1fad07920740
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:33:31 +0000

    cause say disturbing point uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
