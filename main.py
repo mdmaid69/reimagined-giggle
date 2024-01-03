19db16f9b1e9653d4130e99cfc2d0c8b66e362b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:12:02 +0000

    head tomorrow involve quickly sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
