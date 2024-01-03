9be3fdef5b789e154119369636b0388d804e7aa3
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:45:44 +0000

    lookback(on) trip hole direct bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
