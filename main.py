ed4e592e413e67fed437d3119314d34a0983e8c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:10:42 +0000

    loose giveback reach accidentally cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_octal(n):
-        return oct(n)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import getpass
+def get_username():
+        return getpass.getuser()
