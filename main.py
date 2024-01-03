9513995ffd01cdb53dcd9329327dc7940fe0e590
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:45:36 +0000

    night block decision or rent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
