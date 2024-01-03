b3013e9c36a1bd451578fbb41e7d929b694dbbef
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:59:53 +0000

    treat cry benefit jump pleased

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
