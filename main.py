873b0768084b648d614e2f0f04a305cfba438ace
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:11:08 +0000

    pronunciation dark honest load better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
