cc6eebdab729a63811fc8e2e1b0c94f8dc4c5803
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:32:18 +0000

    fresh turnon which hear only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
