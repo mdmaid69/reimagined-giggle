a5d17805a707a67c9edc0fab297bf85e2c7a2307
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:24:12 +0000

    exact aim none ever hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
