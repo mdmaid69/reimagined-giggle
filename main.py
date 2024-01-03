7de924d07f8017b8fc0efccc4fe7073ebafa6d4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:51:08 +0000

    round pattern whistle stand tend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+print(random.randint(0, 100))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
