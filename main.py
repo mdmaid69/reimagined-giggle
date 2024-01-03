c56f94ae58b2e0aacccc56f4babc11fde7123f50
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:17 +0000

    lock strange thing inform offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
