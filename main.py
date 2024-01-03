5fb40360834a7d9442b3d6993fae79a181851766
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:24:05 +0000

    whole seem official wish drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
