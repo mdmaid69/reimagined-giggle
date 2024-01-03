b2ee8bcd06010494a922818a850666ba09d0de97
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:15:51 +0000

    insist smell believe 571enough store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
