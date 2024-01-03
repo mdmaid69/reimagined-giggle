0659d49edadccdce54679aef33e1dafee045d0f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:49:39 +0000

    settle grade aim expensive about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
