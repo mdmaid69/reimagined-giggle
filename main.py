851a63c2ccdb93db30c833208fdba31ea7d6db25
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:31:27 +0000

    hold welcome juice trick here

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
