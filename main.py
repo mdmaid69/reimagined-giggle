de5eb5683cd7d7ee090778f8c74b7211c6b13561
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:25:08 +0000

    try communicate error call lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
