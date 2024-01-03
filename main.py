dee0466b995bd300e4ee1064aa4dbedea9e89070
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:02:32 +0000

    mad command regard respect on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
