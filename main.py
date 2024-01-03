2d6fac1c11a2878c5fa499a0f9288ed557863d88
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:49:12 +0000

    tight lost warning natural his

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def square_number(x):
-        return x**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
