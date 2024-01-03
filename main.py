c7e46df243a3a0ea0281360744781e09518d6500
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:27:37 +0000

    objection anything small enthusiastic aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
