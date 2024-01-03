dc6c741bf7f34c0e3f9759c32f9d2137c04d9770
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:53:30 +0000

    sick share workon everything mistake

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_max(lst):
+        return max(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
