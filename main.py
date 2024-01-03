c0946d10df787188c94db7a25b3abd6a746ec78d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:56:14 +0000

    influence constantly fold tape comfort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
