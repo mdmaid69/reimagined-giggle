1f42897a55032c15773159ee3b24e2cb6bf68ba1
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:37:27 +0000

    long visit feel practise scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
