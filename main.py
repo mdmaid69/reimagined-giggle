8fb3e5eacfd498d2be4e23967bfc6da6817f945b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:10:00 +0000

    trap know Tt somewhere back

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
