9124ccbaaa8d053334d117bd60256f41e2ba9a70
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:56:25 +0000

    previous declaration foreign education family

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
