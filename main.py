52983ba0b241a98e7a63e844bddbc11950350f57
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:59:45 +0000

    appearance payment emphasis region recent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
