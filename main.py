b92055a752251417f85bdd3325b49531090479e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:25:56 +0000

    harm certain discussion mouth stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
