b57e0696e44f312cfa33135f38d74c005697ac5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:44:13 +0000

    cool interfere project unite acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
