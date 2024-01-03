f785eaca5d77babe35bef57db6e98faf6f440e04
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:28:22 +0000

    slice official great score by

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
