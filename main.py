6c98a4818994808b627a7d787a11b92448760257
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:51:42 +0000

    suckme subject independent try storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import math
 def calculate_cosine(x):
         return math.cos(x)
