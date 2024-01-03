eadda9e1e514f4a4f08772d4975fcca66d186eae
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:32:48 +0000

    star sensible cheek book emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
