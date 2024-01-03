5249564acbc6cf6689a169ce0f1f6f2415a618ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:20:02 +0000

    welcome bunch mention answer fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
