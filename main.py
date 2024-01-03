224b3d425221e49832bf17812e702b1a20f20621
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:16:23 +0000

    official majority software gooff left

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
+n = 10
+a, b = 0, 1
+while a < n:
         print(a, end=" ")
         a, b = b, a+b
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
