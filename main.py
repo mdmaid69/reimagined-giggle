5b986813c8a792978ffda076bd9a7a813f22fa8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:43:33 +0000

    him squeeze flavour Yy fold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
