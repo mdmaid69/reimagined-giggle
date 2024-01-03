23e13fd2a24faab5309a68d60609bce8336ab5cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:15:10 +0000

    congratulate fail original body hurt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
