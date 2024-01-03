0fb0e4ae4f914286a8c2bfd1730332b9c7a42b6d
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:13:50 +0000

    instead rush ear worry afterwards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def is_odd(n):
-        return n % 2 != 0
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
