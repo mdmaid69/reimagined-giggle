846b0541d44b83a9a743a51614903d9045ac574a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:27:39 +0000

    exploration quiet head probably maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
