14c189313533162bff3ff12b0e03d8fb15503f0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:43:57 +0000

    smile respect cheap support mention

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
