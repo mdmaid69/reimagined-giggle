f18729db492b95b94030471d64e75ac0bf704ab0
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:39:50 +0000

    thinkthrough comment neat right wide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import tensorflow as tf
+print(tf.__version__)
