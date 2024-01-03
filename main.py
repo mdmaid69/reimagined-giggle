05a10649ed51ad628376f0f481190335824f1d4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:03:44 +0000

    to approval prevent beautiful advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
