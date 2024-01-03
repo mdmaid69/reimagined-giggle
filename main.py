22a862ebe1ec689665a6e7d8172ff9e9b178da88
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:44:15 +0000

    principle regular dark kind rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+def square_number(x):
+        return x**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
