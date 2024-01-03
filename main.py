dab14586dc671c89bbbe99d9ba021cca14ba08fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:51 +0000

    inquire choose vote dark strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
