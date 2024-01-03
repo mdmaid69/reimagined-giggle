a00d1b20b937522e50c3fed397a8acf0c5ab7024
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:42:56 +0000

    laugh believe level doubt earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
