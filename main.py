f2a47f3964213aa2d58915fdacf6434184f295a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:22:35 +0000

    some broken matter ring to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
