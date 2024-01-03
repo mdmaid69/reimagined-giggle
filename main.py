be685a437b92d0dfcc4a172f54fd0341613673d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:58:13 +0000

    possession show fond cross opinion

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
