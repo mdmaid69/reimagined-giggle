d63d06bc347bb4a3820136f9d414858efd40bb1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:29:19 +0000

    air difference failure university brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
