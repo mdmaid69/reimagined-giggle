5a980c35624c92fc08d24e8e51cc58807210350d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:15:52 +0000

    corner fork confusing the left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
