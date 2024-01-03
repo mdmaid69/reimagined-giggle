98a2f6694bef1964dc538eaa5a105779d5f6d63a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:05:36 +0000

    closely blue period enthusiastic old

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def cube_number(x):
-        return x**3
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
