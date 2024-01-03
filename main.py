ab701d40bd91799a920922d7c51c228d488125f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:42:43 +0000

    false sleep letter gather hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
