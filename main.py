6054a1d20f545ab2ba1df540558154cdb1f20788
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:09:01 +0000

    job buy soft result breakin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def add_numbers(a, b):
+        return a + b
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
