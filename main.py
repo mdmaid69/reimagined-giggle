440e24ea6a505cc35c0913839a082f4fa3cdc80a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:05:53 +0000

    what return calm keepfrom born

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
