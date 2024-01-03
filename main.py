29d74ea1004e0c50ddafeafc7f1831283b30fb85
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:42:31 +0000

    neck loss turnoff train exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+text = "Hello, world!"
+print("Reversed:", text[::-1])
