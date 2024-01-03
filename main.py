631fe44c079bbe8f78408161031ee119a43df83b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:31:17 +0000

    add occasion final storm Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+text = "Hello, world!"
+print("Characters:", len(text))
