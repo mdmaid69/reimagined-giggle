bc34c304dd52884435a2e4d68364c4c0272f2c5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:27:18 +0000

    interruption interview owe typical design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_work(force, distance):
+        return force * distance
