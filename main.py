7489b285523ab0128b602cfc49b625bf530889cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:56:01 +0000

    nature call decide exact under

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
