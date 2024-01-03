439d7cff38f0f0017663df5e44406a76578379b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:43:25 +0000

    dry tense day shoot quickly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
