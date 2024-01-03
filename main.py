dc99a6731a9228810e287411711e304d1d668c99
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:52:54 +0000

    hardly month unite good blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
