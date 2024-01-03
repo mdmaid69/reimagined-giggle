7f9a33b37b4b1452b1dfa10cbac0f97b061dca79
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:01:44 +0000

    boil next confident claim admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
