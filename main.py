f20357dad0a7f814d7a4188ae52d5bcf66068508
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:15:42 +0000

    before school tomorrow basis solve

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def reverse_string(s):
+        return s[::-1]
