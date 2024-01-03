d833983102534a2f372d3933fa90357faa5edcd1
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:38:39 +0000

    film recently company discover reason

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
