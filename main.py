8899eb078ae30be41408eaa16c3ba3123ff1e093
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:01:25 +0000

    freedom argument figure choice hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
