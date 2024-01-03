623e65a6393037dd93e04b701d697ed368d8694e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:02 +0000

    original 572enter record crash protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
