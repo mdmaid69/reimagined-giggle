d521439d5e6fb28a295bc275ff3187f9d2d8cb84
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:37:46 +0000

    inside smart large interruption club

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import re
+print(re.match("h.*o", "hello world"))
+import re
+print(re.match("h.*o", "hello world"))
