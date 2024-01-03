a329e68ac1bb7f1ebbfb50145ed251c5a980598c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:01:39 +0000

    test development land minimum wave

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
