a5eed1e9ffe0c23ef8241b72ce554dc510068019
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:06:20 +0000

    rescue question game discover message

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_as_repr(array):
+        return repr(array)
