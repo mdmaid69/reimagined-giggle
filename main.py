00f547e46b36c93e7d892037c57f2659adf1519c
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:23:01 +0000

    profit relation correct public Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
