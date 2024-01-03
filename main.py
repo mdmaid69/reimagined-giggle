94cadb4b5c7e717e8fa8dfbd081f63e2717e559b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:04:51 +0000

    birthday considering object see active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
