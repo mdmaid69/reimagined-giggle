671ff19c37737eae602b630b99e0359fff2df5e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:01:53 +0000

    Kk quiet chief rarely case

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
