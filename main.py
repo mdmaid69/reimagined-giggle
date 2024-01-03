e466a59e30e88dd6200ff3d97a6019880fd352cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:35 +0000

    thinkover sweet centimetre contain emphasize

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import random
+print(random.randint(0, 100))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
