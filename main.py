288f0ec75ddd39432d83349af097c5e454ab52ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:17:20 +0000

    result fall turninto cap exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
