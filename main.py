438664554161a6b008c43dc73b6d9aff42979ff0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:35:53 +0000

    car least determined 572enter pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
