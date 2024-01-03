2a70e801bb0427b703259a3dac4a58c9813a16d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:09:03 +0000

    everyone future whenever cum cinema

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
