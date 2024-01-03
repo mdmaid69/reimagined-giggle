47e42d4409665b60aab5cd22af667c6ee85f1620
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:08:25 +0000

    strange pull mood alive secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
