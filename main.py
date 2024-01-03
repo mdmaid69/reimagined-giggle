206073cd9fc5b30012b3c63864500f8da9c48936
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:15:08 +0000

    finish recover ill afterwards front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
