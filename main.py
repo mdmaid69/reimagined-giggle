68f4cf8322d9e8aca19c312baa9da0400348f1c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:32:49 +0000

    strike really now spoil sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
