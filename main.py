cc74cece892e2d84804f64ddd0fe4b51626cf17e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:59 +0000

    offend plan turndown high large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def get_array_as_frozenset(array):
+        return frozenset(array)
