570934a908036f56124bfed1a6ed8f5ec06c926b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:21:29 +0000

    plastic waste supply without action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
