dd02842454746704cff7316f06b4c1797bf1072b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:45:37 +0000

    phone standard throw into shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import getpass
-def get_username():
-        return getpass.getuser()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
