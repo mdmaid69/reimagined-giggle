5798d0bb255b1d6c9cb932efd042d55147cd5c1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:01:17 +0000

    together picture mess his add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
