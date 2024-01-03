b54ea8e6ac67f5c97e26695ff680f0aa92dcda4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:41:55 +0000

    clear throw slice agree organize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
