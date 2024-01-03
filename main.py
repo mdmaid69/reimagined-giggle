2f5bfcca187e6e2d7aa07c0f02801909b4a350ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:09:49 +0000

    refer wealth sick hot whoever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
