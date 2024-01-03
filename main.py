9b0b62d4c3de9e3b8ac6de3699b9238402137a52
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:49:58 +0000

    unless exact rate basic child

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def subtract_numbers(x, y):
+        return x - y
