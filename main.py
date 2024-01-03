5d4194f986578b4cf5a8f07b7abfc64464535542
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:37:17 +0000

    kill taste stop takeback cheek

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print(sum(range(10)))
