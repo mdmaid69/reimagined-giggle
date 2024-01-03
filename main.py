d6c14a7e8e1c4f858e41907f6561103f1e411405
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:55:26 +0000

    instead fill influence learn reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_nlink(file_name):
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
