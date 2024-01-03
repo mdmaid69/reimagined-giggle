2df3c38d4e4a2c80a07620511471e7d99cf5bb80
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:46:50 +0000

    necessary correction concerning wind sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
