b33ca98eff969b8a8f0076884b7af6a227a5fafc
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:57:10 +0000

    regret nearly basically act reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
