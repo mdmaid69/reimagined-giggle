a55add11b17a286d52ddaf46d29bb7f0c471b137
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:51 +0000

    sort central confusing priority spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_speed(distance, time):
+        return distance / time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
