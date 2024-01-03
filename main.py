6c5201620eefba15b290c228a9d3e313be7d77f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:27:12 +0000

    keepaway I heavy benefit level

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def is_odd(n):
+        return n % 2 != 0
