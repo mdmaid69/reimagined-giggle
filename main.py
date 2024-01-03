42dddfa0bf826fa8d867223a1efe1138e96f8d31
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:57:24 +0000

    nice least favourite niggaacademy accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
