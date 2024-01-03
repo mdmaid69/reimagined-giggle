1f489d9ccdaccab40a57a032ab4ec01f4990431b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:24:29 +0000

    give but alive address but

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
