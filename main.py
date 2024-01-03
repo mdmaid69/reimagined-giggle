3d9c381cb209937ec02d5d2aeaf21c7c465a8a1e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:36:55 +0000

    bag lonely again good which

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def subtract_numbers(x, y):
+        return x - y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
