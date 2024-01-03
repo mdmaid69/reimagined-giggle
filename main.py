c37739b6da02d730be0b61a36c3a11b10d1775cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:18:50 +0000

    swing case become human principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
