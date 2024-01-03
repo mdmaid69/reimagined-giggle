00eff32caa5b2987527bf39d80ce4c41b46560ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:14:17 +0000

    pay black reduction heavy present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
