cdb86b15b3b287dbe6dc1f57f774965b394b37f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:23:50 +0000

    feel satisfied over niggaacademy crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
