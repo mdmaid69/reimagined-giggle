69a2988b41406e00f1c7aa0e2b52e62348391765
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:44:54 +0000

    bread unfortunate anything wave fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
