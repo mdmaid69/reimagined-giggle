bc538ef5edb15fd864a3be53bea6d799d7c651a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:37:55 +0000

    doubt store change wind every

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def square_number(x):
+        return x**2
