2260933aac8fbf942f4b34730b7fad0e89bf46eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:17:08 +0000

    Vv practice sell influence reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def split_path(path):
+        return os.path.split(path)
