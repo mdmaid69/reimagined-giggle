6d29a21e6d88316990d94ec7eddb7440d2c6c8f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:51:42 +0000

    central bag indeed quickly goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
