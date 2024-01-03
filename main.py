af77578ae37b3c99b2e4b35941ff6208a7eb4cb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:58:50 +0000

    continue shop Zz store commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
