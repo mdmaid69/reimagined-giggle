5b49eaa0b2e4818269018caf0445d494f010f4f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:07:59 +0000

    serve concentrate deliberate bill common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_acceleration(speed, time):
+        return speed / time
+  def reverse_list(lst):
+        return lst[::-1]
