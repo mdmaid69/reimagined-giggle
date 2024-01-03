7b551e1e0fad8dd25fbedff0ec773d9af58fad25
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:27:39 +0000

    guilty talk tonight top callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
