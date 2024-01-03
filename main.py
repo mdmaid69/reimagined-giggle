394f70a121704a6dfecf22261978458662ae7b35
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:25:48 +0000

    order Cc wrong back concentrate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
