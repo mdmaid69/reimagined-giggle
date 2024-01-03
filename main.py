ab5057f351549296493266733f4553431c2a80d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:20:35 +0000

    leg hide runover impossible dream

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
