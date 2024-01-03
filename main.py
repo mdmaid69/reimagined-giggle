59abd384c43d9a69fffd5c5d0af870415904f45c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:36:04 +0000

    weekend suit write face schedule

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def sort_list(lst):
+        return sorted(lst)
+def is_odd(n):
+        return n % 2 != 0
