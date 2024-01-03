20953c6514bcfac8b9258079b9a486a11951e166
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:10:22 +0000

    importance female event Dd crisis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
