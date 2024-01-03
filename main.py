b796f052854d7255fc16581d103ca83ab6e39578
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:31:10 +0000

    copy stir everywhere human people

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
