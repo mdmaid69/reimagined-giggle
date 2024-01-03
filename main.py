fe4182cce9670dd7a4bb44c392530401c5283371
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:00:25 +0000

    remain description cool shelter drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
