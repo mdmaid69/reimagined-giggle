4a82b5e129e72af5fe8ad05646e3649c1b48be01
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:05:11 +0000

    coat evidence spring congratulate big

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
+def remove_duplicates(lst):
+        return list(set(lst))
