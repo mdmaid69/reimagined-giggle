acfc46282976273eebb87e5069a599a97d430ed8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:02:21 +0000

    public downstairs hand yet success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
