c53f25f65ef476d0f8fbcc5f99fecef9ed417d5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:21:21 +0000

    office large unlucky accidental innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
