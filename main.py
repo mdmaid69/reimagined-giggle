ebafba05b6ff52cae2b4abf388c3191cf88dfc46
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:28:48 +0000

    wrap opportunity open old right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
