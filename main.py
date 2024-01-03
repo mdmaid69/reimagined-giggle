7a3f61c3f01a16b6b42f5c0c6803f68b91a45e3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:22:46 +0000

    touch wonder whistle trade willing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
