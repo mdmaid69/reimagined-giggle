bea8652142ee65666c21e590a56af406b92a49a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:47:02 +0000

    top judge finally trap cross

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
