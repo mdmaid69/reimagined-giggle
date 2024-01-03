89b03d3df508018ac83f89ba944a2e311b09ac54
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:41:55 +0000

    still finger violent afraid lot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
