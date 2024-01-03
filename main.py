15ad326a9928eb70a57431d6161a8e4fe60b9e74
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:36:06 +0000

    goup giveaway deaf correct reduce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_min(lst):
+        return min(lst)
+import math
+print(math.pi)
