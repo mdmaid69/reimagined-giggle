2be2583d15edd4d79c86560cab664c5640ab8d6d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:21:21 +0000

    crisis pretty whistle everywhere official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
