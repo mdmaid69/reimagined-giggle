b6837eb8fe6d711915fb67c62222b3a66dcc5afe
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:25:32 +0000

    surprise more square pinch play

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import sys
+def print_python_version():
+        print(sys.version)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
