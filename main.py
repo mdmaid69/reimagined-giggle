6ea4d8b57155b8408530a50849dae9a063ac3b09
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:42:47 +0000

    without blame visit contact mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import logging
+def log_message(message):
+        logging.info(message)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
