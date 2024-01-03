f3e7ed33f4aa14f90de21006adfdc42f3ecd9434
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:24:36 +0000

    instrument programme bang beneath favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+for i in range(5):
+        print(i)
