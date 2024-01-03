dcfcf7e58cb207f81baef734a1c5fb9be451753e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:30:29 +0000

    drink relax drink partner create

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_pressure(force, area):
+        return force / area
