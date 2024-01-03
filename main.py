2539a959d1e3b5c87b19fb27e49a38143fa13682
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:24:54 +0000

    advantage strike healthy coat consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
