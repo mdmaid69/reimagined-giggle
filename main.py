bfd8f796710203072a6dc942ffb5abb76091bfc1
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:17:32 +0000

    better agree runinto stage forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import collections
-def create_queue():
-        return collections.deque()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
