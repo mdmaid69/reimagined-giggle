24fb66f3d753d5cf52f5a44f65f5cef14421f330
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:31:01 +0000

    pocket less get release can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_current_date():
+        return datetime.date.today()
