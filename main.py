d046f6e39eb64cc763245fd98767350899b00b53
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:37:01 +0000

    march manage communicate goout plan

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
