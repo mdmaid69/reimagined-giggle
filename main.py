72d59efcc222e509f09a6f5cb0b8d48fc7e16921
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:39:55 +0000

    hair healthy square pot holiday

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
