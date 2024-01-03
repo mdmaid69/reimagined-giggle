dd3e9c1f5a59c5a884fb765e5534c7f1448aae68
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:15:05 +0000

    fly different match live cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_list(array):
+        return list(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
