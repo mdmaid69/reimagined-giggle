f78db82b61a119b4f33bfeceb1b439c616e2754e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:26:14 +0000

    comfortable bringback stop race few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
