5f0b96b71857b6498b8a1b2ca0c9982c209e7c27
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:53:59 +0000

    cool store long address strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
