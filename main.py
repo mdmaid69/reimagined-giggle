bfee8e7062a0984db9739a572cea47fdb2bd2089
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:30:28 +0000

    clearly damage development visit accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
