8a004fdacb8d88bb51bc6eb35e24690bac057a80
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:15:05 +0000

    address after anyone maybe speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
