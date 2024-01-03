84348da6d99d8ec0e7f2cd6877eefda3a53d0cae
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:15:55 +0000

    talkinto party one admit character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
