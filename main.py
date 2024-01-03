e309bdd69d14689c993cc4457e7cc70bb9b7f203
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:24:09 +0000

    basis break argue broad explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
