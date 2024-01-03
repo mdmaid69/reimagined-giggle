9ccaf4c0a29e5046a3f96feeed94f5dcc03eb445
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:49:07 +0000

    join Ff nearly cinema likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
