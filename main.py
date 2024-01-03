7c4f2cbc449f2101fb7c1ae01437a7c9ab8ab00a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:15:06 +0000

    recommend neither tool confused reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
