dc134ca99894e8f3e6dae56a606746914c9eb614
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:51:51 +0000

    warn offend accident exhibition mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
