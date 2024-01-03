9ac28b82526e511d7eb8430d21d6daa92d012f72
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:19:54 +0000

    dream comeover need regret support

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
