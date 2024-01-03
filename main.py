ffef8dc782d00e354bbd7139d21a8ddcdc1b8efe
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:40:17 +0000

    recover shallow fact base work

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
