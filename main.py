df06b9bcf68450ea2ee902bba7e2cfee55ec4934
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:55:49 +0000

    radio except or regard song

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
