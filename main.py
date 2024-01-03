0cb4be79b3dacaf3f90b568dedfa7cf0b3a82ad6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:35 +0000

    come marry wide win accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
