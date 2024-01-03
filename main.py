03b02e5107ac470215520137ccf26c7f6b93c30b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:20:38 +0000

    shake well half round catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
