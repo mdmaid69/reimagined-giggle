fdcfb921de1050dac67ed0884bc31944c0fb9841
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:59:01 +0000

    list stupid accidental succeed wave

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
