99d5b1b9deff7278433a75fd333bd0c8499919ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:07:15 +0000

    apply transparent attract brother phone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
