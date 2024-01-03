10b95d74f9a45bbe8b582b1968546ed74a351fcf
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:19:03 +0000

    workupto black cross innocent private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def get_array_itemsize(array):
+        return array.itemsize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
