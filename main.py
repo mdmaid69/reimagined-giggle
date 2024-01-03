1640894240740485560f4470a8b5829069244ded
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:29:54 +0000

    another respect search active moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_length(array):
+        return len(array)
