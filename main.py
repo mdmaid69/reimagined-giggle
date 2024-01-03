0016301a669689388d622b4d320d632ed6c1d370
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:23:49 +0000

    between broken distant nature fit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
