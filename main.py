1b6583d85c5d37bd8de1b4e579ef1707b14758f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:57:49 +0000

    bank acceptable advise cut land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
