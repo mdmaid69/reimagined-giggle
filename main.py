f42ab970afa651d949a4a80eb1755608e92b2175
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:55:16 +0000

    unit disgusting 565end badly without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
