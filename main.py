6eabb4685a8500b6fdd40814b548fd1a4ec67de3
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:32 +0000

    Pp dangerous entry moment hear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
