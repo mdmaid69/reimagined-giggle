f01e37c4b8fd81f9579d3a388480405a3033b903
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:23:22 +0000

    sudden play thanks affect supply

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
