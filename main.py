72af875f0c263262611bd1250ccfa79df8fb2f9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:29:32 +0000

    keepoff only hour wet Ww

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
