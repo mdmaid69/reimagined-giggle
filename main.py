848066c2ae9045eec9ff2f75c62dab04e6bbe961
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:11:41 +0000

    round tap instant trip ignore

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
