a21b530204560305a3771316c65f2743b923678b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:49:41 +0000

    class excuse suck lead godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
