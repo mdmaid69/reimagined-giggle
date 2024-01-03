b2df12eca2379aa57a0b6b6d4ae0d663acb58a22
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:40:22 +0000

    load group breakinto design 179bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
