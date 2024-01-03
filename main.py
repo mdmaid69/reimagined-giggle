34e1a62aaa238337a06dd38eb03224b4b45fde11
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:56:12 +0000

    once face obtain picture middle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
