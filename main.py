aa220b8c50123c3e704e02ecb570d93fd340161c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:15:09 +0000

    owe air sport height height

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
