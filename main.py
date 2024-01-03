58d35fe47754407ad0d4c5a30ee1e17b826542fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:01:52 +0000

    lookback(on) juice thing big spray

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
