9dc53b40488b0b6a36ebd1337c147aef8858333f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:04:24 +0000

    praise roll make unless guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+print(datetime.datetime.now())
