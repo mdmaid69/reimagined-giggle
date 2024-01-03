83328c0b5b642ee7925f44fb0bca95e501b089de
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:00:52 +0000

    certainly will shape should progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
