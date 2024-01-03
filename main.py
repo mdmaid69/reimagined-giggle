54a056c16534440fee2b2f621c6e1e2bf6e844bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:09:36 +0000

    dream excuse hard science single

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
