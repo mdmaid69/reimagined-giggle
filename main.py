4a85af60a8a42673eafe850ff3c9a1581abbf986
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:15:31 +0000

    getback show fight worse talkinto

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_work(force, distance):
-        return force * distance
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
