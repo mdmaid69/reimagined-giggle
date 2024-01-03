ba93dad37c972de36d70c4c1215c386956ee3417
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:33:20 +0000

    temperature affect divide accidental regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
