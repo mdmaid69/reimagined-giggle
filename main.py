93b840a69f9ba6c96f74647959fe2a0a22df7eba
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:51:49 +0000

    attract way beautiful passage town

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
