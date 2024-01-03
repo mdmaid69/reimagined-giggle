323373bb9befb898f0864ccea23fbcf19a0145e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:30:02 +0000

    pen open matter Yy certainly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-print(math.pi)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
