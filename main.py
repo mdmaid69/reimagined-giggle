c48137547330d1cf54cd45c2a7c1bf25652b865a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:59:22 +0000

    professional good especially lead mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
