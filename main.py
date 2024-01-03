4ff49564ae83cb680a626f0f9992fea33e35588c
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:26:27 +0000

    fear remain odd pile expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
