e3c6109717ef86dcf8746ee179e8f2636448e79e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:40:58 +0000

    far disturb steam star priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
