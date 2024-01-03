5ba56880de0a465cbc9a5ef83e7fc1222ef1521b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:30:34 +0000

    satisfactory boring increase visit scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
