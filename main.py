71baecc577d7c5b28455814a942ce578388486a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:54:54 +0000

    Rr provide base blame bush

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def reverse_list(lst):
+        return lst[::-1]
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
