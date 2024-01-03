ef1e31a11f18bc88f82dbed3a8286ca9fcc163fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:13 +0000

    load ability ilovedicks trust physical

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def multiply_numbers(x, y):
+        return x * y
