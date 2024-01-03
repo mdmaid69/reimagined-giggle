a36af9e216fa97ea1eb5648711dc57d36666ffee
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:25 +0000

    bin lunch laugh ahead whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
