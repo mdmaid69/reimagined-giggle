7901c96f08c2b07e0f80864a12aff89a68b954e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:59 +0000

    anyone level common question criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
