5538a4c8a3f4d8574f472b4fb9cabac63fd8baf1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:47:56 +0000

    crazy front mad broken tune

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_power(work, time):
-        return work / time
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
