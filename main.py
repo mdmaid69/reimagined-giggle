db6faf4490991cbad5fd63c1ab036f66c5d2208c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:55:16 +0000

    along treat start dangerous again

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def is_odd(n):
+        return n % 2 != 0
