6d39bdcefd90e3070d14022573be3429ab917478
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:13:34 +0000

    damp emotion everything defeat ill

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
