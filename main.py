d87f46393f0974f4d6d683955616fc1dd63eda4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:10:35 +0000

    environment lean bad rise discover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def is_odd(n):
+        return n % 2 != 0
