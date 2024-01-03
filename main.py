95845fc366292be23068daf6ae161cd3a7b7f4ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:50:30 +0000

    laugh improve mark recommend best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def cube_number(x):
+        return x**3
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
