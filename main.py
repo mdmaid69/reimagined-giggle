10ba20898a29ba22736e5899465cce14cbdc5abd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:53:51 +0000

    your stamp politics hope complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
