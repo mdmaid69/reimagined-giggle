7e5056ce7d0a9ac19f23d52afa121c066068df90
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:05:07 +0000

    emergency absolutely strike avoid Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
