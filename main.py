fbbe16c5e12131b09f252ca1b4232cd3e9073fff
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:39:50 +0000

    load practical skirt relief list

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+print(sum(range(10)))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
