1385b01060b18dedae7421ef7010a2158858ee35
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:14:10 +0000

    fall settle used emphasis skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import sys
-def exit_program():
-        sys.exit()
+import os
+def remove_directory(path):
+        os.rmdir(path)
