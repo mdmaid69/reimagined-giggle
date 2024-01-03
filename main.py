b8176fdbcca58e9ea6d632015e09e20ba6ea1606
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:29:02 +0000

    force mark other 567enemy exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
