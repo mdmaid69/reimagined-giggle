94e438114641c0b6a92ed77d54391d8326bc0bf9
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:15:21 +0000

    round loss break runinto snow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
