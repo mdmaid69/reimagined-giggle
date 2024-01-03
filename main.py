893303352fedf4104ef4f880313e468820d3e441
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:20:28 +0000

    twist shape need still change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_acceleration(speed, time):
+        return speed / time
