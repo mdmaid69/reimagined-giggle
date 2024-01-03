3720fce7aa986f3f03e454d6ae37d9d53153d747
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:18:51 +0000

    spin can weak wait do

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
