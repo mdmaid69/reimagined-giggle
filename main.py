48e0a0f71dc3cf707a15be01db0a60ba0b27acb6
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:52 +0000

    debt careless pleased against load

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
