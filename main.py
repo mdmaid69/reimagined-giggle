805213e81c20ee6c43f678ce9e2db8bf77be83a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:25:18 +0000

    drink leave holiday down finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_bytes(array):
+        return bytes(array)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
