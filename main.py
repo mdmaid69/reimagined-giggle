1c1f60f4b69f5db4ab155ec42fdcc27388ab200f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:16:34 +0000

    wave warning total carry 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
