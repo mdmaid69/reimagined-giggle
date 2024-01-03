dd0fea5f63b1460a657b0a48ef468b72e70df20c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:03:11 +0000

    even beautiful man achievement me

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_acceleration(speed, time):
-        return speed / time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
