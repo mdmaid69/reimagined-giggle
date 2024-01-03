b16a2b6aa17e8d2da1d1a09c294461c9e43ca2c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:57:55 +0000

    rich wide ilovedicks so turnup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  def remove_duplicates(lst):
-        return list(set(lst))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
