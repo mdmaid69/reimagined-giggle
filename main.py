601a31b1961ad47a6787e149e54353de3b49cef1
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:33:52 +0000

    stare fair great mine announce

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def reverse_list(lst):
+        return lst[::-1]
+  def convert_to_binary(n):
+        return bin(n)
