3168f1a2b83b6409ec122420100c0dd275a83b75
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:45:35 +0000

    let allow organization birthday disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
