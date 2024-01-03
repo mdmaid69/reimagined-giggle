f1e1c40d97bd99844eac6beb70aae7ec41afe74d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:10:47 +0000

    noone fit brave likely lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
