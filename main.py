08f22e967c0c55dffd65af2dc78875721e1e13e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:20:50 +0000

    substance then trap colour mark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+name = "Python"
+print("Hello,", name)
