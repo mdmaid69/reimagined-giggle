e8a086f00b92e0795ba7770e41c8bdc44cff00c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:32:58 +0000

    point owe breakup turnup permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_sine(x):
+        return math.sin(x)
