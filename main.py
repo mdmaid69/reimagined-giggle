2908973aa25b981c0d137bccb5cdf69023ecaba9
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:55:05 +0000

    recognize laugh trade half can

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
