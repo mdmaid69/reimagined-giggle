0df565342781bcf95fef3ff96c057158ba483159
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:58:02 +0000

    wish foreign soak meaning shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
