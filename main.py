761f8a09215a7078af60499a872b02e1556473cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:25:48 +0000

    mouth here kill experience region

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
