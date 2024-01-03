d9b8732f9b35451fda7cfb35ead4dd4708a689e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:10:04 +0000

    not high question lost workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
