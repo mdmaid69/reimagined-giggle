8d30153b66d1c1cbaa7b8afd48553feccda3a42a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:34:40 +0000

    pocket wake change just undo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
