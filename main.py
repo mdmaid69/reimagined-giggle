c70ca91ef0bc103a9e0d2da41626638e45ff0358
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:40:52 +0000

    course goback(to) alive pupil brown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
