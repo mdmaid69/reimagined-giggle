bcb4efa488e5a9d06349d0c10959a05bddd5d4a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:25:51 +0000

    unpleasant hello immediately love careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
