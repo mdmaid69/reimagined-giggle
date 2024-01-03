b275df86bd4fb2ef03cda5cf67a19146e7ef7504
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:15:44 +0000

    form pronunciation fun happy unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
