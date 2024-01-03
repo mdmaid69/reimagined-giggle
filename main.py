cab316a4f5998ed2f7074f07bc95511600355f42
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:10:53 +0000

    top flat hurt finish want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+name = "Python"
+print("Hello,", name)
