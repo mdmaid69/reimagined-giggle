ed8013a05c236b44cd5ec60c30a021ab2bc57e69
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:27:39 +0000

    ourselves exit hope dick time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
