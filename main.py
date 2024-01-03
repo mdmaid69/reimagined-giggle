7f833193cff122260c3d6bbed88e5319b7ef0fe1
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:49:34 +0000

    direction pass statement control hurt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def convert_to_hex(n):
+        return hex(n)
+def remove_duplicates(lst):
+        return list(set(lst))
