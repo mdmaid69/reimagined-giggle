dd0bde9e64789e8c0033a69de468ca3153843160
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:40:22 +0000

    dead shallow form number swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
