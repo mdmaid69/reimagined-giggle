3e126545a3811e28a333c8b2254d3bd621b92ea2
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:24:33 +0000

    before cum afraid draw easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
