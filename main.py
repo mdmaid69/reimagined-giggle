e1c8c756ff1629d98a14b758eb594036a81e9272
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:32:08 +0000

    manner poor cause human contrast

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-print(sum(range(10)))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
