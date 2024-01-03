16e9bd6e23073220718e335fbcee4bddb13efe05
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:37:12 +0000

    plate arrange bound term contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
