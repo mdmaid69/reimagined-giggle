5057920f2c886bdf3a089533807a30bfddbf0750
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:34:23 +0000

    fight us learn opponent invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
