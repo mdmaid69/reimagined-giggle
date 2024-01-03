71243b7214cd82085cfd1cec40957c4ed8bbbce4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:18:16 +0000

    confused invitation plenty worst software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
