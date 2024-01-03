b95b803c26d03705c83d02acf6c2a18e04214bcb
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:01:01 +0000

    brave myself open clothes allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
