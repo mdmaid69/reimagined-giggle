8ec62ac9bdd174fbbc0800977d134ef87795439a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:16:59 +0000

    possible well arm borrow exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
+  import os
+  def get_current_directory():
         return os.getcwd()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
