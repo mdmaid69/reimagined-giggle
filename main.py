0c2e350520c6ef0590de5aaf329bc05739a951bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:25:52 +0000

    loud smoke ago content tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
