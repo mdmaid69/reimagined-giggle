067180979ed0349fd8c01028df896aafa2039a16
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:06:02 +0000

    cover I sport that previous

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def add_numbers(x, y):
+        return x + y
