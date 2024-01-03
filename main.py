fd53fee844dc13e50ba19fccc7563fbf379a704b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:16:48 +0000

    else plant when television expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
