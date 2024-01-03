145e9e020b524d51268f88103de432c2ff285c0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:23:07 +0000

    slowly actual apply negative tip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
