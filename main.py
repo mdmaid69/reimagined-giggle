7db7c6560fd381f17e18ebdb22683a18b32baa99
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:05:11 +0000

    intend agreement doctor organization plastic

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import collections
-def create_priority_queue():
-        return collections.deque()
+import sys
+def print_python_version():
+        print(sys.version)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
