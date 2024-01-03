f2b8b3e8ecf058d60fc7b0b3c499ee7997ed6611
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:09:17 +0000

    Ww welcome takeback replace maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
