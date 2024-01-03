974409113b955210f74a5faa7a6c234dcb21ce8f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:30:58 +0000

    complicated mention against grade own

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
