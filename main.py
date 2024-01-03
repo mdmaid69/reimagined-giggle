ec75f40e8a7f5454b06dd0f2c7c1233bae364e20
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:31:38 +0000

    good further breakin skill child

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def convert_to_binary(n):
+        return bin(n)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
