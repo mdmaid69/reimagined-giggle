ea9c2826f541ec97be866e81bc7266b7fc71d446
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:44:38 +0000

    convincing single common value OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
