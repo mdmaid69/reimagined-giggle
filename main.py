b6e1f6b46dfc5deb59ccfba2215a431cdf49fa65
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:58:31 +0000

    coat live abroad set tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
