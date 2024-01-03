d0ee6f150c99bdcbfaea83daaf90244a45330144
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:36 +0000

    goout art balance road difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
