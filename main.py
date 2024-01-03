4ad1dcdf69fa6a93381db6e659fe5794d4810482
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:57 +0000

    shock sort his pain ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
