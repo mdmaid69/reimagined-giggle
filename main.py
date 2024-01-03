6f319b3613d403b2b04a9d8b30851ce6adb87417
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:31:09 +0000

    owe fail steam everywhere certain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_volume(length, width, height):
+        return length * width * height
