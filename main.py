af1f3baf3899d13972f6ada29e8cbd37f18f96f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:50:10 +0000

    far cry estimate send false

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def clear_array(array):
+        array *= 0
