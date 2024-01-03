46628a0c8694fa675aa5e73499ed60cc7391ccca
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:33:36 +0000

    lead true my child complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
