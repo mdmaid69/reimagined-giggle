66252cc3beb1e57a7a8655dabc5c07050f126cee
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:35:39 +0000

    aim lesson become set sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
