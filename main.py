23a5ff676b3210a72117c444365f238138925f42
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:07:11 +0000

    shelter steep gather yesterday regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
