329fc0ee593527b4c9063393cc9fe798e40de32b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:15:36 +0000

    ever education admit autumn workon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
