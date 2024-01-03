bf11046a57a9ba7a0a996a8fab3e3846c0daf58f
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:00:57 +0000

    rush eager empty cruel that

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
