dcc5c089b16730fc7105f64c26e5586786bbbc48
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:45:08 +0000

    support turnout characteristic thin container

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
