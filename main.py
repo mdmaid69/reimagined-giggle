e96bc0d8d3c347b620ba3cbdd7e4d0cbe2d8612c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:42:28 +0000

    dick through OKorokay pleasant unhappy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
