c3c083d6b7572fc815b224091f20de5bcddb7e16
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:52:20 +0000

    children label permission position ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        return sys.version
