bcc250b0e0a29e3cc24e5971e3307a0ab7199e99
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:28:20 +0000

    mass callback original wise satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
