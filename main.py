3514da042d4219047bb2b28164ab6ec618649750
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:09:49 +0000

    judge after for there shop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
