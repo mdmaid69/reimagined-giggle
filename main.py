b5ffb532077c6cb6640da93af7d7ebd9ac53448c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:40:21 +0000

    brain abroad comeup sure direction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
