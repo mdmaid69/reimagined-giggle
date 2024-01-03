44523282a388b143b2a9fe1e23434cbb5680c02e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:46:43 +0000

    delight tonight mean danger tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
