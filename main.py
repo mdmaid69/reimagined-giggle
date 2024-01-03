448c8943ced989e9f4faf74b9406a3a4165b87e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:56:03 +0000

    surprised group expensive sleep relax

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-print(math.pi)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
