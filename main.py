a3f80b61478951cfa759aeb1443bf98c46f4a2fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:10:12 +0000

    shut suggestion control apart couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
