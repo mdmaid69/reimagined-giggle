76a77263d2fafd1fccbcbd5f20c9aaaba5c4b230
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:01:01 +0000

    wise nose relief bringback giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
