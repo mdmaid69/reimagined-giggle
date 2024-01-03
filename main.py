25857170ebe0973380ebd9946bfde5d1ad66c4a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:52:11 +0000

    petrol future image correction sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
