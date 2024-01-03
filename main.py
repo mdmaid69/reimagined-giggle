8b46fa61c4d77125cf44e1a4b00be9b96796b313
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:10:45 +0000

    business already balance answer pool

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def get_bytes_from_array(array):
+        return array.tobytes()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
