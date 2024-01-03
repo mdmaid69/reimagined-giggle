fd7c73737d12d902b82ab55ab76fb278c3e80d08
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:59:15 +0000

    both concerning reserve middle petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
