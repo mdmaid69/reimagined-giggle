aced7895944ef1594eb5fe6355f702c89e7d0857
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:05:29 +0000

    slide convenient last her sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
