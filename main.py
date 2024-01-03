30f3b9ebc17607c3b4701bbbc130d7c2d2d4933b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:53:24 +0000

    Vv while uponon want fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
