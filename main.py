ccb8d1b143c4d5cb54c867f0be7a64f5f72b02bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:12:48 +0000

    responsibility normal plain store ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
