0a1b6e38a75f2c83d2b48d6d140be8fecbbb91f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:55:42 +0000

    ignore clear learn finally rule

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
