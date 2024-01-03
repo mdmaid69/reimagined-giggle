1979781189fcf1af81f89075ffa9fa79b8c30a2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:55 +0000

    preparation describe yet pot evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
