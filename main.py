7ae42cb5fe0dd9f2bfa82ac3e25eee8c3cac5812
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:32:10 +0000

    time hardly history delight minimum

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+name = "Python"
+print("Hello,", name)
