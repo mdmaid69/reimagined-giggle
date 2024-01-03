3912a93d7ea671ab2ac2c300d5fa4b9c81d38eb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:38:35 +0000

    any pile almost now cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
