60dc54c45b2a7ca83d25c16cc63571b798a98c70
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:22:21 +0000

    break opinion control manage acceptable

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
