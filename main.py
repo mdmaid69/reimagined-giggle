7deb8f50ca33559bd43896791b0840fd9bf21b6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:10:39 +0000

    cross rain service more dance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
