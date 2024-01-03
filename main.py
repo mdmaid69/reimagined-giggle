907ca2df50379a3b5449f4e13deddf53b1d09101
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:47:59 +0000

    hard project reduce transport flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_base_name(path):
+        return os.path.basename(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
