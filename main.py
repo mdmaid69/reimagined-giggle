b9e20d44249dcfd082b065031a9868f125f5b1bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:40:06 +0000

    criticize rain big wind breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
