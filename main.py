ce0c53de23cb8491d3e52f8455065a6f306aabfe
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:52:03 +0000

    platform arrange influence record language

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_min(numbers):
-        return min(numbers)
+import sys
+print(sys.version)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
