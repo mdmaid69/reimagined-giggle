f9289b4f6774b1b924df209df7c470f20483f7d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:59 +0000

    future bank produce past main

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
