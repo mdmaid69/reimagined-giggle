7fc142d72b739b8cdb1964a9b22bf256c560ba8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:31:01 +0000

    once mistake definite pattern future

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import os
+print(os.getcwd())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
