716032606895fe23db5d5c9db23230fb2a814dee
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:54:28 +0000

    soft scared slice plan recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
