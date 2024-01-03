0ead663222756b959bf8eb49cfc1cf7014607349
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:01:57 +0000

    voice brave need group I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
