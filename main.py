aa7f375f47d7ff9a854faf94341b845ab15501db
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:54:09 +0000

    seem area miss takeback need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
