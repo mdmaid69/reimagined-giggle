4db87e9b8a563189948852c2c6654b7bc29df3f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:38:13 +0000

    introduce belief city dark whoever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
