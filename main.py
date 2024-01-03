486eb7e89a0a02c58f58f6cac0c78de13e549c8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:22:40 +0000

    loan walk sore burst test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
