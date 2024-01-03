36db5051f83dcf1fc65555641fd5f136e4121b55
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:07:40 +0000

    mention there hold 564encourage hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
