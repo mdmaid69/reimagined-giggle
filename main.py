073df1c1f81cc51c9681adc2a12c0bce9ec6a90c
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:45:29 +0000

    last twist cut comment number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
