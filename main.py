06d4a4d2a2c9e00fb978f6fd68bab13f62476fe3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:20:27 +0000

    business joke alive opinion understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
