dfacbd76b19f4b4dac329f9a8647d9455395ea59
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:25 +0000

    design absolutely cheque rent post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
