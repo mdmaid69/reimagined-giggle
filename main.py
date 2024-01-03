1a457c5bc9b202074d7c3383d9f9d6f4fc212144
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:34:48 +0000

    book tap shame frequent light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
