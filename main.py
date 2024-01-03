5c6971d8404e511748723fb2b1edded24798597b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:34:56 +0000

    kick level no ready freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
