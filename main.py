f91de890399e9bd5df3939d3d575464cd3e397ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:19:50 +0000

    greeting workupto word life beat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
