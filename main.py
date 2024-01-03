a004ea672d3dda34db25d56e9923afa66d46c337
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:58:41 +0000

    report Zz comein play lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def greet(name):
-        print(f"Hello, {name}!")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
