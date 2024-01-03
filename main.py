7560d9c7e50af991cedba96cd830c9b0a6a80c82
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:17 +0000

    centimetre cruel deaf shelter entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
