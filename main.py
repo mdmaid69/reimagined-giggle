2d38455e7fa9fdc22613236ad8389b24c8c03e9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:19:17 +0000

    afford bag characteristic light tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
