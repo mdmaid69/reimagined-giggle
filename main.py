d1fb3e0818719380d4faebd6d15b3f8b21f9ceab
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:58:46 +0000

    many everyone dance because control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
