e7716b7587a721ae3ad0cfb339b00a7b2715afe0
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:23:43 +0000

    takeout waste use respect now

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print([x**2 for x in range(10)])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
