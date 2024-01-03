2d869299aefd9cb11f0784c906e93efbc2f74fb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:04:10 +0000

    male black old majority there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import math
 def calculate_ceiling(x):
         return math.ceil(x)
