69275e910ba6b91a5bff1925f8c33c2f97227653
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:57:45 +0000

    laugh pattern unkind sentence evening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def count_elements(lst):
+        return len(lst)
