0b64e0ed719c4e43691cb0476ca0eb9906d06585
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:38:31 +0000

    contrast although sister even wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
