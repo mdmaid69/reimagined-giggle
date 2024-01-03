938d1f31957e343aaf711d0219f6cab192e0048f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:24:15 +0000

    Rr wave commit string vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_even(n):
-        return n % 2 == 0
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
