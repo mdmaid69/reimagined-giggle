259064c1a25fa41f33c3afc215c10c6368a1ab6d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:10:04 +0000

    hello heart familiar furniture fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
