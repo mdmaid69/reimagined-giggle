92fde9cceda1e126988451812d95bd0237daca19
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:49:09 +0000

    them contents rather branch least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_work(force, distance):
+        return force * distance
