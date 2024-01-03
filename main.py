964c7b41f804846c10302e0930c50c9606a66319
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:46:09 +0000

    why challenge know underneath cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
