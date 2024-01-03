8d33f2fea6d24844ca95380071215858e3ab548c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:38:17 +0000

    hang weather factory clear reason

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def remove_duplicates(lst):
+        return list(set(lst))
