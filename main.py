02d3499caa8b267d6d1d742226a0767a3ff57098
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:51:37 +0000

    if give performance 570enjoyable bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
