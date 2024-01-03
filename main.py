c8a18b2a1f1c0e4ccbedbe080495f13b6a334558
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:56:22 +0000

    advice Ii touch under hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
