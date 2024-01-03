27425338ab3af3466bc6bde48cea7fd9d296f51b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:10:51 +0000

    suffer bin accommodation breakout sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
