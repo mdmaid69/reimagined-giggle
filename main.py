c42c05cebfc4203c70cdb064063b2700f69278fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:44:35 +0000

    temperature giveup bringback abroad its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
