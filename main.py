dc653075fe4997d803e11c3209f48ecfe1aca4ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:00:02 +0000

    practise once blame gate would

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
