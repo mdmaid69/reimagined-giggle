1f1bdbd92706bb5a6fc96559aafb3efae735fb9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:42:03 +0000

    attempt with idea best gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_as_bool(array):
+        return bool(array)
