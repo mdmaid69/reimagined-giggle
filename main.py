662456856fda0e135255db0082ef5dafa2f09765
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:29:55 +0000

    recommend hand exercise except we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
