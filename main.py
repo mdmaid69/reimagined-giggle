69608348f2ef639bd477ae3c3a12f73154a7640d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:21:28 +0000

    knock congratulate undo spare today

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def convert_array_to_list(array):
+        return array.tolist()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
