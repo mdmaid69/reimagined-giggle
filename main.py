a78966ceb12725136065444d8f99b0959f459e75
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:03:00 +0000

    whisper recent ear possession Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
