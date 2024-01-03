0c54a1098acf9cb4c6e5ad141259f6ceb4c7dd33
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:54:01 +0000

    almost best mad none unless

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
