b798b7942c76a2ac8f52de68bc51bfc9bc06f15c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:23 +0000

    nor sound ticket pen return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
