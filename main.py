e243492b6a01192ab644e2bf8b989d4bd3333898
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:15:25 +0000

    flavour pair rush level fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
