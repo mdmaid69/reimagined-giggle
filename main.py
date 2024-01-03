c2bd24d7cd846dc27acaf23edaf517865e7bd97d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:42:36 +0000

    will cold drink noone slow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
