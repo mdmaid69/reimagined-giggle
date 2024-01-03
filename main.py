835fa50a0bb4b0e59c550838531c5decf313a76e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:17:19 +0000

    smell decide mass detail start

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
