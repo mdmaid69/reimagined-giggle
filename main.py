cb5f58c7510df1320a473391db7d9773c957493b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:28:53 +0000

    clue trial female mine passage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
