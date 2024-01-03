7013ba5e735291a01cd4331c204e3723f96911c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:06:28 +0000

    burst welcome twist view ask

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
+def is_odd(n):
+        return n % 2 != 0
