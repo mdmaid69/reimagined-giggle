9aa73da0f8e3d2deebf1661564676c835ea1e40b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:22:54 +0000

    stamp pump affect admit food

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_density(mass, volume):
+        return mass / volume
