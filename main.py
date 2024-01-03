9d0c7846f18dcf8d0abce6df7ab79938fddb306b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:12:54 +0000

    calm order Bb upstairs valuable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
