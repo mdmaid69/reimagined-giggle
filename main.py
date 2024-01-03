870c96d463e7c80ce594f98f31fbf156bb84dc0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:34 +0000

    better nice half movement frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
