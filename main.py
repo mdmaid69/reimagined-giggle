6d23bd62f473f74bc1fe6615e37b6ce3c2e12b7b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:14:16 +0000

    572enter must approve increase maybe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
