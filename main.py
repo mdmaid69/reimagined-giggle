a32201c32362419a752121bb2987f3512a657e15
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:10:51 +0000

    thing organization coat way exact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import platform
-def get_os_info():
-        return platform.uname()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
