a877a67b8cb986ab55f582993d85a30879085ba1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:52:46 +0000

    suggestion value public only thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
