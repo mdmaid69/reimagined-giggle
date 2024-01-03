737804391f8a87eec6654be3fd71701b555a1548
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:22:15 +0000

    child hold disturbing without mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def remove_duplicates(lst):
+        return list(set(lst))
