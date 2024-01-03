346e17c36c0e5edf9c92c8f67f19426f7bbdfb62
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:25 +0000

    brave tear spend its joke

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
