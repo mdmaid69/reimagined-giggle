b59bfd8efd7cf843f59adf211a74313306de993b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:11 +0000

    instrument comedown week sign moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
