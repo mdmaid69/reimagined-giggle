ba0cc27007bf2cd1ffeada0c4cd4ca3771afd123
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:56:08 +0000

    taste press 564encourage mad choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
