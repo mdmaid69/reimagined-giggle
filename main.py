07dbebb8ee846f3b278b0273f64d9176de474582
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:53:20 +0000

    brush future stone lucky busy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
