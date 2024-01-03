5396dcd0776db95ad7ccec106965d44aade2de5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:29:22 +0000

    broad worth whole carry pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_str(array):
+        return str(array)
