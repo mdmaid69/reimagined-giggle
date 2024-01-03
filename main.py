bb5a5fdbd38789f1fac2d52e25120e512e51627c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:20:36 +0000

    sink promise switch here delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
