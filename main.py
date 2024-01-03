0c95a4bd24c8c8aa49bbea06a3ed7afbd1c6025a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:56:53 +0000

    case mess exchange really confused

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
