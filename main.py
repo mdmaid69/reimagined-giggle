4cc7e78fd862a4de75d6d16bd62da9fed47debf0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:24:45 +0000

    love opinion muscle life tight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_list(array):
+        return array.tolist()
