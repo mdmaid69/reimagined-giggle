aa737501d35535f86b9a65ec0b4cc82c88c4d50d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:32:07 +0000

    natural smell now mean poor

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
