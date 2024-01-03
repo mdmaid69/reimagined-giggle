e16b80eed604b23bdfde104adae6e469b7f80ead
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:35:08 +0000

    trap hour benefit workupto often

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def reverse_list(lst):
-        return lst[::-1]
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
