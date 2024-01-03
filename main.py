b6211baae7f451ac8fe7f16e4b178ffc97f89d2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:37 +0000

    fast unless aware be skirt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
