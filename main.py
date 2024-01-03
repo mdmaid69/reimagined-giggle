8aef523f54dfbb524054339d453873c0e2c0e7d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:39:23 +0000

    serious complain friendly building shopping

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
