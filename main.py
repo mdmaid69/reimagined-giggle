36e8d09f47c3ed960076a67f1a570239e71659a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:06:11 +0000

    certain drive miss calmly drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
