0d6db7489dfbd192326798750fdb5200627863f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:43:28 +0000

    fault desperate guess nigga young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_list(array):
+        return list(array)
