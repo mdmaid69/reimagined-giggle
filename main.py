a43e132fe42e4a1df32b1e56d3572c5907f29ecc
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:26:51 +0000

    disturbing needle extraordinary replace cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
