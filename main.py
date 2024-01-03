d0b212318c3cf569c35e34db5d5662cad8c60316
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:55:26 +0000

    instruction ordinary odd piece radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
