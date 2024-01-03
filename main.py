799ec2dc25e2c8842c532e57433b2e7052e75917
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:06:11 +0000

    general best since squeeze grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def count_elements(lst):
-        return len(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
