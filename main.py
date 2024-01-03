0b2291456046e6b595a2b8d0f7576c79c93021e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:28:47 +0000

    stop Kk comeround equipment bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
