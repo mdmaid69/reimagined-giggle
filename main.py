07b66d598343c185f37bcb9e8302552f7153679f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:27 +0000

    company account damp history simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_power(work, time):
+        return work / time
