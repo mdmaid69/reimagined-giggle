9a1faa483d4eca8a6dfad8c5d9bab0038d668603
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:35:28 +0000

    skin slice arrangement takeover bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
