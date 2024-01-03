8ecbfee841af6518df5922d1b93684100f7fb554
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:08:06 +0000

    although runaway patient different visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
