ebb71ee9fa8206d44d823c35ba78963829e5e2af
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:20:32 +0000

    table used clearly neither shelter

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def create_user_dict():
+        return collections.UserDict()
