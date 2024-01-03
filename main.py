6c61dca4fb30ba7c66ceacfabf62e0306f268ec2
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:40:50 +0000

    pipe middle same usual fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
