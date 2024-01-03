b205c7fe80e28c3f981d6508d88575c3a1dc59c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:04 +0000

    chance needle against use factory

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_area_rectangle(l, w):
+        return l * w
