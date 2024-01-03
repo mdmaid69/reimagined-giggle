d883c47065f43901b2f23d9fa867d49fc09f1ccf
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:31:43 +0000

    relation mistake unable state understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
