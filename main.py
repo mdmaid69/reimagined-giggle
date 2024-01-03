9e775df3a89be37651e7fada516e40b984290081
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:24:20 +0000

    pinch long part goodbye early

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
