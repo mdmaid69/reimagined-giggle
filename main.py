1d91ab4f645856d46bc1483a2092ef0dc42e16df
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:47:04 +0000

    sweep turnoff promise rest pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
