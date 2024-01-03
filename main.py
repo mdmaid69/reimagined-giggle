054784f01874aa3b616b02e28ba554a0d5f84480
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:54:03 +0000

    organize beat effect weak serve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
