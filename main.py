ff7fc51744c4bca401964841d97cb125928e2256
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:21:00 +0000

    worth tip wipe narrow notice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-for i in range(10): print(i)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
