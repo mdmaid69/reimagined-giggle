8dd6c5361e2255ffed20f5626fe1b6c655f6f542
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:14:42 +0000

    turnup pump serve fast sing

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import sys
-def print_python_version():
-        print(sys.version)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
