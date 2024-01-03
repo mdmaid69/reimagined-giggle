22a8f1fa2093425038e02910dcf025eae6d7d372
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:57:37 +0000

    suit wherever whom travel introduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import getpass
-def get_username():
-        return getpass.getuser()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
