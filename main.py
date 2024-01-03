41e05fe5f9c6cdaae754471f05e4ada77ba9354e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:28:11 +0000

    operation Ss nervous suit react

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
