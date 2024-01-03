aa75ccdb5fbbe4f9248518c0fbd63d1d6fce72e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:25:28 +0000

    shame hit avoid basis themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
