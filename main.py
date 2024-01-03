977f0c504398a15eb9254439514094b7469329b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:12:59 +0000

    near disgusting regional flood favour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_current_date():
+        return datetime.date.today()
