cebb26c2f69ced0970b42571369e37e7884b296b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:13:06 +0000

    sea slowly criticism offend business

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
