b1a3388b449effecc229d1a72098dd27c82ab172
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:13:45 +0000

    talkoutof takeback than receive level

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
