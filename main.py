4eb0fcf830feee342a38b78579a3a88521fc2423
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:47:22 +0000

    suck community performance contact knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
