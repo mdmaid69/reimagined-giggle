f4021bdfad4e4d16fd2824c385ba1a49217752c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:54:33 +0000

    blame private runinto wish callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
