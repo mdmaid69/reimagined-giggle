fcfe61e86932fbbf3ed41e5d4270288145675ecd
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:46:25 +0000

    communication advice excuse definite boring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
