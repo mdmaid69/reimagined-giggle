d02bb55793c4fa3dddf6a81022a3d9ab2099dd70
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:20:26 +0000

    immediate base even belief forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_area_rectangle(l, w):
+        return l * w
