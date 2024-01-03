7c450d22b88ebd2320bc68d551f7607fc0fb943b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:19:14 +0000

    loose at further perform luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
