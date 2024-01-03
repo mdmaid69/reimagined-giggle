cff48da79a795d3590cce1b9760022a12fab4abe
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:12 +0000

    sort wise computer immediately keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
