ec314448868355fa450643132e848bbfeb89c1ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:05:58 +0000

    embarrassed possibly pull pronunciation tip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
