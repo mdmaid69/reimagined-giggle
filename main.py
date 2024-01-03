156a874d8238fbf8958e1c20d891881e9e2b7b98
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:35:59 +0000

    artificial year rest minority scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def sort_list(lst):
+        return sorted(lst)
