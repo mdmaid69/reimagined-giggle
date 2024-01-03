e417125978d55d8d70067a619887fff931e1c018
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:22:19 +0000

    mainly coat turnup worried top

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-print(time.time())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
