ec1ba21c4d9bb88584353035fd89dc5bfc73a0d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:15 +0000

    hide set look somebody doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
