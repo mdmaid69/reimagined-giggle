4acda43225b7d8d4139dbf2de03449666c0cd9e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:59:05 +0000

    label full OKorokay brief pinch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_perpetuity(payment, rate):
+        return payment / rate
