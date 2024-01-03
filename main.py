adb65c28c8b2261ae202d182b4bca2c99db40bdb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:42:08 +0000

    lay rare dream flat nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import getpass
+def get_username():
+        return getpass.getuser()
