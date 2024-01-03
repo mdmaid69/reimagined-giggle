ad05c8a06f13220807c9bfbb05e64e5fbaac19df
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:27:21 +0000

    middle by trick pleasant bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
