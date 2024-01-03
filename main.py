db2ceb836d07374e0369212f74832c674bd81677
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:23:33 +0000

    lunch profit stiff worried project

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
