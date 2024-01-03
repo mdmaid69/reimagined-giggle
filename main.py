886efcaecfc3eab6588802d66f7d21ebb57c9019
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:11:36 +0000

    container nigga ball about cheque

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
