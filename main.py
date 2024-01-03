ca040076a76b2cd8c876b468443f0681fd3fee63
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:51:46 +0000

    love tradition squeeze report basis

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def find_min(lst):
-        return min(lst)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
