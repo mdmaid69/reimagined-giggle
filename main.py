924192f7c584712edd0bf4f01f4b4b4f2c77540c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:18:13 +0000

    organize type closed stop correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
