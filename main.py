615d475f2d30a402478a812841cda7eb001b510b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:35:25 +0000

    believe course practise opponent pile

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
