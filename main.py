d70612113c8309d9668cfc84ef4f3d3dc2ed8676
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:24:08 +0000

    again production now instruction breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
