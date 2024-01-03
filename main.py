d2e8a344194334add798878741bbd35568faac85
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:42:02 +0000

    problem whenever politics attract hold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
