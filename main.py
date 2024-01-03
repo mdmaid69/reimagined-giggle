dd9d7310b9884c095617e945a4f552a0044aff90
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:46:58 +0000

    fight limit pride close accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
