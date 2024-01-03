740c40a5aa0d7a400899946c3509bce79fb8d62e
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:09:57 +0000

    bringforward song on run maximum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
