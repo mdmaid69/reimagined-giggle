742b675201477c98a7954035399ae81c492c7488
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:10:27 +0000

    listen stay rent culture surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sys
+  def get_python_version():
+        return sys.version
