b5f8bc6bd4dd88554bcc02b95c6211aa38a19f5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:23:20 +0000

    Ii difficult away shout inquire

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import platform
-def get_os_info():
-        return platform.uname()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def set_array_item(array, i, item):
+        array[i] = item
