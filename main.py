34c457f2484077a429241221acff19d97cd034cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:55:03 +0000

    side office geton extremely sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
