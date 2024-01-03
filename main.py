d1258b1d61c0acd357b32c63afb8bc870283362c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:11:36 +0000

    hang apart suck in detail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
