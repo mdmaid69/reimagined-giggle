9f1fa152450b36e78f45e8c559f894b2192c9e50
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:17 +0000

    always office sort concern leg

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
