f629078b0e6bd127f0e68b689b2aea8610bfb63b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:29 +0000

    herself dish pour fear bank

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
