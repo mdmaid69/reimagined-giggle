f4c6d1503aea50bdc4ce5000cf3a13cb46488707
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:49:16 +0000

    future sleep marry rather perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
