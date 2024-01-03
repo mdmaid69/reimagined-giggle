6d3526e573a7aec33cbcfbd301c64097650f3853
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:02:27 +0000

    gap Ll Pp gooff only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
