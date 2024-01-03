2fc23ba52f490e55a4de73d3a4409810ab92fe8c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:35:50 +0000

    wide tradition along friend cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_power(work, time):
-        return work / time
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
