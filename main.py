b42980c2e3262d6524204b3ea5ba7c7efc42cbb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:17:40 +0000

    sudden callup tongue study result

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def sort_numbers(numbers):
+        return sorted(numbers)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
