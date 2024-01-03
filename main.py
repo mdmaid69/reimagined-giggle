b775d066446307eb8b9b3b868c97dbaa0b5e0e0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:43:45 +0000

    often stir bridge whichever strong

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
