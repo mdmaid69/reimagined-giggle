57b9712e98bd1ba20957a2bf051418b10fc9076d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:37:46 +0000

    less comeup declaration pleasure entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
