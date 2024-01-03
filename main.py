3b0662ecb3b678b31351f6372fcf45d14327cbbb
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:06:46 +0000

    conversation preparation giveout patient human

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
