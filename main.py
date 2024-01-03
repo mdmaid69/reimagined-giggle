f4dc48e0d394892b3913d9b46a750fc1e41ca1d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:46:17 +0000

    loss fly sort the beside

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
