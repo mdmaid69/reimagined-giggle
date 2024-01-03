c9754721cc52083359953f6efcd34bd11108d9f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:44:33 +0000

    brother crash fresh language count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
