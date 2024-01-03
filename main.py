bff15a0765b7b03bd58d72b9b876526f8d8cf5ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:35:18 +0000

    treat organization act herself old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
