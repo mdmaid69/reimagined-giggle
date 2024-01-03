b3ce40a4b8e170883e7fe7642ada8b46491d6ef5
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:54:59 +0000

    shut Ll care welcome door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
