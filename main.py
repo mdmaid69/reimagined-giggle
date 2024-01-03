217709a6d7333c605cad49eb28a67a78e611b129
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:33:20 +0000

    sink special under typical if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
