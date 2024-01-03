ef676a4bfa947fc998e6fe806ddef57aa18c1313
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:35:21 +0000

    effort unkind comment tonight wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
