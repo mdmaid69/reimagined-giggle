8ec2881257a01b2a63729ee2af68ca64d6ea4401
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:41 +0000

    content roof afternoon pin wind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
