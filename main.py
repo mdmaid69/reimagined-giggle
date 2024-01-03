768a7875948075944404f8bda646aec48d2ddf8c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:48:03 +0000

    answer sleep both behave water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
