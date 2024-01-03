def79839cca910e6315974bcf193d274c3faab5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:36:24 +0000

    artificial 571enough nor ourselves excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
