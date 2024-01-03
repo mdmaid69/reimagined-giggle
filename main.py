8a8b8c05056f1feae33659a0879ba1dd70b59028
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:09:44 +0000

    use bored present lock married

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
