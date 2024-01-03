be5c4ceeaa61e358e95a993600db704092758ba7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:43:22 +0000

    least allow order autumn business

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
