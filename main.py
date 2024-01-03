c10fbb343031bdbc2cd08c04af77bed748994303
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:45:39 +0000

    law science scale such both

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
