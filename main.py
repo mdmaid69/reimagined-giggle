30509be7efbdb84d642adf4cb861c1ef24cf2061
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:50:57 +0000

    wear priority centimetre some practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
