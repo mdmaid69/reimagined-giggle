e310e23afdc9457d55b62d61b067b6c2cec2e91a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:04:15 +0000

    cap stiff radio scale hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
