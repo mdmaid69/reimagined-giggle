89f3a3d6bc652b55deef87662b453879d8ba870a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:51 +0000

    play legal kind involve distant

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def remove_duplicates(lst):
+        return list(set(lst))
