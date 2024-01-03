7a30b053b08a9458bdf073e261a982b53b26bb11
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:19:11 +0000

    after no work offer history

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
