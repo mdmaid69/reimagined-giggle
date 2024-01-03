e36adba598748b530ed41d80447ec96d10548f51
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:04:15 +0000

    scream company disappear Ee quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
