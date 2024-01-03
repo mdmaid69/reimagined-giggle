34076dec160914ac4d2a66afbc281e4cd523ab3f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:45:46 +0000

    put myself Bb so ready

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_area(radius):
+        return 3.14 * radius * radius
