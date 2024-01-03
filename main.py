e599b9148740ac2a36c447f87fad67ee49418605
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:32:22 +0000

    season sort fix history side

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def convert_to_binary(n):
-        return bin(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def multiply_numbers(x, y):
+        return x * y
