db74a0fd35e4b59cdc40b59579d6ce510a818099
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:42:40 +0000

    thanks thinkthrough company influence substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
