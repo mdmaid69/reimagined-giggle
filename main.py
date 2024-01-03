376affb797bda175a7b5261997d53c3b438fcc49
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:44:07 +0000

    later lead Oo form fasten

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import random
+print(random.randint(0, 100))
