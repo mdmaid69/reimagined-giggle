cbfcabc45aaf111540d7b1d026fd86b1d9791eb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:20:42 +0000

    similar popular 565end quarter case

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
