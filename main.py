7cfb78eb46b4d827f3832eec316045fa11932cbb
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:13:48 +0000

    picture block friend board start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
