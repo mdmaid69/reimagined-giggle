fd5aa79bed817acda349f8bc53f60af333cfe5b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:53:01 +0000

    weigh compare large destroy store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def reverse_array(array):
+        array.reverse()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
