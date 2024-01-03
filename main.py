914f3ea992b61b7f003b4f4b1cb7f3ed8806b2f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:21:48 +0000

    accommodation connect contrast money accident

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_float(array):
+        return float(array[0])
+import sys
+print(sys.version)
