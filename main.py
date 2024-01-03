4c44acfb14fcebadd21edbcb7b9a8607e0bf2ff4
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:15:08 +0000

    little shrink prove up explain

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import sys
+print(sys.version)
+def is_even(n):
+        return n % 2 == 0
