c2eb8d197dbfc7f86dfde2d5d5000a0174c83a5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:41:56 +0000

    sting we start store tread

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import getpass
+def get_username():
+        return getpass.getuser()
+def add_numbers(a, b):
+        return a + b
