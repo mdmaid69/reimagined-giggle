11a12d97530b4300d97c33287d685c2114b7a642
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:44:07 +0000

    separate empty pressure pity flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
