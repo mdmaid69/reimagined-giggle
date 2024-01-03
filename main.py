4382d6cb8f7fadb87afa3ae4aa48d41bcd64de70
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:28:14 +0000

    go each child chance call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
