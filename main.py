730ad2030d77c8049e0f35050379e2a1b0a9333c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:16:00 +0000

    that feature message quit listen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_volume(length, width, height):
+        return length * width * height
