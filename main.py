ebacfa413eee856823eeee200d179f1b002a0912
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:50:11 +0000

    obvious score learn sight prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def find_max(lst):
-        return max(lst)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
