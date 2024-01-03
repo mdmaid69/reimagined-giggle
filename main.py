1e09239d36477b59c9fb7a99391ba360d2c14863
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:10:21 +0000

    relief mine under cheat destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
