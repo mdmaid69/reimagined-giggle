811e9fcd86c742cebe6fc0ec4f6cb089bc09dbdf
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:45:21 +0000

    smell mix entrance hide protect

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
