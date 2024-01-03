6896ec9ec7e1a021d135952ad33fed74e85351e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:37:54 +0000

    him sort used politics bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
