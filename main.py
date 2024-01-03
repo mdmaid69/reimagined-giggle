961800b87931cadadaa05b167ef09bbd91cfd42d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:00:34 +0000

    sick own apart tough reply

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def sort_numbers(numbers):
+        return sorted(numbers)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_item(array, i):
+        return array[i]
