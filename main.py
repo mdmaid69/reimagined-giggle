b2f5d0e57f17a4092da9eaec8c1460733b5d1825
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:19:06 +0000

    rush matter keepfrom gift exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
