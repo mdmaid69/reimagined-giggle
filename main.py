b7795b9da0f69198a311e12950eb11cae2b549b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:47:52 +0000

    achieve nor it lookaround bored

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_length(array):
-        return len(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
