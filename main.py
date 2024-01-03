4b3bb0e9ad012da19a4e12f9ccd4a52a2b54dc56
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:25:27 +0000

    base trap clothes Ee least

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
