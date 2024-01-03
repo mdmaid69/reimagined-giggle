da25ae8f55d6eeae49a8e611473d68931698856e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:38:44 +0000

    produce combination kill further afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_gamma_function(x):
+        return math.gamma(x)
