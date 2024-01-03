4533117fd9e7a21216f8a795fbee0be7bc2244e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:52:56 +0000

    bill understand will century operate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
