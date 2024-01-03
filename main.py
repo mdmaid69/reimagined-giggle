f5d812493774941fe0c018fb703335af75d601c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:19:54 +0000

    interest remain fill question event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
