0e9fc49b99d1b40a7f4e8275bf17eec13f842454
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:55:05 +0000

    improve sink avoid life lunch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
