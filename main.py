8a32a678f82371837a1ab53d344fcd3993906b37
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:30 +0000

    provide bet several at waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
