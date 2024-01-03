c347960b242e7aa0f0b969243ffe33402c27beca
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:37:32 +0000

    you mysterious represent concentration art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
