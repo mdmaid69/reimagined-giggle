7eb81dccc67866a79fa105a0e9b71e768c001a17
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:32:33 +0000

    find sort Rr breakthrough wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+from collections import Counter
+print(Counter("hello world"))
