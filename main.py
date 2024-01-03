8c4f7ce4196dcf83d412067a1df1017a5a498600
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:21:47 +0000

    cool sudden sick balance letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
