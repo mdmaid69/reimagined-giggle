5c6ab956ab65af68e91a016a2068a20a0a370e4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:06:37 +0000

    face board border hurry innocent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def remove_duplicates(lst):
+        return list(set(lst))
