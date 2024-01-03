a77817e7fafa0e8fa663dc4d5aa4d3a372c05ffd
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:44:43 +0000

    almost together work advantage few

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_odd(n):
+        return n % 2 != 0
+def multiply_numbers(x, y):
+        return x * y
