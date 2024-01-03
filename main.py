cff056c7061fdc81bdbcb6e6701b8b63bc5a595b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:08:42 +0000

    pity drink body building mother

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def clear_array(array):
+        array *= 0
