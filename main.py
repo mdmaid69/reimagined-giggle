4dbcc6ceef3be112e8b53587e5e1d9bd77fb6818
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:51:29 +0000

    usually also commit money between

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
