ee99d267d241219de3040f43b28e5e617e964151
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:56:29 +0000

    university present just weekend already

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
