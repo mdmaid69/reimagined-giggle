e1ea5ef8cf182be2116e2dab08af5436c93cbdc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:58:11 +0000

    intend stiff area answer forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
