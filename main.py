f9e5f64c93422d9919e7ae120a9b21600b2763b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:52:34 +0000

    complain giveaway lay firm turninto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-print([x**2 for x in range(10)])
+  def add_numbers(x, y):
+        return x + y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
