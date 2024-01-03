3b4400b741bc416807ae227cd07b37b3fb0ca2aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:58:27 +0000

    pump minor report car Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+print(time.time())
