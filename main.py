d9f3c3036191cf60c4079705e3bf9ae40c087109
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:44:46 +0000

    quick open sure extremely mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
