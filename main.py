2d517173eb8eb73e10ac62f45f8ac691fc926f9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:34:43 +0000

    basis centimetre hurry reasonable nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
