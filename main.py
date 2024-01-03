3dc07b5bf32f52a0eb2500cab65c8c5235c2c1b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:25:34 +0000

    date benefit thin form spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
