94d8586dccff438680a4c52ed62db99d4a1641ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:03:39 +0000

    prefer simple like importance deliver

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
