64dca31adfe76b4fa8b4f4a9897adad684b331fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:41:13 +0000

    anything turnover use carry stiff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
