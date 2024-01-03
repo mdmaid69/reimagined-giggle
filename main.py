eb9ab6504807f057b3318947611e749f98c30df1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:21:49 +0000

    survive getoff down invitation unfair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_string_from_array(array):
+def convert_array_to_bytes(array):
         return array.tobytes()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
