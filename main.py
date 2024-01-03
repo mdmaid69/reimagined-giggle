1e4a5bc2c4a619040530612a8594bfb3c010f2ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:03:14 +0000

    same straight people only frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def convert_array_to_list(array):
+        return array.tolist()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
