e33468c72eead3b62b8770f315a2e3b7803b4e90
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:20:21 +0000

    offend patience so always wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
