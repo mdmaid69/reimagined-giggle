d41e7f4ccb27d6c5f24cf9e7572061cb2f743218
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:12:16 +0000

    occasion instant lunch container waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
