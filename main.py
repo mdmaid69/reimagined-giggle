0c1c267c347f89cd9108cdc609581398c84b2e75
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:56:26 +0000

    conclusion frame average oil sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
