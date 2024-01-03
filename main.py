59c56f6041ae02c5a5b43572b8e60bc806ee0d4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:00:06 +0000

    include persuade cross lay taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.ctime()
