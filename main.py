873072cd2478c8a0221f171ead05c3468996b8b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:26:21 +0000

    mixture suppose quarter concerned art

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_bool(array):
+        return bool(array)
