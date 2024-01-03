48c5762b7f6aed34f18dd415a37e6489324df13e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:29:18 +0000

    179bite school 572enter show separate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
