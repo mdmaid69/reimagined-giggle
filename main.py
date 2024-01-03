3eded2b7034d52a185bd90d9ecea0368503cc212
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:40:52 +0000

    cover steep object ahead upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
