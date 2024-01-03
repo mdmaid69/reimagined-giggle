8a57cf7ab75b3d91a28d4fba05c45f30414a06f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:17:21 +0000

    swimming on experiment scream hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import datetime
-print(datetime.datetime.now())
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
