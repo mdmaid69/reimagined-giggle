a1c1276a6532028e9bf47cb23598f8505f66a126
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:50:29 +0000

    exactly joke workout load wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
