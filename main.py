947431b2fd1bc78dc8dfb3e8dee60da0a41fec00
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:33:15 +0000

    throw nose pretty like move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
