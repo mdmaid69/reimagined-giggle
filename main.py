838b0e8865e069d5c21b0d4305f09b77378ebe3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:28:41 +0000

    uncertain round against advise boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
