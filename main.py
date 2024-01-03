26850e784430564332ee55f26ca70fcaea1779fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:19:07 +0000

    original shall bringup one nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_counter():
-        return collections.Counter()
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def pop_from_array(array, i=-1):
+        return array.pop(i)
