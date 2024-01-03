af81ed596a6eb33b555ca5bc3b1b7e578dd4bcaa
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:28 +0000

    Rr stretch call damage hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
