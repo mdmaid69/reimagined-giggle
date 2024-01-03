3f4fa3e74bdb2fee3f00660c839e662ac96c6b67
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:01:20 +0000

    communication fun news positive book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
