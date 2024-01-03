cba75798cc725b3e917fa7ed2298919fa6e563a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:27:49 +0000

    real wonder sail flat his

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
