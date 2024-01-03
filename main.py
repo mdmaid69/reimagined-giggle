f2282a23c882f483f7c4a32b4366cc7c12718540
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:44:58 +0000

    Ww target simple mistake shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
