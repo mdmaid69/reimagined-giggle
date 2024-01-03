fb86990ed6da1249767aa65bf283505f248ff6d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:19:54 +0000

    similar lookafter main join major

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_pressure(force, area):
+        return force / area
