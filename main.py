54cd6b68464b43979de7fb584a95ac22d0251107
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:15 +0000

    interview level rent border freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
