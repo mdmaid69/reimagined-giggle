c724efb4fe44519ec43423a0887ccac78fba4ff1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:35:16 +0000

    calloff show more wise cry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import datetime
+def get_today_date():
+        return datetime.date.today()
