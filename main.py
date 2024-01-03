16e8c79e8fbef3e21a8e787f5e31d19ab985327b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:09:56 +0000

    affect sing distance problem recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_volume(length, width, height):
+        return length * width * height
