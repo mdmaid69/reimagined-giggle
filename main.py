ee6782b3109b3792e71d96f8ebe5ee10f131b232
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:12:55 +0000

    favourite cheat lesson fast only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def get_array_as_list(array):
+        return list(array)
