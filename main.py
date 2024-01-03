5793ef0c494992b0b24d952e11eeed91f34b89cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:14 +0000

    no excited way suit measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
