7497635f6c44792d24cbbddf4b818dd946bd58c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:07:48 +0000

    sharp couple fail brother minority

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
