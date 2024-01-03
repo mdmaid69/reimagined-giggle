d1d892c20c41d695cb33f514f45af2fc1126e2aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:40:13 +0000

    go emphasis true so fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
