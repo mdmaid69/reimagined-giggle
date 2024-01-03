0edd77ef81e608c319e5b286f7a36c8921b2b3f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:02:58 +0000

    chase only admire especially cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
