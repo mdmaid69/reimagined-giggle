2eedaf242d13262378a3f82fa1db60056e57695c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:08:23 +0000

    read along damp bottom shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_user_list():
-        return collections.UserList()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
