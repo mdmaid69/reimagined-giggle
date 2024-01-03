7d56baf9de0775520f18ed57daf383b57f33c1f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:27:15 +0000

    trade we good realize because

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
