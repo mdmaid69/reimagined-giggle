6b797cfa34c7df608d6d57c25903dbc2f48b06b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:58:59 +0000

    error trouble affect confusing cruelty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def is_odd(n):
+        return n % 2 != 0
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
