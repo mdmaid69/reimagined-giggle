0967268f83e86fe14af7e88791cbcdcd8d0bd343
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:15:10 +0000

    stranger win land but divide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
