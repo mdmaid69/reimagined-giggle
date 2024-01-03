9599c44761eed54de83aa952a4ba3afc6d9ca947
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:01:57 +0000

    refer everyone draw goodbye break

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
