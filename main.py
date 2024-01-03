0e83cfe98b4c6dd126942a2cca3991c5ffccf47e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:06:52 +0000

    workupto turn ordinary please government

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
