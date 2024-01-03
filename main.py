ebdc1fc1d3d437faf0dc875d0e754cc999a66d31
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:44:25 +0000

    thirsty proud ear wheel new

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def reverse_list(lst):
-        return lst[::-1]
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
