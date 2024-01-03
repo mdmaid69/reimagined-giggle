82dc31fed8fefc29f9b02144ec3308f1f099c460
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:50:25 +0000

    crisis smell spread why tap

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_length(array):
+        return len(array)
