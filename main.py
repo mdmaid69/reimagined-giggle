7e16a601ef4173492a12ae0c2e7e4184edaaed7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:03:27 +0000

    tell Ee shout gap sure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
