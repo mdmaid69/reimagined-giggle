4a4423b4f7d686cc6ae5549a9f400b0e8a00e90f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:33:43 +0000

    difficult big Vv address brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
