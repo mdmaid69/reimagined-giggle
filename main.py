5c68eec5004fbc58ed68bc80e4aa84278bf4d527
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:47:27 +0000

    practise leg present live almost

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
