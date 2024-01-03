08a2571dc9eefd1ad461bdb69a896aff8cb2b6eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:10:54 +0000

    sleep this opportunity attractive cry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
