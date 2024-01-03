bd2f4a793b48741561ea36a3564844dc6a36dd56
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:44:53 +0000

    run better judge complete hot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
