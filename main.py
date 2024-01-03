b4a5dd31b37b3d3a0c749c9ddc76783f75d1abfc
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:40:51 +0000

    supply allow help embarrassing one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
