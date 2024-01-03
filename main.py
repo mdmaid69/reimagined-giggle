3b10a80938d8e1bcd5c5b6baa4a4ff0cc19c22b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:45:03 +0000

    cruel machine seem aim admire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
