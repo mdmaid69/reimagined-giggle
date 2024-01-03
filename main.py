dc8963ff04a86bd5fe47ad20a0ac4227eb3b0e1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:24:41 +0000

    thick film towards switch stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
