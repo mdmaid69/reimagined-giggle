abe9b678e25d15d16e38a3e447bdf497312e9bef
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:22:09 +0000

    dance protest several nigga joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
