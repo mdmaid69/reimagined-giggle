7b09d7712f4ed0006fcb277adc9b68f6287d426f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:47:59 +0000

    may deserve home young soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+print(os.getcwd())
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
