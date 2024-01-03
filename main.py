129b7b356730bb04741afbb3cd60c6806eb5234f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:04:17 +0000

    moon breakfast no reasonable length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
