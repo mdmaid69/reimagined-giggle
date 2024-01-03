c83f50a6e264ea889b1649538d4be062b7edd6a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:36:07 +0000

    doubt deserve press commit pretend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
