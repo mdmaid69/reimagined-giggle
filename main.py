ec7c1132d83e616be3e5cad6ca587a822b89cf50
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:25:16 +0000

    bother odd fold earth shrink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
