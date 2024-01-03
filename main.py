ac2f3fa5cfa3a552622aa8e561782e17b80a37eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:41:58 +0000

    wait way whisper imagine tread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+print([x**2 for x in range(10)])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
