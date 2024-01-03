5ddd835c432100fafc4b418bd6292a97415ca6f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:18:42 +0000

    such discuss 567enemy snow whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
