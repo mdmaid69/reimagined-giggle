809871db3cbc557dbda1fecff8a23f3870a8956c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:35:50 +0000

    admit range Kk hello chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
