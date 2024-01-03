45ade83fdc8051cfb80206594c2ab9c3f956445b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:28:21 +0000

    kill consult reserve sad marry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
