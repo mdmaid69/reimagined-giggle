4fe75e2fcab2784be056d7af7ac61d524947c5dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:03:57 +0000

    mouth square clothes usually 572enter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
