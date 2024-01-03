f51b4604bce94889ee1f90c0077db8be01ff5951
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:24:44 +0000

    stretch example peaceful bin while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
