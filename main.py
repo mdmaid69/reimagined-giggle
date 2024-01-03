8bb0ff21964e60bc4e0c2a5072098cd98eb634c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:23:16 +0000

    safe middle the pay talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
