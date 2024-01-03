b58a8295fe76e03d392220eb73587432cd61dd1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:34:05 +0000

    finish through several you among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_floor(x):
+        return math.floor(x)
