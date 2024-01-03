d266d9f1a896c5477112581c5aa438df504f2155
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:49:04 +0000

    use Nn date present software

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+print("Hello, world!")
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
