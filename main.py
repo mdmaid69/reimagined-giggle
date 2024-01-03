e43fbce3f18201aedb084674bbaf4755887ad676
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:31:02 +0000

    agreement disapprove live spend confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
