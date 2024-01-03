16564a733178ec6eaf85e457aa26eef6636a467d
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:11:18 +0000

    dish unfortunate breakout create affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
