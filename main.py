febc84fd4c12e7eb6c8c3b492d767075e12b176e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:03:49 +0000

    defend the train point sore

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def subtract_numbers(x, y):
+        return x - y
