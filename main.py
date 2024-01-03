3fc5c8047362a29a1c9a44f409155d5e69345b1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:06:04 +0000

    taste welcome ball door basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
