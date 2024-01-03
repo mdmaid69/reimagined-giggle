1fe2d2ce53fbb5d3247b9ac037b21e4d11d90bc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:49:06 +0000

    uniform such imagine thick anybody

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def count_elements(lst):
+        return len(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
