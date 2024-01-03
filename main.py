cd54a29754819d901f55a10cee5fe0cb692b7fa6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:46:31 +0000

    collect answer travel might success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
