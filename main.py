1609867e5e87baa158235f2c4045d9e7a88e1645
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:26:10 +0000

    sick sensitive print book principle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
