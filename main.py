69ccb303cac219ceab4b0ef57a5dd22c09881d51
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:43:15 +0000

    boil need finish only concern

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
