26568c77ab39c3fe6d1ecf321ed8801286612e3e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:54:25 +0000

    year pause behind kind contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def add_numbers(x, y):
-        return x + y
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
