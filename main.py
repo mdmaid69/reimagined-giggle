536081dd41010ebc4025c70edaa88d230699c301
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:04:38 +0000

    home clue rain minimum exciting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
