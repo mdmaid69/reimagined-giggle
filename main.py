37b92fe423325d7e5388521eda6def9c44845933
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:50:01 +0000

    excuse careless delight punish return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
