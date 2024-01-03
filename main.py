7cb4e886b8bfb6595c337c67919c57f7d39ef3d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:06:13 +0000

    float paper embarrassing plan comeround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def convert_to_binary(n):
+        return bin(n)
