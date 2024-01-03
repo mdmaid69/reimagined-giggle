7aa74b7308165a068c6c22ed3e065deb520edb87
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:36:35 +0000

    surface reach opinion thought excited

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
