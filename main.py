c229c8e7adc8204d5b3ced84363ad4014a8b1984
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:23:48 +0000

    rest gap slice runover walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
