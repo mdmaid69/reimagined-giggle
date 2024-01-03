2856cce56a2c82b11847df5b16a45031d5828f71
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:29:56 +0000

    train reply exchange Gg variety

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_speed(distance, time):
-        return distance / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
