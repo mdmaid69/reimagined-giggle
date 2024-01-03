577d1712ebe6f3856167eae58027ef467e26cb19
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:03:04 +0000

    contain guide finish threaten myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
