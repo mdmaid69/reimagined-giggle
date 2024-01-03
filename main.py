968a6de242fba6efe0c2fd4be921f246c1688e27
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:25:04 +0000

    murder test seem discover can

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-print(sum(range(10)))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def calculate_area_rectangle(l, w):
+        return l * w
