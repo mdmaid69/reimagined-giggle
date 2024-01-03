7e83ed8e810428199d24bf35f8c25e76411cf030
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:41:37 +0000

    bar Ii workon shrink visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
