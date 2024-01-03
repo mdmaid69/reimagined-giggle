d2781b374012b557f6d65bacf74a1050ffc1fb14
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:33:14 +0000

    classroom neither walk tend back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
