e5b4b5c4b0b6a4face0d382507d74c18ed6310c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:07:15 +0000

    official uncertain identify control switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_slice(array, i, j):
+        return array[i:j]
