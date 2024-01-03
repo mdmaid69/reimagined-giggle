e828875b00a9c173df9a70fb77eb7052d7a90791
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:30:57 +0000

    across measure cap petrol change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
