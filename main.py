404bc1e4f8326b56d0742ab56bfc8486f9c07618
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:30:47 +0000

    tax during swim there charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
