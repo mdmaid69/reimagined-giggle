c1b58d92c3fc52128833f15d7950d3d81e18fa9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:03:27 +0000

    safe shop turn heat run

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
