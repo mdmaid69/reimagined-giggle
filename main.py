78ac8b9f701c348de41d4e351e686aababf8fda6
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:27:08 +0000

    question view turninto regularly century

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
