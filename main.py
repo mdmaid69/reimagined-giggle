7c15eceb840a6f8fc08d21008c10ce3c6691d470
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:17:00 +0000

    empty bringback case deserve information

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  def multiply_numbers(x, y):
+        return x * y
