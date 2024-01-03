156d1655c5c79b4e5d88ac71b721b366b47decd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:11:11 +0000

    bend comfort score she goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
