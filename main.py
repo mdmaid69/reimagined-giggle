964fa0e7c2452d0e6fd43069cb14e814edecaefa
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:15:31 +0000

    summer plenty disturbing apply tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
