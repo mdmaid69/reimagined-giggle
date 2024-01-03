f643721763dd119b531c80a1f11d51ad92096f20
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:01:09 +0000

    upwards duty comment suggest uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
