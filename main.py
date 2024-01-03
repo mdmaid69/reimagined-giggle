21a1862059fdf0e3d48bc91ad3bcf9ddb8be2e3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:33:38 +0000

    contribute also rescue file amount

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
-  def square_number(x):
-        return x**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
