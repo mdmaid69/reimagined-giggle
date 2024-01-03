e508f3e0501721e4b67662c7759f5ecbca0cad56
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:41:23 +0000

    guard check forward time godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
