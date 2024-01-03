ae2b6892fbb22d97f4a062bfd54f14dffb51a2a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:26:13 +0000

    help prove complete lookback(on) doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import getpass
+def get_username():
+        return getpass.getuser()
