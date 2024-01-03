4e80c3d4f4229adb05b836879c61870b91646a2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:10:30 +0000

    offer alone satisfied busy know

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
