7accb4af3fce33188745c19048e406768b5cbe1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:49:19 +0000

    bottom length account reply shut

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
