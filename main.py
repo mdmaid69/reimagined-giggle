7079352ee3c849001b33cffc76f90362d9ffe3fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:16:40 +0000

    simple within mistake suck afraid

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
