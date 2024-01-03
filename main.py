417ef969c6bc2e54c799ab8d171cbee69cb39592
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:01:28 +0000

    unite offer recognize cross guard

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
