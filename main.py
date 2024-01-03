c7403ae68d535662dc798a05372b9b2523faa692
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:02:02 +0000

    character operate tear or unpleasant

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
