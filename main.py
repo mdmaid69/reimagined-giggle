bc131dbe151e3bbee501b80c2293cc0d81cb242c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:51:12 +0000

    unless trouble throw however Kk

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
