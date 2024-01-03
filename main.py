e52b12f5dfed19756efea201954243ae1ed069fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:06:22 +0000

    holiday punishment advanced lookup section

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import re
-print(re.match("h.*o", "hello world"))
-for i in range(10): print(i)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
