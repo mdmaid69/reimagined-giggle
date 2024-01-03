c4ff853443ea41d1294ff1f6832409e031429fd7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:02:43 +0000

    lunch beat fair somehow moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_list(array):
+        return list(array)
+import numpy as np
+print(np.array([1, 2, 3]))
