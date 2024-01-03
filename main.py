06d0a8bb0a1ca05d17e968d3961f1a0bafb8eaec
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:04:40 +0000

    appeal able wave tip among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
