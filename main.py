53451829837df1bc992e4c0170fff8f5c1b8783d
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:02:29 +0000

    through climb accordingto makeinto correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
