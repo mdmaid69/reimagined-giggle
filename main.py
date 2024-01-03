23f1c0026ae0a7fbe04fd5731eb94cb0c657addc
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:42:45 +0000

    demonstrate gather conclusion increase frame

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
