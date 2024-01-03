3384af99e298e16c0d696433921bd8e860a49848
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:54:49 +0000

    dear explanation holdout final effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
