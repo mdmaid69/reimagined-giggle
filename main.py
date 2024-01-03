6e5357f6e42c43ad552e1a979245aaacf8b08edd
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:46 +0000

    contrast push translate worth send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
