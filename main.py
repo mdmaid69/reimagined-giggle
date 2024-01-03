e8ab35f7db92488c57d59b7628a0add08a7c0856
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:46:39 +0000

    challenge minimum press pity area

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
