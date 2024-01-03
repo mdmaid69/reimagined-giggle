fb3a496c60be868f29dfc5934078f0159a24220d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:57:09 +0000

    autumn match marry prefer win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
