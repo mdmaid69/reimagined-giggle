78e58964914cc129f63d4a3b0df3c104fd8651fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:00:34 +0000

    fortunately but deliver quit anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
