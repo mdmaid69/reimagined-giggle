02d3feaf8f8b8695df02c77920ab7e4337094e14
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:03:03 +0000

    whistle healthy father start Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
