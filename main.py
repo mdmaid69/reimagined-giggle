3f76ad5a32c40fb2c614148e459b4300d1ce3edc
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:53:53 +0000

    rank mainly union firm genuine

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def square_number(x):
-        return x**2
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
