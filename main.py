56699a8d410f3279e9b0018e5649786d1f826ff0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:04:39 +0000

    hurt little warning clean shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
