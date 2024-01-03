baadf1751769b19cb0d78fd7e9cfe7c99025255e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:07:37 +0000

    hope belong follow accidentally whole

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_buffer_info(array):
+        return array.buffer_info()
