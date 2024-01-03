76791d56ea8158f2f7ebf4974057fef91a922fd3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:10:49 +0000

    this evening pretty godown circumstance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
