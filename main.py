b855370f0c7ec3d61175f23690bce02cba31e185
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:41:33 +0000

    club flat so back Yy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_pressure(force, area):
+        return force / area
+def calculate_density(mass, volume):
+        return mass / volume
