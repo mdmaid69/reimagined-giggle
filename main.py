e0be2beefa9d32c0a1e2eeaf5551e5c2ff001e38
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:39:51 +0000

    crash fall should return callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
