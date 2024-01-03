49421d06ebe050213bd43242e7444134c803a9e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:36 +0000

    mess behaviour clean anywayanyhow divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
