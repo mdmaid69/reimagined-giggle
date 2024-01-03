2b06b4a086ae2cb516a24c9667ac3e4969825638
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:56:26 +0000

    process raise raise lock beginning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+for i in range(10): print(i)
+import collections
+def create_priority_queue():
+        return collections.deque()
