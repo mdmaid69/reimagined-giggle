65e21b325484c5c721de75989b5fbae9e4119f08
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:03:38 +0000

    worry science frightened obtain disapprove

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
