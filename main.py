45db7dd61c44fd0eb780958f50ed4ef67177423b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:56:17 +0000

    runinto let which undo juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
