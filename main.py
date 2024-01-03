5a1ad5296bb95426016c0668d00fc8190ba13030
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:02:25 +0000

    thirsty help this substance nervous

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def convert_to_binary(n):
+        return bin(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
