339b9dfdd641c16453c950a5d6066305f238cbb1
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:38:35 +0000

    quantity month curious breathe letter

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
