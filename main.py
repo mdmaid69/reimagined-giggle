45aedc17ab9330509cf9745dcb33d4fb7e67d0aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:25:08 +0000

    habit trousers round pretty will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
