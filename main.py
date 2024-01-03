1dcad77f627f0ee4469165ff44edff112bf5ff65
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:25:36 +0000

    squeeze pretty glad sound event

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
