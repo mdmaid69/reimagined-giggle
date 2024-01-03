8f56b0d8e28014e8149e5ed5b7cffb8d1e26b94c
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:55:18 +0000

    immediately roll fight wish closed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_area(radius):
+        return 3.14 * radius * radius
