39c8086eb941abec1901c65c602ec580d0989ecb
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:37:45 +0000

    leg disagree smell scrape comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
