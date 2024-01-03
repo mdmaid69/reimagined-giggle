fca6066ca1d2e59d1922b6a5257c4dfcff359377
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:01:33 +0000

    wrong bringup crash understand immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
