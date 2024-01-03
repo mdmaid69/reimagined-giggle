2b501f805c2f253655124db0231ec2639c2996b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:37:20 +0000

    tour Kk tall objection money

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
