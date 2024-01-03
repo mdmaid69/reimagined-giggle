50ae3bc48975326840d6bf42e6c59c2c59dc9569
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:31:29 +0000

    value copy public transport declare

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
