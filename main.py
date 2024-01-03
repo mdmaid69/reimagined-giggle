d07d1edc8618eb782ab9347d44659a311a08840d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:37:41 +0000

    print century national neither pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
