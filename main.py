850b0970c2922bd10c7eea268fc32bb2e1730683
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:54:42 +0000

    trouble available Ii whether surface

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import re
+print(re.match("h.*o", "hello world"))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
