3ecf10c93047d97bc6629e749bcbdfa008ce1a32
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:03:15 +0000

    tidy point beginning great brave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def clear_array(array):
+        array *= 0
