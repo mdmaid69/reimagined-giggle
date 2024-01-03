8235a8504ff5e29df45ed3460531b846c7e77605
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:55:44 +0000

    fond sense organization violent edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
