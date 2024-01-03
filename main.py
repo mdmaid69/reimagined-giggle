5bf81bd7e8cc5213e152942d284c4fa6eacc51a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:50:19 +0000

    everyone brown communicate later region

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
