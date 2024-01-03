fba03f6b5dcf0be60d1c523e92d1d9d1391ecb5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:49:33 +0000

    count sight undo second turnback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_list():
+        return collections.UserList()
