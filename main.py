199d466feb8d88fd4cbfbe6a63c8fda042438a3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:49:28 +0000

    her grand tongue complain take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def append_to_array(array, item):
+        array.append(item)
