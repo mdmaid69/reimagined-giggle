2a3790a18376eb7ce1bed088b81d35f0934174dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:19:37 +0000

    strong splash proper although love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
