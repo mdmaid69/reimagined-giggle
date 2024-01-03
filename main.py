b5f1e1e73dd3885da91bf4c5effb16e15b63258a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:30:59 +0000

    stand proud thing experiment rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def add_numbers(x, y):
-        return x + y
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
