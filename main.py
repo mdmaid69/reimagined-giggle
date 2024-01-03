38fe92c5aee57000c6de484288bcf3ae32f27bed
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:56:19 +0000

    them sweep delicate excuse instant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
