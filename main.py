681978b46e2ccb4f14916f1f66d31577f8d0c9ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:24:05 +0000

    fact case wind so height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
