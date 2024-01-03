9ba7cf80843faac5f10b4365138aaf873dc04025
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:20:08 +0000

    reject mess child aware feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
