027796d65c9aaac753812948b23f6f70c506c2dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:01:07 +0000

    share tense pause discovery she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
