2066d006aedc278737bfe372689e6c3aa1d2bd1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:52:19 +0000

    pay form cheek stamp already

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
