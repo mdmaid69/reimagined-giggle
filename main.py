18f5338144126b1c6a954bbb88d51e90f191d2a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:04 +0000

    deliver nose you burn declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
