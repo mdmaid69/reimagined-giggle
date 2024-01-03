522878119fc898718f58ef3bc70ca857468501cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:40:13 +0000

    impression second private machine exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
