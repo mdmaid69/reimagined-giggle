c6bff5567fcdc84906f6539c29e9c06a68934d88
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:39:50 +0000

    but wash cook related trial

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  def convert_to_hex(n):
+        return hex(n)
