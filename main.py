9490e7f74b3e13b0db71e73cffb80173056c2dc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:19:23 +0000

    own debt 573entertainment cause think

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+for i in range(10): print(i)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
