9de95350e31bf82afc3e3bfd21128436d07f512b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:01:39 +0000

    show task narrow favour rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
