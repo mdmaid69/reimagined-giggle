93fd8450a156fe29eecfddedfa4510f4cb088737
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:27:38 +0000

    year let vanish connect place

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
