df72d7db59cc10e34f35e034999e780b2b96d4df
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:34:16 +0000

    embarrassed water trip relief for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_even(n):
+        return n % 2 == 0
