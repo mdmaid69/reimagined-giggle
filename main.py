c8cff18cf9c1cfd7c0553662d9a707f5a37769d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:37:42 +0000

    influence comein but only bottom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def delete_directory(path):
+        shutil.rmtree(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
