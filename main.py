b727054ca06116da327a24b3806ee13a39ec601c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:41:05 +0000

    sound probably interested gather surprise

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
