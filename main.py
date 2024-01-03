a4666b40560071a0d02d309ab5c5ce9a9c328f09
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:21:46 +0000

    deep as makeinto uniform win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
