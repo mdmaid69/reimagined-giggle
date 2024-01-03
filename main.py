dd6a76bcf8de3e811d5d41e0d7766cebb75af2a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:33:50 +0000

    comment holiday power remark food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
