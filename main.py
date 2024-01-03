ddde1031793bb0abf9c26508ec214229c30cea9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:49:30 +0000

    spot trial copy demonstrate old

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
+  def multiply_numbers(x, y):
+        return x * y
