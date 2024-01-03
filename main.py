3cf3d9b5561f6a150f26629c588e2d9d3651297b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:59:06 +0000

    room compete convenient much Cc

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
