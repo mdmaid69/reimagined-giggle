b4f9c4a6fcdc74391a51bdadf4fd5ae6d9547a92
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:21 +0000

    gather sort direction limit mouth

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
