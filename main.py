2b735aa623cb233c599953331f9d86f96d52b43e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:50 +0000

    equal production stage thorough form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
