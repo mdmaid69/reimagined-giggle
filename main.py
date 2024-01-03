e00ac6d943eb84ab68b88928439564a15b25c3cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:14:40 +0000

    beyond march plate develop taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
