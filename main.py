f3155220688b70a3ec988ac5512c050212c70f7b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:38 +0000

    breakfast discovery quick behind duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
