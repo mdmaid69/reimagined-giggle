8443e6cf4b6b2fb51a0f09eccebfbc540d5994d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:32:00 +0000

    performance crime shrink travel still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
