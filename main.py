925a49cbb8700524a73a5ce8622d49caba4d58fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:39:39 +0000

    unfair stick middle anxious confusing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import time
+print(time.time())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
