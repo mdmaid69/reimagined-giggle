b8fe65eb5b05a22f37035c3ffff34a68815ada81
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:44:56 +0000

    real squeeze house stone set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
