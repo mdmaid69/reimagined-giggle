81d1da1266046b0a551656c1ed453ba3126e3292
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:20:14 +0000

    especially visit stop fine fault

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
