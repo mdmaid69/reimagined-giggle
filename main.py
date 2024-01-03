086cc1620374a136b6509c842c79fe4e60f4a4a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:54 +0000

    lead trip man become appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
