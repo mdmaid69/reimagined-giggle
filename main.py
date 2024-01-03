2f65de25d6ed278143753396ead2c9052471d2bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:41:10 +0000

    receive lift behaviour water plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
