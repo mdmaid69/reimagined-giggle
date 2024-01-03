cb8a9a63fdc01441e9202a33a82b0a95611b77f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:59:26 +0000

    insist poison sound calm kick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
