fe608713faac41aab2b590f9e6a41d39a5cebd34
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:11:42 +0000

    earth damp neither even afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
