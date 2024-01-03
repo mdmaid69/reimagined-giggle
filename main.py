f503875b3a74449434b67e396804a41a691d12d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:23 +0000

    fresh she work punishment path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
