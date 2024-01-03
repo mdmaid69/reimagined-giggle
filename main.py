c811da69bc5d6cb6b5cf08ee1e4457573ba00cd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:50:38 +0000

    low information come love listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import math
-print(math.pi)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
