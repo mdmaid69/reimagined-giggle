de12dcb699307caf2533f266c8f4b709baf79496
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:57:51 +0000

    build tell schedule week earn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
