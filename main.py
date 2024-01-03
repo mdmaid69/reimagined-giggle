2a61efe05dc433d76d8a20f83075af367ffcb2ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:33:44 +0000

    figure temperature city make will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
