fe9f34aae14ec86ad3b2dbead236d968c784e670
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:50:28 +0000

    free store even concentrate factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def reverse_list(lst):
+        return lst[::-1]
