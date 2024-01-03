36c1a8856371a39d66dab5e1924cf5fa5ae344d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:32:15 +0000

    border train why comeout whatever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
