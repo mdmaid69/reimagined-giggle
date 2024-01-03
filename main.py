2a4c27128601676be5a610b542b5f85f41f435a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:06:32 +0000

    sense roof road different nowhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+for i in range(10): print(i)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
