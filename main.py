7485e815d41facff9915f3ecd9c138303316e0ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:14:17 +0000

    surface contents actual nearly active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import random
+print(random.randint(0, 100))
