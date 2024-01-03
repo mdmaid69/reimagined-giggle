2de79cdb353978eea5b445889fd4d7b38e06692f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:09:47 +0000

    test remain issue sensitive belong

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
