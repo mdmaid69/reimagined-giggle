5e5e12722809388b9347a02c596fea2d244bf878
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:28:54 +0000

    term pocket godown account interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
 def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+        return 1 if n == 0 else n * calculate_factorial(n-1)
