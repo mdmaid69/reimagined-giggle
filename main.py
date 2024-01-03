040e838112d1c10c9a1eff0c35c13e003743f641
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:57:51 +0000

    disagree godown no suffer increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
