f1a90cc0b51c5dd7d6d6ea475d460ad365a401aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:40:20 +0000

    outside drink grow cheap back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
