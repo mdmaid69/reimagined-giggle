7db264eb21a35680079541591050c0843cbeeeb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:54:52 +0000

    manner 179bite turninto gooff promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
