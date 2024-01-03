ceddf2654d2ab20db56161832d1ab7f23376431c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:36:07 +0000

    negative language task beside hard

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
