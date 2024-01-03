cb7d34716925708930a89022411b7db5627d161a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:43:21 +0000

    word direct importance comment practise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def clear_array(array):
-        array *= 0
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
