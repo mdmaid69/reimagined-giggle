6a5d7a974b4f9a52aba47d854c636465105ebf1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:38:56 +0000

    blame string thorough free original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
