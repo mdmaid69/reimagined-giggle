f7fa52e7ffd72dfa7835476a85957833bd722e48
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:28:36 +0000

    afford will medium never circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
