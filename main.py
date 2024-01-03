3406ec048a7972f47517e167e5f5228a9e8e34c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:05:49 +0000

    amount accidentally contribute holiday commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
