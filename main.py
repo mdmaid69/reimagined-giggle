a050e1c4d9b24e75a7158ba838782c0a494e4285
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:30:54 +0000

    forever spring please everyone safety

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
