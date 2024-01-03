73ee1eb821b06f2208bd42f0124af2927a7391a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:06:32 +0000

    rain do radio temperature wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
