25926d81445d8d31414783cbb37573c28c79611b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:15:27 +0000

    keepaway number callat too power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
