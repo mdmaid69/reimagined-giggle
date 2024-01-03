30d74e5a85d3f587dc138081d0f727e5efb82015
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:27:23 +0000

    trial learn occasional month look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
