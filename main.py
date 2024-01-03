25afea5e076fc11af1eb0209a581a243db42b526
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:25:37 +0000

    fall long travel important force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_error_function(x):
+        return math.erf(x)
