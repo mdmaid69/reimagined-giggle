10dc3d457c2132d586138543e7e7b0079193fdfb
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:41:31 +0000

    possibly bother break car bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
