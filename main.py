9928548cef679812def4a2a93edf4e03ce585b1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:45:01 +0000

    suppose start lost content describe

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def split_path(path):
-        return os.path.split(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
