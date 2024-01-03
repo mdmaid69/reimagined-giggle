91c91d12351c671435d98c215934df3238a66a68
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:48:16 +0000

    sister Nn pretend majority mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
