6ed68c4906f1a73a23406727e877c62b1b7c2372
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:23:26 +0000

    572enter choose normal short satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
