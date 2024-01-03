d8a25e2ebf365b3dda45984c15a76aaaa4b943ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:19:35 +0000

    frighten around but file traffic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
