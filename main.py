f54d358691ba1944368584544d02016619b1e21f
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:06:28 +0000

    niggaacademy expect false that tragedy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
