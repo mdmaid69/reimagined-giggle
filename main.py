329347f6d829e4649975bb05e2b783d4cd7403f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:46:02 +0000

    performance forever last school exchange

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
