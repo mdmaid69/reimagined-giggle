fd15ad97d1ad9a36c4002cfac37d8aefde2a7b9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:56 +0000

    mean 566end clothes apply central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
