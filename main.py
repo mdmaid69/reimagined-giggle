3c682cb0cefd6d852db329f678efb14d1a7ff7e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:19:23 +0000

    address favour achieve average shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_speed(distance, time):
-        return distance / time
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
