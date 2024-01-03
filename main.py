4dea38e1d9d38c1118abb9eced62f6c24a592697
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:26:26 +0000

    hollow day hurry instant that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
