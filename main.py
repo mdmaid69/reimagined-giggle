c8ce12794e865a8c826d9e08ff1c8c347165a1cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:09:56 +0000

    floor separate lock guilty squeeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
