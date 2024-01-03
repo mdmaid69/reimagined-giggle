2fdb30b5ceaeafba77fc15a3975cb3475bb4e7b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:37 +0000

    inquire identify into child concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
