a51afa3cb832c8e70df851c7bb2991097bd8e723
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:41:37 +0000

    cut wind lock task laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
