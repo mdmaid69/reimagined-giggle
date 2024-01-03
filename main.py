2e1755745def7b5982b97d295d7f2f345bbcc9de
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:15:04 +0000

    interruption tread excited some interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
