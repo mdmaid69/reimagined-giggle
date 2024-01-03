79e547763386c603094f30729cdc978c6e9713b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:48:19 +0000

    eat bread exploration test reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
