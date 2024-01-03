0a8ee582f0e911fd37eed69376eb02432e2a8815
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:45:06 +0000

    come would steal development loose

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_min(lst):
-        return min(lst)
+  def is_odd(n):
+        return n % 2 != 0
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
