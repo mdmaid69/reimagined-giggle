f13b8b38c1b7aefc22a5645b5337a499a0dd886f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:54:13 +0000

    complete absolutely quiet wear add

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
