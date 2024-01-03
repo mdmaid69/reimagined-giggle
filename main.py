44c561a83c16790637b5deb9e2083c27ea670a03
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:39:20 +0000

    rare runover dangerous prepare punish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
