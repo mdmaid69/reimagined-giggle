42168c2d2b3b46a8163bd4ff3eb1d43e0f9ab669
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:31:15 +0000

    intend price order rarely advantage

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
