6b9ebb04f493a09f657c9a94a0099774b42eee88
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:15:28 +0000

    hope familiar home slip simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_power(work, time):
-        return work / time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
