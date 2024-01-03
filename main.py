49807c939c5158f2ec9f34c78605d388efc5d4a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:12:55 +0000

    rely flavour system wash shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_slice(array, i, j):
+        return array[i:j]
