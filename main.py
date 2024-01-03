ce57eecfe93acc5830c166f663d78a32af612da6
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:10:14 +0000

    somebody effort plenty mouth pass

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def get_array_as_str(array):
+        return str(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
