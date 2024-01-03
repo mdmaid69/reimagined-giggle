1fe45f8420ec035c26f9cf76297cae08965cd9a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:04:28 +0000

    tidy style necessary complete reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
