49f7d0e3abc664f92f2ad9d8fed4c5a8188cb894
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:24:24 +0000

    search sticky knowledge fire today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
