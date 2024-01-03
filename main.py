d52896ef21fe31f434d5f4dd5f8f6d5fcfd21a3f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:10:35 +0000

    whether certain normal character meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
