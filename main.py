dc84eb6facf98b73907eb6629ef3e23fcb6c44f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:18:19 +0000

    straight progress church convince opponent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
