f3a89ee10cd34e0f6f59a7a4a8cecbd247fc7b7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:34:01 +0000

    gentle whistle uniform lesson discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
