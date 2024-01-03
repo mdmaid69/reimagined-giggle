8d382cf102bc0a50852586e9b19b7ec19a5054ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:46:09 +0000

    splash importance display with impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
