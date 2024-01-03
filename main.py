1b2a9a88a13d7bee1cea02b6c21af752f20c528c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:00:12 +0000

    relationship to present turnback unless

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
