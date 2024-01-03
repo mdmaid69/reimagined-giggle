643e6653dd26de50b0a2ed19c9b438710716e6ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:54:05 +0000

    float listen delicate pour wash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def clear_array(array):
+        array *= 0
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
