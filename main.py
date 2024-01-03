b0a3272db857df909aa4123f8c5fe7e1d3753be2
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:01:18 +0000

    wish collect off reliable sudden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def subtract_numbers(x, y):
+        return x - y
