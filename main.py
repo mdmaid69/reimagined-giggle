de1ef50c91ce3ca08d195ff22c6230b09abd3acb
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:41:56 +0000

    stretch wing half picture nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
