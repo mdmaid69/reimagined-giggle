b2dba6e1fb5a4a4d7ffd30e43ae01843e4546b4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:30:13 +0000

    usual recovery complicated early street

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
