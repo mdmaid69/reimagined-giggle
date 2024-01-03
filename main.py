882ac00fce06a6c606eb1caf66ab16dd9e7e0c44
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:16:38 +0000

    cost ever either measure will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
