699ce8c98e88c75276757dddef1d1b26ae8297d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:17:51 +0000

    surprised operate pretty side as

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import time
-def get_time_since_epoch():
-        return time.time()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
