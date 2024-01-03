04e8b1d2e3b17c8fb6bd05986bc23cad77ee0154
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:03 +0000

    Jj total most spare risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
