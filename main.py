6dc82ed0b8855b3ee0c94b24b8ee5b096a4537fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:40:29 +0000

    deep flavour apparently bit switch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
