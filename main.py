e643a8d4c6a4f2737d6ee2cab3514e72dcb68f90
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:28:40 +0000

    law remark floor notice cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_current_time():
+        return time.ctime()
