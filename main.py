ae4b1ba6a806c2bc65ed0255183d5827586b761e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:31:05 +0000

    reduction fire hide maintain pressure

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
