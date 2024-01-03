1b8772267c03664ab51c562e36efd9551788dbc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:04:04 +0000

    manage usual 571enough flow know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
