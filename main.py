26f7ebc13b2e9c08ad02ce4595f8b0eb1f47e443
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:42:05 +0000

    reaction Cc famous undo recover

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_area_circle(r):
+        return 3.14 * r**2
