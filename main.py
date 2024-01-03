50a442a4b7942b77237921eedf62d7437796f305
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:14:24 +0000

    wipe pair performance break regret

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
