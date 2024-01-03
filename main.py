fcdf5308f75107fe1165a01f986a76abfe95237d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:10:04 +0000

    usually dance move range imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
