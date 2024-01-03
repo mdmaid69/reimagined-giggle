0a08870085025d31a350eab44e4b3161e264bf8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:24:48 +0000

    before straight information completely introduce

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_power(work, time):
+        return work / time
