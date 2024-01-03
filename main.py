d3e1c8808e42921b0ded786b5f09de3a2afba3a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:34:48 +0000

    draw idea beautiful fire flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
