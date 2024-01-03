114d26974d9c12accd03b2cf047c7cec999715f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:25:21 +0000

    description reply trick hurry thumb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
