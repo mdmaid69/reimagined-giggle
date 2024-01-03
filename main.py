65621943f571ebcf565207caf32540f8faa276be
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:01:59 +0000

    today judge tool angry Uu

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import numpy as np
+print(np.array([1, 2, 3]))
