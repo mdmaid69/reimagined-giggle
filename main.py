8575c4a7149b05d9ee96f7b7cb9f721230945e4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:43:22 +0000

    centre piece exaggerate question funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
