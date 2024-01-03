1c0baabd791868677d8f4a7891eabcf9013d67ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:13:24 +0000

    expensive innocent right century brave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
