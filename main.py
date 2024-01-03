8fdb2efae879f11b873b9eae1a88de180c16819d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:42:18 +0000

    describe content Rr instruction dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
