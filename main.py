1b635545fd118de768fafa1c605b322d1c85c149
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:12:03 +0000

    cry well low shrink muscle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
