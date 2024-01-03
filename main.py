a25c70a43cab1b9ed15edffe8336d5b78fd28748
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:32:51 +0000

    break behind priority difference wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
