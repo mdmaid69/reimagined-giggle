1393db618ba24222b0d57b0b535a4b031078f002
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:26:08 +0000

    rest callup freeze big walk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def append_to_array(array, item):
+        array.append(item)
