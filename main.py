b07da4ff857b740f1d2fff84a52c217b03fc9815
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:14:00 +0000

    eat friend successful tie wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
