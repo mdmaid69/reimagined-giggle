2f27e00f921f6f07ccc3753587c71d4817900706
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:44:10 +0000

    takeoff their knowledge from comfortable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def append_to_array(array, item):
-        array.append(item)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
