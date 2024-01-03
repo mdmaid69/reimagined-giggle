fcba5ddb55b0e76ac02107e5463eb53e04c32091
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:13:12 +0000

    constantly stop instant respect correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_stack():
+        return collections.deque()
+import re
+print(re.match("h.*o", "hello world"))
