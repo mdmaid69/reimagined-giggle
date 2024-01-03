ef234f0f2f587ebb311e0242727351220901b0f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:07:48 +0000

    will right approve finish thinkthrough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import datetime
-print(datetime.datetime.now())
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def find_min(numbers):
+        return min(numbers)
