b74e7e392559bc28b3d22cac5d8dc9b2038aaa54
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:06:36 +0000

    house often steady face shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
