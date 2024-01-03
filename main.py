c55b4e5dc11b67342f2e14efb916fbbefd7c00b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:13:47 +0000

    measure fast remove married relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
