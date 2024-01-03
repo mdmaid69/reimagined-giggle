d0c8651ebbd6e7622031dec102032de137c57fd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:54:32 +0000

    callout fit basic study strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import platform
-def get_os_info():
-        return platform.uname()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
