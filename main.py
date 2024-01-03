9e1ffe960ff86b782274eedf36c54bba99ad01e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:09:39 +0000

    turnoff shallow direction Qq agreement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def find_max(numbers):
+        return max(numbers)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
