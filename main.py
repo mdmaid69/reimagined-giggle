113274e0809e76ac3db40e56264536d048485240
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:59:01 +0000

    less ago brush correct deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def count_elements(lst):
-        return len(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
