6537bf8e2a97699289738db3df45f63089923e39
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:38:36 +0000

    depend embarrassing square stand weather

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def sort_list(lst):
+        return sorted(lst)
+def find_max(numbers):
+        return max(numbers)
