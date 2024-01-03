2d13a514f31de50e04759b71ab8d5ebac42a1ce5
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:23:13 +0000

    thorough reason related comment forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
