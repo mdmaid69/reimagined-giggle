2fb95094947b9c120059c7a035afadfe1f6a6fc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:30:51 +0000

    full shake beneath test majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
