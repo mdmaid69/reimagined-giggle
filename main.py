39968862ceba212edf41cbcb025a40628faced78
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:55 +0000

    transparent advice heat education properly

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
