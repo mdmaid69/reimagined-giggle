cc6a5c01e21b5ad6b548712c268211d5ab9ef683
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:01:52 +0000

    behave taste dull charge region

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
