25fd80a234b779d9a27f2e621cd2eb10942d1a30
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:43:20 +0000

    so 564encourage practice talkback prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
