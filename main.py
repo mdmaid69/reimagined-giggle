ce228b3f67113cd7fd2bfacafa8899b519a8ff24
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:39:23 +0000

    discover neither mind stage appeal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
