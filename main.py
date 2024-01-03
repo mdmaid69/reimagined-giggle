84079792d4ec4d9c73d95e06da7e0bfd21af9f03
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:27:11 +0000

    finally thing second hair finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
