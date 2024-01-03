faf7c7d30ad3d56cfafa800190c9dcd9b744cd43
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:15:39 +0000

    warning expect cope offend shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
