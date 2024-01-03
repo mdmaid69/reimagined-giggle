047add4e478becd8eea5b7555239f59ef896ca4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:55:36 +0000

    goup relationship degree breakfast discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
