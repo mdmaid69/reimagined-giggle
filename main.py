cc5ab641f8befb9fb7925466b06ba3a2fffcc940
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:21:39 +0000

    positive wake breakdown die form

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
