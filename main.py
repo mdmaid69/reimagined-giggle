6dae382d88ed68b9c28286490f34d5e3800267c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:48:02 +0000

    right nothing convincing prove shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_counter():
+        return collections.Counter()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
