7f47fe960c763deffaa925ac943a9bfcc80b059a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:59:02 +0000

    hurt will environment tread but

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_rectangle(l, w):
+        return l * w
