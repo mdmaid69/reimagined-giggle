bc4192127c3b9772f18bca94d8a088aebf692165
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:38:43 +0000

    strength thinkthrough bend advice careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
