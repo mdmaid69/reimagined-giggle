294c8ba55d97e720dc69db2918428073ca41ca84
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:02:45 +0000

    closed mysterious gate any table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
