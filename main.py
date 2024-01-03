c4d5be966bfa22a56651858145d4f07ff812cc49
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:18:43 +0000

    story relationship goon danger use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
