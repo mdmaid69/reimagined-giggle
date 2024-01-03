ffa3eb55f08ec9e5ba75aa04beee94ea52b22cfe
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:04:42 +0000

    block actual proud confusing female

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
