88b3fb881c2dda7d0e600dea2e9401fc54bf0106
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:37 +0000

    afternoon tragedy straight opponent list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
