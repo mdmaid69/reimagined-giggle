a64fe908495904dd33777068286c98ad8408f38d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:27:14 +0000

    learn from pin middle lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+x = 10
+y = 20
+print("Sum:", x + y)
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
