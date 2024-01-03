9c2375c04c2c8df10ee52d14539b3d70fd987d71
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:53:07 +0000

    small hurt reserve warn scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
