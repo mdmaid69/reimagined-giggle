1378082545b86535c6c957b77b1157fea648513e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:58:29 +0000

    work mention describe report takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def fibonacci(n):
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
