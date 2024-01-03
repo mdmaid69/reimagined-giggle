cd69ca0cc955b515af7de4e67301bb41d1ee13be
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:09 +0000

    praise precise press fit turnon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
