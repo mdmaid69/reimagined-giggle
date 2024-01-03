5ff9514918d5270ae523ef5704294ee082093125
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:14:42 +0000

    space unkind into takeaway Dd

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
