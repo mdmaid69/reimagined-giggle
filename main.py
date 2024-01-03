f3c9a0528c5557b465ab501482eea044942eeded
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:53:49 +0000

    job interest there spare hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
