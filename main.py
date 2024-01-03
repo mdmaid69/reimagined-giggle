f966dd6abc9c7b1c4a12649256446b8772eee30f
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:26:09 +0000

    born hurt worried pin say

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
