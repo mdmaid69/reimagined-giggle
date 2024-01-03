e04fc4bfbbf454996b5d732e281859577d43f0d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:01 +0000

    trust through shop begin behave

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def clear_array(array):
-        array *= 0
-def subtract_numbers(x, y):
-        return x - y
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
