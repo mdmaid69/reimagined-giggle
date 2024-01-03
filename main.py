c11206fb4ad883a66a68e703a28c4c5462bcc0b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:56:50 +0000

    country declaration instruction tell family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
