4d0bc3d94602cabfcac5f56b7bf5d526eea72c2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:36:16 +0000

    copy smile ordinary win dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def get_array_itemsize(array):
+        return array.itemsize
