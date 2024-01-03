5afea7556851f72698554a298f8c5a5fee310538
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:01:11 +0000

    suckme meanwhile upset everywhere too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
