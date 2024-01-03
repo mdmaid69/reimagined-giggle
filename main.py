5a1586f6372c3689191fe32b69c4fe0e8c05c754
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:24:47 +0000

    benefit approve luck apparently fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_index(array, item):
+        return array.index(item)
+import math
+print(math.pi)
