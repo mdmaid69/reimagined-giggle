c532047c262db4b373638085446eb3bec92ca58a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:23:12 +0000

    clever gate around stupid sound

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
