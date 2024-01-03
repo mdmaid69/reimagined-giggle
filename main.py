16060db9300298e4681b423c699bfe15c28b55a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:51:17 +0000

    news tune space pass city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_speed(distance, time):
+        return distance / time
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
