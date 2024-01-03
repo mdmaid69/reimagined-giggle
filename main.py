6781786101198cb2b0859bf9729b46c1b158c7a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:32:06 +0000

    star commit about cheat again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def roll_die():
+        return random.randint(1, 6)
