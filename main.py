62a2a889f146140fffe099d33cfde927eed81ee2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:15 +0000

    no platform swing collection sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def remove_duplicates(lst):
+        return list(set(lst))
