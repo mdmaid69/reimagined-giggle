b5688eeeae990e64d96fcb4b5f2372484f186d7f
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:36:51 +0000

    build crash splash will goup

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def is_odd(n):
-        return n % 2 != 0
+print([x**2 for x in range(10)])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
