9041a59e4b265b1240a98d924f9c2ceb9d1866de
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:09:24 +0000

    because tend rub behaviour hope

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+print([x**2 for x in range(10)])
