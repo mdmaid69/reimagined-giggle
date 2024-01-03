02d7b59d5f0b0a881942746366ec0c23c39b3683
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:02:06 +0000

    front any answer tragedy advantage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
