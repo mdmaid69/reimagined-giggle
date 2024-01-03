3455cfaf4ae43c76ad4a7e46d959c13385d6c22a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:57:17 +0000

    damage most smooth careful false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import os
+print(os.getcwd())
