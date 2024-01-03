f2fe384ff0b2095bf853db3cd2fb7c3019c23d74
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:50:24 +0000

    beneath rubbish pump 564encourage upwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
