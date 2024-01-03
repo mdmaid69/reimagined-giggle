7517dacc5dc68222a0b398a6601ce370e8e1fd97
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:42:09 +0000

    nice sudden by moon involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
