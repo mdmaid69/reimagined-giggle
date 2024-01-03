15de074f7c92c8e02c6d761aee107a41f9ebaad4
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:37:01 +0000

    yet talkback achievement comeoff 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
