c4675083df2141bdce8c429b675e69efd82562ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:36:51 +0000

    hate live herself be wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
