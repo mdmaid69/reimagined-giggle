65c0bd8da75ed72ee5243f76150c95c69771df4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:13:25 +0000

    unfair recommend woman wish long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
