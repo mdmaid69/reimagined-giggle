db542acd98f8d867e7a20e519ac64a5c028512ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:02:44 +0000

    Ii accordingto fork safe him

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
