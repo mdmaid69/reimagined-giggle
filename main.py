948927e46f53100ed2653bdf0613c7f96e9c64c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:56:51 +0000

    brief Uu this arrange front

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import sys
+print(sys.version)
