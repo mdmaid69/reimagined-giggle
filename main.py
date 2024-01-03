5ab7a62eaa4a1c07c55279b37c79af1e12e23c09
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:15:51 +0000

    check female share method into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
