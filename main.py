c12781143b57dd8f4229872c703f25f4d02828a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:30:08 +0000

    standard tradition blind please speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
