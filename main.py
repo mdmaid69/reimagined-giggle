aa827abc91856f68a92ba90014ea1a8a7f4a6fc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:51:35 +0000

    subject doubt paper angry accordingto

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
