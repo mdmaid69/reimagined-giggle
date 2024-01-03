b5af8770dece6a5918f4f49b9dbbd8e966995496
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:33:39 +0000

    fact last nothing just in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_density(mass, volume):
+        return mass / volume
