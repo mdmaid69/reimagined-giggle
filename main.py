bcdebb3172e0f50604ab9998f97596e353167bc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:31:57 +0000

    arm worth destroy position floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
