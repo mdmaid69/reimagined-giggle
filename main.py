6b83b4086bc2d6fb690158433c8f665e4d42dbd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:37:29 +0000

    dance herself trap prevent frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import numpy as np
+print(np.array([1, 2, 3]))
