2f6424286d0fc7e99bdd79c5b19d8615de4a9c4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:08:31 +0000

    constantly mess solve worst stage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
+import sys
+def print_python_version():
+        print(sys.version)
