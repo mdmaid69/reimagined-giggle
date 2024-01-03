b5a6e83ca55c738c9cb258c4f2d494477a286d1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:06:16 +0000

    general field Yy bridge lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
