2f1fce4c20df93f6e24e6e851c0b36d6e7bc69a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:11:56 +0000

    top road paper make pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
