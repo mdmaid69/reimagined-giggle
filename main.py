50e7dce22a39a09f759fff706310b8f96c612b65
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:14:57 +0000

    out motion there slowly pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
