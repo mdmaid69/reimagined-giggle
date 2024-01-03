c21e230f171e25eef56f3e69fb29dc69218663ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:57 +0000

    crowd computer sick give morning

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_list(array):
+        return list(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
