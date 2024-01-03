4e7d010b165691633128fde8b09a8614040b209b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:20:31 +0000

    will listenfor cost angry mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_as_list(array):
+        return list(array)
