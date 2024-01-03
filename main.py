84d5c710a39f01f42db6671900d264e882e607f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:38 +0000

    song fire introduction secret float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
