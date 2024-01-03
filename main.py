bce14ffca879ed70d6fbf7b7a1d8081eac23938f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:42:51 +0000

    respect bang patience band direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
