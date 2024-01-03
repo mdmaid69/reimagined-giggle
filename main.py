f439b1ddf409a996700df6e5791eff77a0f65b84
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:47:03 +0000

    fond about better over education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import tensorflow as tf
-print(tf.__version__)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
