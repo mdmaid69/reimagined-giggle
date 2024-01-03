bb8a9e574aedbda94633c17ad63ab0dff3c7b7f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:11 +0000

    regularly soft talkinto building various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
