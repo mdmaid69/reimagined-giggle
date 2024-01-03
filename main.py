c0dbf04bd2e2dafdbbc6456b105daee000ad5e17
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:59:11 +0000

    thinkover pocket muscle public shine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
