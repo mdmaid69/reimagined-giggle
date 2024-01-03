84bfd30b25c763e4c2beab4d356276310af845e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:00:30 +0000

    aim scare exist doubt bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
