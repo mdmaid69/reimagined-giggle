7fab838db7b6d8da72bb327555aa417afab59ed3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:05:09 +0000

    either win picture brief tap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
