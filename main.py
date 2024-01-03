bb8071d5f4452d1e25cfe279cc65b8784e29a3ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:20:34 +0000

    unless practice quantity cool guess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
