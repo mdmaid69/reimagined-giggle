8c25e42218dd49747391ddc29e4760ced9411aac
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:41:03 +0000

    Ll pump music getaway pleasant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
