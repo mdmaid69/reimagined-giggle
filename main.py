35942315d0c4c3762d7b62b5d92fe681d33d6885
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:31:36 +0000

    shelter life interview wake taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def subtract_numbers(x, y):
+        return x - y
