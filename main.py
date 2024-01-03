130e8a3c56c5e6e9aec33caabc4755baba3aa2c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:12:23 +0000

    criticism swimming book physical sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
