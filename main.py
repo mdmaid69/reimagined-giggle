c8ffc4544058a8340d711fc2a61406cdb031f728
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:27:12 +0000

    excellent bear date uponon limit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
