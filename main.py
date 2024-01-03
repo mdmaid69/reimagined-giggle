eabc5f17b2cc045fd0a20d231f915f073d5b8720
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:38:13 +0000

    dull quarter protest true put

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(a, b):
+        return a + b
