6c396c810fd4d7b70ece2196acaaed2e3f4970b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:26:30 +0000

    disturbing offer vote bang giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
