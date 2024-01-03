f76968edc550bc731e399bdecf289c29a129011f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:06:28 +0000

    cap only minor plan arrangement

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
