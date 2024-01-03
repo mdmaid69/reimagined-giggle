077d69b593a2414f184ae3751b78a544423a849b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:19:50 +0000

    bridge loose Gg tear offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def add_numbers(x, y):
+        return x + y
