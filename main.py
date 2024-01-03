3682009e60d264706e6095890caa5917d398a668
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:27:28 +0000

    like possession height geton damp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
