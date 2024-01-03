42c56c681dc773d3f342441d91c73213604ea579
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:23:20 +0000

    steal favour breakup holiday sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
