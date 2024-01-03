c1e231c306dbc7ea399a1a656f238b7c465c49f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:59:21 +0000

    guilty check top married lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
