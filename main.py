3b22a7a969d8651f9e6e912a6f5797da7be62f3f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:26:48 +0000

    something content train 572enter rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
