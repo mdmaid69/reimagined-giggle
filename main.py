6ec7aad4d972d3756124ffa1beef66e7203a1162
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:05:34 +0000

    near lose runout disturbing nearly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_max(numbers):
+        return max(numbers)
