c244db93a82a6fa8c1d078d09f19e30f1eb446ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:50:24 +0000

    there wake tradition attitude spoil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import numpy as np
-print(np.array([1, 2, 3]))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
