0316af8164025fd651f437c5590385631d872351
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:10:09 +0000

    friend another pleasant set Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
