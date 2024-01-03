bdd8db436dabf76440722a5d44c3527520e67225
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:28:23 +0000

    geton stiff feed like climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
