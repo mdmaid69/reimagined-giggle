5f9d2984636925c4276675129e44380688a23952
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:23:10 +0000

    flow peace clean boat sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
