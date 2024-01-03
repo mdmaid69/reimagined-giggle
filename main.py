c1f9bab9e1719c282103655909eb8eff977d3ff3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:18:12 +0000

    respect issue march only bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
