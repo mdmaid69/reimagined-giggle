460678e81dfc27f06edbfdf564943f63c2fc2fdb
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:28 +0000

    influence practical act twist roof

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
