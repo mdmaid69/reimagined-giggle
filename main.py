1654d7ec41472e46f3a0a989c7c6fd6c7ab16d30
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:08 +0000

    shout intelligent solve sport satisfactory

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
