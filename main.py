dd23fee1e2f0183c8b1404d7ac416d13598fba26
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:48:29 +0000

    maintain various influence heart under

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def get_array_as_bytes(array):
+        return bytes(array)
