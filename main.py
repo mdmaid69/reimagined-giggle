69cb3ff1163eca839bbf6e3259a7df87ae633f95
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:24:45 +0000

    love below free Cc trick

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def create_user_string():
+        return collections.UserString()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
