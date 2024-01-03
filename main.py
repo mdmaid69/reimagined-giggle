dea1b58ab84945c800524ce524d687b332494c04
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:12:57 +0000

    absolutely board alone ordinary fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
