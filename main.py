efcf1d57e1629d2789ed97f31d37902f732f96d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:26:26 +0000

    exciting borrow whichever calm wake

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
