88930d15c783decc5ea97146c9e42f1fcbf99e25
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:16:17 +0000

    nature thin short the each

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
