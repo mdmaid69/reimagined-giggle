f6fcc6f8f02f601aeb843adf1329e4038df6dcd3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:58:03 +0000

    laugh impressed influence letter criticism

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+for i in range(5):
+        print(i)
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def change_working_directory(path):
+        os.chdir(path)
