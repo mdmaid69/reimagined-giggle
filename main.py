87f576343eb157564a90cbd5a34586dc666edbe1
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:05:33 +0000

    which mark match tip patient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
