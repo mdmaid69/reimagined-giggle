f0a1c830f9ee1530a4dd70cd21aef2270df6e1d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:09:54 +0000

    major mind willing here temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
