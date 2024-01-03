cd22095c5486afe91ee1c8f0b3fdb16e1e8ac747
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:43:47 +0000

    tooth advantage influence used steer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_binary(n):
+        return bin(n)
