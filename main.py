0845aa0647e497841f4baaaa0c0306126a77e21f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:21:59 +0000

    cool note up another complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
