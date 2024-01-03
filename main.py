5dc39bef6f3b2cd183d55b8d27d9b751940c7f6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:01:10 +0000

    doctor rather swallow advice reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
