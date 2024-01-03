6fe9d966d87e03a543f90bc96b8b260b5efdfc30
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:46:27 +0000

    fill convenient will before check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
