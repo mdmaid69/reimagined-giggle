8db1e05f5469be9f6804d1101aa5d6d5d4e2fa11
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:55:24 +0000

    fence closed deep defeat Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
