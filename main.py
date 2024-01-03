a4ba402e49882511be556f4754f25179e597d078
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:49:28 +0000

    task wide snow or war

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
