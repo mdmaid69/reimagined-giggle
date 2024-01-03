e3a2f2acfecbb8d7776b4d89075da5842277b291
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:00:54 +0000

    worth laugh all deliberate fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_length(array):
+        return len(array)
