35eb4f3782c02dfb4ec5a4517ae9a3ffa9fc1cfe
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:41:29 +0000

    needle correction owe simple only

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
