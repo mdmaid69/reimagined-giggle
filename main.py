46a32675d0d39754c40e52106190bfa0f040e390
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:37 +0000

    thanks public present bang describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_repr(array):
+        return repr(array)
