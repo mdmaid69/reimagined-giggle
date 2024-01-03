4b19e3003c6d0a4d0fad6c7f19f6bc448b286644
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:18:49 +0000

    wait pause pour ball object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
