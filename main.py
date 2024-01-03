495251064bfd2083463e51eeeae7d1029be3c2be
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:32:51 +0000

    contrast will tell affect takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
