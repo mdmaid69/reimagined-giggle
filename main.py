0d2842ecd1506dd378b461e05106e51d13f144af
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:36:39 +0000

    control responsible mail tired wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
