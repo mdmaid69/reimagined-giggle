8684e284df1cc68b20391cde270498bf535f2bbc
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:47:48 +0000

    drawing close reply mine apparently

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
