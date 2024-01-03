52e7b01c6d335dbd6d7eb2e7f84382abdeaa26b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:25:54 +0000

    thirsty man science sting while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
