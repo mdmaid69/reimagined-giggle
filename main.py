87ceebb98340cb0f4f5a007fbca6a7a87c7ee856
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:08:07 +0000

    region describe before especially shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def convert_to_binary(n):
+        return bin(n)
