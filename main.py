89a3dc7bcceecfcf6a54f22201341537080c3e87
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:41:51 +0000

    slice match through consult like

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
