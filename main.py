c4b4923fac6298920978886fcbdbdf607bb0e130
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:57:12 +0000

    feature near train neither divide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_item(array, i):
+        return array[i]
