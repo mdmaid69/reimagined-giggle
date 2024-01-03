e1f33bd32d34544c25f6e68354cb51774c9f06be
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:51:06 +0000

    lunch depend hold getout distant

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
