b29bbbf0a0ea9e0a4b1bcd20fa7475caf5daa45a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:33:01 +0000

    any upwards put press collection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
