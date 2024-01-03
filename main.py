4b903eeb5005605114a51d869781c0f4733cbdfe
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:38:04 +0000

    account kind table attract round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
