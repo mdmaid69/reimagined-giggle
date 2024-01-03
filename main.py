57e0691aff1e476847f039245f1559f2e69a56a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:30:51 +0000

    increase evening last climb relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
