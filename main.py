f46f31495a1229108f05e2e5d43b2e7d37a8c4d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:39:39 +0000

    relief tradition failure health last

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
