2c93b59e39c93df22e26565f6cb0f60b7a33fa4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:31:23 +0000

    sentence shout home test if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
