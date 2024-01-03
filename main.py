36480eff71045117d4f9ef94dc99adedbbb9d6a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:28:35 +0000

    happen stamp university relief flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
