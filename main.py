8da5a3486079e9078122e097ea3236dbfabc0902
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:44 +0000

    excited turnover take trip godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
