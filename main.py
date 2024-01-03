47539738d5f2b30ea924c7dde7ce62671f588316
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:18:18 +0000

    forgive live Uu degree turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
