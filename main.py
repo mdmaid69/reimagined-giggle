81eda624df100aa731deb8e944c66f1ed8390d32
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:43:58 +0000

    charge petrol flat equipment explain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def count_characters(sentence):
+        return len(sentence)
