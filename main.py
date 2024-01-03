eaaa900fde897eb095ecdb8e32131d115e2d3278
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:22:21 +0000

    although hardly pay politics rare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def convert_to_octal(n):
-        return oct(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import time
+def get_time_since_epoch():
+        return time.time()
