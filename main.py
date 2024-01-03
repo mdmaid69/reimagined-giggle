e52b084e6655c897f7911b956e877fb87f6fec31
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:37:13 +0000

    like wide bar healthy dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
