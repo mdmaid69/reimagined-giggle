c1d531a21446f5d9639d106f530ea549b6a6a552
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:57 +0000

    legal cinema bored habit task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
