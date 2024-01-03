5a6fcb1087bbabca99d86609c9d93a9e5e33912e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:40:57 +0000

    teach hope before may hide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
