bfd7cd945fe991780808ffadf10a84743c93fca9
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:41:46 +0000

    rent yours fail tall draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
