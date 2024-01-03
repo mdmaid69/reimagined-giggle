16e548da680a278b8d9448ea2cccdf1d80c37df3
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:08:55 +0000

    into bottom apparently towards most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_work(force, distance):
+        return force * distance
