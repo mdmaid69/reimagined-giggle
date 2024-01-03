b6b428d9aef0847eef50df6d6ff5e5a2b562d95c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:48:51 +0000

    regular store field in though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
