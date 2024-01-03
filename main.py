646614b24dab9c879dbfc8f23ca8264c43daf5af
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:27:48 +0000

    care journey rescue yet ask

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
