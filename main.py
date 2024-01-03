941aa10b666d65251710e185c15225559719fb23
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:38:36 +0000

    exam make yours cruelty excellent

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_counter():
+        return collections.Counter()
