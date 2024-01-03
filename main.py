9dd8edf65a3c33f021ca3739b89dd4ccd6f81c3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:03:18 +0000

    contribute matter include believe Ff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
