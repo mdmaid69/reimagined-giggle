01590ea7b0268652b4f9f470cda33dff17196805
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:34:18 +0000

    often club shop ahead state

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
