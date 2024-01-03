3e1c4e1556bbcff6c2b4c97ba0d044226fa3b961
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:10:16 +0000

    arrive after track excellent frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
