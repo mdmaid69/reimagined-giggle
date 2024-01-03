18dc49a3af7fe817a0909cd3e9f831ef4e49a9bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:00:13 +0000

    worth damp ticket trust realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
