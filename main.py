4b59ab86578b06ec4bb3ea2b8774908f08e574e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:11:22 +0000

    luck Zz list 572enter hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def is_odd(n):
-        return n % 2 != 0
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
