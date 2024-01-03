bbb719de95093a4f8a7bb1f6db5363684bd51529
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:07:31 +0000

    play price recently no test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
