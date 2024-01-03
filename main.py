00db40c90c613324d901a31693521cf7a71b8740
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:23:38 +0000

    trust offend modern arrive thumb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
