a1e74dc764c7565485afa4901c8a1588e147390c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:44:05 +0000

    print unable accordingto eager thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
