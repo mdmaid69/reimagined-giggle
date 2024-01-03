d66d3b5b22ad889087e7a1eff91bd0398efcc98a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:35:14 +0000

    getout vote convenient spot flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_area(radius):
+        return 3.14 * radius * radius
