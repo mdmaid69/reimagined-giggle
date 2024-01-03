4124b721440608b05b6671a882740e4fc1b9b434
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:04:06 +0000

    shoulder tax arrive always sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
