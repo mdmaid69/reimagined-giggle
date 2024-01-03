259558bc274fc7800ea163d42d32787e25dc8459
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:31:35 +0000

    suddenly total cross half correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.ctime()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import platform
+def get_os_info():
+        return platform.uname()
