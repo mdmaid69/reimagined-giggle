1e7714d4b662adefb62e0400176fce69a8bf0b74
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:38:59 +0000

    certain represent declaration unusual mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
+import os
+def remove_directory(path):
+        os.rmdir(path)
