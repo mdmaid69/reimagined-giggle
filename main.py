e2a620275be3ade47a0b745f225d48e51202d79f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:25:10 +0000

    floor comment respect keepaway round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Characters:", len(text))
