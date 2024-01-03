110b63da0589bf2a7f8f4f77e328b957dea2689d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:54:34 +0000

    ordinary turnaway only buy steal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def square_number(x):
-        return x**2
+  def count_elements(lst):
+        return len(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
