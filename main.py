3ccb0d8aff075c093402a54b4fa0b745a8c80f69
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:07:46 +0000

    tour rob detail unkind feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
