cbbe5bb99364e8cb0ad931ab1a5b3aefeb8553c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:41:50 +0000

    petrol wrap strange wrap heavy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def convert_to_octal(n):
+        return oct(n)
