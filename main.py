6f0ee0f428d769c8417aba29e59e451abc837c07
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:34:41 +0000

    confident raw one it recent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
