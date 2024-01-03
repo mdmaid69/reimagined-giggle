811b9a7843460cb02e3fcac0adf6f8a9f3afcdb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:59:07 +0000

    most tongue bother cough doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
