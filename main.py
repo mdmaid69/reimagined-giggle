5902b4d0e2069aa50c7fb7c2cfb95458348a5a11
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:17:54 +0000

    comeback fork object attract argue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
