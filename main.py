b425a5a7303b1adea067cac8207eef40d8cf8fa2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:10:03 +0000

    substance keepaway broad production same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
