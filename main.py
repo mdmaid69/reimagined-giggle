f9c95107a24bd49440659cf852a187bf8d1c0811
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:01:38 +0000

    cost Oo inform mind tired

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-print(random.randint(0, 100))
+import array
+def clear_array(array):
+        array *= 0
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
