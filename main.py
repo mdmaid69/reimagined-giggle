eac6ad5bec66f784da1f0db312f50da27fe47a4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:28:05 +0000

    disappear yet calm better cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
