30a3dd415ffe17caf3ccc547aa222208bd9fb425
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:11:14 +0000

    disgusting enthusiastic breakdown touch motion

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
