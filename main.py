ec707c7a4771b0f3f10c5fce393f243997789635
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:34:55 +0000

    price entrance neat for impatient

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def count_elements(lst):
-        return len(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
