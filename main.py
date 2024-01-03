6ead3c23a96dc9d969d5486d1db978f0ea618915
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:33:47 +0000

    disturbing bin paper spite eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_dev(file_name):
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
