b645f24abb3071a65055d2cff1f5cc7e55895a11
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:15:25 +0000

    characteristic include concentrate escape amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
