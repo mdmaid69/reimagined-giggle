fe221a81fff901f69c96de644fa139f4af803877
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:18:52 +0000

    warn direct street steady comfort

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
