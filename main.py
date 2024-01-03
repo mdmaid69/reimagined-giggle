30164863992c72980292393e860eba39b0253410
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:14:51 +0000

    classroom opponent hope uncomfortable regularly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
