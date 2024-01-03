65cd255ad756250ae09c2427fbde9d2c43ef2b0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:23:14 +0000

    lesson remark file so violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
