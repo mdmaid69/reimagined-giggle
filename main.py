c8732958dc3e3cd3cc9eecba16aa444d7292a164
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:46:13 +0000

    female dress bringdown hot vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
