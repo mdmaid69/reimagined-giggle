8a73227e6bf1f8987dcf4e23e4fc8f3496069881
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:56:46 +0000

    advantage disturbing performance kind wear

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
