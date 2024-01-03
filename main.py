bb5abfd9496bf1fd94294570eae7a72d31c6e9ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:55:48 +0000

    guide opposite top ask poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def square_number(x):
+        return x**2
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
