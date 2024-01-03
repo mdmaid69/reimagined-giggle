0c46a375069432a5ca625e05b3f7bff8a0f19e5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:39:38 +0000

    somehow shallow operate when paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def square_number(x):
-        return x**2
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
