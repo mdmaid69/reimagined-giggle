ed4b09166b31ddf4648d8b1eeee4c259f1a62946
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:26:42 +0000

    fear range line visit pen

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
