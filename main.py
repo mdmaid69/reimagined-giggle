293a5c5f4c18646b67b2336745f2b811814cc0d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:24:56 +0000

    keepoff debt call believe visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_as_str(array):
-        return str(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
