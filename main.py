ab726f908a2db79ab94a7f9d54b7d219c4b4d70f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:04:27 +0000

    custom shine scale sale Hh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def clear_array(array):
+        array *= 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
