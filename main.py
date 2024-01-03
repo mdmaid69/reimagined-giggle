bab8936fb67b2353630af3fc6801c3ca32ed105f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:45:30 +0000

    figure pause doubt lend confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
