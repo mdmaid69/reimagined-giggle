85c7791cb103dc81551013aa817c01a0c6c31c3f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:40:38 +0000

    turnup worth major shallow serve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
