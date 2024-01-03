e8b24d43d01082eee2331ec6da8027825f0a1fb0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:11:29 +0000

    mouth maybe best criticize quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
