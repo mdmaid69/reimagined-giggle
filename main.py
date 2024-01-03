d03b422a5b4b570c6d4b950b6546d7e9a83f824f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:41:52 +0000

    once light thinkof typical cheat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
