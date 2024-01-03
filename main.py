0a5fa41a64c01db24dff814eace2a52469292687
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:31:22 +0000

    switch happen expression my defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
