db454a1dfc00732a60efaf5893ae5cbb4f0e51ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:41:49 +0000

    566end usual divide name callout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
