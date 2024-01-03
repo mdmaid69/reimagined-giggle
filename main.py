d5e3e4f8dc6710ee4c209e1a3b46ff3df64e8ec7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:47:43 +0000

    that search agree various slightly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def cube_number(x):
+        return x**3
