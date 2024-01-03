00f87eff5619cb89cb083cde7aa9445e555f33a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:30:13 +0000

    thanks sister rest direction band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_as_float(array):
+        return float(array[0])
