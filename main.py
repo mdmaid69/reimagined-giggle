c68779c98618ad8ec8e8cd6d2c94231beeaa20c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:43:09 +0000

    tragedy fit large ordinary upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
