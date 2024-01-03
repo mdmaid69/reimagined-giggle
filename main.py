5bf01206af3f2e2a78eb67d8ce40baa92ce3d75e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:01:14 +0000

    obvious winter oil between separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
