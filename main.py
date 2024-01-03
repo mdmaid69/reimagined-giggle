ad65e6517cd485ce8795890ace3a5c19382fb418
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:50:53 +0000

    especially call earn fault which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_python_version():
+        return platform.python_version()
