3b56a36c225911f3f3ffb29062d8b1def6e82339
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:47:42 +0000

    row smell unite tap scale

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import sys
+def exit_program():
+        sys.exit()
