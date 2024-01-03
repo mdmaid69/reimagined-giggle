adfee0cd395baa931268bb030016b8427a8b7da5
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:21:58 +0000

    steam on sleep disappointed flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
