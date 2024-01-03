6092d9f0edb1418eb669cab9a28c6a1b168e537e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:29 +0000

    female hurry nerve children mood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
