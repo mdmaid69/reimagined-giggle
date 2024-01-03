4af1ba6e3ff8ba15ebba16562b930326fd7878e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:08:17 +0000

    immediate generally wait amount but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  def sort_list(lst):
+        return sorted(lst)
