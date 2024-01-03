9624b57f5493d76874c75304b3e9a728e3f66bb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:20:15 +0000

    discover shelter day aim keep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
