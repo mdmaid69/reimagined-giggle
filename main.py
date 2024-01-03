6b3551b1b68c6716bd4f0cc818560811cd480c0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:24:33 +0000

    paper sting suffer manage everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
