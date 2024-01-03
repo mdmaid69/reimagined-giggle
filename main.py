2aef21375776337adeff7ea5a2822f9148a0ed72
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:42 +0000

    knowledge rarely content bunch Mm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_int(array):
+        return int(array[0])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
