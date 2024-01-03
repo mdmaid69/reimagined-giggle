bd7f9e2ba45fb7fce06c6e768e7ff86597351e2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:46:23 +0000

    actually broken uniform shopping myself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
