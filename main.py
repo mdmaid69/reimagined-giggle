8658e7919bd11031d851d554f23dd4f896edcf7e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:24:51 +0000

    wide floor use clearly tread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import tensorflow as tf
+print(tf.__version__)
