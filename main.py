8e5cade85120015a57bc7dd7bb1328b36c26512c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:05:28 +0000

    especially act wrap entry always

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
