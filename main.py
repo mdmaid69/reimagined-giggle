23c7ee733155b4b057918cecca848ae9cdc4f772
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:34:57 +0000

    meeting injury similar central best

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_to_binary(n):
+        return bin(n)
+import tensorflow as tf
+print(tf.__version__)
