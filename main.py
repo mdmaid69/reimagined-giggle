73460a82476543e25ae595776de035ab071e9fdb
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:18:38 +0000

    story welcome bored smooth car

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
