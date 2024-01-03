043b82b503ed1c3b49b076175b094453cb24101c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:50:51 +0000

    but busy loan prove weigh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
