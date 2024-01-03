2f7b95822fbe5b3691404a8866cd3dd0b19552c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:17:55 +0000

    equal married exchange bend grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
