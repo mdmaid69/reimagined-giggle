9ac1adb50e3f608fd0aa2756f44b06b0fbc50edb
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:55:02 +0000

    leg tap worry talkback aim

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import collections
+def create_user_string():
+        return collections.UserString()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
