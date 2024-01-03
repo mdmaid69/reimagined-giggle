66f72af1a6b7b7c69d22756a3f934f21366c131d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:59:23 +0000

    measure alive shall container nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
