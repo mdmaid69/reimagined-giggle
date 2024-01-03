2900b08beaa63303f058a72588c0b000d0c738ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:27:12 +0000

    experience noise maybe steal bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
