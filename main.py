e219b6c0c7a1e7e1187f293e27ee29dfb6e16bd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:10:14 +0000

    bang release practise often phone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
+  def subtract_numbers(x, y):
+        return x - y
