94f1a6e086384d20803aae994c6ab86e3d6cdbc9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:25:36 +0000

    disapprove die rule rich suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import tensorflow as tf
+print(tf.__version__)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
