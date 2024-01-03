e81a30a507f698b7f74d1aaabd2de25eba211614
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:12:42 +0000

    see hate roof while giveout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
