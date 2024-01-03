2607340a83f575f4a6c55995a33f32dcecb46ab3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:08:54 +0000

    free dig cover word holdon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
