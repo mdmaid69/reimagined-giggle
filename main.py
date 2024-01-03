41e9df08ea81844802aa96c3ee096f14b80c3553
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:45:41 +0000

    dry bend over order middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
