e3099ad68726d34871bdcd14760c61b36c3b22a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:20:40 +0000

    shall disturbing area certain each

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
