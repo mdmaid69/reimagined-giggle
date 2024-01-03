861972a8fda8e51e0b5ba204a0569cfa5199bdf3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:26:10 +0000

    makeup bored mass terrible cruelty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
