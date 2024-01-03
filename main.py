0aee4a3750cb7c9eb643555bb64eccb75bb41652
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:59:53 +0000

    satisfactory quiet each with always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
