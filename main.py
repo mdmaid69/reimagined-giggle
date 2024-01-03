974241b92d42a79ba2b59f2ca60eb45815dfc2f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:18:01 +0000

    show slice proper within familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
