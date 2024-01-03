57540ba6bb85f903d2fb9acb2525495d10a4e5d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:39:53 +0000

    anywayanyhow mind supply tall mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_time_since_epoch():
+        return time.time()
