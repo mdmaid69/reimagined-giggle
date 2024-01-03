4bf6617a70230d5d6cbec3f57dc2417246954011
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:42:47 +0000

    seat opinion bringforward approve tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
