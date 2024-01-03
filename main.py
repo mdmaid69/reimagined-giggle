7a32dc75244a1f70346192ab15dad5e23a1a66c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:17:16 +0000

    official port than Yy damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
