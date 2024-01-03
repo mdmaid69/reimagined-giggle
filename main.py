6d5b1dabd3fa7634f561a71339d42a1a815e1116
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:44:28 +0000

    rubbish mainly wave pour software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_work(force, distance):
+        return force * distance
+import os
+def change_working_directory(path):
+        os.chdir(path)
