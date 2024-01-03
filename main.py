f49c4e6d460f72b4f0482cf387e6f7cc0055b135
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:15:26 +0000

    confusing defend possible throw scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
