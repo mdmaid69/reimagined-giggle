e04c3af89842b3d5aa68cedf331657d680ed5563
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:46:49 +0000

    crisis of circle well include

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+print([x**2 for x in range(10)])
