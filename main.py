5b89f4246231554a279b5b3e09315d6f133cbd59
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:17:47 +0000

    above convenient during practice sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
