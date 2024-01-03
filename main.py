72070f0c13ef2a0184bf5f22e9aecfd1aea3cea0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:33:39 +0000

    now neck union murder consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
