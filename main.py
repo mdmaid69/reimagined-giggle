dc585b0f05b86bdf5dc73297a2afee13e478d9e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:11:25 +0000

    Itself band deal determined children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
