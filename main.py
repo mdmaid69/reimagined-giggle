f8a7fc9854649507e1bfdf515f39f42519c85f79
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:23:54 +0000

    desperate share prevent none some

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
