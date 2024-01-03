d8b018f0205ea9a5a6945ce63da65d0a2b85d719
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:30:27 +0000

    dress fit vote wide trip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def convert_to_hex(n):
+        return hex(n)
+  def is_odd(n):
+        return n % 2 != 0
