5d2cd699c5ecbaef09f403950688f55739689636
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:28:43 +0000

    whisper kind smoke excellent release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
