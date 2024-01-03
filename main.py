7572d0fa7feca36c49fc1daff9915fbc93c549db
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:37:29 +0000

    steady and target discovery improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_set(array):
-        return set(array)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
