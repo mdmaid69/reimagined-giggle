ea791df069d40fc4d8c2f9cadb72464d0e595045
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:25:57 +0000

    question beat breakthrough lunch value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
