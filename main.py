e84889be377961ec0e0745030b870f621e429076
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:56:45 +0000

    produce needle keen ever actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
