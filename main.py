8c1686b442a527a8aa00a7b812ce45479e2d9733
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:02:33 +0000

    regularly buy memory sore loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
