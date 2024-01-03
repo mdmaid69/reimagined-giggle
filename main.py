7212f4675defd8d7d8dcdcdd06f9f81c88f9c212
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:15:52 +0000

    centre cover sell bank culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
