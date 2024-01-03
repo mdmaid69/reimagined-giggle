4e3267d527f60a9d3deb7da04e571a908b22e18b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:45:32 +0000

    love medium acceptable range collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_length(array):
-        return len(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
