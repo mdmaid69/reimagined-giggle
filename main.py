0950f11d1a7f66d50c05c40711bb59c5e9124bb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:43:18 +0000

    fire afternoon scream press wait

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
