a7de1afe1048ede79ee2d9a86bc1be33b4fd6925
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:26:19 +0000

    light uniform reasonable point accidental

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
