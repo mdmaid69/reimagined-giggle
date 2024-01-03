81bf3eae5426e7b8934fa28dcac4bda7dff33fe5
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:39:09 +0000

    turninto wear file dark read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
