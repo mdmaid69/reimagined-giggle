e087849d4fd5ae952494835c74a67badbe0b8dc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:07:38 +0000

    hard machine standard in goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
