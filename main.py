80e92e2ae56c31a709097bb9259a33cf3add0a81
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:24:21 +0000

    trust cross church male throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
