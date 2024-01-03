16bd4d580c19b9b8fb0c14605eafb41c68813efb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:49:32 +0000

    uncertain pocket crime Ss teach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
