ec877dbe48a4000d62a09ebe4fd1493b634ca766
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:08:34 +0000

    dress moral once tough getout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
