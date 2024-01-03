da1d11b551a7c3c1071d0f2c6fbfb21fb1d9312c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:09:04 +0000

    woman school dangerous happy degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
