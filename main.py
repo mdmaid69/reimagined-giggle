1839bb6a25f5b35bf3e2d045d63d73f251d3abe8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:09:21 +0000

    slice estimate furniture damp spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
