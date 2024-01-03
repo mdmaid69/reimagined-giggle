3e6c87db80d06327fd4921175fe9db7b6c233c67
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:40:56 +0000

    tap terrible case please bringup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
