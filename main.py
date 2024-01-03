91c9b62c27b5672881eb88247e8254713ddb0203
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:33:45 +0000

    available never purpose action university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
