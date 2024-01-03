a3d520e53ec3b5771e7e73cd6d2dda22fbae90f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:21:55 +0000

    actually used getout church performance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def cube_number(x):
+        return x**3
