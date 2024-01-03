b5e1aed6e9d07de3d3f02b2fc38bfd9fced2ac24
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:51:26 +0000

    activity popular pride equipment available

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
+def sort_numbers(numbers):
+        return sorted(numbers)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
