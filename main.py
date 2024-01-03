2a97f0095d6014e1ef150cbc98e3b7fcde63b69b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:59:21 +0000

    healthy touch breakfast swim pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
