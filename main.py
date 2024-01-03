9d136bb46a2d7901a80a577413b439e03dfa78aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:04 +0000

    anywayanyhow black mystery cut complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
