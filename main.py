84185ed6b54b73e5dc77f1536e43f36aa9cd9cdb
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:19:20 +0000

    bus dry press confusing rob

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def reverse_array(array):
+        array.reverse()
