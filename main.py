b22f92ca20e49b43b40a78bffd79f355444b228e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:19:09 +0000

    argument job art present fairly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
