93432d46488051e185a393ad4f0249a7950e3bb4
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:20:05 +0000

    fail shelter frightened block beyond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
