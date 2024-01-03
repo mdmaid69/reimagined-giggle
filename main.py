978956447a23ea1f5e4ef8363cc1a42bb22ad556
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:57:51 +0000

    life ill wall block though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
