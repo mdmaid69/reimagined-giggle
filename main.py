980cb753e28e9cca708ebdcb75414be9a9a5bb4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:17:59 +0000

    while practise style division answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
