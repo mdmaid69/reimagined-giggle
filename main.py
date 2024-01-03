1880286ff283e383695c07d842377dfc31162cc9
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:22:21 +0000

    soon giveout trip bill whichever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import collections
+def create_counter():
+        return collections.Counter()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
