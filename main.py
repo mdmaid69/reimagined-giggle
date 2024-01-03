079b6acd8f2a036ef8f7f834b34d14b0ad85aaad
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:38:52 +0000

    calm pretty preparation exercise school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
