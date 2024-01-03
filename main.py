b449fdc7ee4e5983fa699ef184c6d61abad1e4c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:28:34 +0000

    history undo honest will fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import time
-def get_current_time():
-        return time.ctime()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
