39874003a006b692e546c78a99cf7bac951f224c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:09:43 +0000

    swallow bang none kind force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
