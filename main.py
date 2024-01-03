f64c5314970c13bb1d0d3ea7e314b31ad34940f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:55:20 +0000

    trap style equal drink ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
