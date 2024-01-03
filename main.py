bf0918389db079535c51c0b8e9cd818c2f4d5b72
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:04:53 +0000

    collection holdout accident complain shrink

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
