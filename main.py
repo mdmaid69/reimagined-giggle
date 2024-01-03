7383b9456df0e545f809c2a056b345196bbb9f5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:53:40 +0000

    would age getoff priority shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
