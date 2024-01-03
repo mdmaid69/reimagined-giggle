d29ea4e5b236d746e0c7341364e742a6debf5e7c
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:49:31 +0000

    century old of wind taste

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
