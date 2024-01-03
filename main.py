cf918f2422329a4a61f43658deb6aae676e81346
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:04:57 +0000

    branch moon guess smile until

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
