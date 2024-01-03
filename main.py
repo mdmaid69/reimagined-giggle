aa43087a769f79b3006041ad6e56d2a706edff13
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:08:37 +0000

    bound silent gooff wet noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import time
-def get_time_since_epoch():
-        return time.time()
+def find_min(numbers):
+        return min(numbers)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
