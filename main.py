473062a89362d625174041f74e7d4c1adc7338f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:33:23 +0000

    motion control praise pool creature

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
