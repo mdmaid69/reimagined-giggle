33b9349c85112bd5e9ebd459070d54d54ad46fc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:56:21 +0000

    worth strange ago me private

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
