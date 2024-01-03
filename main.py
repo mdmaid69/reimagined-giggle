dce6bf7fd10d4d45fc1b3a2e3303482b65075be0
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:40:20 +0000

    Hh off colour approval month

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import time
+def get_time_since_epoch():
+        return time.time()
