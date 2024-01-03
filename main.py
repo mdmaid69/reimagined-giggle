772d1582ef68ec2f81eaf30a73a2fb2d475c4005
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:20:34 +0000

    same squash race manage settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
