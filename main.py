255a2948912e7fe39e86b16e86f0454641998056
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:23:02 +0000

    Cc noone mistake law might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
