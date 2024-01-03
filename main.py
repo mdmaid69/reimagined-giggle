473cc4bb4a6b6ddc80d87d9bcadec7bd64a4aa42
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:47:49 +0000

    every decision sticky drive forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
