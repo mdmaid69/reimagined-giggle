9a6edf149163c7f8c4e23bbe2620f2cb8a998391
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:54:04 +0000

    school possible steal contents transport

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
+print(sum(range(10)))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
