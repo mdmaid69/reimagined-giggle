0c841456bf93a48f245f889f61d6e3d4aa8c414c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:33:01 +0000

    after convenient dance drop accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
