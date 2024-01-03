f2ce0cd2fe45d9e07f880b471ef43509df7a5249
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:55:42 +0000

    exchange ball health control bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
