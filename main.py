704ee4ba103034230a45d525388c083d14bd5dfb
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:04:37 +0000

    pronounce club give avoid unite

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
