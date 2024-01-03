409837273f6a0942483f02e49c66d8e6fe144152
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:49:12 +0000

    gift steep precise it sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_as_list(array):
+        return list(array)
