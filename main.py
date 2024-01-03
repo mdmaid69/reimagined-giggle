f1f39123845fc9fc5587e1e3ac7a99baa46622df
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:59:47 +0000

    unhappy prepare need declaration increase

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import math
+def calculate_sine(x):
+        return math.sin(x)
