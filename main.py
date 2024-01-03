26cda9b80b10f12cc2eff7e0852f749351f16713
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:59:53 +0000

    advise spoil order knock bear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
