71bd8d78a9b4ba9e7842403cd823c8ef492be67b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:18:46 +0000

    priority life attempt she score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
 def create_user_string():
         return collections.UserString()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
