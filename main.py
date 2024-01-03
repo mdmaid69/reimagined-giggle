59ee9cd0066ec0b8e6b2b324dca4ff2801fbc277
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:33:11 +0000

    Ss use inquire stage back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
