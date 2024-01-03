0bf6e766a4cd15f98fa6dda0cae0599e893ed3a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:00:24 +0000

    protect special cover quit answer

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
