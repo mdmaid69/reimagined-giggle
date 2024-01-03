8eb6e6c7f7f76151c45f6980c6b0cef4ae78d423
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:02:12 +0000

    568energy without pleasure respect every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
