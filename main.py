8da160946bd9cd07ea76b1de65dc97d4c6db0a2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:14:29 +0000

    aim section soak Kk lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
