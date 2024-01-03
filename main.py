afbc6d90b63ace43c205a794f50b87fd7ec4c149
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:42:01 +0000

    storm creature owe account national

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Sum:", sum(numbers))
