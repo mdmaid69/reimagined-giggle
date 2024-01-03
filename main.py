338b964de2c3702a1e8704b09a6a1aaac29a9d1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:44:41 +0000

    person then steep still ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
