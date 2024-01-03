7c126b9eeb1a6db88987079b5fcbdb2f48412b3f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:31:35 +0000

    priority moment relation surprised ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def is_even(n):
+        return n % 2 == 0
