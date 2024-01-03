169a2df8aca1958d9e11fa545d5d84834d1a2f67
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:43:39 +0000

    lost invite many reply limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
