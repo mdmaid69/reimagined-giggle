558669483de065b2c8781766d35128a476b8c9d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:15:54 +0000

    lay split opponent watch pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import logging
+def log_message(message):
+        logging.info(message)
