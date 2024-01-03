54c217fdadaaeb5bd72f4f0fc634fb05e1538fe4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:26:58 +0000

    secret weekend minority passage slight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
