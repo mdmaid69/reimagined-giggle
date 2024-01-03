a4f9d0f145150dab207a4f2b56a32c47c04aea39
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:46:53 +0000

    lock man admire bath actual

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def convert_to_hex(n):
-        return hex(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def get_current_working_directory():
+        return os.getcwd()
