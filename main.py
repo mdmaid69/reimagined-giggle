8b6541ebed4f7c831b5c30cc3fe271d46b9eaf1e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:00:09 +0000

    explanation simple cost ignore debt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
