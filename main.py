7fd30859e066de004755eaef07e3337a7e86e231
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:18 +0000

    authority majority flat round snow

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
