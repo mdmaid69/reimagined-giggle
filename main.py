a1c51ac45aa4b63a08a0b8bbc92bdc707bef6d99
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:12:04 +0000

    talkback noone own move listenfor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
