daf2f0ad362e7a82d82a07c396ff565dc0fb4f1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:53 +0000

    famous lesson town lookaround light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
