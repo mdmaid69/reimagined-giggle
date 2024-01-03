6c769993c8b007e981b6691a7c18c1ae62ec1901
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:40:12 +0000

    true reduce she cough action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
