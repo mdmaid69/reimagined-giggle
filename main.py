fc959f75a29590a49830204e507492819584b1fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:23:10 +0000

    dish message Gg negative how

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
