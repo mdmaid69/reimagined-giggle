ca8da08f7d3db7094671d5f81a0c004421e497ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:37:16 +0000

    lesson attention open suckme work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def remove_duplicates(lst):
+        return list(set(lst))
