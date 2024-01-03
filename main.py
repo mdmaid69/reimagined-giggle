8e0f9ae5c92c38a7877b500fb299aaf843cb1c1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:29:30 +0000

    phone left beginning Ss danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import getpass
+def get_username():
+        return getpass.getuser()
