8b7e24f9659a3b58045b7635d6e94ae6cf4b3ff9
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:39 +0000

    feeling precise getout bang tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        return sys.version
