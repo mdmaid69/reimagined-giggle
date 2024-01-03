b7e2dc119c94467203b46f4592b08bfbf2cda1f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:35:45 +0000

    touch world wind container relax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
