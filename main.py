d74eb4ab77cb252bbde5ad9291dc56022860fc95
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:10:30 +0000

    attract ability form ashamed practise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
