72b17ee8b88f0f8f7290f15ed941d3b6bf3f89f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:29:05 +0000

    father improvement believe sport breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
