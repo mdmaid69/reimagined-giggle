6504e18beb50991ad3e61857825fb70803bf94e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:20:02 +0000

    beyond lost sink natural Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
