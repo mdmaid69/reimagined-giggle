0d5a9c71c480f5287ffb2546303249ab7ce2629c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:30:15 +0000

    world behind off remove wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
