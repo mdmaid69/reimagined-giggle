6bc2eb5232b1afbd0a5dfb8d80528de975f032af
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:24:40 +0000

    pupil nor bored opposite scene

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
