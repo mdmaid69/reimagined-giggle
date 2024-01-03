d8399e23ce4cd82a6347b3fb3785762c44cc4a94
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:23:23 +0000

    progress Ss shoot cheap mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
