17cd2a2087c4b123e39b240262149834991ecbe7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:26:24 +0000

    Tt this holdout trip ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_work(force, distance):
+        return force * distance
+  def square_number(x):
+        return x**2
