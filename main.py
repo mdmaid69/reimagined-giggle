8bb136a5490f4bf3c51b7226d99229cf63883c69
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:51:56 +0000

    seriously lunch recognize against rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
