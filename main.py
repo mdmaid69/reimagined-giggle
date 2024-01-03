5cefc2eadc447ff56209f7d7ac60c10681f35848
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:58:23 +0000

    spread performance violent uncertain on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
