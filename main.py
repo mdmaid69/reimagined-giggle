e90c3e147b8d77b86dfbe93732ae5602e90868b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:27:55 +0000

    middle on slight Zz secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import numpy as np
-print(np.array([1, 2, 3]))
+def append_to_array(array, item):
+        array.append(item)
