2d136dadfcc68901d21fffff6ec98cf534a67dac
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:53:47 +0000

    one address somebody provide give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
