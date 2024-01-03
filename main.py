27ff419e559080745bb21d091c28a378384f46b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:54:01 +0000

    know past match expression apply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
