981fea548845f5e289a2811085500419aa5a1cb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:37:23 +0000

    transport rain add better slide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_sine(x):
-        return math.sin(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
