f92268bc0c6cffee379f824d2816cbaecd82818e
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:07:11 +0000

    fetch about hang sink conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_error_function(x):
+        return math.erf(x)
