1e36082b8b28dac328fe250ba9b832aaefa74d85
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:22:56 +0000

    vanish pile history beat calm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
