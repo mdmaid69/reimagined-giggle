aef313ae5627a4a9f000dcf78b8137b64b3af03d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:01:19 +0000

    book slow course sting spill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
