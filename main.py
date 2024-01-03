a0d8d2f15a49353eb5fcd51de3a16ff745bccbd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:14:55 +0000

    around promise past present only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
