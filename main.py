ecd936e167dd2724534647b4e1db7551f8d90dec
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:33:27 +0000

    have judge weather surprised bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
