e65f769c145059ae10a6bc808715627343bae34a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:59:00 +0000

    main Jj uniform tune effort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
