4836e2594a4301d78cddcb24127de17c27fe1136
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:15:16 +0000

    service charge clever shut rescue

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(lst):
-        return min(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
