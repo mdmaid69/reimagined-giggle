88dcc0d4b3a3d938fb846c1cc9aa4efd89ce7b64
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:41:22 +0000

    butter bored careful getaway file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_itemsize(array):
+        return array.itemsize
