6ec213e5c26a1c94ca7579d0345322b4c65bf99b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:44:22 +0000

    education concentrate foreign quantity excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_speed(distance, time):
+        return distance / time
