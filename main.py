63c96a0f7bf9551336c10946806855cf37d302f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:35:26 +0000

    lookup grow hungry gentle proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
