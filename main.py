d90d3abd4ce1a2930860dda800468a88fbebac0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:58:36 +0000

    correction bringback consult dress here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
