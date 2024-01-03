335e4bbbd5508266be9121d4f66ae8bdef1d4adf
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:12 +0000

    black quiet insist discovery control

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def is_even(n):
+        return n % 2 == 0
