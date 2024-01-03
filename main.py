244c924adaf9f3e46fef5571c86105d12365233c
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:05:39 +0000

    wet day thank abroad concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
