5bfde8b55bba9cd72818961951cd570e5f48ba97
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:21:03 +0000

    what below lookaround unable muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import sys
+  def get_python_version():
+        return sys.version
