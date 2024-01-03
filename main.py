5420b6b268f7be5cdc2c7067ae389ac9aa87600c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:59:42 +0000

    vision towards breakin stare open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
