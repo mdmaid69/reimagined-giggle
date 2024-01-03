51dceb848992f2cfaaa765e7133f955e795ac9f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:38:07 +0000

    place match Ee myself near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
