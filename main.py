3b616af78cc93cfa8aba817863043909869dbd98
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:26:41 +0000

    ground if risk occasional continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def find_min(numbers):
-        return min(numbers)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
