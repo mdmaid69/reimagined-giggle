21bc49b5ae2b89b9c14291538e198c39a7fd36e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:36:56 +0000

    country invitation respect necessary worry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
