185c9ccf4fa0a0791badf3daafc3f9e607db25c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:29:00 +0000

    charge risk thirsty rare theirs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
