0e6c6100d059614bb18b283b333b38eada796fac
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:27:55 +0000

    tape murder deep centre mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
+import array
+def get_array_as_list(array):
+        return list(array)
