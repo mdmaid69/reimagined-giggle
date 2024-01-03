b1c80738c7799031032c225bbb9f4f3961534778
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:19:57 +0000

    runaway towards project contrast boil

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
