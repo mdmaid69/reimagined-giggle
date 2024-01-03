48e00b1dc51084203e6b2aa144bbfce91c35e942
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:19:58 +0000

    report sale impression copy discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
