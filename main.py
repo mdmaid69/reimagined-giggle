18c12a2d9f62b6e4f2da3f829e7848d60b86cc15
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:55:16 +0000

    reason square survive comeout same

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
