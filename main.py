611d9d736642f92b895094ed9eada05302c3744e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:38:36 +0000

    sting carry adjective breakfast still

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+from collections import Counter
+print(Counter("hello world"))
