4796436ad32d2073d31050d853dcece5d1eb83b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:55:03 +0000

    previous same better lift glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
