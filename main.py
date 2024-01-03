b70edb22f84a5f62d4cb3e74609a008a1641efd7
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:01 +0000

    can the then break ride

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
