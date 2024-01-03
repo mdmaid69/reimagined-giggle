ebbee56e712c6d6e3c15c28b95a1c7be2b97cb66
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:03:02 +0000

    last sure drawing Zz pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_sign(x):
+        return math.copysign(1, x)
