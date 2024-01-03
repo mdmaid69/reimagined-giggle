2d9f9e49bd545ca64e86ac125f47293caa50d275
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:16:54 +0000

    naturally same probably impressed quickly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
