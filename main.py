ed95293028bd316f5a6fc6f94d0b656539a75c9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:24:11 +0000

    turninto rarely disturbing goaway drink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
