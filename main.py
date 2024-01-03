8b14cfb5d96aedafb7949c1ee67a6e8ab317658a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:30:18 +0000

    justice roof speed invite sad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def convert_to_octal(n):
+        return oct(n)
