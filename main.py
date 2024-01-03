b6207632e00104a89b8bf61f268c78bc59056db6
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:43:32 +0000

    slice flavour or takeback willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
