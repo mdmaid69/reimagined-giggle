0e068bf3d0f3c2ef94816ca240d2fe124655da63
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:23:12 +0000

    opponent loud choose frightened little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
