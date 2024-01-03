ef20895f22ea5e04e201c92a5688804a544776a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:41:14 +0000

    expression character peaceful declaration soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
