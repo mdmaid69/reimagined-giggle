a588bc66f8ecad502e9d90ea277b23f1422cd6c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:14:15 +0000

    squash produce bottom fine breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
