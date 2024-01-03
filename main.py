44134639c46864c42ca30003ba1bcbcf0a31ffbf
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:00:07 +0000

    off break copy be plain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-print(random.randint(0, 100))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
