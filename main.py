bd3302f3b74d3fc91e51f6fc4e4caef0a6756ca9
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:45:46 +0000

    report threaten thirsty reject run

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
