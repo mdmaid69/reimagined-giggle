11cc821711705dc52504c4baee21b940d70a0184
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:11:05 +0000

    cost lower permission butter prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
