4265ec72f08de2bbfffd55a1a4b903808b2bcde5
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:51:49 +0000

    direct bet forget justice style

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
