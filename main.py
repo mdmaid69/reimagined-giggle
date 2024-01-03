f1fd84289c7639278372f72b3dd44ebe429d9564
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:32:32 +0000

    warm prevent cut surprising nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
