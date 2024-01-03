7a4d0c39d94212cc037f5943dbd4e5aa98b6e952
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:46:21 +0000

    school bet private sometimes frequent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
