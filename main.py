84d792e9525af82b2bf89094b6208270012e92c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:54:54 +0000

    wipe chin insist tall clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
