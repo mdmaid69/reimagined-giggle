28ecc2e1f68c6bc9a791178c968b7bda147fc5fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:31:27 +0000

    wire suggest paper form telephone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
