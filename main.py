34980022ff268be3403ec695015eef2621cf484a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:19:37 +0000

    hollow bring term heavy embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
