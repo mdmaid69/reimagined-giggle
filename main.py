50ac34289cea7d28b34131a821b817d02ca09b2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:33:50 +0000

    desperate fond price wear trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
