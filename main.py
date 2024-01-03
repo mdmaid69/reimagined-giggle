90ebe9b95e1909f2d15bf29587dcda6876117a0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:54:50 +0000

    refer welcome male start chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def greet(name):
-        print(f"Hello, {name}!")
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
