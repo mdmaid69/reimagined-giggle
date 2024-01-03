3b815f603e45b201da5ec18901835785c203d3f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:04:33 +0000

    start sure difference authority takeout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def square_number(x):
+        return x**2
