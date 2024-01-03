173e08737ec4683f7a9fd948135fb6eb9b2ecb70
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:27:27 +0000

    owe lock hold judge concerned

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
