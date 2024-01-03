4a5550ebac8d709e9f29657159765b3637b9aa25
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:28:55 +0000

    march connected lose interfere afraid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_current_working_directory():
+        return os.getcwd()
