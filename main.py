782912a10020aa092eadd29cfc213654b7acb209
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:20:00 +0000

    spin friendly second 572enter play

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
