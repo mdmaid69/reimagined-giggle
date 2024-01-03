42676eccd73ac1169cdd1e1fdbf641f4167305d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:25:22 +0000

    complaint pile follow myself runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
