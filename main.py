a18e45e3ad6a6eb27766b1df27026b50b4f72acc
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:52:02 +0000

    beneath view drive top thinkof

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def square_number(x):
-        return x**2
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+for i in range(10): print(i)
