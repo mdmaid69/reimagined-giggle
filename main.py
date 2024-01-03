23f5d9452049d53224ede8cbf5bdf4d0d3bcfad7
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:30:53 +0000

    takeout typical and lookback(on) join

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
