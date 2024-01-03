5047deb98369a7925fbe8e6a217eab86640557d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:51:01 +0000

    take dig air crazy complete

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
