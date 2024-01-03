884a7cb3c833bbeb6bba78be796b2b51ca52817f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:50:59 +0000

    prepare apply level stay hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
