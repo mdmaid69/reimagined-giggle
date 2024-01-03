4e52356a041a74ebcbcd2484a69b15f6a8f9713f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:38:11 +0000

    warning arrive public fact thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import collections
+def create_stack():
+        return collections.deque()
