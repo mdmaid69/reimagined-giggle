34b4a272d47acdd2e09d8cde4153eab8d2244e96
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:56:44 +0000

    face correct dish long left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
