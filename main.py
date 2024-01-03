55e9f84c3a18a23e4fd2be3d5a4ee5a2dd641042
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:56:26 +0000

    pity rain complain autumn second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def square_number(x):
+        return x**2
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
