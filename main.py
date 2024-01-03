16f75b60a845cb35ae5320b22817f95d3a23c802
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:47:41 +0000

    age future organization identify tall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
