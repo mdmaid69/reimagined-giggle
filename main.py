da28756e4c4b8378a8dcb4ccf221f41cdcd0b360
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:00:19 +0000

    bad charge centre free stop

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
