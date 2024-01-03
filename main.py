da1c85385b805acc825342382f428d354a242cb0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:38:09 +0000

    determined callback myself wipe solve

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_acceleration(speed, time):
+        return speed / time
