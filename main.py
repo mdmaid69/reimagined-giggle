8b7de5ff06510d5e913f2da51f16572149da8645
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:17:19 +0000

    silent tomorrow storm wrong fact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
