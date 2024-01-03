1ce78bb4b2b6aa0f8db25c74bd4c554296bfeaae
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:51:51 +0000

    rarely belief place tongue failure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
