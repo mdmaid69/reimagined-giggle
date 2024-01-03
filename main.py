f5fc6ac6d9fd0f5bcc722fb4877226b9b18bf07d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:52:53 +0000

    heavy deserve cry obtain whistle

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
