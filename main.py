407b1a571de0e5f455e5617f7288bfac6a1aed58
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:09:45 +0000

    special deliberately standard plenty interest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import time
+print(time.time())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
