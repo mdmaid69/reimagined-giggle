e9fa5e2cbd473837a92c5b0ea0bd08a4dea05658
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:30:29 +0000

    swim smile thanks mark where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import platform
+def get_python_version():
+        return platform.python_version()
