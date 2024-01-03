2e3179343ff1b703ac7d278173db03f151fc4d7f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:31 +0000

    obvious attitude snow suggest exchange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_speed(distance, time):
+        return distance / time
