31c9bacbe5c89e9494d5a1970e1b84a7d32646f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:18:18 +0000

    car age stretch flavour number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
