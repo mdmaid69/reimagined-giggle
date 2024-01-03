82df9836257e6be1a6c7254227d5f2c1ee66b0e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:05:06 +0000

    organize last somewhere game hate

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
