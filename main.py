7bc4663fc5ea58072a99ca1d26b92f77317c469b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:05 +0000

    our soak classroom refuse deliberate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
