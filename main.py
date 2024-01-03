a1d51d345548c1e9df1b5e334720f0aabac2662c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:07:40 +0000

    turninto judge plate pump popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def get_array_length(array):
+        return len(array)
