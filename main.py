9a9dc709493093da280c59f5af83f085c64cffb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:46:20 +0000

    practise unfortunate go ago work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
