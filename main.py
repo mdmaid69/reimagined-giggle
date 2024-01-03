c80a999184a84f9fa74c1ddb2341643dfce7d970
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:49:26 +0000

    day adjective away bear reduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_array_to_list(array):
+        return array.tolist()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
