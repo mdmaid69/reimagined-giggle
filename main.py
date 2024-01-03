d8982e64727424fffc6cfaa7a0143c048ea77393
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:00:42 +0000

    clothes apply tongue plastic tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
