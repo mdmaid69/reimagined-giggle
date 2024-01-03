847b143e0b2b4649d34ee75c8ce5262d5bac716b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:25:07 +0000

    travel combine modern keepout country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
