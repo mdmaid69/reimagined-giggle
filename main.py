e55d48661302d036170b1ddbb62351fd01c6fcd2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:28:51 +0000

    world through almost politics exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
