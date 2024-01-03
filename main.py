7f92970ee54f023f898604d05603fed5f131273d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:04:41 +0000

    surprise operate front rubbish comment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
