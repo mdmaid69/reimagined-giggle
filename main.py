a3852fd5dbcb34ca887fb6966655e5cd4ab6103a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:14:41 +0000

    situation keep history still steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
