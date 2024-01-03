18f226bd588a3e4e86360d7447e91edaa39e416d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:31:36 +0000

    side choose probably occur danger

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def set_array_item(array, i, item):
+        array[i] = item
