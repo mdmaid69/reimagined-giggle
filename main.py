629f2508474c9bf9bc8a08b2c4f7252ef0a34719
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:25:49 +0000

    without last custom friend motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
