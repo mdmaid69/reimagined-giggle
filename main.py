9778fd449972d9b36465b2be8c011d31ecfe8267
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:43:49 +0000

    today involve seem eat throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
