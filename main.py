82a8758425f5417b1cb9f1706a0ed552870eea99
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:39:09 +0000

    might opinion influence discovery in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
