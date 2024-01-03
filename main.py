de90595a7c0456a68b8d28f2b8b47ee13abd5606
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:55:24 +0000

    vanish pump face sink speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
