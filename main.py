053a27358f799446821e02a682885ef7d52a7c1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:07 +0000

    couple capital area view conduct

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print([x**2 for x in range(10)])
+import sys
+def exit_program():
+        sys.exit()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
