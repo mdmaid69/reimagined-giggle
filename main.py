71e3c1c1e7e7f1a9b02ca3198807cd43f7ded89b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:26:59 +0000

    exact notice change majority alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
