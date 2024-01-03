3a797cabe96d071d741b0e17601caa8363dcd337
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:28:05 +0000

    get mood pay 564encourage group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import time
-def get_current_time():
-        return time.time()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
