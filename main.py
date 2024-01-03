9ba5d0cdd8c66d2f5a4ee6d0c344de30de2c1ef1
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:43:32 +0000

    little adjective contents game repair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
