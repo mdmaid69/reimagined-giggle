a3527a96278de4bad185869143dcfc4417d69144
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:12:54 +0000

    refuse thinkthrough pay very proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
