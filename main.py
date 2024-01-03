4297731c4c7c3b1b632e778215fffee0b7519ad8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:02:05 +0000

    lonely long and smile usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
