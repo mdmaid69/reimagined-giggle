73abcd3f73eb461c767050352762d1e48ab42a01
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:44:25 +0000

    storm storm aim bill more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def exit_program():
-        sys.exit()
+  def get_current_working_directory():
+        return os.getcwd()
