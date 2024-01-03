d2f8bf527fbc94903b0df72e718ca90158fc0b0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:19:41 +0000

    certainly bar piece exciting advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import time
-print(time.time())
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
