2541b878d7040281a452b741d2945b253178d24b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:47:47 +0000

    uniform number division experience story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
