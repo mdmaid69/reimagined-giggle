8cc46f259672057e79388283af389f273d4a8367
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:51:05 +0000

    general forgive degree value mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
