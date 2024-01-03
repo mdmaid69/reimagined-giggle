cad254fe7326ddfcfccf9621c8c01c44dcc69d29
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:32:47 +0000

    wrong like report wait anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
