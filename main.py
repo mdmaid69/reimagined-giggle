f6245c3e4981c4f8addc7e91c98983b7421d5549
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:05:13 +0000

    concerned centimetre nothing opinion touch

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def convert_to_binary(n):
+        return bin(n)
