3a37db4795137935c57bd5c6b90c3ccf8a763d05
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:22:12 +0000

    hand record rub funny gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
 import math
 def calculate_gamma_function(x):
         return math.gamma(x)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
