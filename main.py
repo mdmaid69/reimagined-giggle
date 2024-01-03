dc3014a15f73301e80bedbd8ffa55b0f328ac95d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:06:29 +0000

    hide do card upwards slide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def square_number(x):
-        return x**2
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
