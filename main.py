7d4c1b484a756388e59a0a2e011a1a5a17bb8f15
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:34:45 +0000

    split than smooth roll their

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
