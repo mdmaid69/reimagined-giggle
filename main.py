304c450d0226a5b2b5d5f14c5a701d67f1d24881
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:58:23 +0000

    professional town creature Vv runover

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
