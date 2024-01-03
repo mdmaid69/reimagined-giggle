76d03fcd716d94aabcb0d70f24a164f4bacbf210
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:54:24 +0000

    walk trousers play sink account

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
