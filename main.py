d22db23ce869ea9bdcac887da363df57c3b93b47
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:21:42 +0000

    variety feel sing wet on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
