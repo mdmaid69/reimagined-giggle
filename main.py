016c4faa3594a64ff471266555fb735a530fa8dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:09:15 +0000

    572enter proper fast back risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
