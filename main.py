70f8d2bad3d1c7f14fffbdff95bba77b2a3fde27
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:17:40 +0000

    whistle profit measure near mail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
