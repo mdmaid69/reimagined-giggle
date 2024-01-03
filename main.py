cecec6fe67660d83fbe4500062308f680ca36372
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:28:58 +0000

    that life game lend wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_user_list():
+        return collections.UserList()
