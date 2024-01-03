5756ee15d1e18b1fdb390f2a78b246fc00b90b30
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:28:19 +0000

    wear waste sight clear lift

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
