95ed9341aeef0efe8d7c2268cc8cf96f2a119a93
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:45:56 +0000

    repeat slightly expression scream odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
