00acc35d436fefb1e42180a86357c4538995c822
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:15:21 +0000

    show odd experiment match side

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
