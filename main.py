a7de7b9be51129ee0b3cdb4300ad3d3df4ebd916
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:51 +0000

    good environment separate destroy turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_set(array):
+        return set(array)
