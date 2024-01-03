3e5a955ffb2352d1bbfd4e0a34bab911ac7830c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:26:00 +0000

    opposite variety visit tie trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
