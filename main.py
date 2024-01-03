7ae9767d90004dee108be9237b02ec93000d2e4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:59:14 +0000

    hurry basis second Pp negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
