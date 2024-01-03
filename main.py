73bb7971c2d63ca5373125a1f66435295236ccfc
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:15:33 +0000

    never interrupt shoot thick unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import time
-def get_time_since_epoch():
-        return time.time()
+  def square_number(x):
+        return x**2
