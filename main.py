7a5aee10cc4d3f2842a8ecfb0140fc5b1ca78c17
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:53:21 +0000

    unusual ill thumb satisfied guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import sys
+def exit_program():
+        sys.exit()
