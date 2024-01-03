4e7bafa8ff22ff728f6fcdcc2c680b964e0437a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:19:51 +0000

    under eat room criticism meeting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
