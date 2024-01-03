18ead10a3564a3afd669b27fb19af4100465b817
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:05:30 +0000

    wind stop obtain 572enter common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_acceleration(speed, time):
+        return speed / time
