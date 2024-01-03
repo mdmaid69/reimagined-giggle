2c990891bb8c9a3004a6f73e1138e5b3ed863a8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:00:03 +0000

    along official worried thought visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
