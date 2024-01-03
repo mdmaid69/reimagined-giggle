0aabc7e462c25d89219a2f27ff4717fd679a0121
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:11:34 +0000

    bottom easy keepup limit punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
