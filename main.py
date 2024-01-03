6805b3730315ef4060ed82ef459ef3714caef9dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:13:28 +0000

    to tune divide dark limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
