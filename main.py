cca860cabd1e2731b4383fb6b10a167b99a437ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:57:28 +0000

    team at neck clear flow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
