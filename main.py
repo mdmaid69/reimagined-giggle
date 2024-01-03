99d183f73a083a2a31d53ba468f04ef5e2ed022d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:55:29 +0000

    so unfair cost act walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
