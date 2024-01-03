a657d7c04f3a382386434220fb6c98606d645a4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:39:40 +0000

    speak getup keepoff wave hollow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
