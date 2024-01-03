2f0ec6735b9a544de28768fe1303fd6711f80361
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:58:54 +0000

    disagree love skirt mistake bury

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+print(math.pi)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
