a818719a6d5ebfbc7d384982f33a34df50daceb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:04:21 +0000

    offer high material tidy write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
