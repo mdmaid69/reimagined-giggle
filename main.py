cd72bc2618df2994413e08ffb2cf45c5d25b9d82
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:27:33 +0000

    error pipe sound will in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
