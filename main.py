543b1bd13ef8f3b615976ed5f72957f656aff493
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:54:10 +0000

    mystery degree happen evening run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
