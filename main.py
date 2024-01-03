37577147c55f6af12391708e3a5c65eb8ff26025
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:43:30 +0000

    attractive trade everybody sentence down

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
