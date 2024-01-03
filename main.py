419482d9777444e6b14bfd21fd28f625a053cb93
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:13:50 +0000

    official advice list makeinto break

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
