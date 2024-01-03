de4b7f7a3fb804da227959c9fbcf137efa43033f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:16:51 +0000

    backwards people evening have whichever

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
