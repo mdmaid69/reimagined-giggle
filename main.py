2bd2271f365269378e1f27c91169cd19ec83badf
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:58:47 +0000

    art OKorokay slip short person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
