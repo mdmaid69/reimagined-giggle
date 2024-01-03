ff0976b36ae648ae4db443595cfd23e893507869
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:58:26 +0000

    OKorokay separate manage lookafter odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_length(array):
+        return len(array)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
