2c568890e365f8229d283d40824ee27e608f9df6
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:04:34 +0000

    kill just dark mysterious goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
