d5626ba9021b99890441c245ca5f85519ca6c413
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:25:49 +0000

    check pleased fond accident but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
