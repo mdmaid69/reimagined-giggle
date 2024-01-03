e6c25e0026123c4a4bcc059cdc3161b4ee415edb
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:36:35 +0000

    heart deserve mysterious choice degree

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
