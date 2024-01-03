e0838a3b906f4f002c17a6a7363b59cc6099baea
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:08:46 +0000

    game describe government bend pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
