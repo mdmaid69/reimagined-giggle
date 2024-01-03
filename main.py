b0a348de58ef5c1abd8620adf23a8a1f98f5b898
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:29:02 +0000

    post skill afternoon evidence Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
