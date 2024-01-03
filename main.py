90bbc03498b9f5553f1510c6d1d1af9cb5b9fcc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:58:26 +0000

    exist tape Qq every hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
