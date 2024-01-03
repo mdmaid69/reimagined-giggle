214da3d14dd550122fb41ac6c21a97eef800a1c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:56:24 +0000

    lookafter sink reach car roll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
