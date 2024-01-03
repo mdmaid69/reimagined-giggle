41f0d838871bc486f8bd7e455b0af56326a73e94
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:06:50 +0000

    pure still needle cost smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import math
 def calculate_least_common_multiple(a, b):
         return abs(a*b) // math.gcd(a, b)
