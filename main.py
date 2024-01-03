f92e4823e4d3aae2748d5f2f6193111ef8f1329d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:38:10 +0000

    complaint family sure illness boil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
