a01bafbfe9e08a55f8649c554f8490f2b6f421be
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:43:43 +0000

    Oo in connection statement holdout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def is_palindrome(s):
+        return s == s[::-1]
