41afbb3eaedb4b7ccc6bf96e2711b0519503dc4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:42:29 +0000

    exist makeinto allow secret average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
