4d1f78226f2ec273dfe4e1fb5e5d4d0d3e917ff5
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:01:26 +0000

    drop stone bring mail intend

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+text = "Hello, world!"
+print("Words:", len(text.split()))
