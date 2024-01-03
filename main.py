ab5676538e695854d83c3aeab3142920442e3002
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:04:27 +0000

    count tidy excellent forgive welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
