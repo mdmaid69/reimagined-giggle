c34d86fea6741b6fb373a8eaac435ab0a3d14c0a
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:41:29 +0000

    he bottom violence concern better

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
