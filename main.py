408d5b05731c7753c3a93899b529b003417bcecd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:28:52 +0000

    couple teach pretty giveback perhaps

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+print(math.pi)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
