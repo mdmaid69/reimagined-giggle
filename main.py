4bc5005356d0d1f33fbff9da1666469b1e955eb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:13:39 +0000

    crisis control sing sound tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
