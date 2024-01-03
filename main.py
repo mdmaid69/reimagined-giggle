99bc9b8b34aeda1638cc68b086b22a66e4122608
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:12:37 +0000

    freeze autumn confusing themselves charge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
