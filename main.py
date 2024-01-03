a710738e62197304abad4958a62bcc3654709311
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:41:54 +0000

    shop steep tape bottom realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
