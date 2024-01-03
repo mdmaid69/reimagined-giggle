bf0d5c0ffdc204221e8f36376de43fd2294a3dcc
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:01:09 +0000

    fence taste past pull scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
