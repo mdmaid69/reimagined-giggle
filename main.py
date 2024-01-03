39044e5a4e864ffbb168f1a7516b291eb83fcec8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:13:29 +0000

    properly exit regular substance sweet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
