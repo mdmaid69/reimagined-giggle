2b031247d9fece06e55e75df8f6673bc372f3356
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:01:11 +0000

    hungry bring open wind watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
