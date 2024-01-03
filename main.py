2698ba825df9fa506e0774a95933bb3ceab00ba0
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:48:41 +0000

    alive measure purpose compare shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def reverse_list(lst):
+        return lst[::-1]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
