d27bca5202da63bed577f07583e885f6a3ede45a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:23:53 +0000

    exercise use positive out call

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
