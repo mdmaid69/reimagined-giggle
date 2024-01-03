ca68a6be9fdf08eadb76ae0ec876d87437026216
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:52:25 +0000

    supply like cost cry share

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
+for i in range(10): print(i)
