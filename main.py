dd145bda7b6efa01bea7c3cc104ae3a8d4cdda3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:16 +0000

    save somehow mad during clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
