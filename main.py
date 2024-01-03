309b4755a1c776ada83e66c6ff82484189952901
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:54:21 +0000

    lookaround since let forever style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def add_numbers(a, b):
+        return a + b
