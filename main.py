c76a684b1b7bae8fb7a3552f0e88645bb56bf80d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:44:52 +0000

    whenever lie refer together Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
