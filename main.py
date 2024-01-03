19837e114c6672d913ebc94d7831485e95e28c34
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:28:02 +0000

    pronunciation collect blow message usually

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def find_min(lst):
+        return min(lst)
+def calculate_speed(distance, time):
+        return distance / time
