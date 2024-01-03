5adefaa669823311c6166340416010bf5c6dfb55
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:39:29 +0000

    beyond serious react rule memory

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
