b90d46a6fd78f3419dbf29aa27acbfc19e26fe10
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:19:12 +0000

    whole sick grade excuse failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
