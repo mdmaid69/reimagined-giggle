4337fe947ccdc694a9678fc8600e58846467fb91
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:32:44 +0000

    sad extraordinary practice get charge

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
