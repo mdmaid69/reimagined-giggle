91f10d9bb1aeabe842e9dbe246cb51aba386ae2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:22:41 +0000

    occasion drive choose friend sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
