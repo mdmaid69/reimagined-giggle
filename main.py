d8410b8342912a705324344380adfbed975e9b53
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:27:32 +0000

    ilovedicks drink authority plenty watch

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
