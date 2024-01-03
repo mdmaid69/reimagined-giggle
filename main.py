4433851d75d32ba898f00225129b923d49363be6
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:45:49 +0000

    actual before consult platform extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_median(arr):
+        return np.median(arr)
