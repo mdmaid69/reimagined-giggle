3f837cc5fc7ddf7c23ac08db009452c32073ba53
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:48:32 +0000

    uncomfortable comment system appear think

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
