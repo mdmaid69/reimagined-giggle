954f593126eb00bb3d96d59fb056074a8ef99d08
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:03:38 +0000

    wherever Dd thinkthrough call closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
