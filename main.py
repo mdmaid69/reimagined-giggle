f7db0365d23e1a04a787b34b14c9aaa363b1601f
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:53:13 +0000

    splash cook least recommend breakup

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
