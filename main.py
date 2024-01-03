df021c2b927d12dfae290c82eb1b3cb032e0a530
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:29:43 +0000

    preparation ago must deserve can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import tensorflow as tf
+print(tf.__version__)
