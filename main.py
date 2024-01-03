61b02c17287bb9d3005e34f121a3404e067d9320
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:58:12 +0000

    less understand brown accidentally hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
