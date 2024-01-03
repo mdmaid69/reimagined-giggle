565ee692481dbe8439381f08a3d1db7c1fecfd25
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:38:53 +0000

    road foreign bringforward live mind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
