bb76bd1a7d21d5800eff6ec75cb7bb46e6c7ff3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:03:33 +0000

    plan choose claim limit secret

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
+for i in range(5):
+        print(i)
