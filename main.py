3c3845a3e51d43e6e69d0f25da194a1a8be987a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:23:11 +0000

    571enough pay rate nerve ought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def find_min(lst):
-        return min(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
