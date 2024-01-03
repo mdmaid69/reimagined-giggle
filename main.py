7d847c7b2015ac5628608343db6e516c132bc6b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:30:46 +0000

    otherwise expensive ill turnup season

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+print(sys.version)
+def calculate_area_rectangle(l, w):
+        return l * w
