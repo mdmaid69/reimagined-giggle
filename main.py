9da78f7b6ef86d50ca16650a35c90c275e243da4
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:20:22 +0000

    system Jj brown accommodation follow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
