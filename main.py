6b9fea8f800cf044f3b9e08728fcfb930441208d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:38:08 +0000

    naturally taste pocket charge declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
