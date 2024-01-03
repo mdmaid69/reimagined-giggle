f183a7010964394041e957a720902c58654edde9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:11:33 +0000

    instruction march influence takeout keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Hello, world!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
