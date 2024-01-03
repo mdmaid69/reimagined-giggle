a47514ccc8d1ff4faa40e21d4281d3abe944c41d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:40 +0000

    no content regional visit possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_as_list(array):
+        return list(array)
