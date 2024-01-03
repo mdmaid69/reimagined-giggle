beb31a2201410064cb680f6082c57e998c264214
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:04:39 +0000

    greeting develop together niggaacademy subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_pressure(force, area):
-        return force / area
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
