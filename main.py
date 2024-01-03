ca396cb2720efe9921221270091418b0b7e0c6c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:16:08 +0000

    play ours second healthy live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def count_elements(lst):
+        return len(lst)
