33a4e17de4ccef4f288d09db196f69cb64dabd5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:05:02 +0000

    drawing result plan total single

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
