8b8ff0585a3238a55caebc4491067afb7d915ff3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:14:57 +0000

    extremely belong under suit bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def count_elements(lst):
-        return len(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
