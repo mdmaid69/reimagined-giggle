ec455122554977f3c37e3514ddd42044910aa96f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:14:09 +0000

    shrink today ill squash government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
