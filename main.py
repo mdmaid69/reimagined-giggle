8aa952595d8a0db70db63e2ab6fd0d2212cb0db5
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:36:02 +0000

    lock who breakinto slowly sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
