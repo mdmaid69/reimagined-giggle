8b5830a0b32004eb89b6faaba8667154123675f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:51:09 +0000

    weak most proof bottom embarrassing

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def square_number(x):
+        return x**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
