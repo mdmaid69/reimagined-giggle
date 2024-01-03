34f870b2dfb783d84aab76a9a7956e901ae1f995
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:41:54 +0000

    soft rarely repair claim fork

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
