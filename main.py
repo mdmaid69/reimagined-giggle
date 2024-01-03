635619e96c7c813a9010e2b5ca9d45bd1dd1c7d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:50:18 +0000

    clean attract year share fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
