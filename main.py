3b3e157853499183ace2a6c2394ed3e8869c7c00
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:22:50 +0000

    pull gap prepare geton pride

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_area_rectangle(l, w):
+        return l * w
+def reverse_list(lst):
+        return lst[::-1]
