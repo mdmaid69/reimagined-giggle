a3ba94a0b2dff8ddc347dc4d909b87723e9fa601
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:57:30 +0000

    thing keen absolutely crisis wear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_length(array):
+        return len(array)
