69599ac6d54a2e677c4ea81d49470c3b530f4f96
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:13:21 +0000

    look high nervous pure wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
