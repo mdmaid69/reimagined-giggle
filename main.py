d7e89c689815f80fc9717b8862753ddba6924d70
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:02:41 +0000

    wind phone protest towards set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
