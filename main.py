05328c8c6f31534071f74ee54eab8a455b90f209
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:26:00 +0000

    insist recent scheme bite soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
