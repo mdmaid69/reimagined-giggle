e9f2804c5cd020d5ff371c1f64b7d6642404ba2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:12:56 +0000

    loud accordingto lot division holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
