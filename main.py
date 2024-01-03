8b440b4e217876d9e58a7e1bbe084afce706cd6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:16:55 +0000

    turn eat backwards task interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def calculate_area_rectangle(l, w):
+        return l * w
