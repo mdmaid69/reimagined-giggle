1a46593966b9ac9ad06aff3a000315219736f063
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:07:48 +0000

    giveback crowd business window before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def is_palindrome(s):
+        return s == s[::-1]
