c4998f5efce05d9058b1312fc0a0908ee6c4b420
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:33:28 +0000

    education care runaway than small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
