5f7f858bfc1254481238966c64177ae3c796fd02
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:16:49 +0000

    attempt type serious pot proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def clear_array(array):
+        array *= 0
