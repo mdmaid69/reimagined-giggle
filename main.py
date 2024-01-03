363c93676f831f5f51671b0a0cf1fcadd0d70f02
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:57:05 +0000

    glad last holiday it platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
