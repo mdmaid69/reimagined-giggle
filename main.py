58325ed814d74312c6031bec7cb1a3bbd24de858
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:49:53 +0000

    birth workupto when software pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_item(array, i):
+        return array[i]
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
