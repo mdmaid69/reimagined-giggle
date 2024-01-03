76ea595b04d34337a6467e393c82d77561304439
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:15:55 +0000

    sink report better slide cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
