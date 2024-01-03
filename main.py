1e5476e29e42011265b8af6973b4aa0916d612ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:16:23 +0000

    occasional none noone schedule ear

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def reverse_list(lst):
+        return lst[::-1]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
