345726076ffdac42f91e7e45f12362dba6c65d91
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:11:14 +0000

    trade pinch workout down average

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
