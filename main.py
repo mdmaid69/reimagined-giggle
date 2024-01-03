c7128e715a04b132727a23fa62c6cb684c9a01a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:07:51 +0000

    possibly risk repeat right zero

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def add_numbers(x, y):
+        return x + y
