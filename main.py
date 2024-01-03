289242c5b2187fb78e1694af41d2c5a7801f4409
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:25:13 +0000

    worth thing ask bad exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
