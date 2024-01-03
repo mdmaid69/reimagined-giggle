ca552f3d4dbff035f06ab08e83d2f42f375da3f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:04:01 +0000

    whatever favourite do identify anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_min(numbers):
+        return min(numbers)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
