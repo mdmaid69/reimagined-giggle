1d7f803aabfe5124e8ec7864aa5c08923592a835
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:44:23 +0000

    genuine order dear mind heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-for i in range(5):
-        print(i)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
