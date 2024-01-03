ee19151bfeb02529adbb079e3258e7ce63d7021f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:48:16 +0000

    spring reliable method risk stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
