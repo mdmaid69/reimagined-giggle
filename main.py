8f94dab7f74593bb32b93e08927563c251a1447c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:17:31 +0000

    programme container concern person completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
 def get_array_as_dict(array):
         return {i: item for i, item in enumerate(array)}
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
