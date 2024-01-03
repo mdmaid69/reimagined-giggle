dfc8b97f8ec85c4f94d6e6804455c7e3d251ad45
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:24:27 +0000

    pour level desperate edge comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
