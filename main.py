d26bf7dc3ece12b0e69f34984aa44d998e51c06c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:28:00 +0000

    lock can discussion scale control

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def reverse_list(lst):
+        return lst[::-1]
