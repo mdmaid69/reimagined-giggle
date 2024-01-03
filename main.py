911921bc1197c2705f50472a837bd7a26ebd2d1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:01:00 +0000

    properly your chance 566end effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
