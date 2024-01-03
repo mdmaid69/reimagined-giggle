2c3b545550c104f35cc5cd963d82e6605cbaaeba
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:13:28 +0000

    contents opposite rubbish experience forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import sys
-print(sys.version)
+import collections
+def create_stack():
+        return collections.deque()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
