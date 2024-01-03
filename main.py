f80209964e0e31a980fdea57db897b7211b60fa8
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:54:24 +0000

    front repeat tragedy thorough find

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
