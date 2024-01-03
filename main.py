84b020abd772a5f82bb280567b85ecde550efdeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:30:49 +0000

    immediate fault plastic lock smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
