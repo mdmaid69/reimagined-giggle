886c8a2ed56cd658b72374f3c292475ef94f22fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:33:18 +0000

    hurt list substance chase connection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
