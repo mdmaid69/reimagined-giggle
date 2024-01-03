78875548103ac70470d8e54fb7a5075765a1b316
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:13:51 +0000

    harm most wrong declare record

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
