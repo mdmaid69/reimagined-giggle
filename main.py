9ab5e340069fc114b690bd369b7c9b8a26f8e88e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:12:31 +0000

    education battery later under window

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
