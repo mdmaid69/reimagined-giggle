3b50eec13426a9f5f054d4e62e8372c3d9a463d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:12:59 +0000

    street warning variety Nn solve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
