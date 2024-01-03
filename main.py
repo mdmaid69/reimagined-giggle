d385e75192886ae0e16e5dde4adf4c90e75e50e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:34 +0000

    mail grateful juice happy attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
