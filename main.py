d4855e47aeb67074d9719828cf2c6aafcc842d87
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:58:13 +0000

    any slow team software plenty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
