8e55b6ca632b627e19944e79ed6617b533c6baac
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:22:41 +0000

    getup aim happen whom tune

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def convert_to_hex(n):
+        return hex(n)
