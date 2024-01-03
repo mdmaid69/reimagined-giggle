9b3d9b33153e91957856e9a54b7e51bf09693826
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:08:55 +0000

    bar trip lucky cook test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
