d74c9d77f7801dbb9c8c01ea42639f4a5e19a64c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:54:54 +0000

    naturally lot concentration clue meet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def exit_program():
-        sys.exit()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
