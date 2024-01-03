58e0718f746b4a484ba4c059e867d1bbfb654d7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:33:37 +0000

    clue false trap above near

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
