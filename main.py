e092ee0cbb22278f3731799db6300f5c6015c9c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:53:21 +0000

    add work belong future radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
