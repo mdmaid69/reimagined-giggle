50fd2cf71b3d0141bcdeceef90c7edd2472d3d02
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:23:39 +0000

    arrange account broken outside watch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
