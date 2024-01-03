7573fae54c2625d14bc12f18ae9291722c286164
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:45 +0000

    inquire other exam ashamed takeback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
