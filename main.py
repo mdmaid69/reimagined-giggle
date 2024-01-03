ea8bb9716e54e252d4c25183de126d37890cc2a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:00:21 +0000

    anywayanyhow interrupt still original shut

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
