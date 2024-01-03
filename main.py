8d9209961a9963fad09ba95acb49116c0b3f1886
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:07:49 +0000

    spoil year hello although breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
