4840defbdc4c3e2f120adf4165bac56cd132c67d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:59 +0000

    ticket on seem possession yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
