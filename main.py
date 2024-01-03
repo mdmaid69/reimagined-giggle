53efdac6742ed64055a6a66998cdefff36f39fe8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:20:00 +0000

    immediately lonely free tip frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
