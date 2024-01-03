7d4408df32d48e6f4a28a96b0441592f2f5bb84a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:27:53 +0000

    stupid estimate neither suggestion provide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
