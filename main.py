4706782cce60bc9f17086f29b10db9b4550f22be
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:53:43 +0000

    fold special special cross in

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
