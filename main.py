fe214f3e69544d23ebe4c7a54f567068c37e83ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:32 +0000

    find easy about embarrassed disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
