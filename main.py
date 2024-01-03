5773b8227c787e0562334ca8486e8bc80a00c8e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:21:05 +0000

    pump onto switch free fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import sys
+def exit_program():
+        sys.exit()
