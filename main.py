6ddd0e643fe6f8d4d80e69ebc405fd3510965e4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:24 +0000

    material goback(to) high stamp suit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import re
+print(re.match("h.*o", "hello world"))
