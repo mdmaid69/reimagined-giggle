f9587a18631b9407e88c49f517f45fc3934c7d9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:16:13 +0000

    disturbing lean gap worry thumb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-import os
-print(os.getcwd())
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
