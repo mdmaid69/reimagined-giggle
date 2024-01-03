714ba0bf506fe1ee5edd4639c2add3b0c085825b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:19:43 +0000

    total charge run group fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def reverse_array(array):
+        array.reverse()
