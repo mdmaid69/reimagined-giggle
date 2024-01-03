569372557926bc970fc8174c2b9f1ba8e25c8d4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:08:08 +0000

    leave turnon contact near abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
