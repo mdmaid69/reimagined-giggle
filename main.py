a1fddbeb5dfe425aac12aca19b8ceec2437cdd37
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:55:08 +0000

    start fond change react could

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def find_max(lst):
+        return max(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
