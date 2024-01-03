3a2df8ad7471785b8bd4ef757f102415c391e9b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:12:58 +0000

    work them arm half try

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def convert_to_hex(n):
+        return hex(n)
