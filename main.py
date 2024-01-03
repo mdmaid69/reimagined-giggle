afd448242f97b39d4921ded3812c7d2c8ae443e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:25:50 +0000

    active callback discussion reaction piece

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
