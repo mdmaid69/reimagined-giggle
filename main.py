73f58991ff2343b9863b97d6fe640200ac773a9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:55:02 +0000

    century account principle suddenly ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(numbers):
-        return max(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
