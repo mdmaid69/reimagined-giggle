0735f103bb8520edc6ae1ac6009a8ee856eae1b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:26:08 +0000

    content exercise emphasize classroom holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
