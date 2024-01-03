50867c4f4426aafeb1044e4b2344d71dc3e9acdd
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:09:35 +0000

    reject some attempt crash trap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import time
-print(time.time())
+  def add_numbers(x, y):
+        return x + y
+def is_palindrome(s):
+        return s == s[::-1]
