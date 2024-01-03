e536efe8e78cb3c38d735be2e8fa75aa6bee9ed0
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:52:11 +0000

    wise defend marry could worth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
