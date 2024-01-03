2ec7ca1d1c18aab357717ad216d8c252db4234eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:26:12 +0000

    wherever cool breakdown complicated speed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(numbers):
-        return max(numbers)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
