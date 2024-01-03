6baefe11a3aaab587900fe01f94f0164d5661823
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:35:06 +0000

    plan demonstrate goon whistle Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
