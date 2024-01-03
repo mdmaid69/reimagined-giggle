2428d9ea47e886c55545b6a3173fed2c96ed1129
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:16:48 +0000

    half violent opposite our pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
