47f75a1778fd2015018263805a70b6be253ac8cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:23 +0000

    finger look speech brave passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_ceiling(x):
+        return math.ceil(x)
