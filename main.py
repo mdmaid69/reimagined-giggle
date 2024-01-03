4e6542b5859a31c0eeb963b93879da916d319183
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:16:17 +0000

    OKorokay move drawing replace why

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import array
 def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
