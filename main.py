1fc81f8df4ed34c178887164c443472981ea9257
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:01:56 +0000

    defend thirsty shopping visit separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import array
 def get_array_itemsize(array):
         return array.itemsize
+import array
+def get_array_as_bool(array):
+        return bool(array)
