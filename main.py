88d7e5d0d51cc9294f4084953e7560821ac4550b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:21:41 +0000

    quiet live pull computer pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
