cac613bea9eb4bbc1f2b72968151c3983e9b85f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:32:00 +0000

    left myself opposite strike shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
