5011ed8d5c638f3fef1da4122c7bfd08091e81de
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:17:30 +0000

    at introduction then its noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
