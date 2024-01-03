1d54bd82f6deb8c617b3454ca24fd17a2d3dedc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:14:33 +0000

    dick giveback over while cut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
