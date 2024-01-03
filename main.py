85a75c741ef1e5b4ed2010c10fe3f07e7c3581a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:21:59 +0000

    score keepoff snow moon change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
