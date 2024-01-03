226df42f780fe3725ba055a3fd5fe3babe994aa2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:53:53 +0000

    pin period right lunch as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
