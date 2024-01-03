619501ff025bc9a8961aac06a804092e00a788d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:08:27 +0000

    feed dare pure gap chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def convert_to_octal(n):
+        return oct(n)
