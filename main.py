6e766afadde660924f79c61b6d0a4ced1b817e19
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:00:49 +0000

    culture board company giveback stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_min(lst):
-        return min(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
