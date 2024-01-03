f80494aa60931346fa2f487c4a7a5cb4de98c7a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:09:14 +0000

    pronounce step undo now 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def subtract_numbers(x, y):
+        return x - y
