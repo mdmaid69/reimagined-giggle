73fdb359aed44e82b37099e9e4f93de2bb1da872
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:49:30 +0000

    rise door count belong shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
