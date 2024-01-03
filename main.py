42e0fd625179cfba2a556d9ee0112e19cac5654d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:05:48 +0000

    section uncertain noise reach cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
