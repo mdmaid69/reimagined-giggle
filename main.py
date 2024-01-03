8a567ccc21b96cf8d6513438a10915cf35ed1b3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:03:31 +0000

    fire last turnoff search grand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_time_since_epoch():
-        return time.time()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
