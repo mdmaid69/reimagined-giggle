e130658d8ebc2689c99247b4e741c01494aa85f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:16:15 +0000

    poor disturb Oo young keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
