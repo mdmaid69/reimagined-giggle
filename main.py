b5ece2f4836ec56c812ff306491e4b5bdeb8e6dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:30:44 +0000

    coffee thing makeinto interested both

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print(sum(range(10)))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
