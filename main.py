887378bde9b4f7bf817c4b227fa330fa8dfc931c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:03:40 +0000

    underneath description getback attention cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def convert_array_to_string(array):
+        return array.tostring()
