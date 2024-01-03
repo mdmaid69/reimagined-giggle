eb4c62e7fc2763a2b32821cab96cbaf3fc3b8873
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:10:42 +0000

    they tax Ll effect could

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
