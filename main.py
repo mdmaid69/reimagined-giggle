723ef7a2f17bb97575ae55c76a3d7fbb262748b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:57:18 +0000

    pocket music container match decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
