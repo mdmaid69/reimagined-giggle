31304faeb658540cd48534595f81f0fe122a4fbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:38:23 +0000

    clean weak normal later use

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
