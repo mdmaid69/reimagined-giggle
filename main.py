f0fa182fa129685c1e4863702b3830abd90d3385
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:33:29 +0000

    increase arrive change own offence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
