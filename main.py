cc3b09e75799a66e04ad512c6526c16535358100
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:46:35 +0000

    direct run shock compare responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
