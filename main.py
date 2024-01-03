68f5d489ce76e4b8381b0246fa1464b09636a76f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:20:25 +0000

    react equal emotion afterwards spread

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+print(sum(range(10)))
