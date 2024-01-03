cffd9b50b89ba81c4aae77a054f54cb4d48e5750
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:43:11 +0000

    roll invite development smooth discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def convert_to_hex(n):
+        return hex(n)
