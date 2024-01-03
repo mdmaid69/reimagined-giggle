4727f0fd1a7db27b5c75098df34eca946b40416b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:47:00 +0000

    soft insist everywhere stranger strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
