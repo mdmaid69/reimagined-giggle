1ecf39fc72772f5ba91697310fe7725f2e0929f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:27:53 +0000

    mass beyond contrast I promise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
