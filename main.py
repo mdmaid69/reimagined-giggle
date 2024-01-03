8fc4433fbc8def475ffe57ca78418f9c749f220b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:22:24 +0000

    bored sleep national hard anyone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
