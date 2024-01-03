69bab811574047905567393f4904969ab518f233
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:28:06 +0000

    correction equal surprised dark top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import numpy as np
+print(np.array([1, 2, 3]))
