e1a22efb31c5bc5ed10f60a2590b4df3056ce576
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:58:31 +0000

    less content mixture travel depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
