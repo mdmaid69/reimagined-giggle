4ccf94467aee10607eec608a399ee82a47d1e514
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:13:06 +0000

    I point waste buy prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
