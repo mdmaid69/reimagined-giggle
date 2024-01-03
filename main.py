b08a571045f05c8d4054c8613ba050770057b60a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:03:55 +0000

    jump scheme bound hospital dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
