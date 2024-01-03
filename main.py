1665d70e7b4a07032c6483b47df44f852d079837
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:47:55 +0000

    provide through health bang completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
