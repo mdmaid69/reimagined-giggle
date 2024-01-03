dff35ee588d62add352efe61c84f311d3119c656
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:51:10 +0000

    car fierce bite string know

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
