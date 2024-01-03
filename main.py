24b370b043067a0e685ac3b586f8fcba34125455
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:44:36 +0000

    their race object war offer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
