28bad6ee5efbf5a35c5d617aea2d52ac1f6dfc9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:25:24 +0000

    cross often nice more strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-print(random.randint(0, 100))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
