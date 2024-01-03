ee8937e31a11b7e0e17d4a42d3f2f4b9f972c841
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:44:34 +0000

    concentrate promise idea normal sweet

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
