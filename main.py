09b7462b5776d14bbfa686ca3dead89f0c2570cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:58:28 +0000

    fast record kind report size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_work(force, distance):
+        return force * distance
