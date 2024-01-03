f6caf8b864bf4ee136669cb1e85a4b4d08077438
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:44:16 +0000

    will film level cheque ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
