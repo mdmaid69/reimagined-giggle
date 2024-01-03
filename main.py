45edec8fe43575da329f74c039cd9185bd696237
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:04:52 +0000

    available 565end necessary happen post

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_item(array, i):
-        return array[i]
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
