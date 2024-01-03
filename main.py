c872fb00288332ccc11db04314c0aa84b057e2e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:04:32 +0000

    favourite beneath below break genuine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
