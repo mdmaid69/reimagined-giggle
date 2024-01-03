e007a5ad560a9b2a9fbcd73a1c2b5b7580d45af3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:29:50 +0000

    workout and determined force thanks

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def count_elements(lst):
+        return len(lst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
