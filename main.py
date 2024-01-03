1558dd685854a891940d68f635003fcac5a306ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:16:15 +0000

    behind ring practise file create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
