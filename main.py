32645a0b703efe239475d42c10bb8a753ec5a816
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:54:02 +0000

    but rush turninto grand talk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
