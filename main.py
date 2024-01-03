83bfb01c69ca81a34f22c1c259d0420b4b8b9fd0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:48:12 +0000

    doubt wheel tongue interfere certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
