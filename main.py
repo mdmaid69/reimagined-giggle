ecd5b563488b89fa0c6d7adc858253c865f91ede
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:22:30 +0000

    gap mystery free regional unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
