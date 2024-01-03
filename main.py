37abbf368a96fb8323325a098c5a8571f039b0a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:12:16 +0000

    hot complete mind false file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
