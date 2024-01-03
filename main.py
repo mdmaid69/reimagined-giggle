d3d2460571052bd4bc09ab0471ea102f3d11b335
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:27 +0000

    office backwards spare combine bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
