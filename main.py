8bb0f0359fd352b6b550a3298d6cfae1ea1e213f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:51:34 +0000

    length willing shape sink swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
