8d1191594d0dd4fa4cb58c58379ff1f19f6ae41c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:14:45 +0000

    beneath guilty famous long city

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import datetime
+def get_current_date():
+        return datetime.date.today()
