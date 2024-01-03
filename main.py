427752b9fad0833cb8c771ea0bd21596ea1d3eea
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:23:20 +0000

    tall equal price record love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
