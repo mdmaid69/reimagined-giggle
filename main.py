ad7e9090b48f26f281d5eb41e890735f1da3f49c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:20:23 +0000

    right switch used follow develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def add_numbers(x, y):
+        return x + y
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
