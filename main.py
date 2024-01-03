3b7fef46a00016ce8694cb679d554ea1de4ff7ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:19:31 +0000

    impression like short taste bringback

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
