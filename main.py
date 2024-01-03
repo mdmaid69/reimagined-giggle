3d7e00c4e08e21829efa6f168b35967e2243780c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:43:42 +0000

    certainly careless best stiff control

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
