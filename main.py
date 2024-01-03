2e6ad844ca509cc261a5b08c0a147c298b8dacba
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:21:24 +0000

    getup train whichever interview group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
