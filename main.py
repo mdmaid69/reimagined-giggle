900e91234fc2160acdcb3c24d4130603a6c67684
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:49:17 +0000

    mass real tidy actually yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
