94e53e1e9132006ecbc4e9178d26660ed3b50e35
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:10:39 +0000

    rain wind week mine record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
