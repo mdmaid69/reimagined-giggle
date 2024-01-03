bf459a5560035194279f302b1aee7440430ceadc
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:18:41 +0000

    dinner turnaway low choice within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
