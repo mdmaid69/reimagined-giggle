08c97881685a6ad07ab0effe78c6d2baff740e17
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:02:22 +0000

    trade see rain paper teach

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_stack():
+        return collections.deque()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
