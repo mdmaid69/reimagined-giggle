edc7d9f9fd13863fe31fe9cc9f5bdf271de30a0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:49:26 +0000

    notice score film wander to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
