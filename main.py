d937d00ed2cdffecf78ad4c9810337e26e38dc3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:24:15 +0000

    reply strict mix towards lean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
