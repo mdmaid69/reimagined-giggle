7f6688423cab28c86711cc426a69e9bec2d67a5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:52 +0000

    relative lift beside child cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_list(array):
+        return list(array)
