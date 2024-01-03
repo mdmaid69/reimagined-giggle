20edc7ffd90278ad93e375922124b6c696d7304a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:57:26 +0000

    571enough free relief rob into

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(numbers):
-        return max(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
