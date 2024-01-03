f24bb1d8a9190e274bd067fbc5c4183a2275d087
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:01:58 +0000

    read advice warn thank murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
