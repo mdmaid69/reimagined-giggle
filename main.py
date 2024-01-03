6894163de18087fc0aa9d86cbc24e17cef3c8a1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:21:27 +0000

    country through content thin behaviour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_area_rectangle(l, w):
+        return l * w
