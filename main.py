3fde3abdb175683236ed091f69011ec980f12092
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:14:28 +0000

    term nowhere bit cruel careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
