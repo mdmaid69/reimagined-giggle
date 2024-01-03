9d163ac7c93e9ebcd92a73f5af9b9560fe20ba6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:51:02 +0000

    reject famous defeat thorough squash

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_item(array, i):
+        return array[i]
