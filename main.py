c36feb74c97038964788f94199c8646fbf3acc7f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:43:17 +0000

    whole confusing walk minute crowded

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
