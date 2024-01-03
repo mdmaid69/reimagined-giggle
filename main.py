6eb16e1519e8212506abee54f52a4bf685894c45
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:06:50 +0000

    organize meal clothes minor build

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_queue():
+        return collections.deque()
