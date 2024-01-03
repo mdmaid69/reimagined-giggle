b744c6baab500fab4ce9d198331edf9a528ae730
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:11:14 +0000

    hole proud estimate hear confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
