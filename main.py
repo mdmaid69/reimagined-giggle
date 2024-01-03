4f358b3391a4e096765f2fdccb22dcb299806e59
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:19 +0000

    phone exchange float proud start

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
