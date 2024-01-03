a4b1d7f64b9fa56872863fde692e97db15ff4e90
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:43:12 +0000

    way turnout sky avoid past

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
