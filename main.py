ea8935067d4d769edab0caea8aa66e89f6ef2a01
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:47:11 +0000

    hope profit bother alive translate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  def subtract_numbers(x, y):
-        return x - y
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
