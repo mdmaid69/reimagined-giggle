502a04fb63ec23292f3523f37366cb6c3189ae09
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:30:28 +0000

    field report year anxious shock

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+print(math.pi)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
