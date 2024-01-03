31eac6ae1176b9e3a0fce22bb6721aa213facf60
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:52:45 +0000

    loan side patience only open

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
