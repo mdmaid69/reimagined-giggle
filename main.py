e7c62c9ea355e114f184402a5d7cd99a7974e7f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:49:24 +0000

    improve task pure contrast lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
