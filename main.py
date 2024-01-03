a4d6d3370aa32d63052680f27d1b4e88fe0f82fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:30:42 +0000

    thankyou interested measure move guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
