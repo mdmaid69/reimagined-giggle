ce9b101f999b85c560ce01275b84e10b45c2c887
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:42:30 +0000

    corner fond along worth nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
