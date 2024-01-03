2078ca97b21f9450b7002b46b5d4c700a10c14b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:45:12 +0000

    pay after off home praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
