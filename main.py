66aced1a81242fc8bef96ecbd9bbb0c3d94d4d40
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:12:35 +0000

    class sleep 570enjoyable us again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
