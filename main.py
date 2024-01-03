bae71170ce9aed76bb4ff50cda719977e4d634be
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:44:49 +0000

    ahead refer brave length punish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+print([x**2 for x in range(10)])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
