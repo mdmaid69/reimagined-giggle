854e4e125d7b423a2c3b6bfd4b4ce2a53d3ce822
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:55:33 +0000

    pay object offer train depend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_float(array):
+        return float(array[0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
