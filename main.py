8cc14d0051db995fde86dc0685a4b7dc9ba6aacd
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:30 +0000

    birthday instruction exaggerate ill nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def count_elements(lst):
+        return len(lst)
