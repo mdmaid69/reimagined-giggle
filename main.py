13ab4572330bf34a71a60f671bd8c5ee7a8d46e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:23:02 +0000

    century insist row accept genuine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
