da08593d2f32573f868b7a40645e677c74c2e8f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:59:49 +0000

    land draw fill lead wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
