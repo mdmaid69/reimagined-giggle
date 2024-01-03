2a9a628e5c88ed25a2a8615dc63111624decc7f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:01:22 +0000

    ashamed lesson factory get owe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import datetime
+print(datetime.datetime.now())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
