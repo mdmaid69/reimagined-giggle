0c0f61d6b3bacadcfc81834ac8f4c802dd5c2322
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:16:45 +0000

    dance coat rush level cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
