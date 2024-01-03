c0c0b25eb1db47ba3c0fed354d74fd5ba9285ff7
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:44:53 +0000

    one problem push in couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import collections
+def create_user_list():
+        return collections.UserList()
+  def convert_to_hex(n):
+        return hex(n)
