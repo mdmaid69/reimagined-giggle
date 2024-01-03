5080f4f841eb77a2e002e4923b4f9c7f5a498328
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:08:25 +0000

    Ff improve onto compete rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
