8dea9c5f757bc30a400d07562bd8a5ace1bcad24
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:19:35 +0000

    forward afternoon terrible study cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
