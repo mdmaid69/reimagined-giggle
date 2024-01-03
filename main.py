ff7c880df70ccf24bd93b05a73f81fb707cc3a84
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:12:06 +0000

    roof keepaway present factory place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
