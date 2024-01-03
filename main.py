20681dd9af38753c9dfa77afdd1e68b9f04c6ba5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:30:03 +0000

    damage copy tense content yourself

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def is_odd(n):
-        return n % 2 != 0
-def find_max(lst):
-        return max(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
