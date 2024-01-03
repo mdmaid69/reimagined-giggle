9c75656ff8a6e04f107416381b74512ea7d0154a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:00:27 +0000

    last admit around pay aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_list(array):
+        return list(array)
