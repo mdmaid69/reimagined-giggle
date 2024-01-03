886bfb942e37586645f0ab1997326cc20e23f16f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:18:32 +0000

    programme freedom improve bunch attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
