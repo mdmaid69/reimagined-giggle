330d839546f7e93a13e393c285b52c5c2c063871
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:12:11 +0000

    arm long disturb detail powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
