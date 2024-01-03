5a64b51dc804ce7f9e450139759aca0fcb8a765d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:28:44 +0000

    lost mind both yes world

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-print(math.pi)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+text = "Hello, world!"
+print("Words:", len(text.split()))
