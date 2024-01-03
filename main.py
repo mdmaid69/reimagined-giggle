d46275af49885641570050c2215acf67b847ac04
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:40:37 +0000

    smart so correct main Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
