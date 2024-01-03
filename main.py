69c18c19d66ff48212e3954692e2236fc6444dee
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:02:27 +0000

    ask people tooth flat fortunately

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_even(n):
-        return n % 2 == 0
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
