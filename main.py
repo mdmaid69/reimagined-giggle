490be6b893d01f6f5f20f4a417ca6ac4f9753012
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:10:26 +0000

    defeat bunch to method visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
