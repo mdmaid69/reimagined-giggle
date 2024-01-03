2fd1b827f8668326a20365b398108b7a468b94a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:11:15 +0000

    history Kk pleasant grateful giveback

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def subtract_numbers(x, y):
-        return x - y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
