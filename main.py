ca42c1bc927e42144228b51cc8900f47b908212d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:20:39 +0000

    freeze welcome limit surface angry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
