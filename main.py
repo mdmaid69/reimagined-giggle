c7d94f5e94beb4aed4bf625ed7f2cf6b76dd60e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:24:13 +0000

    dig afterwards on debt disappear

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
