b86d9588fe0fe3495b1dc69e93dfe381662c2579
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:23:17 +0000

    black rush mistake pipe secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
