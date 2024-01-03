fabb398b2df28b7a226de43b8cfaf65ac55902de
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:05:44 +0000

    crowded afford basis ever rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_float(array):
+        return float(array[0])
