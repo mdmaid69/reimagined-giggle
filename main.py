60011cf9a3196a18056c130b053fd4d2d1eb51b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:38:36 +0000

    contrast doubt balance connected proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
