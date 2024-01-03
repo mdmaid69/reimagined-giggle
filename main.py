87fb10fd7b80887b846c1ed3ddb437181ad15114
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:37:04 +0000

    recent fun scene defend runout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
