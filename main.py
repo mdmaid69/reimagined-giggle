6e110cc493f0198f6e7fc67a9997266ad1290347
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:52:58 +0000

    ability place mend swim nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
