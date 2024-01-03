7b4f71c4b74a4d51de0d1a1378f4cb096f5396b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:17:52 +0000

    stiff dry handle part stiff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
