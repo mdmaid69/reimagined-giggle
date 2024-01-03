a6f14c6fb1fdf1a3bcfdcc77d8e7b15a11799f50
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:09:10 +0000

    know visit sometimes emotion concerned

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
