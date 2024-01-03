710bc3108964007ff4bc6e4b02d0983df66dc979
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:12:34 +0000

    congratulate wild spite comment state

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
