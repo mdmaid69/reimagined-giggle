6563fd077b1d2af60cd07156a2c6fa6ce40310da
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:35:25 +0000

    disgusting classroom valuable hair slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import sys
+def exit_program():
+        sys.exit()
