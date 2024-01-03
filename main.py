990b5f439b06cfcf400bd6b8e680440574ee3ffb
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:41:20 +0000

    bringback swimming count boring beyond

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
