9f5c1744cb81a96c2355245bb0440b70f60c6022
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:30:53 +0000

    tax failure their goodbye holdup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
