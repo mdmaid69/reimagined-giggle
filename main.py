d9c7fefebd5ace6c210da3ddb362a416ebf0a2f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:26:48 +0000

    ordinary attention by fail invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
