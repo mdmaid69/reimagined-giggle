4f201b7702ae76b1d287a4272ea7970bfd4c3aff
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:43:02 +0000

    571enough sweep measure shelter crazy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def is_even(n):
-        return n % 2 == 0
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
