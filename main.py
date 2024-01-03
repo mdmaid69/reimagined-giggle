3c2a68439c2fb1dddfa743a3358ba8d0d8ee9d7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:13:19 +0000

    equipment turn couple beautiful miss

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
