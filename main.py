5a8093d91659be5b4877fe0d0fc14807e4a42b97
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:07:00 +0000

    aware break room ring government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
