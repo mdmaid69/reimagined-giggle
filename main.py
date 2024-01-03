ee2e0feaf96037478b1491d5ed139c4cc01971a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:40 +0000

    unable bend takeback freedom cheek

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
