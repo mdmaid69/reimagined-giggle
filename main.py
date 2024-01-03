6486a857a7e933affb0f15cd92f1e6bcbc128ecd
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:33:53 +0000

    face telephone company tour feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def convert_to_hex(n):
-        return hex(n)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_area(radius):
+        return 3.14 * radius * radius
