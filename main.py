1fb73d994201507dabed50f9b44988af20dbbf28
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:12:52 +0000

    priority their hope ordinary for

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
