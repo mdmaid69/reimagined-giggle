ad48187d356285b75f4bf4b3ac0f435976ac7c80
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:46:52 +0000

    money basis loud floor like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
