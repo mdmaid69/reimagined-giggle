10e74cb865996434383c4cf78744232b758bc461
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:06:40 +0000

    arrival stiff no estimate paper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
