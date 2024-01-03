19b0947193869454a573bc9b182681a6267c8e80
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:41:26 +0000

    favour attract bank proof clue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
