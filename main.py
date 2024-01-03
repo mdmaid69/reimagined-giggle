2322d4930e234281c9cc4c86d925e4766b8e3022
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:22:36 +0000

    his home match strength loose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_area_rectangle(l, w):
+        return l * w
+def convert_to_octal(n):
+        return oct(n)
