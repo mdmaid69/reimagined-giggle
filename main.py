02bf7026b167a3b0a99bbfdf53a7cc7bd46f21c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:26:26 +0000

    address attempt Zz to schedule

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
