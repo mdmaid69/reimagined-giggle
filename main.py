8f75c6f8848012586e4c7c1197bb75237e865305
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:38:37 +0000

    strict square description goon rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
