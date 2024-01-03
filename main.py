ff0f340c80be0b9e73b3e78b88be480f20d55419
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:34:07 +0000

    slip goaway forever original have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def is_even(n):
-        return n % 2 == 0
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
