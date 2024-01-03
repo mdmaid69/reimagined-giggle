042971e5e171a78a212ea5c716bd39ddc07fbb98
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:52:38 +0000

    contents gather pick trip sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
