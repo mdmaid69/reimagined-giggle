0c85baf0f97891adb48dcfb855613b7eae6c8609
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:25:21 +0000

    basis town copy seriously maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_current_time():
-        return time.time()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
