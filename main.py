3cd3596fa01d6c7ce82be024fc999ee740bcd43b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:00:24 +0000

    practice give pair relation insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
