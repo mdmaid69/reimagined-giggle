8f31510b5f83c960bbf8490e99fb42a3a0562c28
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:30:05 +0000

    your dear language scared active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
