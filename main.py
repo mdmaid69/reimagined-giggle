e2c89136fc2c82c3f619689ebe67df1334e012f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:47:17 +0000

    ride manage pretty expression always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
