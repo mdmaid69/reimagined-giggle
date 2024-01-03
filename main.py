a826d69a59b7e44a61fcb387268578d6460e02dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:47:11 +0000

    pair second Zz goaway familiar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
