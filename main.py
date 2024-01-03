d1d3e45d17e908b8b0b9f88e030db0aac26f7df7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:51:14 +0000

    study any perform fear bored

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
