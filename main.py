8332a44591cd5c51e7f2d1c32895391daa73eb85
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:33:51 +0000

    warn clear strange rough holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
