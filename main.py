2701748c7e4b4112ab8939083c53cd375c1727e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:20:40 +0000

    beneath although down 567enemy indeed

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-print("Hello, world!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_list(lst):
+        return sorted(lst)
