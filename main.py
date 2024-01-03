111ec7ae5440dc53a9d8d5ad9f7f208d6749b5be
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:14:11 +0000

    pencil earth dream image turn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
