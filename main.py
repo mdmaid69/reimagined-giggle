bc035a9f121b3473e2f8fc9a12f5b9146c9b9fbb
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:07:10 +0000

    cheat scale jump often bear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def subtract_numbers(x, y):
+        return x - y
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
