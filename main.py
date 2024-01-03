90fdbec3912df555f93c1e7b3d19146c0c0648af
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:30:46 +0000

    first valuable meet split exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def count_elements(lst):
-        return len(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
