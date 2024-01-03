b5f5d3aa6d08948dea939499244d091ecb401ff3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:03:45 +0000

    this workupto getaway space patience

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+print(re.match("h.*o", "hello world"))
