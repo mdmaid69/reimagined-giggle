17a203e12af77c308b963eb45a8662e69393ea34
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:49:32 +0000

    Pp last soak do good

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
