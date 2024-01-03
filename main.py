6f468d4a0fb9a903f685fcbd2ea5d634e71d94c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:36:14 +0000

    own content afternoon fairly match

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
