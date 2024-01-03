3bf5c79116a62257a95134c171ec0c0cd94954bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:13:07 +0000

    pollution wash case offend pressure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def find_max(numbers):
-        return max(numbers)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
