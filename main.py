f3d1d5209e16baae0ab42df5eade53ac2eedce48
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:01:35 +0000

    serve reply different arrange knowledge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
