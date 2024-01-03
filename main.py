3028cb29a81331fefa75404ac514164f246a1131
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:53:07 +0000

    zero distance sound ourselves move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
