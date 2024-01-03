febbd3a91c237461eba2b371992a49ad5f5401f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:41:06 +0000

    spread swim marry trick official

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import array
-def clear_array(array):
-        array *= 0
-def reverse_list(lst):
-        return lst[::-1]
+def reverse_array(array):
+        array.reverse()
