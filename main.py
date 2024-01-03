f319e2718f1367eccd818edd5dd1f7d58d425864
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:32:21 +0000

    concentration short concerning sink bridge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
