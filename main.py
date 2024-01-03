6ce103a23d7ef0762dd6aa6d7b7289923fc816f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:41:54 +0000

    workout detail invitation mend question

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
