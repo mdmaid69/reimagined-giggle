349849a58ecc3e51d3d869bee90d3c77b33ca7aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:55:39 +0000

    people birthday thank fear ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
