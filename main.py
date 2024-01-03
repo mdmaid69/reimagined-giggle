683d30ed8d18dce65de9f6461cf738d95bdb0958
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:14:53 +0000

    no single mistake muscle obvious

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
