dfdae37168db1bdf3fdc35423532e9f4f7463e12
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:35:54 +0000

    firm pen busy load destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
