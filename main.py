a8ac24e36d33040600096bd918b41407fc529349
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:00:59 +0000

    pipe study glad pain beneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(work, time):
+        return work / time
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
