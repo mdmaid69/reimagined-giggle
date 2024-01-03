ae91d80c7d81e57200f02bc1fa6b01a9e9eeda2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:43:39 +0000

    friendly Ww wide wide shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
