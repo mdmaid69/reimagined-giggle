47b4378e283831f930642c6d77fae4dc89ad4e2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:18:26 +0000

    other mouth put above awake

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
