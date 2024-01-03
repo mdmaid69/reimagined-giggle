fc13c0e451e00296527b9bef235c37fd41babfda
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:40:13 +0000

    importance close fetch consult land

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
