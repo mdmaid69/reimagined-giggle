6c6fbbcaeae47d37dcc6479655dd0b6b7d9b7a29
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:40:37 +0000

    dance tape division interest fight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print(sum(range(10)))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
