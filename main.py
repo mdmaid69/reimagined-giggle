e0a86f5f2b2533f7d2d9bd99cdc784bc735cee9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:19:57 +0000

    shall pretend permission listen date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_str(array):
+        return str(array)
