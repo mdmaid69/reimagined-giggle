6a1fb33e270fa081b211941b4ca756dae3c01e87
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:05:44 +0000

    wish hot spoil gentle dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
