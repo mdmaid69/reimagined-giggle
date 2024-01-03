f0cfde6465bd399c0b9fe8c026beb9984cd48b9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:13:49 +0000

    run cry strike hate someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
