366e1f0a4040361ecf0c30866444810b036fabf8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:54:52 +0000

    mistake workupto beginning sky see

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-print(datetime.datetime.now())
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_acceleration(speed, time):
+        return speed / time
