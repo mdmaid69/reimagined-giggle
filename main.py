28224c51c4b6f634b5e9e72a45f19bef9942d64f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:57:06 +0000

    manage solve godown word hurt

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def add_numbers(x, y):
-        return x + y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
