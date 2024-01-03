8c9ef016bc06ceb8a33d7b211f4153454d1175df
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:23 +0000

    split face face uncertain smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_item(array, i):
+        return array[i]
