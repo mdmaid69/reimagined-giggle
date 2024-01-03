c6f7d3537ee90d3f913d00d17b117c4ac0ce1909
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:37:16 +0000

    565end sea during each there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
