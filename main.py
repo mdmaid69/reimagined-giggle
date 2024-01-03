03e4c288a35d86e78af9a901d8d1249ce8e5a442
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:20:02 +0000

    disgusting drawing bank right action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
