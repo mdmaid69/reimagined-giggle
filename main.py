28519bf45ce3ce3bda0a3487fe3c3d09872724dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:38:05 +0000

    impressive them feed achievement interesting

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
