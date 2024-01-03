142a28080c9da32d2ad9780c847b7123004fd459
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:06:16 +0000

    call send consider loose send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
