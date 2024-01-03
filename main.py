f3211d6591a05aa0a5a59d199f66ede13f91f016
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:03 +0000

    play translate note involve none

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+for i in range(10): print(i)
