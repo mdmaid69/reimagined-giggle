495b292b8b19cf0c3ff8b6cf3e8d98a313217606
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:33:36 +0000

    underneath in workupto drop mother

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
