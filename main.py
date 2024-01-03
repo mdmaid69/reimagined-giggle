0485ffefa3137ae520d7e805532934bc6feb98bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:36:13 +0000

    surface sign shrink tip describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_pressure(force, area):
+        return force / area
