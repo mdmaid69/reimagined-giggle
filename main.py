77a90426038803097eb9d546ef4897af273292eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:56:58 +0000

    night hang plan vanish anything

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def convert_to_octal(n):
+        return oct(n)
