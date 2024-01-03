1cef6c9ceea0355e1d36b415c6835117860e57bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:50:14 +0000

    well cut after debt environment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import time
+def get_current_time():
+        return time.ctime()
+import datetime
+def get_current_date():
+        return datetime.date.today()
