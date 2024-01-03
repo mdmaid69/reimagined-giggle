bad63516e579f73c205e2e2e6a1f5a9beeba624a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:42:32 +0000

    pride settle shade public agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def cube_number(x):
+        return x**3
