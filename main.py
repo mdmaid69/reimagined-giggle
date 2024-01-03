088fa7dc653042604f7124b2e6ec99ee18f84ec0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:35:49 +0000

    one arrive off past surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
