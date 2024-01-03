c52a2fb999cfdfac7b2070f20373aee5481b2107
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:09:18 +0000

    Ff drop shout basic children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_string_from_array(array):
+        return array.tobytes()
