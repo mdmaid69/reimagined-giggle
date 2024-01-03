076c65c7b5c2f119544718c18a2dcdb95dc4bdb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:56:44 +0000

    distant phone inquire climb drink

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
