466a092c8086595f9122c512098d53a15580a38b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:52:34 +0000

    lead holdon since influence promise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-def subtract_numbers(x, y):
-        return x - y
+import os
+print(os.getcwd())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
