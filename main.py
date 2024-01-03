bc80e99199e84caf51e81faad4b7a26935753d21
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:37:37 +0000

    clear blow phone house company

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
