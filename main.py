e9149d73c57fa8acb9cbfd0069894375bce90382
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:23:07 +0000

    moon patience mail operate steep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def is_odd(n):
+        return n % 2 != 0
+def calculate_density(mass, volume):
+        return mass / volume
