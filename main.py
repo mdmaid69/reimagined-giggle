933928923517a743c05e77480f6ba76eceefb65c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:38:31 +0000

    air purpose sorry disappointed favourite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def calculate_distance(x1, y1, x2, y2):
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
