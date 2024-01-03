a5913d727f92cc94de8015ab92721d39c96c0e1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:28 +0000

    justice grow first trust behind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
