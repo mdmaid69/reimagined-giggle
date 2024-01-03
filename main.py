5a063e49197df816ce211ddef883d516a2c35036
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:33:43 +0000

    repair after feature sweet early

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
