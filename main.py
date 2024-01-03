9223253d435c4416fcb01184613ef786a1a72729
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:43:12 +0000

    party clear flight of along

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_int(array):
+        return int(array[0])
