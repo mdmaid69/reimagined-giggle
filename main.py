c6515da2f1a11543522fa45dbbf26d5f32c683de
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:13:38 +0000

    breakin clever escape lower bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
