04d379e524104653333fb6a9b6a9a267f6a6e994
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:33:46 +0000

    shake cap ignore experiment rather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
