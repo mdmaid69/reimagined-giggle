900b34e81ff47e2b2ce57c84bbceb0f481dd7c5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:55:11 +0000

    price satisfied clue gas closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
