ee303f2758e5325a7df042c2941c52ea3280d527
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:27:53 +0000

    evidence hold depend crisis arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-print(time.time())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import platform
+def get_os_info():
+        return platform.uname()
