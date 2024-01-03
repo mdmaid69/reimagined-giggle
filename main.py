4836fe23a351e89781a5fdabdb8a05fe14a623c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:36 +0000

    go simple Jj available bringup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
