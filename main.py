f0da012d9ff2ef1acbcc29408fb838ca872c7086
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:01:42 +0000

    introduction evening lay lucky platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def clear_array(array):
+        array *= 0
