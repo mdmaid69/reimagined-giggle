657dad35186c9821df359fa0055bc42eef7b7dd9
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:48:08 +0000

    bother fill notice turn firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
