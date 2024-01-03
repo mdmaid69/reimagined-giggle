89ff2a21bde16d2077cbe656046e185df9229192
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:34:07 +0000

    habit dare doctor quit proper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
