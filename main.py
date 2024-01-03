7e606eb795af86862be42df0de33d6ee41d0dfab
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:32:14 +0000

    above discuss beside report runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
