f3a7a4686822edded5e0144c32c561e16751ea37
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:26:59 +0000

    Dd force black sink active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
