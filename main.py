ac67df79e023b87323a2490a40024506b8b81f6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:51:53 +0000

    test fat come drink determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
