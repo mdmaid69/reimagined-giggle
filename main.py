bc042afd4a0d973094e57fdc6f5969693e3b0c13
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:35:48 +0000

    clear admire steer closed proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_power(work, time):
+        return work / time
