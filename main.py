4a532f598a020446c2b525a4e481082dace2cfed
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:38:17 +0000

    introduce pretty combination tear regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
