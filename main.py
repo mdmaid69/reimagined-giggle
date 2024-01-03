24629468eb1e15d48ec040c53b456e98bcbf0d58
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:02:03 +0000

    awake fall dead you us

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
