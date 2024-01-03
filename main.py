6b9161640e00a4af1a4897edd5c0fe5224f468f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:10:48 +0000

    rely live science instead win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
