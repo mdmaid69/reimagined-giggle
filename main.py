b6c498055e2176998bbbac2cc733debafacdfc47
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:36:48 +0000

    mean keepaway building collection claim

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
