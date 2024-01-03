1e1ccd3a84970ea06968bfe6dc1c7480043e0273
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:13:28 +0000

    luck leave dead table effect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
