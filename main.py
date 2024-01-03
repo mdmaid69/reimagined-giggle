ffec769f0ff919557082ee7ef6cbb9f302df9275
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:48:03 +0000

    under relative keepoff promise moral

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
