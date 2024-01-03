d49dca135c0209c322b4e53c8619f97b9756525e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:52:19 +0000

    future small experience difficulty against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
