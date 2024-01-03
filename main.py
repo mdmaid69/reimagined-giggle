4438fbffe3dd2ae3242d9a6bfa29655492147b64
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:23:42 +0000

    contribute always evidence consider steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
