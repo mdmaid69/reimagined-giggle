7c28f72b5c18f973b3f94085bbe75b160a5929a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:01:31 +0000

    delicate luck escape interesting fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
