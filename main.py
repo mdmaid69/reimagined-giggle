28be2853d1fd2bcb1244b05a9614ab7665dd3e99
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:12:52 +0000

    not on make Uu television

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
