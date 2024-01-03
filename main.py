811fee0ecf56849d5248d194af46383373b52c9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:24:08 +0000

    entry rush deserve split adjective

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_current_directory():
-        return os.getcwd()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
