5b301b7fc7309769f6cb513f01a84babfb637c38
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:47:31 +0000

    post beat sign precise speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def add_numbers(a, b):
-        return a + b
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
