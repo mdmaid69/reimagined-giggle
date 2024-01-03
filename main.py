43e6d9eb54b8cd3557a9535da432274515e81f89
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:07:31 +0000

    half advanced mean half if

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
