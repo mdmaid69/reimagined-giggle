ba466d4b0b334236f0d11a5f1d5feb2f9a53a59e
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:37:35 +0000

    appearance this roll smell worse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def multiply_numbers(x, y):
+        return x * y
