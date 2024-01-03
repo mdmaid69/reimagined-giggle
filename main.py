8bd4bebcded281e2591c65f17df75219091e9dc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:26 +0000

    view scare hardly work worried

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
-print(math.pi)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import time
+print(time.time())
