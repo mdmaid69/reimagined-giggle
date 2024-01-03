141a46d7d29fbba59878a5a88c91ddd0dd60dd82
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:28:35 +0000

    act present convenient crowded push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
