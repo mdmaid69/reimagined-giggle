1ee791c7ceaf642266c552a9cb4c35797eed3158
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:23:21 +0000

    total scrape break them walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
