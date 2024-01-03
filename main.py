7e6fbefa295cd130c4058ed5084a2180fcea9ad3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:46:24 +0000

    hold Hh group contact might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
