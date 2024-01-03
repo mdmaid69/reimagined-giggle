c476d842f5d6bf92b24cdce840be678edca7ba6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:46:36 +0000

    attention patient usual arrival closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
