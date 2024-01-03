58360144c86922e4fc2b56374361bc20e66ed399
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:36 +0000

    basis opposite him discussion past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
