0bd360105f2f7fc41ee94d9093c63b6df217972f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:36:14 +0000

    trousers act heavy leave realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
