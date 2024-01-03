f3b0511f40429b20f269f239c2f5670a444660df
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:03:16 +0000

    listenfor autumn warning practice wander

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print([x**2 for x in range(10)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_circle(r):
+        return 3.14 * r**2
