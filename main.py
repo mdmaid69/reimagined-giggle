59dbff46a94ce082cad75b36863bce48c20c7160
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:42:44 +0000

    571enough sick capital safe public

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
