cb69c1621eda0d11196916d80c847754de75f0c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:05:57 +0000

    favourite marry raise throw street

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def is_odd(n):
+        return n % 2 != 0
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
