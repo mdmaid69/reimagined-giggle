d1e0523b048c67b2f3c4190e2d7033dea175d3ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:01:51 +0000

    careless this runover beat result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_current_directory():
+        return os.getcwd()
