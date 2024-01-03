89d3511d6cef2077a12befe30e043de81bc14363
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:14 +0000

    frighten relax Yy pour winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
