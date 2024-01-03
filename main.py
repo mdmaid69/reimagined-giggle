575fe190c7cc125a1028d0112253d0389b8cf4a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:31:32 +0000

    fasten nor subject shallow regard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
