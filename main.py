014747fa8139718c9ba4cff8b2f8e96355bc85cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:22:48 +0000

    score somehow unfortunate possibly grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def add_numbers(x, y):
+        return x + y
