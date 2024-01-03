f30858851a718003f4f2b4e9c92dfa125c0a32c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:40:28 +0000

    fly have doubt season lot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
