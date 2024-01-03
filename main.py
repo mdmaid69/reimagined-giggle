91ee3a58a59d389313663ffd2777bacd4cbc0f01
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:42:03 +0000

    right they man awake attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
