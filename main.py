be8acc1e7af1e4041c58ba50304c57e5da26be3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:54:07 +0000

    both physical wear concerning minimum

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def divide_numbers(x, y):
+        return x / y
