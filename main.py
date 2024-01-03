c199d1a1ffc92f5615498b9282a8798d4021f25c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:18 +0000

    temper brief aim report smell

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
