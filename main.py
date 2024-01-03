be98d317bc1a1b02414394099bb3cbaac2b7da54
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:38:50 +0000

    uniform murder rain high 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import collections
 def create_chain_map(*maps):
         return collections.ChainMap(*maps)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
