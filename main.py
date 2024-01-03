f5862061007ad8772156eab985959711644eb924
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:41:48 +0000

    mind beautiful dress mark bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
