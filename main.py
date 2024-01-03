fed9b9ee00b37723a126b71ebab9c14c12ef0cdd
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:09:33 +0000

    knock sit professional waste label

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import time
+print(time.time())
