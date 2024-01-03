0f15115bdb5688967e5451236164c2f4648cd4c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:40:25 +0000

    suffer confuse be official joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
