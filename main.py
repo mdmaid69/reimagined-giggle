e8fdebd85c61c3b4734b119da81b6774f1bb1a25
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:13:18 +0000

    confuse air neat taste nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bool(array):
+        return bool(array)
