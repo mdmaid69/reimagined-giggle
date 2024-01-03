54b21aff2ac72a40cb50097d6f4ba7c6aa090a05
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:05:19 +0000

    since spring gradually match tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
