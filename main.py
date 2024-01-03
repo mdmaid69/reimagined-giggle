bbe62b52cd21112a42e6f7e7e44af6ccadf39aa7
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:33:29 +0000

    wide talkoutof left failure mainly

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
