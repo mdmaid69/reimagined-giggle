1b8b9851cb5fd893c5f1845049567cc6bd412b12
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:09:09 +0000

    extraordinary that mainly generally coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
