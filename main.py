96f6ea7db97a623c900202dee119ae316ee5dc48
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:03:16 +0000

    put impatient we check seriously

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def sort_list(lst):
+        return sorted(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
