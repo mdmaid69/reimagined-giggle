17d549ca676416f781594195d62c383cceec8876
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:17:07 +0000

    position develop butter yet big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
