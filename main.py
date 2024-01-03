f697fea94029e21d8e66bc2813a9fd70f61c8323
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:50:05 +0000

    far past plan whistle flow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import os
-print(os.getcwd())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
