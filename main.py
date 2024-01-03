5178dae785b20bb94dc86fdb10b0885951ffa4d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:56:28 +0000

    school Ww fortunate steer able

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
