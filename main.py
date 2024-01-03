760bccf449eb6547bfcd54de816e8ec89fb26db7
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:21:12 +0000

    shock offend effort off line

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
