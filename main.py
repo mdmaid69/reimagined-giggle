ef7ac0ed519d6f991ec361f5e1fdaff977c2580d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:45:10 +0000

    Jj dream address nurse total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def split_path(path):
+        return os.path.split(path)
+import collections
+def create_queue():
+        return collections.deque()
