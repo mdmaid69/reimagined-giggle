6ca534051e0d48869b684dd2bd16edddbb876249
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:52:09 +0000

    sore drink listen safety anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
