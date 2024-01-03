8027544cc956dea368b080bbf53b32bd3a09819a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:26:10 +0000

    some run exit operation splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
