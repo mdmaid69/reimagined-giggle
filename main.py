879931f52ff580b166923aa1c1d4ebaf041ac546
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:22:34 +0000

    collect Tt corner side die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
