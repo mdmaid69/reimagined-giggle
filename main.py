2199c394b1153e490ea48c705a4cd111e9933376
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:23:52 +0000

    freeze custom suppose improvement day

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def add_numbers(x, y):
-        return x + y
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def subtract_numbers(x, y):
+        return x - y
