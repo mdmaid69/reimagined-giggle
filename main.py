9668b989aa7410d8273c83c1a5bbcd0e9fb55b90
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:46:22 +0000

    at deliberate result cut vanish

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
