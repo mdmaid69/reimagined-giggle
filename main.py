37fd9611dd1324dcc65561fc5d2a4962860deaf9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:15:49 +0000

    heat ago hollow task distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
