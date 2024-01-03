109bdb22ce19be53549cd288b0440a604e78eba8
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:51:48 +0000

    suggest in offer everyone chat

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+print(sum(range(10)))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
