e80e89523357cd1bdc03bbafe3da424757df542f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:08:07 +0000

    pollution thumb regard breakout realize

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_time_since_epoch():
-        return time.time()
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
