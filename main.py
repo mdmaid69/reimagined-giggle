e8aea42c8e1f9b1cd1748cc1151ba172551f515a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:24:26 +0000

    through roof male centre century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
