77a32005ec7de5ba79c80b161ec404c34143f2b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:20:10 +0000

    whichever please flat pull hungry

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
