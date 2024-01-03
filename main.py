8e0698d2e40e0099f1c995daa2195449c8746721
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:11:55 +0000

    plenty who may cope mistake

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def multiply_numbers(x, y):
+        return x * y
