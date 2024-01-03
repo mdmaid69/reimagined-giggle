5ab30f09ec82e650eba8912ecbec3e734fbe26a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:46:16 +0000

    avoid instant content score back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
