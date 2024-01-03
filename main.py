ae21c9dcc6c966f2c9dba08d59336b85f5c086f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:36:03 +0000

    brother false reject stamp murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def count_elements(lst):
-        return len(lst)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
