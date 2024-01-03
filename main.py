ba77710f393f97cde681b182ac627c898683fba5
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:10:42 +0000

    everybody thing entry conduct 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
