bc79286d3a53a70cc120d8dcaa0eaaa226090cc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:17 +0000

    task glass cruel giveup hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_item(array, i):
+        return array[i]
+text = "Hello, world!"
+print("Words:", len(text.split()))
