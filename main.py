1d587d0935d264dbd087bd7234bcef9cf3b8ccca
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:31:15 +0000

    pin holdout advanced swallow slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
