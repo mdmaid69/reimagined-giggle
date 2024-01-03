3a9bc7c6b304d980370b9826c84e53143328d087
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:37:26 +0000

    why provide because flat rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
