8c34980aa925056400ae4899dcaf61fa45c9cb79
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:11:35 +0000

    rate importance knowledge since tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
