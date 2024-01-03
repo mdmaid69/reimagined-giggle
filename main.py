6a42ce87f769e6039ca54ae9594c453f1ed337ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:12:22 +0000

    comein battery food just decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
