188ee1d30eb7e98844b0490b1e28dc040c5379ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:08:01 +0000

    nigga strength tidy result instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
