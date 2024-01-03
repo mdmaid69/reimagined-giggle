05aef87ea415e765cbfbef4054ed603f04c9f60c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:42:53 +0000

    style any far gentle stand

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
