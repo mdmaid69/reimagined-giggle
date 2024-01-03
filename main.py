53d0ae85ce0ac730d7c72ee06a29699931a07184
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:01:03 +0000

    rub physical pencil man constantly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
