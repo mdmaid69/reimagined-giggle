302c0138aa5a2a872f8460e8d8f165ff77e73a90
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:10:23 +0000

    excuse makeinto deep Itself family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
