c1a95b065b90309706bbe660a7aa4140a48c7819
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:18:17 +0000

    whistle into polite pen art

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+def convert_to_binary(n):
+        return bin(n)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
