2c80e114c46e912da7a12e9e11a6c7a9bccf59fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:07:17 +0000

    cut scheme minimum positive dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
