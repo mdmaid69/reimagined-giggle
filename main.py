48af398e3a897b2c4f1a94b63ff8c9671f9f610b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:44:18 +0000

    organize many past battery slightly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
