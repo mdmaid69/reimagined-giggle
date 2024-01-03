5f8f2bb0ebf3c599cc0e24d1d0384609e3762000
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:35:10 +0000

    earth guilty emphasis waste bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def find_max(numbers):
+        return max(numbers)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
