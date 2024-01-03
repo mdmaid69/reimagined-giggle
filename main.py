2783038062eff21831dc9f75889a4e5c7f551e50
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:09:14 +0000

    target hungry visit bear miss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
