40cf4981c6f8c9823b17b9780d36c118ae228de7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:42:22 +0000

    keepaway vote cheque cover week

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print("Hello, world!")
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
