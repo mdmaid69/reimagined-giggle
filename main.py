94a0b80498c1987b86fc9f0835fab45b1bccd0ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:35:41 +0000

    shrink floor direction he also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
