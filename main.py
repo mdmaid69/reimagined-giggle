60d07cd88be315c15bf14efa27c6c4118b6ee591
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:28:41 +0000

    pride whole wall body tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
