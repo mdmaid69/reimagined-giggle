7323da86d06a1e9e65b7f58fa6bebe17dab38cbb
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:20:21 +0000

    opposite sure smell unable turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
