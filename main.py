1710383937aaa37af69030c6c9125e4ea426ed89
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:12:48 +0000

    shop health beside tall hardly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
