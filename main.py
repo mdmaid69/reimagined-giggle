7151f64de272caabb84a7766741a0ecab4eab10e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:22:56 +0000

    consist phone centimetre start himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_palindrome(s):
+        return s == s[::-1]
+import json
+def convert_to_json(data):
+        return json.dumps(data)
