a47b5193e3ceed4331ff6c580c1f0552546ac539
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:10:15 +0000

    miss wipe my nigga hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
