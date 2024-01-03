7bb62448327b9b58da2d8d50e8a08ae6f7ec0985
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:22:10 +0000

    success they change put impression

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
