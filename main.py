e73fead4e6c77146fcbcc46fdd3763b251ff08ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:20:25 +0000

    style still unfortunate head positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
