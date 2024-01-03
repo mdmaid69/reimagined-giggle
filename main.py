d1ddc6661240759a68583710fb7c059eb4f60d2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:56:35 +0000

    confusing unless face 568energy round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def count_elements(lst):
+        return len(lst)
