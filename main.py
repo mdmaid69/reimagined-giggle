fc15d48e503fc37e2cff3257783553489c83c7ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:23:15 +0000

    bus many love typical exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
