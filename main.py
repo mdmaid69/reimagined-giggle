b14937713f6f8308096f21901da1b755077e3f7b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:11:17 +0000

    future violent mend demand Qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
