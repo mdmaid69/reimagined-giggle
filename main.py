6e4932470c9018eb2e2845a557a27d0286ef60bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:24:21 +0000

    forget inquire deep weak earth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
