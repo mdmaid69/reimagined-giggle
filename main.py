5e97cf757108eea040203e19796866640915eaed
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:15:49 +0000

    favour Bb modern control above

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
