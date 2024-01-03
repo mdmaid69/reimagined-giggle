93e1ec5006198cbf9942bdf610877bc8ee3bc660
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:29:38 +0000

    warn fit apologizeapologise top word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
