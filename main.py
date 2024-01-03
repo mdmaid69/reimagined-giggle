b0d21e0ce135047181fee28583c2b2ee21a92e93
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:01:10 +0000

    uniform about fork label declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
