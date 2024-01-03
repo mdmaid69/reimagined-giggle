3884aefea54165c658a2409a9acb50f2bbf55b95
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:32:18 +0000

    just Yy down could swallow

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
