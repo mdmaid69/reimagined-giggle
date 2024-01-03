149711abf7b3bb453a0629362a9518427692eb34
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:23:22 +0000

    central trap indeed over gift

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cosine(x):
+        return math.cos(x)
