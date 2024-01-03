9440ac2ba6c85f30b130bfba27a84df4c4ec8bc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:41:12 +0000

    tip practise union split complete

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_string(s):
+        return s[::-1]
