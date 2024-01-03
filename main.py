5ba15bae7c92489cc1f8b518ef08de6a7dc2b534
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:54:32 +0000

    goout Oo you under late

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_words(sentence):
+        return len(sentence.split())
