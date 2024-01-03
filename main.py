a9d62939cbe26979c66102ac952a3ba090c64ec8
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:33 +0000

    conduct broad complete hand confident

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
