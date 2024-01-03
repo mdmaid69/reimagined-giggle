8dae26db5c9293ef4c1b76cdeacb0f0d3ee2691a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:32:59 +0000

    able right run last injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
