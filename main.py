53a2ddbe4b3f3443c61dbfb36065656810de4e59
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:18:05 +0000

    sign band although congratulations confuse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_list(lst):
+        return lst[::-1]
