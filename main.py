c8f7a213baf5f90fa7e96a67b5eaeee842bb9592
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:12:04 +0000

    direct upwards push noone number

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  def remove_duplicates(lst):
+        return list(set(lst))
