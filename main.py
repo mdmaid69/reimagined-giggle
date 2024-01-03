5ea16d9a4f9db9cfebb17d468ce50cef0876ea54
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:51:31 +0000

    tip rescue recognize sing well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
