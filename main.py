a0897aff9bcee77bc53497237b62d3a46214384e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:28:36 +0000

    OKorokay loud brown surprising marry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_item(array, i):
+        return array[i]
