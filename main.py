248c142f87eb5ef0be932a4375cab22674e37879
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:44:28 +0000

    hers movement time over trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def add_numbers(x, y):
-        return x + y
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
