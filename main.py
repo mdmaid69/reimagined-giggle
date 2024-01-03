b3b826e383b925371746f5848362a765d91dbc92
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:05:37 +0000

    prove goaway they your dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+for i in range(5):
+        print(i)
