8ee270f390ffafddd2eb37f05e68c5f1b54f942c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:06 +0000

    but receive damage national must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
