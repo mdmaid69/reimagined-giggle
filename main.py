427119ee8f0dce89b6dba83fee885598da159c1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:15:49 +0000

    tour rely tour habit description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_bool(array):
+        return bool(array)
