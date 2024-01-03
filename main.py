1c453e98be0ea856f0f12c1d357b23a940373fb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:48:50 +0000

    freedom plant our enthusiastic good

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
