8fcc6d3020f2ae241946c44864d29b23085b0f06
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:02:00 +0000

    lie hard centimetre pinch gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import time
+def get_current_time():
+        return time.time()
