fd0f3bc6eeda858933470786b681eb7736ce292b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:23 +0000

    wave loan fail ourselves getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
