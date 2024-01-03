5f33c70aad7194764b6ba261aba3d552084ae7e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:07:01 +0000

    switch large value finally destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
