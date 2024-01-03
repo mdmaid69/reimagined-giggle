35bf85b887edb0b73bac9d2cfe5f6570d2efabce
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:06:12 +0000

    pile natural thank direct keepup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_current_directory():
+        return os.getcwd()
