c36ef1f845bcb886ee982a67653cd56694921f67
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:31:34 +0000

    related sweep him quiet prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
