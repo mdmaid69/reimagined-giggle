7166b4a064eea4e6186e603eb92bb146e9a424b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:56:45 +0000

    rub call safety rule capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
