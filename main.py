2e83495e19186fbce50f9a7f74bd2ca9ec147728
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:53:38 +0000

    trap someone reserve related sea

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
