bcde04a49bb426b5926f21977e39f102d7e067a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:57:07 +0000

    instruction reply many strike easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
