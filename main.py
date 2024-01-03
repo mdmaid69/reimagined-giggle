31dadd646a620946e6f2c188e993b92a9b285485
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:13:51 +0000

    smile worst suckme something material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
