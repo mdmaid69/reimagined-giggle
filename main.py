501953f56c7cdab28d75dba24294782e89f11f4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:50:59 +0000

    heavy strong dick unfair stop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
