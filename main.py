dd1cf1f2a0cfa479369102576a12a9bbaf48fee6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:51:11 +0000

    play they belief wise tap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
