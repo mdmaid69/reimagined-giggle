ea617db1f492d026e86d57abcf472efcc6f42ede
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:56:57 +0000

    decide muscle keepout bringforward young

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
