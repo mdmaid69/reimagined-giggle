901ecec6a48dfa1f7f3396b50cd27b92c825c342
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:12:51 +0000

    believe personal crash meanwhile notice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
