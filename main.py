61ecb4b698b2af2d5f501f8118f2b744942fbf07
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:27:12 +0000

    respect build world eye everyone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
