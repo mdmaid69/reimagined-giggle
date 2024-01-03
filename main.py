a0fda07e7cebf867a4dcf6620c159ade179cca74
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:04:24 +0000

    almost convinced extremely that forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_floor(x):
+        return math.floor(x)
