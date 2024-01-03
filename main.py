00fe7ce4679f1e1e314aa0598739f77861200c98
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:10:24 +0000

    criticize here Nn keepfrom responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
