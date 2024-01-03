ac80421de843677896e2f5636bc15812b4fefdf1
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:31:30 +0000

    wrong even then fence dress

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
