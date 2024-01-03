56569c343172c7b2b6c168010b869e44a8df0932
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:18:03 +0000

    if stamp care period to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
