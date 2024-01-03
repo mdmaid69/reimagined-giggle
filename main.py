491a271fe3bf8b59156605f1aaf6177744b8c915
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:12:29 +0000

    computer mixture many worried street

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_float(array):
+        return float(array[0])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
