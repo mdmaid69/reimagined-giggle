1bcd859e46e06185ce601ccaa1c4e98099fef503
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:07:34 +0000

    scared 571enough open hole sight

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
