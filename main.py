84ac04c59656405be07b1d46022caab32e9af311
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:58:29 +0000

    Vv petrol ought mind whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-print(time.time())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
