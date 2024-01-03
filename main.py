1d39c7ff664834114034d68ebd21a97a29061e0a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:49:34 +0000

    leave bottom completely slice free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
