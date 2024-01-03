dbfe37d9980f4e347dbb953db7a46495ccb69136
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:36:33 +0000

    service search low fine loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
