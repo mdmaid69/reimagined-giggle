d790dd5a6cdc41f0b2ec1daa4a3c40052d49cb4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:03:20 +0000

    afterwards fierce sister hope Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
