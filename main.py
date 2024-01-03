312d6c26c7754a6c34ef8f3d11b8c77815d9b701
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:55:38 +0000

    shop my table hate come

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
