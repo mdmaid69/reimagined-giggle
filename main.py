8c856cadeb8c5b34a2b18104320b1c6d54878d8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:04:55 +0000

    window persuade sound raise offend

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def convert_to_octal(n):
-        return oct(n)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
