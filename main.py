d01ff4bd94964e443963c1bd085711fce7547955
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:43:08 +0000

    turnup transparent swing me bite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
