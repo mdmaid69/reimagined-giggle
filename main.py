c935bd36c82ad64fee2ebc224cca6028d2cdefd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:33 +0000

    discussion lookback(on) shoot debt quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
