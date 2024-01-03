cdd00db33af92a28ae965e2860f55434bd48e51a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:24:52 +0000

    friendly transparent song turnoff with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_list(array):
+        return list(array)
