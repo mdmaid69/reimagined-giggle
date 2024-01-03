4056ca606fa0b02ee143741361c78a5150e459e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:49:47 +0000

    abroad how although closed still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
