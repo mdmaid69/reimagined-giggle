1f54880ac46523790a46d9f76308ad4e97c77027
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:21:30 +0000

    surprising wait use fence brain

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def reverse_list(lst):
+        return lst[::-1]
