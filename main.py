1aa4feeb84062906e676e58767f156182f1082fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:25:57 +0000

    float now while hungry hurt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_unique_words(sentence):
+        return set(sentence.split())
