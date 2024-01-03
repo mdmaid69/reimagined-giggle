30a359a4161d985e10800bcd4d3e82c231aa8409
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:24:17 +0000

    cough subject 567enemy handle separate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+def get_current_working_directory():
+        return os.getcwd()
