3e275510595bd7cff0db46e9f535ca88649333ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:11:57 +0000

    him bag people rent subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
