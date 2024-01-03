81b0ad0595890a2441790be739e916031bea49a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:39:22 +0000

    country hurt neat themselves respect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_typecode(array):
-        return array.typecode
+print([x**2 for x in range(10)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
