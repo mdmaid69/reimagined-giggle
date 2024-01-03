a0df8e17d7f13474a10d26f4a230b7237fb269a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:59:24 +0000

    sweet sing side climb address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
