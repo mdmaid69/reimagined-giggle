33ce42a7ab514aa2564ce32d0a2e1545eafd8f2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:53:53 +0000

    hurry lay innocent Uu soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
