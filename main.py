ff55e09c304214786ac5c99fa04a79bf4717443f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:37:32 +0000

    under as fairly wave round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
