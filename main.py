f391f0946a594c55d5cb07814006ced92eb8ac64
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:52:35 +0000

    Uu live all support drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
