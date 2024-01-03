4bccaf399291b50e7c0718fc840b58da792d780e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:08:32 +0000

    gradually forgive throw trap talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
