396bf94761503bce1c1b5eb9b5293802cbb9e206
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:47:58 +0000

    major phone name ready its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
