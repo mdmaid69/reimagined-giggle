146c387b0d5d84d9dc027b45ceccc8ab27237389
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:29:06 +0000

    negative politics sure apparently suppose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
