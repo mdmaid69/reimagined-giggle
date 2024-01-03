8a365244472efc20916f7c1121d6a29abfe851cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:43:59 +0000

    bank clean getback win let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
 import math
 def calculate_greatest_common_divisor(a, b):
         return math.gcd(a, b)
+import math
+def calculate_sine(x):
+        return math.sin(x)
