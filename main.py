b39a1bc9fa0b89c8ed05ce62475ba88afb9c55f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:42:27 +0000

    suggest switch broken moon square

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_area(radius):
-        return 3.14 * radius * radius
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
