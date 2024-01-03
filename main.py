54d920040cb73df2bbd30e3d3fbc5efd7d5bf9ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:47:32 +0000

    light Nn talkinto makeinto wing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
