e6a7da26d84e2bdad23469721995f24ec1fb779d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:14:36 +0000

    crash tone object phone error

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+print(time.time())
