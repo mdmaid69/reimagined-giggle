6d34dee26e48e1b78a99c88fde7b3d8f6ff8ed75
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:20:28 +0000

    deep aware fast miss understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
