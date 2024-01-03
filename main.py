41039268eda9ffcd870082497201e17625715ba4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:26:08 +0000

    might fast cinema capital exact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_list(array):
-        return list(array)
+def cube_number(x):
+        return x**3
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
