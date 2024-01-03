e6584f758712d475eea5522f62d13c27b9df872d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:57:49 +0000

    concerning press remember half full

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
 def get_array_item(array, i):
         return array[i]
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
