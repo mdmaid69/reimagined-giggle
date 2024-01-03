99226e725db2b2a650dd169d55716c4eb7e067ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:14:08 +0000

    clean maybe set home solve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
