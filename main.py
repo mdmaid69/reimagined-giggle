9c93fbc458b125d8714228ee94109bd15fb4df5e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:17:26 +0000

    recently about represent copy press

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
