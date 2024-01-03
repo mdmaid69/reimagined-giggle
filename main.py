eb22bb376cc2aad58d9143e8050bfb02c9c4eb1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:13:41 +0000

    glass constantly sore keepup afterwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import os
-print(os.getcwd())
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
