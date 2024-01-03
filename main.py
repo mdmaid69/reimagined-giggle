a31b8968859ec076e290a1e0e7d8d0a79302bd7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:11:25 +0000

    patient receive pause giveback business

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
