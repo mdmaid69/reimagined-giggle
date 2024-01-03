eb2d3138db6236b80d46f41d88b216d7fa33ec3e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:22:24 +0000

    position concerned equal Yy bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def count_characters(sentence):
-        return len(sentence)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
