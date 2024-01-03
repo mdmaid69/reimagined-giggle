c78cf2aba45838a00b2274f77d36ca8335d47d6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:49:03 +0000

    practical seat accidental hit not

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def clear_array(array):
+        array *= 0
