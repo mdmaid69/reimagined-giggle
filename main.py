38133899ed3d632a364a51f0cd7f2ba94fac955d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:07:58 +0000

    on speak recovery contact wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_length(array):
+        return len(array)
+def count_characters(sentence):
+        return len(sentence)
