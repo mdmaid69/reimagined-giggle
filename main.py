265e624e93ddce7fb5b1c01ea83c82420ac8f67c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:07:25 +0000

    switch excited criticize exhibition interesting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
