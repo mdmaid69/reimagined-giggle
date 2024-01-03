4fe35775dfdb012a13b7d89cb93f75d6820ac9f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:38:49 +0000

    address shop influence interested reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
