81c8619c5b5434234521a9dfbcf190f9fabfa199
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:33:42 +0000

    couple anywhere turnup slice exactly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
