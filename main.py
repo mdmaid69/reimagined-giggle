6eef89fcd326507b7345e9a37228e594748923f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:46:08 +0000

    second blind they half beat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
