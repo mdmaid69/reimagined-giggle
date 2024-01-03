17d44bf78552acdd274e883568b920cc0b130de1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:40 +0000

    terrible help unless building scrape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def divide_numbers(x, y):
-        return x / y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
