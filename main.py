f62a2cf5641223b0586ca3f1adbc12720d97df44
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:42:13 +0000

    book thick least recently could

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def greet(name):
+        print(f"Hello, {name}!")
