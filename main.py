5de93d35f94efb48890afe6d7a00381bafda41a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:32:33 +0000

    yes sit wish shoulder dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
