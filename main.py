5b9e834fec3ec4207c9b33ace42cc43e1c5ef8af
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:35:40 +0000

    pool really count slide various

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_max(lst):
+        return max(lst)
+  def is_odd(n):
+        return n % 2 != 0
