ea1c0ff2dcace27c3c13ffa43cc0ad9b2c8232a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:53:03 +0000

    arrange conduct strong runover fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
