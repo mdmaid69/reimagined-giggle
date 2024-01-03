518e40a6b8b209717d4ea298505e921adb6d50c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:18:38 +0000

    seem tired scream rich free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
