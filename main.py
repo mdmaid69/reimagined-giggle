d385aa997d61e29da9ff2591c5920011f9ec3030
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:14:29 +0000

    cover wear view comeback depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
