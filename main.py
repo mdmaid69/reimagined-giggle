100f07cbe56ede417fb7e8562418dab33e8f08b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:38:51 +0000

    boil excellent sick bin boring

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
