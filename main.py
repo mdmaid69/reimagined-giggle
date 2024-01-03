a613fdd6fceb433d80c4f42ea855cea242fa3592
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:24:22 +0000

    worse bright wet deliberate bin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
