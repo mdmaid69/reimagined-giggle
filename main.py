e812d908e3c9de7b0bd3c59a801cca0d76da66c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:40:25 +0000

    object impression fond stretch burst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
