3a2f84c02b200d152c075b5e5df8b00f1ce32cf3
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:39:01 +0000

    peaceful sentence environment goon easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_int(array):
+        return int(array[0])
