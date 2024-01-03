d9fabc8899aa3e6c3322870d5cfbfff7f695fe6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:47:19 +0000

    average bottom getback unable look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
