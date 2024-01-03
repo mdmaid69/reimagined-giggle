b6f9b22be288fa123bebacea64e8875fad789c0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:17:39 +0000

    familiar introduction approval angry rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
