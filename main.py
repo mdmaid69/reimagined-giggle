a4be85078ddd13201f114590f7628221e2431fde
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:29:33 +0000

    damage congratulations pair copy willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
