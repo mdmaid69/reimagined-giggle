71bc38ff867d8fb0c617e509116f65dce89cc6ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:13:12 +0000

    content interrupt plate exit square

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import os
+def get_current_working_directory():
+        return os.getcwd()
