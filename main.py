9982ff1ae3b7f6e59d5eac68981c6feb0be33c50
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:49:14 +0000

    many emphasize pain production front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
