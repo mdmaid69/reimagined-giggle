28f3d2cc388edf343ec13c8f8711b72999e8465f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:29:11 +0000

    eye majority distant dig ever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
