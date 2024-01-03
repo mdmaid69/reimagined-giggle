2e5dc09d785cdc7c7e7972089ea358c7ccfe9a3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:06:10 +0000

    trip look week relative motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def clear_array(array):
+        array *= 0
