ad6300403766821a2eddc9e36dbafcd4df159800
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:36:29 +0000

    answer comment worst moral snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+print(sys.version)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
