d473290bd65ed173458967fb3a026cfd62c63c32
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:24 +0000

    manner lie interest creature protest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+def get_current_time():
+        return time.ctime()
