55c8b4c6c59bc9a87084801f9c11a440303e0b2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:02:14 +0000

    needle surprise welcome really theirs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
