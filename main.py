6e3a5adca8d02c6521b36930fa49a9992909549d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:34:35 +0000

    bet love many will land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
