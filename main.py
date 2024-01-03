dbaab760b98cff34cb6d5a618e9cace86418e13a
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:32:48 +0000

    education grade dig workout deaf

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def sort_numbers(numbers):
+        return sorted(numbers)
