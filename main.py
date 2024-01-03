b7dd3c5206f5944df35a631d1e1dd38c58081a74
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:03:33 +0000

    safety Jj create refer beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
