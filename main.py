d1dba4eaa27971883057008ef90dc042ffabab96
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:45:25 +0000

    find previous acceptable mine disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
