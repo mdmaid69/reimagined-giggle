c7d9894a8158f1d9e809da790e3c764a748b6d95
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:44:52 +0000

    smell suckme independent grade whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_characters(sentence):
+        return len(sentence)
