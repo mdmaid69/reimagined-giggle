0843b4330166f3635b91d49e68d9e9495d7350c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:34:49 +0000

    unlucky sister naturally secret land

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
