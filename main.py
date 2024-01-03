ab79f269393b9521bf9b497b6cbc53d3ef84cf22
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:48:26 +0000

    no describe charge goout hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
