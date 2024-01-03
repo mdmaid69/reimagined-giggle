75bfd2b4a10ace11307949c6d2e9eceb39c83730
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:02:08 +0000

    dress attempt vote against gentle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
