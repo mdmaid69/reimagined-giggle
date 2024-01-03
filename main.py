fee88ea51694b15dc70dbafc0cd1a7236a45db9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:01:44 +0000

    future unite smooth water benefit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def is_odd(n):
+        return n % 2 != 0
