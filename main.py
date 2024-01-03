e12b3363cac5f9f4a903b93b8ce58d822f1972a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:48:55 +0000

    wander bring separate track notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
