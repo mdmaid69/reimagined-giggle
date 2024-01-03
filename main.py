703ddb8f6c4851c78dbeb91d1b948ea1354a10ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:24:38 +0000

    open come insist doubt pull

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
