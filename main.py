4e4b3ab3d50e35a21011a517570afddab1e7957a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:34:34 +0000

    tend where I effect thick

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
