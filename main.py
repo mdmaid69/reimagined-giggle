f95e6efb11e1b7f4aca0f426b572bc33db1f87a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:22:25 +0000

    spare downstairs increase bet on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_elements(lst):
-        return len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
