3775d249523a1740ce6f53b96d6bdb6f8a120c19
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:15:24 +0000

    law bright this pin reserve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def is_even(n):
-        return n % 2 == 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
