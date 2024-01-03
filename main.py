6f7bee4098ce1c6a4a33eea21a0d2d82f56fefb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:55:45 +0000

    distant rub bringback lie treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_speed(distance, time):
-        return distance / time
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
