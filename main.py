55ccc8139ce25040685c813b972ccf02b042171e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:32:31 +0000

    silent plain pour union history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
