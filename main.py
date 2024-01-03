828a6d7fbe9ae9f719b326d81e7ba4ef73733bcb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:43:38 +0000

    noone impatient smoke strike group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
