d4ab8eee4e0bb1a46f7e1e26fa6861b577cb5554
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:05:22 +0000

    attract share birth definite which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
