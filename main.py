89a1818de86fd45404d5fbd1a76acb827fbb0848
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:03:38 +0000

    radio soil experience new clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
