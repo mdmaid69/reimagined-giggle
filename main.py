daa53d1490e67a90ae2041b6310a1006204890ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:55:37 +0000

    mark 566end cinema glad keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_current_time():
+        return time.ctime()
