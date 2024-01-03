cf7265a7dc536e154442027f4d7b59b7fbb0e421
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:58:53 +0000

    justice suit science ocean spot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
