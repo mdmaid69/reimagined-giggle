ccc9e6ca7f95c36eb3a06d4905f0c8188afc6f79
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:31:53 +0000

    disturbing aim fast drink central

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
