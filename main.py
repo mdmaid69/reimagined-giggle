d154af3fe9ad02dd5ca97aeab93ed365467c5624
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:32:34 +0000

    juice container turn tap deserve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def calculate_area_rectangle(l, w):
+        return l * w
