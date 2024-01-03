071cd22c9f35b0b53ab05be77fe960add0258447
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:59:25 +0000

    lend trick describe difficulty game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
