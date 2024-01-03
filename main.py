460aab7d19d484004fa2adf81b7452b305efee1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:37:11 +0000

    unite picture admit shut uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def divide_numbers(x, y):
+        return x / y
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
