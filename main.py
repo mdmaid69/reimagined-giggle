eff6079a055475d4e056fb7d59df10754222fed3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:09:37 +0000

    female past getback promise complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_max(lst):
+        return max(lst)
