2dc3bd49849c898fd536034dc1c9e7cbc9e303a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:34:29 +0000

    poison top minute race yourself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_floor(x):
+        return math.floor(x)
