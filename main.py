4c612d43d1d407039615b9768dbffece602b9f71
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:28:14 +0000

    maintain strike spread embarrassed basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
