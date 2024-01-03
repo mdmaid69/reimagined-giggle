4d18d52f389833766deecf7dfa130ac93c768a12
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:16:24 +0000

    geton confident load attractive Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
