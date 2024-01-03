8f7eb276f72dbc7f4accb36e7593567f2cbf0d1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:35:03 +0000

    disapprove concentrate any dance create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import time
+def get_current_time():
+        return time.time()
