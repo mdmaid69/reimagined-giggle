d7bf72bef20094cd1fbf5d3f0d62666635a750de
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:38:42 +0000

    where set song 568energy exploration

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import sys
+def exit_program():
+        sys.exit()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
