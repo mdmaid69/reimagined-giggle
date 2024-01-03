6ff7ed55cd3a80a5233f7151ff7d1bb06b9ba5eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:05:14 +0000

    fortunate which win border tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
