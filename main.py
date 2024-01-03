ef993e51eb6bf0ea9ab8d63de541638f0996a3a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:52:18 +0000

    win seriously effect start foreign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
