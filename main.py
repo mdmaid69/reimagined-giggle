50204d1aeadf625fdb3df724c2fe0174a9fadc9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:46:19 +0000

    cry spill calmly experiment to

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-for i in range(5):
-        print(i)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
