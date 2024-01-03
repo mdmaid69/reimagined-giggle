549f766e21609851651bed2bcbf4ec51e0e05e7e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:12:15 +0000

    share suggest paper tip left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def get_array_as_complex(array):
         return complex(array[0])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
