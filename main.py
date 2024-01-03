91fcb03248d1e53d5ee79a47750baf761994ac60
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:27:42 +0000

    book innocent bring defend recognize

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
