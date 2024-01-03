231802dac6b9a67ff7c068fd7da6577a754d2511
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:44:24 +0000

    behave speed along share throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
