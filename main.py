8cd93c1bcade8b14a284ea68094e6eae62822c69
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:55:13 +0000

    telephone instrument rent owe bet

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-name = "Python"
-print("Hello,", name)
+print("Hello, world!")
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
