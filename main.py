c553ae7323bcc56476519844d988c173beb28bec
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:45:41 +0000

    wrap although popular rarely war

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
