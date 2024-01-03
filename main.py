942116e76683751c8064bb3a58bea57ac18a6ead
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:40 +0000

    few use material inform 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
