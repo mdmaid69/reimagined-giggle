9c333e490f2a9bff221d75e42bef649d8ec78f5e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:46:03 +0000

    afraid separate deep arm slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
