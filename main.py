b794491504cb30442aa0e237f732ca47ae928631
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:22:57 +0000

    flow consult day happen difference

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import re
-print(re.match("h.*o", "hello world"))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
