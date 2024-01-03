7cf7351d4a39ca6e319c289e1da28cfd9f020f12
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:24:33 +0000

    say key matter path before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_item(array, i):
+        return array[i]
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
