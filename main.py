0b927c993b51dcae2ce8fcac7d241b35c6546c99
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:16 +0000

    ask head complain ill for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
