74c47fcfc115aab62291db4acd17c64d33615159
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:04:59 +0000

    ought however protest yesterday difference

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import datetime
+print(datetime.datetime.now())
