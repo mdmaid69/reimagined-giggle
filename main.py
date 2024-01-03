48fb73f9a703e9e192e93e0a07dade08beacf2bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:13:29 +0000

    already brush deep ourselves ourselves

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def find_min(numbers):
-        return min(numbers)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
