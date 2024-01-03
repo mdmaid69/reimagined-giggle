8ed608597dc5c78229c64724db33ff114849622c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:57:49 +0000

    fast shame trouble calmly thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
