5fd4fbed91757a3b816c3f761df684acc5d5d6d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:49:46 +0000

    order collection authority pleasant sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
