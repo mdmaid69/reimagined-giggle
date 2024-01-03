d4ecfb3fc7db12222f6725966a5827da7efa0c34
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:34:51 +0000

    slice study bring exchange bite

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def remove_directory(path):
+        os.rmdir(path)
