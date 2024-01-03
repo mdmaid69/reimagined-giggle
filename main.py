a5f4f006e8e0ec580b7dca794de23d396bb306c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:58:05 +0000

    occasion scream approve first show

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
