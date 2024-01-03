908eec7f2634acfc64d211b409d9a10cdfadc8b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:21 +0000

    custom accidentally suckme feed suckme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
