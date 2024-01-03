a0e8c9996149b5e4b83083718afb2f8dce4b567a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:35:20 +0000

    shoot important him rubbish unfortunate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
