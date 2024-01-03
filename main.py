25dbe5bdfd4f2e5bfc9924161e138ebb0850db3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:00:27 +0000

    finally disappear freedom seriously stranger

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def convert_to_binary(n):
+        return bin(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
