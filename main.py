8cf29f7d309acb704338b4def526f3056eaab955
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:34 +0000

    relation central admit imagine together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def count_characters(sentence):
+        return len(sentence)
