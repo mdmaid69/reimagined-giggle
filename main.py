4c45dd5e178d21b2d42710412965fa13c7bd3f06
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:30:44 +0000

    pleasure apologizeapologise force think finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def count_elements(lst):
+        return len(lst)
