586cdbdceccbea6617a46b97e87d16bf5beb086d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:05:51 +0000

    whole create neck out defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_tangent(x):
+        return math.tan(x)
