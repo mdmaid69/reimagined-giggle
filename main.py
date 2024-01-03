353b8f4f4dd3e3e3600840cd713bab1e2407ab4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:35:36 +0000

    dead behind bang noise forward

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def reverse_array(array):
+        array.reverse()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
