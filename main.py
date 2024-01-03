ec6ab60e3a88a9253e8411d8534fe6b12df16b8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:10:49 +0000

    most announce meal club control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_as_bool(array):
+        return bool(array)
