a2b94fe7caf6b17a394f6eb09039ee919464d4ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:46:48 +0000

    guide beside right shape middle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def remove_duplicates(lst):
+        return list(set(lst))
+def is_palindrome(s):
+        return s == s[::-1]
