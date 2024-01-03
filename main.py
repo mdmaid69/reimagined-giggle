7dd67bfe503d4ca3ea00bb9d11b2717e0936eb66
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:31:27 +0000

    he ring life shut check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def find_min(lst):
+        return min(lst)
