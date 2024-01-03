193a8cc65d835811e52ec77ff1aafcad5bd004ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:06:03 +0000

    careless fact distance fair spring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
