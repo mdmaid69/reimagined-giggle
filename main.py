2db6d2913b45c93e7976917900c838aa53d10702
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:45:01 +0000

    part unfair joke place trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
