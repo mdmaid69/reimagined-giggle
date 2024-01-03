ecabe9a256bdded67b98e8daaa40e1307b6956e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:56:35 +0000

    eat before complain sing spite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
