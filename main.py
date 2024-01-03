495879c1a97d98e055689a2eb1123157c636cd3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:26:36 +0000

    warn succeed when wish usual

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
