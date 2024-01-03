2228d66d988398434101939c58268ed1c08cb9a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:04:02 +0000

    suck form nowhere pull use

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
