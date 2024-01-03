03c79846be63db2842818f60df7d55cdcb5348a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:38 +0000

    with especially tell no television

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def square_number(x):
+        return x**2
