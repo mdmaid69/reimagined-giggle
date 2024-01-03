e60db1a243603dfc6d520d9fb6a47c1aee71f26c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:53:29 +0000

    friendly effort authority favour lookaround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_exponential(x):
+        return math.exp(x)
