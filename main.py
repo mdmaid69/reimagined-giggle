90d94dff91f6fc62076b82cfe68dd6c2a168adf8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:20:32 +0000

    friendly familiar praise least doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
