0eb59d4e97f7bd81c5f860f24094054bef0f9aed
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:08:22 +0000

    teach tell normal our backwards

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
