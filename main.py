3bf322720774407ed5aa966b65fd1aafd30b4caa
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:09:27 +0000

    major track actual boring nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_max(lst):
+        return max(lst)
+import sys
+def print_python_version():
+        return sys.version
