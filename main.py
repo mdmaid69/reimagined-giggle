df2e2e1acf53f32d1846140cf5835fe0ba67a77e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:16:12 +0000

    ground fix suffer takeout difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
