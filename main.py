63e6206d08331ab09535133b5156afe10d446514
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:11:18 +0000

    intelligent Pp grateful mistake development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
