19cbb336f26072f6e7ef0ecafedbf0ce3edd8d61
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:51:34 +0000

    tidy once back watch figure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import collections
+def create_stack():
+        return collections.deque()
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print("Powers of 2:", [2**x for x in range(n)])
