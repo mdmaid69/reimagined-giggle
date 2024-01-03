1f15f26bd9f055ce0abdc6d0ec9144d73d2cc77c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:24:32 +0000

    into congratulate permission flow pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import collections
+def create_user_list():
+        return collections.UserList()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
