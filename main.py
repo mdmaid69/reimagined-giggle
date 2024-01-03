e25dbb5d1a5674ae7708167bf5bdb691f02e1d0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:03:44 +0000

    relationship identify breakout contain comeback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-print(sys.version)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
