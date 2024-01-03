109983716d8c806fe7e7f8dc204e83fc15b669dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:29:57 +0000

    drop be cause callout right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
