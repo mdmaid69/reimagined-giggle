6f237f53d1fd531b171911e000ef328a75d6eda2
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:34:20 +0000

    present pump do alone sting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def square_number(x):
-        return x**2
-def sort_list(lst):
-        return sorted(lst)
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
