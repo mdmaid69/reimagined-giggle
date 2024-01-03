2fc86f2d05ad613c3a3604fe8b35ec6070972a00
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:15:29 +0000

    rare fill vote suddenly concentration

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def subtract_numbers(x, y):
+        return x - y
