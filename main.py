a377b8c04ecd925f513b79ade3ac61ebee1dafeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:30:48 +0000

    meaning giveback middle bottom trick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_work(force, distance):
+        return force * distance
+for i in range(5):
+        print(i)
