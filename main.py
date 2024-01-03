f55a14f87ecdc1b70ca81e28d95209548a3fca5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:30:05 +0000

    along play and press runinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
