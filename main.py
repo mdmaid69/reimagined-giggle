be507ba4acaa838945eb1ab368e85503a674636f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:35:12 +0000

    letter further shrink shout check

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def add_numbers(x, y):
-        return x + y
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_list(array):
+        return array.tolist()
