4f0e52dfa45f9c9a0c7dbe11cc9e48307a254caf
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:06:39 +0000

    necessary own fight whatever break

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
