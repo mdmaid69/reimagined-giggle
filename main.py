54fda3cac36ca3516f1dd12061f2b86255460ebd
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:34:48 +0000

    home start tough nature able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
