ed509f874ec8a1ece717781cbd0caf3a1bce0cbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:25:02 +0000

    towards ever thinkthrough bet thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
