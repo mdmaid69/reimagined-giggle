da252295f1c75ecba070d89ab703ff84a390a2b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:16:07 +0000

    tap most so onto tool

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
