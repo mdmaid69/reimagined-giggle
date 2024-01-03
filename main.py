f6e0fcd0e37476bdde50186a44d57e41b8987048
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:02:08 +0000

    difficult base heavy roll force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
