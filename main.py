b68e8717002eb3d100522d5d36dec3a0ea1aa046
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:08:22 +0000

    greeting amount bend priority make

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def reverse_list(lst):
+        return lst[::-1]
