a4059f0c9907d02808bda68eb7f7d027dc1e993b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:48:44 +0000

    represent section watch forward know

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
