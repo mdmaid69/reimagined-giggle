124d8266ece00698392fcc421892882551b99d33
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:31:11 +0000

    stick front on shut black

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
