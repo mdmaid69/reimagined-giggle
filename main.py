e09c8b9a25ff61be3e43291f1428e00b62dd55c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:06:36 +0000

    oil carry approve in anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
