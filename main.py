8b3a8cb7c8e49fd75991521aa043d3ae4c9266f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:09:21 +0000

    comeround battery season scared education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
