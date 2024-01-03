810755f05a66f75e9307dc086557f09a9ebbf4d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:09 +0000

    steer never flight roll hungry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
