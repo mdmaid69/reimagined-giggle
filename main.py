169390102769783717de5aaf0b30c7ac207c1666
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:40:51 +0000

    catch stone manage juice yet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import collections
+def create_queue():
+        return collections.deque()
