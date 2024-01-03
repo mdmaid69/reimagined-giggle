3c9f55f849a82c54bb6ec731ea568af3a013ea68
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:55 +0000

    check large voice guess drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
