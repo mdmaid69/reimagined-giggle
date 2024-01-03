d1a8354632c9033b36b3266e86e58c98feac9b35
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:33:32 +0000

    mistake ago comeover fast union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
