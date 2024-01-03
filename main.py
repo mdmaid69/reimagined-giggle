38b86fd1aabadeb3f2e8ec066d43e3d292c12ad9
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:31:47 +0000

    clear receive spread giveout true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
