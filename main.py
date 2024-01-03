6a1e753ade5f74590c7d215a3adb22b0027c3b4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:01 +0000

    community left drink repeat roof

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
