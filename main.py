ea76b2aa224ad90eec569bc7d41c1e2445251871
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:27:24 +0000

    rob attract nearly doctor without

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
