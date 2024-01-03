63231ae58669cc40162fcc6cba0461df042cbf26
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:32:55 +0000

    note activity mean region address

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
