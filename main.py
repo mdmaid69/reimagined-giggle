a50918eaedf9fba6c1ca4a7b4747289feed5cc5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:06:36 +0000

    warm support public result excited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
