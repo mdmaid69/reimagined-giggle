bff629f83739e4b27bbf11f2b7f6ed8c827c4e75
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:08:07 +0000

    discovery ocean convince condition person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
