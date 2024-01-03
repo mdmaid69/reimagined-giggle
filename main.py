e853eaee22675229c0b5c7502a9db252ae880384
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:12:04 +0000

    black hang one exactly exact

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
