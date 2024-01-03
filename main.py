9885992b02dfc6661f03cee88fc6a084d7214374
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:23:22 +0000

    risk objection description important shadow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
