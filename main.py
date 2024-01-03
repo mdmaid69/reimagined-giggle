c04e2049a393ee01dba40145326da0750f363ef8
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:24:39 +0000

    pause pencil refer influence such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
