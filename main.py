d20e059640c4d886fdba67790450bc8a33bdcfa9
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:22:34 +0000

    minority emphasis check natural skirt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
