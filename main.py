aae9236c4a5815314a12e938e2015ec2d1d59bd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:35:53 +0000

    crowd ready successful promise steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
