bff6bffeddbb5db908a59a1ad61cec9bf08ff934
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:18:00 +0000

    mix help practical all themselves

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def count_elements(lst):
+        return len(lst)
