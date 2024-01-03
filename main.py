ee01a17650d544a41f2dcb5c76dff8647ae910dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:55:52 +0000

    strict book path once complicated

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def count_characters(sentence):
-        return len(sentence)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
