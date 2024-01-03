cb6bf71f739d38153c5972c7e75c88b8369199a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:51:30 +0000

    square 571enough task ability main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
