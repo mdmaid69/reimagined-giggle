07aec0e673ec06c8e1777f59e58e5e56a1bec910
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:13:04 +0000

    dull bang whistle remark world

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
