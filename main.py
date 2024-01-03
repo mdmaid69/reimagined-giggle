a79e549f877b33dc8e706a24e10ad2b41115ee45
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:12 +0000

    branch cool perhaps treatment scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
