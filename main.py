02f3bc05d7b1f45e337982ca110a4fe278915394
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:23:16 +0000

    float lend drop again through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def subtract_numbers(x, y):
+        return x - y
