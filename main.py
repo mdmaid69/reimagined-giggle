61b4dcd2c2dab8c105118db0609f1e48fd50b224
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:51:48 +0000

    strength zero in far building

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
