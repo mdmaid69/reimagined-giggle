821e2425d516fa0b78504c496078ccb30d79c046
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:18:43 +0000

    set touch doctor onto shout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
