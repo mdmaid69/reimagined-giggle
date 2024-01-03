8029c81e0ceb02d106381d7deb9b34405f925325
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:28:57 +0000

    quality and under language smile

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import tensorflow as tf
-print(tf.__version__)
+def convert_to_octal(n):
+        return oct(n)
+name = "Python"
+print("Hello,", name)
