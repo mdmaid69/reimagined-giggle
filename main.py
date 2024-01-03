58b147afbd7727d710762e1abd7cd7f25ea7eb5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:04:14 +0000

    generous bank recognize class world

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
