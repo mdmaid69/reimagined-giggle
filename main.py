17a9c409832bcc55173822fe96ccd1c88f01d8fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:40:02 +0000

    winter pocket once threaten lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
