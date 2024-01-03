940dec4345b0b84b922338d2d3625a499ca51772
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:49:11 +0000

    satisfied consider sudden bath ability

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_directory():
-        return os.getcwd()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_area_circle(r):
+        return 3.14 * r**2
