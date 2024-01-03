d7261107fd78eecb08f2ef760faaa052784dfee2
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:17:36 +0000

    oil matter tax public mess

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
