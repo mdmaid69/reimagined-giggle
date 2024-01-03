8b61b133d8b9045fadc26ed51a92bd28524ad50a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:56:08 +0000

    everything action by floor sight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
