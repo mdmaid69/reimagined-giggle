aa9b0e3b34dd4944ee681e8ac0df86173ee6ac7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:04:01 +0000

    discussion move instead father vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
