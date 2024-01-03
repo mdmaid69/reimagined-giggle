f3f47de71b8d4640d52a4382e91184125b7760c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:05 +0000

    cost fact loud light last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
