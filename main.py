8eb8318ba8ad7431e053ff92871ba00e9daefad1
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:08:14 +0000

    suggest post quarter waste down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
