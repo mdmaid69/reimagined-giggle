ccce10ed6a52d6836f225627e6f4cadbd1bc3b0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:29:29 +0000

    waste among lunch immediately intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
