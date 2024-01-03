c23c52c917be45b77d8bd7f795647d2111b1f85d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:33:45 +0000

    squash position convincing care indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
