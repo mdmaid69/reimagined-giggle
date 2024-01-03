9a48a6fafe51225ed501efedfb51ad61d82a71ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:31 +0000

    strength breakthrough wealth must tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
