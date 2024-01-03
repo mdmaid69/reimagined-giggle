ee450dc9d9045c7822243f3fc8acc23dbcb01ac1
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:32:12 +0000

    class spread distant permission middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def convert_to_hex(n):
+        return hex(n)
