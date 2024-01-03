f35718c7252e37e37ae8b9d94a0dcceac34d95bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:04:10 +0000

    force so letter consist giveout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
