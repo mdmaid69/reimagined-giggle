82bdf1333e5393c48ea9ef387c9ccf0588214f85
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:36:24 +0000

    mess react side joke question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
