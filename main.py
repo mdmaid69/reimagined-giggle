556c1ed76d68ee808f2083951b6c7e77fb962988
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:39:52 +0000

    stand that spot necessary mean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
