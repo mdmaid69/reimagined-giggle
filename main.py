39084dee1beb9449c5006e4364acc2f7ed3c5cc1
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:41:33 +0000

    whistle fact command marry mention

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
