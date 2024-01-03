f9a46ffc4d2b0bd5e3952697a882d742d8450e6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:00:32 +0000

    shop cool dark machine above

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
