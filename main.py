1143db503de62222ef093a8038456dd228fda2bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:49:23 +0000

    average repair mouth straight unhappy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
