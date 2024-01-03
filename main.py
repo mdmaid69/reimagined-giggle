6344dcec5ac5c55fe1425818ed1816cc31e97d80
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:01:57 +0000

    case region nearly oil impressed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
