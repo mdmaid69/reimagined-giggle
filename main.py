e6493e45dd51281c0a39053510901cc6f08a54ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:28:06 +0000

    sort rely impression limit shake

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
