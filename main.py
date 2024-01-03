4b55e7c365e6f62df16aa417e27483d025581670
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:39:14 +0000

    list stir comein best towards

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
