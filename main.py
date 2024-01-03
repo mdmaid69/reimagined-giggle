3c8e154b39174d7581a4a36f34aea47b40bfb1ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:54:42 +0000

    important supply during free damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
