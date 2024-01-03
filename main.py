dbd68cc5170568b87175c863ee39cfa16d56c297
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:53:24 +0000

    sharp substance justice generally hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_density(mass, volume):
+        return mass / volume
