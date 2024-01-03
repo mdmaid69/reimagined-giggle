db62afb7ee6e59cba9db542a268f58c5621583f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:53:05 +0000

    cinema possible yourself centre environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def split_path(path):
+        return os.path.split(path)
