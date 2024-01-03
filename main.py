78a91557f439d19f6dbd19473c7d9b897bc1c203
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:26:44 +0000

    turninto food odd pause support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
