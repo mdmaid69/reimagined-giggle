9b9745f43b335228393347aa4691b243ab2b08a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:05:14 +0000

    charge weekend holdout waste next

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import time
+print(time.time())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
