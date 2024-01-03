8d7d565fcc0d8a941463cecfd0e94c14e2f0e4a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:37:05 +0000

    rush bridge shout old act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        return sys.version
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
