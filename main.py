b86e6e3b25ebf918e94792fe1f6681ca9a2ad878
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:14:19 +0000

    drop scale nearly come show

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
