ba7130dd884ac25f3702973656455a53ea2851c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:22:50 +0000

    blame stay arrangement afterwards shopping

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+print(math.pi)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
