57e57fad8182ae12b817dbaf443303af7748fd27
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:41:39 +0000

    nigga offence Yy difference land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_current_time():
-        return time.time()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
