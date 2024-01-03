21cab830e249a295011e9f53c8f0b85676f3ca59
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:28:51 +0000

    hurry repeat rarely hurry authority

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
