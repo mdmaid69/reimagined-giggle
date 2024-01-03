40aac5a251451a91f3d3853df4e09efa3e86eb68
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:21:18 +0000

    cheat include left poison personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_as_str(array):
+        return str(array)
+import datetime
+def get_today_date():
+        return datetime.date.today()
