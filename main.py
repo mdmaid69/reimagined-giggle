fd9f6aa34b69aac50754edeec9fb869c7315b14d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:44:48 +0000

    picture accident wave pay top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def reverse_array(array):
+        array.reverse()
