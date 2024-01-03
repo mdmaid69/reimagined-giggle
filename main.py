b1fdaa866f6ecf4b6f2a79ca589d69840af6aad1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:31:01 +0000

    sore mail compare separate interruption

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
