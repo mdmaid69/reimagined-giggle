4aaa9c9187fca0783c02224beedd0f000300d48a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:52:04 +0000

    send surprise turnback study no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
