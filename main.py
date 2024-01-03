18cd14e4adb2d630f12152ca6a166396070fa043
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:44:43 +0000

    hear far trousers them keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def reverse_list(lst):
-        return lst[::-1]
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+print(math.pi)
