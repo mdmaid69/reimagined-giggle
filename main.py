b8bfcb6401dffad8f7b2dfa8805cc31c747f505b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:20:01 +0000

    afford turnoff something might mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
