4702c4612a5844f66a9b75fc65581470319fe99b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:28:12 +0000

    worse immediate development challenge tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
