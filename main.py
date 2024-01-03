61f8102af28afa83e5e988da50b12384f3a58af5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:11:01 +0000

    now that ocean state getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
