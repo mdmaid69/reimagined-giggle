2efc83cd5ae4bde6c631358455edd41d4ab9b8e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:21:50 +0000

    study involve makeinto actually obtain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
