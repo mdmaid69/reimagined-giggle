fef8782f940c17c6f41bb22e94dd441f42f937ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:24:28 +0000

    television family ours keep deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
