b8deaee7e630a07a92c8d2246c919d9d118747b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:54:01 +0000

    roll heart statement hurt error

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def count_characters(sentence):
-        return len(sentence)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
