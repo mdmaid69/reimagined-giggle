2b7420c83cd41930a0806d732b8d221cf162ee72
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:11:43 +0000

    rarely exhibition wild cruelty defend

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
