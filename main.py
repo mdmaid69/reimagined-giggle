ba43dc5612d63a321064e06646b6e5fa448f3566
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:57:24 +0000

    label determined provide straight begin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_work(force, distance):
+        return force * distance
