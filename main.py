9b452d0a3b4fa0eef535948961ee73bf93a7600e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:03:24 +0000

    result open violence kill worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
