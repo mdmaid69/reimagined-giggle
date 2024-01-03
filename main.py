42127c38d1ee5dcbe224322f3034a4e985106d92
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:54:29 +0000

    store protest offer party her

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_list_from_array(array):
+        return array.tolist()
