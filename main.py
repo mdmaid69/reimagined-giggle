20340b091c35848299c913c9885f55e3011afd2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:56:48 +0000

    him way at develop half

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
