25d25e9f018388b989eee4b2bd3fe4bba6c191c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:21:33 +0000

    mixture arrival recent command goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
