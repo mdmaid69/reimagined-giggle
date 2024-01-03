2069f6c4312040e33ab1e9832496bd519af6073d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:57:34 +0000

    part mine match rain account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_work(force, distance):
-        return force * distance
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
