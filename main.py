d97ef8bed2df6d22c2e919ae007aaf2746570fd3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:00:34 +0000

    afford reliable machine quit set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
 def get_array_index(array, item):
         return array.index(item)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
