bc1bd40fe78daa271825ae2da6fffdea32007fca
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:15:56 +0000

    reserve switch related stop complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
