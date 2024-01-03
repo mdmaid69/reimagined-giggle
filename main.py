64b52434a6453b8fe2e22c11a9d4ff615fa48cb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:27:41 +0000

    full care about hurt bear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
