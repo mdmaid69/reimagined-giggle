eea22bd15abe86e7f8db0937586b2097f203e7fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:00:40 +0000

    telephone approval dress straight permission

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
