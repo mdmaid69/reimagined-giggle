6ef2e95b92526dd089aa14b35628fc79cd688e57
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:33:20 +0000

    swim matter thumb nose criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
