f2370d4a6f40f7db0d9d1ad3760a061193f20e2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:22:51 +0000

    could address Ww goback(to) waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
