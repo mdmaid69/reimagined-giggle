0757d8fae19d6ff40cbceec58ae076465956386d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:38:08 +0000

    violent lay start should suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
