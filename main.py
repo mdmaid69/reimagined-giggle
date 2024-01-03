5f3ab184789e25cec984d60390e4ea72f2a847f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:38 +0000

    cry question cum piece meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
