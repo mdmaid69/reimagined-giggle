a7051e1898ce793b8779b626a2df6064447db652
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:02:57 +0000

    lookup shape factory meaning spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
