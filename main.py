b855c50c8eabe864315d02da6e9573239d3b6b26
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:08:42 +0000

    ever lookafter flat stand building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
