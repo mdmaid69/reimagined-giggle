5ce7277f7595f7ca3279cbb6a8365514ca4396ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:27:24 +0000

    affect shopping exchange train protection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
