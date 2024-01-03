3b4ea167644340e582e0010d1537fc50936acce5
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:37:06 +0000

    frequent reduction trick moment offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
