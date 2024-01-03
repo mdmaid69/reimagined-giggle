ffcdedb4c1809fc3933d5bf5c89aef74787e6f0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:08:27 +0000

    next head dark large thinkover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
