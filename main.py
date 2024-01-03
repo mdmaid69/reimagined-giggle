acb330fbad3b21e6b718e440a3fab2de975e1348
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:27:09 +0000

    search actually certainly distance under

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
