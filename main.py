08f7862f4d9d57b32578ab384a0076436419bbbb
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:29:51 +0000

    completely will cough new grand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
