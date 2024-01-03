0873f052c78c3f349b978c6cbfdac46d07bd0456
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:55:47 +0000

    worst profit secret fairly attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
