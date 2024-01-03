7a5e533649bd1a9585083c42caccc4c360a99b26
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:04:49 +0000

    definite runaway invite too strict

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_speed(distance, time):
+        return distance / time
