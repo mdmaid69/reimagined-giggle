45ba5200f46226dbde2744d9dfe9bd3bdc02793e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:14:24 +0000

    arrange large performance which medium

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
