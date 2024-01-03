4980d99afcf856bf33d469be50453da8e8e2561e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:51:48 +0000

    hang meeting lookup talk excited

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
