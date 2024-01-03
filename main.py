af962fb48f85f20fcd4cf0c8a7877ba96c22c9ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:09:53 +0000

    regional shout music traffic brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
