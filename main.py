8cfc3e6b1f3ac0e16bbf9d1744159738e13f0b50
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:51:54 +0000

    thought late try takeoff model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
