5a3ae6774d7a8022bf500734e28e543b95f25ea6
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:28:08 +0000

    acceptable dark chat tidy perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def add_numbers(x, y):
-        return x + y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
