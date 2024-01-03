372683592d7f4a89d00418d33fc04131f0c97b21
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:46:27 +0000

    relax journey steer Uu improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
