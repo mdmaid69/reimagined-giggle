435c789e86a74a6079d3408e9819cbc231dc8ea9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:23:09 +0000

    degree condition tip free birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_min(lst):
+        return min(lst)
