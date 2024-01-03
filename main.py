caee5ad1428629258d66278f1e84bfe747c2ca0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:47:38 +0000

    bet giveaway work average pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-print("Hello, world!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
