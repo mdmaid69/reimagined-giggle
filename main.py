473b63ce3148a91c34ee80a7526c4667fa3245bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:42:43 +0000

    terrible suppose Qq usually separate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
