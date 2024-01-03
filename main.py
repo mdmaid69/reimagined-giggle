0d4e2d84ece42952f27eb666ceb7264b9786d16c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:46:45 +0000

    check sign owe divide strength

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
