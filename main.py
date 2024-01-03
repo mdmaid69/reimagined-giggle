ec154bbb4c778737de09ea287ebfe1dbacbdec7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:07 +0000

    laugh joke could he idea

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
