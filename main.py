452d99bb3318aa29e2fe4e25e7504f8d29d73029
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:07:52 +0000

    she excuse draw away evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
