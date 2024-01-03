2ac03bc6a1f6114e909d6fe4b06df038a927a3bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:15:21 +0000

    whole if come shout top

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import datetime
-print(datetime.datetime.now())
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
