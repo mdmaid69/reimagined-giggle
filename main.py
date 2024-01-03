eda21f01fa88d7e3430fb341eeddae3c0efac70d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:18:42 +0000

    567enemy surface happen opposite when

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def sort_list(lst):
+        return sorted(lst)
+  def square_number(x):
+        return x**2
