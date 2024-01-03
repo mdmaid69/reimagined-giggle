68544a3cd0b1aca870df3e2f0c4937494dceb91a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:10:01 +0000

    base Hh official follow seat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
