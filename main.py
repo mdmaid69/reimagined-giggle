64b34e4cbaa0f829e0883f8a632300f2abf15f84
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:13:23 +0000

    can post less characteristic turnover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_speed(distance, time):
+        return distance / time
