4274d94200fa0fb0fcbfa2c0b510c41af7fc7887
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:53:05 +0000

    club race there dry steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Characters:", len(text))
+import os
+def get_current_working_directory():
+        return os.getcwd()
