e0accd9c33cd397cbfce9404ed8470f278917ac1
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:32:08 +0000

    feel disappointed accommodation runaway refuse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
