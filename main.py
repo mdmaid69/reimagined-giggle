08d1665619dcfab1f0578030a940af4d0e92af47
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:17:26 +0000

    bury rest scrape charge concerning

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def find_max(lst):
+        return max(lst)
+import random
+def roll_die():
+        return random.randint(1, 6)
