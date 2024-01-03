f67223cae90819e5a1c2748ab08744f7531ed89d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:04:16 +0000

    power accordingto guilty left best

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
