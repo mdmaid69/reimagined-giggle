0e22277f731e27e0c284ad2a8c81cc218e6be496
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:08:19 +0000

    sensitive forget home class exercise

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
