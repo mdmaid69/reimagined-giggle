7ba92ca6a56ced1f339dcb9326b41979d4575e3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:27:58 +0000

    use challenge pump available violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
