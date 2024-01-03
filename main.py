8e6b58311d42988b8df41c55b70d2f88ce162741
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:31:55 +0000

    custom up pass meaning they

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-  def subtract_numbers(x, y):
-        return x - y
+import sys
+print(sys.version)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
