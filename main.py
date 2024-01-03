c679fbe03789ff69959f633666c86af5683d738f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:58:35 +0000

    object live tired punishment simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pressure(force, area):
+        return force / area
