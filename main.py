c580962d48ffa479759c5d46836d6aac7434b343
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:11 +0000

    worried immediate problem similar interfere

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-print(sum(range(10)))
+def sort_numbers(numbers):
+        return sorted(numbers)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
