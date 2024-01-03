1e61074ad2604a8071184d032315222cf4f37a32
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:34:08 +0000

    private relief accordingto within either

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_work(force, distance):
+        return force * distance
