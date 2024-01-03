d6e9fbdf8192e1d2a9787f95a7ca949a34214b7b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:52:46 +0000

    slide purpose sort spring authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def convert_array_to_list(array):
+        return array.tolist()
