8aafca4760c3b66d7f37785ec6d6d374cf82971c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:15 +0000

    smell fall burn tip towards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
 import sys
 def exit_program():
         sys.exit()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
