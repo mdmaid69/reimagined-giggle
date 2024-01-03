7614b32f92d0507e66f6d407255a1696bcd2c47e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:41:11 +0000

    true come height aim listen

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
