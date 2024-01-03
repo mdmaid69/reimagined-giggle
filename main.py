c952978384c212475e83e6e744368b363358cc2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:44:32 +0000

    try conversation introduce police please

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
