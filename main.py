df3b9f7cc5331d5355e8546ad312fedbe362444d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:30:32 +0000

    holiday suck chat waste right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
