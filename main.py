f0dba8e0969ae240c43d6801d4032e97abc7aeee
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:40:51 +0000

    scene little onto lock form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
