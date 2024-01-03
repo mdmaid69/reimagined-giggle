9565275d282763d7b470f12b451abc2de2d3256c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:20:14 +0000

    flat declare object many considering

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
