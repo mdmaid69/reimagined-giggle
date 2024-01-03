2e604cde2ffe20f962dcb2157facc5569e4227dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:42:27 +0000

    runover convincing further indeed part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
