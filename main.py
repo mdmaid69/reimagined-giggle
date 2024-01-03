329f46f4b7974a74652e8810296cf8d5097b358f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:23:33 +0000

    rely good class neat pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
