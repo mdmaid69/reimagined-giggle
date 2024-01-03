0f5dc60b655e0ed5449c9aad191ca49dd81cfa16
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:53:30 +0000

    around leave genuine spare skirt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
