d883c7613d739b1a87b2cb1f49ff675b90bb065a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:13:40 +0000

    grateful classroom standard something discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
