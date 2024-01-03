5714ad83036343f08c2fff93ca3e98c7090b7ad0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:46:56 +0000

    rush fresh solve protest scrape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
