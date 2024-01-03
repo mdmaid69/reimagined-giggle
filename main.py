3035a2a8b2d77c853a81dfafb9a9a97d1289b0e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:24 +0000

    wide one temper hand bother

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_odd(n):
-        return n % 2 != 0
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
