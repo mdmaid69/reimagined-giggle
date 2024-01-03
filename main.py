857c9d12c92bdb33bddf0b679b981065ecc477cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:10:13 +0000

    defeat mind shelter realize degree

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
