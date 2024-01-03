217ab168e973bbefade009516d90f2737c11c54e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:33:57 +0000

    eager explanation tool rare tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
