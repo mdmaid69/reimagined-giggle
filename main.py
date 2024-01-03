77bbf1492ac06cd51a518b5aa9373ee32b64b0ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:01:09 +0000

    terrible pipe complete false attract

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def reverse_list(lst):
+        return lst[::-1]
+  def square_number(x):
+        return x**2
