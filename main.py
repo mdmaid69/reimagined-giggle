35bb0278098d3c11dbac9d023c76910f9ed15b9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:38:17 +0000

    measure trip comeround brain duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-print(random.randint(0, 100))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
