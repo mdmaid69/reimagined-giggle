765b434868c676d55bf2952d815e9b668dab4bab
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:24:01 +0000

    moral to loan means gentle

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
