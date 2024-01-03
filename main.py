579fafefe2af503d761a6bd607963b53df59e923
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:25:58 +0000

    combination without child worth order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def divide_numbers(x, y):
+        return x / y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
