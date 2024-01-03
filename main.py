38de9ab37337df05a9e18a9d0038d4f4da033bbe
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:23:57 +0000

    careless food visit Ff boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
