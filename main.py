af08be18cde7dab9f39fc27011dedc2c9be7f855
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:47:42 +0000

    straight about go breathe suddenly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
