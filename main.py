899c7922001f12dba074cb5b35070cdc7841fbba
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:16:41 +0000

    less sing blue bath behind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  def sort_list(lst):
-        return sorted(lst)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
