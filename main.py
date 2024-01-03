4540f7e178a3c8125e68c3861f87df03a06de13f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:30:09 +0000

    firm will within clear chance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
