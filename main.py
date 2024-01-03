ce5b0b33aecb545e61a432599d8ac3137ad39572
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:30:11 +0000

    bath print expression team consist

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
