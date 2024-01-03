c983ebc56f43d690350f68a2c9e6733756b0d8a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:47:03 +0000

    seriously blind couple rich lead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
