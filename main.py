695be5bb8d17796ed31639ca3f04de8ec5a5fbaa
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:13:18 +0000

    round busy disappointed include cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
