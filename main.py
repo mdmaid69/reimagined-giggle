1a1140e77c99f089686976d4075e75e7a8142c19
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:18:37 +0000

    typical Ww field disgusting runout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def is_even(n):
+        return n % 2 == 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
