4b0412d4157c74391935eb27f64e3575e912b30c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:32:00 +0000

    service tidy power pain wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
