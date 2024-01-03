929a83eb325446af0a3e78038b2b37952f30fbc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:19:23 +0000

    solve or exactly plain shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_gamma_function(x):
+        return math.gamma(x)
