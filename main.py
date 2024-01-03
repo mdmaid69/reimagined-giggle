c1f416063dd640bb5fbad9859e2524848206c11b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:15:08 +0000

    patience now certain year cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import sys
+def exit_program():
+        sys.exit()
