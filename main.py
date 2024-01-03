9f6f45fc456bfce6dab658773ccff8ab295fd2b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:18:12 +0000

    talk hardly guard still even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def cube_number(x):
+        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
