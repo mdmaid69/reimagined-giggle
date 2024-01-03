b00758c7801becd4a5d8cb2b6b674d5b3b42296f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:42:23 +0000

    myself bridge priority find himself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
