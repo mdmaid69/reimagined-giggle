5b847634411baafa7d3278a6e27e0dc40fe90d3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:58:10 +0000

    slide handle fall strict speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
