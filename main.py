61aaa85fff4a115ce94852369ae9f1ecb0cfd0d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:17:53 +0000

    compete shallow voice murder unlucky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import platform
+def get_os_info():
+        return platform.uname()
+def find_min(lst):
+        return min(lst)
