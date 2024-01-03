452deb5afe94088f78c43febaa3c6930f62322e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:17 +0000

    blue pair contact favourite person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
