d01933b8773d3b8dad40515bdae3cc43972463e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:58:57 +0000

    loan message tonight used quick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
