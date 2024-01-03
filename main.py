ad3f3a1986720622cdcfd11a1b3b6195fa6c0f1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:37:35 +0000

    compare complete different thankyou regularly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def reverse_string(s):
+        return s[::-1]
+def convert_to_hex(n):
+        return hex(n)
