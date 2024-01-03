a53555cb0c281b8a2b1733c80e7b7e2ca774c40b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:10:21 +0000

    education post especially guide long

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
