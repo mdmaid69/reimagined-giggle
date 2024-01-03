618baa078462efe11de15e02e44bd8c9b0d10461
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:58:50 +0000

    necessary air produce meet you

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
 import functools
 print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
