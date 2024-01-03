37f77bc01ff703d380aa5c7e82f614aae2a4e26d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:33:20 +0000

    shrink order bringup hot straight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import random
+def roll_die():
+        return random.randint(1, 6)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
