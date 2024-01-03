a75a74cf0b9e03c1656482dee4172e98609d47dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:19:20 +0000

    he general relax answer pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def get_array_length(array):
+        return len(array)
