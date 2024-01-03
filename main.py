814c095334d5b2538279546f35f12f485d04e3df
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:27:23 +0000

    mistake deep squash noone mother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
