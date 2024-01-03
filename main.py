d40ccb7003454f65b90434ec0571b35ef4cb713f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:39:28 +0000

    secret measure lunch live squash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area(radius):
+        return 3.14 * radius * radius
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
