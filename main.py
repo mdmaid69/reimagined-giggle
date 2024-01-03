e2e18880639d62e657afbfd64dc2b534670bba1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:05:35 +0000

    future shop lean many shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
