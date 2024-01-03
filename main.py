9e22bbea5465b9e4941c987d0c96688a0545f059
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:16 +0000

    breakfast like stranger drink lie

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def clear_array(array):
+        array *= 0
