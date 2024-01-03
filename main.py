c41eeac4d25c0d93405b46df5da3702b8e33be6d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:10:35 +0000

    some marry gradually maybe fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
