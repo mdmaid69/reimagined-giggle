503e0f3327a0222e8be38e08dd388e789ec49a36
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:31:50 +0000

    innocent danger traffic own slowly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
