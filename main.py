26b396afa9aee420aae1483dcb67f6f6a4b9a773
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:10:25 +0000

    spot mistake means confidence song

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
