702fb174cdea8505f417b7cdda521ec6c5c4c358
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:48 +0000

    here room excuse convinced climb

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
