5edc528c03f759dbbfb38b3d3cbf011e05de539e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:03:06 +0000

    worry improve respect succeed butter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
