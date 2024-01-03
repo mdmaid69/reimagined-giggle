e4181f63ddf9fe0591dbb5e557a9f7ef5fd8c8a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:13 +0000

    house getoff future mine reserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
