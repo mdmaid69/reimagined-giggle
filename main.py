eb20e34aad8ecae32a70f1fad86d40fc12fc5fcf
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:59:20 +0000

    fly sale tradition sky comefrom

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
