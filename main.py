7f57628fe7e3be472c46e95690911947e290113f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:04 +0000

    tidy software cheque agree interview

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
