0fef62382e72570d611ca59810194891a3826bab
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:23:11 +0000

    179bite thinkof any gather dish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
