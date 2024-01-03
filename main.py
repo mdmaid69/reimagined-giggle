bc5186030207109694ab576bce2f33fcd327fe8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:44:41 +0000

    better comeround Pp number pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
