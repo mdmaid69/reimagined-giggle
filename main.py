cb97f33be8ba56dcaee56d888e8b0c28d50b6db7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:40:22 +0000

    escape 568energy defend innocent respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
