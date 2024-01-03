acde94ca1575eef0d41a382775bfdc98cab0f4b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:57:54 +0000

    with along if we expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
