869adf9b62de55f7a61d99474293430d7cdc3681
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:34:52 +0000

    for visit callback world work

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
