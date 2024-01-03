46e29e9ab8faa8be6082e79094ff551fc0ad3c3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:54 +0000

    race trial fight know mix

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
