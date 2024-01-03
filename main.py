96ddbc48ec9da649d92a9cf19ebc03a62dd5782d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:34 +0000

    hair unfortunate urgent period complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import sys
-print(sys.version)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
