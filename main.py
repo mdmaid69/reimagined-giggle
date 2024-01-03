0c3d54c93568b2b19a90a2179a5d95728ab68eb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:12 +0000

    wealth team characteristic market hole

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
