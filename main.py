92dd3dc9eeb85887b23562ad4524fc28e3a1eb44
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:36:52 +0000

    further regard land sing both

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_power(work, time):
+        return work / time
