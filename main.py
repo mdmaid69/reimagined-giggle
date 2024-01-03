dec7de69f43b419ad1c29906f49e3385f073080c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:35:00 +0000

    interview describe formal health release

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
