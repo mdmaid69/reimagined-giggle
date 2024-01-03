7beef168b3db1003856dd8d139b02b991a17a060
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:48:52 +0000

    several always here breakin extra

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def find_min(lst):
+        return min(lst)
