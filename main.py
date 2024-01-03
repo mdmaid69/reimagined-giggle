f0e57bd80396707a2a59d7864c5b1ce7b42ec1b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:47:29 +0000

    supply identify neat hit demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import sys
+  def get_python_version():
+        return sys.version
