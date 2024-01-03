fb98f33211551aabd09ce0427313816a1ed2b5e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:41:22 +0000

    swallow debt equal perhaps gentle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
