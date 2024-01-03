a9f3802ba2bceacc331efb9cfe8499baaf15f8cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:46:36 +0000

    make marry lock feeling steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_min(numbers):
-        return min(numbers)
+def sort_list(lst):
+        return sorted(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
