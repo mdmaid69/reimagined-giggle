3c9b94d9a90619454909c9389fc4527f3dd93f14
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:30:09 +0000

    chief night holdout sort close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def convert_to_octal(n):
+        return oct(n)
