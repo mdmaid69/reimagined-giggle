f104629c8bceb103e065924f2b54eecb4a43465a
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:37:32 +0000

    sting circumstance bend keep rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
