ea568498b634c71e8d7503f48b60490aecfb13a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:35:47 +0000

    receive kick upset pen cut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+print(re.match("h.*o", "hello world"))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
