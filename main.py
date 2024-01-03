e3b2e3f3021bff5cf60bff07f61663d29b28c84a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:15:16 +0000

    clear undo dangerous rub Ll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
