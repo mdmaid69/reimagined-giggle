bb851a531e4dbed8bb4bd1e33e9bb14d55c1f475
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:26:04 +0000

    explanation acceptable both loose sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
