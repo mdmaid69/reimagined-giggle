cd53345ed0494e4a9183bb9846dc26f6155faed6
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:39:55 +0000

    degree please challenge comment declare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
