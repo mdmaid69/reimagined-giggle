de059bb6ac3a3911824fe4b8450fb1fcbd487d0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:13:25 +0000

    miss ashamed prepare ignore authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
