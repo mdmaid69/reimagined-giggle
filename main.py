efa7cbe029aef9b0c67078d3b000090668abdb24
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:43:40 +0000

    shoulder through flow combination after

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_area_circle(r):
+        return 3.14 * r**2
