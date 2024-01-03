6a4b360f8dfddbcf87a08532df4730a1a41f1fad
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:20:12 +0000

    note group record once education

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def convert_to_binary(n):
+        return bin(n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
