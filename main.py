990ed8692bbe507d25227b28e8844fd9d0ab7219
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:50:17 +0000

    major usual keen relationship position

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
