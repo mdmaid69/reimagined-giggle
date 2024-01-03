7d43b2392e3f45a45a862b55fae9b354e94c5652
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:54:50 +0000

    drop low neither call along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
