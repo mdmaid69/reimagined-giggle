ecdbaa9e80b1598c8a5a1fb764f138dca5f9f5db
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:26:16 +0000

    neither stamp waste age game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_str(array):
+        return str(array)
