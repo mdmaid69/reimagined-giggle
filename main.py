aed6daabdd3e8a097fcd2cae411556b1404f66fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:04:49 +0000

    572enter square in behaviour noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
