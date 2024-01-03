e9ef883871d8ae18c9d637b5feb23517e80eeef2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:52:38 +0000

    call permission work taste uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_odd(n):
+        return n % 2 != 0
