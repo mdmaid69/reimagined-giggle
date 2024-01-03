bf3e3b963aa2c6c923e07f1a642215c0a609de94
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:25:27 +0000

    odd progress read tired across

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_binary(n):
-        return bin(n)
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
