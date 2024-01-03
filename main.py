30f178056e1335d1f74ae9e8cc5136940a94cc27
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:16:04 +0000

    commit sick so effort Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def greet(name):
+        print(f"Hello, {name}!")
