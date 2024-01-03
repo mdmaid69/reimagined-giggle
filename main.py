68a7651861ace49929eabf56f3a7890b4cfaf08d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:38:52 +0000

    shall body plastic argue class

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Characters:", len(text))
