6b04440724ba4eaaf30b81dee8380b69222eb833
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:56:35 +0000

    side neat trial never issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
