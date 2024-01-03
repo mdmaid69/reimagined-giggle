7c0c9b658fa51e61b52595f099b4fd191ad701e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:10:15 +0000

    run brief company fit perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
