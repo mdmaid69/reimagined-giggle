5dbcc685b30227b208fc046e293bc56c994ddab1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:27:58 +0000

    exit direct measure dead cap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_string():
-        return collections.UserString()
+  def subtract_numbers(x, y):
+        return x - y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
