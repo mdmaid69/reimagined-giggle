884b73dad1199ac4cbb6e8baa82325dc34fa3cb1
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:57:15 +0000

    disappointed embarrassing dangerous normal discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
