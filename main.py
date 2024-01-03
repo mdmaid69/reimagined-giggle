823c5fcc8540a0860b3af1d89093e177a33f7d6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:05:29 +0000

    our figure object suckme throw

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_palindrome(s):
-        return s == s[::-1]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
