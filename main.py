e260f0babc59644b7ac994cc86921f2df898601b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:50:53 +0000

    rain comein table answer concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
