aa530951ee4521f81ce985f40e0047f348dc89b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:16:18 +0000

    holiday near it rely reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
