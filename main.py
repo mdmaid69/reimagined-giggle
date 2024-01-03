3c28201008cce4124a84aafa5f799a1d2d464170
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:56:18 +0000

    pool boat present immediately children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
