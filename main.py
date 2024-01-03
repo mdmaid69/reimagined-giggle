a4635225b745ba361ea06f276e2f804a6eda3cf0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:59 +0000

    nurse takeout Cc neither eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_palindrome(s):
-        return s == s[::-1]
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
