3e5162187024f0d208ea3ec7c49e2eac0b496ce5
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:39:14 +0000

    quality force cook definite tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
