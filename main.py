d9fa6b3639d6844ce53ba4a2350a1933a904dac6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:48:58 +0000

    uncertain set march opposite system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
