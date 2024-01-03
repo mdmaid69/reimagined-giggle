48e5a1e9f725b9c57557380add36fcc9c8ed7ae5
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:44:46 +0000

    face ilovedicks beside pump cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
