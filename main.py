f14422857da8b84f8a37f80297183cc6c4e6544a
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:39:25 +0000

    close speech show fall extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import math
 print(math.pi)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
