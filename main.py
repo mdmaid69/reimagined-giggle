de8f33d8bdd13c91550859a1a937a751d3385797
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:40:29 +0000

    minor getback this disturb chat

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-for i in range(10): print(i)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
