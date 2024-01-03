37ffa700d6c190d135d53cd392f33847246bc8a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:41 +0000

    slow emergency damage smile goodbye

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import os
+print(os.getcwd())
