17921e23653f73a42ba6d0f2ff5b0b856667d7f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:40:45 +0000

    poison secret 567enemy house just

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_set(array):
+        return set(array)
