ace86ea4efc6db0733cc9e4d1128e6b01c26891c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:49:09 +0000

    length polite delicate trip aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
