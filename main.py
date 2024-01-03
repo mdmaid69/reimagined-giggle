5f283cb2f2c65b1a1538b125356158751edf3c90
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:51:22 +0000

    strike both compete provide like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_tangent(x):
+        return math.tan(x)
