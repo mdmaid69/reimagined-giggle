9a1e627170417d454d5082ba0a96fe33e1ca9d06
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:44 +0000

    stupid pity comeround earth still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
