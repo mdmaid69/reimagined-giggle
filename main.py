84b84493606d95fc0cb077ae91c4bdf4bcb74159
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:42:47 +0000

    sharp hard gap office fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
