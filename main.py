5062b526b3615955f32fd3c4b034dfc3eadf48dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:32:40 +0000

    spill tour regional comedown like

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
