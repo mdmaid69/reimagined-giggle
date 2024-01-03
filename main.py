5f4d21603b275108cc523511b46f4f87bcce7705
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:37:47 +0000

    tool keepaway value doctor willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
