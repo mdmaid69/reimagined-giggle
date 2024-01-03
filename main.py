4247d6f2eab966afa68c8c8ba9ca717a38fdc7d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:47:14 +0000

    progress know coat connect giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
