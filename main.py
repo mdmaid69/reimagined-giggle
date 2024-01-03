8c674a529e677c6f939e8e492276fe74641df6ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:29:50 +0000

    laugh tradition thinkthrough inform delight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
