aeba8de58c4de2bffa552a19956461c11890872b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:39:23 +0000

    muscle shame react desperate kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
