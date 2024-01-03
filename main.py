be347e4bd9b582bfcfe9f0376d3942198141e8a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:57:47 +0000

    cut call partner star frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_item(array, i):
+        return array[i]
