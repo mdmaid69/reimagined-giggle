799ad5dec08a4d281af33cb91890c53d414623cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:33:02 +0000

    so basis work noone completely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
