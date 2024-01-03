61142edfea18f8b117533af81219adae14d7bbe9
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:05:07 +0000

    shock backwards go muscle Tt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
