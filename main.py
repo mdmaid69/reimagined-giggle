81899b4565b91e451c9ffa893b7cf6ef28c9c25e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:18:53 +0000

    indeed want repeat foot into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
