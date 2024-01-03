3950dab14e7abf613244f8135afed04693be10eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:26:08 +0000

    divide benefit wipe turnover smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def reverse_array(array):
-        array.reverse()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
