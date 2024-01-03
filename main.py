60be7ca7a4e4716fba641df4e1a27ba5baba5951
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:21:11 +0000

    goout story walk may smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def is_odd(n):
-        return n % 2 != 0
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
