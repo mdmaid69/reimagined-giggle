62514a7a420cd95b1f13496e723a217c4063d571
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:31:46 +0000

    nonsense related care interview stare

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
