6ec8669afb77ba602fefe5578e6b2b5ad518aa21
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:58:33 +0000

    similar evening make habit neat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
