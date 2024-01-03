c60d4a13203b08a5e51e39c6b08a05b5c5c62663
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:13:44 +0000

    pattern sorry effect system spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_palindrome(s):
-        return s == s[::-1]
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
