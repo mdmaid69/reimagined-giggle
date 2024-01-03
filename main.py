079231e1dfb687d58701ca8fda2d72c38c60ae31
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:40:01 +0000

    within over related file argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
