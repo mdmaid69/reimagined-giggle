f767e479480873934f5406deccc7ef1312d8c252
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:12:30 +0000

    towards cheque would practise right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import sys
+def exit_program():
+        sys.exit()
+import sys
+def exit_program():
+        sys.exit()
