a99fad09eb99a0565e30e11c231307a5ffdcadca
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:28:42 +0000

    exact goout sister quickly exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
