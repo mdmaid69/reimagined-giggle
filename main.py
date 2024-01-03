02caccd3f174b1bb950c9bfd97449ed64ce32787
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:07:18 +0000

    responsible steer ever freeze cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
