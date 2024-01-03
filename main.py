3f34fedfa4c559d4f5e0037391a265e9d0184f3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:33:24 +0000

    correct treat admit lie success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_stack():
+        return collections.deque()
+import os
+print(os.getcwd())
