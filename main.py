cedf5c27ef38402e8fa310b47a284ab053cc2603
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:52:50 +0000

    image towards out arrange regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def get_array_item(array, i):
+        return array[i]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
