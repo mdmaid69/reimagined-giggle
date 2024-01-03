626ae8611040f77a4dfa5a8617e7fafd864cb6e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:08:32 +0000

    connected Pp bunch equipment never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
