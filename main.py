ea436be605c8b0f343ae60276a7a280df4b4c193
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:19:41 +0000

    trip software modern generous eager

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
