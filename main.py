bcbac608bba1d88c1d8c1895cadc955a8b5098a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:44:47 +0000

    recommend use please uncertain computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
