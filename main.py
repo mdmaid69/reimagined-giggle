2b78a3087368eeac56c6ef53422bae7f1ff220c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:08:55 +0000

    test myself opinion honest remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
