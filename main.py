537db387c091c7d76715748886bbff4956ec85bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:40:41 +0000

    fashion record plastic difference alone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
