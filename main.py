656867243fcbbfafd651de72cb69700b00408ca0
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:56:36 +0000

    at stranger male anyone broken

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
 def calculate_power(work, time):
         return work / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
