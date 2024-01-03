291bc0bebdcb96fee6e792fb9478b042a4028642
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:36:38 +0000

    different large arrive kind away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
