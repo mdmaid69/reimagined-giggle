17698877f5f488345c232262e7fcfd6a7993a258
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:50:32 +0000

    dry check address test still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def is_palindrome(s):
+        return s == s[::-1]
