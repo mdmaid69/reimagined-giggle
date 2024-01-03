8687113fa053f25b4b5185a4c026821f59a225e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:59:04 +0000

    actual relationship swim if anywhere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+i = 0
+while i < 5:
+        print(i)
+        i += 1
