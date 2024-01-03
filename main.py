4fccf511596d1350b51c13831a820471ba9f7f81
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:45:37 +0000

    experience dry underneath all mine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import sys
+def exit_program():
+        sys.exit()
