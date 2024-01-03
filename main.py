e737b6af6bfcb6b8b858f7a7e5e119aaf48b1ad9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:13:31 +0000

    event let whenever hurry alive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
