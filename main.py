b1781ccc9963e4f8ae00e420baa7bf9268f37fe6
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:13:54 +0000

    thorough count receive crash extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
