47b3fddc48a415aedfa93f204b67b9454c2a6351
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:55:28 +0000

    choice season injury shallow pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def sort_list(lst):
+        return sorted(lst)
