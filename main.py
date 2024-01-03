4c0abeee56a25cdfaa5f7fd70f756930f47dfa1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:38:11 +0000

    well characteristic storm accordingto feel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
