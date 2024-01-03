100a03f17125ccc9e3ee0f02f97067ce8b47eb46
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:42:53 +0000

    tune clean fairly getup tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
