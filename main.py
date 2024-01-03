8c8ba32384960e65050e8bb219690a30e7dccf05
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:35:43 +0000

    sleep fine friendly today convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
