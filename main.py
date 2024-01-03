f44a806892ea7df9aac3271ea010be3bcce0a374
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:41:16 +0000

    low club platform rescue contents

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
