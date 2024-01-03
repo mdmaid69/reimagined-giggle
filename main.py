0e3dcbeaf0586b9ba3082d3cb539a69794851240
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:50:41 +0000

    pour interesting have world harm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def convert_to_binary(n):
-        return bin(n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
