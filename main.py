8f47d307c7de6cf9235a341103b53b71ecd9f00d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:05:19 +0000

    roll on allow surprised shout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
