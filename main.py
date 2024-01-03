a7cebb22ef9a30af0836c4dbdc9d3b0f19d0a147
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:54:33 +0000

    window supply young lift 570enjoyable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
