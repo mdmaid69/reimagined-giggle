f7e10d5ac6ae8a71e686922559192dfbfcf8ec0a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:58:40 +0000

    571enough hard never light excuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
