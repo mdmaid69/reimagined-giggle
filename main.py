c81dae00f49712b2b794f65ae4799e303fc3e130
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:48:08 +0000

    happy ocean equal dress steep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
