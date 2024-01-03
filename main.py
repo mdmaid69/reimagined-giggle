5e0f27ea5a47be6cb0ef4e214bb8343867c28a73
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:47:04 +0000

    accidental climb absolutely himself organize

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
