867e1e4feef0988b0aa08eb1d3c0b0011c95d6a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:40:15 +0000

    refer comeback use stupid block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def is_even(n):
+        return n % 2 == 0
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
