7e0336cc7b777d1e4de78b275d787b7e1195dd9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:27:06 +0000

    mad visit everyone middle grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
