c8205e47762861ec149a0d6e7776ddb0c12f4545
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:34:09 +0000

    talkoutof increase narrow flavour chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
