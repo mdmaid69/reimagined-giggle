67f5520ccb4ccaec6c9770c8f0537873daa3b868
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:32:08 +0000

    operate possession bored bite sound

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
