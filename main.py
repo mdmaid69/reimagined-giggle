191aed96d1d6e199caa873f5ba88c978172ef775
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:12:35 +0000

    relation favourite draw spray together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
 def convert_list_to_array(list, typecode):
         return array.array(typecode, list)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
