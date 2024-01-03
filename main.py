a5c056c8eeeb722e2c7b2ab67bfe28c98f54237e
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:26:27 +0000

    that new float fasten now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import time
-def get_current_time():
-        return time.time()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
