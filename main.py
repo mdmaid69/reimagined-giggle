9fa8068ad1eef12796444abd1caaf94529d022f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:14:49 +0000

    rather close yes confidence some

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
