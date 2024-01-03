d040e5d50f4af7f864fbddb7103edfb15699c74d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:28:37 +0000

    tidy makeup unpleasant week equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
