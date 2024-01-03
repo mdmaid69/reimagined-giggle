344e3e3d66f6f96ce0997d82544f8b0b8a1df825
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:18 +0000

    concerned short learn secret term

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
