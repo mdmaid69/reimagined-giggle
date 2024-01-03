7e689e6e75ce85d24412dac8fc59a9083bc6e6b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:14:06 +0000

    practice left range forward nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
