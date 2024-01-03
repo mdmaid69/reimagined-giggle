f118c77f9ef181ac4c2f98690aa8fac02fcc2448
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:44:35 +0000

    go show will Rr last

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
