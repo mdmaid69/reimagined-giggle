83b17fec5ebd7a4bdf896276a5a8fac67986779e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:37:35 +0000

    excuse cold talkback final compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
