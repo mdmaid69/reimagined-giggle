c7fb2b7b0170c6a1f03169f62bf570275cbb510d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:36:17 +0000

    still information match listenfor relationship

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
