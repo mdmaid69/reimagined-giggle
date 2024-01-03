d9a066827fd952d5536f68bb22016cbf8e23e381
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:29:17 +0000

    owe difficult over central reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
