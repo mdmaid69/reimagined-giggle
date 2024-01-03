f2e11b1ceeed928281b8d3e5ac0823cb843a36f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:18:09 +0000

    prefer contribute hear event cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
