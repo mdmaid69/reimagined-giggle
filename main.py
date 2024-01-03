15f532e40a85d38a0665f25be5a6952e90f1f9ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:43:05 +0000

    pupil necessary hear threaten swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def reverse_list(lst):
-        return lst[::-1]
+  def convert_to_binary(n):
+        return bin(n)
+import sys
+def print_python_version():
+        print(sys.version)
