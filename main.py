7ee0fe8f58a437dce0c7672d158d28e0f748d565
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:33:07 +0000

    holdout against clearly nerve drink

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import array
-def get_array_as_list(array):
-        return list(array)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def get_array_as_str(array):
+        return str(array)
