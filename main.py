dd4c41eb1ffa6febc66998561849306aadb4af9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:42:48 +0000

    underneath store blow once spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
