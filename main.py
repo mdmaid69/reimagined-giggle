fce32388725cf49dc39ab746f057f88ec8a1d527
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:27:00 +0000

    us manner contain past else

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
