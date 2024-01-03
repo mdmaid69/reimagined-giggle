b7a27740b7b1712df503f148c315090e8bdddc61
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:43:59 +0000

    rush risk scrap perhaps ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_max(lst):
+        return max(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
