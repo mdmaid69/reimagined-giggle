f803e0bcbe2c0fd4f4b2288a87a7a30dee90f9e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:23:42 +0000

    sort interview step shadow throw

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_current_directory():
+        return os.getcwd()
