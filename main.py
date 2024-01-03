dfe79dc96d0f023c9e430680954680222e00d289
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:27:15 +0000

    takeback tie probably beat mean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-print([x**2 for x in range(10)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
