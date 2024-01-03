8c5eadff30710891bfa9ca4f4a3617a33e108434
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:10:29 +0000

    burn intend substance hope later

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sys
-  def get_python_version():
-        return sys.version
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+def exit_program():
+        sys.exit()
