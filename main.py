4d580bb43945fce60ad7418cefd3e20bb17102be
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:09:01 +0000

    worth wake height smart move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
