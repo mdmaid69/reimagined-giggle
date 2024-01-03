1afe3104650dfb32dd1c96707e5a193a74e5912b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:46:46 +0000

    letter that leave address lay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
