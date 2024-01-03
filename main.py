955bcd4bb1199ab239534f47ae055e1c0d2adabe
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:43:03 +0000

    mail some cause band loose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def clear_array(array):
-        array *= 0
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
