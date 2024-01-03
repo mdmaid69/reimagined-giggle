8fba3cafaa8d074d325fc35189b36e1d9539b67b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:37:06 +0000

    hour behaviour field weak home

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
