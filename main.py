575219866e1b8ea7f13747f6f27f320edf232ce1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:58:30 +0000

    fit objection mention how up

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_item(array, i):
+        return array[i]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
