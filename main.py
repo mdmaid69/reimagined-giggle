8cbd83d081167d99a8db0beab0c83f422c83ec8f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:23:30 +0000

    tight soak back split with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
