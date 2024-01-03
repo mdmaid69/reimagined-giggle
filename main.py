9d72ad081fe43ecaee300266cc0224c767792b8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:21:22 +0000

    taste in towards balance bring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def convert_to_binary(n):
+        return bin(n)
