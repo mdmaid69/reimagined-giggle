abba9a433d08981434daa39f6fe2d4f5d943ae1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:23:19 +0000

    immediately reject surprise black sink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-print(sys.version)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
