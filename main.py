8a415aa51980650d3ed18440695c73b479e656e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:41:10 +0000

    afraid mistake exit soft create

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
