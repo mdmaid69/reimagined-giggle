71426cd5f50749f09c001fead0486a489af228c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:34:25 +0000

    as contact wall 564encourage yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
