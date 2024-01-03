9fac3cee8a9b23b10457740dd350cd2c16b2d44f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:33:09 +0000

    accommodation well tooth sharp interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
