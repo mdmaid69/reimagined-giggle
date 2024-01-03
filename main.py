e227709664c3066812f5bff64b9fd25b7f2587c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:25:00 +0000

    justice manage place work star

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
