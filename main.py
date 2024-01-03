33a6b3b3ba12cbe73a6ec558d7be549a261cbd2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:55:51 +0000

    throw like expression miss exactly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
