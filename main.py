4cb01d7c548b211bb072f83670446bcd49f66924
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:46:01 +0000

    sudden expression whenever estimate generous

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-print("Hello, world!")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
