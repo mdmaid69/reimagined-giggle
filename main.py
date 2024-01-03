390e290d509c69f7a3d9f73aee12463045eaf6fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:08:34 +0000

    justice Rr fight whichever plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
