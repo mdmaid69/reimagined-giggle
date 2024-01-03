a596055f35adf4a0e0a5f914b4aec81c89650e16
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:25:22 +0000

    raw match area in to

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
