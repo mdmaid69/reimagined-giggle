be85f770aead0c70585f6bcca69b0844ad81a23d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:24:09 +0000

    lesson discover tax support corner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+for i in range(10): print(i)
