dacf1473c71f21ae9e940158e485205aa9edead7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:06:15 +0000

    interested well treat crowd ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.path.getsize(file_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
