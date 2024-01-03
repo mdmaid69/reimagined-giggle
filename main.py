dc26f60797cce562e977ce607d1d3cf7d374bf5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:27:45 +0000

    lesson doubt destroy suppose difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def multiply_numbers(x, y):
+        return x * y
