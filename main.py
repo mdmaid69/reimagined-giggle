ffb27f66ba408d4e958591c9f3e17d0b450fef98
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:15:57 +0000

    anxious cause consider experiment term

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
