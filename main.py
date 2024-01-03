f7140084bbb29c602764e4d4e6a664c70d2117b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:39:26 +0000

    permission male need hour roof

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
