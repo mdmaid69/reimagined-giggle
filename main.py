598baf543e451e98919e5c88092733b7ee7ffaa4
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:30:42 +0000

    sometimes stare provide communication creature

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
