6e57e3727539b4443faa896539f02d29ce6bac2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:29:18 +0000

    comfortable have pocket tip comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
