e4e79a00765a48807d1cd12e6573d0900a83ca7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:32:38 +0000

    I dream neither fashion fat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
