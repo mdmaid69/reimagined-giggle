0e4f0612b77236fdfed49896a14b47791af693ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:28:25 +0000

    settle gooff about influence patient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
