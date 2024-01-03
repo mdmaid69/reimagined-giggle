b837efb83ad6c5e1bf5fe6da13458f376d7b465d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:55:20 +0000

    anybody behave rid very stay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def is_palindrome(s):
+        return s == s[::-1]
