302ecae9c6660ceeb345d960d04844397dcf34aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:07:43 +0000

    impression thought later people tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
