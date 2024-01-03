d01ecc5999790969431d887611b56af97ea11d6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:46:45 +0000

    smoke feel lonely stretch return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_itemsize(array):
+        return array.itemsize
