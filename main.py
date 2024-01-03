3f8298c710ef8a1511f811f0b2d35ea9b1a30da6
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:06:28 +0000

    anybody point out coffee string

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_today_date():
+        return datetime.date.today()
