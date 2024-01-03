c67a3c3feab1b7076aec697997d5c4ba1c8b67fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:50:16 +0000

    reject our forget health that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
