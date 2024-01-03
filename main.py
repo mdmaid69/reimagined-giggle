f7d39e744b14c155cd6c09ede183b150da5896a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:42 +0000

    hard operation floor minimum future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
