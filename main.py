b24a45a5fdb6d24bad33b8a15b454d4c2b55acc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:19:27 +0000

    introduction description sore surprise the

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
