ff7498f5261f2561f32712b1bd59375c9aa8b6a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:25:25 +0000

    connection home set tear comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
