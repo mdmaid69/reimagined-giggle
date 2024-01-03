78418e427f5e46c44221606176a83c63578a8862
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:56:19 +0000

    ordinary consult tomorrow private Itself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
