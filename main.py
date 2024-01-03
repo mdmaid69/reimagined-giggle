84c28b70cec353458a1c46ddfb2d54d77b02b89d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:15:25 +0000

    hour turnoff generally leave active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
