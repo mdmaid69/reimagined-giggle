8b125025671daad073680d6857e7dce68f96861b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:25:23 +0000

    sensible everybody care turnoff Mm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_min(numbers):
-        return min(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
