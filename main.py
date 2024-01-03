52ad6cfb3393b0ea1f01174067feda80c4fdacfa
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:39 +0000

    kill language perhaps mother polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
