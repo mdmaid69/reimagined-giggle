6e8155c383f8d1f0f1957162cdfc8c0bb0be3bb0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:57:24 +0000

    anyone comedown equipment plenty load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
