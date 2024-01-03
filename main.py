f278c200c8e1589209bd90bc1cbd2118bdee2b13
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:29:31 +0000

    art tense once earth crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
