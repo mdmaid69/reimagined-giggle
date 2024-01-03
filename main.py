2d4f0e82e292e5c786b15d1b57de69b065bb5862
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:25:56 +0000

    stop grade wing insist comefrom

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import random
 def shuffle_list(my_list):
         random.shuffle(my_list)
         return my_list
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
