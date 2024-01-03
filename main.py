080cf5dca4186eefbca5a7aee2e2f63d089f1d38
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:05:48 +0000

    like point female noone question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
