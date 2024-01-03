a4cdb249eecf4aeae31a1cf33f9be7bf2150b9b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:26:52 +0000

    moment although game matter marry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
