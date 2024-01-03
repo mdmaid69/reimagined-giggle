3feafc78335646cad4dcd30884442d7e021aaac8
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:28:41 +0000

    mind badly mystery worse relax

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def convert_to_octal(n):
+        return oct(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
