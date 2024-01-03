e920918a5049bfac9277dbaf1ef74c3aa18dac01
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:05:27 +0000

    suffer store treat risk paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
