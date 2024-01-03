3eaa26f13779616ab605f0ebe53c8dac8a596d1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:23:27 +0000

    upstairs sound tomorrow move relationship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
