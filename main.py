a36b5fec31ae729c4844c3bde47c943fdea9c5e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:18:00 +0000

    evidence who hand entrance suggest

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
