7219049d6a7ec2bef4b16f06b5c24935b15edc50
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:43:51 +0000

    what welcome workout correct deal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import os
+def remove_directory(path):
+        os.rmdir(path)
