b724b1c975c9d8b2bf1406e002928f1092d75c2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:01:10 +0000

    mistake false kind alone occasional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
