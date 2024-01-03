5ef7346032f4d274579b821a5ca8e4c5ac60f5ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:05:52 +0000

    another food protest object afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
