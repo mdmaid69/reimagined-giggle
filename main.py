9ef1e57d2ba3b5283051b1ec64943872c9d5b463
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:04:24 +0000

    have wind congratulate exact though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
