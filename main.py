e2e922625190d3bd0fc51c5729ba716948ce3a03
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:08:59 +0000

    outside sensible silent of pretty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
