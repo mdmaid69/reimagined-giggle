a7eef26a3d0ddf7db5b5af9d4a90e0f35b423034
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:36:59 +0000

    slice arrange machine roll worry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(10): print(i)
