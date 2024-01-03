512ebbc4ba8785ad4e53617c4accf7dc164536c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:07:27 +0000

    tooth open hold free window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
