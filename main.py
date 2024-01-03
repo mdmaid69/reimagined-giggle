f8a30a514c8b850872f0ffc22109438bfb4ea5cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:57:42 +0000

    find back afterwards workon dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
