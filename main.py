dcd21557c8d63a80d887e3b0c770b03760b54393
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:05:36 +0000

    draw direct squeeze make talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
