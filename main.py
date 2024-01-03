563c3f40a7695c735611793c156955b84b4aff02
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:19:52 +0000

    load circumstance wherever cover ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
