71e968f1dfbb8faf9194f18f39c53c35f8f03c04
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:17 +0000

    description clear remove fortunate health

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
