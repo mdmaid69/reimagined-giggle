f478ea80a72ca91abc1611261732ebb9d849af51
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:54:44 +0000

    education crash tone scrape right

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
