d7dffe585acdf7262eead2ca47ab881c29904c27
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:13:47 +0000

    ring sleep row afford gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
