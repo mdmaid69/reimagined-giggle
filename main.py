14853a405f5eee2160668dca9dc77f954e72a272
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:51:05 +0000

    unlikely drawing male something welcome

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
