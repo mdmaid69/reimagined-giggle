ca07b6c5f14eb677779cbe63a00291daa9a6f649
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:48 +0000

    suit Dd to by distant

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def count_elements(lst):
+        return len(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
