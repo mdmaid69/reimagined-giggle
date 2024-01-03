dd1613a3a45ec1ad4f5daf88f02a7500ecff5977
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:18:18 +0000

    variety extremely fierce probably neither

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
