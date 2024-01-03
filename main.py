7f5aa49c4a3392876c72ebc5158d8547eedc565c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:08:48 +0000

    butter comeback time definite side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
