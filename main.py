128724c83a92bd8b69d6f6aaba2365c6e05d6484
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:54:07 +0000

    practical stretch workupto prize goaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
