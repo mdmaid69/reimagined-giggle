2f3fec0dd03ae01d323cb74a668c7541a96cdbc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:24 +0000

    typical invite dick kind development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_typecode(array):
+        return array.typecode
