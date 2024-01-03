6eac5ec95dafc65560a6909ad95407f27d4e6f3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:38 +0000

    disturbing description convenient family computer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
