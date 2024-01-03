4cde7946bf12a0bb19c1c0b4130ba4c1f1472c9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:13:32 +0000

    memory outside impressed sister none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
