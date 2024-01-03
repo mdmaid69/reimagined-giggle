7616bda0c9e16e4738236a39bed29627b2f38b6d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:36:01 +0000

    for else ask suit friendly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
