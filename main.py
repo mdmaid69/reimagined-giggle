1674ba9f1cdf30b79d0f0dae39c8cb5e117c2d45
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:22:28 +0000

    arrangement muscle compete accident last

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
