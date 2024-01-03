bc0ddd802d07f816d3880f7acee83e7315d12ea4
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:59:16 +0000

    above push peaceful damp suck

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
