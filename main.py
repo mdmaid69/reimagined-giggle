4dc5bedbdef320e898b4fe15ebedd21887bdbd41
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:06:13 +0000

    guide repeat intelligent record but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
