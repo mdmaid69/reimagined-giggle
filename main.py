18723db37381ac887ccfa81297185ad54991d76e
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:24:07 +0000

    bet natural each although determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def add_numbers(a, b):
-        return a + b
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
