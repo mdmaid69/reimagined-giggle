9c40d41dd2a8865926d7f508ac7a93a34c6f1e3e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:42:33 +0000

    afternoon moon but on key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
