de928dd2eb2f983daf6de24fda24687a2374079a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:34:41 +0000

    bath equal lookback(on) fill pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
