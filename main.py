7ef3ce2f0806cedb568c91337bf53b63ba592fdf
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:39 +0000

    visit constantly number game search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import numpy as np
-print(np.array([1, 2, 3]))
+import sys
+def exit_program():
+        sys.exit()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
