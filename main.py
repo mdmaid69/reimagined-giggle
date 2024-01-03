9f05e4346bdc6e01206e0e66d90767689a918554
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:15:47 +0000

    miss same of shout drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
