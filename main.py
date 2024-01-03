dad95b17efd1ba5108d342e3dcd0b8d33b4eeefb
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:42:55 +0000

    style gas pause sensitive appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
