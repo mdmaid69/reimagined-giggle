42a2d3ba26c001b01367d2a2888a50589aac1d28
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:49:07 +0000

    hello leave goaway trust disappear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def cube_number(x):
-        return x**3
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
