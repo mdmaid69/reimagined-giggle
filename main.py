23cf6eddbeeabe0df8899650c44ed58b266bd7a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:04:02 +0000

    children responsible maybe slice limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
