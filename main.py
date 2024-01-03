6cdc8924ac70821b97d7de3f4309784890906965
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:20:21 +0000

    reduce loan second trap stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_typecode(array):
+        return array.typecode
