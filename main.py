361a0b58eaec5bcc7992455e2ded7ab59f701c19
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:09:54 +0000

    further touch interview Zz wire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_user_list():
+        return collections.UserList()
+  def square_number(x):
+        return x**2
