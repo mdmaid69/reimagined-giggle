675822d93b76ae58d6b47f2ce32020f1b4e25d15
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:40:08 +0000

    previous telephone lie stamp emphasize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+name = "Python"
+print("Hello,", name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
