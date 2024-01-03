ea35244da8991325ec13fbf290caeab19f81b1e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:42:51 +0000

    goaway stamp remain over life

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
