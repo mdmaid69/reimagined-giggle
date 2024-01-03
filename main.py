615485379f7949fdc1067979ed131f3f6f7004e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:37:56 +0000

    time repair bus information generally

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("www.python.org", 80))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
