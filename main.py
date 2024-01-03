b77a905b26604a21e17291c1410dbd31ce81c6c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:04:49 +0000

    separate expensive table principle practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import re
+print(re.match("h.*o", "hello world"))
