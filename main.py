a26cabba6da602cad1d9e3b0d2fe20762cec89ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:30:39 +0000

    promise neck prevent practice bend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+for i in range(5):
+        print(i)
+for i in range(5):
+        print(i)
