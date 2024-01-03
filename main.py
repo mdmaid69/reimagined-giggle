fb1f056efab8dd78b40727e3c8544d0b9fdef5b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:09:07 +0000

    sweet sure exercise paper total

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
