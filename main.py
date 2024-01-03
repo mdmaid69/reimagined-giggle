18bfa55a6dfb2f06004bd050898de7967a4210d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:50:56 +0000

    anxious pain all act stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
