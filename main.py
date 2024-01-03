e47c44cb7b10d36feec6b7b73e814d32be5e57d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:14:35 +0000

    do suffer surface defeat fasten

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
