33e8613381a7398490c5cc923f5c5ea87d8aea1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:27:43 +0000

    nigga suit trick sport promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
