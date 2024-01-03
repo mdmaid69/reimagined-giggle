427f8cca9e6285bbbd110d48ee5fbecfe7d05b1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:52:53 +0000

    children seriously abroad bath already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
