546d835975a6715ee110ccabfad37e3ad3773026
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:41:51 +0000

    while remove create dig trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
