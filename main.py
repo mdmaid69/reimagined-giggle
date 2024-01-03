4ff6b7f838481661b787d0200f5fe7984c32c532
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:23:29 +0000

    fairly middle serve peace scrape

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
