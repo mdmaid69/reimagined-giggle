acee69b30e39dd8c9970bb89a7b629536e363e1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:12:31 +0000

    bound flat swim stretch would

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-for i in range(10): print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
