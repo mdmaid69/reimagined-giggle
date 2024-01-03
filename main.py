bd6e8d592301b22895309fa53ed43063e960cfb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:09:07 +0000

    disturb example uponon flavour fierce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print(sum(range(10)))
