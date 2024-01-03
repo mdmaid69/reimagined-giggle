7e7b8517ea8963618980706171d2bbd3a605c9bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:18:50 +0000

    sick hold point wind steady

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
