ae63f3029a1b5549a11a3020593816c53b129ef7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:31:02 +0000

    callback paper adjective production well

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
