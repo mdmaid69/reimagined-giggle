cbc243ba619ab4ad2d1db329cc367321580a8cf7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:03:35 +0000

    discover or minute fit free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
