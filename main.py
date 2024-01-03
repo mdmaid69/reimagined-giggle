1e51caa8bf14e4a18cf1824769f31c82cce0f407
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:37:21 +0000

    certainly sleep lonely principle prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
