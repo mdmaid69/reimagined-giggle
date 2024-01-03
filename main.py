c71bf51cf627306c3405325992e327d35a88a4d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:21:47 +0000

    Mm year home beneath 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def convert_to_binary(n):
-        return bin(n)
+import array
+def clear_array(array):
+        array *= 0
+def convert_to_hex(n):
+        return hex(n)
