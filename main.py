c37baa6ce63c75240cf4e4e790253bc43241ae95
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:19:54 +0000

    low bang rise money quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
