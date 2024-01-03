e36d3b0199c88b5f3786f9830f7d17bfb058d085
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:07:00 +0000

    service tooth suggest bringup represent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_power(work, time):
+        return work / time
