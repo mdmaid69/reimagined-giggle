3c137421b919731a412a11d84cf8e3580a09148d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:46:58 +0000

    whenever contents stare smile communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
