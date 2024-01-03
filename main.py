8e61ecaee2c91b44f0948e839652ba5d209169e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:44:37 +0000

    comedown holdon sea reasonable second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
