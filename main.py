f907d4e22230a10d9297a3dcbfc92fd933ef6821
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:14 +0000

    hate forget connection evidence full

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import matplotlib.pyplot as plt
   def plot_graph(x, y):
         plt.plot(x, y)
         plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
