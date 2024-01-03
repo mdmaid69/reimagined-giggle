09bf6758b3d409ab941396700fd8f96b298d8e63
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:35:24 +0000

    getout ball respect exact mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-print(time.time())
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def append_to_array(array, item):
+        array.append(item)
