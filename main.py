9de9702512eadb6391e7cb64e090bfdd720da8bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:18 +0000

    lead pause foot decision capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def convert_to_json(data):
+        return json.dumps(data)
