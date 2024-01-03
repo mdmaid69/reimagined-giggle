b2651f4bb1a9b576aad71704244867a367df6af7
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:31:33 +0000

    at fear comeout in love

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_float(array):
+        return float(array[0])
