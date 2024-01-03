cdcf123c1ff126673449ec3ec90df78c02639590
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:23:12 +0000

    since sleep solve makeinto cheek

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_speed(distance, time):
+        return distance / time
