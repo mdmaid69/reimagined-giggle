5e5c1f5dd7efc7f34d93b6a5b063134755f75f32
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:20:16 +0000

    hit respect coffee avoid talk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-for i in range(10): print(i)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
