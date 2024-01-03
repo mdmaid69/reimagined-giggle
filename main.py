8e72a21880d6d458c22e774099fa2f311f62d0d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:24:30 +0000

    different confused common busy stupid

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import random
-print(random.randint(0, 100))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
