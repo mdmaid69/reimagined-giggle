8b9852ec920032d2e2af56fb8d97bd062e734d8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:54:08 +0000

    cough building tap bank cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
