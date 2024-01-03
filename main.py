09c31107f073597e7b397ab0420db454d52cf74a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:24:50 +0000

    sensitive attempt some damage say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
