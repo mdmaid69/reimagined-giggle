2399f708b561c29faf5bce15f468fe8b0e59ecc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:05:51 +0000

    mad poor delight pretty meal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
