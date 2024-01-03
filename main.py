b694acb69ff390e89c51017bf27815e70b7244f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:57:59 +0000

    sale wet rubbish simple school

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+for i in range(10): print(i)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
