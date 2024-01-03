161bdb7d4a5e34b7196e5e98f091c74127654570
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:44:02 +0000

    criticize software would help substance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_str(array):
+        return str(array)
