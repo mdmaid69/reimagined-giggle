c0686d499d07c35395d3ebb472ebbfebe35540fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:42:16 +0000

    praise force able characteristic drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(a, b):
-        return a + b
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
