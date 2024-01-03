f5b4ab8093e1d154e070d8a796e198ced5d49fb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:19:01 +0000

    tip after explain centimetre miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
