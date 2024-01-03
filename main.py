c30e2d12fd0aa4096f15d5dfb80d00afb7af4ef6
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:59 +0000

    rare private reply note disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
