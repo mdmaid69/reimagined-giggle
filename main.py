48a73c06798ef864fb6e1214492a730cdb760df9
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:33:11 +0000

    successful guard immediately neither worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
