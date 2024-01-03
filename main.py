fc513fc12bfe56aedabd5d80ef6105ab49af6d84
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:06:08 +0000

    disagree square deal knowledge deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
