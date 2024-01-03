f8836aba4c0451a311e8d18b6254d6d18f617ee8
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:28:12 +0000

    maximum guide sport neat heavy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import time
-print(time.time())
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def remove_duplicates(lst):
+        return list(set(lst))
