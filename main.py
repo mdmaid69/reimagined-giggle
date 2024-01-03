301407fa461b16d8cc8a57a04fd7544f18935675
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:31:55 +0000

    it grade fat block Nn

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def cube_number(x):
-        return x**3
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
