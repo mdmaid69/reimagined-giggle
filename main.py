6b843b6196e6cf02253f06f9a0ece68b9df0697c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:26:59 +0000

    lookaround trick genuine goon good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
