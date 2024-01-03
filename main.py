080e88ff480447ca5f7af07de2c504a39218d8a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:06:21 +0000

    reduction imagination sick doctor or

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
