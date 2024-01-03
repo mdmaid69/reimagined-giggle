85e3eb8e46233ee6193faa999f9412ce61aa96c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:00:58 +0000

    key mistake extraordinary share remark

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
