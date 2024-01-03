3b2c295e2f757200772b935cd22c95fc1c8740b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:47:06 +0000

    achieve enthusiastic most Nn actually

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
