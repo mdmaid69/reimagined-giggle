c8033ed3f46d486cb533adf954b3319ba5c47a22
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:55:20 +0000

    regularly pure woman disagree minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
+import sys
+def print_python_version():
+        return sys.version
