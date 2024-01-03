69f05e4121f8fb2e0ca279aba02b69bb34744907
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:29:49 +0000

    forgive guess police culture Oo

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import numpy as np
+print(np.array([1, 2, 3]))
