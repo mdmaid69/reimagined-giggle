f82685cbb087338da50e1e9b6218d1c2e7efc381
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:31:42 +0000

    fasten no unfair support shade

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
