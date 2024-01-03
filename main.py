3c06ebe7c0fd8c72e81fbf1b285758169889897e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:26:30 +0000

    promise may simple awake freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
