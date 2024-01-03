5091a33c8d154731e9ae0b23a3fd09586ea314b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:20:04 +0000

    off mixture settle straight cover

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import random
-print(random.randint(0, 100))
-import time
-def get_current_time():
-        return time.ctime()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
