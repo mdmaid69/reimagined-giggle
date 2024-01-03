5f2c950235558992517444254fe1f48723a267d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:18:12 +0000

    weigh load popular sight again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def add_numbers(a, b):
+        return a + b
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
