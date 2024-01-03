4af77c9ddea20cca07694d10450c79dcd40b9144
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:55:34 +0000

    century announce Pp subject compare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
