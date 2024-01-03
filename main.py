10d74d5e1d3a8fa45858359f8a63f308ed2aafc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:22:34 +0000

    push bringforward food bringup scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
