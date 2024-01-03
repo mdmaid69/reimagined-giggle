4aa9be5426b522c9334eaf81e64ac3d7d71722a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:35:17 +0000

    introduction joke stranger save desperate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
