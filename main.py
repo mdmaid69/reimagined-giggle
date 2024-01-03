5a623fdd1be09ebc6f6a0e66a3ca067de548f67d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:53:34 +0000

    hot thankyou use build father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
