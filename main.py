e5e88d24ebf13d291b6bff55d468d21c65f9ec15
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:22:24 +0000

    prevent zero plan brave rescue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def find_max(lst):
+        return max(lst)
