c213d45f4ed68113b7d2fce456d61d3a6dd84beb
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:28:21 +0000

    Dd load practice enthusiastic object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
