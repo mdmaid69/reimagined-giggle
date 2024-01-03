e7a0b611189112b5d030f0c622a95e2ff10235d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:46 +0000

    accordingto keen post frighten store

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
