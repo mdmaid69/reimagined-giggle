7a1c79771a421ef391646fd4e1aca3d2e63cc361
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:04 +0000

    pot dish brother rubbish what

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
