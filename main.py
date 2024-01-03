a8cfcc66f1a52a98647539c54294626fdb4f636a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:00 +0000

    comein build Pp later valuable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def get_current_working_directory():
+        return os.getcwd()
