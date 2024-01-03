3b42c1236c731f98d66f725b947cc04280363de6
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:48:35 +0000

    be speech valuable fortunate start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
