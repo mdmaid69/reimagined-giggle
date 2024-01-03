0b7af0a9cb2df180e22c181d9a7a94cbec9131fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:42:17 +0000

    suggest limit talkinto motion weather

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
