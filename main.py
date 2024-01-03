8d390d21e5d292b72715be73b82a3fd852dbd23a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:59:41 +0000

    visit environment contents no secret

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
