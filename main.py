43c16796be0531f22149006230902b4cb4a3b0d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:39:05 +0000

    lesson computer increase anyone usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+print(os.getcwd())
