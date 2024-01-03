2cc01395b4c09c9199c84cc349a3f895e49282c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:24 +0000

    answer somewhere keepup purpose arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
