cad1a0e6bbb7646bda79eafefe5ab926b3b3c5a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:07:46 +0000

    bet to advantage war lock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sys
-print(sys.version)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
