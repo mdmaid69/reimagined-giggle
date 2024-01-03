f3884380ba65526c2196bc81db48275449092c96
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:24:44 +0000

    precise still married family name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
