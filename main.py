8dc66bb1dac26b7a28e8b3b3ffc018793811e1ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:34:18 +0000

    rate well afterwards share circumstance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import numpy as np
+print(np.array([1, 2, 3]))
