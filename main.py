d430a72720ea536aa49b5dd17b66f90067413b55
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:32:40 +0000

    cruelty emotion prepare centre under

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
