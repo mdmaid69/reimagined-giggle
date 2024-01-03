40322ac37a6cd0b92cd18ec721d31097c5fed48a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:09:16 +0000

    cut nice job boring take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
