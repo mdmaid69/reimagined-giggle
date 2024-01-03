9c1b3c131e5ab0a7deba801da86a1b2a9c67880f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:44 +0000

    dream compete excuse failure characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
