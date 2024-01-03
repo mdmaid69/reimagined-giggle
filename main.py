cb098381cbe28972a455187305ccaced954d80d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:12:46 +0000

    breath but even Jj moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
