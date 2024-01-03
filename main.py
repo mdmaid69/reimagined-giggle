a1d16d6c136250aae599b79c3697866d2097a9bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:04:04 +0000

    involve breakup sore forgive necessary

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
