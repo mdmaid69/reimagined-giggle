297dc761603efe6b6c606b90a390995a9f4545ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:00:06 +0000

    makeinto perhaps load spite war

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
