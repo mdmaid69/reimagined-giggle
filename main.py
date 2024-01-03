48e2c63541a7f0892712a8a0cf08a641d511fc8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:24:18 +0000

    comeover unfortunate obtain breakout search

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def count_elements(lst):
+        return len(lst)
+import tensorflow as tf
+print(tf.__version__)
