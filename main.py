24ef8b3a8e95c3f37bb0a4dc89c4e9fbfc08e26f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:47:49 +0000

    song period word lucky list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
