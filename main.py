445aed00b2aeb5304646eec9d5b6fbf29030ea2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:08:30 +0000

    married dull exist moon hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
