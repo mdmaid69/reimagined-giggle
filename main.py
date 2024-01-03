16338eee0f4db6cc299e41a564c48fca11fdfc2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:53:09 +0000

    evening crash belong disappear mixture

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
