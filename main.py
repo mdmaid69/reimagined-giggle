7f408a1bc101f18f7a0be6e39630c149e86418aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:44:41 +0000

    sound tongue illness quick room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
