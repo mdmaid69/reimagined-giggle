bb79f2fa73d688615b99a870dac172be70308deb
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:10:13 +0000

    war goout whom uponon frighten

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
