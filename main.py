cfb4e6f1cddae2e86f8c88971a678977b41c9bf8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:12:23 +0000

    then right play professional would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
