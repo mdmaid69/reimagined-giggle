f0a9a1861812395370ef909d8f63375f48292b1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:03:50 +0000

    respect range cool temperature cut

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
