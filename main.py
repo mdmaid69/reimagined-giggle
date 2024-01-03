b4c7937a1b3e267c8946770ab7993cccfcc3a94e
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:01:10 +0000

    plenty offence kill trouble surprise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def create_queue():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
