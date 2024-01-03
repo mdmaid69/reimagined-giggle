2154debd894de2137e6b8a194e384f786cf64626
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:14:31 +0000

    wake sell prepare genuine introduce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
