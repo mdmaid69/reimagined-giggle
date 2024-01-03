fc51acf12afbc3e702940d43dc60e17a40ca2d6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:03:15 +0000

    anxious beautiful whatever 567enemy charge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_power(work, time):
+        return work / time
