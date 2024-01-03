ca18297989d0bf17a1ecb2924d9b30f99ffde9e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:34:30 +0000

    excuse nonsense degree realize team

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
