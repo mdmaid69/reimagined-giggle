fb034c84a066e8ffd2adf4e8ccb4c3bfe5bf8040
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:48:24 +0000

    coat sticky top excited before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
