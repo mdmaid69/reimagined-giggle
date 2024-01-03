bff126fda99f45a4ab1699a8b65f05023a060582
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:35:56 +0000

    concern balance float inform careless

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def reverse_list(lst):
-        return lst[::-1]
+def find_unique_words(sentence):
+        return set(sentence.split())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
