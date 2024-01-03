1dffbc75e1c9857ffec558784403ca141911ed55
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:08:24 +0000

    pupil goodbye goback(to) guess proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
