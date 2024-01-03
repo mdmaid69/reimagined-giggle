d2a0ae46a6e7385435fbb8f72e3c5d7e1415322e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:54:26 +0000

    swim urgent used around shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
