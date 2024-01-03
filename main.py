ba928e33a5758d16cacdec6fbdc191afe00ce92c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:47:27 +0000

    appearance contribute type explanation last

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
