49edec5cb1366050faacadf650af8433731c4fbf
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:35:53 +0000

    mean favour machine view connected

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def sort_list(lst):
+        return sorted(lst)
