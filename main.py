68ebdf090899ef5f62a9b1a2c95f9f652f87d75c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:32:16 +0000

    drop branch fair pass science

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
