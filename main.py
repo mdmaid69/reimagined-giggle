e9123abe93a44ec33720f9ea772b0a34c395509c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:41:35 +0000

    expensive typical 564encourage sound age

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def extend_array(array, iterable):
+        array.extend(iterable)
