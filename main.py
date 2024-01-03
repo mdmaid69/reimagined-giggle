9c57b7515f50ac33d64c370b127441404fc536a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:10:17 +0000

    by hard second health thought

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
