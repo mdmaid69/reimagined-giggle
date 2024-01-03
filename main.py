caa0edd9e9d7746033021afccf9a7d5a131d7570
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:42:12 +0000

    drawing intelligent protection by range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
