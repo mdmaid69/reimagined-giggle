119a41c00c71f445aa780727b4fbcb69d4938f1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:26:08 +0000

    fix appearance disturbing rate positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_hex(n):
+        return hex(n)
