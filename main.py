ed0c84eb841a56665bd3180c0d371db83ffc55c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:51:36 +0000

    polite apparently strength frame nervous

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
