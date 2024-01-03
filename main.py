83f27cf18ce5a5ce1006d7699d5459feb2e3a56b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:17:59 +0000

    bring Uu travel before down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
