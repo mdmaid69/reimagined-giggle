1ec78e906a2fd8d1fe91dae1da762e538fbb3021
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:23 +0000

    step confident limit appearance dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
