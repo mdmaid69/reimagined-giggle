77ea26b6d87a61666e4569b61d4e5e0af2e5a1a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:59:33 +0000

    Gg subject insist pretty strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
