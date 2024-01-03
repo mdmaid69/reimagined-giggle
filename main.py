af8b6c3403a2852b7a6517e290626df279353e82
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:44:15 +0000

    minority phone always there miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_string_from_array(array):
+        return array.tobytes()
