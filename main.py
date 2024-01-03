93b0f8b688d4bad1cc1f1251616ff5e21430df09
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:21:53 +0000

    foreign purpose face pupil makeup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
