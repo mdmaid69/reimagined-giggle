43c58a8553a04c4c61f5053b3052e89ed557823a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:31:16 +0000

    suffer turnback behaviour their before

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import numpy as np
-print(np.array([1, 2, 3]))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def reverse_list(lst):
+        return lst[::-1]
