16e7f1c8ff7567abbc87befe21bbeae46456901d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:30:41 +0000

    between deep partner spring address

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_max(lst):
+        return max(lst)
