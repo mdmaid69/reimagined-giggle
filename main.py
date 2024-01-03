a5e65373a954cecd3e27cd905fd76ec369db604b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:27:25 +0000

    cause total quarter yourself rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
