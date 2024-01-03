71bd905172f47000d85b133a8884771656cf7e7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:48 +0000

    firm trade position share ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
