633dfd04fbebefede6455a01e7fa31ef1e075feb
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:02:18 +0000

    return base apply disapprove tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_power(work, time):
+        return work / time
