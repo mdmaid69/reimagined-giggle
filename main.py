cfbe8a809053b18e0662d138f5fb7a236ec3db85
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:29:48 +0000

    work instruction exhibition switch change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import re
+print(re.match("h.*o", "hello world"))
