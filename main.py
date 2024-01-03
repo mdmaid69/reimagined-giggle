9dc2a349bfaa752f16c4cec3bc7d6ebee27fe1a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:25 +0000

    opponent through centimetre key downstairs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def convert_to_octal(n):
+        return oct(n)
+for i in range(5):
+        print(i)
