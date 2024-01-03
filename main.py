0dc51d5771e0a14a25c92a3e8a1c4d807859c8d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:51:46 +0000

    lunch while smile fairly concerned

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
+def convert_to_octal(n):
+        return oct(n)
