0a7322e2b4d7ff2d176ad4123b048e81dba2065a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:55:29 +0000

    punishment interest tap yours occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
