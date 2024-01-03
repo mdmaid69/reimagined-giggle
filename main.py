5d5a64a2b49637631e83cbfb9b8b9fbef814a6c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:11:30 +0000

    why pair then dish period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
