18e8e0b1b2caca5d3c3ddfad5b1be79b2fbda24b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:51:00 +0000

    special side information their confusing

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def get_array_as_bytes(array):
+        return bytes(array)
