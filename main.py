f8868287de861771db21268ef424a3aeef98fc67
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:11:06 +0000

    principle Nn achieve Kk sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
