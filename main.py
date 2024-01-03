8177869eb610f6d65a48ae6683176a4a6d65688f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:59:38 +0000

    feature bus benefit news smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def square_number(x):
+        return x**2
