27bc6e4d111ca27f42474e92f1e2a4a231498cb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:37:56 +0000

    runout passage back type interrupt

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import os
-print(os.getcwd())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
