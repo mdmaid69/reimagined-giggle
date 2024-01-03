0c661f451ec9c4c482fd3ff4a64d9984b1b4c95f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:54:34 +0000

    use genuine appeal step solve

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
