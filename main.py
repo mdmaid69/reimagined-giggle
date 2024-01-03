8954f250f97c42eca0a6340ebf758b4838597187
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:22:13 +0000

    forever trap cinema main mine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
