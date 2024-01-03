3450bc9865d10f34a51aba09ca4d45bf53567068
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:13 +0000

    actual health dry abroad reach

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_exponential(x):
+        return math.exp(x)
