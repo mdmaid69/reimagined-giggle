ddbbb757cf0d0a32d120886705c4d6f20fae11b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:12:42 +0000

    relation way accidentally flow quiet

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import sys
+  def get_python_version():
+        return sys.version
