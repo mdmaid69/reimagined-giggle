9dd944939aafd2c11320a7339b64bce3dd3a859a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:22:43 +0000

    juice content reliable meet trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
