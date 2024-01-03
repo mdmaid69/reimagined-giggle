b23e26c8d88356df17947cfdb8e1cf7217bedcc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:29:57 +0000

    than wire snow opposite guess

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+print([x**2 for x in range(10)])
