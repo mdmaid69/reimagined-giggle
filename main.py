9aa4615020c80a9bf845ded24b6a080433274f5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:06:20 +0000

    read fence influence hurt declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
