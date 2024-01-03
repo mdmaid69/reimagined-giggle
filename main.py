4a9a48f3d33020408d9ec175013dc56699d724ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:33:35 +0000

    trouble task destroy no switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_force(mass, acceleration):
+        return mass * acceleration
