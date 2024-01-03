f612f7e0f84ade5a615b8741b23120b6baed0eda
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:43:52 +0000

    beat sore favourite rent scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
