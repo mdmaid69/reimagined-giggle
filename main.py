ca8e57f937fa2d22af869f20d0134cf884e96a3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:05:00 +0000

    strike welcome concerned occasional use

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
