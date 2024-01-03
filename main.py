46b5b2c7d241bf72b42c500559523f25db5b9f04
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:42:10 +0000

    belong own because increase machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
