58765cea021c4ff5d06dcb3189503e390b113625
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:19:46 +0000

    glass goback(to) space tidy spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
