5472e3d5b5d0a0796fec74544a34c9021c5fd391
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:25:11 +0000

    runout kick however Kk keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
