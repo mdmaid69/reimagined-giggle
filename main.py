b74963ad08976720c32573ed3a841c8e4769771c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:30:30 +0000

    left front belief narrow inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
