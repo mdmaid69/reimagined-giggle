7e82f246a170f29319d3e7412ff1f442646a9144
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:16:05 +0000

    black regional thin unlikely bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
