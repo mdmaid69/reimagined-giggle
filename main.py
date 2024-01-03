1fb08d1eaf9d5b72411f14cc1946b62780418b50
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:06:26 +0000

    ourselves duty glass pinch surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
