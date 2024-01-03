3a4d8ba19401b954358dc10fac84865ef65505cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:07:29 +0000

    switch drive repair full round

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
