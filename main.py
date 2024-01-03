dad6300351975e68a9821cf4de88fc054b9735b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:13:29 +0000

    that decision official object rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
