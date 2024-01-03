0751602a3d2955180e3e569eaa92852d5ab749cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:17:34 +0000

    find interesting begin left marry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item(array, i):
+        return array[i]
