c524e05f0a7a045aae809b5696d7132e8e86dd4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:55:09 +0000

    town how appearance care post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
