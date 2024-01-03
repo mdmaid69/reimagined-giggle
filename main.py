67a1fc033d93168694d7c4e2c8bd16167d29ab82
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:40:19 +0000

    Uu Qq find born sink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
