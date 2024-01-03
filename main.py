359e2d14ee1e790c5cbc55590096c07dc84b05a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:22:05 +0000

    border thinkover possibility disturbing introduce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
