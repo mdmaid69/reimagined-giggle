da39cc1b031e0551e8c54c448e3de890d798acce
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:03:43 +0000

    opposite gift shopping clean used

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def get_array_index(array, item):
+        return array.index(item)
