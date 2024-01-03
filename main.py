906723ac8435eab5fc1717c40ea0e2aae04c29e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:58:05 +0000

    punishment beside ground spill thankyou

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(5):
-        print(i)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_speed(distance, time):
+        return distance / time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
