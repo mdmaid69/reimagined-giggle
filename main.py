48d4dca3bade521230d160d39582b4b75fc9898c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:43:20 +0000

    immediate reply underneath weak high

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+print([x**2 for x in range(10)])
