b380a7d2a05f43d480f6461d0aa3111cef0afb5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:24:12 +0000

    kind themselves lookback(on) mass keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import sys
+print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
