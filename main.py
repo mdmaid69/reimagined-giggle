45732037ef20b999377c658ec704dc8f506bf5c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:11:21 +0000

    pay name below pocket trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
