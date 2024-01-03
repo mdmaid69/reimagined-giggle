f8ad96ea995a0d378bc1127f53bd5c609a81222a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:22:22 +0000

    clear rub whether beginning burst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def count_elements(lst):
-        return len(lst)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def count_characters(sentence):
+        return len(sentence)
