a13654bee27c31130e531c5d8a2c92d13d63e325
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:00:30 +0000

    operation to team design exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
