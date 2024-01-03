c92fb8271b51e29e4c36b084fbc66fafa410a18e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:12 +0000

    thinkover rely manner tip instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
