aa0f77230a7bd7bb4ce7179f595930a7c1716c20
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:47:53 +0000

    practice minimum comment very truth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
