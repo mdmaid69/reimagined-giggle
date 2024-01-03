f66374a0afa3269f39746288ceccbd4184598ac9
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:19:46 +0000

    breakin label yes guess popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
