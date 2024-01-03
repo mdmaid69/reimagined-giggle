9ee04a174f77179dec8dbcc781bcbc5892d083d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:22:45 +0000

    doubt trouble callback relation talk

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_average(lst):
+        return sum(lst) / len(lst)
