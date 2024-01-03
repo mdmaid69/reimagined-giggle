aa1a3bdbe5dc521428479e5baf99841be39641d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:08:43 +0000

    quick Ll shall grow wonder

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
