2dec0aea5d8d13d5e88fc4ca5ff4c785ce423bef
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:09:41 +0000

    insist basis ring tip sort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
