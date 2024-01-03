a450265d4ddb2b253b5b2b5490949e87ba6ffedf
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:20:34 +0000

    lucky rate early bet agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def get_array_as_complex(array):
+        return complex(array[0])
