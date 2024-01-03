9f71c9deac71c5c6c5fd9be630f725e3c58a877a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:14:11 +0000

    strange row people even concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_power(work, time):
-        return work / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
