9b05ff5fa8ca3c74ad747a1d79641c6c7bdc8ae1
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:04:41 +0000

    city hot collect offence fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_list_from_array(array):
-        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
