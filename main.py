62972eea07d2fa8c47b1380baceab1fb9bd43556
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:56:20 +0000

    aim light wish promise young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def get_array_slice(array, i, j):
+        return array[i:j]
