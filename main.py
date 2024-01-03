445ead8635a6b9094c5a9c94bd6cafe4eb8b426b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:13:44 +0000

    street wear hand what discovery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import json
+print(json.dumps({"name": "John", "age": 30}))
