e53ea30e25b2ba5744a08e4fb630b9c940c4aec8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:27:31 +0000

    clean Mm university jump name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
