a47c8bf698033e110c5d0ca68fe0964c2d763f58
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:28:54 +0000

    steam office character meeting quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def split_path(path):
+        return os.path.split(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
