f9cb1dad09c4013c14088ae817dd79a48346fecd
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:30:07 +0000

    tomorrow shrink meanwhile threaten undo

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
