6a88d303e7ce00e62ed42c19b9252f449e57e758
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:24:44 +0000

    trousers use brush mistake quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+def exit_program():
+        sys.exit()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
