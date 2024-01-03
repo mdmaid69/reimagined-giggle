789a4512694472945db8730bd951ddae2bb3ec9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:00:22 +0000

    invitation allow plenty public brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_power(work, time):
-        return work / time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
