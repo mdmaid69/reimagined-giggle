151f1244f4a1dedc8a620c0410f46cb2fc0eb84a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:52:36 +0000

    negative collection comeback run influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
