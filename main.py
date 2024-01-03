a01811d8e79b4c041975fe119274791987e12ddb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:49 +0000

    OKorokay sentence drive practice opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
