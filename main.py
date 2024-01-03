3f80fc138ff49c70d187bda4ee8480c5a82ef1a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:34:11 +0000

    hot clear little try certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_bytes_from_array(array):
+        return array.tobytes()
