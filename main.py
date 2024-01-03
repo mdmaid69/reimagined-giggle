336fb4d4f20b0e8898c48a485f1e7cbb49f58a80
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:27:25 +0000

    completely reason whether giveout make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
