51174deae7f2dddd7bade07074d30f517753f126
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:42:32 +0000

    approve throw win always skin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
