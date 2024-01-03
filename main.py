d63bf7c057c18372c40549a3e0cf69972271488f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:53 +0000

    lay clue team travel every

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def count_elements(lst):
+        return len(lst)
+def count_elements(lst):
+        return len(lst)
