31a18f80c1830c13411b7a9140245316a2d9189d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:33:19 +0000

    difficulty male quiet distance strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_characters(sentence):
-        return len(sentence)
+def calculate_density(mass, volume):
+        return mass / volume
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
