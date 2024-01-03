d25cf481067f7641dd4af0b3bbb94ad493f6b63e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:20:07 +0000

    tidy equipment wish physical feel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
