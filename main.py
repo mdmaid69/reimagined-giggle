83c0926d46ea2db5ba6bb54a9a0c27f4c093e438
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:56:07 +0000

    interest damage suckme star divide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
