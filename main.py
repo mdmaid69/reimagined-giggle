d09d13b1d0f121ebba8ec136ef831324a4257633
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:43:08 +0000

    type belief still nigga direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
