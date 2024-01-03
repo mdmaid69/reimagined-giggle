7a3cb841b27935ce95be14dc720fb92b69797ee0
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:42:42 +0000

    pencil action receive score large

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_work(force, distance):
+        return force * distance
