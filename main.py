43664e63586fc447e04e76fddcbd1d0b1c329cd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:03:05 +0000

    handle term loud there spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
