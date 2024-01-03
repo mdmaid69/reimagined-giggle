4237b1cc322215579ab4a8f40980e52a29ce3c0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:46:06 +0000

    bye secret course empty pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def sort_list(lst):
+        return sorted(lst)
