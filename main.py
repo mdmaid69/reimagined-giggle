f2b31c3a2e304d9cf7827bef25417090c5096f87
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:06:37 +0000

    mouth breakup muscle fast guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_max(lst):
+        return max(lst)
