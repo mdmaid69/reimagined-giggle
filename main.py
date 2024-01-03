f5c3eda006015356df9e9aa008b9a43271ed5a88
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:38:12 +0000

    talkoutof separate information add weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
