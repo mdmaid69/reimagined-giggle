4041b064e003b47187c61fcbd23cb7b5baeedded
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:37:39 +0000

    Zz weekend praise achieve manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
