1532ed6c8b8faf8deadc2396a6278f176a8d0d29
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:23:03 +0000

    swimming future pair sell now

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
