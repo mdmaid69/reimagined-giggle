ff8fc11b8602dd5ba2ece6ae51617b961eef14fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:36:09 +0000

    division so child flat match

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-print([x**2 for x in range(10)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
