ac8fd339ddc00c269720dc6e8273dd0e1dcc6f60
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:19:51 +0000

    to defend comeup mad brush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
