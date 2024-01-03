69fca9cd19b11cc5233b0c2a1dd5557790bbc248
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:07:39 +0000

    somewhere create zero flood sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
