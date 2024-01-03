6942889e99d2898d143e8748b6fc71275a652403
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:13:35 +0000

    need slightly entry pure justice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
