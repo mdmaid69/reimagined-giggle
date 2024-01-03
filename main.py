d112924b55b2ec3348f31f6e7010b3932b949cfe
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:03:18 +0000

    holdup floor increase hand shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import os
+def change_working_directory(path):
+        os.chdir(path)
