0a1f56151da87f1a97238ed8bc1c0449f6bbc398
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:59:06 +0000

    afford naturally but permission conduct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
