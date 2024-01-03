c5dd389fea0a04210376b1211d79c35cf5f6cd57
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:46:29 +0000

    suddenly against so emphasize progress

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
