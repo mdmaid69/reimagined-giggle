3360bcd0980aa1bf65f818d07186937d05715cd7
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:33:36 +0000

    keepout earth straight long shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
