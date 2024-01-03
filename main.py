60bb5f61f968ffdb7d5558426a680c3efbb1bc6d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:08 +0000

    gate interruption freeze line region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
