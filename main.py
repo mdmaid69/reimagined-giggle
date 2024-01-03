7e21e22c1e604fbf907fc3d460fbd3dbb221fc4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:06 +0000

    science story judge Tt trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
