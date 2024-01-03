c4eced943fa6bc240e1b1c207b128c6a28c0340c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:27:34 +0000

    necessary close doubt pile shallow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
