79e175980dc5d553cda68de1b496c1ef4624cd9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:10:05 +0000

    party front split confuse 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
