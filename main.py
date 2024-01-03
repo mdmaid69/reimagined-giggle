8f11b2180443c4fe578006fd98da154c225a6b97
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:58:49 +0000

    rather across ours damage discover

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import platform
+def get_python_version():
+        return platform.python_version()
+name = "Python"
+print("Hello,", name)
