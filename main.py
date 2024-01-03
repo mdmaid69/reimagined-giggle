cc41a0276a5e4419995637f4db194cb2fec98ed1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:25 +0000

    row anywhere progress hide over

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
