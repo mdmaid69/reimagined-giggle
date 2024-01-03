8d5d5a06f58c45da6cc33dc9f039a9e91aee432e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:59:43 +0000

    polite film eye giveup word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
