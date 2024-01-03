ec5760e429cce2a14375f5cfd62997e1affac630
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:01:48 +0000

    mother especially pot concerned lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def reverse_list(lst):
+        return lst[::-1]
