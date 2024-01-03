ac801031a54f83cfd180ffd7abe3ac16d061a4c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:49:26 +0000

    top punish listen giveup dry

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
