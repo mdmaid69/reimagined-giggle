eaaa8df361d752c9ba7e0b866ba1876e1b49f6da
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:24:12 +0000

    rob cry now stop report

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
