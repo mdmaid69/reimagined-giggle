2a246bb7d3296d590a08c75b62f99432c2d057f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:42:58 +0000

    apply sound finish admit come

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def is_even(n):
-        return n % 2 == 0
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
