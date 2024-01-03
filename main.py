74c3888aae0b5d3047c860a51f8c2da2335694f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:28:16 +0000

    scream part getout guilty face

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import sys
+def print_python_version():
+        print(sys.version)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
