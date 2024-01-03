1ac2d5ced252cfd6be2f7c4c6d0a67ea2f6f605a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:47:32 +0000

    letter forward everywhere closely suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
