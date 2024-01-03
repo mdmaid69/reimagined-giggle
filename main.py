9e3cc821a6ed2b84aa1329dda8d54436f030f3bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:30:34 +0000

    bus together wrap large every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import collections
+def create_user_string():
+        return collections.UserString()
