55aec60bb7196684ddcb8fa1957a9b40f3a72e33
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:10:41 +0000

    summer this square pocket emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
