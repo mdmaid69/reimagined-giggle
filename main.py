50ba32754dbb935f2668c0ababd737fb246653c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:04:06 +0000

    wire comment be extra check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
