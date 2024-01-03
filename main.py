d873d3c7188bb699323fc323f807131209a672e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:18 +0000

    score comeup unhappy instrument calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_floor(x):
+        return math.floor(x)
