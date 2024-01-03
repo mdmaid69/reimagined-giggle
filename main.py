c64945ca678428eb1d0633234c8f2c6a46fcd2d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:22:25 +0000

    Uu unfortunate secret communication talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
