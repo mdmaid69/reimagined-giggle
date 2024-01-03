25de3acc22e4e089a54e096298206c6aed916e9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:41:20 +0000

    beside throw because middle brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
