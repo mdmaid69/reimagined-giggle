1625100293ce2452282ea1ee6932978ca204675e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:16:58 +0000

    cause body correct form consist

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import collections
+def create_queue():
+        return collections.deque()
