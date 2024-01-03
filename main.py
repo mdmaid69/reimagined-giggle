6d6f0bee5c2b628cc5c2ba2680c02de0f1db179d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:01 +0000

    unkind blame pupil loan also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
