a00603c2849c79e3db95ad1e753b1ecc515b5d2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:08:27 +0000

    exchange give dare interest must

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def split_path(path):
+        return os.path.split(path)
