077867e2ba173de7ee40465743479cf26bc68764
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:56:33 +0000

    complaint glass profit debt interview

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+for i in range(5):
+        print(i)
 import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
