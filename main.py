7cd7bde345d7296d94a492f6390fdcc09aed4fbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:47:19 +0000

    meet definite burst stick makeinto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_item(array, i):
+        return array[i]
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
