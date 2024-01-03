8ff2c9b0b14abd4a2692ce0b6705b48930e3db94
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:28:53 +0000

    sure connected charge block promise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
