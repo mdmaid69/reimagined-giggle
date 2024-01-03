b2e223a20b5102c2de8c0104ce82a4370306be3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:59:26 +0000

    wealth complete list hold fresh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
