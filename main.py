1a69ac07e66f8f27aab979afe38e35919e2f1e47
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:57:26 +0000

    phone left dish forward appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
