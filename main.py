6b4224d5227b0121facd5001ff83284dfe732bfc
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:02:39 +0000

    clearly something weekend thing explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_base_name(path):
+        return os.path.basename(path)
+import re
+print(re.match("h.*o", "hello world"))
