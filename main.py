31094027a9b5cd80655d5ffd3af0081a69a13826
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:35:21 +0000

    567enemy aim exchange day discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_today_date():
+        return datetime.date.today()
