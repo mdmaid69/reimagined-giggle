75b4481d21495988a5086f308b9c2817e9828bee
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:02:06 +0000

    nature wrong copy rare listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
