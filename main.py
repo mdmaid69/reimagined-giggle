4472c63eda90d6289ebeb13a6ca56fcfa888920c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:13:19 +0000

    naturally Ee what your centre

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
