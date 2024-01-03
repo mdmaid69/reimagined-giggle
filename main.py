e4fcf1e6435244b159c0b90ceeb311bf84609752
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:28:55 +0000

    turnup push sad sore partner

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import tensorflow as tf
+print(tf.__version__)
