284b88b0affe870262cf14b715dcb98ac3b5edf0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:30:47 +0000

    thanks that lookaround instead thanks

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
