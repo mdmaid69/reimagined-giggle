7b4dab8ff82a7c847d437c0f073633ea22dd9ffd
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:42:49 +0000

    little lookup whoever period something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
