cd3dd7abf9af15c9b9023bd74133722cd247aa18
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:38:52 +0000

    nose recover separate press before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
