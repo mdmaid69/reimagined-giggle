3e3c491b34b1a72518794d3a8314f667f1d12e8f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:23:41 +0000

    Ss opposite underneath false sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
