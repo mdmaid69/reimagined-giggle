ffc73b5dede69b125a0043189a74e5aad937b6ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:11:47 +0000

    bye vote task dick doubt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_list_from_array(array):
+        return array.tolist()
