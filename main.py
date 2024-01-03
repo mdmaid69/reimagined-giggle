5f577d017598a52c8420c4a8d2a44047f55e8967
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:10:09 +0000

    bound combine insist skirt murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
