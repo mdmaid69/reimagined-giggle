1b1d7cdad984b39845afb109b06f18d42f1fa676
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:49 +0000

    well someone profit later wash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
