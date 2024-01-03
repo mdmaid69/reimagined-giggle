ccf8f7079b0b8b9bde8756e031e7daf232a21ded
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:08:08 +0000

    save downstairs something direct event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
