19c516a68a5367e13229419c49a1b56135630941
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:25 +0000

    situation safety flow card lonely

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
