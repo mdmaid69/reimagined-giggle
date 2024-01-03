6a3f1fbe25ae7de1e8463dcd80c416a50641073e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:29:31 +0000

    free shut light complaint burn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_rectangle(l, w):
+        return l * w
