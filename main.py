fb0f8f8cb6963eaab1e50d7dab4e7a4f89bb68aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:32:33 +0000

    whenever cope definite keepout Nn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import re
+print(re.match("h.*o", "hello world"))
