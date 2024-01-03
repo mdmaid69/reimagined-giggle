f24912fcb1ab86325bcdd7cc90b341c1c1988cfd
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:13:16 +0000

    loud part settle hungry glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
