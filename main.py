dbbe5f8661f90cb44dd47df1cabd9faf41604a3f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:26:12 +0000

    steady skin react escape level

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+print([x**2 for x in range(10)])
