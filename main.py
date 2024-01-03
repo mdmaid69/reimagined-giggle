6d93575fcaeee4051c28481626adcc00e85a18dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:50:47 +0000

    shock ours bringback wealth all

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
