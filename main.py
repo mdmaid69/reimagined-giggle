facf546809bacb67130a123afff96bdfa1506513
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:47 +0000

    interfere example shoulder little class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_min(lst):
+        return min(lst)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_array_to_unicode(array):
+        return array.tounicode()
