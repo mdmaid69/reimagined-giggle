7acfa8ee01f6fefe06e1dd979b918f14e8fb435e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:24:30 +0000

    less lookback(on) fetch image able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def remove_from_array(array, item):
+        array.remove(item)
