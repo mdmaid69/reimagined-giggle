0b21090f1f637e0f9c18e18868ff6891eadd7c77
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:35 +0000

    order foot everywhere cough prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
