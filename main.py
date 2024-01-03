e5f20461705c1ab254d7c5135d312475fa513e30
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:19:50 +0000

    manner hurt reason rest apply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
