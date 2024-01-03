55bb428288dc12b586beb3e52af6cd1821db205e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:24:14 +0000

    turnaway kill proper uncomfortable talkback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
