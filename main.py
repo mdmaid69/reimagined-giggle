5d7e4b60174f2126fdb9fe7787c9f62d1008d9a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:05:50 +0000

    butter sink past copy total

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
