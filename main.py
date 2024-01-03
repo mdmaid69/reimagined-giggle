d010655fec941803b9c57e5ab99379c4105048ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:09:24 +0000

    check ahead both male phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_floor(x):
+        return math.floor(x)
