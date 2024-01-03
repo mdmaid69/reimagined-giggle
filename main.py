96906732da3758cb647d4560fd1286031782837a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:25 +0000

    brush love sort punish recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def get_array_as_frozenset(array):
+        return frozenset(array)
