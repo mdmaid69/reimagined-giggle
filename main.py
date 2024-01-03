4c7de14e6c98069507c0ee024b0a6b116699b3f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:42:41 +0000

    support game quiet guess band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import numpy as np
+print(np.array([1, 2, 3]))
