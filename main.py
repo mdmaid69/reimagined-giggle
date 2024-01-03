7c26fd9b56fb575373825973787504d92e9c2c30
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:22 +0000

    dig profit usual instruction clean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
