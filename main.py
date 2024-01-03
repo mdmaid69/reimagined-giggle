718d47fc6379cd4f14ac6b680796758850bbd1f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:50:43 +0000

    there suit type boat convinced

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def remove_duplicates(lst):
+        return list(set(lst))
