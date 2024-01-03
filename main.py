686e79aef001e22d87aa01172e1d32451c5e7173
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:19:33 +0000

    correction 569enjoy ahead well fortunate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_max(numbers):
+        return max(numbers)
+def calculate_speed(distance, time):
+        return distance / time
