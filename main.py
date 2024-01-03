2f421a3557d854335f6b454f7f6e81c3cbd1264a
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:04:06 +0000

    show actually freeze spoil like

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_repr(array):
+        return repr(array)
