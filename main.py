911e63192b573572a61b9441df62538c5b3c53a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:19:55 +0000

    personal long describe health bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
