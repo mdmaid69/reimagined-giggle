31374916adeaaefeb19f88fbb23678c14ecc5b17
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:17:39 +0000

    second least by suck occur

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+from collections import Counter
+print(Counter("hello world"))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
