a4d0a752a39dca76bc1928438f7cb47b956b88e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:34:44 +0000

    book this suck process experience

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
