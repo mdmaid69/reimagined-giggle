e419b6d03df7bdb527a1e97e26a9b49bb6a6ff6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:47:04 +0000

    thought Tt tend custom awake

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
