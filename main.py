5281bdfc18ecf7dafdcc375e0a309440807bec9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:33:54 +0000

    comment used upwards land narrow

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+print([x**2 for x in range(10)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
