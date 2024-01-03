41a10abffc00f79c57aee52cfa0136b12c4d004c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:15:48 +0000

    cinema write public actual slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def find_max(lst):
+        return max(lst)
