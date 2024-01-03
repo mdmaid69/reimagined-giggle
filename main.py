77a65b51c8bb9563e74af920b84c816ffa80bac2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:53:03 +0000

    usual print left ring life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
