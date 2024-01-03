3b0e30b75fd7f41fab5b365a34b4905f7dfc4a9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:55:45 +0000

    left such answer impression voice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
