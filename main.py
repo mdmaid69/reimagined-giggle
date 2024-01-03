600ee70fa26084330f3a47b563815e3db6d99025
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:27 +0000

    give split wind trap late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
