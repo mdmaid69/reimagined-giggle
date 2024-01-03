47191eb7d4af9f2e92b07c9a8f4983bc93e64c51
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:30:49 +0000

    visit region stone move street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
