abdf0f0ae1c11de8e9f48f02ae3dae4a6cb0986b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:37:29 +0000

    mood tune imagine sport married

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_min(numbers):
+        return min(numbers)
