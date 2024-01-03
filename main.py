47990bcba574085b10d636b944134397b27f1bf1
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:53:50 +0000

    event discuss fierce basically play

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_perimeter_triangle(a, b, c):
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def calculate_perimeter_triangle(a, b, c):
         return a + b + c
