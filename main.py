c40286d227ce1ecc5a3533f8dc2181ce4bd22dd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:20:45 +0000

    circle bottom path warm deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_typecode(array):
-        return array.typecode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
