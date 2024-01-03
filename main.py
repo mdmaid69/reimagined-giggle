9e48f1e938f4082e8dbc61a0d642f0c44c1633a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:50:57 +0000

    Bb deep better back collection

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
