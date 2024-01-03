a7e452469ddf20c3479046548ac1babf4bd88840
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:50:47 +0000

    evidence opposite yourself issue wide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_density(mass, volume):
+        return mass / volume
