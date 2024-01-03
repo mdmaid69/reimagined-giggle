8eeae283f75e52c048895ccc95acc8559596ec4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:53:45 +0000

    constantly pity nothing bet unless

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
