c9d6efacb8482a23afd4d4b75cd80e4124829429
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:36:49 +0000

    Rr patient when buy seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
