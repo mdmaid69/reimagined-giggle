20ffb5ee6715a140f8d74b6ff9cf448cc6f92fd2
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:16:47 +0000

    listen rely secret other listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
