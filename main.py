6d31940a75a54cd70b454b1de14beae99ccff542
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:44:46 +0000

    clear lower deliver lock slightly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
