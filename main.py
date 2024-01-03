1bdcbec0647cd2e071ed2439b0723a4c4a8fed4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:11:22 +0000

    trousers complain strike eat prepare

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
