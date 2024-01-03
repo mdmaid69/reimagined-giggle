4718fbef999fc2976cd98913ecf34e0f785a74b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:02:16 +0000

    worth unusual high open paper

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def multiply_numbers(x, y):
+        return x * y
