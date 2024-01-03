3050eb5d1e1594ca2fd9e06e01294d1df24d18a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:01:17 +0000

    workupto yesterday and previous step

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
