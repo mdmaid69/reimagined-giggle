631edac7147adaec282d2654e55dd5e75f289d85
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:09:47 +0000

    care shadow solve belong thank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_list(array):
-        return list(array)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+for i in range(5):
+        print(i)
