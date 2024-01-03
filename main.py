117fa61634409e1fa02345f257c5302de4010b30
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:59:20 +0000

    difficult shoulder ordinary body roof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-import array
-def append_to_array(array, item):
-        array.append(item)
