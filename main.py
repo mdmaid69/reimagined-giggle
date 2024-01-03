b6fe74f41fd0b53da42941fce2cee4d9c346cee5
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:13:50 +0000

    tired successful number apart and

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print([x**2 for x in range(10)])
