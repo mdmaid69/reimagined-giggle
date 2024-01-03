0243ef981b11a8cb8ea675588dd673d364a3f7e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:33:16 +0000

    extra reduce extra excuse common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
