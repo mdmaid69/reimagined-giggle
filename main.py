68a37ed033433ce25885e57a4364abd06ffbcfe1
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:07 +0000

    hand scheme beat tidy fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
