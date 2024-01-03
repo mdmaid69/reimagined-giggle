dae087ae7f8f5c7a49489f980059bdc655c9be0a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:07:43 +0000

    petrol occur commit exaggerate complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
