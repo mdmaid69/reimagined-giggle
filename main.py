ab2f104d9b6fa41a5e1d66072fab7ebf5676bbad
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:09:10 +0000

    perhaps hope open organize speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
