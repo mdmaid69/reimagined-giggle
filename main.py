6bbe70b02d8ff0c7e4a5d88b240e22db363852b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:00:06 +0000

    accept butter because recently string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
