6b1e519364f4e7af8903230b46208076caf5d7f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:09:17 +0000

    bottom turninto mind model holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
