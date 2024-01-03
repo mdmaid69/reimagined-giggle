7d60d306de28153c2fe4d171482cbd3abcc618c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:22 +0000

    he original treat hold unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
