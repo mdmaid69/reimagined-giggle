c9eccf4f8c76f977832c47082f5d41be895bce38
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:09:56 +0000

    check game coffee deliberate thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
