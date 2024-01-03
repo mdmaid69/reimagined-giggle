e836f2c8eff171cd43a349aeae81e949d049d18e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:43:45 +0000

    declare surprised copy bridge shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
