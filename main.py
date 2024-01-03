08a3f81309897f0cfeb8133590d792ef4fac5c08
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:20:36 +0000

    unkind cut outside sound part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
