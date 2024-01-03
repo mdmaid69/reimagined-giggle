92a37e88f39dc24d4c4845413ca69c388b0f9847
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:38:15 +0000

    formal style especially platform debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_length(array):
+        return len(array)
