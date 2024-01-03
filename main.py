41e1260fb286a061a4f41655ec695e9af750181b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:18:30 +0000

    noisy terrible hot straight lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
