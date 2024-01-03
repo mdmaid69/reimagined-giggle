fc1839c5975e8cac464912e3480496cb8d7cdf01
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:05:50 +0000

    road example father confused legal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def divide_numbers(x, y):
+        return x / y
