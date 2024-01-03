5c004bb6475ac18c9c8b158aac8a7dec68180430
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:49:38 +0000

    block careful negative school sky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
