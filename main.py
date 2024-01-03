f2d42def20a1c64c785c0a69f0d7cbfd8c0ad270
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:46:47 +0000

    upwards wonder replace less violent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
