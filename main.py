5f7ec3131595c0e495e35dd0a0b7326c4dadb962
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:17:41 +0000

    favour hand morning comefrom fault

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_power(work, time):
+        return work / time
