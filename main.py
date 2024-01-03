18b99768dae19ab648060ab4907168c9b0e86b53
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:29:11 +0000

    number file Nn authority direct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
