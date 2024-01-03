e2e70cc7d814099f1ff148a28792a19854c9f89c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:07:34 +0000

    error recognize clear yours whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
