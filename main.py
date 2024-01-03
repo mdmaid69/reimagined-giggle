da64fee8cafa481777cb9a5fbf51552b0468cd45
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:39:42 +0000

    brain closed rub whatever instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
