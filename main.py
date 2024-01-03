13600497eef376a85dfea6ff225b54d71996c037
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:29:53 +0000

    acceptable argument smell mistake trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
