1687d8deb89752f767bbeae9488a73d28f1681ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:53:39 +0000

    talk sleep divide excuse section

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
