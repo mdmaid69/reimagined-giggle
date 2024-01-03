6bee08b083dc428cde5dbf0b2b71f5d95f23a72b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:57:36 +0000

    joke unfortunate block no whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
