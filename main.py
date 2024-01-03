603351a1087f1939bba5893b4333cad24f31ec03
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:57:24 +0000

    firm size flow whichever declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
