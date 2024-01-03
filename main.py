b5b0eb44a453b6c6bb4db52e2745c68781b74773
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:35:37 +0000

    peace pretty benefit her vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
