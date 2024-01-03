7298db2c02c0973aa21efc36ae1ced2bfb6a1c61
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:40:27 +0000

    foot pronounce occur reason peaceful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def sort_list(lst):
+        return sorted(lst)
