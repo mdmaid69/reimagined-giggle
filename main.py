81dff918fa96a7173c3c37a2e087b666d0ac3fd7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:38:03 +0000

    calloff steady aim emotion claim

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
