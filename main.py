803c6015d8b74f6e66d7a9caa09c0ab711062bda
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:06:20 +0000

    leave goodbye now politics drawing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
