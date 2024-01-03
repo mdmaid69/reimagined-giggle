0269ba02121deab9198d0ec9a13f02106f6886d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:56:41 +0000

    stir comeback few claim especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
