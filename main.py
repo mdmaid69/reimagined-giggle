6892fea47d200fde134233c51c32ac5aa9edd504
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:21:57 +0000

    sight getaway correct occur call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
