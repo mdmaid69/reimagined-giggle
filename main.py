abf19452502e2b6749571e48a9dbdff12ff1dcbf
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:27:21 +0000

    pupil trap possible embarrassed holdout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
