e625bf9405805ed864f83fb1182401d1a57753e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:41:50 +0000

    mouth change offend height hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
