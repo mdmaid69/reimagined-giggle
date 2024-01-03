73cee64851db11292d9d862a212dd30282e90f01
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:40:07 +0000

    experiment takeout jump accept bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
