7f2cbaf837adf50d7d8bcd6845f6110a6ca5776f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:47:12 +0000

    middle recently perhaps brain entrance

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
