19e7592588c0b844391bb6cab475fd987e23075d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:45:59 +0000

    though afterwards giveout trick come

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
