dec11c592a2075ec463fefea0a20c7acdf98acfe
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:38:33 +0000

    judge whether Nn shake impression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def reverse_list(lst):
+        return lst[::-1]
