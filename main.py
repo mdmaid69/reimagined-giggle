795ff78727c909fc952078e024680a27a4bf329f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:58:58 +0000

    programme mind flow treat moral

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
