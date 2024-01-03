9aed41bcc701cf732ab25a9ed6eb232dcb1a18ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:22:36 +0000

    tip often central suggestion information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
