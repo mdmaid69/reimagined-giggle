215b215062cd5c6c0966dc7b761b49fecb3f1a3e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:23:30 +0000

    announce careless tape attitude change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def square_number(x):
+        return x**2
