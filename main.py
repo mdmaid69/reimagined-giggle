cb09575a46e572769a20cd02a03ae6dc3a391673
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:33:01 +0000

    unlucky everywhere pen spend sore

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
