6b9f89a604debe5049ec1b87ae96611da9754c4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:22:02 +0000

    cool she laugh face sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
