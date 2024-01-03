132d54adb7274318fc52fc2f1c1f681845931de3
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:04:10 +0000

    detail careless dead alive care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
