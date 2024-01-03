a59f1bf7e92c90985fe930c821116af4f38a6f4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:44:48 +0000

    so advice whom frighten sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
