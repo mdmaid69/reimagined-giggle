2bc94c49a13bd62f9d915819be6860d90f5fd0b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:52:39 +0000

    uniform bend achievement turnout give

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
