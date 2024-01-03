2a99b3b420f577bc10f092c2e69c57e2f61323d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:19:22 +0000

    waste team process wake goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import heapq
 def merge_sorted_iterables(*iterables):
         return heapq.merge(*iterables)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
