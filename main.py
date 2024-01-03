2617a3f86de498b97e056453a132dadfa0347300
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:17:57 +0000

    demonstrate story equal argument my

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import random
+print(random.randint(0, 100))
