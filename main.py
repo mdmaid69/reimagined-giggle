8ead14455e946776b0f27a412afc489f0e152e15
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:29:03 +0000

    slight raise murder environment same

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
