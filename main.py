c9f582a196eab7293517108367b10bed4d597d28
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:38:46 +0000

    practical behind waste so smoke

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def remove_duplicates(lst):
+        return list(set(lst))
