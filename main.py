7c478f577868b4b048f581dfefc4d0e087022830
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:04:22 +0000

    voice sound story gate reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
