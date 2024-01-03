e7619c383233e29c589bb23d041acef12829c6d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:18:24 +0000

    little somehow among sudden serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+for i in range(5):
+        print(i)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
