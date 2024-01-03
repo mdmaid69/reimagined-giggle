3ed304d37af1116f8d7528a61d4b2bf494fbaad1
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:16:32 +0000

    part interrupt poor sound hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-print([x**2 for x in range(10)])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
