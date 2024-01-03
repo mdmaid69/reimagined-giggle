67b63c221aedce6556719dcfa3ac1a6e624bbbc6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:28 +0000

    lock police burn keepup scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
