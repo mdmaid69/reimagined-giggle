5bf07463c3aec97bf67896477cadd4443964d5ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:09:34 +0000

    offer nothing connection swallow cost

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def multiply_numbers(x, y):
+        return x * y
