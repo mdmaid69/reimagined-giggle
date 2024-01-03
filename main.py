22fb996ac2b127c97bb5be78d64990ba51eb620a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:03:57 +0000

    serve rest normal wing violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
