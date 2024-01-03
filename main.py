540f5541598373ea41b2627e96dbad8f7b1c3b8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:15:12 +0000

    tell protest react makeinto supply

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_typecode(array):
-        return array.typecode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
