587ad0cbc831cacfb1997c3c42f79507b9a3728d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:47:22 +0000

    impressed listenfor popular sleep cover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
