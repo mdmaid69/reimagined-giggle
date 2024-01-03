209dcd690df53c3c555ef077a0d48d996ec5e6fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:04 +0000

    cold band argue passenger trial

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-print([x**2 for x in range(10)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
