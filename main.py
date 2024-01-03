2c0b43748da9362073393f4347836a95d338f3e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:49:42 +0000

    reply score seriously account goout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
