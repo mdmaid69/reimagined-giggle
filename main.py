1f8e85012bd62aa6b77829a01d49407a60db2858
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:13:47 +0000

    guess fast police now comfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Cube numbers:", [x**3 for x in range(n)])
+import sys
+def print_python_version():
+        print(sys.version)
