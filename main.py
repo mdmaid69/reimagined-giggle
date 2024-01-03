b0702830eac19dc4ac79665b27295400132dbb72
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:36:56 +0000

    complete refer alone bath plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
