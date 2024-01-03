1ae39cfe2d76a3b85f4b912ab8901e00462c5766
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:59:48 +0000

    vision important soft check trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import random
-def roll_die():
-        return random.randint(1, 6)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
