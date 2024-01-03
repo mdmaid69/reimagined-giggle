967432b9a6e9fa57eb8ebd92af6c071e02045e94
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:34:36 +0000

    key mark influence wash organization

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
