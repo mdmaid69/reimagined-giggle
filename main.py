a2331d6767870a0ed70490395809e5a4d6c4b456
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:17:20 +0000

    slowly respect load have dangerous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def split_path(path):
+        return os.path.split(path)
