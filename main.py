443c07c015e49d19e875e7c24f37fded58d610b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:33:20 +0000

    force he kind whatever part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
