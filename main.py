ce886b51f82de131db67d630b2430a91efde12a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:40:52 +0000

    its by test recommend kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
