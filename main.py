0c4f8943d886f28362eb28504c93687900796eb6
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:19:53 +0000

    organization wish fortunate past fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
