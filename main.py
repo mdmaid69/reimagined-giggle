45ecb2fdf121885689bb7f5a4b39e39506dd05e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:36:29 +0000

    smoke pretend twist myself clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
