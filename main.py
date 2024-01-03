170da06ee284da6bbdd8e31ebc987327ed198682
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:34:18 +0000

    mass bye board thinkthrough meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
