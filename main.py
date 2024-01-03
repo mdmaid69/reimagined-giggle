ea0b4c54c7072f8289175edf989677a7148b8011
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:04:03 +0000

    interested minimum love child climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
