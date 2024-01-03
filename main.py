03e8e775f4a4a5dff0e5a2b54e9fd77fcf54c4f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:21:19 +0000

    early married among balance brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def convert_to_octal(n):
+        return oct(n)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
