ada3ff8c874d430f00f832343f32374dca4d38b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:54 +0000

    precise spot influence word boil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
