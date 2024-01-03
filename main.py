fc635228de54a632cb1f2a1054acae3f189ade65
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:50 +0000

    school urgent satisfied may original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def divide_numbers(x, y):
-        return x / y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import os
+def remove_directory(path):
+        os.rmdir(path)
