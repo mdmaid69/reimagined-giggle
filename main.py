c922154bdf085fbac714a9dcb7cc8f5a5acb529a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:59:14 +0000

    acceptable she attention produce runout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
