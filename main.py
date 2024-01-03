a2bce9aefcbb6f0fc7ce458349cb9567e6a787ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:43:38 +0000

    bright target my complete our

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
