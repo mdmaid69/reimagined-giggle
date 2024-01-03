948e078f3637df8de6c5778059af331af0743baa
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:56:17 +0000

    train otherwise print someone human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def remove_duplicates(lst):
+        return list(set(lst))
