e70590272476cd384f6989c41f442f34a38eb358
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:46:05 +0000

    proud pity cost right cook

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
