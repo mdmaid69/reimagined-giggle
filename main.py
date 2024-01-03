c6687f189f71b947228d3e0acb1389418308ebc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:51:54 +0000

    noone wind appear turn distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
