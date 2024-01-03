33fad2523ff8943202a3fb23eba83db8b6bbb3bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:30:00 +0000

    treat close amount squeeze maybe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
