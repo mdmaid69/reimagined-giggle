4c4680b566bf61d7a48d10b3b4e488605b6b3362
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:12:53 +0000

    phone load tall circle frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
