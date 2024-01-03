867233f9c9975be67d2f8f77d4e398825c136c84
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:36:19 +0000

    judge start personal drop agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import array
+def get_array_item(array, i):
+        return array[i]
