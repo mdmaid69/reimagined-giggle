bbaafce8d7462fc9dc207dc3dc7a509cddb62112
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:53:20 +0000

    circumstance sensitive government and organization

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
