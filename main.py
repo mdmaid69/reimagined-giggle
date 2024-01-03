53ce2f5982b320b041a7b2d650426436dbde206f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:02:06 +0000

    find regret everywhere bunch fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
