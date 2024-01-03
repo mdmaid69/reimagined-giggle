2296e522c47c9f06eacda2a1b64bffcc8b314ced
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:19:23 +0000

    contents cheat confident bright hide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_floor(x):
+        return math.floor(x)
