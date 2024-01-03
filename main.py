0456c76048e0d2ed19a5a30c9a9c3e36141a7423
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:11:44 +0000

    middle shelter sensible sometimes married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Characters:", len(text))
