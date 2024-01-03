56afcd44f22dab5066592e37a9672341b3a69125
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:06:25 +0000

    declaration emphasize city giveup free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
