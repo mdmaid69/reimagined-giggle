5fdc10b0a29dc4562f32062b1de25687c8e1947c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:02:09 +0000

    catch steep knock throughout disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
