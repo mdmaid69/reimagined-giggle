0792343a6df1f3c164d077491ce31d791fb38595
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:34:48 +0000

    intend wander very passenger mouth

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
