39bad697389514cc0ba0c9c7e0c8a2c6d7ce4810
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:41 +0000

    month strict whom deserve imagination

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def append_to_array(array, item):
+        array.append(item)
