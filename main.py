e110adb68d6e4fc2ab093ce5c0b776fc4d1e8da2
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:49:09 +0000

    send complicated government any makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(numbers):
-        return max(numbers)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
