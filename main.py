b38e21a11758fb8a252d83eba7e5d8dbacf39564
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:04:27 +0000

    culture approval lucky stare eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def divide_numbers(x, y):
+        return x / y
