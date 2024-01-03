b78584f9eaa7499a78906426532fcc3c7ad905e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:44:49 +0000

    life busy second no care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
