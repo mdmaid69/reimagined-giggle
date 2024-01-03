27c942ff2f390352e9935de13005d84d0cf339e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:15:26 +0000

    block attention lookafter machine advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_floor(x):
+        return math.floor(x)
