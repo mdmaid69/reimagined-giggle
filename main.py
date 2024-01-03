0d1cda05d2a239c2943a689c2881b0f11d674980
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:47:20 +0000

    port false Dd frequent please

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
 import csv
 def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
