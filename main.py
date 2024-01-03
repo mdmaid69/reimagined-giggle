b3338c29439f3024ab6e9655a73163d8c00268d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:40:23 +0000

    rough will space telephone love

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
