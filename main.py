2e2d4e763cc6547f0c3b44f0f0f7ff8f7f2750d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:52:36 +0000

    file tend seem equal drop

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
