c94a1e35b9e6f4f63d42a3b4cff00c2962260622
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:06:19 +0000

    refer hand shock waste holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
