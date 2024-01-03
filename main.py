f3f59adcf7700ecdff3fd594cfc04b41fc763f9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:59:20 +0000

    concentrate failure spot Vv curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def find_max(numbers):
-        return max(numbers)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def reverse_list(lst):
+        return lst[::-1]
