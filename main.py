9aa1a05a79c8b420c10e077ec6d1cf75bd1ba865
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:23 +0000

    artificial offence thought accept purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
