96b07a8ef4fa59b006893d99e940527a3b2c4482
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:53:30 +0000

    clean turnout destroy light positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
