aa2036a47ac43e704fca4c74b5e76c1dfa69acc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:33:04 +0000

    fetch worried shine least claim

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
