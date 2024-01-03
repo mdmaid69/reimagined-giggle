4f0ac52d4c5ded9cfc1eb18e6d972c274ecf5594
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:07:06 +0000

    to possible supply guess relation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
