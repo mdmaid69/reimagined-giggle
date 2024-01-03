75c18e174950b60edabeceae9e2fe0d995c2bb97
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:26:50 +0000

    final oil appearance confusing like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
