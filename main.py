115992d8657031f3c25e2a3846bb1649dc53a6e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:33:27 +0000

    look storm powerful who interfere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
