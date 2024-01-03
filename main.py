a099e8355277c3ae3a751a6010d7a9ad714c65a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:48:22 +0000

    influence 570enjoyable chief listen trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
