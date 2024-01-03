b150edae896d025a38be7312f45920e672e471c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:12:20 +0000

    marry happy wrap glad emphasize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def convert_to_hex(n):
-        return hex(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def multiply_numbers(x, y):
+        return x * y
