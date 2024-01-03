4e54566132cf9d438c72dfa1dfba8823041feea7
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:19:17 +0000

    news offend content middle sea

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
