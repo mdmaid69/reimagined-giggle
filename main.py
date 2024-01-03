5bcb00162a7437333d0a897827d0664c7c7fd0c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:20 +0000

    below press label offer apologizeapologise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_set(array):
-        return set(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
