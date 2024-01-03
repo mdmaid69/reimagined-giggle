94a998006cd30952633fd25d10a0b29e0343d345
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:53:46 +0000

    cover doubt explanation perhaps still

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_exponential(x):
+        return math.exp(x)
