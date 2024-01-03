12c95f45f238648dec8c8338ee57bd7cac0f13d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:45:08 +0000

    importance eat cap 573entertainment slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
