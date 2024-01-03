2aba2ec91fb247fddd575f52927ec8ddad62918d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:34:18 +0000

    sorry classroom yet prove hurt

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_even(n):
-        return n % 2 == 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
