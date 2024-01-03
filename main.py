fd08843778c5b85a45cf89c082dc8ff380ed29fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:35:35 +0000

    weak race shape borrow poison

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
   def plot_pie_chart(labels, sizes):
         plt.pie(sizes, labels=labels)
         plt.show()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
