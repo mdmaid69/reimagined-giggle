11bb465a7f8af10bb049307204248a3d7b8abc7f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:00:24 +0000

    flavour long dance medium sometimes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
