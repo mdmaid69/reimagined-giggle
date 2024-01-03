f17797d6e53f61991a8079ff4aaab46792bd5e87
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:38:47 +0000

    getup wear keep side actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
