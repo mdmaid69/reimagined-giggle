e21b30704b2946babe74656038b229377c57d019
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:48:57 +0000

    Oo write call certain difficult

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
