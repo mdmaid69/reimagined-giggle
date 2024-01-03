f18d6b69a6a2269bb720c29ba5479aade6a27399
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:47:19 +0000

    pay himself force fair brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
