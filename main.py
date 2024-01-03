1a0a5e8a3f3720a39bfa22508be7145e162083eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:07:13 +0000

    flood past along impatient soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
