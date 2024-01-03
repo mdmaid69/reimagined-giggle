a8b26baac51386da225f60852ff9d43eedc92185
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:31:21 +0000

    stamp comment university section fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
