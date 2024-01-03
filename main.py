176797d4c3012c116eb342f2874186fd6f3bdcbc
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:45:47 +0000

    distance bet theirs organization union

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
