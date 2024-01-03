da7fa566c9e8c2bb13c72cc008c1b4f07f4b8067
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:52:30 +0000

    system rest talkback impatient exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
