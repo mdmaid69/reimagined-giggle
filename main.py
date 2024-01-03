fc65db89deb4a4fa8eb73e585293356361d47c03
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:24:29 +0000

    occasional pocket matter study else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_string():
+        return collections.UserString()
