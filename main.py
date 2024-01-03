f05617a5d9286ca5726087119bc29c5cbb3bb548
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:40:51 +0000

    equal path lower shame difference

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
