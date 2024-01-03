3ffaa7028138544d8b80cc0884965482ed51f6ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:48:05 +0000

    various present fashion callup over

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def find_max(lst):
+        return max(lst)
