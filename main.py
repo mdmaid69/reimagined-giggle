7b492e403f98c27691828278fab0ca252ca68da7
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:29:06 +0000

    disapprove stamp complain return reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
