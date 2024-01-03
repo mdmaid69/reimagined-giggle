e79023f96b47defb8cb9ebc5f1a5396a6f4289a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:37:14 +0000

    impatient relax common broad slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
