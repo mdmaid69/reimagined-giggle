6c4085374dba3a17120057fb6e44c99912514f9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:41:12 +0000

    tool division connected command shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
