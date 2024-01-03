ab909dd9692ad40afe1d3defbff7df982ad431e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:38:12 +0000

    strong lookafter remain shoot if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Reversed:", text[::-1])
