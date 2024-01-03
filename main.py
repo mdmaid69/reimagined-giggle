5341d61364781f258d2044038b5d4fc1117c72f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:31:27 +0000

    wear characteristic complete treatment know

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
