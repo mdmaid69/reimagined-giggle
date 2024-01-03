4fbd425c947a365136fe45322925bb2e64f193aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:21:34 +0000

    eat his myself error choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
