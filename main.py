2a31ec917b2dd0f8de086fe59f9215005f1cd35d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:20:33 +0000

    wherever face worth direct train

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
