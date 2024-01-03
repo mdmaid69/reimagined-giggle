2714de216410895937e570ce49d0d2e1306475fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:18:41 +0000

    silent decide exploration offence before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import time
+def get_current_time():
+        return time.ctime()
