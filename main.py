f25d3f7b28b9f22395ca85e7b6f4ec7a4fb1e536
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:50:15 +0000

    consult fly children fat account

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
