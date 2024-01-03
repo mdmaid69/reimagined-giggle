3757c9d1aead2f2b533deb59b8a2e44a150b1a69
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:44:42 +0000

    tooth pronunciation air relief front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
