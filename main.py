ae8a2ec435109b9bd19e0e7d6f80f5aa4186e850
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:43:17 +0000

    region impression delicate order interrupt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_density(mass, volume):
+        return mass / volume
