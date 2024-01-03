fb79c3fe55fb501c3d8ebf9e78b25362a1b7b468
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:11:00 +0000

    run takeaway temper wake declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
