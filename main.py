fe5d34718737a8e97dbbb0a5878559b3e1af209e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:31:40 +0000

    estimate concentration best idea second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_exponential(x):
+        return math.exp(x)
