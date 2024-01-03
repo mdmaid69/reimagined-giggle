a5f442b1858e30d45a3fd6942511d4764af96894
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:40:58 +0000

    land what address free well

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
