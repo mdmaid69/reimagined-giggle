1eeead3640a60622c095cb3685fba8163ec04bec
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:11:20 +0000

    guide remember naturally so wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
