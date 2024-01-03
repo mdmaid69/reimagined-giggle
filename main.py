4adea892c8fb53d0fa3f71096d3ecfa7162f8390
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:32:13 +0000

    colour reliable bored rely dance

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
