2c937c2a10b0ec10e927719869e369f16728f6ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:22:57 +0000

    absolutely car equipment legal crowded

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_binary(n):
-        return bin(n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_str(array):
+        return str(array)
