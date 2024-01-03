94eb7ae1a998df47d8ec800ea1c00b245d462de7
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:02:16 +0000

    go remain contact unusual comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
