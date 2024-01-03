d1b948d22a8b9fdc4c9e2d4de8734011193e8784
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:44:19 +0000

    careless positive away sure guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+x = 10
+y = 20
+print("Sum:", x + y)
