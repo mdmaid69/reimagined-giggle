34253ac6c3d5a481da4ee07c990d91947e04fa61
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:35:33 +0000

    among comeup meet Ee wonder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
