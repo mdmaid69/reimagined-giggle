06fa93b3b2abc0edf613ebcc8be27ff4006053d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:50:16 +0000

    goup giveback best term lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
