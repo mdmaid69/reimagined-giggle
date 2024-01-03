f8b8447e1150e763fcf63f2e9fd1f68cdab9d0cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:42:14 +0000

    conclusion harm rubbish discussion commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def sort_list(lst):
-        return sorted(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_item(array, i):
+        return array[i]
