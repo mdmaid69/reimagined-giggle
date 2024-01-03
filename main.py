2f0ac06460179e06a8e7e4153f59968b7eaf3a5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:24:13 +0000

    wave geton dry thankyou Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_even(n):
+        return n % 2 == 0
