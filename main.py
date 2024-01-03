8ba35e94ac3c97585e2ed465f540845986d6c4c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:21:17 +0000

    talkinto cheap comment strike because

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Characters:", len(text))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
