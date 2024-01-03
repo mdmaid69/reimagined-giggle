c212bb191c8ffcdaf0d299e5f9e224a68d336c2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:11:55 +0000

    rid stupid nature above owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
