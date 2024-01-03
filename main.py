77cbdb79805b75679cbd0f28c830095a3ff0135c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:54:21 +0000

    study today remain dry pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+x = 10
+y = 20
+print("Sum:", x + y)
