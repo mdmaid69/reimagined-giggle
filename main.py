cc228dd2b214eea1d2f0ee5bd4f91803d90c7f06
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:33:07 +0000

    ride suppose upstairs murder practical

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_work(force, distance):
+        return force * distance
+def calculate_acceleration(speed, time):
+        return speed / time
