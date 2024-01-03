89f98b91566ccec6a860f4fafcbb80534ef71d78
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:44:18 +0000

    length hard last far example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
