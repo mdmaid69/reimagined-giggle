4ce240fc056b3f061fbe68bb81b443e361694359
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:43:58 +0000

    contact calloff related opposite turnback

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
