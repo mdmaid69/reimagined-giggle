a30d0e4e99cb4e33f850e124973b1c17b3701eb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:23:09 +0000

    praise keep record space invitation

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_density(mass, volume):
-        return mass / volume
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_work(force, distance):
+        return force * distance
