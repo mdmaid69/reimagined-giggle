9ce62dc0711c9300e4ee44bc2231c8b4bc0b8613
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:12:35 +0000

    open demand mess runinto difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
