6590f1eb51955df7b0a1aa8f3d16484d1f83eed2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:20:28 +0000

    country happy issue priority work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
