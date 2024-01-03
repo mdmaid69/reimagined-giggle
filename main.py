98ed325bdd101cb967dcfa7752dbf1f3b489b736
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:22:11 +0000

    reserve surprise Vv waste patient

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
