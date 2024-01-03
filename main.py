2a1533f2b8c84e58889cf5ace106de168aae8b65
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:19:25 +0000

    apart give alone science thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
