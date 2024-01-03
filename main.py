762e9f5b21eceee80b0fca07da6ecbd6c945d94d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:25 +0000

    nurse believe round tense see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
