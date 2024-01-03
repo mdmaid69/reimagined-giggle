752c49c4c006bb8005e5cd320a84a6ebba74f3bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:15:34 +0000

    perform better search near blue

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def sort_list(lst):
+        return sorted(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
