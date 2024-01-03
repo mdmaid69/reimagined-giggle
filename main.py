bd65fbb4cbe2738ac13544b77ff88fd37e3654da
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:08:43 +0000

    Jj description splash preparation more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
