214df5e232047b96b9d5cf28229f0a89e63a4506
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:34:22 +0000

    clothes address equal recent slice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import tensorflow as tf
+print(tf.__version__)
+import collections
+def create_counter():
+        return collections.Counter()
