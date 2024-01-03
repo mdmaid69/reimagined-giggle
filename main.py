d877272f101cdc5bda45ad8ab43a2d8942d859a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:38:38 +0000

    thinkthrough lock lower guide careful

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
