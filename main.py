eea17dce040462dff08fa50bc488e9463f25d260
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:33:13 +0000

    club attractive holdup lesson repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
