c1e1cb9863dbf0822991db0554ac1ce0bd6a0ebc
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:48:48 +0000

    talk minimum swallow remove century

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def is_odd(n):
-        return n % 2 != 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
