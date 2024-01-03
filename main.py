69979d03d090f2a0f7a132252300fd9605d0756a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:39:57 +0000

    joke argue price dress ocean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+print(datetime.datetime.now())
+def calculate_speed(distance, time):
+        return distance / time
