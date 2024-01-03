ff17e3e485c1e2e3fc1ef402e08e746a598cdf30
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:22:26 +0000

    discuss opinion everyone real ticket

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_pressure(force, area):
+        return force / area
