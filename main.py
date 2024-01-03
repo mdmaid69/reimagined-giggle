6f985b7d10db909b6126599bce8c8d6fce684307
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:41:35 +0000

    base country material declaration century

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
