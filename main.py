0624ded0423013b93cce8afddb08590d759b1085
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:18:35 +0000

    lesson unite speech murder pleasant

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
