5582919a9eb9ccd0271cf7715af33a01d4cf4cb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:31:17 +0000

    service want abroad calmly square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+text = "Hello, world!"
+print("Characters:", len(text))
