08c23a8296a824e562f0fff4701defd0bc21a514
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:46:07 +0000

    anyone sticky shadow official congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
