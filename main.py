6ab5f5a02272cb08bb10a1a4acc4a30de6bfddd1
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:46:42 +0000

    wish during task contrast average

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
