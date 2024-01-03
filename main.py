40d56aabe366f82f9f8d62de98432b70ef732fc6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:50 +0000

    pool flavour sit subject difficulty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
