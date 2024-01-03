16b045d696f7cc0921a5e67df1c5b309ffc730b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:04:34 +0000

    keepup ahead holdon afterwards bored

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+print(time.time())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
