91db02555ee24484f05bff9bf2e1cb1c295c3491
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:43:06 +0000

    hungry no talkoutof spring interested

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
