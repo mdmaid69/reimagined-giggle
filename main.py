a9ee42d69796cc17b6f49178e46150f04175abe1
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:51:37 +0000

    worse regard strict process ilovedicks

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
