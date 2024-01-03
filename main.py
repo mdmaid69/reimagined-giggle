379060e410a179c198879d3d323d3ca05a4d45f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:23:31 +0000

    ilovedicks ride modern hair rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
