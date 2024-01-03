cb5ba5af909bf315078f27859c9ad96708c53d12
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:05 +0000

    sit look anywayanyhow tread basic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
