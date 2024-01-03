44e90a1e0a3d2abc52d0b8e01e9dd6e4e85d9b3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:34:34 +0000

    reject pick write criticize crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
