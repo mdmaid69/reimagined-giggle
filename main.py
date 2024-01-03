d8981221119fe3e0ef7b765aab11031141e199aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:19:04 +0000

    measure fence sleep feeling extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
