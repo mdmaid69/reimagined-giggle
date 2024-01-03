6e00ed12a2720e42bd133ce603eaa0f2528570c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:14 +0000

    professional meet 565end talkinto majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
