0f0d2d137dfd90e23b6e6b623cb4240dd168917d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:17:54 +0000

    confused touch failure criticize disagree

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def sort_list(lst):
-        return sorted(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
