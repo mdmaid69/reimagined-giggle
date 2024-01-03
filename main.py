ceba1e308c451db892d4f94b0e1f2f3764e94ee1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:49:11 +0000

    instrument confusing earth report state

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
