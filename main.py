20e09b329c1d560300c97d5d817899fe2d6d3ee3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:31:17 +0000

    judge spoil fit press connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
