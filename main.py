3f30032abe095ede8e4bd7614c52c861de18da3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:43:03 +0000

    church empty generally phone head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_user_list():
+        return collections.UserList()
