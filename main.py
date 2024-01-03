b1e461e871e0d7f3187af7843ca48746e50410f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:14:35 +0000

    impossible list scene sentence time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def get_current_working_directory():
+        return os.getcwd()
