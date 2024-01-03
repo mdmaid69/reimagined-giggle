4aae16fae73db30637c8ce6e2e4e513f4cce26ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:06:27 +0000

    office rid independent mix sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_area(radius):
+        return 3.14 * radius * radius
