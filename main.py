560e999d5c99ceeb3c43aeac644e4c5050475e4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:37:19 +0000

    fear present behave convincing plastic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
