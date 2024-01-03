b446088b5bc9e09e88abeda0606b4d3fb678db95
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:27:54 +0000

    tap traffic recovery mail test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
