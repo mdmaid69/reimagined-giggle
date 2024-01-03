3c82c2687efa82fadca6a02778c6c95310671982
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:47 +0000

    independent task stamp cover brief

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
