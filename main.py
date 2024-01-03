d3d78da4ac149536b5589e1f62c43e23023d9e50
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:46:29 +0000

    see reaction warm base all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
