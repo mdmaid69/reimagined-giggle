092942724a491a55a6232499d9a04e72dc1fa843
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:32:55 +0000

    correct fear agreement border point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
