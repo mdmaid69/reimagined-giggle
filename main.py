447b2a2fd546d4da51ffb3b2901236c24c999a9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:09:03 +0000

    angry scale settle judge exercise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import sys
-def exit_program():
-        sys.exit()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
