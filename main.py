2e0a91790fceea1565ce08c0f5facca9cd68211f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:53:09 +0000

    regional instruction taste opposite abroad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def set_array_item(array, i, item):
+        array[i] = item
