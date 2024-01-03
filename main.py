4773c773cf2d700504400ab91ffbb31807772641
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:09:52 +0000

    story height lead awake bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
