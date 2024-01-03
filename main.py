e6c6a4dcf28b1da1af79527c4b4a93446efc3020
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:37:40 +0000

    strange reply load modern thin

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
