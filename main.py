2e986b5b79dc7e93eb3f6d0c16e717b3ffea359c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:56:08 +0000

    wash you tone him criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+name = "Python"
+print("Hello,", name)
