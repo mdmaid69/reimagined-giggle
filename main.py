3e6d50c8b4f55c90b964d6b77a1f475b1e0f06c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:45:08 +0000

    burst trick gradually back test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
