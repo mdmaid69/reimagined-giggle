d2f8239e0db766e58dad0163e3e38deac6df1319
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:51:47 +0000

    us yet Jj hang guess

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
