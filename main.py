7b90e83b1a2a9f5e469435c5046b582cfd69dcbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:42:53 +0000

    legal break we flavour glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import sys
+def print_python_version():
+        print(sys.version)
