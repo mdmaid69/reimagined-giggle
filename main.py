5cf9b21920096a7268a5a68fee950b96fce812fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:41:56 +0000

    touch Bb great hurry no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+def get_current_time():
+        return time.ctime()
