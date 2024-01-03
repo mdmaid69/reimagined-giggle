ef734d1e6a98bf4794c6df98671a3ad3d39264e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:27:25 +0000

    beneath government hard desperate unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
