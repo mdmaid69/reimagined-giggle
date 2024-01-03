05e04b869302c3bcbbcfb9d43fd311850e2f3d85
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:31:43 +0000

    already mood uponon father bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
