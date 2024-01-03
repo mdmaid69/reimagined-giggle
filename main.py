77587f91bf803a95b1132ec90af3fd085ac09f28
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:11:00 +0000

    discuss whistle loud limit colour

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print([x**2 for x in range(10)])
+def calculate_speed(distance, time):
+        return distance / time
