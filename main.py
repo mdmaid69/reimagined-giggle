1212506651073c0af600daa0913b37ff4ce441a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:29:18 +0000

    getup tax comment point depend

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
