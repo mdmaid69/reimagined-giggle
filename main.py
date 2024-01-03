de9cd01e15e9921ed8ef0965805633f1ec69e62f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:48:55 +0000

    cry read down always perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
