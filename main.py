edffb7ee0768577dd38706b760b94bc10a1c6935
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:00:16 +0000

    around matter its active tip

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import platform
+def get_os_info():
+        return platform.uname()
+import os
+def change_working_directory(path):
+        os.chdir(path)
