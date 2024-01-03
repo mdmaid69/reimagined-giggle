e922d08323d95dc741d2e8b9c9b39d36c0998fe0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:42:23 +0000

    lead OKorokay will concentration tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
