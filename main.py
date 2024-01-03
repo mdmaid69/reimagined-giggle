d2f64c8efb9b2914322b943e4bc85f6c2d5094fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:44:45 +0000

    number either target yourself difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_index(array, item):
+        return array.index(item)
