c44c727ed1fa9396b67deb0bdff875bbb7e18198
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:50:21 +0000

    raise insist quality popular gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
