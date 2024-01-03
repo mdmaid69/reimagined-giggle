cc5280eba254781e13c11e0e66a046a8ef4a13d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:24:18 +0000

    contact begin pause smart grade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
