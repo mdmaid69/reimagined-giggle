3c7f1b8e5e772fbda9150dd54c54e0febb474f5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:29 +0000

    purpose nothing hers present in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
