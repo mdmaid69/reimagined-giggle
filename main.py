a7c82c4008d02bb3de308ee2c52fb65c3c015673
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:17:02 +0000

    gradually suddenly 570enjoyable in rid

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
