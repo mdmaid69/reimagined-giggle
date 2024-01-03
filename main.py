664a661d90bee697d3c27a62380f1e67cda97ce4
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:16:04 +0000

    turnaway wait pupil among dinner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_floor(x):
+        return math.floor(x)
