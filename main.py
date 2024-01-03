036ae5f2e97b5eda5e1ca9326b2b6d6ab001d656
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:38:25 +0000

    pen pair we strange put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
