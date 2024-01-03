2ac597d71e45ad02148a9d273d59109b195ae3b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:12:17 +0000

    such beneath exchange sick follow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tensorflow as tf
-print(tf.__version__)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
