fb3960ec89fe3998b10d02ce20118a83784910e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:16:21 +0000

    runout control action pump me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
