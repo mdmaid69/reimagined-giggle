6b48c49ccab322eadc5301368e64fbdb69912747
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:42:15 +0000

    it speech risk square pleasant

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import os
 def create_directory(path):
         os.makedirs(path, exist_ok=True)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
