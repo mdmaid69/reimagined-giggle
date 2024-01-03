34f4847c515a0ac4810536390d529f7d10e4aeae
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:45:22 +0000

    grow town minimum collection cap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
