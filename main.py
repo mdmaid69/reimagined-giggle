b51822bafc71b31711251dd8b51d7ef6ba28c776
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:20:36 +0000

    kind they direct strange 568energy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
