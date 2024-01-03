95ee5016ff5fa6ead74fba2edb5f06b7d6e20580
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:16:19 +0000

    interfere contact solve load decision

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-print(sys.version)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
