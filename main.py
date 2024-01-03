4656cad2a27920743260305c6320b965e9de1ac0
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:12:01 +0000

    press snow collection turninto lock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def cube_number(x):
+        return x**3
