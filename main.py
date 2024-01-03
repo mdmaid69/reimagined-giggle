88aa8339d60cb09e7d7c407fddd7657866cca761
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:41:17 +0000

    deal shout which practical extremely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
