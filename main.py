b687da2af6442bd32b73e5c3602ee1e9083f81fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:13:45 +0000

    splash basically funny tight share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
