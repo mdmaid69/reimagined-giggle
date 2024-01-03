f2bad3ac66255ad986c066f008f1920e4dead00a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:37:38 +0000

    art bringup interruption book brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
