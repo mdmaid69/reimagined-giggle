acdfcd88aa0a8c74d0cffa19462e004fefd0e543
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:29:49 +0000

    combine minority close cheat immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
