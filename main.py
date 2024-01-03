3f0c2578353bedbcfeffde49b8c46a92cbc36b4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:26:26 +0000

    bridge stone sink path ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
