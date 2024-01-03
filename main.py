e503e706df8b80dcf330e72bb48602ea6fe28986
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:58:50 +0000

    turnoff care commit poor usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
