eaa5c819af5b70bd4dabc6ed1c942267aa4695fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:45:43 +0000

    couple boring show lookup shame

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
