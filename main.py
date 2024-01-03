202e9e4f17ea5b42f211b437a76937e1961dee77
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:07:48 +0000

    borrow hit appeal ours unfair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
