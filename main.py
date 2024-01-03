2d5f7037ac10d9f14920bb74d76764743c9813d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:29:40 +0000

    time change wherever imagination go

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import time
+print(time.time())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
