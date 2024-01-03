6c824e8ce35d0c251dc36dd2c2a168b3eb93332b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:27:35 +0000

    instrument few crash early Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
