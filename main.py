78f3e2bedcde65204cde903888fb654a02bd2f80
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:25 +0000

    improve apparently pipe light safety

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
