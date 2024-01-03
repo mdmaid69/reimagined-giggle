ee038e80b9a9ad1482f0463c7829b6f9d03a074f
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:13:46 +0000

    nice study relative geton thank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
