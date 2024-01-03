7977ac74157da87bb813fd284e94ee8adaff6e19
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:47:50 +0000

    separate rain state along whistle

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import collections
-def create_user_string():
-        return collections.UserString()
+def get_array_length(array):
+        return len(array)
