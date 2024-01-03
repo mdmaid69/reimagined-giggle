95d03ec453a280b55549582057a06e3ffed0cf00
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:52:26 +0000

    lose sometimes touch film affect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
