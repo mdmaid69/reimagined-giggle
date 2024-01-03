6dd71892c5ba71527eaa70172802905465b3f59b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:52:04 +0000

    thought fast word proof meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
