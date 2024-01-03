28cb044e9bf1386bb1eca9a91adc1fb220d85257
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:01:35 +0000

    promise best task tradition pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
