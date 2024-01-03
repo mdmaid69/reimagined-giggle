16efc81e2b4541e54743d8a493d92a5e8126c136
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:04:30 +0000

    birthday may form fasten narrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
 def multiply_numbers(x, y):
         return x * y
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
