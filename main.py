862fdbd0d7795dc1567c9eff0005b0bcc4d94b9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:32:53 +0000

    big seem couple different mess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
