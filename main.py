d4e89f0bd1463a64763afeb74cecf414ddcdfaf2
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:22:14 +0000

    entrance considering view beyond lesson

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_work(force, distance):
+        return force * distance
