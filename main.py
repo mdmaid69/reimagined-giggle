e6a09eeeeff9385d8700c72b80f4ee567719de81
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:43:28 +0000

    himself concentration rush hang even

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
