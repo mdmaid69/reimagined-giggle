91293fa6fae3b54d61dc7997ec38b4213c13959f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:18:31 +0000

    quit pattern bunch note increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
