a4943ddf8b6c00699d234b5ff3551740ad396349
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:40:29 +0000

    cover neither Ww responsibility reject

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
