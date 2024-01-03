aaad8348cd4384a81f5dc1165a29f10c19edc4f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:24:37 +0000

    support admit fit pattern someone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
