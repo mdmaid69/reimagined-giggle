399bafc69bf0f02657e1760c887533cbd67cde1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:34:48 +0000

    lock today somehow pupil most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
