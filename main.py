7f7a2093899dba0742684f8f44705b68dc17ff87
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:51:51 +0000

    interested forgive conversation gap hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
