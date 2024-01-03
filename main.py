11bb4827ac7abd33d0faafc1ea060ab4759c2fac
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:44:09 +0000

    treatment house remain cover snow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
