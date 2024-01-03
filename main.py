e03852c2ca9ed7fb9a8b50cd31363054dc1c1518
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:41:37 +0000

    know myself subject themselves system

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
