03e44d58cb5a470e70ff8934cba372d43d74bf6d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:48:42 +0000

    long appeal independent light strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
