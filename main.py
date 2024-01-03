d0c602cdab2f0ba176ab1ecfc9634879f756d5b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:51:16 +0000

    area squeeze achieve broad powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
