c483049ccc4731ec2811e03204e6afd3c2137a6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:29:53 +0000

    destroy along low careless also

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
