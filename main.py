b2dff7bbf108c76e2f55dd8b4294091620c82b23
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:05 +0000

    someone past instant wash flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def find_min(lst):
+        return min(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
