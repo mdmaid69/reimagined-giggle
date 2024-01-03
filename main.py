a54593f32fa1bef2aa6f93c42814aa24bb500d84
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:27:13 +0000

    related bound surprised apart ready

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import sys
 def print_python_version():
         return sys.version
+print([x**2 for x in range(10)])
