f91f7c90074af4e47774867d99b0dce76afe1c46
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:03:32 +0000

    broken basic human right promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
