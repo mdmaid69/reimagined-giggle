0119c29065913799425a54913676e3a2b8ea4222
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:41:41 +0000

    flow noise mean young middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
