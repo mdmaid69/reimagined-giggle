9a209b8a7043560603ee18817cee5be868f58d75
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:42:32 +0000

    free holdout forget flow talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
