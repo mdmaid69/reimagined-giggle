2ed50813c9f9159c51896b120f9ffa875b06e17f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:04:42 +0000

    maximum gate waste female emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
