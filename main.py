00c3414665d5951056af153a7d0108b00bdeb9f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:53:45 +0000

    basically please lookaround inform adjective

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
