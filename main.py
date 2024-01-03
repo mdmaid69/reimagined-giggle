5bfaf1c2b9be5c96f0e34c761805ef5338245648
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:16:04 +0000

    practice well prefer room left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tensorflow as tf
-print(tf.__version__)
+  def add_numbers(x, y):
+        return x + y
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
