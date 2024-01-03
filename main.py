a2b7b096fc597225f796c94a4d5191e75a3ee915
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:39:21 +0000

    instrument leg bottom shame store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
