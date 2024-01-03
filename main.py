2397d990edfd9185e55a79a0c27c3074ae126fbb
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:36:06 +0000

    improvement declaration bank sign scale

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
