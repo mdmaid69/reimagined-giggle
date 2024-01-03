b457b0c676d08e7be3e1bd85afba38f936c48a9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:05:39 +0000

    touch avoid nerve thing tap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_even(n):
-        return n % 2 == 0
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
