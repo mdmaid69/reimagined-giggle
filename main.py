1a12e60f4631c9c32bdfaf65db24381588ae4e65
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:43 +0000

    belong both business criticism Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_item(array, i):
+        return array[i]
