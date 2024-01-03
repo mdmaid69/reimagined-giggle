c56ef70152c3d8bddb6e2db49a8072c789bca457
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:36:43 +0000

    climb remember available noise petrol

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print("Hello, world!")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
