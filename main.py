c6bf6659b6db20ccfaed95391f4dbbf4e76bdfa0
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:47:15 +0000

    plain ocean exactly clothes between

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def add_numbers(a, b):
+        return a + b
