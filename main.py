4dc7f8f4017e4d60a7c8ac9dd37b23965aac6c62
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:33:22 +0000

    position change sort tired watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
