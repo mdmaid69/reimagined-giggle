3968333cc0cbd42e08244ae61a60ee0b89a36b62
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:40:43 +0000

    sticky cover schedule turninto report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def square_number(x):
+        return x**2
+import platform
+def get_python_version():
+        return platform.python_version()
