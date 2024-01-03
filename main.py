372f08a1fd5117749bce65b26b4cefe998e66924
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:29:46 +0000

    march each arrive man watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
