3a9fbca12953dc5edf917e2493fc0805d5b58b33
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:06:03 +0000

    shadow embarrassing handle during must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+from collections import Counter
+print(Counter("hello world"))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
