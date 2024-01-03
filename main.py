2203ef281a2a8837fe482990b5b17be253be1713
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:41:51 +0000

    naturally government intend seem bang

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
