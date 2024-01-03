069e33759e53ac314d946acaf174d581a1b84c37
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:02:10 +0000

    charge different play left spite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import time
-def get_current_time():
-        return time.time()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
