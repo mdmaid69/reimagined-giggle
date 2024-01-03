7224aeda252ec94ecf94e7f728fdeaf119624e21
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:34:59 +0000

    lean protect shall neat please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import json
+print(json.dumps({"name": "John", "age": 30}))
