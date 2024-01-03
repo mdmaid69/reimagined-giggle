645405e046ce4f06b175e9ffb89a6d0cb95f7c31
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:03 +0000

    similar undo scream fault mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
