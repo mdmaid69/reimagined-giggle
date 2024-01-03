de6a0bf08d6f5e25430d43ca533977bb3f1de4ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:05:45 +0000

    bank office confused correct dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
