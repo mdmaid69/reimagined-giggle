07275b03b1748e12a31797e112d64095fceb0e9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:50:20 +0000

    lower notice sort him goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
