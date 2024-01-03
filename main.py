52b5f1f927f08618859a5e71bd603e99376ea5f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:28:12 +0000

    uponon considering attention tip especially

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
