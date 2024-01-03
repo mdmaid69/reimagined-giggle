1a6ac5c182581e61ce1d440ccbf0b8f0b76bed67
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:24:57 +0000

    responsible uncertain crowd fly joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_area(radius):
+        return 3.14 * radius * radius
