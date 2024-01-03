a78c3d45d0678be84f78491df5753df24263ae28
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:32:40 +0000

    same plenty to introduction argue

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
