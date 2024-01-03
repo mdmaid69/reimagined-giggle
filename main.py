434730667686ea1370bc46c937a7ce7a8966161b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:22:31 +0000

    shelter race shall cope unhappy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print("Hello, world!")
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
