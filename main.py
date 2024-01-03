b87be3eaef9aade078a053e69b02998d85cb32de
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:54:29 +0000

    expression tip repair motion with

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def convert_to_binary(n):
+        return bin(n)
