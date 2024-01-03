22847151c744dc7cb208929447d6db1b558c920d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:22:20 +0000

    level stranger notice smile exploration

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-from flask import Flask
-app = Flask(__name__)
+import unittest
 
-@app.route("/")
-def hello():
-        return "Hello, World!"
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_work(force, distance):
+        return force * distance
