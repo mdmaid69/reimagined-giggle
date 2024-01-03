4bc54d07ea9877bcc98e3d9f2ef0e9a7883421e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:30:02 +0000

    clothes gas wind keepoff country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_list(array):
+        return list(array)
