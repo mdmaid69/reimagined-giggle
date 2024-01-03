37eb429be51218361e0c4f588e8cf2497ae9ae8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:20:40 +0000

    confuse disappear peaceful giveup fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
