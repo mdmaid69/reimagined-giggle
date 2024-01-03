25bef51484e060daaac6f09614b79e4f1ecd4e1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:45:50 +0000

    thin matter beginning exit slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
