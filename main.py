f6c90c330c1c18ad6036eb015aa502191a50422d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:19:51 +0000

    recently thinkover lead rely ride

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
