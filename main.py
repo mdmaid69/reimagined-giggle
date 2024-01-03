efff79d13d2bd3a5e5680ffded5ca679dbef751f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:35:09 +0000

    standard soil can active pronounce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-print("Hello, world!")
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_array_to_list(array):
+        return array.tolist()
+def find_unique_words(sentence):
+        return set(sentence.split())
