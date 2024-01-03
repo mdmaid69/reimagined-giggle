72cd8e63276b796ccd50210b4e64a4b2ad44f150
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:38:56 +0000

    getout passenger rule content bag

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+print(time.time())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
