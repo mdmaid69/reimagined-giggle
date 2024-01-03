c466bdc866c362ad2ed4757fc946cc98b918849a
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:24:24 +0000

    cinema company near uncertain neither

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
