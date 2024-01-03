df5d58c8fc634701a93eba574eca617028c8232a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:18:36 +0000

    suit her disappointed blame part

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def cube_number(x):
-        return x**3
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
