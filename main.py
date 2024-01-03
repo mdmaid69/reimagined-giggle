c4f807d4ef330d8277b6a229a6c8f969e5a3bf38
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:00:38 +0000

    hardly another either extremely interested

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
