673cc3bbfafdaf8e146902f0ffa3c3a79418f4d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:37:58 +0000

    relax loss country spoil country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def subtract_numbers(x, y):
+        return x - y
