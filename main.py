bcdd2ba4f3662b1340466bc4e29586a3fea2b2bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:14:46 +0000

    unusual steady total complain demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
