e92a623a776b3dc766a0d0554137a26a32c21bc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:37:05 +0000

    mistake otherwise theirs callout different

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def find_max(numbers):
-        return max(numbers)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
