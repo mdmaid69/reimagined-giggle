e80827da0cdd5223079cea76c53fef8969b8cd3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:31:05 +0000

    behave life unit regular lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
