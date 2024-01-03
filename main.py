84e1971f0daa75cdba05921c879d3588c48bd81b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:42:10 +0000

    bite shop influence stand please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_username():
-        return getpass.getuser()
+  def remove_duplicates(lst):
+        return list(set(lst))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
