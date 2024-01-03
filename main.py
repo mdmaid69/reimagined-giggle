75967ef3b5f8fe92d620444b79c989f0abfca248
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:18:10 +0000

    clean loan single forget check

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
