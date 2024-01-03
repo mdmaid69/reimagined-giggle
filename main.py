51b0685b877d1e7dd003d0ada24276b5574399d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:41:52 +0000

    kick sort leg flow pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def reverse_list(lst):
+        return lst[::-1]
