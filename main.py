1c0d028504b8d7f6a04c77fff2d77d54ba8ec23d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:12:14 +0000

    key contact price inside do

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
