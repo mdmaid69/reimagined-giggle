7d152468e02ee162830edac50a1a34a704322dc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:34:48 +0000

    even hurry maybe keepout hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import platform
+def get_python_version():
+        return platform.python_version()
