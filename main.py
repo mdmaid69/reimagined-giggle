a6c43f4b3a3d2c47ee3495f588fdd4a2195847dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:34:27 +0000

    quickly train unfair port list

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
