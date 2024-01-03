93d2d75b89f377dfad9c84570bd2418a6afa0ab6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:43:12 +0000

    store thinkof relax people willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_current_working_directory():
+        return os.getcwd()
