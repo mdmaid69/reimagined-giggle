13f18f5e576be9f71711d9b1485b304f74aaea71
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:39:49 +0000

    earn punish operation rescue body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
