5defa9ec0c755acabaaf0d7be345e0f2507374e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:12:39 +0000

    wealth goon out serious promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
