4b90e07576473222690fd1c973625ea630749a3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:01:18 +0000

    justice sort advantage front comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_perpetuity(payment, rate):
+        return payment / rate
