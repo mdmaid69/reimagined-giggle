e0edeb17f9d6a46fffed502c9d456e9b91aef2d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:45:07 +0000

    interested attractive deliberately measure represent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
