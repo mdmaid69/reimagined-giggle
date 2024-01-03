e98c32c9236c88dbae1779466e17cdefb0a4d6a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:50:28 +0000

    extraordinary breakin disturbing handle achievement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
