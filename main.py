8f1a0ec647d8d2425923d7a15989d9ff0560d1b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:30 +0000

    unkind similar benefit at thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
