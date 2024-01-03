c6ea3466cd5628a770d85df25afbde8b7eaa2a63
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:52:44 +0000

    order bath height shake first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
