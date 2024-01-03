1c9cf6f46d0da63fbf573b5464c6114b9d25ca1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:59:12 +0000

    hollow chin nigga naturally happen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
