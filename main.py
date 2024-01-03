eb7e1cbeed3e4e0fdb0008b627a556fc4400e835
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:07:02 +0000

    its dare this battery listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
