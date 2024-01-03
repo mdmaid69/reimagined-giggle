c53a3f4463b00cd9169e3e0b1ede800172abfb32
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:56:54 +0000

    recommend change hit plenty if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
