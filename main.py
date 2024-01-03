ae4b0f8dc2abc0203076a50d3fd8acc667d99596
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:09:19 +0000

    Pp union skin protection match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
