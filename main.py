a1a526586913c0a79d8c761c22141aaaabb6c426
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:53:45 +0000

    quickly needle region sensitive tomorrow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_current_directory():
+        return os.getcwd()
