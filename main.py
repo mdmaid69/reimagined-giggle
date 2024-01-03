2dc099ea920962d00afb86ad8719246271724e48
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:42:51 +0000

    address disgusting last whistle broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
