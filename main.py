680c9b896d773f20b33cb4e73296bb5aa3135904
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:46:15 +0000

    combination several use responsible trick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
