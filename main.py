16e420156325c844850ac8f3f5c2a2285772f341
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:35:48 +0000

    thorough serve short office car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
