e3f6d0d1833dc8d0972dafd8608f178e20c2fde4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:10:00 +0000

    professional challenge keepout rest love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
