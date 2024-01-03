64541bde0f8df7c9d12918e7e73f8bce4fb89767
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:10:10 +0000

    announce usual realize identify charge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-print(sum(range(10)))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
