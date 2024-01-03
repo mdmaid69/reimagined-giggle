ba62119391ecc815774e9e1c1bcdf9d8c0cb6bc1
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:21:28 +0000

    rely cover together big concentrate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def reverse_list(lst):
+        return lst[::-1]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
