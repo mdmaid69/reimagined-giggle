e1ab3281dbd35253b1dcac77894f9ae66517d52e
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:08:34 +0000

    doctor breakinto every near feel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
