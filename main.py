1e5f2d573b2ced81a50ae1e2e6f9a8e23f570779
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:19:44 +0000

    division fierce reduce apply fierce

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-name = "Python"
-print("Hello,", name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_area_rectangle(l, w):
+        return l * w
