3a63478c2f914877e01dc5ddac32cfe512b3664d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:58:40 +0000

    branch spin bear in nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
