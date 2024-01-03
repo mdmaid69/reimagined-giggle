e021841a7a9ced0e9b1fe3c44294c70284ce70a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:00:57 +0000

    crazy slow moral usually criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_octal(n):
+        return oct(n)
