ef23ef84c7c699bba6d447bff48aa6ca4eaaa8a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:32:00 +0000

    little strange born careful difference

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
