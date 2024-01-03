0f337356426000857d9a41952bcf92414569290c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:15 +0000

    innocent intend only official scale

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
