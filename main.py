39fd33d04c71d8ae260e93c3f1a9c39495a3419a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:08:44 +0000

    score however old task understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
