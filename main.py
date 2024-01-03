1336acf09690857f12cee4390a8b5a381e3da7f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:38:14 +0000

    ours project big sleep bringup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
