de732d1d1e43ee13b130039b523569c162fae9d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:32:53 +0000

    cut mess backwards group will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def subtract_numbers(x, y):
-        return x - y
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
