3dbe72abfce9b3ef39f6ae329587c1f57ee125b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:16:49 +0000

    money healthy admit opinion post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
