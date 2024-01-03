4b47d365ff027b0bc612c501be6a5abc8603a560
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:30:00 +0000

    Nn coffee holdup outside suppose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
