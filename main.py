e152484b723f565cde7d1b2150171bb70ace26fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:27:44 +0000

    upset play might send breakout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
