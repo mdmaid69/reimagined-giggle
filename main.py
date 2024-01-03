6c1849bed779a7380387e0451f22bbc98161761e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:13 +0000

    greeting tour just breakdown uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
