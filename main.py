9ff036f41a8c31c1c49b5b429eb5410e05724de2
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:49:31 +0000

    black tense brief convinced active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
