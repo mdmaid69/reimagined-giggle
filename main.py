9feae2de608ff229a4d9b36a5a7bce2427ae48be
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:32:08 +0000

    party pain reliable sting concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
