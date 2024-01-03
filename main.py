362a5043406242a7c0aee35bbfde66e3ca5ba078
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:15 +0000

    eager sing occasion head callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
