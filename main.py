d85b3d062ce7f9aa35cb42cbcbc241c1f88fadd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:07:00 +0000

    correction organization fasten tune commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
