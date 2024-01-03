3b7a4775177cc87c3d63fddf826176ab489ae56d
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:04:43 +0000

    away reduce order clever mood

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
