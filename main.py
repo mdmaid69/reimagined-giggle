6a0d292fd868e1c6c815d8d225ae4740e6ddac8c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:11:21 +0000

    talk break insist blue for

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def subtract_numbers(x, y):
+        return x - y
+def is_odd(n):
+        return n % 2 != 0
