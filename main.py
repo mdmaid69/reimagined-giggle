0336195e9942543ad9d682ed92cab5ba1a12837d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:45 +0000

    finish record stretch follow though

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
