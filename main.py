f418aa8aabc8ebdb4ef9178f643a9de2bc948b87
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:31:13 +0000

    roll downstairs sing refer reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
