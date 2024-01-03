dbb612716c4f4ffaf96543ff020ce2a3e84a1172
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:54:58 +0000

    anywhere tax polite announce music

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def list_files_in_directory(path):
+        return os.listdir(path)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
