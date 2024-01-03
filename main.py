de74b2187cf6e4d004d5508cfa963c1a41316cba
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:14 +0000

    last blind Qq handle sense

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def split_path(path):
+        return os.path.split(path)
