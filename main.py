bda2af1d4b6332d3c4aeb0fad870fbb7458aef06
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:53:57 +0000

    care impression drink better level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
