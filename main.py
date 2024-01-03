fd8a3777a8b62be23b5a0c16be0b2fd5fe8f1521
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:33:28 +0000

    position invitation nervous hurry each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
