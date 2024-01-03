cb046abd620188704fc5d5752735ce70bf7698e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:12:06 +0000

    road ball negative born common

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+print([x**2 for x in range(10)])
