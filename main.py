990974c978a2893e5a051115b76d1bbdee817ffe
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:34:45 +0000

    regional block tonight cheque bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
