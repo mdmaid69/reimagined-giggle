f2e2184ef7db95dcabe15da201045a4c5e40eaff
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:16:15 +0000

    we keepout concentrate gift wherever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
