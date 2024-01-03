1e24ef445a1bcae85d0f0bf50901acf0f6bd81e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:09:30 +0000

    boil improvement new pleasant goaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
