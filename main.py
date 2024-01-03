fca861273c86e2d1d477abbaa9a9799f9e5b261a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:40 +0000

    custom bye impressive bad letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import time
+print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
