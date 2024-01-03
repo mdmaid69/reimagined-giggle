3a495907db94ee044a26e1ec21ffd5c1fe3554db
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:06:07 +0000

    force unfortunate central carry age

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import collections
+def create_user_string():
+        return collections.UserString()
