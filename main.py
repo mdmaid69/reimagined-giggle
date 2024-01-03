e2237174fb8e87917774aa72b74d3c53b474f9b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:39:12 +0000

    survive statement success maybe art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
