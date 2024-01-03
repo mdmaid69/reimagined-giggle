3d4ce8aadf0e0993db9208efffa7780f041a16e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:50:38 +0000

    bar alive certainly breakfast straight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_stack():
-        return collections.deque()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
