88a5dd1d0128c1a716f09e78a7fd9427c0de1a39
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:07:02 +0000

    identify write behaviour event away

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
