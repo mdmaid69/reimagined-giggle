7b0bfb52869fd8be28eb85b588585cc9967f2c6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:25:01 +0000

    leave face bound country fit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+def remove_duplicates(lst):
+        return list(set(lst))
