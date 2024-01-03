7d722174b3ff03c7aba234beb74f5d08f13635d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:49:52 +0000

    wipe dangerous preparation kill second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area(radius):
+        return 3.14 * radius * radius
