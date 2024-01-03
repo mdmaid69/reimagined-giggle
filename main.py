6e48f86bd1755e1670a090731879fd6512fcb509
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:49:20 +0000

    prevent holdup pull nerve whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
