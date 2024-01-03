a505b5a185b9abd349ceaa13bf923bb193979637
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:19:48 +0000

    public stir satisfactory stage life

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
