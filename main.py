285ceb20af26a7072db2df3dc9b8d01791c799d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:12:12 +0000

    range usual avoid least giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
