d0cb53c5d73ccd212f1eb341a19264c061381fe2
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:39:25 +0000

    doubt television silent justice more

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
