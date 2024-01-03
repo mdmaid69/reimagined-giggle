9689087a561e76096da538f12c08c79c3057621a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:03:40 +0000

    label average upset lesson difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_directory_name(path):
+        return os.path.dirname(path)
