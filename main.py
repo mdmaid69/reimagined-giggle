da0f929dfc05d2fd5621a90ed8e6bc21fc4183c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:18:01 +0000

    worse clue down exactly destroy

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import math
 def calculate_root(x, n):
         return math.pow(x, 1/n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
