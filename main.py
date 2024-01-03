a4162082b22c71dfb8b2f8bb21ac5f75d929c7a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:30:07 +0000

    actually difficult vote right mail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def sort_list(lst):
+        return sorted(lst)
