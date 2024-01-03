6f34bf8c25625b547d1d3f2387fb598b6ceeede3
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:16:56 +0000

    female suffer move home remark

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
