0b39cfdaf54de3276f836b41a12b6a08de147a9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:05:19 +0000

    surprised beautiful knowledge down excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
