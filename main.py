2a893f73db8ff24b4c5157ec22db401af60be7ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:12:26 +0000

    straight suffer pronunciation Zz whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
