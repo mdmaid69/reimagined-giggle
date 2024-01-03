0516dbbb8624be52666e9200aab1ee7d4bbdf979
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:16:17 +0000

    left favourite bag upwards tidy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import collections
-def create_priority_queue():
-        return collections.deque()
+import sys
+print(sys.version)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
