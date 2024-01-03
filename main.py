a6696c33d5cded53fa7764ea3c818a7e8916d2af
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:17:08 +0000

    close harm hole live degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
