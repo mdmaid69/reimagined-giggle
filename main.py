faf2f1a99f12df1bcfcad27afb47664dba669fed
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:09:48 +0000

    moment duty seriously relation giveback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_power(work, time):
+        return work / time
