145ff23fccef50a8ed6e48f0417c7c8727d4cd1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:19:28 +0000

    surface actually aware building through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
