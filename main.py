f4375b0232a737b26bc79505f36688dcb1c5e635
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:36:53 +0000

    children standard apart sound payment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
