5708f35b2161590946045bcea027dcc98b1c1255
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:36:12 +0000

    goout term whisper rarely congratulate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+print(math.pi)
