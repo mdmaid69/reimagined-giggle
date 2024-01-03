7bbd566ef4aefe721b40a87a6e4957b7af2a3170
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:44:04 +0000

    ticket concerned contain art ashamed

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def convert_to_binary(n):
-        return bin(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_tangent(x):
+        return math.tan(x)
