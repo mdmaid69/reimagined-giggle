fdd8915115e87cf9051070a39f09b21de0dff7c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:43:28 +0000

    frighten mystery criticize onto suddenly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
