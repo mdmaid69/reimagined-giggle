e04e50d1bf5cc57037813177c4e414ca137005aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:05:47 +0000

    normal thinkover shock make yours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
-print(datetime.datetime.now())
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
