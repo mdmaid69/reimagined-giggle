13a06b095b99bff000ed47b69d54f302d0f0b579
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:45:09 +0000

    their Yy spill cruel kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
