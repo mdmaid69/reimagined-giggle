4c61f473898e0afbdefb665da649349ccda7f719
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:51:39 +0000

    compare help shake lucky used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
