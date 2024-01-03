33b30cdf709b18ae651ead1303563ae9f797ebd3
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:34:26 +0000

    calloff trick attempt sink improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def reverse_string(s):
-        return s[::-1]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
