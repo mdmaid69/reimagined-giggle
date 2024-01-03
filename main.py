4359c5acf2cd62223930f5a4299be4820f7f82bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:40:53 +0000

    pleasure home exciting week fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
