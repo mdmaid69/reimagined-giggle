0db6a32d1d3434fc60c4312f53c1246301281b0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:15:54 +0000

    rarely warm your blame send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_speed(distance, time):
+        return distance / time
