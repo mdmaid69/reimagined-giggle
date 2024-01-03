8bc0bb6a0ff2d1344bf106584b1eac3803f3b449
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:17 +0000

    win achievement shine deserve twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
