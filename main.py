59f007c55727f7875ea516d818ef7fb64ba1e038
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:42:37 +0000

    deep letter keepout left rarely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def add_numbers(x, y):
-        return x + y
+import array
+def clear_array(array):
+        array *= 0
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
