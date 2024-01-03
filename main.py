eb4349ecf56c8f42bc0e1fd11ae9be8b33b7e78f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:46:03 +0000

    matter ought brave hard everything

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
