71b4a0b246cef72ce7ecea07004b936ca930280d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:19:29 +0000

    discuss innocent equipment unfortunate broad

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
