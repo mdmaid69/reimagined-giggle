763eae0f8e0584f966bc8196bf3887ed0276a92d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:20:17 +0000

    Rr nature collection spot behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
