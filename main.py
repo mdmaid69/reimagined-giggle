5794fa7fccd3b448612b08165064e685b3d6672e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:13:37 +0000

    admit tidy project neither sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
