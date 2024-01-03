ca1ad8ff038e1d98bc5d48d38bd6680d68007022
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:34:57 +0000

    finish succeed comfortable ball deliberate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
