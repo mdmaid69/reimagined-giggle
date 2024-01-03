eb1caf7c1fee71bce1cabefdd87f47469d6a6097
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:55:38 +0000

    lower beneath no pen march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
