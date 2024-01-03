d1b0034486c1dc9d38690455725e6dbbcbf2bd0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:58:49 +0000

    exist light ear face cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
