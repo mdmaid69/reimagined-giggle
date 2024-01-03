c44c777b2c3ffa6bc0dac3dfb90e199e6801d9f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:47 +0000

    declare shock patience telephone rule

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
