428cca0260422a6c8e952cebd3d63f3050d3c3a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:26:03 +0000

    genuine pause needle borrow pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
