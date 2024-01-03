44d53b5ce2e1b5ced8aeed9b86bfd86a0617232a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:34:59 +0000

    hide radio sorry rub abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
