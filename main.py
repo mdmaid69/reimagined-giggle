1b15562d5567a952921127d9bf3fa17b32ceb486
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:54:24 +0000

    smile sister run office hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
