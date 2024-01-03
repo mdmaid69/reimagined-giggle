688999b15d0bec90cdf23142e33be0641813b698
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:59:57 +0000

    little doctor experiment regularly mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
