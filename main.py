f94e733047dc90af07a34749923b6cdf96369bd4
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:29:29 +0000

    steady throw set though talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def greet(name):
-        print(f"Hello, {name}!")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
