b400d10de8131eb59a3d39dcaed47542ca11d330
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:53 +0000

    business train extra emergency unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
