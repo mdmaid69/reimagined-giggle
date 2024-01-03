234f7c99318f5d6ef2d0cc8cba00796a901c134e
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:29:55 +0000

    able look talkoutof stranger load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
