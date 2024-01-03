aa9b9b61bc22fbd470b3ecb64f45403ef622cf13
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:22:35 +0000

    container present tired probably OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
