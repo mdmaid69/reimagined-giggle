7b260e5cfa74ed1f3020f24870f91e96b337ab80
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:57 +0000

    ring century choice affect damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
