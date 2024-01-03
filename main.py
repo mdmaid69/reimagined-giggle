7d83c3fbfea55adedfd8ec5b17a2ee464b561267
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:39:32 +0000

    finish reduce other charge precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
