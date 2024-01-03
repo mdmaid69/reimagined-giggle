36e00261a48230e7bdd4921655b7280f5b5309ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:57:30 +0000

    absolutely skill measure market instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
