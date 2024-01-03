78072c289f89b11b3f4618c6b96384f64fb5f1f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:47:10 +0000

    important eager quantity speak goon

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import numpy as np
+print(np.array([1, 2, 3]))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
