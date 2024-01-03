fdfd2aa160dd9e65623ed13d74c78f81361d9429
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:38:42 +0000

    wind sight language satisfactory keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+for i in range(5):
+        print(i)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
