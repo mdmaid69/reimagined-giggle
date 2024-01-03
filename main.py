ca2d07ccfb31b0bcc1aad2a9a671b33f50af52ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:47 +0000

    wind pleasant check impressive politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
