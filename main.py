a3c59dd41f464db2939285c8153d304a72f66a44
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:00:50 +0000

    too subject harm confusing true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
