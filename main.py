1082d4ada5ed2ab6afaae63877e33e2f443e7d28
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:45:51 +0000

    achievement flat part date habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
