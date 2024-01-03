54ee73bd83bb48421911a0c3623f98005da97d49
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:21:19 +0000

    serious somebody splash wrap face

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_area_rectangle(l, w):
-        return l * w
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
