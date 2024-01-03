9cdcbf51a5a6800fc555fc0456a8bee9c629a638
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:57:00 +0000

    exit natural foreign somehow appearance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
