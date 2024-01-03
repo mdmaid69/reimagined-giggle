a70ae7e36f176d6d1a4d098f827248bd65811b86
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:08:18 +0000

    juice sensitive pride reply with

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
