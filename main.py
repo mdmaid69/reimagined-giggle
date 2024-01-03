be2251675033ca98707dc742c8528e28cdbdd5d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:13:30 +0000

    hold luck conclusion increase memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_force(mass, acceleration):
+        return mass * acceleration
