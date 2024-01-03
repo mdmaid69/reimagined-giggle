f45880a58552e727b61f4f23d68e54ee4001956d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:35:35 +0000

    square classroom and similar error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def convert_to_hex(n):
+        return hex(n)
