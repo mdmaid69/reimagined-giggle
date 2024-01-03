79a8a836e1d53467047c2024f34a8cb2921d10c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:02:28 +0000

    one suit fact sweep sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def find_max(lst):
-        return max(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
