624f4ecef5ef36e5d7627e10b4d68f440eca04bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:32:02 +0000

    wonder pen born occur music

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
