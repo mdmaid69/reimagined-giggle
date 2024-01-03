bc4c254e4698cb07536b7c9559c45afb487e1591
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:14:32 +0000

    right rush preparation rise during

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import datetime
-print(datetime.datetime.now())
+def find_min(lst):
+        return min(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
