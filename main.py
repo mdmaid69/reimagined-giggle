186af4d94db554e269d66522141375452bef32d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:45:08 +0000

    regularly yes however tidy opportunity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
