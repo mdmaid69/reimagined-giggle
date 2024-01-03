98540de996bfe069126137407e47e9d34c7ca345
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:31 +0000

    house used comeround dish hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_hex(n):
+        return hex(n)
