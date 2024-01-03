10ebf60e1075688479c1477f68a181d003707030
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:41:00 +0000

    direct fire Tt lock short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
