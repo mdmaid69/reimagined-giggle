b8ce4af2ed346b63f75abf6b420962231d7d4df1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:46:46 +0000

    bye repeat taste odd why

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
