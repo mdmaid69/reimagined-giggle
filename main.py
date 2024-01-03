9be3063f0950cb58dd4159171acc3b1276ce6004
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:06:45 +0000

    guess needle attractive relationship obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
