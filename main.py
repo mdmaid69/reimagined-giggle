2c859a43ded8417017b2f6a3e47002c8904039e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:03:34 +0000

    we sick sink improvement tooth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
