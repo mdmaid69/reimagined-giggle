ad2a7e7b14224791e6f7525fee6522631ecc7bed
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:47:08 +0000

    now learn interrupt from conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
