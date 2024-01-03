636a426673a05b9d520a6b17448768ba77df869b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:41:31 +0000

    behind behind illness flood government

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def count_elements(lst):
-        return len(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
