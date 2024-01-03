6a5009a4033d0414e1a39f451c0e1e8f72c0c95c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:13:33 +0000

    defend name drive only fairly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
