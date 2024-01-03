5ae18efc5db8dfa29976ff7ffc7c6193b3e130a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:03:24 +0000

    swing would slight on opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
