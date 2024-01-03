3873f50ac8035edabf1a8013903ed279e493a81c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:01:57 +0000

    even there safety angry satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
