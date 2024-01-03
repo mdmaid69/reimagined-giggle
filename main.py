e75f7df3364d395bc1a7520458e36773adf7404b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:37:59 +0000

    bin theirs underneath turnoff Ee

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
