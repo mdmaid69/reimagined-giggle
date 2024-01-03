839acc6b788b05c0e858ac680eeb6dcee8c696be
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:29 +0000

    food outside rate present future

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
