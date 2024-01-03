7ea070a25ee9fa44a525bfb9138ae478ebd777af
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:54:03 +0000

    bye deal square shadow herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
