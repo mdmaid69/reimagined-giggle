42140f21f13a988b0c4bdece680affcbebe22485
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:33:15 +0000

    become out destroy as send

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
