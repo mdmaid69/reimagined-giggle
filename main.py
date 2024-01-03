e905a3d0928a0c4fbd405c092d4af0e77b657540
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:11:41 +0000

    illness nose natural tap wheel

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
