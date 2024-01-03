7c453d9d5108fdb79957d3821052c9cc25dd9efa
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:50:06 +0000

    bank suffer creature busy autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
