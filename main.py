2584ef5dc48a63abd0eb1fb5d7306dbcf1d74810
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:37 +0000

    since contain stand style step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
