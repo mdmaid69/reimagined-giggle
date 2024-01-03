28e185b612dd5d5ff0e0202546c6a123d2ec912e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:08:05 +0000

    pocket unlucky be Cc win

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def add_numbers(x, y):
+        return x + y
