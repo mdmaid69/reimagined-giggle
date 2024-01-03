cbb171a36c52f4a8b257518a9d01be25a3c547e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:40:26 +0000

    unpleasant respect doubt personal whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
