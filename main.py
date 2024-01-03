95f7f4d7c8fbc28452cc00a03397e7031a7cc66d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:23 +0000

    answer this repeat dish style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
