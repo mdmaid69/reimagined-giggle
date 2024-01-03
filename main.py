c5934703d075d28b375522058570f38931ecafa1
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:53:26 +0000

    experiment fill plastic search whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
