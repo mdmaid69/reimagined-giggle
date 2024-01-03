a0a9893af93478b42f36fe1b0432535d6e28582b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:26:05 +0000

    yes market 565end punish late

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import tensorflow as tf
+print(tf.__version__)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
