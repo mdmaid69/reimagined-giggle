205901e95be6a934926e3e4038074648af0a1ba6
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:57:54 +0000

    play across raise chase advanced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
