858b6883c44b787460b6a098d5172bbba612814b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:36:54 +0000

    lucky acceptable pinch colour game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
