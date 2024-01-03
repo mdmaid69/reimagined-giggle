f6c362566fc014b6bcea1aa02d1a4bdd131457be
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:39:25 +0000

    empty wave bad hate stupid

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
