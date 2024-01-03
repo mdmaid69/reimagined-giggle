09357ee490d1135e88b2bf23e76f17e6a0498bdf
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:53:57 +0000

    run where impressed notice wear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
