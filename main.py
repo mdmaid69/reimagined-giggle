44a2425bf554a1b0466c8bd605e41aefd779df39
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:08:38 +0000

    next land left discovery hers

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
