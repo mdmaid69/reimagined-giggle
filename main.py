c81e3ffc0c0fb1fd991aa83836cb6160eb112f73
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:01:42 +0000

    ticket it ring forgive wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
