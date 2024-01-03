f38ce7e393a8d3543a3cde495c3663c726ab3c11
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:48:24 +0000

    give voice hardly excited minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
