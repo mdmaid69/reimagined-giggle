65e4fe3b646adb0485181cdb019455bbf5262636
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:14:49 +0000

    giveback unpleasant love unfair look

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+name = "Python"
+print("Hello,", name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
