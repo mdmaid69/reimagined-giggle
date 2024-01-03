ed760b81d47f2c31d49af19e399ae1c516bc9395
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:38:42 +0000

    pupil close each do measure

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
