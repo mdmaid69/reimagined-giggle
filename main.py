70ac3747eb8dfd009ba4fb8f80e1a87ea7374d61
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:02:00 +0000

    wet minute whistle private holdout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
