fcdf5270318d9c14a6261c032380e561c29ad122
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:23:08 +0000

    side drop draw content after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def convert_to_binary(n):
+        return bin(n)
