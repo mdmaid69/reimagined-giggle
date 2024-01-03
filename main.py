806a4fd4c8a68abaab98d6bf39d960ed7c3e35cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:28:27 +0000

    turnon vision your create emphasis

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-print([x**2 for x in range(10)])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
