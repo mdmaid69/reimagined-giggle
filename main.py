e0d3d6718d018dc9893c6ee10f6c65dca7aec440
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:52:01 +0000

    drive build duty boil takeout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_min(numbers):
+        return min(numbers)
