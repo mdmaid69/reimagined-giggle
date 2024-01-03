40fd863f8f47d1894288a0af3c13e7ff006fc7c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:29:56 +0000

    takeback slightly same anywhere takeover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def count_words(sentence):
-        return len(sentence.split())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
