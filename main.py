7e5e3646d5818687b40de98c6929cc5b39979e42
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:28:24 +0000

    turninto hello health strong paper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
