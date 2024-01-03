23c5d6c31de89342f295ea2c7bea7b3735ff29cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:41:10 +0000

    Mm so cross have interrupt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import math
+print(math.pi)
