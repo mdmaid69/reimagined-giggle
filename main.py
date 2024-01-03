573314a53987e2243acaabec5a10365390dcbca8
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:14:39 +0000

    large wash shop imagination hers

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
