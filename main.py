8cc87c2c2d2fd638210ca90b04cadbb088f7ddf8
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:17:06 +0000

    disturb earn heat this consider

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import tensorflow as tf
-print(tf.__version__)
+import random
+print(random.randint(0, 100))
+import random
+print(random.randint(0, 100))
