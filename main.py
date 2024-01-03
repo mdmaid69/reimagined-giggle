c73cd6c3f04a1d604fe0d85a51c1998c62d81d17
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:24:52 +0000

    muscle attempt lay constantly cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def count_words(sentence):
-        return len(sentence.split())
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
