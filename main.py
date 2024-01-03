edba5d08673e89b8183959bfa7744f722fbd18a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:00:20 +0000

    action 179bite firm care nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import numpy as np
+print(np.array([1, 2, 3]))
