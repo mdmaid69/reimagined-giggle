f4d9172540363db6980963568c2a9425cf726e08
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:44:07 +0000

    stop cross pay challenge letter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import datetime
-print(datetime.datetime.now())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
