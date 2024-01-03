a1a0fad5ed9bc3f69c2d05ddd1270b3bfc3683a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:49:59 +0000

    callat upset job indeed comeout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
