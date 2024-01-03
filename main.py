941cba3cc50e88d9d2be68cd92adc83d5d41bb49
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:16:29 +0000

    off fall sleep goaway corner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
