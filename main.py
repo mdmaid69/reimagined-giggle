28ebc126cf7430593c1c9eabb3a604f05131001a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:07:28 +0000

    any mention make most ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_floor(x):
+        return math.floor(x)
