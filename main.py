0f937fbc277a71712b2547756cd87946f5fe3b94
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:47:39 +0000

    important treat part disappointed touch

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
