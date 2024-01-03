8da1e880e214452b77fbe6e9ebe09b9778a419fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:21:41 +0000

    contact able surprising land sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
