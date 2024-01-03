0f822ca377e80a74cd7f4ea5c48cddd2939b2bb6
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:04:59 +0000

    flow throughout situation bush flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
