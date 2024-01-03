c46c488ee3426123d00d9cd865c9f6224d24d18e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:10:49 +0000

    ought quality century should insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
