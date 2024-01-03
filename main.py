e98cf75deb1b272a0af8a7bc6809acdebfc7ef8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:20:14 +0000

    thorough minimum ago introduce even

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
