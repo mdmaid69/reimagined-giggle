865560533d102a32b4f23609a23785a586e51306
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:26 +0000

    generally old often mad phone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def subtract_numbers(x, y):
-        return x - y
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
