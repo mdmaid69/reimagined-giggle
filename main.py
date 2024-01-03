6f5e5a2e127f624167a2f37a85604bb2cf91611b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:56:42 +0000

    pleasant turnout inside unusual unite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
