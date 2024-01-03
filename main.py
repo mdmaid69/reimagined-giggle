9440a09d5f5478b15ce1de18b09204ce28d7ee3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:26:59 +0000

    food water thick better adjective

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
