43dfc37ede560a97d2efe5a630f2ca86a66c6eca
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:05:34 +0000

    cook smell lend interview sale

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print(sum(range(10)))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
