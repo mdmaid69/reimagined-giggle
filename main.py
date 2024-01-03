7e0a892afb444b1c9fad56c70a10580d78e4da17
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:26:49 +0000

    happy face label born rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
