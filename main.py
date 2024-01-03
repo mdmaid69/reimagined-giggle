9ed8c18a2fb001f69f98201073d5cb06638263b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:49:12 +0000

    least police pour continue congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
