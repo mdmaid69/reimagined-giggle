74bcde9d192f2418438f78ea95e44714a8cb56d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:53:47 +0000

    notice certain concern freedom big

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
