db561bb4e03611f0b9e33fec64db41f42bfad94c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:48:45 +0000

    rain consist slightly welcome message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
