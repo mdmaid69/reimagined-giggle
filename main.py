a6a5646c7fc59d6c9d3a9a04358f4a9660797e75
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:17:35 +0000

    male tidy stiff swimming exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
