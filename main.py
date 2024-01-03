4d2e25c7aa0d478778604b6f34922b40c712003c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:49 +0000

    really treat list trap black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
