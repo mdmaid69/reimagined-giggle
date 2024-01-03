669a39ba0efe1a15208d8502bf84b1eeb49a8e88
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:18:33 +0000

    along breakup big everywhere bottom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
