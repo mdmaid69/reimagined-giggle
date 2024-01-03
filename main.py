43ee1d7180a0e9ad89d3d0d509fa8d213184abe1
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:40:32 +0000

    seat rest decision match concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
