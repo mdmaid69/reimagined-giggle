7963a8b39bfc130024cf114273c7f66998ce4482
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:40:22 +0000

    sweep waste spite intend interruption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
