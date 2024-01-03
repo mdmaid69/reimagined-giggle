f55759420de7b28597089554c8013d615964d676
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:54:41 +0000

    freedom bottom turnaway takeover wave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
