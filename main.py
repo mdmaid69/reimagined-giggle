d9c6413ae2a1bfd15d6b14a961fb5783f5d02b68
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:16:11 +0000

    target question period frame police

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
