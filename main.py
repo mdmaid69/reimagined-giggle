d7af473638e6441a71e7b96e60432080f014446f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:14:04 +0000

    begin well human fold once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
