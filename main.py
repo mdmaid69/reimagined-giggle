7822c256c17fde55b5d761ca53ad9201fab282a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:43:47 +0000

    or shame attach break letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
