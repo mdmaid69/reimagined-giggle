8ead13a97954f55a48daea340e59fc59dba7fb21
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:55:08 +0000

    begin hurry apparently turnaway underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
