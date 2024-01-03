c43fcfcc408b846e196c8fea8c33319d4e5e3204
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:32:12 +0000

    meanwhile accordingto fact sometimes direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+print(os.getcwd())
