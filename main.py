633d475b097261d80faab7c1e26102443751516c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:08:24 +0000

    environment crowd clear run race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
