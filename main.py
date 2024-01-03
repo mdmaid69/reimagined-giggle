1c09631b444bedf74ab35ef914e1296639f89c1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:34:14 +0000

    through turnback grand evidence awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import sys
-print(sys.version)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_volume(length, width, height):
+        return length * width * height
