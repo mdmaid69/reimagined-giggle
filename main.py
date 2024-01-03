fdf4290cc182a2376c55f5a815216bc8a77f7cee
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:16:22 +0000

    more tape live square treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
