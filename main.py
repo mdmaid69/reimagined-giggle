0e92c08aaa3e628bc7090f23f6cb3e37ec9a5ec6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:22:05 +0000

    artificial favour important rid plan

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def sort_list(lst):
+        return sorted(lst)
