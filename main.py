fa330dbba72fd3a17959e200fe7511f166dc7fd0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:10:54 +0000

    frequent than impossible getoff Rr

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
