91cfa8b4b0018e8f5eba978619181ea48bc8bfcc
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:09:14 +0000

    impatient scheme possibly board comein

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
