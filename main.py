c9df5049ccd3356981a9b8565975c730c59268e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:01:01 +0000

    geton hit sound really none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
