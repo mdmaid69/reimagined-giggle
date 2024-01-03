5a1a68158e79cc0214a4b9a71ce7c79d7e8913c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:54:51 +0000

    impatient inform sink already prove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
