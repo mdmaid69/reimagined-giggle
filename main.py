3470f62935f5b9652584612a32ca674f92b4f59d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:11:06 +0000

    number little roll dear maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
