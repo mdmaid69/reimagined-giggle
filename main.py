d913ea95153a89121fcc3e699c714190f5f7eca1
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:47:12 +0000

    she deep wide plan late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def clear_array(array):
+        array *= 0
