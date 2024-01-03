f1d4497a0a9fa8a1ba4e9e353736c94e92b69369
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:43:16 +0000

    woman pin face temperature responsible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
