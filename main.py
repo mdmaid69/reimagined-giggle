1a3a17b54e10b858206c3d57761c34d5ac631663
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:49:49 +0000

    lookup force responsible government stick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
