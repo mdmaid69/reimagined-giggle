622ab80c43d6aea32741a0715f194b9382f84648
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:06:49 +0000

    consist damp cut dig slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
