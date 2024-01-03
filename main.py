209178d19cb601372cda00d4fcf7ef5cb33ec76b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:07:13 +0000

    stamp expression damage watch fix

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
