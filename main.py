42e1d24035b1d3a6f20f8d7d0104f1b4643c6cdb
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:32:24 +0000

    sick no platform blow shape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_area_rectangle(l, w):
+        return l * w
+i = 0
+while i < 5:
+        print(i)
+        i += 1
