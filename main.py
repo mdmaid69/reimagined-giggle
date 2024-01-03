1f20f97e4c07d673b50277dc75b6a39f42aa160a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:36:08 +0000

    mix behaviour bush standard suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
