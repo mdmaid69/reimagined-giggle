fe5dad82e5753ed2c3c819a221342be3c9f12661
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:45:16 +0000

    cruel remark criticize preparation remain

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def find_max(lst):
-        return max(lst)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
