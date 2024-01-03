0f70b3f94f6bd70946c6afa01655918f1f0fcff8
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:37:18 +0000

    object Ff dry brain please

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
