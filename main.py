5f3bdd567e0ee0098f7cb51cbabe0599e97d1c7b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:03:31 +0000

    payment description run light matter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
