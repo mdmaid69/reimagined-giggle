f9fb1974b5fb63d6ca0c7be2b09c037fa2c1e026
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:40:15 +0000

    drop calm right 573entertainment share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_floor(x):
+        return math.floor(x)
