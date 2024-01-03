6fa2f1e6f0f1ddea66d725970f884fe73d336d5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:54:46 +0000

    father pleased most sure mistake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
