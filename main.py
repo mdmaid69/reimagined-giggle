05db075c92628dcd1c74e040f73be0fabd38b314
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:02:39 +0000

    advantage tax come loss machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def convert_to_hex(n):
+        return hex(n)
