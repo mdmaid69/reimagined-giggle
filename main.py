091793f8bb4e9adb5e0affb6b73e1e73db8038f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:42:53 +0000

    peaceful breakout beside deliberately sort

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
