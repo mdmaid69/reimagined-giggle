32deb4366dcdc8360e8fd9889bfb41d3a9876f22
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:36:28 +0000

    season fierce play favourite custom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
