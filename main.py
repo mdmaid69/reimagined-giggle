aef8891ee17218ebcf9fc7148a1a98a2b7ce6376
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:38:02 +0000

    bother mind turnout rarely by

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
