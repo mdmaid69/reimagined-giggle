4b41c044f99e80eeda9308d951eeeeefabf163ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:39:47 +0000

    smart lean under chin bunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
