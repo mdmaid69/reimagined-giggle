db40c3f8ad065418af7ea1c1a2d1cf2936fda5fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:00:19 +0000

    well under underneath then proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
