d034b1b8da0a4bae6f33e72d5f1bc5e7821dad42
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:10:41 +0000

    shout original repeat instruction vision

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+for i in range(10): print(i)
