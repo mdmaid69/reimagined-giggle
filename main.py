b14fef8c4400643929f31c34c4c831b6e519fab8
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:37:54 +0000

    fresh feature pause threaten sweet

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+print(os.getcwd())
