2e813a2752437775e4d60dbacb614e56b75b25a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:58:06 +0000

    Jj getout walk loud hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
