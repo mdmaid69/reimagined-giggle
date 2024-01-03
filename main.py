a33b6e4ba50dfb9011df3e7a8988e7a2c43b61c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:17:55 +0000

    explanation speech breakfast steal murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
