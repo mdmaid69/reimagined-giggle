c8714fdfaabac3d122ba1b62d5bbfc8aab6b8f1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:17:04 +0000

    national purpose comeback season card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
