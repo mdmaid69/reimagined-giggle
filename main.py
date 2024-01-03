ef5cb14192cbaab97c341818670165ef21ab9d6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:02:37 +0000

    practical satisfied stir base mention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
