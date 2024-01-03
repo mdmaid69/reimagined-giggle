6bb399d400d91b7b7510652a6a615cf6d8ebc354
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:59:12 +0000

    home something past treat their

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def add_numbers(a, b):
+        return a + b
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
