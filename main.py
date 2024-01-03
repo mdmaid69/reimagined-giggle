83902bd80fbc481a5c2ad3ba661f7addfac92f99
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:50:07 +0000

    skill teach development die watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def remove_duplicates(lst):
+        return list(set(lst))
