9339bc34154394d56f87e4093ad711ea87078182
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:35:08 +0000

    willing thirsty top recover share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
