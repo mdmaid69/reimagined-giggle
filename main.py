7d5f7fe917ad576940efe9c2e35dac738a6b6647
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:08:10 +0000

    loss hospital neat empty responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
+import sys
+def exit_program():
+        sys.exit()
