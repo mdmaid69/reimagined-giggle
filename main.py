aed9d6f5ea9a2804d2bd75bac28370a648cfb671
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:32:43 +0000

    dare at tax position slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
