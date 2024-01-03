c7736a1efe67cda35f556acc5a8734a58b7f804e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:20:50 +0000

    result keepaway regular hit play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_environment_variable(var):
+        return os.getenv(var)
