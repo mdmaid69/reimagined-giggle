4604405461ea36f2f01e9596c9981684056879b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:08:18 +0000

    fill handle sort trick conduct

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
