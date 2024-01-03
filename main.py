5e1de2775330a0798865bd5e9c5d0e5f98663a43
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:47:17 +0000

    spread together workout pour crash

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def cube_number(x):
+        return x**3
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
