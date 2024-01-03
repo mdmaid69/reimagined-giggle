5fcd9991f54d71ede928d89d45e38b362c9dc39b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:42:59 +0000

    wash upset union here file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
