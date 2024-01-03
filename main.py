c5708eb1c7ac7141f152e5c50a0a29bfb9e05349
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:07 +0000

    total storm everybody pair basic

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+print(sum(range(10)))
