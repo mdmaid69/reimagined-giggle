1d1309177ab713c4c79167f802f55c7d15798dd4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:18:59 +0000

    tie brain behind freeze through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
