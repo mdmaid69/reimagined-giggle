5a1263f95a52787fa16951a38a8eb7600a67ba80
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:26:47 +0000

    pay blame crazy emphasize centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
