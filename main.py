e3dff6949c0b937a7cc45cce64a5f2393f19ac47
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:28:08 +0000

    accidentally change plate study wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
