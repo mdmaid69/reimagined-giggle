7d1828fcfc9492296e4b39aaad42acd123376c27
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:20:19 +0000

    fire inform tip develop advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
