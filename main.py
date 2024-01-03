95b932441eb4070789d8c16c4ef2380eee045ec4
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:47:07 +0000

    purpose put sweep contact number

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
