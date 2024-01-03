b433d17def35a5c19609c26388e88602bef38dbf
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:47:43 +0000

    excited married study bite pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import math
+print(math.pi)
