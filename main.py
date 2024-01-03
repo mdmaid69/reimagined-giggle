fff95c0f33744ff4baa9a1523be3b74e5daf5743
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:33:18 +0000

    pause scream floor see basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
