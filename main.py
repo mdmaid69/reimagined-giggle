b122da266d4b93917e70fc9c2ffc1849fbab8e27
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:25 +0000

    flight practical listen apply operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
