fcf241b6e29e3728fe7bd34aafac61f3d416acb4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:12:13 +0000

    thick runover hungry independent remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
