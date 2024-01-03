2a25e887ccd55764d429b016cc35afd8594cebb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:55:44 +0000

    badly exhibition Rr escape total

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_logarithm(base, x):
+        return math.log(x, base)
