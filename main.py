1d3c99f293cbb30abb726bfcac699bacda5b88a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:23:01 +0000

    copy makeinto lock man shop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+for i in range(10): print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
