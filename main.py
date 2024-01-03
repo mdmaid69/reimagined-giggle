83563f5dce3d08e22912004816b3ecd65e044b75
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:39:48 +0000

    afraid float Oo eye hot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
