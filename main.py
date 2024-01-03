ee675ba0d53310b8fd089daa7f166be91de12d16
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:15:24 +0000

    abroad door allow 564encourage lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_array_to_list(array):
+        return array.tolist()
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
