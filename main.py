9c16fc36387f6ef9e672d8243009ccb263dcc11a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:25:32 +0000

    fond interfere callback hang take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
