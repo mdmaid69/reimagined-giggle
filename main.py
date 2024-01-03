24207cd3659a2bc19ff9c49a9425c35dc1c9f84e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:14:49 +0000

    confident freeze measure runinto content

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
