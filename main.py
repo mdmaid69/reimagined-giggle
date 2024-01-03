efd101980fe2e6e56938dabc1b56177140d1915f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:29:35 +0000

    mail score Bb paper collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def cube_number(x):
+        return x**3
