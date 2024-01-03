96726e8e75296dd9d3785d93beac4e26a4c36a5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:28:10 +0000

    foot inquire silent basic recommend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
