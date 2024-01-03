7f7127910feb43a68d737de78076e2e697b2e815
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:23 +0000

    bang limit whoever future glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
