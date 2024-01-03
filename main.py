262c5c671486bb653bf1cb1030e9cceb411ae583
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:41:50 +0000

    sure suit expensive happy although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
