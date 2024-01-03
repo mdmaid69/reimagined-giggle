fc5fe3c3195a4d4c3b6d11d6f9d40b25d9a07c1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:00:00 +0000

    borrow ask artificial its strong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
