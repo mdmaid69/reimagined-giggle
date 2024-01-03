d4e1d2b5a4c9bdfb307e2fc753614b52e1ea7fc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:32:52 +0000

    practise unlikely breakfast ourselves criticize

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
