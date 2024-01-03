20cec0f8217ef883eebebb8e9bc6d122cb14cf6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:25:41 +0000

    travel wash thinkthrough cheat price

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
