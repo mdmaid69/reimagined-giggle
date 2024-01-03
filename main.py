d0a410504b03cd1698736a9099f9da807f6c6b0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:09:05 +0000

    reply would afford comefrom concentrate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import time
+print(time.time())
