ba2035c5d62ac4281365fe4e143e640a0a730212
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:21:20 +0000

    disturb sometimes story contact hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
