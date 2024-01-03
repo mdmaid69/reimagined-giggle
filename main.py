f94bae6dcaa731e2857f6a193eb0fd37cf6f74e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:09:11 +0000

    must silent related now bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
