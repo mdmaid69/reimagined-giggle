974f037ba86ab13a89a7fecb07609c9915f3686d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:52:42 +0000

    hang pretend trick about boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
