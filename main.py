d3f7671fc613412e9edc802e6cf3d51845045c84
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:57 +0000

    kick rest health uncomfortable return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
