de9e441bc88615e144f3a8cb02d9d1bef6c72c1e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:03:26 +0000

    area separate getout turnup hard

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
