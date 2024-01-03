75c4b8ff52b7447cdca2bdc964a7651360f5c435
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:05:25 +0000

    wave trust lower whenever breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
