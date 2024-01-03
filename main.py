707bfdf3673827c3dc1fbc0bb7ed8d4cac1d237e
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:57:18 +0000

    thick Itself statement kind human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
