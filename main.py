62d2232beb1b5b30db8642d4c528996e83380393
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:45:58 +0000

    attempt evening several under crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
