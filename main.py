be8a3a1ae5650dc32120f25fd90a1960165e6542
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:19:50 +0000

    worry sick everyone along brave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
