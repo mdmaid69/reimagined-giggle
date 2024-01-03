2c1bd38fbf1fd7448eb917cefdef5e4b00331b70
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:34:33 +0000

    above breakup fence test cruelty

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+for i in range(10): print(i)
