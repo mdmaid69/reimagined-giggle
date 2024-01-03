29345395b61b673379b83a210f0e3bbb08f3b6c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:56:52 +0000

    Kk issue ball identify hand

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import os
-print(os.getcwd())
-def calculate_volume(length, width, height):
-        return length * width * height
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
