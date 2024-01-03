9b41d1d557b0a4d26312f87eba56655980b38087
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:02:44 +0000

    talk snow appeal attractive body

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
