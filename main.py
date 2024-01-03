90e8a311698dbc744344edc05373d6d14fd75a4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:14:52 +0000

    them power result report gradually

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+print(sum(range(10)))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
