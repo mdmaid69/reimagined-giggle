f478bab96b7a48562f3f4d19ea6948d130098f5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:03:26 +0000

    wide mouth prefer interruption shock

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+x = 10
+y = 20
+print("Sum:", x + y)
