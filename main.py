d9ebcca6b61e4f2273d244f7fc479412eebe888b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:11:37 +0000

    container announce about against disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def reverse_list(lst):
+        return lst[::-1]
