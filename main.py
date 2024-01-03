35934296c262b3185746df29c7a180b3f69bfacb
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:07:29 +0000

    defeat row age definite talkoutof

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import sys
-def exit_program():
-        sys.exit()
+def get_array_as_set(array):
+        return set(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
