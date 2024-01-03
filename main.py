dae61dfaef62fa647d2583cf1770064479b905d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:04:28 +0000

    ahead weigh quarter comeup 572enter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
