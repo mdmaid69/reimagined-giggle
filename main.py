2f8a83d4acf9877fa389e2713124e0db8b5f4582
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:24:49 +0000

    blue journey bin sleep love

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def count_elements(lst):
-        return len(lst)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
