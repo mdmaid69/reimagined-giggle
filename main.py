ee8d89647926af4302fc0aab0082445334f7bcbf
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:16:03 +0000

    train react mouth comeout plenty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
