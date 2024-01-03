24c87705a486e843a95bcf5fd770d972ba2317d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:46:54 +0000

    tall study vision hardly pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
