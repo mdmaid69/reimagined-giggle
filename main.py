4b2563a678ab0244baf29091ddd40f50382dac14
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:32:59 +0000

    surface object hardly arrangement cruel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_area_circle(r):
+        return 3.14 * r**2
