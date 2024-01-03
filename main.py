34d900f13c2897e56c54dae5abdf61aae82218c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:18:11 +0000

    Ii passage save form list

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import tensorflow as tf
+print(tf.__version__)
+import re
+print(re.match("h.*o", "hello world"))
