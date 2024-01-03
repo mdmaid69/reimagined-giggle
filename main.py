1d023c984676fdc7d4d05e1fabb8c921cde51444
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:48:54 +0000

    reasonable contact club up sign

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def remove_duplicates(lst):
+        return list(set(lst))
+def remove_duplicates(lst):
+        return list(set(lst))
