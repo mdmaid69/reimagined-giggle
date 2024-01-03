afbbfaeebbd90e2b55fa2d8bd34b98814e4be22b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:18:03 +0000

    race concerning accidental clean check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
