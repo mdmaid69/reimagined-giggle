2c160f3022ee6805a87e8e8152e876e68462d79f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:53:39 +0000

    yet hand always attitude spend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_speed(distance, time):
+        return distance / time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
