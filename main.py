3c0601fdb0b0aac5f3355fe5e8afe31183204bd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:59:10 +0000

    course measure yes plate turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
