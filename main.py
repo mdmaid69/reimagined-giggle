aa730d0b3720b9caec6e1c7a87b92f4e3f81a6dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:38:03 +0000

    fall hold transparent interesting many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
