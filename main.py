d17bfad12f5e5b6dd2b9e3af21ec568c36f7f8f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:15:33 +0000

    complain church dry reach persuade

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
