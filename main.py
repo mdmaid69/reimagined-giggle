3f70a0dc07b40cac3abfe164addf83e36d07b1be
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:52:37 +0000

    achievement towards sensible organization dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def sort_list(lst):
-        return sorted(lst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
