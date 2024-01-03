e0b908e9577f3ad69e427098f6cd73e4a1a7fc23
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:10:03 +0000

    rush skill estimate uponon push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_area(radius):
+        return 3.14 * radius * radius
 import array
-def get_array_as_str(array):
-        return str(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
