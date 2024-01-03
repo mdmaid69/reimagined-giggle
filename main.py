67042b1252dc967d797ec78bc202e35ff5a75f8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:54:54 +0000

    search wish subject goout smooth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
