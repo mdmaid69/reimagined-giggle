cd40b99e49e1d7fe3750e802f1d4e6b07a144d9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:14:10 +0000

    by drop warm fair quiet

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print([x**2 for x in range(10)])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
