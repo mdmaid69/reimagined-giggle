bdbde029b2c36176c9a85dac2f501c98d1f476c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:21:03 +0000

    safe brush ability nerve expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
