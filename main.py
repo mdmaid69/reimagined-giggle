c649a19bf3a30295d9432510a01ffc9703588089
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:37:37 +0000

    habit ticket give negative suckme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
