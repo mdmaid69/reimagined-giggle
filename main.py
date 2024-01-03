03c4b3fccead5b6edd585f7a50f8d3aa44d38b9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:21:26 +0000

    live seat contain preparation cross

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import json
+print(json.dumps({"name": "John", "age": 30}))
