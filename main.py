71f387e1ecc4313d3445abb76732d102a9fe95ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:00:08 +0000

    attract round come occur truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
