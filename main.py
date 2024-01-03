b753ba5ff46cddd9265182b8bfc67fc8087e2aa5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:10:30 +0000

    punish protest danger someone warm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
