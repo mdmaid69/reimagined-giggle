f5f28bfd923d15539b8c87d25e1f4ba15e03d877
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:14:47 +0000

    catch avoid fork anybody from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_current_time():
-        return time.ctime()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
