b75413d32a8421c17addd40fd8df1780404b9268
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:44:59 +0000

    tired takeaway respect memory breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
