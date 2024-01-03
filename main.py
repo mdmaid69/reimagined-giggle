2d0b6795b03da5c211e0e40eda32da3c95d99e17
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:35:35 +0000

    turn born whole nowhere address

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_array_to_unicode(array):
+        return array.tounicode()
