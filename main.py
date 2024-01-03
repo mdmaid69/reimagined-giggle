a4c8ea635b54a7c9d9bfd1afde2ced5b80e928f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:55:19 +0000

    respect card pay plate blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
