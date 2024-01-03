74cb8bca2e352848b22db1f4153c082b23bf734a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:27:20 +0000

    television doubt lucky deaf back

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
