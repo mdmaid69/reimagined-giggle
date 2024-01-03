85796834cdfb73e26db10a9bc9c03b537f8cf3f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:03:53 +0000

    nowhere murder steer miss test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
