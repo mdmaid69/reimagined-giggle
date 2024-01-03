ddb5a0f8115f2b923e12383f5d8e4a15566978df
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:56:38 +0000

    middle wherever intelligent her shoulder

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
