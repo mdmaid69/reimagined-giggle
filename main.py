a558981d5b01f1718c55659b43f3cf0706805ed0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:24:15 +0000

    luck slow connection break accordingto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
