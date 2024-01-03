f4ee7c6d7e5fede29aafe416bcb99a47a2af84fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:18:33 +0000

    plenty old apologizeapologise everyone tidy

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def count_elements(lst):
-        return len(lst)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
