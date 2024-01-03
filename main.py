4504431f6f1168156d70f1976ec8ac42a817f6ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:24:41 +0000

    situation brain float challenge not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import logging
+def log_message(message):
+        logging.info(message)
