c9757bda74d5f921e25c841101d60d34f436501e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:07:43 +0000

    noone pronunciation hardly laugh variety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import time
-print(time.time())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
