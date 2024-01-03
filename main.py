8f81bff70a23c5810f09d478cb2ee427d8b42c2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:06:44 +0000

    mad something blame immediate community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
