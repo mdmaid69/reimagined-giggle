c515c7a436393ab9746a6a646560d3618bce7e5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:36:06 +0000

    actual own union tape strength

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
