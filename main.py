4c7dd3fec0a0463d2ddfda878593fd60a845c278
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:12:29 +0000

    ourselves uncomfortable hurry sign trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
