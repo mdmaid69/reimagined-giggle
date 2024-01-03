559e808945fa21663732001a403a8becde1eb0a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:03 +0000

    sharp after welcome dance see

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
