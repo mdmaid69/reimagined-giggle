b434a542313108b041289e011503a133572c0da8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:08 +0000

    complicated guilty manage prize dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
