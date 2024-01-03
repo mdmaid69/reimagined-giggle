05b63f1b62cf3bd2bc594345a44a5c94aea7ee74
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:24:45 +0000

    dangerous chief than shout system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
