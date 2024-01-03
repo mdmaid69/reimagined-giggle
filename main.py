ee02ab67e255303ee44793c19006e9e36eee422b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:10:26 +0000

    cost prevent his mean far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
