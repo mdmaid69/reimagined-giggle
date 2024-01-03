606058aa096d779c78fce4c41bf4f207937d2ca4
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:48:50 +0000

    freeze lot ourselves getback better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def count_elements(lst):
+        return len(lst)
