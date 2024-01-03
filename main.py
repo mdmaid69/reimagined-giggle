3d262957a139f982c5142d2689214acb5d36f1c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:27:26 +0000

    store tip perform organization wire

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
