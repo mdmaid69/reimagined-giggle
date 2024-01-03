d9e27af010bfe33890ccb0ac34a43ded62f838cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:30:46 +0000

    whistle well thorough into beneath

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
