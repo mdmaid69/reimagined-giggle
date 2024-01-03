a2a66f9753ec29d149d505543550d5f0e0c85510
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:12:09 +0000

    equipment special include open unkind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_list():
+        return collections.UserList()
