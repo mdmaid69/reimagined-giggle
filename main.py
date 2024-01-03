e784e880ecd2506b1cab0e001e042da3e4646c43
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:12:59 +0000

    otherwise by game 570enjoyable unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def find_max(numbers):
+        return max(numbers)
