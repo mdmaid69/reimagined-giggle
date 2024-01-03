c3a6a87ef41e0ce72784dd5399b505c11f195192
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:45:54 +0000

    maintain fault off 572enter complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
