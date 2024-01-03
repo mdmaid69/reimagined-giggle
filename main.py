924f14352b605627011c54dfeb8aab00d422c0d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:52:10 +0000

    night test normal flat result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
