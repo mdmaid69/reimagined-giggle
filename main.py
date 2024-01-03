0833aa9a1d8af5f425cbf808aaa45aa2ef4dab17
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:40:16 +0000

    provide lie expensive force figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
