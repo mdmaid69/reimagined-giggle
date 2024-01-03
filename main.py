f1a248ccd2974b82aa4e39911df9188f63b63125
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:48:25 +0000

    best trip order steal beneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
