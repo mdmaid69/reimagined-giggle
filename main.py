14a82790c2c5fc4bf7cb95af2295420fdb871e2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:09:22 +0000

    write full speech male passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
