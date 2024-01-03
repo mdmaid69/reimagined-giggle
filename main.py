4b67f00ed1cc2bc05457949ac71a1f28a32273d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:32:40 +0000

    already realize formal home imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def reverse_array(array):
+        array.reverse()
