90f9b22cef30c757576e74cca8294a9f5aa02566
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:52:12 +0000

    discover goon calm somebody ever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
