bbbaa3bc0f61f80e24811a09f9b0198723926ff8
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:15:01 +0000

    sit comeover raw centre offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
