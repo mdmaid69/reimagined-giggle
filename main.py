7d35e3064d1b64bb2766c45356d1074a1c4823fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:57:55 +0000

    safety miss day kind praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
