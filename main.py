bca80deee9609e672ee448167ddde539c9081dd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:02:38 +0000

    gap unpleasant rate whom crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
