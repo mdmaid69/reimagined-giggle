dc742816dfa65a01bfd1ae27bc6059ec3f7eac5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:50:18 +0000

    stiff square even interview turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
