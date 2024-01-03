cbd68de56e32d74901b920c403835282d367eb6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:33:58 +0000

    regularly worth harm right remove

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def convert_to_binary(n):
+        return bin(n)
