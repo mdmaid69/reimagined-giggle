fd5694708d5a4533045c32754c2213bcce1c4e96
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:03:59 +0000

    fortunate shrink suck steam which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
