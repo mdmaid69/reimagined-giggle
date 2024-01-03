f0a64a5e4ee4708326c7ef83b81bc6096f2a15da
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:29:50 +0000

    clearly number hello show similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
