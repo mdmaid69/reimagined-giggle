eaae6cdd3a71505e468814c9706e94b2f3318c69
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:24:16 +0000

    impossible comeoff pen attempt trap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
