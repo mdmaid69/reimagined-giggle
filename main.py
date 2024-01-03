f51b2ed7a5c41ef8af47918bbce6cd42cd829e87
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:22:00 +0000

    matter taste cross improvement rely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
