c514040c056de5c712b3384ce8a9c0e55bd64828
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:52:56 +0000

    admit apologizeapologise belief case start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
