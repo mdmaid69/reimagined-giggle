135147d242b22c3190b6a91af7ed95a016a63a58
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:51:28 +0000

    turnaway statement swim disappear behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
