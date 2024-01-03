0c1f9cad43e2e5c283cc5dbae46c7b5028ab2a0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:29:24 +0000

    apply joke close give behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
