20cbbb802e64e9d6637cd27125314135529ec7ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:28:52 +0000

    attempt talkoutof below translate difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
