6c30e294fdd87ed9d12f7a12f81cca43f15fffcf
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:33:50 +0000

    play love peaceful beginning base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
