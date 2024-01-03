58ba4253c1e37e25bb8f2ecb55106298cc384604
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:30:26 +0000

    570enjoyable patience fresh belong as

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
