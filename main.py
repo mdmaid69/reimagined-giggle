7492a665318625850964883b1569c10e039ffc98
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:30:11 +0000

    create maximum someone service so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
