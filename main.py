4018284de871dcb9a9f183be4ab1b5d55a3d2027
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:04:29 +0000

    either back hope payment muscle

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def divide_numbers(x, y):
-        return x / y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def clear_array(array):
+        array *= 0
