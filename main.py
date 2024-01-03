0fd360e4067dc8293b1b1bd8331ee6a087ad947c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:56:32 +0000

    regular home custom heavy sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
