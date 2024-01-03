e8fefc2a570283922893a5bb2313e0df5b3bcfec
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:16:46 +0000

    dress secret minor involve grateful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
