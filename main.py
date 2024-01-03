f133a9fa7553014a9424d9eb93a14537f5b09b04
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:44:02 +0000

    show space beat friendly contact

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+print(math.pi)
+  def subtract_numbers(x, y):
+        return x - y
