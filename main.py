9395dce893b5b0a95fcbcb1fecd5006b43649dbf
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:45:20 +0000

    part front bottom space rain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
