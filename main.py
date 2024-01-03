0822cc97f1eb1458706c8a6ae42e43e2f2d3552f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:49 +0000

    nose switch wind snow undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
