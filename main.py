59b15211fca66aa4502bc0674abdd5a099ff737b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:00:17 +0000

    rent arrive responsibility terrible still

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def sort_list(lst):
+        return sorted(lst)
+def greet(name):
+        print(f"Hello, {name}!")
