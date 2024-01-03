cac5f7a15a360a40df742dda289ea41c23ae9c68
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:35 +0000

    travel wish start belong present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_itemsize(array):
+        return array.itemsize
