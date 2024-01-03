33d53b41d8ec36111d77503c6df83f48deee1f49
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:31:24 +0000

    slightly sudden urgent nor chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
