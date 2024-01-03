88cb57b08d1cbe7baffd2753d6713ca91f8dfc0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:52:21 +0000

    runout organization fat occasion learn

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
