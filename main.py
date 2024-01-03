e0e1f06e51b3d805a222c9775ff8068693259515
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:38:15 +0000

    up hurt thumb pocket trial

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
