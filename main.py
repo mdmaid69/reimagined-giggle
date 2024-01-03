81f9f04df7c276c1f00b01eb3967ef8964eab4ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:14:40 +0000

    band another glass window odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
