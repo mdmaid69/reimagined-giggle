5f9acf2e76729aa3c9df31a229048880f2ab65a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:57:56 +0000

    pure help mistake immediately bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
