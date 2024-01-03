498ee834b5afb77e1e2fbc9a5056b84775f99c46
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:24:06 +0000

    pay environment idea breakdown off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
