51b9a50d699a85c4b76a5f83d7967175b004725f
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:23:34 +0000

    spare rent runout second improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
