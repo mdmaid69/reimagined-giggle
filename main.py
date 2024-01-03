caadcc63d639071752c9a87a2ab44d497d19be54
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:55:47 +0000

    bringforward brush properly behind welcome

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
