3b2ee20a967e83440e05af9cd7025703d72ac667
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:12:36 +0000

    makeinto advanced cost suck fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
