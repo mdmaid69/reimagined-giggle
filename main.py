a2154e283b96a8d3209f4c2124480420c508649b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:17:50 +0000

    schedule often brush juice attempt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
