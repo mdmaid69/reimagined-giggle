83112ef4663e5d5682afbdbe6730dc3a7878dc5e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:43:07 +0000

    they spill talkback stop each

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import sys
+def exit_program():
+        sys.exit()
