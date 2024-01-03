126eddb8a24dea2f2879c86cf51f01b80b606b92
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:50:42 +0000

    emergency do act boil actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
