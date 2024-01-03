e5105fcf337eac2af391867528c40d52d17df0b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:27:14 +0000

    possible 569enjoy cold measure criticism

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
