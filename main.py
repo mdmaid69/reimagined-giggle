42793719944662e12846e4ddc6696c3ee7f3346d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:51:33 +0000

    advice unit pretty somewhere why

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def subtract_numbers(x, y):
+        return x - y
