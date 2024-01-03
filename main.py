64d2ca5d99812488591d7d574c37d2439a30cd9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:11 +0000

    married public mistake organization corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
