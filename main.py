31c4aab3d63c04f0f49d8a316f6cb30ad9c3a6d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:50:17 +0000

    than clean bringback willing test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import collections
+def create_priority_queue():
+        return collections.deque()
