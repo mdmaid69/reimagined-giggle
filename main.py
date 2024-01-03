7a2ec1ba5b79bf076decbc945ad1bd69eec566a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:30:44 +0000

    once advanced thin round march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
