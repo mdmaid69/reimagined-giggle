0da7e268e5bd45d71ee5f89e50b1e3c0deaded66
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:04 +0000

    getaway public start related arrive

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def sort_numbers(numbers):
-        return sorted(numbers)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_set(array):
+        return set(array)
