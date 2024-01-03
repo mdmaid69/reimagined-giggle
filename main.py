00d2e0a80b983e825350f9c719cef14bf81ba058
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:01:08 +0000

    splash hers herself meeting weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
