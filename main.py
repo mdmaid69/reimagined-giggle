beb570f34f3eb524e79afd31d0ed36aadb9300c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:20:32 +0000

    sound correct our advise turnback

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import time
+def get_current_time():
+        return time.time()
+import time
+def get_current_time():
+        return time.time()
