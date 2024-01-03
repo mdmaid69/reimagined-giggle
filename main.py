4ff0f314b84c9cc043c5c0f5cc578fa4e5c1f162
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:58:10 +0000

    party club whoever goon flood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_volume(length, width, height):
+        return length * width * height
