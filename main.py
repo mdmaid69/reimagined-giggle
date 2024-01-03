5f240a7d6d38ca710238670edf8f4f1b01f4ec5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:36:02 +0000

    Qq relief strong hot worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
