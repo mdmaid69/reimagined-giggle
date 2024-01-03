8891dfc0764204322951761a688d450d50974ef0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:24 +0000

    science state choice past minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
