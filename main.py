212ce3e8d9d79677f488ac185d88ea4131e8fd12
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:24:19 +0000

    everywhere thanks gather rescue file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
