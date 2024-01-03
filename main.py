281b620661b301e7d210dacc994e2dd887360712
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:30:25 +0000

    underneath Zz fair bad blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_binary(n):
+        return bin(n)
