c45f4c2e9252353f0fb86447df4d6d707b8f8f9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:08:35 +0000

    deliberately unkind pattern whole practical

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
