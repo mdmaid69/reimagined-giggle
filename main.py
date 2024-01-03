422a3111300d62defa2a88301ac488f38cf0446f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:35 +0000

    mouth guilty over come able

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
