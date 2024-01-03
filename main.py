ef9b870341d1a03e9f9a94aee8c676442214252a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:39:56 +0000

    develop wet needle recent means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
