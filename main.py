b23304f8df0dfb8f6b38da9ceeb409ee1e2a6d52
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:28:07 +0000

    unkind meanwhile major call impatient

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
