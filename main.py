fdb6ef4d7f74b69121b49602b517e5a9433fabb6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:06:27 +0000

    soft they prefer hour evening

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def square_number(x):
+        return x**2
