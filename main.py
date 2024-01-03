e620f604e0ff8868fbc594c5d3cbbedaa4f13995
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:07:17 +0000

    direct house cause us bank

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_work(force, distance):
+        return force * distance
