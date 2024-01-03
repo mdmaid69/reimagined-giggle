8610dc9efa208c66502ce2ec2ce084af2953a9a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:02:08 +0000

    inside lookafter dare belong noisy

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
