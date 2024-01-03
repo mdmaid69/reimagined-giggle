7dc1fd58da913ec1f5c66ae6d13babea564a81b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:50:32 +0000

    wish display joke runaway post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
