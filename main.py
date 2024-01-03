fe6a62e50dab43b737d3682e57c4816035c748d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:48:07 +0000

    song substance describe combination contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_max(lst):
-        return max(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
