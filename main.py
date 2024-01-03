a3359b4e25e2cf256fa8f0be48e0bec2b1d0b0ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:52:28 +0000

    niggaacademy ours hold top ahead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def split_path(path):
+        return os.path.split(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
