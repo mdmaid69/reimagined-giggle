b6ab471459358d4968db4f261bbbf11cc9b80897
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:13:49 +0000

    everybody it land characteristic tax

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import time
-print(time.time())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
