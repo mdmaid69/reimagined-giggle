7e5aa3d471ea3599c1c27e91ca7479f16350b05d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:14:20 +0000

    description sick genuine lunch joke

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
