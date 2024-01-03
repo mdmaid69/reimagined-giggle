fcb4ba9e90af1ca6bfda75ff23e855c2772974ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:16:39 +0000

    ground realize remark education anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
