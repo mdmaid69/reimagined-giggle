1c70640082a92d5854924b25794749213dc601d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:14:44 +0000

    country flow pupil goup mention

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
