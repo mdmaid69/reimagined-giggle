eca5535487b622724263c7ad9c0cbcca6b2838e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:54:56 +0000

    once turnaway apply opposite wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def find_min(lst):
+        return min(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
