bae0c22e483f04f6ca60ec671fe95286d4ce20f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:49:24 +0000

    union cold law worth lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
