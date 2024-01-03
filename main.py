9aa32965ed252fadfc5dd56ec0fcb9ad80d3bd4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:50:14 +0000

    may off thin hear worst

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
