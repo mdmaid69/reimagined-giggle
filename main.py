98b6908e54d7fa5b650c94416f7ded147e561bc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:51:32 +0000

    about fall beyond into neck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
+import collections
+def create_queue():
+        return collections.deque()
