c7e91e38d0d5d58e42202c0544dffd15307aa02f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:00:00 +0000

    confusing moment Jj gas whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_item(array, i):
+        return array[i]
