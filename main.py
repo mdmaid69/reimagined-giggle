c00dba7d1ae563bce70ba6208429b2bb7820eb17
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:29:03 +0000

    exist achieve only regularly this

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def get_array_as_complex(array):
+        return complex(array[0])
