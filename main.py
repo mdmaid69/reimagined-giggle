1c63a92b1a53dde029ea846ab00e096f26d09c76
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:06:54 +0000

    cope cut appeal illness natural

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def multiply_numbers(x, y):
-        return x * y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def reverse_string(s):
+        return s[::-1]
