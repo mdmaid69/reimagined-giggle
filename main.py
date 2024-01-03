2738c5b0f02e3c584e45552c1cd61314f0c2663e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:27:12 +0000

    circle rough ready find enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
