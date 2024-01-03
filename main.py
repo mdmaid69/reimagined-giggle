26dfa948ca3ca50a2f2458c19bba05eb3b533bd8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:38 +0000

    force edge land character bored

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
