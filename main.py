6bf5655d84e8ada83089e36dd3cf5f2aecf6d3ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:41:52 +0000

    pain someone public slice wealth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
