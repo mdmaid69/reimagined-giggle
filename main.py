dd263145a6a168d05ef632684ba76ac18acd6437
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:30:36 +0000

    own dish keepout address myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
