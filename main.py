04b7e9d5e45cd6a2cfbaf4ce35c8cf5ff8dac1d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:57:05 +0000

    since you sell owe invite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
