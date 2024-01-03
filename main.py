ce3bc256b27b0253f138370ff066a9adc77df2a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:57:35 +0000

    shadow clothes thinkover sing below

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_work(force, distance):
+        return force * distance
