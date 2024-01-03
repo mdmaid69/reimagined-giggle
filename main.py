ef60b7ce8a507bf2804ee8511a6ff5a52c2d7619
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:24:17 +0000

    national Mm way excited yet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
