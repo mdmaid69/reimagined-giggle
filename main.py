7b673d1ee95e13333abb80127d4e416cf88739ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:14:22 +0000

    estimate risk chin conversation race

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
