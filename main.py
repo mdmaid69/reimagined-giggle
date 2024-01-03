7eaebb713170d440eebdd899d4ddbd6a3b6dafd4
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:50:45 +0000

    storm confident trap eye form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
