7e48093fb3967ded0c2953d241f4121824859cbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:52:16 +0000

    ours section pass dress hard

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
