0535990162a889abb5617354b31e6bc80f05c622
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:06:53 +0000

    rely acceptable light extremely old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+def log_message(message):
+        logging.info(message)
