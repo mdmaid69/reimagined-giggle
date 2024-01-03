825d6cb1e86360fe6b671f7c7ef87278d99cbe9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:44:37 +0000

    stir giveup better average passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
