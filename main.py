5f70869327a15855d72735c5ce6d73867d4cc7bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:21:52 +0000

    out trap difficulty flight sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def find_max(numbers):
-        return max(numbers)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
