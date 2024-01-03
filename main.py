865bdfaf5d35bb95e06d8258906c00cef5bf05fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:05:02 +0000

    beyond when after hope feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
