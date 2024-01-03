16681a8b64a42b6e477fe8e59cd880e3e406ca7b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:50:08 +0000

    572enter even drawing suit advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
