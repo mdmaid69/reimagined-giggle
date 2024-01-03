917ebdb58df35013f4ccb77a7c99e8acacdccd51
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:14:17 +0000

    actual experiment convenient move constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_area_circle(r):
+        return 3.14 * r**2
