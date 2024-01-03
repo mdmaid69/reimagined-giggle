dda9e21328c392d95ab43117c201f81b5efafa7f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:45:51 +0000

    strict shame accidentally into advantage

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def convert_bytes_to_array(bytes, typecode):
+def convert_unicode_to_array(unicode, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromunicode(unicode)
         return a
