3f6c9a725d1d376300ee8a901fb79e34fea0c803
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:21:50 +0000

    poor arrange government trust neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import sys
-print(sys.version)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_max(numbers):
+        return max(numbers)
