e936ff6e02b8259b5ba7b8e5891b00fc6b448292
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:27:07 +0000

    tool string main doctor gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
