fe42e768c086acb5c17afaa819c0b5529e935332
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:19:10 +0000

    shrink miss turnback invitation learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
