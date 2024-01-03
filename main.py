5c89dd2d6de2065a6115c81350d434b0c7e3805f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:42:52 +0000

    male closed pinch exchange sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
