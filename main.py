e1ac14820de532161d43c70f191aea6d22769975
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:04:44 +0000

    slice imagination wind defend control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
