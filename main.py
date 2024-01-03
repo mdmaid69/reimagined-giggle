93b6be025b081e7db59c891304b8e7b438d2bab1
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:10:19 +0000

    frequent pronounce broken exit offer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
