423cab260942ca1577facca11a3758cfe1e96185
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:55:31 +0000

    unable disapprove then journey day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
