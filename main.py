4bb7749a031ee3612be5a801269a95c74641271d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:59:47 +0000

    goout disturbing test peaceful leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_elements(lst):
-        return len(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
