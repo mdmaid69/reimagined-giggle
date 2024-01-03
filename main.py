351ccc0855422d1a555521756b7726fec7cc1ccf
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:48:39 +0000

    either whether and majority exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import os
+def remove_directory(path):
+        os.rmdir(path)
