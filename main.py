4261d2ef028368c78f9961e6ba085322a423d644
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:52 +0000

    muscle hard subject prefer near

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
