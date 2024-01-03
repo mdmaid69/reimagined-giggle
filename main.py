14095d5fc7022fcbc45a8881614d89ce43864430
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:22:14 +0000

    university kill describe small band

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_density(mass, volume):
-        return mass / volume
+x = 10
+y = 20
+print("Sum:", x + y)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
