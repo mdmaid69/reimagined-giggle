8ea222850754741973f91697b70388cb58344d3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:51:37 +0000

    wherever draw match pocket talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
