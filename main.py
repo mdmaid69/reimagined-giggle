f9c284f3fb1e7fc60381d0e51eeabd2d5c1fadb6
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:13:19 +0000

    record study Nn switch tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
