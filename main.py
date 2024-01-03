250c56e3e5b612acef80e8cc3a8e98e050ecf42e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:27:39 +0000

    party travel answer proper comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def log_message(message):
+        logging.info(message)
