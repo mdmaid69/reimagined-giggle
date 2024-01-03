77261261ec92fb6373d0f9448eb1a3cd2204a71d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:51:18 +0000

    live key always activity vision

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
