996fc41f1da67300c5dabd36de33957404eac89c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:24:07 +0000

    injury some understand choose friendly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
