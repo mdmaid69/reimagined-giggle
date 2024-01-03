4eb7d5efa5c05f86d99ea434eccf0ed2a618abae
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:52:05 +0000

    here minimum suggestion equipment sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
