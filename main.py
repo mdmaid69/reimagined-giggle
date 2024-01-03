c1dae17c57a559a4c716035042a9103c151bbb3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:37:29 +0000

    some dig expect century lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
