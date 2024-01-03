d154ec0e4643c9d705db79d72a2952ecd7864aaa
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:43:43 +0000

    trust issue mood trap argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
