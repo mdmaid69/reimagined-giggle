4d2a75703ac695bb2171a8fe995f9658637c2c13
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:41:18 +0000

    bunch shape lookback(on) rough mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
