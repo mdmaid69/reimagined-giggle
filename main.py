7263d70a975afbe7c1ba9e79d0fa385e4ed54950
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:20:12 +0000

    maybe frequent owe job display

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
