5d6d9fdbb152839f95f75bf97175c78d286e204f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:37:55 +0000

    treatment instrument shoot surprising lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
