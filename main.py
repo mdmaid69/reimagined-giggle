077702cadd9a533697f183e80e08b695a2b8c369
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:24 +0000

    sort friendly dream straight free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
