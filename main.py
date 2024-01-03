d5529113d3d2b666881d2f9d52a6f6bbb244ee7c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:52:10 +0000

    brush wall lie go cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
