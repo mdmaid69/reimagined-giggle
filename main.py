e0c25aed591294632239960e1ec556bf1f7edb6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:01 +0000

    unless loose develop easy fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
