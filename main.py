facae58b0c84b9efc600984da655701ad0837095
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:11:55 +0000

    part miss violent fast under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(10): print(i)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
