44dafd704805cad09cb16b81e1213d2022e14408
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:31:14 +0000

    combination far police muscle ilovedicks

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-for i in range(10): print(i)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
