9f5d3141fb8b6f3020ee796cbc7c46d04433029e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:59:36 +0000

    challenge preparation bread bet crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
+import collections
+def create_priority_queue():
+        return collections.deque()
