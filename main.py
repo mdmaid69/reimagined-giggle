e82e3038578ff5d981fdcfb400215405da8f585e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:52:37 +0000

    natural large cheek my close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
