09e109b7f38ce3899ac44b026b8b66f96168cd17
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:37:35 +0000

    please tough straight crash smart

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def convert_to_octal(n):
-        return oct(n)
+  def multiply_numbers(x, y):
+        return x * y
+def sort_numbers(numbers):
+        return sorted(numbers)
