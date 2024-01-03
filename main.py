5a0a08d43a22061ba49f8fa2ee6d7366c5378467
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:24:04 +0000

    fortunately choose better direct takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import platform
+def get_python_version():
+        return platform.python_version()
