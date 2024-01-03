1a8c5fdf10a69d7700859dcbfbfd728f20514a88
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:05:49 +0000

    black project tense creature medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
