3e7375c1dd7e3a3b90b4fd4cdde3fe968ac1b35e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:42:40 +0000

    content rid meaning shape stop

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def is_odd(n):
-        return n % 2 != 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
