a4326675529935818e951f5a67e6e548ac9bc78f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:33:40 +0000

    quiet connection treat cross town

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
