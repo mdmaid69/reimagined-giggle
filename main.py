5fbcbcddcaaa71fa7062428ab35fa70708ebe2e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:40:33 +0000

    story stretch however hardly regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_odd(n):
+        return n % 2 != 0
