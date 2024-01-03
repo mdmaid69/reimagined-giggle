50c95463fc92878fd4cf25af88928f6db09451c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:49:49 +0000

    dream autumn pupil shop convince

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_density(mass, volume):
-        return mass / volume
+import collections
+def create_user_string():
+        return collections.UserString()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
