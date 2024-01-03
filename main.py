ab6129139d347685398ad33955296aa39087881e
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:21:13 +0000

    morning happy embarrassing fond order

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def is_odd(n):
+        return n % 2 != 0
