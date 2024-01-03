a03a807e43b9ddf5a05d2d25271fcbd426a2bbbb
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:34 +0000

    beyond first hold agreement copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
