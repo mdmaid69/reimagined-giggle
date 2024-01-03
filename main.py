b57fe1ffbecc16faf704523085a7b7bcfbbeb753
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:47 +0000

    closely tradition movement say length

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def is_palindrome(s):
-        return s == s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def is_even(n):
+        return n % 2 == 0
