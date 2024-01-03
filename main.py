d5b3b62982dc7b006abdc5dbae35ad457342586e
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:08:04 +0000

    ordinary custom lonely towards until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
