20e598334a83af5e147c9d2df0c2146470d086e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:06:18 +0000

    bite Vv serve constantly thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_typecode(array):
+        return array.typecode
