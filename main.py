60a9839c31acf63f1a4e4f33a7414663836a8c45
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:19:16 +0000

    sweet pass knowledge tense mistake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def count_words(sentence):
+        return len(sentence.split())
