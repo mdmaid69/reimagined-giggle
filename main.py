24a3c4d321a6aa17add54b10046ccb51efc58697
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:10:35 +0000

    doubt rich dangerous report burst

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import time
-print(time.time())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
