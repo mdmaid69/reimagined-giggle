24b825c81731cac925eeaed451028b95d7d2390a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:59:21 +0000

    piece press best about Cc

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import datetime
+def get_current_date():
+        return datetime.date.today()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
