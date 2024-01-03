401f9b1cc077fdc26fb6d90c188a11791b65823c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:33:34 +0000

    kind previous address also big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
