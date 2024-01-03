4041b804cb8277d9f1a7851a0e2796b6a71eed6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:16:10 +0000

    workon bad really calloff Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def is_odd(n):
+        return n % 2 != 0
