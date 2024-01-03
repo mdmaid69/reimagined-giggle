92918a2385cc80cf4317dbac152edf19c4bb71af
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:00:45 +0000

    contrast dinner heart under unhappy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
