56ddb47184bbc4071c7887b961748b68e5c43f16
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:28:49 +0000

    passage ticket see drop give

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  def subtract_numbers(x, y):
-        return x - y
+  import sys
+  def get_python_version():
+        return sys.version
+  def square_number(x):
+        return x**2
