842972618bce24e766319223bc49617ef068ce25
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:43:57 +0000

    tip clear allow rain possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def exit_program():
-        sys.exit()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
