7ee1f77d552fed14d67ece6e04af33582c6c70e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:45:13 +0000

    also birthday bite when absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def is_odd(n):
-        return n % 2 != 0
+def remove_from_array(array, item):
+        array.remove(item)
