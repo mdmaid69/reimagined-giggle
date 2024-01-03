d51edd0e867e95077dff919e0f85927592a60867
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:22:49 +0000

    mess soak twist aware pot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
