61a6ccc8a3e015ad1fb266749fe96b5acb1b508c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:08:34 +0000

    feeling borrow protect university bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
