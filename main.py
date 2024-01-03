585bea5f42dfceaa2374b387eec77966f30bea15
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:16 +0000

    crisis morning part loud weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
