020fda3c5b141522e65be068231624d74eabc334
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:14 +0000

    view exact without Ww cry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
