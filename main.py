5f3df24f021de5d34eafa1662a0fe252251a0e61
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:51:30 +0000

    tell direct giveup bringback sick

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import time
+def get_current_time():
+        return time.ctime()
