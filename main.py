a482f4871c8dcb0b15fb278ec61ab81d24230f0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:47:51 +0000

    swallow next uncomfortable lookaround calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
