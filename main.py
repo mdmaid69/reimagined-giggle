eb81edc996516390efb02cf6e39f6df0ad43f225
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:54:44 +0000

    right distance true impossible to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
