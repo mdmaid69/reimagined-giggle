dfbeeab516c48f4803d011d87dd3cbfdf8d53c49
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:58:49 +0000

    distance own pile flight carry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_area_rectangle(l, w):
+        return l * w
