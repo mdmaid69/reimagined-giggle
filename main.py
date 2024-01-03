753be7fe38fb9b8af5904bc82b8a5503fb7aa173
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:57:54 +0000

    birthday fashion pure Ss set

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
