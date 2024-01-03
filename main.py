b6c97ac91d942e16f24e7248c378fe18c4c15831
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:41:40 +0000

    talkoutof image concerned demonstrate plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
