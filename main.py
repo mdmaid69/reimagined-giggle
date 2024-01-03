08090417178ebcd018cda9e0b361af31d43ee27f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:47 +0000

    play hit forget wear fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
