eded0183f8f7d2181d74d46fef37fbc46350c485
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:19 +0000

    although few environment 573entertainment health

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
