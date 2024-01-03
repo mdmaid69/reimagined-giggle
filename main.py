d17e12a8eac72a7db5762a26defc6bb6304459f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:22:43 +0000

    right average later shape friend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
