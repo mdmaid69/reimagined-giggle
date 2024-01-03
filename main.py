1b9814effe7db96739b3c56961feb3bd681ae7c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:51:56 +0000

    protect sweep false provide takeout

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
