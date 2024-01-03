5ce739fe6d5814b02f84ed1dfae8614685cbf3cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:12 +0000

    dream recommend spill quantity speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
