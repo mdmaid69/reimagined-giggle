c4db1ba76492cebaf732d8d76a2e6fcd6ed0371f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:16:28 +0000

    show intelligent fair fork rich

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
