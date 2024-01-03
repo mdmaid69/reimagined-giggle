01f2f796a9f68f3f20471ce2defe946a405ed5dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:43:38 +0000

    against point takeback damp that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
