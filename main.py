feca58cd8da0487932feaf9bd0bef1fefa845b1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:00:04 +0000

    eat able still whistle man

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-name = "Python"
-print("Hello,", name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
