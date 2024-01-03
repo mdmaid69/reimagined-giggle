7e17d76b8e43a6386df220000c5ac455b4831471
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:13:19 +0000

    wall show question interview lean

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_volume(length, width, height):
+        return length * width * height
