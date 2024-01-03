d885b3df21545a3b3c7f12266af5e4c2ec4e3d61
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:05:59 +0000

    it famous much embarrassed past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
