69cdc8b7dfc6413fb9ce58fabd4a456bb0681f50
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:27:57 +0000

    further fun male refer with

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
