918b1225d05343f279783f9a2dcea664fb7b0687
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:29:34 +0000

    worth pause whether muscle message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
