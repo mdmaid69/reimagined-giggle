ac1e3d0637543591354ac3c5bd367784949e7962
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:44:54 +0000

    after sister both handle move

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
