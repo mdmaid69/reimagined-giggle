1c36a1eb3c1e81e83a2a73a0662568af7511fa2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:44:08 +0000

    telephone rescue dark hospital while

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
