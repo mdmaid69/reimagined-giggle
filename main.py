09533ccc133c05615c72f0b3c49dac6f34ef4627
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:25:58 +0000

    comeup below tradition fence card

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
