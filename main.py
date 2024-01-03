45e174f94fe231a5c229ae78773da99eedff65b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:42 +0000

    use guard stamp guess while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
