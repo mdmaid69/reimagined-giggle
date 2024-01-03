cdc703053e07f9c0ba864ae066167cca83bb58c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:05:22 +0000

    long burn grade move if

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sys
-print(sys.version)
+import array
+def get_array_as_str(array):
+        return str(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
