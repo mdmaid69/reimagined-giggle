ddda52079d57e243fcc0ee80a3e51dcaf6c296d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:25:59 +0000

    murder call about minimum underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
