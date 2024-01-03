002398c73e79479e19de0437fecabd8a0952f6a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:32 +0000

    bit admire scream breath dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
