e955d73ba28c595dce666330dbd634afe887e242
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:30:47 +0000

    shine expect we search relationship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
