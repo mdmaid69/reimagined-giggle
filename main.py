90aa8ce971458c7396470cf15cec73951db5ad66
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:03:42 +0000

    get shopping considering introduce accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
