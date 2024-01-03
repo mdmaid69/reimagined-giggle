159076f1c5915c1f555a3ebe900b7aef1a2c17e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:14:38 +0000

    spot wrong sure even memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
