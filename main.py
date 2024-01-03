ce4b82f5e1a110c25e0027332b230fac539a5ce8
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:24:48 +0000

    fill judge tired slow suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
