6fbd8586916378b279c60b4a522980f107e85eb0
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:33:37 +0000

    middle speech way match survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
