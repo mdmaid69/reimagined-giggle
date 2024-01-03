10637262603d7381fc65c04bf97590d9354e6cff
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:37:58 +0000

    extremely crime try damage allow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def cube_number(x):
+        return x**3
