3a2b54385535f299743e63ffa0c39ffe50e76615
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:15:45 +0000

    check ought discuss low knowledge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def convert_to_hex(n):
+        return hex(n)
