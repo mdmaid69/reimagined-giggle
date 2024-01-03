5dbb43f013b9e0a5a88c249b5432c2ec64818f6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:30:10 +0000

    bother adjective finish needle feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_string():
+        return collections.UserString()
