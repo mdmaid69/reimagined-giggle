b7271d6bfd17b38605b6ec7096b8cf9078604a90
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:19:56 +0000

    ignore disgusting politics possible far

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
