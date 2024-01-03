c75f5bfd79ecca94085ab7ab105f78704a701809
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:26:34 +0000

    strange star boat past runout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
