7ef1211b0f2ea2f0c5e7dfcab75758141c77ae1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:15:07 +0000

    stone equal clue room matter

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_queue():
+        return collections.deque()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
