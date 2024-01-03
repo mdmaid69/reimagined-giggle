22a5fed873f43e93ba82a558b35fdd668be726d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:44:00 +0000

    complain push dance relationship woman

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
