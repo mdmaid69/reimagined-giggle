677821f05b56633b28358b8c7667a6e4cf8fc70f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:51:46 +0000

    when however fond control ride

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import math
+print(math.pi)
