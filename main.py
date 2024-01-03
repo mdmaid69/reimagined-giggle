604384f0ddea09eb59df60a3bff5f9c0f6b3df08
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:35:11 +0000

    blame shadow offend accordingto joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
