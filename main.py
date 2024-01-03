161703b2d9134d1b2ed1ba36dcda4ed229a5f74e
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:14:51 +0000

    weigh once themselves considering point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_typecode(array):
+        return array.typecode
