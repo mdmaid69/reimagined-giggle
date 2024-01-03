9b79a15578c26378386056b113390cfbd26e8b39
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:03:05 +0000

    collection seriously whoever correct wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def sort_list(lst):
+        return sorted(lst)
