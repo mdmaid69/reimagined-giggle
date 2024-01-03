f77f911157da03b25b90ef68ea6eb00f833667f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:48:27 +0000

    corner support power talkoutof around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
