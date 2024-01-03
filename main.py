d5e11dc5a722b599050e980e494f24e9ffe80f1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:43:20 +0000

    already goup test roof buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
