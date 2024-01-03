81a59b93ae9671aa28c4f9a6f14716769cc32968
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:57:51 +0000

    slide seat colour heart hospital

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
