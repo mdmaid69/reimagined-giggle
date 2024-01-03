f6439352e8ed25ec5ff22fa057362497c02606ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:58:05 +0000

    shout film view suit continue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
