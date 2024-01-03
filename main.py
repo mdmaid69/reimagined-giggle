bebda2a8fd4ab66abf783754ff5e6ead84c5e0a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:02:10 +0000

    jump slice basic talk damage

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
