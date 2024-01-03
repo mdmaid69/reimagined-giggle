d420cdef024d91cbfd237e4d6ab2596a19dbdc60
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:44:52 +0000

    depend sharp realize equipment climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
