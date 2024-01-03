ae351fc319fb2f9a9a219205cc6cf66447b607f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:40:59 +0000

    score jump skirt round tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
