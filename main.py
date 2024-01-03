4afe49f7040ae0e82f90a3cf90ac067985c46600
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:01:32 +0000

    achievement example which complete workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
