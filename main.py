1df5d35ed1b7117e78adddaa84438f1eaf73edc5
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:33:50 +0000

    much identify Rr yourself future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
