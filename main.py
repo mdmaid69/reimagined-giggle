be80e4b3594ef7bec45285f2d5b6999662e53a50
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:05 +0000

    choice very progress upwards prevent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
