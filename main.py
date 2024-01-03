4cf5c05b705cbde27018a7e1f3d1562a6448a6aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:27:41 +0000

    after upwards awake television scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
