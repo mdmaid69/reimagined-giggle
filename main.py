787731694d966c3c0bbb3bd0ac636deb3e405eee
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:59:50 +0000

    disappear getup beginning turnoff Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
