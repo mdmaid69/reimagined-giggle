5f4e574a1d935d5a474228a5e0b0803493a965a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:28:25 +0000

    finish trap bottom balance experience

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def clear_array(array):
-        array *= 0
+print([x**2 for x in range(10)])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
