1049ff19c2a7324e2eab4238567cf2802cc57579
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:44:47 +0000

    full chat doubt pause storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
