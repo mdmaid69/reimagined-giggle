cb65c1df8cf0e6e1b9aa3290792d2e5bacd48c7b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:30:46 +0000

    fast square guess same sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
