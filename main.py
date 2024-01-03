b6bb7649868efe749549fd34bd60660bfa511931
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:03:09 +0000

    single consist unhappy care rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
