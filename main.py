795e6627fd75c53d9b9c4aaf7f61d993680d7e27
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:33:14 +0000

    combination fresh sensitive lonely power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
