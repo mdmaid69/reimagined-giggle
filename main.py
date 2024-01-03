3ca3c4638c9e995a1b52e6891c70d7dd86cb3a34
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:26:46 +0000

    during accordingto emphasis estimate shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
