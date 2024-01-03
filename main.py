511071a2dbf6c60d25deaf21b95ec56447a5fc0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:40:08 +0000

    but rarely performance knowledge eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
