00112e1ff9ffe5de5ad028b21bcb269e30a746c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:36:10 +0000

    try over burn drive drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
