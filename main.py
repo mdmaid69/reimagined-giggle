c6c1ca8ca321b38c28df6d83ad1b06c4ed0d57f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:42:00 +0000

    weak react agree worth weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
