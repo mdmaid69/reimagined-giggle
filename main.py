a2450c2cd2f64f624668c16c581385b7d9664e24
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:34:45 +0000

    national brush break until worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_characters(sentence):
-        return len(sentence)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
