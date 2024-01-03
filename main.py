5fd1d50c40f94b517321083a4a351f8ad7525734
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:24:37 +0000

    brush lookback(on) support produce waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
