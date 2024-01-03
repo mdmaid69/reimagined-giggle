cf06502c4d6d75e172a5c2387196547babb65a53
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:59:35 +0000

    image tall Pp greeting instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
