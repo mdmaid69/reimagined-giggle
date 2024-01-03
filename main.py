2a50424dcd72528d621eeca06d10e0ff238d1ec5
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:21:57 +0000

    identify bang doubt improve give

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def cube_number(x):
-        return x**3
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
