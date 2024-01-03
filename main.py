5d1e42705134542842f6c7e8459d3d2174e5301b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:35:53 +0000

    pour safe mainly bridge compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
