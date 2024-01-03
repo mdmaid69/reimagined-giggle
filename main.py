6058cb9926583529db885b1730b7b2710da8b36f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:54:31 +0000

    unite its goup honest progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
