4dcdaa679c711aead7b54acaa655a2fe9d776d67
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:23:04 +0000

    main operate inquire hungry place

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
