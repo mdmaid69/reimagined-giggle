0f2e1a378c1a81aac44703c60bf3a03046304abb
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:48:51 +0000

    us minority offer there flight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
