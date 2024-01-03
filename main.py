e113cb8e4ca0d0e516ec8ef1c80ae756d4a36ff0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:48 +0000

    show sell feature fence shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
