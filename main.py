54024242a7abe9f6c4bb524b5b1b95c0c7196cd9
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:36:27 +0000

    fear stand circle anywhere beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
