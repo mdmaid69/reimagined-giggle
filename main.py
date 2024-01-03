015f97b511246343117df5f0720cb98f99489047
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:28:28 +0000

    immediate before vanish raise wait

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import numpy as np
+print(np.array([1, 2, 3]))
