28585a5176eac79aa74ae6c31071e4df4454bda3
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:32:57 +0000

    pump scared cope hate peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
