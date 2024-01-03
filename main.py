84955bd529d0ebcf345631d12bca887a66527766
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:56:59 +0000

    contrast hate choose pass scream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
