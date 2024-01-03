e0c770e33320c631e2d0468d48414a04143e02d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:16:54 +0000

    image new share quantity worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
