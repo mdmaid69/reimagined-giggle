912c8a5de44b9654f833ee31da4409b50a02c7f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:31:55 +0000

    practise ride quiet excellent apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
