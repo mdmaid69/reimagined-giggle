5875685fc05b444c9960b5efb99f3a67824f42b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:36:00 +0000

    idea concern reduction university row

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-for i in range(5):
-        print(i)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
