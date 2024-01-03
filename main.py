6666e2019b94d59a863663e96907a5c6b61043a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:45 +0000

    often solve home respect more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
