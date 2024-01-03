7f49328c611d872b6efc414772dcebf147035335
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:55:35 +0000

    sure explain print reaction keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+def print_python_version():
+        return sys.version
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
