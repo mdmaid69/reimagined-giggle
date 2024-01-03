2ddd66a56847c1dd36142de4ad283cdfa37d699c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:53:29 +0000

    warm performance bored in underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
