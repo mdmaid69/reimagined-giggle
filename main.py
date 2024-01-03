2c793ec2e81ddfd0a8db7f71b17a23b21a47f50d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:06:18 +0000

    uniform embarrassed modern turndown effort

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-print(sum(range(10)))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_density(mass, volume):
+        return mass / volume
