99813ad16c2ea01cd49a218c0cc69f6c31286619
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:35:31 +0000

    unkind payment follow fear characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
