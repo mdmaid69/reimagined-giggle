faa80c8f7693cb760073b7f639d17982a909f66b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:57:35 +0000

    police wake press subject brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
