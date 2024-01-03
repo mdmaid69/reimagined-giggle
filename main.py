f11de222990c9594a90e2b236f3ba9d7cb9fe204
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:01 +0000

    worse sleep learn mistake difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
