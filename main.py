c8a9bd3e294286b7d28bf15619f75dd881706691
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:52:05 +0000

    rich clearly settle breakin powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
