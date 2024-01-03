499502c06691e610ac09d9ea42683df76d0e59bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:07:15 +0000

    building pretty collection OKorokay pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
