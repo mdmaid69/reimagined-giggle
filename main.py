50f35f92541683087a66e78eb179f4a3e5114137
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:34:48 +0000

    winter emphasize bring really square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_area_circle(r):
+        return 3.14 * r**2
