41148bfdaad75798386eddc7e8d1bfe284dd952e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:27:16 +0000

    invitation form skin succeed quality

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
