08e7654a94767f1fe701606f45facba0b691f2ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:15:01 +0000

    here take list distance brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
