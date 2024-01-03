de3e45630a2612e585bf1018f62d609b9e87476b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:24:25 +0000

    please peace mother loud proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
