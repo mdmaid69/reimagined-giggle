9a982fc49c04aa8e5e14ee6e4f81343d0e6a39cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:51:40 +0000

    enthusiastic shine at excellent platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
