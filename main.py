c56a1e90093d0500f9acf4a15d29a654d179465f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:31:10 +0000

    runout weak education size rent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def add_numbers(x, y):
-        return x + y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
