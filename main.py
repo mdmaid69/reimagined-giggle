31597e6735edce39a2273018abf05ce55801bcc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:22:07 +0000

    uncomfortable peace myself principle key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
