c1fcb6e4336b8b5dc01a4333cd2c0827c8c7d0de
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:19:31 +0000

    coffee formal something pipe tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
