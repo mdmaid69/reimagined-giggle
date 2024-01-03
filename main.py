4ce30ffb10d7a502429c240d618ab027f45377ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:13:21 +0000

    learn slice violence do sit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
