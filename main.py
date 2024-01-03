b1861151d76a8acf7810d1fc85a4df9012f7ee37
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:43:17 +0000

    comeback level touch hurt sleep

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
