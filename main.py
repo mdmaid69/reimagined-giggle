be7578b33e0fc80f85b55f3b4fa9cee6164b920c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:44:08 +0000

    present spring admit buy back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
