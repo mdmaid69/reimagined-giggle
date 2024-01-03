f8503ab711abd1ab895b406df8553a1c7a1f51b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:34:58 +0000

    pay properly replace deliver mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
