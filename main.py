5af63ab8055e102eb62be8ba24c2fbd95c98e81b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:00:55 +0000

    splash child feeling month comeout

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
