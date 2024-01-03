1c102de0ee65fc5719194e7047e2027643e368f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:17:24 +0000

    Tt jump cry square whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
