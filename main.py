24f7e92dfea7c69ad1a0f514c027ed1e63a5a5c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:31:44 +0000

    behave murder weigh artificial pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_hypotenuse(a, b):
+def calculate_pythagorean_theorem(a, b):
         return math.sqrt(a**2 + b**2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
