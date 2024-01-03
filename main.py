7dfae444d3c02a8a27fd54f79d51057661fd8c57
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:05:58 +0000

    kick guilty steer next never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
