66385cd1cc62e55cc4ce9a996a4979d8536ab6bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:11:37 +0000

    spill flat circumstance properly inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_density(mass, volume):
+        return mass / volume
