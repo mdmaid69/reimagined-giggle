6abb8f1a76ac1b87eb2f780a64dfc18f39471010
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:05:25 +0000

    standard scheme on art muscle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
