7b28a37410d71ae4fc6be2999046e80637dcc94c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:20:08 +0000

    depend duty behave wide intelligent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
