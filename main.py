e494d3fecb6602d6bb90f1288227b015c1bcb29e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:55:25 +0000

    570enjoyable dress obtain 572enter clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
