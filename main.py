95c92869793ce63f4ab893e6a55a983b4ce7303f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:44:44 +0000

    turndown wait smoke possible prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
