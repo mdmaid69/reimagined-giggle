a11f12fbff3788cadd7973ce2a574dc05b968719
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:14:39 +0000

    ring polite nearly belief transport

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
