14faddeb6ae0707b1402418d8b4218cbeb42ab62
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:17:54 +0000

    callat character favourite store Ff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
