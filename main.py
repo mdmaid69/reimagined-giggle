5325146426d0d7a85a74ae21299b3f72b0e1262f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:27:20 +0000

    attempt deal burst last rain

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
