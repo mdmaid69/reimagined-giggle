f2092de9cf68970fcb647cb9b64ee583dddf6d79
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:19:01 +0000

    peaceful along invitation pay reply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
