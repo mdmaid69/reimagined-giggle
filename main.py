2a551e49e6f14a4d08b489a30f754ccf54fc0cd7
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:08:21 +0000

    react boil aim contain nonsense

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
