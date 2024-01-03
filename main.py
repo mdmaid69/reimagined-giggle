388fdc22068ee01ee5099fa177a7113c5ad9c6dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:45:06 +0000

    proper mind division plan grand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
