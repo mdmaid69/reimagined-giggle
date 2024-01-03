207a3cafd4ec339218b8d0b320e2e2a24ceff6c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:31:35 +0000

    Uu manage danger her suffer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
