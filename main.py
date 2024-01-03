3ae42ef70f0040ef239987578a036ae5f957c795
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:08:30 +0000

    blue direct turnoff unfortunate up

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def square_number(x):
-        return x**2
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
