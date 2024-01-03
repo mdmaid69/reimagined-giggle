db88613b6938759db4095cb7c96df592d6b39ca4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:22:50 +0000

    doubt foreign hand tune change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
