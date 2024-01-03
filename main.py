c2b68bef9b6a89095af9755deab633d97d6dc48b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:37:12 +0000

    task confused second thirsty back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
