ffa5d3dc00e2a3024ca4cf70a7d20a570aff0bb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:36:02 +0000

    react blind poison bottom battery

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
