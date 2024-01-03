866246f7d25d2dc784d98243da66c4272248fd85
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:42:06 +0000

    567enemy mistake big train realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
