4ae20716e56c0584c1e8e09648cdf3fcc633c541
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:47:24 +0000

    common between public charge bank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(5):
-        print(i)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
