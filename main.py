e418e16c9e6af11b14305625bb84fe88f82bbc98
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:09:54 +0000

    protection admire illness club choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
