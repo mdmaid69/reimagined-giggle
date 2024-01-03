afcecbde69e6a7bd204763f23dc64a5e499bc90d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:40:54 +0000

    practise steam hole land exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
