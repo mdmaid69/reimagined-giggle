03dda6780b1f90cab00c01601c1e23ae65c706ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:52:09 +0000

    dead comeback object blue bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_error_function(x):
+        return math.erf(x)
+def count_elements(lst):
+        return len(lst)
