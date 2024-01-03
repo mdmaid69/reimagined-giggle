d3d58dab769ca8213981a5329cac3788a0e4b6fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:06:40 +0000

    be label town shout compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
