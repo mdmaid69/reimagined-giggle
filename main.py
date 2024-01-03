aee9b6149ec92c888b4cebfe4a8919a611e2b51d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:10:40 +0000

    slow typical tooth song brain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
