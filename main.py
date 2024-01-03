d108d7ea14bd8746c45c47252516e58cb913ba74
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:50:12 +0000

    smart thinkof fond half increase

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
