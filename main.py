1518567fb80f66ce806821696b66a1a22df853f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:47:26 +0000

    there wheel voice spin see

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import random
+print(random.randint(0, 100))
