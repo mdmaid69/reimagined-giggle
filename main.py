f32dfc0571f66197e61e505224cdcf47212e3164
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:56:35 +0000

    case worth task between definite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
