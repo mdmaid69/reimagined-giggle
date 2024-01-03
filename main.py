2f4198de35c113e4879b218b3d126f3f04f33e2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:38:08 +0000

    meal set exact under would

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import datetime
+print(datetime.datetime.now())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
