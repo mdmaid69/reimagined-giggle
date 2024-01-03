110a947836f7720648d2c4353c8051ba913ce4b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:57:47 +0000

    skill disgusting damage anxious communicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def count_elements(lst):
-        return len(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
