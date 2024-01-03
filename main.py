d2ecef147368931318a6cb223bb90c07b8f66e8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:49:05 +0000

    difference return reach top dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
