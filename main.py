57c60eb59e4a1742dbd977ec8d17e568dca417d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:23:37 +0000

    way could free pass geton

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
