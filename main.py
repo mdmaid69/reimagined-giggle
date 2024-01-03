81baff553246ff4c5b1e8a0849082e0cf7598778
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:14:30 +0000

    valuable face protest knock ashamed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_speed(distance, time):
+        return distance / time
