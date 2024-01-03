c6690b78c3663627373f466b2f486c7e95aa8f4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:41:17 +0000

    close lay distance petrol complicated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
