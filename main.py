7fd4a538812ea29eb62d94e67886d7afa4c82f0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:19:02 +0000

    exploration criticize after comeoff mixture

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
