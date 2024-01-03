e5d1d59d62a690bdba76b2212b7e28229a4e2c0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:21 +0000

    joke hole familiar admit tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_as_int(array):
+        return int(array[0])
