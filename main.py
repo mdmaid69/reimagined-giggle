d3a31666fead85a2858223da7b18c7e3025ab688
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:22:11 +0000

    scale whom truth dream ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
