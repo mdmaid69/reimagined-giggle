91f0c2a8bd1498448ad77940c98fdbb90e86baf0
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:33:03 +0000

    rare mainly 569enjoy circle would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+def roll_die():
+        return random.randint(1, 6)
+for i in range(5):
+        print(i)
