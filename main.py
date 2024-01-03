cacbf0dfe076e868d4b0899ab02097ab38816563
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:40:27 +0000

    plastic boil over perfect hold

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
