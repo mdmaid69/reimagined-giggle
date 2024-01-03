e73e38e1a0eb998521a86e65a22ec075b7de63e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:55:32 +0000

    taste idea scream lower soil

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def convert_to_binary(n):
+        return bin(n)
