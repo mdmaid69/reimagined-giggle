56eca037310def25ad8bb7d9c9096f3a86926599
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:39:51 +0000

    ground lend extra independent behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import os
+def get_current_working_directory():
         return os.getcwd()
