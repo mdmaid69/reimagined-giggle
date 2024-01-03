5824a9075fe17d4a76af8d4a23901111b0f19bd1
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:05:46 +0000

    previous runinto watch this see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
