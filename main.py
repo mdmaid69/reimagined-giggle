adba584efd23eb4ccdebe1e8b81cf3cd224ea218
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:06:05 +0000

    ability rent nervous like sort

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
