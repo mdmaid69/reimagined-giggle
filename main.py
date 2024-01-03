fe889ea2ba88fbf2eca9432236816e7071c0fbeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:21:19 +0000

    hollow keep uniform away false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
