692107dcecd12848883b6918cf8089172429f730
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:43:26 +0000

    floor force never hand address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
 print(json.dumps({"name": "John", "age": 30}))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
