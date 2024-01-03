446c8578fd4ab6d0ba987d536b017ae9a0174bff
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:44:25 +0000

    sound opposite giveout admit modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
