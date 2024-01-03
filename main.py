728c677abd42b53132e8e3659bb309b2c04a688e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:28:56 +0000

    harm so instrument beyond similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import platform
+def get_os_info():
+        return platform.uname()
