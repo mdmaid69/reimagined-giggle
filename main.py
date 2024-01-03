4ed6af51fc42589a44181d32db097f2367e511a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:33:16 +0000

    argue transparent everyone often occur

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
