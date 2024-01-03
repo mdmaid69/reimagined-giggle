a4b581e3fcde5eff2c86bfd80468777a52ecc92f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:11:44 +0000

    full many so juice practice

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
