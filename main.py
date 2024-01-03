dd8e1addfa788f4e9d4982a7770ae754e2b8f25c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:41:16 +0000

    gather breakthrough statement turnaway politics

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
