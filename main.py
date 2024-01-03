95a6069185d4c32a04af04bbe7b1049f5df43994
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:36:04 +0000

    way centre watch thinkover hard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
