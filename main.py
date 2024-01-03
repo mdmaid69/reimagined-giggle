309c0634397929d8ad22a2d6d0c87c4916763f23
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:14:28 +0000

    mean positive project reason opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
