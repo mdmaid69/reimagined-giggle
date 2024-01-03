4f512de07f9237a690adf2a5f8fbb0869a570536
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:39:41 +0000

    stupid programme underneath normal the

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import numpy as np
-print(np.array([1, 2, 3]))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
