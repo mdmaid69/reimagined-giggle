6c064a9770fb6a5927e764b115b5b66eafd44cdf
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:11:36 +0000

    study reduce burn advanced sometimes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def subtract_numbers(x, y):
+        return x - y
+def find_unique_words(sentence):
+        return set(sentence.split())
