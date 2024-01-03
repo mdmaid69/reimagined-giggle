cf5c39494b6fb46de8106dd168a2a05214ec7c5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:38:32 +0000

    back cope later share in

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
