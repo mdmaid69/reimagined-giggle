4fb07cc1d1cab61a53eb0de5e04faab11943bdc5
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:47:58 +0000

    protest runaway trap real day

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
