f60ed653c0a6b79dc737a359d76ade523a4860cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:33:05 +0000

    find while surface male heavy

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import re
-print(re.match("h.*o", "hello world"))
-def sort_list(lst):
-        return sorted(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
