0be6bf2459070bec09a4652b5c139eca461fc8b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:09:56 +0000

    about sensitive smell probably justice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def find_union(list1, list2):
+        return set(list1) | set(list2)
