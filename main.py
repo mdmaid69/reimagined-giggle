ffd4d20d26c37f1a466d128b2b8c2459992dd026
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:20:23 +0000

    light Dd expression keepaway contents

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
