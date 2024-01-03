c557a72eb39b7a625dec42dffee3821b172663c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:54:33 +0000

    holdup for crowded fill legal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
