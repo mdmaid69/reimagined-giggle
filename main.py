2732258c04c68664fd892690fe59d01c4c6bca7f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:57:43 +0000

    noisy freedom healthy correct modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
