780aaf5d694c8614dd896e5fc4a18604c366123e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:07:04 +0000

    deal spill young mixture ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
