1eb740a945174eaf2862bded2a9fd8da9468a900
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:34:00 +0000

    laugh disturbing blue eager cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_min(lst):
+        return min(lst)
