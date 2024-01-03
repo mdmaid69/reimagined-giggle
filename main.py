0aa119031bb439d19e955dc4734389ca83db2743
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:33:48 +0000

    tread pay union person comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
