7b7f233d3e614f451eb6b4863e7d1c70415017e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:17:44 +0000

    maximum close description over some

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def sort_list(lst):
+        return sorted(lst)
