38a44fb191ea9bca5decb8e8485363297056d90c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:04:15 +0000

    answer lean comeback choose government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
