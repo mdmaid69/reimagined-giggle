8312419384d8412dbc59e8fe3cf76c92be79452c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:11:23 +0000

    now happy right something measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
