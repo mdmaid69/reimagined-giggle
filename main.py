0c7df101de35e669377763b46ae04f413c8a37c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:47:24 +0000

    right disturb regular look minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
