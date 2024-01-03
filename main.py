ff62d1ea12a6aa535881b3c1c7d04e7412079e2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:27:17 +0000

    aim soil priority field sure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
