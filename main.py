1268e1b6db2ecf664bf9ac63ec49a060888cafc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:47:52 +0000

    deal clean throw business of

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
