a02f778eb608e8325fc57b32041d6c4eef7b9847
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:02:03 +0000

    none scrap who print let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
