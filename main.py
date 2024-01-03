0f8338a8e77772c71f37e12254726206b8b96671
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:36:12 +0000

    market innocent sensible tense mouth

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
