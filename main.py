2747f3b9e6288bfa1700b626f94a41e2e7c371b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:36:50 +0000

    crowded ear grade Ss fault

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
