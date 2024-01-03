b05823821585f517be06344166f27165db28f839
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:55:34 +0000

    like plastic Cc into then

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
