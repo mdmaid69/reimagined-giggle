f7620c92572b0312e08385ac30afae2d5f96ab93
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:06:07 +0000

    sure bringup runinto whoever benefit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
