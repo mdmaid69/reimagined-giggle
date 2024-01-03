50115b2d4650a52a76788b476c8a096d302423e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:57:41 +0000

    mine degree chase mind ride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
