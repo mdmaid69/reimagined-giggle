099f7f4341e339bb07059771b21193baa2fbc962
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:22:05 +0000

    car past condition since care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def count_characters(sentence):
+        return len(sentence)
