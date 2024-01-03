8f810958c67e9b8e7f895e7ae05cfc038af6b00f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:20:55 +0000

    wear getback long month illness

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_item(array, i):
+        return array[i]
