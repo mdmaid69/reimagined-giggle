2bcb35f3c7964a024b2383f62d3685a79bcae38b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:00:12 +0000

    swing secret mine appearance keepout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+print(os.getcwd())
