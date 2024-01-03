c4572366933c86d9a2b166b9bc92d68b81146d2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:26:46 +0000

    identify confusing now schedule right

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+print(sum(range(10)))
