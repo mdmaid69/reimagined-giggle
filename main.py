e38c735a88f652ae59ab4479b4a868a9f8b43729
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:50:33 +0000

    possession wealth row division pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
