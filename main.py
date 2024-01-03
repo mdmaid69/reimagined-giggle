e23a47a07797bd2eda10244aa822a076106f082c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:47:14 +0000

    bang young send slip tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
