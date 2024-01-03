0cc72cd3275ebd04d006a478e656dd2c2b4874c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:37:18 +0000

    whether hurt nature pay sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
