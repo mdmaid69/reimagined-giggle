d90fe538c3a4b8827efd8cdd9587d65dccf4c533
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:56:16 +0000

    pinch majority touch Yy complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
