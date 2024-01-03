0578b745cca8515373081f969817db8f4a76ea98
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:55:46 +0000

    suit people final behaviour direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
