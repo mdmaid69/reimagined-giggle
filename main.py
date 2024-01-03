bb9b8de59c8640ed77ff417ceea07af22e441232
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:28:43 +0000

    story natural unlikely clue threaten

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
