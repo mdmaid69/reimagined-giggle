54975cc41ae0bf1c49bd07b137f363227f3326f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:44:16 +0000

    attitude bite unlikely 566end most

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-print(sys.version)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
