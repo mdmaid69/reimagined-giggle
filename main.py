92eb6118f4299aa6356ec2c3dabf5e027af4c8ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:05:04 +0000

    sure field bring power load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
