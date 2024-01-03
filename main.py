b57a3a9c28f15179f4868bfbe75259f4b709d30d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:06:15 +0000

    snow nature their home impressive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+print(sum(range(10)))
