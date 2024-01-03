577f1511b38335191a5c38aeb41175726d92599d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:12:56 +0000

    national goup to all trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
