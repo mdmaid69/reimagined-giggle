3889b8b74247c8620f56c1862a9a2d6bd34639c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:30:35 +0000

    shape news sea plate shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
