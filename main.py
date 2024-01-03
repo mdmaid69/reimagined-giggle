9b4ea410ad761b1a6e6683efea219d0c45aca8b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:35:01 +0000

    watch approve juice name wash

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
