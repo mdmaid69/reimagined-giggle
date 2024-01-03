ce967c6bdde973e2bec42c5e5dbc6288b6ad3582
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:16:09 +0000

    Hh soft curious ring angry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
