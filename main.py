92ffa102ab28da63f18ad6b0ab3d6e3e9913e412
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:01:16 +0000

    land Kk marry immediate strong

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
