d6cfa9353162d661d99ca368bc7ecd173b0ae711
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:12:01 +0000

    live put tradition perfect communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
