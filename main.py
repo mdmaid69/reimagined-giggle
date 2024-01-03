8e9391d36f8ca72abd96e9dbb5cddb2c8acaa833
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:51:41 +0000

    declaration air use naturally basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def sort_list(lst):
+        return sorted(lst)
