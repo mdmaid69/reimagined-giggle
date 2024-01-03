1af7dbb2f20fe7e9fb7267dd3d526fc7669c43a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:38:47 +0000

    jump bet correct attention simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
