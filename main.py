8a4b86681f4954a42d9e1641cbb28472aeba8f9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:41:01 +0000

    programme supply wish match tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+from collections import Counter
+print(Counter("hello world"))
