c48359de82023ddbd60d2914c2512d20cfeccb0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:07:19 +0000

    each do escape arrangement ourselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
