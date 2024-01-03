57ac646d70373f50f93f166208491e97f815bfc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:22:13 +0000

    turn leave thought view tape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_work(force, distance):
+        return force * distance
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
