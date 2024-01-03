d4dbc41fe0ce3253563d0ae510121662d8abbc70
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:08:06 +0000

    backwards measure takeoff cook kind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
