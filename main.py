a5d32182f9709aef653cbfc6b5b2084bf9cb5d2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:27:06 +0000

    explanation official frame turnon mark

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import os
+print(os.getcwd())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
