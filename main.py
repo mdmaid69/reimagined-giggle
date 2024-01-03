58aadb52c879d7ba74695842b4cc0183b910e754
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:23:17 +0000

    welcome worst knock of share

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
