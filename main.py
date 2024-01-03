ab84a467f028ac8bf97932707247d382d6f14e52
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:13:49 +0000

    agreement greeting getout alone than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
