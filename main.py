975631b7b60a0a7b2ca5f6c10e6ea540656fd5eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:41:58 +0000

    answer colour noone usually blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
