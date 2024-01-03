f3c89f67a8bffd20dfe442fb698571b67b67e220
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:54:29 +0000

    reserve slightly bringup clearly dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
