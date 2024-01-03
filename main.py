7ec849b70e76b7a993ea2a62104c4f3b12025e85
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:30:32 +0000

    eye other way straight centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
