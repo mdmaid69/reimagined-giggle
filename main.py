60e8010c317a5f9a34046fc255315d439b26eeab
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:15:27 +0000

    apply happy wave across still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
