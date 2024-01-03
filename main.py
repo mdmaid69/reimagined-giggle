219fd62cc86ee75e5bed092307d44a3571ca3394
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:35:01 +0000

    precise morning switch rarely relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
