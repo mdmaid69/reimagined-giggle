1c7136c3c5ab344b5a78952256866195f6a842ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:35:49 +0000

    goodbye church state affect container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def count_characters(sentence):
+        return len(sentence)
