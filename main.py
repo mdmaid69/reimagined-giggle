b8c2fcdb34e670013cd6ee6da75cdac495565416
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:50:41 +0000

    unfortunate tend story certain tell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
+  def calculate_area_circle(r):
+        return 3.14 * r**2
