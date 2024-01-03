8da3aebade3e329d52d235e9e796860fce3aee19
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:11:06 +0000

    pretend workon contribute anything mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def multiply_numbers(x, y):
+        return x * y
