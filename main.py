360f419018f541d4f3bdd1b3c11c5c68283e2956
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:27:02 +0000

    degree tie uniform dig him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
