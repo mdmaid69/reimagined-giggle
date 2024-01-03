b0908fe104e9c7fa7c12128616a08bf8074f75d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:18 +0000

    while Ss build constantly side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
