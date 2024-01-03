b9e737a82d12cb44c91e899aef88fa9a65a5b555
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:27:12 +0000

    on enthusiastic computer scream illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
