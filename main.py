02226073a5789b435976b1cc6f4250939bde3755
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:56:34 +0000

    advice pretty however poor address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
