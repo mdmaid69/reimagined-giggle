96340e35ae6bbc1988d00ea8679643d8e60cf7d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:10:46 +0000

    summer test enthusiastic wave write

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
