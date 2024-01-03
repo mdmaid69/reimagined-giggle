aabb0060e08cababffd623c1703f06d11f0346dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:56:29 +0000

    work gentle clue confuse earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
