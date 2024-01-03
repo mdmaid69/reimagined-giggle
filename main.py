bddf26433f284b376690efc42a453059bc43b77d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:42:24 +0000

    brother turnback mistake ahead service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
