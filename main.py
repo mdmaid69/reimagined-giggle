3c01eac3ba67c4c16f9e71faef7d1d86fc2bc36f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:31:15 +0000

    in left public born put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_floor(x):
+        return math.floor(x)
