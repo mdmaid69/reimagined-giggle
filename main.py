c1b4d0922a5e94fd945d5419d3ecd857290f7e71
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:39:58 +0000

    delicate rare confusing goon man

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
