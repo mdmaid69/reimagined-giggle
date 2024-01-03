47491241df63006f9ecc4552ba6a38c33799d6cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:51:37 +0000

    education destroy contribute field lookaround

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
+def find_union(list1, list2):
+        return set(list1) | set(list2)
