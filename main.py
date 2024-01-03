bee23ec4c6d62e70bbc48654a334ac4a48d6c78e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:26 +0000

    mention splash on our hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
