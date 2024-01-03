3d69636f1d9a39a2384936c2e929b1aac9f5b844
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:06:46 +0000

    definite only payment height round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
