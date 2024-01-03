52a11c38cb4ff42177ba264a95c54ea776e35333
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:19 +0000

    black tired plenty innocent whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
