a2e669ac73f14944f1f002be4ec2d38fa253110d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:37:06 +0000

    money definite behave study press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_max(numbers):
+        return max(numbers)
