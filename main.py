572b3fc93395be4cd0a356e22cc79c353e684505
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:06:53 +0000

    wrong run apologizeapologise middle middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
