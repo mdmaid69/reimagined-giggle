1e84e55277f25b6054798f3616c7a013c40bd380
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:12 +0000

    hide comeover lookaround poor hide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
