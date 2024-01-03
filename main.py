107e6a53eb4afcc3dacfa739eed1671e7e7c2305
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:41:58 +0000

    modern delicate slow limit just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def cube_number(x):
-        return x**3
+def convert_to_hex(n):
+        return hex(n)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
