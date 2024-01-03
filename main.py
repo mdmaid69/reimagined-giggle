7b2a97cd9bd2ec0460a507bec165160b02b753e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:16:51 +0000

    scare own violent fortunate light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
