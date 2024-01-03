bb7a8bc73cb8555ae6db11dea6208290777cddcc
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:07:22 +0000

    and recently mine getup satisfied

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
