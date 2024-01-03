25d72c4663a36127a1fb4e5216d6dc044f555056
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:29:29 +0000

    question anybody to press practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
