f4c10953a1216c74b6b4573feaa92673769111a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:24:53 +0000

    help recommend special combine hour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def reverse_string(s):
-        return s[::-1]
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
