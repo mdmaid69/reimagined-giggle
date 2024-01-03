7e02e20cd4b5ae60e47c032842e3d1590b4c37a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:20:44 +0000

    occur ask male pronounce television

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_length(array):
-        return len(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
