678a72444d903e66e6d15a044a145aa10bbc8f97
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:47:14 +0000

    future talkinto entrance afraid side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_index(array, item):
+        return array.index(item)
