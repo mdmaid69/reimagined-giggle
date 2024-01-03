71e9dc01c1444752869e165ecf3964e52eb49ffa
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:02:10 +0000

    straight contact profit closely cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
