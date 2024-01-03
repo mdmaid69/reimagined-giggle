a5c56c1476b79488fc16e4ce0a5effcdc20cc371
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:49:35 +0000

    lose story breakinto bit near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
