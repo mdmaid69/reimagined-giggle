4b1bdfdfa3ba161ff4cff89d0716a4373d523eff
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:30:50 +0000

    upstairs rid below trust genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
