012e9af97f729f24d5eff6474546d0be9cbf060f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:19:04 +0000

    somewhere pupil really getup age

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def remove_duplicates(lst):
+        return list(set(lst))
