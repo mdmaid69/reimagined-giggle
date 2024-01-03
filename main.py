c170161d33569b42e1fc2ada032b5aa25d094bf3
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:58:26 +0000

    behave accordingto union project nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
