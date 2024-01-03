d73ff0e6617cff1ffcb8040a8193b8ab204c8dad
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:11:40 +0000

    mix beside confident regard coat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
