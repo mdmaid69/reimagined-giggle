b52b2785a8da5be9ab7ae1031455a0991ec41aa1
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:04:36 +0000

    admire aware impressive pull such

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
