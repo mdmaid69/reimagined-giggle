518f4fbb37ae75a18b8bc769319e0e8a1939e5be
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:00:32 +0000

    improve wrong soft remain protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
