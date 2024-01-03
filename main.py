5bd0430b26303db9ee9ed9b4962616bbe8fcc610
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:00:42 +0000

    else friend board fat quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_current_directory():
+        return os.getcwd()
