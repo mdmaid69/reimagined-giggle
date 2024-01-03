871abfb744e9f97291de00f94b7004ea60439de4
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:15:46 +0000

    object consult shape travel collect

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def subtract_numbers(x, y):
+        return x - y
