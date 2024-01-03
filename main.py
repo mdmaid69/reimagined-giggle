74ad1bbe426a7695d902889faac01a61e5082a8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:18:14 +0000

    judge traffic debt note home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
