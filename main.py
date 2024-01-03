a75565eb1564f5256bf9837d301dd0fc6497149c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:07:07 +0000

    roll turn awake issue negative

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-def exit_program():
-        sys.exit()
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
