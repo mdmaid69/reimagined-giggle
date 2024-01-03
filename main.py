c378dc2aa589754bd47ecf342bf3641753b4bf70
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:57:08 +0000

    plan they advice value pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
