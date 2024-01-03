5accc3a158b505d73b11a4dde994b5d48fb63cbf
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:01:18 +0000

    loan likely performance warm runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
