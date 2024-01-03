8893808544ff0e84f93ef75c7d0f952ab45a9cef
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:44:41 +0000

    they snow myself lose goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
