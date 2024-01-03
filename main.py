1efbeccf6543574cb3d8c7cb4437e396bd026b2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:45:27 +0000

    unit club thin recently club

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
