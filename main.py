71ac146fe405f2d4a7d32793bbca5b569f9337e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:41:06 +0000

    give should care with valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
