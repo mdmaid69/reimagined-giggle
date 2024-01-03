08fcbe215ad78c87e201fbf940d412d38e92ed2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:37:36 +0000

    one do taste wet believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
