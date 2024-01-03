7873814a5c9788e56871a9db2ac012d830f268dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:48:05 +0000

    period whatever stamp positive make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
