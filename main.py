ab40a280a6411f6281a9dfcc645d56fc4b9bb731
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:31:37 +0000

    bar disappointed patience regularly pocket

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import time
+print(time.time())
