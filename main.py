dd039b4c31a7db152fe6424b5c2485460e39de5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:56:09 +0000

    definite die Ss concerning spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
