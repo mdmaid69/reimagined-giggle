d11ec374e4b74ab9b71db6dab1bd737b54ac8622
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:35:11 +0000

    exam busy prefer round man

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
