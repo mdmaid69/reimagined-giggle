1bdb683e6be85718c9d87476a763021c4bfa6989
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:17 +0000

    fortunately rely spend hard properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
