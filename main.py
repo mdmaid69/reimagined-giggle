c0d0f5e78d0f45ac22fbdeee305852f0ec6fafc9
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:59:37 +0000

    callat generous walk constantly emphasize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
