4b3c2a885fab95778615077a8bb2b8886a90f90e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:21:48 +0000

    steady constantly contact cough show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_str(array):
+        return str(array)
