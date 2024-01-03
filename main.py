40285e19848abb7723d9fcc28c875ab4384135f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:55:07 +0000

    post into unkind exit wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
