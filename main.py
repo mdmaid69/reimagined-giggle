3d319d895a2115e97414029a564ac0801003633d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:26:04 +0000

    fork consist fly path treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
