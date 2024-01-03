ece0681db9b48b5931e6409f5cb326cacc396222
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:21:23 +0000

    organization mother hit bet large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_even(n):
+        return n % 2 == 0
