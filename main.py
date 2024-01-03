144c00ce4524fc31f05fa34bb2b5f4c3e401122c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:53:40 +0000

    defeat giveup extraordinary prove point

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
