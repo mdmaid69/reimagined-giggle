4cb5d46f24d3884b15fff4236ec46b2be2ea41d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:46:27 +0000

    worst measure copy seriously artificial

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
