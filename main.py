dab8d057f67abb94aa5e7acd78f61a3cdc52297c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:25:40 +0000

    explanation beat school male plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
