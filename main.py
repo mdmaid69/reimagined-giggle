fb4e41b6e3a880b95bf4fa5b8f65df603f418acc
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:24 +0000

    unpleasant rise fun software callback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
