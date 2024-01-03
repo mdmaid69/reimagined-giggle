537e80fc5fec541fb056ffee009d9348d2ce4ae8
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:20:40 +0000

    boil light sing funny passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
