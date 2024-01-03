edd55cf2c1153144a801e0a22d62495f7c351fa9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:42:09 +0000

    where fairly yourself tip below

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def create_user_dict():
+        return collections.UserDict()
