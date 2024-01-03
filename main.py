165e494097e269a4844b72f3f2c70152808241bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:10:24 +0000

    check list on change priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
