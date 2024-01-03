06af85567d17523b335dd313eefbeefa7d6eb123
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:02:13 +0000

    little tap together unless save

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
