9144bf64233db8ead29113b66f4eace239350c3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:46 +0000

    mouth water future another breakfast

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
