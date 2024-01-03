2495a5e291d7b4c4c5ab717812e805023e6d8514
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:06:27 +0000

    matter Ss damage car appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
