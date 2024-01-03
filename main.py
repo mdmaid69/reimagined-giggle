498f5dcaf8760f27f4fe236a7756cc0d9ac304df
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:45:16 +0000

    limit throw kind another change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
