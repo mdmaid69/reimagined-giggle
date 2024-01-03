cb4bcc91daff8e8bc24d533b1e134c329e501486
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:20:13 +0000

    tragedy poor curious length like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_sine(x):
+        return math.sin(x)
