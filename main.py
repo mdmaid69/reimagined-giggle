ad77f87cdae264a8b2d39161d427d81d171ab9dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:40:22 +0000

    store trick pencil total 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
