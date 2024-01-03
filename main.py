a63c7518fabee7d5c63d5e837d63cb38e0eafce9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:13:33 +0000

    satisfactory cross fast home broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
