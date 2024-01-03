37fff3ad7e4820087e79af7fb8abb799af889369
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:43:50 +0000

    measure Bb tape talkback instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
