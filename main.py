f1d0442e8cdab0bbbeaab088187721e344c7c37e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:35:08 +0000

    activity usual harm juice become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
