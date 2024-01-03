e68f19c42846e3407492f80abb457882b54a0f27
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:11 +0000

    unusual suggestion power excited rather

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
