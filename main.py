d0d6fdba1caabd296fa2c926853bc140bf44ec95
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:56:36 +0000

    practise push move replace cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
