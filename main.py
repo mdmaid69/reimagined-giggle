9be5bc590706f4761be5185a859c3158bf7a5b07
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:06:15 +0000

    dream empty develop crisis deaf

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
