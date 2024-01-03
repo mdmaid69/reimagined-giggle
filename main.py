f64789bb6acb591f6448c8903760a30b0cdd5ae4
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:56:00 +0000

    education compare stone indeed watch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
