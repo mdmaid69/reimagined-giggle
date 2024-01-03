4f4b77593aa05972b6690ba600b3b6edbc16bdea
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:37:34 +0000

    talk television circle fall pot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pressure(force, area):
-        return force / area
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
