72e713999119f22bef65a62060c7f7f086dac6d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:28:36 +0000

    suppose behind spare smoke badly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
