8ff877c32b466f3fc5f6e51dd19c8e050fccd409
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:41:02 +0000

    anywhere speak danger substance bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
