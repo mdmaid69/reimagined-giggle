6b50f8416eb5bf7873030a24c7ab88c0cd7fa4b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:49:11 +0000

    pin suggest correct wash try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
