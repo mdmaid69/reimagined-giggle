53e05705123d23e0f00b5ac208cdbc39625fb3fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:43:06 +0000

    shelter church declaration breathe miss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
