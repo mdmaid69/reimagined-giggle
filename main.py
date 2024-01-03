b28865d0f0f70ac552a33b0d8600ceb0c09e9ba8
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:24:24 +0000

    bury reaction sign increase tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
