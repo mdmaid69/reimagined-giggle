248951c019f081a10c5a1f9c88591f612066e023
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:06:42 +0000

    tap wide over concentration climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
