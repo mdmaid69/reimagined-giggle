00139a5dbff15049cb70b9fc53f906d2b75750d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:55 +0000

    ourselves juice wrong danger size

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
