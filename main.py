cb2e560e6d80ce649442ff0f79a073a47e4cd22e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:41:49 +0000

    their foreign typical seriously after

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
