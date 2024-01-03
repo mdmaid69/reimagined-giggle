a341ae742c6c7b774ac945e6e57644fbc4f6b23e
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:17:16 +0000

    convenient goodbye negative edge summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
