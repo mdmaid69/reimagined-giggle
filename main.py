495d0675b20c79230fe082578627234c787b242a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:36:22 +0000

    bin bill bill start female

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
