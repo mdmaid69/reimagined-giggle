3cc5dbb94e085e07211b8d3ca3cf4bab17d350b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:02:01 +0000

    own boil hard secret control

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import os
+print(os.getcwd())
