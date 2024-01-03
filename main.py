33ae07053fb93f92d5bb8dff5c06ff22b016f378
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:36:10 +0000

    takeout contents cheat Cc still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
