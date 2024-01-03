b8d735275b17c48f8209f6f1d99e23a48686b63d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:11:55 +0000

    suit basis classroom change our

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def log_message(message):
+        logging.info(message)
