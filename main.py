720dee823995804308b1dac8484f7fee45b65bbe
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:07:25 +0000

    male male pity share information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
