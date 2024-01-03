b4e50d3d2f73b3abaeef0d593fe837c01c72f888
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:09:50 +0000

    there talk reaction sweet rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
