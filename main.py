217d1fdf9bc4121e9fa21d3dc86adb7658329931
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:30:19 +0000

    admit lose commit measure card

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
