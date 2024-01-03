644c465ec501eb5731ee4e13725fb728c3c93aa7
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:30:34 +0000

    mad fit prove pronounce front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
