1df35be655634cc02721422945b8acd7faeb4b84
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:00:01 +0000

    pen system say whether that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
