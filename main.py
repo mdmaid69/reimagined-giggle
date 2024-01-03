2618ed24692c379de392e719b524c21c9ab73c97
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:53 +0000

    desperate injury introduce human exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
