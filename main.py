87e400b3048afe463bff428af741f00ee724bdeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:41:24 +0000

    use makeinto balance sensible error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import collections
+def create_user_dict():
+        return collections.UserDict()
