cc011380de539f8eb2974ee40a90a45528096dc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:41:54 +0000

    allow win confident 564encourage unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import time
+print(time.time())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
