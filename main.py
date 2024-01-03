14aa0c22a809d3b05186a675a2e508f0f95a8aff
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:15:24 +0000

    invitation bang lock lot gradually

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import getpass
+def get_username():
+        return getpass.getuser()
