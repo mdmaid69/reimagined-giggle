528dfacf32f18c6012ee25aa96579a1fb5bd4414
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:10:24 +0000

    roof transport insist live phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
