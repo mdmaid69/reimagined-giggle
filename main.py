0c667aa0c50a07d5d9a1434d8d9a6d5c92eea839
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:02:21 +0000

    afraid charge total finally Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
