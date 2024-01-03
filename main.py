a5ab5819c6604aa953cae9ff419916869eabdb4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:15:46 +0000

    support office wave never clear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import time
+def get_time_since_epoch():
+        return time.time()
