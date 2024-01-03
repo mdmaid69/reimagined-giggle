8a84ffa19e53dc2014880a8c960e5aa1cd9f5130
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:49:48 +0000

    strike butter often quiet take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
