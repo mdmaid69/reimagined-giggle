cec96bd17022e2301044772c07181f501461db83
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:04 +0000

    unable ilovedicks enthusiastic improve full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def square_number(x):
+        return x**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
