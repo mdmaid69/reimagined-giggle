69b457a4706b9f0d7298be3a496f927eb5f426ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:25:51 +0000

    gas awake instead base summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
