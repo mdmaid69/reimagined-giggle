8ab2af61993436d5d7b157f67faabbb9367808b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:45:58 +0000

    escape right basic day straight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+print("Hello, world!")
