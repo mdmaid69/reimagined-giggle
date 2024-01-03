b8c3ca5b7f9222aff762f51d04963d674635ce46
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:42:25 +0000

    makeup custom clever fix chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_list(array):
+        return list(array)
