826e9714c39fc716457aab070179a3b67258d935
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:29:52 +0000

    unless accidentally subject space beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
