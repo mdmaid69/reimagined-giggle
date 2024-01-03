31805955fd3d3f74acb44cfb77c3f89bf62c793d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:39:12 +0000

    mess voice shopping succeed rough

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
