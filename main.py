21440399e7b1ae2e69e35ccb309f556426807a5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:17:46 +0000

    unlikely call experiment normal lower

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def multiply_numbers(x, y):
+        return x * y
