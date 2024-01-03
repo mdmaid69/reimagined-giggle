c6c102a36b2d6fb62e5371878adbd0a598fb5502
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:29:30 +0000

    nice same Vv cold ours

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
