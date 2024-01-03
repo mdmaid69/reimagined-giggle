9c04e58673299e0e0d2706c988926276a1fab191
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:24:09 +0000

    make narrow dear heat wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
