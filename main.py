e786066c5a5833d7b35a324a5126b3b5c87f8df4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:20 +0000

    order earth unlikely some walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_str(array):
+        return str(array)
