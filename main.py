487dec84cad887f72d483461d743aad85aeb3a7c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:34:05 +0000

    allow fat opinion zero sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
