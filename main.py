9fe08da9eed69e64c3fa6c09e2b6dbc7ffd3137f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:38:18 +0000

    takeoff unless discussion mysterious impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
