3ff2858969748848376a674c43eeae3591aa08f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:30:59 +0000

    shout grade Ee stand rank

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
