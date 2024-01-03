710be18a568a3771436b345a1fc5da0b10f80fb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:15:05 +0000

    ground mysterious think become usual

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
