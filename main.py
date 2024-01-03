b5b5bc1ac576c9172540ba4a41382fc5b28d87fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:28:35 +0000

    shame market children friendly keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
