94214e8a589d38376ed409079dcc3008cc5731c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:47:59 +0000

    sort report release duty season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
