1d557471e1d073ccc5041fb18f5d775cf4ab52d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:43:47 +0000

    sleep manner injury strange space

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
