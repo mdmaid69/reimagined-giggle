db089779b81e96d754cb770904a88aec51c41ef0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:19:16 +0000

    serve company sign usual when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
