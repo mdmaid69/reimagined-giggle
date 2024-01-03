e101216bdc1124e0d9148718f5604c80991543ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:15:13 +0000

    environment valuable run store precise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def square_number(x):
+        return x**2
+def reverse_list(lst):
+        return lst[::-1]
