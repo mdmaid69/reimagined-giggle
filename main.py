836c2b3ac0564ec71cce7f18f7f86a68a010cade
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:14:59 +0000

    band appeal talkoutof visit city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
