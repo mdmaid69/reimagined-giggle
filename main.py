c6a4f4acf7657c93cf6e7fb3618f1c111025f7a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:38:49 +0000

    tour thinkover treat hollow damage

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
-print(sys.version)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
