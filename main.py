767ac09c2c9be4c41f9498027c2feb8d40205327
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:00:00 +0000

    be pattern goodbye schedule intend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
