e84f8f50a128df4d3da31cb1165848eea2a4a6a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:45:15 +0000

    broad easy move favourite safe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def is_odd(n):
+        return n % 2 != 0
