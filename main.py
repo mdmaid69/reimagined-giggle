3a1f8f22d9566c25a1cff7f5881e865ad44938e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:29:27 +0000

    news complete increase handle nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_queue():
+        return collections.deque()
