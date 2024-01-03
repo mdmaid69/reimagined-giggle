169d36b43d5b008774a7938a3fdbc8309443def5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:42:45 +0000

    ours bridge equal hollow club

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def is_even(n):
+        return n % 2 == 0
