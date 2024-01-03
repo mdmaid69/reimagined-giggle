df02c884c7be459b391746cf17e7cae1139eb76e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:51:41 +0000

    form however believe figure tired

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
