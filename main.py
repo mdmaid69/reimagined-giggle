28ed447f6383e58110036c21b8c7bdc680105072
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:39:08 +0000

    we we into purpose upwards

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
