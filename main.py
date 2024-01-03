62c879072761a2d80efc449026d8cec45613edf7
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:23:31 +0000

    cold slip of reason tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
