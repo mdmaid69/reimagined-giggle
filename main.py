d1781bb510fb777718f72869f8b0b9564c88fc0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:41:11 +0000

    on Jj smart holdon company

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
