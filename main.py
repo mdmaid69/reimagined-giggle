e743cb1eacebc8c9236f1a618dee9399c79f0ca7
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:07:22 +0000

    impressive power constantly weather off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
