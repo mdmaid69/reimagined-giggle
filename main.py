d84559330d9747278cf895d794a4df29452e524c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:28:01 +0000

    first shop before wise fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import time
+def get_current_time():
+        return time.ctime()
