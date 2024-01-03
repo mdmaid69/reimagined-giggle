59bc8850444284274ade943ff250cc5286014a4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:47:49 +0000

    probably note face what open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
