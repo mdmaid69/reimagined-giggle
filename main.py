8afa24b6d513155cca67e193ace5179aea8384ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:22:27 +0000

    possession favourite you how workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
