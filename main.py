6dc0a0c6d0af5b2b1c1c79b13baf50895fb30db1
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:25:56 +0000

    live communicate wake butter influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+print(math.pi)
