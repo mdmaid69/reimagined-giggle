3b8531f710734865fc7f67781254ede2cab55f50
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:07:51 +0000

    sensible as too convincing smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
