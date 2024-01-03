ab44dbe6cc577a8a397bb637ee430bebc478e375
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:37:51 +0000

    flat urgent charge mail tip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def multiply_numbers(x, y):
+        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
