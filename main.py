58028f49390692d85d69a9a6666d39a55fb28eba
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:43:40 +0000

    paper spoil so past strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+from collections import Counter
+print(Counter("hello world"))
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
