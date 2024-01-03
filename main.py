25289414d7e16d9e7ab8ba33b7e528faa518d0c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:40:42 +0000

    sea dead absolutely whisper valuable

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
