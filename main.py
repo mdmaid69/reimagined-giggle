1e03e172aaabae23c87e95137cfc0c958bf7070d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:20:14 +0000

    change rank apologizeapologise break sweep

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_acceleration(speed, time):
+        return speed / time
+x = 10
+y = 20
+print("Sum:", x + y)
