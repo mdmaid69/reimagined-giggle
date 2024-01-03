a4eacc61129aa71362c5a3057d60e72f100be111
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:22:26 +0000

    wherever shop shock gate appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
