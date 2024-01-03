c9dda4a9de5b9811dacfeeec66d284b944bbc471
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:00:03 +0000

    entrance foreign collection meaning pot

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def is_odd(n):
+        return n % 2 != 0
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
