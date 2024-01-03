9a358173600e7652b3e200a9c5257d4f3853c0b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:20:02 +0000

    load married bringforward dress exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import getpass
+def get_username():
+        return getpass.getuser()
