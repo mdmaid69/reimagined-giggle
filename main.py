99497196146ba65be38a4d378aab5f362fa82047
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:23:26 +0000

    swim undo fire about stop

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
