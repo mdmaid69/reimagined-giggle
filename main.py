15cef0c5caa7631f3cb12835690314839b7aa615
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:05:50 +0000

    pupil tidy sure best pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
