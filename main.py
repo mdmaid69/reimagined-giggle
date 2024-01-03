e60db1f544989bb477f6890c45e9fdd8204a0ce1
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:09:19 +0000

    capital next bottom blind arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
