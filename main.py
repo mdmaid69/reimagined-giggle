a2b7a4185e2b87992ef553fc81a4db654a3acb99
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:46:07 +0000

    popular connected feeling nigga classroom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def delete_file(file_name):
+        os.remove(file_name)
