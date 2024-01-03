5e6c2eff40169160c4d35ef764a40e6f2a0efdb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:15:59 +0000

    rent try on on ignore

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
