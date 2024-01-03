a03c0c43e7d8e0cecf51e1ef0e2ab1bfa0e9b492
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:15:03 +0000

    medium want appeal sleep connect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
 import array
-def get_array_as_set(array):
-        return set(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
