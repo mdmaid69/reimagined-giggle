289f275280212564cd3670f325894861ce447d3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:25 +0000

    anywayanyhow cut cap scrape favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
