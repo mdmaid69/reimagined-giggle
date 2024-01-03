697a4fa51c65257ee75f4aa6f0b236fd75936aca
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:40:31 +0000

    bush butter flat help land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
