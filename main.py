099b9179345089be3e1f9ecda2d23f2ac838d438
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:49 +0000

    confused declare balance flood waste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
