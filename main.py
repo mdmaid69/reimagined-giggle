2b6d8cff025e347ebfb2edf825c400de3b2d010c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:11:21 +0000

    picture disturbing about comeround getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
