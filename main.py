e569b0de330671afecf0538e24562165bbfd9c11
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:43:45 +0000

    bag field service sell dull

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_unique_words(sentence):
+        return set(sentence.split())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
