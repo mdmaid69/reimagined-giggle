204f9b65d22128d4304fff777d3bb5bac6a6c59b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:56:34 +0000

    aim violence taste message together

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import time
+print(time.time())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
