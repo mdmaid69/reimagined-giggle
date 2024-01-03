d005b346b48c34c51340ca8588bdd8cf44c0291c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:47:49 +0000

    though want challenge communicate certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
