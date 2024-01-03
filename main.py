8beca8d36ce179d2196685533b41f8b8d86b9add
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:11:49 +0000

    benefit edge rest better noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
