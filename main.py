6e27f008d62e32925030ee138ecd23f89c577e4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:26:18 +0000

    mystery book manner unfair realize

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
