57fb1d39628fed21a4c76c8d0bb10dc32e453352
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:56:06 +0000

    bringup they hungry feeling approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
