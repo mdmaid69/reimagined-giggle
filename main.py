c13d6697c6503ead59a8caa3b29675187d8abc60
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:26:48 +0000

    wind justice politics disapprove picture

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
