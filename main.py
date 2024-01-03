f684472b54d93d0f278d1d60593673f1c9f036ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:33:47 +0000

    broad store press like correct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
