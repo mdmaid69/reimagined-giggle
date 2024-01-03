3b63c2a7747a19d4e05fcfdfed848aa924a1cd0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:28:24 +0000

    thinkthrough gooff dull affect callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
