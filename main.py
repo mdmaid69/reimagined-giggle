775a7d8008b41e17beeb53d53ff74c308f8beeb4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:07:13 +0000

    rain earth national hot study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
