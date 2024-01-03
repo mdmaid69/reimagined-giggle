3030ba50131725ae590d63bdced177198d6d8104
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:26:40 +0000

    gradually sure dish achieve else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import platform
+def get_os_info():
+        return platform.uname()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
