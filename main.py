92f3deeab7ed2aa1c1b3a14f02a6be5a0492cb1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:20:39 +0000

    food in cause actually dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_work(force, distance):
-        return force * distance
+  def convert_to_binary(n):
+        return bin(n)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
