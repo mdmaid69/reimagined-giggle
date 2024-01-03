a1c67b5a89a5d47c3ff918d11e2ac24678ab7050
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:53:57 +0000

    Jj kind protection plate hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
