f50f0caffab6eaa9a7a56e0139f8481fe436fec4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:40:32 +0000

    pride throw pass 565end of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
