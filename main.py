2878bff6bc568c6bdcb70d25a8aa67903cae1b11
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:09:43 +0000

    so believe size bath doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
