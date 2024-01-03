280a8edef365b46e6d25f9f082e9e66d094aa436
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:49:22 +0000

    flow interested nowhere far learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
