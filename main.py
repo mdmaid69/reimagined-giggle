1ed0e158661c0c3e6b11b6633be3a7af1ca890a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:36:24 +0000

    left hand nonsense treatment terrible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
