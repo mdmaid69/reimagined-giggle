a53a488fcadf2bb5ccae0cc74378b1920a0aed86
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:21:25 +0000

    sort remark tip display remember

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_elements(lst):
+        return len(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
