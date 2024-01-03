bfde7f287a9f32bee2ba9a796c13f32449890b85
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:58:03 +0000

    holiday direct frame high copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
