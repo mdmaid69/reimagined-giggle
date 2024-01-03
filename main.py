df5317073dab37c9bb0b335aa4a3e4e14252ca59
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:07 +0000

    forget meal sink measure half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
