459e12a75c36d5e88caf04c03550d246d58351f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:53:49 +0000

    touch swimming identify concern search

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
