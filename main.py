c1a1993445e23c3ae8889bc6deaab22d130951b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:43:16 +0000

    show getup advanced weigh guide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def multiply_numbers(x, y):
-        return x * y
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
