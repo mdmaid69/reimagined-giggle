fd0b3f0a2d1a9fcd87bb796a8b6a094b874c097a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:40:51 +0000

    rush rescue seat government about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
