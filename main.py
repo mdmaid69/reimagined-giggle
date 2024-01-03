f2271469f750d29d7883e895d316615b4050d02d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:49:06 +0000

    negative leg use negative sensible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
