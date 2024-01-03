5e7b7c645a3f1f3255b2e030dcdc5c69b574868a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:15:17 +0000

    entrance after cheat frequent balance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
