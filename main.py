b264e603bd27df29ebed8f322b64e700be8cd2e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:16:32 +0000

    sail her popular accordingto measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
