370d2a09969156cde2b5af7df10f6055ee8218a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:08:39 +0000

    concerned brown attitude turnout genuine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
