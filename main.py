c35cccf2d793ec3082887602b42ad7e028b4ed58
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:35:43 +0000

    trade cruel valuable illness violent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
