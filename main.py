88898fbc5d5287d0d6a93ae0dc7fd756716acb99
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:12:53 +0000

    tall collect meanwhile subject certain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
