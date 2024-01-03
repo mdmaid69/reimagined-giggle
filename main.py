55561216c9baf556d06b768e89a2aaadc927032d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:20 +0000

    detail rid point balance protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
