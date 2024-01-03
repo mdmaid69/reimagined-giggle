8593319c51d1d0c2ec36e784611992d79ebee768
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:55:12 +0000

    proper people leg black walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def change_working_directory(path):
+        os.chdir(path)
