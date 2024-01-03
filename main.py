ef11b3892926032082bb62350ddb208c382ef941
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:46:21 +0000

    bunch generous pencil copy sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
