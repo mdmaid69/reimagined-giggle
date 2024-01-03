4524f4a38a97c226ca88e33c2ef7d92db5569848
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:17:26 +0000

    protect hope offer thirsty country

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
