4ac1ec3608af8af85b103843c97e7a6cdf6cfa7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:07:33 +0000

    sing score breakin live she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-print(random.randint(0, 100))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
