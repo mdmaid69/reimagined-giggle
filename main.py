4e2cefa728bc694fe08a1ba273cdc8111a49c9ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:46:08 +0000

    creature walk slightly word closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def get_array_itemsize(array):
+        return array.itemsize
