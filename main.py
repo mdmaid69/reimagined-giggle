e8eaee70117e6fbcd5d2b0e8d07083cb4f6a8ef1
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:26:46 +0000

    ought thin secret short test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
