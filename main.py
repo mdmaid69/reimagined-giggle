ea1fc44ebca396eabdd1c4946f9610943334f03e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:51:14 +0000

    steer whistle autumn hot large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_area_rectangle(l, w):
-        return l * w
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
