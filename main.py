9ff96932659cc4ae04a0de6bb4220ebb34000517
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:44:02 +0000

    tear sense label hair accept

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
