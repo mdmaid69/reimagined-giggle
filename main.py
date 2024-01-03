ee47a8e43d100aa7274fe98258118f2b5a16d8dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:43:09 +0000

    learn immediate left 567enemy water

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import getpass
+def get_username():
+        return getpass.getuser()
