3753c629afda08c70a10913bb4df9ef7aef15212
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:41:56 +0000

    handle keepaway mine wheel possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
