9c1136ee9f80f8cba351300ed31462c314813963
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:07:11 +0000

    Ww Zz rough suddenly risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
