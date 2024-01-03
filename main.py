1224f81986b4bc2eeb528fc07f6ed41eafded77c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:30:08 +0000

    makeinto unite scare official piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
