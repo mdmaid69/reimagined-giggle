560948067a7aecae24314065d6f65117b05943c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:20:15 +0000

    breakfast home delicate fresh fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
