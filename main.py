04ee09c7569156e5202d718a24dbb433f692b319
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:51:19 +0000

    design stick again deliver substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def remove_duplicates(lst):
+        return list(set(lst))
