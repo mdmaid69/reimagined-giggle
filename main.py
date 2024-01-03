0535c24b88880306890b85d73c537af129c64ca2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:06:41 +0000

    pretty runover ignore only conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
