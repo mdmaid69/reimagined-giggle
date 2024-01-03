727dda04a5389ec991624b6cb43343f01b6ece49
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:01:37 +0000

    suggest lot platform acceptable appear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
