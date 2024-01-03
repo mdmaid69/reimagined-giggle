ffc953a68957f6458f897b5725a51410fd2e20a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:00:08 +0000

    face force poison repeat whoever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
