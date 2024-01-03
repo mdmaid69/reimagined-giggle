30142f35f5ccaeac338c8545b9a79c65c18846f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:49:58 +0000

    cruel ahead relation equal along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        return sys.version
