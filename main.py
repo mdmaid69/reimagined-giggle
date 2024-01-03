f3a9c4e381f95d2d63499b0494fe3cf6d9f9be1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:41:22 +0000

    benefit object match deserve scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
