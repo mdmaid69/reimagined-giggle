0c9d60b1bf2e0e3e9daad882011b3f7d3749e460
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:46:54 +0000

    issue where gradually rise freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
