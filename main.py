07f417ce99be907f94c7e5086b1bd923f2a421d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:18:18 +0000

    bringback emotion year thick Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
