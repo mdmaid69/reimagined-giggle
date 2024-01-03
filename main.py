d444943d9570a50f75b7eaa3dfac61de59a60f52
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:34:05 +0000

    dress indeed that gate interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
