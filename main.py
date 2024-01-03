6a5d03bf91597b2efeaa5babdf6e1e0470311754
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:33:24 +0000

    previous decide do answer same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_force(mass, acceleration):
+        return mass * acceleration
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
