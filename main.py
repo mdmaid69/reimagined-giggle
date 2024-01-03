617dcc99df2fac2ab480ffd6ada2dd6281446dae
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:25 +0000

    like lock meanwhile poison should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
