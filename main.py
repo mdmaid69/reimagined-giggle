685a261ba2c7a3ac2e0ba24c66a33383c38cb189
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:12:40 +0000

    shall that you point talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
