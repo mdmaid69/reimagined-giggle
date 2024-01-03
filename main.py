8f2011815977c54d6098ad449773f9dbf39d2d86
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:49:45 +0000

    region intelligent represent thing but

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_hyperbolic_arc_cosine(x):
+def calculate_inverse_hyperbolic_cosine(x):
         return math.acosh(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
