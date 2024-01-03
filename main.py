c808b9e31a2750d0f320d9369e08c99609e46c3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:44:05 +0000

    sight stretch community quiet turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
