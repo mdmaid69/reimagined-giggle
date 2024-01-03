fb1536c33cd71e5a88c83583d6ad60c80cae8790
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:27:44 +0000

    sweep concerning piece lose copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
