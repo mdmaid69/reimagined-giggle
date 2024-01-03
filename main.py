d7ba00e0944482ed4ecd720530d27281840cbed9
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:09:58 +0000

    detail maintain properly apart comeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
