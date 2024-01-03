3c0a9e72addf0077dcc1c160b4bc7210249a37f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:39:49 +0000

    then dance approval spin quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
