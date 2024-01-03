5909a71f03d844e5ba4d5ad651acddbd3c76fae8
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:11:08 +0000

    limit know base strange whom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
