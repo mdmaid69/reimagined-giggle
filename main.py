61e044cc00aca94e9b0cc0ce00ab8e08e7508dbc
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:41:54 +0000

    fall thank excuse size brave

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-print([x**2 for x in range(10)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
