1b024a3b9593f409dc6f2a5242c0a9d9dd808eab
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:39:31 +0000

    themselves disturb belief beat right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
