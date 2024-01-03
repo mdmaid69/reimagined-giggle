5352d0739540207abad6269a9cfff243595fd8a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:37:54 +0000

    possible come fix disappointed increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
