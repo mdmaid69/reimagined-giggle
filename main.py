714e68fae4ddeb6bc7be02e1531e987a6a21c183
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:09:46 +0000

    save noisy really little cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_as_set(array):
-        return set(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
