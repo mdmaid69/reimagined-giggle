5044dec960f171c2da3f128a8e8d2cf8e189dd10
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:15:09 +0000

    press correction nice service early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
