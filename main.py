4675b1bd789a2a8048e895dedf6d4c0823497473
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:11:22 +0000

    cost anybody callout fact claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
