e36bb8979c7b5af7097ac1d687388aba48fc003b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:18:52 +0000

    refuse Kk move freeze only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
