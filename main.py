e2012c4f4804711c8ecd840d91150e31a639c171
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:14:22 +0000

    walk attract activity copy equal

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def remove_duplicates(lst):
+        return list(set(lst))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
