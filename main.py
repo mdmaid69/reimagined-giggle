f7fb1b0a417e7fb0b82d6bf6dd99a19565c785a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:22:54 +0000

    rule air last violence fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
