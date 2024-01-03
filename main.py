1c1ef6b8afa980161cbb41b760947543d61858d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:00:56 +0000

    cut use he strong dark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def convert_to_binary(n):
+        return bin(n)
