ecece4b1ce2dac0ee5b8ca49aa7519c6c4c55e9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:33:20 +0000

    none fierce fasten speech gentle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Square numbers:", [x**2 for x in range(n)])
