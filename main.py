0f4d99a5046400e92b6cd55eb74d1d1996ff2799
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:52:07 +0000

    minimum fond change satisfactory fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
