36f06cc47bd872a38e06c6a3968afc364e5d2012
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:07:10 +0000

    practice fault class bill winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import collections
+def create_user_list():
+        return collections.UserList()
