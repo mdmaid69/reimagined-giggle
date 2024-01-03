37ce049498dcd6008ce601a3ac46f3a7fcf54aee
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:55:35 +0000

    look string follow everywhere burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
