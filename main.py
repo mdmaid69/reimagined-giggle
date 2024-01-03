b838c6c56059584be5e68c204fe2700a6d8f399f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:13:14 +0000

    take poor quality post mood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def square_number(x):
+        return x**2
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
