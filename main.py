8823989d4b3acb0a530f432b467b800469a19895
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:42:33 +0000

    professional evening grade war only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_power(work, time):
+        return work / time
