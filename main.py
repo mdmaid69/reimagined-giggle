97403c29bf63d73e46cbb6ac38d141bb32d2c5b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:25:42 +0000

    nerve community wrap few miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
