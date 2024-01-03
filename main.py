88d30911ae307308ae61c86a8ae2fe6939e06d8c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:41:03 +0000

    nowhere left pair kill straight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-print(re.match("h.*o", "hello world"))
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
