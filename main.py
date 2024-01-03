ea441ae3f57267bdc0fb34f3eec813c55cff9962
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:21 +0000

    who can book Qq service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
