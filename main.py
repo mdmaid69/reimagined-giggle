57e2153e1509c27980ac4f1b2e4765e2cf9702fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:15:56 +0000

    occasional workout guess unlikely failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
