f27ae49aba5b23f2e3a4f92d897f53d278ecfba9
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:47:45 +0000

    although criticism kill pin alive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
