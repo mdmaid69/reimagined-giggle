c92591ad8392c9dd0157474459d0de924507e55f
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:37:54 +0000

    threaten describe insist consult breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
