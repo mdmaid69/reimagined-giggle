49fc8d3d9ecbabab0adabd81dfe66270eed809e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:28:06 +0000

    drawing behind repair rate community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def add_numbers(x, y):
+        return x + y
