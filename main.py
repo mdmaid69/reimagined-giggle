f62b21f681f29e3fc4b518ede7423f9cf2d87b4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:26:13 +0000

    wind long many mine possibility

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_even(n):
-        return n % 2 == 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_circle(r):
+        return 3.14 * r**2
