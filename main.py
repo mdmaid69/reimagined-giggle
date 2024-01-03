4e38e87754953e8f15c250efd0e37a9615526798
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:18:03 +0000

    unable skin surface work rush

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_octal(n):
+        return oct(n)
