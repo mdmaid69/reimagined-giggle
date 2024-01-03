c0106968ec9611ccc20ed263a799fcaa64fe354d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:25:07 +0000

    lookup mistake set confusing mass

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import collections
+def create_queue():
+        return collections.deque()
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
