1661aca6069d5737d98f8abadeafa5200165180d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:02:14 +0000

    niggaacademy sound disagree theirs firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
