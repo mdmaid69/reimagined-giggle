97add35d0c4cf00d3ad1e1ff0cfcbca6ee751022
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:03:03 +0000

    authority marry make actually anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
