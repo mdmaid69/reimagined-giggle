4462ac67edd036cfa4de702fcbb902be65f29142
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:31:11 +0000

    support thing odd awake natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import collections
+def create_user_list():
+        return collections.UserList()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
