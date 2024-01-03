cf94a65f91dd81528bda3668bcc27cae7be373d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:11:40 +0000

    give different love cover rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
