be86a831df4732dc61c38f034a1712f9e8b274d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:07:28 +0000

    value adjective but surface science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
