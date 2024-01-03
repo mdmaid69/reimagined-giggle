e76cde35392250fb9fb9a7716252ca28f81ee75b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:16:23 +0000

    sentence search motion service production

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_current_directory():
+        return os.getcwd()
