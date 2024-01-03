58f392055c714d431f4936a242757b0e50b0ab28
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:13:44 +0000

    onto fortunately write whoever slice

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
