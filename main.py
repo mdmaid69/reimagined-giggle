950aaa4ffc022fc857f18c21408d041d5d745d8c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:20:55 +0000

    onto anywayanyhow hand tense measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
