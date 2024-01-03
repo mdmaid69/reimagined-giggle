85bf57d4e35120ab2af744eb19252ae8b948e77f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:53:55 +0000

    skill bringup our train convince

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def exit_program():
+        sys.exit()
