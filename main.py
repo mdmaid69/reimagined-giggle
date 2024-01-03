719927565793b66144ef675d6914593984d46d7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:56:56 +0000

    bye meal concentrate smell to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+def remove_directory(path):
+        os.rmdir(path)
