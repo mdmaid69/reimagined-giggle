1df854034bb6cb3d4d30a620cea656e53bd02911
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:38:14 +0000

    drop wind comeoff behind absolutely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
