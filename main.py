93389df09c62cb6cbec6df36168871461b0ae4c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:47:15 +0000

    funny right murder depend tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
