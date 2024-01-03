a950bef685f9c6465205a50210a22384aa00087e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:44:33 +0000

    shade once blue music ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def find_max(lst):
+        return max(lst)
