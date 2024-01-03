0ee162e98cd8068df0553f9b9f9f0f86505154f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:25:30 +0000

    release Zz minority tight crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
