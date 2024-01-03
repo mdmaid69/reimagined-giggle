7070aa604170c8b95ddaad544733033b3b062e8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:05:59 +0000

    proper here hit cruel scheme

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
