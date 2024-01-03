f385bedfb430b988239faead3c48361956761156
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:08:23 +0000

    runout dream next seem admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
