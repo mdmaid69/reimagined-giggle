ab75a0f880b981be76164f3fadf7c1de07550781
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:09:47 +0000

    cope giveout excuse approval mention

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
