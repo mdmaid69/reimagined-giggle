bd199e7b15af9dc7880146f4a0673c55880600c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:54:56 +0000

    basis skin successful uponon contents

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
