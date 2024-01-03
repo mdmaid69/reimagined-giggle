ef1ccedd067b0aeb9cfc80127a03271a86cee08b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:17:16 +0000

    account forward sea above already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
