d92609bb3e65aab4b2540dbbbd5e531b42c96742
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:41:02 +0000

    we run you drawing underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
