0c706a4460d1ada026b681b6be00da321736b593
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:14 +0000

    lead quiet near breakinto congratulate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
