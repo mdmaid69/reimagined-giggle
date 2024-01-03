2be4212d5f8124b6d98f7c2f2724a502da52343a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:29:43 +0000

    mad circumstance seat lookafter window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
