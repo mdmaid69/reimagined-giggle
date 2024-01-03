8aa430a67de80a02a3e58024dfdbaa6273c75a51
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:03:40 +0000

    remark motion expensive themselves earn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import time
-def get_current_time():
-        return time.ctime()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
