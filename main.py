e2ea4d66a642fe8416e0222caba2a5f09e64d85e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:11:19 +0000

    correct breakinto breakfast supply grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
