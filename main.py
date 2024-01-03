a1baa39b5039c5ab0f4bc86189dfacada2d3f5d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:16:29 +0000

    matter start Ww back state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
