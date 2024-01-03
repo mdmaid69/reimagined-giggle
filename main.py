997f82685648c2af0248e6bf2ebe13de230a04f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:36:32 +0000

    giveaway disappear apologizeapologise ground uncertain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
