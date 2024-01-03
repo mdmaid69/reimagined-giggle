06ebc72f4d577bf8e7de14e8295fff892b37603f
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:50:40 +0000

    brush smooth rush conversation free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def convert_to_binary(n):
-        return bin(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
