d1fe4cf206e77950a0c4bf2110a0c4ae039e7774
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:34:07 +0000

    grateful turnout loss swimming runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
