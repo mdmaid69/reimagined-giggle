b5dacc7fdbad793790152d821ca9d4d327254687
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:26:43 +0000

    free actual big expression cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
