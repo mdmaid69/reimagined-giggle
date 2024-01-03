1d76cef816e14b30560566f2e5892222ae13ec80
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:19:29 +0000

    seriously deserve unit usually state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
