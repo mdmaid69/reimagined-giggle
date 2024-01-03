6416c4484100e514e816b5c6f336fdb9f7e0b4c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:07:24 +0000

    measure burn sink boil cruelty

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
