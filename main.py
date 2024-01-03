bfc76c5b78c6a2498841326902d297b1d538cc3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:51:26 +0000

    description purpose climb display wander

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
