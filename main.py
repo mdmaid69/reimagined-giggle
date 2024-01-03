c11caf0434ca3a68f127949fe0cfe911db2489ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:11 +0000

    for strong dry most dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
