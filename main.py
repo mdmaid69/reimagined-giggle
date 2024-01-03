9c65f1348bf43cb2b43a50c827f5ee6d233fe268
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:29 +0000

    once light play get introduction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pressure(force, area):
-        return force / area
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
