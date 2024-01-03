2ff3899b0f372065a036ff5d1d2de8f247d6b11f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:49 +0000

    split constantly study danger exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
