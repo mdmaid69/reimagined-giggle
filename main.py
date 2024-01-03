b6765f7f6a175e83f107f455b9ed7da6b2fad03f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:03 +0000

    some straight between getout duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+name = "Python"
+print("Hello,", name)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
