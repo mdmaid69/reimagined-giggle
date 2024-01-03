5351760874e755119211c5f82ad149762a934ba3
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:33:01 +0000

    file mind his variety fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
