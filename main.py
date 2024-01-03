217f7caa103e7031c2896f6c9fc72c1dbccb413d
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:25:45 +0000

    target rest increase travel shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
