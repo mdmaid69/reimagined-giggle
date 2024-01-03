4df80ed9e67da8b728c618b24c98d4c1d93b1954
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:18:03 +0000

    position sleep quickly challenge like

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_power(work, time):
+        return work / time
+import tensorflow as tf
+print(tf.__version__)
