dc3c5fd1e4d7a8ba1f65c4fff1c8dc459c56f1fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:13:49 +0000

    impression effect about talkinto so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
