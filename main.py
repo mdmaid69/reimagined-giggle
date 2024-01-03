d182290123f51f55858f0a63072a8e6c0aba0018
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:43:39 +0000

    century change tap favourite entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from collections import Counter
+print(Counter("hello world"))
