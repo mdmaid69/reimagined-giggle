7c8298dd5babdf35ea5bc7d3e8323ca4ed0145bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:04 +0000

    load avoid best close long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-name = "Python"
-print("Hello,", name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
