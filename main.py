4372dd0902f793441e48e4a1aa1549debcbf59fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:17 +0000

    glass proof negative arm children

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def count_characters(sentence):
+        return len(sentence)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
