8cdc232bb359c2fe3a9f5b9924695087b4c68e51
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:23:33 +0000

    pleasant life sorry relative practical

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
