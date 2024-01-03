2938934fe8cbd2103ae5695fdfa597f4c05ae26f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:31:53 +0000

    food mark patience scrape underneath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-for i in range(5):
-        print(i)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def add_numbers(x, y):
+        return x + y
