e6370d5d1217b1712cb42f63da513fd21c3713a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:58:25 +0000

    both like make permission level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import collections
+def create_user_list():
+        return collections.UserList()
