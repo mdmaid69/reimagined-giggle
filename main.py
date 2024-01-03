2d0b207f2220b761afa00f0ce3954bcf78052484
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:56 +0000

    play birthday person certain fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
