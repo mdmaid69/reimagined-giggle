94309a250b6ee9867fa49f813419d80f46f836a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:55:26 +0000

    achievement shadow design nose pretty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_density(mass, volume):
-        return mass / volume
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def reverse_array(array):
+        array.reverse()
