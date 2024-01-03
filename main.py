f9e0304066ed2a85fb7f46c8ad5785cab32b70da
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:22:40 +0000

    dress cook change contribute row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
