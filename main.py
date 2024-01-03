8300c32b3c33e595f3de94ac4dd91f0f27c2e052
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:05:06 +0000

    squeeze sink breakdown miss careful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_max(lst):
+        return max(lst)
+import collections
+def create_user_list():
+        return collections.UserList()
