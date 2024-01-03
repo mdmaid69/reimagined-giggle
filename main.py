dd7faa7dcdad825c3f82a22cbffabab0539b5ba0
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:39:54 +0000

    blame push break among pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
