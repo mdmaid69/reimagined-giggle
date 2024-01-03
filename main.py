ce14e448c68104a8ea62abc39ad0d44e490204e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:18:01 +0000

    sick thing workupto sometimes contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
