e3ddd14ce7b0892e4999e7ca230d0a5670f4cf44
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:31:47 +0000

    invitation passenger film live maintain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def clear_array(array):
+        array *= 0
