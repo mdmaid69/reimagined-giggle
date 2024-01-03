1c215453d0d72da6266fee750da31d59f7635120
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:13:37 +0000

    trip bury half interview language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def count_elements(lst):
+        return len(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
