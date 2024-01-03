0ef1639beb59defeb9285a42f7e7a66081e540de
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:18:07 +0000

    nervous nonsense rest cook getup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def split_path(path):
+        return os.path.split(path)
