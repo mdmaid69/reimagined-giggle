f3a546f23645b7adafbb4d894955bbf6ae1aafbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:59:03 +0000

    spend confuse television underneath obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
