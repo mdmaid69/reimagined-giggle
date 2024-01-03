3375d50a02ee9377e6aa64f898bb1bdd48f14cc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:25:52 +0000

    wide yet letter custom die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_current_working_directory():
+        return os.getcwd()
