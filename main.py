3603dc221fdcad12415125747eba066f11ffeef7
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:46:11 +0000

    match fierce round successful who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
