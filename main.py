003d1ab8ab4b96b9f5ecef798646684fa7d48e78
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:43:36 +0000

    pronounce figure prize match fortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
