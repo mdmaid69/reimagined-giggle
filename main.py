3a9159e66265906b4ef1fce71ca275b75d261c62
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:12:13 +0000

    cause stick off dig twist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
