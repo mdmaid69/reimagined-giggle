73169b1a9adbeddfbcd12186bb1e638afea2ff90
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:08:21 +0000

    mood culture mood beside tune

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print("Hello, world!")
