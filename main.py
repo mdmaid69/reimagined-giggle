0bf3dfa6571be288a75821bdb2f1f7f74dbdd94d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:55:49 +0000

    long come interview hang scheme

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
