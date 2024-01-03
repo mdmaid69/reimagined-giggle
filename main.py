648c36734805f8f2c1d0aa749392f45a7bb81b13
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:51:27 +0000

    invitation mystery foreign scrape track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_current_directory():
+        return os.getcwd()
+def convert_to_binary(n):
+        return bin(n)
