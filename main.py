799d2143a67c1b95a82dc3e4d35d92308ce1833a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:23:18 +0000

    rise minute mad better break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
