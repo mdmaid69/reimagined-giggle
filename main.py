d51851fcf8bf2f6ce9dfc95c79921c8387c955fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:44:36 +0000

    impressed car hand Ww chance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_max(lst):
-        return max(lst)
+def calculate_work(force, distance):
+        return force * distance
+def reverse_string(s):
+        return s[::-1]
