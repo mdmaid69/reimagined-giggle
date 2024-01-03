aff86fd46a69e5edfd5dc8a9b63d94a54ab6e445
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:24:39 +0000

    fun back cold hope tour

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
