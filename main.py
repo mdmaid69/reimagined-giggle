22fb5984e5608ac65f916f0187c934225ca95320
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:13:45 +0000

    bar nearly which fine 569enjoy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
