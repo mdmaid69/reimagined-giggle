0a92f1d24175b18e339d91f95cc2b66a8d9eef3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:16:56 +0000

    buy under Itself deserve crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
