f2ab3a30d2bba14d34c2176d23af2d3ce302d5af
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:41:04 +0000

    buy keepoff thing win pronunciation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
