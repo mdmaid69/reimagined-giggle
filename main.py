ed9b9f0ae16b49f7aed3de820caa12b6e46cf54d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:25:26 +0000

    there exit relationship remove nigga

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
