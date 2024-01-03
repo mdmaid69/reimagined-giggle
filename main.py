b7f800b054441099e211e01071c84dfcecf38a35
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:42:50 +0000

    aim large everything mistake cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
