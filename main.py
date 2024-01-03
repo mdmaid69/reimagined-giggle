09fed9d49f07a1962bad4580caac5d0fbf1c307f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:42:06 +0000

    create curious top round deliver

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
