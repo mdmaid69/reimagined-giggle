c468089e97b0a7b34d5db4b93bc658d172cd9ee3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:53:51 +0000

    song separate direct city thumb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def count_characters(sentence):
+        return len(sentence)
