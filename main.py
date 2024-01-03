d2261686db8400844f62ff24cd0da59059c808e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:20:04 +0000

    on cap inform match language

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-print(re.match("h.*o", "hello world"))
+def sort_list(lst):
+        return sorted(lst)
+def convert_to_hex(n):
+        return hex(n)
