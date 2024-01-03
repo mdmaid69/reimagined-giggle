74bb955df20b59a7193f26ae5faeec8f29c03dff
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:16:06 +0000

    situation force young warn step

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-import collections
-def create_user_list():
-        return collections.UserList()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def reverse_list(lst):
+        return lst[::-1]
