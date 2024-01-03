15b42ab422c7c2991c6a5033814a5f2469fdd750
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:20:27 +0000

    responsibility wander properly basically zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_min(numbers):
-        return min(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
