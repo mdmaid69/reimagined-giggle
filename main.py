9112d4b5f509da97b9ec5c76fdd3466d147472c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:41:15 +0000

    first area pride what work

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
