9536febdeb815a8169c96549ce3ed08810eee717
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:30:43 +0000

    discover odd suffer unfortunate smoke

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+print(datetime.datetime.now())
