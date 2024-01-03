90b2ab1c879cedd80f378402eca941dcedaf98d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:54:39 +0000

    pleasure priority stupid kind Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_time_since_epoch():
+        return time.time()
