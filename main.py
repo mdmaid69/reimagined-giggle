783456aa8d90c929c262b0206edd97b4c79101ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:31:15 +0000

    crash dream its offend pity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
