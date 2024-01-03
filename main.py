1ed63ab0b0bca7418ca05f5afd7a54f262701a27
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:55:01 +0000

    news polite forgive cry welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
