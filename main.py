c0825e8c2a5afbb245f80790ef6600461da0a3a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:18:40 +0000

    thirsty direct holdon undo risk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_bool(array):
+        return bool(array)
