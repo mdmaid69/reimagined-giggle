d7a6aae2bafe49a54148ee883c4b5f63647400e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:47:25 +0000

    to fix hollow study thanks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def remove_from_array(array, item):
+        array.remove(item)
