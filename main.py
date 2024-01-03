49a9d9696b0d2a17f6dce67dc8c2f7621037c98d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:05:32 +0000

    point Itself cruelty calloff education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_string():
+        return collections.UserString()
