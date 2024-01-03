4c96248fd4d6308eaad1771ae00c21d9adeb7195
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:33:07 +0000

    bother wait urgent hurry style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
