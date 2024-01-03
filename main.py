589467ced2f0b816bb2efb0271fddf31edd5d1bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:52:43 +0000

    Ll bottom afford learn wish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_speed(distance, time):
+        return distance / time
