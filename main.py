b4c54ac13a8bcbad8dbf7e19ccf0a21bedca25e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:46 +0000

    authority land comeoff earn kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import collections
-def create_user_string():
-        return collections.UserString()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
