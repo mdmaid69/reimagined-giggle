51014231bc855721b7454d53aefc217339948ce1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:39:05 +0000

    nice special should strange world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
