91460f431f016aa58bac0c2c6c8f9f7270008bf8
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:15:18 +0000

    medium game stick awake will

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def convert_to_binary(n):
+        return bin(n)
