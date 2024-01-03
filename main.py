11fae8c6ea6ed1cb8bc6df107c2df2297f38004c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:05:09 +0000

    then bound dry welcome possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_index(array, item):
+        return array.index(item)
