61cc6714ad545c1ef0b68231087bf6c394a9aeb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:32:23 +0000

    exam would now play cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
