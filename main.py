806789befbed3e06955fafe1d631723548a776a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:07:12 +0000

    niggaacademy best meanwhile pump lock

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_set(array):
-        return set(array)
+import collections
+def create_stack():
+        return collections.deque()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
