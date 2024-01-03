9468ba3e99d0410c7ff1e395e85a7ce0f96ea7f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:37:30 +0000

    record combination deep exactly outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
