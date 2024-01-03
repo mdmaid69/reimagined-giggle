fe431e6fcc3e999731dc7ed2b7acf6697cc2ed2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:04 +0000

    half couple estimate friend chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
