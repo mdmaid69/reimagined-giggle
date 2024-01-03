a931cfa15bb134169180adf66f5b63b2c3dc8f62
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:46:25 +0000

    lower ordinary gradually represent achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
