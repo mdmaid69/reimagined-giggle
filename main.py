aabe86dbb64be5299d836ccecb1cc4a37d66ba79
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:30:05 +0000

    charge sell promise gentle content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
