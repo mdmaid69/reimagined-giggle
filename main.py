98da66a248739acea9b2e8c27b6c00ab226f0199
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:21:25 +0000

    Rr unit runinto pride shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
