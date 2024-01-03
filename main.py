8ea5d8d3508fab70593ada24fc3fe929a94704aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:14:27 +0000

    worth whether earn clever justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
