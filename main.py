e0fead1c48f918c296f027b8cedf20c4dcace4bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:01 +0000

    reduce fairly bet address mend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
