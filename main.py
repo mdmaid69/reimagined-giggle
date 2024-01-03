5237adc96fe9aeffb503b0187c977daa622d4813
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:09:39 +0000

    pay border emotion simple plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
