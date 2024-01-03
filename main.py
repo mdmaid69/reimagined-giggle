5926bcebcd412b940e4e7610dfb3c94632f4f654
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:05:51 +0000

    straight moral rid me goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
