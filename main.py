1085f310f08b69c0cf079e7687f2a6f3eb510a36
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:09:47 +0000

    picture sensible mind offer healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def add_numbers(a, b):
-        return a + b
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
