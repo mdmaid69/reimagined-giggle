1372f20f13fd7858c89daf3f014334decffae8ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:32:30 +0000

    tradition damp various behaviour piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
