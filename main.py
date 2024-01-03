4b3a284c41d9286a041a53b7efac1f9a29c9289c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:45:39 +0000

    yours any cause plain few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
