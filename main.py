646c6e9d2028558346cb25ce2c4b953703b17443
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:47:43 +0000

    drive friend so visit sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+print(math.pi)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
