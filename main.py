2e153353ea8c4a926c0c4a1de275d8cce9c1ca2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:39:53 +0000

    band Itself add avoid belong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
