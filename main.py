37e397b63b02691deb08c3db00d47fb419c3f64b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:15 +0000

    address do news average slight

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
