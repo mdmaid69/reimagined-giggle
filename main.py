e47b4dc4c11d4f1c52b6c2fdc9139c2e7f7abf78
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:17:50 +0000

    marry statement while object butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
