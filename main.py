3818668eb3bb84554211e8d9847a6215a5a9ee27
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:16:00 +0000

    clothes scare Ee hit fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
