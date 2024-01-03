5b8d33f3da7e97e9284ea9d75c68f6b4c810a200
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:47:40 +0000

    worse suppose car purpose turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
