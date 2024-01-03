81f346e54850f5d6714f35ff41165254c42469b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:21:29 +0000

    your start improvement section minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
