dff21ae587947318a4de32a650122d0d732e3e53
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:32 +0000

    reduction undo she comeout direction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def count_characters(sentence):
-        return len(sentence)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
