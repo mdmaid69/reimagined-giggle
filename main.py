abdce9ce050ed76b0c62a5b8182a348171a822e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:46:44 +0000

    pronounce method slight drop bag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
