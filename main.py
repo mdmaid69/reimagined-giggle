ad549fa3246e00200f9cabd8a1d478f5ee089d23
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:39:29 +0000

    choice injury destroy hang front

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import re
+print(re.match("h.*o", "hello world"))
