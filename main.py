ebeeec9465ee41effb492900b14bfbcfc8bf2439
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:06:15 +0000

    blame nose exploration goaway telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
