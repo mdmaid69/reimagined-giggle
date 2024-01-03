d987bc2a5d64ad648ae8dfcf0101d08dd24259ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:33 +0000

    benefit division main unable quantity

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
