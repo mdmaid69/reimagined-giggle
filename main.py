a524b755a567318678122b1eade978447e747c23
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:08:44 +0000

    wake violence mine start mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
