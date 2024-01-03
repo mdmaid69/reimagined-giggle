84e01fdf167470b29cff65d7ff3fc1ccf088e84c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:27:01 +0000

    appeal club hospital clear side

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def multiply_numbers(x, y):
+        return x * y
