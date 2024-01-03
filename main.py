56055ae3a1a7994d04c20c6e6b11f6d6b3e46714
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:03:42 +0000

    sing time throughout you circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
