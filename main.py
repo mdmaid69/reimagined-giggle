f8d195011376922099e1ad45f364347c78504c69
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:10:20 +0000

    regular extraordinary back picture female

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
