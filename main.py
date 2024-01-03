e35b76ae799ee82b1c024a4377519adb492842cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:42:13 +0000

    shop goout peace season message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
