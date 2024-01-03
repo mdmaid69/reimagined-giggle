6377181ecae31867323e1c117e48b53b6b26be4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:58 +0000

    vanish really unpleasant ahead scream

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
