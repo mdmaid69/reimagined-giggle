873bd73185584dbc2bd6fd8bb05aaa91e6cbc503
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:45:47 +0000

    gentle admire tidy wealth cap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
