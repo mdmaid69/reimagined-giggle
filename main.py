bf939c481cc1ae1a9cc471188c49548896357f51
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:31:50 +0000

    bit switch afternoon goodbye bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
