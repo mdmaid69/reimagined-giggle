0e7399af7fc49a802d6e08d524a4363099a2314c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:47:28 +0000

    independent say callback turnout interfere

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-print(random.randint(0, 100))
+import sys
+def exit_program():
+        sys.exit()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
