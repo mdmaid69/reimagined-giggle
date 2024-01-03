dc54213bd7fd2e421d455dfc96f6f40bca0eaa37
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:24:45 +0000

    well pleasure lie city free

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
