e3e426ed550b2e3757d4f60f95112be55e030a0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:35:37 +0000

    we independent minute rub warn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def convert_to_hex(n):
-        return hex(n)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
