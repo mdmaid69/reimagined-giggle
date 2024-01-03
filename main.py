ded94c3529c766ace9c60ffb8e99078bc7882561
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:47 +0000

    minority gradually expression tomorrow shrink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
