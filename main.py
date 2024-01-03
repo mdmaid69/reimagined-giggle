9628bbe6b2fc225f6faabef116e30d47b416b360
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:27:54 +0000

    Itself goout each plant downstairs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def square_number(x):
+        return x**2
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
