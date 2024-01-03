4d4874a8d7ad4d7438312dbe9da524a2da21a6d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:29:44 +0000

    divide everyone sudden busy correct

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-def find_min(lst):
-        return min(lst)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
