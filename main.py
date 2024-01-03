7217b39fa0c91393f5e0255338e84f666e920da2
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:38:40 +0000

    begin see pay loose new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
 import array
 def convert_array_to_string(array):
         return array.tostring()
+  import os
+  def get_current_directory():
+        return os.getcwd()
