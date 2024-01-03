364c550706473ae02c1ab595445147c900426ead
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:01:18 +0000

    side we turnup position patience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def convert_to_binary(n):
+        return bin(n)
