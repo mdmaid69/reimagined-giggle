ec7ab92ef00333fad23e14f5ea28cc32c306c44e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:15:55 +0000

    someone will choose like lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
