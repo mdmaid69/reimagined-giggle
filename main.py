dc5072d1e0f317e4720161408c53603f8a9d5385
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:34:43 +0000

    step tall brother false combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def add_numbers(a, b):
-        return a + b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
