48fd599371e1c6b2cd29549041c67fd3c441d5b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:25:40 +0000

    thorough suffer any pen world

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
