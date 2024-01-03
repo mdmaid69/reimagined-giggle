3ec376503dd7443f81ce08426008b8b049133e7b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:03:41 +0000

    lonely pause rise edge uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
