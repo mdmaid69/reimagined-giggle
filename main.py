40195c93c1594bd1bfdef875e2aa769809770b55
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:41:26 +0000

    limit steal watch surprised Pp

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(10): print(i)
