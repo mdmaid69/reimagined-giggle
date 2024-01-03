c03355bae9581e1968dfc2478cb119500fffe429
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:03:11 +0000

    notice everything concentration spare freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
