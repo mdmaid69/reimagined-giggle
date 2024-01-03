439f79af59409490d56573f419929f16e9113fec
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:18:07 +0000

    keepoff argue arm quantity fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
