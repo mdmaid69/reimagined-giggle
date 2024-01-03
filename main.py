76f77fe38b2c5fae5c8f57091636fe2cad9870c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:36:06 +0000

    arm recent bang sleep unless

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
