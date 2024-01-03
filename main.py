2ac32bf2bea12eacccb8dd02d24c6d391e917450
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:30:30 +0000

    defend amount disturb compete callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def cube_number(x):
-        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
