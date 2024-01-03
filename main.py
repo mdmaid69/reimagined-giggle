4073ef49e1dac8fc78d8a65d038851eaf900cbbe
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:25:01 +0000

    situation move now lookup wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
