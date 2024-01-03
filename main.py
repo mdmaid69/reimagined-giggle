c4c5afb3cc9f5dc442fc5ab040c3a9a5fa58dd6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:25:28 +0000

    finish appeal about direct minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
   def get_current_working_directory():
         return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
