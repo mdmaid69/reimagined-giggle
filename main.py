86a389567f1a7c39358af70750a24cb110036ca1
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:31:32 +0000

    door lucky bet rule arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
