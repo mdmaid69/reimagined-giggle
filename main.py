11895c12f8ad540ea39e3104955dfda2324888be
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:02:24 +0000

    case tidy modern before future

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
