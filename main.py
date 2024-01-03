242d9b5e38f798e4987b77db54fcfa5405e8cc0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:21:50 +0000

    broken wide convincing me Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
