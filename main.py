e633ba93913724e96c0428cacf5156133b79335f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:50:02 +0000

    interesting dry interview bright so

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
