0543ca2e3c1abda9992395c53b38d45e4bd45c6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:47:52 +0000

    likely hold correction declare most

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
