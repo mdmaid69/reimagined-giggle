0533348da99e8844bae88b99b6db3667c38e4b45
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:13:21 +0000

    recent contents wander wealth interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
