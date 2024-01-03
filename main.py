d922296d391d3dab9e93b7024ee21130038c21db
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:18:54 +0000

    voice direct distance shopping pure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+for i in range(10): print(i)
