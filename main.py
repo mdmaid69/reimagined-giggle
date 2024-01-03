d31dce2bba3c68c05790262639a171164dad845b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:31:25 +0000

    Jj stop temper wear good

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import array
-def get_array_item(array, i):
-        return array[i]
-for i in range(10): print(i)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
