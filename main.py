e031858abbd46502325f2d21de70000cdbd560bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:38:42 +0000

    feel sister demonstrate year familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
