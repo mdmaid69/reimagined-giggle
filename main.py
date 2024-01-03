88c4ab938bbbe2170cf05d45248cee278e1bdf89
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:31:08 +0000

    who ring circumstance wealth do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
