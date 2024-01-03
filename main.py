812a43fe8b3aeea15790db854164fb6cb0ddb5b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:09:17 +0000

    drive could tend tap broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
