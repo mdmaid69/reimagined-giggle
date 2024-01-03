aa882714e9ea76906c8f9ffd9743fa51d0615bb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:19:43 +0000

    hour same front clean workout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import collections
+def create_priority_queue():
+        return collections.deque()
