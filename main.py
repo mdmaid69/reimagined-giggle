4866b94c3fd9e7c626b2132639299d4e14c67b1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:11:15 +0000

    slight slightly need clear bend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
