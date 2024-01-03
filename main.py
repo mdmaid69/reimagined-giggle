7f5c93d2813f21159480dc28d023d89f794ad2f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:05:36 +0000

    rescue place benefit arm fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
