9ab2909196e53a104e14118aee6769f77e78bbba
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:33:30 +0000

    ask feeling possibly discuss snow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def get_array_as_float(array):
+        return float(array[0])
