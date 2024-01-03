8bbc4e6ee269a9b05d1853d748e4e1945b688fc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:59:57 +0000

    influence still evidence move cap

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def convert_to_octal(n):
-        return oct(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
