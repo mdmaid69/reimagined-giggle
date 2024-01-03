5a1b588cf7413e4566b4a41e7584af7a6ccf37d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:20:33 +0000

    delight no cheque unusual brush

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
