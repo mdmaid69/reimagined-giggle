1093a9c5b866f9964cb64d4f70f914a6832b9407
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:20:46 +0000

    Kk ring ticket who decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
