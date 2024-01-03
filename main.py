ab8483d5e7888dd99a7eeadec83e65e73707830a
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:21:13 +0000

    order slip bread stretch tune

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-print(math.pi)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+text = "Hello, world!"
+print("Reversed:", text[::-1])
