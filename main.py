eb756e5b4514e106c0e8c1f9084e15861bddcf4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:22:39 +0000

    flavour 179bite line down convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def reverse_array(array):
+        array.reverse()
+import sys
+def print_python_version():
+        return sys.version
