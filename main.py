c2133cb81c2bd82bb65d3e5f0df47039218de920
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:03:54 +0000

    slightly season battery interfere mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
