081e3994a59dc9e5e22f4fcd384fbf2927091166
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:42:32 +0000

    habit occasion duty the love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
