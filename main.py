8a70f58ad3e40556a3bfc699704344e102a992ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:53:12 +0000

    straight hers name or solve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import collections
+def create_counter():
+        return collections.Counter()
