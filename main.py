194d1e90395edbc28a896dea17c5bd228351739f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:48:13 +0000

    remark clean dare admire I

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
