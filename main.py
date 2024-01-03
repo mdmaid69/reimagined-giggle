739f394cb9e7e3e0ac69f52b747792befd3c570d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:23:38 +0000

    arrival attempt decision far decision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def square_number(x):
-        return x**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
