eaa6308869178a0e82c75b6981b8790e9199b733
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:13:48 +0000

    shop slide club as very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
