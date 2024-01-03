e600990d552af678421d4dd91902fb1040672d05
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:59 +0000

    acceptable worried paper themselves soft

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_power(work, time):
+        return work / time
