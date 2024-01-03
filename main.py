fd8f2ce67752f1b145b4fa32ba4e37388e25a0bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:42:04 +0000

    genuine turnoff same surprising fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sys
+  def get_python_version():
+        return sys.version
