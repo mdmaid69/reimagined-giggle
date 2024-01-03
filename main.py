b807cb3e197e1d4bee3c1ac31704590ddbb8bfcf
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:17:23 +0000

    edge comeround on charge develop

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
