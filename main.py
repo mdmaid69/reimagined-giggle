90b7c73af123ab63c095c6e11dc10c7bf6219d7f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:16:17 +0000

    definite law flat main matter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def subtract_numbers(x, y):
+        return x - y
