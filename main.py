4d57b136fd1036a0581825449cfe44eadf99cf1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:00:02 +0000

    round you show model close

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import array
-def get_array_from_string(string, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
         a.fromstring(string)
         return a
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
