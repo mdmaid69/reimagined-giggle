70c2143ee7ca23b34ee2aa2eacd71bde79dc756e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:48:13 +0000

    recover yes sudden beginning couple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
