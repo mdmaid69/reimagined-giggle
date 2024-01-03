b3ba3dc78a892c47c0fc8fc115610980dbffbd8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:26:53 +0000

    wealth job rule they suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
