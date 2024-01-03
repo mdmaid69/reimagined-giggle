7626012acbf9ac89cb34a1bb883bfd635b2d375b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:00:34 +0000

    distance careful Ll advanced yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
