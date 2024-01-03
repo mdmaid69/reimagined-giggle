e501e6cc990d5759d4462fd791e816526095e64e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:55 +0000

    direction finger weather lock song

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
