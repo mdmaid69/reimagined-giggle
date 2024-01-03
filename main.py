6492fd33430a515f28f08c6bdc1e27c9e9c39e75
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:41:12 +0000

    miss task regularly thin wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
