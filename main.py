507075806e92ae97236d448fae1cd2fbe7dd32b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:57:12 +0000

    fierce main catch steer name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
