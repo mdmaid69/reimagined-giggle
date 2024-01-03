71e4c738157148eeba875ec7cfdd175ea4266bb6
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:01:08 +0000

    once exercise proper promise visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def square_number(x):
+        return x**2
