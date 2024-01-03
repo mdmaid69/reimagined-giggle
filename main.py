5ccecd40b919caae7c62ba6669ad1e54a52544d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:49:17 +0000

    schedule danger born smell major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def check_if_array_contains_item(array, item):
+        return item in array
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
