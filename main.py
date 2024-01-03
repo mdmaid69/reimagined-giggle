5bc093dc026f4bcb6a118d243374a42fcb9943ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:36:51 +0000

    breakinto sleep brother ignore drawing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-def convert_to_binary(n):
-        return bin(n)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
