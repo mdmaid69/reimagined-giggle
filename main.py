f1448161d9c0441b9903dbdb815a0ee40a27f525
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:46:09 +0000

    immediately of ill certainly bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
