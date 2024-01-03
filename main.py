1cb7c57577cb4f14ee34273865e71fd59b6837f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:43:24 +0000

    awake education afternoon uncomfortable so

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
