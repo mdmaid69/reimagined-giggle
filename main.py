73f38a581830198d6596d809709c9b4de087483c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:40:39 +0000

    use callback regard march draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
