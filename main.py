4b6b82d7af9753d0b3c4c8b7782eb55e984dd7f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:54:14 +0000

    vanish deep reply suckme favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
