f73ffa7dd5d30f08b30f24bea8e39a08f5349a3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:40:07 +0000

    skill approval rent behave backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
