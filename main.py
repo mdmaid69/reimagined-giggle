75f657c4bb00418604159539827d9a42cdbbbb3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:43 +0000

    back early train goodbye favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
