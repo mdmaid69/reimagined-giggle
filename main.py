a73e67578904075a0bcae61ad326f7fe9059d3d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:19:57 +0000

    walk sound attitude right fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def cube_number(x):
-        return x**3
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
