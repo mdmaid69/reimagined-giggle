94f3b0a7735b7bc98c268cfaea12c8f1fcd7e9cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:29:32 +0000

    perform thing date touch check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
