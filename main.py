bdb4e22e00d5121d14e7a204317be0db88f2d89c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:05:45 +0000

    charge contain culture post seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
