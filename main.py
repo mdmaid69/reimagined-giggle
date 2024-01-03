099fa81379dc6ea3198463b2563ea1d27c42c449
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:54:21 +0000

    special see force good happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
