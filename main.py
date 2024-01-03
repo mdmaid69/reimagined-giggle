4f129929ed487a7afa7edc9aa830bf53e559ada3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:57:21 +0000

    corner complaint slightly funny regional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
