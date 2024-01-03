37c640274747787a20d8334d57bd1da4ebbf10c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:30:34 +0000

    step close rare production stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import platform
+def get_os_info():
+        return platform.uname()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
