2b233693dd10441fbf324cdbd2214f636a49bcb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:34:13 +0000

    imagination steep fun OKorokay fashion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def sort_list(lst):
+        return sorted(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
