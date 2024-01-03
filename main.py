bcc5870ea761c2165fd7fd89719d003d89efa4fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:42:19 +0000

    fat sight recovery flight bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
