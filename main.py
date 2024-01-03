64fdbb2f282fb235739bbf36c78c06ec0f63df95
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:41:45 +0000

    pleasure statement ring present size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
