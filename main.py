8302c5523eeebba3ca0adf3519c98ca10141cdf4
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:03:54 +0000

    peace unlucky neat genuine careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
