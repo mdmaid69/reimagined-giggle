dc29cc184862fa21caed58213c7bd7ac3a50ed86
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:49:22 +0000

    generally little criticism estimate something

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
