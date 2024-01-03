f8dc801743e94f0c639edc512719bbe5ab5a1af1
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:33:31 +0000

    cope touch treat tend dull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+name = "Python"
+print("Hello,", name)
