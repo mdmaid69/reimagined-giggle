8c64335afe5e40f160392f852f42b240c5b449a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:32:47 +0000

    drawing spin else set list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
