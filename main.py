ca812d921a0e00dc97af5c1e98f805c559bad9d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:34:11 +0000

    address run admire probably cost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
