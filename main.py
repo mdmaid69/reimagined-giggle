87fa1620df2711237c8ddbcadb2403e36a73c17d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:45:56 +0000

    operate ride hope free base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
