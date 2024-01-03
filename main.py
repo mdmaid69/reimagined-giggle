aee7eeb436508dce88b0df502c744a22f466f8a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:41:24 +0000

    principle brother breakfast home throughout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def sort_numbers(numbers):
+        return sorted(numbers)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
