ccc2bc536c5b951c3496a9c6aeb4a6c58d13164f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:53:55 +0000

    square turnback form wash permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
