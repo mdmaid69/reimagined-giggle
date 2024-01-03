692ed7d18d9f91e0448a25556c72877da0b7df10
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:34 +0000

    zero act address such conduct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
