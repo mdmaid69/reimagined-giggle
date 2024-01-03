08a7d155ff15faa5d23ac864218ddc882e16b2d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:00:08 +0000

    bound sit description genuine knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
