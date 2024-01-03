d7398ff973b2d3d96788a6ad19ecf872e03c2538
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:57 +0000

    turnup maximum bunch mail minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
