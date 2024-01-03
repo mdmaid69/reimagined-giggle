7d55f6e634a8b1d5a7bc105152c1cd080b12bf8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:36:18 +0000

    spin spend her supply forgive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_exponential(x):
+        return math.exp(x)
