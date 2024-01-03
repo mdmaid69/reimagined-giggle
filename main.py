69b6bf64e5f33639f6a9927b01a2af4739a45ba6
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:22:42 +0000

    agreement sudden travel standard acceptable

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def subtract_numbers(x, y):
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def subtract_numbers(x, y):
         return x - y
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
