8411aef3304f6b10fafd9fab8f0515ae93a58686
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:42:43 +0000

    miss glad official border trick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
