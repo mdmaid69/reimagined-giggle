09cee27c56409d325f0a8855c1657694c2869c10
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:11:04 +0000

    lose may plenty plenty influence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
