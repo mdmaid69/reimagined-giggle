5bb64df94722a4d9ca4143badc1815f267c44d41
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:46:56 +0000

    style demand picture space dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+text = "Hello, world!"
+print("Characters:", len(text))
