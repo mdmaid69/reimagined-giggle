704a03fd4699bd0751bf180e4f8b5279c1d4d266
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:36:24 +0000

    base hard suddenly mark test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  def reverse_list(lst):
+        return lst[::-1]
