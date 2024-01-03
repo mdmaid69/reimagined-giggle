e8ed967727ade33190caa4ab6d2108bf33ad121b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:15:32 +0000

    bet announce save building actually

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
