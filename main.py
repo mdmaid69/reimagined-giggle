c9cf6f08655146b0a029adb6ce55e02a2b1c1a35
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:28:58 +0000

    chin mistake noisy comment crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_base_name(path):
+        return os.path.basename(path)
