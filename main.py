f40bf2de2838f778bee81073acf450ed8a52a107
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:26:45 +0000

    no square fun make 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
