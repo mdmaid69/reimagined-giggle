0ab540f09b64d20e6f086f85f3d0083bcc662cbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:12:38 +0000

    party tap wave sensible division

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
