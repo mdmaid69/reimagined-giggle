4f82720a28603f191735e3d5a91174732e6c819e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:16:56 +0000

    store report this swimming thing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
