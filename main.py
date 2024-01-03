3cfa3079c50824dc12395f409532de1be96c11e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:08:17 +0000

    produce sorry forgive clearly left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
