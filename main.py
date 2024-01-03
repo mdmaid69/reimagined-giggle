b94cb538ec0a1f4afd1d418a7ee4e7a3f5361322
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:12:17 +0000

    grateful check so afraid reserve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(10): print(i)
