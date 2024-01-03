8f29abdd137c237c2c15955c64eca4ccce5af161
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:44:30 +0000

    inside perhaps old slice lock

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
