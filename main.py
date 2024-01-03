9442381a976396889e01c0d2e210a7fc190625a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:36:40 +0000

    gift fetch sharp train warning

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(5):
-        print(i)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print("Hello, world!")
