ca146910f9365c7e1dfc19126eb3110c5c8023a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:25:12 +0000

    disturb grateful determined priority father

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
