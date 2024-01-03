780d82dcab6e4182e8c1d111055b6d85782e9744
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:42:48 +0000

    criticism defend wander mean welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_complex(array):
+        return complex(array[0])
