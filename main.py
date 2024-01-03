9aa273f093a0d3fed3d00d5589d294e721bc4702
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:53:28 +0000

    emotion identify now seem tomorrow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def get_list_from_array(array):
+        return array.tolist()
