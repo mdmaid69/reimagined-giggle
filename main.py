43fe8c39ad7ee88fa5deb62f4bfc58555d826d74
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:32:11 +0000

    people fresh shout active up

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def convert_to_octal(n):
-        return oct(n)
+  def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
