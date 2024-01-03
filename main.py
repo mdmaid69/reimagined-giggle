6923a909f72bb4d5107320f1f29ff6d9aacf2b9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:41:20 +0000

    love modern bright against certainly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
