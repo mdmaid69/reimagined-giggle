604fb639ffbeb9a8801c3ba96aa4f91cd71328b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:01:54 +0000

    declaration close him maximum round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
