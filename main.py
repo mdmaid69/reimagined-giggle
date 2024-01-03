7fcad81fe95457d39fa767a0e3518822f712262f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:29:39 +0000

    invite television protest careful deliberately

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
