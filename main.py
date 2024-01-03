94b0104d738085b7e1c81f44b57ca50ed1d36275
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:26:06 +0000

    spin off damage also put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
