4b4fac5f4b36219df78d396c402e2c0ca903b6b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:04:58 +0000

    after contrast in there share

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def sort_list(lst):
-        return sorted(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def remove_directory(path):
+        os.rmdir(path)
