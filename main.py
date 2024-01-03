6e84418a9226d154a7932247a8febe60f9df268f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:50:45 +0000

    care coat cough smile sure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_union(list1, list2):
+        return set(list1) | set(list2)
