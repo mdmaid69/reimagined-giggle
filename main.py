682f050ac09f124854c1a616fa6837bb4a15f126
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:00:41 +0000

    somewhere chance sure quarter split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
