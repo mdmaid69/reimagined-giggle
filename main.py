63155fefee2739fb27c345c719db7289b49cf323
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:58:07 +0000

    turnaway reduction busy surprising worst

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
