8617b7b294d55b616b25cc9a71b77038bd2530a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:19:56 +0000

    beat might lead tour spoil

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
