42af94a8b3761c07d6d44244cfab995306db2a9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:45:07 +0000

    care spin ring chance at

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
