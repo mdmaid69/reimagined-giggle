ce21616aaba5c48f52f283ef549b430fe83e1879
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:19:09 +0000

    welcome advice negative embarrassing weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def add_numbers(x, y):
+        return x + y
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
