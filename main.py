180af7be5861fb330c9049747ee6eecd8876849d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:48:31 +0000

    settle belong borrow low straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_directory_name(path):
+        return os.path.dirname(path)
