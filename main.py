8896bd697fcf53678ef3bc94b3a8683bfaa6d52d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:35:18 +0000

    constantly boring attempt lookaround everywhere

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
