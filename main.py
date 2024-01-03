b10a6bf21b5d8ca5bb50e178b7f920063b6eaad9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:13:55 +0000

    react fit expect most communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
