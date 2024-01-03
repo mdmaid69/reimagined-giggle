5bec23b3981225c5a9d5fe3b93ca424cb6760c70
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:52:19 +0000

    drink listenfor takeoff pair present

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def count_characters(sentence):
+        return len(sentence)
