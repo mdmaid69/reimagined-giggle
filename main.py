6f060edaf6bc40c3d8728dc5ad6b2e36db32ca22
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:55 +0000

    everything translate television create gift

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
