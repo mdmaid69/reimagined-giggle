e8f8f51cfde0e62b99c4a364d7a21fa9926a6cff
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:34:22 +0000

    team communication too exist amount

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def cube_number(x):
+        return x**3
+def find_union(list1, list2):
+        return set(list1) | set(list2)
