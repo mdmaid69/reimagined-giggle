e50ff367e8059f1a71073a863eb942c294bfaff2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:34:24 +0000

    wing university still shop disappear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def count_characters(sentence):
-        return len(sentence)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
