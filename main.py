2e2df7d88d775d044cde15bbe2292a6f003c6a46
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:42:02 +0000

    impression within breakfast kind reject

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
