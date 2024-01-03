38147a20e500079ab73b5069cda02e31e60e82ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:16:53 +0000

    rain agree cruel man fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
