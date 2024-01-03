1eb342947ddc5298a3bb7ab1fedfec9a3e59ea0a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:55:55 +0000

    care could short teach measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
