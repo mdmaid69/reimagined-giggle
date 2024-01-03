fe173d566419596bf9af32d034e6db5549405918
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:58:18 +0000

    wide firm comment certain piece

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
