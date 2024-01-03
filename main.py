fc7403fb241579de362b8fce2d5dbdad3a78c4d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:15:07 +0000

    listen stamp without right method

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import collections
-def create_counter():
-        return collections.Counter()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
