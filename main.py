a7ffa59db28cec3e922cb5a23be07aa147b0131b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:24:24 +0000

    anywhere question tidy branch thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
