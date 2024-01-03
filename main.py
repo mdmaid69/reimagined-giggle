a985aca9aea9b2da8f2441afc220e0460ff2a1a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:29:54 +0000

    I neither mix future scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
