03e8cb92472e4893c228ec4954176d3c9d6ff697
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:02:02 +0000

    bus far please bang job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
