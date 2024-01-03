5b7e1f520dac95206ab115f0a78501afe73fa561
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:09:11 +0000

    wave finger best trip fix

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def find_max(numbers):
+        return max(numbers)
