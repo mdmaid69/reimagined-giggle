dd873dcdd747352a558355c97a65c3ce802bef3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:44:24 +0000

    door takeoff square mine long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
