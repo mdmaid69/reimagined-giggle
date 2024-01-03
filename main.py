03a1a8cd211e6a8c9bb89f00dfdb51dfba3a4d07
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:45:40 +0000

    trial face shut influence summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_hex(n):
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_hex(n):
         return hex(n)
