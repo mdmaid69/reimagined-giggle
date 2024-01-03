6572e21c24713bb162d0d2086ec6561e7bd29daf
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:00:32 +0000

    steady indeed tip getback succeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
