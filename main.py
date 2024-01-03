7435ec73513c185f5f5175018d345f493ae605c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:06:51 +0000

    brief yours deaf pleased with

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def add_numbers(a, b):
+        return a + b
