aa7ffae6b9c5a52e3110cdca7bd8ea80352bb9dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:13:38 +0000

    congratulations communication comfort ilovedicks custom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_item(array, i):
-        return array[i]
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
