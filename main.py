a36ea9d18f234a98cd5d33d5b9a57a0ce8444b27
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:52:30 +0000

    should test giveaway present prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
