3657a59d2cddd29e51993dd05f14b091482d15a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:41:20 +0000

    doubt finally today no goup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_characters(sentence):
-        return len(sentence)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
