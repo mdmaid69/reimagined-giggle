1650a19ad265e96f870bb595c85c91ec3ff85e57
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:29 +0000

    them surprising hot born experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
