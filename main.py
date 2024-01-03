f5b606abc1daaff688d2455151a2fd2e44603eb6
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:14:10 +0000

    exaggerate kill inquire neither listen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
