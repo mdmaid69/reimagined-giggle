46994e411eff524d4da21a786dc2288e53a16e23
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:46 +0000

    clear workout date sorry exciting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
