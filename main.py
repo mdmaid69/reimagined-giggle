03bcaf2f1924733dae41670e3944d2360ce5bbb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:31:52 +0000

    position block flow calmly second

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
