034746cb80df66ac8c937e1f24d8df2c944e3cb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:30:01 +0000

    pleasant front gather excuse care

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
