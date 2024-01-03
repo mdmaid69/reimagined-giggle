ac8c8d39d880d599c67ae0730e4ad38d6aaa6081
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:09:39 +0000

    towards us receive put responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_max(lst):
-        return max(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
