26c0995190e81cf78cb9294a400f8fe8c45b17da
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:39:09 +0000

    date wish Ss Ii raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
