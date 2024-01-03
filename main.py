593a4c83931ee2c76196c9c66daa689c1a5a258a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:34:36 +0000

    care bend sort clean disgusting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
