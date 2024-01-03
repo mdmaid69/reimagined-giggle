8590fe13d8e7748ce1d2107c23e2cfb8fb1cd6df
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:39:29 +0000

    179bite centimetre whenever yet careful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
