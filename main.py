1375c9b9be887ed1ca8f52937d25d4a43d582bf3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:36:20 +0000

    hear obvious short switch immediately

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
