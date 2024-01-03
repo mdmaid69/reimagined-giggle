2c608f8c272bdceb8603c2b75dd30ef704a3219b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:45:47 +0000

    process sit cost meeting exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def count_elements(lst):
+        return len(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
