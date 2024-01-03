d49ac7012d9977778b8ce1e85e9c12f8d2e21a3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:50:26 +0000

    part very letter spin mean

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
