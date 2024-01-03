048fa4eb5da274ee67309f4833f0b201d499e7ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:43:44 +0000

    need calloff scale print danger

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
