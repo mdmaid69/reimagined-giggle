059959cf511765dee9154e2b470757a31c0c1447
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:33:41 +0000

    crisis politics pleasant cry normal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
