62d8857cabf2a2cac8ebd25d73dd3d6fa4270aa3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:12:53 +0000

    nothing soak over clue rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
