390c28415e13de48aef17d3211cf5d7ebb8cdfbe
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:31:18 +0000

    develop Qq laugh table moment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def reverse_list(lst):
-        return lst[::-1]
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
