f30b66fd53d1a657a5bab9149168895d39f7927e
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:50:41 +0000

    container long immediately male safety

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_min(lst):
+        return min(lst)
+def cube_number(x):
+        return x**3
