ded9a14aec6e2e90220ad376d9a7ca0825b76d78
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:11:37 +0000

    about send fair freeze appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
