8313cd06bb759d6142951651a862ad725b745d7c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:21:29 +0000

    letter persuade answer welcome connected

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-from collections import Counter
-print(Counter("hello world"))
+import os
+print(os.getcwd())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
