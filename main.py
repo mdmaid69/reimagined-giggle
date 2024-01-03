e5585e3c017b5fdc778dff19abb3346a2c88fb99
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:22:01 +0000

    belief suggestion holdup herself design

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
