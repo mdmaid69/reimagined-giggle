3c5ba6eb0560927fd2e29b96a8c89df249acff2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:19:38 +0000

    regret centre plenty meal lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import sys
+def exit_program():
+        sys.exit()
