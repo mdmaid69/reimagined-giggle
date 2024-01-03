d659e2a0fda65955f78437f5f546d3e51cd18968
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:03:46 +0000

    pleased preparation start remark dangerous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
