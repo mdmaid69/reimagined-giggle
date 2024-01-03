c7f81c62b53ea752d3df380d226964ccfc832296
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:15:22 +0000

    shape sick help personal scheme

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(a, b):
-        return a + b
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
