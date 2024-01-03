768c42cdb0053ffbff25b43f135d79748f04c0be
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:20:58 +0000

    reasonable strong mother bank earn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+for i in range(10): print(i)
