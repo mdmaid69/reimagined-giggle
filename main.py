2c731cd63fffdb123dc5a036238b04e33ea28267
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:36 +0000

    weak onto turn approval favourite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
