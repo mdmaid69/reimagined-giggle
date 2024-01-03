6365ff385e8679ad8fc07733699e2fd5c9aa330a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:45:53 +0000

    contrast centre deep mine passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
