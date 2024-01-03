58c5db8e53c15c04e4fab41022fd49d22b3f095c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:39:21 +0000

    come like towards wake getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
