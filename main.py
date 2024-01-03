fef19d0a671b1f753dde9d8297288735d293dc2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:45:24 +0000

    turn experience patience need so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
