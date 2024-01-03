f6276c762efa28e4b8f8a2f22f7d057ea4dcba60
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:38 +0000

    unusual ride closely further know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
