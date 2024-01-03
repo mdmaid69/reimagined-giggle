40888cdf246ff5d250791a5d7673c302784a5ac8
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:46:40 +0000

    combination exit away smell card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
