2c29a526ba35063f1d5e2892d73b4cf2b4aec86a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:51:29 +0000

    remark present sentence situation best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Words:", len(text.split()))
