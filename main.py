590aa9624a51394e13018d17e06fe2fb841673f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:19:14 +0000

    life press ourselves lookaround now

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
