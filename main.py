cc5911d9abbe9cee813c4b7619a25d5f79596438
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:57:47 +0000

    dead sea female crazy turndown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
