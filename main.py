db3bcc60700c7d65d5d1f42df377ca9f7ecac1ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:19:34 +0000

    gate share interest many deal

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
