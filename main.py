da45cee628303124e72a4a88a1465af71b342218
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:49:45 +0000

    cost takeaway brush anything case

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-print(sys.version)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
