dd255dbf5691efed061d1ed86030155428811fc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:42 +0000

    factory combination mind benefit waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def create_user_list():
+        return collections.UserList()
