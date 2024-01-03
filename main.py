c4f8c8666fbe9341124fa6f7f07888d75edfc7e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:07:28 +0000

    lie fetch his steer imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
