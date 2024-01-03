0d8b839570b399968c03ad9ec46537300ad20b82
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:26:34 +0000

    state dance and value front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
