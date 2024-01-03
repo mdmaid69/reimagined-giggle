11051162b8a55c791f676440fa00eb3a34efd04a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:43:04 +0000

    entry related information lock impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
