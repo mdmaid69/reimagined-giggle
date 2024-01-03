1496c4c313d5808e5ea82fd4c2e9845b95f40600
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:03:26 +0000

    kind key bunch breakup it

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+for i in range(10): print(i)
