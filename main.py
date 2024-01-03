6cacf7cf72fc3201eb87e10f1ee81b49836b8821
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:34:12 +0000

    similar married cruel swim low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
