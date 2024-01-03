1102025d58778a42cf491fdb368b66f8e323d3ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:55:39 +0000

    else performance communicate fierce material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
