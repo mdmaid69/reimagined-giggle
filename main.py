39dbea3dd3c2186b4e82c43b449c1cba93312f18
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:20:34 +0000

    mend active change deliberately heart

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
