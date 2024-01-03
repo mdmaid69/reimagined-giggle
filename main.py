51af43ee87cff62eaa912f2fe859dcaae1ac6a6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:04:50 +0000

    defend crisis check conduct goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_item(array, i):
+        return array[i]
