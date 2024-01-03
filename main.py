b4df34901187982cf4a40ab76d7eefb0a86b04cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:25:04 +0000

    culture window duty sensible tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
