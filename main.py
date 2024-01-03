693e540138b384777007234cfcc2ff84f54d74b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:59:53 +0000

    mark solve advanced expensive obtain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
