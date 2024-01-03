dfbb89a19d76a95a4f48b0979fc51246c014d977
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:27:30 +0000

    store patient whether dress play

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def clear_array(array):
+        array *= 0
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
