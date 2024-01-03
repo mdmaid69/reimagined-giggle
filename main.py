39c74b3eb07eae5711f7a22245282a6d95da42df
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:14:10 +0000

    consult shoot favourite main joke

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
