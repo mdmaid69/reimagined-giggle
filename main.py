4039b60944b4d7321c2e53911f89c77583ed9948
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:44 +0000

    cry whoever artificial smile reject

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-  def square_number(x):
-        return x**2
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def sort_list(lst):
+        return sorted(lst)
