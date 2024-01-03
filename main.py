1889a827c3161ab27320e04ecfea451696cd9938
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:35:05 +0000

    with lead wrong 568energy contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_current_working_directory():
+        return os.getcwd()
