bd292b3124d7f57179cda6e224a965f2a2cdeb80
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:14:10 +0000

    right ring wherever term equipment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
