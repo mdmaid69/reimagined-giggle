34d6a759cf505e7021aaa622ad83b98d334cc2e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:01:10 +0000

    knowledge turndown record finally boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+print(re.match("h.*o", "hello world"))
