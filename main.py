362865cf7f5d12c174677dacd282b26837ac78f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:59:20 +0000

    valuable false while Uu talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
