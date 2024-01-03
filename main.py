e3b051409ff574c81cf5ee8b0410f6f328858d64
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:26 +0000

    dry contribute you letter fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_item(array, i):
-        return array[i]
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
