201b13c1630b97085706ddec19d227be83c989e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:24:57 +0000

    language available close 573entertainment many

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
