618da65171642af724da240d2e30f11bb7f80b09
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:22:12 +0000

    fortunate peaceful near furniture promise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
