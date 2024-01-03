50685f023c4405dae5acf600698c12adec935b39
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:54:32 +0000

    square 572enter plenty reaction shut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
