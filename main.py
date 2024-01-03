868d913dd1021d1e525d955ab027c2eb825fd175
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:54:24 +0000

    back close window summer word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def find_max(lst):
+        return max(lst)
