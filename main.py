441b0442b2de15b14bacb5bc3e434b3a9e0dd555
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:35:52 +0000

    imagine meaning imagination 571enough valuable

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
