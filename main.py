4841393f2ae132637ac89b58ddcf9c30808aca4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:30:35 +0000

    build whistle stare ocean over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
