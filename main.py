62d535f6e098508001bf2c1387c1245e92b9dd0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:23:24 +0000

    within and lead arrangement bet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
