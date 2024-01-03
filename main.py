379881cae1b1b057567dc09115982bef457a5669
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:42:18 +0000

    say touch later better performance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
