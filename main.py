67966119ee1e293b210b91527bd416f6771450bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:22:05 +0000

    choose may inquire accidentally through

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import time
+print(time.time())
