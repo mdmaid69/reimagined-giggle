bbd7cbe80cbb4b0b25ee448d69a058f257879508
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:04:30 +0000

    able different on if book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
