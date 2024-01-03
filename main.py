b3400123388bf9b6d9421937ae1957a80d8049a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:35:23 +0000

    waste push idea score ear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
