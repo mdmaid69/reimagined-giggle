d08591f542d7fd1830b5a498d543e974d5dbc4f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:50:51 +0000

    free address sort exchange him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
