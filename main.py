28eca79d0617cda3085818c26c0b6473a42d76e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:29:06 +0000

    rain poison cover deaf success

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_rectangle(l, w):
+        return l * w
