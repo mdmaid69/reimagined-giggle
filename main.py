1b508134e90fd77fe02b6c6c3c132ef6a7705e0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:24:43 +0000

    earth situation travel freedom thinkover

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import platform
+def get_os_info():
+        return platform.uname()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
