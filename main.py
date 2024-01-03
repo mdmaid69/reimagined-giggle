7cec70f3fcf7c7f5f5be52f9c886260639ee0926
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:19:35 +0000

    spot machine organization comeback chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
