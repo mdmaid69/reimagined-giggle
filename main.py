cd8df114bc70ea48935f0a7be938940f3fa78ff5
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:33:22 +0000

    stand chin clean in doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def convert_to_octal(n):
-        return oct(n)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
