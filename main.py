ab564b3d4371184264b14694a26c45ffce7d2299
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:58:35 +0000

    length bottom Hh journey colour

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
