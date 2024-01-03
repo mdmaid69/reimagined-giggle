f1c774fde72b9e392524de40613db48cec6903bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:27:02 +0000

    mood depend arrangement behind strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import numpy as np
+print(np.array([1, 2, 3]))
