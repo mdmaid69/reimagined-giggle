68d710ed2144bdc0ea9b180174e167c2e458e028
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:44:57 +0000

    character match inform back far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
