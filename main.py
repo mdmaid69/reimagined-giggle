a921d63f4e151784fce6f77965915cf6a4e2b509
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:28:05 +0000

    cheque distant steady taste excuse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
