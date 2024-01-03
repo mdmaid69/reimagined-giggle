9ff9d2dcaf9b8374f03d72df05427873ad41596a
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:14:53 +0000

    sound off later suddenly approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
