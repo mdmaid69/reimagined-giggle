2ddad0d2109158fce7137408c6071e1a4fbddd47
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:25:24 +0000

    mess weekend scrap sensible behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
