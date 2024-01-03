c296544b0d06b818f540d236b7145a3398d5b160
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:10:16 +0000

    company nothing behind board row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
