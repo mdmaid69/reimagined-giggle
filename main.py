b9814370cbce87aaacfcff32ca9ec11bd396c8d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:55:28 +0000

    thin dear turn either except

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def split_path(path):
+        return os.path.split(path)
