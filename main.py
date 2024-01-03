dbd5660e0e660e16a04bd8c7570d8044943dfd4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:33:04 +0000

    satisfactory science exist urgent relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
