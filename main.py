0d3b65337c8f1a38abd4563ab0374548d648b459
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:50:37 +0000

    sky emphasize lose see 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
