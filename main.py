524214b06f8ddc7fa6c06f90aa3323ea6214f081
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:51:56 +0000

    grand man minor afternoon answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
