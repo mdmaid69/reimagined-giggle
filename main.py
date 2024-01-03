9db54caf3def15b520fa8e3ffee34a3a418ddf72
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:45:46 +0000

    bottom thankyou question evidence deal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
