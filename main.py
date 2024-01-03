89b0f47199c413923384c22a5c847fa1377e6b11
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:56:32 +0000

    bath load surprised proud holdon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
