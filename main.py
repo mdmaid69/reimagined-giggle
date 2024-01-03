7d6c47a2461bde44ed9e6ca7b59d201432171dd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:54 +0000

    trial confident pleasure snow callat

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+print(os.getcwd())
