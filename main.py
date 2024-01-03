eab9fcf1c568a4424573a4d9a9ff5748a53b9af1
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:38:15 +0000

    splash drink hold up great

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
