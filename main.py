0cfb5e6fafa51999508712fa366af51d4592477c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:03 +0000

    worry firm guess skill pronounce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
