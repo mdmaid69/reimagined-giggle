05991e85dc0962ba96b9e11fbfac23687632ffac
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:50:35 +0000

    once worse country obvious lunch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def divide_numbers(x, y):
+        return x / y
