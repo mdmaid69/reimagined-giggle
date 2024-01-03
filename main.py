5d20552ac987774b2df5d127e0f8d272acf2d694
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:12 +0000

    challenge left train wonder final

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
