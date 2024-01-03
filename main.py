163d17ae391e36f978d3c364f9596c89fd03d05e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:13:34 +0000

    pot another favourite tomorrow loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
