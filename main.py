36c07e2ee7427b87e80b5b3d6ad26a7b1ab9cfd1
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:51:33 +0000

    skirt paper rush careless shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
