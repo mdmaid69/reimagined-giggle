7b47d99dd6740ee23d8dafd37142bc945c877aaa
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:32:28 +0000

    whoever respect pretend shoot whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
