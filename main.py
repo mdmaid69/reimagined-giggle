bbda9dc9b2c33d68aa3fee53877eab687f208275
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:22:18 +0000

    boil great regard reach glass

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+for i in range(10): print(i)
