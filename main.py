97b572495b2ce658457ba964727d0942194f3d4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:02:18 +0000

    dangerous order wake occasion plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
