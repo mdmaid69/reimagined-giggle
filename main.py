9b0442e09b2efd7850438a47af6e6b5b4674384e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:56:03 +0000

    cope bet minority sister send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def calculate_median(arr):
+        return np.median(arr)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
