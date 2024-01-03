1c568e67722fd59c1345c0b1ef19e346517684f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:18:16 +0000

    570enjoyable die low stir whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
