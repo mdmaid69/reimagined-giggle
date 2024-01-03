1ade83fd677960f777ce0f0dacb6ea150883ed44
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:08:57 +0000

    force rescue the laugh please

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import os
+def change_working_directory(path):
+        os.chdir(path)
