ee960449c32daad55e71337fe6fa06ad892f0284
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:00:00 +0000

    skill stay compete convince practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def find_min(numbers):
+        return min(numbers)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
