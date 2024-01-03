138505e6b9a68742e58eda991c43909d3b540f75
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:51:30 +0000

    switch label busy mess we

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
