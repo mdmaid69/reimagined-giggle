b902114f1106f980ac2381965dcae2ce714efa36
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:04:37 +0000

    limit piece pretty fork love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_pressure(force, area):
+        return force / area
