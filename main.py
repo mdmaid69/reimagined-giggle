9bfa5550d784a24bbefa785b8a542f50ac5c3753
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:37:10 +0000

    frame go only description before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_min(numbers):
+        return min(numbers)
