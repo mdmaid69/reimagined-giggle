61b50c3a74bf0340378aa1294fe6c4b14ef4ef0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:39:54 +0000

    remove or goaway calloff bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def clear_array(array):
+        array *= 0
