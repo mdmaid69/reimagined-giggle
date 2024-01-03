f0931b58a79b0527337aed1878192f5d71122d9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:52 +0000

    house unfair start represent exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
