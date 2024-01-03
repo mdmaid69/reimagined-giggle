024de0e46b39957dee6e1bbd3de3aec95da14f6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:33:02 +0000

    quality surprised common contribute so

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
