234557f5c2fce784335cdd641a6a7f70dc73ae43
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:52:59 +0000

    Qq total post priority reliable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
