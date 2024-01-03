111d0c27fec5ab093ece141c367c41b003cdb48f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:16:37 +0000

    brief expect prefer measure practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
