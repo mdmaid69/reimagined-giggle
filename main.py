b58a281ab43217a961a06d72d622d6b4c6a17eae
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:25:22 +0000

    keep hardly wild line exactly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
