6a1582d79c73f6c0469012053c080376d266cc51
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:13:32 +0000

    behind consult write copy some

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print([x**2 for x in range(10)])
