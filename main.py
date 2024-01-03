186fec13d60150c54be485b897044042c26dde1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:36:59 +0000

    want top soon couple bend

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
