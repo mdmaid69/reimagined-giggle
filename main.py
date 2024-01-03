4669dbb87870268265ccf3037341678f85746831
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:53:01 +0000

    plan show sweep image freeze

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def is_odd(n):
+        return n % 2 != 0
