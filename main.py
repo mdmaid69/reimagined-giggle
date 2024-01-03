d80db90f43e29d15923a67b7541b814ecf7a5b4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:01:13 +0000

    hit healthy answer total home

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def delete_file(file_name):
+        os.remove(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
