4090efeed91c35ca8dd8e2d630be74ff1acc9ae6
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:05:47 +0000

    consider fresh his criticize course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
