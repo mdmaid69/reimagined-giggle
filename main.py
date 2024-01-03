c86e7fd52c61a7668190063c7998839cbdb8922b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:31:43 +0000

    belief list rise disturbing cum

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print(sum(range(10)))
-def convert_to_binary(n):
-        return bin(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
