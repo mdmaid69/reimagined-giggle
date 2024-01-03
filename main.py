89671607940c9f6e841c682ace5cd35978866b7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:12:29 +0000

    awake negative find experiment different

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
