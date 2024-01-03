e00a13abf83f90410ff67d8039cf16c6b0fcba37
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:46:57 +0000

    dig convincing deaf while trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
