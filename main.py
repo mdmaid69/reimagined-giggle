1ed16dc94696d83a20e3ec806a82b2ae506468df
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:07:09 +0000

    turnon report cry Hh fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
