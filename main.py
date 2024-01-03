b4b4bbb2f98eaceef25e985ddc26551beef9b775
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:25:11 +0000

    back breakin separate shoulder sleep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
