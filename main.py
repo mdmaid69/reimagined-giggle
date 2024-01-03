edff000719b44317962e43ca2835f7e8d365f75d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:08:02 +0000

    may care clever less special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
