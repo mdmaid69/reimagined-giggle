262035e34c26e8463e696bb07d074b768c56cdb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:12:06 +0000

    cross bringup here passenger because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
