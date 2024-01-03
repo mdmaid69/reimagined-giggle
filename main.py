6428bf286aff5d55b02cad98862ca5e1da24b3e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:21:18 +0000

    praise radio skirt pinch leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
