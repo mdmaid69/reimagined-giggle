8f6199b578aed7536aa5754d0b3c59c85ae00e00
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:11:59 +0000

    surprised behave sudden tour patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
