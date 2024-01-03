c175b2b2dd8a977374b011e65d4b851999058337
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:14 +0000

    fire healthy direct unfortunate cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
