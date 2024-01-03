cc50733ba99789eaf209d9dad369d7ba3972a3c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:31:26 +0000

    571enough head keepoff unpleasant swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_str(array):
+        return str(array)
