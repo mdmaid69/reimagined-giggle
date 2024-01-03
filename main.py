5a743c5bb69f0219bde97d8153edd63b0143a1d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:09:50 +0000

    church must square everyone high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
