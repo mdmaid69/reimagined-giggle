12fd402df4f0fc22b94950c9ffdd4b4599634bf0
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:12:53 +0000

    573entertainment oil turndown offence when

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
