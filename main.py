6ced184c2ea3a9b52a4c381ce2ce7f5175b91ef5
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:15:46 +0000

    many advanced national force group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
