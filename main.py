91a4fa6ba45b808210d0b4003737ee12d2fd014c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:25 +0000

    little emergency pull except round

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
