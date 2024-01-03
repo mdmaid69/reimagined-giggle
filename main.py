7dc697a05bab226a8caa1815cf07c397c8f915fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:27:55 +0000

    ought I prevent draw successful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
