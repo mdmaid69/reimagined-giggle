1f5b3639334d43572aa802dd0339259d8c5638b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:42:43 +0000

    plan character hole possibly move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def is_odd(n):
+        return n % 2 != 0
