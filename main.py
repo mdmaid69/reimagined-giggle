48e1199aff4923476eee6e0fa6a68aa9e1a72ab2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:08:55 +0000

    answer influence free too set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
