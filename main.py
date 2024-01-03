a012e536f064b5552e6386530d73289e37f17d87
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:55:43 +0000

    copy in guard command comeround

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
