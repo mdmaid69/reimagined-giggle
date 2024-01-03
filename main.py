23f5a455c02fb431ad6d50f9e5b3e3ab20e7991e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:24:31 +0000

    accommodation already pin popular collection

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
