7eb54043f04d161762a35914278fdaa3e82eb45e
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:29:00 +0000

    emphasis trap cry breakfast connect

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import re
-print(re.match("h.*o", "hello world"))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
