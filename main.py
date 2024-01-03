00d1d29164b218d91b488c835a0bc3ac91f21c49
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:36:35 +0000

    especially other trip mess freedom

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
