ef235ae41143787bcc1bcce1b471524af5e5b118
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:53:30 +0000

    low up behind arrangement ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
