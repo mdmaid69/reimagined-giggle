0ccccae55a2b4ebad8d4eda9c3e2146aac6582b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:25:16 +0000

    success near worth measure cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
