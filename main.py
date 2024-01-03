7fea85ddbe84f338a79530c706d778bb3a8253e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:18 +0000

    rely anything container thin bang

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+print(sys.version)
