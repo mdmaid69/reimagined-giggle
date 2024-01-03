6a47d9dd485bda6a0902c3b1d6a558c917255274
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:27:47 +0000

    train flavour process fun science

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def add_numbers(x, y):
+        return x + y
