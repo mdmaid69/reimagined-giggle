678b756121e6ee3b3154a0d24c0570dbfe038347
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:29:17 +0000

    listenfor flood little confusing confused

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
