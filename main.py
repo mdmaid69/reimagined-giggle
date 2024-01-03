b0f1bf45ad9f1894e2b46d8c179eeffb200c003e
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:36:59 +0000

    message 565end comfort surprised give

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
