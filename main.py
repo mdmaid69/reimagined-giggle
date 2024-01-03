fa881929a66e320df7e74ac62357a3a7ee0877ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:49:38 +0000

    rest would bury respect consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
