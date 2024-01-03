3b70bcf5a5f7178978801a5b62226663f9e5422e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:33:18 +0000

    basis medium pipe exit butter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
