0dfc20a78a2d94118c62e34f96bfd59e7b569ea0
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:25:20 +0000

    argument offer nurse actual excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
