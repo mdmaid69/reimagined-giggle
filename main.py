3c44e77f9174fb60834de27dd52f713258b12734
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:07:10 +0000

    emphasis remain centimetre post know

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_set(array):
+        return set(array)
