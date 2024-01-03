bd873ab4e4ad8a1bce647d31dfd64dbf146f544e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:00:23 +0000

    regular holdon cool variety failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
