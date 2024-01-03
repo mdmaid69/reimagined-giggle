00b6da8aa73db8989c7ea427532005a01cb4e181
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:20:31 +0000

    busy shake that plan 572enter

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
