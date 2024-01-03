def3d061adedbc40afcd9e6854afb70bf24c51a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:24:23 +0000

    rise well fault month both

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
