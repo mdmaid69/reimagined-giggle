cedd630bc445af5ad9887f2b467fe5e33add0d2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:59:24 +0000

    expect contrast wild list hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
