230703db7e1f81e0b817d48ee34bb47007b16d8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:49:28 +0000

    show closed opposite damp situation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
