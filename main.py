9db3920bc51ad497bec9a986e7a1345613526f6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:39:38 +0000

    runaway stop breakin hit content

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
