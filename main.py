e0ce739c9330cf6662764186b571bfa2f2fc491d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:44:25 +0000

    beyond feed surprised take flight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import sys
+def exit_program():
+        sys.exit()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
