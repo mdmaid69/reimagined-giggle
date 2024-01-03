3250ba68d8b918f5e19656992fe1531c1acb5442
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:25:23 +0000

    free crowd lead ask front

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
