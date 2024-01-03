676a59e62ddbc7ff8bb7e8f8b92ac79452aba8ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:14:03 +0000

    any decision please condition even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_min(numbers):
-        return min(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
