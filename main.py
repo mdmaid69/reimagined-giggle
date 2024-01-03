fab094ecf94de55d0a8867ee1fc1fae332bf2e95
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:50:24 +0000

    prefer free walk comeoff interfere

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
