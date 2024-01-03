99f96fd00d89e181596c096e05bc80a3c3a31a75
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:52:07 +0000

    factory out play pleasant popular

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
