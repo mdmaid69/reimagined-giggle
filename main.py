bac6a202db9eb8e7dd3b8d0abfe1eb4693d7b4b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:33:06 +0000

    argument past suffer present upstairs

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_elements(lst):
+        return len(lst)
