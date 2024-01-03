69503eb241f20cd00fff184649b2a56199973301
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:54:23 +0000

    pleasure spot himself pleased pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
