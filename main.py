7c2ca5e902a580338975dd7314e83f2cfb6ef7c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:54:17 +0000

    when careful crowded break criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
