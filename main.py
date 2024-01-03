f3ac512b6975fc166033833844650055ff1fb3f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:33:59 +0000

    drop someone damp keepfrom language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
