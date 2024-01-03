5dca355fb7e6281dcdaf9298b9ec4741c13324cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:08:29 +0000

    be prepare food deserve mainly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print([x**2 for x in range(10)])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
