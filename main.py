69fb7f25e45bb5e288f53688bcea257c7072acb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:27:01 +0000

    shape ill both branch weigh

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_volume(length, width, height):
+        return length * width * height
