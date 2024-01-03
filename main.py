2de57fee901ee3d89fa4289999610d9b8f5960f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:17:48 +0000

    rely beautiful responsibility importance operation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
