e93a6e31112064e2dc2a8fbfbe439163444bb644
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:24 +0000

    mark popular constantly organization value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
