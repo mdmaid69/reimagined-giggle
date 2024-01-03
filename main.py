935f16bfe3df40728b3ffc75fd97b1b3ed76dab5
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:17:10 +0000

    male same famous taste doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
