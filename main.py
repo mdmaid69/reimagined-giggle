a90967880eb0f6c7903860f6f1590f3967b47ca8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:11:35 +0000

    old cum class regular commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-print([x**2 for x in range(10)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
