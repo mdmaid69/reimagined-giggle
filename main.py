90d6baa536d7dcf88e6208f087ed9177d43db36a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:42:34 +0000

    about join every exam attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
