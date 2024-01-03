c2d7a81f9e762806c52ab39d284861fe603fc143
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:19:09 +0000

    child call steam foot hair

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
