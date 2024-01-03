0516924106f433ca2d535c30fe77244b668907ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:02:51 +0000

    thank work only rich flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
