4f9dbf6c6e6fbd9b6865a809fb56e3ff1a51cf10
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:37:12 +0000

    sensible you right limit holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
