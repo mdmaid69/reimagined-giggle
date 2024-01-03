98082afaccaaf56e07c57bd445fb4311273b0e64
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:13:26 +0000

    naturally noone ticket follow as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+from collections import Counter
+print(Counter("hello world"))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
