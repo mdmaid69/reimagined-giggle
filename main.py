09a0711eb2d96e61c3c1986eb365f4d620f69b99
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:23:31 +0000

    turnaway system pin comeback wish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
