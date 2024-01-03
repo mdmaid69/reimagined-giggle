5f09a2cccfcdacd745f6aa90526d5477c3a3201a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:25:43 +0000

    normal lost sort pretend class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
