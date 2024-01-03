519c55fea805b67521769a1b6bfd7f788e3d6f2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:20:23 +0000

    rare snow disagree expect I

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
