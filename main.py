2a4d2b2ccfc5f6fae997927ccdcfd8ba69a096f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:30:17 +0000

    case careless guide talk chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
