8169793eb0bf9e749e819a04ddd20dd07104e26d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:54:19 +0000

    react Ww lucky can thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import collections
+def create_user_list():
+        return collections.UserList()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
