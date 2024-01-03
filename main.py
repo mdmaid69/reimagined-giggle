1891b4b8c72077252e8e36f8e1f572ecffa6e961
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:29:27 +0000

    anybody freedom choose match level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
