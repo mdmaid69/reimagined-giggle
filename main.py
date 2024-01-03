38d19f8706d6d489ba92b8985de332b34d7af31d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:47 +0000

    machine plastic key car brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
