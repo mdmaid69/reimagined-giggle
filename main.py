a63b2c2fa8c4285a973269fa1084e13d2bbbd67f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:18:24 +0000

    mistake thorough flat house activity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def sort_list(lst):
+        return sorted(lst)
