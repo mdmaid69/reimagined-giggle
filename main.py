ebb5bf33e0faf9199baeb9a2b09e8cbd0c2f1625
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:41:12 +0000

    hungry tend comfortable once noise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import tensorflow as tf
+print(tf.__version__)
