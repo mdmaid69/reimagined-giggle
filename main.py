baee19797c6ae15e0a5f11069262ddac048e299e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:22:00 +0000

    mean petrol underneath develop bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def subtract_numbers(x, y):
-        return x - y
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
