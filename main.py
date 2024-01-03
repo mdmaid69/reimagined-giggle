1b5cf75391c97ab38a556730f83eee033daf8cd0
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:28:37 +0000

    thought take wave workupto responsible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+print(os.getcwd())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
