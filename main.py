be74b04843186262f5746f50c10fabf05033e9ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:57:36 +0000

    brown knowledge sell power instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def subtract_numbers(x, y):
+        return x - y
