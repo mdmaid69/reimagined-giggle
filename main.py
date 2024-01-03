0013a384e88cfd58353eb8e3d6c72245dd739f53
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:47:19 +0000

    Hh holdon geton serve warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
