09d8406ebab6f85ef37f73aa7996edc26c23d1c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:15:19 +0000

    week getaway disappear common describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_area_rectangle(l, w):
+        return l * w
