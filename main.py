68b863e54f0a7e7b51392f7e828ea796587fcda9
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:12:53 +0000

    quarter face town woman beside

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_length(array):
+        return len(array)
