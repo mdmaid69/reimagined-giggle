ebf8d1f638cfdb762c9b06eaaf43e8584b3757e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:53:30 +0000

    peaceful dinner personal run breakin

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
