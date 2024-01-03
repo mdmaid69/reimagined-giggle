2ffc38a30df6fae6b511d24201a85dd2cf0d3e42
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:02:16 +0000

    underneath lucky danger bread move

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
