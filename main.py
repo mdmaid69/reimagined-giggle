c4c1ee0597025eed789e37ae1501c47b582692fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:47:05 +0000

    Uu unlikely ability wash reduce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import time
+print(time.time())
