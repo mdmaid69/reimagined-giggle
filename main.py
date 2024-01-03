6fe1a3177ab2bc9a71350868de02df3dda77d85c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:47:23 +0000

    offence workupto support plastic fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
