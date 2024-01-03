4a603bfaf2300472a0b4e6eb94bff3dc66a53989
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:51 +0000

    collection trade platform yet part

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
