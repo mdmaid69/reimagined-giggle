be0192f1941f1da1e91b420eecd2a54043ceba6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:54:14 +0000

    sticky house street spot issue

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print(sum(range(10)))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
