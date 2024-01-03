1c55a75cd3205050771d06d5989f4cf71fcdeaed
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:41:25 +0000

    include accordingto contact book fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
