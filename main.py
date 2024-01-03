4aa51fe63ff55395bf494ab31f6c7e4bfb7b92c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:19:51 +0000

    door male share attention trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_float(array):
+        return float(array[0])
