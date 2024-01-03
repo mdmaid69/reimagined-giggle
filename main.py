17372c1cf5b89afeb96af1e896a3b5330e4d60bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:10:50 +0000

    wake quantity bank 571enough occasional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+for i in range(5):
+        print(i)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
