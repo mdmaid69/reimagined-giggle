3957f434cfe8904af2943ddb9f28b57a01ee3323
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:23:57 +0000

    innocent some hungry consist success

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
