60ce9b14d3fc6efe02e9befc642aefc505d52997
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:16 +0000

    rely central any mood married

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def is_even(n):
-        return n % 2 == 0
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
