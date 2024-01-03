3b0e2d7737c4f923108db4d85be3936a14e0c8f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:08:40 +0000

    down person responsibility ourselves ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
