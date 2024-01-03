b0f038866470459981d344cb41801e236cd201f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:25:09 +0000

    anybody rich insist notice yours

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
 def find_min(numbers):
         return min(numbers)
+import array
+def get_array_item(array, i):
+        return array[i]
