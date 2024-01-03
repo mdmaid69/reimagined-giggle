f9852bf473539e03f8d3a489f63fb6c56fdc8b42
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:48:28 +0000

    lookback(on) frighten shall trade 572enter

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def exit_program():
-        sys.exit()
+import time
+def get_current_time():
+        return time.time()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
