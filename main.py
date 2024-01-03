657f216c6e2e2a0cbd16d1b9f7ce8701555d2e97
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:22:22 +0000

    flat gentle often flow apart

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_work(force, distance):
-        return force * distance
+x = 10
+y = 20
+print("Sum:", x + y)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
