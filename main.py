03bd2b46a6e707bf6ab343d980e904d3a5c237ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:05:35 +0000

    degree show shall study unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+print(os.getcwd())
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
