439cd4484ed8c16628afa4a927683e6189aaf6a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:01:15 +0000

    calm introduce type business skirt

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
