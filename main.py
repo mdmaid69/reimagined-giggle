e26fd0632648a5a58ae024b47451e8f6d041e596
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:17 +0000

    shoot else draw software straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(lst):
+        return sum(lst) / len(lst)
