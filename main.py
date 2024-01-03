7a2f8fa3c208a88cd3e5775d97abd2d5aa8daf8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:49:33 +0000

    apart I scare backwards practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+print(math.pi)
