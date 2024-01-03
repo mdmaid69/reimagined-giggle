1c6c5fc55ea39594ddf5f0c8d80c36ee3bae342a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:26:57 +0000

    flat help stone place trousers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def count_words(sentence):
+        return len(sentence.split())
