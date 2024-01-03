54166a05bd301fed5b30109482eb7420f6eb2277
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:05:20 +0000

    will part passenger Ff dinner

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_power(work, time):
-        return work / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+print(sys.version)
