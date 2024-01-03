e3722773f459a3c6a4a91fda7f7a6807de7f44c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:23 +0000

    fortunate spill month certainly breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
