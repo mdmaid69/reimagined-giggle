8e7d00f70aa8d92ee81b9d5a1d61b629f2c436ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:32 +0000

    dark mysterious ill Dd regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
