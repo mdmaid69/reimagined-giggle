adc813669eda405de8cf0ac2bf870fffb5a34fda
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:18:39 +0000

    suit tape their love callout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
