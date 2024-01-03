f328019a7bcb4f0da620539024e6f9fab4de0275
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:02 +0000

    yourself pride about short bus

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
