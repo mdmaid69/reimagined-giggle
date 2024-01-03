d814c0c4169234d5bd1557f58e136ffb655c04a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:02:13 +0000

    sport software pipe drink closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
