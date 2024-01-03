10ce911e6865d1d37a028f61758f214b66ec7df7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:58:02 +0000

    spray possibility pile game mass

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
