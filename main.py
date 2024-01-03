3ff84c60603cebf27cc7af5d7ab1b1f1734bc17a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:18:39 +0000

    if prefer hers used call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
