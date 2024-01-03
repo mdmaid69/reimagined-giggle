2eb969027e477d0115220b714aee4348ca59ac15
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:48:43 +0000

    plan load rob below cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
