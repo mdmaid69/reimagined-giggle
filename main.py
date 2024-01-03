1dfa26964f815a70727c5e32f91adbf620df57aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:26:55 +0000

    last worth previous lookaround mind

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
