9c5ecdda4762df0033c16dcbb744cf005fee484e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:15:07 +0000

    hurry convinced obvious after picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
