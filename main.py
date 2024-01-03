2b083d591ab076d5d98531bae07dee2cbc2e4ed9
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:59:42 +0000

    onto want wrap least business

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
