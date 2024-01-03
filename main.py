8e67445da5d935ee0c4383f61446e3aa4d1fa3ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:25:08 +0000

    separate breakfast force think 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
