da4117731640982bd9f327ec7fb522f77183a337
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:14:39 +0000

    score Dd century work slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
