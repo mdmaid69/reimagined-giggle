898fae812802efa5560c7839322a13f8725196b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:55:20 +0000

    broken attempt edge distance guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_priority_queue():
+        return collections.deque()
