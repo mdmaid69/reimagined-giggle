b6d75eabf1b16a3989e9978c46ac1fe0047b8dfa
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:00:34 +0000

    passage their open ever adjective

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
