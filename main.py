a909037e7f0f28bf8c24098032a3dfc920659968
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:36:32 +0000

    chase human shout ever offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
