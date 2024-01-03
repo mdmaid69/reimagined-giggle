cdaacc7caf19a94d699d6fef9b29dcbe320b7f86
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:15:52 +0000

    vote smile affect whistle instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
