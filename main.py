2414edebacbd447d4e5019452dec2ec6dcaa5aa1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:05:47 +0000

    themselves complete impatient slice man

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
