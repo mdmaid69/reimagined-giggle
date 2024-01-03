665a6043721dbdc26bbd836efd84edb4b6325d84
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:38:22 +0000

    Ee may also expensive bored

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
