af4b5bf742b952f033a78fe783a32805ca3b2a4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:54:03 +0000

    rain close will decision fresh

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
