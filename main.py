5a34b8cdf026159656b42d32bcd2e736f24ba8ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:11:23 +0000

    empty unhappy upstairs door her

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import sys
+def exit_program():
+        sys.exit()
