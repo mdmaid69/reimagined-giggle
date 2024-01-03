2ca63eb18e3ff2857ca7a7f1f9b991db13b54d9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:26:42 +0000

    other study government rule heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
