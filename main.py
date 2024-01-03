a2f4cb14959cd0fa7444a4fb2a51374571e5f4a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:00:04 +0000

    admire interesting sound half bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
