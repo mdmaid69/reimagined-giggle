b664cd7a1c64cf4cd5ca381ef3e8052cafc1811c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:03 +0000

    play leave attract lie giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
