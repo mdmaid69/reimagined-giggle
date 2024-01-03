769efd21a212c86e41e157a999960c23847d698d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:48:07 +0000

    pin concerned thirsty loss row

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def reverse_list(lst):
+        return lst[::-1]
