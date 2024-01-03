6d3da721451e0152eebdf5ca5ee281aff93180b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:09:28 +0000

    fail hurt birthday custom bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
