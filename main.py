f8a6fdd29ca2f2987e638ffbafd3f9f8467b8831
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:12:13 +0000

    immediately travel also next Ll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
