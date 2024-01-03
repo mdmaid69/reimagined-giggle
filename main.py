bb0ae4cd2579ba88c1f005e22abf9796a5793ebb
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:53:32 +0000

    silent idea lookback(on) loan further

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
