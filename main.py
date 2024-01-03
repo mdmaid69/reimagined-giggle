77f0bd1f7dad652c0102ee3b0f2aa977b901ccf7
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:40 +0000

    so spill position getaway himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
