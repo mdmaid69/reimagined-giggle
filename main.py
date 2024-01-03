334c1018cf19fe62f0eb2b30a3677d95ac673800
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:00:02 +0000

    benefit smart sentence him ground

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
