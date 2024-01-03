6f0c3397c7d283c8bdb2a5f50facdb896518ef53
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:11:59 +0000

    grow statement warning hurt damage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
