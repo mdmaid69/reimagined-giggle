981c1d13607510a9d2dc7b47fd16eb0027b3a731
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:49:40 +0000

    mad my month minimum example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
