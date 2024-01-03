5e1133ee97a71ccff4ea2f76fdfc47f57718ab6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:29:25 +0000

    apparently concern gentle afterwards violence

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
