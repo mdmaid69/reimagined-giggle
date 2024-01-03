ccf68e1e0ef5d7439ff8dd800961960c0002bbaf
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:14:05 +0000

    expensive compare familiar tidy answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
