82f1442446efb53945b74f5b54ce5ffa003e1b13
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:56:23 +0000

    breath busy sure their return

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def factorial(n):
-        if n == 0:
-        return 1
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
         else:
-        return n * factorial(n-1)
+        return -1
