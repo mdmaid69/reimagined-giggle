aa01ec0c8d5ac0894c6abf4b91223bb91ff1dfa4
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:52:20 +0000

    sky special better during drawing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_octal(n):
+        return oct(n)
