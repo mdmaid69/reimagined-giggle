46e268aa98c2d66d2848183e4345dfd262980342
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:15 +0000

    right music duty sensible moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
