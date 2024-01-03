46a8a62988330eebff151808b6d0f836a2d954e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:23:53 +0000

    innocent Itself achieve ask professional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
