ad0d1847212f72ff8d41e545abb8b9e46dbf1622
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:35:23 +0000

    let degree definite popular circumstance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
