57031a5862b40b47021c779f3de668d309213e86
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:38 +0000

    table shop shout interested benefit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_list(array):
+        return list(array)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
