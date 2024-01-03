03c44396297a43dc591b0cf5f575bc9dd68600e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:26:31 +0000

    breakup target doubt die general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
