2bbb4e755ce7561c2137cf9ebfaf6af1e8201843
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:59:16 +0000

    platform emphasize suggest warn equal

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-print(re.match("h.*o", "hello world"))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
