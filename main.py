0b0c56e62583f82fa82479cd5974815dc477fbb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:34:18 +0000

    leave store move manage quantity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_work(force, distance):
+        return force * distance
