c3350f5295a5f086c8b5fb71d9d4ba24fdc1751e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:34:09 +0000

    store anywhere phone shout family

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
