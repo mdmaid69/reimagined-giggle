fc13ea2ede0302243775ea74dfe9cf60838edbc6
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:32:29 +0000

    fast report poor bill connect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
