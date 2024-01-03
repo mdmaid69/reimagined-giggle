2769e8d97af8ac67498f467b30d5efc1dbc9ae6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:49:13 +0000

    art bush anyone amount central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
