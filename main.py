1af37522db7bdef87ee81f9bad2506e22098e67c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:01:06 +0000

    pot fence begin produce unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
