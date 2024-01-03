4b917a233bd02b30145e1ba8d1184a9adf92b680
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:46 +0000

    food unfair hungry increase pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
