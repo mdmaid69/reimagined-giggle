e4945915d8e76588e8bfd8e6ad1d9caa5cb6d7cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:21:14 +0000

    size holdout large fear loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
