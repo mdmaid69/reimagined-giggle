5b1f42488125ecf7f18dd95909ea6532cdfdafe4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:44:21 +0000

    wrong spot personal usually punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import time
+print(time.time())
