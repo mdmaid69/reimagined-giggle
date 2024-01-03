37a3462fb222b6ededad5c0e69eb976fb27d5885
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:34:44 +0000

    subject job famous one apply

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_as_list(array):
+        return list(array)
