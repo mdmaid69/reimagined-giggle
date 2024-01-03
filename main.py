7031cd30921937cfc4a8b1a86f28507b459e0e46
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:12:21 +0000

    succeed to grand although regional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
