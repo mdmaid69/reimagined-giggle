6c322b6276a357236e5977e970de422873052d8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:31:21 +0000

    cut bringup sleep keen change

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def is_odd(n):
+        return n % 2 != 0
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
