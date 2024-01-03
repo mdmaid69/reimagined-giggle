9e42b1688f6a85d63a19b15fe4b2c71bf6597934
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:18:48 +0000

    wheel fight conclusion their embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
