45cef995e14da115f6af4331ef18f4ce0226c94e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:03:50 +0000

    belong market into pure go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
