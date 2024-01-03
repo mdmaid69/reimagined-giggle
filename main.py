d6180f045662cc5af3bb4173cb6fd1313fa002f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:55 +0000

    influence defeat whom left trial

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
