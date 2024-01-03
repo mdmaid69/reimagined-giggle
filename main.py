2a3eb5dd5679959228e7d5a3ed558fab4bd5a9ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:14:12 +0000

    communication comeround once breakup wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
