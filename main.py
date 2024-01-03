b533fa4192a39270b6b72072328b9b478f7313a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:41:29 +0000

    that round fat slowly cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_list(array):
+        return list(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
