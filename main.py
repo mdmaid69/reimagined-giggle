cb4eed194a5c102ae45282df257f32374e35459e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:25:41 +0000

    land patience stage old replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
