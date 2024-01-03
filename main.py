f67f5524c79dfa9bbbd37d6c2c89c29f2da992b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:18:03 +0000

    burn bringforward during weak him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
