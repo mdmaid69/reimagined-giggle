9ff956cdf3d8a535ef7f919038499eb186862d66
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:47:57 +0000

    content kind difficulty tragedy call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
