58e9bc3e13956e97f22f737eacffcee11b65150d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:40:57 +0000

    school pipe couple bend exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
