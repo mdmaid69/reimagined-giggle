bd08f4a6ac8b18bfae7504f24d5959d8b0853d32
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:07:58 +0000

    side how surface cap begin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
