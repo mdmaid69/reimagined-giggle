a771786889abe70fe72630f70f5766668d7dd3d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:58:03 +0000

    purpose total hold book above

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
