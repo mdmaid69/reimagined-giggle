6386c78fde10b90ec3439847836f81d3e9ebba76
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:21:34 +0000

    profit compare careless by hard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
