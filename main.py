75ae2a817b891e4ea879cdbca4140ed094cca1d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:32:15 +0000

    extraordinary cum big clothes sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
