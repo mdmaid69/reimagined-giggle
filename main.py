04284670083c5770f694e66fa863e7cae139097c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:37:12 +0000

    left range lose excited now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
