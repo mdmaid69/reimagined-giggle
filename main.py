ee106f7c65be54abe0cc722be09383e5052eec0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:35:15 +0000

    little wander table opposite exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
