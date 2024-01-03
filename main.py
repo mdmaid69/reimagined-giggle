047094a832229c556409532af92e5386737c6555
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:15 +0000

    draw prize city on really

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(5):
-        print(i)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
