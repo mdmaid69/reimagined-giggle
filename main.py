56f7f48169ab47fe0a90fb8ca0bfd2554fa4caab
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:32:42 +0000

    reach keepfrom around correct exist

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import sys
+def exit_program():
+        sys.exit()
