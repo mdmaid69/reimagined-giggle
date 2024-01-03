d68330e4eee1e237aa3674aa91987cf64f25b8c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:09:32 +0000

    search nor opposite goup disturbing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
