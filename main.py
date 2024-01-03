049b22bf2538ab317f4f86cb4cf85b3dd0aeed2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:33:59 +0000

    habit satisfied responsibility absolutely sport

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
