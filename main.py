a250a5b5c25583184419c1cec2ba87718d520c59
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:45:05 +0000

    advise season live deliberately tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
