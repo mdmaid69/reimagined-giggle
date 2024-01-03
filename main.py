1ef60d2cc5166ada5dd42e5ed83e1ad74baab860
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:44:08 +0000

    law fit arm discuss other

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print([x**2 for x in range(10)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
