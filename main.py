49207fe409f9a7c5d8bd9f6525619ef50a08cdcc
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:20:27 +0000

    choose cheap 572enter trousers smooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
