4379417785e22cb64907afca603cac72ad87a1ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:47:20 +0000

    moment appear work neither later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_sign(x):
+        return math.copysign(1, x)
