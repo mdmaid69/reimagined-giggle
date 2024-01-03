aab520dd665f71041c4494b6913312f3a4fdaec8
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:29:14 +0000

    effect teach undo move move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_typecode(array):
+        return array.typecode
