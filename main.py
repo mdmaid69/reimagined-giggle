1c1b5899343265d021468c30d2967ac254a30ab6
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:22:29 +0000

    mainly print interfere scream move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
