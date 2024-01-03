f8b7d6913a8bc3160a4c77a9be3e08ff71b77a8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:05:04 +0000

    repeat provide tidy interest appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
