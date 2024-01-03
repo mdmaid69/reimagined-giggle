2149da13e618a515d06b4b237b6a81ef99309fff
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:48:20 +0000

    include harm extremely guard trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
