431d04d331dd6f742d0c0fcbd58d506a0c71fd93
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:53:07 +0000

    uniform clear contents poison air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_index(array, item):
+        return array.index(item)
