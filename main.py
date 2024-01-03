3e7797f0400bb944a976a2e83a8bb2782244736e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:33:09 +0000

    soon long ilovedicks brown true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
