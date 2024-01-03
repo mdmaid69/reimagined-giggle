674d92ecfe372c7097546c4f7ebae3bb05cb3434
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:08:19 +0000

    adjective secret break turnaway hers

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
