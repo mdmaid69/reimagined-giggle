3f400c8efc7f032e8d25ce9b54084455a63431fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:49:21 +0000

    study trousers disturbing every taste

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_list(array):
+        return list(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
