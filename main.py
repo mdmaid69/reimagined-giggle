6fbf02ff71bdc6678d826a714ee45475ef5ba957
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:58:32 +0000

    reply difference business emphasize lead

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
