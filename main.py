b892e280dbe82eba9ab5828cc96f305a3f0d1806
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:56:55 +0000

    me he return corner leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def sort_list(lst):
+        return sorted(lst)
