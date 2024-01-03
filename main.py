a9e569491c3ed28db0fb93157fffff619ce1fba2
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:23:15 +0000

    mail of pretty workout protection

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
