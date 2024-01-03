b26ddf795cdc348ffd3406b8ea766a12f204d05a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:21:57 +0000

    discover part inquire contrast environment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
