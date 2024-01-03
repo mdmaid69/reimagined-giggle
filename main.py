a2a1c274328301820744846d298e8cfa942cc879
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:58:10 +0000

    book comefrom official cross stone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
