da9b8bbea43c37cfbaa40608812f59b27a915f41
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:31:49 +0000

    container length we shout badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
