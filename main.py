f67477103bf4aeadd41244fde840b571e0209d91
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:34:39 +0000

    turn last basically breakup rob

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-for i in range(10): print(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
