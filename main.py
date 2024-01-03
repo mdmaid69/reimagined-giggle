e048731e1d662165ead69c78ae9d14b62a9a6595
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:27:48 +0000

    start science whatever container surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
