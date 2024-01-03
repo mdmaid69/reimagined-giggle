26d0af03d476083318ecf760c6f44763cc804e05
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:22:06 +0000

    thankyou shop autumn building fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
