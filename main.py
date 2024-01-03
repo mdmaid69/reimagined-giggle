4c113eef8cc662145ffd9fa4119af54beac6c3e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:28 +0000

    against create lookup intend we

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
