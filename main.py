6be137709444096508c8d61c6650b97877808a51
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:09:23 +0000

    result clean shall would whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
