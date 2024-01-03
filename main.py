9f20975aca0c747469a855e2ace3828a66c29c93
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:33:31 +0000

    every base story leg spread

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_rectangle(l, w):
+        return l * w
