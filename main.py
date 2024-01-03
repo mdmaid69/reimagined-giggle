6957ff2fbab18d399a31b5c84d066eef82ce1cbb
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:31:09 +0000

    dare excellent performance sleep tongue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
