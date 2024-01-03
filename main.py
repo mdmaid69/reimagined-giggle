20537dfbfeed4532470d663ded9b2381967a6de2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:08:14 +0000

    programme shock early tend pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def reverse_string(s):
-        return s[::-1]
+def calculate_exponential(x):
+        return math.exp(x)
