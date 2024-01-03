c719c52d55242dc27fd679d94e3a362958184a78
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:53:08 +0000

    Qq curious company introduction cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+x = 10
+y = 20
+print("Sum:", x + y)
