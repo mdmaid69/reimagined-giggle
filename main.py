b30c2eb270db4607646660ed082494ab1c7fecc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:30:05 +0000

    mail harm during sweet petrol

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def multiply_numbers(x, y):
+        return x * y
