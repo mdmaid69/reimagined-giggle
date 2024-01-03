f7618f1ce2aa39c7632dd8fc8b0152d568e42bca
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:20:19 +0000

    since Jj lookafter succeed unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
