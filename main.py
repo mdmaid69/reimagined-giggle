b0cf4c3609e8494df3f889836e5ab823732eec68
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:08:20 +0000

    part remember suggest violence instrument

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def is_even(n):
+        return n % 2 == 0
