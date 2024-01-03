90258695bf7151cc93c7eb7441555c3b695b2150
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:03:22 +0000

    true late look wind confusing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def add_numbers(a, b):
-        return a + b
+import time
+def get_current_time():
+        return time.ctime()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
