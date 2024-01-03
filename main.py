b70a5979da4c418fb76912c6051d021e55efaf78
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:30:28 +0000

    cost into family ilovedicks come

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
