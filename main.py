a37a0d587550285f6aff219a6e84d963b25efb2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:51 +0000

    guess emphasis learn arrive speech

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def sort_list(lst):
+        return sorted(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
