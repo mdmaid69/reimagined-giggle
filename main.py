375ae5dd006e3cc3970876ab2715466da1351d7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:19:19 +0000

    well protest fasten themselves so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+print(datetime.datetime.now())
