64ed538affc19a2cfc69bdb7bb29d5ade85ee500
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:27:30 +0000

    nearly circumstance moon trust female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
