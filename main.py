11e7456fb5d20a3955fe648434b3d46b713e31c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:10:49 +0000

    even tell strength take check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
