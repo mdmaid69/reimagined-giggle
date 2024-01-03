cb87fc456e6d0d6b5982c0eed3b61427c03e9f34
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:27:57 +0000

    worry recognize replace one disagree

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
