e51d43cc671452cbacc72299038ceb8118e2fc34
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:14:37 +0000

    natural scare comment until word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def square_number(x):
+        return x**2
 import os
 def get_current_working_directory():
         return os.getcwd()
