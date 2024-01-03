746cd548936989c4535201e933827a8ca93dd32c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:19:07 +0000

    so benefit born answer you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
