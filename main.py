2926a8d91204e4e7131e8a07c0f65dd60b4e7d05
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:16:47 +0000

    start hurry criticism day stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
