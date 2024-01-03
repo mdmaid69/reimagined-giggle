7e6cd5f7bc0c747b44b712b6ed1a36920ae77e26
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:33:10 +0000

    backwards responsibility direction keep watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
