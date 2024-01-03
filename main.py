d1cd9ab254e8a463cbf4c141312dd8418bf784e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:05:54 +0000

    567enemy solve holiday advice lead

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
