445953d75486a80e0a3a7ea9aa180ecd89c17dab
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:01:54 +0000

    gate used group holdon wonder

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def square_number(x):
+        return x**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
