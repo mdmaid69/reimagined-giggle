491080b02db3cb1caf132e04302a9186463d231c
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:14:59 +0000

    hard cap holdout sink pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
