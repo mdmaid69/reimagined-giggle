8c089abd5a8601a6b34d5a88669db55caa1d42a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:18:34 +0000

    touch table agree spring surprising

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
