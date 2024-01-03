2f12b05ce79ffaf2da0aae828031abc105a836f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:54:21 +0000

    bank wind either eager comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
