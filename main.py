4a811889e4e1964e0768674aef0ed683b5dd37c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:50:21 +0000

    year shadow win bringup brother

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def convert_to_hex(n):
-        return hex(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
