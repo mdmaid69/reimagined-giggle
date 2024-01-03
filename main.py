7cdc88071422fd20c4877c2bc93ae9ce5f9004a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:24:10 +0000

    profit life cost at honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
