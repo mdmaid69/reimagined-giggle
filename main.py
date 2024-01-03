630fe4ace3cf2a587b53b2968d311252e8d1e928
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:47:11 +0000

    suffer compete already direct bury

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import array
-def clear_array(array):
-        array *= 0
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import collections
+def create_user_list():
+        return collections.UserList()
