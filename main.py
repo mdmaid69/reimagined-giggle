ab17c9e3499dca97d33e2b0d26a1375e0a053601
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:21:21 +0000

    brain earth frequent dress cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
