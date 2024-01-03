19a86f4ec5ddf2512b06bbec12a79ea69ff41515
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:39:55 +0000

    wing thorough blame window tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
