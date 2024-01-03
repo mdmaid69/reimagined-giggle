251ff2df8b04f3f88cb512e11cfe279db233c40a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:27:57 +0000

    listenfor condition represent long judge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def square_number(x):
-        return x**2
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_itemsize(array):
+        return array.itemsize
