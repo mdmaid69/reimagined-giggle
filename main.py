7643146a4623366bfffeefed32143d3aa1209653
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:13:10 +0000

    air train run nose second

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
