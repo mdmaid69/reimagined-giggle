0e37a004f764a0f00b09ec81cd0104c1b7eff9fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:16:35 +0000

    much prefer typical pipe produce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_binary(n):
-        return bin(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
