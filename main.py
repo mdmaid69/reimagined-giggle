4dfdf83b3d845e633c5e3433bca8281af277f1a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:13:08 +0000

    cheque comfortable shrink hold hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
