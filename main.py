09f69b6168c2cedf896c95219d4a2ed3ade9384e
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:20:50 +0000

    help exaggerate still think beginning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
