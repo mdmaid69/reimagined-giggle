76d23a1a83abee9126496b81b394c74b7c075929
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:23:59 +0000

    tour sell afterwards boil nose

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print([x**2 for x in range(10)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
