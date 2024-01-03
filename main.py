bc64ea48efce214d25413d0bcf6f681a77001734
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:19:17 +0000

    clue distant shop instead true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
