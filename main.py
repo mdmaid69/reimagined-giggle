b1aa9608be47f4792e43536f294417374bed3d15
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:55 +0000

    post study themselves character available

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
