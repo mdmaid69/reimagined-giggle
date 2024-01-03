bc8ff7bbbddabaefe09206ad655fcffc226ba842
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:36:43 +0000

    extra happen morning breakout thinkof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
