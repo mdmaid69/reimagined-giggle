12a7fd033c8ebefdaa46883b0fe5372d852aff82
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:45:26 +0000

    steep conversation fall prefer pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
