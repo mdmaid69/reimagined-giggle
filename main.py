c25551f2ff02c137682d44ce3579d0d7dab47c14
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:11:42 +0000

    careless solve stone involve phone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def reverse_string(s):
+        return s[::-1]
