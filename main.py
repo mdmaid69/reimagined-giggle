ea92da24a13529b7deee47290841bd5e689c0200
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:02:27 +0000

    whistle date after people before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
