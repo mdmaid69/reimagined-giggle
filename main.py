7c14d54be2e8b7aa7e2745ed98883863a1cd9038
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:08:02 +0000

    rare otherwise ever makeinto Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
