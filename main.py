b4e76d540767a70da318b4aa7067d9cca52fb4aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:02:13 +0000

    continue door apply several pair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
