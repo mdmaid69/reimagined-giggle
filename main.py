4d558b88c927c36e82d9dee7827de23ad0e62ea0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:51:20 +0000

    rich bus hand appeal control

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
