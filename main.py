2ad2108065446be3c6a014158ae2354509bb1d05
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:16:31 +0000

    account break manner drop opponent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_odd(n):
+        return n % 2 != 0
