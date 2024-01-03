ef272fe8f9ac34f737b10e2972994924e2fc0702
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:35:57 +0000

    cough home reason need scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
