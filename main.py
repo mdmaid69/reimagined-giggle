413bb846b76bc0d588d2304cf7d51eef3c4b09f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:15:59 +0000

    trip glad decide later occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-for i in range(5):
-        print(i)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
