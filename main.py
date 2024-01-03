f3431943294b8f907c0417e2b9cb60b52134076d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:13:49 +0000

    invite demonstrate dress real almost

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def find_min(numbers):
-        return min(numbers)
+import array
+def get_array_length(array):
+        return len(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
