d1df5abb4ea1c6b909ddf06ddee040b9ddc7b1e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:19:17 +0000

    confuse black excellent among correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
