fb4f5adb7bc29569a361aef16e5e91f50abf06f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:37:39 +0000

    few equal new cry muscle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
