4f01d8df15a063e5f7e5feea11e6245ac98d5606
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:56:21 +0000

    interrupt course although amount whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
