7b439bd1bbeb980a3d89e8ce15e33f5fad67f67e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:53:28 +0000

    unkind priority stiff tone friend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
