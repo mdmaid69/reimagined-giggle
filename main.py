4e4c9edf3891a8ce228b77708820264d60a0043b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:59:47 +0000

    neither attention matter independent straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
