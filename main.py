c5bea033b78ee43ed515cd2c170239e557649b0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:56:06 +0000

    blue dance getup scare weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
