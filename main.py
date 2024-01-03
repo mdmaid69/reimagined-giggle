ceaf1bd60a75f9fdfaada4fed1a20ec782a2ce78
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:49:04 +0000

    range yet organization test police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
