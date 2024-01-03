5b72d7926563dc8c4e159a70f21b2cb8d8b26739
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:31 +0000

    wealth belong brain godown language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import array
-def get_array_as_set(array):
-        return set(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
