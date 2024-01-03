6b31d2514206a99fef5b0d6745cee4d162386b6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:49:28 +0000

    suggestion future callback environment suggestion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
