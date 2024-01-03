4628f834392680fd2e610d7bda8be7b8d9fef715
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:07:41 +0000

    Oo door bringdown turnaway stand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
