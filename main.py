e7608f12e7b06c1fbcbccc94e534c32a76b47792
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:59:07 +0000

    breakthrough confuse final club top

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
