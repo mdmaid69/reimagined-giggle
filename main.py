18dc76df85f60ad6d15df9d47160cd3699e2a189
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:24:13 +0000

    dangerous pinch figure yours fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import collections
+def create_priority_queue():
+        return collections.deque()
