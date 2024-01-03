63af131daf0da3f22be4b03236418eeae9dd387f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:06:04 +0000

    threaten none cap hold acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def sort_numbers(numbers):
-        return sorted(numbers)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
