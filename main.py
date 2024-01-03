ef951a5346b215b55a6a0d56a657b43b215d1431
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:36:32 +0000

    protest slice sing miss estimate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
