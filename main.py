c0c956da585f342c8e717a3e589b00894225574e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:38:42 +0000

    true use insist guilty preparation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
