711b8f5ab06d2acef95c04b14657bce480aa0e0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:32:34 +0000

    man professional steer about deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
