746b4c4ef33246a6c45a0cc4714f04d54c3397c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:46:52 +0000

    bar switch wave anyone 567enemy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+name = "Python"
+print("Hello,", name)
