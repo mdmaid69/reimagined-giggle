321cb5c7e8e694d0127dfe648b59c7ae58237d54
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:11:51 +0000

    cross present school neither well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
