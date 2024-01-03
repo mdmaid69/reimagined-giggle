5edcde40f9319e6482a5a35b6bd28ed21b411264
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:23:08 +0000

    ability no problem mass model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import time
-def get_time_since_epoch():
-        return time.time()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
