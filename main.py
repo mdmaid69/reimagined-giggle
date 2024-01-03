d65dccbe4c3d09d56c63f37c1500fd56b111c23c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:17:21 +0000

    keepfrom upstairs Pp study better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def split_path(path):
+        return os.path.split(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
