40ba005c613bc0e4a813c87a0b000097a6337d8c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:05:22 +0000

    declare subject guard occur mass

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
