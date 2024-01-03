80ae382b68674885cf079447ab00a24dfcbac8aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:13 +0000

    of regard still past class

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
