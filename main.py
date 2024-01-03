a00b5b4e4ca57ad161d5fac6d020591c41980497
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:13:43 +0000

    explain culture tool approve although

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
