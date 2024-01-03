d9d2f3d4a407f692752634dcdbc2f1b69055c399
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:13:52 +0000

    wet recently sensitive sign path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import time
+def get_current_time():
+        return time.time()
