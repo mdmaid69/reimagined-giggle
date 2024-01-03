f3ed540178cd4b5e47d4a1c64624fc7cd2ab5797
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:31:45 +0000

    major Rr tap together male

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
