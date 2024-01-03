aeb51e883bb33eb4f039b3cc9130c340fbc73e54
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:29 +0000

    brave gift belong wake wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
