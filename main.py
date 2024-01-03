bd9839a55164d4a5abe8f8f9bfe2f46557778e82
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:57:21 +0000

    cut anxious keepout than active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
