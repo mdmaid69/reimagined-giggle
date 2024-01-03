6c9e083d63b3d2fb2e23e623444dbd8331c71a95
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:19:17 +0000

    part hang wheel track in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
