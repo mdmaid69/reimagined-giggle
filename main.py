2fbd15919ca8d38bfa93743d2000d0eb8d787d91
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:00:19 +0000

    somehow talk return really nature

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
