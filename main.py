d4041e6264e320400afa2f9589bd99a885d9eefc
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:12:19 +0000

    unite loud preparation understand run

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
