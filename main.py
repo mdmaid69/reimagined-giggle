45767d9b808852cc3620826cb78e351574704bf7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:23:47 +0000

    off follow difference near tape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sys
+def print_python_version():
+        print(sys.version)
