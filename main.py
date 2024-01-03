4adfc05c61bcc68f94ca2219a1015c147582a53a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:56:56 +0000

    failure keep off tooth shrink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
