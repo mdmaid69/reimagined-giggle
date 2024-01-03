61cbc39a030ac5065815a5c6034c84372487fd17
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:48:03 +0000

    recent die defend quickly herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
