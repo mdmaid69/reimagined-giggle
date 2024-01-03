f8ecd681d7765b9b6d39c948661b9df5e93327b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:36:45 +0000

    teach influence experience might any

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
