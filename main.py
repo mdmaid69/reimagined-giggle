89af250c82598d9e0193f09ba823322798b8662c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:25:01 +0000

    talk immediately book crime runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
