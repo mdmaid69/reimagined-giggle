2911ad667111f0a32fbd789dcd0ff1964d0639dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:27:02 +0000

    guess by drawing cold skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-print(math.pi)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
