89ec41c2efe0ca6f1aba9f4a90c2414f2ec72104
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:06:30 +0000

    drive weekend trick question bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
