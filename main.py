c6b2c012c4daaa3e5f8ac08a249c2510752bd901
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:38:29 +0000

    ourselves Oo warning loud step

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-print([x**2 for x in range(10)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
