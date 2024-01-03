758e2c72890b08640529a7feb3f15c08650cfeb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:24:55 +0000

    floor similar recognize belong contact

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_speed(distance, time):
+        return distance / time
