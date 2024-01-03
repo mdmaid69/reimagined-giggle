39a0cbe15963573ab4e9dc8488458b0e8e9f37b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:53:55 +0000

    suckme steep instead flat business

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import random
+print(random.randint(0, 100))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
