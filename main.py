4fdf07dcbd1f742fb4b819091cd18ae155d127cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:23:29 +0000

    attitude murder divide butter develop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
