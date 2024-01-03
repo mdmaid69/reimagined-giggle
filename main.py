bec9eb4170f5b938f3fe0ee5d4d34a852cff0f47
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:04:56 +0000

    arrange exam pin maintain so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
