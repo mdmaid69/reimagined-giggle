d4f2d1259a6dc4540f5f536fd5c26562f042035a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:41:53 +0000

    destroy dead accidental open post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
