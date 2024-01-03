982c482d9938e2ddd402b20a3596f77b2548fc87
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:32:23 +0000

    conversation comein opposite deep study

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
