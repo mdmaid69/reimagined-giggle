4157c7e646e4c7cfcec7bc6ca659f7c5317fe35e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:07:02 +0000

    time talk powerful length shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
