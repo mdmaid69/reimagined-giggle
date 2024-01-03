77e37af28780dd0404429cc83a00fb5024cd61c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:26:50 +0000

    taste turnback sticky gradually connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+print(sys.version)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
