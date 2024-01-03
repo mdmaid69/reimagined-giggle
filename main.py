997c200f380e227fc440d1056173ce14e3142648
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:16:30 +0000

    plate gift store try perhaps

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import sys
+def print_python_version():
+        print(sys.version)
