a5dfdaa6ccf40210f3e8c6809987d74ee5558218
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:21:55 +0000

    start hold sight direct between

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
