e6c9b6931d1e61f62bffb1c3f37488609db651a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:35:10 +0000

    now fact me busy rub

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import numpy as np
+print(np.array([1, 2, 3]))
+import random
+print(random.randint(0, 100))
