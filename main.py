959aefa8b7b282231402fb860c1ff1ff2a1ffdaa
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:38:47 +0000

    duty mistake length let move

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import datetime
+def get_current_date():
+        return datetime.date.today()
