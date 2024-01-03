e0ffe187f6ea269e7db50aacfde7f4e7dfc592bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:31:23 +0000

    mistake sink bang innocent deep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
