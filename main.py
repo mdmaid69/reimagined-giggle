d75ea65667a6d4a79b8bb783d02585370bf75f4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:19:19 +0000

    snow heavy rescue somehow beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  def is_even(n):
+        return n % 2 == 0
