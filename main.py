bcb7fad6de0b1f9ffed32a99e15307e9a6dc27d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:05:15 +0000

    friendly mass soil night criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
