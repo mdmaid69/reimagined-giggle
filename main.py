19c855d31fd1599422ef7b314bcf8410359bf363
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:56:28 +0000

    deliberately himself inform worst mine

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_max(lst):
+        return max(lst)
