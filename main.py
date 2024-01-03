15be6cde8a823a9f854786fc88787629d62b3c2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:20:34 +0000

    course niggaacademy Ll report them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
