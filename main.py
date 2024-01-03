f99f149d9da6d78375594dd81dd88df91e24929d
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:20:21 +0000

    loss tradition feature rough mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
