cd74a3a8666199068d968ac921e9436501575129
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:24 +0000

    coffee important suppose actual apart

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
