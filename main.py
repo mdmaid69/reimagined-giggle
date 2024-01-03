53147092022e77fac14e0c5cf89cd4295b3e4238
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:04:58 +0000

    anywayanyhow wish formal without workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import os
+def change_working_directory(path):
+        os.chdir(path)
