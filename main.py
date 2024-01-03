cf25c7a55bcd5910d9853ef2532825fc4d1ce5e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:26:49 +0000

    foot little on lift preparation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
