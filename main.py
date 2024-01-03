52876026d7860751f426bc5f44a5c1dc29043b31
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:51:08 +0000

    wise on fault them description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_speed(distance, time):
+        return distance / time
