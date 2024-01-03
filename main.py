8797f0c434a71040f22a5954fc15281cc4461622
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:09:11 +0000

    autumn most mad sound on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
