5a5d13b6c028308f3121ecaabff9dc46ab551a89
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:31:17 +0000

    march twist reject fun love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
