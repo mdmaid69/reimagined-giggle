45e26125b7d6387a2c9d70d041ff1fd6fab6389e
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:15:32 +0000

    slice bin charge have day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
