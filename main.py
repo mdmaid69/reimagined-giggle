5b5d37d5abd95daab1db34dd4878f7a5bd343671
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:30:45 +0000

    remember advise call so mixture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def convert_to_binary(n):
+        return bin(n)
