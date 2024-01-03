a3c0fb9b1ee87ad9fe0000bc17d40723b2c817b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:43:50 +0000

    kill polite lift typical book

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
