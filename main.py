c6039fceb55a6c041935f18121ed0a4e3427927d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:47:17 +0000

    wise report skill her noisy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
