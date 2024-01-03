459e492bf8bd715d957a6b68796637ff5e5961a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:09:52 +0000

    print quarter live wide deep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
