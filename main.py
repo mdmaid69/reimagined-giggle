a27790faccc02b2e24dff91301741e287ab58763
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:26:09 +0000

    communicate protection minute appear scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
