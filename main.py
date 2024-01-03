677593b8698a3c9e466f1fb6e213a7d852dfb2ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:04:32 +0000

    bath tone respect afraid reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
