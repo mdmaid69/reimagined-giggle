3a4543ca520826f4c906dfd3042984bd49358ed4
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:35:27 +0000

    edge keen agreement early object

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
