b0bd0684d8c92dd74ca9cf70a40424d2439c5f57
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:21:19 +0000

    show drawing reduce sorry separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
