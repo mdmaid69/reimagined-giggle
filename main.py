ed08d5382e235aeaf802effb990f0511c988a2d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:32:52 +0000

    strike breakinto introduction attach lock

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
