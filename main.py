c720cdcae5c019386af73f7a6ab94000f4a7d528
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:46:20 +0000

    fill hers glass either second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
