b79fe8a074f3f06b99b810f10811457e2e46b235
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:39:47 +0000

    common march plenty off much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
