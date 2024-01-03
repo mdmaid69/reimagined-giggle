097118bb4d2f6b75ffe20249b95ffde25f1d420f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:21:08 +0000

    exist run shock father squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
