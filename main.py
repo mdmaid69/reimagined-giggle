9b3cb12d44bfd1adb662e8b67c055e8bf294ade0
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:57:55 +0000

    pronounce breakdown Dd patience strike

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
