4bb6a08c667aa056c8862eeaad95fbe3564a5c41
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:33:50 +0000

    side luck prefer excuse tall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
