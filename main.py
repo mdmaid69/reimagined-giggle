84201b0656a5c3de7ac628a0714f8dfa33e9d35d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:48:16 +0000

    brief score crime tread basis

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def sort_list(lst):
+        return sorted(lst)
