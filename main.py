328585cf69bb4ef42cd62afe605fd67cac629b12
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:55:45 +0000

    Rr blame succeed ring sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import sys
+def print_python_version():
+        print(sys.version)
