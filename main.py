f0789e8f09c2de261639aa176b1753c3ce97f80b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:42 +0000

    underneath pipe bet door runaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
-print(math.pi)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import getpass
+def get_username():
+        return getpass.getuser()
