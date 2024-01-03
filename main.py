204c970f9a80a4c85750e85084b5ddf758eb62d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:33:03 +0000

    around declare arm band raise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
