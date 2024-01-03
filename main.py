18a81217d7fce83472f9a3aedab0fd71387183a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:20:02 +0000

    very ask want miss that

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
