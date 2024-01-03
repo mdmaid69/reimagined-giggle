6a32040741e7afce13a40f62c80fd7bd30b578f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:33:05 +0000

    branch state high long order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
