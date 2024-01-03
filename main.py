afbe244d56e6969ab135ad1059665204578dbc73
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:35:31 +0000

    drawing recover ordinary understand charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def reverse_list(lst):
+        return lst[::-1]
