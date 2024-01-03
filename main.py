041754ba499a972484fc75a8933505dc72e4fc18
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:17:46 +0000

    cost television gift charge trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
