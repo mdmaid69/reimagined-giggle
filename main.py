01a1b0c6c79466ac6c5840f90bf1484fedc4d74b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:34:21 +0000

    definite contribute operate phone unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
