224921547bf74ccd0133d7e3e53dc4fdb368beca
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:01:03 +0000

    escape wire lucky storm bottom

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
