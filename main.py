f1eae56fb0878f4504f76ee68169ec2ed2e3f3c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:17:47 +0000

    its answer angry protest connect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
