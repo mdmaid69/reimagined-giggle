cbb70201149327edb797f7d37de21fdda40cdd76
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:59:32 +0000

    bite crash turnback definite profit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
