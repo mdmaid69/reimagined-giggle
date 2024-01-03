68cf887b7cd0a3a82344cae5bee4caae4e3e4cad
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:20:48 +0000

    shock basis cruelty platform pretty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
