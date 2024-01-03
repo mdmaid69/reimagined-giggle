d388e2f56605e597fd7eeec00c6d1a61cf4f5f7e
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:43:50 +0000

    drive dull curious lookup connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
