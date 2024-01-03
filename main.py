a5b28ae0286bec63ccf85ef281e30c7daebbb4b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:25:25 +0000

    reason treat praise hair news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
