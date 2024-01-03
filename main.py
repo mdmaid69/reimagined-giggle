120b8bbbfa74c744515d4c089f7823caca9203c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:54:24 +0000

    afraid everything instant situation face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-print(math.pi)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
