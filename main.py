9a46a8743c2b3109dbefc2829b6d4bea77e622f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:05:39 +0000

    impatient claim point chin soon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
