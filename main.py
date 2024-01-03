ccd4f1b9220888d0edf881cbf573d758ce382343
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:45:12 +0000

    wind expensive reply wish pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
