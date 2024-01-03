d47dbcde249cad61127b83ca2dc91cf529db6015
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:33:39 +0000

    narrow comfort alive afternoon loss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
