f571d9bdceb1dcf1da40a34ddeced88874b02b7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:35:59 +0000

    plant education discover have wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
