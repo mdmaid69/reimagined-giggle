e569808782dedb5a50a4b2b13cc074dea4ae7c1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:14:40 +0000

    impatient pay further judge pleased

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
