b2bb24e9dacb8dcc80d8879406c6deba6279bdef
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:21:06 +0000

    think win battery refuse mass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
