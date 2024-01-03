607f72e941e5856e4ea02c6e2e72a73a76eed5cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:45:26 +0000

    male door receive branch round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def reverse_array(array):
+        array.reverse()
