1ea1834864d10bb92c49c8bb366e84b40458e57e
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:51:20 +0000

    artificial wealth holdout fast point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
