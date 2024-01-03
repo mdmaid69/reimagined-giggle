b13f045089fabbd0524bfefbf95d3cca042c5bd9
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:45:22 +0000

    responsibility trick strong defeat laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
