4a4d4553e4ca8c5eb1f47be1e38accf2aed0130a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:24:45 +0000

    pause through identify wake hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
