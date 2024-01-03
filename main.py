408b9b42c27b0c76a37d07a03f5f2fb9e7ff4764
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:48:59 +0000

    cause family until completely niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_pressure(force, area):
-        return force / area
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_itemsize(array):
+        return array.itemsize
