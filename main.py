c1219595c0d846abeb33aef1b3fe1756c56d0ceb
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:32:10 +0000

    keepoff right new boring admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
