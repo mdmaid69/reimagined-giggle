5a3139cbf61800be33efa014778b069f0fd331a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:40:24 +0000

    beautiful blue outside present broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
