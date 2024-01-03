9dba64cf33f84abcbc56f72bc00c1fddead952f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:36:54 +0000

    too shout lesson abroad comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
