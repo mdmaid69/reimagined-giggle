758b996d4623021cf659d917b6e11698e32f6c3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:38:16 +0000

    feed apart circumstance skill drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
