06d62b11825f86c342193bfec778f2eff190ce2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:07 +0000

    division fence whatever object plate

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_work(force, distance):
+        return force * distance
