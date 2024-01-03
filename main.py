052893cfd7ce5ceb1dcb349aea88730318a256a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:56 +0000

    succeed already turnback announce complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-def is_palindrome(s):
-        return s == s[::-1]
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
