aca70d0bf83fa15984376a85d570ada8d6403aa5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:32 +0000

    area lay order profit past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_max(lst):
+        return max(lst)
