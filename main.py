59211a96d914904f6319061fab9a9c433cc620db
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:00:04 +0000

    pen find official shallow sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
