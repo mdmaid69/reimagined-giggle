31bbba0a3d1b3c8625c3a3b7644d594c1a337e6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:03:47 +0000

    close trouble great inside pour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def cube_number(x):
-        return x**3
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_set(array):
+        return set(array)
