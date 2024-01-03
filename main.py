b10930b85fc998d8b0446e554c20de1c147116dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:18:59 +0000

    absolutely cold breakup frighten force

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
