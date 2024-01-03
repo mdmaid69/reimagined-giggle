17014de9f012b264bb66de0f66e1cfafd2677986
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:16:11 +0000

    rather report bright home sail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
