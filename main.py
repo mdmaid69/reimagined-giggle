6b337cd577b00804520031ca029683b5f1566418
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:01:35 +0000

    dance shoulder might government admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
