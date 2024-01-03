a388ffc4c639d0973310ecb5eb53258f3a4fbccb
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:52:32 +0000

    equal vision valuable more unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_current_time():
-        return time.time()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
