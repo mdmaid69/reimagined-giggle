d4d72e9b65a66b139a4d28cf49b5d87dc75801b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:28:10 +0000

    protest afford 565end post style

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
