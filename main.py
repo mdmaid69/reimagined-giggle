1c837d78bb036613391e28e5fd7e4ad4e06380a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:57:05 +0000

    try square plan dark flavour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
