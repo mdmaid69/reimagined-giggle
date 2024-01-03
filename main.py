e9e6a5525e7d422b7557e7c1673498079185ba7e
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:52:33 +0000

    defend strike computer trousers of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+print(time.time())
