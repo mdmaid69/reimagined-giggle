aa46c3599e34c2b77b3cda95d57967526ef79b00
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:43:49 +0000

    unite necessary bar represent paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
