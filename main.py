4c076cc277696e49beb29e3e44df8542fb852553
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:57:16 +0000

    present door schedule yet gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
