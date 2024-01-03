b55f43d0254b3727eb96326568fede5a5f6bce41
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:33:51 +0000

    suffer freeze confidence identify commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
