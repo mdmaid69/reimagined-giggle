17455b9eb8adf0fec01e26769e93543ad4dffb9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:04 +0000

    post early lost forward arrive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
