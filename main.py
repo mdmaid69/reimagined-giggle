17ce7146eb5337f638b6738bb904b5e4d0ef92da
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:05:07 +0000

    concentrate far band sense murder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
