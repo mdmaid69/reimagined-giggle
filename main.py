807e9a5a1ff7024db9b70aae2e775b7beae5fe88
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:40:56 +0000

    yet port declaration sale mood

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
