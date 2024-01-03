fd019bc3f1243ee7c132e68e5ee87cd246ae9627
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:58:44 +0000

    partner intend owe let Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
