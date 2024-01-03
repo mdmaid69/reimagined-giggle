016a13d86f7af77af72d3392e06468aaf4a2794f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:04:54 +0000

    chief rain dead contain 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
