424ce79fc71cc18dd26001ab306bb70e81e5b20b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:35:03 +0000

    disgusting reason move boil dream

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
