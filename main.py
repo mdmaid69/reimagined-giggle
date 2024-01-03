7724de3e51c9b0ccb8493ba7db2bc3d6d3b41143
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:55:05 +0000

    crash need replace actually down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
