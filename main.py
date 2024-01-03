264a7d669c90b9e540e1cab97d223b51c5c31249
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:06:55 +0000

    bringforward record ago birth bread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
