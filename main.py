3e2bfabd5c8b91d381ec3a305a3ad7eb057e69c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:21:03 +0000

    stamp still succeed colour poison

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
