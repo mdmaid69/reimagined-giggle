0c96bff6a763b6d148d93e3a0c78eeedeb404db1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:58:32 +0000

    smart usually bin protect aware

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_variance(arr):
+        return np.var(arr)
+name = "Python"
+print("Hello,", name)
