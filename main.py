2afef71020a5b1a1a82c1b0cde9569f9bf32a143
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:37:14 +0000

    clear crazy far car makeinto

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
