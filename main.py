93f490c93ce3c410bc98375c3ff7db7c34d50340
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:35:37 +0000

    moon stop store pressure information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def get_array_as_set(array):
+        return set(array)
