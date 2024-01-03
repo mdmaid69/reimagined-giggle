0967215c11f5368bb0260895319e7cb6fde7b5a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:07:41 +0000

    any so trap offer professional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
