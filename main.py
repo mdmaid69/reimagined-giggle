d08d4e8bbfd15cfa53336ab7f2afecef04a87980
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:45:05 +0000

    tap instead waste meeting under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
