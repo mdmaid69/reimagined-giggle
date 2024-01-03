1786e8b9a5c65528bb2f76425695e9fee0906e24
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:38:23 +0000

    public definite reduce importance lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
