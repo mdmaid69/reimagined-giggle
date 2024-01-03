c5fd64ab47a80d876b24428d08528268d8c3d16f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:26:53 +0000

    wind rest secret sticky future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def find_min(lst):
+        return min(lst)
