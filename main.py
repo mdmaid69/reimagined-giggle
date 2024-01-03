339cddabd3b5460200e9c36084688bf047406d58
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:07:17 +0000

    dinner will moral believe flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_item(array, i):
+        return array[i]
