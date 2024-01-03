278a4bf87faf9e629a6147ce3013bb27487f664c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:08:52 +0000

    determined modern Bb mark dangerous

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+print([x**2 for x in range(10)])
