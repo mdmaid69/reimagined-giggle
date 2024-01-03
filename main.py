65348306abfadde7fc65f8dc17c1fd3e9afcd0f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:02:25 +0000

    speak house show straight public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
