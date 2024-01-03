db459eaa4cfde774cc8c98bb30d45bde5051e12f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:03:14 +0000

    neck light spring light comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
