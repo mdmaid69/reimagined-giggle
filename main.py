c6c8e3f9c183a8c36f469a666ee3a0b403769064
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:34:50 +0000

    object soak will steep Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
