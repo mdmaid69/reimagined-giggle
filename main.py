44f6e319f3b4300cb1452ac737d3c30c356d364d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:27:32 +0000

    harm punish lay prevent demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import sys
+def exit_program():
+        sys.exit()
+import numpy as np
+print(np.array([1, 2, 3]))
