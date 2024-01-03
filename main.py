4ee47a647246d6f1f582b54b5541485c551f9ebc
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:42:13 +0000

    grow generous land anyone nor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def add_numbers(x, y):
-        return x + y
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_item(array, i):
+        return array[i]
