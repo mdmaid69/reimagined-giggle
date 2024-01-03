fdfb75a630c9f181c43a6d00cbebdd8365141cea
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:34:31 +0000

    angry confident period quarter accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pressure(force, area):
+        return force / area
