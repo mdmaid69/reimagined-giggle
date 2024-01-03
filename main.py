0a67d322cada448c762556bcc7042f2db96ea4c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:15:01 +0000

    path waste steal closed call

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_as_set(array):
+        return set(array)
