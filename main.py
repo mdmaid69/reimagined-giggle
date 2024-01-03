92b132ccd1fea567c00010bd13f6f0c5f787612f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:33:33 +0000

    smile satisfied contrast treat read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
