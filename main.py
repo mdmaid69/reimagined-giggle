5b7fa4bdeed81a763edd105635d01f1c211e8ae6
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:05:41 +0000

    weekend disapprove direct knock hit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def clear_array(array):
+        array *= 0
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
