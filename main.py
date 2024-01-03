b2ab2a78666949d19df122172f5ac8a656716a84
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:36:23 +0000

    may where fat crowd reserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def add_numbers(x, y):
+        return x + y
