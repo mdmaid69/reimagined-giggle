a231d3f9d266c39ea77ead9bf12fe58f3404227f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:25:50 +0000

    object subject keen tax realize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import time
-print(time.time())
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
