40f0994b113284fc78f4445d2c29166dadffea57
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:14:31 +0000

    hang artificial total class me

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
