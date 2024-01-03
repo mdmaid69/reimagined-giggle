8e313fe8632282588aa600b282f83992dab907a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:46:21 +0000

    fly doubt lock round shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def clear_array(array):
+        array *= 0
