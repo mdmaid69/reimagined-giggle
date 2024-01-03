89f90a885ac750629d203ae20483c83b2b80a254
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:44:02 +0000

    rank comeover description climb later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_permutations(n, k):
+        return math.perm(n, k)
