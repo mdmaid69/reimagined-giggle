ab4a082a223b6de2ad296ae9a3612e7089a47822
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:51:37 +0000

    pattern urgent report prevent disagree

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def clear_array(array):
+        array *= 0
+import time
+def get_current_time():
+        return time.ctime()
