4baa61113312bab96fad1ffd2b9a1fe203fcbb9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:50:46 +0000

    tone very though serious pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
