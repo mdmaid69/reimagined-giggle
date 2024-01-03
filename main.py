cf1d98976d2e183da5283d0879be5652a062aa52
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:36:04 +0000

    thick wave brush kick benefit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def square_number(x):
-        return x**2
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
