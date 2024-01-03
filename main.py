4dcb8b293a06c01f4dd15fa9795c9db8f1cecc17
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:01:37 +0000

    achieve truth plan autumn pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import random
+print(random.randint(0, 100))
