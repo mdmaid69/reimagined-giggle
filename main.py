82d3a5bf6673b04e6dabfcf0febdd45fc1c1fb64
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:37:29 +0000

    himself recent imagine live badly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
