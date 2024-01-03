dac371e62f5ecff58021fbdfecca4022bb926c9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:44:24 +0000

    choice project talk permission ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_item(array, i):
+        return array[i]
