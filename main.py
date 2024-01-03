5fdfd428aa122cd53f676fdf6472e910c9ff7062
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:04:53 +0000

    official declaration raw upstairs share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
