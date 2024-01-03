5426baf39761d54616472f5a95f1b03ddf934f8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:14:20 +0000

    valuable past test official sign

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def reverse_list(lst):
-        return lst[::-1]
+def find_max(lst):
+        return max(lst)
+name = "Python"
+print("Hello,", name)
