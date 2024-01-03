1bb24a65677ded756eb260c69eff7c03934d2448
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:55:57 +0000

    oil neither upstairs sell prefer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
