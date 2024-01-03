c5b2867ec7fe5e695a03f35abbeea9dfcce9c07d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:22:45 +0000

    mess open would original home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
