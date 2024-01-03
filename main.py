61d259efe0ca63a70064aa0c8665440b65bca16d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:03:22 +0000

    performance force foreign road general

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def sort_numbers(numbers):
+        return sorted(numbers)
