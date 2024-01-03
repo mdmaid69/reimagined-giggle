6d4ab076d599379cc2722a8e658ddc3af75dc5af
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:18:20 +0000

    next exercise steep large ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
