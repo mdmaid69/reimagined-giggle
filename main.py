539501ee0b65404892cef462115b591272ec3ab7
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:55:54 +0000

    lay bridge flight arrive obvious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
