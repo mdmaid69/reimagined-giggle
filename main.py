018160582b7ab7851603b15e5f4ef589444fa697
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:51:09 +0000

    many certain our while search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def reverse_list(lst):
+        return lst[::-1]
