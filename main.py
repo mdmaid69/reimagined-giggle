a4cd8ff37731747eabd2bd60e4320436944f0573
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:18:47 +0000

    card peace bottom comefrom away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
