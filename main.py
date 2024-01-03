aab9302b7f1d943ff1cafc052a856be5b52726b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:19:27 +0000

    advice conclusion back answer temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
