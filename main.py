594ab917e67f9cbdc9dc69a65fc58e44f40513a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:51 +0000

    roll look yet lend on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
