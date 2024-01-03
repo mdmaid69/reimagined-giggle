9c39b295c8222b2afb7ac04a997b73b08d2db17f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:45:02 +0000

    harm bunch turnaway butter demand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def count_words(sentence):
+        return len(sentence.split())
