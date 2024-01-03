2fc9a9331556c1428eabc8f5428d2249cbf43dda
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:54:30 +0000

    occasional its undo shoot distance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
