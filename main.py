f5af4c5042b75d2c117def227c1cb26d0dd8632f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:19:09 +0000

    bank language disgusting match only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
