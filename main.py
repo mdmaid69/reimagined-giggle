70c95d2af5f04fd58ab40db0332e2871d7470c2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:09:37 +0000

    pause water grateful hard habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import sys
+print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
