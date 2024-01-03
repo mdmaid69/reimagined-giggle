a15c28c701d0751ca4c76374d049465073db5442
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:53 +0000

    turnout general cut comeover responsible

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def sort_numbers(numbers):
+        return sorted(numbers)
