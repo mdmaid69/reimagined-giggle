1c2d47f36158711855217946d7c78148fd411f31
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:17:49 +0000

    deep pinch afraid cool draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+def get_today_date():
+        return datetime.date.today()
