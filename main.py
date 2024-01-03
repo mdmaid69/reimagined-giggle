5e5890ee6d4b9893ef941ff0494c695d34d18d8c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:51:43 +0000

    search summer score thinkof wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
