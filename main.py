5a05255509b4b29d1fa8c1bed82d236a15e9abb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:31:42 +0000

    foot answer check will reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
