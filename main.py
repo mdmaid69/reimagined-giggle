8e24ec8ffd7cf8f6bffe96d70168e115993611e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:14 +0000

    up welcome likely national fierce

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
