89668e43cdbc88b67f81e1b8634ee364ef6cd166
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:50:54 +0000

    strict moral onto thumb custom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import sys
+  def get_python_version():
+        return sys.version
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
