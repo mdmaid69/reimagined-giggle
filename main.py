fc66f449b96fd03806c47b73fe192d6020f8ad4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:53:38 +0000

    fine half keep pick disturb

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_density(mass, volume):
-        return mass / volume
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
