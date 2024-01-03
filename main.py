f284036e234046d83d792cc5b25e75808a220f9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:59:32 +0000

    swallow plain turnup themselves themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
