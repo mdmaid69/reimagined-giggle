fb22c5d70aaaaae13621a460fcb3cc7cbfea1d78
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:40:47 +0000

    mood invitation popular drop with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
