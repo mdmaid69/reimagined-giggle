2cbfda884dbdc24e4b240cc5f7052292280c4344
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:17 +0000

    shout apply test none thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
