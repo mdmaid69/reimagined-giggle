90a9dd25fa6b1740dfdd7e20f7348f410e8076e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:40:59 +0000

    pay change know negative hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
