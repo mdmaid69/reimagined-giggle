34803f6ecbabf12319030aa87cc42e10849569a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:00:56 +0000

    sore else block mistake thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
