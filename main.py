2efa5ffc2400152a76a52dfc3ddf122b4c0694d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:40 +0000

    unusual before lie up bush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
