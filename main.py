8fa29e9cdb69adc72e0422f5a9044023f09238e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:09:11 +0000

    slice their threaten certainly strength

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
