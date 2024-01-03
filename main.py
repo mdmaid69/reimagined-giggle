f29475679c8c9978b018883c6456beff24630400
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:09:05 +0000

    stamp in switch middle will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
