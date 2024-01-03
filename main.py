66a17ff0e6489973bfed167240331d8a4c728c85
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:34:55 +0000

    comeround zero dance react shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
