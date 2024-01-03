dc5c8866f4b196d7042801253712c4fef7ae8ec0
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:38:08 +0000

    practise once wind neat reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_current_time():
-        return time.ctime()
+  def get_current_working_directory():
+        return os.getcwd()
