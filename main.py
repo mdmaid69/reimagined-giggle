2b6593d2e333cfb9cedea5d2317f56f3c5908f75
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:29:21 +0000

    recovery damage performance turnback angry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_power(work, time):
+        return work / time
+def is_odd(n):
+        return n % 2 != 0
