559a951d322dd09159dcc11fd3990e5991dadfa5
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:24 +0000

    comeback discover bottom powerful strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_complex(array):
+        return complex(array[0])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
