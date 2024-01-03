b6ddecb9f07c23dcdb3e32a8186603f5c99e6637
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:16:29 +0000

    loss treat admit compete present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
