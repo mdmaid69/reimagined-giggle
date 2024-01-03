5a62c47a51ec2fdad354df15522cbd390be9341c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:52:29 +0000

    push steam comeup apply through

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
+def calculate_acceleration(speed, time):
+        return speed / time
