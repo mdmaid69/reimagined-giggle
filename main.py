9000ee2b28dba88addcfdebfb2f145ca74446ffe
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:12:08 +0000

    plan aware food needle peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
