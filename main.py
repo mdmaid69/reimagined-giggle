e18492f1ed74db959760ea693d692bc3c9468a7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:45:12 +0000

    money sweep thin best stretch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
