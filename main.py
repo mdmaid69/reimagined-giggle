166f329eaa01b4f01613d0f1967774a20b273c14
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:11:27 +0000

    subject anyone degree finish pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
