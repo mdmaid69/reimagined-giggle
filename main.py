82f3ff321296543e29b4da112a2c5b84749ac20e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:17:22 +0000

    cover narrow now deep instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
