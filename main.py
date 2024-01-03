ae8e2640207dcf515a7499244d2788e7c37d8e4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:08 +0000

    mess runout sign ring patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
