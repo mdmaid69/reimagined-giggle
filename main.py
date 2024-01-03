2cd1f170c1ce3bc129e2d09a70a8318db6bd4082
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:00 +0000

    impression secret imagine factory environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
