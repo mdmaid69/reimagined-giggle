8d1a08a5bff7b33ff342e240a61ada9a59632aeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:24:30 +0000

    height yet comfort zero estimate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
