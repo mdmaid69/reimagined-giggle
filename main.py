2db16ee84539dd459b6b40007752e1742c02fb9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:40:10 +0000

    something intend admire genuine call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
