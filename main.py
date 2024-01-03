0f16f8044e6be7091bf11b9014b19497b40e098e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:16:19 +0000

    away exercise air confused top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
