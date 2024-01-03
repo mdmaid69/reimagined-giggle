5a02349193eea92a5718905cb52e33922f10f246
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:55 +0000

    people ago couple false proper

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
