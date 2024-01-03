267487b3bbdc9528c5a2326552bd73e668ae5d75
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:53:45 +0000

    cool quarter throw compete anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
