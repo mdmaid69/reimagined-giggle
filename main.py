24411c36b5dde71710bdc43c2024f3b5605c4590
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:59:51 +0000

    make pencil you approval false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
