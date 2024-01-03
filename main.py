7151b7b37595af1af620c07af8799830feddab99
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:55:32 +0000

    advise proud develop seem crowded

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
