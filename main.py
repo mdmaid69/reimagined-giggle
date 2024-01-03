9cba3b0fd1f7db0ce36ffee38b0263c17a121f08
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:17:08 +0000

    show thinkover office careless lock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
