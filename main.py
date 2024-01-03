baa97545e9158f7d094808b15d47ef5aac5622d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:39:12 +0000

    judge argument joke catch people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
