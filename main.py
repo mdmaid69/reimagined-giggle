d8dfb20b3c1ce547cbf4605040a54b518bcb5674
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:28:33 +0000

    perfect borrow force comeover cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_octal(n):
-        return oct(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
