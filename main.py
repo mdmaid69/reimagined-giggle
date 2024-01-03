754d57fbc31e43289b8a71e4a7f92e411c33eaef
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:14:09 +0000

    change substance repeat almost book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def split_path(path):
+        return os.path.split(path)
