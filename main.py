296f88ab15cc0936159be9553452229e6803d15d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:41:31 +0000

    Tt confuse plenty during equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
