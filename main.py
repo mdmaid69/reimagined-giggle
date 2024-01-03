912313f5b73ec89f39489d5898fca32563fb54a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:53:27 +0000

    Yy consist shoulder gate quantity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def remove_from_array(array, item):
+        array.remove(item)
