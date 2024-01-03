4acd784241c12118f45b029705683a85b9569548
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:59:26 +0000

    computer unkind thinkof file town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
