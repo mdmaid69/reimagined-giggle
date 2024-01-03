39b0b01bd51259a0da59b8cae6a86c449a7abc84
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:21:06 +0000

    wall that can lie after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
