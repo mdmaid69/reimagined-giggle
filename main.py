fd7bf3cdf4daf2a2ed4290100de396dff3254e1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:43:35 +0000

    since degree turninto clue tired

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
