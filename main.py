17d5b60b0846637fe439ae45b43f9b9bef7067a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:04:30 +0000

    turnover on and patience rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def find_min(lst):
         return min(lst)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
