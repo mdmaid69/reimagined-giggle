c0f9cfa320749d536c0d9903e89c345b5edf88b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:16:30 +0000

    agree join love 179bite plenty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+print([x**2 for x in range(10)])
