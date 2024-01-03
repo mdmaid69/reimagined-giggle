0f5b0c3fefc7396bd5d28fc439ab68a48a46749a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:23:56 +0000

    poison nervous control comeround emergency

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def count_elements(lst):
-        return len(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
