1440aa92e6d11deff8b0b81b837443bb31a93e30
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:51:29 +0000

    bush attempt them whom characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def convert_to_binary(n):
-        return bin(n)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
