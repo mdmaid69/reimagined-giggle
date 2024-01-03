9b71d0d4c40cfea9ede5271b2fddd0f1c8f2558e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:01 +0000

    progress cry share office offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
