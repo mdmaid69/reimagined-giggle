6ffdb5d3bfd1d4efef888baedf1614045918527b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:53:51 +0000

    contribute flood trap niggaacademy makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
