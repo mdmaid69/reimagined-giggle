9be1ce41d51f299d18418931d8280a41c038884d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:02 +0000

    road bother partner prepare accommodation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
