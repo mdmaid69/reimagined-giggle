072546350d9f741e20be49c660ddfe0eb5c87599
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:57:03 +0000

    power environment object comeup under

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def remove_duplicates(lst):
+        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
