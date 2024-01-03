234c438a6b33572c66c263fb3713c15d92144b3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:30:58 +0000

    uniform joke grand love general

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-print("Hello, world!")
-import threading
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import queue
 
-def print_hello():
-        print("Hello, world!")
+q = queue.Queue()
 
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
