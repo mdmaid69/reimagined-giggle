f3a3a6c5f59f0df36e1a74ed4af39ef63debad8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:08:46 +0000

    uncertain regard bush finish ring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
