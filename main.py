a9210afdc4d931be48fd20685d9cfab550b91ff0
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:20:15 +0000

    carry increase breakin expensive thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
