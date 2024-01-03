49749ca1570ff78ecdfe88fe159834dc206ffe94
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:38:56 +0000

    afraid hospital prove job keepout

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
