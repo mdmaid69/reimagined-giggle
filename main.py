31855faca3f68f89856a4db4d9ca88f153557d90
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:21:03 +0000

    zero print nearly force path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
