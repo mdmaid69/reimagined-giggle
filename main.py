2668efde7d6a6321f0bf232b2389e19b43351944
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:26:23 +0000

    point unit female follow talk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
