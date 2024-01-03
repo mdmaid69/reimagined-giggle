34a174af7f2f8ebdfa2bd045393552d448f7902d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:01:58 +0000

    contain relief cost ever spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import getpass
-def get_username():
-        return getpass.getuser()
+for i in range(5):
+        print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
