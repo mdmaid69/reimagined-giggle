585ce6171fe1e74df2628fa01c58bdc172b53599
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:15:03 +0000

    busy tune agree announce heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
