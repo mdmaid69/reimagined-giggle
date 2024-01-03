c3532f58d73f7b6752d6d35f252c836246a9e8da
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:05:54 +0000

    vote shadow shoot pure knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
