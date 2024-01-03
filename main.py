983f7aa19985007badd41f29ccb964adb53a1c5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:04 +0000

    kind sort thick unpleasant pen

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
