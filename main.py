2d685b37486355cf3ac61b12b3d4dd1ca37f2900
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:51 +0000

    priority sink better uncomfortable would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import time
+def get_current_time():
+        return time.ctime()
