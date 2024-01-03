941612471fb0d62ff385419c311e222480a73a75
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:24:11 +0000

    private lunch something world their

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-for i in range(10): print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
