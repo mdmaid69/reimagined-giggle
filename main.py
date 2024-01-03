078ea3499c33d11dcdb71edbbda9ace93f880f2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:34:36 +0000

    tax such material blame now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
