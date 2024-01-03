5eba8398747e09bae78fef10c2895b36bf41551f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:04:21 +0000

    during correction tense pride admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
