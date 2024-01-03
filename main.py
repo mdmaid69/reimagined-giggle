5399c4576a3a5207556ec9c6f5147731e16bcb42
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:02:32 +0000

    prefer anybody sad once dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(10): print(i)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
