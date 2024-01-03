f3e408140d411d346ac0d76e95f42cf35103aa1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:53:11 +0000

    lower rain agreement path finally

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+i = 0
+while i < 5:
+        print(i)
+        i += 1
