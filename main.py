dcfd87bf5cd222444dd35f2469561e7b5890fec7
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:09:31 +0000

    book public declaration interesting sink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
