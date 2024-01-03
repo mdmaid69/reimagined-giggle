86ff010ad77765a6ea27cbccf5220a176ab39ccb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:52:57 +0000

    black prefer family whistle interesting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_density(mass, volume):
+        return mass / volume
