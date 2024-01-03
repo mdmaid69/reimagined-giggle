ae988fab1a0fd1852db27ad7743a3a2cecb65ed6
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:29 +0000

    occasion stop remark slowly dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def cube_number(x):
-        return x**3
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
