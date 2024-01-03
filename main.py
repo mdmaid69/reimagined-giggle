82532eb57766325e484912cfe3feb6c9fae3ac80
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:37:50 +0000

    basic than light aim smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
