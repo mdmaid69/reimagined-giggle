86b728ae83942fca8f2267cf03512f842d31e7c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:18:14 +0000

    majority lunch match follow freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
