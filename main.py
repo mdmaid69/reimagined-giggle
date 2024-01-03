2cde7ea6e61bda1d7b600a540a7cf6d46748320b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:46:17 +0000

    obvious criticism turn lucky sleep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
