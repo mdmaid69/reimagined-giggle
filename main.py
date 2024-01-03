dbcf430e60e67c8bf25d0daa46a9920433456d40
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:01:39 +0000

    major dare touch colour direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
