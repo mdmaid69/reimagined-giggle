73371ae50217004f2154131dd4d2ec9f57353a4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:55:40 +0000

    part successful traffic holiday display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
