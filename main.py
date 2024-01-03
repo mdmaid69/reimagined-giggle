b5866379aaf9482158452155670f7a80d58c736e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:57:02 +0000

    visit field downstairs imagination pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
