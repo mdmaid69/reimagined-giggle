c0f06cc45c35cb1cdddf94c33142d9ce5740207b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:39:50 +0000

    godown likely doubt dance cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-name = "Python"
-print("Hello,", name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_length(array):
+        return len(array)
