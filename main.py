d41017e6a839b59c9bd7051ef38635ca5564286f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:05 +0000

    rest opinion pour murder fast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_list_from_array(array):
+        return array.tolist()
