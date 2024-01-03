5e623c0a6e363b780a5e1f37e7b6d0b59fd0f19a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:50:07 +0000

    ask official regular please herself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
