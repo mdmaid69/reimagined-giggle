f2564c8aabcc0ed08b06994acdfc6881e555b8b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:19:24 +0000

    breakin goaway view home invitation

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def convert_to_octal(n):
+        return oct(n)
