2f568a0f7a0a83f8910bbb65419a7f41d6b3d1af
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:10:58 +0000

    firm turnover any thing recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import collections
-def create_counter():
-        return collections.Counter()
-import numpy as np
-print(np.array([1, 2, 3]))
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
