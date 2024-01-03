785e6543915b1f2545a860109c2efaed5a3573f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:03:22 +0000

    put official towards clever between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
