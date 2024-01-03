1faa38bc2526d1d07d8f3e204289a1fd859bdef2
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:40:14 +0000

    action achievement manner stir interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+print(random.randint(0, 100))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
