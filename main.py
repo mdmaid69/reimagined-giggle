4efe4b49b4c52904177a0c488e7a9799d534c799
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:50:21 +0000

    open lucky part owe special

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
