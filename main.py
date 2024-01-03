be1dfeb61bf263a24a429b5da552de03a308b092
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:00:02 +0000

    even square through practise fortunate

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import sqlite3
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
