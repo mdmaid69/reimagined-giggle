d79369b9d8d5eb4a0da545c34d1a91d38b122a5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:18:42 +0000

    uponon excuse within single half

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
