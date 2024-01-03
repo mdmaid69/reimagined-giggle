26330b0643b5d8c77d6d57baee640ea9d611559a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:12:36 +0000

    world risk pay underneath sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
