d60bff08c41202ed1d877d64d151e565febb5e02
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:45:00 +0000

    greeting cheek slide hide dream

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def is_palindrome(s):
-        return s == s[::-1]
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
