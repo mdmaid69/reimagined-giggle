f7db7900c4c28afa994e48d8d4ae5dc8028c7d85
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:27:29 +0000

    substance cinema smile yesterday estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
