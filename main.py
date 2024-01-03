a3583ab85d2ab24f0ae0909ff2043f3ce0b27f86
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:01:03 +0000

    smile computer mind usual warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
