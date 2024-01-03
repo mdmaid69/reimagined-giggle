99888a9410356506bc82057696e3d4934d423548
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:37:07 +0000

    herself combine beside wipe test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
