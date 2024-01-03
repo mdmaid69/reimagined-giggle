cee6d1161d84a94ca3a99440259e3b16477f165f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:53:32 +0000

    week start nose sink rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
