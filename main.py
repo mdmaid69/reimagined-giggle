e630ebaa33513218cf1a64ab984a463345425fdc
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:21:09 +0000

    stupid reply temperature once result

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
