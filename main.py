f35dedfa27db1aebe9e37d1507ccb32076059a3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:19:17 +0000

    emphasis height natural brush question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+print("Average:", sum(numbers) / len(numbers))
