44ab9b04d554c8b7b6397d6b0c793eb70e6dbeef
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:31:49 +0000

    meanwhile here bang experiment sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
