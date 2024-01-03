e9356d023b8d673eccbdcc84b3b8dec4c998b205
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:35:36 +0000

    anyone theirs yesterday scene drawing

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
