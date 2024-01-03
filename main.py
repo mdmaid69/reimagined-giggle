612467450dc0cf882ef8c8029c3083417d5beae0
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:47:40 +0000

    male display extremely completely blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
