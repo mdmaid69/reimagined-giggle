83e977c9dea628d654aab5d0ff6cc03b37cef270
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:09:47 +0000

    impressed then round maybe with

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
