24bd1e32a4ab9ded234f517d8da0c4c9d890a6ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:02:24 +0000

    idea butter protest still may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
