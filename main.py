c321bfeb1b327578387bb1c243720a20639064bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:47:07 +0000

    butter badly approval creature comeover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
