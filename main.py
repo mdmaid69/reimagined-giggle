e8048d408fe76fc800b287458d5bea7ab88cc22c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:34:15 +0000

    welcome collect bunch likely female

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
