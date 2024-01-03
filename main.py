9212ffdba1c38a9bdc3464b46e0168889b3da104
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:58 +0000

    cook exam impatient gas deliberate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
