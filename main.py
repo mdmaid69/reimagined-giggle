ffdd75209b89e8625de07ed3bdc39a7443665895
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:09:55 +0000

    natural comeround calmly I sign

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
