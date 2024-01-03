ec5fdd1b6ca9a2628562e3b2fc89aefe48df8ce6
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:44:45 +0000

    thorough recovery must lookup wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def is_odd(n):
-        return n % 2 != 0
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
