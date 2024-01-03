0b1f4ff714fc4624dfcf66f2aaa700e4598fad87
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:00:10 +0000

    share on by stranger copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
