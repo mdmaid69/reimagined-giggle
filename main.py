6875a3160b6c357f61cd125bbced12cf894f31f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:10:15 +0000

    concerning promise control leg bet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
