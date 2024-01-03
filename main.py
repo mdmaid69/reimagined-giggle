4d9ae7b6616d1b2b3e8aa2c28447534f94f1b114
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:37:50 +0000

    friend too nature comfortable emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
