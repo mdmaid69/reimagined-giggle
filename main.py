ce9566173a2579765f4aa1ca8c0cf12590ce53ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:28:41 +0000

    flood behave whistle style form

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_work(force, distance):
+        return force * distance
