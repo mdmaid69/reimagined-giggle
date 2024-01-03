e8bdf889392a83195d965a930eab96a16a71b5b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:58:44 +0000

    kick remember nonsense exam answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_file(file_name):
+        os.remove(file_name)
