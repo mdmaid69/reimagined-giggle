5b62c31553479d3532ecd79954985a4062a8d2fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:34:19 +0000

    buy leg strength makeup familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_str(array):
+        return str(array)
