afa6f61564731aa87e1a7500b559faf2e8d71740
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:51:19 +0000

    total class noone exhibition answer

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_item(array, i):
+        return array[i]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
