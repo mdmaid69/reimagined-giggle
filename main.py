f580060f38057952cfff62252182b734bd00f3fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:36:44 +0000

    prefer complete work 567enemy experience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
