b3a6b35d49114d59673defaa3da3e55c1bc522a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:20 +0000

    slice job reject through sound

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
