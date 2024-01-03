29ec93896baef28c764ca962ccd585a7a1a4c9a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:19:14 +0000

    tie send shoot trade doctor

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def add_numbers(x, y):
+        return x + y
+  def remove_duplicates(lst):
+        return list(set(lst))
