cafc78df981cb0acfb0907dcf551c8c86c3f0d80
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:35:59 +0000

    upstairs wet communicate tip direction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
