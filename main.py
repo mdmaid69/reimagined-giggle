5d95e0e7fb4c42d1c697fc48386589bcb14958cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:00:01 +0000

    mine dull Uu point tooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
