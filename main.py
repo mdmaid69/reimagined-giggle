c6526a3fe928d10e9235b154918becb829903e8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:53 +0000

    eager trap health swallow dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
