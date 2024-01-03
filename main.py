6df0ae83c05cc4aa3ff4363820ffe912e20a1c52
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:02:45 +0000

    should sell trade half ever

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
