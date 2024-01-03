cfcc71dc00f58f4b38b9793b83c479b7ee71601d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:53:28 +0000

    somehow pencil pay over track

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
