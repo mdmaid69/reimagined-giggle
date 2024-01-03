1f857ec8f912a3f27f0d1a0a17561d19f2a7c810
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:10:24 +0000

    feed save 179bite dig form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
