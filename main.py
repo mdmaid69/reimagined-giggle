a02c2e8f589d6fba2e12a0c8c7b3df84cc1cfcde
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:56:59 +0000

    noisy keepaway cope pot their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_index(array, item):
+        return array.index(item)
