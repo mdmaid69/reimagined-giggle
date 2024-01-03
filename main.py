95b319e9806e5b995e8bea89f42b2b91257734f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:28:08 +0000

    ashamed to corner wrap subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
