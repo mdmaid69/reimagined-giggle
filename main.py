7b86ef098b20848e148409b570308edacd594a1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:32:36 +0000

    guilty at interested last at

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
