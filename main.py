a062e0b754d6c62f540833c3f6f1ab52edbd225c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:32:26 +0000

    tax shopping bet murder honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
