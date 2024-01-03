ad81bea8167ace1684c75f04e36aadf9ea40cb5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:34:39 +0000

    hit talk workupto pot yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
