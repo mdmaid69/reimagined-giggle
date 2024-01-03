5345954ba19c9b0cec72d9ee51a69d85ba7f517f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:57:07 +0000

    pause Bb recent exaggerate most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
