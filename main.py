cf81441fd3d15e2ba31468d439ccc8e31d2c1e44
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:00:43 +0000

    within variety neither he violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_area_rectangle(l, w):
+        return l * w
