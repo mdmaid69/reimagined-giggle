945d94240bf2e12a0fad59b1e8a10e2df0ca4fb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:50:53 +0000

    bang further supply cause busy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
