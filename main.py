a350093471d615939b6eee9a52ddec52ddf34ae3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:26:15 +0000

    finger wall beat behave calloff

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
