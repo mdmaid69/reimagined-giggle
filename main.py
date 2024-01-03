be5d3b84869c0087484a361fe5c4a3bdcf631930
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:36:31 +0000

    form lay sleep child nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
