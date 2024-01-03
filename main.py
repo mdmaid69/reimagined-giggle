f34d3cace74e171941a28b8ccb738d69183328c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:56:08 +0000

    scheme nigga such uniform weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
