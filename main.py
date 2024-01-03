dece2d338b214bcd25c9c95d2be078d4723da403
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:32:06 +0000

    careful mean weigh play intend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
