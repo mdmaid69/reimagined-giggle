ea26b2a72f9ca5400406242f4171210d2cbf82e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:49:14 +0000

    sail keen nature war count

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
