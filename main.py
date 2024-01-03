2c0baf27799b8dc8fa0c2b785397cffeb635302b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:16:54 +0000

    Hh pen blind room official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
