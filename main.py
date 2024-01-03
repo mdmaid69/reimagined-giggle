5fe22731207602b6d9d80aa33432fcb767862b61
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:27:25 +0000

    difficult teach cost least survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_rectangle(l, w):
+        return l * w
