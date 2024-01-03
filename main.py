482dec5070ad4c1dd9697f3449fbbca184fff917
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:26:22 +0000

    aim Ee possibility keepup neat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
