38660c7662f760b16f706f8734725d53d1ee4bf7
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:35:05 +0000

    already insist set home exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
