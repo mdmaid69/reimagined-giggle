99cac2cebb48e9c3051bc2fe36d9cc4d5bdff0f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:33:46 +0000

    comeround at sentence proper about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_typecode(array):
+        return array.typecode
