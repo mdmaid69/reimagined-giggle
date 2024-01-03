146a20193f48c4d62ceb35c902d7352c69b5d9f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:45:23 +0000

    total guard considering later unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
