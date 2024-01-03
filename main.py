0ee14e44ead3141bde0a23cf76d78a7212d79ba1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:41:08 +0000

    division simple connect achieve wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def remove_duplicates(lst):
+        return list(set(lst))
