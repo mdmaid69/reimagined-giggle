c56f6433173428f79f3967987d7255d19d311c7f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:02:28 +0000

    hour possible combination male evening

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_list(array):
+        return list(array)
