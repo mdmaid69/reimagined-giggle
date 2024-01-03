8fb51d22f9bacdc07178803c54b0c21cbd8c33f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:38:11 +0000

    match read charge unlucky completely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
