5d124d6f49ace5230df88ba51b199ec1764590a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:10:45 +0000

    before over book raise Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def change_working_directory(path):
+        os.chdir(path)
