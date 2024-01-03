3035bb913f60d3ebe10316bc5db4d9f5371b0b97
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:55 +0000

    demand perhaps rise combine best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_octal(n):
+        return oct(n)
