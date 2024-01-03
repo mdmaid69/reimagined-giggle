a17f26752060df2434b964bed6ba8eddc177652b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:41:18 +0000

    sense wave crowded dry feeling

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import sys
-def exit_program():
-        sys.exit()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_item(array, i):
+        return array[i]
