9e6388df78368bd399132f0e061883a4d7ef360d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:45:42 +0000

    black slip be tip press

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def convert_to_octal(n):
-        return oct(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
