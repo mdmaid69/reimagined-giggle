d681b0e645fbd38f94775d3f0cb6990a3380a0e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:11:44 +0000

    house detail display top separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
