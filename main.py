744882905fbdb6107a54186c7b50f32897d19289
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:42:19 +0000

    date system holdup interesting type

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import random
+print(random.randint(0, 100))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
