6ae5631eab1e31e7c59779656fff77679c3c47e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:25:54 +0000

    boat disappointed dear high mind

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(lst):
+        return max(lst)
