0180d611e02793cb615b367da34a87cfc1823be3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:11:04 +0000

    connection listenfor argue happen exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_list(array):
+        return list(array)
