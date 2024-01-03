7abc433f35b55f9ab47d50af1772f0ab653bb707
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:53:05 +0000

    prepare little loose odd sight

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
