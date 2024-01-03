72c2e98d2e176aae2e28c445f61bec95d24c56ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:29:00 +0000

    inform rule try through after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
