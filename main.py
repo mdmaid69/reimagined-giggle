51edf7de1d0157c64a62b048e6617af881213ca4
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:31:24 +0000

    scheme unlucky today final open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
