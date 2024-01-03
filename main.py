f02e92a12d166683b3ae5a63e8350bf2a789176b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:05:47 +0000

    idea stretch arrive light rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_current_directory():
+        return os.getcwd()
