d963758fdc1c950dca7d0aa73e56305df60377b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:36:02 +0000

    loud sense demand bang file

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import numpy as np
-print(np.array([1, 2, 3]))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
