40c9617ae6af8f2c3c4e5e67aa6a346d1efca5dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:35:43 +0000

    colour number towards practise after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
