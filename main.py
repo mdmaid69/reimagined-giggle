57e7f98202a9f33f32df5888d0f7c8fb2c61db50
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:27:36 +0000

    equipment false start school sorry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
