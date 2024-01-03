9c7da432cf3a69e1d88c7ed61a6a12c3ea52e288
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:11:54 +0000

    to crowded cough left mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
