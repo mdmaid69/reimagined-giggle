f9f637bcc25897c671bc1583652752f9ed880bf4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:21:16 +0000

    hard Cc survive speech taste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import platform
+def get_os_info():
+        return platform.uname()
