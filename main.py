47368c54b7bbf7fde697386b52ee9774060d0045
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:05:26 +0000

    education thought determined late case

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
