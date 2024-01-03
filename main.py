a05eae52bb9e09c43b7aa11c1e19c0b02ad8c60b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:56:36 +0000

    whom emphasis deal chase breathe

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
+print(sum(range(10)))
