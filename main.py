026a154ff186fd40bb9522af517499ab30381ad1
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:22:47 +0000

    skin compare proud fire cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
