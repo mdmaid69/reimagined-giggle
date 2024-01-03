7a375993d4dd79eec53d46e391873002dc0468dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:20:12 +0000

    part theirs education dare want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
