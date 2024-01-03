998dd4b094b5d4e5d2014e32bc73c9a86e55aa3f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:45:14 +0000

    warning guide turnback object concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
