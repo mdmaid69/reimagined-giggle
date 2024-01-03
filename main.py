d12ca1b1a5c94303fb53dd237b6437acf26d4abb
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:22:40 +0000

    relax drawing early sensible tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
