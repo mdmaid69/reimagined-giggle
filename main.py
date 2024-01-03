dc6216b374ced0f0c9fa320413242cfcb8a0fdf8
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:20:48 +0000

    other honest ignore measure foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
