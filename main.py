61669f8264a9feb3fca93f105fa8784978e4b834
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:25:21 +0000

    fear well square fly nor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import re
-print(re.match("h.*o", "hello world"))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
