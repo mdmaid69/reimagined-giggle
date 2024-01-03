bf0cd08b0795249e4f3498814ddaaa233148ea48
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:11:56 +0000

    replace measure smile unless geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
