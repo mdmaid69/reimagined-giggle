4fd6319ccd190a101f2bed7cfabe53b7b3311847
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:40:19 +0000

    dull demand convinced male giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
