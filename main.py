7b93ae046febfdeffe96f6a2f58923495cc7fae0
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:02:54 +0000

    transport visit inside middle beginning

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
