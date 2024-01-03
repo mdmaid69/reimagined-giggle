43fb36e56d4c82bdf27da2000f49c48ca39bf939
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:33:42 +0000

    recognize in right introduction shock

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
