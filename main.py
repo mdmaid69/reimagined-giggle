82834c739ad928d59ebe82a156ec86e18c0f1230
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:10:51 +0000

    nigga hard produce record accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
