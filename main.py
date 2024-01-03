c32728f0ef565083b8dd6e32f76369e041d57da6
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:48:49 +0000

    tough company cheap classroom far

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def sort_list(lst):
+        return sorted(lst)
