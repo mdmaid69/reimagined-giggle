2dcb01b9f2ab64e5981917846d9090accd07f108
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:18:32 +0000

    pause vote game room bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
