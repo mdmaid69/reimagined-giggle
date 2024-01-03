b6d8448f431f1c6ac0bccadadb948e206e19bf39
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:59:22 +0000

    murder pool recent war suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
