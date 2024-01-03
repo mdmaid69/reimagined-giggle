e30fe79e8b266905174c1bb6b4e2dbe679163845
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:54:30 +0000

    Ee consider pile finish agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+print(json.dumps({"name": "John", "age": 30}))
+import platform
+def get_os_info():
+        return platform.uname()
