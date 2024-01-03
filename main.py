57f125afef9bff70a5ee00401541d7dacfcedeac
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:35:39 +0000

    turndown connected of excited base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
