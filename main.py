1b6f3c6a6b82f05e2223e76c16fc9b3bd99fa554
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:28 +0000

    minor save powerful group take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
