67dc93a75793b873c39533975204d8807f82be32
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:57:32 +0000

    spring risk cause shoot sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pressure(force, area):
-        return force / area
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def subtract_numbers(x, y):
+        return x - y
