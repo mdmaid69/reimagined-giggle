81793f3d909a76ad4bde9f59dafc23f327b2048e
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:11:33 +0000

    vision defend impatient result whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+print([x**2 for x in range(10)])
