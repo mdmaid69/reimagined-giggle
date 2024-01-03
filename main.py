a5fa798e9b20428892ea413514165cc92e167a0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:29:47 +0000

    on copy tear bad argue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
