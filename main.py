92be14b26f637b8525acc7dcb44923962ef1889f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:19:33 +0000

    nigga main giveout fresh brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
