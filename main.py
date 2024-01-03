518294a74b9abd049a71628f6f1c56b7ea98c2b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:21 +0000

    on wave old disgusting mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_work(force, distance):
+        return force * distance
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
