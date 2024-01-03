124f03f3fd82837bf1d69ff24df892defbcad2ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:04:19 +0000

    shut noone bear scrape usually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
