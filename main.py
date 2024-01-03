2ca13242d901c0169ffff55ce1a6ffec53d4429c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:16:52 +0000

    tip chance patient attractive eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
