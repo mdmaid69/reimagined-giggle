cc0881ff366b552517cf92ad168baee70eb0099b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:25:59 +0000

    performance block under bringforward show

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print("Hello, world!")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
