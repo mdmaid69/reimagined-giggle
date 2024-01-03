52e14c1340fc148137785b0ff964dbbffe3ccddc
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:54:02 +0000

    fix many wealth pronunciation however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
