8b1159ca9c65c5f6cbf921059b53aa98d3c21643
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:32:25 +0000

    honest point listen trouble laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
