8405e983233d3b51eec472b45e7510052d67f680
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:13:21 +0000

    base block have trap contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
