9cdf54c4b18646495260c5cd2fce5ca6b2ff53e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:30:54 +0000

    size well typical cook otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
