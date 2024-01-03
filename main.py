d477b8c8c53f6af1b8c159f18c9a3bcc22e29a02
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:12:14 +0000

    wave everywhere represent rush yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
