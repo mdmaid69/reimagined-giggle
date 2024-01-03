ddb1ceaa6e7b53d7c97ff56d8bc699350a975e6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:47:58 +0000

    protest advanced consider offer Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
