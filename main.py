08e8f83b560380c8b1c11c48ea823db7101ac5eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:30:55 +0000

    range moral Ww home present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
