3af3a322229602f0189d1ba4e95bbc3f8ab3bd8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:09:16 +0000

    catch past godown curious importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
