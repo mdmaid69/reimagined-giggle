b97f61be9ad7d6333e3a951bb5de79baae433cc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:26:13 +0000

    believe begin sore deep beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
