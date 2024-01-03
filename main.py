d846e104e59b2ee3bebccc65c445066f026783b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:30:06 +0000

    lower it far detail smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
