4342e8e9de758070dddfb1021190d3c037343c39
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:36 +0000

    fine squeeze bar during character

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print(sum(range(10)))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
