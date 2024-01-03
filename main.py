5e06711bc12f50edea826df9692c969926139ffd
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:37:07 +0000

    conduct cap base target frightened

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
