b63aad6596c510fc04164c8b469eac3abe89a0bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:36:28 +0000

    innocent sting everything instead deliver

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
