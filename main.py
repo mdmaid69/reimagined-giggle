320ad337e62bf80a8db378ad467108f7966324a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:08:45 +0000

    often plant claim mean problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_typecode(array):
+        return array.typecode
