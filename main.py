688304147033531ab65fb04d72a7f5fe6e01f1c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:03:09 +0000

    paper taste make fun dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
