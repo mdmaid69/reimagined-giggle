4a1c7fbd27894e6e27bea0529e11b2e01a1a8189
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:33:57 +0000

    rely treatment profit file cheat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
+  def convert_to_binary(n):
+        return bin(n)
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
