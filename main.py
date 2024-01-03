4d913035119f4e7d41a1cebc2a9908dae94b68a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:02:59 +0000

    now responsible comedown gas trousers

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def square_number(x):
+        return x**2
+def reverse_string(s):
+        return s[::-1]
