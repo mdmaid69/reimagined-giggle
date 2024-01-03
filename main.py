79f4b0b5f575b79a3dca9f894b4cf64d8f0b1ce8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:20:54 +0000

    sweet load offend warm mention

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
