07e93a80a80c333bc2553984b6d66f85c52223a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:04:45 +0000

    pupil chin last spread appearance

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
