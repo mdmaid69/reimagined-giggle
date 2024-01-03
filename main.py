1f755264059e0ef4e00c895cfe8ce866e8614a9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:40:12 +0000

    566end will contribute school moral

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
