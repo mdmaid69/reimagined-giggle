1fb6e77510420667f4f9e76683d7b812370f54bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:52:01 +0000

    possible pity theirs separate aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def get_array_as_float(array):
+        return float(array[0])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
