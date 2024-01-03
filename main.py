2ed32cb996474ba2650e756f9e652d5a835332a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:56:46 +0000

    closely shopping ahead evidence ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def divide_numbers(x, y):
+        return x / y
