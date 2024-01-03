9ed6253495a6b1016ddbfa64926fcca0c263e855
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:33:18 +0000

    on wealth ride juice my

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def sort_list(lst):
+        return sorted(lst)
