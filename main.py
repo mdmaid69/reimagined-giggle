ab2f0f144e07682faf5f27c1ff34a63581b6c659
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:14:08 +0000

    neck impatient sign explain offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_pressure(force, area):
+        return force / area
+import array
+def convert_array_to_list(array):
+        return array.tolist()
