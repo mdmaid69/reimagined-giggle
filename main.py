312def032152c7a9edb986ee8f2d1a8ba50135b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:54 +0000

    fine talkoutof key thing traffic

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
