71c689f68b73ecf459c7fcde9cadd302ebc5b753
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:05:34 +0000

    Bb interview fortunately send dangerous

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+print(os.getcwd())
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
