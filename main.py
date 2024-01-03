b90e672a941b3e29863e2dc712d75e2757b7a850
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:11 +0000

    pencil sometimes me upwards possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
