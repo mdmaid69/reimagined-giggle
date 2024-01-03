c2ad2fd1400b47bd4aa4bd5ee5b55a776c3726bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:36:57 +0000

    church unable comfortable sudden admit

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
