ed98d99cb84f20c5a215eb6fdabb4276a988ea88
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:09:58 +0000

    behind ready fly introduction sell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
