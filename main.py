376e7e82a24d463025c1c0242d439bb5f3ed5ecd
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:37:20 +0000

    luck wish agreement opposite police

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
