3f01d40cea59b0465acc08a1156be0b19b178101
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:22:31 +0000

    car that delight communication light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def add_numbers(a, b):
+        return a + b
