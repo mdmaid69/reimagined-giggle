37a39d36f07cb927d4fe427f9567de233d7f1285
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:38:27 +0000

    task back party abroad record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
