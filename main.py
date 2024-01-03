b4d7849cbaf3310cf9cc5cfb6b831610d2b1acf7
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:53:48 +0000

    immediately little Ee each set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def cube_number(x):
-        return x**3
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
