ec5b41aa8daf627513b59a9932c0bccebd8e72d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:24:31 +0000

    prevent once set experience connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
