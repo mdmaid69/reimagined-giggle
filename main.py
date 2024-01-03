2a4013bc0bbb2b0c73415ccd2f86cb7cf8c2175c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:31:12 +0000

    neither interesting connection silent problem

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
