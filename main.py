6d369f0cc623a455e72d6a847ea1c7ce1b9fbe30
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:29:13 +0000

    break comeup eager weak house

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def clear_array(array):
-        array *= 0
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
