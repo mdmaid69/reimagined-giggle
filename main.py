0f24eb4a78a588201f5987124531a5770da89054
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:36:49 +0000

    comfortable point country gift marry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
