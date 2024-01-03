44b37ef5c6eeeae636f6e8a0e645920457c13f4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:57:49 +0000

    point schedule coat nor cross

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_string():
-        return collections.UserString()
+def count_elements(lst):
+        return len(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
