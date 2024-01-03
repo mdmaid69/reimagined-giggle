477dfaf8a419116fb09804f6c805c10a299a1e6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:09:31 +0000

    high hair doubt friend test

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
