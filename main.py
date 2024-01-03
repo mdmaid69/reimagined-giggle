a4b6d7bd63598171dfbf4ce0ae48b153df365bf9
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:58:17 +0000

    fast autumn spray react grade

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
