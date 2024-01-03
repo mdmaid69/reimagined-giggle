f39cc3bb9640141b95da4bf8ca062784d45c8108
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:31:59 +0000

    dance aim contribute opposite field

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
