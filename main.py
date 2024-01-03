755b1ee0396b515f9579e881a0bbed860810e195
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:34:11 +0000

    drive future hospital guess opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import numpy as np
+print(np.array([1, 2, 3]))
