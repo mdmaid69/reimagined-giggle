3b9318c80aec7b84ddfb1b5a471549d421436938
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:01:30 +0000

    means independent first suit advice

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
