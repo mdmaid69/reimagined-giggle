d1269e83d00d9732010da1c0e6ed6f7c3dd8b21f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:53:50 +0000

    shall state wealth Nn preparation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
