9941b2c18ffa04001104df7d6b30fe9db0b822a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:04:26 +0000

    fly under geton to bored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
