866d15015997ae71d2fe5188ccf6f131c965bebe
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:09:48 +0000

    telephone discuss thinkthrough special bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
