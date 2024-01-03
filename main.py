9b03558543a315335cc0bef956a057e59393d8f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:24:41 +0000

    slow swing hand wide receive

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
