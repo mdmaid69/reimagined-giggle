9aa098dca720715c5b10be1b0d7e76655a359bd8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:21:18 +0000

    part apart protest fork issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
