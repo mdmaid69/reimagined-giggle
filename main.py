dbd35582fb321385b6edb42870bf847b31b53d28
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:05:11 +0000

    excuse cry home unlikely door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
