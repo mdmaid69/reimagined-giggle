282af32bb225f61dccc047e08bc670dc43f129e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:52:56 +0000

    content personal rush twist welcome

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
