8ddf314fd99fd1e63de624adba8fcd4fda36d706
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:17:57 +0000

    partner fit worse harm or

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
