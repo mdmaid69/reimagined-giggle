da60a47cf7949cc6fb43ecb132ef46f48a29c540
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:45:51 +0000

    favourite mysterious machine forget together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
