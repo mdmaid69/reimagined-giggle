f35184a6b140d0bc98bba7ebf395a8d6261037a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:39:52 +0000

    actual mention ashamed available succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
