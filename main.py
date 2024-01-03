509d594e462ad8feae80020ad112a8e1ee5d2fba
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:45:51 +0000

    better Cc equal left left

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def append_to_array(array, item):
+        array.append(item)
+print("Hello, world!")
