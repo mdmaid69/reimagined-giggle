77da1cd79275f3be23114d377c1e03db9a050863
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:51:39 +0000

    port remark better upset target

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
