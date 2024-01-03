9f373cb9adffc5b925d611df3d5800178c7f3e3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:35:30 +0000

    lookaround draw rate finger rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
