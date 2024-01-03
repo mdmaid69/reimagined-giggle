4fd25dfc42a29d355fde8d120d23bd232789f52e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:17:26 +0000

    pile smoke one attempt zero

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def count_words(sentence):
+        return len(sentence.split())
