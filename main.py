ceb5d52df38418ca1e85514bf417ceb06aacad0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:19:05 +0000

    form borrow row breakthrough dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import time
+def get_current_time():
+        return time.time()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
