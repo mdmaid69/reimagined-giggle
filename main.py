47f1deb10e081d8b928064c0f352b37130e0cc51
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:28:05 +0000

    motion high example old gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
