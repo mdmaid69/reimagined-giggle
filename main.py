6cd3fb85583e7081ff7e50a8b713a5d061f4a0b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:55:03 +0000

    now safe stare big store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def reverse_array(array):
+        array.reverse()
