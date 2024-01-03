d168d3a51ac0788bb894a0fbdac3360f0e7085f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:23:48 +0000

    sit appeal worry breakdown deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_as_bool(array):
+        return bool(array)
