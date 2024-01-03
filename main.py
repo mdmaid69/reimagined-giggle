62622b02afd372b934f71b777f3cef043e727f1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:40 +0000

    label keep region card concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
