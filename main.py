d676717941a116afe619714861c490d153bafbd3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:53:13 +0000

    risk circumstance in interruption beautiful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
