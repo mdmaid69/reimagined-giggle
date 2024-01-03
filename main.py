5f666cfaf90100438fcbffeef4e4e0dc67536a36
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:40:29 +0000

    sound other pause them wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_pressure(force, area):
-        return force / area
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
