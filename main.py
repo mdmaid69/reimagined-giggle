3db0bc902a15edcb91d13554f46c61934d5d1906
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:32 +0000

    charge comefrom spite furniture pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_str(array):
+        return str(array)
