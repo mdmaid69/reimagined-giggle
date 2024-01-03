b615f536332322d21cce1d44b63939c2fb59392e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:13:12 +0000

    only attractive callback each journey

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
