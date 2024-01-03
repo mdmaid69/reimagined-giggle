77cd17cafe8c1be1cf1c7d04c0335bc9d2522a1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:34:27 +0000

    shoot duty doctor natural owe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
