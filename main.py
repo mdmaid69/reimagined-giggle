5b46ca7c259ac9002933f276866eee5b3ae3c709
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:11:51 +0000

    future new bath worst takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
