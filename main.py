872726012af517c65dfdfd244c7e5a9af820b542
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:19 +0000

    finish enthusiastic of direction afterwards

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def reverse_array(array):
+        array.reverse()
