8d8a191abcbdb3836ec411a9f6951ba02e772b9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:44:15 +0000

    wave different male fly no

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def get_array_item(array, i):
+        return array[i]
