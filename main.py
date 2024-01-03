42198e8e0351abb826b17f922081447de8a2eaa0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:11 +0000

    rid brain food callout length

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def add_numbers(x, y):
-        return x + y
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
