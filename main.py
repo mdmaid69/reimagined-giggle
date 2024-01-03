476d59df0bf2b6f04431092d72b0333d95ab74a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:55:16 +0000

    shop glad swim early punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_sine(x):
+        return math.sin(x)
