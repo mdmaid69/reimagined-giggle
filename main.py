47921be7979387332e821a25fde9aa85cccf68f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:40:51 +0000

    active fierce position careful child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
