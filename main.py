a36a51683adef2403afe9448f1cb3690ddd5d562
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:39:05 +0000

    scream strike drop noone excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
