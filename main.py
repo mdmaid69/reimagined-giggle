b7de67a4c0a4c455656a839d5a2eac855e939cec
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:04:57 +0000

    touch politics talk bite uniform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
