4f208e29a016bc4187a579741026f7fbc8a9ee15
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:49:10 +0000

    just pleasure immediate hard spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def create_counter():
-        return collections.Counter()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def create_stack():
+        return collections.deque()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
