f6b4df5ee1f4dd1f8027c02b43bd09f941c7224f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:46:15 +0000

    scared replace criticism separate in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
