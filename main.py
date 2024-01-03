38bd7295b7dd6452461c1b4c9f42fbcd80746b53
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:37:57 +0000

    final knowledge have keep really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_as_float(array):
+        return float(array[0])
