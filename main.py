e0720b51eb5eb558d59528b95177577e8623f8f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:01:39 +0000

    involve mistake noise succeed matter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
