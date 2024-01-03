4b7e4d56daea99c6ad74557d515e04fc1957b9df
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:00:00 +0000

    benefit goaway natural off later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
