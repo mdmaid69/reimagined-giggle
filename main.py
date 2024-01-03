780db5ec7f9bd0d89bd151355f741476ad42fa55
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:45:59 +0000

    somehow escape between development cause

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
