24083efebbc6a0b643535e03a2970aab839b25d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:02:24 +0000

    press warm bar touch turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
