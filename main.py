f888efc4df455622769b41339521db801f50709f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:29:21 +0000

    agree cry supply hold holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
