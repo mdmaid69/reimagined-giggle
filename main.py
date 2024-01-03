945a8839c01ac148a9308d6b3943282dab6c21c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:09:34 +0000

    bread image free direct approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import datetime
+def get_today_date():
+        return datetime.date.today()
