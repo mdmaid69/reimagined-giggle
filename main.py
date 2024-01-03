89c8dcd9adc301e87cf497fc4d2f08b242e5532b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:24:42 +0000

    at value wall watch national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def cube_number(x):
+        return x**3
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
