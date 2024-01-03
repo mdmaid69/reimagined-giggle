528e732da1a0f7984800e1bf2c708c1ef5f6e985
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:21:40 +0000

    organize this downstairs idea fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
