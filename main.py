3ef0f13206c5231bc73c5560699ada372c6c28e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:05:38 +0000

    along calm calmly limit telephone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
