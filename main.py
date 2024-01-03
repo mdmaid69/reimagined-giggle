5af0b558b647430be690a6b186d60071e28c3c38
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:57 +0000

    label disturbing eat impatient grade

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
