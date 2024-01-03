a057be839045a83384a7f02215c8dc91940be1ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:38:49 +0000

    avoid rescue laugh pronunciation prefer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
