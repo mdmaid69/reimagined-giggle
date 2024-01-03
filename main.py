c101e6180e38cbcdbbf3a35d6ff2bbaaa606afab
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:27:19 +0000

    will relax carry lookaround there

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_max(lst):
+        return max(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
