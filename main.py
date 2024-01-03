e42e0e2cf9b25f5a73e03efa2c1a2ca9b08c0fd2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:04:35 +0000

    enthusiastic die dick can stir

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
