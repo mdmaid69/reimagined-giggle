6837c39f92b08bdd543829a2dfb9e4a9cc14cc8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:19:14 +0000

    spill sick along Kk period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
