1d372f86861affb48376407d989451e19a306259
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:13:58 +0000

    marry breath truth ocean balance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_list():
-        return collections.UserList()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
