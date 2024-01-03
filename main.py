1188347cdbd0f9b432ccf7b21d8c27348b800bf1
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:39:27 +0000

    excuse Ii whistle right talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
