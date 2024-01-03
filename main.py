9b5546d108ed7b30878952c564012657f9860cf8
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:06:36 +0000

    successful agreement few late object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
