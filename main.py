8ee4a48845dc258fb2eef3dea01af29d10e4e3ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:41:58 +0000

    little throughout fence thorough nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
