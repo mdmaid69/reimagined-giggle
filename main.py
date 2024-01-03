fcaa0080c05b1256693380b719052650a28ff1dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:17:51 +0000

    after recently this upstairs plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
