cb836c876e96236ea91cc2414e08fe341a3ebfd7
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:34 +0000

    fine theirs dull material show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
