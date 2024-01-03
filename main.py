98f13e2ae24145fe96eec34df2155928c44d60bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:41:02 +0000

    money off table afterwards breakthrough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
         plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
