e0c0eba1e60b42ffd805923c7680aeaa958c509b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:23:38 +0000

    over recover appearance tape act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
