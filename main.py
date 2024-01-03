a66e78a1333ecaec9a8b36bbae71b48b8e671a39
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:43:53 +0000

    measure walk hollow hurry purpose

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
