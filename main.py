b9d8678191f7ccc5076bf09431424a47ce95843e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:15:57 +0000

    inside healthy pull turnover over

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+for i in range(10): print(i)
