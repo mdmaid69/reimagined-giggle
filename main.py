7aef92b087c3862291745633e8e0fc6f222f5648
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:38:21 +0000

    shine appearance pronounce serve smile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_force(mass, acceleration):
+        return mass * acceleration
