7e6a85fbb598fb3f8b9b17870fc3b95bcb067646
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:29:41 +0000

    production public strict bringforward ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+import sys
+def exit_program():
+        sys.exit()
