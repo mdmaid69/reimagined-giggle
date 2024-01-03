39a00c52fa4958b96be8e4e3178bb56b8edf2268
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:00:13 +0000

    around store design produce spot

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_floor(x):
+        return math.floor(x)
