29f61fd9da499f484718edbb9840399accf70749
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:17:02 +0000

    callout level produce neck bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
