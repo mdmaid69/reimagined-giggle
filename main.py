c08a3377e52f021850708d1e7fc4661d004281c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:16:55 +0000

    height lucky near the glass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+print(time.time())
+def calculate_pressure(force, area):
+        return force / area
