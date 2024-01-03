6224876800e63c8a3eb0a1ac7c731fcde8438309
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:28:59 +0000

    payment accidental plenty butter difference

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_as_str(array):
-        return str(array)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
