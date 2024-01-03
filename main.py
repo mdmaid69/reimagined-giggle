3d774d976ac7750d7feefd06f329a39bc690acca
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:03:14 +0000

    speed main reject neck famous

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
