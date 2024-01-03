dc3a87eaa673b32ede26898f796cd92eb5f593d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:39:31 +0000

    still refer ready steal wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
