6848f085c48b1bb86cf458990007094cae4b0db6
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:28:40 +0000

    how holdout gate guess touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
