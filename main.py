b7fc5cae1f5f50f7877ca8c79f4cdd79225e2847
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:13:47 +0000

    spare study thankyou warn callout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def remove_from_array(array, item):
+        array.remove(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
