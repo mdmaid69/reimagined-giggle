27e26ea47a9d5ddcb75883b432105d1559143d45
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:33:40 +0000

    its region morning past occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
