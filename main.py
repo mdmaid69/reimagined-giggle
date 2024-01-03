c7d062a3e591126723ae9082aed9ffd68779e692
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:59 +0000

    beneath Jj imagination birthday rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
