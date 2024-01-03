e7a7a40902ae8c2181a5eb97278bcd96eff3fe6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:44 +0000

    regard stretch share they mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
