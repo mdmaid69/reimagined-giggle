140dd83c66f47210700291e2ffdc20c47d80685c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:09:08 +0000

    process listenfor brush food maximum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
