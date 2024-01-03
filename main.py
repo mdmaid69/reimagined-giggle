4b43f3a78d45fbf01e7562e4f852fb5dfc0bb3ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:33:33 +0000

    slightly test waste comedown change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_base_name(path):
+        return os.path.basename(path)
