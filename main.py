a7a4f407055f4c4cbc8b8e25d59f2b09274d59e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:09:38 +0000

    getback please test takeover like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
