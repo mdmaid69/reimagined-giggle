27c115ecb4b7e9518473fa30fe3cf85da4cd6523
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:43:10 +0000

    left success cause centre smell

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def convert_to_hex(n):
+        return hex(n)
