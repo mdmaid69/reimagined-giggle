b2052a08b8c52115b738c56d215e5dd97175f63b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:35:05 +0000

    recovery 564encourage nor godown past

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
