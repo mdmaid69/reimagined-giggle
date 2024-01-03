16d4565abbfa1f20433686fe005c7502f2aed21d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:20:07 +0000

    talkoutof he almost rest secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_int(array):
+        return int(array[0])
