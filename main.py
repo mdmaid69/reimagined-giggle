515ce0194a51897f695d212c3a99a380555c7845
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:11:12 +0000

    luck office with flavour zero

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
