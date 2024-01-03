eed8493b1a5f46f27e6e844e6809bc453f35729a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:12:04 +0000

    hate want cum sorry open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
