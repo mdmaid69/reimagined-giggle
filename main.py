661d2f72e465cfe85cf793edec4d93890a52b748
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:01:14 +0000

    offer present tongue company future

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
