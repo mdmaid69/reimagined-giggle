17716ca71cca24203ea518d426262b84daf54d63
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:30:38 +0000

    list relax sore respect violent

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
