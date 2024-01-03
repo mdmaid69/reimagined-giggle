16958b44b83c5e3a325e16bc4ae43c386e9ea4a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:56 +0000

    touch leg disapprove work dead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def find_max(lst):
+        return max(lst)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
