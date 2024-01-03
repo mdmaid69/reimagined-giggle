359641dd61c5063e9298aa31251e7d9574a62b8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:08:11 +0000

    everywhere more process see Qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_length(array):
+        return len(array)
