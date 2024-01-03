605da3cbd98e6bc076e8003a7ec27ba25b9a97d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:32 +0000

    cruel hour holdup recover direct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def convert_to_hex(n):
+        return hex(n)
+for i in range(5):
+        print(i)
