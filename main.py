190a2d46e41d07951527c24a20078fb1e39c5148
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:26:39 +0000

    turnout above single possible rely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import random
+def roll_die():
+        return random.randint(1, 6)
