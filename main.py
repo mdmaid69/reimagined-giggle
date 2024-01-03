6d69975149724449a50bc82cbd6ec6ac76b4c3aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:31:02 +0000

    vanish brush variety high will

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def square_number(x):
+        return x**2
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
