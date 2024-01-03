e90e24947a5426eefab5a871076a7448948d2f15
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:53:44 +0000

    Oo fresh least present illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_work(force, distance):
+        return force * distance
