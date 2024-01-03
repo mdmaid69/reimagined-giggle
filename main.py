1a00e584273e47bc095c4f5a994692f8a8f8b4a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:39:03 +0000

    bank hers touch strike 571enough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_pressure(force, area):
+        return force / area
