89d32e014c93ed08cae4c559d45e653827c7c47a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:47:54 +0000

    field hate unusual decision comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
