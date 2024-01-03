54df35f5917bcac7c7630d09e4c7dc1bd79e4841
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:38:34 +0000

    general himself excuse up corner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
