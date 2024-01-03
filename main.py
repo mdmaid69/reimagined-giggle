f2f828d55680092dcd5bd8c36d5cb6a76b6c3c7e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:09:55 +0000

    doubt fear blame bad estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def find_min(numbers):
-        return min(numbers)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
