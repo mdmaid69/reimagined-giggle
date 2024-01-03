faaa996d75ac3122fe4290029f9ddd3676098e92
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:52:43 +0000

    clear through will breathe Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
