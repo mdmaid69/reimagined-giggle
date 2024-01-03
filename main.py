174e6df71254ae0a68f3ac56f221ea8f571608dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:32:15 +0000

    allow face answer authority tape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
