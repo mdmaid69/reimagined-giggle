d78869579ddf5a5fe07c8becb107051ac448de0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:26:06 +0000

    distant basis lot refuse count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
