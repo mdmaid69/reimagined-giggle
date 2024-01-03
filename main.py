d613ad9e52e5febf7d2d851c9b588051e354a841
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:19:41 +0000

    fear system sing tour out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_work(force, distance):
-        return force * distance
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
