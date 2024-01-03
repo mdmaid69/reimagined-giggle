6bcd9f6f23d53c691195478ec21f9155133a6521
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:42:03 +0000

    gift dangerous pass loan private

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import getpass
+def get_username():
+        return getpass.getuser()
+  import sys
+  def get_python_version():
+        return sys.version
