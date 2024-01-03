21d2d9f9d74f7052380bb027f1e5f0f731f3520e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:54:30 +0000

    involve long project stretch real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
