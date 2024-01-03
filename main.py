1ba6e54436b56e211fdc2ce04f0da45df331397f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:21:11 +0000

    name spray favourite fear ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
