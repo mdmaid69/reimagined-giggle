cf516948c89668af54045db5f5872fcda044212b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:31:29 +0000

    figure confident comeoff runover switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
