af2f739b0e60fdf135492363bcb93d19ae79eb33
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:54 +0000

    order warning agreement turnover sky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
