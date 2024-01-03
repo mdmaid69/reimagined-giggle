7b81631446dd949ee29f1eecd202a515ac16c3e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:30:15 +0000

    common score back light lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def square_number(x):
-        return x**2
+import os
+print(os.getcwd())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
