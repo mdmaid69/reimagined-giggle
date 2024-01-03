b93da9d98a24518d7bf97ba409e0fac3f91454a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:19:16 +0000

    boat blue condition worst rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
