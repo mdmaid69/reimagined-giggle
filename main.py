31076e953c1372416d3e31409e86cd9a54f20bb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:41:00 +0000

    piece dinner constantly include circle

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import math
+print(math.pi)
