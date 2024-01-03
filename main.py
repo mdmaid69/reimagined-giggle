da1eee8f946b23c66d02d0896ad031b2e1a2f2de
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:14:49 +0000

    used black himself fire comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
 import os
 def create_directory(path):
         os.makedirs(path, exist_ok=True)
-def calculate_area_rectangle(l, w):
-        return l * w
