1bd81ed7975141c91337011b3580f5064534ccb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:34:28 +0000

    genuine contents write proper prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
