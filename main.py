d9cac2e602bb74ff01d1c14fc77052bd343c31c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:30:42 +0000

    mistake smoke ride along beyond

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
