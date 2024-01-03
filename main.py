ad54c654fdd1d01972569e4a244d5005ecee9367
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:42:04 +0000

    minimum hope could course getback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
