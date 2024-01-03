1fc00a8857e8f5d302d8567f75eff4fac24d4516
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:17:12 +0000

    round work relief breakthrough swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import time
-def get_time_since_epoch():
-        return time.time()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
