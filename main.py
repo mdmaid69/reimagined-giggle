0040fca7ee3888c0cc3baf6974b5ec6c202271f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:55:22 +0000

    waste go 567enemy thumb wash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def find_min(numbers):
-        return min(numbers)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import sys
+def exit_program():
+        sys.exit()
