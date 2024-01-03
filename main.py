28ad8b9f68a61230ccc4b6ab83d34820eebc04ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:35:07 +0000

    pretend belief train trick print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
