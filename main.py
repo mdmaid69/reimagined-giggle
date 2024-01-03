b5032b630e06b86130889bc6b6e1fd83b79bd4d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:02:49 +0000

    hard wait pretty blow across

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
