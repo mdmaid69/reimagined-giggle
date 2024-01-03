9896e85880d5b34184d978bf79032be9950c1a0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:12:16 +0000

    share world public neither their

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
