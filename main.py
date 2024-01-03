f94efd73cc39687080f81337336fad9463d326f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:13:28 +0000

    legal cough argument shelter unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
