aa6be59d5d6e6c4472bfbab61d6371b7950dbca6
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:57:58 +0000

    care translate store later path

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+print([x**2 for x in range(10)])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
