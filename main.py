09549490a4e01a420dc116d5ed18f98ef34a0ae5
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:10:17 +0000

    bin talkoutof run achieve female

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print([x**2 for x in range(10)])
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_density(mass, volume):
+        return mass / volume
