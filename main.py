01105b79cdec3670ac0032afed0fbb1018c32ffb
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:37:18 +0000

    bag yes different crash behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
