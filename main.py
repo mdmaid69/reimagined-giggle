991a3dff6469bbd54d9871440c2986291b25c251
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:52:57 +0000

    anywhere expression case track successful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
