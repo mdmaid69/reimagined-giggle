658eef0e5d9fbcaf56ed619df029e6b1b337a530
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:31:21 +0000

    coat steam body which anywayanyhow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
