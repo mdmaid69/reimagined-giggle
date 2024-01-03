fe8a21a12396a138bfa6373c646c24e6bddd0397
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:33:05 +0000

    telephone clear object Itself pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
