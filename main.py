5467552c2cb72ee570e6a4c3919c13ef01659e0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:27:27 +0000

    shall film arrange escape moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
