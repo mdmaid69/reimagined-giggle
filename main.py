8465eac438c1c45a1abb594430887f33a2122306
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:09:26 +0000

    slide promise think smile extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
