0632edfbd3863a02224416589819827e88a54291
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:59:58 +0000

    deserve spare recover now stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
