0ed3f3b4c9235a00aeaabb0f8e42c499dced47ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:42:48 +0000

    laugh flavour tear intelligent smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
