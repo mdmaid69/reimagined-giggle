95e2adaee5ae88b3d50b286c83a6c6c511211c30
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:46:38 +0000

    allow noise reduction safety hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
