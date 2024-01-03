30f855484d0bb3b4c047b66908c2e27ecb267300
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:50 +0000

    delight allow pupil care complaint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
