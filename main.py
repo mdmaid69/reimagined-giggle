9f64c108967e09ceff7049cedc0870f355420fb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:52:39 +0000

    keepout turnoff them cough workon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
