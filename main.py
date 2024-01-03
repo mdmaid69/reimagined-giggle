b56b56c5c6b53c8050da20535453d64e3790583a
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:38:17 +0000

    pretty skirt job concern harm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
