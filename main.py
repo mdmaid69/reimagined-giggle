ecd7111b218bdf895e4c2bb7224eb5af8cbe573e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:02:16 +0000

    birthday air bottom culture after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
