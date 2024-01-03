59c3a27d822046dcd7566576df0e64c83970d2d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:31 +0000

    foot Tt to it sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def find_min(lst):
-        return min(lst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
