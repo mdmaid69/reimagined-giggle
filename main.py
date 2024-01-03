93404ee386b871cb1fe94bca4d95dc3cbf5554b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:45:29 +0000

    strength concerned habit breath range

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-name = "Python"
-print("Hello,", name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+for i in range(5):
+        print(i)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
