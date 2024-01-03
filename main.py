96b7ec3951f114a117c771ed110184598a2ed4dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:07 +0000

    lift ring test Itself quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
