987ad1ab069f4343b2e6b59bf4068c225a15b811
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:39:03 +0000

    capital spread count eat near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def get_array_as_bool(array):
+        return bool(array)
