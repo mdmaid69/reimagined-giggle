d7611b96fe5ab5d67a2486f4691763a676f6fe2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:21:47 +0000

    certainly react improvement rise unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
