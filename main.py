78e174d20eef540efd5bc9b7001a5d1035682d24
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:21:03 +0000

    song study row clean news

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
