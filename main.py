0c6392e0c9c4479a4f21348d188899e58363c31b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:41:47 +0000

    file advice hate authority possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
