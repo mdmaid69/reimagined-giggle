bfcb17f364d8167d38c8e0faa2779b6cfd693998
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:03 +0000

    react something pair meet disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
