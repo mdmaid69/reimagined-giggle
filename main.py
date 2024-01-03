09a64ad7893764eca82f723e1eb17617af856a34
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:39:50 +0000

    burn can realize depend upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_item(array, i):
+        return array[i]
