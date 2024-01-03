dd38fff32994702d49051342f80129dade631be0
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:58:08 +0000

    bottom height uncertain identify fortunately

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
 import heapq
 def create_heap(iterable):
         h = list(iterable)
         heapq.heapify(h)
         return h
+i = 0
+while i < 5:
+        print(i)
+        i += 1
