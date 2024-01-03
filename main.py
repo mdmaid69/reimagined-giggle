df10713ac5b27733537419812c7e109d06dcdb17
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:18:23 +0000

    drop sort total Mm used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
