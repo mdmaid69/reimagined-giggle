51176a0615d78853bae3b53ee5f3789f786d69f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:20:09 +0000

    ahead willing sudden government beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
