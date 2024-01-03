666c321bd217e00d5d8c681beafe21d661352261
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:46:13 +0000

    noise wise with you lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
