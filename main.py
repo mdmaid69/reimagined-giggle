75aca9ceb5ba807307284743b6a01dd9ce0628c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:10:17 +0000

    which accordingto scene contrast influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
