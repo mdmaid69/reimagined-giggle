fd054bcd06598092278dd2b33c11102bc04d22b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:05:56 +0000

    interview exercise wealth traffic unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
