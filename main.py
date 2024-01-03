5977b503b36833e133735a55e1bb71acb6c0e7ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:47 +0000

    her own load grateful cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
+import array
+def append_to_array(array, item):
+        array.append(item)
