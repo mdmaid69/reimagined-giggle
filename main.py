38e4458ddbd929870a9aa7329e1b71df174f3d35
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:22:42 +0000

    union tape use star separate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
