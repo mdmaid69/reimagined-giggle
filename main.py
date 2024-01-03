e4d662c6557693178444732b3d3743ef5265ebb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:15:55 +0000

    public back answer then use

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
