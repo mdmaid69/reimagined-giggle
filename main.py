c3be411e6dddbfdace797bae04d4ca7109bdf7fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:02:03 +0000

    regard flow nowhere slice word

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
