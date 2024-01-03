b031480a4fffaef37223722db40ab5a0b45afdff
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:13:34 +0000

    nearly pollution preparation surprised commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
