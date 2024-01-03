cf5b9022665f6049067ec72e374c4e7219fc2318
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:21:05 +0000

    vision strike treat clue slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
