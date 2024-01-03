0ae39ece1ecc0596b669c99a32578b0282ca205f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:45:37 +0000

    naturally arrangement about simple communicate

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import time
-def get_time_since_epoch():
-        return time.time()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
