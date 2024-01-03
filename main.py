7ccced3bb70e72501b6a09bff9b207a1bad76c1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:05:24 +0000

    basic bear fight lost study

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
 import array
 def extend_array(array, iterable):
         array.extend(iterable)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
