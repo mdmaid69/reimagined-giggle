25266f71e34eaeaafe99912fb72badba4a0072d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:39:22 +0000

    tidy kind breakout entry law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
