351f137f3a35ef510f1f9269b2265cdce6cb16bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:38:53 +0000

    consist play frequent dinner disgusting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def square_number(x):
+        return x**2
