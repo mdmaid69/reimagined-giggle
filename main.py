3f02d217e57b1eada3b87b2dd050abad19afa837
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:52:06 +0000

    situation throw call study issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_item_count(array, item):
+        return array.count(item)
