3defc24523ceeeb543676f7ffab2407d0feedd97
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:23:40 +0000

    female crowded react fail whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
