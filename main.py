d8e4c4473d93319cefe572f5be6b846b8cce7c54
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:38:04 +0000

    into 568energy hurry leave stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_elements(lst):
+        return len(lst)
