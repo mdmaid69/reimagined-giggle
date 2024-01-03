9593b215ec2f147f91f85f91357f8afba8a0dff8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:03:04 +0000

    pipe argue fresh regional choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
