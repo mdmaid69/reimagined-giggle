0a015f14129b86a28424f6470579fcd6135d74d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:19:27 +0000

    Tt breakup refuse ourselves serious

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
