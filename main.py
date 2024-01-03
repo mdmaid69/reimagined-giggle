f1c9ff5df388d7537b9993c5ab5a4469a72c11c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:54:40 +0000

    good hide period break roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
