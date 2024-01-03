a0fd496bc15d0373085f7743b299ab3963d04a07
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:56:14 +0000

    help cross notice bend answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
