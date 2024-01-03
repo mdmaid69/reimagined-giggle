2dc50d1719ffa8aabecbdc16d1290690332a8228
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:06:34 +0000

    bang them beautiful abroad speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
