bc5e92ff0ad30471084f1203ef4f021c9d333241
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:48:49 +0000

    compare suddenly accept explanation mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_string():
-        return collections.UserString()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_index(array, item):
+        return array.index(item)
