2e9acb9e5ee3268ab5c23d581ef98d71eab5c8e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:40:55 +0000

    stranger Qq excellent flat furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
