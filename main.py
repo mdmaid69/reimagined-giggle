e58cc60a6a57d1520f7453df54d2cce3091386ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:50:59 +0000

    remove relative kill tough man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import re
-print(re.match("h.*o", "hello world"))
+x = 10
+y = 20
+print("Sum:", x + y)
+import numpy as np
+print(np.array([1, 2, 3]))
