6fcc2d28cd35aef327e2cc7d2f0f218ba7d37347
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:45:52 +0000

    within Vv plan wish emphasis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
