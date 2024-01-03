ab441909d806a4704f447fe5e0429ca09f5111b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:54:08 +0000

    wheel politics impatient beside pass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def multiply_numbers(x, y):
+        return x * y
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
