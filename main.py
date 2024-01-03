9675709b9cab999f6f18cde5441bc287c0a64a21
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:03:35 +0000

    plenty sail before prefer report

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print("Hello, world!")
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
