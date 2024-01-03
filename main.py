57a6f7ffd9999bd434af3efc377227734b5eb597
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:44:02 +0000

    pinch introduction spray describe everywhere

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
