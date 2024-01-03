65111d160f26e52d1c6b0b59061f8bb21e2490a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:37:41 +0000

    either long limit judge ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
