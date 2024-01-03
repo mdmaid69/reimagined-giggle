b52a1334ed5c9175884dac6740166c72730c1608
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:39:19 +0000

    well mention knowledge nose address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
