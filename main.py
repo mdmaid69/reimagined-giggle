ce88fbfa8d29e5306dc8ad5eef715d532debdd26
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:59:52 +0000

    breakfast disapprove shout disappear whichever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def count_elements(lst):
+        return len(lst)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
