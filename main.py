a7ef1471760e7a3c8518520c5cd3ad55e57bf78f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:48:02 +0000

    wonder software callout claim compare

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_as_int(array):
+        return int(array[0])
