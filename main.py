0723b11d2d2fa6ec7ba381d1749ca7a99348ec1e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:27:03 +0000

    here right juice how properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
