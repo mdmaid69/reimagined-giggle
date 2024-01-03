ffa5a57a514d4ae89d04f337cf5affff63af47a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:39:43 +0000

    official drink meal who fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
