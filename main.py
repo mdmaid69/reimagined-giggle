379f2fde4fee917ea8fd413313d31012fec0fbfc
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:19:58 +0000

    test software comefrom less grand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+print("Hello, world!")
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
