2dcfee66a1c3a520052098e209ab2b758d32d155
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:18:30 +0000

    protection between consult dream tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
