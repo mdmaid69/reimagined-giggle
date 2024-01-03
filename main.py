a8e8e85431658d96cc6029c0769e42c946728e8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:52:40 +0000

    ours lose punishment exploration bill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+for i in range(10): print(i)
