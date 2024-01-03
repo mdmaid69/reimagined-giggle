caad6cda8407c3e632048c63319d58a8e1441b36
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:59:28 +0000

    happen refuse age wall blame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
