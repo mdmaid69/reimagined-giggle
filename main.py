e209d3cd170b0894d7c595f2960c3390288a8751
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:03:40 +0000

    letter scared possibility build afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
