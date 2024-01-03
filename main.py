a29679f6216adead54322ee3c17914244dc4cb0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:27:29 +0000

    tie lie tour excuse variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
