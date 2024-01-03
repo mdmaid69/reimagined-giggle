bcacdcccf5348eececebb5bedbe60baea30f977c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:54:09 +0000

    second traffic thirsty standard stir

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
