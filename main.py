deacb8a9788b531ef7a0112f2d6c1deb9dc97d4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:20:08 +0000

    tonight advice phone machine would

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
