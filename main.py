64bbb6a5a79e998cd68149ff53a8bb13e2358b3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:22:34 +0000

    breakthrough patience pleasure path plan

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def reverse_list(lst):
+        return lst[::-1]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
