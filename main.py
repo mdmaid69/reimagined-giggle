0109380607f90aeff59e80636dffd679f7d76c43
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:51:16 +0000

    unfair effort task another score

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+def roll_die():
+        return random.randint(1, 6)
