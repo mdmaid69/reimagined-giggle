b471a0edbcfcdd3b2957fa6e824d249e3e97901f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:36:18 +0000

    tax disturbing far centimetre bin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import time
+print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
