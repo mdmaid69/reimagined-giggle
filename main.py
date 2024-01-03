b044b9b63885ba30ba58587badaed2090025c358
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:44 +0000

    fail interesting patient tough debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import getpass
-def get_username():
-        return getpass.getuser()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
