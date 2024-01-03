b0dda6360d7904637cd905d2c05afb714ad10d22
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:49:07 +0000

    top before unlikely blue this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
