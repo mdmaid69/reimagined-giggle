582bc6dc1fe3d9c54264f58e45774cdbdb510aa0
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:34:44 +0000

    high picture block week several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
