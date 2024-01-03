b86a31d0a8ae4fa323be1a95b0195af6ee526093
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:00:57 +0000

    whisper instant safe Zz round

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
