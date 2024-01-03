8b6a0bdfc2b18e419ca9e2f4d1f1a432293258cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:36:36 +0000

    grow pile patience turnout rarely

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def is_odd(n):
+        return n % 2 != 0
+  def square_number(x):
+        return x**2
