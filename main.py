88bc205ba7b13c5b64156d7de00591c9013c22e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:31:40 +0000

    marry strict bringup 179bite would

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
