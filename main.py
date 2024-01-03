13f83386674ccc8514059d07f4b0e005b9c1ec3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:55:56 +0000

    thinkthrough boat all drop community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+import collections
+def create_stack():
+        return collections.deque()
