1a377750a7fa5614b9be39f37e4a1865e421106c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:42:32 +0000

    organize will shadow come left

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
