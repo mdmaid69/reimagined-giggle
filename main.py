7644beaf0756a129e23bd11aa106f2110c3e35dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:05 +0000

    perfect accept extra 566end leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
