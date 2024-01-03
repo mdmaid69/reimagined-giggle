8b4317c05890ab22d157037d30e30577191c70f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:31 +0000

    breakthrough car emotion shame interview

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
