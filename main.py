fea42fef028981fdb6f4172709899d0faca05183
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:29:23 +0000

    excuse hardly often excuse lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
