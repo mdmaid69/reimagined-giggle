267091a5e5bae4293c32de960aef7241a632e4d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:57 +0000

    566end so unfair rain law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
