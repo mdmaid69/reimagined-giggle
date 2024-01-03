60bcfc08a690f090f200f7716a7b7bb205914135
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:32:51 +0000

    central tread field face right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
