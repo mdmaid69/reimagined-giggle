e8ca73f2a5fd7c19cc963462b561c82bc957dc66
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:34:48 +0000

    possible science scream nor brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
