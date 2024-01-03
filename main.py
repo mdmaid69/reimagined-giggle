eb6d0af2e203c570d92a1d8475033b0d97256681
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:53 +0000

    wind comefrom alone shame unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_even(n):
+        return n % 2 == 0
