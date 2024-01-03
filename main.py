4dc73899230e56fb3b5fd807cff83020aed445bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:17:31 +0000

    rare post part familiar pinch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
