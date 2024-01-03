81a027b046fb051114727f2b03e161a1ede36639
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:42:19 +0000

    beat tidy objection fence hour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
