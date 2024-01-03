6a285988ec6066f613d733e7d717c6e7a24e74b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:51:44 +0000

    turnback stupid project roll regret

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_min(lst):
+        return min(lst)
