f05c86c2a3b2103fc551c85eb0f4e75a323c0b9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:09:14 +0000

    large pass offer getoff image

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
