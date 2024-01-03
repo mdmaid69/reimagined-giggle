07954c0643659896cc04b2376fb69b2757f97bb6
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:41:28 +0000

    record change actually stamp patient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def remove_from_array(array, item):
+        array.remove(item)
