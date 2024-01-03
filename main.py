666a92c1e29e32db1c6df6b0dc90fb57f36828b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:16:59 +0000

    address maintain stranger bringback willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
