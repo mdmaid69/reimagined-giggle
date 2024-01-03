d15877b5809c91d43f459efc5650d49f1b583188
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:34:56 +0000

    activity agreement freeze nigga thanks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def find_max(lst):
-        return max(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
