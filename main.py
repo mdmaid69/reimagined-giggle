4fa2996dbbcc07bb9104e79b388aa0932dce6221
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:47:05 +0000

    workon school practise yesterday rob

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
