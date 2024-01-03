918d069a7df01d1c7d0b3c02c55c12d58eeb88eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:09:34 +0000

    mistake sick phone trial think

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+print(sys.version)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
