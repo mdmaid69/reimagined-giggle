5129e347b1558e6ea9903b3d845b11f036dc8af7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:28:45 +0000

    cap draw private proper weather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-print(time.time())
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def convert_to_hex(n):
+        return hex(n)
