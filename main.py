da7eefb038d36950fbe2815486ced0c8098257ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:08:06 +0000

    love exploration star getoff modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
