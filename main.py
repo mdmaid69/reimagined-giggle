2389a0550022f6bf6715f887f683e161d0e572a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:17:55 +0000

    design brief unfair level class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import random
+print(random.randint(0, 100))
+import datetime
+def get_today_date():
+        return datetime.date.today()
