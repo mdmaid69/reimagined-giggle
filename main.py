19cea54b1892ac940f11ec524757f3fa49f74e8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:39:31 +0000

    chat hope true wander exercise

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
