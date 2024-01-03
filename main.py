37a99d1f23047ca7df172cda6bc05a74854e5eb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:55:32 +0000

    future skill price possible neither

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def cube_number(x):
-        return x**3
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
