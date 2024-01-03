e3cf8ce6a7ea8e39ff99162b932d87b69dd8b018
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:09:12 +0000

    relax disappear store certain base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
