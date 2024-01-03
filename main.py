934481ca57ec4267ab93983195c1a629f23f4c6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:13:45 +0000

    run case reliable runout joke

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
