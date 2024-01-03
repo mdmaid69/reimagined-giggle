80278c03de90a5e033afdc249b953b3cf9dbf17e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:38 +0000

    complaint fairly fence exercise niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_power(work, time):
+        return work / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
