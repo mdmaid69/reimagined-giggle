4438dadd030b0000cc0e1d7ba5533ae0840afdaa
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:48:56 +0000

    grateful unlikely post comeup cold

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
