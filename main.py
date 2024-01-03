2d12d06365a53f433a9107885ae3cd663566307f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:10:35 +0000

    cope degree would hear can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
