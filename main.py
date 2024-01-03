3fa4178fa8996a338a3ba7e266b4d835a6d27d28
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:08:24 +0000

    along callout rich day least

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def reverse_list(lst):
-        return lst[::-1]
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
