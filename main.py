5de5a53166b3f040cb8a1c6843fa885e5aec4d5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:32:02 +0000

    prize tie better yes half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
