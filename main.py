12265c34c34656ffbd3910c72e762afe23760ea9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:56 +0000

    include below protect impression disturbing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import re
+print(re.match("h.*o", "hello world"))
