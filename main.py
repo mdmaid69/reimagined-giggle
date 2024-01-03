20a4642e54bf7e2ba4b2e0798f52101e2ec5682b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:01:14 +0000

    flow definite admit slight noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
