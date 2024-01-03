76297880459fa1060d62e8e6a1078773c424ab23
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:04:39 +0000

    will respect record near platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
