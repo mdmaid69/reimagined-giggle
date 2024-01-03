2137274fd204025e379f20822de4dfb9c8923d27
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:22:32 +0000

    scheme final tense long brush

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+for i in range(10): print(i)
