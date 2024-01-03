212b0fd5867686f39bb558f9e23a8a62fef40fb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:03:14 +0000

    extra allow run get soak

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
