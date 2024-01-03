ab50595cbe8be93d3aa99c59e4ab07c7f7b9513b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:31:30 +0000

    gas drive together glass priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
