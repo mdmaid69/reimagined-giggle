312f58249a984113797004362171e60064f2743f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:47 +0000

    ashamed and miss waste exit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
