0a92b2d0cc827aee0d7d2cfe036cba535bc9cd70
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:28:09 +0000

    common pour far relative scrape

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_sine(x):
+        return math.sin(x)
