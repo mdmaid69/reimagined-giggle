d3ac58e5e832df152a3507aa9b7c7e0df0805895
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:01:51 +0000

    Cc someone maybe method guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
