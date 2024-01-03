41e97b9d7b1db384c6cb5ef524452d2ad59bba9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:29:10 +0000

    gap instant die container sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def clear_array(array):
-        array *= 0
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
