401007c461f8614b5da7524040fdc13e24f346c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:38 +0000

    spring day exist shout sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
