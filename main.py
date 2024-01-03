1130a509d39df4ece2eb589d9384c09f34c27426
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:24:02 +0000

    confident lookaround low scream whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
