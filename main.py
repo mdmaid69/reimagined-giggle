b4aeb5a6c622fbf924170eb0327f367df8717d4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:03:50 +0000

    owe custom sister water swimming

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
