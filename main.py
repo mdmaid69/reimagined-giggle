6eceaa94f29ef593abbe2031c93827d14d876ee3
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:18:16 +0000

    protest bite commit serve everybody

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+print(math.pi)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
