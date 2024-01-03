c8d9071524ce7448574930f28228c4f53a853711
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:30:20 +0000

    question doubt serve murder praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
