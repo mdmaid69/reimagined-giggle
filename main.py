fed2f73acad261073c0cab1062faf4dd334292a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:41:39 +0000

    swing tired burst comfortable wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
