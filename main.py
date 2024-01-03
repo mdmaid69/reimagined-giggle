3c755637d43388db09c77525cf1fea0214faec7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:57:10 +0000

    lucky various Ii worry present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
