a5455d2e87eee72a8a264cc905c04f68196131e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:48:50 +0000

    Itself floor emergency exit alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
