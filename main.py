63edd4fb2ec0a2be6f5328e49d0374ce79b99dd9
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:33:34 +0000

    search company shout there succeed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
