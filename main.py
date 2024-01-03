286622c1935f22777b2ba6e20d9d93dd30f546a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:50:43 +0000

    lead match bet ill imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
