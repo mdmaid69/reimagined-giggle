ca97554a4c460a0ce6ef59a539401d966b947e1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:58:18 +0000

    satisfied past future much steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
