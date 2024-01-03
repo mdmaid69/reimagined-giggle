1ee9133c06a2e40a878c0b99139cca13756997d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:08:02 +0000

    hate block close sure special

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def clear_array(array):
+        array *= 0
