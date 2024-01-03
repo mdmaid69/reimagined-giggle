81fa97a52d81467385dd54f064314bdc3e35aae8
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:56:37 +0000

    result few light ashamed within

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-print(sys.version)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
