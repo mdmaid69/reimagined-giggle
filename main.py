25a29d1b3aad281b14c920cb6502ec192dda9520
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:27 +0000

    opposite persuade wide comeoff promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
