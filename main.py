1f46f340826e8c9b7fad2f11d6e5e393d06ccf4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:41:52 +0000

    pool Yy style remove spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
