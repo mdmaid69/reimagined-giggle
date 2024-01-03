f5683c8cfa45aeeda25b415374aeb0896793a0bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:51:22 +0000

    embarrassed face child takeback pretty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
