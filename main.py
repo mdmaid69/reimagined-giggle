2e99d8e3fbdd576b9e03830d03ff6de33c9e077f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:51:06 +0000

    book week return much think

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
