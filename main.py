e10cf94790ebe7eebeeb5acc365a2d2b82165008
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:52:04 +0000

    lookafter mind glass miss practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
