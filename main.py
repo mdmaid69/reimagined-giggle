16ed165eaacc39b28b76b58728d0fba226a6bcaa
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:18 +0000

    anybody regard switch get ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
