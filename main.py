3a7d7a31243260078f8cd7d9df3fbbe28802ed6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:51:45 +0000

    smell object level whistle surprised

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
