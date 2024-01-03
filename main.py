382e371684f3dd373b4ebe4e260488c9179f8905
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:45:58 +0000

    hard immediate spot tense rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
