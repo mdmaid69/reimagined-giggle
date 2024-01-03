59ad2cecc71c3be8d448ab13cb497f1119b1275d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:45:50 +0000

    inside shrink lean hello glass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
