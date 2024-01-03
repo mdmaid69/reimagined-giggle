ee4870b2d9326cfbc9c8dda7eca8e5d31c45dcd4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:45:53 +0000

    common race breakin lock always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
