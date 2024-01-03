bd79d48043079521c5366f6b580d4534402ec300
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:32:11 +0000

    arrangement whistle sort force through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_area(radius):
+        return 3.14 * radius * radius
