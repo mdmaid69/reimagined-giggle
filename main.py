158267d8f867ac62de302626f50a0b0ed3e85391
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:36:40 +0000

    tradition used year part opposite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_even(n):
+        return n % 2 == 0
