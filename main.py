624f4a1cfdb39044b47cd5f17f8720c5d7eeec35
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:51:18 +0000

    hard separate persuade maybe pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
