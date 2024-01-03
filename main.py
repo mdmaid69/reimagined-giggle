3cb6297ab0312029ab0121c51b644a3d8832507c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:03:21 +0000

    century Ee that tongue but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
