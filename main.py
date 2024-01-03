35349e1d108aed2dca53249a67896a4369b771df
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:24:05 +0000

    touch noone inform handle scrape

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import collections
-def create_counter():
-        return collections.Counter()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import sys
+def print_python_version():
+        print(sys.version)
