93ae7d9aa5bb53e7c6db1c219dfac6fffbdc9089
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:55:38 +0000

    adjective foreign badly whatever children

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
