c16c56eb492f17567115340ecf78c0cfea84c8b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:43:00 +0000

    throw ignore fashion hand process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def find_max(lst):
-        return max(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def clear_array(array):
+        array *= 0
