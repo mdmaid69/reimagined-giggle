ed84a490e0ace8687a8b804ba08a4e84247cfa05
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:59:40 +0000

    everyone education decide receive cover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_power(work, time):
-        return work / time
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
