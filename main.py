01da9e47c1f56f87c75bb5dfc4e570ca1bea9ab1
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:56:58 +0000

    creature stone foot theirs thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-print(math.pi)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
