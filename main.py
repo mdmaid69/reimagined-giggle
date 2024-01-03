54e67081540408c9bb8e591cc0ca5e5cb28aaa02
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:46 +0000

    term wave question business shine

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_power(work, time):
+        return work / time
