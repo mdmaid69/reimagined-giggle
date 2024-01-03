3d763bb6ad5182a2822cfb546d4244b39a3da77e
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:42:55 +0000

    neck tongue smell himself mood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
