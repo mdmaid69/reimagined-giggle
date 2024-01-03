153ad02d527e53357d30981aa0d57969325cd2d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:41:20 +0000

    mother rain early typical makeinto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_exponential(x):
+        return math.exp(x)
