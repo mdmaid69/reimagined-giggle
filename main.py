e99b8da8857d19985dfd48488400fea354ff607f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:59:08 +0000

    square belong moral greeting cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
