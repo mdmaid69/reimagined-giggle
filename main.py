1fa8768c0a7a6354ef1afb88034b04b3be4cb7e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:24 +0000

    appear remain strange touch government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
