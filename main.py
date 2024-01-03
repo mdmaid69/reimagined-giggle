18fe15790989f8353b921c659a93a20694b123e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:44:12 +0000

    Bb neat into used completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
