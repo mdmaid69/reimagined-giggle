ca3071f2b9ed6b5a7cb885916a668eb183f98e01
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:32:40 +0000

    amount region bush position star

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
