89508f7aa20c2da6138029ac81b71dd8d5aded6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:19:26 +0000

    young genuine mark why deliver

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
