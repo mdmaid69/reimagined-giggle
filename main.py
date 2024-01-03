8b5f3ce5a7db708559062b255a1b0294c7d6dc41
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:37:43 +0000

    cross book charge regard limit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_odd(n):
+        return n % 2 != 0
