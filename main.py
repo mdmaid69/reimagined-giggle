3041a6a3687a6f6527e4fc7310583608d57fb6cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:40:03 +0000

    sink cope today thinkthrough crowded

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def reverse_list(lst):
+        return lst[::-1]
