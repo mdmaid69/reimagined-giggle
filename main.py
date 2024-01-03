79124a073cc83675d07f69b9cf7984ef5474987e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:57 +0000

    square straight chase runinto prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
