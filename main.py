32a7220946b632588aebfaba45a8929d69b1090c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:13:40 +0000

    Yy determined press design advantage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
