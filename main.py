98be747adb19d541277ac3c5b42280112fa731a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:35:07 +0000

    constantly nigga comment bit expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
