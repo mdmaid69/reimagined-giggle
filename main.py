0012a08b5c883fc14a5ee27a3532420b634fe76f
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:20:52 +0000

    anybody decision benefit comeround bang

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
