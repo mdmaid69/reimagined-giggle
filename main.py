893ebd5c5dfbf86d9237d7c2e633ac98b30855e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:29 +0000

    could rush drop anywayanyhow tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import time
+print(time.time())
