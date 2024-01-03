e129aeef8fec3464b3b570a4641cb72f35cb7b6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:02:21 +0000

    apply far telephone child not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_set(array):
+        return set(array)
