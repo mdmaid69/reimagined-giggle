14279d193f842c9e0337c724b3f6a9ea526543de
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:13:12 +0000

    holdout still recent future play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
