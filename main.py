0ef00a65e54a3a02932cc0a83527c70d46ce62ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:59:30 +0000

    soil exploration fetch board suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
