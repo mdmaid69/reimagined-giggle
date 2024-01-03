de99ef1219c2cd020ef34686cdb9bae6a1b412e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:08:25 +0000

    dress late and Vv tear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
