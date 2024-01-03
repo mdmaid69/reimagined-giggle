043af511c74d6578b909c453395c0f3316a9729c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:55 +0000

    smell lookback(on) injury nowhere inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
