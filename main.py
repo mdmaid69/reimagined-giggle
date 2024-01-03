463e8177fbac417c2bf9f82768f0c2e2fd98a67e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:30:08 +0000

    excited loss uniform first foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
