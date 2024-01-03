3a6ef32dca4b5f7599424df39f3f8ea7d01e6330
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:12:23 +0000

    song rent limit live party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
