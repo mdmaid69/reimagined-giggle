f15fa5c7ed5fa944df425c395d892015081b5309
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:44:52 +0000

    error someone beat cruelty child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def subtract_numbers(x, y):
-        return x - y
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
