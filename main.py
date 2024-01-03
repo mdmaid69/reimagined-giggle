7381ec33a27b4be1e5eb30a6c775381d0a2702c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:07:32 +0000

    miss alone often figure poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_list(array):
+        return list(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
