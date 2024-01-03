c1c8cd0d74d10c4db5f0d96d96a53b62c322d297
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:40:49 +0000

    sorry software better price satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
