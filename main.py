0ea40a3c64bc37b2b442fe1e70456aa6e7d13fc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:24 +0000

    trouble disagree power steep pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
