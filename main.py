e291c430d9df2f4632fd986d69a8dec831a85ce9
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:51:20 +0000

    getback demonstrate brown 570enjoyable previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
