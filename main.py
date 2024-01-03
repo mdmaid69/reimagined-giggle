c2bdd27e1de0203299e00323bb96c4e15f72121b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:23:26 +0000

    yes exit declaration accept straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
