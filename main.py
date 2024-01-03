c73e5fdad3fb4ef86fa5c4c3386d403b46494608
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:32 +0000

    increase border obvious call meet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
