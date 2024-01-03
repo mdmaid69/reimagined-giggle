c6df7d789122b66c5d830950cede1632f1ac1c53
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:24:47 +0000

    display juice pleasant giveback common

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def greet(name):
+        print(f"Hello, {name}!")
