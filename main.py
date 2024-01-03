9f3d102e0199541f75a55d3e8fe647d5436047e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:43:36 +0000

    lose dick image operation frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
