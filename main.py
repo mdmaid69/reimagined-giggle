22de72b8b72415cee743e4b3cde236e214b680b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:15:03 +0000

    system wrap favourite file getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
