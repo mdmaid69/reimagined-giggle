b71017ef4a4e748c1d099e43d450f06fa05a6f24
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:07:34 +0000

    and advanced club party weigh

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_power(work, time):
+        return work / time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
