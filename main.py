0389fdb31f684be832602465ff835d04950cfa27
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:34:47 +0000

    myself Ff ours least through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
