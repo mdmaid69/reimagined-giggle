d73f6abf4be7ea535166393115bc977ddf256980
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:40:18 +0000

    mind switch immediate run determined

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
