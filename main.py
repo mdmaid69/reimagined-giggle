0314589f274c5b8a3df141e8d419b96cf00672a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:20 +0000

    rare fork really form exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def is_even(n):
+        return n % 2 == 0
