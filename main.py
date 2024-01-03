25fc8cd411bf9b1e08352c9cf93f726fc1dc1848
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:55:52 +0000

    number fly miss raise load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
