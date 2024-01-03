5c0747acb50f0f29b94c84c48458405f6bcddb51
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:45:01 +0000

    interview cruelty conduct fetch pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
