43083dd34d071661565300374bcaccae4499a133
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:16:01 +0000

    bill post generally holdup between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import os
+def get_current_working_directory():
+        return os.getcwd()
