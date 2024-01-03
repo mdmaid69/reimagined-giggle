1e333fb726eafad0330bda4962d0d4e5b98c673b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:16 +0000

    surprised pump Ww Gg meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def split_path(path):
+        return os.path.split(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
