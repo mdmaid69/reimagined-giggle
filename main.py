367407ca62b95f72038c6e13d61e4ace0ebe260a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:41:37 +0000

    noisy pipe watch underneath education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_current_time():
+        return time.time()
