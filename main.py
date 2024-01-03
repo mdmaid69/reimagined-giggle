650f1646077debfe5e41d9c2fba823c9fb7220fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:17:19 +0000

    guard play scrape far guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
