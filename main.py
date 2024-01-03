4fd5fe8a1259e37d6229193357cf4eedc812be6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:23:49 +0000

    stretch mood underneath pressure switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
