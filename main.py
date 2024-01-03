9da6c58be5a3a743f81b6b16bb8678ec17f9a99f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:47:21 +0000

    match strict draw close on

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
