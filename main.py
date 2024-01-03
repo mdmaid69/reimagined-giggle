d4cae2e531526749f50d14c7ecff6761d207c4e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:08:06 +0000

    shopping personal friendly bottom bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
