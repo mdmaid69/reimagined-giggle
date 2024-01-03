cdf6e1a052b868a4adf7e47c7133efd6122ca9a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:42:01 +0000

    quick hurry whichever nose blame

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
