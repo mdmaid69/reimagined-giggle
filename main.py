9cf7c4466c90d6b81dccab165e61e92d4fc6f960
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:58:39 +0000

    moment show they spread pay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
