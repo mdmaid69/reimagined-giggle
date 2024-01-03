46aad8dbafdcf23dd9af8f47a226344be40755bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:10:51 +0000

    along well deserve recognize whatever

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
