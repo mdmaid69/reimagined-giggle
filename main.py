e975a871f3fe30edbb213036ce3b4286658b38b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:14:53 +0000

    full private previous fortunate tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
