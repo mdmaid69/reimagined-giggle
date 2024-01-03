297def5df8fa048b01e21bff9dc4140b6388af99
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:00:26 +0000

    steer benefit release underneath awake

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
 import array
 def get_array_from_file(filename, typecode):
         a = array.array(typecode)
         with open(filename, "rb") as f:
         a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
+def calculate_density(mass, volume):
+        return mass / volume
