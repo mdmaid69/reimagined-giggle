1533b0e00f876b445ed4a25a9f7ec36db7430434
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:25:12 +0000

    visit related congratulations damp argument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
