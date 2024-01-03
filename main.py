a1d2ea300e39c8d3b4e151dc6ede22bcdeb2a224
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:30 +0000

    scrap steal keep organize worst

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+print([x**2 for x in range(10)])
