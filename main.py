eda944bf339565c1d45c662698dd22beb13b437c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:42:18 +0000

    key point somebody means stay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
