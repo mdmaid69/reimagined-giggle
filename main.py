dbb37241b72067311e32f291b24bacb16889f36e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:35 +0000

    have recent lead nature road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
