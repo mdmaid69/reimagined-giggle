1a89f8808e19b2713e9b2fd36d182c77cb5c67ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:59:44 +0000

    interfere indeed blame search modern

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
