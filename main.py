120cbff4d1d4ebb33725a58f833420986c0f6b45
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:22:21 +0000

    substance wave emergency certain protest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
