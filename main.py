9e3ecf748f268380b81f9a36ad2a5b5eee8ef7cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:24:23 +0000

    high affect glad border ashamed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
