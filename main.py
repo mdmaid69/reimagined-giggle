ef1673e6be87ac19e9fbe48303bacf48a19da7e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:40:58 +0000

    practise certainly repair teach modern

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+name = "Python"
+print("Hello,", name)
+def find_max(lst):
+        return max(lst)
