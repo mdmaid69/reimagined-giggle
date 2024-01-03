68fde3164d2709c51ae0e6a4a5b9faa4ad763597
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:22:32 +0000

    away fear remember arrangement separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
