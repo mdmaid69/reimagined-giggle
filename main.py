afe24d9fbdee2bc28f27a8cb68c08918059e3ad1
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:59:41 +0000

    meanwhile whom storm turn bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
