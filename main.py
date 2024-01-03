e6b48d4807a566bcd9cf85a10f6b778309294a7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:36 +0000

    win surface church pupil stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
