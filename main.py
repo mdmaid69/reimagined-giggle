c6826b4125fd6327bf9eaff55e68b2624dd60a9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:39:18 +0000

    tape squeeze surprised hers phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
