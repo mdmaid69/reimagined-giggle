b18e4d5d0416f582ea5a3ff281f1cdccc462f4a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:51:19 +0000

    little takeback operation swing game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
