711bf08fdd5b07b1953b2bdbbda0282daf4c7ed3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:39:46 +0000

    Pp rest phone bet exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
