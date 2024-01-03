472c85ddf46ca1f0cfbf744ee42e4b61eb3e9ed3
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:19:33 +0000

    turnout valuable tear come square

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def reverse_list(lst):
+        return lst[::-1]
