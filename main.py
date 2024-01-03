e0908c5c5f9216de60136e9c3576b5d40cdb251c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:38:57 +0000

    live offer lookafter match explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_string(s):
+        return s[::-1]
