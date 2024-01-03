6798a1da6b124fa273bea0a145d666aba8ee9db6
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:50:11 +0000

    present use waste event complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def multiply_numbers(x, y):
-        return x * y
+  def get_base_name(path):
+        return os.path.basename(path)
