336187254b3bb83220c9541b45e4c4f407286074
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:05:33 +0000

    pity many still odd remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
