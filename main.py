9c5afa0b633da5ad9345a26ac676ec7c28387dda
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:39:26 +0000

    snow Hh dick admire wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
