1a83684638812fb347c7136fade9d90063dfbec3
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:11:58 +0000

    swallow it speed give exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
