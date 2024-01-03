a6c65ae10de1876c98512220953f00857971d3b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:12 +0000

    meanwhile shut blame comeback embarrassing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
