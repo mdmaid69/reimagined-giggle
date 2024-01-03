624445e14d758fe80301bcfb8920993092428544
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:48:59 +0000

    male near switch damp protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
