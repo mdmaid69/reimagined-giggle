a8185c1a8afbb935647f50cdd2f3d59f5c012fd9
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:39:04 +0000

    powerful record close breathe rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
