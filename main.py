15e871bc3f6700daec9ff95e2d3f7b3db09ef6a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:18:14 +0000

    keepaway light record properly dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
