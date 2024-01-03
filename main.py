402abdea4d7b2067ba1b6b6f6d69a95ed2a83da7
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:24:46 +0000

    scared need offer mistake relative

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
