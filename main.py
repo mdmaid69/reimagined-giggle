52cd8da3676a4116995550be35ed1f09c3e694da
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:23:25 +0000

    return level shame immediately come

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
