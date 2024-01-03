dd555fa3a7df991ae2607af553a3d43043c45698
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:05:09 +0000

    getback Ff OKorokay track store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
