b1313f9a06ce0adcc19a9c9ecfb9b486d228e315
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:40:35 +0000

    love single fast decide contribute

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print(sum(range(10)))
