271f52b5fbb511102103163f7495bd7efa1717cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:37:44 +0000

    genuine estimate basic what normal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
