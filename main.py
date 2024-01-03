040bd3a161535fd5a3bbcf62dbb359a1404e5774
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:15 +0000

    real appeal plant wrap attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
