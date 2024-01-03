78a1c5f18426b9c3a33b017008f39d7a9108d466
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:48:39 +0000

    port crowded thanks warning meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
