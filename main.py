64be721d77cbea9c57d8bc93a1419e078cfc8b5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:53:29 +0000

    declare paper dear wake friendly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def sort_numbers(numbers):
+        return sorted(numbers)
