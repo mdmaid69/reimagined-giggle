aed16f7afa299fc5aeb1ec8cf54efc6fffe2aeda
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:34:30 +0000

    everything swim brush length supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
