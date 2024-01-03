522f0239c18a3bfa798c4f83a385f6f3280a2492
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:00:30 +0000

    keen much pin quick hour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
