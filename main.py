d24978d7a1e135d34851d0160f0dea76c6c4d3e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:02:24 +0000

    drop rub rough shade mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(numbers):
-        return max(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
