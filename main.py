43b231b196acf8bcf9a15260e2bc935496a2c874
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:08:41 +0000

    conclusion dream create tear of

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
