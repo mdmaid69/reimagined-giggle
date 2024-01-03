9a9d338c833cbd9c5e8e4d70f43171a17193de29
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:31:52 +0000

    even storm your past tone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
