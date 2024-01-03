dc2e9f6ea80c2cb1c74a91c5d7eb393bf982060d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:33:58 +0000

    acceptable image as plant fault

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
