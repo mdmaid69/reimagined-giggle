673925099943c469a32a118d5d5ee25f84e20978
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:55:15 +0000

    curious secret feeling frame factory

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
