ce57a392b3446b94b9cece95f13011da49f6a5b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:19:17 +0000

    quick stretch entry sentence squeeze

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
