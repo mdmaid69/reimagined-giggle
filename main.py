be12f89fdad959b15f5afe03c297c77d75b36a82
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:37:48 +0000

    seat cut guess cheek clean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import sys
+  def get_python_version():
+        return sys.version
