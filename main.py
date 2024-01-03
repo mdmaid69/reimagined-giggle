bac2b5f881a47c6ed1a991d5c01ded50776af73e
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:44:06 +0000

    shopping air low among party

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def is_odd(n):
+        return n % 2 != 0
