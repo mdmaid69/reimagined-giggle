0b2f3f613a26a77b069073257db3cce49a72a5f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:05:27 +0000

    sick public bush exactly support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
