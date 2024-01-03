2bf548242c9992a03a96c6cf805ddce9de0312b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:59:25 +0000

    steep model reserve switch furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_current_directory():
+        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
