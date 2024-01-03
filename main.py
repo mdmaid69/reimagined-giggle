471227f39e18b19ec45d67e1dd1cd85b34fde5c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:51:59 +0000

    reduce black stage seat bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
