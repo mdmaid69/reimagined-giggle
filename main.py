41f02eb38922f9f213fbb0076ecf3c591761d678
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:51:31 +0000

    serious children snow people fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
