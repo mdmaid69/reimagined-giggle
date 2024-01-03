d43b231e7ac333a8dcd806dbeff5c81919e523cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:10:52 +0000

    disturb close under enthusiastic train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_item(array, i):
+        return array[i]
