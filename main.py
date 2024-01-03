65ee18240a8d687c9256ce30c47ca88d2a926018
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:09:51 +0000

    tap brave trip different whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
