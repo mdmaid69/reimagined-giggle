84eec7340bc8fcd02d2fb381d46249070fe3a206
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:59:39 +0000

    runout direct moral public crime

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
