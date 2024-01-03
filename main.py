6479b320eacc659e5f6132a98073fb9ade156ae3
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:03 +0000

    swing company reply advantage purpose

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
