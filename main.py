e9af45926db2366be0ba60b80b951597d62bd436
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:14:08 +0000

    lean interested influence wall pass

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
