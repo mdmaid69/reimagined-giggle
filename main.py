96fc416010d127e7138ecb699f0fd1dbb9e6f148
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:31:56 +0000

    hand middle compete broken regard

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
