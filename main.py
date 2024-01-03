302fb2f5371468394b997fc71c398e4eec49049f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:00:15 +0000

    we majority slight leg generally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
