5f03ce75dd26932ae1f80e0d21afe08103caf423
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:58:11 +0000

    anyone somebody drive thinkover criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
