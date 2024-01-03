6daf0aa682948bef7e192120f021bb7f8953cc13
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:47:37 +0000

    sound drink beneath rich task

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def convert_to_binary(n):
-        return bin(n)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
