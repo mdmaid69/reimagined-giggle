1da46521d1846704bc1e22eb1d1addd0577a158d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:48:27 +0000

    shine catch rate explanation table

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def convert_to_binary(n):
-        return bin(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
