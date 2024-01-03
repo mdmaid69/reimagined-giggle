a5a9418bb674578c027edfd7ccee7a82f7f461c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:32:33 +0000

    second full substance suggest effect

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
