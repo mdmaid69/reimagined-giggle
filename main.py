cea6dbff61907daa0f0fbdbd6b2120de397e1751
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:51:18 +0000

    early probably figure argument attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_list(array):
+        return list(array)
