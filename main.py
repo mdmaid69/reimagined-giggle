5f94bffa39518b3b08bcd75996940ef40def2460
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:20:55 +0000

    callback no ring character odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
