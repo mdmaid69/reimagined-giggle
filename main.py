d196a95fe72fe867cbf6ba3d27d3e0776f532de1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:22:38 +0000

    never matter wonder state him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_factorial(n):
+        return math.factorial(n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
