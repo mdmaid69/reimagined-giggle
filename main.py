0274322e3fd6f27bc893560c696b87192e41e697
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:12:43 +0000

    relief push outside trust under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
