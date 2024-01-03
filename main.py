f5da6eabbf9ea3735dcc9edb47e179614fe22ce0
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:30:04 +0000

    hello you tell Itself music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
