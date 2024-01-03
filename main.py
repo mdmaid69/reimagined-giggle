5a7ebc9cff266fe3903f732dbf80189b0f9e56c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:00:34 +0000

    star love angry bet black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
