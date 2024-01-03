348415824e8d827013ab94ecac9ce290c7a24e06
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:41:59 +0000

    embarrassed relief mainly explanation cinema

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
