fe11241e2d4c1f75a77f6a2199fc272b54256604
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:08:48 +0000

    occur complicated plan time priority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
