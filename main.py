e711f9c6bbdf29423835f68dd7bc346356678c79
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:29:23 +0000

    thankyou translate spare image character

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
