e88abf134699037db1777c9cf4be91ade111561d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:56 +0000

    inside number bunch straight guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_permutations(n, k):
+        return math.perm(n, k)
