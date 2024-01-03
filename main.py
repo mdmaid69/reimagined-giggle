e277c5fe0b51e1992ba4c87413311d5bb4bbd4e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:32:46 +0000

    door mean search safety bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
