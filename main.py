cb7caeb5c065eb6884ee1f4b83e9d530b2cefb78
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:03:54 +0000

    do great information temperature fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def reverse_array(array):
-        array.reverse()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def convert_array_to_bytes(array):
+        return array.tobytes()
