0ff9899944d5e129445f90749b02ff7143d1d7c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:11:00 +0000

    accidental there rank willing noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
