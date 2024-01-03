4dc2df60aaa374d5f6641ac3ec36a279716140aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:42:17 +0000

    shake wide clean minimum pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
