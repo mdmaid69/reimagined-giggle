8415913ab02927f68b8b163d7d518e21d161fd00
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:32:00 +0000

    thirsty mainly in comedown pump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(5):
-        print(i)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
