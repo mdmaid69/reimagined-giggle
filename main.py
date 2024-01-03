45195fb747468819d89eab3aeab0137e46edfafa
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:14:26 +0000

    market genuine fork keepoff talk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
