d048ff5a5dc98054c73bddbbebdbece89abaab6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:58:52 +0000

    display lead both flat pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
