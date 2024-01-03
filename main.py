dcbea497fb1cc9ce0371c4e02d050b1e523e866e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:49:35 +0000

    gather spend black punish stay

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
