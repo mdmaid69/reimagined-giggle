709e0931f69bf63532997dba200714e983761f92
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:34:44 +0000

    turndown plastic workupto fortunately regard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
