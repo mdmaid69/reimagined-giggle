56cdaf1594f0201e50b6b0c3ab22603b3a16a2b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:28:20 +0000

    about hit cross genuine if

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
