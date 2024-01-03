ee589298a72d3d11fe634a35bdfc7df0a179456c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:16:43 +0000

    choice lunch what whoever season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_item(array, i):
+        return array[i]
+def find_unique_words(sentence):
+        return set(sentence.split())
