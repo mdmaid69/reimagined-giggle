e410af44fddeb55d49707023e9b678cf3dd6796c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:51:51 +0000

    press search back paper myself

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
