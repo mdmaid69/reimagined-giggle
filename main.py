5c183a1258617b1c68029cdab2e5ea9fe8e8d001
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:57:44 +0000

    beautiful hold far payment pin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
