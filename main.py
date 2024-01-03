2f447e87867042145c7a0d28182f1349a9de45d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:09:41 +0000

    name neither damp build honest

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
