2dc6f6f3f7a6fac4a3ba9395a3b1f03713386084
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:10:28 +0000

    your experiment fill hate onto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_odd(n):
+        return n % 2 != 0
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
