faa362e3e04171a79a23f26fb6dc2d16dafed827
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:58 +0000

    somebody squash achieve eat niggaacademy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
