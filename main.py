fa89e51af492ad02f4bbc0ccb84a3610558191c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:24:58 +0000

    moon on warm congratulate treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
