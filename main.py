9c09ac3774e3f97f17c159921bd9269b425fbac4
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:40:04 +0000

    dear spite bright interrupt giveout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(lst):
-        return min(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
