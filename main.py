45c7cb9f0f1fb06dd58ad366aa5aad83135f9f2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:27:53 +0000

    calm kick opportunity government behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_speed(distance, time):
+        return distance / time
