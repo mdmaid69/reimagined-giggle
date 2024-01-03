42af85ab330e75645a098798c37180e8e0d69149
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:32:07 +0000

    OKorokay up left wild cope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import math
+print(math.pi)
