6a7c773fc31bbcf0fabd79c41e70a782c40f2d15
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:02:23 +0000

    opposite pay touch container far

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
