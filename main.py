75af44188b49211f5bc44b7c6f7a6c8f59607013
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:20:35 +0000

    fear cool stop of criticize

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
