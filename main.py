8bf62cd4fd1708dbeea2042574f7ef06543ad7f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:50:26 +0000

    affect shame load uncertain turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_speed(distance, time):
+        return distance / time
