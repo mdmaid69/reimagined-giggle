b4adbb6cf8dfc41010c4eb446ac9de9e15a6c84c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:29:34 +0000

    peace definite woman port now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
