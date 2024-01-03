d43df500fbb3b3bddde8dd085f941708dac1f00a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:13 +0000

    unusual result considering thinkover train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def find_min(lst):
-        return min(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import time
+print(time.time())
