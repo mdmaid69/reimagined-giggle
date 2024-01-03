40285124506918be2ad4f787e91c97661825e364
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:47:05 +0000

    waste before lift hope black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
