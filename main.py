91c9b6699a342ca7798c76ac19ddb5270a9b70a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:14:26 +0000

    can confidence scream prove true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
