a7fd924c10ab0b788077210b78abbfda97d74bc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:57:07 +0000

    spread advantage Bb environment expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def is_even(n):
+        return n % 2 == 0
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
