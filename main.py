eb651e53ad924bfcb5fcf717ca36c78b6d8468b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:55:11 +0000

    interview control grateful nervous address

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
