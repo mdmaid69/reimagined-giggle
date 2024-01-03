20e99d6ad4147b8a5da267e4573fd98787289284
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:04:35 +0000

    hit round trial hold worried

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def square_number(x):
-        return x**2
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
