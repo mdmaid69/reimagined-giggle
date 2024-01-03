a2dae2ccef28233204cd9009da72e1effb9beaff
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:27:14 +0000

    tooth interest comeup city shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
