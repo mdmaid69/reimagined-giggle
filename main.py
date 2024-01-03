d8715af5e5e3b3febc31877b32a4e9aaec2aac09
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:33:28 +0000

    unusual danger building wear tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
