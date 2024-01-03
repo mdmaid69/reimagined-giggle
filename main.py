0fe0150b93ae034e10f86f44ce1688c552db40a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:57:14 +0000

    circumstance try fetch ever water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
