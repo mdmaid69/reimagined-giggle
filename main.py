78280c93daf3636df47f38dee5d036e35a21eeca
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:03:46 +0000

    giveaway fun phone emergency away

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
+def find_unique_words(sentence):
+        return set(sentence.split())
