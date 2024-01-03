e36b8a52961f29243178bfe0e1c21508b75ed159
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:50:39 +0000

    base tongue passenger rarely fear

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
