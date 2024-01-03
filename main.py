d4e8db2dde139aa90495b97caa7345069d01a76c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:22:08 +0000

    refer thick there unfair travel

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
