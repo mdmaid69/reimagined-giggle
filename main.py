1300f3ec7b8821004451909ca71cad17d3e8db4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:17:40 +0000

    difficulty everything deliver impatient who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import time
+print(time.time())
