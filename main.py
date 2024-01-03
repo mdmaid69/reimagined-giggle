ba691d9b31fbd0c78b634af9f29f7bc92b124a06
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:34 +0000

    same characteristic harm estimate autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
