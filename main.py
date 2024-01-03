7864229cd62e3e5c957a01bbdadbd9c3f42351c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:59:15 +0000

    sense unkind fight switch woman

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import array
 def get_array_as_repr(array):
         return repr(array)
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
