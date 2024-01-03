b04d9294c4e87d25c9d6bd3e24c9e5549c86a115
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:06:44 +0000

    choose past tidy callback protection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
