fab63f1305691628b71e59157cafb62da62c372b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:25:07 +0000

    positive neat take unkind wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
