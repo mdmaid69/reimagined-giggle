4deb409a4de85d389ee99a26863240a600f1d9d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:18:13 +0000

    purpose loss worst previous back

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
