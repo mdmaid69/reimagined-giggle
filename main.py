5c597bfbb517b2008867d79cc49fdc929149c03c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:06:52 +0000

    breakdown handle beneath good thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
