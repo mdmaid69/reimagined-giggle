e0fcbc852ad82e15b16a6c1e582afc0f91822bd3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:47:47 +0000

    track circumstance borrow light true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
