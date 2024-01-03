0d5424bfcb9d3315670fab1da1ca8b43d96e9036
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:48:22 +0000

    ought practical reaction hurry explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
