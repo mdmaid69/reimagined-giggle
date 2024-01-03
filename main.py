5a2958983e0ea24312f5e7dcf5c942ff504badf1
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:18:52 +0000

    promise warn manage love onto

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+name = "Python"
+print("Hello,", name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
