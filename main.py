0f8d65ee2029c571882fcff55993b27d3f57f2b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:49:15 +0000

    centre task earth since hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def is_even(n):
+        return n % 2 == 0
