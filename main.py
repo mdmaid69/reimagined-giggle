0ebce129dc7a019a551a857345e4561b255bc7c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:21:00 +0000

    accordingto neither equipment attach criticize

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_power(work, time):
+        return work / time
