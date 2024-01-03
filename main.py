9aa5394df72be51c6c24f9987c32604ce475a5bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:51:39 +0000

    much environment friend ignore flat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_power(work, time):
+        return work / time
+name = "Python"
+print("Hello,", name)
