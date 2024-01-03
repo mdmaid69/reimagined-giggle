083b3c3cb62fef55fa3388e2984f13ee28a07b92
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:13:49 +0000

    drive some complain upwards late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
