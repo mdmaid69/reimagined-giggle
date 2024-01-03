2794d1f68e66445d938afa5e2932589abd8d8320
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:33:41 +0000

    battery tie cover ability whoever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
