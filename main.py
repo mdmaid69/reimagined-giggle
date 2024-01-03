b50a9333ace06549a3ddeacadc7c93c88c412a72
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:09:08 +0000

    571enough snow personal increase autumn

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_average(lst):
+        return sum(lst) / len(lst)
