88da7c1f193d6f6d40ed5569c535f574abe776b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:53:26 +0000

    glad clear hope kill else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
