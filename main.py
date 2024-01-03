5bc64eaaf1528234941c9e380549dddc6983ff3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:17:28 +0000

    test Pp constantly pin recognize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
