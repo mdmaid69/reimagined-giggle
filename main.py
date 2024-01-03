be176afcd8b2ce9ffc97bed1853cad2b842cb16f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:43:24 +0000

    love scared squash round aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
