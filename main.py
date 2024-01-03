7b6ae72ba445fc134792a609350cb49f8b640323
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:42:35 +0000

    opportunity interruption hard notice through

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
