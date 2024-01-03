2f0f68d10ad3eefe9c7dedc7ba35de72a1295b35
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:41:21 +0000

    excuse sister under skin wrap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
