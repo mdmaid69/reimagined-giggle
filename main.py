bdddcaf5298bbe1c961470982435daf1da647703
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:57:37 +0000

    mention fairly spot wash freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def remove_duplicates(lst):
+        return list(set(lst))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
