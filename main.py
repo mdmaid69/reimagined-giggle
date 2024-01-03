967ab2325c93bfb9f7d08fa87767f16a687d5e4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:04:34 +0000

    dig OKorokay only record punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
