739e84a622b8881d2bfecf22d3c602233751113d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:26:07 +0000

    passenger tape relief noise bread

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_floor(x):
+        return math.floor(x)
