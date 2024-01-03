28091b358a141172e1b9d9e02efe9ff3261e30cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:18:01 +0000

    favourite trust once upstairs platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
