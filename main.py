d205962a92513be549ec9c7417156be31d7255e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:12 +0000

    unhappy borrow final confuse commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
