254f55b257611dd71a020b2138aa3a20458bb09f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:55:43 +0000

    turn event evidence loan measure

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
