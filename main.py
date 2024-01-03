8a224841b21ccda40994804646254b473e52a6a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:07:08 +0000

    position workupto permission manage brief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
