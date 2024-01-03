8a43a37bd76237a36dc89b71496980b244b74a4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:50:06 +0000

    slide afraid foot public by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
