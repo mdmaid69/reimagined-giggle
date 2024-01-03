01a4deb1426af43350b39d42281f2d3c92f2ff85
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:23:14 +0000

    travel course similar lead door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
