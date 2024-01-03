c0537cda81f5d752afcfcf50b38a0d477f1dcd93
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:55:31 +0000

    several turn turnout murder song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def find_min(lst):
+        return min(lst)
