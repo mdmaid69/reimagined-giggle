2c1c049ab326e59ec60c06f3c5386c794ba896c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:34:05 +0000

    drawing cheque suck damage turnaway

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
