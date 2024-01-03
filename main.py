e529b075580a96c6eb6264c202d9b0d968633cf5
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:40:30 +0000

    hour bright drive sense track

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def count_words(sentence):
+        return len(sentence.split())
