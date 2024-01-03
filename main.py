7d19b4e5089787be3fd8d7763f6a7a5e84666b7b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:20:54 +0000

    point base among steady interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
