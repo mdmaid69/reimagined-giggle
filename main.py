f6010e63a468353f5d91e515c4597cfb79095c59
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:58:09 +0000

    circle glad polite spot neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
