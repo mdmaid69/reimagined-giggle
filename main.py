6db1c04bf98c4ddd55c4ed5fcdafbaea1186d905
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:53:43 +0000

    limit range plan nose sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_item(array, i):
+        return array[i]
