f4be32df8d2bd63a9886ebd2f6456e74c0eeaf09
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:58:55 +0000

    wash science split manage either

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
