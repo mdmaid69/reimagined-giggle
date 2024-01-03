9e7aabc669392d765a585a11d1e2553a4df64027
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:41:42 +0000

    full help honest disturbing length

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
