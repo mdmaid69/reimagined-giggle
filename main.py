57001c8a043d892b54eb3ea7ca0bb94539b98787
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:53:40 +0000

    least announce excited loud pull

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
