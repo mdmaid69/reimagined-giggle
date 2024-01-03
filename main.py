e319ffead1c902843879ecd67ef4361ac86ee5ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:50:41 +0000

    worst father although permission uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import platform
+def get_python_version():
+        return platform.python_version()
 import math
 def calculate_cosine(x):
         return math.cos(x)
