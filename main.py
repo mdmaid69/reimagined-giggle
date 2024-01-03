34e3bef3f5b45ecf8935e8dec068294a337af2d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:54:26 +0000

    story aim private hospital to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
