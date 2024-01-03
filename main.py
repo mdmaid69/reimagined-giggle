c3a2d37e4995b2d2e2d81df30a5f63f3113e1858
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:49:17 +0000

    aware who dare damage scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
