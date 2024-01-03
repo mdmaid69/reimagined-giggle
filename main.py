6a7d2c2d2d4fa6ad13c22def61089b7e48eadeec
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:06:31 +0000

    emergency clue certain chance judge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
