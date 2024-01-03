57b3f4abe957418544f2421a9bc7a6133aabe6b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:03:28 +0000

    stone worry instrument substance thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
