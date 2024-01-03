12c350f2ad1bf67a6160dcf8654748fef383fb21
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:52:13 +0000

    reserve Yy plenty real embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
