055af071571da7e34773634e19eb6e4b9bdd548f
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:01:01 +0000

    thin hers open must before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
