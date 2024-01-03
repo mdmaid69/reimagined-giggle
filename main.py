5d33bc39cf29fe240d5e2511dfa8bf512e709bbc
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:54:43 +0000

    steam eye far stupid slow

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
