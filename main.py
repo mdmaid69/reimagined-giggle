09ca19cb7ed4a33bf874eb580235249c13902388
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:30:50 +0000

    Ee name hurt shame straight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
