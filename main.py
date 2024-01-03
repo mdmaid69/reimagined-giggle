266ed9d321d72d5a97deab7e1bcba86748dc2b9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:01:49 +0000

    square rub today your record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bool(array):
+        return bool(array)
