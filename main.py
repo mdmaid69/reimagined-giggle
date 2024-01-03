fe5b1467f54eac1ffbe6083d5c6b669029e5804b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:15:57 +0000

    substance warning thankyou doctor protection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def convert_to_octal(n):
+        return oct(n)
+  def count_elements(lst):
+        return len(lst)
