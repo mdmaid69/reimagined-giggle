a8e25bb2bbf7b4e1190d28ab181fb9572a9db457
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:23:06 +0000

    worse permission closely tread problem

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
