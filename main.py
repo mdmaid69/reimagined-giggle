35fd007ea06d1c35211c0872985d6c427ad8074c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:16:23 +0000

    bury snow dig organize runinto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def check_if_array_contains_item(array, item):
+        return item in array
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
