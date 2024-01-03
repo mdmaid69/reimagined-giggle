fa9917ae6c843df6808b4563be6f2e3a129ec472
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:40:26 +0000

    concerning loss female workupto accommodation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def convert_to_binary(n):
+        return bin(n)
