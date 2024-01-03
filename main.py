a8b9c8f50270b46b75da08afa137807b9db6a1ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:12:19 +0000

    brush lock interested lookup forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
