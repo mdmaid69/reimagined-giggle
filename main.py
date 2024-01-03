ad1bcebd0976eca24f0b96edaebd2f8d004b1e62
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:08:41 +0000

    unkind immediately muscle earth drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
