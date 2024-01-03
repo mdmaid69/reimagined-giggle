fd8d0e1e3349fefaf5896bb8cc8f6d35579d3f6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:34:54 +0000

    proud form consult port view

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
