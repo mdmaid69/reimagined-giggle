eff7db15ca00d423ae6a9c1128420a6dc064ab49
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:20:47 +0000

    myself teach free gradually birth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
