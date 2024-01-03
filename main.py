4b8bf77013f6d0f6358e51265e7fb3d9eaf8eb61
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:19:30 +0000

    method neither pretty degree game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import collections
-def create_stack():
-        return collections.deque()
+def create_counter():
+        return collections.Counter()
