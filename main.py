666e7121355ab06f8b4247c51971164e9e07d7f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:08:10 +0000

    admire narrow post pity expect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import sys
-print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
