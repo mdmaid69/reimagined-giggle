5cabc15fc6303655a0640e2bae8a616d491c0d57
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:54:42 +0000

    shine everything rough getoff send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
