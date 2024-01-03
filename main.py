c7d9f46d2d16dc7c808c65830c776007a4da70a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:39:27 +0000

    shrink through wild plastic official

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
