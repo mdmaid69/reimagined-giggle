9e978cd7312991650ff71fb497e71a01056015b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:28:45 +0000

    control 572enter discover centre we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
