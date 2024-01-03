dd2640ed49a367c62d58f8870abc1b8d339cc24b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:46:47 +0000

    earn official dangerous hit pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
