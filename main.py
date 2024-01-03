28efca5c01835df6f31624f18f81b77e5f3ebaf6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:46:30 +0000

    weigh scene load note would

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def reverse_list(lst):
-        return lst[::-1]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def clear_array(array):
+        array *= 0
