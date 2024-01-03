ea556c22c98272343c2c77482447c0a96d4c6b81
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:59:37 +0000

    music tap invite familiar wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
