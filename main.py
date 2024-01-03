c292b065aef8854e63e6dd313f3e4a6c69304c39
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:42:00 +0000

    week scale debt intelligent attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
