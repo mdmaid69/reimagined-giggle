d8a0fbbb6b56a8279a499d69f1cb9d7e9ed25b10
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:00:57 +0000

    relax rent lost chin material

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
