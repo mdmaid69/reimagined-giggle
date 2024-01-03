b707bef1473da2bd51bf2c771d3652e6e2d615a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:33:07 +0000

    talkinto away blow whether laugh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
