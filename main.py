48aef918faa2c1c5b6faf07473296cbe2540151f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:09:14 +0000

    event machine turnon disappear laugh

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
