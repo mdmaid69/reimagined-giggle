e8f858cc05fbf9e059cc7cc2be97b5222ed007f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:42:58 +0000

    memory wire neither hot dead

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import getpass
+def get_username():
+        return getpass.getuser()
