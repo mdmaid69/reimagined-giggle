8f67ca30a533737c5951c959d11af38bbb1c6346
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:15:14 +0000

    well no closely dance car

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def find_min(lst):
+        return min(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
