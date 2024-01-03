0ee1008de1b8df18d945753a736366916d5c2b41
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:51:10 +0000

    wipe flat welcome fine report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def reverse_string(s):
+        return s[::-1]
