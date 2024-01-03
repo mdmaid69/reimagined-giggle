ffae5d56d997a8bc394c2e4a089ad2c088a79bfd
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:44:46 +0000

    branch pronounce there advantage concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
