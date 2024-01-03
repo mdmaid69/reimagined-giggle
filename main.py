1e7559613a2f2137d60470c5528d48c1db546521
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:41:09 +0000

    habit probably married score feel

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromstring(string)
         return a
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def create_stack():
+        return collections.deque()
