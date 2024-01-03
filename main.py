7fc1041c8ef5f1298dea764a420e43cf08f678df
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:05:47 +0000

    cross ground press answer sorry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-print("Hello, world!")
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
