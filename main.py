47cf68a871c86bdcc55a9c1166536e905e599cde
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:53 +0000

    glass interruption wonder unit disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
