70df8e0fb63d223c5ebcf399f6856372b7a1543f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:35:17 +0000

    office solve bring comeoff present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
