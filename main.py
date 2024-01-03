0efba966864fbba6600cae9d2ae64476895be34b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:23:45 +0000

    fire set characteristic recently possession

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_density(mass, volume):
+        return mass / volume
