bc414229fdb7c65d2cf3091b007baeb678812d15
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:12:24 +0000

    work against control 570enjoyable bag

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
