d1bea47f3d270785b151e105c3af4e4b3cc94435
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:57:45 +0000

    limit everywhere centre love suggestion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
