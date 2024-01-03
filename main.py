42c985f42e8d664cc4b732c85a097e4923d11f32
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:17:58 +0000

    more nervous interrupt mood that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
