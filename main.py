6dddd704057edf0f9ea20188f1e5ebe48d2fab48
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:34 +0000

    stage record over exhibition study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
