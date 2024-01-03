daa63cc489d83812767455dbdcf62957a83d460b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:52:59 +0000

    dick none sure pause me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
