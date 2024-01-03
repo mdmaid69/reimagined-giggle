d6013b10b594d60a2ff52e0efe476052ccef8ee0
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:42:12 +0000

    frequent luck alive bar miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
