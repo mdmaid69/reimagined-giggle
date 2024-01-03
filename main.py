a17d11f109b8b42e47f39cd8bcdee5e9ea4312d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:31:44 +0000

    before friend prove culture importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
