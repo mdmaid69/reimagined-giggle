b5243dff811b153de5327e9867416072e811ba4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:09:31 +0000

    noise glad stranger complicated journey

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def convert_to_octal(n):
+        return oct(n)
