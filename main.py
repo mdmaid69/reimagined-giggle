681fbe828352536d2ea4db6ce42893d62aa14c73
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:36:32 +0000

    refuse round open push blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
