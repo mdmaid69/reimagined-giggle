11311c965d6a6c7284f2c5c2c21ce72babb950fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:35:03 +0000

    deep undo cold late trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_sign(x):
+        return math.copysign(1, x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
