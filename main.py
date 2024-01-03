858933a5db1e9a860391d7b1ddfa65bf7694bd69
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:11:26 +0000

    stone disappointed ticket in explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
