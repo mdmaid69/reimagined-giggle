6244b2281f6293f7576066a82cee98ef86e430d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:28:43 +0000

    surprised idea cause ground original

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
