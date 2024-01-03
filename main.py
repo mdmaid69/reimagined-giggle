18192456422b61f97746678755c0bbb6d79ac859
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:57:34 +0000

    cause worried disappointed loss repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
