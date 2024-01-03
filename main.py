0304126e6b12b5d4bb57960991b67ca28e4e3512
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:03:51 +0000

    tape apparently trust extremely design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
