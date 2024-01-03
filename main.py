8dd2aab61b48bdf3c93b33fc09217833c9f9880c
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:31:04 +0000

    succeed moral fact month last

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_error_function(x):
+        return math.erf(x)
+print(sum(range(10)))
