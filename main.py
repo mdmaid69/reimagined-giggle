7a1fa72bc1180e7fa3e5e3a9ce005592bfc9c392
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:02:53 +0000

    scream friendly length cold question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import time
+def get_current_time():
+        return time.ctime()
