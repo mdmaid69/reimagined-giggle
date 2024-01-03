bd1abc6ae85caea6a24d9cc3c95dc8496315a31d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:22:04 +0000

    success realize bringdown raise mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_set(array):
+        return set(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
