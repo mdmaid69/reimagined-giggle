8a250e646f0f6a7b7238d021c76e92fbd9ba731c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:34:22 +0000

    service hello instead post switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
