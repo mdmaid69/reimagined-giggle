a52329e61e42912bbaface971a061fb6ea478c64
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:03:31 +0000

    Nn there beyond soft order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
