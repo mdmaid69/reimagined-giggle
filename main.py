6017cccb1f83df9821942c1f8d1e8a9a66c2f038
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:22:47 +0000

    treat song pain length tone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
