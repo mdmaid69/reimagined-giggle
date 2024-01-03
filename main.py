7cce6fe6130b9ed5cecd259670133fbdb8d70d95
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:45:15 +0000

    bother easy clearly clever wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_item(array, i):
+        return array[i]
