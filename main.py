89d0c154e36b586b3640f898971067ea77f2a22e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:17 +0000

    computer guard spoil from I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
