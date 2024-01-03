194cd068fdf3dc6e8625d8a48265d136ba6bf0f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:10:00 +0000

    cause container correction first lucky

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
