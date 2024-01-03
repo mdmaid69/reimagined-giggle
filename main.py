20c18241eed8eeff23a001c2c18582f6aeb0777d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:52:05 +0000

    bank explain dare bear thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
