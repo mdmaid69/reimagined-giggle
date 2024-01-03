970c20f0a4d314648185ff22cfca046621fbfcb1
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:24:01 +0000

    yes round somebody poison market

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
