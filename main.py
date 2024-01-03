ac1494fe6ce02b19dc25d92c87c1922eb1c9ca56
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:44:59 +0000

    government play breakup shut fairly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
