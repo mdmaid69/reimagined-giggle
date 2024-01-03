348e2530f49d430fbd11dbc0a2956505c89a96a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:21:40 +0000

    build experiment bad morning produce

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def is_odd(n):
-        return n % 2 != 0
+def get_array_item(array, i):
+        return array[i]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
