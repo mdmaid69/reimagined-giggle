4e1c472b87c2f528f4397a65b7b1fead757ece6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:16:13 +0000

    represent dig hope moment every

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def count_characters(sentence):
+        return len(sentence)
