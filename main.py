87ec39e9702dad9224e9cf9f38a588d209b1f9f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:18 +0000

    sticky light relationship area Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
