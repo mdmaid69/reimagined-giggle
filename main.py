9dedf123b2d6d25c563db7d7248ffaad5c166ccc
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:01:10 +0000

    action only mood genuine here

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
