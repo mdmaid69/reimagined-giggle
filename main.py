3f8f0b7f94b8b28c78f60afd14cc16586ad53ebe
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:26:37 +0000

    piece force you silent condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
