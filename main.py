1c4dc9fc8a056f75dee1fcd2f4c0dff225f1fee6
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:09:16 +0000

    town disturbing order either phone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
