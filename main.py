a96e67c6e2f616cc472ae305fd6b113a90bd5034
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:37:19 +0000

    false injury autumn class hungry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-for i in range(10): print(i)
+def find_min(lst):
+        return min(lst)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
