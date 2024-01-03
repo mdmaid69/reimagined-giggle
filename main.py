0b767a9d37cb2f5d088761ddceb5f911efd8606e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:51:43 +0000

    world evening downstairs performance ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
