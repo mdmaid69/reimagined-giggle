6c9070d35e231d4ceb104a25617aab87a0b68fe1
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:52:11 +0000

    distant tear tonight emphasis enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
