58a5c41886bd34d1512fe790a9ac867bc2de5ca9
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:53:51 +0000

    seem would inside shock flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
