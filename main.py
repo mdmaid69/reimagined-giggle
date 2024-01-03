12c3d8f3efac50547fd31597db84edd80ca96c66
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:41:39 +0000

    minority steep left arrangement relax

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+print("Hello, world!")
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
