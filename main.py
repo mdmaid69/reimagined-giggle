d11897229572f45c6b272bdecb47c3d4a03b96c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:37:57 +0000

    increase point usually transparent stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
