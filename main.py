037571f98fe503d915195d44c3b5ceb36b80114f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:38:41 +0000

    agreement possibility introduce reasonable steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
