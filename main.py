e9934ed1fa6bf59db7142f3361ae7007080b4eeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:06:40 +0000

    sensitive damage brave order since

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def square_number(x):
+        return x**2
+import os
+def get_current_working_directory():
+        return os.getcwd()
