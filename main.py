589b5e662accde45859f8a710ea99a1a8271b8f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:04:03 +0000

    somebody turnout wrong experience surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
