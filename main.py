ae2b2d164c7dbe293f8c8e193a1df0bd1cce721a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:02:40 +0000

    lookaround vote lean because instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
