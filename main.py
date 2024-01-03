3904360907a0f40345ab96a7bec57a1b0eae4ce0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:15:49 +0000

    forget shade open smile say

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def remove_duplicates(lst):
+        return list(set(lst))
