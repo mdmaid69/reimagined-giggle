2e170e2f889d88dad14d1552e6f383841cd4b289
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:25:34 +0000

    climb fortunately stamp safe event

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import collections
+def create_counter():
+        return collections.Counter()
+import collections
+def create_queue():
+        return collections.deque()
