304a84669ba72e4c59be89c4babc746ca0b0d686
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:11:33 +0000

    discovery spin Ss tour business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
