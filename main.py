2b0b900e6b22e707b026603a154821ea503d9864
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:53:04 +0000

    interruption accidental shelter great minor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
