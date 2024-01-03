ce0fbe8ad3e609d0f1d8939e544997ac3d03d062
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:39:08 +0000

    grow deep unkind more gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
