0a88c8cb1d8cf8084b201837e8c9c53ecf167c31
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:34:44 +0000

    makeinto mail paper breakfast it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
