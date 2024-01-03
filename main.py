093363e59121448179021ebe1f37068da14bd19b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:35:32 +0000

    indeed form pin front movement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def get_array_as_int(array):
+        return int(array[0])
