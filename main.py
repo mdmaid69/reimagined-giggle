4a8781b5d904b4145c1d93c5e64db5996efc2aa4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:21:08 +0000

    realize sort warning pure later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import sys
+def exit_program():
+        sys.exit()
+name = "Python"
+print("Hello,", name)
