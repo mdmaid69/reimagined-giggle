f50f64e41920c9e0250e196ead95ce8b0a46a304
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:30:23 +0000

    eye system who importance something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
