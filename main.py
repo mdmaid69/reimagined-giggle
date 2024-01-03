bcb6b126531d648f0883fe81b519b7413e06f5ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:16:21 +0000

    impossible shop piece activity temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import os
+def get_current_working_directory():
+        return os.getcwd()
