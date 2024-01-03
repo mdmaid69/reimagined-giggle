2bacfa44c87c301e0edc3dd92ffe9799ea1c53af
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:54:31 +0000

    slide school anything remove petrol

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
