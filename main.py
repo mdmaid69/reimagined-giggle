d87544150484d7f1ba594b4303cf15a935dd48a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:13:19 +0000

    food model movement spill active

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
