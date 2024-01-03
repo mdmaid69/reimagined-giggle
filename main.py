85dc5ac754034eee6a4b153028e816acdd583a99
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:56 +0000

    always tap approve smart somebody

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import os
-print(os.getcwd())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
