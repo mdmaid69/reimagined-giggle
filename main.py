35f47fb539ef357fbeaab9cb54635e958811b1c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:01:45 +0000

    safety sorry agree demonstrate certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
