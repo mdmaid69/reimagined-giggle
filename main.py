0d1e2e43012e8c2046c5ffda4621b1c55aee7a4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:33:24 +0000

    swim want turn middle put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
