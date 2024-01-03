5f6e098de2bf496380328b270ce562253820d7f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:34:08 +0000

    morning action hurry regret pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
