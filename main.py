6d3483407bed9f658a49bac4f9254e549cbdff0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:04:16 +0000

    cross point tread interest bottom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
