45f67de4e50da288123c57af78c6bf65259616f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:59:26 +0000

    joke sad warn neck tall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def get_current_working_directory():
+        return os.getcwd()
