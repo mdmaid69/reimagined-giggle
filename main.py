425574619143b51cfb0fe86262a70577440e34c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:47:39 +0000

    whole steer plant central sting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
