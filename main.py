c4ea506d93c260dd259eda75040d860145dec991
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:25:14 +0000

    visit claim add ring pleasant

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def cube_number(x):
+        return x**3
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
