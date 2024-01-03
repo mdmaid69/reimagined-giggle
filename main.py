50b949a6c803d5cc466d19a905df9bc774c22249
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:07:14 +0000

    couple path responsibility call average

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
