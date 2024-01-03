e79bfd3fe0238628b34a701ddd30575b80bf983a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:48:10 +0000

    happen strange plain risk calmly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
