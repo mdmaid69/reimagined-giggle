e6d376a32c85cf5d24f0c47f2bee23544bddeafc
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:45:01 +0000

    board waste forgive before repair

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
