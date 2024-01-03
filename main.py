84f9149bcbf694745ce27af6123786c2d42e548a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:26:24 +0000

    moment angry ear spin Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
