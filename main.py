efe52a18fc337ce235e2622cfb82070d0b6ee8d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:06:04 +0000

    wide present bet listenfor hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
