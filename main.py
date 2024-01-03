5c4e84309a0d469b67952efc0987d773de827257
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:54:26 +0000

    record add suddenly weekend branch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
