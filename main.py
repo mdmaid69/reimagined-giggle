9e2f474b1ac45f97d328cc876c420376c7cf8c26
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:14:22 +0000

    coat range contact workout press

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
