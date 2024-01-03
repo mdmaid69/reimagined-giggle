58213d7d15e9e345ed318102f171334240f69948
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:39:01 +0000

    genuine block conversation intend deaf

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def split_path(path):
+        return os.path.split(path)
