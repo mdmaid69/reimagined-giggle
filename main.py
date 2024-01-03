1e26347be168a10e22a9ea967d90678b05ce1da4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:29:21 +0000

    valuable dress fact rush congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
