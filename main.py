814919d3358a00ac9f22aad62497b895915b3eab
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:42:17 +0000

    bright could lookup board project

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
