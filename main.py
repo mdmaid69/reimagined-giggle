adca8ca30a93dabb042abd5bc3b1364da52e86bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:51:26 +0000

    expensive any chat chat wrong

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
