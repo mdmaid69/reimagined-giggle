e2767b21fefad69b559432c4ac9e276c560ee587
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:33:42 +0000

    fault ear first pipe inform

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
