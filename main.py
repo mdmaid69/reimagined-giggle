199064b9eeeb5e46b217618e3357523dbce19096
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:17:37 +0000

    grade name mind Ii surprise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_palindrome(s):
+        return s == s[::-1]
+import tensorflow as tf
+print(tf.__version__)
