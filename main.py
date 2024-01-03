e4cb44e59899f0ce53692ede46977e94f0ddf8a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:49 +0000

    shape name problem accept estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def get_time_since_epoch():
+        return time.time()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
