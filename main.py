dee9a80dac6967f70a8b71d2d70607c8ffbf326e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:52:13 +0000

    taste bin turnup trick excellent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
