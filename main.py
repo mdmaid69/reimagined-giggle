4141f4d7f01aa7a5f94c1b52fbbaee78a6f8584d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:35:34 +0000

    entrance table breathe every speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
