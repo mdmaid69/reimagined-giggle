c128560cc4fe7b5539eac7c7df92aa299dae272a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:35:19 +0000

    necessary visit breakthrough permission forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
