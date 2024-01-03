8f9c3a8bef26f7e1ceea2fd9f690c79e58f156f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:00:09 +0000

    show know roof actual send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
