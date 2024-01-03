f42ea14ec8e9f4ed75b87ffe1eb158795ceb0f10
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:07:13 +0000

    look to injury offend sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+def remove_directory(path):
+        os.rmdir(path)
