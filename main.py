eafb8030498e43febc8503c949b0219c16e25dd9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:41:00 +0000

    snow black alone failure once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_current_directory():
+        return os.getcwd()
