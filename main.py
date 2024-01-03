1d464118afecc0aae2f2a1920e25311ed191fa33
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:48:40 +0000

    tune we pretty official crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  def convert_to_binary(n):
+        return bin(n)
