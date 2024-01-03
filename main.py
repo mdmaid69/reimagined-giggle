23dceddcad23b66fb0f086fdbe10fd5a2e5bb2e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:25:53 +0000

    question unless march book inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
