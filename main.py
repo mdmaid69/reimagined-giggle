3dd30fbeee1400b688ed8e95d6856382f8d0a243
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:26 +0000

    culture fasten live pure pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import os
+def change_working_directory(path):
+        os.chdir(path)
