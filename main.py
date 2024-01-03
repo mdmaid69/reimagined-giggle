299dd55c5436bee247357217e77776343832b1eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:19:13 +0000

    track value petrol open character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_current_date():
+        return datetime.date.today()
