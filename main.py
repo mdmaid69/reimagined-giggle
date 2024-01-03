fed98036c00068eac2f190dab266a4bac4a5ed82
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:06:54 +0000

    art match watch force peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
