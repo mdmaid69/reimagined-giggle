a75199200a373f323917511fc0807c7ca2aa6070
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:36:17 +0000

    through maybe willing love force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
