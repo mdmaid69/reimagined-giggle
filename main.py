0e503950447b7630d250c9b5f169a1f09fd3bf61
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:22:23 +0000

    now science pronounce weigh calloff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
