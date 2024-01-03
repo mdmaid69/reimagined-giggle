cde0a5f8b41cca97f1c7e360b95543e2700f078d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:56:52 +0000

    bottom breakinto maximum spill exciting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
