f60652f0cafa038d8fce6eceac01c727cadfb220
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:47:16 +0000

    approve disappear speech second later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
