a03059e82309e39ad78d02344b58e887aa2a80ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:08:42 +0000

    production famous wing shine speed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
