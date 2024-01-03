0e7e8aac98ab9fb6356ad87e3e86fd8063285d19
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:24:04 +0000

    light fast mean loss confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
