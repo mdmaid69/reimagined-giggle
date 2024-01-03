77123298b31bb60da42fbe43dbea2bf824c2671a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:35:45 +0000

    need direct culture hollow ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
