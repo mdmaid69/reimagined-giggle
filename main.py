7efc1794595e400a2d019ac61e19ecb53fdaf7f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:15:16 +0000

    once bus protest dull tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import sys
+print(sys.version)
