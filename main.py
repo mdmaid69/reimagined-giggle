2c2bb1c16f99dad5b780476f1dead1064e33f066
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:32:53 +0000

    past muscle opposite shout favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
