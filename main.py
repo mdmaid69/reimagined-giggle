a84624cc7f37017735e52307241f1a7f2da75277
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:57:55 +0000

    top check nice come combination

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_power(work, time):
+        return work / time
