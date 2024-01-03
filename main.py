6b0b8dc188ff501aa8fa02400bc13061743adee3
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:16:10 +0000

    before send bringforward schedule still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
