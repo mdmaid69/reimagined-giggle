79efd3cd6f486d03c4a4fa6ecea0973cae2a31b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:29:07 +0000

    tidy want stand important except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def is_even(n):
-        return n % 2 == 0
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
