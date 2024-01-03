20a30a3f19d84334c7d5eb2d17c6ddbd3179500a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:36:33 +0000

    town there I chase estimate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
