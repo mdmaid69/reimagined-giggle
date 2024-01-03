4dbba7821c43f6c01c87bc20b73c7e9e14596cb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:09:57 +0000

    lay completely hold light cruel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def convert_to_binary(n):
+        return bin(n)
+  def convert_to_octal(n):
+        return oct(n)
