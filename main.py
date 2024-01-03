cd2f440e373a6ad32bfc222f1c5891ffd06ee56e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:31:10 +0000

    chin ocean always criticize almost

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
