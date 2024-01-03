7b620f84edf92ccaea4b7b94c1f9829b76acb609
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:43:02 +0000

    Bb risk arm kind exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
