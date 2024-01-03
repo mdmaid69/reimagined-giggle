d646b7f610f186b792a4db0553a445deca37d986
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:17:44 +0000

    off touch anything comfort sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
