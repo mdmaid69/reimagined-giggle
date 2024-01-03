1d3351fa34141044d5c664a1b3467cf2dcc11410
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:31:35 +0000

    reasonable now person dish cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
