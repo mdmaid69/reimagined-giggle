9b29596597c08e8ef85c0efcb020964cc0eb5b42
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:24:36 +0000

    survive difficulty Kk receive front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
