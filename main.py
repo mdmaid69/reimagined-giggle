35b826421391cef79a766d1733ffa41df932e733
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:44:43 +0000

    bunch shopping further cut deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
