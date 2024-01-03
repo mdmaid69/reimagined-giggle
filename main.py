34d8d3df904ad1d8b17061b378c0c0c1e14e4597
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:53:47 +0000

    concentration seat pin limit combination

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
