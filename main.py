0166e16f7386f031561a479a3d782f1dae2ab837
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:51 +0000

    region breathe silent forgive thirsty

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
