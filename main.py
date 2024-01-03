9d0d96da64e6e5b0c65ab59383ba79fe052725ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:22:31 +0000

    card vision reduce sale harm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 text = "Hello, world!"
-print("Words:", len(text.split()))
+print("Characters:", len(text))
