5c7c54f0e42c710916d91f98a3b4a1e693fe8172
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:29:13 +0000

    knock we temper runinto chat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
