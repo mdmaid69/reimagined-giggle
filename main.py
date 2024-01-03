b90a1fc5838fc40ac4d5bf35d5c2683e63a190c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:08:17 +0000

    whether medium bush meal instant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
