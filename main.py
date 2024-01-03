082fbe45fd9db52af1128198d710a6921ad3ef03
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:21:35 +0000

    remember afraid on offend over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_min(numbers):
+        return min(numbers)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_error_function(x):
+        return math.erf(x)
