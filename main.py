7c84254120f2ded013d5a36862c7c96123e8f30d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:08:23 +0000

    excited not word unlikely majority

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-import time
-print(time.time())
+def find_max(numbers):
+        return max(numbers)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
