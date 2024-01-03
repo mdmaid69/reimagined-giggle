0bc55c505fc337ad74ada26b5f7b43c7579aec5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:20:09 +0000

    keen production print chat breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
