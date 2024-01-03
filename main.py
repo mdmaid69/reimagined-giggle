d3d1107ea8477b1a5cd9b8844765e49cba27be43
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:10:11 +0000

    mine rain search enthusiastic near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
