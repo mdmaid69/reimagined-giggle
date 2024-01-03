bcadbe8df2c8dd3e39dc393a3f5e433aed8bc887
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:03:15 +0000

    average rush ignore practice certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
