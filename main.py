785d11bc95a6ed2e67ffe1bba1e9d8703d7f50d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:42:39 +0000

    concentrate watch uncertain shall strike

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
