23c2e2ea1ba488e2d937e4062ad96a83e5c9e1e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:00:44 +0000

    difference mess pin claim subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
