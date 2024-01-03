1d75ff100df344ce81d1b8c4a76b139b7372e972
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:14:21 +0000

    contrast rise secret achieve suckme

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+print(os.getcwd())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
