6fc331d16eaa6b1c93b30bc5c1371ecc33d0124e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:08:31 +0000

    shop upset but describe unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
