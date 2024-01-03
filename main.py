c093de87f210177f071b561786aeae381096b9f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:51:10 +0000

    attempt on contain shape difficult

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
