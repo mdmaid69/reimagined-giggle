0d15f6e5b274786633b92f6772d449c48a74445b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:43:00 +0000

    city suggest Nn dry punishment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_current_directory():
+        return os.getcwd()
