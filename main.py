b7568fbbb023d4ab901f95564a068af5db513725
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:00:52 +0000

    activity activity find maybe container

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def is_palindrome(s):
-        return s == s[::-1]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
