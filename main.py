fc7acb90b48ba05fefd49ac8ede529aed813fc80
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:22:45 +0000

    for hair remove market set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
