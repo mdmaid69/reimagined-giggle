d93d2281c7dd4f6ccd851fc2c0e000da4ebc62ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:27:40 +0000

    muscle material alive more mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
