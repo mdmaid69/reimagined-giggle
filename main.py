130513eb131b4dcd9ad3c9c4cbdd771c32f6268b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:33:08 +0000

    really true pump long sign

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def square_number(x):
+        return x**2
