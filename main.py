4c438a67b5672e5b24212cd3ab5127e99b3e8f24
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:01:34 +0000

    trade shame listen solve case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
