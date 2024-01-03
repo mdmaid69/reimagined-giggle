5376352ffc540db7ba6f6bf2a42b82c780513ece
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:27:16 +0000

    temper this no fire consult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_speed(distance, time):
+        return distance / time
