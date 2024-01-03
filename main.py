db295955110958f0dded714b537ddf9e3482842c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:53:11 +0000

    yourself turndown failure announce listen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
