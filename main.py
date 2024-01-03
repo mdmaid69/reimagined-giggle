2fc1a955413f8f014b0dead09dcf44700c2d75f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:00:47 +0000

    persuade voice negative as too

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  def sort_list(lst):
-        return sorted(lst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
