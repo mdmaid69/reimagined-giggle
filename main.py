50de38a2e02e8d03cb7a3ce91d0565872ae25746
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:07:46 +0000

    closely rule necessary stage sticky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+print(math.pi)
