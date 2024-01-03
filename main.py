50ee80c7ed97e765f21cc7904fda40959ef9f6b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:17 +0000

    distance tone after oil give

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
