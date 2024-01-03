98a11770fa8eeab2468e33dfc6529784ab8a83ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:56:32 +0000

    welcome trade after left imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
