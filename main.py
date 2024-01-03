44695bf8caf8b4d953a7ed05ab643b614221a9cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:00:28 +0000

    nearly label extra moment exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
