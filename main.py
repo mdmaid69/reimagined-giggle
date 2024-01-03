d07dfafd5655e446284033532e181d072b2ea887
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:36:33 +0000

    speed time government whoever stop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
