676ef9fb9cc0e404c16011634d205ae5a6a2e67f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:27:56 +0000

    this earth bringdown event holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def square_number(x):
+        return x**2
