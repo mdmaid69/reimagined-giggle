eabbbfb63b3a912ec2a5dca9c1924a8f689e2ddf
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:00:34 +0000

    add state worse attractive crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
