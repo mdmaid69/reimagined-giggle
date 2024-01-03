11c211e5f3eb7354236b00865323b3f03834428c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:05:51 +0000

    who suggestion part surprise attractive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
