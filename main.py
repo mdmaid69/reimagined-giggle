00749085dd768758f39ecbea2261a64b61d7501a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:44:23 +0000

    whatever wish kick cut bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
