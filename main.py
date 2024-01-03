db1eb071d35309a066371f3ae5ca6f622bd2d017
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:28:28 +0000

    touch spot squeeze hurt slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
