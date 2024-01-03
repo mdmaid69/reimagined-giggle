c3971f53c83fdbaff714ce7ab4205e2e98ca20a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:58:59 +0000

    recognize shine confusing slightly base

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
