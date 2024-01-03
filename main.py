aa548f6c445a8505f78ab88bdc7fa1c997819ad7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:27:19 +0000

    only cruel plan almost offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
