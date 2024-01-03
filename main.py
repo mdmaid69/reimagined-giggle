faf2f94d36f4f53de070fa85e43b21b0fdba9053
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:29:48 +0000

    open top indeed attractive equal

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-print(sum(range(10)))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
