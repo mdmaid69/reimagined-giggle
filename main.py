5b17486304e6d23f9350fd082def5a2b916939b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:10:29 +0000

    dream strange variety then thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+x = 10
+y = 20
+print("Sum:", x + y)
