be788cd6b42277b15c9c33bee270e494cede6cf9
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:04:57 +0000

    storm lay appeal bury child

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
