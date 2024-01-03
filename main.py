99c8611b97464cea299e3fb69a6d3ee487cdac89
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:09:11 +0000

    trade honest attention bright contribute

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def convert_to_binary(n):
+        return bin(n)
