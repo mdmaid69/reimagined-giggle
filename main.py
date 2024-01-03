0db9815d7e3cd71fc612cb6166290ab35253e4b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:17:59 +0000

    custom down workon lock unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import random
-print(random.randint(0, 100))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
