455e10cb05e67ec18604e20246166fa614cf3f8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:35 +0000

    sale workon sign let clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
