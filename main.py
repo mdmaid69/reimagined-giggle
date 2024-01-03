8eb2feec51dbd2ce968d0ed45510054d7c719b75
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:00:55 +0000

    reasonable Ff convenient keen blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
