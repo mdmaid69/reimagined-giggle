f551de02b255fefce6a663a8140b2c597067e0d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:31:37 +0000

    lost brave patient reply 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def convert_array_to_string(array):
+        return array.tostring()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
