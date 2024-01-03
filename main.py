613034b3ba5144b071ebcfc7975d09466eeb6b6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:35:33 +0000

    age breakinto task colour opposite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_volume(length, width, height):
+        return length * width * height
