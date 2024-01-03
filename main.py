e4740d8af71d14c832b4de36c3a398f17a077209
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:37:55 +0000

    deliberately achievement chance secret pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
