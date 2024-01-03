d93ada51bd06399f8327563a33252049659123d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:12:30 +0000

    passage backwards borrow tall agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
