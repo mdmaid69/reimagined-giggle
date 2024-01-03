6ca906d2685a2eee31d22b03652742f9eadb9679
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:12:15 +0000

    spray consist disagree afterwards already

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
