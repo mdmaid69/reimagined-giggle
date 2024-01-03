9149ef905ab36de6c7453720c072d485fd88ba33
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:33:22 +0000

    achievement hope meanwhile train relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
