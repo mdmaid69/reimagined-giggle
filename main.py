f43f7590fa2d5b1e935c605b9ddfdf3294a7dabc
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:03:46 +0000

    strong talkback owe brother available

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import datetime
+print(datetime.datetime.now())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
