5381eb07d184ffc02f76371279020c76e752557d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:23:42 +0000

    welcome aim sit risk tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def remove_from_array(array, item):
+        array.remove(item)
