dfc9e8399a615e53a0466eb30d0fe8da049850dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:22:23 +0000

    with cheap space suppose whoever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
