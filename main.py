836314ae422379ad8ba42835467d1e2b77f175cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:12:02 +0000

    mouth voice stand convincing enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
