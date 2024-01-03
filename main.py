2314bcbe9c7a5951350eca48be032210c9a3ad9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:15:35 +0000

    567enemy table contents live hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
