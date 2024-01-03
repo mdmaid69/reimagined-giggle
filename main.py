f84105e2f0c00d137cd7336ef3627ec2848dea8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:37:11 +0000

    shelter lunch contact getup begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
