7f189a2874fd4f04d4164ea593e9a7eae1597e5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:48:48 +0000

    bet better destroy peace cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
