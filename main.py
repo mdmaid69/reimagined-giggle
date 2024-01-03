e6c23fe562dc36a3ecbc27a6b3b25d86b9859150
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:31:06 +0000

    satisfactory activity variety without floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
