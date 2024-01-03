15b414e1f9983abc46d8584dd3d09315aece26f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:44:18 +0000

    already else tragedy uponon mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
