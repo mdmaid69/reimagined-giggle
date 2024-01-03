992d511b306b3dbaf6d2ba22fbed667d95ab339b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:22:59 +0000

    spite married next easy challenge

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import sys
-print(sys.version)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def convert_to_octal(n):
+        return oct(n)
