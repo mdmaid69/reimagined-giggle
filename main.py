6ccdf0788a3aa60be00b5de2670cbbaaa8f534eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:22:22 +0000

    lesson mean illness sound people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
