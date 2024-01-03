bb628ab9f935676dd414f8f11e706384415dc499
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:28:39 +0000

    566end sharp height address skill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
