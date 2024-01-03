1cdf123b36fd844d1052e672af3f825c006bac8f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:25:07 +0000

    unable show voice hello improvement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
