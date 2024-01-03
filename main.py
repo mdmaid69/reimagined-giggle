8ba2b088837a56d6fd77b4d3d4419fe605d35e4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:37 +0000

    more regional sort fear appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
