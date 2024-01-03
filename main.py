f0df1bb1c401876511f12bbdd81940c502b62301
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:01:17 +0000

    influence discuss test worst print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_speed(distance, time):
+        return distance / time
+import datetime
+def get_today_date():
+        return datetime.date.today()
