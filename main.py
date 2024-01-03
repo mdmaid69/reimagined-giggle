8c982623d278acb1ec39145a2c6bb30d185e5334
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:03:00 +0000

    record clue survive uponon street

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
