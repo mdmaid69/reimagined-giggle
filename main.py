3cf55898c3d60fe1755812fe89b8c20ac0fc70e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:43:40 +0000

    several lift brush cause fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
