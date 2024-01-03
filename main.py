a85ab50058499be17eb48d9eb229fda919a9aa57
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:36:26 +0000

    deep reason love world various

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import sys
-print(sys.version)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
