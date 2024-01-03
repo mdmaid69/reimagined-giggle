22add628ca38973e85cf564cb4d71bf43c1c5d65
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:52:28 +0000

    later pick why later emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
