204a57fb75b688f9de8ad139123aee9b8458aa53
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:44:58 +0000

    place time count giveout delicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
