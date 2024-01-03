50b8bc2421c07b4a751e3013a33ee68e247afff4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:55:27 +0000

    uncertain dear regard slight danger

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
