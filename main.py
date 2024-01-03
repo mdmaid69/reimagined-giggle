3e46880326e67c1ae53a910430025718ca5f0357
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:00:05 +0000

    hollow power similar land keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def convert_to_binary(n):
+        return bin(n)
