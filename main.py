c775ae15d159d028fa58c5422bfa9b0588017a47
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:03:55 +0000

    vision future congratulations cost criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
