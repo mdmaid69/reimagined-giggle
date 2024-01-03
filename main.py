bbba7ec32aee213814345a41c2ac6149ae409f9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:10:35 +0000

    out agree know passage definite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
