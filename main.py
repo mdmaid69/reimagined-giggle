223310b15b4803d291d8674048d75a71b4d3e76c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:10:19 +0000

    my morning kick Ee total

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def multiply_numbers(x, y):
+        return x * y
