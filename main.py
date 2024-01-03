de27d4ce97d9c64c87dc4d111ba93ca9a6bcb08f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:39:14 +0000

    give lesson ready delicate any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
