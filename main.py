1ce3aae22eedce7f40eec3977124b0290ef1bf4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:53:34 +0000

    apologizeapologise custom floor clean behind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
