e082deac911d6a88b2cc0a6fb5acb0510d26f235
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:06 +0000

    nice thinkover proof offend the

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
