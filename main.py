5e09a7ee40c4657d65a69a4df85f2e2bbaf6cf92
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:36:20 +0000

    bad lower quickly care wrap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
