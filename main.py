28c870454bcecebfc04f39e977d99b4445870cae
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:49:54 +0000

    roof advise violent grade act

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
