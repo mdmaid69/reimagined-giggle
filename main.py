6eb179695d1af448b9dcfe62dd21c2a59e00c492
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:33:05 +0000

    public rubbish responsibility happy certainly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+def count_characters(sentence):
+        return len(sentence)
