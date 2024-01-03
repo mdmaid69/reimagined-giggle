ac2707aa78a13b15329a3c9fb6c1ece942b7cd27
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:59:30 +0000

    finger cost yes to divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-print(time.time())
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
