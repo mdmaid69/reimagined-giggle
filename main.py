7c9b75b32e371492aa9292232a19382df58ff7cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:38:25 +0000

    excited weak anywayanyhow connection skill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
