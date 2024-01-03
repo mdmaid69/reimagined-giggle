1fab17264d6c2fdca9d7d937e49870375d0ce237
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:32:45 +0000

    news fork juice will round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
