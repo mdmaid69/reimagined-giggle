6a863fd3d2b2a048dbf4be2c5a7d7e99137fb13d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:02:53 +0000

    brief shut afternoon herself total

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
