6c630454f05be7c9dbd81b40875d2344f4b7c574
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:51:50 +0000

    often cheap right out free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import os
+print(os.getcwd())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
