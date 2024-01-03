7dcc035215f4fba71dfa9fd3687695b06fc227c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:25:36 +0000

    object partner attempt impatient up

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
