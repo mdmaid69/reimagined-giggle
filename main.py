069df9fb09c366974b31fb0ea87b808f70d9ccf6
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:35:19 +0000

    strange today drop man lonely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import json
-def convert_to_json(data):
-        return json.dumps(data)
+def read_from_json(json_string):
+        return json.loads(json_string)
