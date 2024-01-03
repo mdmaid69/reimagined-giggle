193a48d442e3832762339ea45494f73ad427d4c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:50:16 +0000

    operation scrape experiment succeed without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
