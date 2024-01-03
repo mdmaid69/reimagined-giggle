e6efd21054a79c9001a7b9cb46766d1c60148b0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:13:23 +0000

    only interesting sad control collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
