3df1326670fe16cf1c1766603ade46788e2e3c3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:32:59 +0000

    surface show use bill guess

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
