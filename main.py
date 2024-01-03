5f509ad771c523dac86408b43b720986873b70da
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:55:52 +0000

    tell dick solve load flavour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-print(math.pi)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
