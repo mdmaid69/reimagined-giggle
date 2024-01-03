3988cf3ecf6710540e1ea544463916785bcfdcc6
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:17:02 +0000

    popular exit heat platform pollution

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import sys
+def exit_program():
+        sys.exit()
