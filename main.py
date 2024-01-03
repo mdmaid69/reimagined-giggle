81429eefd76eea98499c6f6d01118b2bf27c6d08
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:59 +0000

    week possible dig stick bus

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
