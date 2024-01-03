73336d4e8dd1a20f135dab90e1db66efe7dae555
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:33:15 +0000

    lock side ours butter for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
