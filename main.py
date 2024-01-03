948dd298f7f5bd66bbf6e49565858477e1f77582
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:21:42 +0000

    variety detail right lookback(on) order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
