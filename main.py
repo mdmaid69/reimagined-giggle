5d8dec8dbd36f6a06baa9fe85fbdda639693ad9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:09:45 +0000

    murder shock swim last reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
