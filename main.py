93c7f7ebbb2593c2f93124a54c7dac910b00c538
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:43:03 +0000

    translate provide support bridge rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
