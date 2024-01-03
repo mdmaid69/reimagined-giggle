6ab1e69cd88f2b482157659df14a30763a69da51
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:15:09 +0000

    male plenty statement trust level

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_square_root(x):
+        return math.sqrt(x)
