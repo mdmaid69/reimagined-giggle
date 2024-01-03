f09584d90a9a194f4d35639c97412f6a6127e853
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:13:16 +0000

    Yy convinced none lock address

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def is_even(n):
-        return n % 2 == 0
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
