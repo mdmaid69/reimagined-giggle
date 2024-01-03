21a5d37c78c070db29567f93fd42ea7661e7c855
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:48:10 +0000

    disagree suit telephone design nor

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def reverse_list(lst):
+        return lst[::-1]
