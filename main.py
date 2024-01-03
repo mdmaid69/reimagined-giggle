c534afe3258ecb1b873ba1329aad8d0988f37acf
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:21:10 +0000

    funny material top range squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_item(array, i):
+        return array[i]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
