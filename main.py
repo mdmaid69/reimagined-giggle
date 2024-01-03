b010b28ce8f6ddfb1e9cebe4f5aa184a70fe3066
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:40 +0000

    clear watch approval search opinion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
