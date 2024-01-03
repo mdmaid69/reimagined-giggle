9206cd2a393379a4cbdc774fb3f0fa11d848e2aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:48:53 +0000

    drink use odd will regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
