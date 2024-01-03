b65cbfccf7f2c2dad8ee9bf0bc55cb627dd2fbda
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:44:03 +0000

    escape both chase record narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
