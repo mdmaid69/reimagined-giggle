ea9b5aeadc12aa0399d009e740f61996f2c0276b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:48:37 +0000

    convinced simple gooff influence drive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
