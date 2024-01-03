93117f7b994314d5a2fbe34f703588b6cbaa661f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:31:41 +0000

    Yy team invitation future frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
