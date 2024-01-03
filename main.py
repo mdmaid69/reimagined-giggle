3389ecc2cc386185abecd18e80054f1f83c6f5bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:00:21 +0000

    happy play stop question about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
