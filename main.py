f7836839d34fc348ae40d64ed953b1c212a337fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:36:25 +0000

    clean rush drawing this refer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
