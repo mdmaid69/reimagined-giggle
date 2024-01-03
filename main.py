5be02c40e0379500e6959a0cc8ff7a50beb5ec43
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:57:50 +0000

    though line age ear comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
