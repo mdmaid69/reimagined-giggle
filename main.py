e0b764477f5b23267f0cf43022390d12981bdbf9
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:09:17 +0000

    rain combine every condition true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
