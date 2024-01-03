1d6eee5be1bdcd2524567ad6879015c6bf458eb4
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:59 +0000

    hide law seem anything raise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
