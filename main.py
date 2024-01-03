3f8459e6d90eecf166b991080bd02a47ce20ecee
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:08:04 +0000

    previous exploration reduction goback(to) bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_elements(lst):
+        return len(lst)
