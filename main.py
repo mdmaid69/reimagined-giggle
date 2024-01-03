c0032faac33c5fd120065d901c0355a08ce797f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:42:33 +0000

    soil bother yet storm exercise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_item(array, i):
+        return array[i]
