926b34935bd0f4f0b6ec4eeb7119c32f9313b0ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:38:48 +0000

    thinkthrough unlikely note bang heavy

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
