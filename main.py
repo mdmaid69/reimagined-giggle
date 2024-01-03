26f9d958ae68d0bfdf317274008d3c9e9bd07fb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:08:41 +0000

    573entertainment hurt throw understand ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_array_to_unicode(array):
+        return array.tounicode()
