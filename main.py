05c81e2ba2e7fbff81c4a146c945ba2d6c7ad1f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:06:42 +0000

    formal mean freeze black expression

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
