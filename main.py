ef492e2849cad4ffd47dff32830c8ac0e52f5a50
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:17:47 +0000

    top Cc day glass country

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
