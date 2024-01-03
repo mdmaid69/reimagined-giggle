32277a8ace95b4336e209b02fc8937581bf2089e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:43:33 +0000

    throw understand attempt rain goup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
