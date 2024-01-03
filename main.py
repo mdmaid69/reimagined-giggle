fc4df5121bf57a63f740fdda991f576df833e1c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:18:33 +0000

    easy proof cum performance declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
