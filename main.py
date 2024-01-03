0a2a7410f63602d320f3b544a1e92e8ef99424d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:21:35 +0000

    glass grow tragedy past now

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
