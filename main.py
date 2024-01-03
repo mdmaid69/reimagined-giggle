0ea384a6ad103cfce78147dcc32b70ca754e2788
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:11:57 +0000

    talkoutof print getback person aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
