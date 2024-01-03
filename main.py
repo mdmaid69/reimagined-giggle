ddabfaab0c104584142439048fbca52ba6c1e39d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:50:26 +0000

    nearly unpleasant sad size down

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
