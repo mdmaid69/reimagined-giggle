0e3bde3c8fc5f32db15468af6f61700f1812b43b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:07:28 +0000

    exchange avoid comeround demonstrate direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
