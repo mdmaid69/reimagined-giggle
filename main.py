d15c2f8b769d1a5fb87d062ea1edd2e7e7ca3def
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:05:40 +0000

    last popular school clothes already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
