e57420e7939975f1169d0f5a54709de0312366fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:23:58 +0000

    police vanish group equal result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+print(math.pi)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
