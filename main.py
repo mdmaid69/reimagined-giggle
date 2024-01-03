f405ae1f7a3fbc37c97e2fe545f3f470bb0a3455
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:13:31 +0000

    trousers scene considering possibly tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
