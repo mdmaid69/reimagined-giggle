0e0559b55831c2aebd30806641ef094ad517e27a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:00:32 +0000

    position organize shout used why

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
