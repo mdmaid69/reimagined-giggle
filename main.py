19a325bddae96136726bbebfcbf09dca764fda60
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:34:37 +0000

    Ee visit month offend beat

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import time
-print(time.time())
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
