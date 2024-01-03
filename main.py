5e43388990bf8c7b0abc2176a14a2f3b6dc4ad66
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:29:02 +0000

    total disagree branch variety laugh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
