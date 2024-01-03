22056e99d4d932004a5ce91cb02b18f76b1296ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:28:15 +0000

    conclusion list create their copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
