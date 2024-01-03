2ad455642bd43c9e0c653b1da702b6f95a8aa4e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:54:49 +0000

    thumb maybe my workupto right

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
