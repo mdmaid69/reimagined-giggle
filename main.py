bd7a7df7e99e9a2a62651f7703b3ef5765b4ec8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:21 +0000

    sticky concentration original shut time

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
