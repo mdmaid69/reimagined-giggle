35e1e5ca4093451527192da655ba219925ae2787
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:58:27 +0000

    busy design talkinto shop manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
