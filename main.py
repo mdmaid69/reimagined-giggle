db512862780ce9433dc95cbee29972375aee3d2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:27:14 +0000

    slice yes modern pile so

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
