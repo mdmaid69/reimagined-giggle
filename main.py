5faccd58bfb6985d7c2a9e558458b3c8891b9921
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:51:15 +0000

    obtain programme bill unlikely slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
