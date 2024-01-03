f362e96eb9ce75605fe06e2fa62088ee9c5de480
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:22:23 +0000

    emergency ring house fast paper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
