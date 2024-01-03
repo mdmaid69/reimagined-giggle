f8de185570ff636ac7395819091093cdf2254de9
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:22:01 +0000

    offer wall sweet plan suit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
