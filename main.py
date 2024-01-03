a54ca82bcb208931b8950fea11f445ab881194aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:53:35 +0000

    now closed order ever legal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
