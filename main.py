59dd51924add3cb198c4350188acbfc354a711de
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:25:30 +0000

    cruel deal call interesting message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
