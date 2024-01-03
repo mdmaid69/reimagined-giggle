a61d387a573e7ed09e508f27fb84ec16cdfd2377
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:54:52 +0000

    none eat block great 568energy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
