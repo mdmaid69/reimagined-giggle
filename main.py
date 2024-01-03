030d31596512d295ccab8711a0bd1e868c2e106c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:52:42 +0000

    only content natural safe anything

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import getpass
-def get_username():
-        return getpass.getuser()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_as_repr(array):
+        return repr(array)
