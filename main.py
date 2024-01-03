8a514e12a3841bb6ef4a0beb584fd72741c50d70
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:06:32 +0000

    worth angry catch opposite guess

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
