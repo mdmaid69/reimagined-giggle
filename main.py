35bdf184e2a3cc51fd1ca2f5c5ab209ef87f3894
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:37:26 +0000

    take variety free address laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
