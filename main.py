8d6df6ff3f78539461fc7f228f36c2d088ebcd51
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:27:42 +0000

    life mouth female makeinto past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_user_list():
+        return collections.UserList()
