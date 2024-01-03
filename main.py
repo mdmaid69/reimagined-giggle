1a82c46939ea7816fcd5371523b50f59354bab74
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:14:03 +0000

    afterwards present favour dinner dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
