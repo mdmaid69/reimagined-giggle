2ffd2880ccaff1d1fccf1c92cd7e57a55c6f0297
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:59:58 +0000

    gas old land discover help

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
