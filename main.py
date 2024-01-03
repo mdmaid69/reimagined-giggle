e47986dd4a4896191c01129330aaac0f136ed722
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:59:20 +0000

    goaway frightened disturb corner arrival

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
