ca0e697e180491dcba4fce63b7c9c106a024af64
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:54:55 +0000

    pollution corner joke but support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import sys
+def print_python_version():
+        return sys.version
+import os
+def change_working_directory(path):
+        os.chdir(path)
