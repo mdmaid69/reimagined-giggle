009bf4b5e4dd383a3dd0584526acb6a3351c1491
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:19:27 +0000

    normal behave wheel recent remain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
