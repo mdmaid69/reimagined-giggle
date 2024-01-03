3bfb8d2bcf9c340add5eacee7aa3a3f3e903365d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:03:39 +0000

    address mix immediately pain argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_speed(distance, time):
-        return distance / time
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
