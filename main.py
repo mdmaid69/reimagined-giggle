027fc946b41f897c1c5760b14af0d195c65001a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:03:34 +0000

    add expensive sorry like suit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pressure(force, area):
-        return force / area
+def convert_to_binary(n):
+        return bin(n)
+def calculate_power(work, time):
+        return work / time
