1ab393917af4e374fad7fdfa8e8de90f1db76f0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:27:28 +0000

    earth division game hungry wild

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+print(math.pi)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
