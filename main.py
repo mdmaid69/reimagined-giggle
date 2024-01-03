0c3290aafb0c4d3e83e0221d1e071f02b8dcb558
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:39:37 +0000

    fast frightened wonder tonight important

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
