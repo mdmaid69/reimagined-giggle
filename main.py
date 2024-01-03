d600e37de7f64a5e3bb7042baedf0f7ac01810aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:17:04 +0000

    cope show after weekend unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def convert_to_hex(n):
+        return hex(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
