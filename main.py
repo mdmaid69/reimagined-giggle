453b5bcc4b4c2337abeaaf88db94145bfdeb3390
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:38:43 +0000

    nervous material force Yy cap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def sort_numbers(numbers):
+        return sorted(numbers)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
