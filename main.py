fc0fc9b08d0c5bb0394e73d503b075952d876c07
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:06:05 +0000

    569enjoy come and view getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
