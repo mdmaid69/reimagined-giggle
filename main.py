bb2833be0ef1e24c6a3c73d8f0f676381acc680e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:00:47 +0000

    flow noise arrival sweep judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
