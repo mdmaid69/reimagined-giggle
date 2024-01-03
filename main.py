6858c20e014d3854ba0cc7dc3985677b9d7948b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:36 +0000

    again bury concentration flow top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
