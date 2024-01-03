e6d8b45f811198d9a58216fdea833da878ac3f1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:12:13 +0000

    crime common rest address term

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
