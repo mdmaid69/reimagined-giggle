fbcbe0ab1c44190f3d72b54a983835b3fa859141
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:03:08 +0000

    niggaacademy base book off angry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
