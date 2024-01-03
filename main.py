fe06007823404f5eb59a89c317b49b5092cd2452
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:08:37 +0000

    spread solve makeinto settle nice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
