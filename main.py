19df00330c390d3e1dce756d952085898d9b9386
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:10:32 +0000

    belong study deal drop much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
