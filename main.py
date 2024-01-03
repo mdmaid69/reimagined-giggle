51a065374c8a02cb23b65228a4b92893a77ee986
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:13:52 +0000

    fail its flow touch drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
