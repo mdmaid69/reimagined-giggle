0da308bfc2f9645d4da6a2db1b856018f00cb26b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:12:47 +0000

    feature crazy shine night will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
