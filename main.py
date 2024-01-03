869bb19715cf24dd724fece337ba55cad78f5619
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:50:03 +0000

    yesterday embarrassing lower rely goaway

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import os
+print(os.getcwd())
