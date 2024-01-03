51f1683a04597eb29cd28038512c0e3af6db4fb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:29:15 +0000

    post perform over exit object

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def convert_to_hex(n):
+        return hex(n)
+def convert_to_octal(n):
+        return oct(n)
