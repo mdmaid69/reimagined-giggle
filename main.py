0b06bc5c785b91c23e69bce4f57d2ee612a20d84
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:04:32 +0000

    ready reliable comeout section blame

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
