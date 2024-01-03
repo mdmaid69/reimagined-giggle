7caa85ce74217ab137b7b1fe7e33c96de8a94a9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:22:33 +0000

    wish spare fashion breakin provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
