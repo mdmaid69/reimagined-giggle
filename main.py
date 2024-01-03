73f072315e928e4e2ee40190fc0e58f343cdb2c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:44:50 +0000

    stare copy possibility wire love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
