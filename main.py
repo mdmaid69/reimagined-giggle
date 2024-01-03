0c02ecfa1265c5bbe4db7eaa21bfe0e05faccceb
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:24:48 +0000

    protest term high surprise anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
