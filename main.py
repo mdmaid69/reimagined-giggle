6b5103b9619e177bff732955faa7b91c913afb53
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:50:16 +0000

    at slice strike bang square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
