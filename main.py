94e0f88a5183a39e1e85cbf0b37c5e350906ebf9
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:06:44 +0000

    laugh ask tongue get suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
