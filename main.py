2f5d88826b44dd5cc55c71b52601ad8a1a5979a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:59:37 +0000

    fortunate basis effect foreign under

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
