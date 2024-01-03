170ff59b06c4c1752b3c8b4673cb25d59efc83eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:26:18 +0000

    team precise brain comeoff colour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_numbers(numbers):
+        return sorted(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
