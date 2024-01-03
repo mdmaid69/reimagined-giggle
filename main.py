4824c357a68167b4726f721998276628e9315546
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:44:37 +0000

    convince patience whole boil plant

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
