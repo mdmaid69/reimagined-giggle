290ba3f74c42bcbdd9dd5db8685b0640c8c194fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:55:27 +0000

    from influence break smile 564encourage

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
