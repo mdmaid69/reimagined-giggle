cae0cb7568486deebdd4c36eb72f88b84b67e1c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:48:31 +0000

    nurse instead nature comeup lead

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-print(math.pi)
-import re
-print(re.match("h.*o", "hello world"))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
