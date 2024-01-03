101cf9e682d3677e8cd44f1b3ab37a5f81337b5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:55:37 +0000

    throw neither plenty Ll lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
