f4991a91cf357b1256d3e88f280af9de8c034401
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:44:00 +0000

    majority free mass turnon person

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
