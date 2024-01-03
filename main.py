4014057c501a4af214f1cd88fccc953a31112bb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:09:57 +0000

    sleep send dream unit performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
