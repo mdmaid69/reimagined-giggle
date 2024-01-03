5ff66b52e9eeed97e2931bad69f6d1bf7c310530
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:23:09 +0000

    coat receive around music different

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
