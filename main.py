2185ecc2ec75fa86aa5c8c7a01a62de9b0ab08e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:43:25 +0000

    sometimes flow fix away so

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def divide_numbers(x, y):
+        return x / y
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
