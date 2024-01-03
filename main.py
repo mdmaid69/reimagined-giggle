7da262087bfc191713e61c20b6a19089e4eae580
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:34:04 +0000

    calmly road cheque claim recently

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
