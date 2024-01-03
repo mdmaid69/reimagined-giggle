ed388e792fb9d577f2b07bbb976efb10476e3a55
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:49:18 +0000

    tired bringup argument paper average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def get_array_as_complex(array):
+        return complex(array[0])
