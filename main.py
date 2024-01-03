bd97e96330c212097a102355298e054d321e8dd2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:52:26 +0000

    class include knock bringforward firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
