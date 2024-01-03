e785a757befb04949d1e2858b39154514325ef4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:08:53 +0000

    consider necessary bound tie tooth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
