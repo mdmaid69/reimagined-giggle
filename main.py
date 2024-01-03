27135bca9ce593ee1524593b4da5bb5cac26249e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:44:22 +0000

    open food interested always combination

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
