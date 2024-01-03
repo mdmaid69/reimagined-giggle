a5915283894fc8f06ffc366738803259914754dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:18:36 +0000

    wait left declare cost gooff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
