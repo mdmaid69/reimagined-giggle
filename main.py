fcdee0d2cabf920782c4b0d7000e1ac9e6ee0f59
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:24:25 +0000

    worst carry autumn picture tense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def cube_number(x):
-        return x**3
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
