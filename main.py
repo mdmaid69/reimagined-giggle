1460c6b2cce5ac162837791abb4e971bc12360c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:47:16 +0000

    produce factory turn summer seem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
