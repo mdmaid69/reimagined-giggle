3a84942855b9951b1494a656419637c38639c757
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:30:03 +0000

    getoff otherwise we definite word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
