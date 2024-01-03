cc250b35b3e064efd955bf62c860f19d4e3b87ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:03:59 +0000

    plastic limit cost model more

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(10): print(i)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_area_rectangle(l, w):
+        return l * w
