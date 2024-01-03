7fb277e1620eca36d66e4628c651f9762418e28a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:08:03 +0000

    support almost thanks control prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
