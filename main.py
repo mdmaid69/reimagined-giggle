6752d254b9f32b94749c9bdce98dd6e9fb5c1f29
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:21:06 +0000

    callup sore worse than quick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
