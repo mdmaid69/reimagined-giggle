91c215398406eb98d5bddb85a6453cdef7166213
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:10:56 +0000

    shade dear practical be claim

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+print(math.pi)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
