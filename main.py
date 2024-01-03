ec5b0f6fe59be36295d29397d9566356c59a7284
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:03:53 +0000

    correct train impatient few rubbish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
