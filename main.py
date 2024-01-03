d4b4d5feed40758b6248409fc4852f96b2cde8b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:53:03 +0000

    top separate tomorrow equal seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
