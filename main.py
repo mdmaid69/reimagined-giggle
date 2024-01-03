4d5e71ddef3dec2f8f15af32f93e607f91fe342b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:02:32 +0000

    mainly pull Ii sister priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
