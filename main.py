da8babfac1f619137160890d4a07f4275c063131
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:07:52 +0000

    sort please tired complete round

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
