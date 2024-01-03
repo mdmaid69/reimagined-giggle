c0c7a62660eedef1e2761993e982762c3482e45b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:01:05 +0000

    564encourage factory breakdown noise finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
