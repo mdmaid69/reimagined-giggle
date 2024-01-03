dd4e51f76b1a45e07ad7dc874481a25bad036e2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:51:00 +0000

    fat communication promise politics convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
