12e6806d8d824ab651336ac74bbfdfa12ce286c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:50 +0000

    do shame tie tense ilovedicks

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def convert_to_hex(n):
-        return hex(n)
+def get_array_as_bool(array):
+        return bool(array)
