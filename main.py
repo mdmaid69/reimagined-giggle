8429b456e1762d59df108bf6c3b71d507b2278d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:15:10 +0000

    refuse Rr connection repeat unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
