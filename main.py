a6ddaecbabf159360fdd8274000518ef743294e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:06 +0000

    insist mistake talk suppose arm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
