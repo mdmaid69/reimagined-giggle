af53bc36f3574f492c656599b2414fdfd23cf00b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:50:54 +0000

    and tense light makeup programme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
