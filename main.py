5ea598284723d17e52627772c5ee329203ed7385
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:13:55 +0000

    survive heart simple effect poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
