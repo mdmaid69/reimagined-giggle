e6ef00b7242a5babfbd7054ce873eda920a79f88
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:01:47 +0000

    speed mistake duty unfortunate breakout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
