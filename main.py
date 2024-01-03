c556bbe74faea2e1ddc007c2eb8928a7fd3ee652
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:44:57 +0000

    politics subject gift left second

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
