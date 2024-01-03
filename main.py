2e4c4cf8a2a56490ce517fcc50d1160c21967c1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:32:17 +0000

    rely achieve workon evening point

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
