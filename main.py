91255c9a89cb3f1215d3f553958f3e1b5df56bc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:44:56 +0000

    characteristic phone giveback instant justice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-print(sum(range(10)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
