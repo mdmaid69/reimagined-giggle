52bcf7f3895e057639bd654d3d0447dd3e3ddac4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:51:04 +0000

    nonsense apart slight home bad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def is_palindrome(s):
+        return s == s[::-1]
