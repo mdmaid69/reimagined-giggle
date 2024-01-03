e47d79c1cf8289dc9b4b90bed3363e8de9f41ef1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:06:23 +0000

    usually reach ocean whole clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
