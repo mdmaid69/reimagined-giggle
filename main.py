5b4454abe0c233148df4a35f0ba136af9f3930a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:06:57 +0000

    mixture wind couple set worried

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
