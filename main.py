cebbfb04285b65f9c85799e9622509a1d76b2ea3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:33 +0000

    score spread custom include coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
