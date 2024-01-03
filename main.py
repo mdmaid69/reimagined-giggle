81d2e40ca87c69d8e7f00490527c2fe30ab3517e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:03:02 +0000

    buy and trust pause no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_elements(lst):
+        return len(lst)
