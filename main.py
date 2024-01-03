e8005e1f6861ecbe7fa9bab578ea57645462c612
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:14:56 +0000

    suckme pick thumb answer bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
