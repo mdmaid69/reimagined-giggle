d218a09fd8a746ce53d0e86325c155166dc44f51
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:58 +0000

    food warning behaviour regularly price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
