6a029ff7ea2ff52e2280fc0149c06eb837598633
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:51:14 +0000

    Nn 568energy share achievement sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
