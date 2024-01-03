a79e93ff9fa7986a6056ffd0f68ccfc650d0ead1
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:22:33 +0000

    pin represent goodbye save during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
