b841376529c4c3887a7cc84da080237557826302
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:20:10 +0000

    little tell mine direct anybody

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import datetime
+print(datetime.datetime.now())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
