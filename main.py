c11f62ec6f617fe2fa8b03271c0b86ace20d4c65
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:11:55 +0000

    bill most clear car anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
