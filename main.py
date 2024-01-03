113d08050b24f696caffbcb97f0c7730dc57aa63
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:57:33 +0000

    professional letter break trick unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
