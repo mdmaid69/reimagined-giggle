51f10d5b3d690b5542852496d0ec8c564a259407
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:45:33 +0000

    what powerful deserve apart scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def find_min(numbers):
-        return min(numbers)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
