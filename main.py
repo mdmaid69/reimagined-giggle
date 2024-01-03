ee0587b5b67c1a3c56716c547a1542bc6b542082
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:25:29 +0000

    habit shock degree region separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
