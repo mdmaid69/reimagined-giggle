35951e95cae9faf6c3e509d62b228c584c476374
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:25:50 +0000

    getaway afford himself neck stiff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
