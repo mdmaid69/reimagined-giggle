8bf80254a5154cb9338aa963ba3caf255b1c1ccb
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:25:11 +0000

    next say shelter invitation after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
