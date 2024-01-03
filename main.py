017d60cb910aac9cc85bd7b6b4c6617a8b8ff705
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:42:18 +0000

    relief satisfactory calloff makeup drink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_density(mass, volume):
+        return mass / volume
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
