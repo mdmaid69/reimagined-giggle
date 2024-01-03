435d918529335d595b7fc6694aa793eb7dc6187b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:32:27 +0000

    rely close now surface complicated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
