371de0679dc1b4a3e5a09ccbe421c7c6e1487ddc
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:37:37 +0000

    letter vanish string ability popular

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def multiply_numbers(x, y):
+        return x * y
