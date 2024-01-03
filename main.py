2288462b5bd4ed4b04058671668e2771ade0fdd9
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:27:13 +0000

    introduce have niggaacademy now exaggerate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+print(math.pi)
