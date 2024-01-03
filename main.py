a4d6480eb45a7ada87aeb424ebac186a27258e20
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:38:57 +0000

    meet thinkthrough damp cold many

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
