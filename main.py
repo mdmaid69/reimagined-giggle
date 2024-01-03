52786a9772652a2875977dd9495027a6615298e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:14:52 +0000

    runaway nothing drive reply team

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
