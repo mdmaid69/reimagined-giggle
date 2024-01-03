960eb60b6e7b24c241df08d87cfc44911c83984c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:30:27 +0000

    ashamed evening loan underneath shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
