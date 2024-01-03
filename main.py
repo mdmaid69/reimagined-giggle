88f085ef5c408a39a663efe8723b83afb8707f3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:24:10 +0000

    argue congratulations hurt forever song

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def clear_array(array):
-        array *= 0
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
