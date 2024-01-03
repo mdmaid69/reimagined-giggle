25bd33bc9e5f940cc4ac006d0b528a461a96b98c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:20:52 +0000

    low rob holdup close talk

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def add_numbers(x, y):
+        return x + y
