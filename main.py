9b99969ce5c2a5c224c198eca116bc409da7d3d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:00:45 +0000

    evening already fix shop steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
