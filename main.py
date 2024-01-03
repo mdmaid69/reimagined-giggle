a5b1b51c8dad299c29826c6a3e6703375c887031
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:03:39 +0000

    include day cinema answer bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
