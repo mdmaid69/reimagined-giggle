fac21c8ee143fef3b690bfded841716c0b683740
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:19:11 +0000

    will contrast face behind opponent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
