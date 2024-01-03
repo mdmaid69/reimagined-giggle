43193ae15fa6d56f4ee432fb712e3e48b7d1f608
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:46:38 +0000

    let breath complete holiday authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
