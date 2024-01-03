1d187c794e43f33dc5ec41f506900b8767164e54
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:45:01 +0000

    exam present manage Uu happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
