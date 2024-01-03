e4c53cc7a5ee74523aac4584c64edf8c43752c4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:05:59 +0000

    if simple march advantage bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
