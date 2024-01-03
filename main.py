dd72ae1b6e19cfb84f155e3329673ded31f6e540
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:46:12 +0000

    task public object better eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
