f8b7393c45e9f91b779b7bab4b25250acb344f2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:28 +0000

    firm bush air face shame

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_set(array):
-        return set(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
