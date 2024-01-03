4736cd2f4051a8a5555ca5df480e5ffe5637b53f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:52:45 +0000

    wave distant even nigga foot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
