7c23ef4e49132cb761feba88fc6ee4553cdac20c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:43:51 +0000

    across flow tax water wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
