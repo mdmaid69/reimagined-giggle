9e125ba3eb5a46d4acf4cbdad8ad8c07cb31679f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:12:51 +0000

    leave excited wish fire accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
