512a358a1c954ed7b464133bdb2807073e441173
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:29:40 +0000

    behave trousers spite so unkind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
