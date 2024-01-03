b83e42002b1341184573952935eaf1e34c6b6492
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:27:59 +0000

    callback wish makeup odd slide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
