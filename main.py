6531bd766a7c1e43c08d6c1701a6a904d8749dd2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:50:00 +0000

    precise introduction permission divide similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import time
+print(time.time())
