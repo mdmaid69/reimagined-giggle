ee30a5c4b757b15580025b3b5759f1857f80d156
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:31:07 +0000

    mixture light spring scale excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
