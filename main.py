3ec6c983b0b3ebd9490fe21bef1246c8effc3669
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:59:08 +0000

    seem long top later beautiful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def add_numbers(x, y):
-        return x + y
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
