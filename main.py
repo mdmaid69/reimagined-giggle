41901dc41daf40fb812938ab241cda873c8ccba7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:37:29 +0000

    mistake section joke greeting all

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print("Hello, world!")
