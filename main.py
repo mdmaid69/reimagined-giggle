c74e1a01a0af254feb072ad41edf627081061ea0
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:37:56 +0000

    takeout consider previous chat comein

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_list_from_array(array):
+        return array.tolist()
