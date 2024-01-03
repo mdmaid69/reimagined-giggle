ff89f7110a3274b6e622b93045f85c1c3ea2ba49
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:57:28 +0000

    snow obtain say main complete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
