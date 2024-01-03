c1c4bb70d1193b3aa6ea84ede1b988ae323f5895
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:08:11 +0000

    quantity bread regret loss mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
