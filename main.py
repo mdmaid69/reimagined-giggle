00063eeaff56f8aeea879456656a974b22e649b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:56:50 +0000

    question our failure tradition steep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
