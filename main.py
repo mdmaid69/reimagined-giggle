006d5c55b4408fcd119e75bcc962bf21fd4f70bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:54:09 +0000

    room duty favourite by account

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
