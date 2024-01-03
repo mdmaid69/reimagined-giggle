df9854c312c50992ee08a120e4a7a6f517516df8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:58:03 +0000

    disappear raise occasional test dark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
