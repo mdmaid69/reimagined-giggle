70802efa80a39d0635332e64e6adee2b08ac78e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:02:44 +0000

    179bite apologizeapologise fill bored dark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
