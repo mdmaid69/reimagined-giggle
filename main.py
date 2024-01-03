5d2b565d20da7dfd59cd29682f8657f2c5473929
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:35:55 +0000

    too total change look harm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
