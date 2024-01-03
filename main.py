6ef28dabcfcc371230f07ee2184fb73cccbe0fa7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:09:07 +0000

    contrast community news mistake impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import time
+def get_time_since_epoch():
+        return time.time()
