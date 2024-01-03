08c5425d06259da74e4cfcbc639fe938d1772715
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:31:31 +0000

    eye announce raw appeal offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
