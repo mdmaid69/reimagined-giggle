22a4fad25b0bc4aa56914b4312d9007401f9ea2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:23:59 +0000

    relief embarrassed serious loss figure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def add_numbers(a, b):
+        return a + b
