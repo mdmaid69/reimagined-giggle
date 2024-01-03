aff1f93fe69b071d60a3bbf12e038dc6c82b9e8c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:26:23 +0000

    plan consult mark question what

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import array
 def get_array_as_bytes(array):
         return bytes(array)
