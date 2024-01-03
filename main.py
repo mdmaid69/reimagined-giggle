ebe334a027397a433263e2aabe82a085d4117f0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:32:58 +0000

    impression load runover risk recover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
