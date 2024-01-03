8cfdaec7e0750457c7d5cb46c9899c551993a830
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:54:27 +0000

    your image dream light favourite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+print(math.pi)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
