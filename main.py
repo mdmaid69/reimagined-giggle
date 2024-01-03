bb8542fa5548a04f0e8ef42bcfb6f08b52a20ba9
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:26:56 +0000

    about desperate touch raw fun

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
