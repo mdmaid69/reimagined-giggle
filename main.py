8fff1d70e2121b37e755b05c96bb901260b3344a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:10 +0000

    ahead secret straight appeal near

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
