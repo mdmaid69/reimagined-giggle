f51d1399f2d4509d2741627f0709910e65b248cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:39:43 +0000

    roof complete cheap lie dear

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import collections
+def create_counter():
+        return collections.Counter()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
