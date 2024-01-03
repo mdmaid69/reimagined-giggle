03e801e6e9723a2933054cbd1d2650fd35aed5aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:29:46 +0000

    towards balance confident tip neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
