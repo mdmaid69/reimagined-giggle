6700df073cdbb415a184d759cfe56a48f62a7601
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:19:01 +0000

    feel makeinto lookaround getoff reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
