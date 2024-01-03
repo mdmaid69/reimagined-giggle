df19cbc6efe2e7847546ed16b61b60d8fe9974f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:11:56 +0000

    getout century fat breakfast cause

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_repr(array):
+        return repr(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
