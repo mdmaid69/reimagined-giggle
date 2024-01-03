3a18a72eccd029f47c0fa5af8dabbad7d740957d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:43:24 +0000

    world claim road trust connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
