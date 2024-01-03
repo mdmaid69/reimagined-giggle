b5ce0ea21a4fb6069ebf892ee02c2d7b898efc5e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:34:23 +0000

    behind science limit shall pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
