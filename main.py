c86f75e94f5e03dfed5fb84a71c69ffd256b674d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:20:19 +0000

    lookup slightly warn let communicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_work(force, distance):
+        return force * distance
