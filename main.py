20fd063e702b1f26131c93fd3ca344f50b330187
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:27 +0000

    try answer check brush comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_typecode(array):
-        return array.typecode
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
