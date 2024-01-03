b6f742a6c245b0dd8bd23ac21e963b0d5c27ea1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:48:16 +0000

    material kind culture crash could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
