bae8e5c09d0b49bc60c246b1229dbd6a5e794768
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:48:08 +0000

    cope simple interfere minute proud

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
