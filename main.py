562345f862e9d5b78dc4e60596bfd05e2165811a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:46:07 +0000

    pocket tired goodbye proud turninto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
