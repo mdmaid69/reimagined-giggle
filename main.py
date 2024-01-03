05a52f6f4dc672db029feb987042f5fab6bbb2a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:08:13 +0000

    affect measure valuable order command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_float(array):
+        return float(array[0])
