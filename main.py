da5e154dfa6012f656c00a4df3b0a79e53db51d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:39:17 +0000

    exact before necessary production doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
