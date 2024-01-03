0df27604613d68085f5059c2c2513df91683ed04
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:16:58 +0000

    tomorrow car holdon say learn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
