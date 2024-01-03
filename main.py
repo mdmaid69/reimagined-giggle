78d1b36d3cc8632de5d7c9e5782fec4e4fb18f6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:36:04 +0000

    vanish scrape programme fat bridge

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
