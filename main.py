47cdc9317b8e9cfd785ad5878547d3be3b0a78d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:57 +0000

    thing little politics past suffer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
