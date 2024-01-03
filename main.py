03afd4c9796260e62e98731525557336295d4254
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:11:37 +0000

    roll other fly nurse theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
