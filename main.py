5d72ade71e651c9983693b85681da218b3480798
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:41:33 +0000

    trust right study occasional callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
