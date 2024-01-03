5606cf4034b814b9e1c3ea6a2942668bd3c98200
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:33:48 +0000

    floor shopping away child violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_length(array):
-        return len(array)
+  def split_path(path):
+        return os.path.split(path)
