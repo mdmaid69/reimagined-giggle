1ba1e3c5451b6d08993cc2e4557f8e5168c821b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:42:28 +0000

    of mistake drawing open undo

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
