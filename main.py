61a1f42db7928f0a7f38277d43917d6aa01628e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:38:05 +0000

    sensible Ee confusing wave middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def convert_to_octal(n):
+        return oct(n)
