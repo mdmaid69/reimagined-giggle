badf680f5ae2fa5e66252c0410c0cf2f7920fca3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:34 +0000

    grow provide either neat arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def subtract_numbers(x, y):
+        return x - y
