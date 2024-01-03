20dd2026521594f036698516da8230bff59289a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:37:25 +0000

    exercise unlucky involve exchange exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
