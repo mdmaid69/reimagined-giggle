59a1152e9b0f8d57c9136ab97b13dfd3ab73f9a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:23:11 +0000

    clearly physical other ought fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
