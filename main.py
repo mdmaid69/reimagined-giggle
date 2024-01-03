20ea9e2172fa43482a57e806a0b33cc281f32f0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:58:04 +0000

    shake visit length she warn

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
   import matplotlib.pyplot as plt
   def plot_bar_graph(x, y):
         plt.bar(x, y)
         plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
