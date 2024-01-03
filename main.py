94f650324f7125eefa8da5c028eabecf48a6d496
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:14:11 +0000

    thanks take permission must connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
