e64d644f023ed2812f8836c901d094fa61ef9e41
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:02:23 +0000

    once sorry blue Hh guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_power(work, time):
+        return work / time
