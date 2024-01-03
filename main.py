eab0bc4ab641c79caaa71c672ce2d75b52151a39
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:41:53 +0000

    bringdown general personal unhappy already

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
