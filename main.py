063d129d9b98435d9d5a3cd971a803c8efa6d611
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:25:48 +0000

    blame interested swing space go

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+print(time.time())
