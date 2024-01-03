c8aada20e0146a1c156f4ba9f9ceca0b527d12b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:18:07 +0000

    since scared prevent dig turnup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
