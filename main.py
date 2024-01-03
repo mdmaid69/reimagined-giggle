e6c0a0aa3c9190310ca85520160abce6e2cee1e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:50:17 +0000

    left look determined turnover declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
