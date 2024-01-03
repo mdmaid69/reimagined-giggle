6b6b52c553f18522ffaacba12ab20c8d5eb065a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:56:43 +0000

    car dear criticism hang knock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
