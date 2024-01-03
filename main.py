819d1e563ebb1f6c849c795692e04abf4d6007f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:30:22 +0000

    honest loose common bet patience

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
