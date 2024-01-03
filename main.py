fa10b2e09649cc8c418c4648baf1eeccb55c3e54
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:45 +0000

    live read subject bring tune

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
