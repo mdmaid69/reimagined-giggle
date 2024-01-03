087d1bbd218157237074e365ecad1d291d35aa15
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:29:48 +0000

    wide on improvement chief distance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
