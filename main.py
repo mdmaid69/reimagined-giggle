bc8e29e2f2f1ac471470b9e99aa97de8dd1d86b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:56:14 +0000

    popular near scream cover show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
