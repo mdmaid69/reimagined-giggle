7d5c64b2b889e46d417c8b2dc5778db98360a31f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:24:39 +0000

    song excuse find culture central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
