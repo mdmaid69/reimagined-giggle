c85a5ed5437171f42d05cb1ddd7f2f87643ebb2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:45:53 +0000

    little winter maintain discover lower

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
