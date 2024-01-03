ce8176ba92fa085efe01bb3a45de299ba4d1a7d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:40 +0000

    face decision possible experience bunch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+print(math.pi)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
