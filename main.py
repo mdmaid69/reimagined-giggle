461f4dbb811813eae579dcef1407f59ab076b039
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:33:52 +0000

    target suit mess keepfrom within

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
