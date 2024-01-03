f731af29b701d48aa47679f9a74fefdcea04e107
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:17:25 +0000

    reduction game cost power taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_density(mass, volume):
+        return mass / volume
