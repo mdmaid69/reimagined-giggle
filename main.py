856035a3caf7e8473b0e70ad3ec67a6bc4ae8f22
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:46:11 +0000

    spite set neither activity pause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
