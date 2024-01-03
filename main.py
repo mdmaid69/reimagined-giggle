d2843a050b4a4f3320b87b195496a78599f268d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:28:01 +0000

    onto afternoon smile public issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
