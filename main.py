1897244c54f4ed1e2827b1676fb8991fcc0b7c5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:02:35 +0000

    from follow yourself turninto safe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import collections
-def create_stack():
-        return collections.deque()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
