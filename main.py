8326bd37e24d7e9152f06c0ddad9fd8e12e7b798
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:04:53 +0000

    cover sure connected film carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
