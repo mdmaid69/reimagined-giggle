d665f8553cad0be89cdc43679ad71982af565347
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:35:07 +0000

    sensible refuse high regional interesting

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+print("Hello, world!")
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
