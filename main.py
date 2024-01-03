ae1ea7a04aca14e13bf10236520c55587787942d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:37:41 +0000

    remove answer clean especially happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
