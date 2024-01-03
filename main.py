2df8478f2cda17683ffe28e5f614e5039a536d72
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:16:33 +0000

    provide smile space see certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
