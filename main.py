aa322fa2ab15bb7692acdbeb1c6df6e756ff85ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:22:55 +0000

    bridge true needle protection shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
