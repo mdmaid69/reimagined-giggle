a53703cd7735bfd2a92a96874a0dea6476f533b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:35:00 +0000

    help call admit keepaway somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
