6c7e4665a23d62ce99558a9a2def1b7f1d418b28
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:28:52 +0000

    than repair actual explanation attempt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
