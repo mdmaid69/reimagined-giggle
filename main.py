75eaab9ef5ea424498b6d969d5cc9f053cdd4986
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:11:29 +0000

    back so in hurt getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def reverse_list(lst):
-        return lst[::-1]
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
