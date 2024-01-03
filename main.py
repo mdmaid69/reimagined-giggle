cbefd656d53fac5bb4e748dda2a93f47197989a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:16 +0000

    quantity contribute plenty clean certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
