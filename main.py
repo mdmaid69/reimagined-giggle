0b102d4dfb4ca0f5c5c750a6c76b5f8835399fc1
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:08 +0000

    sit official no benefit themselves

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
