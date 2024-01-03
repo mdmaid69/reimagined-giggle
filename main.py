77ef2a0418b5284e0dcbae82bd9ed08e5ffd4a40
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:40:05 +0000

    quickly there recent around aware

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
