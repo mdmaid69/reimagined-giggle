1af8fa504d512f2306f731feeb7bddd26b7ab287
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:34 +0000

    deal whistle under downstairs commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
