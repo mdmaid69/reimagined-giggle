97e9796e494a76b500e2bc358d9913e4e80e39e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:50:25 +0000

    spring hold frighten 570enjoyable goaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
