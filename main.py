936b92adb81ecf9a592c9e8fab4a15c276c34dbc
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:02:10 +0000

    affect factory worry poor Bb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
