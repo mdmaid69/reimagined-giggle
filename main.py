77e8ef681ef7ef669a9c890b63a24c30e304162b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:02:29 +0000

    breakup record lower hate little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
