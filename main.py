821ba931faa110790089c1771582e1bf29f7eabb
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:52:03 +0000

    black pretty apart present surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
