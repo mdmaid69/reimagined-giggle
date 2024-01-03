4ca915aa7743d2371b42736b2adeffbd2cb26d1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:14:15 +0000

    note book represent related favour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
