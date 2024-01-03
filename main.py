d06222be292f7d35386dcb3e4159d20d6335867a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:23:41 +0000

    thick obtain flavour backwards whichever

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
