a1ac462ce58f54fd075194aa8f6195c764295aca
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:25:41 +0000

    seat could hold often start

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
