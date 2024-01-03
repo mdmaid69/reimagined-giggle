9c6f1052deac145b64c1bc7fec919dee097b72d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:39:46 +0000

    settle fault start reject service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def square_number(x):
+        return x**2
