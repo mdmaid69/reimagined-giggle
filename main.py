a550660c59da8fdc2ea5e1d86df1a8de267eaf5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:30:26 +0000

    watch tradition object level mouth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+name = "Python"
+print("Hello,", name)
