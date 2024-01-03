f0725a9eea0cdb93134247fe6ddff5f9203cfe3f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:44:23 +0000

    union polite phone fire rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
