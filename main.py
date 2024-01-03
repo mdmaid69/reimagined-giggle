c339ab7ee614656fd0a718baffa10d794bf9f941
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:57:32 +0000

    beginning wall weekend destroy goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_min(lst):
+        return min(lst)
