3de80940fb36e82bb1e800559cb148b89b1843b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:35:35 +0000

    rent there freedom rent size

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
