343bb7a1b52a8805aaa2435a0411b121d3c4fd98
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:02:00 +0000

    mass to sick illness truth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-print(os.getcwd())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_even(n):
+        return n % 2 == 0
