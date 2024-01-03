811b0d2241c2bd79da6595ca68cd2b7bfc4f0ab9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:15:46 +0000

    independent light average notice tread

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-print(os.getcwd())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
