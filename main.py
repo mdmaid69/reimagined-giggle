da64e97648cbc5c702c974040a650080ab5e6f2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:49:56 +0000

    sleep store speak close clear

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def cube_number(x):
+        return x**3
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
