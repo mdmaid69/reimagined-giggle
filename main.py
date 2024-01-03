6be56763cc9b5c97ad8250511cbe74c71e91437b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:52:17 +0000

    best strike try wheel callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_float(array):
+        return float(array[0])
