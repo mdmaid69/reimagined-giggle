4cf79216a77b6d7d02632aad9adb1af155ae3f95
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:50:36 +0000

    let brush satisfactory but yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import math
+print(math.pi)
