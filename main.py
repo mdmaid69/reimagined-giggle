f228463f24499a427be2c8540ae6b55cc6bce40a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:12:10 +0000

    shelter consult from who backwards

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import numpy as np
+print(np.array([1, 2, 3]))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
