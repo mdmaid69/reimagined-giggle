39039721020ccc1b689fdf5c334fc9fe3811984e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:18:33 +0000

    not punish second sky contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_str(array):
+        return str(array)
