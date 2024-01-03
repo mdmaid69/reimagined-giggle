42b7c1774a3893ddd4089fd18679ab4969e68b71
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:52:19 +0000

    plenty takeoff stretch thinkover silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
