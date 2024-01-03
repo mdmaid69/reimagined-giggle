32ab287041b9cd524dc8086a852de50a9226d4ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:37 +0000

    guilty smooth mysterious unkind should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
