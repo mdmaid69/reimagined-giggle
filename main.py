72703a1dacb373c9093ede075f0cb00362ad9a7f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:48:45 +0000

    content read unfair exhibition road

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
