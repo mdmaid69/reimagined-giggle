59dc9d634f80de2826b1df8f2b6d83328d10d78e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:26:28 +0000

    job fine insist laugh related

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
