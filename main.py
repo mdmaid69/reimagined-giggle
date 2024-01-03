9815bd0cd52c93c2427e658cd6d61a3fc643bdbe
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:53 +0000

    female second sure total base

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
