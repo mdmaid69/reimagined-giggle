0534c9bfce22cd7662a547022de2344e6c0b891f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:58:17 +0000

    trick several base crime wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
