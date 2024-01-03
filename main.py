3131c8efbdcefa8cac852dd2b0640ce1ade1fd8f
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:19:43 +0000

    careless comeoff behave worried there

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def find_unique_words(sentence):
-        return set(sentence.split())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
