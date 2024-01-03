5019ea3e6abcf9dd2b1b0a2a992d45f723616f50
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:25:42 +0000

    decide label all control wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
