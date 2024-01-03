15e8cd8eb0b8b45c594968fc828e390335dc858f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:00:45 +0000

    tear everything child union feeling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_power(work, time):
+        return work / time
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
