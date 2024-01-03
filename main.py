24a42081c07a4267bb47fca335102be4a222c2e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:06:22 +0000

    sort disappointed takeout waste warning

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
