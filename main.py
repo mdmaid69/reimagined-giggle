0784098141f772d4fa0c816a7e5ff636e58da495
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:24:54 +0000

    pin level fast earn lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
