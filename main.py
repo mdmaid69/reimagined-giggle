f4bc9e10368559c1e85809bddfd3249833a4f55a
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:12:39 +0000

    past peace simple average willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
