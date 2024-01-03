78ab1d573c58feba8c517b4d1ba9d18008ea3e86
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:25:30 +0000

    tear never himself talkoutof handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
