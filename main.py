12c11819cee5faa26bb5a2048848274fba90c35d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:26:13 +0000

    crowd mystery window make lend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
