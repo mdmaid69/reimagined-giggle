41f3ce148f96a2be8c7dfb4bd9f89f1fc63be795
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:04:04 +0000

    sweet teach flavour discovery reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
