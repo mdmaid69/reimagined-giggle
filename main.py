8f36e576c759891fa235319aa73ffed4748fbb91
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:47:38 +0000

    mean Yy behave square flood

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
