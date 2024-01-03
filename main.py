715033613be4dd14dc92cb7ff7645d4a758e8773
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:24:53 +0000

    immediately hospital themselves telephone noise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
