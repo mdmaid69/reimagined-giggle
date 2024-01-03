dcf6f4aa20276d5951f00e197af779a202e9278e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:16:48 +0000

    cheap estimate comein perhaps listenfor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
