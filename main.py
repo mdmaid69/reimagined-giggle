00be791bebd792e8a7c615da34f5aa2765319bfa
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:51:03 +0000

    win break giveback content different

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
