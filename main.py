b079a8a24c395622aa960538c4c6aa9d4b37caed
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:59:28 +0000

    upwards party regret remark sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
