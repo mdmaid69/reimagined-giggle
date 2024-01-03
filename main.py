d4b90574f38b11783e52972b69b569af0a5df7f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:05:19 +0000

    method especially wonder regularly blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 def add_numbers(x, y):
         return x + y
