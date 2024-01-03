0b394aa8641051062b572a413d6851fd5075f7f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:34:14 +0000

    discussion side month turninto chat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
