92f1c8e8d5dc29f64fa55b9e07717c34326e599a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:32:57 +0000

    very tidy offend also grand

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+print(sum(range(10)))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
