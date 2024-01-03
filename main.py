71cfe00784fd9a95f4e1e926bacb5e6eb71a3b95
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:59:11 +0000

    broken mystery lesson group period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
