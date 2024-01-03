2bebe12cf46f38cdd2df19e34c041974e3d533eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:41:03 +0000

    dream great fasten whole shoot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
