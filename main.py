7d32624bdebcc37c4e9b82fe5cce524b3e2a4ffc
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:08:07 +0000

    contact war needle mix travel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-print(math.pi)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
