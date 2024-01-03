cbacfa5f770f6246ba8cebc07d486dcc11efff36
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:05:23 +0000

    comefrom shop lookaround event feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
