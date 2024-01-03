e28e9a15ef62f5ba7cbd12bf5cf039140a31bbbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:13:06 +0000

    risk swim pile change her

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_work(force, distance):
+        return force * distance
