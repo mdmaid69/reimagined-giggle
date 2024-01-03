c943af432a9a73048fca535ee0a1a03778849f51
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:20:54 +0000

    damp state impressed project smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
