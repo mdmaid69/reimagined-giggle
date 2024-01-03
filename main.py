f487bf29f712a85bf3d54ca88992af787a9e7c6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:56:05 +0000

    brother cover big rank period

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_to_binary(n):
+        return bin(n)
+def count_characters(sentence):
+        return len(sentence)
