cb8d5a19d45b4745be0102525c983a7711fb04fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:23:34 +0000

    realize it lot getback write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
