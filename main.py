ce24954af5bb3759a866f23f8b2c2009696d165f
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:45:22 +0000

    tie godown cost meeting centimetre

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
