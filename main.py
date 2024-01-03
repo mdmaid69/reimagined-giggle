bcf02b1ac536f99709dd54204584ec8df88ced93
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:32:30 +0000

    danger inquire standard abroad difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
