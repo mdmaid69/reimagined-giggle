fe4e05063f24e7cdfe9944967dcc0e07ad0d43a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:10:49 +0000

    size rarely strike maximum low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
