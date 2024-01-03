21a39f4678b742733370ced03aee5b8d75f34582
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:24:06 +0000

    move nor body middle wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
