6245833fcd7fc4e2b51a1465e93edcbf768ced20
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:41:50 +0000

    nature live second year least

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
