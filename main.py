8dddd1587b0574cfbf1718b356a40da393e04dcf
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:02:41 +0000

    successful wrong main them Rr

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import json
+print(json.dumps({"name": "John", "age": 30}))
