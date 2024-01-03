bf2bf18d5813547593045133cdd520933e40c3c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:12:13 +0000

    among patience activity skin round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
