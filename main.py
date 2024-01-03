c21dd5251a70754ae7110dfbc0b70c9ac6629eb6
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:19:57 +0000

    consider pretty remove interview against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
