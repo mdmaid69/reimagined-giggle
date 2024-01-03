6ec01bc91b69132f3b44627f8c2ee50195dc00cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:54:14 +0000

    wet difficulty regularly draw television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
