6f1e8f7bfa0c2b7bfe3d364f99a33a892e5505aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:52:55 +0000

    since fortunate use unless unless

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
