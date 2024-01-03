7a7d262132e19786c5ec533f38895a28264c27fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:00:31 +0000

    proof indeed chance my necessary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
