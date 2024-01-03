89a12b09cdcd7252a4c9d214b4994f02be4f91b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:04:35 +0000

    friendly bang threaten anybody drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
