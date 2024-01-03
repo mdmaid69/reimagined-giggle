0a8f69fe1a81b0a3a0a11ee01ffb689f1c7e16c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:19:33 +0000

    you snow sort hole yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
