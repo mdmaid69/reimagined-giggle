8b9f2ab454edad0cb08f0cd1cd307a697a53a91b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:00 +0000

    fit skin Bb sticky genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
