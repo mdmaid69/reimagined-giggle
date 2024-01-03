17651f5404c019d9a6a477235d154dc82eeb9cdf
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:49:39 +0000

    deliberately lookafter finish meet shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
