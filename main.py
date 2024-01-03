462ec980a5a6ca151625d200a0d061c9a5877e26
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:43:27 +0000

    go season war market content

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_pressure(force, area):
+        return force / area
