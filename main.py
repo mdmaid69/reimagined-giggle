ce0263fc46c546c5916ad420add068cc9090cc9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:30:12 +0000

    various thinkthrough class where skin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import tensorflow as tf
+print(tf.__version__)
