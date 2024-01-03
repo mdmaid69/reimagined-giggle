317b5bc1caa1b67c373428929052ef3d0011b03b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:02:34 +0000

    interview honest work imagine lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
