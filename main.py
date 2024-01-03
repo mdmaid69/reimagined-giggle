6258bd4cfbe26ba8a58e779cbe5e4e2fc2acc7b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:01:57 +0000

    satisfied curious sting pin success

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
