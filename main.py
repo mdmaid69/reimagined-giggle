69a7298c537a7bd651d0da783dbf1620dea16f22
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:38:08 +0000

    only statement substance burst onto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
+import array
+def get_array_as_repr(array):
+        return repr(array)
