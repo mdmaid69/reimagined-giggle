3a57bd2e571a3672293673a64e1b4b0a6d90d404
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:22:45 +0000

    sharp communicate list human stretch

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import sys
-def print_python_version():
-        return sys.version
+def reverse_list(lst):
+        return lst[::-1]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
