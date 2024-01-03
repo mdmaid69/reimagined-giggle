d9919383ccaffec15337c8bf4290c53b9311bc1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:17:24 +0000

    giveaway noone situation over switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
