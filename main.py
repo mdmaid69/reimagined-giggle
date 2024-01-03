7b22187f8816a42264679d19ef309874eca4f1f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:35:21 +0000

    light tense any short loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
