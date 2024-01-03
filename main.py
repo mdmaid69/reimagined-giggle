b735ae02242b09c5e9edd04da5dc877032bf6595
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:14:22 +0000

    sentence sick female keen myself

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-name = "Python"
-print("Hello,", name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import numpy as np
+print(np.array([1, 2, 3]))
