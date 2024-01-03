32f5de5847813c0395f119a622db58f835354ffa
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:46:21 +0000

    desperate perfect tap direct near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
