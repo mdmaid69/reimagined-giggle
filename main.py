a392b90787184daffbb553d6ca95b0a3a2e73bfb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:48:37 +0000

    holdup her thinkover yet relation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
