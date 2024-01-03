c66e1a7a2a90bd4b2affb4944b09eca5fdbcd5d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:57:37 +0000

    plastic clearly job rush reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import collections
+def create_queue():
+        return collections.deque()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
