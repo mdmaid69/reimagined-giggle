b7b3ded657b58205ce87fcee9a54be36ce89dc93
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:58:19 +0000

    stick scare rescue recovery unable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
