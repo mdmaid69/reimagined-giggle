0dd8c82206a39a682419c85865d65b2b187ad740
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:22:48 +0000

    swimming pity moral curious thankyou

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
