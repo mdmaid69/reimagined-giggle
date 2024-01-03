bc36002ca3c083688eae3f92f562ef1fd3b278fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:51 +0000

    favourite everything getback sticky receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
