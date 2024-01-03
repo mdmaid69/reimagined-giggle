a142b12f68953ce304f27e7b8adbf8d0347c77be
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:15:46 +0000

    slice scream go child mail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def is_even(n):
+        return n % 2 == 0
