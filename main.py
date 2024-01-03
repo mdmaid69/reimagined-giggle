e1ca1e0d7fcdb56537ef25ca35c19dc88ae95d4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:03:36 +0000

    carry terrible physical profit uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def convert_to_binary(n):
+        return bin(n)
