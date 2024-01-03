f71b344533e3ef6fb69416b2990546e9f9790068
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:58 +0000

    production help science substance afternoon

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
