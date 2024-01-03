cc7b79d355cecfa430c7169ce7750b566743b896
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:25:25 +0000

    alive union cheat sudden bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
