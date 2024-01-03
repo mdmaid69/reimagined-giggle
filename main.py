f9e194f2d68e2634b09bff44c12d6c412ad53d44
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:05:28 +0000

    stupid strike up equal behaviour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import collections
-def create_user_list():
-        return collections.UserList()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def create_ordered_dict():
+        return collections.OrderedDict()
+import array
+def get_array_typecode(array):
+        return array.typecode
