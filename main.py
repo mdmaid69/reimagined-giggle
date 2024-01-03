3dea8f962ceda20bb1d17d17ac77b4e3145bdfc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:43:16 +0000

    properly dead awake price lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def set_array_item(array, i, item):
+        array[i] = item
