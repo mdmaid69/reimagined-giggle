117f822ed78c0b2f6ff487c7e8d3da3cb8b6c7bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:38:25 +0000

    laugh ride everyone contain violence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
