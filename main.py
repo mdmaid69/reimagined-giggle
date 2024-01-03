458ffa126db7c332058581c46333ac1977c56279
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:50:18 +0000

    copy watch usually week change

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
