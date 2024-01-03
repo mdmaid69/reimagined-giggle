3e0222c29a32245765155a05870f97bebbe74252
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:02:25 +0000

    direct store wave pronounce score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import datetime
+def get_today_date():
+        return datetime.date.today()
