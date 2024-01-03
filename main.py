a92ce1fb79fe18453b84e84316d5a6c8d3405885
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:18:28 +0000

    out error country 566end gentle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import sys
+print(sys.version)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
