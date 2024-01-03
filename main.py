401bac2dafbc5ce51331a424e4a5461d737c1f22
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:40:25 +0000

    surface study Hh draw prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
