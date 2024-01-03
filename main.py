5382b6a70439f26ea328bea186c4001af65a5996
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:07:21 +0000

    record wonder thinkover before hope

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
