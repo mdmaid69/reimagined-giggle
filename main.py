3aaf5a4660cac0f358ed208135366976210f7836
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:53:07 +0000

    closed show forget bottom exactly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
