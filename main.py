09d4dbfdced5bbc1aef163210a568415451622f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:19:42 +0000

    scared behave evening mention across

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
