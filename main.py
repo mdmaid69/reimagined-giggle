a8d590fe2a02cb0639e33fcb37a85880e95d995b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:18:38 +0000

    debt unhappy damp represent affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
