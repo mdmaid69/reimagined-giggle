25619c7b37022ec3a20d150a8d44ddbf54703c9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:01:11 +0000

    somebody train before connection report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+from collections import Counter
+print(Counter("hello world"))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
