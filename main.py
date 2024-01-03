5663387f5ebc4b6d81dfa744535d0cd526698cc9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:00:41 +0000

    odd flight otherwise smoke about

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def calculate_area_rectangle(l, w):
+        return l * w
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
