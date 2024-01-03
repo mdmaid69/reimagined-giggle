fd038e010b972e491d2a1206ff21773538b24ebb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:47:35 +0000

    muscle far confident keepaway enthusiastic

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def iterate_over_array(array):
+        for item in array:
+        print(item)
