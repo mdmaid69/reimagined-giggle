710f6772b4063dc9b637e089b95ce27bd902e44c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:19:48 +0000

    state zero risk great write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
