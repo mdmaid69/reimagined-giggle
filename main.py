ae9e3345fb867520ea38d104820a053bd12aa8af
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:49:49 +0000

    watch little move look right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
