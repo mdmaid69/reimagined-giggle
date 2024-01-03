10a3127b24d64ff655eccc72caa9ba821b489599
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:36:50 +0000

    to fight moral friendly whatever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
+def find_max(lst):
+        return max(lst)
