868052d4570bfc152da0c2b6bf1537e1f285c230
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:32:54 +0000

    breakup smell last commit relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
