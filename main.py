49261aa2d35db89be671012b5020342fcb407cb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:36:00 +0000

    572enter pass prize giveout emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
