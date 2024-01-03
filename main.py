5387869d302000f596edd0f36347bc6e01fb2332
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:39:31 +0000

    he regularly strict side inside

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def divide_numbers(x, y):
+        return x / y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
