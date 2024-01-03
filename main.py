64fac0da40bb18993e247e4b39e33d69f6e3665f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:28:16 +0000

    hole likely himself careful see

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def find_max(lst):
-        return max(lst)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
