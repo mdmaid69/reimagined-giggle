5d49430dfabe4f5d874eda0e58648e1b398396d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:33:50 +0000

    set smile succeed modern regional

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
