cefd149410dbd6fc9ae022ff4cad330c461e2669
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:52:40 +0000

    match best accept stir nature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_username():
+        return getpass.getuser()
