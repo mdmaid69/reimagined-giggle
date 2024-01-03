db8109894e0f48480528fa3668d8f738e00ac778
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:28:02 +0000

    unlikely shoot unfortunate relative turnaway

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
