0dae4fd5f6753fae7dda6d1f480a4e4157bfd9bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:07:19 +0000

    cost smile list tense one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
