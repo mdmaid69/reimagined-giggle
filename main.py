dbda9bfb6c5a593e548e2e32c68501f7a4781391
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:59:31 +0000

    gradually delight solve after total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
