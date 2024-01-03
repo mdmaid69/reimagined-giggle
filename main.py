f9de9eeb5da25923f81a6b4f990179802518b180
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:54:16 +0000

    whistle boil warning Ll famous

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-import os
-print(os.getcwd())
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
