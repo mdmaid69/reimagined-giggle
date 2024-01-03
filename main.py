d65d4fc148df579477207b5a8eecf5c07a7c1ee7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:23:03 +0000

    getup small influence translate mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_sine(x):
+        return math.sin(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
