4d816fe7232bf9cd7b4a4bb0e320c866171cf032
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:19:41 +0000

    over hang see me high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        print(sys.version)
