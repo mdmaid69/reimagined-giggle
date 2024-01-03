042926bb35b87fee3d0be93ddd58fbd08187b218
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:24:24 +0000

    alive most 573entertainment give always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def square_number(x):
+        return x**2
