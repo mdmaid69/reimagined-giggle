5cd3e3fdc93c904c69af5133f06539e546d1ffdf
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:13:06 +0000

    cough basic interview convince comeoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import getpass
+def get_username():
+        return getpass.getuser()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
