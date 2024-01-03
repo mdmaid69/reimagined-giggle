0e3f2264c805dfed141d9db4d68b868f6cb6520a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:01:34 +0000

    expect embarrassed first stare glad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+print(random.randint(0, 100))
