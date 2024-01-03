f93d32f40fcf24350f5e0c63d3b1fa997f3203ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:31:31 +0000

    temper odd makeup criticize Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def square_number(x):
+        return x**2
