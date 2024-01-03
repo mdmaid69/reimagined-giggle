0e35a3ce0e13148037f2cae6d99f44dca1f3f563
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:09:35 +0000

    hurry pinch cost favourite peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
