77ee89f544260526c6316be32e532c3fbb0c4d9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:06:24 +0000

    force aim breakdown seem miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
