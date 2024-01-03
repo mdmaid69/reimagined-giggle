6b8fa53b9f265dd7183082d1cac15100af727540
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:56:59 +0000

    bear turninto slightly beside building

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
