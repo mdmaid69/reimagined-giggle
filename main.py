9e5cab40f2949ef8e69c9cbe4b89fb9616a9265f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:23:54 +0000

    test fast slowly public suck

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
