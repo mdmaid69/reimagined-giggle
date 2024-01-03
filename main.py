060b42e562a823b1a710b822694ffabfe56e1a66
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:04:19 +0000

    forgive needle weather cum piece

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
