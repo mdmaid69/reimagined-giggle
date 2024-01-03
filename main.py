d8aa58176cd3835a1d7590e13a3ad9982611a083
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:20:52 +0000

    lose holdup honest hard cool

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
