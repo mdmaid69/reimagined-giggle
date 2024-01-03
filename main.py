aef13d7ac9ac08b61657d2f16d1b954303df2f6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:12 +0000

    hand miss on talk teach

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
