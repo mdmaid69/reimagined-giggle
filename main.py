79707bd29e1575c73ac491601463aa70449a4998
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:40:48 +0000

    hurry wake responsible thing trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def convert_to_octal(n):
+        return oct(n)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
