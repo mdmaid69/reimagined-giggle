b3173a3dd498289fb6405db24c8d8e59ec2d3e05
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:12:41 +0000

    also Hh goup in look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
