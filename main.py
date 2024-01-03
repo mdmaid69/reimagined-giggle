54cb2f99c0a5c1d3cdfbf4bbc7b82e45d8299fe6
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:58:59 +0000

    protest something dream soft offer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
