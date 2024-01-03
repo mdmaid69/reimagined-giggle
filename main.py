02920e431ab971a25223ea21606acfe2b4e74da2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:11:50 +0000

    immediate holdout reliable computer print

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
