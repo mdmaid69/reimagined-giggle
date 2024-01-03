50a5159ee44da7a3917d7730993926fa8cfbabfb
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:03:38 +0000

    from careful order bringback left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
