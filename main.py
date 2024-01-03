2dbf15ea707ab88cc5943b909244e58f826db207
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:52:14 +0000

    problem contents project fire best

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
+def is_odd(n):
+        return n % 2 != 0
