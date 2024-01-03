f20253284fe3f5a156805244e66330e16257de91
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:09:14 +0000

    port Dd runinto life worse

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import collections
+def create_user_string():
+        return collections.UserString()
