00c7f0317449183a4cd2c83c12127fc25536a91f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:45:20 +0000

    everywhere out understand drink willing

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
