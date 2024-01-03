c0744e143950ff4981ba6a67c1ae60784a4eabe2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:26:42 +0000

    convenient urgent upwards condition university

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def append_to_array(array, item):
+        array.append(item)
