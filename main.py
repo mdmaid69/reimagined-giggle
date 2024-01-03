6a316bf9e5e360af6100d45b06c373181eccd71f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:36:19 +0000

    seat perform trick instrument borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
