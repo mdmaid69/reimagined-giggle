ffb94bde989da8af267169cc2d3cfa9a3edcbe7c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:24:48 +0000

    refer makeinto couple operate service

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
