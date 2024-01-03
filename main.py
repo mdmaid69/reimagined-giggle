b571a5867bf4f2783e7153f502195c7dc4c42455
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:37:19 +0000

    tight hit object across keepoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print(sum(range(10)))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
