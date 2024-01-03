18a94e0b0b3bcf34fb6e1d917cc617c38d71878e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:04:50 +0000

    lock about song birth noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
