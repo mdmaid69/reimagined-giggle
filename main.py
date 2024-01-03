8a6245e7c6d3f43f236580a6b3d063ce7c4feec0
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:20:06 +0000

    care cross breakin behind accident

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
