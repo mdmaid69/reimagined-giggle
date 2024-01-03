9a5ed6a22b4b2a7b4067814f920c0a7e9557526e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:20:21 +0000

    duty lucky afford fear Ff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def is_odd(n):
+        return n % 2 != 0
