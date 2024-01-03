588711af29cd507345aaeaa2fe74e686fd9bc85c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:19:54 +0000

    crowded name expensive stare measure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
