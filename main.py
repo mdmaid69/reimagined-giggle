c498083dfa306cd045f93eebd58b9fcaba5d51d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:46:56 +0000

    fierce trip broken make hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def delete_file(file_name):
+        os.remove(file_name)
