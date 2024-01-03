35748bbc794562f19bb0d216cc8bfa96bfaa1a75
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:27:56 +0000

    makeup related skin scare long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import getpass
+def get_username():
+        return getpass.getuser()
