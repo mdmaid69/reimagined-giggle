cd45c0bb4001f8ca2ad573c78367c0c3dd0541cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:02:17 +0000

    trick grade fortunately name sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
