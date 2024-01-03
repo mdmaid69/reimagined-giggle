beb8cd42dd80afeac6ea9f0d9f3f9b647e6142b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:54:12 +0000

    object anywhere discuss plate innocent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
