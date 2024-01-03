a2a687b46f1bb5e4d383e5530feef5a96cf9a4b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:16:37 +0000

    coat hope separate development benefit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
