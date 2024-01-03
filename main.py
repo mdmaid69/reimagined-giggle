38457e75204ca0f6e3f6ceecd9d3ba857eedb737
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:43 +0000

    region sentence more interview nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_item(array, i):
+        return array[i]
