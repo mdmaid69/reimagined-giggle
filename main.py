e8a2bdfc0ffbba583c6bf268de54c26dca9e4761
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:32:07 +0000

    thankyou long blind team makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
