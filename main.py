7994296029e4516aec512552dc0a479f94817f01
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:00:52 +0000

    along unite world phone another

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_characters(sentence):
+        return len(sentence)
