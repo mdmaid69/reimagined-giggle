3ab587b6221bf0d9545246fdc2ce8da4fc9a559e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:02:30 +0000

    post along regular coffee free

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
