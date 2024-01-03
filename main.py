e5fe8161c73a75fc8210934b7d34ca6730cbd55d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:34:46 +0000

    deep pleased children empty about

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
