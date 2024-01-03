fbb6ac96d79b6515ac29d8b0d204840a9789b566
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:21:31 +0000

    actually regular opponent surface honest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
