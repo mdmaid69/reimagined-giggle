577acbc5f5c83d7a3cadf37bcea1619c54b56155
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:03:16 +0000

    happen clean accidentally situation disappear

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
