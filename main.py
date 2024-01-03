f3b3ff724cc8c82b290c6fbcd0548a466fadc29b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:13:17 +0000

    light goon company indeed both

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
