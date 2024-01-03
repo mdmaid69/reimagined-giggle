665c660997e37a8b2b487c5142247f6c326a3bb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:57:51 +0000

    reply surprise welcome trousers dare

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
