91d601282878418e688054b3fe1b8e0e6644ac47
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:00:53 +0000

    only makeup noise range since

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_unique_words(sentence):
+        return set(sentence.split())
