587411f1b75a063723d6e31ab9ee02f8d0a66f66
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:52:59 +0000

    avoid hungry short part healthy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
