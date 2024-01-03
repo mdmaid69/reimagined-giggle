f2a60b55afc7c2442c68b4b94ed18a2e84b21e19
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:08:47 +0000

    ticket break disappointed doubt Qq

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
