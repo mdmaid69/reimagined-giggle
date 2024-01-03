255f43b2018d12e2cf4d89c4d6a2eea74eb1f730
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:07:34 +0000

    apart generally judge terrible shall

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
