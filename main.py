4a6a12a7192d671deb852f7fabaf0956d687c923
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:14:07 +0000

    niggaacademy exam shrink argument strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
