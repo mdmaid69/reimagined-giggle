bfd09bd3672ebfbe43d2cb6f7a80066320667f8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:48:54 +0000

    few shine fix mend centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
