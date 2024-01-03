06be7d5ae6ba24f712dc809a5091c00e9c06e452
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:26:35 +0000

    lend regularly jump legal effect

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+text = "Hello, world!"
+print("Characters:", len(text))
