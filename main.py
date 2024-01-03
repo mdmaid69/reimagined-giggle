04c5b3b5e12981fb9aad3b07b1393d47440b8d83
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:41:26 +0000

    place best fun stay shade

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def read_from_json(json_string):
+        return json.loads(json_string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
