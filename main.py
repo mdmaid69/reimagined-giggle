1ae716c20d958f7ff9e504d51dd696d7e31f777f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:57:13 +0000

    claim base still legal on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
