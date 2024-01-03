e6d87565969c7e9ab5057b1607ae5b96d8e337a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:49:26 +0000

    secret from very leave message

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
