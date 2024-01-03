6addb3758f073f350942dcb25d94d51710a8077f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:27:34 +0000

    room either excuse Rr Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
