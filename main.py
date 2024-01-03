15822a77efd7437e99a8b9337eaaab8cf4847df4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:40:25 +0000

    promise stamp switch continue search

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def convert_to_octal(n):
+        return oct(n)
