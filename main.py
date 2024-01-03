ae89c26e2e25dbfe17a6df353104bcd6fef6b74d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:17:11 +0000

    present spray patient lost insist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
