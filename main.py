a6eae1e006f4be04072b039095e7ab3ec5a75f8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:28:50 +0000

    Qq admit port print rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
