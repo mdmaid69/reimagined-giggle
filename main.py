abd7b15163f9e6b3773b1f17493bbbda234297ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:49:24 +0000

    area under meal set act

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
