3b526cc50804a5edeaec2e1b27d165b8f217d1a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:34:46 +0000

    rank unpleasant healthy well debt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
