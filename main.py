1d74f86b99497cbc57eba2ccee3c15ced7f3403e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:13:25 +0000

    accommodation away explain grand yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_speed(distance, time):
-        return distance / time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
