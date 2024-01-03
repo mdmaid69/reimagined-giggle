c0e98358e7074437ffbae238a2db9cdb52a29376
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:42:19 +0000

    accidentally level Rr fold fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
