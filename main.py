b67ba38c2fda91ad6cb0385c77c95be28dfd2cb6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:22 +0000

    learn task bound sell coffee

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
