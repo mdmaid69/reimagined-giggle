613a4e9fbdafc1c653fd6c51375b8eabcbe5ea68
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:45:41 +0000

    bank suggestion collection ball almost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_hex(n):
+        return hex(n)
