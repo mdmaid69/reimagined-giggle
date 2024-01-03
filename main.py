b84ac136503938d6c86719f9111fa6b5c981d808
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:09:01 +0000

    nice beat really out quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
