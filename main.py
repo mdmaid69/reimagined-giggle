8c633e9ff774804782eb75d92b12d9fbf0a6f7fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:27:38 +0000

    stand order bath gooff demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_unique_words(sentence):
+        return set(sentence.split())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
