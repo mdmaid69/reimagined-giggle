153092077366d01ab61af9ea2fda4745f6b3155a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:24:26 +0000

    today arrangement substance any remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_current_date():
+        return datetime.date.today()
