6bfdb01cc5b430cab411667f11e7a6b2435d7807
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:55 +0000

    their finish stiff set expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
