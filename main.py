d6e177376b64f119da243c2ac608958de3ff15cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:07:20 +0000

    pupil suffer lookafter interrupt long

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_repr(array):
+        return repr(array)
