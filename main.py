ab930faf53e3e3a24faf86252108ef89067b68c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:12:35 +0000

    notice after comment back draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
