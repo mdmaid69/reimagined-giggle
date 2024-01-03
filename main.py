d2387d1971977d0a90967c51e9960ee3549c080c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:27:14 +0000

    risk agreement throughout sense giveup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
