c3f5b6475bebef5dce8321edddb3c4837a990989
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:39:44 +0000

    tend violence kill wire approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
