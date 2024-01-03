dbf80f01098df246eb3abc531617e520eb64131a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:34:05 +0000

    hit direct bringdown strict fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
