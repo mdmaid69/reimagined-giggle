bd40e030c7d6b06553e708498982231ee8c0c20c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:50:59 +0000

    be please put greeting state

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
