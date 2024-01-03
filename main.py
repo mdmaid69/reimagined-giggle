b374626016ad09cd7a7d026353a4dae5bc26cef0
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:01:55 +0000

    lay catch knowledge uncertain correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import sys
+def exit_program():
+        sys.exit()
