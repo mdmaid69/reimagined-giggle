eb1f35efce1de1455da3675f78c8fe3183c17143
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:05:08 +0000

    play wear before noisy suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_floor(x):
+        return math.floor(x)
