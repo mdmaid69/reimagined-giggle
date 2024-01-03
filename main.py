c5cb80fb395ecd9691cb1be25c2eae3665adbea0
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:13:13 +0000

    here dream contrast sensitive care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
