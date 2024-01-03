9d4548ccba404f512b918d2e2ba7fe1bf8b8630c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:03:27 +0000

    through smell contribute funny live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
