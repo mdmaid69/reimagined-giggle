a3ea05ec632f975cdcce248b04c0477d07f621c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:06:35 +0000

    cost create disapprove goout key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
