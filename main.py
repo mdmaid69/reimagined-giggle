c0b7a74b76131318901e2af3d39485f76939edeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:35:56 +0000

    typical lookafter crash score out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
