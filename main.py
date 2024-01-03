d53ee71fde6ec38f69d749cb973d2211f5c93693
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:12:32 +0000

    touch influence breakdown deep fun

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
