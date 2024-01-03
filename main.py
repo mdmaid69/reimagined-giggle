eee469ba511ab11b3d67d6967c835a25950de8d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:58:16 +0000

    some favour impressive opposite mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
