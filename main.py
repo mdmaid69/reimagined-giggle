df5cc15aca22da1277bac25277278b262088cab5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:10:10 +0000

    recently mail positive mess admire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-text = "Hello, world!"
-print("Characters:", len(text))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
