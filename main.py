4d95366f233f4925a5b3acf636d197f8e7fcac6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:21:11 +0000

    minimum cope treat fairly impressed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
