9acd1af03f11ade50069f4e6ec8893606e41c11c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:53:15 +0000

    opinion border street pay purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
