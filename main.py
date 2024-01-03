e43222e560005f42814f28e67b289403b86d8f79
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:17:27 +0000

    vanish justice ear afraid butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def create_priority_queue():
-        return collections.deque()
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
