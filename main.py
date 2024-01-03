1647de1e5ce18657cdf1bc7118b05722e483556f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:37:12 +0000

    into personal expression forward defend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_bool(array):
+        return bool(array)
