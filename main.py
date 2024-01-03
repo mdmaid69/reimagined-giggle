c1e37da4572e326f4c521a187dc758ce490b1cf3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:30:13 +0000

    scrap like last put tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
