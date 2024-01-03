88139240d6683a3a710bb577910ea8de16f5c457
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:04 +0000

    each guess shall weigh phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
