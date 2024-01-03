5d49fa293778a0ad2ca21d9a4c3939624fefab54
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:55:55 +0000

    form fork cold free keepout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
