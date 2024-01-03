080f54731b1cc6e55bdb4b0f6bb69c36413b59d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:17:57 +0000

    hot important run start later

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import numpy as np
+print(np.array([1, 2, 3]))
