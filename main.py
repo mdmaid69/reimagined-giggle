bc7464133feb39ceeb18f91644ba39b0af62eaeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:19:04 +0000

    create constantly fine fall film

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_length(array):
+        return len(array)
