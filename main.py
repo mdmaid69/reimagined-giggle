f1e59d25d431dde0c3f16a35890e00114cf934f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:44:50 +0000

    statement disturb callback hair snow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
