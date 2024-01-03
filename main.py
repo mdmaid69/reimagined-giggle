1517d5aea7d403039a2101e7809c5b2658254988
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:36:18 +0000

    few argument pause yet forgive

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
