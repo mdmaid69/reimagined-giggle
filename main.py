b16bea901e9f0929e76d8e28423e9467ff09c884
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:07:35 +0000

    somehow water result book every

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import random
-print(random.randint(0, 100))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
