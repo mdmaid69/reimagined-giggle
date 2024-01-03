72a41cf3ff4954a6e397845f01680e58cd312246
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:53:32 +0000

    praise hot me surprise fine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
