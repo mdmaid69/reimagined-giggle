9e8e6aa63b8cf5e7acf9b3e3a91e6a5f981855b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:45 +0000

    shelter manage dry earth aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
