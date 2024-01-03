44a5434c902c2706ea78d8446a43f5269ae458b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:35:16 +0000

    relax little responsibility shall develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def cube_number(x):
-        return x**3
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
