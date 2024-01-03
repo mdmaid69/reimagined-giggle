a8ce362db24149a42c4c54632e587fbf86b73996
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:34:01 +0000

    sting substance love tough moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
