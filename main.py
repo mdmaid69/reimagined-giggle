fe480ac312fe8b440acbfaa7a243ae2e983fe52f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:15:15 +0000

    born community process keepoff half

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+print([x**2 for x in range(10)])
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
