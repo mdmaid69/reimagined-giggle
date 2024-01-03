eb78ab49a4be6c6e3db4d5ab60904bcafa5fd0a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:42:51 +0000

    no claim talkoutof software environment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import sys
+def print_python_version():
+        print(sys.version)
