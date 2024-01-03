90674a1a6bfb1923bd24d0cb2e10cc1494e2d973
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:20:49 +0000

    stage mother lot appear probably

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import collections
+def create_stack():
+        return collections.deque()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
