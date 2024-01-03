04f11d1fe412f752964092bcdff5b229e1883456
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:46:59 +0000

    search common fork type busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
