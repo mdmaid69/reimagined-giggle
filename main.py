07c234f2f9479e0ebd011459269a13f3c3e5243e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:52:22 +0000

    outside sound various way rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
