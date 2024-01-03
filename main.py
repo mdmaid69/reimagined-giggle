ab95b9a3ba3e0d2a734635864220b920a075a538
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:56:49 +0000

    letter food soil trouble freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
