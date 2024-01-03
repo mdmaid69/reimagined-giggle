110d88e8b330b8b3a00cde3804a11753bf2fc902
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:23:51 +0000

    disturb admire advice far sweep

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
