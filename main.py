6b4a5dfd09af8ff1e07074dbce94e59ab801dffc
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:16:54 +0000

    disturb anything correct sell first

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
