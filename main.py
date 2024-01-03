ace7c3813869afa45908f82ece0e2fa88121e27b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:15:44 +0000

    start takeover welcome unlucky pretty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
