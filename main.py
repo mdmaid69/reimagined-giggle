5c3868f99306f571f7736e2c059849f748d9888f
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:28:42 +0000

    instant worst several picture season

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
