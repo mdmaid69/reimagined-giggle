fc146501f482f015dc17bb5560ffdb33c95c2be3
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:02:48 +0000

    welcome destroy rich bag bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def greet(name):
+        print(f"Hello, {name}!")
