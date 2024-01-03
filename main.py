42382f70b539b7914afbc3f659698bf07a5e8737
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:16:43 +0000

    possibly exhibition fix hard spring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
