b423c9f9bdeb55a66486af14af726dfee814aa12
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:33:11 +0000

    smell lose father touch Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
