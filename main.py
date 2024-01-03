ed11c06a2c0b82af4082b89ebd6317562bf2f4c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:42 +0000

    friendly flavour card team recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_str(array):
-        return str(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
