f2d011f51c021e5d1692868b39738f36c681ec79
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:40:23 +0000

    knock far share guess herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
