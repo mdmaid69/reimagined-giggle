f6bc8c8310ef2e94a30fb01c10614aea13360058
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:18:08 +0000

    apparently real impossible turn scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+print(time.time())
