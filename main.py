2fba70452f3094442d42773b855c9756b247ac34
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:52:48 +0000

    several account survive public phone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+print(math.pi)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
