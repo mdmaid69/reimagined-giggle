aab92afb251bbd8c2f6d61d31ed7c32b4f46135b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:19:05 +0000

    outside aim sharp scream over

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def multiply_numbers(x, y):
-        return x * y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
