7231bb4d62051ee27477b642ec5d209aa587dc1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:39:35 +0000

    concentration blind turninto understand deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
