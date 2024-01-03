422fa8eb11ae2ed5861a5bb2f5a7c54cfe3e0796
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:48:40 +0000

    for news classroom Uu event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
