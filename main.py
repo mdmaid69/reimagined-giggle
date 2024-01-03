ea2da75180f68382601cbc873c5f35eca27bc68a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:32:17 +0000

    expression action near gift forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def sort_list(lst):
+        return sorted(lst)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
