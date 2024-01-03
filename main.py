d9b88e17d62aabb6ac4a3b94325902427c73963e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:38:23 +0000

    circle deal if open include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_pressure(force, area):
+        return force / area
