1aaa338db023e2af50b5071a7b3272a2e088bb59
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:23:52 +0000

    pay count grateful callback belief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_volume(length, width, height):
-        return length * width * height
+import os
+print(os.getcwd())
+def find_max(lst):
+        return max(lst)
