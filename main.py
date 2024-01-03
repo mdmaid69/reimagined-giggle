aea835c55e4e8cbf0bc4ae7b8d60ca9dee7bb396
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:15:59 +0000

    climb problem rather downstairs kick

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
