772d2c8629bc4278c33a1ac8031daeb620467f22
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:17:12 +0000

    rub exist drop holdout cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
