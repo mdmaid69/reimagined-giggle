ae5da84ec68df872acea964a498d00ecad7d4f78
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:03:26 +0000

    another complaint somehow getup nigga

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def divide_numbers(x, y):
-        return x / y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
