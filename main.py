247220287be4bc22efba0fe618c5e7b4bc2c898a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:58:20 +0000

    question compete motion Cc authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
