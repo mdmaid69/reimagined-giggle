bf473420cdc9f1526721bf267636b452f1ecbaaf
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:31:26 +0000

    regret pleased 564encourage gooff comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
