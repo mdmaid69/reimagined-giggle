4ba1db1285c91608c4d1942dd8d223bde5d154dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:22:34 +0000

    taste display protest combination plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
