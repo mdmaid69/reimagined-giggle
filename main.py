d6129fb1eca1f854187f668d93f55252189bb026
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:19:45 +0000

    Ff spray pure unhappy summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import array
-def reverse_array(array):
-        array.reverse()
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
