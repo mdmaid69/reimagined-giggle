d211ffd44e48944e697647fa14dfd35191d21381
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:30:33 +0000

    outside or discover window funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
