07d653dea3146d6ad979dab18cc114efabf1015b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:49:14 +0000

    muscle fire distant prepare save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
