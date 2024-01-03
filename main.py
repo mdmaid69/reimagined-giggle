ea87e72ae462a49617f86be15a4b6acb9486e08f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:18:02 +0000

    tired grade fill get float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
