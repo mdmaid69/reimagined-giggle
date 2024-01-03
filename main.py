f3dfb5c871ed3f734924ce5ee623daa6133c1d69
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:08:35 +0000

    private basically mainly skill rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
