e0040fdf1fb02d914032d1d8983fccfa2861b459
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:21:04 +0000

    before arm why grateful basic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
