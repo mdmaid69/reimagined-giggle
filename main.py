9995b2e4e86fbc870788588cb8a6df25c4f97ebf
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:49:51 +0000

    handle inside callat church bear

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_density(mass, volume):
+        return mass / volume
+i = 0
+while i < 5:
+        print(i)
+        i += 1
