2bd8e18344d2139c86c38a170e5de94a0ba73c08
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:30:09 +0000

    backwards land long used frighten

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
