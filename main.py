cbab8a61943621e239f15d2fb7e31313dedc8dd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:55:15 +0000

    watch and lunch there mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
