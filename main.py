e334007ca7716f476048786565a4db84f6bad507
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:03:09 +0000

    decide fit fast lookback(on) waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
