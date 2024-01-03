74548101d55bd000ed281c9c3f4bb5f4f8e2beaf
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:50:27 +0000

    unpleasant instrument getout stop force

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_power(work, time):
-        return work / time
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
