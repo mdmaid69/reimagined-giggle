f7a6158abc0f3a1e1ca068ad009254eed93b855f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:08 +0000

    compare force post own drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
