a67bcf5e01008537a93618c8fe1d4a46bc6830ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:04:31 +0000

    tonight organization yourself forget however

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
