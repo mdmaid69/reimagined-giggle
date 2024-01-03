eb8cce4b9cb207f9d767939e931eb8664d4e0179
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:56 +0000

    frequent workon sea shape comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
