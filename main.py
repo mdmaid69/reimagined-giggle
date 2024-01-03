0399ce2c09109297f6995c4139b20edb857bc7fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:23:19 +0000

    thinkthrough force respect close admit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
