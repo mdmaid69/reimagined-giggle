c9969a61d340521a2d5eade15dd4baebd2725ac6
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:08:44 +0000

    there criticism dance show just

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import numpy as np
-print(np.array([1, 2, 3]))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def reverse_list(lst):
+        return lst[::-1]
