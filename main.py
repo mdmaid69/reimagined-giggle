3b035f56436e5da7ae754bf26e915c10804d1e2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:51:55 +0000

    exploration scream treat ought tough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
