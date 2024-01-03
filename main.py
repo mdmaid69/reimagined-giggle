b412e2d454a1ce8740675ac997235cac92cc8040
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:55:10 +0000

    need rescue sometimes natural attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import datetime
+def get_today_date():
+        return datetime.date.today()
