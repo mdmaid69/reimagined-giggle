b2d094226df8193259608d7a0d9e899a67d0d71d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:35:43 +0000

    rough history job spill criticism

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+print([x**2 for x in range(10)])
