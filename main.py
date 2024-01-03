7e1baf69f402477f345f233f21cf224371a462f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:22:51 +0000

    war pupil used connect then

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
