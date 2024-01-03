335196397c919f8b60a80e3648db2e632c07ff11
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:27:45 +0000

    right count hard talkback material

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
