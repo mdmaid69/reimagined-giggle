20d36ef8d54ac4557ba0c788c863ecc13f79fa1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:31:12 +0000

    solve grand run damage intend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
+name = "Python"
+print("Hello,", name)
