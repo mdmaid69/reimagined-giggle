fede81a242d57b0a6d55852ed35ab438c37d266e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:20:25 +0000

    because whichever concerned difference likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
