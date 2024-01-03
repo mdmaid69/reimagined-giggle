37ec5d5991bd0a54e405c708b26aa9c4386adafe
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:40:33 +0000

    especially dull tough neat tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
