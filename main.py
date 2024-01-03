f9f1c00fa0d645c643779ead2fd24748ffa9964c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:06:49 +0000

    dull turnon cut limit far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
