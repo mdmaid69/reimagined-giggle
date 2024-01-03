632b4c0a77d743722240c3da814e70090e7b98e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:34 +0000

    pen makeup responsibility Zz goup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def count_elements(lst):
-        return len(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
