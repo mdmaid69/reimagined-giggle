61d66bd9ff977b8ab0a0136ec6e62114a22ed206
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:46:46 +0000

    coat yesterday gate towards government

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def add_numbers(x, y):
-        return x + y
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
