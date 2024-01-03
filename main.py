598ec2d9e9c379f7ede569c87e1179eca1c4c670
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:04:50 +0000

    wake word apply weather Hh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
