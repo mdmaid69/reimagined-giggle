64363e3bd1e2004bd1e9f8a86cccc83e996e5863
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:11:47 +0000

    well sudden burst only single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
