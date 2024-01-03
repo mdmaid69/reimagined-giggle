93580770683e5a4d769b347ccb6126497f5764bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:24:17 +0000

    negative stiff mine book inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
   def get_file_creation_time(file_name):
         return os.path.getctime(file_name)
