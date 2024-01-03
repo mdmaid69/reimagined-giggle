d28526f014f0fd9178c597e99d7689bc29874a65
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:57:16 +0000

    within split swim agree major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def cube_number(x):
-        return x**3
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
