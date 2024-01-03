38596a8aae0509499a480d466fba72adbfcef061
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:52 +0000

    instruction later mix mend brother

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_area_circle(r):
+        return 3.14 * r**2
