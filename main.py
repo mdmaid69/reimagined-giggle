ffe02e849091a11d91da9edf6236d87ad58ae615
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:46:56 +0000

    happy firm underneath later usually

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-  def reverse_list(lst):
-        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
