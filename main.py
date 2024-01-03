01926f481aee9686155caeaea8d4bb57911411d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:49:09 +0000

    bin nowhere evening naturally giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
+        return os.getcwd()
