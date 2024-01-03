5e440a2a58c14509bcfe81dcf67c6d6096690a22
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:06:24 +0000

    strict peace praise spot theirs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
