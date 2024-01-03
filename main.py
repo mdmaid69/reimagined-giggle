d601d6907b058de0811ff04cfee11a3c2f9e49e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:33:07 +0000

    weigh frame precise turn compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
