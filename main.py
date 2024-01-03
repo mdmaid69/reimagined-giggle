d558c13f23d0084973f617bf1d9b95e74128d48b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:19:43 +0000

    beside system smart trick guide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def convert_to_hex(n):
+        return hex(n)
