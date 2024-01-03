4962cd3e182daf5cb742a24ba284deab83747376
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:51:16 +0000

    same wide base upset frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def get_array_typecode(array):
+        return array.typecode
