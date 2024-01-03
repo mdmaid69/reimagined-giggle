6968cef20d1dfd07fc820880f6d536dad44963e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:20:06 +0000

    benefit reaction problem slowly shall

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
 import array
-def get_array_as_list(array):
-        return list(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_repr(array):
+        return repr(array)
