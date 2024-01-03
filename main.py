f3dd80f81a893580f8f25940cb18715c22bd4bed
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:25 +0000

    thick book or extremely cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
