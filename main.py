817e44a3c64d72b525c2181120047b57b477dff9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:12:34 +0000

    yesterday face Ee worth practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
