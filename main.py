c3eb2e817c40660995dcd70297c78e6df696bdee
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:59:21 +0000

    Ff wind past buy turnup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print("Hello, world!")
