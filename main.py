4e2ea16478d9ee5000f9904c7601b45c2511e227
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:23:32 +0000

    ticket bored justice favourite region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
