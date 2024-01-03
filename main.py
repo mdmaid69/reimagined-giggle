b291ed448f2437d24a23de2f508da2d0db2f85ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:47:11 +0000

    shine vote sit healthy really

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
