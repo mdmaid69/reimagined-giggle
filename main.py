d72009688d67bed93a71af502b0571ad044033f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:10:43 +0000

    mine method traffic pool down

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
