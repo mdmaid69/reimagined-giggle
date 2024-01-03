916034230208fbc4e26a419fc8d10af899a32cf5
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:24:22 +0000

    fix just address free previous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
