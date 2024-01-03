dd6b553cd651db4b4a1f923c0f2c9594a956230b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:23 +0000

    problem further anybody organize his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
