65f514f18cb651e23c50c65bb6f3c8fe84e8f154
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:47:35 +0000

    last mess mad list earn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
