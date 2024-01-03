28ed032bd2edbc074103570d8b18b435520170f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:00:23 +0000

    ourselves tooth fire responsible shake

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_volume(length, width, height):
+        return length * width * height
