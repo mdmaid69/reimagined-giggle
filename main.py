a0bd9a6a496e77d55e0a2201952954a3e5beffa7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:27:19 +0000

    pride example not crash believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
