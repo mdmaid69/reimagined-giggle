9168ef6f36c6e69d2cf2d40fd38051c6af47c881
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:31:02 +0000

    mad either long shrink grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
