278c256450f401a94787cd81d2455f380602f2a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:22:38 +0000

    bottom Jj I he less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
