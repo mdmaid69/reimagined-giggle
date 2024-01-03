0dcf2f8bb7e0724b2abf5d07b663ab10dc810dfd
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:37:31 +0000

    over national book threaten instrument

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
