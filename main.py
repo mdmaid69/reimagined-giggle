9eab6b4d97cae5ca2334857d04100771cc4265b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:49:31 +0000

    learn street brain perhaps view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
