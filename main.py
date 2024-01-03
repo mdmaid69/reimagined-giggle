0d8179577f088ef06fd751bdb080ebdf2d87a32d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:09:49 +0000

    deliver lower Gg press protect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
