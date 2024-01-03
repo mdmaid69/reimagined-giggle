3a0bbc45759e5e2b8662d71efd4ff96f612f3e53
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:03 +0000

    actually pure lose at tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
