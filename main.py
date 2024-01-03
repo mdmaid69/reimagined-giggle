bffe2a9f63b336290324e5ac923cc3ab2d6f76da
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:27:42 +0000

    law hope maybe round teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
