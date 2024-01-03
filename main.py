37d8f928da5a7118190ac689d52eae2315a298b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:35:50 +0000

    odd worth respect schedule exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
