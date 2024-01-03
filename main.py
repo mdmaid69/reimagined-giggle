26df76db70dc56f5c1f3ca283878091a7bd44054
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:41:59 +0000

    possibly silent trap everyone account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_unique_words(sentence):
+        return set(sentence.split())
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
