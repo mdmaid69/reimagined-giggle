6c1433df89759b1917e8ec47e58c60cfb47b81ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:00:37 +0000

    grateful OKorokay them statement schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_palindrome(s):
+        return s == s[::-1]
