3b4d16f1cca89f448fa561a8009e5064e7136d97
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:26:30 +0000

    coffee 573entertainment tradition so target

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
