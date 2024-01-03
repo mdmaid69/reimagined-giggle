7d4fd8c93f2019de048fd61ccb3f93fbcec4add1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:56:52 +0000

    include getup mend sell unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
