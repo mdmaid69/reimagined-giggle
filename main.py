e97871a0527df53877d3b669b5b5928426683369
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:42:13 +0000

    wing match name across some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
