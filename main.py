67468ec6ebc8c7e01653d35e0025b7fc60b5f836
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:38:23 +0000

    want abroad lookaround share bath

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def convert_to_octal(n):
+        return oct(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
