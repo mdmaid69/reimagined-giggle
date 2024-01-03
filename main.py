9ee310f96fe411abe0aef76368daceb2280f3c5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:50:05 +0000

    stand Cc later Hh design

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
