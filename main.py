8aacd4188ccabb8c225544e1970a2cf84a601a02
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:59 +0000

    room system single building stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def is_even(n):
-        return n % 2 == 0
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
