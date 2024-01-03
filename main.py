a2f9bd10f068fe89750546e93c769c4c17128be3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:58:55 +0000

    high sleep gather to capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
