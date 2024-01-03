7b388b9f78f4285a486378543570c011ce760d41
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:30:19 +0000

    late agreement their rest form

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
