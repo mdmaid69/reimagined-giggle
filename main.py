829563ea962753fc568ebe99861a66ac36a55b14
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:49:40 +0000

    pin train couple keepout cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def multiply_numbers(x, y):
+        return x * y
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
