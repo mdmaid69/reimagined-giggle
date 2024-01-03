7b3aadd6bf1b218f50423bbc84393a28abfcb51e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:01:43 +0000

    call close point subject afternoon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_length(array):
+        return len(array)
