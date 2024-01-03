6415a780bf6d89e5669eff2f60fa7d81e94a71ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:00:30 +0000

    accidentally inquire cruelty confidence nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
