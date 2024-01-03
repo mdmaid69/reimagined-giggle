4526a84588b3705a1488d293d05fc9e3b3d19328
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:20:09 +0000

    emphasize rough school train claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import numpy as np
   def calculate_variance(arr):
         return np.var(arr)
