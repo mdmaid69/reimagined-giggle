5b2394c05d4a64a3324544d9603ee80fc9851833
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:07:40 +0000

    present callup formal giveup doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
