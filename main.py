7b7014329679fba4e639093e1383412e400cce11
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:46:46 +0000

    dress people pollution full whichever

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
