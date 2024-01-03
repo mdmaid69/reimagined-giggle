e21dec370fc9fdd3bbfc03a6afbc770d2faf2ab0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:35:54 +0000

    land wherever memory record word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
