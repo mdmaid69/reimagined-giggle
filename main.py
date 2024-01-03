c506af43d7dd5092fcdd47e00deb2cca6911045b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:25:21 +0000

    callback poor call set himself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
