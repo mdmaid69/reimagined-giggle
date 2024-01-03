81e7bd4fb6a4c328000e2323164b2db5d99ee6ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:47:01 +0000

    open terrible progress question wind

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def convert_to_binary(n):
+        return bin(n)
+import platform
+def get_os_info():
+        return platform.uname()
