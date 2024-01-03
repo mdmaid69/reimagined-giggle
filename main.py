fccf41e8ed7859d1e506f86492f1356cc685c0fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:13 +0000

    low travel post flow plate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
