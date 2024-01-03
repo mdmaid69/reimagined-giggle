29bbbf307491bbe832e8ef9c3a65bd6fa04eabc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:06:40 +0000

    seem report nor pleasant print

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
