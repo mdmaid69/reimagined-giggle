09c5100349d8b4c99ed9404d4411d67f8a604eaa
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:46 +0000

    suit break bite without hear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def find_min(numbers):
+        return min(numbers)
