71a241db19149221e30d9c63bbae193c081b8ea4
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:08:25 +0000

    art until development seriously myself

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area(radius):
-        return 3.14 * radius * radius
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
