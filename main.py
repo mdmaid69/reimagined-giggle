165863362e8907599d815ffa426d4385051eb75f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:26:04 +0000

    false hurt afraid obvious strange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
+def convert_to_binary(n):
+        return bin(n)
