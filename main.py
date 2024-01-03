98881e6dd881a21c1fabedb8517236c4f1e5613d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:30:31 +0000

    contribute miss gradually that shoot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
