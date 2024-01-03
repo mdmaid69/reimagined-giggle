5f53196e92209a7c100a8dec982ebb689e7c710c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:32:46 +0000

    crash word recovery happen Ss

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
