ba57304936c8fb22385b6688a67b77d577394720
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:48 +0000

    lower honest content issue influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
