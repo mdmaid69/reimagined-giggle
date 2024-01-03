a8b6cf8e6d964a84d278aefbbf82ec1b6b187f07
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:20 +0000

    tape road after fork admit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+text = "Hello, world!"
+print("Characters:", len(text))
