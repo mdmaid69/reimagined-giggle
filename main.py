8590f835ba1129fe0fa73bfdd3e1a10eafb459c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:58:13 +0000

    round quickly remark fail mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
