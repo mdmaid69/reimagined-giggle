a50db8cde9b26186051e9998e43ecab2d1ad5919
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:46:27 +0000

    later shop nervous great ours

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
