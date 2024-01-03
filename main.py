64a88acef296c2a1b7c089f968de48901cade648
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:00:35 +0000

    expression notice raise thankyou pronounce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
