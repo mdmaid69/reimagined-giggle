dfd5ad03b532654c45bf51a09c79bb5dba805a63
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:51:37 +0000

    remain inquire angry rent room

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
