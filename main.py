f73e73aba6f774fa974ad4c72c95e25f5e39f70b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:10:18 +0000

    do reply Bb expensive better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
