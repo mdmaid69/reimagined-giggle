e78c00a69746142be1a6bb7c417f09a9c6921170
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:17:09 +0000

    turnback surprise plenty character change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
