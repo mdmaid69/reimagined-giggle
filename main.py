5f5b1959cc8c6913b2018649129a6414037f8f31
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:57:58 +0000

    stop mind angry experience want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import getpass
+def get_username():
+        return getpass.getuser()
+def convert_to_binary(n):
+        return bin(n)
