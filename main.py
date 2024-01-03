27ee92f26fe48c179f3d2203b51ef8bddca5cb34
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:15:30 +0000

    probably patience thanks come open

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
