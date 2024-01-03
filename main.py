efc5f9ecbf1cd43beac2415e0716eb7d79873ae9
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:17 +0000

    refuse definite how here she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import getpass
+def get_username():
+        return getpass.getuser()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
