269be31d64171465f299530ae23d09d1fbeb20fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:59:02 +0000

    recently related pity difference roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
