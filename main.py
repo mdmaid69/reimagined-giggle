2df3e42d2b6e0949eed1f7c16189910449c142f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:40:18 +0000

    give holdup every grateful neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+print(math.pi)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
