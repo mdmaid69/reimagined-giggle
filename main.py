95d816f5d220f9f6de54a63d477ccc09740f3198
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:52:29 +0000

    heat basic callout relationship prepare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def cube_number(x):
+        return x**3
