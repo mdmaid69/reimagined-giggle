fe16c67df06ab776fa315a0f4c20a49f47b8764a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:14:44 +0000

    able come worth noise rid

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+print("Hello, world!")
