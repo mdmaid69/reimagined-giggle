bf5239f8fc2bfdf3dcff7bc03f01d86c12d9cf18
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:51:39 +0000

    convenient call sea somewhere various

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import sys
+def exit_program():
+        sys.exit()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
