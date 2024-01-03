84a1b1fb38d04d938c4fc87da63415d881cb70b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:59:34 +0000

    ground bother this slowly female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
