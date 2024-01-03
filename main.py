72b55e6bcd006fe869a2111668708f200734181c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:08:09 +0000

    eye important damage myself memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
