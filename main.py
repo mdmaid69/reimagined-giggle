00cbd258756662e52dad31ff37278f9ef9809851
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:12:33 +0000

    tomorrow lock hurry might star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
