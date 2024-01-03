1eb84cd3137212449689935d2cdcd4cee0df9298
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:06:26 +0000

    me breathe tall computer detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
