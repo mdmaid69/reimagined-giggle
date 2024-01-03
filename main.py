38927fffff29b78dab9da964e4b7bf5b6ae3241f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:38:09 +0000

    rest rare abroad prove approve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_density(mass, volume):
+        return mass / volume
