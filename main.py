92773ef0cccd1d5a4123874f1283319549d2fc36
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:06:30 +0000

    sea comeround Itself mysterious one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+print(random.randint(0, 100))
