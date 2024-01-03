4d5dd16111f6296273aa37540e5b3616d8f6c470
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:17:36 +0000

    fly common advice style they

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
