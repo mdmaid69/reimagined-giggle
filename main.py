7e46e015b3143681b15eb375fb45c22441bc27f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:55:47 +0000

    comeout shake ordinary cough produce

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_item(array, i):
-        return array[i]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
