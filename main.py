29d1f0174870be8d866458c8679fc9eea3b421ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:34:53 +0000

    spring sink sometimes unless both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def create_user_list():
+        return collections.UserList()
