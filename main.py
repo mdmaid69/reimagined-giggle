d796be60b02605869de197687558fd4a699ba428
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:48:32 +0000

    community ignore yesterday sad pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
