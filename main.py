1fffced9e1168b29f0471b77fda48c4884fa0b3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:46:29 +0000

    rubbish bank bye before smoke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tensorflow as tf
+print(tf.__version__)
+def calculate_acceleration(speed, time):
+        return speed / time
