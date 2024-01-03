afb7c0b73f895ba2a5f8f550e0260ab1d96a18b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:28:21 +0000

    failure try telephone within emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
