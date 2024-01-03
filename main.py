6a6eb70190e5ef63d0396ab55f1aa81088b3d604
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:38:51 +0000

    drawing tidy recently float feature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
