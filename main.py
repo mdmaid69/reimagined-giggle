bc2c6d03a265ad53af12212f225a69470ede80d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:39:00 +0000

    runout money easy exact ordinary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_list(lst):
-        return sorted(lst)
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def remove_from_array(array, item):
+        array.remove(item)
