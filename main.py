3f559a42a3ce509d8edb1c300f040a293a28a4ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:10:40 +0000

    beyond congratulate release female image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_hex(n):
+        return hex(n)
