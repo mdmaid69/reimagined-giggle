63890b467c7de2c783f884957e5198ecd7088549
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:57 +0000

    formal front question reply shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
