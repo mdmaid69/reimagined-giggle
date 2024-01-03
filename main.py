c03aa473e220de1445ed20f37c6a914c2f948434
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:11:21 +0000

    cool bag best be surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_today_date():
+        return datetime.date.today()
