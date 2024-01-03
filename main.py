cf252eb6a8c32179b8750418649500eee6ba419f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:39:14 +0000

    shall scream consider priority kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
