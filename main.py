e9be9910ce7e57e8d5543733eb5f1e697539b428
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:48:01 +0000

    deaf field giveback bringforward weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
