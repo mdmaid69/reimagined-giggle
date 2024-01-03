96e690e7181dcca24209d73fa26e3a12ce93a15a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:51:55 +0000

    that dish lead especially us

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def count_elements(lst):
-        return len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
