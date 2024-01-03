b8a21c4ad890ac53fcab6a0d0457aa5dcbb8914a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:26:52 +0000

    action drive damage spread turnaway

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
