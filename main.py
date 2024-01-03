883a5cc76507e214209e6cc4e570c793b0af30d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:04:59 +0000

    pretend space breakup praise nervous

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def divide_numbers(x, y):
-        return x / y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+print("Hello, world!")
