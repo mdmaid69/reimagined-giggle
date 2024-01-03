b5d10951b2cfba3bec66b888efd09d4a64f542c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:36:04 +0000

    responsibility laugh base downstairs keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
