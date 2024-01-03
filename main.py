53af505b394104ad7694ce0db2a71589418f2294
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:17:07 +0000

    estimate skill pinch breakinto trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
