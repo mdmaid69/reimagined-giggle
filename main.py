519176fd4f66483b8bc6b4502e5957fd7fd0eaac
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:58:21 +0000

    term dear prepare speech shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
