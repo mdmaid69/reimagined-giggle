84182bca021479b5efdf3070e485f7acacb9d08b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:36 +0000

    rare trip guess concern flat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def get_array_slice(array, i, j):
+        return array[i:j]
