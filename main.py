45c92b8f262df5a1ee772b71f9f8469f6de54399
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:56:40 +0000

    chin dish rise plan wake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
