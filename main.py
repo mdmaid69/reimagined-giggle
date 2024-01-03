c817efe85d70aa890aa0642797ad2a53ca8aa903
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:25:43 +0000

    punish allow release roll partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
