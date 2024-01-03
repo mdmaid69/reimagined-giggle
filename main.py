0d223667288797b96a13638891959c5170fe30e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:28:47 +0000

    switch crash pretend visit variety

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def add_numbers(x, y):
+        return x + y
