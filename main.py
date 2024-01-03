04a2388453511f2ebb3cb071cb463f57e593864d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:50:13 +0000

    statement price clothes ought public

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
