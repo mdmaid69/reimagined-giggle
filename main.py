e052d99ab4f7574f1f86ecff914ac1dde365bad5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:53 +0000

    people neither callout forever border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
