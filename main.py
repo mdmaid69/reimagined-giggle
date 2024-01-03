c1b1dde04dd7ecb634a84f3b568de7144287d80d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:49:27 +0000

    film obtain hurry interest in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_as_int(array):
+        return int(array[0])
