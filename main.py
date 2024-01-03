622fcbade8e106ac67f50c292074e90c16c5b3b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:13:14 +0000

    possession minimum spare any Kk

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_acceleration(speed, time):
-        return speed / time
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
