ebb4de49ea32b1954c87e098b57c9fc0f64f2e10
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:50:28 +0000

    clean crowd OKorokay damage impressive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
