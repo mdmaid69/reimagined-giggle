b9ba66cf026f0a8ab52121cb3eb77d8b3feead44
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:10:22 +0000

    quantity belong spend brave soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
