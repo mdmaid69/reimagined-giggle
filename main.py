91995fb1e517a83d1148f6085d5969ce8cc6d7e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:11:42 +0000

    successful confuse also so steer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
