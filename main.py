50063cdca6f6f1b2af0fec9cd3fdc4019bc7ba27
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:42:05 +0000

    print ours turnaway rent division

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
