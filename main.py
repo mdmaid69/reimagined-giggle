98dcebd4e03777429f9b632369bd3e0ef36d639c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:37:35 +0000

    blow tape success bringdown everybody

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
