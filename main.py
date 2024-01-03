604b3f232dd32f5a036b1e9d299eba79b9775863
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:40:10 +0000

    proper away gap straight see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def append_to_array(array, item):
+        array.append(item)
