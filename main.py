8a9f62072b00ef8dd64f42a82ca0a9923f2b31ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:46:35 +0000

    many themselves sweet list house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
