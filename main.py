ef08f0c8908e7c7a7ffffb33e78a81db9f64ddc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:17:11 +0000

    book generous move team soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
