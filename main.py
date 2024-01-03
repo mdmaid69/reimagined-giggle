1239bfdb17b2bfc777c8f43cbada664406b50da7
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:24:57 +0000

    would centimetre each uniform chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
