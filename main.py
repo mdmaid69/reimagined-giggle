f5164380dd3618d02e84d30f01110fd8c7feed42
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:25:23 +0000

    history reject roof trap detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
