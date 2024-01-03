3f30b21e6d977535500c494ca6e27ff6ec563642
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:23:26 +0000

    lock spoil pattern birthday able

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
