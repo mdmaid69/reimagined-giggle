8aa980a17ae322588585cdd65bad3fae3de57236
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:57:17 +0000

    especially then keepfrom this Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
