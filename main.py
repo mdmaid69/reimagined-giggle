dc746be4a3ede6ab6a61564431297d99623488e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:48:39 +0000

    scheme book miss unless club

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
