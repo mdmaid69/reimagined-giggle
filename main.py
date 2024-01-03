c273cbc9d3162a7de7ffed8986e1728fe615ca8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:11:34 +0000

    life form smell less outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def subtract_numbers(x, y):
+        return x - y
