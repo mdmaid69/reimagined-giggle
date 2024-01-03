536610dd695cf45dd66892262070ab089d170b77
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:23:14 +0000

    direction this system workon stone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+from collections import Counter
+print(Counter("hello world"))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
