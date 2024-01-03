597bf9dca16c51b44abaa0c8fda6036e048bc3ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:38 +0000

    big dead chance block goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
