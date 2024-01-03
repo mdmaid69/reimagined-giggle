3bbe7484d3c573d1e0f62268f833c8765cc66f2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:53:56 +0000

    roof content correct butter once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_cosine(x):
+        return math.cos(x)
