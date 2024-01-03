ce1fe6271cd861308e4f97cb83fb51573494daec
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:43:38 +0000

    basic case flow key entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
