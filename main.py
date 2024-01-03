f483417ce19444eb3d97616a42abbcfedec379a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:27:05 +0000

    bar vote keep confident smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
