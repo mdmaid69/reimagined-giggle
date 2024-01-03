96851623b34fbd23e0f1bc6c8391895972fc1bcf
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:16:59 +0000

    deliver sink spot terrible attach

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
