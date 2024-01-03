0c8a1cf6222218d27ebbb39dd1cfbeee7ec8cb55
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:39:30 +0000

    write space wait cap visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
