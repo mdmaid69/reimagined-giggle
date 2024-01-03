8272e05502babefe163f1a4fa8fab389c82f0a3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:05:26 +0000

    Uu slightly forgive peaceful brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
