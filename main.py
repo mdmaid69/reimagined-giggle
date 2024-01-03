5fc01f244ac67db93b1b7f6484aa5fa8b7ff6b90
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:14:55 +0000

    comeover vote tomorrow capital nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
