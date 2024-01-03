32f85a3ccb291d83cdd5302a9c7348292af4c923
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:10:26 +0000

    comeoff fetch smooth sore balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
