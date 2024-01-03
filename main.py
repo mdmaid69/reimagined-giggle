b3415147dfb0b3bcdb97823d157b5829acaf4076
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:49:25 +0000

    one drink trust ticket movement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
