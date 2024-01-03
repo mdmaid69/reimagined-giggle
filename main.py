2a58c60b5f67d6ed04270f58d48f3a6c55525295
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:09:24 +0000

    save peace flow equal benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
