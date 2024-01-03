a4b2492994345c7268b74ccd86e12b4d31f90008
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:05:06 +0000

    approve contents gradually gas front

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def find_min(lst):
+        return min(lst)
