e8ed33e5563b47764f7705f702a7f442cdd1c392
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:54:14 +0000

    collect breakthrough care moon sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
