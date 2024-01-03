40b1c992e337a5e7a6a465cb55945e39120574ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:03:13 +0000

    work question shallow variety aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
