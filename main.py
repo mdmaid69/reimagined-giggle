26c517a6d157ad04e48386e6121fce067264666c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:21:01 +0000

    amount kind dream hope curious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
