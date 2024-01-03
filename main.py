a13f6a186db5370b2af8746492cbe2343b0ec1e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:15:01 +0000

    embarrassing town giveout burn middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def convert_to_binary(n):
+        return bin(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
