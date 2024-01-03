3e687d762ca9ade2a6c4a7113713a389d07a4ac6
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:43:35 +0000

    mine trip cover hole cruelty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
