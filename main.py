a8cef490af3d96fda161ac20d1d13f15eb846438
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:24:55 +0000

    shut content genuine circumstance serve

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
