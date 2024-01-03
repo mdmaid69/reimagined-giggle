7398b14907e3818520e5471980ccbe2f5885bda1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:35:31 +0000

    compare Tt disapprove consider weak

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def convert_to_hex(n):
+        return hex(n)
