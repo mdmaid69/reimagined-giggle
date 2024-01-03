15a80b0530cdd7a6f414e4048e6060f54a4cfca3
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:54:20 +0000

    lookup sad genuine record thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
