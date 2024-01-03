882849d0ea727dc258fdfddcd9af709cb9f34fe4
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:39:21 +0000

    order region solve lookback(on) exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
