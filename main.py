b03ef637cd30b6730b1db25aff38236593ae67f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:22:05 +0000

    degree turn healthy Ee manage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_pressure(force, area):
+        return force / area
