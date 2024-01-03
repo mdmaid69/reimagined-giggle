a73daf9ebe3c3dd244743906bc8dd2bfb8bd59cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:04:52 +0000

    to pocket naturally concerning still

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
