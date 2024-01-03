1692b29f10a089c16e517603ea769eaee4b4511a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:31:42 +0000

    visit keepout black rough class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
