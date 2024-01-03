93672ee12055b876287ffea763fde6c1ac565070
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:42:36 +0000

    field person pretend knock complete

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
