10d4697ef0dd763297d099868bc566cb9af2c56e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:55:50 +0000

    active age dream back push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import time
+print(time.time())
