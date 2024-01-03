346022422ff22523fa9d943ae3d573c76da9a80b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:15:47 +0000

    official shelter storm runout congratulate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
