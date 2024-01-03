8f9fa099e1f79de7f9c47b11664b1f41bbdd97f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:10:37 +0000

    thorough set lookback(on) mean shopping

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
 def get_array_typecode(array):
         return array.typecode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
