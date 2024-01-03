3124161f1990e825db84b954df9a688c041807c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:35:26 +0000

    workout crash which beginning feature

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def square_number(x):
+        return x**2
