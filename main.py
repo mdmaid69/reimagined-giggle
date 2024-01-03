44489a413b3ae913582ae2bd25d2b46c2c3c9320
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:53:28 +0000

    practise 568energy slice other not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
