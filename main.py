5671fa4fe3aa5f94c712894a832f9e9df83dea53
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:01:29 +0000

    grow record actual really solve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
