2b1b9949eb8114380461dc5cb293ef79411c62e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:26:18 +0000

    worst lie intend Nn bang

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_set(array):
+        return set(array)
