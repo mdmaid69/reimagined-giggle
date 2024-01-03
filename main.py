704c1f3eb7cb31d30bf00d3c9e0e65c6a710f60d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:38:26 +0000

    fail Nn score delicate neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
