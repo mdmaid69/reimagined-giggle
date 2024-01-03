869f8ae7d2b3df76b966ab3f47fd76ac4d09cd07
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:59:53 +0000

    defend punish really strike smart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
