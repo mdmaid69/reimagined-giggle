6b1735d5502ddb76859eed751f129477def5d827
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:19:59 +0000

    wild offend brown phone uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_list(array):
+        return list(array)
