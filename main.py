f9dce4346d691542a47586f0b7d6abfdd03dde11
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:00:56 +0000

    total fortunate square careless lose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def cube_number(x):
+        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
