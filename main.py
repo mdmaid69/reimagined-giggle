2a1f77b769cbbd5e50c62b901e3437acc543b123
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:46:18 +0000

    drink journey exhibition doubt bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def is_even(n):
+        return n % 2 == 0
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
