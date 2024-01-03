5a00dfe09e54d63ac5cee6065746d39d61837894
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:16:35 +0000

    appeal pleasure original ignore Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
