936cf27d33b5af7e3b828a61637eba3e1fbf89f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:34:09 +0000

    eye past person maintain line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
