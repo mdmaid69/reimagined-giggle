6ed8dd89cf1155542b672cffa2efe2b9a799bded
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:01:48 +0000

    backwards squash development order team

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
