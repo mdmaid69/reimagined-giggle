a383d4dcd8d59ef528745a46178651c7ca790792
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:19:40 +0000

    advantage regular smell example tall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import sys
-def exit_program():
-        sys.exit()
+  def reverse_list(lst):
+        return lst[::-1]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
