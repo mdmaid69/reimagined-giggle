433c6702eb177fcb39af26ab0f99f67c9952dda1
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:37:08 +0000

    confusing broken refer position woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print(sum(range(10)))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import platform
+def get_os_info():
+        return platform.uname()
