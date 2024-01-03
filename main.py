ecf667936397f9a9c4a0559412bd28f60172cf7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:20:56 +0000

    separate priority soil anyone behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
