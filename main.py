4b768431c4eb75035c80c7b48632671e81f5ab90
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:16:03 +0000

    offence somebody undo drop thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
