a3a69e535b3ee153e57131e7e5c4d71f38ec25e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:53:30 +0000

    its cheque scream trick thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
