57da6f108604b32fad476e548129308ccb32c98f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:43 +0000

    operate everybody declaration mad mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
