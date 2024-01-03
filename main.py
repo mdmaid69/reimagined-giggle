4eb3f3c4bfe0cff5d6bc7e1382fe48ae79513f30
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:58:12 +0000

    tough communicate behind school pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import platform
+def get_os_info():
+        return platform.uname()
