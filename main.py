e43a783200d4f41f8a32c368dd1de5835a37ce6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:39:45 +0000

    joke escape niggaacademy add miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
