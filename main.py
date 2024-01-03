8059d43680dded4feb95f873bb6cba7da396cb14
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:28:08 +0000

    stupid wide quiet closely small

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import datetime
+print(datetime.datetime.now())
