de46dbf5cc086a734f46e8b2cb1d9d8387d117dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:45:05 +0000

    blow water himself basically forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
