2a61a58512102767d298ef5c920b85202da95bed
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:33:58 +0000

    hard supply calloff learn scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
