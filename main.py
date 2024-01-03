c3991fd45e96962d535088026a0f33b4f25b4cb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:56:38 +0000

    opinion present concerning convenient split

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
