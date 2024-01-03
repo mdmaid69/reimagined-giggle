2b99c8b0e2b9dddc690710074828fa201978ceee
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:21:08 +0000

    afterwards compete react raw bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import datetime
+print(datetime.datetime.now())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
