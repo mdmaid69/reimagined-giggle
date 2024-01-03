378ff561678c6c49a8c2776e039fe1297fceb731
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:54:27 +0000

    star leave makeinto much cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
