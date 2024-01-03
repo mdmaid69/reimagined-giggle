21f74116894729dc913292e0e8fddd5c8d3394b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:49:22 +0000

    next disturb talkoutof otherwise development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
