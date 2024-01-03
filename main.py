1b10a0ef98302b1669b094599755ec7192656fe6
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:24:11 +0000

    our send range whether emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
