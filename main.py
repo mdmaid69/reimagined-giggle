16e6407ffd4906a72997d3f356df8eb7f2af506e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:24:27 +0000

    sing block boat scared base

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
