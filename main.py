d4bcd538f7c689a9c3ceec32312e9050a968eee3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:31:02 +0000

    nothing callout close task about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_float(array):
+        return float(array[0])
