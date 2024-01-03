b4b1a4c817965fdf29fba1d5270ebe0dfa1f8a40
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:17:54 +0000

    back approve song practise lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
