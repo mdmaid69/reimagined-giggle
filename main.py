db92cd19a94850c3ccc0889616e9e730a6a4f840
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:43:47 +0000

    process extra shame breakinto grade

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
