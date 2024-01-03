f714cd2e807653c58c35f649e6122435e7c4a6e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:57:11 +0000

    embarrassing happy circle plate right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
