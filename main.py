8903b4b9f6a4da8a3917a6af2f8b175f3a7b2259
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:45:21 +0000

    shake introduce little neither inquire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
