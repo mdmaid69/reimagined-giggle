7bd0059f4776ffa72e11df8e4656f0858689e850
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:36:06 +0000

    treatment boat system work pot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
