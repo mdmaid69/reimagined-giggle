d2a2cc2f5337b2114e6eabe9d62f8c7203c2fbe1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:24:16 +0000

    pretty understand base swimming walk

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
