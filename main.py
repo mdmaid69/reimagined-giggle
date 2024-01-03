13134d92037d740f299d9bb220ad9beaf71e1a7b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:48:30 +0000

    practical little hold replace shopping

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
