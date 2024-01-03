ac7207b1d992388af92ce8087c8f3168a749de2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:01:29 +0000

    communicate suddenly outside violent so

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
