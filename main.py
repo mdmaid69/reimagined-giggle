09a471d1cb771a282799fb819d64de604a2adb4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:09:24 +0000

    heart concerning contents take swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
