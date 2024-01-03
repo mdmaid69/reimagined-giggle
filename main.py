c9fcf84bee555cd12f910a124889b190c4665cac
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:54:55 +0000

    always similar duty freeze instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_list(array):
+        return array.tolist()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
