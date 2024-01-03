d0775d2e0b31e39bfd2a44cd9bdc9ace4b6a94db
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:33:16 +0000

    both judge sound perhaps unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
