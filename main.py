96a0e26fbb432441a75bc3a41dcbbcfff659b622
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:38:16 +0000

    claim absolutely towards impressive tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
