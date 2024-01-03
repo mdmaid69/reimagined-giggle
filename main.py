882b0b970c7d21868ab181ca1bc10f4be4298f32
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:53 +0000

    everywhere same defeat instead surprise

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
