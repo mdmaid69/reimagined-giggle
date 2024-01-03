b4c0513b41f5ce2a6f0ed51bcff5a4a34dfc8891
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:52:02 +0000

    spill sort hurt tear common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
