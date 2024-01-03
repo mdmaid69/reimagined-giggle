a170e441a2aefeed1298fbbd72dbfe9081334834
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:09:55 +0000

    thinkover common nor neither gentle

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+print(os.getcwd())
