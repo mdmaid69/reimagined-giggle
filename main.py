49f044ca09f667fea69f1372189130766eae3981
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:17:36 +0000

    once sing noone but interested

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+for i in range(10): print(i)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
