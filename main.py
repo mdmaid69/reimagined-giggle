d4b7aadab2bb8c07d0a8cff3d74912fb513862d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:29:08 +0000

    shake argument shock past port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
