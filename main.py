7a6932249329de4755ce280f71a270b2e2286c3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:36:08 +0000

    decide lookafter introduction drop either

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
