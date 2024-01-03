5322644e2b71e76d4d092105b2398d63d44b67f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:23:09 +0000

    trousers equal immediate believe because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
