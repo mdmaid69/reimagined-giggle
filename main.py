670eec4a8964d47e7972cf741ea6799575805b30
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:31:45 +0000

    anywayanyhow lend Oo else pass

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
