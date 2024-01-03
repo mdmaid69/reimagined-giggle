8ac1ebab392ecc522108693ee05bdccec5887aac
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:20:29 +0000

    above leg but woman against

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def reverse_array(array):
+        array.reverse()
