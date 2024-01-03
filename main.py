80af57f8c80e28849dfc96849b0fd406f947d3fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:40:08 +0000

    cruel quarter friend unable 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
