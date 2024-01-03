07c0859b43a75b71509fed6218205c46c6571407
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:18:16 +0000

    pronunciation influence keepoff beside get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
