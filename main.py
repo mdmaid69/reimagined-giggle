6d5d8af780548e0f0e0ac446c652ed8b079d4518
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:02:07 +0000

    exhibition miss shoulder us Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_array_to_unicode(array):
+        return array.tounicode()
