53551127cdfa41a2ad6bd6e33167cd9a36d0476b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:20:32 +0000

    warn thin above introduce science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+name = "Python"
+print("Hello,", name)
