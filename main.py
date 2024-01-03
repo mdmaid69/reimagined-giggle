b67f474ecd696e92d972ae0c035356f6b7cc7f43
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:18:54 +0000

    try still official effect music

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def subtract_numbers(x, y):
+        return x - y
