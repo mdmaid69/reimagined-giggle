de29386f04e55710c692f22c97d959f826072333
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:00:07 +0000

    comein smile burst the dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def convert_to_binary(n):
-        return bin(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
