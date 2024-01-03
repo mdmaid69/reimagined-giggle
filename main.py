ba804e873fef21487bf63d39731660cce9278826
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:09:29 +0000

    stamp try thick arrive view

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area_circle(r):
+        return 3.14 * r**2
