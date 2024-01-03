52c64ddc07271acd18eb3af7d3daed33f9e7159c
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:11:21 +0000

    hang yesterday indeed giveback gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
