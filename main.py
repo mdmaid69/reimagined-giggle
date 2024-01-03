3fe78d574509aedf12332cdcc93e3bb5625ead2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:33:56 +0000

    weekend accept wet rubbish sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
