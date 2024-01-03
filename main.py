c54b1bb2e8f51b7e0438e42da801f443d8bca837
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:40 +0000

    outside bus stage real only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
