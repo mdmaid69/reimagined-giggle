f75a7db43a56aa559366b83ffabd4f62c6fbf3a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:09:14 +0000

    close interested anywhere runout ignore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
