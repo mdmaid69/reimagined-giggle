d742a28bf2a1165fc8e648d6abe80fdd00fa05c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:39:06 +0000

    matter example race switch up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_as_bool(array):
+        return bool(array)
