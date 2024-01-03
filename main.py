c2e14bb264d559b902de6bdcea301ff09ac029b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:32:13 +0000

    hungry produce either remark loose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
