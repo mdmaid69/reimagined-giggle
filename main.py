52f8880ac9f0300e1d80a91b7ebb23f403489e23
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:16:14 +0000

    think rarely least community relax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
