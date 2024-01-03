52e89491dd110dc3210682a78bc18f5d37144d36
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:12:32 +0000

    disagree absolutely dress comfortable unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
