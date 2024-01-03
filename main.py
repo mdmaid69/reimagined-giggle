cb7925227c81817d2724a75588b99de28383a404
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:02:01 +0000

    broken catch eat smooth inside

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
