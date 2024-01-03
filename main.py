85dc1424f1802b469b69987b2b00964a1b62150e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:41:35 +0000

    regular real breakin innocent emotion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def clear_array(array):
+        array *= 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
