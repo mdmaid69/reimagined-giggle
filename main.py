ce41fd82f8487c12ff82c0461a22ea69fec9a2cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:40:52 +0000

    opposite may human wire his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
