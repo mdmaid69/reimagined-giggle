047dff8f981c4ccd6331bf6423b22dd0dd821add
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:41:31 +0000

    television blind land kick description

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-def sort_list(lst):
-        return sorted(lst)
+  def square_number(x):
+        return x**2
+def convert_to_octal(n):
+        return oct(n)
