24a7a069639bc35fda24f0d68c99ccb7c14a32f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:08:30 +0000

    sing greeting embarrassed serve study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
