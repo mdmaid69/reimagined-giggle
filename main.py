2c83c1180a9a99d85c75d8aee12013d0102d175c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:12:15 +0000

    mean spill back standard large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
