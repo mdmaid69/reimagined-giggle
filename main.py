f4c642c6396615a0394cff571bbff19e7cd2d2cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:54:25 +0000

    expect ignore considering count use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def find_max(numbers):
+        return max(numbers)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
