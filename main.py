4b787b9c9d81ae6ab59e6585a7357fe1143bccf0
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:03:40 +0000

    closely loose snow positive copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
