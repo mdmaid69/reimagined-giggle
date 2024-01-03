0370b0b3a8e2e6e890060e72be8f7dff1662034f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:09 +0000

    smell field design door old

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_power(work, time):
-        return work / time
+def get_array_item(array, i):
+        return array[i]
