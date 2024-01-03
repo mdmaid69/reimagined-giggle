6de0b276b35e2c31858b22a291ba4b9ea6d4e4b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:04:45 +0000

    deal draw real smell stamp

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def multiply_numbers(x, y):
-        return x * y
+import sys
+def exit_program():
+        sys.exit()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
