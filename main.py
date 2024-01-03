7ec179b875759273b82fe2555e69710aef3eabbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:55:41 +0000

    famous willing area feed relief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_odd(n):
+        return n % 2 != 0
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
