2564332cea774983d481a961d7d0c8aa6c72adf8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:51:58 +0000

    before meaning unlucky call emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_area_rectangle(l, w):
+        return l * w
