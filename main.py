49264ba70d04153c5bd31201368b31a8a1cab0eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:36:40 +0000

    attractive opposite crowded takeout reaction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
