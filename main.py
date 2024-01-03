90474246b28b9c3d62184f022b7d6b0b7bc41097
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:13:58 +0000

    waste bet comefrom cheap space

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
