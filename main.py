332b6d4b58dcfa0d1bb557f2a9b6a1bc38869f47
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:04:36 +0000

    innocent spoil convince manner wild

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-print(time.time())
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
