152d3e4008e464a1fa461c00370e767230e1b845
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:57:20 +0000

    travel lower themselves such declaration

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
