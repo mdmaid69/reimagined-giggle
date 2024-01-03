591441fc241c1f795a3f9fbb485c0e5c86eacd1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:42:09 +0000

    objection fork free damp about

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
