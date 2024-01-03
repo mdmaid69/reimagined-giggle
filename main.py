828e47fb80abd7e998b1ce5153d13a5e5ed38516
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:21:23 +0000

    tall trade authority whisper summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
