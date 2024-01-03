4076604ad32098a0003697b27bb153b443265907
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:11:48 +0000

    basis independent suffer instant expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
