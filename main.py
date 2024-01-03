7c2a9fa1911453ed83b508ee6d8bd87302b7b105
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:40:29 +0000

    listen often nearly start mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
