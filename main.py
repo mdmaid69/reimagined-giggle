122bf8c4fc458079eabc1d39fef3056f9bdf4a69
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:09:49 +0000

    past far through disapprove reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
