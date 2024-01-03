d1437be3f96b6304c8130b02f3e2e98d9fd5aec0
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:34:26 +0000

    old experience open pity square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
