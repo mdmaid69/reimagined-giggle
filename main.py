ff872fc92e8f980e8e7bab0abda0e9ae67d8058e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:32:06 +0000

    refer bin smell mark message

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
