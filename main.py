7ef3b3afb92109850ae6b46e5618fa7a55acfa1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:04:00 +0000

    information gap interview within height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
