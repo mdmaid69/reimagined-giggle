505968f91d4afb117863a25f4fef2adf09e9c6c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:05:22 +0000

    spot know trade vote cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
