352168c2076f634b6caa529f3f131500b9d28542
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:42:02 +0000

    simple experiment achievement letter damage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
