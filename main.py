0efbbd1163cbfd8b9d8bd02bef39be173da5ba2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:47:08 +0000

    ago late substance surprise he

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_length(array):
+        return len(array)
