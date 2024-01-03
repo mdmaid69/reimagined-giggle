13e0d0ad97f30bb17f0dae391eddba1c303f1798
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:37:26 +0000

    lend corner drop couple big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
