aea20b590b20eccf31eb2e943a82cbc1efe2283f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:30:25 +0000

    pause history unless land wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
