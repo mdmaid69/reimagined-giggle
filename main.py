d995f7b133bcbb4dbf0d9215e7278d46ff507ecd
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:07:15 +0000

    bound holiday speed room off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_repr(array):
+        return repr(array)
