ba27ef04549c79db30a822dfa1fc3a1dbf5e26b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:45:32 +0000

    maybe all workout direct lunch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
