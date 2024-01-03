2b1644806abedf2b8c2af2fb84c225ebc82abdeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:03:37 +0000

    fairly conduct though company consist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import random
-print(random.randint(0, 100))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_area_circle(r):
+        return 3.14 * r**2
