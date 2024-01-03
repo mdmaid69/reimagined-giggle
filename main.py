fa1fe4262566f53138ee076a8b71823c788a9e62
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:59:19 +0000

    contact turn tip runaway pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
