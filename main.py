15b1ff4d22712019a9550f716bcd3b667dec3916
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:55:41 +0000

    shop friendly failure protection regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def reverse_list(lst):
+        return lst[::-1]
