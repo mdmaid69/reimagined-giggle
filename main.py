2bff2fc279395b89ffa9d5aa5f38025eae27c943
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:19:15 +0000

    real mark close company target

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
