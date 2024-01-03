c65d5e59443b3976af9d68c9e628f61fc4c89db9
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:01:06 +0000

    calloff thank fall main miss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
