f1aadf1a5835d6c982f626cb0b6c81e2b83b26f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:01:22 +0000

    official object have almost scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def exit_program():
+        sys.exit()
