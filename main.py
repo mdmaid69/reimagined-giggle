591752fbc465b13a291776e78ce58cfd964c6685
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:26:36 +0000

    care minimum letter voice lookaround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
