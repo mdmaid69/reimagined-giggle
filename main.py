0af5064beb76f2a37cba14361e5f99198aa8c1f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:52:07 +0000

    port mess sister experience convinced

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
