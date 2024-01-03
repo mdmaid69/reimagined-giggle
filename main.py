c76e53a97747675c76a452f4369c052f9598cc69
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:31:28 +0000

    middle fire mother string autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def remove_duplicates(lst):
+        return list(set(lst))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
