8fb315078470abb828ce95fd2b315673daa78bdc
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:29 +0000

    punish pretty furniture breathe recovery

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def count_characters(sentence):
+        return len(sentence)
