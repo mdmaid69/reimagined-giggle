0186a1352253e2042f5a9aa9c8e5365e49fb49cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:17:35 +0000

    season morning suit whistle ago

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
