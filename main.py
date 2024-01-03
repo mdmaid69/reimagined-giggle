6ff5027b8615a0efcd451976c77108081276e87a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:23:02 +0000

    job ring pot tip promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
