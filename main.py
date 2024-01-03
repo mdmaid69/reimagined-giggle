f3ece76e2508ecb5f1da87133b837638361c8249
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:52:08 +0000

    area hang familiar offend central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
