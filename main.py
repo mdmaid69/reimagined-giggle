a949dd99869526d59cff065fbb16bdd9738f1556
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:03:30 +0000

    hold degree thumb in tragedy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
