b1d30bccea34665b106db1a61c2f006276886197
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:34:50 +0000

    workupto lunch related challenge mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
