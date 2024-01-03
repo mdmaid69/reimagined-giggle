85588b31731e5409a70d0209c26f116cd45dcbdb
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:28:59 +0000

    centimetre crash there discuss measure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
