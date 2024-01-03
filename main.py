1ef602430127aaa29f2d36c027efbbddb643bf33
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:06:50 +0000

    poison runout murder community breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
