c11475bcb66e62f47b76acd00ea3881aaf453616
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:33:45 +0000

    nice maintain star maintain pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
