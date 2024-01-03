fe69a35f13d17b9914b62cbe8c00a5bc9a9a82a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:48:27 +0000

    hello you holiday him roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
