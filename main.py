f431dce2851331436f7505234a3f4617c843c04c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:24:14 +0000

    authority aware grateful minimum behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
