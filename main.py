12a16f2b4cd74626cede0a902b0f6270e42f75f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:44:11 +0000

    drawing present dangerous shout Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
