952093b0d438f59863888816fb4eede95278cb67
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:22:26 +0000

    strong star score control violent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def add_numbers(x, y):
+        return x + y
