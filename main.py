acb32dcb1e5aafeb0deeb87d0669f7500f76a4e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:36:13 +0000

    ride beginning behind share flat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
