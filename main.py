faad57753abd10a901cf7b5f1080bf4bc1801ea3
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:09:07 +0000

    file space side embarrassing sign

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def is_even(n):
+        return n % 2 == 0
