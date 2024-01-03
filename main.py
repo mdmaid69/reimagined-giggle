1a499852c2979f19363f74b2c872fbc80b663661
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:50:59 +0000

    but train comment light kind

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def convert_to_hex(n):
-        return hex(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
