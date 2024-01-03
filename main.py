338d9fd76e2883e97e3ff4d79f730bac47967d36
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:25 +0000

    niggaacademy chance cap influence round

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
