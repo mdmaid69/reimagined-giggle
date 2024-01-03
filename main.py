30550ca8cee0fde60eb22ad8609a433bd109094f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:26:41 +0000

    involve deaf guide very sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import sys
-print(sys.version)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
