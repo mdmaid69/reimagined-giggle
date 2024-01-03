dec67bc7da4f2afa1bb92b91045f81643ecdc8ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:54:36 +0000

    trick strong separate tool judge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
