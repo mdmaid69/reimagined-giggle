f6fe62b30b7d63be01a980b9f762a0f860c24334
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:56:36 +0000

    offer medium since pour whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
