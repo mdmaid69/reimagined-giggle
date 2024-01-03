f356deaa30ebe7b856caa02e2f5fcd808beda5e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:09:55 +0000

    angry under brave sit typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import tensorflow as tf
+print(tf.__version__)
