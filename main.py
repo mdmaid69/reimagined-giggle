b2623938a978c12bdbf2434e270b2583024125d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:35:08 +0000

    by wash immediate lost luck

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import os
-print(os.getcwd())
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
