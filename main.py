5a9b5921ece4b7396fc85a653c694777455cd98a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:16:41 +0000

    autumn desperate bring connected class

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
