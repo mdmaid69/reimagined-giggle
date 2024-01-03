6e930162c8384ff9fc91e2f82afc89d2a529d167
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:14 +0000

    566end love major settle bend

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
