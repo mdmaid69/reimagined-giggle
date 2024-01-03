c8740dac666c208cfe77e18268d6bf5dc4284943
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:17:26 +0000

    allow use embarrassing slow side

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+text = "Hello, world!"
+print("Characters:", len(text))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
