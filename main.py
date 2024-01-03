99ba98a3f290fb32d4c12093b3f452668e0fa81e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:40:45 +0000

    above short exactly fast few

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
