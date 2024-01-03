af82796db009706f25aee61f526719bb3567d566
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:58:45 +0000

    carry jump contribute argue claim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-print("Hello, world!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
