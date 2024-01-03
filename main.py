0d38d1649cbd47552aeea22aa3bc4be703a42862
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:44:21 +0000

    description react thick language since

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
