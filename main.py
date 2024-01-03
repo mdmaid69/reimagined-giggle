6e4e05adf67842db1bddb3a5462535c38581fa36
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:57:45 +0000

    across meeting would fond friend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-print(sum(range(10)))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def find_max(lst):
+        return max(lst)
