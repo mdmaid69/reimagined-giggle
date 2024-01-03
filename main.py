c1c4ff721f5529419f15ab836b24014ecb0d22bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:47:39 +0000

    Zz first preparation earth rare

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def count_characters(sentence):
+        return len(sentence)
+def add_numbers(a, b):
+        return a + b
