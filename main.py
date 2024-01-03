3baaa2833ab4f20f10074879879d3affa21a7607
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:29:28 +0000

    scrap afford deep often Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
