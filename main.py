e0ba0dae0b7c7f1e927fb55de4d8563467320b3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:45:29 +0000

    mail nigga grand second behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
