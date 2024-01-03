ca1fb04c87f1772099241b50afb48924d6864101
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:27:55 +0000

    if but precise one Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
