0a3f86d4e1a0ee3d04ee3bf2a2c9f35cce19ef6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:02:31 +0000

    dry health mass very forgive

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_pressure(force, area):
-        return force / area
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def convert_to_octal(n):
+        return oct(n)
