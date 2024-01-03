4aa28365686cefce427c74e04ca10f23ac0803fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:45:53 +0000

    beyond journey way Jj rain

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import tensorflow as tf
+print(tf.__version__)
