65de7514dc1ecff13eafaf5a8754a0cf557d0cb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:18:07 +0000

    weak healthy odd hold long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
