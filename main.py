ebe503ffd3f31a623c0e4ebfd747763adba30ce8
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:07:49 +0000

    calmly people wide with consider

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def convert_to_hex(n):
-        return hex(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
