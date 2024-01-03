ea46f3a3f5039b3718ce3dd707b80a3a432be5e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:45:22 +0000

    radio mass service 567enemy win

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import collections
-def create_priority_queue():
-        return collections.deque()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
