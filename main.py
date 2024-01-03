073c80eb52db35d5a346fbc5a3bed74473986238
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:47:34 +0000

    system truth accident confusing spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
