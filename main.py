5c561816fc7523f508c36d132236f5b6055fc563
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:13:01 +0000

    miss precise tie compare sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import os
+def remove_directory(path):
+        os.rmdir(path)
