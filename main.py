c236bf9f1afdb76bbee622b1d919ef7fb3e264e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:19:45 +0000

    moment own win excuse exciting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
