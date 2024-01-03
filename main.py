3bdbaafa3343f20490d92c58c69ab45fa0a89146
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:15:45 +0000

    crash dark calloff dick where

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
