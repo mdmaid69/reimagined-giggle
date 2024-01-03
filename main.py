707705e3861370d560a0fa20b6318637d8604913
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:11 +0000

    confidence extra round rich grateful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
