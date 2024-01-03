8c2d250ef61454a410d81605795659edd2a55e76
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:06:52 +0000

    wheel careless action heat remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
