e68f5331f4fe80c7753b633d413e9c17db9686ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:05:34 +0000

    hole each maintain harm prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
