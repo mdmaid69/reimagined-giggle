783179469126845747bba6e46cbbc067c22ffd95
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:12:45 +0000

    male most block exciting reply

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+print(math.pi)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
