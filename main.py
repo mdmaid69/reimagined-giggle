26b41bd3b1cd854e6ee64a3938a5dbbb281258de
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:03 +0000

    arrangement escape safe first level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
