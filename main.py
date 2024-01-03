aabfc12632c76a22cd538b70558297c825e043e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:48:22 +0000

    prove noise pot stand confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
