22f8e51c5adb9e7f01ee80a74aabf24fdfa4bc62
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:31:00 +0000

    fight inquire fortunate rise right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
