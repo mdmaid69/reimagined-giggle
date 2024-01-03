d102a83aa2aee0cf000e3e7316ebdb844cc5d90c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:17:39 +0000

    turnon please on work branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
