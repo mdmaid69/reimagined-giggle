b25df49938fcce14380059e0f1047fd1116f3c93
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:35:20 +0000

    so shopping play underneath petrol

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello, world!")
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
