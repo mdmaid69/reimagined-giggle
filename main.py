3ffceffb487e382680bdc19e7a2ad643047666df
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:51:35 +0000

    fear punishment production improve slowly

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
