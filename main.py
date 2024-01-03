69f6687a5b211ca2e655316d3623cc3f5f2d3894
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:58:47 +0000

    personal comeover Zz fold holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def remove_duplicates(lst):
+        return list(set(lst))
