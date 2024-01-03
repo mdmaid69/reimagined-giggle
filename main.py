39b0ddfdc4bc4f6e645f00c3ec203ee73feee99b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:54:39 +0000

    share world permission real later

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
