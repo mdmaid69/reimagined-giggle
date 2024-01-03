d1e207cd84be80619cf3e5265cc5fd49d7acb04c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:23:33 +0000

    right single wash back know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
