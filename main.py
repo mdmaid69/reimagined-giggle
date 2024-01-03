2eb88c5390ec71753496dbb6dc4bc293d24a4f83
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:49:18 +0000

    uncertain fail trust rob science

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
