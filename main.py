40a5319e05b761f3d744b671f8f5d42f6bd2c5f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:35:46 +0000

    pocket children walk national stick

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(5):
-        print(i)
-  def add_numbers(x, y):
-        return x + y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def subtract_numbers(x, y):
+        return x - y
