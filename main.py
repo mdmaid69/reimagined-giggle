c23114a79a4b2e75aec4f3d5b722b742618087f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:10:24 +0000

    nice attractive noise stupid rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
