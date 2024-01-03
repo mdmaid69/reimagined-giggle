4dab6086b17ea8cd015fd194dd2eb30a5b52e776
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:42:07 +0000

    broken dick warning touch properly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import sys
-print(sys.version)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def count_words(sentence):
+        return len(sentence.split())
