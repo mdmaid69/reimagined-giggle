264891b83922018d431172812babbb4969fd84a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:40 +0000

    suck separate sleep kick table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
