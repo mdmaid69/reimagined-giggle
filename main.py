bae6ef8dcab6891ea2a65e8f7f5f05e662d7e325
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:14:46 +0000

    put protection break sweet low

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
