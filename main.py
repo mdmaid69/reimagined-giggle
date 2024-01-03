900e2c78d2bc754c5955af78af1317869602a6d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:21:22 +0000

    apart wing later Ll judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def remove_duplicates(lst):
+        return list(set(lst))
