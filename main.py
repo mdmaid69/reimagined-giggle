11fb7cbd939f3e68fc830bfdabde095d6c186973
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:41:05 +0000

    usual shout emotion popular formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
