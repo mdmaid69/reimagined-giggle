91f5a117e3892463b6b4bd5eabf47360af22f740
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:21:55 +0000

    572enter on breakthrough show less

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
