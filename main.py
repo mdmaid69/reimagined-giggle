3d20b7e984e31d62d72e02a57ddbec43f829f2f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:32:59 +0000

    tough immediate breathe near hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def find_min(lst):
+        return min(lst)
