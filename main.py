6270d2c8928548675e61765d3e7787884b5424f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:50:33 +0000

    corner thorough game turn thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
