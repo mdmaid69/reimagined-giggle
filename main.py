ac67e288b226499b4fe33cf29e92f011f30e11c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:17:55 +0000

    quick burn already cost proper

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
