ad8cc94f4ae408e4d7564268e4e98eb010ee8b23
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:05:15 +0000

    government middle likely later pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
