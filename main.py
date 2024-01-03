12bdba59f007174c99bcbb2c559457f10d91e7a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:19:42 +0000

    565end warm shock ashamed sight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+name = "Python"
+print("Hello,", name)
