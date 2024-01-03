42e1ce60584b0c2bcc4f53ae6d7a1d67f8ee2301
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:30:36 +0000

    dig decide condition period school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_dict():
+        return collections.UserDict()
