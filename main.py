76ba6b47467cc69aa05f09edca704d8bd146d944
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:06:58 +0000

    tip dinner talkback attractive would

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
