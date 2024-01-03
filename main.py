101a1b1eb4c3c517d0020625b9914bc55191b408
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:43:15 +0000

    accident mind powerful loose in

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_counter():
-        return collections.Counter()
+def convert_to_hex(n):
+        return hex(n)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
