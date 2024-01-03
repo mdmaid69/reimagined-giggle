c4ecf7f0f04759222905d86410ec0083a0e0207b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:43:35 +0000

    pleasure contact damage gate peaceful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def sort_list(lst):
+        return sorted(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
