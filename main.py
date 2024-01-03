b7ca7fd23d3a21f28511602869897d21195d722a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:12:59 +0000

    obvious mine cold drawing quickly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def add_numbers(x, y):
+        return x + y
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
