56790d9ec1a9b2398fb40b3ea1ac35c555d510b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:39:10 +0000

    save disturb price force sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_octal(n):
-        return oct(n)
+def find_min(lst):
+        return min(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
