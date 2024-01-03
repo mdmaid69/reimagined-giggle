ffe6280bbf4d0010ba2de0f84960e2df1bd7e0cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:10:46 +0000

    hair clear hurry myself ride

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
