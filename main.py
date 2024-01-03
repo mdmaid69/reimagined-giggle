77bce751490f76f928398c8071462995dec13022
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:52:53 +0000

    normal Oo rest fault wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
