83f9f5cc411dab7585c6618aed755c355ad20ff6
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:15:52 +0000

    each theirs doubt difficulty handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
