d2e6139fb6ac5eb737c0bffa5cc966fb1b1981c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:37 +0000

    pleasant dear long either fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
