b35aa104dbbf0480113e5da3701675930ab3c00c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:22:47 +0000

    too breakdown subject high indeed

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
