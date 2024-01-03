810a810e372eec8ce6c9f72522cbb9dbcfa7a7e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:43:59 +0000

    second if base fire estimate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_pressure(force, area):
+        return force / area
