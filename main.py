710725b6c3e076e0485bc399ada8ca5268da49a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:08:48 +0000

    gather future my blow mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
