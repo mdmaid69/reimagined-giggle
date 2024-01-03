11ca3a448a289d70391a27e03c69e3f956a8c7cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:52:41 +0000

    shelter spare eye slight foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
