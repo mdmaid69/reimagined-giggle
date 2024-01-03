c34c5332454e7fe3e149e7e964eb0bf8180d8b2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:28:33 +0000

    drop education generally flood eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
