7b5c914969f8fb1116255348980dbed06811092c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:19:28 +0000

    listenfor exactly small up soon

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
