725296a8fb8eeb10ca019e2924c94f42ffeb1a96
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:43:50 +0000

    object street deliberate congratulate search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
