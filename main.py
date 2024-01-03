a1c268829df03c8f220422c713bea4a135e1f5fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:06:13 +0000

    behind stamp move contact recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
