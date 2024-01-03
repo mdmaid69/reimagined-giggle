317edc356b0299980f1f315d03eb6401ae44f484
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:34:20 +0000

    base complete appear welcome sale

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Characters:", len(text))
