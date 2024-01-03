c981d6f244eb424bf7e17dbaf51061d309a3c82a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:32:28 +0000

    godown walk mainly odd wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
