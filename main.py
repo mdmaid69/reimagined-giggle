56bdf63f5ce9035a5b8a52afec9f3b0ca7d9749b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:42:51 +0000

    gentle circumstance pool protection hard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
