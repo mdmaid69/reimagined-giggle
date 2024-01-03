ffb15d0fd3806c5c983e9ad93fcd54063ba14f23
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:36:10 +0000

    delight read flow appeal long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
