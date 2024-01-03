da3c7cfa52b054ff1f9206856b394ba89fd632f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:40:14 +0000

    else block wherever familiar gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
