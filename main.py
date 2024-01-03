b27f321ee66127153cfb157f3bbab9d3729d1dc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:21:28 +0000

    bunch regularly year tip 571enough

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
