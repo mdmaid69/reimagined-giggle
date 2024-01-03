1cfbd64b8ab3ff676c64b0416f3d8d10f1669215
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:58:57 +0000

    pay justice lean material thin

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
