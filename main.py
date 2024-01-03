679dd5b622c09bbd54b9f3d11a186f6790f6b05b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:33:09 +0000

    bye light complicated fill bag

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
