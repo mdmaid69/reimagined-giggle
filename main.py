001ca7b1c1ea9e0e4e58c4eff16dd211fee5cc97
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:16:11 +0000

    pretty file store mainly waste

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
