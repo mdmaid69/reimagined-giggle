bcf7644b7a3e61b7ef35da67d4cb37587bd80273
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:47:24 +0000

    Jj rest stiff do disturbing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def sort_list(lst):
+        return sorted(lst)
