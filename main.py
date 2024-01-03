1ddc6db5161de753b00a70d0e4070aaf92347f13
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:14:52 +0000

    section image standard software reject

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def convert_to_octal(n):
+        return oct(n)
