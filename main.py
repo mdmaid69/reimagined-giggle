b224ddcfbec27a1102b506e8ff340509df275e9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:07:27 +0000

    touch towards physical exam very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def split_path(path):
+        return os.path.split(path)
