fa447f2921ddf524b6c7f5cbd6be52004f78d518
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:23:01 +0000

    broken sea sad pencil emphasis

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
