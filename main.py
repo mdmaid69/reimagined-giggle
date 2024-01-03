058a8cf91a1a2e70ff8005663845e553984ca0ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:32:10 +0000

    present dick contrast television thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print([x**2 for x in range(10)])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import sys
+  def get_python_version():
+        return sys.version
+import platform
+def get_python_version():
+        return platform.python_version()
