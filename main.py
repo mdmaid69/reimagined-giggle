c25423a2a55c4a23547fdbc18b7320593ac8dff2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:52:04 +0000

    press wipe person convincing female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
