ea1583525634a29a8e6ac7e11765f14a35ae16e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:37:11 +0000

    tonight pile godown shop can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
