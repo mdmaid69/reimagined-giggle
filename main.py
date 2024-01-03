cb63163f4a26fae6caac89aae56f4286eaed728b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:49:35 +0000

    world carry post company neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
