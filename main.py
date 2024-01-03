51c915d2dfd2c99ecbc7e497c48de8fe67917b0a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:21 +0000

    true difficulty wind lay rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
