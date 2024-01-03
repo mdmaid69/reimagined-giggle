3c31cf8ead8c0c0a576403a15b2eedbc58b00cdc
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:01:01 +0000

    disturbing goon marry discuss concerning

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def reverse_array(array):
-        array.reverse()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
