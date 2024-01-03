fc2736a8c2483ba7577e0b6962821aa4716ebdbf
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:46:55 +0000

    contents stretch happen tear chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def add_numbers(x, y):
-        return x + y
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
