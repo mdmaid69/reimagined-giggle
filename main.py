4a9ab5afa66f46e6fb107ece6125ce1ad9099bd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:58:22 +0000

    makeinto phone clever use spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def delete_file(file_name):
+        os.remove(file_name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
