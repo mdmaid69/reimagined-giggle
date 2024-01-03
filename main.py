64a1744d722081e32d9a91aa70cd16fdfba5db45
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:38 +0000

    union throughout minute summer mother

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
