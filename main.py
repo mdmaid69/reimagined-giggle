f6120b9fecd8c4b738e6f879ad5958d23b0e7125
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:52:52 +0000

    consist polite operate blame rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def get_current_working_directory():
+        return os.getcwd()
