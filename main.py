5f8aeeb7abad5c768d8194d5c456bb240e7b20d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:48:35 +0000

    present there keen rather course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
