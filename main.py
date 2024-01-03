e7a3d95a45d7416636e99549fb496f4462672fea
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:27:16 +0000

    giveback crash create discover Gg

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
