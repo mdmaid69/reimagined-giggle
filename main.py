715395e8ee422f21219e44b8d8114cedbd8d5eb1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:16:33 +0000

    press common swimming mess special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
