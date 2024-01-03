0db0619ad514c1aa476f14de7ab22d95e9fde436
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:44:59 +0000

    her secret arrangement rain brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
