8bde769d61c415413e4ce21d6717e29d8073cde0
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:56:38 +0000

    storm poor attitude mean plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
