be8823acf5d14d23e447ef1880839310f837b68e
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:55:51 +0000

    release healthy male hungry compete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
