2e7a73ef42edefff7f960407a51c805fd6dd84e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:08:19 +0000

    house certainly single genuine strict

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def get_array_itemsize(array):
+        return array.itemsize
