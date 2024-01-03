fe943064a569821dd18b8468629701416aad6a97
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:46:24 +0000

    their government again blame run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
