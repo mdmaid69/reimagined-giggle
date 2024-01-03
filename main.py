42c236ad920b5c93645a80aba39b18b987829feb
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:21:20 +0000

    gradually comefrom naturally crazy cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
