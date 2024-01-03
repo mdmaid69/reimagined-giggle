7a0515854e6d318385ebe3b02c035b469caec70e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:21:35 +0000

    favour disappear state turnaway border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
