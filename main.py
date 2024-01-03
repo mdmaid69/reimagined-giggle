f66436da671a2e06902693a17ee7bfd8f981ba0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:12:39 +0000

    popular short juice difference trick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
