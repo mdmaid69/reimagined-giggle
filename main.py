bd82e80ca542c51d063eb2776ab10669e3ca8d94
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:57:40 +0000

    juice tight my unkind total

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
