4d97df3be5760f16f15b0abcb0452367ee43e57b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:32:27 +0000

    some sure determined myself start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
