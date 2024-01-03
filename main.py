eefdf888bfac5814a17528083261f10a46e666ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:10:35 +0000

    read tonight never opinion comein

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
