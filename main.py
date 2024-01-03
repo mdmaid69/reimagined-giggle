bc91723bdc7c6954e1cbdeede565ec085d84ec34
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:46:38 +0000

    little place country country favour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def is_odd(n):
+        return n % 2 != 0
