fde18f97c906ae33d1b0f15193888fcd2d139912
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:25:34 +0000

    interesting goout live active female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
