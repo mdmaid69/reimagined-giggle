8e3b8da239bf6c359d79c820a8efaf4647c73551
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:57:10 +0000

    sore arrive somehow explain Nn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def is_even(n):
+        return n % 2 == 0
+import array
+def convert_array_to_list(array):
+        return array.tolist()
