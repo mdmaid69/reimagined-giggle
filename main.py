a4a0285190b3c1bad16f57d8310d1ba1eb03d71f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:32:17 +0000

    block complete open straight image

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
