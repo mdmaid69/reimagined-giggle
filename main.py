9548aed18d0c05b533503f37f642940e70527a4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:21:46 +0000

    characteristic nearly apologizeapologise unusual rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
