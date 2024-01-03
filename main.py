290d94cc5941bd3902369dc46dd34578b59efd69
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:37:20 +0000

    left apply direct alone getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def is_odd(n):
+        return n % 2 != 0
