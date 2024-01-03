25e49e9ec53fbd9514654c497abd22c752eeba49
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:30:15 +0000

    shoot tread involve ride minority

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
 import array
 def get_array_from_file(filename, typecode):
         a = array.array(typecode)
         with open(filename, "rb") as f:
         a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
