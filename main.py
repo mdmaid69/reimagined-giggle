71b1d2e395b1e3f0bf19c6e228e9a42ff372432b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:54:01 +0000

    calmly argument soft rest visit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
