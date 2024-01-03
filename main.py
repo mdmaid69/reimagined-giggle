c3cce5cd58f6bc02610341042fbfd1898cbfce59
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:44:58 +0000

    zero spite right command print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_str(array):
+        return str(array)
