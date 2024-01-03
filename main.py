43613e7b71380ad00ba90a16801d51ebd7cfdc38
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:00:55 +0000

    clean have believe attempt name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
