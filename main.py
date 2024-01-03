94167824b2aaac9fdf6049d78b6943b0c16dced7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:20:59 +0000

    lost early runinto shock smoke

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
