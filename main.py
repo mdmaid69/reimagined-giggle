b2bb3e1222751a635ddb999cedae4f6217ffa0d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:46:10 +0000

    say fasten control every debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
