feb51f50fd16033cdf1ada5b4fdac836fcdf2f52
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:05 +0000

    goback(to) combine tall achieve amount

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
 import array
 def get_array_as_format(array, format_spec):
         return format(array, format_spec)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
