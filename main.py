61656db2e0dd46107ed553ab02cd14d52a6f79af
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:53 +0000

    hand little turnaway score manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_array_to_list(array):
+        return array.tolist()
