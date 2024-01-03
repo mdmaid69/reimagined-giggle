974e44e87f6b123c768aaf0545e3e472aec16b8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:09:58 +0000

    feeling difficult anxious ago undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
