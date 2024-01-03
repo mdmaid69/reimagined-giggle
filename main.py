c0b853f1109a51b39f6f8269f3eb4b40bcebb3b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:37:00 +0000

    sore book generally uncertain limit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
