37d41594e2411c6a66a0d12f7a8493d1500b58d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:25:23 +0000

    empty completely choose limit opportunity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_list(lst):
+        return lst[::-1]
