1311cc477783b2a2bf9dafe28c7f98c6ef9da8c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:06 +0000

    day close community sound case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
