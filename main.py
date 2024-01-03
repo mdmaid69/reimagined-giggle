af515eb86f4a25f77ec47963e133141e1c0dc13f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:36:25 +0000

    defend win drop discussion live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
