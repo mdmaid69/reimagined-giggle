7f4cb0e215cf7cfc49da6125892e0efb3fa82955
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:56:31 +0000

    hurt everybody war spring look

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_max(lst):
-        return max(lst)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
