7dd5c7484cb17ecb66884fd80be1c6c32419de44
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:51:52 +0000

    shout turnover previous earth favourite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
