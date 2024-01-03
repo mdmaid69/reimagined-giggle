38ece56d9bbdf37f49e45333199de70c345b7890
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:07:38 +0000

    drop computer call wall afterwards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-import os
-print(os.getcwd())
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
