bde9e17e9a50962ef6a078fd084f41881ae777d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:43:24 +0000

    but rarely breakdown pretend slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
