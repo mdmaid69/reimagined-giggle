6c2e1d41499e6e4b8214ba6a5cad3f1deaa53b63
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:49:19 +0000

    pause passage legal basis emphasis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
