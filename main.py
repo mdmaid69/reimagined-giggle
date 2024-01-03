68f3e9e23cfcbb2cdae583b44205624a1fb3bf04
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:23:24 +0000

    odd right save command married

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
