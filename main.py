bc989f5b35a212c8444a9ff894dd51012124e39b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:18:56 +0000

    hurt will government disgusting taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import sys
-  def get_python_version():
-        return sys.version
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
