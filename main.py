058e989056e2b50169c2fe4650c9bcd048ac358e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:37:35 +0000

    yours lock responsible clear doctor

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
