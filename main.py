97e40b9bd3ba349aa780f7d61cba1e968026b980
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:30:37 +0000

    help innocent feel blow thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
