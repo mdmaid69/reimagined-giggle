4cce63f33c6273996d5e8b8d91ec469b216c0022
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:26 +0000

    picture fault mine lonely height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
