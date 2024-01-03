1bcb2f91d443edff2af519cf72d0af8045692844
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:47:55 +0000

    cinema open level receive eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
