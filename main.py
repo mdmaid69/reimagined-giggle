1b538d0dec5451cb6cf342110b3a1bd33bbca450
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:54:12 +0000

    drawing runout kind slice people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
