54f466c40d4f305ad89f9d256fd3942035e3c53e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:53:18 +0000

    side tense final town country

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+name = "Python"
+print("Hello,", name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
