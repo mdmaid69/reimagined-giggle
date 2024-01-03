597b97f44e4b3b541fbce29249fe81d53dabea8c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:37:29 +0000

    drop statement exit nonsense fond

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_length(array):
+        return len(array)
