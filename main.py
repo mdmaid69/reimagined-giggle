22870b27f6e459d7a1de6d11a34d1266a7cf820b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:23:26 +0000

    towards summer swimming trap innocent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
