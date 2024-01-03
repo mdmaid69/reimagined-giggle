00cbe4875f11e27d444b513c4fd204d779d2cf62
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:43:54 +0000

    contact clothes pain its gather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
