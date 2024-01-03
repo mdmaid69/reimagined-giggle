51c3468bc764b6e75d986363b8ba9baf7e491c18
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:37:02 +0000

    round by bread brother square

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
