9a7b4e30f9d84bcd579f9299588d5fe815a155af
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:32:10 +0000

    suckme with lesson waste crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
