f2d7806e012bfb757a429e7bd7fb2aad0b3594f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:49:01 +0000

    probably rise indeed badly colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import time
-print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
