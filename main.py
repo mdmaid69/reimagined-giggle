8d3ad4f965783b4448a49fdad1d6c1e60fa113e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:13:38 +0000

    flat match attention agree building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_palindrome(s):
+        return s == s[::-1]
