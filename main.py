a423c4e6b11ac5d6af868af6ae0fa5b11f9cf692
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:11:30 +0000

    godown strong again push radio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bool(array):
+        return bool(array)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
