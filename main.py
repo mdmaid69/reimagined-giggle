deda822eeeffdb16171f67f4c71ce36a4634d96f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:36:58 +0000

    but office spend man sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
