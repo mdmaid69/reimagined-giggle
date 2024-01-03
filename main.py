665d8cc1aa6634b8985f5a9b6b6cfa3d3e66d38f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:52:52 +0000

    detail comeround discover or debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
