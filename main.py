1d40631c404da8fea77ae95f975f200ab5079cd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:35:31 +0000

    considering tragedy now father substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_acceleration(speed, time):
+        return speed / time
