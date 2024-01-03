f2b89426569e7707cae3e2f7379203dd7618563a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:46:00 +0000

    instant step hot off tape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
