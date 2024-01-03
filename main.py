03ab2a9ba995f04fb3f3557d2a8b230681bb7a6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:58:17 +0000

    uniform pass its lookaround strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
