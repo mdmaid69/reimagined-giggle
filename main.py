94cb5f0bdc4727d215f8c7063223175e5538d0a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:22:07 +0000

    prefer wind fast duty remember

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def sort_list(lst):
-        return sorted(lst)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
