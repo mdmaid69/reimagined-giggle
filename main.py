13b92606149a11ad94380841891a54f3a19d5e26
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:25:53 +0000

    567enemy Yy past 564encourage every

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def square_number(x):
+        return x**2
+  def square_number(x):
+        return x**2
