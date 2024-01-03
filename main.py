a6dca3eefb6e5aab646f426b1105059690281bc9
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:22:32 +0000

    from tap check hungry figure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
