cc881dd5e0f2a24b6097044e44aacd07b7f8851d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:42:34 +0000

    convincing date like decision dear

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
