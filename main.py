5bf69c3bd31591fd4b0a2bbbe4ca22f537487379
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:46:18 +0000

    order include crowded pause before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
