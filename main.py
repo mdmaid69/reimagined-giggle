15e29632a6b40292e0ccac53e79cc935614cdc86
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:59:54 +0000

    favour other desperate shine relation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
