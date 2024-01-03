5ed79fb4cc630221f17852706c2fa0fae1265c53
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:42 +0000

    agree calm before degree equipment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def reverse_list(lst):
+        return lst[::-1]
+def square_number(x):
+        return x**2
