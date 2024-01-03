7a915531253a4705e6846e49021b8a8e7148766c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:08:32 +0000

    basic concentrate frame herself world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
