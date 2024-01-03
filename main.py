81abc98e077083df1e3029b1c59818d33bdeaef9
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:47:22 +0000

    warning thing sticky shoot definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
