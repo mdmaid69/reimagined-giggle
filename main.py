9a4f01133316ec3f1ca2a2bb7c98fd3ad34191ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:55:06 +0000

    dry square breath collection process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
