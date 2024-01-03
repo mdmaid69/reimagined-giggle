782e528a9645b870a48d1bac120b39be514d0626
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:31:42 +0000

    criticism success shut instrument warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
