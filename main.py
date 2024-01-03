22dea33f665052d560a4905dd45147c82262ab9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:14:29 +0000

    top mass success approve plastic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
