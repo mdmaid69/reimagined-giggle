25ce8cd142dc48df96256e65d577af111d55e0cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:33:52 +0000

    extraordinary swim point demonstrate break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
