ba9966a24ddc683335943d227e935e2390d7dbea
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:19:07 +0000

    makeup branch equal sport whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
