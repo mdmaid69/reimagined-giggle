0d02164b939ae20418d90b1ce81029a417143d8f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:33 +0000

    than doubt admire themselves influence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import collections
-def create_priority_queue():
-        return collections.deque()
-def convert_to_binary(n):
-        return bin(n)
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
