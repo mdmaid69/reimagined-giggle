593f074fe17ab64279a06e49391c4503cb3e48d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:24:29 +0000

    ready be praise half admire

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
