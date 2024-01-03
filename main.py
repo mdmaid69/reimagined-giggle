a3b98c97c42b6c730559a8f146cf8ca3ce6c4972
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:45:12 +0000

    offer discuss tidy nerve when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
