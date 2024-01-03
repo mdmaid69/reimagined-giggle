2dc7e0eb910eb4929a76d95855db843bf6087dc5
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:08:40 +0000

    godown short information tragedy relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
