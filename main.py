75953d8d2c9aea7aed4dad94580696a19b5b7bb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:47:27 +0000

    offend fat listenfor line hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def convert_to_octal(n):
+        return oct(n)
