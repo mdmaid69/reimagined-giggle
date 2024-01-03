e90c13084a4fbfd3c6b118dbc803d580dcaa2669
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:25:31 +0000

    model claim port pronunciation dish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def convert_to_binary(n):
+        return bin(n)
+  def convert_to_hex(n):
+        return hex(n)
