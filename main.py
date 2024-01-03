c55ca982357c1678625f562e780cb78bdff53c2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:47:43 +0000

    ashamed home week future Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_work(force, distance):
+        return force * distance
