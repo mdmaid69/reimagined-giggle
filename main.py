c5ba075a35410c661a56031e207c59238f005189
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:52:18 +0000

    sure colour cinema fast unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
