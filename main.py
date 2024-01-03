b5055eb3465750dbea30f67bd938a9f41e2849a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:59:17 +0000

    goup fortunately damp Hh related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
