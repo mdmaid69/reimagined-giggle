f5e75b9504712531f1093a36a2f2751262bbb911
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:59 +0000

    pleasant miss table pronunciation level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_sine(x):
+        return math.sin(x)
