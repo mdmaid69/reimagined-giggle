c6688dddf21702917cbb36916d9c5a19215cbaae
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:00:37 +0000

    tough care steal introduction entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
