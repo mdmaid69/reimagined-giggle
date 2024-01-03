1eada80ed1e90ca537b25aa7f0c2e23dd63c19fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:34:06 +0000

    hollow sell bring late shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
