bc59dcc7c6b51ef476c6fb9b165f06e1dde135c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:48:05 +0000

    bring gooff walk sit minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
