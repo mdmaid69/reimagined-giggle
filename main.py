066d1699fe8f763b0f2adba64d22b0e3ebf21c92
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:27:00 +0000

    achievement point risk breath rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
