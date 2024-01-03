250c48521a8b48825b138e4bc63c3ed6db64d8df
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:32:05 +0000

    over whole race summer deep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
