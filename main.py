b9a060d6fc61d68d1ae02d232dd56f5110830cf4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:56:06 +0000

    brave cut simple than hold

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
 def square_number(x):
         return x**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
