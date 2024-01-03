6ce147ae33fca43609d6d8c57614d08c5a283545
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:31:24 +0000

    side clue border hope outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-print(math.pi)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
