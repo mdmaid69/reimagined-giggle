3a971902e5e9bdfd86c52cb49cb003a51df4a853
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:33:04 +0000

    admire pass off offer justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
