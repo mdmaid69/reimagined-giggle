458a6272dd6da82b101a37e0cdfeefcf625f014d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:09:28 +0000

    complete cause Zz limit down

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print(sum(range(10)))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
