6e45033a5375ef889488daab7e4319cc9ac5ea42
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:14:40 +0000

    attract ought belief with brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
