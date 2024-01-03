21e8e7a7383eccef14931ee382ee2cd572b113ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:55:36 +0000

    popular such government illness cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
