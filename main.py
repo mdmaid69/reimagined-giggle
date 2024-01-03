f6f7baab5acf0a81f53f867e21edf861a7359ffd
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:17:18 +0000

    skirt die Rr pause also

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_pressure(force, area):
+        return force / area
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
