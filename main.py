f4034768b19911a525e4e653a04ebad8fee5c46e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:52:59 +0000

    soft rent callback tradition wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
