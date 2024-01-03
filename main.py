43cec21a6251e3fdfbd49f397dbd7bb05de4f974
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:27:43 +0000

    body by whoever report science

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
