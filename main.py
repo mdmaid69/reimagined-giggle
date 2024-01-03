1ea2a98eec15a952127804e79cb317b8a92b5bf9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:38:53 +0000

    bored interview collect like claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
