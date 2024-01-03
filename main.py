c25de88a50283616a3fa48f3716ca4f6994fe472
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:54:54 +0000

    true person plastic long read

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_octal(n):
-        return oct(n)
+def convert_to_hex(n):
+        return hex(n)
+import math
+print(math.pi)
