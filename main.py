27b1b9dc6789babeab0f1afc9049b266ea6387d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:32:26 +0000

    sail collection though visit street

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_even(n):
+        return n % 2 == 0
