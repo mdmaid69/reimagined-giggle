989bfb126e864ff499a24d205bac9fbd6c386b5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:44:10 +0000

    scare final control scream nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
