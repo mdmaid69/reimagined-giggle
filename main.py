f998511a871f97cf8b23e37c555734b3facf434e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:32 +0000

    rain fun nose urgent concerned

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def square_number(x):
-        return x**2
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
