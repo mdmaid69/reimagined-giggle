2a81f3688f3ba07d0e8c274ec2a9f181412a3149
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:37:41 +0000

    mainly heavy call practice quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
