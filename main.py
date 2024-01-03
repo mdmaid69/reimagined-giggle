00ac1c5243140a52ca3765098d22a5436ffe1441
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:38:44 +0000

    measure spin voice criticize activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
