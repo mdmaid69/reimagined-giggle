541b221b5f57547df06c1de1195757eddba9a373
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:17:00 +0000

    rough sort somehow gas design

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
