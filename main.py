3f6ec43e03de421c6da3ce6b03b0e6b87b3ff733
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:32:05 +0000

    music plant future dinner turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
