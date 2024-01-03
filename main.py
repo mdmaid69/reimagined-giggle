8b72c0e5ccded0b46bd306d6d9dbcacf129e7c5e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:26:31 +0000

    brave suggest relax safe Vv

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
