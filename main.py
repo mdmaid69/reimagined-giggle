6d0953e9de21aafcfdb224854c5949db5d17ab3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:33:54 +0000

    cover holdout poison by principle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_length(array):
-        return len(array)
-import tensorflow as tf
-print(tf.__version__)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
