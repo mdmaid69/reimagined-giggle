13f8661719ac22fe40c624ecd81433419d25be4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:12:37 +0000

    force comeout frighten influence sick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
 import math
-print(math.pi)
+def calculate_sign(x):
+        return math.copysign(1, x)
