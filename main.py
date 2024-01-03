5b2dad649deb301c06f66080f4202a886a0642a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:50:39 +0000

    from yet question broad bush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
