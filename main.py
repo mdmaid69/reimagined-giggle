cc2f670d8d3d50a376ef3f937a4370baf3d26a1e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:14:04 +0000

    experience ill plenty congratulations present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
