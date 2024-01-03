c009038c79dfef35d3adb5a43af0a47dce85b628
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:32:44 +0000

    secret suggest tired should on

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import sys
-def exit_program():
-        sys.exit()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def reverse_list(lst):
+        return lst[::-1]
