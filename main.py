676d776ff7d7416a26224e9ead758c8b87cde971
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:48:02 +0000

    teach occasional film makeup burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def get_current_time():
+        return time.time()
