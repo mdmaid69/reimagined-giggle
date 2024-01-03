5ffca58a60727b3cd55f19acb55747d969c0e823
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:49:08 +0000

    completely operate play twist cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
