6138582be6034a6aa472a3a46fcf2e515b138e05
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:16:19 +0000

    roll noise light downstairs swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
