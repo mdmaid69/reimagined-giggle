efd6d447365027f65e9d1b4c5390e157e6e0989b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:46:06 +0000

    neat affect needle direct teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
