208b22fef8082214d18410a55c46ad68c209ac1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:22:28 +0000

    fence uncertain value left offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
