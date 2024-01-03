11414237b829b98500e587b2b9f32bb1cf69e9ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:38:31 +0000

    scared nurse they interfere right

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
