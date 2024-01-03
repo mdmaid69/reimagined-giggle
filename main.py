e4cbbbc2d862bcb112d05f56e56c01c038ae68eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:52:32 +0000

    steep match reach crisis shopping

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
