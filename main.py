2a803989c133acebb618ba5035c5b9a8e37d27d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:27:32 +0000

    wrap congratulations godown shallow do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_power(work, time):
+        return work / time
