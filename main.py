e08f89e0e1ff541658ba11dbb2a9b6040106fe9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:32:20 +0000

    basic talkback better drive careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
