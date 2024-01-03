2e1f2e78225cd332708e7eabbad4b2028eed925e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:21:01 +0000

    emergency star interruption news success

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
