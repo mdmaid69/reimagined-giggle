4fd3b526e3e9f7d7fce179de9f4db4fd350acb7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:50:35 +0000

    substance stamp score season raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
