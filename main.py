a4279ff3812262b19dc4189ce7d54a63bb9ec00c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:21:39 +0000

    instrument pronounce over bush shallow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
