ca90aed3e43dd54456c9187e870c19013c7a9ed1
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:00:49 +0000

    satisfied dance attempt fall fresh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_work(force, distance):
-        return force * distance
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
