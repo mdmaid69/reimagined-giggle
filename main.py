a21edfa85280b165843b86acfc68f2633607a42e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:57:22 +0000

    tread season rid neither disappointed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
