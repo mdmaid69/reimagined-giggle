0e9798ca20d2880ac0c37a0b75a1f7bf8bf542bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:59:42 +0000

    sure smell choice strict shout

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
