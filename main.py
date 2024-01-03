4433c01b5261d8e82cef4db7faed6fa64ea5d7db
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:50:32 +0000

    trip getout peace affect contain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
