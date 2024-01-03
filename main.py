5d1ad0a635893399a9a078f16f5cc0e1cbda6538
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:16:46 +0000

    clue return force move meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_max(lst):
+        return max(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
