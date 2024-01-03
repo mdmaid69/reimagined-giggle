eb0e08f20d81ee3f356b2277eb7fa3bf4fc3c168
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:41:24 +0000

    soak prize spray transparent word

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
