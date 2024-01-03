53531ff81b771891e45a59b199160742cc5187cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:29:39 +0000

    quiet ever introduce delicate doubt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def change_working_directory(path):
+        os.chdir(path)
