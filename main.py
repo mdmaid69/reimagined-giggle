a85bfe68c248e4651a81617b5e8cf9f25d46d138
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:29:18 +0000

    payment near partner nature track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
