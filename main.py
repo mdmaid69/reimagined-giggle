f59659077a42aafe32c150cc55cb793bc0ce68c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:50:11 +0000

    risk meanwhile know process Yy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def set_array_item(array, i, item):
+        array[i] = item
