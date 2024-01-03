7468d32145b478383a22c26165cc73620fa21cc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:33:25 +0000

    anybody art again where name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
