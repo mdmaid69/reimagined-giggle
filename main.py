ee0125627d2fea65f735644c96b894613a561697
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:01:15 +0000

    every joke action direct programme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
