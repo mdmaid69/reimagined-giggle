dae559ad5eee626b8ec8e0d424ce41232795eece
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:53:48 +0000

    leave owe remember impressed rush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
