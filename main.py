3c7551a255ea8da8c619a04d2b8123c28dee400b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:27:17 +0000

    shop bridge recognize fear comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
