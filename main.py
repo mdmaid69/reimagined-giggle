a1799e46d2d8a5df8eff1e756ba6b3208f043e3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:11:14 +0000

    possibly yourself combine pour difference

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
