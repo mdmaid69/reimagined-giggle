3254f0dc14878172b93fff12c054e9a9c75b9250
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:07:19 +0000

    idea save must appeal nearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
