36942da5979672d432bcdba934f6c10f31b11c12
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:06:26 +0000

    floor stiff card goback(to) modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
