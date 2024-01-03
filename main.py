2f4ba18424a35a49a257ff9d8b26be5d52228f29
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:28:21 +0000

    child surprising base guilty question

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_force(mass, acceleration):
+        return mass * acceleration
