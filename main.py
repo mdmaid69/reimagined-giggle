c567b6c16077b95500000bce8aacc4943465c76a
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:20:52 +0000

    interview spoil little afford share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
