316cacbeaf7098dbb7cc5024a3b7ad30bb1ee93e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:47:52 +0000

    confident almost guess form force

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print("Hello, world!")
