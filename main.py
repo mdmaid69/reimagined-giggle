ffb6c53128d0f24175b1aa4a8bd62df9d8799c80
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:21:04 +0000

    party mention seriously defeat treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
