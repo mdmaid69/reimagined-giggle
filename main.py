965caf1eb16ef6fda2e884b1a73d449af9fa7d78
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:54:48 +0000

    respect beautiful fill Itself excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def greet(name):
+        print(f"Hello, {name}!")
