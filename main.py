60525b511aca670aa19a0a9ea813db76991e3c7c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:52:05 +0000

    distance shelter Vv uniform reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
