8ba945bca51f4d7a1b234046c4cc72b495135fff
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:09:41 +0000

    shade flat image neat past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
