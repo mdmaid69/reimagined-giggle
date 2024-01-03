be0ef3aea64358e7910c44d55cb40a5a79814039
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:23:20 +0000

    pin turnover temperature across now

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-print(os.getcwd())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_length(array):
+        return len(array)
