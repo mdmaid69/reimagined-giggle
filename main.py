9f0cf47d6dd2fdde43b12a4ef37d240adca1d537
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:15:27 +0000

    giveback which estimate right explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
