341e40b377555d32a509bd03fe4ae44c4a88d0cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:11:09 +0000

    risk doubt goaway take light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
