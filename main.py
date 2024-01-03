5e70ba111fdc408b764779435695caf895f51cfc
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:54:15 +0000

    history concerned choice home comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_list(lst):
+        return sorted(lst)
