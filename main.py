e755cc49957a36f227b9db256be00b79ff94a8b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:35:39 +0000

    soil separate this soak happy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
