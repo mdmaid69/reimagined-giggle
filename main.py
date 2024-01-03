000f1df895a2fede940cfce0ac3ab594710cbc31
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:11:16 +0000

    suckme what same early Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
