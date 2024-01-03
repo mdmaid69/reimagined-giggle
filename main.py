1e15c4b4a5777d181c35a4b0bf5a25c78450913b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:23:28 +0000

    567enemy contact door tell comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
