d03a1ee411928538a0f5b0682ac0dd747f82ae23
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:59 +0000

    bringback warning ought concern respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
