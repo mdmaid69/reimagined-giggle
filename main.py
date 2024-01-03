65cfc2b16aa7b4bb5830134972e12808aa1966e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:59:51 +0000

    ahead estimate suit or usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
