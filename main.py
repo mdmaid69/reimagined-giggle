88bc73bbbd3a199f79033e5f07da33059919a254
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:48:47 +0000

    keen bright little clearly performance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
