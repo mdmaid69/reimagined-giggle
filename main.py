b28289add0619ac97fb05549d5fe922ac14f99ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:22:30 +0000

    fail water phone yes taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_set(array):
+        return set(array)
+import platform
+def get_os_info():
+        return platform.uname()
