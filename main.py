c0671f828350800f9742bdd5d2ad1befa2db691f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:54:16 +0000

    hair happen fierce false control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
