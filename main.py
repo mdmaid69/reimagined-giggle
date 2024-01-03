c51aa40398678d916ab96defd1c5811cfb5cd952
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:17:53 +0000

    surprising section unfortunate empty attach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
