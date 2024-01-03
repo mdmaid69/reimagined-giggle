b306e5c34ca40a4f5d8c8eb1cb4a5d7b140b3f9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:59:19 +0000

    pollution spray branch wise fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def get_current_working_directory():
+        return os.getcwd()
