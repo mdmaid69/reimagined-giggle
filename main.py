d6c8a91b81262f99c0869ca076ee1b650d741e4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:37:07 +0000

    fire runover transparent similar guess

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
