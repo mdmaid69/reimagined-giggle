a5c440731a979be75f015f1602459f6a2748aea5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:03 +0000

    head path rule pretty care

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
