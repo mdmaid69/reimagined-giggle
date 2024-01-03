87781ca0bcc9b93fe47d5246b36f0cf9212728b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:59:06 +0000

    about exchange storm miss thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
