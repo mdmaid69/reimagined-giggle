bb8992a34f670c5f2a339d75bc4eaabcf8163328
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:40:55 +0000

    recommend we poor floor Bb

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
