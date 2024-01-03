9db149d7956468844776ec3175d0ef2a8fd2427b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:24:46 +0000

    main any remove stupid model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
