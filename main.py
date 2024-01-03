8d6ba36304c11a34ce22768d8233e81325a82c3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:27:10 +0000

    no later family remove risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
