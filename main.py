7e1afdb1b428e1dfb46e69c771c88d670b11c197
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:48:39 +0000

    face by rob bank my

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
