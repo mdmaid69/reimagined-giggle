c965235f67ac9814f174d0f05fb465b77fe2ca16
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:48:32 +0000

    stage undo desperate radio pleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sys
+def print_python_version():
+        print(sys.version)
