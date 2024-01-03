e21a844d63b86bcebfaab1f53204aa46187c39d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:00 +0000

    quarter bite description company busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
