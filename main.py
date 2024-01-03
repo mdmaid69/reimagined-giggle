755810dbcb311194a3a7519d580c22ff88ba35f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:06:47 +0000

    warm programme cost young excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
