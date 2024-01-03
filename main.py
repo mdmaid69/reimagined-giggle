bf5db9a9ef60b23839e6c1845b31be635ab55adb
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:28:08 +0000

    bright manage main danger quiet

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
