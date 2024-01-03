f5bb637ef7c5d7c0a29158e4f053fb64557a006b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:48 +0000

    regular effort turn give rain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
