78e84b4a014e5a6b39c721913c93f24450fbed21
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:16:18 +0000

    protection ours pause tradition but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
