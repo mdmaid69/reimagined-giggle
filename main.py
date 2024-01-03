8c2ded928dadc690c46d520f4de895d79aa2524a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:01:37 +0000

    fun illness get behind opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
