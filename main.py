132708b46d1733a14d732e91980e3e6a93d4fde9
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:12:06 +0000

    available keepaway interest point relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_length(array):
+        return len(array)
