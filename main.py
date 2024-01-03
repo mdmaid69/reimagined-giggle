ff31e9e27d064767ee29cdf3c599ee26748e29d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:26:25 +0000

    by guide almost broken relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
