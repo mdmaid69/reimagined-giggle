9d7307f1e7bece94001f60412d4636ded500f16a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:23:47 +0000

    achieve recovery office collect bag

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def is_even(n):
-        return n % 2 == 0
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
