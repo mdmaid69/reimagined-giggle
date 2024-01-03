4ae307e1bad02c7073474469238ed73949e691f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:03:10 +0000

    welcome sorry trap earn ours

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_unique_words(sentence):
+        return set(sentence.split())
