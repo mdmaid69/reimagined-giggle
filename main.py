2d0bf756f484a1dfafa45a0b9ea5eccd55bb2e6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:42:26 +0000

    opposite because thankyou shrink backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
