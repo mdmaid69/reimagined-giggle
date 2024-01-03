bc4798b21908e4fb009829187351dc6e382e32f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:50:43 +0000

    funny pretty modern prepare abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
