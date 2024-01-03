6a5d2047af1b76022b54957154cdc0182882af08
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:43:36 +0000

    judge fail unit light beyond

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
