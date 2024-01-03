e5c2d94e14b9bcdc231d43527322dde489ab5b00
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:53:08 +0000

    convinced certain cook lend somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
