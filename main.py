17d0c75c9cfa77dd5f3559927c27b14dd0f9e4cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:20:56 +0000

    quality couple skill separate answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
