c7c6b57d66fdd653b4a77587d0731198e3229a9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:47:51 +0000

    set 179bite fly Ii force

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_acceleration(speed, time):
+        return speed / time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
