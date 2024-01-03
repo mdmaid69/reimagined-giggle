8394182a57b18c908f8e1a113c046f67c01c7c6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:40:19 +0000

    remark roll deliver tap the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
