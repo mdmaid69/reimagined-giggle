3143dcf4f322fc05031c0b887ef33cf862956a32
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:04:05 +0000

    like forget cool until age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import time
+print(time.time())
