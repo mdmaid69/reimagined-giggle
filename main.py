9eaef7f31fad6f37d074ef0f43bd55e3723b257d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:16:10 +0000

    situation relative separate own Pp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import os
+print(os.getcwd())
+import os
+print(os.getcwd())
