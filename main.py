34a2797f6cbeae3d7450c437a29c22542d43dd2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:49:49 +0000

    card turnup birthday mean compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
