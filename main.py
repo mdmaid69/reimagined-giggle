85c3c09fe33e5d7fecf1685f64f90717f0f60a11
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:50:56 +0000

    announce forgive at prefer raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
