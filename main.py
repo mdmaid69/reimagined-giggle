d9f8de8d06c3476f7dce1d201a5b746c355d4605
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:51:03 +0000

    everybody waste such bad part

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
