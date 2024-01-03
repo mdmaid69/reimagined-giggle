1a1d5ac1be698be018b9099a7c8d3bcca41592b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:26 +0000

    bend cry backwards deep total

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
