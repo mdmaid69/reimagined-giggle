90857bca3342a58f00508df8d7879421eb9147f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:58:20 +0000

    run yet must fast anyone

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
