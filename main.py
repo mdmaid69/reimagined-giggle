37c064af9ea656edf5eef9cde71cb126baeeada3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:42:41 +0000

    swimming attach purpose extremely breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
