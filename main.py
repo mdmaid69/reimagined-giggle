7c6d04bfc3ff92f5bfaa94d7b89669f6e0eeaaa3
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:52:54 +0000

    destroy sentence suggestion turnon cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
