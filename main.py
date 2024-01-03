1f5768015c5088374cfe1a75628ee452d392895d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:09:24 +0000

    term 570enjoyable affect hers nigga

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
