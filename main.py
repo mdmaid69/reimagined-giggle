ef3bb95b6058791588fe9999d14c152145d0489f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:03:35 +0000

    confident attract much worry by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_list(array):
+        return list(array)
