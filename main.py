d3441f174bf6317de403445e0ecc31fc85427a0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:59:50 +0000

    entrance swimming decision solve hair

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def count_elements(lst):
-        return len(lst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
