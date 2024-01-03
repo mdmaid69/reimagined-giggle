7cb5e7d671603d5a19b8c44dbd45a32b7711b92d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:10:09 +0000

    serve familiar high unusual feed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
