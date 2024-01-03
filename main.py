5830ca97a5e168bd7fbcf87100b780e8fa336e8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:34:00 +0000

    ticket transport switch secret wall

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def reverse_list(lst):
-        return lst[::-1]
+for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
