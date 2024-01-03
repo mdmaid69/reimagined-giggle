3e500194885b1f5f103e761559c1d80e8fe4a588
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:37:28 +0000

    error seat concerned she exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
