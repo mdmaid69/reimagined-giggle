7b4fe39aaf6109d512dee4e9d64598940be76267
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:06:35 +0000

    dry violent easy embarrassed official

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
