30708afe036117c3a89114401c0296a8aa9105b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:08:21 +0000

    service grateful sleep pretty criticize

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
