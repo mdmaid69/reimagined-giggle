32818c97d48bde68d6917c9c61c5e93ffb57b4a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:46:32 +0000

    real story probably seem occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
