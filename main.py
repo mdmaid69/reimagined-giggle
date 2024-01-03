17b58f5972a3e0471810d4dcd4aed590cb8dbb8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:18:16 +0000

    callout least seat desperate shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
