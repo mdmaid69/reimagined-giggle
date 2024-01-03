81c764ccd7c66969f521b92f7010d272a9c742bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:03:59 +0000

    weigh ground disturb wire slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
 import array
 def insert_into_array(array, i, item):
         array.insert(i, item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
