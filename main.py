209178fd59deca89d0baf218905919379ea462a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:17:15 +0000

    once machine hear burst left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
 def find_max(lst):
         return max(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
