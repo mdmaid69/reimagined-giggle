72c2dfdae5053a9045c7c664d27bb96f9a8cc7a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:55:24 +0000

    job country like meal live

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_min(lst):
+        return min(lst)
