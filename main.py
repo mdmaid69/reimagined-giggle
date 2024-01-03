d75f9a4b091603d2219942367f9d7b73b906f922
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:04:07 +0000

    hope Bb fire anything spray

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import sys
+  def get_python_version():
+        return sys.version
+import platform
+def get_os_info():
+        return platform.uname()
