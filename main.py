80c73c991a311375fb50a86ffe92b4be651ccc26
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:52:05 +0000

    fold mess return kind know

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+print(sum(range(10)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
