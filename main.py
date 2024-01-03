b2d1b8582ac23dc0bd777b5744b239b1f9cc8ffa
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:53:59 +0000

    wash quantity wide plan of

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
