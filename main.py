0253f4a9ad14429482e768c85f1261c43807059e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:17:49 +0000

    think contrast unhappy Itself minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 n = 10
 print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
