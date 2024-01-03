b9b6ff12a1a1b97338dcd23c179368d70d6a8994
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:18:00 +0000

    judge divide preparation thirsty protest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
