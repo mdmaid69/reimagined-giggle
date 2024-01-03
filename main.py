8e0b9a27e1031eab994638b7ad46285f0eba200f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:49:37 +0000

    plan disagree fun once recent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
