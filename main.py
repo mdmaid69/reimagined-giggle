c4e268a9dec9df28f46d3f3f4c053ff4753baee7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:16:35 +0000

    instant bang service bin calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
