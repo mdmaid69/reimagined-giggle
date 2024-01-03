a93e0b8500d8e8f95ed6d0323b15a00bac2097c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:36:39 +0000

    scared onto wonder somehow finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
