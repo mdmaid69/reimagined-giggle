09870354a143b011a5391a18d26c9c774432a07a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:11:38 +0000

    turnon sell goup little unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
