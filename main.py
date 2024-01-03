ce833603cf062865b13b0bc232521d72fb2686ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:56:11 +0000

    well shallow behind downstairs spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
