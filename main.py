711aaa125671d086e3b07b1a3880ec737cefbd2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:16:32 +0000

    whom off believe silent operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
