3631438d3da745c40afd01748636f2c5a584a02c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:44:42 +0000

    no dick between pen lend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
