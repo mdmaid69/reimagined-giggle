f870629e4d46dc7b08d90df4f087cde9b922b5a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:14:18 +0000

    bush various embarrassing account still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
