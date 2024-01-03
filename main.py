2c8447c9460186aeec6320bc2a3404d0e79cbeee
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:15:39 +0000

    forever work contribute certain weigh

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
