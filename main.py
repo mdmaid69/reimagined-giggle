d108758057905db49ce4a4ab6c5573092ecc778b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:02 +0000

    of average turnaway arrangement share

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import numpy as np
+print(np.array([1, 2, 3]))
+def sort_numbers(numbers):
+        return sorted(numbers)
