a8bd1ec268f807d0c9a6e50777245cb03cf68f76
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:15:03 +0000

    wish style dry dance invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
