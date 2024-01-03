e49053ef70a3f469aa3f449ebf26eda1cff6c97a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:36:45 +0000

    vanish upset comment impatient talkback

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
