4c18927897cac83fee341e5d938050dead19e1b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:20:23 +0000

    building calmly either bring excited

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_max(numbers):
-        return max(numbers)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
