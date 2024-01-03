b00ddc27e4a8c0637ed5977006a0a5e075e8ddeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:38:17 +0000

    work clue rarely ground town

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
