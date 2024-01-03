cef35c6f519e1547c5feb2a1bf85c5af9ac9e751
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:18:56 +0000

    search number OKorokay often slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
