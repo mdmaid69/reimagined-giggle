b2587c4cb3c27118adb4184bfccda9f71a8c2062
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:26:06 +0000

    turndown open chief except right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
