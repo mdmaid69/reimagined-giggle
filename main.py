bcd217b2f9a3c45b8f6eba55520308f2ffda5381
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:33 +0000

    various area date wealth delight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
