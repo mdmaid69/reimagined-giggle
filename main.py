590be9ffa27222b9e997437c6795195d8b470d28
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:51:10 +0000

    off closely getout surprising moment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
