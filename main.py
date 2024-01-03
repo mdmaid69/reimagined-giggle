159d04f355fc198f3f11cf78a76782f17942040c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:27:27 +0000

    passage present building I share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
