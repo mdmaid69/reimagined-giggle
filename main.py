e1d8a9be51485711c948996fb6e8696f2e5b8991
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:16:21 +0000

    treatment wrap interfere tough light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
