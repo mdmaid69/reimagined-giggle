e69d95dd148291655937eb18daaea5f986319e3e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:18:08 +0000

    guess balance joke tall 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
