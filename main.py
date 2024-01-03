1df12e6b1589e81f66a5591c5f7304ab86a1893e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:42:39 +0000

    around sorry man dead persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def count_elements(lst):
+        return len(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
