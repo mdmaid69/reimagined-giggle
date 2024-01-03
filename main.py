cd43bd1d4323ff38f6a5b7617ae5f7760f4ad344
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:48:51 +0000

    573entertainment alive children choose target

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_current_directory():
+        return os.getcwd()
