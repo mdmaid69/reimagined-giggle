99e02182bd318f59515b3e4d9b107c060af5f1c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:06:26 +0000

    bound although other cruel open

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
