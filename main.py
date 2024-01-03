4a7b12318901924a59a69d9dbced047f6165e7eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:11:18 +0000

    conversation block confused forever care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
