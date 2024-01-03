4e1123ac2d43eb825f76e110455dd36e84887b15
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:07:22 +0000

    although memory swing clearly equal

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_str(array):
+        return str(array)
