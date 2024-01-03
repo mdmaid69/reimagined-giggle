e3305e898a40fde39ab4ac04295f14f99104f126
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:40 +0000

    turnup safe failure prevent half

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
