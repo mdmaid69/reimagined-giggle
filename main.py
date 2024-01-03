637bec4bc71b3d62264868df41cad5503d335ca7
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:40:36 +0000

    pause thanks computer call difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
