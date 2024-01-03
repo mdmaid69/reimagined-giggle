d4172adeeaac72983553f2d5a2ced367ddff3832
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:40:27 +0000

    similar promise time repeat car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
