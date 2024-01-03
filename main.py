41fd7ef30364ba43a8cf65f46f7d8c20c03b6ef2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:26 +0000

    accidental agreement dear suit blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
