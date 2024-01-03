91d8349ef9407a021021ef7ce1f97bbc3e6c7d30
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:11:10 +0000

    speak again task second giveback

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
