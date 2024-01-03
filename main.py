08beb3ec3b1e9955074a2517099bcaf32f56efca
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:19:25 +0000

    persuade talkback spill brother breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
