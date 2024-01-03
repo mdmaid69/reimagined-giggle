42516731988b56284a62698dcf562707894c9e96
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:26:44 +0000

    pay poor little Oo runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
