31a0c3ae02de1ccbaa5c9e0f5bbf407b94b48ab5
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:26:23 +0000

    knock herself worth spray transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def set_array_item(array, i, item):
+        array[i] = item
+text = "Hello, world!"
+print("Words:", len(text.split()))
