58eba81704c08a6b2d6801b9d6b21b67c78955e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:08:47 +0000

    sort stranger run persuade maximum

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+print("Hello, world!")
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
