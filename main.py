77d9c4a96d2eaa5f93ba64f993149c32e58e5054
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:58:08 +0000

    clothes sentence laugh circumstance side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import time
+print(time.time())
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
