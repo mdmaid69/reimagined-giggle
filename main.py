dadfc39b994ba92f14feef7857abcc8a3fe04cc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:34:35 +0000

    climb wrong desperate direct breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_octal(n):
-        return oct(n)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
