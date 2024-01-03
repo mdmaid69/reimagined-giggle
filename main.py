bea193486d11232cf5da900dce9b80c7a67b0166
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:47:55 +0000

    waste section interested practise standard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
