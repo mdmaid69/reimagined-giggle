1efb6ba5c95845d40ef4f22848aa1adc17433d9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:14:17 +0000

    Hh float press roll expression

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
