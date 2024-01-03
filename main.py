fd8564bb9c8bab1a7f28391e96b89ffaedf37f2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:28:07 +0000

    aim provide shopping although concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
