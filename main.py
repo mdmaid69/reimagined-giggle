a63a70f6a9f7fe9fa74fc76b1f7abd12731ad54e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:48:19 +0000

    famous value exploration target field

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
