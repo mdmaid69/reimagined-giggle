1a78f569f12811749474e4022344ea06d5e97317
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:54:00 +0000

    talk bound hurry call off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import sys
+def exit_program():
+        sys.exit()
