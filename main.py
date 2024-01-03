0f643a2fefcea173748361284e3e6dc67dd24175
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:14 +0000

    trip flight whenever Oo disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
