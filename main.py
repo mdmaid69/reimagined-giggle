e3bf71fd10ee4d7b23cfe6170277d941dca4358c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:36:57 +0000

    operation generally equipment room love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
