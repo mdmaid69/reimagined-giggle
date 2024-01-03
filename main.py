919e37d7c83abf6db8f1aa8a604180a4d5caa1e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:04 +0000

    lead drop sharp teach importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def reverse_string(s):
+        return s[::-1]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
