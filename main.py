e406fc234ace0b883d2be86de6583d86a415a7e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:13:53 +0000

    contrast in evening wheel die

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
