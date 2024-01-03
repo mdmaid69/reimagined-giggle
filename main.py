90b4b9892ad3e5288cf196777dc7d66f897aa465
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:28:23 +0000

    display good listenfor Ww sweep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
