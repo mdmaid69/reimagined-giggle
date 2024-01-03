70514e51d314014183b076b226c2c526f1c63fed
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:11:23 +0000

    birthday birthday getoff list border

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
