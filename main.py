e1f51f96977f9259613d00d88c81c48560143f17
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:26:20 +0000

    square discussion favourite accept plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
