4730f298478a017721ef5f54a867f0da3cd36576
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:21:56 +0000

    accept store plastic road ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
