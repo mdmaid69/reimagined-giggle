3749863c01405bf7999f659dd999d82e590346bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:01:24 +0000

    theirs sticky press content plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_speed(distance, time):
+        return distance / time
