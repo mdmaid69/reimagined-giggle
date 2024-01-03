51b091852b57cba1a1e3391759e3744c5f42dc52
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:10:11 +0000

    force regret cover occasional rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
