ed99c9ba895ac7147080cc8e0542ed11772276b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:19:15 +0000

    wealth finish takeaway affect activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
