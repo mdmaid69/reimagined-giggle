5753af02916bca58922b045ddb1b7a85712b738d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:19:28 +0000

    second main morning base Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
