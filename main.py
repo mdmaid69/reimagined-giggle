303b482b8e563a9587767ae53959598fafaf4d27
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:49:57 +0000

    meanwhile limit avoid journey urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
