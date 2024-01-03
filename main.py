9eadbfe581c590f8188c906e0a7db7b814cd37ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:14:13 +0000

    serious store principle dance birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
