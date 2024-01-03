c21c240805ed875d80bed81dfcbf708d3c3a419c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:27:17 +0000

    light final regret report artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
