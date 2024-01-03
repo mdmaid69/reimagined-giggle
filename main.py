a7c7e8bbcc0cbe3226817723274035da89d7bc93
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:30:04 +0000

    stamp tour offer famous characteristic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
