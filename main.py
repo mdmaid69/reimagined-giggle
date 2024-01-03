723e01a00df9d293ee547f40a712767890862118
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:30:29 +0000

    fault professional calm squash comeround

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def is_odd(n):
-        return n % 2 != 0
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
