ee1d6f08f61a0aba4fa42ef3ec9eabf354a6d0eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:53:57 +0000

    translate embarrassing noisy entry attempt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
