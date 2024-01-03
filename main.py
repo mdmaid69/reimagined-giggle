a439cbf6a807c964a6e54cc591b385876000224f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:47:11 +0000

    feature pronunciation previous broken comfortable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
