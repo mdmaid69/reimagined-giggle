a61c331da090c6a95bc3650d1106f849fb7c3965
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:58:04 +0000

    ought relief cross threaten narrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
