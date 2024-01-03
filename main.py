1f2d99cce7e15e7ae7fb0bb4dfaa477e352b6ec8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:06:48 +0000

    journey quality picture moral arrangement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
