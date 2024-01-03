56a79fb490b76783147d5a3ddf3b7dd96128052e
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:27:40 +0000

    get refer may pretend destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
