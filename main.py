e612d4f106182a8ccec57fdeed4f6debdc7e7c9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:17:14 +0000

    strength failure wander shrink search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import os
+print(os.getcwd())
