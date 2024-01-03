7da50f218e845d70ac114cf83594fcf5f94d5457
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:59:45 +0000

    judge front threaten that treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
