040604c071e53e2d590e61948a01e00d6dde1a69
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:36:58 +0000

    plant what through crowd university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def convert_to_hex(n):
-        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
