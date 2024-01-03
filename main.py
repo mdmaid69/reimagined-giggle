502e55d9a4b2b0210d61e78324e422ac530fdd7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:19:29 +0000

    opposite smart who failure disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
