63e515696b107262ce69c9b56f6c7e669ad97beb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:49:33 +0000

    part die may crowded supply

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import json
+print(json.dumps({"name": "John", "age": 30}))
