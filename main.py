b587dd84e34cec2f6b3e38bd1ea89c36c84f04b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:01:54 +0000

    fill shape hole none mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def convert_to_hex(n):
+        return hex(n)
