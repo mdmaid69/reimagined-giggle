17ec0b25d5f02ccc99e790ee28ed3862b4207075
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:34:52 +0000

    trick precise sick through stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
