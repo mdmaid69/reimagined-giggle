5862a499fc098990042f5f0ec77c58fdae59234a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:41:13 +0000

    area offence dear ourselves responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
