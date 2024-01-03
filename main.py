de8d718a0d70c90fdf244e27b5459f6f0cc6612e
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:18:27 +0000

    mix duty add giveup keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def multiply_numbers(x, y):
-        return x * y
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
