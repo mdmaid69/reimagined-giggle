0e54a448b00704f90bf0fe269d90119151639225
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:02:47 +0000

    mouth issue dig means worst

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
