178ce3135b0654dbcb500a01b8c37b55418575ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:50:08 +0000

    tap ago cheap circumstance closed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
