dc7c56fa801a1b8ed7b83e31900e3046b967d3be
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:01:12 +0000

    picture breath race touch lay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
