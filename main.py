3154c73b9d55f896c9a5eaa23206f1ca58c15836
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:22:06 +0000

    equal already terrible likely air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_palindrome(s):
-        return s == s[::-1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_max(numbers):
+        return max(numbers)
