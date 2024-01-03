3dfe81d416af07945b98b58edd9cc852c2ce269d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:51 +0000

    slice cough connection edge frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
