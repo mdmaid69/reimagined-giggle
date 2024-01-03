82e2bef415af84f7ecac77fc9413dc501af0aac1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:02:39 +0000

    polite thought sorry accidentally exist

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
