3cffaa6c88d1a537492b3ff90d13d69ce7767ffd
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:02:04 +0000

    good mess extraordinary blame wash

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
