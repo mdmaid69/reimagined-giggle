826fd344568c8c1e40ba811029132cedde37c6ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:59:04 +0000

    action maybe pretty bound crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
