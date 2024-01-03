c0e52234ff2a77f860c9fd06b7d090039d4e54a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:24:54 +0000

    yes thanks shut prevent loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
