679549f53b14e24ce99f2f3001ef4c71aa22fbb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:53:37 +0000

    compare thank intelligent total sorry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
