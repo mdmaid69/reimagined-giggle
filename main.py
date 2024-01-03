76ed20432fb8c990567e17ae95114731ca3c663c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:03:38 +0000

    concerned can season race harm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def cube_number(x):
+        return x**3
