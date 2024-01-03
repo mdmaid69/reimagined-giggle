190e5412f2d20ba4c74b34450a0e91078e7751a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:20 +0000

    future attractive wonder demand mend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_speed(distance, time):
+        return distance / time
