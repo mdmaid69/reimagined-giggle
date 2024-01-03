fdd455d1ac0509d4eac5ea50f2ef29d83afa3341
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:45:02 +0000

    broad person game home recent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
