b6ae2faffbbbf2a3fbde8d897e100ccdd2fd4c08
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:21:34 +0000

    payment actual turn promise rush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
