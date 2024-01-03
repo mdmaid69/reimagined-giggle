f55906bcfb5984ef75cc53a4d019852716790db3
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:58:23 +0000

    consist picture know say bringback

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-for i in range(10): print(i)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
