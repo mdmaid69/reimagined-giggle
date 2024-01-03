595a91216dca1e0f5e1c69042f8851430471e2f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:19:51 +0000

    basically anybody turnon extremely season

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
