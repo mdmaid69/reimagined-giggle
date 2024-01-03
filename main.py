8776b04195564bff0974c1997a1a95f1cb6fe05e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:45:54 +0000

    long smooth window round apart

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
