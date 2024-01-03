cea660941e4d2b894e7e3ab088f66323e112f129
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:08:44 +0000

    but miss by to workon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
