b16f735f62e721987864d7efb7e692f6c579e5c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:06 +0000

    minimum container wise meaning swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
