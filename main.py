bf7e2d6f7368e74b8168ea1571a70246d34c3c2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:50:19 +0000

    telephone single range bring big

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_density(mass, volume):
+        return mass / volume
