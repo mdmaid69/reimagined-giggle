a17a1a07dbe47890e94a603aee7239549026885c
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:53:00 +0000

    copy force strange matter pretty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
   def get_directory_name(path):
         return os.path.dirname(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
