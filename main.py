6f568737ab68afc5af2a533885a7ce9f154615a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:04:58 +0000

    answer release standard close breathe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
+def convert_to_octal(n):
+        return oct(n)
