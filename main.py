1a37b59446b51a021870edf75bff2159b688980a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:14:22 +0000

    except Gg to borrow judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
