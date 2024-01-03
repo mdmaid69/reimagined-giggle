2a5a5ddc6352255314a9c58e408ce57af1372d72
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:00:30 +0000

    image keep pleased perhaps kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
