f097aafe25ee6198028cf05fe65bdb111c84ce2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:01:16 +0000

    computer challenge delicate abroad makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
