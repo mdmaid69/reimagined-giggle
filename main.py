a94bf7c268539b28be20689d5fbaf4341fc1ca4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:37:37 +0000

    succeed attract season wide trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
