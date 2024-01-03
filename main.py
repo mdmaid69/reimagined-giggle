10cf318dc9662c3fdef7b2115c4daead6900a41e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:21:15 +0000

    scream giveout ilovedicks means possibility

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
