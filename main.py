8ea129ea254b2843013d854bae821e0f7345f6ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:02:43 +0000

    pressure inside bill hour repair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_to_binary(n):
+        return bin(n)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
