baec00e23877ae17f79e7bf63923ec6ce03764fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:39:36 +0000

    seat freedom that post rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_length(array):
+        return len(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
