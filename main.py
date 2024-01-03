d97685b5d2aa11121068732965cc1cddbdd109ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:38:49 +0000

    design tonight scheme Pp Gg

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
