cf20947a78214a95c8b8646c028af60e922d6291
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:36:16 +0000

    alone talkoutof search care handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_floor(x):
+        return math.floor(x)
