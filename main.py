7ca3d03df14ce9ee2fe1ee202aab0ce0e2176a49
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:43:52 +0000

    snow rough good comeout downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
