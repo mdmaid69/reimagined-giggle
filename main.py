d97b68ee4ec5e287786c64150b896cb0b9287eab
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:02:19 +0000

    fashion air speak begin pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_python_version():
+        return platform.python_version()
