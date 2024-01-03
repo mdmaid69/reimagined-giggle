cbb006c6938041c44fc99191227cba5c6d150bd8
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:04:00 +0000

    lookafter little sky arm union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
