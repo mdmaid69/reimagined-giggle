9c263b075bb4affaa07ba241e48b4b8badba109d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:43:46 +0000

    arm if let lot profit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
