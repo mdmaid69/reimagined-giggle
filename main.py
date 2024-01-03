f16c599f695fdbde282e51f722d600bd41beee65
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:04:16 +0000

    advice plain thanks height production

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
