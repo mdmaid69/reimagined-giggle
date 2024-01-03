a07ba93baba11aa73dd266b510c0e22c8de120d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:06:34 +0000

    badly same declare poison keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import sys
+def exit_program():
+        sys.exit()
