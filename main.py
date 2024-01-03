98a2039aaae02296a319a0be19082fa3bf8d9d9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:42 +0000

    confident sing congratulations should report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
