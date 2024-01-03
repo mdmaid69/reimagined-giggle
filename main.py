931a5b15fc44711238bacfd905c7f1b9ae7fafee
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:08:56 +0000

    inform old plant mess rob

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
