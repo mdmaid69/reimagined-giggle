0b31b05fa2931cf7760445f395ed13de74aa68d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:21:29 +0000

    against space whichever interview judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def calculate_power(work, time):
         return work / time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
