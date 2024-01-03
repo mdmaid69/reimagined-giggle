f6efec5f7fb780aad235e094f92c598e6597abd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:04:00 +0000

    brush expensive whisper limit paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
