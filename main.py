735759b05e082a929e3702d58b78a9a369962795
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:47:46 +0000

    callat innocent thought disappointed bar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import sys
-print(sys.version)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
