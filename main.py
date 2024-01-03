274519dd4511d3494ce876a9083f5330d934194a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:42:47 +0000

    offer dick track sink finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
