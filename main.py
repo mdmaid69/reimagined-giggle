215e71d1b0997f5d9114246640c1cf74240c213e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:16:09 +0000

    kind watch theirs where entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
