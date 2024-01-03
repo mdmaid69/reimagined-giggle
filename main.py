ecbddf5f164c55dbcaba5bc8adf3b4f48a1550b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:48:53 +0000

    lock mention block quit aim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def reverse_string(s):
-        return s[::-1]
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
