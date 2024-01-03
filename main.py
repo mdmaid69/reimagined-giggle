514135184802077237b116effb525c1d609ea751
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:26:19 +0000

    protection again powerful recent earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        print(sys.version)
