5bddff8c13f050bac39c52254e866c430e378b41
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:06 +0000

    neck so soft finally Itself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
