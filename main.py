9874c647b3bc64e9c9e57990080dfd0b203c7857
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:12:25 +0000

    mine want marry fault own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
