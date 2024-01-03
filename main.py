8c8b0416fe10208e55a647215db990c072fc2baf
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:13:38 +0000

    forgive careless few corner pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
