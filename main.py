b3b7893fc6d174e874a6b5481210a203b6e58985
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:45:57 +0000

    anywhere step boring run wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
