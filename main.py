f419d7ed1556c677e77a86c2cc47b3a01ea5931d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:36:54 +0000

    compare regularly getoff tight of

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
