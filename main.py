0ea635d26da3a0217346dd03df60fc35afa136ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:20:59 +0000

    wish pollution target regional expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
