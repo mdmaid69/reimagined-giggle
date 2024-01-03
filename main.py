4f8d1c1770ae6824f2e518d21e3d05048a9376b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:41:18 +0000

    development again represent who father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
