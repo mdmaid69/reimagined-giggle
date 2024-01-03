8427166c917ea7bd679c85fcd6492bf6a32f29f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:24:28 +0000

    fork deliberate even interruption nonsense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
