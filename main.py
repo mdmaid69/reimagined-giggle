23363b8b2c89cd7b944580bb0009f5e5a35efc40
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:38:14 +0000

    honest pencil if bridge display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
