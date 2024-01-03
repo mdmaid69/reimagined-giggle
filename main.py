ad808b1140e80b3b14eac925b9ca5556dbc4553d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:51:03 +0000

    interest read keepup possibly basis

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
