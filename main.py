bcdee6200ab6de920bddfc05658b4ceeb250965d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:07:02 +0000

    friendly appearance cover spring undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
