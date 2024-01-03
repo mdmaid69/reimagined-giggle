486daf0bb4edab62aee9055927106ae90a0d052d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:04:28 +0000

    attitude in reduce 568energy brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
