f90251a1f11b44a87c584bf710e09f451b650deb
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:47:28 +0000

    here petrol how on sentence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import datetime
+def get_today_date():
+        return datetime.date.today()
