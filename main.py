c3f494f731d28242b78565c5e03579f943b6951e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:08:23 +0000

    government old brush disagree kill

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print("Hello, world!")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
