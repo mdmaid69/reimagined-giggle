e224167cbfee47b72acc045e05102102a9d9a9ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:22:20 +0000

    draw middle single sometimes apart

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
