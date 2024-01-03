9f06cc649dc11503d4825ecae24d596d8886a187
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:35:15 +0000

    strike polite mend sticky date

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
