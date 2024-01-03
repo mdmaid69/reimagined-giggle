12136c4004bc98c093eb5876f6a48dddb1c6dac4
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:14:36 +0000

    expression word start honest better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
