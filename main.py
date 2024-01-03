ff2e8eb94225888c3f35095d6ee68d55404c4c5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:15:28 +0000

    match Ss hot dance apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import re
+print(re.match("h.*o", "hello world"))
