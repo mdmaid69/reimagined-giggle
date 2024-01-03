bbf76441d3b8b8c404a49f6b7e55b5ab30a9353a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:12:53 +0000

    create adjective none 572enter break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_index(array, item):
+        return array.index(item)
