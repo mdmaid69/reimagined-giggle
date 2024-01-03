5343bc0ec3e4a1d18d18cf63da2dd496e3300b94
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:02:21 +0000

    attract pour pipe store content

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
