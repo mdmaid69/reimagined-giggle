eafd360ce91ea9bd3a629c840b5c72c6526fb6bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:49:38 +0000

    shut bang proof temper world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
