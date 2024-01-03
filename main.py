52b3642b863fa05e7fff4a0bb3c516ece8732fb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:09:58 +0000

    sport choose dress goodbye scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
