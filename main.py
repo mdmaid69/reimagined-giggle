743826469fa847210a909de486c0f1a177d8a1eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:38:35 +0000

    plenty empty slip during anywayanyhow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
