46dcad410bbca82df3eb02dcd3c1dcf247539eba
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:12:49 +0000

    on prize moment bringup again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_as_set(array):
+        return set(array)
