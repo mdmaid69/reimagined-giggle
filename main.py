8e5049213441a8021d7145b6e339069f7c1adc6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:20:08 +0000

    bye extremely listenfor so instant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
