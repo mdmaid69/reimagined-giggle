1592f19f4675c956987e42353011ba8b99444927
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:26:14 +0000

    swimming wear year 179bite passage

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-print([x**2 for x in range(10)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
