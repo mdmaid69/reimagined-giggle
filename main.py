d5b51e6cf7c3378ce40876e0b2bf9cc0448beafa
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:51:50 +0000

    turninto hand fold apart rest

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
