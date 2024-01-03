15a2df67e6a2a8a62e80c50e461260aae3a40914
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:38:06 +0000

    central raise objection check understand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
