48c5e30c98482a434bbebd25f19e68097ff10a3f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:24:10 +0000

    doubt stone finish official sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
