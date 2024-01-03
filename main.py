22a1ee1c7dc3dadd5a8769be1f1467982bf819cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:29:23 +0000

    spill realize start clean bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
