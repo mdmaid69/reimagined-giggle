20acdba98e5b1c8799d1a8ea777831f514488a39
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:39:09 +0000

    university evidence command unit think

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
