61b01bacb01632c0d12d4ac1694a20f64f431043
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:16:42 +0000

    help our hand manage today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
