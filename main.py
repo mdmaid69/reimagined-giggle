28f235a80b5b33d774d59b9da7330b70ffe1aacc
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:55:13 +0000

    manner hold hear right arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_length(array):
+        return len(array)
