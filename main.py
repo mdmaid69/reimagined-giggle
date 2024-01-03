f4a226e191bcb42610336bc6ff44c621f69129a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:15:03 +0000

    teach attitude attractive slow method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
