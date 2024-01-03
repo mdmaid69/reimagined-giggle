7e8160c2986298c3eccdae9105afdab3d994425e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:39:09 +0000

    recognize slip compare act traffic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
