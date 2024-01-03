90bafad449e2173494e34cbdc02e8c5c182ffc0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:54:29 +0000

    hurry niggaacademy standard speak test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_sine(x):
+        return math.sin(x)
