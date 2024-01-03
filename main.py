baf728a5f845c279fe386509817ee45c15dd56c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:29:50 +0000

    Ff manage deliberate sensible window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def count_characters(sentence):
+        return len(sentence)
