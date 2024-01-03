b3f5443f29744b01e692e8558e6564cab8d3a912
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:49:01 +0000

    comein legal market else police

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
