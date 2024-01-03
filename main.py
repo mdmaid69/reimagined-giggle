e7756c4d39f101c00b77acefd3523f7cbe04b8bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:12:21 +0000

    study live separate cap stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
