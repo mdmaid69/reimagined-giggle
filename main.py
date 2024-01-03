7468865d240b9b4aeabfa109880bba27d6d31ec8
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:28:46 +0000

    place who that ignore switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
