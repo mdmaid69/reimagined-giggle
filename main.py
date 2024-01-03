2a2149be137f6649006090c57a748c78834d3062
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:41:54 +0000

    considering close satisfactory correct unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def convert_to_hex(n):
+        return hex(n)
