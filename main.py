19e505328281a89372438f32aa4aeaa56efad2b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:10:55 +0000

    row dance weak search everyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
