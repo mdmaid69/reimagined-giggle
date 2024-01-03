f258f5fc296fd465df96d59979a6cd06ee60df54
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:01:45 +0000

    yes approve laugh yet physical

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
