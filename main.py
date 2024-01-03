91745ca430fe850db5b4a16438064f8becf7fbc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:07:00 +0000

    matter neck movement dig throughout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_binary(n):
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def convert_to_binary(n):
         return bin(n)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
