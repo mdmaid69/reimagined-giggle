dbc452c34bae4fcf4428e96d2b54f59fdf7ea1e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:58:29 +0000

    it turn easy tone 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import os
+print(os.getcwd())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
