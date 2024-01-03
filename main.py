1b0f4c609ade3aecabeea0b8757848bd09196879
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:46:05 +0000

    makeup century spring container swimming

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import time
+print(time.time())
