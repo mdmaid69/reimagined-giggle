a605f678ca4afd6200161abeb0be866660b4a0c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:50 +0000

    birthday fast stick uncomfortable prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
