d93c2999898a5f165f217a952ed220c322bf3dbc
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:06:22 +0000

    collect find arrangement in comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
