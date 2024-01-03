ad91dc880d65053eb59af56084f9a10d54d437fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:03:21 +0000

    shake deep praise discover slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
