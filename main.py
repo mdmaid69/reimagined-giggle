fbfa63016f809c38cb3db9ba5efcffbf6405a880
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:24:16 +0000

    also left matter confusing operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
