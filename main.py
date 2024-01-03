9058475176a02a337d9ca9f5194bfae8138a64c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:32:30 +0000

    allow city insist draw 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
