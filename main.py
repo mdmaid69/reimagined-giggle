4cb344ea4bc35e9b1fd7dfef0301cebff1ade78b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:58:53 +0000

    cook other shame press congratulations

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
