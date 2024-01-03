0b36451cc5c6d62b9677fc3c7f13e1e035ff97e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:10:39 +0000

    independent respect bother method furniture

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import time
+def get_current_time():
+        return time.ctime()
