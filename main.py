2c6ff7966fe98b30b68a3b1ca4f42939f53bf25a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:40:35 +0000

    passage takeover neck may miss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
