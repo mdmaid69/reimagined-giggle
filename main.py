902c162fa329eb093355bb6986564764f925dce3
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:02:52 +0000

    sail on touch trip brain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def count_characters(sentence):
+        return len(sentence)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
