7035b1e7e9f09099b66a2fdac7adf777d60d2678
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:20:55 +0000

    off but question dig along

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def convert_to_hex(n):
-        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
