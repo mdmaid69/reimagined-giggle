6662019a076e0d0338bb1f26c87a149d29249142
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:36:41 +0000

    continue actual especially inside suffer

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
