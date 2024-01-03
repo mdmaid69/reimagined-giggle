53994c8d64bc19ad1667d1a916a413c0aeadd8e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:44 +0000

    building exhibition plastic where idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import collections
-def create_queue():
-        return collections.deque()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
