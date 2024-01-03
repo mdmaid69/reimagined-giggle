5d16161baeab404bf796fb8ffdac326b6e75d365
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:31:28 +0000

    advice load expensive fasten around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
