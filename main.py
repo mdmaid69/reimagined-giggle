a9cc9659d71c5328e30f720259226d597e2627fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:01:17 +0000

    surface present feature anyone himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
