c01306ffe931d2856ba57c8c85cb6253912a92b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:37 +0000

    makeup sing equal holiday big

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
