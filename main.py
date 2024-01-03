9322f64490ecd7def80bffc72cfe61446d872ef3
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:07:30 +0000

    comeround combine leg round satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def reverse_list(lst):
+        return lst[::-1]
