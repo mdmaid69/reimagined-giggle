ea409b7deccdd85c0b70ab0fdeb8e193cfe73389
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:07:30 +0000

    flat break belief persuade design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
