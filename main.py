b3f0281fb7ed3c6f8e086932b40fff80f3e23b3e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:59:18 +0000

    rid programme cross quit present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
