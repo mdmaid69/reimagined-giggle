97ff9f1429d032f19ca8b79fac530f68bc657e4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:29:04 +0000

    door attempt schedule burn squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
