75bce7949fc92263d24190f789d081f612b8e33a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:47:58 +0000

    cheap light shut marry suckme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
