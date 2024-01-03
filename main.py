1df7227df612fda3b4d345efc39485c8aeb70963
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:58 +0000

    frame burn excuse length right

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
