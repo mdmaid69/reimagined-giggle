c9b92c9689cfe146659f24e0f3beacf2737260ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:26:51 +0000

    usual whole every goout suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
