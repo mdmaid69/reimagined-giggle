17867e33525e4ace2012ed928f25d1ae5481322f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:13:12 +0000

    surprised best divide cough needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
