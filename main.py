47acee5e3640c2e91b44cde12599066d20be2861
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:20:22 +0000

    dream cost although various break

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
