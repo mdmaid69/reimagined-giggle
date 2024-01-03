c8896bc873430a3447b4e23a7c5a201e9aff7736
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:59:44 +0000

    remain distant Mm next phone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
