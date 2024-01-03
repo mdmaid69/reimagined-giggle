fb98e52f3b544d336c7c88279c6d3abfd8e9d16c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:59:12 +0000

    wet breakout himself Ee correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
