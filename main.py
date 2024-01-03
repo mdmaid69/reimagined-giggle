ccf016140e2d10164c465da50485720dfd234b5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:32:28 +0000

    cry unlikely turnaway seriously patience

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
