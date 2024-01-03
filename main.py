ac8838345ec51f2c89e8e0fb2f4d3cb31b7fecd4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:20:06 +0000

    uniform cause prove game build

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
