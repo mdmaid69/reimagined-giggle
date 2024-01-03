62b9913df1ee8e6b2899b5985025b296a410890a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:12:16 +0000

    much onto sting reserve file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_octal(n):
+        return oct(n)
