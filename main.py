c2c45540f42a9e092a2a54854e7acd9b175fdd17
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:39:23 +0000

    favourite deliberate petrol mistake especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+print(math.pi)
