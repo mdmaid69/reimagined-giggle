971309f167e27b2fc39c718abf56bacdc7a835d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:58:31 +0000

    score accidental walk mouth previous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 print(os.getcwd())
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
