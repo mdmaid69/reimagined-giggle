0f5b381ae55364b31ff21c4ff817138decacf694
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:54:22 +0000

    within fall everyone refer face

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
