6302df7fd13ec7eaf371fb8eff88ab801976cba7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:43:52 +0000

    constantly memory less illness fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
