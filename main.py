40770fe0e111cacae4abebd8b58a9d3a905eca22
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:04:46 +0000

    free transport 566end sure lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
