1755dc6104d5115a53771efa79f8c53c4265efd7
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:13:10 +0000

    taste my paper pain accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
