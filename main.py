9279b8ccdff57cb2c5899d7da8e3f43bdcece72d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:09:35 +0000

    division wait match Ff sensible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
