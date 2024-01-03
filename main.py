a17d41fd7c834bfc86719d858a95bee94858d777
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:32:34 +0000

    shape major past organization drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
