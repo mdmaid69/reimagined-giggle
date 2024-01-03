dd12e8aa0129941611e6d20192e49a8734de2215
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:16:42 +0000

    smile club fond brain afford

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
