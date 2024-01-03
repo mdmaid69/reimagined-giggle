7a6838d96ed2ad48da6947ddc4e2dcc1fbb4da8f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:32:08 +0000

    breakinto tear miss Kk branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
