0f2149b3b7756e45c6885de913219e237da47d7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:31:40 +0000

    create well building estimate always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_str(array):
+        return str(array)
