ad532bd15bf534f6b7c1c0b4a6babf620556d25f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:22:36 +0000

    phone beat direct combination jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
