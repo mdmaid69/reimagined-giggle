22f688cc7de2f433ed6faba5bc5a765dae6bdba1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:17 +0000

    love beside exchange post offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
