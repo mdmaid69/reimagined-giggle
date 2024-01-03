d1e85a30d3032b57129a3ba9bc7e7067c5afa481
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:05:48 +0000

    advanced public check stop actual

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
