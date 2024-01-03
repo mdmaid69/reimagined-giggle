1d0aeb9a48adbf7cf98c9d29fd18bd9cf1592767
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:05:36 +0000

    frame prepare mention thin comment

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
