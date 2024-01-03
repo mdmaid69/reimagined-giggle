e111478c3ee4c5d9f53c4f087c53a097de359dd3
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:13:43 +0000

    port turnover reduction worth street

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
