cb02f643ca403bbb52b0349935fee5aa6a0786b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:11:27 +0000

    as fit since show lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def reverse_list(lst):
+        return lst[::-1]
