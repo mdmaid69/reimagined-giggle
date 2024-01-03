3c629a24eb1d903699dd58dd19385fe821512b94
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:39 +0000

    now land mass fine temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
