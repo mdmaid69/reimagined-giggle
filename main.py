edebbbef657fb77e323c8551518d08f9ec48ca8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:40:33 +0000

    safe split brown entry hope

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_float(array):
+        return float(array[0])
