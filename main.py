b2d999b6d50e48867b551756c8d070a839e79797
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:38:30 +0000

    shout model round few tidy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
