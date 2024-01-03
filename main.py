5608d29b032a0769b29373949ff9d1c6fbfffccb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:00:24 +0000

    whistle spin goback(to) entrance of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sys
+  def get_python_version():
+        return sys.version
