d73dfbc2ab7e9b9646984eb7fe96dd8a0f4f7a85
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:58:27 +0000

    available head born simple sweet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
