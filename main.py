592e1d6ded2eaff1d03efab4ba3d98ff099e4d83
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:47:20 +0000

    instrument life fasten test improvement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
