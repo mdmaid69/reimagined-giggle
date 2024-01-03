71cbe8266e47250b5dae8d8e1388f011079ae911
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:06:43 +0000

    passage sell mail scale will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
