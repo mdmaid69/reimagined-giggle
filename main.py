8013388c16ed754c67b503d551a3c7c533a43d93
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:19:18 +0000

    upwards explain store coat breath

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import platform
-def get_os_info():
-        return platform.uname()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
