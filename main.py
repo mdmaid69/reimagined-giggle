766a0791d1897f65626237f74cd56a62278c8a6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:04:23 +0000

    through pure total him swallow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
