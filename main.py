59f91bf497ed981e7d963f25876921e40f699f8c
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:41:56 +0000

    morning flow high ordinary pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
