98e6cb052037a7aa979aa2e3f1eb584f5576ef56
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:37:21 +0000

    fierce mistake mind Pp impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
