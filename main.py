1615395bcf3d6977e0653e2100282918ecdde3e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:22:17 +0000

    disgusting turndown protect important lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
