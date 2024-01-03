144195f9c8bbc27c91ad8dd87c7e72eb7b97034c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:43:02 +0000

    birthday join cook whatever negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
 import array
-def get_array_typecode(array):
-        return array.typecode
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
