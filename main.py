45e8c8ff95e4ac573722d4d5a64d611e5ccca65e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:28:32 +0000

    ask 573entertainment calloff wrong under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+def find_max(lst):
+        return max(lst)
