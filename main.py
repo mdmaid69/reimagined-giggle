73b4f03ae8c65c21d7ab2acb12b43c20e2c2a8e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:14:38 +0000

    answer shout business turnaway after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
