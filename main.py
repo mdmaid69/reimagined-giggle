25f38968ca9e1a111721a3924c295c2db7038eb0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:46:52 +0000

    fit recently both bridge dangerous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
