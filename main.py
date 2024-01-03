0580a9f3cf4a6be6253e104c8121f6dc0ff22b01
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:14:41 +0000

    passenger grateful pressure avoid left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
