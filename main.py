a3247bff5673c800f9622c081268b270b80dc8fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:00:28 +0000

    safe welcome brain lookback(on) serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
