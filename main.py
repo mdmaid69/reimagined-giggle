e7b5bb2f630a937703407020db207bea90284604
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:50:22 +0000

    fun foreign deep door command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
