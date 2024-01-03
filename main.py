40d99ccf7cd5d0bfa1ba8d0ee565a7bb2459d64f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:19:52 +0000

    uniform regret file even about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
