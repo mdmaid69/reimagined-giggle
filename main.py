64609ed3a42c765a25816a78c7fbd9c1c9ca54c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:47:13 +0000

    decision frame shock disagree by

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
