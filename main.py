9dbc88c42b2b2967e2cac2a7909a0d880f32416e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:37:08 +0000

    cause about sort scare picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def find_max(numbers):
-        return max(numbers)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import os
+def change_working_directory(path):
+        os.chdir(path)
