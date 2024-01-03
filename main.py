9e334cd5e6f3aae19c189632711ece9dc09ce5ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:13:09 +0000

    freeze visit passenger luck surface

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
