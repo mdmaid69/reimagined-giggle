2be436b1b9b278b0c1d6ae66a4ee885ecc8aa9cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:44:42 +0000

    Bb in ticket quiet exchange

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
