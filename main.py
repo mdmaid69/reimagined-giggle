2a355f6472cdcf10ff8f068ae009b8f60e81d913
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:58:35 +0000

    intend joke correct turnover under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
