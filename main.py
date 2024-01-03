e2f72b413f9c39652def5f3db55e5875468ad737
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:04:19 +0000

    wrong convinced hot present concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
