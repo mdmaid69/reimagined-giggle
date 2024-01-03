8a9a00aeadccc8a93e70cd82cea26f808751684b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:19 +0000

    belief pleasant risk choice ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def subtract_numbers(x, y):
+        return x - y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
