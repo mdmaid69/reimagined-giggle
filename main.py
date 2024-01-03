c818d744cfef669cb2677715de7036715ad03e79
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:08:27 +0000

    holiday error payment ride choose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
