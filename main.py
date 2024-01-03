0a36936690cdcbcfb78b69090c126960605ccbc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:53:11 +0000

    stupid drink smoke law Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
