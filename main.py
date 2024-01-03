6273b73505bb2c07925b08c96bf22ec6aa96d8bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:32:14 +0000

    if shake reply somehow hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_acceleration(speed, time):
+        return speed / time
