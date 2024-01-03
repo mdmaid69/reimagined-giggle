b9ed39c78cc292e548601d540949fa05711e855f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:38:33 +0000

    drive film turn method like

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
