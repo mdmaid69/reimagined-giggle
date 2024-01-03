ac24de03de23e987eb7a6d1ba0f5dc30d1bf07c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:15:58 +0000

    model operation holiday spring television

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 print(tf.__version__)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
