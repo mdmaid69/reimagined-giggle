a56d61a86bcb8998dee18543483cb64b689ef561
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:17:10 +0000

    suck complete mend convincing finger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import sys
+def print_python_version():
+        print(sys.version)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
