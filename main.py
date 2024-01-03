5e74a0b3edb14c853e3ec88ab0100e7052c5cf52
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:03:37 +0000

    dream burst general trust bye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def find_max(lst):
+        return max(lst)
