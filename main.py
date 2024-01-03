2bd7bcafaef1e8fa0150c0d74e9d934267e84a22
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:18:37 +0000

    whole determined uponon willing everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
