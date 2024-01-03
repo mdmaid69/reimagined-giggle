e9b28ee9018e3598830771139503aadb946eab77
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:41:44 +0000

    announce play blame sensible luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
