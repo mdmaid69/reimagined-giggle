1ca159886368d1157bec3c285602a31b8fc21e5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:56:50 +0000

    slip label improvement course hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_density(mass, volume):
+        return mass / volume
