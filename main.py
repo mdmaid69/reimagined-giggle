c66a9e7ae14c912bb4a176053db59f4598220e4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:36:44 +0000

    spare cook plenty confuse moment

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+import unittest
 
-{body}")
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_item(array, i):
+        return array[i]
