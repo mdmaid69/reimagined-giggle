1233525db90ef798ebbaefcf7e54332f66affc32
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:51:39 +0000

    voice dress close satisfactory interruption

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
