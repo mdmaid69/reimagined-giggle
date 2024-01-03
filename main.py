3139f491d70aaaf95594d1b12a6d43a6b63ca7e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:59:01 +0000

    mixture progress tongue sky swim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+print("Powers of 2:", [2**x for x in range(n)])
