98356f112bb093cbe85aada8326818a529bb2a20
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:52:57 +0000

    language earth benefit shrink meet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
