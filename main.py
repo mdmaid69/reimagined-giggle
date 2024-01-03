fa507e9f3f731ca53b509a66029b11f3f5bf7f09
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:52:18 +0000

    word smile from excellent lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-for i in range(5):
-        print(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
