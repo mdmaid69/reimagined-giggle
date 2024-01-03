f5943f17a5118c4314e0506c2ff5890df5bfa369
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:20:01 +0000

    slip repeat underneath fetch his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
