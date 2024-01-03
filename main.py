97b963b0f4567f69b6ff13abe6e3e0c933de9b0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:37:04 +0000

    able doubt character centre allow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
