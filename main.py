e2cdbd382fab15207fca05152b2d3b6836d7c00b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:53:55 +0000

    public cry case tread cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
