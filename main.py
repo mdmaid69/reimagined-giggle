2456c25db228ce43f640b108dbd48eb23f672b76
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:37:32 +0000

    offend business admit before Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
