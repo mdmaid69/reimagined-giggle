72a191fbcfe3a182592841e94e4e8bdc882411ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:36:28 +0000

    health control temperature nerve emotion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
