55d12ff0a4da55d93ec8abdbcd99ed5a315e22f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:44:05 +0000

    Jj discussion whatever as professional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print([x**2 for x in range(10)])
