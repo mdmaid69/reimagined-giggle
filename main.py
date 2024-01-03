f2e564b5b8a3facec265235e640c68a771706457
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:34:58 +0000

    pause small separate justice sign

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
