9ab2287ba4ccb09c02510a5bab19d88d7b88eb4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:36:34 +0000

    grateful convinced passage drawing deaf

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_list(array):
+        return list(array)
