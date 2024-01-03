fae4b98a02c26adbd248484aeee42dbdc85e90ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:59:05 +0000

    major crash bottom lead bath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
