a644b80236a827e9cb6afd9c17d27cb0223b30f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:45:03 +0000

    afford study Ll congratulations keepaway

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-print(re.match("h.*o", "hello world"))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_min(lst):
+        return min(lst)
