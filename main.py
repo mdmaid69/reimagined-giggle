f1208bfc542e9c5f2136b861b846b441fe5a905f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:45:53 +0000

    study sensitive extremely fail takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
