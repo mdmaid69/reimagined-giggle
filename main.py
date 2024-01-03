6f2d515439c059230d597beb94e19150ca415e75
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:03:40 +0000

    cope switch forget burn wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
