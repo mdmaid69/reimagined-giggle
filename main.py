a7b252761926832f72d9c02c074b1d93f6cba07f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:03:49 +0000

    keepout stupid answer start check

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
