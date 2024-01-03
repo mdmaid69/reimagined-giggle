ee9576352bd5e6ad9a2178c4907f0a2980b613e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:04:17 +0000

    entry sentence wander children serious

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
