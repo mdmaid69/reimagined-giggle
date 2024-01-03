dc4952c1f5574d486d6e9158358b1b37f9895df2
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:51:51 +0000

    brush awake forget away bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
