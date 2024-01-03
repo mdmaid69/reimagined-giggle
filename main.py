2071b9276dfd2b2b0f765e22ad55b9b46f01dd37
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:23:03 +0000

    anything tidy hollow up perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
