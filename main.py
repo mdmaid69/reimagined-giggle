e9e77a500214e73c31f757e8f948b0f30f511627
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:28:28 +0000

    cut date estimate over swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
