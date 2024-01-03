c1fbd279c18daa9fa2361eb6e689892bdb758b74
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:50:00 +0000

    kill raise equipment tense behind

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def sort_list(lst):
+        return sorted(lst)
