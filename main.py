37c88142be16e67bff515e20448ed4775aa00403
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:51:58 +0000

    below takeout visit national talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def find_min(lst):
+        return min(lst)
