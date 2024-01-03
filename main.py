558c3a80002d226a26d72b6967e8072ffba6d1af
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:49 +0000

    available fit switch 565end hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
