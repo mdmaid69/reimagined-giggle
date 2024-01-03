4835b720fb0728b8000a7ad1613340fcf40a375a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:37:48 +0000

    constantly dull modern brown platform

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
