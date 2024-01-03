a8d44a356c82b085fd681b71ef74c37c26215daa
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:24:08 +0000

    success bad left autumn drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def is_odd(n):
+        return n % 2 != 0
