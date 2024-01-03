43ffe9c5fc0e0492860958c0dd3274675624f21a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:32:21 +0000

    die fortunately careful likely hour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
