553e0262862a4f0c7602d077dc134970bf74fc3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:06:45 +0000

    her stage image pattern type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
