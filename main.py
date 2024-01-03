28572d13990639c2b5d063c298287bcf94ec4efc
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:55:48 +0000

    load match compare continue try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
