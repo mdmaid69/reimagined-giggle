08f3dc06b7c4e5e9bb5b34e59515db488fa76400
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:54:50 +0000

    mean plastic precise tense cheat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
