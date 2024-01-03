97b9a3f42d185e3a879334be60f427b8151f000c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:02 +0000

    whichever fall be as rough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+print([x**2 for x in range(10)])
