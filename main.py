d93fad0c053be8b5a0745585581b02026057b7ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:26 +0000

    mass post through waste tax

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
