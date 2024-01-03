9cd831addcbe0ece0ebd0017e2389f29aff43ba5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:25:35 +0000

    art knowledge fierce boat nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def square_number(x):
+        return x**2
