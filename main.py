5f0c413cb2f376b76f5ed90e58b15d04efa8db48
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:15:41 +0000

    beside act loan dry circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
