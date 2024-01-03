e3599bfbf69050a3a963a86d3a00532db8b06d9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:57 +0000

    direction block burn correction sight

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
