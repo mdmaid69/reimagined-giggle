6b95047ddcac5d471f924833a9e7ef8e2e60b032
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:03:17 +0000

    stretch regret single 564encourage side

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
