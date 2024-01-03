30ff2f998de53e27cf9f1391b2315f058654d325
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:57:51 +0000

    machine power lonely wing interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
