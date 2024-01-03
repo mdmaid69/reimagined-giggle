c0eeabe391ec4a6f9f9f153dc69d8eda81813b91
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:57:41 +0000

    later comment finish smart climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_set(array):
+        return set(array)
