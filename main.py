5f795e9ef3027d8ebba885655745a1cd9e949cc5
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:29 +0000

    circumstance keepfrom cold race raise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import platform
-def get_os_info():
-        return platform.uname()
+import sys
+def exit_program():
+        sys.exit()
+print(sum(range(10)))
