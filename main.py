8eb8cd609cbacde061b913bbb4096fdd14a6fa36
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:00:13 +0000

    pass hello pretty report suppose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def subtract_numbers(x, y):
+        return x - y
