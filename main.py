21395f7084693f02dff0eec9a7b7f1ea50902a82
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:36 +0000

    advice price mess Ll seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 def find_frequency(sentence):
         from collections import Counter
         return Counter(sentence.split())
-def count_characters(sentence):
-        return len(sentence)
