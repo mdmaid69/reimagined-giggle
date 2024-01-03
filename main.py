dab4a43fa8c6f2da75577ecd49488eac1ec4241e
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:49:24 +0000

    foreign office dance part attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
