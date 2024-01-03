76d35a6e1d7b3e86cf1e1d79f07d4f5a25a72492
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:49:43 +0000

    year condition yourself burst beginning

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+print(sys.version)
