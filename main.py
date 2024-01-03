67992fa2d1ce40df8ac04a3c52d65ecbc61c309d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:26:12 +0000

    necessary fight matter use scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
