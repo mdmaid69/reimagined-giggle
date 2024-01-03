7f0f9b66487bcccc216bbf9694cd2338a396fd61
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:15:15 +0000

    difficult hurry offer sensible slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_user_dict():
+        return collections.UserDict()
