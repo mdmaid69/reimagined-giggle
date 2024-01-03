a03d4b5ae663899a59e80448e7bd5a60705bfa36
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:29:37 +0000

    comedown out will use waste

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
