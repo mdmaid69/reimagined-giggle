eacfc017b77058ca067cc19723ef7ea2ee5ef90f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:26:52 +0000

    wait stay find especially damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
