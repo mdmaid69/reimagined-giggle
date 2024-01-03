441fcf70a62c9952856aa4bd7367db6709fc1aaa
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:12:05 +0000

    accident close century cut uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
