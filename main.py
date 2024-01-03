e12c7cf0a7af1d502e09dac78796f13e97526153
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:10:09 +0000

    transparent trade reduce worried ever

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
