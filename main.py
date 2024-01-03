4b5d94ac6332da936b15fec315293f1b93b142bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:25 +0000

    thinkof mixture smile tax breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
