f2258cf8ac1ece8a3c7397ae80a623ade31f2c0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:39 +0000

    brain wrong forget total path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
