fffd8821a961dcbc1135fc743b1a5938ca22fb8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:10:44 +0000

    section mother inquire year duty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
