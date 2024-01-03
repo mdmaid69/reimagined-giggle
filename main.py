cde5aed42a55bdacc03eac84e0abb612c8b62f3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:17:14 +0000

    low own inquire sink by

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def multiply_numbers(x, y):
+        return x * y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
