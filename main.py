3bbce6bc9388fbe2361f70a09043c1b05539e93a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:30 +0000

    payment by war good blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
