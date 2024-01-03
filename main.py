a5fe6241b363e86a0c96afe05fd2628aa49db8ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:54:45 +0000

    clear father mass sting rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
