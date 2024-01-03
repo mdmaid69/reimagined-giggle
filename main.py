8b4c44c8ad418523b5a13ddebd00cb6b3150e24c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:49:24 +0000

    uncertain separate trap spare speak

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def is_palindrome(s):
+        return s == s[::-1]
