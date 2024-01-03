596267807be7ee4ce8cc733ff315f8afc4064f2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:27 +0000

    ilovedicks fight strength blow out

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_set(array):
+        return set(array)
