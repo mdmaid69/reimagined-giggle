368d655f76057d7a0f97836f23c0c127f8e7252f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:36:50 +0000

    secret job precise moment 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
