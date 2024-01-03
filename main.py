282254a8542eb699c015a6f8e70e91bd3cad6fa0
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:40:23 +0000

    opposite not suppose face available

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
