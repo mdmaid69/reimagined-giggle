f4e81058525df8f0278415b53109553116930b62
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:38:09 +0000

    section sometimes load suckme solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
