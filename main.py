63db6cf959d41730d5cbca68c736c8a63c76d51c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:47:50 +0000

    history human takeoff party use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
