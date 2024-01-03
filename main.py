ef0b17ec7dc175e9034d150baee83e5322635080
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:01:54 +0000

    their turn talkoutof genuine OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
