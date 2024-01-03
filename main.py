d215767b8dd37172fd7ad242f860bf3b6ebdde55
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:20:25 +0000

    win goback(to) apparently guard sale

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def add_numbers(x, y):
-        return x + y
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
