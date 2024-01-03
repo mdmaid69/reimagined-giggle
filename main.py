c46f4203cf3b479f1266975b7f337c1c483ac63e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:49:09 +0000

    concerned stone means describe clue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
