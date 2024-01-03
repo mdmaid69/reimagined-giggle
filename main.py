4f6557d5acc8263a6bc6e637d0eb3e8de66d1bb1
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:39:48 +0000

    delight beautiful muscle ring passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
