4245fe97145e75054f2144fd735b0b63d3321792
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:59:09 +0000

    scared anywayanyhow lock conclusion regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
