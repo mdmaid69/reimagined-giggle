17e2ed704fe519f39d81f7b66e22a214011f46ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:06:26 +0000

    natural adjective pencil breakthrough may

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  def cube_number(x):
+        return x**3
