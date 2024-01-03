7e0abbaa5cfbed241485db6d1e106956426a6842
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:19:52 +0000

    live stupid without floor clothes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
