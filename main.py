e58351929c7b8d6c17720d14a0f477e7146b07aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:47:26 +0000

    classroom accidentally type region surface

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
