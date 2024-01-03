389f4ed962384c692630932d02c197f8673a10f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:56:54 +0000

    second comeback trouble them bringup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
