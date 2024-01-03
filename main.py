3a31ea528dae2e4087865c3bfa1e118ec8356901
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:32:16 +0000

    loose female cap favourite rate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
