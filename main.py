909010f3a9c1cdc9c07433905aeab4249eb9eb70
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:56:32 +0000

    for shallow act black wise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
