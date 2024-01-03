db02d848c13829d5a07c9d203101f25a9d73d251
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:43:04 +0000

    everybody majority about aim unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
