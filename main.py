53cb59142bed2ac364c90c9798ee28b5c2efbe6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:56:36 +0000

    flood 566end bringback shock prove

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
