dc288ac0509c44f688ca30cea40f224daf70521b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:14:09 +0000

    stretch band bread change unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
