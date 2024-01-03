494024d88bef16181d93991a3eef0a99ef637590
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:26:43 +0000

    hold damp tone repair art

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_length(array):
+        return len(array)
