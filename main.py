0d8fe0a3c54e1f3660dcd157ee38a3ab912ccc10
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:32:58 +0000

    offence proud opportunity even understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
