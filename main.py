ccac98dbf5e4015b65713dcd74f96ea9d06ba66d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:43:38 +0000

    form free naturally Hh turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
