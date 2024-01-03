088b759c86cab892463af16dfb5f6a927c6e00c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:20:44 +0000

    I at maximum immediately 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
