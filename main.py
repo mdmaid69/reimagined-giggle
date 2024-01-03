71d37dcbe4654a4ba355090514d65d2ae6316b68
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:03:11 +0000

    scheme apologizeapologise somewhere crowded home

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
