f30012666bd672c70b38be003cdeddac5ee72355
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:08:55 +0000

    workupto ear nearly giveaway healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
