a20d9905bc7e24741590c759e0cd3acb710a63d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:44:58 +0000

    acceptable accept zero passenger fill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_error_function(x):
+        return math.erf(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
