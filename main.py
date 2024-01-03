57b94ecba17ead8b4592b2ae1127234e66ae60f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:52:47 +0000

    sail pick lock sleep thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
