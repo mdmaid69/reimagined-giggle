38bee8760908f6adf23c1e6a0f870958d2f9150e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:31:30 +0000

    sound indeed back evidence intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
