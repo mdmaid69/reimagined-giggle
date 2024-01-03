b991859a8044b687eb604eda2d883fc107427dfa
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:18:09 +0000

    steer better tight turn holdout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
