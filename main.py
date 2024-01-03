f88698e4f7d40eb4508715d05b2ff184ea18c480
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:14:37 +0000

    Ww 570enjoyable cut collection convinced

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
