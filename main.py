8d03bbc4c78caff3c8dc01c1ea7c38967d10c3ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:25:55 +0000

    flight subject spare attempt inform

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
