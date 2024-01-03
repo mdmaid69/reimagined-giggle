508a0f31c381078b5674b577411734902af5f963
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:36 +0000

    glass 565end complaint brown Uu

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import datetime
+print(datetime.datetime.now())
