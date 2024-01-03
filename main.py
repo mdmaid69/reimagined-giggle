d62d14b886d8c926687d9867c5b36c4538793b06
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:26:02 +0000

    decision suffer dress children hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
