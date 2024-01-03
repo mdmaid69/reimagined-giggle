f18c2169d1db2af87689a36a2e03080617e9e175
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:14:16 +0000

    follow listen chase intelligent cruel

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_acceleration(speed, time):
+        return speed / time
