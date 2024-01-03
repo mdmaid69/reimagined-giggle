a825e7573c0e88aabf9fefcb44045b32e189bf36
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:18:19 +0000

    release identify expect sign plenty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
