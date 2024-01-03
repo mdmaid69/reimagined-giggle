5cd0a915c94540fa61fcb4e77c43ab8d3a0ee67c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:13:54 +0000

    tonight pleasure bit gas bend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def is_odd(n):
+        return n % 2 != 0
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
