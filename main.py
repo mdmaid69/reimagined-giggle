d8c4627b28b729270bdd33ab8b1ace1f2241c93c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:51:12 +0000

    creature bang tough date Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
