27b214859a9ab2fdda56bed2b17f18b281004f72
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:13:36 +0000

    jump offend straight swimming limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
