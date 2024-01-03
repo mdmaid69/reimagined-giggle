cb1dec33a9587cc428a0f2334c6657c75a0b68b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:39:11 +0000

    protect even push runinto interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
