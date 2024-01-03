ce8ee97117a939966d3057216acd915b94ebe006
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:01:29 +0000

    at heart cruelty workupto even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def add_numbers(x, y):
-        return x + y
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
