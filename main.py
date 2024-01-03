91bdcdebbb98da2716689592afef58b518555a0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:39:26 +0000

    pile properly minute laugh scheme

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
   import matplotlib.pyplot as plt
   def plot_scatter_graph(x, y):
         plt.scatter(x, y)
         plt.show()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
