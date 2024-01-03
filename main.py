eb88e0a460e2a3da512fd18658142b1611dfc570
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:49:11 +0000

    crowd calm criticize label claim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
