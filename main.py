9912d1eb70e1237827c86cb76da0db9459c47b85
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:36:12 +0000

    pronunciation forward conduct probably thanks

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+print(datetime.datetime.now())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
