76e8d226732c8fa161ed55b048d9abdcf3e52fca
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:18:34 +0000

    feel actually check believe uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
