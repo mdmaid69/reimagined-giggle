16b123909a7ff2fa78004e30dc9eb7c9f08b6243
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:57:01 +0000

    bill distant lend mean combination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def is_even(n):
+        return n % 2 == 0
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
