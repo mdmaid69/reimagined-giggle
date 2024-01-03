22c0386a05875570091ff1934fe9a6c245b1c1e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:16:56 +0000

    reduction form blame cry present

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
