e4c2b65d30588f3d5d08f1f0b88a20d5d2d2b5fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:43 +0000

    bread pretty whenever serve tend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
