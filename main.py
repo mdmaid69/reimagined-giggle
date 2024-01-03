ddcb99b344450590a4a05c44f7af733524bea278
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:49:26 +0000

    aware receive offend false fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
