f79e1ff5e3399d77246d0f676f841cf2d483aab3
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:11:24 +0000

    round yesterday climb step measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+print(os.getcwd())
