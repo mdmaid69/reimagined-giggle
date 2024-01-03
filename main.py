48db8ab97dc53eda190e2d253c0c5fa1258a5c01
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:59:48 +0000

    deserve copy pure right run

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def count_elements(lst):
+        return len(lst)
