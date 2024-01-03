78eea134b545bcb87717a39b008ce3680139e845
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:50:02 +0000

    move across interview sure succeed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
