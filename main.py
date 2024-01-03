b561d800c4639fcf63ef58df856ee15d3006dbe4
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:10:49 +0000

    reach spring proof suggestion interruption

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+text = "Hello, world!"
+print("Words:", len(text.split()))
