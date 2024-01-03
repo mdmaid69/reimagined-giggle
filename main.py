22768e898ca9d61ac771bebd70e4dcb7ec3cc0a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:04:21 +0000

    hardly be hate offence wheel

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import collections
+def create_user_list():
+        return collections.UserList()
