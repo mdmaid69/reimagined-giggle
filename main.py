d9c95527c1c4805b586baf50cbeb3584e52cbb8f
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:58:34 +0000

    bury immediately wash introduction survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
