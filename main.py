db90a7391564321d9fa808a0d21ba39cfd91d62e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:45 +0000

    save funny sticky emotion equipment

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
