a2e741dc055f481d99edd6259b94e34aeac94c79
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:43:22 +0000

    interview hollow getoff book hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-print([x**2 for x in range(10)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
