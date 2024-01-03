5523e622a2488866d8e41445bef97db462dbbd98
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:13:54 +0000

    interrupt instruction pleasure touch nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
