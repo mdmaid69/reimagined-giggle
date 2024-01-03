5277d39f975e7aed42ed75a675b45260f06a17e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:36:26 +0000

    crash calloff think surprising regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
