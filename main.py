58ea9664c21d29bd9920b9d18da073ec02f8e670
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:59:28 +0000

    cinema steady urgent autumn government

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
