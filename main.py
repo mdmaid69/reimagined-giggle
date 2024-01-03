fc131ef0933d1f5dabcb92bc152e5d2bdbcb7c36
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:53:36 +0000

    still talkinto traffic teach special

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_density(mass, volume):
+        return mass / volume
