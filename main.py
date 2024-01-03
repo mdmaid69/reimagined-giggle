2bbaf14ab6dd1f810f5b50e792475b1ab761fc60
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:26:52 +0000

    soft wherever visit opinion angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_cosine(x):
+        return math.cos(x)
