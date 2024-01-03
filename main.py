fa0eff96bef69ac88f3e752c76635763be805e7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:01:01 +0000

    cause method comment obtain freeze

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
