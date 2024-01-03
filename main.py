a0d196d94e748d9a1d4c2fa5b3605f9b3d66dee8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:32:34 +0000

    deserve sell not ill them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
