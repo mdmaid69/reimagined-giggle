efc8ab318e04279848c418e54f793a8f2adf0b95
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:08:37 +0000

    heavy pleasure treatment progress moon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
