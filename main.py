79385f3b42cf85e8ad8984373a69e0cac652177e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:11 +0000

    worst bye month wide meanwhile

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
