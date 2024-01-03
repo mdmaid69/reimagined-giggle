63619fd2bb12a201c7e2987d1c79b5c55f71b693
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:24:15 +0000

    neither people pair offer circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
