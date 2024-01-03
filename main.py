2f4b96d483a78c562b149d1d36b18450104dcba6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:03:02 +0000

    shock fear pattern basis consider

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
