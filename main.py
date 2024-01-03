e50c9fcfe8e6e9747baef1220a737721f71735aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:33:44 +0000

    brush school if alone surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
