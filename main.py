c0e3db4cd0577320aeb3535c8ad1d713e9ca3065
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:12:29 +0000

    pause light turninto powerful last

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
