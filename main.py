8a06f9b24cb33c8aa2d59d0c73d449e6bb5db948
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:38:59 +0000

    keepoff everywhere deaf worried copy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
