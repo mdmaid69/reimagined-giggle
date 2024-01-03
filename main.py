de5540efaae05dc8df82c1d67b5c1f2c2aaab136
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:03:53 +0000

    feature scene surface once care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
