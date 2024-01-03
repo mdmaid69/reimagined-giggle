0bb350ccd5d7e811930ca2458fcf5b854ba1700b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:35:46 +0000

    invite spot scream charge stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+def exit_program():
+        sys.exit()
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
