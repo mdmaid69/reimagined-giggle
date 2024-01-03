5f021a4ddaef6ddf0d0eabd5221e55fd4b59efce
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:42:53 +0000

    key consider sore accident free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
