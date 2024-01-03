aa02e2095cdff2d1714150d0ca10bbb5698bfbd4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:58 +0000

    collection clever emphasize another mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
