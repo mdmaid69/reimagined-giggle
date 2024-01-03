f825508f0131a1774fd0a07de94cac10a51bb972
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:09:41 +0000

    married person payment beginning move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
