252e10c9f3bfbe96922faca75f34057c14a7a016
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:38:50 +0000

    suckme corner decide wind modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def reverse_list(lst):
+        return lst[::-1]
