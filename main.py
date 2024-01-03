d97d77f881c7cdf2d0ec427248c0ed0693cfad44
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:48:59 +0000

    line field goback(to) shame separate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
