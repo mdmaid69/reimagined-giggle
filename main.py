3a00a42b4def583a9503e139de41bbce51a671e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:56:40 +0000

    quantity score object means remain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
