14a6835f985bdc780865985c00c821270439e050
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:28:29 +0000

    reason contents present Ii chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def remove_duplicates(lst):
+        return list(set(lst))
