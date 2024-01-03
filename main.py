0f6bdbbf5250cfc8621ddf5a5697581726611c5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:28 +0000

    avoid describe rest cause pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import logging
+def log_message(message):
+        logging.info(message)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
