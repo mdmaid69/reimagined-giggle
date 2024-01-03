d7cabc0146f96a5b7dc8d2640a4a217d7d89e779
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:25 +0000

    bit brain sure giveback dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
