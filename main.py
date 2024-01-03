fa12170ec482e3970101a3dff0710a5854e86714
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:26:36 +0000

    clothes familiar difficulty excited official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
