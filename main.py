698dc82e92d8016d5ccfd29b6cd2ebc0f9da82b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:52:28 +0000

    them instrument through big operation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_hex(n):
+        return hex(n)
