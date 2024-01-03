d85bf371a8790ddd6d02a9f1919a7cd447820390
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:33:10 +0000

    rarely container ordinary mail lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
