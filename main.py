ea1b65db4b5e31f990c8cc0948689477a196ec16
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:36:38 +0000

    worried foreign unkind hole with

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+for i in range(5):
+        print(i)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
