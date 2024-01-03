ca1b96299777dde6313e84fb57b01dcb85ce4194
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:35:33 +0000

    situation swallow 179bite comeover leave

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
