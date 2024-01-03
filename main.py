2131b3534aca68778c2f1fc10fe492374b65a7bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:22:35 +0000

    experiment start subject rate takeout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
