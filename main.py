bb0e2424ce5ddf91eb711d7ab8b754b1edd15a66
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:43:22 +0000

    violent pour bother find spot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
