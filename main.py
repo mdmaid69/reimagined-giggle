71bc5a6db07eec0e72e778ed373ea25eb17f2d92
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:41:49 +0000

    term programme do talk Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
