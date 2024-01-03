c9318d834c420b6a1258b0863703f2e7f70663c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:44:58 +0000

    lonely rob close noise like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_current_date():
+        return datetime.date.today()
