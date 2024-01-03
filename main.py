79421d3bf48b872145348ae7e225bbcaaae86ab9
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:13:08 +0000

    not by temperature quarter spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
