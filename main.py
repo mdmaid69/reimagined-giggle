8470b26987deb865ce21ca2610ecb7d4feb6ab2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:42:00 +0000

    turnback odd strike single ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_current_directory():
+        return os.getcwd()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
