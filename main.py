ca7d4ec200685f112d533060028b99e5e1503393
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:14 +0000

    nothing breakup tense worth holiday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def find_max(lst):
+        return max(lst)
