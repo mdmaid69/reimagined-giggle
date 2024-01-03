6fa450ea830062fef8e38c74ef566ba12c739590
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:46:46 +0000

    build ought go down only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
