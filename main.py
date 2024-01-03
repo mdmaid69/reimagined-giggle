a467e841eedd787e44decd5201ecfa490258cbeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:07:47 +0000

    actually pattern sentence different basic

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def convert_to_hex(n):
+        return hex(n)
