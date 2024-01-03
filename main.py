da9b6b430941d253c0115a0ae10690d74b754f3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:32:20 +0000

    fail view preparation rule prevent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import numpy as np
+print(np.array([1, 2, 3]))
+def multiply_numbers(x, y):
+        return x * y
