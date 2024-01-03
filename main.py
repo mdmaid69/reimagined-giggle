6a94b0007aa2f0a4e234e965201f947d6728d179
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:17:16 +0000

    skill relief race funny search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
