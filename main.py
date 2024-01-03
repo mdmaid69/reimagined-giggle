d6b4ebacd053a2b192f539a9d4107f06c1fa7c1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:10:54 +0000

    thirsty fine raise computer nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
