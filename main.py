435cc658c9b14ef42a5f816559e1451bc9224227
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:18:37 +0000

    class somebody point crowded waste

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_circle(r):
+        return 3.14 * r**2
