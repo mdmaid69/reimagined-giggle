d883ff30db2c670a2fc3447787191e4820f3c52d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:50:18 +0000

    present become water allow face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
