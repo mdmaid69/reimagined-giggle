cba6e4fe10acba78f648bdb41ddf0cab2025f766
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:53:22 +0000

    fly takeover belong something hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
