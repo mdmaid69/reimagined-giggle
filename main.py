adfe5f3f338e7b547adead75e75f811d44973aec
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:32:48 +0000

    beyond character test figure area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_exponential(x):
+        return math.exp(x)
