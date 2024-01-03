108f71a98863b3a6b05edc6b4879d1110c9b18ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:14:40 +0000

    once clean string break many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
