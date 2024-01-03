b0cab3b7e462da9cf55451911cbac23253219670
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:49:45 +0000

    future question right change address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def roll_die():
+        return random.randint(1, 6)
