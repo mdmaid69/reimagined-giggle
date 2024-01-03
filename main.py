d42189ca7734e99a95754a677bf2a2e90cd3c102
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:32:47 +0000

    responsible evening curious ours activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
