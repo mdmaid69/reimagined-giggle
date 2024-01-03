0a90582c6e89037b926d0a40044f75b415afd4f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:22:55 +0000

    beautiful hungry disappear reaction coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import re
+print(re.match("h.*o", "hello world"))
