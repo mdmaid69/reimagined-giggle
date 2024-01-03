37652678c74d451e6dc891b3110f33f15e89e695
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:22:53 +0000

    must heavy forget class opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
