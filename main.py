3effc71c92e7fc53b41fa621c2a298903554f882
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:42:29 +0000

    breakinto injury public search after

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
