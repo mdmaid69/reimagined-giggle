4d516b35267f6acbb36d29e0355c004240a657e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:33:09 +0000

    breakinto cum flow entry godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def remove_from_array(array, item):
+        array.remove(item)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
