efa99b8862e79b18591dea83313b5fd07a004b1e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:47:37 +0000

    cry decision man undo broad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
