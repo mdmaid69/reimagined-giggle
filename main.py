d3fd87fcd6a40f8c52cc3835a0ffc9329c7a2520
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:13:38 +0000

    more comeover raw face cinema

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
