dfe3dce7b4ec3c4b68d67eefe217a347ac4d2496
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:45:10 +0000

    tap worth close century reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
