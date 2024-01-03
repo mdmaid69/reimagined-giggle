e176a42dcd39b62ecd5a4f195fe9f1ac539d4579
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:44:54 +0000

    trap recovery help interesting too

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
