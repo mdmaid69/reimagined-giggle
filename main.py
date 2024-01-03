e77a7033dfd1b6d0e8bfb4f57ef81cb7be5d0379
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:49:48 +0000

    stretch patience vote Kk touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(lst):
-        return min(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
