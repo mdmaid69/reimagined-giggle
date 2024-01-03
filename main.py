b50c87a2edf217f5ea9781dba62eedba0d4af132
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:03:22 +0000

    stay leg create Oo giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def append_to_array(array, item):
+        array.append(item)
