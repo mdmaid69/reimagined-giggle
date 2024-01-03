2aef18e11cc6b898348f1ed928586152f2211b04
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:27:19 +0000

    sky pretty butter breakfast holdup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
