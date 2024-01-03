54b73a7a40ce007c85ddc857ea8b91e4817195ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:02:45 +0000

    occasion there part account ocean

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
