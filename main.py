32f9f23a9a93abe9b2207a5835a688d545c6825c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:32:06 +0000

    off road so unfair minor

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+print(sum(range(10)))
