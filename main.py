5b28015cf8be156c888bbf7b08b72a5c63f3ba6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:51:53 +0000

    meaning bringup name approval worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def count_elements(lst):
+        return len(lst)
