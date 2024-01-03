6f3acbf672b6e27d6c71355d0251b41ed3694df8
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:35:10 +0000

    wild disturb ashamed temper display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
