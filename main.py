03e17b06fc332edd43b87a3c7619f5c81d1a65f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:04:54 +0000

    nothing tap door swimming snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
