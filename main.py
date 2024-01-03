0157342d4de98803fb6307f89de4711cd92a4ec4
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:13:53 +0000

    human nice open since impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
