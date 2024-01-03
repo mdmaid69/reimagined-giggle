bd5f4780a9f3d9fd80353636904bf969f1619376
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:53 +0000

    programme aim present claim vanish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
