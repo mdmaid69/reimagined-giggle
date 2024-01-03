cdc6188e07d12a51e9120a97366334e234c606c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:25:25 +0000

    beat risk behind fashion burn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_list(lst):
-        return lst[::-1]
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
