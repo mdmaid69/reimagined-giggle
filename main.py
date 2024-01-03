7aea8308e111766f2f13a9e164b2607c60072694
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:32:23 +0000

    grand left fat giveup pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_pressure(force, area):
-        return force / area
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
