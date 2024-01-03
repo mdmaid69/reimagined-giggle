9acc6827bcc5c68bef9e16948cc3a1a3b15dd24e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:02:32 +0000

    gate order development another rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def reverse_string(s):
-        return s[::-1]
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
