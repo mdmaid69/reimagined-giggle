8551a0cd3826b307a919b008a06564be32c05760
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:43:01 +0000

    destroy company space goout down

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
