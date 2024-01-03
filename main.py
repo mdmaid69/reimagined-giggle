10acd850b627315676ffec3784257965964941ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:08:21 +0000

    ride characteristic soak after surprising

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
