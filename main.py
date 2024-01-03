51ddd695daecff87c7715d0d410227bea54f3f1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:13:10 +0000

    understand genuine cross pleasure weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
