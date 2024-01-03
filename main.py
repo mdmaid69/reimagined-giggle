95be4b98aa8883289107ffc40723eeccbcb98c37
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:22:11 +0000

    perfect mistake one entry march

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import platform
+def get_python_version():
+        return platform.python_version()
