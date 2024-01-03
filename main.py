7b4dd6e1bd17057794ad34752acbe413e23ab40a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:17:39 +0000

    project bother seem drink under

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+from collections import Counter
+print(Counter("hello world"))
