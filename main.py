2802ee77b577db1b18b3e07bd9d8e557b9ffbaa1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:47:59 +0000

    file ilovedicks now complicated get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
