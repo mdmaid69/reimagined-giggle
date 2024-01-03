d9ee33209835904246163241056fcc50aeae1092
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:25:07 +0000

    past love talk wide pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
