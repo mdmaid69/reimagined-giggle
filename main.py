918ed02320e4a1dc820138a0aa9984268e16235e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:38:50 +0000

    exist wear pile release exploration

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
