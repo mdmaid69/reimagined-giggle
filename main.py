ddb231ebbe7aa8d543f7b8ed16f70d80093b84fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:45:01 +0000

    there view I speech character

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
