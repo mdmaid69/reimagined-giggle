a2976923371f6bf234e3a0fac443ff80da22e9fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:55:10 +0000

    dance dress wise where produce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def find_min(numbers):
+        return min(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
