dd7c7b765e23ac2ad6276fe04686c636b81208b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:03:48 +0000

    generally moon wash ill visit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
