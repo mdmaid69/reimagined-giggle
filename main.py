db308c6c8e48bccf5850d8501141ffab06fbf402
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:47:08 +0000

    rubbish whole rarely church spin

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_repr(array):
+        return repr(array)
