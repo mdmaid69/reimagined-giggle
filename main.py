2b47eda445f271a4c7390560d2a39c6c1e1c6d09
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:01:58 +0000

    strike round office eager centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import tensorflow as tf
+print(tf.__version__)
