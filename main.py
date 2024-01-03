efeaed176b4687973fe08038fcebd38814216dc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:33:57 +0000

    set bunch wise question reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
