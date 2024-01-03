3be220e7e8fdd1c6586e066bda5aa92018107181
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:17:03 +0000

    winter find team contrast twist

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
