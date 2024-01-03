691cc80fbe109c2935c0698fbce3b361267ee3ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:05:55 +0000

    hurt art keepfrom rate peaceful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+name = "Python"
+print("Hello,", name)
