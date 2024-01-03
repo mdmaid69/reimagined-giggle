75a3905260e4b8101383ca75c9406d3fb2e3bb52
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:53:40 +0000

    quit say thank rise flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
