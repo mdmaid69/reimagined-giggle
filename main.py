4c67ef23a9cc93397d2fd4f2587c53c0cbc49f62
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:19:24 +0000

    area none sick sudden through

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
