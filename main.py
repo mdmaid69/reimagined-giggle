ec6c6307f7a9b87eb9c488623def8beae6015021
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:45:11 +0000

    instant twist sticky operate answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def is_odd(n):
+        return n % 2 != 0
