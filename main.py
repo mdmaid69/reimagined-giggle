4ebe6f249375c6b8f93a511379e1f6453dacc126
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:09 +0000

    fairly finish glad laugh poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-print(sys.version)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
