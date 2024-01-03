1c25357ed71b98f82348e3aeb6af5cce5f8ee9a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:43:14 +0000

    combination considering majority display reject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
