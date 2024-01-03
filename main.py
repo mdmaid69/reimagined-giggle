37ebd1f79baa87e4b6440a6be645e84bbce3a5e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:12:47 +0000

    Dd scrape birthday maximum fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
