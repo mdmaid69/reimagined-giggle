10758adf6b82d423a84c38d9cf2e1d756ff0569a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:37:55 +0000

    mess first appear object bottom

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def clear_array(array):
-        array *= 0
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
