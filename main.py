d1eccd2953f2dbf34feb703606c3461ae2211c07
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:33:05 +0000

    to cum pure reason gentle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
