6c509da5c9fbf63a0f816741ec8c540e0c2bbaad
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:16:31 +0000

    play care loan manage hurry

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
