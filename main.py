503fda5e1d9b2a6b2859561b60d486bda2c91fdb
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:40:56 +0000

    soak to emphasis tomorrow load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_factorial(n):
+        return math.factorial(n)
