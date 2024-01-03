b909bdf1d45487ba42d76bb59ea6fa3d8af6f144
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:44:01 +0000

    several door remember least raw

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
