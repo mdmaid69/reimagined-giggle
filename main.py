0ef2e6e4fb59ad9daea5a0057ab89238dba3cf34
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:34:41 +0000

    length glass road me lunch

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+print(sum(range(10)))
