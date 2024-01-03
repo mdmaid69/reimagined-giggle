5754542a961340881b1326257086dc0484b90c5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:06:07 +0000

    spot even bad race head

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
