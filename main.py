dd1b73942715ac12ac6e651d037ad9f568af4408
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:09:37 +0000

    wave against command window odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
