0c4cb278215784ee8a0658fda252d20a915c5e3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:50:41 +0000

    him big patience we sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
