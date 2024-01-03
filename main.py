75c32531cbe32101586cfbe2147424373c373c85
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:12:23 +0000

    band petrol getup collect abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def is_odd(n):
+        return n % 2 != 0
+import sys
+def exit_program():
+        sys.exit()
