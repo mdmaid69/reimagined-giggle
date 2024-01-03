90cf5b711dc26483372d27e800164ff1cbc56cf1
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:47:02 +0000

    steam split murder hide container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
