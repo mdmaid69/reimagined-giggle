4682de2078d91695fd35d8822343ddc0a2267233
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:33:19 +0000

    possible takeaway tax tell extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
