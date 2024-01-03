a7e9723a0cab70e70a01b7313e08651e7fdc91dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:50:09 +0000

    realize Nn behind help sore

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
