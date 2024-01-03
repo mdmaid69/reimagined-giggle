fd0aa044c90883294d0554423143012836aeac5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:01:49 +0000

    natural high I object cope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
