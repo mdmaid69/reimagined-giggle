bab8e7f7b9a1ccdd027acd795fc930f207132d2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:16:21 +0000

    yet care total under exhibition

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_octal(n):
-        return oct(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
