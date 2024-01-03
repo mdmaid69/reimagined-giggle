5f9124f68927a88922eb1a0602afa56f049abc68
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:04:09 +0000

    street your sore shadow preparation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
