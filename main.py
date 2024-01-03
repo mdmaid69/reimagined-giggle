0e4b22057b213d1ce1dfcde43206b17c68abddd1
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:29:25 +0000

    able start lift reduction thumb

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
