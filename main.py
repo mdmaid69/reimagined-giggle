b827e132dfee270f08b91c6c54ff2767d2cabac5
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:27:05 +0000

    compete wide accident slide out

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
