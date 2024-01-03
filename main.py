17e251e1580c75a12bd0dc4b7fc81ad8dc9fd85b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:31:31 +0000

    guess term not forgive challenge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
