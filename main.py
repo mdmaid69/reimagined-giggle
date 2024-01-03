efa81e34690152d0383412bce2979bc32072e47a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:00:25 +0000

    impossible necessary early behind cough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
