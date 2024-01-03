900b51c38da3b0f8a955c85ffbb2c3798dae0b7e
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:14:07 +0000

    statement letter bored cut flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
