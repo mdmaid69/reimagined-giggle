56a56a3dcebc9bd916c1649aaf09d81f634c39d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:06 +0000

    event minor equal rough soil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
