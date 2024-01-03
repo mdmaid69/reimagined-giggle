0b17cc9571624cc4a68d2314202fb40ac8a41138
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:37:16 +0000

    load whatever thumb expression walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
