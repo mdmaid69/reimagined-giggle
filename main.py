6d051a2d850ef0c7e22d6196d6394a2d78f56fa1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:48:35 +0000

    crowded between send surprising traffic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
