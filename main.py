1a697f4bcd41d8c9b260ccc28c3ac5d0a8a99f7e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:57:32 +0000

    limit innocent production child trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
