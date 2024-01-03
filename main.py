844673ae16040e52d92ea8546e4756eacc6dfd13
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:31:26 +0000

    thinkover Qq blow bill war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
