203af0086a4967461ffbb0930c3992cedf9dae5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:24:13 +0000

    excuse rather ill relative organization

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def sort_list(lst):
-        return sorted(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import sys
+  def get_python_version():
+        return sys.version
