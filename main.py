5d760b8e745da838300989bc9719df7db169fb12
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:04:48 +0000

    join imagination calmly approve cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
