96feaf74468c63a8ee3bae34e2f27dae7bf49288
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:08:59 +0000

    space himself mad follow mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
