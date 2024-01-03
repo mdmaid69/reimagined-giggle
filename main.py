9f555f7d165b9e7adeb6f89e2c5d8d73792640d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:07 +0000

    sad good sense mention service

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def convert_to_binary(n):
+        return bin(n)
