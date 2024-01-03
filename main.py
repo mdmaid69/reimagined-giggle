639343fc033463097eba7a095f05fd7d15b8345d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:33:12 +0000

    test border chase open provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
