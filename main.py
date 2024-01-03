fe8732d04cb478d958fd95ceb6fc8265b86da245
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:45:49 +0000

    watch impressive accident opportunity half

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def greet(name):
+        print(f"Hello, {name}!")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
