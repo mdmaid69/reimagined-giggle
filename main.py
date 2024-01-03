03abd865e70167b711478904c4741457a5928aa4
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:14:44 +0000

    often rent unlucky badly couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
