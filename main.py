bd3848c30dcd202d3eb62c32ed7582e3d9bd1b00
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:12:17 +0000

    permission criticize whatever hurt ocean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
