78269946f31641118a3321c84660541fee07cc22
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:25:34 +0000

    strike apart comment accommodation buy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
