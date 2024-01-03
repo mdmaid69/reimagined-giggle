ca33ddaba4e2e542fab2f94abdd62c183ab19115
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:55:39 +0000

    injury flat attempt related promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
