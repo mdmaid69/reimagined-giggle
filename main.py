d9e55de8a7bf97aedf9f00d2347124a3b180d476
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:10:19 +0000

    fresh behaviour sensible better accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(x, y):
-        return x + y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
