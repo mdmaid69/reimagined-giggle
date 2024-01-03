7f19eb18e868803dd16f4630ef2e97ce19a54cee
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:36:04 +0000

    somebody period search man backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
