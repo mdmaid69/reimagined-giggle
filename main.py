d769a1219c9eda769c7cfe3adba318d14f76cd1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:19:40 +0000

    slice nature either shelter early

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
