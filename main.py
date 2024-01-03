3b4134624e3ad4edd0e710ab3752194c5402afe6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:53 +0000

    earth like shelter there trip

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+i = 0
+while i < 5:
+        print(i)
+        i += 1
