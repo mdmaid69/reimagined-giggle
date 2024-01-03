ee82e5e4a9eb9daccda9f34658d32b203d3409e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:56:44 +0000

    form 567enemy little snow steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
