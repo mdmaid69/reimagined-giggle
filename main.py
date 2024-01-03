b92f0dcd23eb9f8600b94434fdf8c3c6883cec22
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:23:30 +0000

    appear runinto certain break ashamed

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def reverse_string(s):
-        return s[::-1]
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
