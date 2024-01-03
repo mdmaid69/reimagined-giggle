87d2e707bc918f3d3ce1d1105dc4dd8d70554e77
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:35 +0000

    declare pretend basis intend clue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
