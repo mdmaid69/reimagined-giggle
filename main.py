7e51738468f1197125ddf65a6880cbb32f5672f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:59:50 +0000

    favourite cut modern active his

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_cosine(x):
+        return math.cos(x)
