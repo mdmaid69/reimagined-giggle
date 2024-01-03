aaee569b23a77a29488972151ab77dba6e3e1b0a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:08:22 +0000

    buy poison scrape split hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
