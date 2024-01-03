1944c8e87f72d7046054ebf09c927ff3b7b23448
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:37:34 +0000

    characteristic place season might careful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
