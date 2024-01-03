c9dbe3485c4f8c803b4076798f128f42aeea6ed2
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:04:25 +0000

    powerful yes personal wear say

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
