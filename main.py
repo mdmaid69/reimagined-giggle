e29ff34393180c212686cd71b0d244f430aa1b24
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:36:42 +0000

    thumb class used raise display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
