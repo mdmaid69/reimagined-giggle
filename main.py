00f920179e727f255f6a44b9c0e85c8c02295e58
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:37:27 +0000

    abroad female entrance hurt wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
