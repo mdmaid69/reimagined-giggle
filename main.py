fc5399dcee5cef73e9134f5158b8689440750937
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:31:33 +0000

    narrow stage statement nonsense reasonable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
