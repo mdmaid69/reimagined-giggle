333a9751cc684fe973fc8ba1a92adf0c8c5a02a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:05:36 +0000

    bunch giveout party manage really

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
