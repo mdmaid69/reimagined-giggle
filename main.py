e0adf71f31118149f479d68f96b558e1a6089110
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:53:06 +0000

    female borrow consult claim taste

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+print(json.dumps({"name": "John", "age": 30}))
