8db1b8376b9f168226ba684f7e5ed54be820201d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:22:20 +0000

    slight look intelligent mend negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def divide_numbers(x, y):
-        return x / y
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
