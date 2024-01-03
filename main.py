1637d35981ecba5907ccd86b4d87cff107a04fd3
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:30:07 +0000

    school church traffic shade path

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
