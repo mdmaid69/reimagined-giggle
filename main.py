08cd2da83a0b22a5994f0cc7b5d97b60fd2e9384
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:08:54 +0000

    Mm it tomorrow report organization

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+print(os.getcwd())
