f133df8d10030f8bdbc773c5fc7d027de8d22747
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:59:03 +0000

    practice its rush its pride

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
