7d71dce77e56e7663cc7422e0eed3ac30ee26556
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:21:43 +0000

    bang up my ashamed Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
