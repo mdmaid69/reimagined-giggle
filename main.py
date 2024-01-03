09a12d70d49a882e97ccc8121df2bbe9a44a3ae3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:39:07 +0000

    runover dry Uu it group

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
