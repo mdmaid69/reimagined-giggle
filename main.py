095d02020c758394459259ab29554e1e11114b80
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:59:00 +0000

    state mark close convinced disappear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def convert_to_binary(n):
+        return bin(n)
