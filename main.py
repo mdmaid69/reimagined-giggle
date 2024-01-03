ea4457035b8960f4826d1ed527332c75d0607153
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:01:33 +0000

    stop risk live message bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
