d40ee366a5f89b5a69e83218699ce42ad6252ad6
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:32:43 +0000

    watch estimate match chase evidence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_pressure(force, area):
-        return force / area
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
