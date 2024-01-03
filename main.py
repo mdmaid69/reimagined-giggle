ccf2427f08fb9296f0b3d37e7ad22249c749dcb0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:00 +0000

    send hardly stiff no unless

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
