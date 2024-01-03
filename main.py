f03adf37f94d2f201c45014b0b9278a73c569a79
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:47:01 +0000

    clean result earn thanks disturb

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
