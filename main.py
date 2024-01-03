6d490c8f9df7be139656a1b1f0d11f0bad6ffdd4
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:48:20 +0000

    stamp serious consult aim sure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
