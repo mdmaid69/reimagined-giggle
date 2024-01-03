05746ab11e98f24aee4a1180960c37b64c8d1eea
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:53:06 +0000

    check tough wander bringback second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
