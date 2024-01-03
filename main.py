e2eb03c7442eb19c2535b7795382f34862ce7481
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:37:07 +0000

    excited nor invitation may sense

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
