75136ada58483aa37782d03715fe8d8fd4b4d87b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:23:41 +0000

    cause operation boring frequent crash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def convert_to_hex(n):
-        return hex(n)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
