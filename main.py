98b9002e8ceb472a68a5ca1a3b46dd26aa775a50
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:30:03 +0000

    neither convenient direction niggaacademy the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_today_date():
+        return datetime.date.today()
