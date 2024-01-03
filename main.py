0b4dfa2bfd3824546b0f63fc25069148b20aad42
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:50:36 +0000

    right swing temper Qq popular

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
