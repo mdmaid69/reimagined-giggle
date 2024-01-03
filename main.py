4eb1378477ad778a3abfb6be3b85dfe972695fac
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:07:21 +0000

    clear unite tidy capital Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
 import collections
-def create_user_string():
-        return collections.UserString()
+def create_queue():
+        return collections.deque()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
