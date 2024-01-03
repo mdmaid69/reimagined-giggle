969944ce0c213302a474f532cbd62906f088e59e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:28:30 +0000

    fit together turnaway here warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
