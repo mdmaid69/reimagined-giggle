90a281e9a2de4d5575ab57a18d985598ff28e32c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:10 +0000

    post beside contents male excellent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
