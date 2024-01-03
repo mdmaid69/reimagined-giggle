d1b2b75ade3369a42089177403384e3f235830a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:37 +0000

    office admire afterwards equal fortunate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def count_elements(lst):
+        return len(lst)
