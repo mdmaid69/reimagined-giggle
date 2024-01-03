4351a987d0547a12c576c2c296f2c237d11a1b04
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:16:30 +0000

    city agreement beautiful respect equal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import tensorflow as tf
+print(tf.__version__)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
