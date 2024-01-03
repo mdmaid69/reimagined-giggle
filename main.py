3a90668b6ea099d342dfa113063c32fad84164a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:52:33 +0000

    aware broken tidy rain quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
