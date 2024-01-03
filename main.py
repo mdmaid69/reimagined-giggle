a1f52d77f68cad6ed11b729e420fd7375f88ffb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:28:14 +0000

    possibly tip smell habit meaning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
