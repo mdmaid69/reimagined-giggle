1f020823d80e04a9e4fec1d2e4e7c97cd1cc5f90
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:33:10 +0000

    where date than play fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
