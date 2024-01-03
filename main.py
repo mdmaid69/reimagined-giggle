39015f421af54fdda1d29dacd57cea577497baac
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:22:47 +0000

    recovery talk picture deal later

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
