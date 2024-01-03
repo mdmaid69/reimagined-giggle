141aa3f27ec613de9c1a83ee199af99c3514bccf
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:12:49 +0000

    kick pay weekend crime tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
