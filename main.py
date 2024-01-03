1aba4502dcff4e05bb078de2b82ccc9f8e2c42a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:54:09 +0000

    weigh sore sail reduction on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
