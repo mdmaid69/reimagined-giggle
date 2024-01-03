b96964decabcb3d923f787b5fb32268fe20f33ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:43:18 +0000

    design once prize out warn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
