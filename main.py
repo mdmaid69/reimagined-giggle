e7c8f067aef18c25442a445059851cd3135caff0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:47:03 +0000

    object Vv beginning convenient 179bite

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
