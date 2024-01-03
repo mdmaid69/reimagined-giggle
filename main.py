bfd2f3e1923bde5403d4a6de6c73d4f9836d8dee
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:38:19 +0000

    building punishment everybody behaviour long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def count_elements(lst):
+        return len(lst)
