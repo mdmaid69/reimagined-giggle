0011b6d48fd979aab3263ab68aa48fb18b420704
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:41:14 +0000

    war computer rare block change

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import tensorflow as tf
-print(tf.__version__)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
