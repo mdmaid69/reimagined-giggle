b6e40dfe1c18d36717f0c6f7850404993d1c50f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:34:01 +0000

    copy regret serve advise sleep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
