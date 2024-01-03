fa82673d7cf0c5ea7c620c41ad94a57707cef568
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:10:56 +0000

    that by invitation mad describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
