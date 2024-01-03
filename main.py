896ed7d9ece267c4deec2ac276dbf8d321018191
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:37:34 +0000

    movement family spray soon old

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def convert_to_binary(n):
+        return bin(n)
+import collections
+def create_counter():
+        return collections.Counter()
