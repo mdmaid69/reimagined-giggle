fbd37e05045a5cc448f9afc1633d59bbc2e0c31f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:26:40 +0000

    operate drink disappointed moon waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
