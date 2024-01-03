713adf2d994c8f6fe31e34088678b351d1f150e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:06 +0000

    strong Nn follow use almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
