c025f5d7758548d07290b2bbe8b4eeb3469e45c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:21:38 +0000

    piece trap maximum degree threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
