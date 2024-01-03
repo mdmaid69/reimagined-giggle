77b32ecc238ad15e45128cfa1aa8346b43111cba
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:17:41 +0000

    properly imagine damp congratulate effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
