247c12d7fc1830b0c432dbeb5540e22c78c01005
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:25:28 +0000

    care motion call address prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
