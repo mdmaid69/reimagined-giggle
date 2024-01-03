d9ff3c1c7cabb235634d861ebf91f699df1137eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:26:34 +0000

    spend appeal exaggerate television careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
