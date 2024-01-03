1d7ff2e73af1238a3106e38e2d6fc655ef98ac1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:49:53 +0000

    father slightly warn danger time

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_acceleration(speed, time):
+        return speed / time
