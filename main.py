def7310bc8874fe5636aa7ba9e7ca443d6a4be63
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:30:30 +0000

    ring impression that feel thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
 import sys
 def exit_program():
         sys.exit()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
