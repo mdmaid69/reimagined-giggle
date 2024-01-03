fec2c3f8a38b6855b781be16267685650bd16590
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:40 +0000

    street offend detail dry little

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import datetime
+print(datetime.datetime.now())
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
