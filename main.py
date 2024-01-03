f04ab297a3db1ae91ba325d057bc6a652dfc80c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:02:49 +0000

    confused provide wrap calmly television

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
