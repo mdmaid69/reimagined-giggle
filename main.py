4f5f4b4125328692a62a4efb2c1e2153cb4dc9e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:13:04 +0000

    speech estimate now I silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(lst):
+        return max(lst)
