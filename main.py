58ab7fd62eddc9487cd79305d39f9d31e90a8fb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:44:45 +0000

    train bye occasion cross public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-print(math.pi)
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
