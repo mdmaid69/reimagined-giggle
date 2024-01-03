210cb4e92617cd3f3798b3e3dc8720daa8d6053a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:33:03 +0000

    reject water fear speak mad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
+def sort_list(lst):
+        return sorted(lst)
