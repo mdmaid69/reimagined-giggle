6587d712041f6953b5e3ae6669ad76c62a93a30a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:13:27 +0000

    choice deep from now turnback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_max(numbers):
+        return max(numbers)
