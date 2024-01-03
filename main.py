24c611e972571d0e7bc33b2988b5ff770405abcf
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:33:36 +0000

    apparently neck firm man unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
