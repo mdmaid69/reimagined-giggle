0817e28d89e90dbdf3a563b18b0d2c4fbd4e75db
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:08:11 +0000

    occur rush tight doctor ought

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
