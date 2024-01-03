749e0501090bb8034ce1e347dad61615987c2f43
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:24:38 +0000

    way instant weekend surprise right

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
