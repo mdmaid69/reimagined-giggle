48a6ae36a5d1839c89c680a30d9f7e0d8ac55759
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:41:21 +0000

    thing separate machine shadow sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
