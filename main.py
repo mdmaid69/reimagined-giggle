7a1f4328ddaa62926f45f7b6f3d439c9ce1dfa42
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:10:45 +0000

    throw drive able average step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
