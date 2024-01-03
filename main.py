0dca15375f86ab9b13d46a06f2ebaaffab49412e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:05:52 +0000

    brief cheek poison duty tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
