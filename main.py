3b901a4122f262d6da7edddb64baa178df981c89
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:31:02 +0000

    skin mainly identify learn dark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
