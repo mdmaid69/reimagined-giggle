4cc59f7ba6b7214b07ca31981f2fcc4d457486a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:49:56 +0000

    tall project claim pleasure pencil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
