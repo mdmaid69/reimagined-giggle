076037bea0948033f85c6f202c3c8df94f489310
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:30:07 +0000

    impressed thank cheat crime lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
