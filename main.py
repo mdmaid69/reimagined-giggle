0bcb185cc75c4efc44dd52973e690862f34aac98
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:49:22 +0000

    deep activity Vv holiday before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
