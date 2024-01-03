7c3693c4681a7045b52033ceff7183a6958a98be
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:35:48 +0000

    next material check human point

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def reverse_list(lst):
-        return lst[::-1]
+def convert_to_hex(n):
+        return hex(n)
+import os
+print(os.getcwd())
