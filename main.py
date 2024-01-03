11c222661b7143bf5f636c662c101a983eb2c01f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:11:42 +0000

    Ww apart keen arm total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
