2535041d5c0169b55432efe3a239bd24e5f6aeba
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:30:54 +0000

    poor niggaacademy left issue turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
