eab476acb63f840f256e13c33aadd569c1b771b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:25:12 +0000

    comeround woman relax money row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
