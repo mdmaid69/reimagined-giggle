04f0119bad205b8bb06cc24a9d7c543d9aa3208b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:02:44 +0000

    our report lift prepare capital

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
