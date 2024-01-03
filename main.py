90f4f567b83a2dad4e5c11b5b9a50e957ef0945c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:54:37 +0000

    treatment just depend lunch again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
