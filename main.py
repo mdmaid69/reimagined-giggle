3d85a4afc652c1b75f6c4d109e234d296e3be260
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:49:58 +0000

    transparent blame school the print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
