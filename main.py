4183c61c991ca5cccf3267c23750a88dd5968ccc
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:06:57 +0000

    bottom party listen about tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-print(math.pi)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
