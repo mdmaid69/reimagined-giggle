3361629f74ff31ba0c58ef4f75e3b14fd2b935de
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:24:01 +0000

    light custom go approve job

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
