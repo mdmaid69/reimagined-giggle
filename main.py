b052becf89b26f70011b1deaa5e39ec87d776870
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:40:33 +0000

    introduction arm unhappy full rare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
 import os
-print(os.getcwd())
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
