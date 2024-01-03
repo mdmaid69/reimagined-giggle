0147a438f14bb744cc257e58bde05cb27d8d68bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:20:48 +0000

    hospital future rate spill wherever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
