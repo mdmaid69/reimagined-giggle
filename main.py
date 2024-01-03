6bfc0e66a58804d243c7e405565818d2076830f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:20:41 +0000

    principle pronunciation sudden holdon settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+x = 10
+y = 20
+print("Sum:", x + y)
