b5481fa07cbd612efcbd400dd0222dee99163fa2
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:55:01 +0000

    new environment mistake communication borrow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
