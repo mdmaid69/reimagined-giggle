e654a3a8000987e8956082da48fec3fc07bb90b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:42:35 +0000

    suckme pick achieve minimum gap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(10): print(i)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
