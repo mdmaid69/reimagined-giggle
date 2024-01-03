2280f2b5f164676bd4aeb549c91a19640e15fa88
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:41:22 +0000

    office drop identify less plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def find_unique_words(sentence):
+        return set(sentence.split())
