00c1b10a6670afb823c5720a467491b836c8ebcf
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:38:43 +0000

    cover handle Nn stage pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
