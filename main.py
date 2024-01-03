402f99d37b38febaf0361e6f380cc8b74899fad9
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:09:54 +0000

    wait goup group software substance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def convert_to_hex(n):
-        return hex(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
