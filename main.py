df2f52923a8b080bc72d44e37a5637699e17f622
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:49:51 +0000

    everyone favourite calmly contain luck

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
