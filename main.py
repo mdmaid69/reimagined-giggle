e543c2f89ff0ba1f3bce1099f50c13f04d0beb84
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:02:52 +0000

    rest computer keepout reduce university

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
