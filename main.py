b794196ae319240f2e8ed1ecf07b767b812f9a44
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:35:06 +0000

    ever principle blow forward calloff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def reverse_list(lst):
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def reverse_list(lst):
         return lst[::-1]
