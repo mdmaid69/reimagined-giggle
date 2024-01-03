3f09e153aafd204e92095e2972239bfdd604b96e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:27:57 +0000

    land climb squeeze post care

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_area_circle(r):
-        return 3.14 * r**2
+name = "Python"
+print("Hello,", name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
