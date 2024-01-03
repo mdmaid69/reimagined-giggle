0073445b31619b2c453e91341f228c41426f2abe
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:19:36 +0000

    trousers nor equal sleep after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
