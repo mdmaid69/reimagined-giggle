2a9d7e68f178343ffb674bb2cd9b633179e32c82
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:49 +0000

    build thing combine uncomfortable afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
