e2801c43a8bab4622ac10fa5142a2b4af2f82436
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:31:46 +0000

    interested completely create scene price

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
