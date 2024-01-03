a683df7af2607edd2ab67637bdce4c610134cee6
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:25:27 +0000

    evidence record under will emotion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_area(radius):
+        return 3.14 * radius * radius
