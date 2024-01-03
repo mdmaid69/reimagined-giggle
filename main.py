622c848de727bb27eb13ca9bb69f7a958a0b4058
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:04:11 +0000

    finger win light 571enough fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
