a4b51fbdcc9f31e159a8b0adf90d3a1be698c05c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:52:55 +0000

    square Jj movement computer formal

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def is_palindrome(s):
+        return s == s[::-1]
+def find_min(numbers):
+        return min(numbers)
