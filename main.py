b59cdf4d99a6b765a0c31880a85c395c47377c81
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:55:15 +0000

    say offer serve impatient stupid

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import numpy as np
-print(np.array([1, 2, 3]))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
