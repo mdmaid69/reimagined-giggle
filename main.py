9e141e75b57c40ec1c7b2e217c30c251768c4211
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:13:29 +0000

    ordinary exactly some achievement open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
