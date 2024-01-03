b353a4e0cb3ede0d3e77ec9714517bd2f665443e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:06:46 +0000

    explain equipment water hold about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
