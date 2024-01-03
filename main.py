d101fc9baf5847b613fd73bed93683f8d7e52988
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:48:41 +0000

    normal lend goout detail Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def get_array_slice(array, i, j):
+        return array[i:j]
