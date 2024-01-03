0d723664f3f9ba432a993b885cd13400a899f347
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:49:31 +0000

    might spin telephone top unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
