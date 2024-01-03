eb07b9399f7f37b8d273a9591ad8c8d810802696
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:13:40 +0000

    result patient cry on keepoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
