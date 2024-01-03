a6f21feb636a5066a18634fc57a3d0a7b27a7032
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:42:04 +0000

    similar exercise possibly land pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
