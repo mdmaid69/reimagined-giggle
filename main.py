d5443e3b679a7589510ebbfc71a36cbbecd6ed34
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:14:31 +0000

    gather regularly suit neither simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
