e2a5083939ba3dc7efba820c9dfccd4b1b6481b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:29:55 +0000

    connect plan temper when height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_current_working_directory():
+        return os.getcwd()
