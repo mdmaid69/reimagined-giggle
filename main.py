07608892c73003ed89c5e28dbce98749e155d335
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:16:59 +0000

    separate middle either poison care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
