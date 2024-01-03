78e2733a25ee3d8143bc6736da4a9799b21406ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:12:40 +0000

    peace you keepoff bridge music

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
