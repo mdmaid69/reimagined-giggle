f2b531dcb35f75a5d234c9a37de3fa2daffd5908
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:09:28 +0000

    along generally spill sink sick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def square_number(x):
+        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
