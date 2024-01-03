8f21821ba2be802a30a33e566f1f1e7b19b7aed8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:50:38 +0000

    move threaten learn mess party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
