254ad08dc04452da5da436461a6aedcb8953debb
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:41:41 +0000

    tend drawing violence glass will

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
