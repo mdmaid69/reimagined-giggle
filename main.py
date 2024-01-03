afe0f84ace175eb283b9e2d7add4624c4f2bce18
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:47:37 +0000

    always convenient centimetre giveaway line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+print(time.time())
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
