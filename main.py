486a75cf527c0519c0e7b7a74d7124595d44cae3
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:46:42 +0000

    star story defend stamp gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def count_elements(lst):
-        return len(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
