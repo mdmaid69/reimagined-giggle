443130e0e7be16fffbacb447dc4a61ef769469d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:59:15 +0000

    immediately approve influence emotion extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
