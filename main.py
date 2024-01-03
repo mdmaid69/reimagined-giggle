b48de96c112a661f65ea731ae12a9d451cc3b4e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:08:42 +0000

    trip debt responsible confusing distance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-x = 10
-y = 20
-print("Sum:", x + y)
+def find_max(lst):
+        return max(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
