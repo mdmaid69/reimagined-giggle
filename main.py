aeac167e0e747007e0a40099e615321e6394fb4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:14:47 +0000

    spring neither dream goout fall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
