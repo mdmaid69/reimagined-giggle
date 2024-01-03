c7e21317ef0ec3630de1e2101cd034ecc9d1717c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:29:33 +0000

    full definite true instant comein

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
