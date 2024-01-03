8993edd387ccec1ea361831e86b7154ca95c1d06
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:18:19 +0000

    appeal luck cover operation attention

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_power(work, time):
-        return work / time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
