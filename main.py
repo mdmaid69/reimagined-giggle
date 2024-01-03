5924884a9e8f588276a3c271058d7d8a7e5e96c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:50:27 +0000

    worth dull wrong transport prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
