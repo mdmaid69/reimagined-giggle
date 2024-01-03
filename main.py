e61c638845979d5473976dcdba9452692a778dee
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:23:34 +0000

    really storm 568energy Oo your

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_acceleration(speed, time):
+        return speed / time
