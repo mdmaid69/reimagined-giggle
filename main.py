4649635d99d44b5e95cfd20fab64c34cb6de033c
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:56:30 +0000

    gradually seem radio public touch

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
