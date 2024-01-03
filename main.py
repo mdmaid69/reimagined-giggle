cc19ce05e312394493d0d3d40dd288589aa77324
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:38:31 +0000

    send last love suddenly breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_index(array, item):
+        return array.index(item)
