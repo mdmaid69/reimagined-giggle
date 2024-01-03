85da6958b08f7824ddba8530253d4cece9901d0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:00 +0000

    warm connection reasonable kind say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import os
+def remove_directory(path):
+        os.rmdir(path)
