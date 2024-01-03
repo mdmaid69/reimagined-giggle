a47b029c2a5bee7d6d78bc39e76947d6cbfd7317
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:02:52 +0000

    window even runaway nonsense downstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
