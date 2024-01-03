f4d8c3864a4734abbc0abea47d1102c92e1b9dd9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:11:05 +0000

    lookaround glass soon relax money

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import tensorflow as tf
-print(tf.__version__)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
