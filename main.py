4ee6dee0cc92ec8b375f3e6063da3e78ca12b0a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:13:27 +0000

    which refuse chat waste stop

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Reversed:", text[::-1])
