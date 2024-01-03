0dee933f904213fd58c5d0283604202bf2ccd117
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:15:38 +0000

    middle begin turnaway sudden nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-print(os.getcwd())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
