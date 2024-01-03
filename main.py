d21e6b504f325a2191cf192641df6a959ecc739b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:43:37 +0000

    problem flat hollow naturally explain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
