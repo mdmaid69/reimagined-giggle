e17ae102526011fb962456283e4faee27f338272
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:00:36 +0000

    accordingto tape 567enemy mail she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_bytes(array):
+        return bytes(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
