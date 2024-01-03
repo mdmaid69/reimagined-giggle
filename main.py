fcc7c424e0769e5a6246f5b16653d0d064cd1754
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:59:03 +0000

    tape far fair base afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
