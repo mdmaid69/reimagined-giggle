39208fd7d9099f261f629f797507d8152337caf2
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:30:51 +0000

    niggaacademy circumstance lookafter remember light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
