edc2ff1947ee3c7a4624e8be2d54150aabfbdc0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:20:29 +0000

    undo it threaten recover attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
