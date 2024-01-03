084b9a3eee936bf8eaadeee6eb1367d82257e52c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:14:07 +0000

    famous flow mainly interview estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
