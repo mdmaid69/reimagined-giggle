7cb2c649d15af75a9dd54f2ffccaa44c7cc78402
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:16:34 +0000

    size string and popular regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
