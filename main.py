57052249be53c67762af007c9526f01b55267c93
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:22:57 +0000

    bottom bound occur performance bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
