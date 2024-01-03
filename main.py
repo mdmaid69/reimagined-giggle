47ecd2aad4f9e8f294e71c5272247124ab3a0de6
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:03:29 +0000

    connected who not ashamed mood

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def is_even(n):
-        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
