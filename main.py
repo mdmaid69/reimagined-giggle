9a519305fe1812e3f669ebef365042caf3f69113
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:08:56 +0000

    surprise tooth like crisis wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
