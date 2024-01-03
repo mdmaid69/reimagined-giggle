d1628673ddc336456f0d3b357dd9c5bcea691573
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:42 +0000

    needle swim pass must lean

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
