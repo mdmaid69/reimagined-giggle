e4539cf2115d93a8c16c063abadf199d831490c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:23:54 +0000

    needle pain method ought perform

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
