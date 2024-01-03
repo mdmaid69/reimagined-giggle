9247a1380b91d76022676ab69dcc21a876556436
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:30:59 +0000

    slow phone hollow shoot seem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
