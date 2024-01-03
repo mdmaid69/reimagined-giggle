f45e609995f532334359df82d79b0d20d28adf25
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:36:55 +0000

    duty various rubbish wrong confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
