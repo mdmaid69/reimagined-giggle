7e2a694e2f49606186096d94b470dc28d4064f8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:09:35 +0000

    thank police calmly nigga but

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
