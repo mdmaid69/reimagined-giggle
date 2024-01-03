653b4415529debd8029c81c0886aa7f599a63f89
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:03:10 +0000

    unit so wall our want

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
