802329db7da1f3763bb35b359a4503a5d9608e03
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:40:07 +0000

    opinion chat yet within tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def find_min(numbers):
+        return min(numbers)
