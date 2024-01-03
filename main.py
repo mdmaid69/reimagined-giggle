fedb4ecbf3c28797ba03b741ee45208d503083b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:33 +0000

    content 572enter secret able go

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def is_even(n):
+        return n % 2 == 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
