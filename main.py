08447bc83a4161dd12a36ad4d77b0694744d31b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:06:56 +0000

    onto impatient star drawing shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
