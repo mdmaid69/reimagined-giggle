1266fbdc552223817f9360d69f9520d018047475
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:53:58 +0000

    through sign positive instrument smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
