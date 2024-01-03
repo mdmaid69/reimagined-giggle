0a3f6636f2cb5291cec64d4e00ab3c210a4059e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:35:00 +0000

    under allow shout exchange though

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
