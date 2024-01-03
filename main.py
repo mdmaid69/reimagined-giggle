47619202eed0ec25d5ae54960cd1d92684ead463
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:31:12 +0000

    hair movement mainly mind freedom

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+print([x**2 for x in range(10)])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
