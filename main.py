b500f3faa453c29925c1382266c5ed3a2691ffc5
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:38:04 +0000

    offend tired direct left exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
