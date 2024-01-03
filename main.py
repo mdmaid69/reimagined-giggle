b660b4c892536f308c1b88ce56162928cc0dc041
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:42:21 +0000

    far whatever pretty major harm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
