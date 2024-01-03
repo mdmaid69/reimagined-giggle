57cea543944eb35b2624a61d6d1c080d08aa3811
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:27:14 +0000

    mother fall importance between explain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
