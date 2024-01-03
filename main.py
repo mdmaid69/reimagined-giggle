6439797f114778578fb0f948c7b255d41d6dc4f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:45:58 +0000

    our clothes judge calm modern

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
