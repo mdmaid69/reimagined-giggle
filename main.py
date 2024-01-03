762099b7a8e37add19d325c05907254ce929f73f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:37:54 +0000

    stare here slip view excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
