0b04c06d4662c8640558d45f5d0b28a6343ca1b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:07:38 +0000

    chin trip without suggest male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
