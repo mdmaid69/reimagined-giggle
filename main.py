7f1d722fcc9f6afd19a4787525d53a07cbe53a8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:27:45 +0000

    scrape feel onto dark slow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def split_path(path):
+        return os.path.split(path)
