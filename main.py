b34d872134d0f9e46fa9d3793d458d60b0988c55
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:24 +0000

    season content welcome exact final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
