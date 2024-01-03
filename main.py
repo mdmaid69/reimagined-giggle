197c465c23dde17a626cf752c5456313c6d751ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:54:10 +0000

    lookaround score list autumn ago

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
