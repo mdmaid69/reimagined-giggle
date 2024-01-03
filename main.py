29ec77e9accd0e1156403c26b84c8b6a62f0b050
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:40:31 +0000

    sure aware friendly depend trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def count_characters(sentence):
+        return len(sentence)
