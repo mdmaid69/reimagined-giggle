352f9e151e9c8d7a39b75fa449cea69ca6ffa167
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:40:06 +0000

    pause mainly work ocean remain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
