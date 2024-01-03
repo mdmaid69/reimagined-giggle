ed8e0bb037c57f725708b7a11f9c53c5deb585f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:56:56 +0000

    fact people it bar true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
