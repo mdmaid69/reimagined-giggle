d42ad1121e9759942f5e6da98b220e5486d5232d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:11:51 +0000

    window slice tape anything force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
