6470e245440982e71e9693777f64f303ae39b7f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:12 +0000

    proper thorough off less light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
