86a77ce5af38c6125c761a46eadc167e738dc154
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:38:13 +0000

    visit grand gentle remember wall

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
