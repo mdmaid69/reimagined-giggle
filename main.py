07d206b62a6d70730c23dcbc32d7e53087554626
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:27:42 +0000

    deliberate Cc table future completely

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_pressure(force, area):
+        return force / area
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
