da33b03a3535aae31be5e03ac9d3d35f4b8a3457
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:59:51 +0000

    mood hold Gg creature breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
