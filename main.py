1921cec6913f29903478927c4a053ed94a9d90f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:24:23 +0000

    special minute now him cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
