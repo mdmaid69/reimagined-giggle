e56c6f9e243b681477aec247c033eef64890e9d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:57 +0000

    corner clearly pleasant do aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_octal(n):
-        return oct(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
