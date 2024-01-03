21b883836afa67123260ddf73af53c55254b5772
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:50:32 +0000

    depend bringdown swimming up market

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_factorial(n):
+        return math.factorial(n)
