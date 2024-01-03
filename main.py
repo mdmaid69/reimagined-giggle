94cef28c9518a3b7f1add0c9aad4fd68106e984c
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:52:28 +0000

    major taste company party moment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
