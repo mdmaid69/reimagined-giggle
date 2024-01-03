3524846697dbd60728838689d9b4b02ae81d5bca
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:54:44 +0000

    drive train limit wave tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
