e7ada6dc26a8274a27a915f9f1ab312267594f2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:16:32 +0000

    supply issue field spray section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
