6f1df0c5dce8446bb2d73649aaa6935b4e54b3d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:52:07 +0000

    so try pile trip short

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def subtract_numbers(x, y):
+        return x - y
+def find_min(lst):
+        return min(lst)
