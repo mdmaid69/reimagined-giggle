bcbd0a55db98d85572ec1258fafc95726f987696
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:44:51 +0000

    even but section too exam

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_max(lst):
+        return max(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
