9532bb96651c35bd11b91f8f259bde02e107cb4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:24:18 +0000

    air bang trick Ss record

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
