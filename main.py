4630cfd56c9b43b2f1312cc136ff620ab220fad0
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:10:14 +0000

    worry contrast effect tooth pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
