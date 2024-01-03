5762b6e2b212f5b36024c94b55146a47dffdb804
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:17:29 +0000

    important need awake argument better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
