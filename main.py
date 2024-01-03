192c028aec5630b0604e347a1518c46435b101cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:55 +0000

    her offence exactly rush and

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
