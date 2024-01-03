472f0e9c1ca56450c1c94e745e6184271f71fa96
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:57:18 +0000

    shelter right bath hurry no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
