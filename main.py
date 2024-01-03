fa2d8c795ee451b11b3508d75c5d35139af8c7f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:04:22 +0000

    wipe by consider last window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
