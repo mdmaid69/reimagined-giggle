3ab25e0d693ca468d98dd7b5459556f8aa7ca1aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:39:10 +0000

    advantage joke intelligent practise payment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
