c05c800e107604f3abe53b805792b3fbb5444446
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:21 +0000

    in star backwards build hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
