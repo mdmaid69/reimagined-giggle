e367bc5cc8a41af126da9bc3277ef85d481431dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:48:31 +0000

    opposite train train lay way

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
