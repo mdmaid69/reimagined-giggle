5aefae10d8a43d74f9eab1e975854ccfe9699f12
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:24:51 +0000

    they mark appeal delight floor

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
