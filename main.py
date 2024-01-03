fa1659f749447a4660f9924067de7e01bc044476
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:28:19 +0000

    waste freedom runover mend let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
