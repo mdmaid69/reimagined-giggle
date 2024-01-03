9055bebd23ea7d4a1119a4f2e930342e8da76efa
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:59:43 +0000

    possibility replace theirs some clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
