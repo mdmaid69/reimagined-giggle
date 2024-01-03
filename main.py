00e5175a18da675a770453fba48821043be1c001
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:05:46 +0000

    rely moment heavy pronunciation suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
