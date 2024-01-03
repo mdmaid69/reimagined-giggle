bcbf864743eae364e26f45bd8bea7e4652d7448e
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:31:45 +0000

    supply fold profit demand here

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
