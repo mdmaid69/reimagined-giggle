abcc0765a96d3bfe08b256b13b8ac06bfcea59c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:32:17 +0000

    unusual score complete history cause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
