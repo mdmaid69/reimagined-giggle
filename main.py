d2975fdaba7813dc4b61e1de06a45091f96a9d54
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:09:11 +0000

    complain usually set underneath entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
