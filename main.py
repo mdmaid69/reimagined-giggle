b0afa7ea0ded6ebaddf964106776cdddc996486a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:58:19 +0000

    weak open only hope twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
