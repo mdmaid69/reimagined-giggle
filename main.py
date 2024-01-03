7d2bc9f970b66e099ced5f9f49b9c84fc865c4af
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:04:39 +0000

    deaf near discovery power time

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
