a8d30a363ba1a347c5dd57bace152744fd589965
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:24:39 +0000

    before go attach message load

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_item(array, i):
+        return array[i]
