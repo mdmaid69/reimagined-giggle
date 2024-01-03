8ec19b44c30d866dbe8ac25dc5bca67fec824540
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:28 +0000

    plain danger wall mine cross

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
