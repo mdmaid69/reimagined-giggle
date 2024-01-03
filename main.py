b18516929cf6484fc4ee34f8c2d390bf0cf38ae3
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:42:16 +0000

    label listen either fence occasional

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
