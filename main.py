9943860c54b168e85eb9904398a8a89e0373d900
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:48:49 +0000

    sight heart without positive ignore

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
