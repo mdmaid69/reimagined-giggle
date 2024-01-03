3823d1405689829c32943d7bcb7ffb1a08bb4306
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:50:22 +0000

    spring stage adjective protection brother

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def find_min(numbers):
+        return min(numbers)
