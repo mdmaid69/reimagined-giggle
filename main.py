ca9d91fcdb4bf092366a9f8f181a8c90ab628f86
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:41:00 +0000

    repair risk lunch comeback fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
