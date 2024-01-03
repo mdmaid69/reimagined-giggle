da084352ad64be38ad993ee105379a8310778b43
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:00:00 +0000

    situation protest course afterwards height

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_item(array, i):
+        return array[i]
