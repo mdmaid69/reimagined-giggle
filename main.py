f43ecc888542209013c38c599ab7d1386749710a
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:21:30 +0000

    invite guilty among false nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
