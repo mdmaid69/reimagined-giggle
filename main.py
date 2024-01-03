cfc642ed69e1aed6b737c9455591ed063e90b00d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:40:13 +0000

    permission uncomfortable avoid truth but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def remove_duplicates(lst):
+        return list(set(lst))
