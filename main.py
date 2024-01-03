b9431e30353e94d950dbc128ebf6958ecdb2b887
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:55:04 +0000

    live Cc takeout estimate discussion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
