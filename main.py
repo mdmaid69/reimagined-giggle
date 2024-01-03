14eb8086a4ec86ff2e158784ca96722f61652d09
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:28:04 +0000

    travel grade round practical order

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
