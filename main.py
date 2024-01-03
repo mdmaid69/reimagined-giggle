c4d16a5a5707dd5f05e3ca4d280614bab97fcac8
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:10:37 +0000

    plastic about dick maybe comment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
