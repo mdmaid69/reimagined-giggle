0617f947fba8aae06337ebbcd4afd38b725d8167
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:22 +0000

    little lay immediate confuse swing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import getpass
+def get_username():
+        return getpass.getuser()
