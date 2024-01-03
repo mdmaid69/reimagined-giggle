95f83cc5fbbd969d128277d6c1714b67918e0f34
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:23:28 +0000

    climb exciting Bb confident emphasize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def reverse_string(s):
+        return s[::-1]
