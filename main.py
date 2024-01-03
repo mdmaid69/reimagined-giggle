cb328093e9959f223d5dce3c474fbe7a4d0627c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:51:57 +0000

    repeat rate form personal name

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
