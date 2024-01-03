e4f111ee7fee30d3e77d3876358dc64420abda02
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:41:00 +0000

    brave 573entertainment towards ours shape

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
