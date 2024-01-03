08ab86ba2bf8249f1e7b64d2a3aedeedde69bc5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:17:39 +0000

    runinto perform right recent laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def convert_array_to_list(array):
+        return array.tolist()
