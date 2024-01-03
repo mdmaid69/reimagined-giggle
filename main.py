eb1c2edc3cd9695e7f56f7facb4b521b240cebc6
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:41:08 +0000

    wander knock into hello comment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def square_number(x):
+        return x**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
