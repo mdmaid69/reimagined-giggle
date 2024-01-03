923216713b545890fb80b016b12199d3d3932145
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:27:28 +0000

    Dd spread post learn on

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
