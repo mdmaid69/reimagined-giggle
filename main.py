222f5f5f9f570a8973dc50bf5188056e1fc30a84
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:24:11 +0000

    truth conduct protection direct Dd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def reverse_array(array):
-        array.reverse()
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
