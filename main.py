8a7db7742f11f46ca133498fc50022f14780081d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:22:32 +0000

    later drive introduce soft comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
