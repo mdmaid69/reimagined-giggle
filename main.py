042c44483dd110f95448bb3b4f48220076d1ebc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:29:28 +0000

    row from intend right sink

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
