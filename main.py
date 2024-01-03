785a2f397cf987811e6d2efe290f21767472b102
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:53:47 +0000

    woman main invitation edge arm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
