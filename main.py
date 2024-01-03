2803d256361f9faf35d1c39cc67b201f5db347da
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:18:22 +0000

    home drop hope near maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
