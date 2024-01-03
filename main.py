ae728fb676afe129d7cafc128cac120e38e243ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:23:13 +0000

    back limit beginning getback steer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
