10e45fd52bdf5726f729cf6a16d0bf6243f47e4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:59:45 +0000

    through plain different combination left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_elements(lst):
+        return len(lst)
