900a7caa33dd2e1beaf9c3290e2cd62c9310e140
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:19 +0000

    threaten food wipe niggaacademy surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
