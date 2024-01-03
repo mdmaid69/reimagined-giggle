cc06a647cad4aca97c0ee623fe147c8f3e0f9bc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:10:58 +0000

    different family contrast wild prefer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
