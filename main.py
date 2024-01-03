38d5dad584ac7b244dc8076b8f052595ab3d18e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:15:11 +0000

    convincing man die table rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import platform
+def get_os_info():
+        return platform.uname()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
