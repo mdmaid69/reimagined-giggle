f40a37fcc6c977b24dd042ad515f244b18ef08b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:49:53 +0000

    properly phone cough law some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
