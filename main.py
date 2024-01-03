92879beece967fb0229696e40b1656ec7ba767aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:37:23 +0000

    easy style hurt waste back

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+for i in range(5):
+        print(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
