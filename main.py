1322a5f34591a92cd841fa3f8766e6b63b6528a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:25:32 +0000

    dress perhaps hand ear willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
