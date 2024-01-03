0e80a18a145e02db760af435bcc260c22c9cdd0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:20:46 +0000

    contribute impression take aware apart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
