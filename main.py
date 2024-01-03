594d00dc218d80ba7d30099596d3ff6565ac78dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:04:41 +0000

    somewhere improve alone flat Zz

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
