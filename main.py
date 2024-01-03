695e3595dae1d825fb5aaeb285479509b3ad1e11
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:25:20 +0000

    side clean stupid taste church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
