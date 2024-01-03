e6f96622d6062f62051a61c2bd7e04fca63d015e
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:30:21 +0000

    characteristic increase little expression bread

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
