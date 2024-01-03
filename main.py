9a9ff2a98d74154b53b6697c95b9b378e379f533
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:36:43 +0000

    remove rubbish strike inside thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_even(n):
-        return n % 2 == 0
+def remove_duplicates(lst):
+        return list(set(lst))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
