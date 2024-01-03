1be4740e0aa4088e7d76f9cc30ac5019684ac325
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:10:25 +0000

    serve correct dance Nn Jj

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
