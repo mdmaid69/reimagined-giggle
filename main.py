f86fd55b909696044d6df1de0ff60828a169cee4
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:11:55 +0000

    tradition place paper stage tread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
