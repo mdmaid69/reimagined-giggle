0e16231e7a9506eaf3d2213c574e5d34a5b26d6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:52:35 +0000

    tax giveback kind without moon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def sort_list(lst):
+        return sorted(lst)
