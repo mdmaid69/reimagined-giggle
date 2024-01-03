5097bb7c5c3ce28c2987efd916609f4d8256fedd
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:07:13 +0000

    find probably can stop nice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_int(array):
+        return int(array[0])
