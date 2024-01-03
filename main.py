993d92945444015e80538c8881a1ce1d564e601a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:39:55 +0000

    polite form negative available change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
