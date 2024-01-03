0b2a8698abca256b58b200f4ad9fe78c7c5b1142
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:17:10 +0000

    backwards special water rob wait

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
