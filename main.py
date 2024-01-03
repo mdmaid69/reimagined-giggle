848464be45db2eb3385885b18c92d8d0f82718b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:38 +0000

    judge collect would worth wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_rectangle(l, w):
+        return l * w
