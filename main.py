dd40f5f573e79458c29dbf5044a482ec928fb472
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:03:32 +0000

    rub keepfrom tone joke possession

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
