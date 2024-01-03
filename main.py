38d7d8d588c27edbc7b2efd23ccba526fb878e33
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:10:12 +0000

    murder produce support operation comeround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
