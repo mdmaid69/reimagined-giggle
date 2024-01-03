13d65dad031a667663852a0d444c54b3f9308d4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:41:52 +0000

    sound represent environment scene keepaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
