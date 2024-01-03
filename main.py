4d1d16feef9c78c57132e7ef4dca6c5725cc7c90
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:22:28 +0000

    little gap try call brief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
