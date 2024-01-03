9bc44c2d2e022323c84994ebb09f0471b5d68fe5
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:08:48 +0000

    suggest practical 569enjoy pause cool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
