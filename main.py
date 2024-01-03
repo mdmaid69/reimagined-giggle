aa216fd72bb443f3f8d7d9c18268763d65a82efc
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:04:03 +0000

    tooth benefit father petrol your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
