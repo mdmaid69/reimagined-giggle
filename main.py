f7523c60fbc24145bb215b6a87e33da774735614
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:24:23 +0000

    deserve slide luck against usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import sys
+def exit_program():
+        sys.exit()
