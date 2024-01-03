fba810a878ab11066a5f3d19db0a85446f0dc88c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:49 +0000

    runout rush wherever accidentally know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
