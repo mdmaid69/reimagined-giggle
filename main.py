3f97f63d225e3361187b22e81467f7e5ae257024
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:54:47 +0000

    appear smooth oil down brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
