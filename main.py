ae0ca022d55045aea72670cb40195a3b96977011
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:58:04 +0000

    cheap draw scream progress workupto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
