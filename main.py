59774465b2d1c7776e08802f7542744327006573
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:57:08 +0000

    criticize complaint next include fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
