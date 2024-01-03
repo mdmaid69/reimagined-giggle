cac4f8662362dc9ffe87d8b57194a533e4e0e1e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:30:43 +0000

    late disturb fire touch then

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
