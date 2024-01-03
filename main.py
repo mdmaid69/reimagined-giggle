394ed40917213bd1dbfe7027e184893bae87d76e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:36:55 +0000

    face average hello low key

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
