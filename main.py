9307623fd2cdd97908b446c3d5d01d835cb5b36c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:59:01 +0000

    argument 570enjoyable previous song should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
