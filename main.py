2d58fe8f78b3fc3c4f43965e0e9d8e932f7f124f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:15:01 +0000

    Bb yet power strike artificial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
