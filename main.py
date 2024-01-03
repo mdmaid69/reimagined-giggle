864414a4a725024bd3d07e13f5e6ab996c391c60
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:52:15 +0000

    perform responsibility steep entry opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
