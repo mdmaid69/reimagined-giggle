11b8370b0f95c82d897fa74e6e937b69dab9ca33
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:35:57 +0000

    recover breakout increase shoot responsible

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
