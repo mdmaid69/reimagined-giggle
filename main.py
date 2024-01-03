dffed7cadf893f3da50062566a1e15917453c36b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:40:23 +0000

    dig activity address authority discovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
