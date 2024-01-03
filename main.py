d045062adbc6fbd38ee657b77a44e6482646de5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:42:21 +0000

    pause anything breakup earn quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
