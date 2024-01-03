bdfb3bff71a564f2105ee95ecdf626873fd03d57
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:43:43 +0000

    empty worth gooff dance 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_buffer_info(array):
+        return array.buffer_info()
+for i in range(10): print(i)
