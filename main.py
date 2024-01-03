e2b35fa2fc70fe4f95fa113cf9ee557a85e68bcc
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:09:00 +0000

    star beneath lesson question holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def reverse_list(lst):
+        return lst[::-1]
