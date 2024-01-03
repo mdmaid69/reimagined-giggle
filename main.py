260bdf6fdfeeeb97c6ddbab32471f79abb249e41
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:10:50 +0000

    important shoulder murder rent march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
