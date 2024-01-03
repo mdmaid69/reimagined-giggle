ebd22d66577783f072116c5eb62dca715cdeb4bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:13:32 +0000

    funny meeting slice name enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
