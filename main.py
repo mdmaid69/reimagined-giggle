8df123f76d1089ba8312c3e308538281db9e213d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:35:00 +0000

    clear next wear empty name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
