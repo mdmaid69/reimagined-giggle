f8ca1c8933203e5afa5305ad49aed505401cf826
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:03:18 +0000

    plenty use practice trap load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
