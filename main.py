46814d5deec53d0e87db9822dc7514cde97b9509
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:25:20 +0000

    permission share after ahead creature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def greet(name):
+        print(f"Hello, {name}!")
