a56220ec1bcbc6703e5975115b641f9afae72c4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:50:01 +0000

    apart as affect speech clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
