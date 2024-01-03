98b5b76fdc9b4afee181ebdc8a66186fc7f735bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:30:20 +0000

    whole reserve most Ww exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
