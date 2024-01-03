c31bdaff3376d5e55cb57bfe89165240fcabe540
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:33:40 +0000

    hard remove boring avoid confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
