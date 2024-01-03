247f36bbcfa140b638ede5f52bf39fa6b035237b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:59 +0000

    thought block boat birth believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+for i in range(10): print(i)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
