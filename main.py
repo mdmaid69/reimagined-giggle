319cfefbab9f1fe0847f272b2717771837cd4e01
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:24:30 +0000

    careless again cheque snow trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
