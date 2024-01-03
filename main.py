2d6311660b8bbd64525f6c332ab403382e6bb53f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:22:49 +0000

    wide shout succeed accommodation present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def remove_directory(path):
+        os.rmdir(path)
