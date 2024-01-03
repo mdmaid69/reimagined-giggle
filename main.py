b6d897203ab7b0f739f8df871a9f27029c961795
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:44:17 +0000

    turninto whichever nothing colour health

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+print(math.pi)
