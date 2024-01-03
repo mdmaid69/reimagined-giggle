6233cfb5ddace21c268bae6715d95b31955352d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:00:26 +0000

    evening separate calm catch takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
