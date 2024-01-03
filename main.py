b63964a010e0890a0b9c8f567215dbfc2fcdfdca
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:21:07 +0000

    public pollution bet through level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_str(array):
+        return str(array)
