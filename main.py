ab7c368dc16626e0bf59e8b11ca1f60b5bb7d260
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:18:23 +0000

    exaggerate somebody now she floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
