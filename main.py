706f368670d7b0d82bb61a4c34b44568b1b04d92
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:34:24 +0000

    him race Ll pipe stranger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def reverse_string(s):
+        return s[::-1]
