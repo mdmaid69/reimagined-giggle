46e1e02150c496db5bd11e2efa00699e20119d40
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:35:54 +0000

    smell winter emphasis achievement black

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
