c8bc52c778c3a59bf67d6c59eb1081b89c163056
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:35:13 +0000

    talk unfortunate food block if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
