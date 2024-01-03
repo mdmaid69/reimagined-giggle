f3a8a3015c932fd0fef24ab21836c23fb139d2ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:57:04 +0000

    fix spoil laugh pin too

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
