f9bfedfbe2c35d118531dcbc64b6c50ce116fa61
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:44:38 +0000

    wing bet gas sweet land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
