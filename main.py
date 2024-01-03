ebff317388c7957b2207db1c97f61f6e7a4a904d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:43:19 +0000

    almost whom sensitive scene motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
