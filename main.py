f5d335be2a5f87a4df64f23718739c8a5966a2c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:02:55 +0000

    decision pencil slice argument share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
