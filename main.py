da88571b2c9f2df15469cc92fc8190edf9d4003e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:11:51 +0000

    happen explain treat could punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
