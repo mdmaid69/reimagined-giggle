521cae635bf178d2996b982eda084b9f784dcf18
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:13:57 +0000

    of complicated fail die exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
