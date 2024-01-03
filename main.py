4c9c1a325c6b9e5985236b8961b2bbe0b20a0753
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:12 +0000

    shout film number nonsense keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
