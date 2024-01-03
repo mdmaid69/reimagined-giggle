c0cbb271d0998b269611a0746af5dc8f99f449a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:21:38 +0000

    holdon land bridge off comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
