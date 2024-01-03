a9cec6be155900db262b80e754e9daaeac6861bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:17:37 +0000

    gentle again press innocent why

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
