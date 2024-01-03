edc098d52fd695a93be490f32c927f9a76dc6810
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:14:13 +0000

    talk destroy maybe shape but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
