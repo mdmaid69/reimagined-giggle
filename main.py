67455361da829190bbcd21360ca3444097c1175c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:52:35 +0000

    fire slowly punish plastic surprised

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def reverse_list(lst):
+        return lst[::-1]
