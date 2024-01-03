1efb045cb3b9625f153c6bb5411e3f1be1dfac2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:19:32 +0000

    phone break his crowded judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
