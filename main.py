ab18b296a0bd06959ce4d19e8b244813036b0e2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:44:40 +0000

    realize mass discussion alive fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
