26b478a58611e2d25e392dec06c06fc3610cdf91
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:14:00 +0000

    dead secret way pity pull

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def sort_list(lst):
+        return sorted(lst)
