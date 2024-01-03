db16642ac744cc091807f59d757e6bb094806990
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:05:10 +0000

    health race approve only 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
