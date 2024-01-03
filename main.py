f7945281233dcfa77082ae6bd948154d6e6c12bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:39:47 +0000

    open satisfactory treat will damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
