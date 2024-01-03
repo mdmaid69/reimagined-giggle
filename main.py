48579ba41e8ac5804806606adc42644ae8216be8
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:59:31 +0000

    read test jump getback punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
