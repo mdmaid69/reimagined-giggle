9d16bec950dbf5f0f2582a7d031e038b1dfc1221
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:52:23 +0000

    holdup difference paper afternoon silent

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def sort_numbers(numbers):
+        return sorted(numbers)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
