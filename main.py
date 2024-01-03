b3bb684222e91ef16bad5b40ff9b1e95264e7c7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:21:42 +0000

    question sport letter same hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
