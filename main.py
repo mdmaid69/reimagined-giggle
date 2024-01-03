1962d1df58f3d9f436e2a7de09686be93859b1a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:00:11 +0000

    honest achievement serve behave deaf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
