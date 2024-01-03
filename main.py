fb59ac6c74cace9420592886a00d05ed86385317
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:42:45 +0000

    organization lonely replace quiet before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
