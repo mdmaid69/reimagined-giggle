1cbe1056f30965477b278f2b6df4529af0defca2
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:38:13 +0000

    influence skin challenge nerve appearance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 def convert_to_binary(n):
         return bin(n)
