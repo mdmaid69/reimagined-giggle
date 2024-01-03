1a673377583186e784eb6ea6d2dee0507aadcacc
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:37:52 +0000

    cost broad mine cover poor

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
