bcbedd9e7a076517d04fab9b895634b99c8ad29c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:12:08 +0000

    clever lift thinkover shop fix

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
