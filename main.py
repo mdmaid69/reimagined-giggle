7294c4271432e862555e14db0bfe45df0c980527
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:02:34 +0000

    promise Bb interrupt common lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
