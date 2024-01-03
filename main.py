bb390863ab03fc5bff281b0772bb0c1e579fcc08
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:32:51 +0000

    wave file should takeover cheque

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
