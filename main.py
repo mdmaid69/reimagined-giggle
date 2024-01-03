d6842e995caf0bcb1281f9be70bb1e0c8e3e8821
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:46:50 +0000

    reliable shut spoil alive record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
