8c0824143ee8286ca76ef4258212639cdceb4dde
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:17:59 +0000

    soil activity downstairs real favourite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
