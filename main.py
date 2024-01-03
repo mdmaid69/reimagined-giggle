01372bdc5ac0e5e5af15832c666c8d8a2a7ce46d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:18:01 +0000

    interruption fire offend book doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
