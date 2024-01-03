3455baac9cdf281b6d13add3ccec9a2ef9148d45
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:58:33 +0000

    hurt common beautiful spill able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def split_path(path):
+        return os.path.split(path)
