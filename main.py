fa9903cee0f757cb0c2b94f159a472eb9b7806aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:26 +0000

    once hospital able tape race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
