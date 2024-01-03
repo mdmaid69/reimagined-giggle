3875f23523267782ffafedc36a51547c575ba5b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:47:10 +0000

    gradually close score fond conduct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
