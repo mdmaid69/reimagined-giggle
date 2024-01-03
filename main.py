5ee1cd83f3458ff83bc2850b3062d64af619ac51
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:38:54 +0000

    finally just rescue error goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
