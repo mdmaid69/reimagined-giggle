2b1f95ff55b42958c590bff15c586efee10f6f4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:52:14 +0000

    prove question prefer combination please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_work(force, distance):
+        return force * distance
