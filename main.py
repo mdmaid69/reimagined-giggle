bb65e898b96efc5571af34eac199e1527156807f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:30:52 +0000

    advantage hate scrap Qq estimate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
