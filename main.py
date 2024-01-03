27b726b4f8498d5bdfb0b7e4a7f26639415c7aca
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:17:28 +0000

    seriously law authority physical main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
