b2c63fa21a07f553d3591331d2fa096a2538906f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:21:06 +0000

    equal approval mixture attempt arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
