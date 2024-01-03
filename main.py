7c583870a546ee4a3455766f022716a568f3da45
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:28:23 +0000

    control kind under not violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from collections import Counter
+print(Counter("hello world"))
