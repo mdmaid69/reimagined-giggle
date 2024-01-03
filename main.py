3eda85fb1cc1824b35037476e951c96973d23eec
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:55:35 +0000

    character mend confusing prefer strange

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_odd(n):
-        return n % 2 != 0
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
