300b2226003868f42f4bbc473aca4bb375b97078
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:01:53 +0000

    future earn us pain us

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
