2a33264534b186338d1c92ad6cda07a515c0cea3
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:07:54 +0000

    receive miss soil total closely

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_int(array):
+        return int(array[0])
