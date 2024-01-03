63cc2c85f13f238f1c70cf876c2bf3863e046a64
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:46:02 +0000

    reply waste little getaway yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
