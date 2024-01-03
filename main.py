62cd85768756a12ec60bfc8769e8444d170f6a5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:31:49 +0000

    grade tonight their practise matter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_work(force, distance):
+        return force * distance
