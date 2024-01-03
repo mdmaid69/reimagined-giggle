597df2b54df78df02ce9bb77d8613f491fc177e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:26:12 +0000

    business left instant demonstrate concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
