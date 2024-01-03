ceb2c19b43b56d13ed41b5e5108c1d77c7f93daf
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:58:28 +0000

    space agree share pick environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
