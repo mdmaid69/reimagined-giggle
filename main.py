49406a0dce0dcbe232c8dcdf05ab42c6f18a3024
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:21:27 +0000

    weak deserve female door both

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def is_palindrome(s):
+        return s == s[::-1]
+from collections import Counter
+print(Counter("hello world"))
