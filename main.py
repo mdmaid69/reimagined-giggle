1ef49a74ba8120267f0239b3af1f98fb38294461
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:49:26 +0000

    apart pile fierce trouble ought

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
