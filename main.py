bd3c6d76fa675aca188c002fbfe7427c2a0f310f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:34:14 +0000

    Ii takeback represent handle valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
