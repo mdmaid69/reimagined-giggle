99650c7e4fc56d8b21d9ee392d754477d80d6af6
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:05:11 +0000

    design back rather offer unkind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-from collections import Counter
-print(Counter("hello world"))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+print("Hello, world!")
