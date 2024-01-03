88fcce2d7e5567150de70c3f3f4403ebd97dd6be
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:21 +0000

    lock shall when lesson possible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
