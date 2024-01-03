cd2b1608f15ec2d6a34c72ffbc9ce6496c584c80
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:55 +0000

    go principle interruption lot protect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import re
+print(re.match("h.*o", "hello world"))
