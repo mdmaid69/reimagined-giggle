0dbdc1fb4ca0f5ace821268e74fe7773754b7fb0
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:27:30 +0000

    rent great Ff soft violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
