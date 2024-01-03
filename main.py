d3a3a10c4f65cc7bce83f7f4e46be273dc6728e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:14:20 +0000

    shall calmly free everybody conversation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
