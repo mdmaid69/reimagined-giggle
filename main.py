62c7a005f485bc2fa0260dd00617fa7bb4f9ebdb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:46:53 +0000

    persuade build mess definite 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.time()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
