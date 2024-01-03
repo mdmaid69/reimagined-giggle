4d3ec4b7e959f604d2a03dfe2a22849e72f326ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:40:49 +0000

    easy free single depend opinion

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
