cdbd0f979020bfc9c79e4c1f5cce0bc2a4f2333f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:52:25 +0000

    chance yes pattern steal goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
