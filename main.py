7b8fb89071ebb0597ad583a68ef8aadfe8ad9bcf
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:05:49 +0000

    deep practical cut pretend complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
