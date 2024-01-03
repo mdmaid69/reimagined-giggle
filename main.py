313843a450fc816903978b8429a8d1c50cc5e3e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:10 +0000

    man active behind right claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
