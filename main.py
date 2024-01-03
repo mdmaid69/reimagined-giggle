d5fa7bb7e340d46e1a9accad0b33aeb51f9e82d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:15:27 +0000

    sentence way 570enjoyable describe cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
