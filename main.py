284f391fb037b4afe923ed204a72787d8dfef893
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:53:04 +0000

    butter church criticism children fond

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
