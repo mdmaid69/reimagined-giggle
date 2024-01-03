24e85e7175eaa783b4ee5ef4a0790fd5d4d8e88f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:18:39 +0000

    principle drive convincing open force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
