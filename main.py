3f1f978615c520d277053966a3fd2ff8d0092a87
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:53:09 +0000

    cry thumb onto action feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
