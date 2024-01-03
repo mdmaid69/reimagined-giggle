5eebb8c83a15ed42e82b5a8aab0d4deab0ff99c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:26:57 +0000

    cost she bunch rush lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
