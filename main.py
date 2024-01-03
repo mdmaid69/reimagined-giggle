efcfd925dfccad70f59ab259f4ef9c44f69bc097
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:16:58 +0000

    measure present support train exchange

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_pressure(force, area):
+        return force / area
