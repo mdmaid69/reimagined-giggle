75c34589da37d639c9c763a094066b2f552e48d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:59:41 +0000

    bit any breath eye however

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def cube_number(x):
-        return x**3
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
