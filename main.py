cc454f5a5ced2ec955e56de1ddcad0d5cfe643a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:23 +0000

    bound material violence scrape rush

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def calculate_area_rectangle(l, w):
+        return l * w
