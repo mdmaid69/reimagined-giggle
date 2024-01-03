79f297656bc1bb6460ec6bed4b3f5da2c3bc7199
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:39:32 +0000

    least frighten like uniform test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
