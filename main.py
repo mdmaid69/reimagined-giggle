3636e490fce11fcc73cf0c4def2ff155a7d414c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:57:14 +0000

    tap plan face congratulations disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
