f2d6871bebd171a8f990c0c3d9f1911bb194240f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:42 +0000

    glass Yy pleased hide serious

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+print(math.pi)
+  def square_number(x):
+        return x**2
