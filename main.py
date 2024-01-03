f5f99789cc4289a2fb13dfe11ef2d298c41774c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:27:57 +0000

    block foot near some 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
