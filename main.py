648f7f68e17707ee14a8021544edd30dc12dc7d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:37:48 +0000

    suck burst press smile table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
