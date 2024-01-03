276d816ffe41b7a74c087b67029f70cad06ffc59
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:05:32 +0000

    usual claim proof among hole

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
