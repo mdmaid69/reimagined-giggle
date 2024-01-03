0ca346a46e8cb09cc27041e41bb7131f7626bba1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:51:57 +0000

    circle choose early choose fight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
