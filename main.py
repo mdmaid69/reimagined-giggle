40b73294bf415a33f967b992895b2ef5efc9347b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:51:08 +0000

    claim number visit forget reach

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
