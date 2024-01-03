c4192f482ac42fb2688768f6e0f4137e79a628b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:34:19 +0000

    description character style blind sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
