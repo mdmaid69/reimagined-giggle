2bd0f7a0d8ce4a2cbf75855cb3f2c9c12ccb5e6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:35:37 +0000

    mind difference pencil extraordinary doctor

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
