6fcbd75bc453b9c342341aca3752eb940fb43eaf
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:38:38 +0000

    success rank compare direct Ss

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def sort_list(lst):
-        return sorted(lst)
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
