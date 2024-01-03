af9e0247d061fbed9ac649f31be47f812e5825c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:19:15 +0000

    beside stamp fond cost file

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def sort_list(lst):
-        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
