9ef64f6df1a42308dc911cd6c909930c27930753
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:25:05 +0000

    dig shape risk hate advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
