db69d3f59e24ae6f8dd4b812e72ca04c6db8b741
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:05:05 +0000

    any reduce come bag murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
