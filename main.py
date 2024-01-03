a952ae98cd5d0ef26a635d296ac599cd6b73e266
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:42:57 +0000

    weak turnaway neither comment yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
