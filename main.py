59127207e8294d9e4b68f4f622c6ff8587fb46b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:54:58 +0000

    worried prize exciting whom lower

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import time
-def get_time_since_epoch():
-        return time.time()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
