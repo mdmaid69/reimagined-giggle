95c27edeab7a477877ecd756faa2439cf4347e82
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:37:51 +0000

    thing season politics right compare

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def square_number(x):
-        return x**2
-for i in range(10): print(i)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
