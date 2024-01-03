be8bc9f1d54a85b26dc68651c175de8b31d414ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:40:37 +0000

    steam offer proud search half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
