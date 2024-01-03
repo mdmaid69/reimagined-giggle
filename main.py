565a1555609d7e87a5f1237b7fff09ba1ab1da4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:04:20 +0000

    dress to club science thorough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
