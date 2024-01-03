cfd5e1e1108ad4bdbe200018593d894488777b99
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:54:02 +0000

    feature arm far weigh basic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
