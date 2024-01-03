a71f5bfa9f6dabb6321912b657f1cb58cc12b16f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:52:59 +0000

    hardly talk art meeting plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
