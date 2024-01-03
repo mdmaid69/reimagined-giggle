07828a3d6e30bee9446f61e25850363cb6f1baa4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:07:55 +0000

    fit square contribute ashamed book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
