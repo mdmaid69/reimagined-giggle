85a69f6b93a6be8e155ff1dc821713620206a173
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:01:47 +0000

    replace yourself congratulate walk pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
