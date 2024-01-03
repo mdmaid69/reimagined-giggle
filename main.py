a137e5042ffb4685cc7cca35e43c4c068c91f39c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:10:20 +0000

    first closed yesterday language seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
