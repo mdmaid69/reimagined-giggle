065cec0c3a2ecb8dbe09994010212d9d59a4beb4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:45:25 +0000

    free pocket letter brave reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
