1d7a43333d626f3f2cec521a8b0d3004225bc709
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:53:25 +0000

    judge office early share several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
 numbers = [1, 2, 3, 4, 5]
 print("Max:", max(numbers))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
