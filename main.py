8a0f5031e772a01a08d3b2512fec8c5bba32daa5
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:47:12 +0000

    thin nigga spoil pity welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
