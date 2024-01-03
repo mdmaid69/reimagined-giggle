010e7024daa24afc4260176b2dbf15c4da456cee
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:12:42 +0000

    doubt Vv lock calmly crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
