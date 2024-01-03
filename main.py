068e2952d5a68dd67302ba81026264f8be20b4a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:41:08 +0000

    comfortable set slightly tie weekend

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
