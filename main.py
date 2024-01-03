001f8d19f2c0fc285c806dfdb1fa6bdac24173db
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:39:41 +0000

    flat once calm view exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
