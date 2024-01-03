b51052bc711333f93d671e8c3a6df5302b3f9bd9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:10:55 +0000

    discuss black great bus recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
