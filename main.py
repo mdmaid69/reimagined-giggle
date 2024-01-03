3cdebfa04704ce429ab32dd898f28112993868eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:26:33 +0000

    outside pronounce delight Ww sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
