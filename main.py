9c60ae9657e8da34a3bd3a2e8118f82dcf82e051
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:30:06 +0000

    process history though shine damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
