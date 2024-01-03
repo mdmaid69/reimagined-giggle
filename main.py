14782f58f7d87e5473c99d5e13a7aed60d6f5e0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:48 +0000

    previous score glad usual sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
