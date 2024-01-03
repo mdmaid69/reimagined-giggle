12505b6dabfac834bea270f8f7ec813a205b3e2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:08:02 +0000

    as walk here tap holdon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-print("Hello, world!")
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
