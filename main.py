cabeaf55e40064c4432d1dbfce4d922485b1a9e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:42:58 +0000

    boat attractive train further solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
