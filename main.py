31fb13dbae8b4990b74d34ca8d29dcdaba8e496e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:09 +0000

    change feel post quit mix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
