b492891e5f52d6b08febaf1ca5659c369dc22728
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:09:07 +0000

    spray shadow determined at aim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import time
-print(time.time())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
