95a2549dacb3554ef46af2f8ab1eaeab1886b1ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:52:51 +0000

    lookafter sound between chief leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
