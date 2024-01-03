f2419be6b676140a356192eba6aa7e7f25bf8544
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:34:03 +0000

    own food coffee Yy attract

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
