48fa8355c8f666173b6ea78ab1e6f9b1e05c0636
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:56:30 +0000

    suggestion badly cause remember reject

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
