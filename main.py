8f46de7e125b39c5ea15c55bad00e5e718a0d174
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:13:09 +0000

    personal awake slide contribute brother

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
