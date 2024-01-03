9a81e9ff5a70b61e369027b8aa1cde76785fc394
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:52:01 +0000

    comment sorry remember crazy bottom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
