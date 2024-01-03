6ae7250104e763e23e5c84a7addf95ce8cc71d0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:25:56 +0000

    lock whole them stretch happy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def find_min(numbers):
-        return min(numbers)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
