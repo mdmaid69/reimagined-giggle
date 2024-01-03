5695ebdeac0ff88c24fbbed88a16f5814a7e6a36
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:10:07 +0000

    lesson relax separate suppose record

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
