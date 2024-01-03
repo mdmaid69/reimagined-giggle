6e870435856948d1a2153e71a2bac0f26e964378
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:44:48 +0000

    impressed warn unite people phone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
