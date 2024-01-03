1a67528322a30c36e9d13f3d2dadde4a8088d632
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:05:36 +0000

    will slow wire priority partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
