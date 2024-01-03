18c99be0b54e07e29770843341c3ed86d6227d3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:40:39 +0000

    bunch hold welcome to guard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def count_characters(sentence):
+        return len(sentence)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
