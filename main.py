e38e9dc8058816a9060323f82307f1efa529e9e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:23:17 +0000

    lookafter heart protection confuse cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
