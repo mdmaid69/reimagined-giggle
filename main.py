772da4f12bc123f9e8c45ce39a448c137d0e189f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:23:10 +0000

    tap objection surface switch class

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
