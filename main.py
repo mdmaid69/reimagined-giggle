e1678d92dcbd34da693db379c3acbe74f54a5753
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:29:30 +0000

    black politics concern each fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
