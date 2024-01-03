328d2a5ec05b4b99d4e1b85c3352ef077b9cdd2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:45:16 +0000

    boring quiet invite drink slide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
