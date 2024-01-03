8490067e19cf426bad34f19ff986ba86a5cd5558
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:01:53 +0000

    so rule well protest joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
