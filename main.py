533f80466e5a1ee7110777b023fcea4cd7e8e40a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:08:11 +0000

    lesson relationship discovery matter general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+print(os.getcwd())
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
