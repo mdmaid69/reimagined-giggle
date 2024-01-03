e7219bebc81f1a701aa2ab91e4d02093a83e9ca2
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:15:20 +0000

    doubt plate equal roll happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def add_numbers(a, b):
-        return a + b
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
