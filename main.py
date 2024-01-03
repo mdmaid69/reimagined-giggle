2fcf72b9a33f2ea81abdde6e67753f082a99c853
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:13 +0000

    wake basis goaway lay pin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def is_odd(n):
+        return n % 2 != 0
