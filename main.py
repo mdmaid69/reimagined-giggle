8e6a2af31d2fac281fd7a4cd1f560a773106ced6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:00:51 +0000

    dull standard conversation long slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
