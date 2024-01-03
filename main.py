9db0c192c080072be704683e245a40958481d7b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:10:55 +0000

    full murder juice familiar board

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import sys
-print(sys.version)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
