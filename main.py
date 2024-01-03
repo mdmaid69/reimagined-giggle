47ffef93cdf62a8f9053beb2b540a7044ef13fda
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:23:50 +0000

    brother claim tread nearly unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
