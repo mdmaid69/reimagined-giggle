553c99c1351815c7bc14b2309f45435747b1eed5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:56:27 +0000

    write loan schedule turninto spread

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_set(array):
+        return set(array)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
