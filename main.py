0efdbfd38d7bdf805722cc69539cd9e8afc59256
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:46:28 +0000

    they serve choose wide shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
