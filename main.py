ead9c87e83efd0c480b91ba4c6644966dc37c478
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:00:21 +0000

    supply thick powerful impossible circumstance

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
