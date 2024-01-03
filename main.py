b5899f973ce4b472d6d5c4f31556f660d837339f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:46:09 +0000

    culture suckme cough test shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
