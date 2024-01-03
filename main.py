c2ba6956d247710357052c5098cba20274d9fc05
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:06:17 +0000

    patient friendly square interfere petrol

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
