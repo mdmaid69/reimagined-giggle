6885d81cf338e4cf524d2e76a776148e8f57222a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:54:01 +0000

    middle receive mad under count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
