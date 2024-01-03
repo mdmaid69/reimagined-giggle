1c8a58a238b07db0fbcde920555aefc20654ab1e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:11:35 +0000

    instruction care obvious wealth slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
