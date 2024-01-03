a5c9e56942dea2c13b70ad25277d54782a0a0672
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:30:38 +0000

    journey pencil twist even uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
