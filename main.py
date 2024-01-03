b9030fb7374b59e83935f37dc0c8426923677541
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:53:36 +0000

    lucky section available summer bar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_density(mass, volume):
+        return mass / volume
