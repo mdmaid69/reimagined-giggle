1fda718b33fc1663d488cbbff08a36307a5b493b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:01:09 +0000

    food light else memory progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import datetime
+def get_current_date():
+        return datetime.date.today()
