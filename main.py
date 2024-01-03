18025fdf1183d62cc3fc0004f3011f3a5c8917c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:37:40 +0000

    wipe worry tidy occasion because

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
