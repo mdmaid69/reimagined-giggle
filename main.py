296234ca44f60d147a05c7e19ade2579be4ed8be
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:51:27 +0000

    dangerous within destroy interview strong

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
