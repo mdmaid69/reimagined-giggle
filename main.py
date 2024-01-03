ec4ccc1feed1cbf4e81d1a86c30701547714c4a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:46:13 +0000

    substance pride present ought character

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def convert_to_hex(n):
+        return hex(n)
