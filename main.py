0b151d690412a2973105a5631f9c1828e61519a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:52:52 +0000

    draw content again scared wait

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
