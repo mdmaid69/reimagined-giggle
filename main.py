51249093b375bbe71d30f0393bf2fe398d8897c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:08:47 +0000

    spring education design than area

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
