59c9536c3b1d10ec3f1e9bfd9ac772017c6f2fef
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:45:06 +0000

    light shake little relation present

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_work(force, distance):
+        return force * distance
+def calculate_acceleration(speed, time):
+        return speed / time
