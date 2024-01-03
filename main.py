dab0747f578b034387cbdfd91a0a1a13fc798abd
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:53:28 +0000

    discuss black sure direct frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
