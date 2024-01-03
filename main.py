5843b928315417553937231a22670c026b394345
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:07:04 +0000

    remark press minimum buy true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
