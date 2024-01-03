c3d4c4b83ebc6cedd0873b503500394f5df49d60
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:25:35 +0000

    needle trap unfortunate paper model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
