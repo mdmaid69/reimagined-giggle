3ebed2ec8c54aec8a1cd94fbd2fdab39a6648c22
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:05:51 +0000

    fat deaf order advise please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
