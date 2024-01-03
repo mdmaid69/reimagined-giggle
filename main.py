638819b996317b2e994129f2f20b1882c436fcc1
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:19:22 +0000

    callat system fierce custom rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
