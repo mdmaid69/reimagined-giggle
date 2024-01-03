9dcdf3d34a742ead43a12b1c68e0ecb0fc8c54c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:43:21 +0000

    future important getup week majority

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(10): print(i)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def append_to_array(array, item):
+        array.append(item)
