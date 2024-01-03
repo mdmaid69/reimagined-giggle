9d64c38b84228d8d9d2cfe9964392bf26df53048
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:42:27 +0000

    sentence urgent Nn big bang

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_as_list(array):
-        return list(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
