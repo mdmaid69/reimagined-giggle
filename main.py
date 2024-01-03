db68aaff2c4d43524e402bf258ba6c016df88d62
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:50:14 +0000

    mouth spot either anybody reliable

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def square_number(x):
+        return x**2
