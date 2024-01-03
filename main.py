e91b86fbf393410720d34c49594fe778451fb382
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:57:14 +0000

    feature conversation join trick story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
