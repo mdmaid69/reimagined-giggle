f2271815bc80ca53b876df6c81b28aa365999e43
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:13:27 +0000

    value agreement region ilovedicks steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
