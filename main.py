a3d65ac2c3450297247cfa43ec6eec6b1a4e8bc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:20:13 +0000

    safe would object act sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
