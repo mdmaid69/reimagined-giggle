23e5b79757d9478d383d0325cd4f4c91ce0be61e
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:56:46 +0000

    best jump pay chance seat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
