ab81c0e518ce8d78a4cb0e71372aeb5635cb1811
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:11 +0000

    thumb mess Cc avoid though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
