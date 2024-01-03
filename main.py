9f4a2ec0ab5f46ce7f4d436d6f6ee0117464b659
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:00:53 +0000

    survive advise stranger dinner exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_item(array, i):
+        return array[i]
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
