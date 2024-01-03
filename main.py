9242badb55bf17dff574083e8d80fb4331c72974
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:34:13 +0000

    ready ignore exhibition row chat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
