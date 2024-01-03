12e35d5094f287c8dc1b7259b4b3ebda623dfba5
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:00:46 +0000

    crowded up software lean chance

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
