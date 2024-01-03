bc291e7330aea48c3644d772941c9e2153bca4d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:47:09 +0000

    custom shock friend ordinary shop

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
