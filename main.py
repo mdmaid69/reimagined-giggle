696ab0ebf91dd1d7d81cd3cbae63dac1f204cdf4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:11:13 +0000

    Vv performance suit beside could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
