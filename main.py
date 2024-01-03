2c9219885e528dac89986d4a92b5ee99de9d4045
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:20:50 +0000

    outside crisis deaf minimum uniform

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_density(mass, volume):
-        return mass / volume
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
