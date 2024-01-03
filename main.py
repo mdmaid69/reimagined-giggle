f743c7eb5fca13309fc7d643cdd043c3d85df3c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:42:54 +0000

    profit smooth fly neither near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
