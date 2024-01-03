0380d305fc4bce4d163fde3dfc899be4db1f7644
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:33:29 +0000

    whistle imagination smile land once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
