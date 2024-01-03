b0b2472c0c3b5e50a2872ad35108d778b5af2e36
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:20:52 +0000

    space behind next near buy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-name = "Python"
-print("Hello,", name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def square_number(x):
+        return x**2
