2d7a11e035d3069178e8969afbaf0d75bfd10ed6
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:38:04 +0000

    about either frightened experiment evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
