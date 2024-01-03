1a75f0e2b5e9e9469986f420e4268e43cd9fd4c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:23:05 +0000

    record risk search phone carry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
