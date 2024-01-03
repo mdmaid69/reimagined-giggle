632888ac46cb6d80b569c9aa2dd42c1d0d911ffa
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:22:37 +0000

    narrow respect comeoff principle aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
