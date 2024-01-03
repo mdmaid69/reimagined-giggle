2e02058ebb77bc9bd3d7b6dbcd926d6da2a98638
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:11:03 +0000

    complete contents eager include polite

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
