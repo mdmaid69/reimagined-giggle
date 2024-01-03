5c6ef237bc7834252e575ad384aca5ee99375ced
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:19:17 +0000

    solve respect separate comeout possession

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
