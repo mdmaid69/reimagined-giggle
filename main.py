3012e41275d73a5bf992aed278422259aeb2ff4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:06 +0000

    close as about length takeaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
