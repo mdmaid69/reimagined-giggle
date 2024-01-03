3ea5fbf2f24cafcd22b1ba137a746f8d3959e0b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:09:40 +0000

    progress feature still acceptable thing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
