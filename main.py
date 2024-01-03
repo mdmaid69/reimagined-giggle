7cf239584b2539b0c90aceeebccf2873247eb42d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:46:20 +0000

    laugh joke bit cry takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
