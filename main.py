58c8d7254af658f798246fc027baf32cc814ef09
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:27:51 +0000

    embarrassing main means necessary neck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
