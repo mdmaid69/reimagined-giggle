ec66aa843216ea199de8a470262b22ca8b18fe2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:29:00 +0000

    complete it stamp clean raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
