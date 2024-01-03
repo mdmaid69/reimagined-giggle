6465c3d53068b478b367c72ae350838c9edc0d43
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:27:28 +0000

    wonder trade smile bar set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
