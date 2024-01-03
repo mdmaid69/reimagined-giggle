7b5f19c8aeed83d90bdcac1a9664fe3bffe96777
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:22:25 +0000

    write soon only getaway error

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
