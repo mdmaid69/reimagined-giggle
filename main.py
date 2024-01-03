4b5e0c454a28abd7942f53b870b1b28a6bc40daa
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:15:47 +0000

    proper height mistake thing pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
