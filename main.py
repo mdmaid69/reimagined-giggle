71369deae7fa3d61ac98ccea6836c55834a80345
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:01:25 +0000

    tend worth lookaround behind invitation

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
