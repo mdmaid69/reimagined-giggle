db15e56a87cee0770a8fae994532d9c538c0686b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:55:14 +0000

    solve influence cut fair need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
