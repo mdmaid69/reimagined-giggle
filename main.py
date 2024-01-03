126302d87eb71d4f571f17500b572f8fcbbadbeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:35:21 +0000

    beginning relationship mean nor emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_binary(n):
+        return bin(n)
