4ab0b7150626f7e3dce967437845099c8a4e02b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:59:16 +0000

    married unfair mystery flavour crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
