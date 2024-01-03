ff78043d71aec3cb34e9b1807f1ff3fb34cf50af
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:22:35 +0000

    hungry imagination improvement tip quarter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
