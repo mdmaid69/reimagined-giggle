6e98f7058def0f79977c19001eb41c62bb35e789
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:04 +0000

    beautiful spring transport ring organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
