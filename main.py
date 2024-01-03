a00bb2f1fa80b438edec97f7116f19159f5801af
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:29:29 +0000

    advanced often church comfortable charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tensorflow as tf
+print(tf.__version__)
