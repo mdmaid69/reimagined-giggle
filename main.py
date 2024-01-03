44a9a519d503763ec0d62785c8959020f9167506
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:54:12 +0000

    actually move famous over powerful

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
