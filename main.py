060fa70cf83f2a1d2a52a69274bd3847e5d32fea
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:44:40 +0000

    general bury meet manage attract

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
