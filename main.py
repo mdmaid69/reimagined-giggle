097a7c4e87de8181c66b8628fca355fefd7e4513
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:23:04 +0000

    in he you poison race

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
