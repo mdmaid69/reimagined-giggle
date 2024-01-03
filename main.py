1a05dbb2c103a78c247eedfa19eb711cadfa7cc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:42:21 +0000

    mass he demand lot take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
