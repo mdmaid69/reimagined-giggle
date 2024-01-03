4a776d0e4e847f692018c2bc4808a396fcf72b79
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:52:53 +0000

    trap protest certainly wish still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
