ee86673e058c90e84c2f3ee1c2c8491a7054182d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:47:23 +0000

    ocean deliberately realize contact hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
