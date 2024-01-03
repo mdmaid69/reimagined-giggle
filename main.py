fd3bc42caf3bf34b6cb9408838cd32d40c92d7cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:25:41 +0000

    not valuable month claim its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
