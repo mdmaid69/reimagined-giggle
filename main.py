21ce0f6e870dc28506a9a0dd4917ebf40b3bc956
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:15:02 +0000

    ashamed method middle feel height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
