4fc1bb2ccc57e0b3fac931b7999a4fbd3e32d8ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:50:59 +0000

    hurt proper sudden breakthrough finally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
