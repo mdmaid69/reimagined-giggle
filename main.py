24cf34783ed1cca745ecdbc075ee200e6a300b52
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:56:52 +0000

    meanwhile still now near gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
