fcd23ed2438d08530165624fc95c2fea6fc8ae64
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:03:57 +0000

    sound secret describe stiff common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
