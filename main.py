0b37e4932aacd507cf18657c2a4c496fccfde6c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:48:03 +0000

    used party suggestion through depend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
