524f481124cb035256ffb9a6ab9a8574a9c7bf7f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:15:43 +0000

    Dd several Ii safe undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def square_number(x):
-        return x**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
