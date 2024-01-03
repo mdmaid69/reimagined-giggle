34c619b680f7d5ce92618ef2a40550b736554978
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:04:00 +0000

    limit large autumn solve see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_index(array, item):
+        return array.index(item)
