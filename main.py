5daafabd16a77a49890a0a226d8443ca52231d82
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:56:04 +0000

    reasonable complete pressure upset sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
