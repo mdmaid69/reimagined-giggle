8760ce2ad1f7c27ad278af263f297b85fe625b02
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:33:47 +0000

    clean fit burst hers position

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def get_current_time():
-        return time.time()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
