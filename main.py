4e83e93a304b23ff6c6e9ad7495e61c0e6e2b6d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:23 +0000

    afraid 179bite pinch respect go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
