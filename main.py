d6481e89228fec2d12b24ddea8c90d1d5b446ea9
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:33:16 +0000

    opportunity mysterious consider container base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
