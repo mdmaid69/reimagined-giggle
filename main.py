c03e38f2b860bb30af7ca975350546e6c3ddd9e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:41:30 +0000

    noise quiet under exist reach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def subtract_numbers(x, y):
-        return x - y
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
