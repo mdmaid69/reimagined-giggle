5d27e1715e344f2fc95f0b613c17f8f6cbf5dbfa
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:32:25 +0000

    always unit summer score motion

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
