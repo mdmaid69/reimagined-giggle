2da81fc1eadc11e53e6b29bdc7e94f966a47fa25
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:46:44 +0000

    comefrom beside meal turn blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
