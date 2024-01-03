27befc260c41e6cb93a0c03183df4081be33a384
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:25:47 +0000

    thankyou away play leg fast

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def square_number(x):
-        return x**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
