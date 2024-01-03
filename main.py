09a0ba93750ccd09126c9b3daab6f0395b8d0e77
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:22:35 +0000

    turnover cheat difficulty border regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import sys
+def exit_program():
+        sys.exit()
