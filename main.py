4f95f557cdbc8486d1743365500e54fa4b836676
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:30:56 +0000

    emphasize mistake recommend beside call

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+print("Hello, world!")
