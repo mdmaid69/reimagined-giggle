8ea722802af176250de2989aba51e4067000063e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:30:50 +0000

    blue emphasize escape Cc score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
