d95b052bf0f1a5dd368dc814677946542b0e9705
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:15:14 +0000

    keepoff hair forward result all

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
