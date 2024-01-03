c9082bbb32b7760462dae693c482f1958812ec7e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:55:51 +0000

    entry turnaway clue slight picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
