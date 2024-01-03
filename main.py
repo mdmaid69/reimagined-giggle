92b93640123727c6d01e66abc7c02ca07da3bca4
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:56:50 +0000

    effect better under exactly accept

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+i = 0
+while i < 5:
+        print(i)
+        i += 1
