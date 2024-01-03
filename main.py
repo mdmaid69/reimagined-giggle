1fc95d869dc2bd85d34cf7afffd737432454868a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:39:02 +0000

    rely catch round criticism consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
