d04637cb1bb71285a68f41a0bf2a76071deadd7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:24:39 +0000

    lose likely open song hard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
