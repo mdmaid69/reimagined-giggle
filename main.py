e97b430238d420fea952f5033f1c85e69fbbdf28
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:28:17 +0000

    bit work Bb criticism circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
