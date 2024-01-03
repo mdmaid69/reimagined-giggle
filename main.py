1983c72de9d88f0d174019e3aee61cc24900cefb
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:34:46 +0000

    previous pretty first destroy time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
