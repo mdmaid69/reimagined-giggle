4bb0c252814a868fb1f1693e75117892b9ebbb4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:21:16 +0000

    interview ability off increase side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def find_max(lst):
+        return max(lst)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
