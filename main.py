b8c66d11bd4fcea765a9a80c709dfea045d273d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:35:44 +0000

    true fat order rich embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
