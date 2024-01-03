ae73ccf20f2fbaa2c798d3f40a1638de80ad8b93
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:19 +0000

    roll present difficult term teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
