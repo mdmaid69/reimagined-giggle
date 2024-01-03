36283e25477f635dc84693ffcf28544e31614adc
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:00:49 +0000

    also underneath arrival praise generally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
