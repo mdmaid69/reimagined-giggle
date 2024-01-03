00502cc912b2e17624d6bf81289f2ea04f013e5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:56:47 +0000

    expression punishment rush improve long

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
