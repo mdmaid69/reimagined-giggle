c82b53126448c6ad557ba715ab891ae24fc2b974
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:22 +0000

    few doubt chin unlucky catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
