a4983483c442fccccc4c7d028333795eeb5f3fa5
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:49:08 +0000

    treat school tune bring scheme

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def is_odd(n):
+        return n % 2 != 0
+  def cube_number(x):
+        return x**3
