f19f0c523e164ad6476546f92e4647bad7f8f563
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:00:34 +0000

    appeal recognize war takeover community

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
