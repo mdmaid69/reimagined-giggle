ea2d852117521a53f3541e256227dfb3c36c85bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:08:56 +0000

    touch rich develop fetch whistle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_density(mass, volume):
-        return mass / volume
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
