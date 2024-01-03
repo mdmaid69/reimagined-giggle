9fa3a81186880a35acfe75234436680171fd6d46
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:05 +0000

    connected part cruel machine famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
