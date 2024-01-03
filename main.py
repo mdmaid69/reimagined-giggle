3df2b732f5a3391b106c5b90a2225390e31787d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:00:56 +0000

    top deep natural turnon mail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
