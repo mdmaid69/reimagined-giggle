34d5995c765b4d8831da314ea809a72320f45bff
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:43:06 +0000

    lesson breakout terrible book be

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def is_even(n):
+        return n % 2 == 0
