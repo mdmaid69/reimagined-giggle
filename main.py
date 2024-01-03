3a618a701792c2caa2e713a49208cfb683af7df5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:44 +0000

    study today spite flight feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def multiply_numbers(x, y):
+        return x * y
