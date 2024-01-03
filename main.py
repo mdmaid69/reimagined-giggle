e2b5faad4f723b9bd6144b8446a0f180efe9bc3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:06:38 +0000

    hate quit shop normal period

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
