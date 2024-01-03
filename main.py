c0dfc766ad0d9d5734a1725fb3fc74dac5310b53
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:18:23 +0000

    lunch popular regret chief tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
