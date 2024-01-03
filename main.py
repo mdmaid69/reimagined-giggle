80a80f7a5283650c53e1a54d80dc61c86de45628
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:57:20 +0000

    friend far rest independent represent

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
