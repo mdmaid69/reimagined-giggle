5045f19469660163fdb439f86cf9d7521f76e96b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:01:09 +0000

    offence invite watch contrast sink

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def find_min(lst):
+        return min(lst)
