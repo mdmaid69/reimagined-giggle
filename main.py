b76531c8fefe2b386199abf33b8d2068c3fe32e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:57:14 +0000

    apologizeapologise storm breakfast person takeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
