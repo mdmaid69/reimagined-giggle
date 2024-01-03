0a2f27b51fb8af25187fe052f33088e2aa75104c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:02:47 +0000

    corner prepare tip shake yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
