7d35358158b604e9808d276283c7c00ba1d6338f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:22:03 +0000

    opponent various branch show workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
