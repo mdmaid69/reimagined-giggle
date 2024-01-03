8458953fc47db86affdbb6dc1c45de01013d96c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:49:18 +0000

    control tie Rr say effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
