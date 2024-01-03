c373b22cd90a14913ab8cb6d06dd8b03ed8360a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:03:24 +0000

    whether basically light as comeout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
