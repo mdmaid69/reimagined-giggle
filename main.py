c93c5ea2157281a21a9e3d9f5923397875a0f91f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:35:01 +0000

    building entry reduction basic stand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import os
-print(os.getcwd())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
