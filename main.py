544ccdcb317450598181f09c1ef0df9a041b388a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:52:41 +0000

    cope shut life speech variety

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
