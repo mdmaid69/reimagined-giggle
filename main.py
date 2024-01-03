8daa5159c8650a3ea5e191ce5058aaaf545b209b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:35:32 +0000

    down win large positive medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def append_to_array(array, item):
-        array.append(item)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
