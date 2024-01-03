637037c91a7fe89ce22cf95b7360ec54b9cda5d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:05:40 +0000

    cool connected respect Rr moon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
