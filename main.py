1bf94f8b815697bb70f65a242996133a8f524738
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:36 +0000

    closed address value float split

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
