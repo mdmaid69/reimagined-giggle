56ef36ef92ed1a08efdb55074b216588a43b759d
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:30:39 +0000

    married silent reserve send movement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
