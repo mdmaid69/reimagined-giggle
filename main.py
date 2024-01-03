fee01877994559e83df70b74a868842ddcee678f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:14:43 +0000

    abroad appeal advanced air combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
