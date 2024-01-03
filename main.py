24c5aceb69335e3a91ec85cb9fae614c60866d8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:35 +0000

    equal official light knock expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
