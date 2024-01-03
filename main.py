a3edc5e6a43f9cbdd8cbb7fc71f65ec192b64038
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:57:07 +0000

    exam collect used commit comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def subtract_numbers(x, y):
+        return x - y
