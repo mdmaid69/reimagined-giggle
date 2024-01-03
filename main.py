d26894eae2ebc0442ddf606f03acdaaa0b3e92ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:28:37 +0000

    fear near doctor again branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_power(work, time):
-        return work / time
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
