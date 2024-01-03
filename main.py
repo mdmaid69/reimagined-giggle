a76b4f6df6803610d18855078afc852054bf7555
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:49:14 +0000

    plate may place on wonder

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_current_working_directory():
+        return os.getcwd()
