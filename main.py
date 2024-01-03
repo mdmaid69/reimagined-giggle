b789048569f303d49ec5e97d32d3475800c3a010
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:13:02 +0000

    successful detail regional shut mean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+print(os.getcwd())
+  def add_numbers(x, y):
+        return x + y
