e2a5e73495a3d392c26e1feff32d9f180d6a7526
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:08:04 +0000

    shallow lunch further address interesting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
