da6f6ff1239a1adc9c66c0fb4190e91778234d28
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:10:58 +0000

    scrap load borrow bye thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
