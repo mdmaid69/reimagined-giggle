6b953f33339bc49f441ff75711c2d0d8b93ff3d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:51 +0000

    and getaway tall learn somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
