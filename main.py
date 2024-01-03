0b7e088b4e8e32da90994321af52c0ec62f61457
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:04:15 +0000

    kick keepaway pair Mm room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
