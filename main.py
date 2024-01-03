7428f61dfa89c6bd178734d365fe5f86aee9883d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:08:51 +0000

    behind yesterday town hair keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
