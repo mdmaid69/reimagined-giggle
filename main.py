3de798fe90bc490946f29fdd3b6b0b3cb23423e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:57:14 +0000

    line translate relation means side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def convert_array_to_string(array):
+        return array.tostring()
