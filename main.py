430151b3447c4a05005ff93dc55907a5e253fee8
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:50:11 +0000

    match afford lucky belief medium

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
