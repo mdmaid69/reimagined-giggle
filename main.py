2a18ef78f9342e19a0d86124a950ee83d8120533
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:31:08 +0000

    space wish flow below wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def add_numbers(x, y):
+        return x + y
