7f84ce647af9e902d1f5bf3e288560dc53b1af2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:57:43 +0000

    increase eye ilovedicks alone already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
