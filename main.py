8654d13d48230b166686327ab9bd5cd636376212
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:06 +0000

    late practise reduction except better

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import socket
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import unittest
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
