7b7912d84c5835fec9d50862fc2ad826aae6fbda
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:55:39 +0000

    getoff pocket cold design container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print(math.pi)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
