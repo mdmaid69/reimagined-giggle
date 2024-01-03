c13dbc72973fe1fe70c20f588b466aac3d86bcc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:40:45 +0000

    spread could left free pleasure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def count_words(sentence):
+        return len(sentence.split())
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
