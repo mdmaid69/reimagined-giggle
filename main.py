2607f1080faa276318ec050d97b9732724de24d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:42:39 +0000

    better exaggerate peace vision make

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
