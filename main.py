0c8c35a07e28d59ae982d80c613ca4ffb8266e72
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:01:45 +0000

    calm whom sink afraid choice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
