8b167508b77e02c54c6c58f8ad4a38359dbe5a66
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:38:40 +0000

    dream live actual air stand

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
