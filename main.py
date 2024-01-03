b03b189125e05370d1fb4c41d46e0b03d2883a0a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:45:29 +0000

    different copy various design vanish

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
