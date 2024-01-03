5ee562271e071eac539d41f4f6275ce8f23f9ddd
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:20:16 +0000

    on match luck comeback think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
