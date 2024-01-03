7147770f3469a36009dba2969bfdcec8d61e88f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:54:36 +0000

    relation sea callat scheme put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def is_odd(n):
-        return n % 2 != 0
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
