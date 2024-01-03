1bcd23e9ed81a78e537a9db5ae6a8bd55e214e5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:58:01 +0000

    result reduce trade sport now

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def count_characters(sentence):
+        return len(sentence)
