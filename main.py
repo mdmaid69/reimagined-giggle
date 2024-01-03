702a540a7e2082c559e79591470457528230ad4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:01:23 +0000

    show ours sound cinema boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def square_number(x):
-        return x**2
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
