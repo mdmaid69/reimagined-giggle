ba48860cb0d8d775f544184b650ad3400ef5563d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:11:48 +0000

    guess concentrate shoot edge listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
