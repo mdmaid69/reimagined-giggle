d4151788fb734379a02d03a910d2c23c5051cf94
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:43:08 +0000

    ground brush hot furniture Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
