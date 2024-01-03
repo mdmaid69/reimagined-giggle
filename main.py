636c47f8fe4850e746a3e4569124f429ce9f9e01
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:43:12 +0000

    influence holdout challenge Hh getout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-def is_odd(n):
-        return n % 2 != 0
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
