18b7a7dfe4e359823c2cf3b3f28c867a6665a672
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:52:27 +0000

    majority amount deal legal angry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
