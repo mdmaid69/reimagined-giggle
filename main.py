3eb18af347ad260207bb4da06a3f913734623f8f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:31:26 +0000

    workon partner otherwise likely might

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def square_number(x):
+        return x**2
