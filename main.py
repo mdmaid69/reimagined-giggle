8524f1a3022a939aecf7bafed0a1adaa2a546768
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:15:43 +0000

    answer yes round provide control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
