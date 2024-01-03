ef96bc225dcba02e7cb07e2e4afcb8d1a0a43dfa
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:42:53 +0000

    cruelty fear cost best fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
