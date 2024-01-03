eeaf390a6941566740acd90db343b0c77e8016ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:58:36 +0000

    live worst sister love Vv

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def is_even(n):
+        return n % 2 == 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
