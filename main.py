b3420c726e8b87fcd921017cedb09cf8295ea7d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:00:43 +0000

    distance manage battery fetch frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
