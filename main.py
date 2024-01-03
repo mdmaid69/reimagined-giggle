780452beb952e8aebd86b8749684f672c65e2032
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:41:41 +0000

    serve method plate more fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
