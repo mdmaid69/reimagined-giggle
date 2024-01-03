3cdd87ba906cc6f26ca327c026ce87b56b311fd3
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:54:35 +0000

    boat doubt steady impression mood

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
