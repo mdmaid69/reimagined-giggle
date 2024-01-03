2393d77aff465f9dacd261f92dc4fa0bf9901bb4
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:06:13 +0000

    mark brown mention completely bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
