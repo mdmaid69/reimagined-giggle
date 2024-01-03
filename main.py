fcd147cf51157dc2daff8855108c7ec95730e0c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:29:23 +0000

    justice through lead grand careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import getpass
+def get_username():
+        return getpass.getuser()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
