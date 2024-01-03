ea7a1875f028fc5734a63befb76ab3699ead6d0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:50:37 +0000

    wherever spring pocket wear test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print(math.pi)
