e5de8df0c2829e391c36a46c70cbb9391f00d8d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:36:01 +0000

    male generally lonely difference relax

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-def find_min(numbers):
-        return min(numbers)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
