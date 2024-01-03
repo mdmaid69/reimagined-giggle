140e14ab07ed966376184d9c0ff296e847a66a9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:37:57 +0000

    less justice without remark practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
