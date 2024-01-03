3a10e3e7cda47b13973e3b1526c96af5b6bce1bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:15:26 +0000

    Dd slight across collect slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+x = 10
+y = 20
+print("Sum:", x + y)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
