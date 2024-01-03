2cd550eb1355a41120e9ad0adb29d57759e46ad5
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:09:41 +0000

    switch woman mean fix paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
