5433a3307ad98391d501712c8609d9bbd6dfd208
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:05:28 +0000

    by certainly rain emotion search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def set_array_item(array, i, item):
+        array[i] = item
