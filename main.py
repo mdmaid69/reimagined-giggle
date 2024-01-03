9e55f2f594b2657ddb488bfe43932f14067f49a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:12:57 +0000

    file cut realize separate remember

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
