0ddb59fa9e0add25de99f78ef095339073752c84
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:42 +0000

    love goup final port achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
