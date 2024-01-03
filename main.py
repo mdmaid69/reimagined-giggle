2057123f72460e9e11d8aea80bc267c4eb38f84c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:10:45 +0000

    film improve soft willing they

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
