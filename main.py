016a1a49e09760e4d2776de1b084a7cefc63db9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:45:09 +0000

    contribute all swimming best what

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
