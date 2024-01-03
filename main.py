dd605698928806aa832c471af6137f06d1e9f634
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:01:44 +0000

    exploration collect bath lucky city

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
