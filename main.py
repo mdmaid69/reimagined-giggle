bca9eabd430aaea0be48838ef8123a20652c1dac
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:01:40 +0000

    tomorrow exploration of ticket teach

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
-for i in range(10): print(i)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
