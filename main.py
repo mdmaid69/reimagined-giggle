824df5b9630f06cbe8c456fc23c3840a0619aa1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:25:39 +0000

    happen dance divide holdon sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
