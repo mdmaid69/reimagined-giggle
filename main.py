12646ca1a005ffa1ea9a3e5819ff0909c27e5c77
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:25:31 +0000

    left comment heat guess dear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
