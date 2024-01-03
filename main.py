7c38afecb702442f49334151fccb477aa07c55b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:05:38 +0000

    interview everywhere rate free worse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
