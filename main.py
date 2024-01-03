5f08e967c4d9fd00858c4aa16f797d236cdb2b4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:42:41 +0000

    emotion win community pot have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
