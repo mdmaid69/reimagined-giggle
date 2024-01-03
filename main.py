4f93a4a8ecd4b33e2d80584922e942c31cabf533
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:30:52 +0000

    dish put damage kind soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
