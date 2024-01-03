19f671c17a4a7dedad658973e5cffab3aca2b119
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:57:47 +0000

    pleasure hot mix age sleep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
