194218fff96fde63136e9f0183b7db1ac078fcd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:57:05 +0000

    shout bang sound feel if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
