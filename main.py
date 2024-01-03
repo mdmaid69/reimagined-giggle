e01326f979444136382ac66448d5fab4a048f3f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:36:47 +0000

    dig radio content 572enter back

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import time
-print(time.time())
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
