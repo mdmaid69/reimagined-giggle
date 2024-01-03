7520a9ba16c55a0b6efc1d1648da8e619168b013
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:18:27 +0000

    bar science final hate payment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_area(radius):
+        return 3.14 * radius * radius
