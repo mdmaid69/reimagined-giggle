10c367cd788b65ccf5e13fcc3387f31dbbb6d191
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:50:32 +0000

    welcome free somewhere new suckme

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
