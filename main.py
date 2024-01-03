e280f1f4d5787b4c41f3f432f7202fec258e5eda
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:06:10 +0000

    home congratulate convenient road anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
+import array
+def get_array_item(array, i):
+        return array[i]
