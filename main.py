241abd252d2d8604c11b857bf95ee97ad3f93b55
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:11:57 +0000

    draw do mind window influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
