d4de7ce539e971c990420e506da828412c73f3a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:36:33 +0000

    order eat direct fault underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_current_working_directory():
+        return os.getcwd()
