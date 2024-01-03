9950e3abaf1354723ab43f48cfbfe605446b92ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:31:36 +0000

    appeal answer declaration steady spray

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_current_directory():
+        return os.getcwd()
