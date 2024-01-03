298b90e610fe25c2d2a74fa2d0c537e1d1196057
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:32:47 +0000

    feel throw idea impression used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
