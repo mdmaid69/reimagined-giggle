06187cff5539c32be3b0c6101d4b79b70a322af2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:49:28 +0000

    print bringforward fat know bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
