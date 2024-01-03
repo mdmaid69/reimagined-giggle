0cec63691373baed5fe1d39d9103fc851b9c763a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:36:16 +0000

    shall maximum accidental attempt school

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+name = "Python"
+print("Hello,", name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
