4f68c9ecebe3bf8b571f3e0855f51a8de78d32df
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:03:45 +0000

    much exhibition remain shadow up

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
