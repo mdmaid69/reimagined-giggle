370fa315fc8eace5383982d7cfb80bbb140bdde1
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:36:22 +0000

    describe forward difference low where

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
