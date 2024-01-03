d78989ccf36584238586bdaf63cc705c807312b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:27:55 +0000

    female judge exchange introduction rid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
