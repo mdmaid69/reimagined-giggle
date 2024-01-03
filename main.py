bae5da83a1d2917c08b5842158c64d2307725df8
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:38:08 +0000

    be bath whoever shake tidy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
