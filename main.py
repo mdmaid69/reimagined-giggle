accc9849eb4821aa12757a3dea1d00f460779571
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:36:32 +0000

    point several constantly wise bringup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def create_user_string():
+        return collections.UserString()
