6692173e1c3e28783108e569f01ab8df3e83a430
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:51:02 +0000

    change nothing unlikely save something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
