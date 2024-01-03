f496738a853738641e6175b99d9fbf1fd1a71547
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:13:06 +0000

    quiet view defeat vision description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import platform
+def get_os_info():
+        return platform.uname()
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
