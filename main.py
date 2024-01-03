1c34aaf9496b8c0fba7e3b05c7df0277a8ab9a01
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:18:01 +0000

    later visit treat opportunity into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
