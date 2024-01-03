10d22e3b7a0530bd928758e47cfc48d0d565de4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:15:05 +0000

    possibility head human hole language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
