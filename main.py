86858960d57f80dd6767c59d0284cfc645383bd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:38:12 +0000

    wander border bear 569enjoy brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
