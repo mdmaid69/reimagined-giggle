b042bbcb325978048bebe4a94d02fe97a924d1d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:27:50 +0000

    jump speak guilty manage contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
