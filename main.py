44794af1d1811ecd10add09ae1d43fe812a481a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:29:35 +0000

    negative world describe deaf occur

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def is_palindrome(s):
+        return s == s[::-1]
