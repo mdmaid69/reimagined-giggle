445f810b97f37d27c679efa12ae25c15e7def496
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:11:10 +0000

    calloff body my blind difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
