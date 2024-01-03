12974b373cdf13800296e498117666a4414eb5b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:40:11 +0000

    long blind open please discuss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
