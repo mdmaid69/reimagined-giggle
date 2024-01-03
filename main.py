1743749f286d5e3880436d2a8c5d41a7dd5df99c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:09:43 +0000

    fall trick interrupt word kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
