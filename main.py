e2239cf14dad32e313959c4bd8ba88497c8e53dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:59:33 +0000

    block study private possession responsibility

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
