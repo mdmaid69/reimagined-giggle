312d9be06039aa042530d0511ce457ba587ad7ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:36:43 +0000

    temper exchange shoot beginning lunch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import sys
-def exit_program():
-        sys.exit()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
