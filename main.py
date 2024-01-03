4f3e3a68c39b35d2dec9422bf837aae8f8368221
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:20:44 +0000

    570enjoyable wash battery progress explanation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
