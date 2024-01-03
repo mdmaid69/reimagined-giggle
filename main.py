f6ecbf06749d70cc5244af959293c82240ea6bea
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:21:24 +0000

    tomorrow study step position over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import platform
+def get_os_info():
+        return platform.uname()
