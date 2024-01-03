f1162eb48c1faa70198e0c98311c1ee32fb7ecf2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:38:54 +0000

    compete steam partner refuse centre

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
