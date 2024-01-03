462c480646badc9e2a5c019c0a69df05e2f6b131
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:24:40 +0000

    odd warn drive weather medium

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import threading
 def create_thread(target):
         thread = threading.Thread(target=target)
         thread.start()
         return thread
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
