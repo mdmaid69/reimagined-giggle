a78fa5ae9cff4e03e2d739dd9eda65d1e87582bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:57:56 +0000

    pretty promise introduction mistake half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
