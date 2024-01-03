ac9c3a7cc5ceb1b76b3f1150313ef6e1088e1ed7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:55:18 +0000

    always reach watch change keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
