925bd9cf1625fe7b089cb3e2112acbd78da5eb2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:03:27 +0000

    rain mend form succeed fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
