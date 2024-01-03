e6151511fc8adec7010b2ec00d6bf2ac55add3b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:48:28 +0000

    combination chance bar phone plenty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
