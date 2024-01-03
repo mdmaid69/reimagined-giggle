c9443dae3c81d6b860daacefacbcd52c4662d97a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:00:52 +0000

    journey account snow unfortunate also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
