a6f24209fa2304e2a980c5a5d8afd33759484d1e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:00:01 +0000

    angry sound advanced family promise

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
