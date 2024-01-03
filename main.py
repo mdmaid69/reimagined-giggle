decca73cf3a0135ab6f68cb9b2388b5f25e148e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:34:20 +0000

    sleep present set television today

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_max(lst):
+        return max(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
