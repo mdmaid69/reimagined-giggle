79f6f68efd5f978763e6b38db61d5b58a2bc4a0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:39 +0000

    contribute unkind mark possession rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+print(math.pi)
