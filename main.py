3b2092249568145017fdbed33064983050fd7bcf
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:17:23 +0000

    deal surprise but recovery attract

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
