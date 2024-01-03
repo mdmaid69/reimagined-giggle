4e8f2a69979f7e84356c603032d120db1a72d473
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:20:19 +0000

    everyone off neither variety apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
