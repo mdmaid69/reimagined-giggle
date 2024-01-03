fe3b61599c249eb3b1a92eadfa2c8254ebfbe8e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:53:23 +0000

    quality hour represent basic fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
