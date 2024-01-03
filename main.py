7d6a573df09ff68294af6eee2fc898d71a37845c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:01:23 +0000

    charge good arrange improvement takeout

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
