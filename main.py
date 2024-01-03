2f7adb5dd61e7ff7982606523e7e43e4ba46f87d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:09 +0000

    what unite runaway can tradition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
