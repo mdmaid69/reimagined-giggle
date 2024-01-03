369656a2fd1351ab487dd7adb3d2970ec64122df
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:45:24 +0000

    classroom like big road imagine

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def find_union(list1, list2):
+        return set(list1) | set(list2)
