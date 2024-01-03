dafbee673a4f808a69141c45105369ee21b3ee9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:21:35 +0000

    cook clear this smile judge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
