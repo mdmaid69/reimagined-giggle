f0a4d85b7a83911224795dc37dae29e3c3f2b823
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:31:09 +0000

    foreign stiff Qq opposite hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
