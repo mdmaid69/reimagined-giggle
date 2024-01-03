3ff67e7060aa4cec254693d5675788c4b15fc399
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:33:46 +0000

    breakfast trap sing environment interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def sort_list(lst):
-        return sorted(lst)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
