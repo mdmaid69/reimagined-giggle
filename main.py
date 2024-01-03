0ac779867ba8f5dbcac20feabc8b32210eb28bb0
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:57:57 +0000

    straight ready replace dear dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def is_odd(n):
-        return n % 2 != 0
+def divide_numbers(x, y):
+        return x / y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
