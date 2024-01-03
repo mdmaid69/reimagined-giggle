bbb4cad85b188c97b86f64d7ab309f3d1a77ad69
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:25:31 +0000

    where themselves that not day

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
