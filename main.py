94af74ae3cc585e21789f59ccf64e31958bc5e43
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:40:14 +0000

    wealth hold add avoid speak

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ def write_csv_file(filename, data):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
