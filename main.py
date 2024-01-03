329105a7b63720f4eff76fe5e81805b09bef6a3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:52:26 +0000

    flow period play calloff polite

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def convert_to_octal(n):
+        return oct(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
