bc42017485ece9700a6c224d5c3a9f3b94594ab3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:23:18 +0000

    rubbish 567enemy famous deliberately change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_current_working_directory():
+        return os.getcwd()
+import platform
+def get_os_info():
+        return platform.uname()
