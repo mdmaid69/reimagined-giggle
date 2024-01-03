3c8367e02c8ddc0bdbe2a7921947d4bbe6646b47
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:05:06 +0000

    lonely fight autumn colour meaning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+for i in range(5):
+        print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
