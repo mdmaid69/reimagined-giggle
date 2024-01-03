405c2941c4e3ea41a51b9b459e804bd4ff059e8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:25:14 +0000

    keepup apart rather that giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def find_min(numbers):
+        return min(numbers)
