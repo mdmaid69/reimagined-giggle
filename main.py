99107a9224f9b89fde96bb0c778477aadb75e13d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:49:28 +0000

    afford smile dress formal flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
