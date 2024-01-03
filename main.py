8f8acbef021d87423a9b25169331dd01db2cbe30
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:32 +0000

    that somewhere search pause refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_string():
+        return collections.UserString()
