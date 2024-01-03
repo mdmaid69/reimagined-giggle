c4f2aa489ae9d88ea5ca1db7e8cadf7c2c3dfc28
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:57:56 +0000

    ignore please 567enemy nor stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
