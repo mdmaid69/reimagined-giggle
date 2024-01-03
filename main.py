533b8098b0db20e89e492035693c65767fa4574f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:22 +0000

    life deep turnoff rough class

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import time
+print(time.time())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
