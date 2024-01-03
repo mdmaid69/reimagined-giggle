59d98c95a2695f33c9461421cdd5ced2cf73a13e
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:33:42 +0000

    black yesterday possibly admit view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
