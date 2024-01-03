43b7f654b2d63030364c9183dd5711a1487e1d4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:42:21 +0000

    sharp journey chance ride standard

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def reverse_list(lst):
+        return lst[::-1]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
