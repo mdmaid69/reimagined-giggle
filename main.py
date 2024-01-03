bb95d3e56b3e652ca6005126254f9befa9f359ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:13:53 +0000

    clear shake behave spring boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
