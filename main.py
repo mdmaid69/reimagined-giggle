a52ceeaeac8e77ee6b5ad68f7fa8515b938202a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:13:33 +0000

    can nose shut transparent divide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
