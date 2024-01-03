1601cdf5b97e7a4b4d69d2f378357913cb5a4b08
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:29:41 +0000

    balance getup position suit receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+print(list(itertools.permutations([1, 2, 3])))
