f9fc23ee359bb941c25226b78d031b833ea9245e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:08:47 +0000

    shelter union slip pass foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
