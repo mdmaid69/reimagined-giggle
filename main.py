523c4f669a9a44f4d3772264eaf812fa2a02e5f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:30:47 +0000

    fall keepfrom music port waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_float(array):
+        return float(array[0])
