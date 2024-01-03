5340482665b448f88eff9e73a85b118705294504
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:19:10 +0000

    keepout off health whatever tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
