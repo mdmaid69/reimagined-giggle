3ed3ad7f97746c883eff670e94cfd1324c5e1544
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:26:44 +0000

    sink song expect force pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def is_even(n):
+        return n % 2 == 0
