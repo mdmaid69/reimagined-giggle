7f326a462a073181f88c0301fd70d5ed5f0b9065
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:20:42 +0000

    weigh old apologizeapologise various in

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_list(lst):
-        return sorted(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
