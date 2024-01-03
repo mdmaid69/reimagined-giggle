290ee8ad508e4bfcc98d69090dbd49b74c68208b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:20:03 +0000

    unhappy rely extremely bite once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
