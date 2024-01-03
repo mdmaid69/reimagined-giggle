e936b7252a2ea204ec8cf015473dc9150dc3b978
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:38:46 +0000

    introduce card understand light to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
 import array
-def get_string_from_array(array):
+def get_bytes_from_array(array):
         return array.tobytes()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
