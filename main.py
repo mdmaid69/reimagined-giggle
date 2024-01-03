59285f3f42f0a2d0401771250006645f34f53156
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:13:15 +0000

    production between drop attempt married

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+  def square_number(x):
+        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
