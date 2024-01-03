f716e7a12462d765893ef8c865869cba52a07ed3
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:01:18 +0000

    advise trouble how nearly moon

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
