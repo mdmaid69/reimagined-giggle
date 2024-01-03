832bc4fe92d48d5d69b33c5720ae7c9460974462
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:54:06 +0000

    weigh feature tool early forward

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
