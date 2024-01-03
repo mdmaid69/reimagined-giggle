cf83fc843430a48f4ceb8082371033a841f47be2
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:29:02 +0000

    edge water slide different turnover

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+name = "Python"
+print("Hello,", name)
