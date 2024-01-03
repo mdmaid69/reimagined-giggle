99f58c8686ee96094d6410a39b4c52b654026696
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:39:47 +0000

    team food their refuse live

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print([x**2 for x in range(10)])
-for i in range(10): print(i)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
