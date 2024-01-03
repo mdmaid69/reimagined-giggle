6777e6c698d304a40ab18addf2407f35e872d009
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:16:33 +0000

    slice belief force cry clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
