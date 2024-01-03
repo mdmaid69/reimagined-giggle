f6af6e3e22be411abfd941deaa33e8bef1c7c7e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:06 +0000

    meet imagination warn attitude shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
