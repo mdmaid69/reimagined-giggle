72fe7eefa6b366efc331a25af9794c26a86ee11d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:40:10 +0000

    so measure long birth game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def multiply_numbers(x, y):
-        return x * y
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_as_set(array):
+        return set(array)
