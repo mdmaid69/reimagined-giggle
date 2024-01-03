415e5cd4145aeca0db6e023703b1bf824bc69892
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:31:03 +0000

    pump sore fail own keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
