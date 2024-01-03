a3cedf15f94b8f75979ed71c355c89486db2c11e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:35 +0000

    holdout soak fork block rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
