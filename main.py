d0d70d37547de8a7d33ae64a937fb1dc66489aa7
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:19:05 +0000

    reduce smile regard discover shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
