3717c719c1877cebba54d0b3999550b43f66b704
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:45:14 +0000

    float send mistake authority lost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
