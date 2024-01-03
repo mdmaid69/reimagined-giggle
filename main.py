3b8eb183655ea5ad79a229961f875773acc8465d
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:37:23 +0000

    exploration makeinto pretend suckme swim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_elements(lst):
-        return len(lst)
+  import sys
+  def get_python_version():
+        return sys.version
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
