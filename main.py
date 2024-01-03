4e0023855141d849296cd982dfe75d9bb702ea67
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:11:07 +0000

    patience eye shelter mention stage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def remove_directory(path):
+        os.rmdir(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
