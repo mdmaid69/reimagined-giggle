4c27f285fa0d8b7ae87e39bd74c8d729e3e1d840
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:17:23 +0000

    deaf bridge mess improvement dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
