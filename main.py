0b4215c4b1ad5f8328de5705c76ba4899bae3271
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:07 +0000

    both conclusion breakout mind 568energy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
