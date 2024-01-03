b522269718f2f1851e609354e008382fe8fcd90b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:45:19 +0000

    substance train sail note quickly

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
