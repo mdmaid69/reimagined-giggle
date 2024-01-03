264a20d6e9138c8cf893b6521dd6ad1193fcfe4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:05:05 +0000

    few point threaten interview prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
