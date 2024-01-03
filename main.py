3d63f09183704d6874b058ad63f69bda1827e88b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:11:27 +0000

    draw Yy attempt whether wrap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
