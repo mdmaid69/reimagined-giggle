f865a0fb78849db8f67542e03279e39e2cf5e46a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:23:40 +0000

    goback(to) good track may true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+from collections import Counter
+print(Counter("hello world"))
