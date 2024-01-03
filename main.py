f952572cd2738906d4bf404fd16c22447a4faad5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:26:07 +0000

    Qq means everybody turndown plate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_list(array):
-        return list(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
