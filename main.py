fefb254b4d8ea0b462048aa15ecb3be3320aa139
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:06:32 +0000

    science tomorrow warning way may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-print(random.randint(0, 100))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
