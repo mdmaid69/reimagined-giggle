38405829a6307860b4ad3746b11b41d7a9d08559
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:47:33 +0000

    flavour wash frighten flow spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
