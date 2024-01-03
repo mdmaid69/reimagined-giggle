6b92c300249b9834366424dd5955123afc3ec86a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:14:14 +0000

    wide us danger cap common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+name = "Python"
+print("Hello,", name)
+def is_palindrome(s):
+        return s == s[::-1]
