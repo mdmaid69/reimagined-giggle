90a0a6f60b72e37a72e58abd243691b6901370f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:00:38 +0000

    heart eye pool change present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_power(work, time):
+        return work / time
