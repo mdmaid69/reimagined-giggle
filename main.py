1d4d2d88bd6440502347c02f0e6bc90dd700cfbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:23:46 +0000

    fun after skill rare clothes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def cube_number(x):
-        return x**3
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
