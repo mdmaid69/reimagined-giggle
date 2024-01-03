8db945a530deba9ded8d7be1bb46879497df93b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:23 +0000

    people low peace long live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
