9491433a478a5dfc310cfdd1d96a318167bf664e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:30:53 +0000

    black comefrom by concentrate occasion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print("Hello, world!")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
