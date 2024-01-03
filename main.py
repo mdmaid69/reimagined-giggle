565ac6c1b79ff5f610d9cd131c7eaadd29fb85ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:00:51 +0000

    steep thing geton party maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
