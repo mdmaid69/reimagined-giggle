755dfa75a079719d15ce99bb04b0dd601c448aaa
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:25:13 +0000

    chat charge Vv steady whistle

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
