2f6296d5fc9cf4f34e6e1b91993d606804bd0124
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:36:08 +0000

    unfair start study at explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
