a37941137b7db8b73095be4d5dc9834cd78f2814
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:32:14 +0000

    shame provide laugh keen yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def is_even(n):
+        return n % 2 == 0
