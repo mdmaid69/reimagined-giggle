b2a5c2c208d47f4a4ba9f18a51fbef8b614c6989
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:27:34 +0000

    floor regard nature public teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
