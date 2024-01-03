fc203b458d560993f626c97330091ecb09d84c00
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:44:21 +0000

    sensible room several desperate impressed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def find_min(lst):
+        return min(lst)
