3ebcd872e61d9d9ceeb3dc5256a06bcaef8440ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:41 +0000

    hang emphasize impressive emotion brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def convert_dict_to_json(d):
+        return json.dumps(d)
