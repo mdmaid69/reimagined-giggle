085e443ccf2126c85b68c9df44af93fc7ff16fc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:20:57 +0000

    important comedown mess popular instead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
