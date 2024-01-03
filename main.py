8545f604e8a3e0eba0171510eb3da8c5d1b9d9ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:48:27 +0000

    interested convinced exchange coat measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
