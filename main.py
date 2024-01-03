d5440c9ee2a032be504fed45bc6311f1c3d336db
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:53:14 +0000

    joke warm since belong century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def remove_from_array(array, item):
+        array.remove(item)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
