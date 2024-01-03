a51ae2e97e0f8ad97168427818418a7d9bf3b67f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:47:26 +0000

    section cross schedule long consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
