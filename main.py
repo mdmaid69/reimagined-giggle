9101c46fb5fe03c0dd0410560f2ab20d1545c6ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:23:00 +0000

    office convince raw value flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
