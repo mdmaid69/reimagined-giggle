9a9dbc7756b194477e510d29611d0159c0b7610b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:50:43 +0000

    programme squeeze fall afford achievement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-print(os.getcwd())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
