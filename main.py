dccc6d4b03db2996b9c1a08ec5a63e40d078bc41
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:12:33 +0000

    only concern thinkthrough nigga whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
