37aa8ca35fed2e050f0d2c1c556a13253b01ca96
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:56:10 +0000

    blue command sensitive live radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
