5d3c49e81e6e49fedc177b626d37a9969b64dafa
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:34:08 +0000

    classroom dig length wherever serve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_even(n):
-        return n % 2 == 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
