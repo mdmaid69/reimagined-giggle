01e7a84cdafb2b765bd3fd6777356ea4be8151b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:19:24 +0000

    old affect stretch either apparently

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
