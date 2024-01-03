6ba964032040f2cef17efb451ad414d100a89068
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:33:38 +0000

    quickly urgent total succeed neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
