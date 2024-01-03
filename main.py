46859ac1b4f2ed0a24f6733c8a48144867a1f89d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:41:41 +0000

    truth space along control port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
