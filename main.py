3575975ce5c4ddacb356b63ba77b047c60b9cd8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:43:01 +0000

    date doubt passenger plan decide

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
