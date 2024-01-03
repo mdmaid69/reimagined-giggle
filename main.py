c15f8c5bff3cfa147fa0badb56bfdd5cb15dfb05
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:22 +0000

    go knock advice issue list

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-text = "Hello, world!"
-print("Characters:", len(text))
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import collections
+def create_user_list():
+        return collections.UserList()
