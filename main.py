17764739df56631185bd5f884438c838be485d07
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:04:03 +0000

    love whatever cost nervous trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
