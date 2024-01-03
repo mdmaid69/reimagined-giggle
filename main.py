c866c87fa715ad54134b943b3ab7a6763f9c7d04
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:50:53 +0000

    opposite ground treat immediate body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
-import random
-def roll_die():
-        return random.randint(1, 6)
