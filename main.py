277c6cb1a5ad22fa29926cab62d48816cf93504b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:54:11 +0000

    machine heat clothes help load

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def square_number(x):
+        return x**2
