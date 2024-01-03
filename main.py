1ef8e858c3eabc441473755a7147ab33f8205c55
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:10:45 +0000

    capital suit unfair similar grateful

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
