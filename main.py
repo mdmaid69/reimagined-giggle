a0506de652e83d9fc587f618ac7f5e61290aa00d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:59:34 +0000

    pride letter crowded argue production

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import tensorflow as tf
-print(tf.__version__)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
