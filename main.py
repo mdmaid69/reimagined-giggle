8793b411a381a12d0724ff25d941c7cfa67ab991
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:29:48 +0000

    watch speech mess violent temperature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-print("Hello, world!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
