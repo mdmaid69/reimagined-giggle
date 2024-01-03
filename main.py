9c1e4404407c8c03f824f3e556965213b9ccfdc9
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:42:37 +0000

    must sea morning worst feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
