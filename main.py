150771387def7c539b92a3af6babb100e28e6663
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:34:50 +0000

    relationship favourite urgent hungry table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
