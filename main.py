7a4f844ea13da494a1ec2a4da4552cd718a241d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:25:36 +0000

    breakdown but open mind possibly

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import tensorflow as tf
-print(tf.__version__)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_current_directory():
+        return os.getcwd()
