587a9cac93ffa705465722da84a44c43f8a6f618
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:17:53 +0000

    cheque pleasant crazy comeround ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
