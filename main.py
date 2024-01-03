274345005da92c4426b06a9add636c1c41b2ff9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:35:02 +0000

    best live identify pain name

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
