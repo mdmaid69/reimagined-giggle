e5baa0a4eff10aea2f46398a5cedd82e909ee845
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:08:59 +0000

    among police show share left

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
