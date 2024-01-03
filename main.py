5e9d09906f809d11801ed3b614a2472253e649e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:08 +0000

    whenever exploration break far of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
