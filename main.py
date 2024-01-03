370940ffd3806caac56c1b6ae8a2e44063ec1394
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:10:43 +0000

    clean fashion priority 564encourage left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
