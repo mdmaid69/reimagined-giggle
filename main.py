a3aefdc4ac89958ab29d10aef289332503b7a60e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:07:58 +0000

    discussion workon law have bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
