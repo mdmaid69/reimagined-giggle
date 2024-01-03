8547b3eecbdae818c0ee443f809c1951279c8978
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:39:06 +0000

    stamp failure bottom hello under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
