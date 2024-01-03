25525fddc00f6e2d0e1a0c8d5e28b7038c894242
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:43:47 +0000

    copy transparent prepare public medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import getpass
+def get_username():
+        return getpass.getuser()
