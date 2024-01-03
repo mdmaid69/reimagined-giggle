6d706df293848e361c493e8536d3c6fb0d36a332
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:29:55 +0000

    correct big damage bound expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
