55124ed9a27c013f5658e57f72eb0fed4a12c049
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:30:43 +0000

    union life dish affect Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
