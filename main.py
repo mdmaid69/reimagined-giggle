1a3e71951efa2cd452150fbca8330f55919e191c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:57:10 +0000

    such turnup hurry corner play

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
