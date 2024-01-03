3ec9fafbdf517d529b4950bb9d769b93b971ce9a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:12:06 +0000

    emergency attention comfortable evidence treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
