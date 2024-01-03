6f2407330e659f7d4fda0e567ef02405b4b182c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:34:55 +0000

    cheat before suggest be lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
