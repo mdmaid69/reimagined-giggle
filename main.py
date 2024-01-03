d32c3f76880957a7f1a4cf48834c5e4a4b92df05
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:20:19 +0000

    live swimming claim example production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
