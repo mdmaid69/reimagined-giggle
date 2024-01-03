4ac3a2a3dd9ad1c65e605c36c19977cf62d536c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:12:25 +0000

    start hard frighten wear public

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
