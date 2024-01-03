3f174b2f4d0273162e9b858a96c094aaafca173e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:13:26 +0000

    blow star allow opposite certainly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
