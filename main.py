e9866efad3ceae683c6aa722212f7959adf1b3ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:52:38 +0000

    dare voice after kick mine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
