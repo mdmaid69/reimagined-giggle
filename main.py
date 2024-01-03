0e1fef76b1b8cd06108e77e8f509e4cfa7473471
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:13:58 +0000

    thinkover everyone round exact himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
