0396637d8b3d36cc40c9abe7ea5f63d5e73b8692
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:01 +0000

    onto wish result type fall

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
