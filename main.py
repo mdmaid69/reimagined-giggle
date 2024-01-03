d9c0fb278a723ec1bf0d20c693256d738ff7a662
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:22:11 +0000

    boil rain mine talk represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
