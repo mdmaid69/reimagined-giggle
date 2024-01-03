6e68abe7647d94a0af25ef51f3722e8be87933a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:57:15 +0000

    second likely most there fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+print(math.pi)
