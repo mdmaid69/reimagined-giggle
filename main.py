2d6cb1c702a926a0eb903ac4bc3b5280e294ecc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:33:00 +0000

    uncertain advice surface delicate hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
