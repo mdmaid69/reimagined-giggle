48a85163b9e2f73e3468e6688347e5d92f398609
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:05:27 +0000

    shut progress test sensitive claim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def square_number(x):
+        return x**2
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
