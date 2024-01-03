4a4e9619768506e0f79aa1f3946bcc71cde1faa4
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:03:06 +0000

    your last situation exaggerate bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_current_time():
-        return time.ctime()
+  def get_base_name(path):
+        return os.path.basename(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
