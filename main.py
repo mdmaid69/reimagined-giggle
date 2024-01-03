85a1e18f6d68a95b533c8567f45c79d68cdaa131
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:29:36 +0000

    downstairs perfect receive press least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
