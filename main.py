844513ec1234139ea919c91cfc04634c2ab87bd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:35:18 +0000

    passenger task use mend basically

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+for i in range(5):
+        print(i)
+print([x**2 for x in range(10)])
