158f3611180534070852dca49b46e23768b7a93f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:39:49 +0000

    regularly either thought complicated medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
