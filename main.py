4dce6f959bcd0c0eececb8964718472d3f5ee117
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:59:22 +0000

    month wave supply telephone connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
