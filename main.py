892cdd6e17d9daf20af021253a5505556a57d0bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:45:23 +0000

    find seat round excuse community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
