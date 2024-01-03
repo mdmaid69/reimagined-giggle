b668cbee0851305184388c009e63577205a21df5
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:33 +0000

    with pity turnaway identify bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_acceleration(speed, time):
+        return speed / time
