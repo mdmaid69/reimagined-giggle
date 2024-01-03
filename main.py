39eb9d8bb293f9dd7497226770080f0acd38a2bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:32:25 +0000

    print error professional affect someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_power(work, time):
+        return work / time
