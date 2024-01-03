2bbeab40c6a7644acd2812a88c5aba5dd7f1e019
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:07:54 +0000

    either centre warn physical own

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-print(os.getcwd())
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
