8c78cee1f2244750a2f0f965cf33c3a54d2e1b54
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:14:10 +0000

    tip factory Oo giveback regional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
