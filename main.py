be78f82ea4c5584476f0164447b10b39ce9ba86f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:50:44 +0000

    word pencil relief related handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def is_even(n):
+        return n % 2 == 0
