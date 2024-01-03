4ea5c84dee78148a8c2f44b6a16e4c277edfe12e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:13 +0000

    mass rich shop below further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
