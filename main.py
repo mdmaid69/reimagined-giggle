663600727f13acb5a5bffe11976398b90c9e6cd9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:47:13 +0000

    fun injury effort 573entertainment fairly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
