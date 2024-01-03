9a3cac0f0adc3baebeb96d11d2e52089eae693d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:48:37 +0000

    on rescue rich ring dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_directory_name(path):
+        return os.path.dirname(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
