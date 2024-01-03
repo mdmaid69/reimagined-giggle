6b37656fca504e051e098f78fba4163ff331d35c
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:25:54 +0000

    Dd shut many ready bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
