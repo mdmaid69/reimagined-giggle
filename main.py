e5a8aa2db9e2a10dd6453821cd42468a88b0c105
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:55:03 +0000

    badly cut stay once pot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
