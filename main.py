33ca7f0ab42e3cf8221fbe674e1e2b42e726b02b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:23:28 +0000

    who tour Yy stop badly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(10): print(i)
