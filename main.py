cfc025f2ebd38c2976f913f651170fd972ca5c44
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:10:42 +0000

    short speech side guess office

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
