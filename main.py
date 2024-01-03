e3045591600f740c1be968f4962a1a1def7c974a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:04:41 +0000

    harm confusing sick quantity nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
