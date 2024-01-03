ea0e0a84aba77b3b604d1989b0f8772c99329f47
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:41:02 +0000

    fat could hope mess comfortable

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
