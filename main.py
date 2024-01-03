93c98289632b533a00c3284ee4bf5d49e2ca7cc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:42:58 +0000

    spring rent Ll present shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
