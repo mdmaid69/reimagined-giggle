15ecfb3eacc484daa52af96efd5fce9f3c2d95ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:48:23 +0000

    organization upstairs prevent closely fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
