41774fe1ebd8e3b1c842f23815f0f0e476c29db4
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:58:55 +0000

    impossible weigh wash guide 572enter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def split_path(path):
+        return os.path.split(path)
