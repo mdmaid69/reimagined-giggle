da16c5b5ca8b59b63f7dfe507d565638f54da50c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:11:48 +0000

    corner conversation radio wonder defend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def create_counter():
+        return collections.Counter()
