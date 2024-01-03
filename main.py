8abbfb5e5fc1ad38318f1dcc22fdd88550d59e49
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:16:21 +0000

    block visit brother thought warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
