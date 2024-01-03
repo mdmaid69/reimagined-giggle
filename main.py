d6c2326ee254ed0ed776e5efaeb6ec8cddd11236
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:01 +0000

    habit keepout model street intend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
