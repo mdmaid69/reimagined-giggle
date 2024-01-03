44f45a43663f634fb9cbff7c80c07275523868d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:25:16 +0000

    street generally place condition perfect

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import sys
-print(sys.version)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_area_circle(r):
+        return 3.14 * r**2
