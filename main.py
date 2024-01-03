9d49da33ec63a228bd69a561a8c2c9fa23e25042
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:37:04 +0000

    stage acceptable determined return alone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_current_directory():
+        return os.getcwd()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
