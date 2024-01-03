2a00291b9734ad169ba34c3f83ea7b080b3c4b1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:07:50 +0000

    file detail kick the turnout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
