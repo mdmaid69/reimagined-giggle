3a0c63ceda83e62a266c3c1789cc6328ae5eaae0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:28:52 +0000

    command result occasional like duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
