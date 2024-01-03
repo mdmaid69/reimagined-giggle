227f97c6490978427601d4706abff747a10a450c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:02:00 +0000

    sticky discover support sign Ff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def is_palindrome(s):
+        return s == s[::-1]
