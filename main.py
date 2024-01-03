8ca933cd05c918989ba5cfaf2e928a465587d752
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:08:04 +0000

    place onto scream base long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
