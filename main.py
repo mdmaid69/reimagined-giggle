4f32ed0d6edc7317d117d5efd9bc34826af5256b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:00:32 +0000

    race form extremely bother inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
