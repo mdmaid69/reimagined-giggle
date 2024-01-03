7041db96eaa12cc46f5d000a56eed47911f4404d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:52:30 +0000

    middle about star smile whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def add_numbers(x, y):
+        return x + y
