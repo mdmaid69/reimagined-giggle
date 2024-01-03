dd40a21ebdfdf872fa220f39c2470493aa2c6117
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:26:02 +0000

    food base reply sister takeoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
