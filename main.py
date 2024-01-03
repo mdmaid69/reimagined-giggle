3cbe15806114395c429a9393c777ad2fb49aa1e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:44 +0000

    government apart sad unless art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Characters:", len(text))
