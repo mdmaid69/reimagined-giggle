9f9e01b0f2fd77c84453504387eb723a792e0cb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:39:08 +0000

    calloff shut reaction fall across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def find_unique_words(sentence):
-        return set(sentence.split())
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
