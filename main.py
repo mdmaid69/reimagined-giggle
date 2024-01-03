ced657f29f9573d6fb62997da176ec49a79ef563
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:23:18 +0000

    autumn mysterious side rid hear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
