efcd60ca7a62d9a53cdc03015529fd79534a645d
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:32:42 +0000

    wall refuse needle not appear

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
