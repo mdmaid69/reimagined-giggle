8f5667aaa925ba845a331206e3930a9667b3539b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:40:34 +0000

    exciting yet voice practical loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def convert_to_hex(n):
-        return hex(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
