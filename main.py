e9420b46ea00cd87fedf7ded0a1633984470328b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:16:49 +0000

    succeed present possibly wish sister

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
