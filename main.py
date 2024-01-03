a83fb3142bbf716703e78e1c43ab28d8b3d2e8a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:44:48 +0000

    scrape understand birthday classroom pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
