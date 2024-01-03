ca2125f876906d6c4618d794d15d94b3dcff4038
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:55:22 +0000

    fix evening relax game length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import platform
+def get_python_version():
+        return platform.python_version()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
