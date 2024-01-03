7b4b1633b227653d390a2f43f0943e1ad736dfb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:07:42 +0000

    thinkof some loud arrival chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
