cf01a2ae7267082487da6f3f481326e3530e252d
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:11:27 +0000

    guide pencil 573entertainment profit describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
