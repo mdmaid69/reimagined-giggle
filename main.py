57bbae25c5bef702b44e69ad23764cdb96b15753
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:32 +0000

    workout service get receive job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
