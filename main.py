038e988206827a8b725499e89d96aa856b9c850e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:02:28 +0000

    affect takeback shine control yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
