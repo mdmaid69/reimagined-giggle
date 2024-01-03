d8c7db065096bc3760ef8c8ac17ab7a4f6eb3edd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:53:38 +0000

    threaten cheek buy expensive immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
