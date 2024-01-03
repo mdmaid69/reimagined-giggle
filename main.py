ebd70b0a4eb4a63c461a307088f4b8294f9232c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:11:10 +0000

    search shout dream operation reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
