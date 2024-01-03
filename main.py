729440157b368ebe271803d6ec1118045191670b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:00:55 +0000

    turnon gather loss no influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
