57d39b40aa7ccec274d86f52691cc9e94f754e6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:16:03 +0000

    tense noisy relative invitation collection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
