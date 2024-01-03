02cee2d2bfac8bc33ffad1ef5e067580b8bac9b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:13:08 +0000

    flat her middle among splash

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
