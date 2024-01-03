6779661bd8571498513278cd188daaddb8e969c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:27:31 +0000

    shake round busy but special

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def add_numbers(x, y):
+        return x + y
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
