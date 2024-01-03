93b482dee717516091441706b4dba2db677fd447
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:57:18 +0000

    tear situation sell frightened out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
