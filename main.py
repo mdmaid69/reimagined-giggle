31786abb8ff7f7495b9bc6da280046317c6447fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:38:46 +0000

    strike official hang arrangement constantly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
