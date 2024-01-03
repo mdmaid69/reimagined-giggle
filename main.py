86dbecef62e3c64228acc47ab5e5bf9e078e9273
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:21:42 +0000

    tone preparation forever dangerous minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
