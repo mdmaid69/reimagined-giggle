394e01a0db4a81d2dfedbf175c0fe8855e67dd7c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:54:03 +0000

    side start rank too satisfied

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
