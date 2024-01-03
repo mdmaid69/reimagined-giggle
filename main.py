48066474627d30cf86bd58eaeb07e2fcdd47c2bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:09:03 +0000

    shopping weigh get exchange attempt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
