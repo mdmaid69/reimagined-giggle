5b513b49c50ac3e99b9267212140752d26651f43
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:08:36 +0000

    increase refuse bored polite ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import getpass
+def get_username():
+        return getpass.getuser()
