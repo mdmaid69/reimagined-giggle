73e1243f1d93e782bcd51d1cbb30ef5f61a84ee4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:20:55 +0000

    kill around hard lookback(on) estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def remove_duplicates(lst):
+        return list(set(lst))
