eecf40419acfdaa90fb30b629b1db6f1955d5014
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:26:00 +0000

    light mainly cause at feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def subtract_numbers(x, y):
+        return x - y
