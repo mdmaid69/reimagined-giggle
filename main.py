80e00a8f0be50904ed5c30f47e005c0d0cb8270d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:18:35 +0000

    embarrassed against cause brave help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
