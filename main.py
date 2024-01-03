dc6a54e8093acfd36cd535ec4af05dda63c71ad5
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:01:50 +0000

    mark weak spoil spring keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
