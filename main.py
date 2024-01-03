fd40bb1bcb147ab89306edc8babec841972f368b
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:32:39 +0000

    information behind music famous breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
