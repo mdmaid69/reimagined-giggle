f2a6e8ab5360a34f991d164829e00458726d73df
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:59:17 +0000

    reserve especially really aware complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_list(array):
-        return list(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def append_to_array(array, item):
+        array.append(item)
