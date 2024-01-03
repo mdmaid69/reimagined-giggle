e0999e2469962bf3c167d085ae3cb9dffc5319e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:07:31 +0000

    practise clear turnup pupil discovery

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def subtract_numbers(x, y):
-        return x - y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
