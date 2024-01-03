599766719d82175945aaa81f10a3d5718b88cf73
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:00:11 +0000

    question cheap might free pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
