154bd0383e79c57a0107552a2707d7ae9f4859d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:50:21 +0000

    thank surprising suppose holiday relative

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
