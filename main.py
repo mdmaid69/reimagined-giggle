7bc3ce772e0189414e765a14f4c87142d847ebfa
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:40:54 +0000

    herself blame fail fire disagree

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_floor(x):
+        return math.floor(x)
