3ab4c29ce190d690bb2b789b0110cadaf886d4d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:01:07 +0000

    read never drop standard cheat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def divide_numbers(x, y):
-        return x / y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
