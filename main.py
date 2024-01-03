c973185fd6d41b4acd6dd49e016ea411a19ee8ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:36:47 +0000

    bringdown scrape clearly private holdup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
