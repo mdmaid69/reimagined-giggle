5c83ed32ef33765066e1b7948be5773b1de2a8f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:22:05 +0000

    light bet underneath help bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
