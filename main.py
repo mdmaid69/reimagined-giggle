f9f32cc05fce0ad48af9073179f9fcccd3fc78fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:23:46 +0000

    shallow remark goaway rest stiff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
