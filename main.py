53f8220435cf0ba94ad0497243fb9c651e3135fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:31:07 +0000

    advice uniform responsible future branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
