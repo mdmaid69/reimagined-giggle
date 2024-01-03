b077ce016963f4cc5179b0530d2c2b565c50212f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:28:35 +0000

    nature organize buy finally cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
