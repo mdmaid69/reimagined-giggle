fead7cda37bbf961468996cb6186b19856ceb6be
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:12:15 +0000

    between weekend refer lie flow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
