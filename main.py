7d60a1bedfd8bd2309f62ba38e1695448ebdbae4
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:09:34 +0000

    know fortunately please hospital by

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
