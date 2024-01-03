c180bd71ee9c866647e08cccb646ce7196c083cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:33 +0000

    by fill mark discover consult

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
