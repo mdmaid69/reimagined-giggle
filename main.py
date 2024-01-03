3697aa2b4b13c105b4b1c18b2364783d27e32422
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:15:12 +0000

    shallow important whistle acceptable satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def get_list_from_array(array):
+        return array.tolist()
