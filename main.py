075d24491286be85e52a21279ce154d51838f1f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:08:39 +0000

    custom whoever whichever argument advice

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-for i in range(10): print(i)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
