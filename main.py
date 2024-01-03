3bfaf68671267f0b23d94aa662b07a579331bc19
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:43:00 +0000

    again case complain top health

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_float(array):
+        return float(array[0])
