3ab91f1d17900236022d86923b4897419045b510
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:53:16 +0000

    empty order excuse lock relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_stack():
-        return collections.deque()
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
