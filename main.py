f495624aa721ae61f0cb8b6e7f2cfedd807d3ddc
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:50:41 +0000

    moon contrast getaway spoil company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
