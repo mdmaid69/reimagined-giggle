bae6fcb82edda03e64c0477a13cacee6799feb88
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:28:05 +0000

    disappear getout sport tired threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
