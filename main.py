63f3c8632e07a66c648fc8a1d7e8f8e855e1da91
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:28:15 +0000

    miss pity this record silent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
