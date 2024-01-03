2e285c8c0f1d49083f29c93618d5332a48c52592
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:54:04 +0000

    spray no yours score myself

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def square_number(x):
+        return x**2
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
