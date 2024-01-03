6ced7a90b304bd92517ec7f87b4bf1a72b385e8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:44:05 +0000

    warm difficult apply avoid balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_list(array):
+        return list(array)
