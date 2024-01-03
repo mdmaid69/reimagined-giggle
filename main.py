86b81260b60619f8dee0948e50288dc2e84f83be
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:44:01 +0000

    behaviour sensible gather many there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_floor(x):
+        return math.floor(x)
