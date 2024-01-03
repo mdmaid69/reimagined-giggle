f92de0a10c8894a9350dd1c79fe49e62d0ad6d7e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:58:48 +0000

    pencil promise goback(to) of fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
