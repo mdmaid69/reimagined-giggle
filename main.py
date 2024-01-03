b532ecfb64480607f5bd8cd4abdfa77e91495ddf
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:51:54 +0000

    fault bag fasten appeal taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_item(array, i):
+        return array[i]
