6996d92b6cda2fe937d8917bae73e04544cdb1c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:49:24 +0000

    tense complicated say minimum rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
