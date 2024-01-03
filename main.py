63341a34e14c9184b61a4b9bb64547594899f2c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:05:19 +0000

    school progress wrong fly move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
