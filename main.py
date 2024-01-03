02ee200d4960d55d2d1b29f6fd4307fb4d552675
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:14:26 +0000

    line rest trouble left worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+x = 10
+y = 20
+print("Sum:", x + y)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
