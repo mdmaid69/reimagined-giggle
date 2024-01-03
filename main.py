7bc3ecc5e87c8a0378c797ccdd610527ecb80d34
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:48:15 +0000

    mine threaten confuse make excited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
