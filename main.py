8e9c55ae9d8133b6299dd44c040a6a5ed27549de
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:30:54 +0000

    whether failure snow disturbing steam

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
