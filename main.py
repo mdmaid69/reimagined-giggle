d15e89da65e1c6f5ac4ad0fd430508eda0501c34
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:56:58 +0000

    rich pressure software possession sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
