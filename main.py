ed22f002203e1e15cf6ad4d2ea9b0181be4259d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:11:10 +0000

    division match getup split whistle

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+print([x**2 for x in range(10)])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
