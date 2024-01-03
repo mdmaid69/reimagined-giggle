c0e1ce6e40f9dcd9e803a28bda860da1e8f9fbc8
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:15 +0000

    hard smooth expression other track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
