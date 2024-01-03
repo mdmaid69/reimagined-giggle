1dc836ac897072ccc44834c2443b20d42432f514
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:39:18 +0000

    bend enthusiastic plan cross reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
