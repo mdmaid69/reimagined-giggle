d9c6de47eb26db986ce0c647cc77e2baf9aac084
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:28:45 +0000

    cry spring past defend advantage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
