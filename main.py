7c7676b889d38ae2cd2dafe983bce8ccc3784392
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:33:42 +0000

    priority challenge owe disturb accordingto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
