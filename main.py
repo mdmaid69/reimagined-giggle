2a390b5f6faf2d2c0ea180451e0899e0561152ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:52:49 +0000

    vision build start sign firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
