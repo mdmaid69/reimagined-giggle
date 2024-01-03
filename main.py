38437f621845f740651bf78187e17219ea6a49e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:11:16 +0000

    whatever previous loss nice confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+def get_today_date():
+        return datetime.date.today()
