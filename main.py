8f4f2954dc5838a2e716ef32100e2193bd50b2c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:11:33 +0000

    praise awake if progress tax

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
