0aada4b41445862b09b3b1dfe130eabaed2c0e5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:52:29 +0000

    Qq chase choice soft behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
