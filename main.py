8e76a4b1e292fded7c9468ebcab64a39fa74b2e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:00:17 +0000

    drop lesson disagree home 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
