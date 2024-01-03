4f3d5bebcefe7a243d9604089304f3cecd34b06c
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:33 +0000

    relax understand impression become claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
