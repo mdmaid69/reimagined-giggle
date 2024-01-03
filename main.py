3c69b6d8fe2617856de485f24625488808ddaaa5
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:57 +0000

    deaf far slow usually admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
