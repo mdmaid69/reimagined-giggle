38db318cd41d3325a1dc78c776a1cdbde08f723e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:55:11 +0000

    divide interesting shut inside Dd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-import os
-print(os.getcwd())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
