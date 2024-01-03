7f47a183a720db5335edf1d1450ec82abe219a0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:05:42 +0000

    generally stay shut transparent everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
