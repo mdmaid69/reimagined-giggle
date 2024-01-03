75c27744315fdc17d144bad749147ed2ebb08afa
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:58:31 +0000

    suggestion press disapprove from shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
