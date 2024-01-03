a1de23247e5ae9dc6dced0b1ce2e577002a73834
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:06:27 +0000

    thick deliberately along post pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
