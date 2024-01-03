fd505099a7756cf6058e3d861b02f30c5486b00d
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:47:07 +0000

    burst pay quantity notice build

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
