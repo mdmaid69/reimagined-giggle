9ddf4fad65a20d4f5a851fcb35a0b6013ace8620
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:47:25 +0000

    wave need convincing hair operate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def reverse_string(s):
-        return s[::-1]
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
