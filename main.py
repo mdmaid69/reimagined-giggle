b65c23855e0e8cd4c0c34deb706ddd6a2dd5f09e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:08:03 +0000

    hold total come rarely expect

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
