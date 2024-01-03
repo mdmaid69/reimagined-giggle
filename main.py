a82410f1be72f741d974f71e980fe344acd9d1dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:18 +0000

    late our want correct open

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
