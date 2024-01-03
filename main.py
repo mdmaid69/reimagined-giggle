d4e6a74779a0de73b6c86e4fd4b8a2fc02d11be6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:37:53 +0000

    autumn shall turndown border plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
