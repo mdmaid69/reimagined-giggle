79e874df765786a583a0c0d99587b8c99f20040d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:15:05 +0000

    why now now plenty mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import getpass
+def get_username():
+        return getpass.getuser()
