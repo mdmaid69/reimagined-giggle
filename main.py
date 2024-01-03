79ad20ac3c7f0964dd556c6eb13e2f60f3749a26
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:37:23 +0000

    copy thin subject backwards mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
