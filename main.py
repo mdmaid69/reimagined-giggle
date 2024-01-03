29266206d4c94c00970765f19df8cb104ef0dc3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:51:15 +0000

    goodbye bear basic compare young

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
