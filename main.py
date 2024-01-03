b8c694deabb3af8a6510f12e6c23a16eea7ff60c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:19:58 +0000

    office back reasonable substance build

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
