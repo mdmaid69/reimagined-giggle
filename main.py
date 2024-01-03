e2adfbe19539004023c1074afba985b6d28e4335
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:38:30 +0000

    order lay favour level impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_exponential(x):
+        return math.exp(x)
