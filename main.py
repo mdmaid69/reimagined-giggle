437a7fca07dec63b5eb4d34350d5ca20b1ddc7ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:17:57 +0000

    dig much pick hungry post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
