636de26fa9bb2e7ad0c897c538e89455c55f160e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:18:16 +0000

    responsibility roll injury between steep

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
