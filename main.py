90e91c519eb1f4a6cc68e770deb3b3984b450491
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:53:01 +0000

    cut before sleep immediate during

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
