4489d871f8eee102d6b33f9a6d4dcc63ebb93154
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:09:08 +0000

    form frighten cool grand male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
