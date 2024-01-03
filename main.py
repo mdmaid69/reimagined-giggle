08ee363fd7aad2f8837a36f39bcc7dd358665b6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:35 +0000

    holdon control way stop figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
