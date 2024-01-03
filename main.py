e51e9540feb13139a53023ff410cbaf27e4c4dfb
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:54:17 +0000

    opponent onto side share smoke

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def cube_number(x):
+        return x**3
