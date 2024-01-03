a97aa82196426c0a65f1ac1d1ceab00b82144821
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:41:07 +0000

    television concerned long snow quiet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
