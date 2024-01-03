a0f1ef6a6fecbaba500b0211801c682309879cbc
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:44:28 +0000

    bringup part guide evening interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
