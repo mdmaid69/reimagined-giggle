14eb2eae00053e9d0fa2d1e99c3f4b263e8acbce
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:11:29 +0000

    aim various rate need cope

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
