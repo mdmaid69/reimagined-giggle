d5d40906aef9fb59476f126b37bcce4ae82c2aa2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:18:29 +0000

    account negative fast deserve early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
