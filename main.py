e0162b3bf4ef52bc6099f1d14f22e5216c4f2968
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:45:09 +0000

    new may increase emergency lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
