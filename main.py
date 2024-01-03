41ba68c3c46544337202899209f60b0d631fdf40
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:13:44 +0000

    frighten meaning she strange discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-print("Hello, world!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
