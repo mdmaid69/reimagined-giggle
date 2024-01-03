ed2396f1851fe563d2f69cac0a6a85c10748c658
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:31:27 +0000

    lesson yours child exactly listenfor

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
