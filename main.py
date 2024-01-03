a303e379a2b25ac72cffdaf2aee864808c64b5df
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:17:25 +0000

    contribute separate achieve shoot opponent

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
+n = 10
+a, b = 0, 1
+while a < n:
         print(a, end=" ")
         a, b = b, a+b
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
