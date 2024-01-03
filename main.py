1ee9e6f6042a61deb66b35c19be6fa8967aa0965
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:01:58 +0000

    better remain often shelter bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_power(work, time):
-        return work / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
