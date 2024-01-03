9fc4f7e169e76dcabba44138101bbec10583740f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:06:27 +0000

    Pp real least solve direct

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
