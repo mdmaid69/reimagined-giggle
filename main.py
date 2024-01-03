87b24d002d52d3616d5069718e7716f0c58bead8
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:20:43 +0000

    favour accept trap tidy scrape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
