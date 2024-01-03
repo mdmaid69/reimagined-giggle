71c2d90cf79813fd3177ead1c609143d016cb537
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:16:12 +0000

    related no uncertain mark show

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
