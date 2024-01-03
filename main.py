ba4dda9ede25aaf71e12103ca5b8b95bba0532cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:23:35 +0000

    except excuse weak should there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
