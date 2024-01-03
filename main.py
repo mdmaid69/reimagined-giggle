b739d5c49c35e43ec4de52888204b01919dd71bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:33:04 +0000

    bill forever law theirs of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
