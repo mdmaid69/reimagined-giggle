96941b2fa739aa8e0e4eed1e22c75c4f9052f365
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:49 +0000

    rarely myself he rarely report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-for i in range(5):
-        print(i)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
