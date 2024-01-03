7448dcbe511998dcb2628f8e85acc29ba1b141a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:17:30 +0000

    cough spend exhibition hello conclusion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
