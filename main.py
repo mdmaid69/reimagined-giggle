521450d7d23381bbb4029adce1c986a46b27c4b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:11:48 +0000

    shade face bang priority guess

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
