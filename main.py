bdfba9ba2798c21e10115141830a2ab60ba35cca
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:21 +0000

    door fun my recommend still

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def reverse_array(array):
-        array.reverse()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
