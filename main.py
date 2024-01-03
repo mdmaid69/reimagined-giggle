05bfcfd2255e66771dbb216097af08e5bafd1cd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:48:43 +0000

    shallow give study contrast unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
