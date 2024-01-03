419f982dd5df761dc003a291fd634800253a59cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:11:46 +0000

    sign give black almost talkback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
