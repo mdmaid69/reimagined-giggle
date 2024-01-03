e72c56d98cd8421448fad49745ba6e4c0d740064
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:26:48 +0000

    complain else read getoff concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
