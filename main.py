2cad3a7d731a7d7e41d6979fe44ddc9c574318f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:09:20 +0000

    fetch underneath good tongue freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
