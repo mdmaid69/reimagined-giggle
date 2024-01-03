613e5f3d21075c6a7b68292349c7b069a085a1fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:29:26 +0000

    slip beyond address show thinkover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def get_time_since_epoch():
+        return time.time()
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def convert_array_to_string(array):
+        return array.tostring()
