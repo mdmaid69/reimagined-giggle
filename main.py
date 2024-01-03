1c465d6400b3e6917f4bc0f4efbc7b06ac23a75f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:35 +0000

    error turnoff seat fortunate lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
