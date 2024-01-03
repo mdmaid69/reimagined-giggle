f14500222bde057c605ce89d32086309137e298b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:39:21 +0000

    more force spring regard deserve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import sys
+  def get_python_version():
+        return sys.version
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
