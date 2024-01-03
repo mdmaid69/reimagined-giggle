6bdd10d2f856107b2a7339943bd58e77651ff626
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:24:57 +0000

    nor square steal let before

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
