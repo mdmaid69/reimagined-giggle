6030bbd52c7977076b8d9beffe6a8351cdd80d60
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:21:08 +0000

    design doctor snow dress later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_binary(n):
+        return bin(n)
