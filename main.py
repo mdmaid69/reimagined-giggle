c76ed30c1046c282477c99061938e3fe3fd3bd16
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:56:12 +0000

    Ww swallow Cc rent once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
