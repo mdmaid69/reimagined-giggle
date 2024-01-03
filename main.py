ba533a64ee09acb93ee9636a2812f35a736eadf1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:01:52 +0000

    traffic play traffic chase trial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
