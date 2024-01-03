01613c54810b96814e5b3fef9f0e5c65a17f80a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:38:34 +0000

    surprised hair regret job share

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
