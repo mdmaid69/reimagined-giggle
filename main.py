fe9ee6db151e8ec3b4744ea1dadb078cf079fa9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:12:34 +0000

    preparation habit could frame getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def convert_to_hex(n):
+        return hex(n)
