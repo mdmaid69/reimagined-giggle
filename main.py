76a396f77d3bc5891f4043af1f80dd7e75b696e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:14:15 +0000

    war effort my can frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
