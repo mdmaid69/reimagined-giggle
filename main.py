8c680888fa239cc4749c7dd56be77952c3e264fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:47 +0000

    obvious muscle earn run big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
