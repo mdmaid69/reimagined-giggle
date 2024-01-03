77869ddb58ff9fa81ea86d6fd7b5880b6189d164
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:21:47 +0000

    shine them afraid plan large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
