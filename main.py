7663b072cbcb955c4b652b969e4fdb68e842ad45
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:41:24 +0000

    telephone ill funny unit fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
