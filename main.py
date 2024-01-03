9e08e24a1337e4554a35926498a20fefe29e8e97
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:30 +0000

    getaway equal will workupto lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
