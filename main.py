8185a192953e2c2ef3930e22bec0c2c835404537
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:18:54 +0000

    introduce mainly development interfere refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
