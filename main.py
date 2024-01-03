3fd5c6ef13c8da03790f071f650b991221a16f2c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:31:28 +0000

    serve sensible against Itself sharp

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
