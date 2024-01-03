125e321944aa7e954412023ad3c2ce82c3a43c54
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:58:55 +0000

    couple tour 571enough want find

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_ceiling(x):
+        return math.ceil(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
