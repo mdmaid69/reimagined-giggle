5010d1bcf5ed3749aff22918e2f1e66273f229ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:58:00 +0000

    spot tight properly ourselves apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def convert_to_binary(n):
-        return bin(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
