62c0dc0508f18a70cd5da7ee1fe7a2bd516695cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:43:55 +0000

    pressure soak borrow basically partner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
