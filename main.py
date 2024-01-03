ba93363d609939793636e562c31993ae575daa01
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:52:00 +0000

    area perform second niggaacademy bend

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
