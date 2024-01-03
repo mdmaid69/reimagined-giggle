80070d881395f5691d06917f85882588ab8bf028
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:23:15 +0000

    same absolutely disgusting turnup mention

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
