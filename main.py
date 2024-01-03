551d976bd13d6ebbd3021c998ddc22cd185fd071
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:20:18 +0000

    centre mind doubt limit far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
