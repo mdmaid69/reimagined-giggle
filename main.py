f6ac5696a936268ec6d7eb2ad1af3c62d2a4d3db
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:54:02 +0000

    prize naturally lucky ordinary skill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
