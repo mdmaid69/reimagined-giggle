b4fde64610dabcdd2530417babaa3e454819f8ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:50:52 +0000

    do children over reason process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
