258801057a7db379191b8714f45ecaa385c33033
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:52:03 +0000

    life impatient basis suffer artificial

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
