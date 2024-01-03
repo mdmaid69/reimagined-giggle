9ce3f39840b976d9536383a30347304e241e5992
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:54:30 +0000

    tense key normal song heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
