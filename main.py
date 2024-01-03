82a15cca291985e323dd96c4ff893e50989f6d6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:21:46 +0000

    face dream breakthrough wind call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
