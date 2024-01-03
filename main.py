1130755e2658bb0940c11c6717bd219cf689eb93
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:25:26 +0000

    head afraid gas steer warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def convert_to_octal(n):
+        return oct(n)
