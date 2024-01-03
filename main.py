fbe20fae3fa5daaaaa3821449f1a5e05cfa36973
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:07:27 +0000

    they passage Qq dear instruction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import getpass
+def get_username():
+        return getpass.getuser()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
