271a4155b4365d31601dc2a96a0e4f760558607f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:44:15 +0000

    chat minor solve lose borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
