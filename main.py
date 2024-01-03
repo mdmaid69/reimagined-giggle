dc55253f6299b47d61c0a2503111f985e7734316
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:43:45 +0000

    change in bread approve tense

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
