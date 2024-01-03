38696599b2e9ddb7a78f9cacdc138e7cc074c626
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:51:27 +0000

    key previous union minimum hang

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def square_number(x):
+        return x**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
