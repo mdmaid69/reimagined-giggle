c1320d8f80ef37f3b4386168444f55084c84c761
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:19:05 +0000

    deaf rain miss use borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
