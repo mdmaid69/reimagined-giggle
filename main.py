8091b9b4acb6810132282e45acb4a80f9a14fa46
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:34:28 +0000

    breakinto accidentally heat prove Dd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
