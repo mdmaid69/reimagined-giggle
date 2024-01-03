50c86d1ec5db12531a7f292f4900730c501708d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:22:38 +0000

    odd arrange bury beginning note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
