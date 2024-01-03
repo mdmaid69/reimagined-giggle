c77426183f85cb4aeab0cde9a0631b6f4bb81bc6
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:34:42 +0000

    who collect poison sudden both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
