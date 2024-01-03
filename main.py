26cf55f86ed1800e5a34799e71488da92df75311
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:35:02 +0000

    dance underneath swimming fortunate criticism

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
