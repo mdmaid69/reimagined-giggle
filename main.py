e1edb38fa5e4fc568939ee35a50a2529749e3366
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:40:11 +0000

    pretty wonder uncomfortable hold history

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
