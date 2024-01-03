29f06c146cfaf953f3fec35a17edcbdbf1ad40f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:07:15 +0000

    beat glad calmly last can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
