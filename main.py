8032bd5896e064b33559d9e853b9ad060795da16
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:17:11 +0000

    and bringdown ticket goaway always

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_logarithm(base, x):
+        return math.log(x, base)
