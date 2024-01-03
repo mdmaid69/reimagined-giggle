fde5b951ac2e23af384c8b812a2fc30610babbd2
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:40:58 +0000

    admit contents instead forever bread

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
