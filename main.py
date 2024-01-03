b17748696e06756b593661ced84e3187d73e7c17
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:56:25 +0000

    568energy equal worse organization family

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
