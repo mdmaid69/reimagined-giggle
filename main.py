52fb244c3852192ec57c90b2958aeeb8035a7bb0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:18:38 +0000

    slightly by case bother century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
