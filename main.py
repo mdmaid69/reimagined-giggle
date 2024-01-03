2196cb5f17e5f3369528863d90c66ff184ce7291
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:44 +0000

    rent patience opposite belong chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
