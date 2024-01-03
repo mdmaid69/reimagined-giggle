80529dfbba253054df90a457676b4537c67828ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:57:47 +0000

    after hurt distance leg any

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
