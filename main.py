d62af404227cddc17860279d212632fbadadeafe
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:30:47 +0000

    schedule for swim estimate may

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def convert_to_binary(n):
-        return bin(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import random
+def roll_die():
+        return random.randint(1, 6)
