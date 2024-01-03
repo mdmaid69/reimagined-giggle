d09b60abe6ff92a46c7a0953cacfb34c5e71b179
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:04:05 +0000

    fit load responsibility again organize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_area_rectangle(l, w):
+        return l * w
