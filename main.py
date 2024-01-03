49d29f7064e2b499dc47c193905cd4469b8f85f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:55:15 +0000

    space kick certain comedown car

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
