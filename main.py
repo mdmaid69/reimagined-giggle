bc7ffb605fc8d7691620d0b952de54361aa808b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:21 +0000

    such crash friend priority accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
