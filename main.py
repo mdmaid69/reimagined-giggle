9fe1b133bae34794aac381040785c587a0e2524a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:00:04 +0000

    count switch calmly knock mainly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
