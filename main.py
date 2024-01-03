cb1908608ea31cd31a26b6981a83e1ff895d2397
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:40:21 +0000

    surface confusing complicated after block

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-text = "Hello, world!"
-print("Characters:", len(text))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
