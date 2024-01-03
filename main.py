ff9d4963f501210de5a1f0663ee2bde968e23b24
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:28:31 +0000

    onto else side miss mention

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
