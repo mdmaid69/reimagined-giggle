15316b8151790bfbd0159724b4e6fb1aa534fb17
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:14:53 +0000

    area artificial should constantly new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
