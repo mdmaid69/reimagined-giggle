203ed2bca6f71dbe2af80ac7c4ac877a5ee9a7e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:25:19 +0000

    bar tight base comment talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
