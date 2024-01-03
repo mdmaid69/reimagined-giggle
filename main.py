dd596c5b9a3f8c6868be9ce945babefa899e9406
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:21:56 +0000

    block understand tidy top pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
