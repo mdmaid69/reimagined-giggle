a81fca0fc97524d56bd295ee9102a8905d896e08
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:12:40 +0000

    name check hello generally arrival

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
