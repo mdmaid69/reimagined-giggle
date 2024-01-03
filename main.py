7faab44ce91670431c84304805aa6f82175f36ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:58:41 +0000

    so regional attitude rub dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def square_number(x):
-        return x**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
