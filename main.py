4f2bb99180f4255cf2f0566379a27a423443339e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:23:08 +0000

    passage threaten chance her 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
