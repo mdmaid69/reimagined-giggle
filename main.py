e300c6c2e7d9c30f205ecbcdba921f4751ef7717
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:15:03 +0000

    noone formal list remain mass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
