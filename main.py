2993d4faf6e04bd793e7a4391080aaef79a606d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:00:22 +0000

    level perfect order over survive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
-def get_current_working_directory():
-        return os.getcwd()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
