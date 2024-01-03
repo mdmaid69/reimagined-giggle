868144df861208d4ca302bac2aecd7b700473bbf
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:19:49 +0000

    cool transparent replace be precise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import time
-def get_current_time():
-        return time.time()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
