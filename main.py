e3456ecf278750eafb8b444cb28bdc739af201b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:52:15 +0000

    never but practise purpose float

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
