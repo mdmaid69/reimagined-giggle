426e9e0c96025719b06032c43b7aa0ae47ae80cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:05:47 +0000

    square trust stop feeling tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_power(work, time):
+        return work / time
