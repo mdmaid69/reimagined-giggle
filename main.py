433ef05a5c29fd2f1685612ae81ad88a48ad6cbf
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:57:53 +0000

    suit themselves nice branch even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def count_words(sentence):
+        return len(sentence.split())
