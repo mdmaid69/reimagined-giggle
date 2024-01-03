00d8f9b3654b4726f003ba2a5c93804ebe5ab8b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:24:56 +0000

    tax suggest genuine dead able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
