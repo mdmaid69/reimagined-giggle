d42d802d646d08919eda31a725ca0b63bfe46db5
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:58:38 +0000

    project Jj passage home within

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
