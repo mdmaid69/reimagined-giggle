6d27c48a218ffbcbaa2a52e9705270be580ba176
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:44:45 +0000

    do simple skill tune organize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def add_numbers(x, y):
+        return x + y
+def sort_list(lst):
+        return sorted(lst)
