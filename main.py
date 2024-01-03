6adf78859dab585a5dca7888fa6e231bb87df95d
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:53:44 +0000

    far argument bit report practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def count_characters(sentence):
-        return len(sentence)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
