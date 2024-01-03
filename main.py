92b21308d32cd5ae8ad702306ac9a7c7e73994ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:09:54 +0000

    with suit themselves copy before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_float(array):
+        return float(array[0])
