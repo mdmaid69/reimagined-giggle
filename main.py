412dc46e105936f1b46a845eaf07fc4919f34735
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:14:46 +0000

    cruel sense mother 565end comefrom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def square_number(x):
+        return x**2
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
