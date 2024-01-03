b706e392c647e012f192d9c7e0cfcbcdcd7995ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:56:42 +0000

    accident stranger Ff start turndown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
