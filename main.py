c202eb6c18ad713b2de19350db2c7f1ad0a2ec61
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:11:34 +0000

    name delicate ignore finally rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_odd(n):
-        return n % 2 != 0
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_item(array, i):
+        return array[i]
