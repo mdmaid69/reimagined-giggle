e5f43819356501cc03e80fc3090a2dc9d7bd4b38
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:23:24 +0000

    live while arrive early wall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
