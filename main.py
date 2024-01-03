454ff91c50f86017d64fd40d77bac6665d790ec6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:15:45 +0000

    want permission pause temperature impression

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def convert_array_to_bytes(array):
+        return array.tobytes()
