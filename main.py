9d28f087ce9e5b7a2c536c304120d8bef7f64b28
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:13:25 +0000

    imagine geton make wire hollow

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import collections
+def create_priority_queue():
+        return collections.deque()
