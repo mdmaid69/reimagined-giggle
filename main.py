ce3c6d03e0d1ffb4ddbfc35dc282e68270f70c1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:10:53 +0000

    downstairs them embarrassed dark evidence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+print(sys.version)
