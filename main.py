7b09f62152e1a5f6f9f4f2f13dc710401bbc1ede
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:46:06 +0000

    case trousers course bunch interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
