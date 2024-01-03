0ab3ad5977bc868a408ce52c7345096a12242edc
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:27:45 +0000

    character give future please fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_as_list(array):
+        return list(array)
