95b72b8ba37bfafb6c3c5cbabf5093d9a9cadae1
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:49:16 +0000

    rise relation insist birth attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
