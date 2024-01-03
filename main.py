400b664e006b207761b415d0951092bcf23db54c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:33:54 +0000

    raw beside special wonder Uu

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
