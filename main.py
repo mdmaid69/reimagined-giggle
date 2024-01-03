a7d63f3219db96e420f8a81df8aeb3f6d980edb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:39:49 +0000

    pronounce drop run into together

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
