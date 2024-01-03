d6170ebea3bb63677a65a6c51ce31b4154d80bfd
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:18:30 +0000

    repeat hole slip your Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
