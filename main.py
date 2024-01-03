6210c6472a706a4fb199707e403972d908297f5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:54:00 +0000

    passenger left stone sticky national

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_speed(distance, time):
+        return distance / time
