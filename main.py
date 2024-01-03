95298b8a28a26ea0ecc3ead6bf22696ef179d690
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:36:43 +0000

    language dream settle temperature wheel

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
