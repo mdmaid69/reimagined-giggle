f19f53a9a91aca3e5026691f30e6b1e7010bf63f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:13:25 +0000

    getback fond shut pleasant put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def subtract_numbers(x, y):
+        return x - y
