5bb8fa77c613ed89ce3177d4e1d464ecd3fed71d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:39:03 +0000

    also call my lesson terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
