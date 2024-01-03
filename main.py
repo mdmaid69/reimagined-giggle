08844a640bf82c18551c1c06ad528420710ecfb4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:55:23 +0000

    zero climb chief notice language

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
