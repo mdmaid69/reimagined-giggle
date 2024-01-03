099237e703d831d0bec58e33bbf96a3701eb0b69
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:07:05 +0000

    criticize cost speak nice slowly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
