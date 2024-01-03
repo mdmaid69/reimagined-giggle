778727e318c4e334ebb7190b2187a22237ad7bcc
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:02:42 +0000

    kind size gentle she wear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_array_to_bytes(array):
+        return array.tobytes()
