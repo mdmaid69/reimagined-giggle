73b425f09522cfb303b1357b4f65f34008d2cca9
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:33:36 +0000

    regular near season further protection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import collections
+def create_counter():
+        return collections.Counter()
