4b29d2f5781fcf7bac7642ce9da3cc7cb9bd8853
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:24:29 +0000

    slight exercise advise while recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
