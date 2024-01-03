007e568afea84723579bf503e118773c6889c833
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:41:01 +0000

    such nose float limit special

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
