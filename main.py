3b642cfcbd35437791dfbe04470cfa6b98ddfc40
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:14:44 +0000

    obtain tense gradually mean Dd

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
