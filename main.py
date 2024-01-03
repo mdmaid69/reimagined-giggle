21ab932d890cdc389f20a3f057e883751aa812a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:01:49 +0000

    regard Itself thinkof walk fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
