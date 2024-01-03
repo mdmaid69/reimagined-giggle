e73c4f890dc2ac8a3abcd98f3c1a538b1dcfd5cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:03:34 +0000

    most in pretty far limit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-print(os.getcwd())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
