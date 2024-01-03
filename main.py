96ca5023397baf011b58f22eb8bfa31caa2e123a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:54:19 +0000

    closed waste offer keepout harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def is_palindrome(s):
+        return s == s[::-1]
