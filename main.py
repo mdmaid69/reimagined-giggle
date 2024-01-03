e8f2c57af833f81331f0e7dc55f862e3d408d53f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:03:48 +0000

    bright everyone listen once wave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_density(mass, volume):
+        return mass / volume
