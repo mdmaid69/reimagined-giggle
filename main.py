7cd0f6e368546a25f1080649b69e446d62b379fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:44:28 +0000

    thankyou impossible cheque explanation space

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
