ed7fbedb26a0ed6a703aa80af2f1f8c5eb56e051
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:55:39 +0000

    impression sad goodbye professional takeover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
