6c85e1b24b637245a87c8383e73a3be3e054c88d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:48:00 +0000

    comeround hide clothes rob shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
