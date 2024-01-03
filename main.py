2658a3a59b6177b9d1dafbb03a7380adfd82006d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:46:02 +0000

    getout punish stupid company or

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def subtract_numbers(x, y):
+        return x - y
