c62c3ac4f3423ef3081a80e4727a061ed15b10d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:35:43 +0000

    organization terrible steer fault total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
