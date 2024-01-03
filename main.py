a222bbf833f4aa5f665bff204b86349f0d9b193e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:38:08 +0000

    test branch difficult Bb near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
