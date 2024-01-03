c5842233d433d70e9cd4839c2e5b015c70b77944
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:18:59 +0000

    her service light similar vote

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
