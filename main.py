11d92243181af37e26ccc49773c00e5c72bff2c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:22:50 +0000

    dig invitation commit bright nature

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def calculate_area_rectangle(l, w):
-        return l * w
+print([x**2 for x in range(10)])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
