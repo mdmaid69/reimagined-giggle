682e855c08edae537470424930f0661085bf7b9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:16:52 +0000

    door direction crazy process spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+print(re.match("h.*o", "hello world"))
