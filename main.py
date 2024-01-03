b652538df2389434abfb5183ebde1015ba9d4a35
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:25 +0000

    callat standard ashamed than organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(5):
-        print(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
