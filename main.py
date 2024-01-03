a14073333daba358ee640a276af378604df62f6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:03 +0000

    convince recommend experiment call hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
