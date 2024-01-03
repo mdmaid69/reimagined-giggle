90cdfb2bbd09a2a289ab0bfd3c08fb6a92cb8a4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:47:39 +0000

    broad convenient aim definite decide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
