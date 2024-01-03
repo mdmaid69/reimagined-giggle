f71977db877ead3a54720687eb3609ddafcba33c
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:45:24 +0000

    among instant holdout estimate record

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
