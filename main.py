f1c802ebc5f4938dc7da22e21a6fe8231734610b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:07:28 +0000

    breathe past lookback(on) nothing today

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
