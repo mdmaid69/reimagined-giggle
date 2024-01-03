b090e105387c9868ef67522c8d08a8ab3f05334b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:49:09 +0000

    spin someone consist raw lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
