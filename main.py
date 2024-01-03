cce74d648758ffeccf694690d7609b96e72c8339
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:46:42 +0000

    shrink upwards whoever plain sea

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
