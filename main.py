81d8a15f6000b992aac553af1250c441fd2525b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:32 +0000

    close flow thirsty furniture needle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
