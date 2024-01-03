43ea42762ffd98931bca56c179f5ad8a219da4bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:35:41 +0000

    callback swim welcome necessary food

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_as_bool(array):
+        return bool(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
