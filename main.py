992a942fbd122950457c4e2e6e5c5201501f900c
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:24:14 +0000

    breathe release public family low

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import time
+print(time.time())
+def remove_duplicates(lst):
+        return list(set(lst))
