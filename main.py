c4ee6c7fa1ffa371b817acce49f4930bd999aa3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:41:12 +0000

    distance bang lucky worst count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
