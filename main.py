c777eb60a31ef1ce6acbc82aadc496ec9470549e
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:02:23 +0000

    life similar pay big anyone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
