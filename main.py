ca0ddda997d905656173b7e0fe75a3c91f201be9
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:29:54 +0000

    dry own dare suckme geton

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def find_max(numbers):
-        return max(numbers)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
