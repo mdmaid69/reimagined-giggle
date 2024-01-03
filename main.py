d47fe69aae1c266f24f2f395899e829279094f56
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:29:25 +0000

    them culture careless urgent turnout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def reverse_list(lst):
+        return lst[::-1]
