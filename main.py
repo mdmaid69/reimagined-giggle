a8532b4947eebb5cbb75f2bdcb2f48134a41d233
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:14:04 +0000

    cope future realize satisfactory ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
