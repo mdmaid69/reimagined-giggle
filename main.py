c0f0f4275a09469331de874362e042715ae6f359
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:45:18 +0000

    responsibility polite born search confident

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
