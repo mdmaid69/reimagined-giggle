ef0e1bda95bbc34fca50e12e466447f1f89b7817
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:25:27 +0000

    probably exist tense rush legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
