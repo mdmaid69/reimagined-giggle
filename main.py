3c9a49de25b0e1893013116a59a529b52e011d53
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:18:32 +0000

    embarrassing every physical water door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
