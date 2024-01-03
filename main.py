e3ef4df5641ca492b176a652c1432b94eb144ca2
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:21:02 +0000

    hole someone effort but impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import collections
+def create_user_list():
+        return collections.UserList()
