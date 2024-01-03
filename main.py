bf17e16b3d28e2f71c444ff723b2d287d3600225
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:13 +0000

    really shout clear keepoff perfect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
