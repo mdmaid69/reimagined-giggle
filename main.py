8e18bae8302eb886241d00888078e002813bf6f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:14:28 +0000

    centre variety seriously definite pencil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_permutations(n, k):
+        return math.perm(n, k)
