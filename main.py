9fff2d10e7c4c3387d398c2eecab93cfe4220f3f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:35 +0000

    even must count however someone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
