6201be7e2a56019a12cce03b7f32de48b35dbf6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:47:21 +0000

    perfect spread tomorrow close secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
