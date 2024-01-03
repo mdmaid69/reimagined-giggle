b36ef8f3a7b0952ac7e0f052b3ba797ad044ae92
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:58:28 +0000

    delight confuse bringback Nn brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
