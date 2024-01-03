74e0c1c5d013e7712a32ba4a3c5a0958f7b702ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:02:58 +0000

    certain attach pretty flat yet

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
