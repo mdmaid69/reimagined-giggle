e2f017c1c6d781844eaf198219629293aa0ca6bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:39 +0000

    run Ss contrast believe fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
