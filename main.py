067543dacb4b1d473070bba707d98870249d47e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:09:25 +0000

    recently guide accidentally quickly smart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
