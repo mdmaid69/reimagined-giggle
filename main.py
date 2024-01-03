e62e5182730f4d53bc43cf29ed4aa490333209b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:19:19 +0000

    chin little firm concerned attention

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
