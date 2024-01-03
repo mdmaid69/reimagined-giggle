25fe9dc107d4720d1f281ba8e573022cb0db0864
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:42:52 +0000

    just ear sky where refuse

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+print("Hello, world!")
