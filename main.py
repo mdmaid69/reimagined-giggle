337f0433686c0694e04f4f97122db8eef8db6d6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:29:58 +0000

    record remove reason juice use

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
