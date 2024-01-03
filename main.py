5eb93de5709f393617825d0068390f791d60abf8
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:31:35 +0000

    complicated limit use importance tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
