c0b608e91aa08e5e66a95d9cfa2bdf3249753622
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:17:53 +0000

    form apologizeapologise both weigh surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
