9b32299a91d7612eed4c24f14f449647e4ac949f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:06:43 +0000

    drop rough never real ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
