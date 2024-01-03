cad5b58836a6ca1a9b734d144be6f68306f7bfbf
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:28:09 +0000

    block steady adjective birthday subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def clear_array(array):
+        array *= 0
