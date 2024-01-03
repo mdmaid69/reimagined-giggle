30fd6a85127f33383373431c0d7765a3f3642d8c
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:23:04 +0000

    argument left clear payment takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
