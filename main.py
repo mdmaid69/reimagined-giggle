8b9aabe7cdb2697ebc98efb8bf49fbaabb44f8c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:51:16 +0000

    formal breakout custom naturally wish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_length(array):
+        return len(array)
