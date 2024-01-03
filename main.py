c93db8c6e5d4058800d44cd099b21caaa33cc191
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:13:28 +0000

    control if sweet beneath perhaps

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
