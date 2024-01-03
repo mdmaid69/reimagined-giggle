bfbab8ae22c88a9428cc230bcac33c392e52d5ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:59:33 +0000

    whichever towards shock large turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_speed(distance, time):
-        return distance / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def reverse_list(lst):
+        return lst[::-1]
