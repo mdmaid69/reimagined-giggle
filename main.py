ba6f79918871f407d74fa373dfd421ae3d9d4d12
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:33 +0000

    interest summer ignore begin stone

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
