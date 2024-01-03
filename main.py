3eb71983fbd5b584bd0522e9ecac9abafe36ba30
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:10:28 +0000

    Nn complain safety comeup discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
