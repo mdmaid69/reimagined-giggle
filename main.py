e89ac594cf577dc3d4acf4e20e7165344e87f308
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:58 +0000

    impression back laugh see young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
