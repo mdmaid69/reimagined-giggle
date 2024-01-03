3b47999546ec179f616c419e94f08085e7223f15
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:06:46 +0000

    broken possible flood ability down

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+print(sum(range(10)))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
