1166f1acd3a2ea569e342a0949f83212b7eef2c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:11:54 +0000

    OKorokay under complaint finger display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
+import sys
+print(sys.version)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
