adf0c339d627ffc234331d640a854fd9dcd2747c
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:59:35 +0000

    result gas reduce connection present

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
