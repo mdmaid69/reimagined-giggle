f13f1d7227e56a7ce700ea9b687a03c2c1e96eea
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:18:46 +0000

    complaint other geton lookafter undo

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
