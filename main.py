07d01705befd404c3bd6a2a1baf3cc811e3cbaf0
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:53:01 +0000

    we busy problem major winter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def sort_list(lst):
+        return sorted(lst)
