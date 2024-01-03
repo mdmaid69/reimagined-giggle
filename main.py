3db6cbc94345b5a153c2b36d2351e313aacfd139
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:27:33 +0000

    have settle fat gap at

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
