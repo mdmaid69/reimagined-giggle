7992f20382d6037a6be640b3b91f69fa932dfd54
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:25:00 +0000

    within top need backwards move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def add_numbers(a, b):
-        return a + b
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
