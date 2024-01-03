68c4a925817936c740bc39a21275fb4aa0ee351e
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:27:35 +0000

    straight eat scale single obvious

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
