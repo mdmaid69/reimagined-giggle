51f34def8e28943b3885c7f17ce1779898aa3bc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:31:46 +0000

    loud dance answer there cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_sine(x):
+        return math.sin(x)
