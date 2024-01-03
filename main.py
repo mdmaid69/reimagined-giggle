3fed236eaf03af852b8313c84c1dbcd1bdea2471
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:15:58 +0000

    expensive criticize Bb team actually

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
 for i in range(10): print(i)
+import array
+def get_array_index(array, item):
+        return array.index(item)
