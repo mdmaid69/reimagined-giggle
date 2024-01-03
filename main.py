0120df07c1473d5d1d04a844fc69c65f2d383926
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:19:44 +0000

    nearly recovery downstairs gentle number

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
