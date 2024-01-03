c19a5651515471428b3cdb701b85036d971e29de
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:51:14 +0000

    fail respect false gather second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
