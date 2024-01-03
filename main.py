ed8e71a80ce88c49738a01fbd77a400ff642bca2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:33:14 +0000

    practice pure getback ocean guide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
