09b762dcc02cd6027a0166a110bbe8f7eb329a1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:48 +0000

    on anywayanyhow skill hungry burst

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+for i in range(10): print(i)
+print([x**2 for x in range(10)])
