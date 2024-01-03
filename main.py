724e0dd4af84fea3d8f802ecd4b67e32fdb43f68
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:16:22 +0000

    cold stop hospital live food

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def multiply_numbers(x, y):
-        return x * y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
