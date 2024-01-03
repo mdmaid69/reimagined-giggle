2e3db26cdedeb6e4f34e869fc7ff8b4ecf26f815
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:15:10 +0000

    smooth wind bear whistle frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
