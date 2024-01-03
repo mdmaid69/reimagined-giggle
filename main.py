fcc80794167eafa74e3cf4773e0a1e425156dff7
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:08:29 +0000

    form goon car muscle catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
