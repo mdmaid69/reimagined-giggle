5f2f24e23c1bccdccf660615e86437b3d52efe40
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:10:32 +0000

    die complicated authority rubbish situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
