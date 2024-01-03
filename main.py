96cff2fce8cb362ee9daf474b77da0330cba5c3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:46:35 +0000

    statement ourselves land warning close

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_list(array):
+        return list(array)
