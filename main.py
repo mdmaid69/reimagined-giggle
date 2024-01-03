4cbd80a33718315ea7e8d6d9aef37a115e65829f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:38:33 +0000

    strange mess surface window ability

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
