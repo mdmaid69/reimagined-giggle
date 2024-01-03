fec19372429da11cb88ef596d140720edb88087e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:30:32 +0000

    activity anywhere collect identify scared

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
