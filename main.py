d24b09b0872a69ca59e51cdac8a182eb9494b280
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:17:42 +0000

    drop art praise bet claim

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_power(work, time):
+        return work / time
+def find_max(numbers):
+        return max(numbers)
