4abc18c0da818d0e54abd03135732a8e65955490
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:48:27 +0000

    report promise Pp holdup who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
