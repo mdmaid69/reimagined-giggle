70b3911f15c71250c6398c686ce1da788aa3a311
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:35:23 +0000

    heart thick call mend each

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
