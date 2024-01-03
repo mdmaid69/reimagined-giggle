e49a1b489198bb2924fd7b4de2877b0285e86c39
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:19 +0000

    culture interfere needle money example

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_force(mass, acceleration):
+        return mass * acceleration
