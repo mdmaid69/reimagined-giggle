a543d70b1cd034a578eaab4e503c5b439561ff12
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:07:44 +0000

    power middle rush bath remain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
