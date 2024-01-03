d3ff71c0235b3b23b9ecc402d7fb0d9c7cf19095
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:35:46 +0000

    before turn occasion getout bite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_string_from_array(array):
+        return array.tobytes()
