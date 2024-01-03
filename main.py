f8b4c7c255f22787e641847948925985b46e2a12
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:03:26 +0000

    against shape name cope rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
