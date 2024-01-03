af86792fe2d4f35ea5eb4ee2c5ce4060ee8c3771
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:49:54 +0000

    pause material by sense damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
