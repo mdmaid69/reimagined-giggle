0ab27f5afe3df6bb6506cf448906c97fe9627e89
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:56:15 +0000

    date never neither tear example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
