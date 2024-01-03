ea94d55b59e4e2f2377b95339e466b00fd8807fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:33:18 +0000

    physical fire range during table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
