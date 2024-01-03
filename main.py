87f426e439b0092d338d5d26e158089c70f28f74
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:47:21 +0000

    care file risk attempt throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
