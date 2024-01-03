6b6a5b3c854cdf82e386fd652f55768424379bc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:47:34 +0000

    broad thirsty shall fat near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_set(array):
+        return set(array)
+import random
+print(random.randint(0, 100))
