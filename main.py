01f356b1f0acd54d238c93edb535c83508f3c031
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:02:51 +0000

    sport boil closely air slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
