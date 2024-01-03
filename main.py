fbcb8b4ebb8e39ffb63ba58fa5a8bf9cbaf2af77
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:29:13 +0000

    curious profit noone translate place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_permutations(n, k):
+        return math.perm(n, k)
