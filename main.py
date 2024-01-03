9ced943fd309c18e4092df99cdae38a76a635b52
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:53:38 +0000

    win precise movement yours turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
