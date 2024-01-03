53e2abbb1d2277bc810c66d8177c3e48ebd6bacd
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:31:23 +0000

    sick note programme arrive personal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_work(force, distance):
+        return force * distance
+import os
+def remove_directory(path):
+        os.rmdir(path)
