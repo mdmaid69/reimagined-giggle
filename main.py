e74c8b0cf84f6f5c578bb776b3cfa7f5fed85db4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:08:03 +0000

    sensitive often apply show careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
