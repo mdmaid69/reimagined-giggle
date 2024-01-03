73ded42e41d2fbaac48296948d1d2ac307a659f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:31:17 +0000

    deep all meal Uu priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
