3b5820baa384479d1d814ad3391487f3547f2c38
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:27:15 +0000

    leg between race rich police

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def multiply_numbers(x, y):
-        return x * y
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
