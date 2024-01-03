54c51b2517463a9644b0ce4441fad64a60d03e08
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:51:59 +0000

    around sure cheque would fashion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def divide_numbers(x, y):
+        return x / y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
