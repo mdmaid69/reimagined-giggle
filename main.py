f3ecf64c3974e2711b264f9f129c43ada36bba32
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:41:20 +0000

    interruption dear relation land appeal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
