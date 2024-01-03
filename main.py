ffe77dad083bb16cbae9930bfab768ad8a87eabb
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:27:41 +0000

    long cut bang aware ride

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
 import array
 def get_array_as_tuple(array):
         return tuple(array)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
