13dd97ba13c456a1292e5a0bf4b0454e6d635bf0
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:09:13 +0000

    right display disapprove bet their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
