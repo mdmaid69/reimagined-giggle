cb97482680f61ce7c53c895f1cb14785a592ae39
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:49:05 +0000

    deliberate value unusual remain wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def cube_number(x):
+        return x**3
