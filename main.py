5de9697c1cb71c042a4760ec05b4331a6e8c71ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:29:19 +0000

    organize coffee ordinary general model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_bool(array):
+        return bool(array)
