efb91f98d11f081cda1001ab5adcafdc49b0e265
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:01:54 +0000

    flavour affect it past relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
