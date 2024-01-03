8747340b6d8687ea87fd0ed91c6b47517a2ca291
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:46:07 +0000

    stretch experiment permission realize course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
