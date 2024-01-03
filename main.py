3c58e2138d923693eaf75126e4620eb4b717a912
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:24:58 +0000

    acceptable eager disturb slide present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
