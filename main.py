0ab5a83ea50232233ab70f617d2373ee1133e18f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:42:00 +0000

    instead finally outside dance scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_complex(array):
+        return complex(array[0])
