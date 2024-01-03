b1e2f64169c26878714f1d043ca43f14a34028c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:04:07 +0000

    world tie proper around rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def sort_list(lst):
+        return sorted(lst)
