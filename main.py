d1267aa97a44f7734725dd7aa3c2fad5ce5a1c95
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:43:09 +0000

    famous pass miss could quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
