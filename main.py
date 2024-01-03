6f9848536060c6bf78647cd3583f66e171faac46
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:44:33 +0000

    567enemy whole extraordinary workout call

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
