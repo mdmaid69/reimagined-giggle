c27bf1063fe1fa9ef99159645c4f5abb25f2c7fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:44:35 +0000

    rate bringforward strength trip quit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
