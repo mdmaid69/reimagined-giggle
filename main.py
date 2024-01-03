ab7134d535637f042ccb96c7b2917d6c67873436
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:55:35 +0000

    angry than to pure car

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
