ff3cdec2fb1ee60e4c7ecc9b43edd7a1508c5f3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:21:22 +0000

    ought front nurse easy flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
