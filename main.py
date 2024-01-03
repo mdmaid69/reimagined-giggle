f49ed7242f736119de8f1792722050686d9a3f4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:43:19 +0000

    physical side rent exam fear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
