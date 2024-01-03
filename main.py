3a04b96b649e6a26756adf2c43fa074aea51c686
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:20:29 +0000

    race turndown Ww surface disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def count_characters(sentence):
+        return len(sentence)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
