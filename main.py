f3b0db4531f8ce4893e34b872b8cc3487b0c985c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:27:32 +0000

    bringback shopping quiet awake search

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
