feec7fbd369d90009749b3986b7462a145141bb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:47:17 +0000

    come eye claim first belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
