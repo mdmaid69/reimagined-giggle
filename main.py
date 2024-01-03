fbe618a1a7624e9e1edb7dee104902bf0c94d2e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:09:25 +0000

    strong trip marry soon 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_ceiling(x):
+        return math.ceil(x)
