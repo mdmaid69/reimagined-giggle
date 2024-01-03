2b8a7b193e72108a0b7bb80b01fb62fb715ca8a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:28:13 +0000

    treat imagine speed family perform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
