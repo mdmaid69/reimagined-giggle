9c3d5e4de7808f511b7d739b30d6ce698dd87e47
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:33:46 +0000

    weekend government buy uponon gift

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_min(lst):
-        return min(lst)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
