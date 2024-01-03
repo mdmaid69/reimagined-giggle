a484285e3f1bdbd36256d58e14bc5e347ffa71f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:13:59 +0000

    afraid wave bit swallow everywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
