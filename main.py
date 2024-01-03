c5b687ee8d82f26b1f6f985269b9f49ae4f80d87
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:25:11 +0000

    nice situation goup lucky wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
