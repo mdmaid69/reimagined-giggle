aa125094f33d63f19a0238fc8054a03d6205b94a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:57:39 +0000

    obvious practical close shoulder return

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_list(array):
+        return list(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
