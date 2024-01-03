404a1479d7063624e6b170384ef9bfd068b7d08e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:26:15 +0000

    feeling vanish cause because busy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import random
+print(random.randint(0, 100))
