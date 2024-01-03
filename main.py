fe0b8df64ee72e9db2f69a6774a2d450c8172d1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:59:14 +0000

    question goup cinema head connected

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
