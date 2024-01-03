94799bde79d4c979e6810f09adf390c7f10468a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:14:04 +0000

    transport mention surprised and furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
