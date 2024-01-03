da6dc94ce7a7b0d13f03dfc51b7d1aab99524de2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:14:49 +0000

    narrow goodbye cough mail properly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_speed(distance, time):
+        return distance / time
