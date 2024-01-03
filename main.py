5b93f6c53344be933f81e6069b770df6bf59fad8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:27:08 +0000

    me order slightly destroy defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
