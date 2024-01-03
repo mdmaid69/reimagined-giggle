b4ddcbfabe3057127c12b4dba399ee68f8663244
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:13:40 +0000

    score table 179bite light search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
