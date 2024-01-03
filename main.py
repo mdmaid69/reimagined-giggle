f9b75527262d902c9c239bc782fe9c07be5e884d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:20:51 +0000

    dish Cc earth keep mass

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import time
+def get_current_time():
+        return time.time()
