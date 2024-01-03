4534d5ac04fa1729f4fdf78e30c911afb453a725
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:04 +0000

    hold patient weekend birthday last

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
