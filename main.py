b0dca856e5c784b12ad3515e3c83b163296b91e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:32:39 +0000

    black perhaps computer mess search

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def reverse_list(lst):
+        return lst[::-1]
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
