5ab15d4af01be6535b95b87367fd36e57d62c56f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:45:14 +0000

    when fault here drink tip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_max(lst):
+        return max(lst)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
