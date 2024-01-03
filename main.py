221add20022f6ecc47c91358bebaea00e58e558d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:59:33 +0000

    leg buy war blow block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
