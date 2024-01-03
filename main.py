0e111f246743e9b0c9b504bc52dafa1065ef6957
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:27:51 +0000

    impression back lesson computer approve

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import os
+print(os.getcwd())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
