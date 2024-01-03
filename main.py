e78b2a0ba5982fafca6958cd1d153af4d706d25a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:37:58 +0000

    behind cap want load so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
