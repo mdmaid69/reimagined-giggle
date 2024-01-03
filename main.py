35df2151c60bb4b66525d85694a60f619af9156b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:22:07 +0000

    control wrap curious nose tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import platform
-def get_os_info():
-        return platform.uname()
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
