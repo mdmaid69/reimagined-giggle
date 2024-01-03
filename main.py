603af5103b519c9c3415545947c021d5ac2e27f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:36:04 +0000

    steady skin breakout keepup square

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
