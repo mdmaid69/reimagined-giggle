b0afaea08de01e90b1d9d3e9a2ad0ee6a5454cca
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:57:32 +0000

    rubbish partner point frighten tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def find_min(lst):
+        return min(lst)
