fbae393d532fb14bb8e1971e4504d7a329866614
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:00 +0000

    love satisfied talkinto Cc uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
