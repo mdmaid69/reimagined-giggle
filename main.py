1c626495351cee19de964b7ea1447862a17d7484
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:43:19 +0000

    sorry dangerous boil trick beyond

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
