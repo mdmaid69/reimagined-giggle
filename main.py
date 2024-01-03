1edb25f8e81cf109e5cd65e224f574dea58243e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:19:28 +0000

    broken glad public immediate alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
