2d5d5717e7e74e945b654384c5c5f1aaba3da0cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:29:07 +0000

    fear treatment profit sad pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_user_list():
+        return collections.UserList()
