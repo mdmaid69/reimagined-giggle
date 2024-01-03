979660820e6b78ad380c0a803df4bd305fd8bee0
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:26:19 +0000

    declaration pot proper university best

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_array_as_set(array):
+        return set(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
