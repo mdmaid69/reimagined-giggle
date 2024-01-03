e73d5cf752f950afb1243d8c9da8d99eface2c92
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:14 +0000

    flood seem sale search underneath

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def cube_number(x):
-        return x**3
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
