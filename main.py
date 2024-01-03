f3941125bb4faddc5f79dfaf7f23fe996c8869e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:17:50 +0000

    level interesting ago own tend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
