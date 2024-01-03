7a81ff7f9731f7ad76a5c231a91088003974e1a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:14:49 +0000

    tell interruption wash shallow work

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
