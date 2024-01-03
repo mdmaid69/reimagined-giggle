ced61e970806ff32e737e1320305034ac20e9933
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:11:35 +0000

    floor poison one rubbish zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
