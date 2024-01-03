30039df39191c94c974eb587029caa0fcc12a9ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:09:15 +0000

    cheap long frame near home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
