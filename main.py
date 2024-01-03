2e9948ca62083caeea452608e3575dc4e0174251
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:52 +0000

    confidence pencil defend balance 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
