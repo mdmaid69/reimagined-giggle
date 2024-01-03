723d194ca1925af37b0c1d3a872cc15aeaa44bc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:07:52 +0000

    talkoutof service sweet comment wave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import tensorflow as tf
+print(tf.__version__)
+import os
+print(os.getcwd())
