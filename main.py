d94a6dc1ea48f9d7568c655bb5f7489824c0b5fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:00:43 +0000

    trousers breakfast new hurt risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
