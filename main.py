41bcfeeed140ad555be6d9abd45385e6279fc635
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:11:49 +0000

    not surface tie pot Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
