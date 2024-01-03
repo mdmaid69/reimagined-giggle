467d16770a7bdd863cb3cd4b972b737026294a37
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:10:22 +0000

    consult include lookafter whistle include

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
