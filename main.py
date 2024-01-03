3eff98cf0b14c3be2dd3d5aabf7935a910eccfc4
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:04:43 +0000

    bring line sudden back injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-for i in range(5):
-        print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_hex(n):
+        return hex(n)
