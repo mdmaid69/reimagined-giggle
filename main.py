79fec61df057cdac8e7bffc646f77f0722c1c16c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:13:11 +0000

    service rain sound winter level

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
