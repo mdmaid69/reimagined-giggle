e9711d5bf9081d4162574f4764246cab31aba569
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:36:42 +0000

    suppose admit exchange information safe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_max(numbers):
+        return max(numbers)
