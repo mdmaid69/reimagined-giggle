e480fc15e4d12d1244441cad74f541c833e71e5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:35:39 +0000

    tap measure raise material develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print([x**2 for x in range(10)])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
