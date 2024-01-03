7c73ef7457eed2320acf7d6d6f24625b043c3a65
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:31:27 +0000

    low skirt tune wrap model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_work(force, distance):
-        return force * distance
+def get_bytes_from_array(array):
+        return array.tobytes()
