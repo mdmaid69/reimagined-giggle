fbea830d7c6c6d463bf795d7095261494f0bed26
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:31:45 +0000

    confusing summer threaten advantage positive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
