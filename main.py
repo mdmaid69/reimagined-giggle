806eeefc73180a57b85d0bc3bdf188321414e377
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:57:14 +0000

    temperature lucky period goup design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
