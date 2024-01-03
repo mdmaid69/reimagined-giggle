85b8970b193bd8396cd15425b8eee2037ffe5072
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:27:10 +0000

    water worry news characteristic famous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
