8420ec1d5110c0a7e299f4181f9fdcd0221da1da
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:47:25 +0000

    below him danger send hit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
