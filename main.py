a66ca44727e0ecf6fd044704c779b4103a9543c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:00:52 +0000

    visit across tend regional book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
