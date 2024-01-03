20c3b96445f46e0232aec95b53e695e19d7389a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:13:32 +0000

    square culture show niggaacademy juice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
