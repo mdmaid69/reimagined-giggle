e0e4204155c0f10b9db4548b361bc6e3ffc9bb03
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:41:33 +0000

    certainly factory raise ourselves grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
