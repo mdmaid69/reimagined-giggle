500c35b13c5805e88a8052b94f7be8c2e6545cd7
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:12:39 +0000

    rain lunch settle owe difference

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
