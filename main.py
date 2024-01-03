b1ceb3742449003d44ea159c528ad4b3cdd39fef
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:27:57 +0000

    door maximum please decision pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
