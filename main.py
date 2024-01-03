8abce7fae41ebd7f9a0cfdcccfcde3fd023ccd8f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:52:37 +0000

    bend spoil surface difficulty shame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
