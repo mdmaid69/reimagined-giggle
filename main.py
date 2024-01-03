3e5b2c6a4b75d353e609ff9373f5dab733f61bbc
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:59:47 +0000

    behaviour reach air disturb comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
