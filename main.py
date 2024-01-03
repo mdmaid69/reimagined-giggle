ed285f1284ae4aaf2c0535fb05aa6ec59afb2451
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:27:12 +0000

    through damage hope curious crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
