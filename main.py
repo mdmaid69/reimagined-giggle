dabce070233321bfa459b9f7bc4766467b097d5f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:14:04 +0000

    terrible fly big burst convince

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
