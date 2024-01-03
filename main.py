47ff55d0fab36465b745a9fbf4033ad61032b356
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:36:56 +0000

    hope yes collect Qq gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
