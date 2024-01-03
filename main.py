593a26014603a80f675dd09754b230f4737a28dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:43:13 +0000

    harm pressure announce own some

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_counter():
+        return collections.Counter()
