79779740480263317ef93c39ddee5479a92aa9e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:50:03 +0000

    gift game always opponent roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_as_int(array):
+        return int(array[0])
