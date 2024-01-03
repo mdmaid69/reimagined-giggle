6f8781b863ec1c2346c9e895e06909e1eeb699e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:45:58 +0000

    black fine anywhere good stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
