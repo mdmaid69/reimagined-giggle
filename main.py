4db3e26400c3759b936df7a8ae23a4a63117c52a
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:18:03 +0000

    turninto survive along collect effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
