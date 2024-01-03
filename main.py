aa0abb41d6f228aed5350413c5b426cc6153cea4
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:30:19 +0000

    land prize greeting light lift

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
