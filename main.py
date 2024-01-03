98c0c8ead8a5eab3624bfa1910034b9524a2aff8
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:35:13 +0000

    kick trick apart field inform

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def square_number(x):
+        return x**2
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
