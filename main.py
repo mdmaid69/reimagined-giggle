d629f956242f7ee87f44600c7e2381129a9ef6ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:52:11 +0000

    your turnaway comeover cheque Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
