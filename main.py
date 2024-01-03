56e4b20d9ee5b9833e55f0c9beb09625d881460d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:56:16 +0000

    any sister rubbish connection century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
