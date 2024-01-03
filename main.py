ea3e299a779528a01fcf776e04eb69370813d050
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:27:10 +0000

    reasonable song sleep arrange equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import platform
+def get_python_version():
+        return platform.python_version()
