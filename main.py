e16879e8cf2761824218d748288be2cccc48a2ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:02:57 +0000

    tip dull how well big

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
