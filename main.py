0e298415f0003b9c7d6efe58cd5523fe124c6312
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:51:08 +0000

    so experience justice hurry polite

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+print(sys.version)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
