09faeb40e4c73784c78d3a4a2c9bad719498c5d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:00:15 +0000

    more water case polite weigh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
