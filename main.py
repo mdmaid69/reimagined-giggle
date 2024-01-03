3a29723230c8cae7b1844e03987a35bb27630d30
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:17:21 +0000

    release perhaps father uncomfortable uncertain

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
