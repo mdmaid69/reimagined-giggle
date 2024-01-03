9cfa56928602afdd3447f48bfb1c3fbfb45812d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:22:58 +0000

    paper disagree release man angry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
