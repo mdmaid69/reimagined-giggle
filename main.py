7b1a23ce5f6d233844acc800f248d7207c5309d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:51:01 +0000

    quiet load see Uu keen

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
