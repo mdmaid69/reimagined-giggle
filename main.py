db900b3ceb4990a52c95049a32f6d5664ca7e49f
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:58:31 +0000

    office waste obvious polite embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
