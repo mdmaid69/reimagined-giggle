12312f026c492c2c7815fcbfdba22159edbde86c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:24:20 +0000

    still tread see twist defend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
