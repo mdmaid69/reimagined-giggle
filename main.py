f2a27f8fb5c48dd28383b6bbc62a83ef0ca49380
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:41:22 +0000

    save depend tough guess cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
