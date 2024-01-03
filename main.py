69d1ebc28eb450e0b93d87d5909ba7fbb48c93ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:23:43 +0000

    lift past surprised 565end shake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
