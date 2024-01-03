afe877f849ab009a0efdb2f02a78d6fa22dd6b97
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:10:36 +0000

    rule worst contact prepare guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
