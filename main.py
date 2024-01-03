be92c42d253727aca9cc55f6f9a89f29814028cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:59:49 +0000

    scrap himself size respect over

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
