3b04b0b73bef6e29208da3cc87c7d72ebf145613
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:22:32 +0000

    of his set near pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import platform
-def get_os_info():
-        return platform.uname()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
