0ffaf4d8e28a609a1c7ee2277198b70f1cde85bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:34 +0000

    Yy undo move clearly responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_string():
-        return collections.UserString()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
