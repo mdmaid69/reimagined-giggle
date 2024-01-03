3263d13f2b224cae8620c1b2cd96a757f8fbdc58
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:04:47 +0000

    disturb object male turninto trap

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
