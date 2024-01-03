33c701aa062fda8e491f05da505b8d816dd4fefe
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:52:15 +0000

    past kind hide right difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
