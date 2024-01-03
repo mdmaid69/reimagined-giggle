d4f4a08c487086b6d3ec8fea76bc73ad7fc9fa62
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:21:01 +0000

    understand set power tense bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
