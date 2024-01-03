7168b1b9a1569d4249d31d01c483c7c8482cb52a
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:03:08 +0000

    present hello luck 572enter cruel

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
