a097ddf0afa62aed313b33db13901af9a2870c91
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:23:47 +0000

    address ring information sorry willing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
