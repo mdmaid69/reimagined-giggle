dd15f1b564af6865347afbf943ab51036ec45031
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:45:25 +0000

    disappear makeinto family present realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
-def find_unique_words(sentence):
-        return set(sentence.split())
