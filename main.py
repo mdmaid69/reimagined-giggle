2b4e515804c5149e87b8c45be4722a899b2b65cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:41:01 +0000

    ticket bury present within live

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
