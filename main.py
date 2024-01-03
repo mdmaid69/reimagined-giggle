6e955b86bfa06f0cd763277533758326fb61a11f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:40:32 +0000

    takeaway record clear left 566end

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
