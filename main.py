062b4f22aceee1a0d4f9e42e302475cb8bc32c2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:38:31 +0000

    because either stage before start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
