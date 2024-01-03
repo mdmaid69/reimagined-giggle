6fc1c77e73d77d120ba58072e9aaab972f7a3d0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:38:35 +0000

    invitation squeeze greeting object angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
