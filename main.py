46ad33a4c97700cea0fcea7ef0de60714857c6b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:27:35 +0000

    this lookafter band birthday however

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
