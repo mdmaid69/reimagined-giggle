68cf69286698b96c30ec8be5b0a7665e305a6db6
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:05:04 +0000

    valuable upset throw journey convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def clear_array(array):
-        array *= 0
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
