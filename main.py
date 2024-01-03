7987a100555e6d8798394cc839fd35f932eff58a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:25 +0000

    warm adjective old wear impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
