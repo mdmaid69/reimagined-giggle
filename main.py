a6152cd81bf408dede555db0b76c353f90aad0e2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:29:06 +0000

    proof worth that stare blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
