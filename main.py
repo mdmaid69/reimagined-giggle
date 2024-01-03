293bd81fb41a72605a75cba86cc6089f7c42f61d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:17:35 +0000

    567enemy tie avoid new middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def multiply_numbers(x, y):
+        return x * y
