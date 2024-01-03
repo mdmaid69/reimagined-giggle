21f883ca89896be44f285e772ff25e87902fdc15
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:14:34 +0000

    stretch slice miss shop practise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
