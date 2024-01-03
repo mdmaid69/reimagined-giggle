f3c20c980d6ab2f7897348473bb74f42684d472f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:16:00 +0000

    fail under react turninto actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import logging
+def log_message(message):
+        logging.info(message)
