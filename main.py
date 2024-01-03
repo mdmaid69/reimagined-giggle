151eb8fbfbbd831a9ab3a490eb1127e59e59479b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:37:13 +0000

    tax its by hurt visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
