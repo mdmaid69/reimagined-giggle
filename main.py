ffbe557422e2f7b78d4c10065cab2189c3f96b03
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:05:59 +0000

    push treat scared giveaway cheap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+text = "Hello, world!"
+print("Words:", len(text.split()))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
