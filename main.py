4595eb40377878c4290c97ddb90eb54830f62598
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:50:45 +0000

    settle interview seat polite base

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def find_min(lst):
-        return min(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area(radius):
+        return 3.14 * radius * radius
