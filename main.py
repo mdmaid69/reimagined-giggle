462c9e815b8cc98532f89b596c489dd4817dcd95
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:02:04 +0000

    burst feel weekend offence side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
