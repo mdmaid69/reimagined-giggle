463c3dfe027d64d875a7bf9c7eedff62d9ca52ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:57:48 +0000

    giveout rarely kind workupto on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
