98b99a56f94b232d4bc85c788efa44e0bbc427cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:00:17 +0000

    agree unkind original sound lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
