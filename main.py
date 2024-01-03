843c6ab7a61f1f93d445bd23059c1bf6183ce040
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:58:48 +0000

    present spare noise my transport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
