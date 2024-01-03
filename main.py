1b0d626b820c9d26d736c60e2ecb78a96f522ecf
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:32:59 +0000

    raw gradually strict load beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(5):
-        print(i)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
