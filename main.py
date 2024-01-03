8a278cd284ecf7deed4bccab6c18ec198c328d72
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:48:18 +0000

    comeout giveout repeat free load

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def add_numbers(a, b):
+        return a + b
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
