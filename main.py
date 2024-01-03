0bd3a2cb343a727d71dedaa03be9dc43fc19e095
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:16:03 +0000

    heat cause suit Bb power

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
