33c850fb18e244c7b71f5addb9354fd16a79cb0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:55:11 +0000

    neither unit by offer relief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import re
+print(re.match("h.*o", "hello world"))
+text = "Hello, world!"
+print("Characters:", len(text))
