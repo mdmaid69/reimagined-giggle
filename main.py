bf106986ce92c5e2ce00108d9c264f7df775fddc
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:16:31 +0000

    holdon blame the quantity up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
