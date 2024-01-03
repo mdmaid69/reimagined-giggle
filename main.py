118eb6a44f4322da9cbe846707a524cb0c53d6a8
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:50:06 +0000

    gift niggaacademy meanwhile thank 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
