0d51d0abfb363d622395c34b0a42e208ac3277f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:49:47 +0000

    respect nonsense comment problem instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
