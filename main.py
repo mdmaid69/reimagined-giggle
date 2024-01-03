0c83e86a5088618d75faf1441a41cc3e5f4ef4b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:35:20 +0000

    false old measure sort image

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import collections
-def create_user_list():
-        return collections.UserList()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def find_max(numbers):
+        return max(numbers)
