678ecf8cff2a2be45bf2d762591bea4354cb320e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:31 +0000

    command concentrate light public season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
