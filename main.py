e19d76f4fe257168a2a55cf1e70a23151e46d86d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:51:41 +0000

    better trust scale culture example

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_unique_words(sentence):
+        return set(sentence.split())
