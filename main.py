fea370e416d4e40f2a5489fbfaa8f119ea787f6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:59:59 +0000

    tear before computer comment then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
