fc85a2c2804ab7f19ffce5572dc82ebaadc052e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:35:22 +0000

    interview fire modern usual failure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
