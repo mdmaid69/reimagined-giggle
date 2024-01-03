b7c29004f1082f114631aa58caaf02d6ec1e67e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:57:39 +0000

    may bath limit support swing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
