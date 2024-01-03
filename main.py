9a92298acf44678b39b2b42515823326222d3272
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:37:15 +0000

    Jj emphasize mine right right

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_area(radius):
+        return 3.14 * radius * radius
