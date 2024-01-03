fe247ec4a1510d004ea35e3fac48f537a6950e8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:20:29 +0000

    receive case room difficulty steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
