1162624fb1cdd5f1095d08600a1b1023829eb963
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:15:51 +0000

    weigh stretch afterwards branch runinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
