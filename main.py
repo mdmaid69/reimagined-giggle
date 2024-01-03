acb6285a57fa287846ae1dfb8b6da92a7401a7e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:40:03 +0000

    giveback ask movement either child

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
