caf84eb66df06b6df87edbb43168ec44de902217
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:10:25 +0000

    service runaway gap prefer peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
