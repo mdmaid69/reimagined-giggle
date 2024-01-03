2b75e9ca21ebb61be0bcf6055a3e6b4b75fed5bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:59:30 +0000

    mix proud ready whistle rain

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
