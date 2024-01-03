09928bd66f50c4f24d26f84bed3e94d465b02a86
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:16:05 +0000

    go win slowly thanks say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
