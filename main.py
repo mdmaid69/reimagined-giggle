0b119c673865f310e7131e54718c03fdbba0db0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:32:54 +0000

    alone lock disapprove tough ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
