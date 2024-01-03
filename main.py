de7e25c0956ed4ab1d40e397df118e0639e5673d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:26:08 +0000

    continue comedown everyone spring together

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def remove_duplicates(lst):
+        return list(set(lst))
