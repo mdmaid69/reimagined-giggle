f8692c397a91c6baa8d3a46dc4bbecf777bc6a0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:51:02 +0000

    less punishment practice opinion tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def find_unique_words(sentence):
+        return set(sentence.split())
