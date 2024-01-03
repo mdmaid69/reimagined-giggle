5c04ba4e0975cd6d461e4c7e275b79f352f9dc68
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:12:03 +0000

    turn start more bottom appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
