b0bc4716a4a02611d8ac559f1e68adefa4da605a
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:04:20 +0000

    wide create curious high live

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
