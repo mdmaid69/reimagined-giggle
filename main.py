af40720b72df6903f36e7922cb2474e7ea873121
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:38:11 +0000

    572enter activity hollow surprising near

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
