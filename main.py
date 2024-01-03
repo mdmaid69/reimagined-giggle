bcf4b67f9b990f27fcb2e6ddaaf2c0f313041c9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:46:45 +0000

    comment high disappointed Ss square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
