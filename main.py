70420f3e621b820c26113e47bba254d860cde5ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:32:25 +0000

    pour strike custom precise cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_str(array):
+        return str(array)
