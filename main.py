918d3312baa6bd6c1ca18a715fd45bb0b3ab3733
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:28 +0000

    sore sweet almost once geton

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
