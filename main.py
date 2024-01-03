fed28adbd8734667224eca6f3d8d34fac5d61657
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:31:05 +0000

    under disturb respect base dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
