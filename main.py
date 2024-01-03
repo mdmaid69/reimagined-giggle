f55af58a39a88defc42af6d21c52616ba1710482
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:47:16 +0000

    that cheat town pure female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
