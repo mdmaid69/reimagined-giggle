218ef536371d0c479ed75292cb39936205552c5e
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:27:15 +0000

    bread favourite nurse guide sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
