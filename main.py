a5fd17b910cb105a11b43de3edc6657eca6e62d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:35:17 +0000

    lie promise tip wear alone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
