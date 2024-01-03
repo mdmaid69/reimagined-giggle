72ef72875f028d56fceece57cee32080eed20889
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:04:02 +0000

    worst judge only either genuine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
