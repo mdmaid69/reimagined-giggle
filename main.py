a178390d4c2b33af545a0bdae445bca96ef2361e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:30:51 +0000

    scream board family plan wide

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
