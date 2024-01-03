17e1183002d439eb4fa13983088b72df4155dfa0
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:39:34 +0000

    party immediate cook neat before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
