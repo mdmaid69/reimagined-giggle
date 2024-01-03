e2b502e04cb992497e928ddcc27c5f5eecd909ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:50:53 +0000

    shame board accidentally anywayanyhow call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
