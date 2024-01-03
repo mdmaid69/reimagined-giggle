75222bbd157984e8cc1161c0da7906bd449636f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:42:21 +0000

    damage beautiful average Itself pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
