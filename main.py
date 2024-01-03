481c037bccf02276e9b77eb3688120e8b5e6ee89
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:39:16 +0000

    model contrast special blame borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
