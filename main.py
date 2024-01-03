ffa95a1f826ee9abebe772b424a74ac9aa3a1024
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:15:17 +0000

    tragedy fault original married speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
