9715831dd1bb93cb4b5592266f53418ab4f1982b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:50:19 +0000

    regularly will company apologizeapologise pause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import re
+print(re.match("h.*o", "hello world"))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
