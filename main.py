cb1e9910adfff65d5160d2dff653ef5e02ba937b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:10:45 +0000

    among send mess minimum walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
