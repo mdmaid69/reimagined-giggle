a53ff099a25b1ea13a5689c4c5ce7b8b4156cd23
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:45:13 +0000

    together house bit notice favourite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
