14426292bce134a8db203c75be5c4755d516b1bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:24 +0000

    team history scream male unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
   def get_base_name(path):
         return os.path.basename(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
