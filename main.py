39059dcf55eee22ea7802715f3e6dffb01b6cdbc
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:34:24 +0000

    although front cut remove increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import collections
-def create_counter():
-        return collections.Counter()
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
