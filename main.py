546a96c5cf6880d68017254911d0fcf966f4e320
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:52:16 +0000

    influence shop today apply agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
