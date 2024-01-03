d3fa346dc39a4ceb0c91792c957328e0c4479c06
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:21:01 +0000

    rush consult roll pollution authority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
