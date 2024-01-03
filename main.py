eb4cd49efe8119bc6c6dc14ab2c6cc3610ace9e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:58:43 +0000

    anxious accidentally fold callup experiment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
