fff377bc7ee2fd9b636f6c5816fb66bbf57595dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:17:21 +0000

    cook involve with note protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-print(sum(range(10)))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
