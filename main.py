c2588ce7b4c82960b539c3b40397cb0e7062ff2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:14:10 +0000

    little reject judge word tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
