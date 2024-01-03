f8e83c9da09c2fad6108bf3f9b66a9392866c708
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:53:59 +0000

    turnaway border limit peaceful mix

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
