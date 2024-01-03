57570aca402b6fb8f3d22d7af139219605f71ecc
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:41:22 +0000

    quick guess dry check pause

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
