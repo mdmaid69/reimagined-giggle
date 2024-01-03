5ca8cb7cbb4babbdcde74758326cbe5b3f3a9b10
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:41:02 +0000

    community stare steal undo television

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-print(math.pi)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
