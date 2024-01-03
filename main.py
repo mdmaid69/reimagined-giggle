0a39d27b1f667f249e6d357127a9a55ea67ef3b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:40:54 +0000

    split uncertain hurt little when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
