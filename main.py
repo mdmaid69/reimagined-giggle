ae91d9a786de7c9555a13b100e97ec4d35674990
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:35:02 +0000

    recommend patience file flat tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def clear_array(array):
+        array *= 0
+def greet(name):
+        print(f"Hello, {name}!")
