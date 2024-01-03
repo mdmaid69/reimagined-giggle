43dbc8fddf49619e441f8c2c72c1b59707e57ad8
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:43 +0000

    thinkthrough region impressive mark true

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
