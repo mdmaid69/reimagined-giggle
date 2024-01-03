f84fb81c7c61e7e6cfaa76c023502418118d6777
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:15:52 +0000

    deal about wave please steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
