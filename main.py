2c87dc3a5b3906b8094e2411ae38935803f8dd4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:16:12 +0000

    turnup performance lock bored sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
