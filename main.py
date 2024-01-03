832f74ea8ef03f94e13019a72b3b0631b31910dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:04:12 +0000

    terrible dish expect discussion listen

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
