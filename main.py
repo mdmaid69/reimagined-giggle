ad26e633a5a37660bc04f9dddcba7eb063eac434
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:08:51 +0000

    twist sing come official improve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_pressure(force, area):
+        return force / area
