bc165c98f5d0bfe4c42c95564679f50c92043583
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:18:03 +0000

    behind compete through impression emotion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
