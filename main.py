4162c98326ad7f3fdb9f7a90761faf2b08841a63
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:51:37 +0000

    round every closely regional nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_speed(distance, time):
+        return distance / time
