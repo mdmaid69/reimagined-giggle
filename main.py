5d125875cff5715478ca6dad5ae60731fec2a054
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:50:36 +0000

    idea image avoid dull skirt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
