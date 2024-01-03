f9a88ce2312822ea6a9e85cc1351dfb7f28fb94a
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:21:58 +0000

    medium home congratulate raw recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
