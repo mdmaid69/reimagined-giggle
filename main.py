215de9d0e25b3e497aac5702a71dc1d3844c8552
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:36:41 +0000

    crazy content tense coffee cruelty

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
