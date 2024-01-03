3fcc7485a2a34228e83b82f49ba58b8392838898
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:48:49 +0000

    country turnoff spring vision start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
