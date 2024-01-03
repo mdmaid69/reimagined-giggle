bf4c4748b075d46d84a65d14027fdebae3a1379f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:51:32 +0000

    national meet respect except bottom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def remove_duplicates(lst):
+        return list(set(lst))
