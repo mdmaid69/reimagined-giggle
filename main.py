b6b2c17c000c942f8d49383b8015715162f789c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:39:27 +0000

    post belief practical exchange push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
