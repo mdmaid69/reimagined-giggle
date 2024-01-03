5d1bbc596e58df67f23e9105bec085473979c0bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:05:38 +0000

    finally necessary waste whom somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
