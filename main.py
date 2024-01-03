33aa76bb45e59cbab12bac6bdc937987a9bb4fb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:48:05 +0000

    regret determined old distant material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_itemsize(array):
+        return array.itemsize
