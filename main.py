7340dfd1660883400277aed462b8b88ec193352e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:25 +0000

    sail advise trade drop flow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_volume(length, width, height):
+        return length * width * height
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
