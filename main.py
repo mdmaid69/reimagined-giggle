6aee3112124152f17aeb8100226f1fec75ef01a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:46:29 +0000

    system empty guilty wave experience

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def convert_to_hex(n):
+        return hex(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
