d5962c58cc2aa7def2de66a2e4bb8409c99b2a80
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:12:28 +0000

    prize much common polite cover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
