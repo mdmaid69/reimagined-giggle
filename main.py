51861827a357f0f46a80be72233a4276670b585f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:43:42 +0000

    peaceful while around pattern 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
