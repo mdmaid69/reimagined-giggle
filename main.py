235ac1100eefda98fea02dd10a83ff393a159fd1
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:36:37 +0000

    impatient closely nigga none comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_as_set(array):
-        return set(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
