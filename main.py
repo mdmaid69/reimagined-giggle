85b76f9cfa4e413aef0861d0507b2ad454424ad7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:45:27 +0000

    run sea threaten fight side

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
