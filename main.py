356f9745172ec967ce4bfc7b8d186b07b7e4fad9
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:30:11 +0000

    week bite nonsense people getback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+text = "Hello, world!"
+print("Words:", len(text.split()))
