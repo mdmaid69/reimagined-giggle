89f2fe4704ae3ed767fce726c8ec7dbff3447b5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:24:07 +0000

    memory deal note fun whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
