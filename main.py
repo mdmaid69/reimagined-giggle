520db358f2f12e4bd8a373b947ae2972a5588b8f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:14:31 +0000

    chase sweet protect enthusiastic cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
