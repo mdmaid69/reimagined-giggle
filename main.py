588c69c672ca6066deb417fce3d0cff838f6dda6
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:07:30 +0000

    study able whoever estimate learn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def cube_number(x):
-        return x**3
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
