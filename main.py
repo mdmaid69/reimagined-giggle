f74ffe60bbede0704a8aaae56be96bc090248cd7
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:30:54 +0000

    soak duty month chat bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_sign(x):
+        return math.copysign(1, x)
