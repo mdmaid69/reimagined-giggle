75fd5a9f2a749d453c29a230de0d9924eec5444e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:03:17 +0000

    war connected all steep harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
