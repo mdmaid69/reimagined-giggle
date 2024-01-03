86d671855697c54f893c1c18407e919fd5b91495
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:38:04 +0000

    fall guide job place shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
