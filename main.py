6e4db4d1572b05445e02102208b5a60c7aa0d0f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:11:55 +0000

    go weigh nothing low instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_list(array):
-        return list(array)
+def clear_array(array):
+        array *= 0
