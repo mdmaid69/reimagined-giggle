5f67f05d78da2473225faca754315d36e7c22195
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:41:01 +0000

    skill own damage wear left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def greet(name):
+        print(f"Hello, {name}!")
