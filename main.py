9bb03bdd3105ba6c7a81ca30c2676f7ebd546bb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:01:10 +0000

    his hope production better live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
