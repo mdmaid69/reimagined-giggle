0f94a7c728ca7d3687617a96366986c0b5a70dc1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:15:25 +0000

    do prove slice last spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
