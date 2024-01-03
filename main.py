22945f045a8a8a8086fdc04e3d78bc944213edee
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:14:32 +0000

    crowded film fire callback have

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_density(mass, volume):
+        return mass / volume
