7b9c660a82d1260a83fe069148703a0ddcf8abaf
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:27:58 +0000

    polite lonely difficulty regularly finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
