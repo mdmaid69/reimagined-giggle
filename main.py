e41ce0fcb4bf94a95f3b24a5cd63acb96da576e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:16:29 +0000

    discovery excuse steep ride no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
