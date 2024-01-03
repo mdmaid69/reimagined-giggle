44adf7b13224f0a39f068bc10bfd24fc14fc89f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:08:29 +0000

    group brave mark Gg bother

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def convert_to_octal(n):
-        return oct(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
