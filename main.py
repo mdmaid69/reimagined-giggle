3f8a80db3ec994456d645f65611e565933ae341c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:31:07 +0000

    insist thought hole trust search

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
