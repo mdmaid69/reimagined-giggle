6c1df9dac4689c1dce37cd183b232cb6be274cdd
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:20:22 +0000

    effect choice defeat hear suggest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
