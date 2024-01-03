d77c0f84f9fed15be7aa278fc4224c20e28bc45b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:18:43 +0000

    gradually after greeting lie early

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
