b355b618fd5910d28d97e558d38913782a48f81b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:53:26 +0000

    kick callback appearance sure breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
