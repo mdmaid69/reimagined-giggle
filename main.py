945e0e14e41d74c13031fc85cfb2cba6c39d1bd8
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:55:10 +0000

    sound walk gooff general train

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
