212a92de9e3941612819a1cc3ae98f37a014d8cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:35:50 +0000

    workupto wish matter evidence convince

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-print(os.getcwd())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
