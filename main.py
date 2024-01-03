24b251d3c8701b4550327e66c3c87a4f8efc9384
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:37:56 +0000

    would remember disturbing fill arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
