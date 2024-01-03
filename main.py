b5bc891268d05ef925a0a39fdc92837df25a8688
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:15:43 +0000

    wind worried hear be art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
