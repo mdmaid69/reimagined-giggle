e7a53bf8a8cde163930ef46241924eabd7147e3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:04:33 +0000

    admire thick already violent least

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
