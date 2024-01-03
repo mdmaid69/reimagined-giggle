e878cc978a4f6ad69734980b91606eb778a2ee5c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:20:06 +0000

    task outside offer estimate birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
