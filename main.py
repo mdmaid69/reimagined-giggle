0ddb3003a2f39ccb94ae887fe4b8b86362208f1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:08 +0000

    preparation instruction minor type butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
