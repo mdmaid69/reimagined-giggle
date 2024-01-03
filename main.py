af8a0b682e65872b6edf5779b7937ca54a62fb93
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:36:53 +0000

    better skill influence heart everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
