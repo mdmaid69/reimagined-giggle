f703c59824a3dd0c8ad61ff69cfcdb62c823d183
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:20 +0000

    previous goodbye pretty black good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
 text = "Hello, world!"
 print("Is palindrome:", text == text[::-1])
+import getpass
+def get_username():
+        return getpass.getuser()
