cfece8b0d3a60391d1a03977c80c9c493c287db9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:07:52 +0000

    stare right shall concerning great

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
