5f756a58a17703fcac5e2fc5c311efcc3a3ae0ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:17 +0000

    positive everywhere interview official cheap

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
