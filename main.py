1525359617b9f5d3a105fc66b159c2277fe3428e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:35:30 +0000

    wrap ourselves control sport year

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_power(work, time):
+        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
