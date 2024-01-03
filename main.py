bc6ce7b85b7821faafc09081519321088acdc4a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:18:15 +0000

    car lie hour sweep dead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
