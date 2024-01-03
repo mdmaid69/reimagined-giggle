a49040b1d2177817fc230a7a669faa60602b4d12
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:47:36 +0000

    reason but combination within person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
