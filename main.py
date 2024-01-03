5f54d4bc35d5f44705bc33a5d19f3dc6c7b57439
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:18:21 +0000

    government 564encourage responsible reaction warning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 def convert_to_octal(n):
         return oct(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
