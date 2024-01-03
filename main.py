9f1127ff18906995e33ce9e993bd67bbbf227416
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:17:46 +0000

    abroad achieve further wave practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
