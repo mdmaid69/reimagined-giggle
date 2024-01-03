0d2dc0a6e04deb3ce9bba23311a55ea08622f20d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:07:11 +0000

    progress listen tonight thinkthrough polite

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+from collections import Counter
+print(Counter("hello world"))
