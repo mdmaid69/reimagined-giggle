7bc85a691440361a80535cbf5f8a1de59e777994
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:08:37 +0000

    unlucky worth satisfactory shame reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
