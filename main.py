4900b1e3445d4bcf5262d13bc504e3f98a6d60af
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:25:24 +0000

    empty share eat possession laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_float(array):
+        return float(array[0])
