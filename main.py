8d33ce7697a6dfcc2cfc380d1f2961bd5f022af4
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:48:30 +0000

    commit picture more battery comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def clear_array(array):
+        array *= 0
