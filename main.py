c37e7b72384ef6dcd9fff798235a93feaa304489
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:32:21 +0000

    size solve fat interview sentence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
