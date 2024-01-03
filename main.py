7fced929123cccb76d9b419f8ec515a0b028bc91
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:00:14 +0000

    mind tall criticism goodbye gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
