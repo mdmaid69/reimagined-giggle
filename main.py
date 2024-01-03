0ca7c05ebfbf7ff4b1241b2f50ec0252a1795bd8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:59:36 +0000

    Gg comment supply scrape mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
