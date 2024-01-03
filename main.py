02c90b2f81f0028c0eccef71a00f08abdd4e2338
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:15:31 +0000

    live scene pile noone bringforward

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_area_circle(r):
+        return 3.14 * r**2
