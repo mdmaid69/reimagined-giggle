6fb7d740090363381d29947f594900f9c119fd03
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:48:02 +0000

    back kill pure clean breakdown

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
