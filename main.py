40a492ca6270bac0dfdbe6466c205405f8a035b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:12:12 +0000

    pleasant also fortunately congratulate tone

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
