bc2e3d6cd36c709fdbe6ca306ab528841087b112
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:32:33 +0000

    value together question turnback fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
