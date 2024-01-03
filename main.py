162da887bc93392048ac7931ba9a5ac01e757a04
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:37:37 +0000

    once Oo minority tie forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
