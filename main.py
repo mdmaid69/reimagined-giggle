ae4ec39cc633645890f9d68211afc8902028b2fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:03:44 +0000

    the nor forever comedown discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
