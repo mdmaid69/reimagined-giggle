b4d35ab665dc142ca0847b6e322dca03723eaa55
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:47:51 +0000

    room offer approve extraordinary finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
