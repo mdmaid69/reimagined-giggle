60cacb38a336ff755be658e735e255219432198f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:02:21 +0000

    collect write together press use

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
