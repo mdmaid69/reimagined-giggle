e117603bd10408aa62658b8085213c269f993532
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:42 +0000

    phone who left far clean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
