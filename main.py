1563350116bcb9067a15f97fb1cfa7ad3c2d3e1e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:03 +0000

    runaway gooff check nowhere spring

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+print(sum(range(10)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
