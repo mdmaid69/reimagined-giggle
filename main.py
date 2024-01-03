333ad753dfd12d78e53f8611cf03224a27b87784
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:43:15 +0000

    cover handle state likely quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import re
   def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_user_string():
+        return collections.UserString()
