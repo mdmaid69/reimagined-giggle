00c8f3820e9a882d49a0a67266a518eb80496b4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:57:59 +0000

    land pretty side punishment light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def square_number(x):
-        return x**2
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_power(work, time):
+        return work / time
