6132b25fe60fedf0d1bbd77dcf608fe6dc39d7da
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:40:23 +0000

    smile flood fact accident contrast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
