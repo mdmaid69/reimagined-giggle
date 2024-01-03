6594eacf500f022299dc068b3615ddcd52060d80
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:38:47 +0000

    period grand hang scrape noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
