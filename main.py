77322d1a1ba8e581b314eb87ca8142fc51efcf8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:06:02 +0000

    instant ready brief hit mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
