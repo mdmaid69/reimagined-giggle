af362d532ef296f4b7f8dc41e63bfc046d82aa81
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:34:45 +0000

    miss under contents offer scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
