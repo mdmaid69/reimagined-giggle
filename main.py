1f588160e9e82856ee2b68ac980bf3f73f6ac1d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:21:27 +0000

    565end brush refuse born big

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
