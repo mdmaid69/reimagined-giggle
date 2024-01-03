97ac1591ea8aaac8d7f19b2ff0e51cb586c6b417
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:10:47 +0000

    occasion unit bite switch live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
