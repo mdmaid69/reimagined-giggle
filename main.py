d1a60d7b65fc6acf891b8508f26ea93135bc5a02
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:41:39 +0000

    deaf goodbye connected contrast bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
