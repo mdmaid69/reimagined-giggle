6bc20e995022511dfade5ee3e4af8df44ad46ffa
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:16:23 +0000

    approve flow quit reasonable prize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def square_number(x):
-        return x**2
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
