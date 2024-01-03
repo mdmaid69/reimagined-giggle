24bb556c8763ae8bb0956d68a7fca04d33086e14
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:49:27 +0000

    takeback natural family hold ought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def create_user_list():
-        return collections.UserList()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
