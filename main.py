b8a81e0306ed194a66521e6f64a4fa278cf0ddc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:10:11 +0000

    noisy smoke ever drop behind

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def set_array_item(array, i, item):
+        array[i] = item
