a113f4f5ff8f79a6635ab5fbaaccf3514dbad869
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:32:34 +0000

    568energy powerful each wipe criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_typecode(array):
+        return array.typecode
+x = 10
+y = 20
+print("Sum:", x + y)
