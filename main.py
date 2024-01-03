518c8a79480dd319afc98768ecd152abbb2e43fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:46 +0000

    listenfor original grateful bye in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import getpass
+def get_username():
+        return getpass.getuser()
