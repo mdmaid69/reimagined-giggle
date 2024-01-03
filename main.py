790d8049171e0f7f2a499d862790004fe8660189
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:27:59 +0000

    proper apologizeapologise once argue lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
