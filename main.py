3e6fed921229dd3fac88e75127305da4810bfb16
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:05:15 +0000

    kind than charge reserve out

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_circle(r):
+        return 3.14 * r**2
