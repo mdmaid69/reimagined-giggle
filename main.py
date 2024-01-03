3e8eb971f556b04e4c26c8debfaf530b93180787
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:26:06 +0000

    shake fine free genuine tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_list(array):
+        return list(array)
