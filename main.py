64ce4381d281bf6b7f3f8a2d69cc6a99b1686d86
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:23:11 +0000

    makeinto rare go present feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
