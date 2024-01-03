85158804ae696955e0d457d1639b84f2defa2de0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:49:55 +0000

    comfort whoever pull complete sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
