988fefac4fab735e62d673850791dc2b1f533df3
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:29:08 +0000

    thin chief other now tip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
