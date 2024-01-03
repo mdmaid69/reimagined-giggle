91e885a01bbbba9f0d8e568d6865ba5368838aeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:18:29 +0000

    if callat children remove mood

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_speed(distance, time):
-        return distance / time
-import getpass
-def get_username():
-        return getpass.getuser()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
