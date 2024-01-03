add226eefc16e3604efeb3fff2dfdfa2f6f427ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:00:43 +0000

    at Rr necessary if cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
