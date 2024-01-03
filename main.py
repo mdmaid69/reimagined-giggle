331e9cddf62e7c40f845e28087deaa469750153d
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:35:22 +0000

    art 566end Oo goodbye consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
