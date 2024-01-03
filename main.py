427bded34580282f1c727a145aa9d72850ff2626
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:00:45 +0000

    shallow joke brother convince sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
