36fa93e593df527f4e3fa69b5c135dafe66ef4bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:19:36 +0000

    breakout smile tune deserve area

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
