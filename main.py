b5bab2324310e76b13ee8761085214870069568d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:30:00 +0000

    go platform shoot hard difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
