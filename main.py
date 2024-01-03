3af85f3aeb41a79b8f3fa2ff663f4f018f3e710a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:00:02 +0000

    forgive answer pronounce communication reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
