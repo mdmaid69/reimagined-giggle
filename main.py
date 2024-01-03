0f7cd394a04b898b3c57e2e0de33129327ebd427
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:06:00 +0000

    trial complete age trap instead

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
