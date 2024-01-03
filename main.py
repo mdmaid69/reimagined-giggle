1a8ea16ae4ffd4c37dbb99f60fe57d6791619caf
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:01:48 +0000

    shop damp callout increase gentle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
