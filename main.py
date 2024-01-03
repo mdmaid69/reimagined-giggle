4c05558d0bf820e6b711238867824702ebd445de
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:04:42 +0000

    stamp time half hit risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
