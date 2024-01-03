8a29105d31dbdc92659650790d9dca11a28aa456
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:16:47 +0000

    arrive impossible case post though

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
