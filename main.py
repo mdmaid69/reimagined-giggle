081acc6b75ac304ae1cb4d85837b3b022325e925
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:37:39 +0000

    pattern office bound advice read

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
