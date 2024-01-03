e99d1d90ea80bd679b74fc6b41bf3b17cccade12
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:44:20 +0000

    colour cry runover poor left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
