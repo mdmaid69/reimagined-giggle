f6b00c6eabc9a1971424fde73a386ac53f2f7c74
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:59:58 +0000

    anywayanyhow involve tooth female actual

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import sys
-print(sys.version)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
