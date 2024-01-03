703e89cef0723de4a15f9c476510b2e22e123f81
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:31:40 +0000

    mistake Ss war market Itself

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+print(sum(range(10)))
+def convert_to_binary(n):
+        return bin(n)
