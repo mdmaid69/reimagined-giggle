86763c3b10d8ab13c56e849a3bccdec9fc200290
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:00:04 +0000

    stare negative especially back bad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
