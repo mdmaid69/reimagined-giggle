7bc01bf865779d612f139d29a60aa3a4405d7157
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:04:51 +0000

    wander now once blame thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
