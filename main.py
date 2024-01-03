671bbf028a96affc603c095953e905adf9625b6b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:56:50 +0000

    eager nurse attention copy sink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import os
+print(os.getcwd())
+def calculate_volume(length, width, height):
+        return length * width * height
