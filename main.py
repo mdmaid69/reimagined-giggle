ce26607a80cbc7a7de635e286681a067207ffeff
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:14:38 +0000

    national Ll closed warm small

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import time
-print(time.time())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def multiply_numbers(x, y):
+        return x * y
