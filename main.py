fd68e5ecdb1ac8c9c9080f54772208ced4cf807c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:07:08 +0000

    art threaten blame round free

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
