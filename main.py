3ba973fc7cdc9cb291734ecff992a98aba8feca9
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:41:18 +0000

    somehow rubbish wash last first

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_typecode(array):
+        return array.typecode
