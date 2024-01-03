81e1a8b336768e260cba910ddb56bbfdfc458167
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:39:44 +0000

    float after amount entry blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
