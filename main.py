9a256e01b726d14eaa6553220afcc28c67291720
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:33:32 +0000

    belong matter laugh official male

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_queue():
-        return collections.deque()
+  def delete_file(file_name):
+        os.remove(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
