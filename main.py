84d5333d2d29813fd3296d802356190f47937308
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:25:19 +0000

    free post sit produce basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
