1d1eab7256a993bfb7896320bef270809f505c11
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:34 +0000

    live sensitive probably pull lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
