9b9cd188e7a4891d96e2f44c8dc22acf9d5e6e92
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:37:53 +0000

    hospital well afford bush answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
