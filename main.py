75afa7a94dc9b801e0a0df385aa590a049d4e7d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:13:04 +0000

    attempt union name Mm work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
