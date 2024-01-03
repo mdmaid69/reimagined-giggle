13f19914fa1ce288090b567b0b3d8994546e7948
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:49:10 +0000

    break stick aim movement sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
