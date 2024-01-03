b2a97fc7019b87279f4cc93bced8b0eb52eb6cb4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:58:30 +0000

    run mind operate advantage nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
