6a62f233bc54cf545d669e155a86fdd29efacc39
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:20:57 +0000

    OKorokay receive race Bb bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
