995c21a250ee6233e8edd2a995992bca4dd7aa66
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:52:39 +0000

    tired smile never loss getout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
