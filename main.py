b2194336e4aca099c9567f0ed409475eb0683f3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:10:17 +0000

    cry follow makeup thick pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
