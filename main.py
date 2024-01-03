70b76573e3d51760f57c718b62575e1bee15e100
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:07:57 +0000

    command important rush ought roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_counter():
-        return collections.Counter()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
