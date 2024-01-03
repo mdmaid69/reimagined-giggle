42349abf9e927b8f77140714fe111fe524e2680e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:22:44 +0000

    please sense watch plain rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
