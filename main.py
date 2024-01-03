46e8b62c58ddb19ae3958fcaa105be92eb7ccec3
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:56:51 +0000

    share order tip hello prefer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
