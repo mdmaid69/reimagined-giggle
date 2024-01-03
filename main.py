92a99b0d7e0c7760e5e6323fd46761d26af6bdf0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:55:03 +0000

    fun result terrible simple shrink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_speed(distance, time):
+        return distance / time
