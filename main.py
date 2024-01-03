b76e53647cbf07885e626f96baa9789cfc3133da
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:37:17 +0000

    collection as poison prefer track

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_work(force, distance):
+        return force * distance
