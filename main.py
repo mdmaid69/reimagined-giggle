53603379d2c3eea444d5fd6fbb8fa5e95478f23d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:05:43 +0000

    drawing walk lead prevent Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
