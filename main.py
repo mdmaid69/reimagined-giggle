f2de368345a4034c8026565ea3c552db5ba025ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:51:27 +0000

    brother Cc dream urgent group

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
