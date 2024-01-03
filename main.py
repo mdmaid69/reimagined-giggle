bd1c6170d7bdf668a36ce4c84600249ecadce0c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:54:10 +0000

    travel apart key performance nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_slice(array, i, j):
+        return array[i:j]
