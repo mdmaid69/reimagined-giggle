3841ef1d2954933838f59209f64c3e837c3fa9d5
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:00:20 +0000

    ill perform object noone over

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+text = "Hello, world!"
+print("Characters:", len(text))
+  def is_odd(n):
+        return n % 2 != 0
