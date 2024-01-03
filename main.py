4146275c8940ac404b97c611e23b42619caf4856
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:19:32 +0000

    report phone genuine cover impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
