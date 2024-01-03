ae69294fbe70bf02250dfaed9c615d1aa967df0f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:24:30 +0000

    takeoff just sort display tone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def reverse_list(lst):
-        return lst[::-1]
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
