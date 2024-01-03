d5aed981c65326db4d0f0d7acda19a0b284617d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:41:13 +0000

    forget angry into satisfied normal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import tensorflow as tf
+print(tf.__version__)
