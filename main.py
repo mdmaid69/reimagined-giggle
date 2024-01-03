c68c3e6aba373466131976b9c3b06c8acc608fb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:35:20 +0000

    Uu home snow curious pin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
