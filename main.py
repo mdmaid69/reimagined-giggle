eaff04403b13db98bc323d46c96d01bf32dffb33
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:21:02 +0000

    shame rate dream sweep reasonable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_work(force, distance):
-        return force * distance
+import sys
+def exit_program():
+        sys.exit()
+def sort_list(lst):
+        return sorted(lst)
