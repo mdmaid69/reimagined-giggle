9043048a26706754b7c107242a025a9c16bdd7fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:32:01 +0000

    spend development 566end Qq sure

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
