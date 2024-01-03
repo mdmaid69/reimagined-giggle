da5fd85d35ea6d041e13dffc931f3f0591658405
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:12:08 +0000

    closed head star expression murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
