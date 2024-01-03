3ee9e6d72949385ee9e93aa157059105ae7e2241
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:53:44 +0000

    manner plate touch black movement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_power(work, time):
+        return work / time
