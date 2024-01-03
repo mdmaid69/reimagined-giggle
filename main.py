afbcfe40e63271d8ed295d808711c9fa095f6300
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:49:45 +0000

    regular skill major trade bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
