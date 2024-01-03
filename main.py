118fc28f7dfa955bd0a38bfefeb8beb2bbc209db
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:07:25 +0000

    produce hit expensive clearly in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_set(array):
+        return set(array)
+def multiply_numbers(x, y):
+        return x * y
