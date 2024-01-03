4600d216dc0bdc54872f0a696cea3e3c20681abe
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:38:37 +0000

    game interested up trip dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
