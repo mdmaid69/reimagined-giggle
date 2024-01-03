051d490337c4219490d95124ae4aaaa0013a19b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:30:55 +0000

    valuable swallow phone payment offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
