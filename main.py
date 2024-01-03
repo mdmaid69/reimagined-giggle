4ea01905ec75780107fe64382897ec06a683bc00
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:20:58 +0000

    convenient slightly list key loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
