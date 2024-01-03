34a14a1bc2dbf33b9daab539620a36a65c10e339
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:44:24 +0000

    powerful desperate leg runout matter

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
