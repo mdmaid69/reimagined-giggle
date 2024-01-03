74bca2b2223d04593099321a2282a0c23b2afc24
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:28:35 +0000

    neat peace sit tone spot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
