ab3b90aeb11661490bbdf3ccc6577079adb3efe5
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:40:06 +0000

    pipe up punish runaway quantity

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def square_number(x):
+        return x**2
