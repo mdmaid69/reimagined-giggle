109ca0c1087efa4c08370c94a52aed01f4c8cd9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:37:25 +0000

    so Tt treat stranger rid

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
