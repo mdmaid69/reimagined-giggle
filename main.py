3d8d90b5d894ed7647dfee0bd8f7b98ed51e8014
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:23:58 +0000

    which spend somewhere always impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import os
-def remove_directory(path):
-        os.rmdir(path)
+print(os.getcwd())
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
