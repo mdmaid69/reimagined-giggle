8f1df49be8ef0a236058806d166f35d86c2a3ebf
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:49:47 +0000

    find complete hand tour fly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_user_string():
+        return collections.UserString()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
