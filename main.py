24abdb380cb377ccdb00badf9fdeace61152b249
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:29:46 +0000

    demand raw juice bag Oo

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
