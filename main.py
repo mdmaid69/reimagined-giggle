7a381a4304ff1db6f33ea858e6524d5724469a68
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:42:30 +0000

    someone bit match surface guess

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-print(math.pi)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import platform
+def get_os_info():
+        return platform.uname()
