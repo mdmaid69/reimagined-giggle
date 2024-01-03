feee1dcaf14cf690d30ea25fafbaeba4d4d89a7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:21:28 +0000

    consider hear slow arrange plan

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_as_list(array):
+        return list(array)
+import sys
+def print_python_version():
+        return sys.version
