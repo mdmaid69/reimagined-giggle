e8b6338ddc88421a0a453b5af5b1d11ec2c042b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:59:18 +0000

    expensive smell circle keep measure

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+print(math.pi)
