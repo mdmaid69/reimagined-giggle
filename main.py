8a9a4fb600b01d8e55f1b68af5835df1dbd48d9e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:17:11 +0000

    touch frighten question set live

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+print(time.time())
