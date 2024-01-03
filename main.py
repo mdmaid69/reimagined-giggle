e83bde66a845677fdb536c09e83ad2b98d378779
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:30:26 +0000

    pass tight without branch hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
