7a25f35d6aa9963cb4433bf22f2e6fc01817d631
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:24 +0000

    some thought strike top hospital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+print(os.getcwd())
+import array
+def get_array_as_list(array):
+        return list(array)
