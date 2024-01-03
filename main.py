4be0a1b0e5951ce608b150a3b4b1739e065362cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:22:01 +0000

    forgive test watch most brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import collections
+def create_user_list():
+        return collections.UserList()
