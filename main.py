4dd62e9ae807acdc7ebc837e21c1d338e086d51d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:12 +0000

    week cruel punishment mass switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
