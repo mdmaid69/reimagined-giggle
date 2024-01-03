f4952d96696c8b91167dc9881327281d089cb61b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:10:22 +0000

    afternoon bar wish unlucky town

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
