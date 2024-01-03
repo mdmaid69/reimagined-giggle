e7a763bc61377ce3c3b90c9bb8f2b75c3aed3611
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:21:39 +0000

    cope smart far pretend thing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def reverse_string(s):
-        return s[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
