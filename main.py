9a015ced0253853b648b06b9715a7b59a727f4b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:24:03 +0000

    pure true excuse exact sudden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_speed(distance, time):
+        return distance / time
