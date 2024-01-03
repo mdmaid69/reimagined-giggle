813390b22efb1af7400e091e7e55213ed71f7bd5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:49:59 +0000

    bill port skill delight emergency

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_exponential(x):
+        return math.exp(x)
