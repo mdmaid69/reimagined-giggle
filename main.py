cb5b648c8bcdac75038d5429d886f366ce73df72
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:26:19 +0000

    takeback depend 572enter concerned something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
