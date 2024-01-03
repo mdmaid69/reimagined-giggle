c3ed107bc9276881009176e363e03936a4928c99
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:01:31 +0000

    partner past cover conclusion dick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
