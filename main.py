f98c8beb4d6cdf10da4a7a143cb753ff7ff0158b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:14:12 +0000

    responsibility region lock break heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
