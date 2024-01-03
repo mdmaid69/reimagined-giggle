7e99863090c7514a9b1bceb099efca12acee592d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:45:30 +0000

    backwards communication statement possibly meeting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def divide_numbers(x, y):
-        return x / y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_density(mass, volume):
+        return mass / volume
