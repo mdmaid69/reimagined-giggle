2ecdd032ae0e06c6efff18444627756c539e8333
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:42:08 +0000

    steep feeling remember object distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def reverse_string(s):
+        return s[::-1]
