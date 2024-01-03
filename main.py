be58fbacae0a7748dae015e7fd58e619f731a656
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:26:37 +0000

    use surprised first cope public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
