4a2a7b1d32ced1d93d3e68b820fbe6db54e5b8b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:34:56 +0000

    consider pause winter protection light

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
