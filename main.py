962e61bf36ae51add523f1af324b1fbd93699c0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:22:07 +0000

    basically flat plan instrument trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
