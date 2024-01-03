fc055a0b5f22e0cd084cd5550be5a2face99705c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:13:02 +0000

    town sound control describe present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
