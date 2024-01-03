f9049a9096ddb54765a30727e5d8682ef932d93f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:45:03 +0000

    flat boat illness trap tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def convert_to_hex(n):
+        return hex(n)
