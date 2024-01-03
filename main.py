d354cfbd9e5ca8855cb2086de1e541cf12b38d51
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:16:08 +0000

    otherwise popular personal do belong

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
