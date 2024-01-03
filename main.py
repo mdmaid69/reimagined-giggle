6463d725c158829d36dc06b0f3905f8205c7f686
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:08:45 +0000

    bye recently choose lucky dare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sys
+  def get_python_version():
+        return sys.version
