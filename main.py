4a71d8604493ef55d3070411b12f3b301703df2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:09:20 +0000

    underneath soon claim beat passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
