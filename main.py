34b66ec9e8ee747446bcbc997ec6a2d5cd0f3c20
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:18:03 +0000

    protest play wipe explanation pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
