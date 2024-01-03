62bb1aa3d33ac5150eacdbb1a581aec3151a9367
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:21:11 +0000

    disappointed character bringup scream bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
