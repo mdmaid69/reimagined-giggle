52e890b45e9e7d472149824b4fdd152b79c91e1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:39:53 +0000

    long study represent search push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
