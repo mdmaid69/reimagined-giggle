e38687184338fbdaf1395563f2b0489d0e28feca
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:48:15 +0000

    instrument steal delicate who phone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
