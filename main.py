4195f8a6256477259628d4d40ec886a53d8742a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:56:33 +0000

    knock meaning Rr again order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
