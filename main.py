2bbed411253e0ab0d38fe7dde782a2bcebe0a082
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:54:13 +0000

    call fear do lunch level

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
