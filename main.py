200af6b219713151bd0ed361ce5d0476edf7430c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:00:35 +0000

    sensitive bringup wet content believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
