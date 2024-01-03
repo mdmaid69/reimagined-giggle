e5e8a251ac57b2129368ac8daa4e5fb92fd61998
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:51:15 +0000

    replace bus close spot wild

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def multiply_numbers(x, y):
-        return x * y
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
