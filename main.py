153b07178540aa4ebebaddab5d771445b4ab691d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:40:56 +0000

    the generous classroom size giveup

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
