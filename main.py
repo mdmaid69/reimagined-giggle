7016ee2468cddb533e69a60a96ef4784cf40e13b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:53:48 +0000

    anyone laugh for interview complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_perpetuity(payment, rate):
+        return payment / rate
