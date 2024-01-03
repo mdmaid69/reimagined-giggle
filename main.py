1d0bbc0af165af7892aaa2f285ce985d2c21ac80
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:23:17 +0000

    trial joke catch ordinary him

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
