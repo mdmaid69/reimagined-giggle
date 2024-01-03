39b9976d166e78f63bda8781afc501abfb8406e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:54:32 +0000

    science blind pot describe comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
