4b675422fadf9dcd16fc42a6c0313e1a03ab7417
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:21 +0000

    together present shut shall calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_factorial(n):
+        return math.factorial(n)
