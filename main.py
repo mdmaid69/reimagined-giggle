1ad6f5f10bd830555b3b20210e0d7359e9e37d9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:39:57 +0000

    breakout towards callback enthusiastic unite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
