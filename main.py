54f1299260caf6c4f232e1e3918db5845bd4e3a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:38:08 +0000

    head look matter side determined

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_max(numbers):
+        return max(numbers)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
