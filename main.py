b14896fea75fc642c638e1f76f48f55c9d86b594
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:56:30 +0000

    anyone peace change quantity win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
