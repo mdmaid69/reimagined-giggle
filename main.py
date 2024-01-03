605d1afaf7ae95839ec6cb0bd5663e6ff5d3c657
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:10:16 +0000

    belong bright sticky impressed punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
