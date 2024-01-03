d012409d92cffbfb6592bd72bd0803ca1fb92c38
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:02:26 +0000

    teach interesting our change open

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_array_to_string(array):
+        return array.tostring()
+def find_max(lst):
+        return max(lst)
