b945a25cb07199333e7c185c7dfb6db6fb1cef71
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:20:36 +0000

    temper slowly drink cool situation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+print(time.time())
+def convert_to_octal(n):
+        return oct(n)
