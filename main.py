dc206195376fce3afe07803cdf82372a897dd3dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:54:57 +0000

    interest inside pleasant direct lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
