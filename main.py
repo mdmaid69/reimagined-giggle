db089ad68ee32f5997f41460f67528e8d5b2feb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:57:09 +0000

    control plan law read little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
