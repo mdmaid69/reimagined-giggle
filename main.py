ff36225808c1e0914c098500389f6e4f78aad90b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:03:57 +0000

    table table fork favour in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
