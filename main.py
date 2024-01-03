d51d270e3ff691b54ca252553df80c6c5dd86777
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:34:43 +0000

    provide wrong piece lean congratulate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
