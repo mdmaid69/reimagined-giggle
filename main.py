4b682cd3fe94bbcd614d561d57b6aa89474c2681
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:28:44 +0000

    closely protest development rest already

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
