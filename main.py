3b710650af1cfc5394196c2b81c87aa2e78a0023
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:36:16 +0000

    except know odd stiff name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def sort_list(lst):
+        return sorted(lst)
