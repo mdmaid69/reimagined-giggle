271d24bb2a47eef274787a7db85ad4ef283ea655
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:39:12 +0000

    question demonstrate case light claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
