02fb0226bc48383369982761f51b55c6a3fb1148
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:44:07 +0000

    rely radio water dead temperature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
