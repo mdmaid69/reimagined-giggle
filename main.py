95cb636a5fc45a1269fed87808773e453173cd49
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:46:42 +0000

    raw trip note Dd knowledge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
