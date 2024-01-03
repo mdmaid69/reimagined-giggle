f2d59a770315afa57c0e3bc60d27147a914b16fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:19 +0000

    pinch rest wrong disapprove quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
