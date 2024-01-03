b1fcad4053f79b68d05ca1c93442da2ae14f6d6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:52:59 +0000

    push emergency miss blame chance

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
