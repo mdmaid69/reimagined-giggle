f553d21b0a525d81635a6ce0d9cce88dd109d121
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:44:04 +0000

    bury low arrangement century taste

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-print("Hello, world!")
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
