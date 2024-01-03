068b26e35bf2fa544b3c3b9db98d445519dcae32
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:42:49 +0000

    off respect strict at onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
