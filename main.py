2e02f9d0f40af15560cdb2faa9fc72b0cb8b6648
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:31:07 +0000

    comeup trust succeed material callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import sys
+print(sys.version)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
