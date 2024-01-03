4e77c4babaaec168a859a146cc45a43cbad9f892
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:34:07 +0000

    news way neither apologizeapologise 565end

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print("Hello, world!")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
