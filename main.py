b32bf5b40dbfdbfb39c65216c606e39cd78f440c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:43:33 +0000

    just snow choice nose exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import sys
+print(sys.version)
