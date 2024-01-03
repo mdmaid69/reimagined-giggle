a4a5be7b3e6c6650eb9674ca330292912cffd2d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:21:51 +0000

    test brave frightened travel gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
