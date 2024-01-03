a76ad33d2d30bf45cea77c75ab4d36d9996731be
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:01:10 +0000

    content Itself clean though improve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
