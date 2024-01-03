c58c8007db91b154ab9a250f3ab8618f448aadff
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:12:24 +0000

    advise deep hope say communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_elements(lst):
+        return len(lst)
