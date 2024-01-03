88f338becbfcbbbe8ce47b64fe0248b8d6c82730
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:32:29 +0000

    depend father repeat discuss deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
