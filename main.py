39ad4c90fed9764f106a50559f8fbc1c8615e607
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:47:10 +0000

    all progress 570enjoyable argue careless

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
