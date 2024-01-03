77caf6f2393bf43d2b232a121c74bc2e48aca0c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:11:12 +0000

    hot wander get convincing distant

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
