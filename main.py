cf6205b80e60073fd954d7dd0d21114c37fae520
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:05:44 +0000

    demonstrate depend big exactly Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
