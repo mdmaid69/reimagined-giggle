782a5ec8ed38ead0d588596e64d7a51f739a1029
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:38:14 +0000

    disagree search heavy disturbing artificial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
