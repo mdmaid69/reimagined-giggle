e07c4f6b1dd2a2a194f8247d0446a9b2af94e124
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:48:37 +0000

    opinion mess image table usually

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def append_to_array(array, item):
+        array.append(item)
