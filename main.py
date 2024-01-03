57c637fb4a0435bea66228bff6b2bed7a8167ea6
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:09:33 +0000

    allow main case contact fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
