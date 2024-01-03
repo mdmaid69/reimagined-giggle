9ec8710e5f38a73f6396e98f3b520b8b9ff1b84d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:53:57 +0000

    home Mm post size harm

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
