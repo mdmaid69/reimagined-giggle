63c642ff47a2c2a1bdb6ad8378536f58e73d231b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:18:51 +0000

    tap welcome talk switch still

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
