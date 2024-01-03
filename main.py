4832ac613d48d84e023a3961df2a45e7026c3149
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:58:21 +0000

    rest transport onto collection improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
