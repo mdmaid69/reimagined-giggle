eb11f7ed9f32af11c6fcea8934d431b05205f220
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:33:04 +0000

    create concerned police design present

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
