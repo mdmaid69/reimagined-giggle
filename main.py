39bb87640cf995222ab8f9e3375cfc32a85a5a50
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:07:16 +0000

    blame taste transport recovery lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
