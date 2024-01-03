b53aba3292207572d7d07b687114046879f7fad7
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:59:55 +0000

    concentrate another arrive seem secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
