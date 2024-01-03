d865c70226c6e1c68c96989b3f498e43ac7bfa73
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:27:37 +0000

    protection club dear performance pity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
