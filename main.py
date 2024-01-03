31359958cf947df3239daecf67c0be23fa22bfee
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:51:28 +0000

    mother early prefer firm official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
