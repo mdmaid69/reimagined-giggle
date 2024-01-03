0b3bd981535d2275ced98d5819b2a88b988d6782
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:27 +0000

    abroad teach tap intelligent affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
