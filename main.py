2e41dcb129bc79b3fcb24fa26aacf0f5be878db8
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:33:01 +0000

    circle shock test score born

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
