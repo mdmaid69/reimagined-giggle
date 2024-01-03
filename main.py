f789c75621da2d13d761b52ff515a6727468b07a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:56:11 +0000

    so score Vv 568energy contain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
