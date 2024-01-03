f17502ad33e77c5362e89c86043e86f062ac9627
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:04:02 +0000

    accidental responsibility recommend back rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
