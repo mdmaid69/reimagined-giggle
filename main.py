5e337cc79e8f5bdd432eb4bf531ae9582ca7ace9
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:46:15 +0000

    whistle head storm cover rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
