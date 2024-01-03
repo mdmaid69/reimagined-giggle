8545f6e2daa58664aac1ce575c4a8c5351546e4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:59:24 +0000

    excellent bringdown plate market summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
