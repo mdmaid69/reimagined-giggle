0385f26f08d3cd9423db107d1082850a1df502b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:23:56 +0000

    roof gift piece forget gap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_length(array):
+        return len(array)
