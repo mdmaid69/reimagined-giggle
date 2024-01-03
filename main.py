a5ee359ab5b3f4439e76ddfea2db09fbacc3a3fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:48:03 +0000

    shape callat bright doubt through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
