7ae9a9a12cb0bb187a29068061a4755098bacf29
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:31:23 +0000

    instant afternoon comeoff exciting traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
