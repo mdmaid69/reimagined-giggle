b3c6840a1ea39dfdbd41d74b1972f4557ad48fa5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:10:23 +0000

    left thank lean beautiful child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
