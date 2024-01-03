5f85f0b830e3a8b6d3655d4595b9b1c08db539eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:56:03 +0000

    foreign certainly clearly immediate house

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def pop_from_array(array, i=-1):
+        return array.pop(i)
