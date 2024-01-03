7965cfb760456d12c88b9bffdf34348e25b622ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:32:18 +0000

    study suck bend bright typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
