5a99d4d72a0ab8efb8fb20cc3fc633fcd6afd647
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:22:18 +0000

    mean rush sell adjective send

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def divide_numbers(x, y):
+        return x / y
