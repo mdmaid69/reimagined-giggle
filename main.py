0716394f20721ee4612bc9591f9dc86ecf9135bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:43:32 +0000

    close tired pronounce smile wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
