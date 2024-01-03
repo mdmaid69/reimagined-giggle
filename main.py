ab3ba610705879ab2d42c40df9dd1488e48bc6ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:06:54 +0000

    think reply exactly goup plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import os
+def remove_directory(path):
+        os.rmdir(path)
