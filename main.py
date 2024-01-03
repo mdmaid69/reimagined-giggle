3e224718608dbaf70330303f8e6050dd419edffc
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:23:52 +0000

    moral powerful left doubt practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+def square_number(x):
+        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
