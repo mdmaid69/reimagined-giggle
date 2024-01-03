b86921a92693db8fea4371ab0108b1deb1b5b6d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:23:34 +0000

    grade trick mouth never skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
