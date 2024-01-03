5a27c2e826571ae53915feacc9cb87de64fcdf36
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:58:07 +0000

    lesson crowded fast unhappy slowly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
