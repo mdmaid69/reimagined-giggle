2358308c93506ab4bf8d838d50c3340b0fc97338
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:06:02 +0000

    Ww long notice teach spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
