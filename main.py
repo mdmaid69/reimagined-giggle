efd31d43334e3cb57d3d0926f4dd9bf108254827
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:32:55 +0000

    accept drop unable conclusion advise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
