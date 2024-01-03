0c75d015eb31e3119bbb4f27e68f6e326b3fac16
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:28:02 +0000

    course whichever object feeling everywhere

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
