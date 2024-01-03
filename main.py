9dc16e3e72cbb7b2f4fe035d2dad9006565307f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:05:05 +0000

    turnoff entry bringforward future probably

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import tensorflow as tf
+print(tf.__version__)
+  def remove_duplicates(lst):
+        return list(set(lst))
