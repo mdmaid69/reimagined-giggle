535f208fff73ce9f6e5bf478c5abf8b71e91e0bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:42:09 +0000

    trick lonely lock claim considering

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
