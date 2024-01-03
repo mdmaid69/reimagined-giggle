c9fb615aadcb47ac6a7ffa1223189f56814d10b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:57:01 +0000

    everything feature ground building swim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
