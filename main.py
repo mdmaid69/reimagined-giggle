ddff5fc9af58241c7784969984f120a35b736684
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:42:38 +0000

    earth round thing bet never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
