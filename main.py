27fc2e6f22a7da3818def92fb5954fcd7b997d8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:15:18 +0000

    instead couple herself marry mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
