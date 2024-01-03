a7040894a83c537ba7b85b4ab381e74d7aae80f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:38:41 +0000

    attractive minimum afraid become holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_slice(array, i, j):
+        return array[i:j]
