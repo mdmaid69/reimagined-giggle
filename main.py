ddddb5cda88cb2d098068ae3b1ff27eed7485222
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:28:49 +0000

    preparation surface container hurry friendly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
