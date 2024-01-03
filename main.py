c8ede0f5f44e1c629a48ba1f79b82137104d3bfd
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:03:03 +0000

    rush makeup music marry path

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_density(mass, volume):
-        return mass / volume
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
