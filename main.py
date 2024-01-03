13b906ee908ed255b61a9d24e3e597a60eaf607a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:05:04 +0000

    thank touch size agreement present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
