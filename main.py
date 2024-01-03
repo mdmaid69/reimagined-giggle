5dc3e1e9757a82faa0f33a5f1dadfc2636622324
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:21:46 +0000

    dance concerned choose foreign brother

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
