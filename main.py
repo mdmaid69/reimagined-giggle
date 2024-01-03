6b07371a38867a37201839c4393069a11976acb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:01:57 +0000

    light while next proper attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
