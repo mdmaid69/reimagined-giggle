12b60838717825f6ea45b6b0f18bd6cf9cce05e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:53:55 +0000

    awake measure warning spin else

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
