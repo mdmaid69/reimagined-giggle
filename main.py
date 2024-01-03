725cfaf2547f86a7537d1df7df795b7b6e29383e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:08:50 +0000

    comeout laugh roll statement store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_set(array):
+        return set(array)
