24dbd6b79d8bc6ea5e4efd4b9be02d88ef6b6a8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:45:33 +0000

    out birth sport exam reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
