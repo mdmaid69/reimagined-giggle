ef9910ecfabe84da6fbce55739b9a05809c8d89f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:27:29 +0000

    especially that copy better flight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def cube_number(x):
-        return x**3
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
