1bad915fb16041d3ab54b1df5ef30a6f92fae8cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:19:07 +0000

    neither secret twist catch ill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import sqlite3
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
