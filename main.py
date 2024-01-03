72ee7964df33c731ada3dfdd21015fd74cd16bde
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:39:40 +0000

    city set persuade willing emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
