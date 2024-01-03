44f5caa1eb9278ae2a34f0d5b9a49c03d16de751
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:11:42 +0000

    future by safe callback disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_list_from_array(array):
+        return array.tolist()
