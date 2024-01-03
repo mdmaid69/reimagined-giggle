b7fe5717ee5e138986ee0c5447426c32ae3e86dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:55:29 +0000

    your correct too whisper but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
