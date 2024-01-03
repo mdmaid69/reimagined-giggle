38b587510ffa425be0a4182751a3a8cfc690963d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:24:10 +0000

    comeoff bear ear or post

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
