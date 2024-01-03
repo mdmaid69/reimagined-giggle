c501e1f46b8caa4fa30e058651dbcb43651af83b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:48:31 +0000

    light refuse hide concerning smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_floor(x):
+        return math.floor(x)
