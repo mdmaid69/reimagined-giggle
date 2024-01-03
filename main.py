aac399077d1db1ea8907795672e266ccded210c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:47:15 +0000

    skill detail unable either sit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import sys
+def print_python_version():
+        return sys.version
