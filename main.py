f79dcb7090fcce7ebb56d74cc947e2452fb65e31
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:03:22 +0000

    impression recently list gather left

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import sys
-def exit_program():
-        sys.exit()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def split_path(path):
+        return os.path.split(path)
