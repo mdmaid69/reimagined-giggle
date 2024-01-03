38fd9202cfd6f3ab502a9278e81dadc2640492ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:31:57 +0000

    right generous order emotion Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_as_list(array):
-        return list(array)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
