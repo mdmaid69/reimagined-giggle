1f30fb005e2646899c420de5026492a22da1f869
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:19:23 +0000

    block payment plain pain turnover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
