7cf1a6072f3e2420ce7723ac61f46bd1259c2881
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:59:03 +0000

    time high minority rather yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
