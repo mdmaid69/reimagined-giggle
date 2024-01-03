969060a1889b448bfebf251c58fb11020be1c97d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:54:45 +0000

    second centimetre clean interrupt purpose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
