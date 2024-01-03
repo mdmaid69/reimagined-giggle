5c04d7ebb9a147fe5e3135d490515d8fb2f38f09
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:24:08 +0000

    shall direct come brown fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_speed(distance, time):
+        return distance / time
