7d6fd46aba7535ff6cfe5256604963ef338be051
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:25:39 +0000

    perform passenger thinkover report busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
