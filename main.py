aa51a3680d639fccd43cd483575078c4b7b214f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:35:23 +0000

    team advantage exaggerate special no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
