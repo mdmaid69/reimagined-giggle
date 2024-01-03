4d476fce187f852a8aa476ee136a772c723ba29b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:59:35 +0000

    unusual petrol grow touch narrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_work(force, distance):
+        return force * distance
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
