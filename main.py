753c57e4ae7ca06aa7462d6fe4d14ddb405b29c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:40:39 +0000

    goback(to) dream attention clearly this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
