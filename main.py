b0218fbd982c0eeb687e18643f02347309267300
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:08:32 +0000

    term feed upstairs instead happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
