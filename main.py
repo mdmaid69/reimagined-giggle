7522b562c224f94c5d661689a36bc19bf4ff1cc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:36:12 +0000

    hot through custom war pencil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_hex(n):
+        return hex(n)
