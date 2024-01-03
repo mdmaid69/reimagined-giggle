f0840a28a9ceaad7b914fe16dc9a6767a9cbed29
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:43:38 +0000

    whatever innocent nose for tour

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def reverse_string(s):
-        return s[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
