3f618f2fa0c3c27a4779434a588d1e303b69f09f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:30:51 +0000

    important opponent fashion declaration destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-print(math.pi)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def change_working_directory(path):
+        os.chdir(path)
