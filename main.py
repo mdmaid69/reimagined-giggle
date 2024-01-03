05d994119b8ab14c78c21f4d53089d3eae07f6b1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:00:30 +0000

    comfortable nonsense light comment freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def subtract_numbers(x, y):
-        return x - y
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
