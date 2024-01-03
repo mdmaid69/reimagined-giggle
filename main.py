e5860b98db79e7c33c76f505688768a439e5cdb6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:11:49 +0000

    early answer cheap justice voice

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_current_working_directory():
+        return os.getcwd()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
