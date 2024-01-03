eaac427e2f11aa5d265d460a16735d24c9405728
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:54:27 +0000

    object find kill check line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
