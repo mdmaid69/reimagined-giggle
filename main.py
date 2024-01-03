9a453318d75db4b7f31814509a9f59235a808d68
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:39:30 +0000

    by exit respect mention sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_bytes_from_array(array):
+        return array.tobytes()
