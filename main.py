844f70283f8f1ceed392b4290cca87536578148a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:14:11 +0000

    work blame large invite ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
