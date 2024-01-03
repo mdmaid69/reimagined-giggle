f7771fd9cca0bd45b3396a3c3208fcb2f894cc74
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:50:17 +0000

    fly expression Bb generous cheat

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def reverse_list(lst):
+        return lst[::-1]
