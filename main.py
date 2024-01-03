653b42ccd78e2f3a62de04baf2101b5c4c8c7c6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:09:36 +0000

    operation split miss clear ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
