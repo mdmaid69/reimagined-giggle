b70acaf6d95631d61b1f2ba024e06a3bad263594
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:50:52 +0000

    hospital sweet crash duty such

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
