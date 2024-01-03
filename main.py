1c0273388c73091a7283cf7c7ae8ba9b001add3a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:08 +0000

    sister sick why shall mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
