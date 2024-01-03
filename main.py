ef6ba6e2bcf9836e21f1ff35a219481963638696
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:09:52 +0000

    match mention handle increase beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def convert_to_hex(n):
+        return hex(n)
