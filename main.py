5b6e4f1cedfdc70cf6423ad8938af8ce105e2be9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:34:36 +0000

    their Itself forever raise type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+def find_max(lst):
+        return max(lst)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
