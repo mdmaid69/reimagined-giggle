183aa976dda7fd13d25d0479c1c881d3af905174
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:12:11 +0000

    act couple for grand dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
