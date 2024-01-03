c0aba6ae0878cb3e17a4c42c32745be8625331c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:08:17 +0000

    Jj slice pile second national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+print(time.time())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
