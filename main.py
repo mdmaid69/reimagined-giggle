31adb78d6f41551018671e166c644556fe2066c7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:09:43 +0000

    cause try go oil explanation

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def find_min(lst):
-        return min(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
