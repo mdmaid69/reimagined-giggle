43401ec1b34049b27a3ab824f416ebdf9b84aa04
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:13:14 +0000

    bridge really light congratulations thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
