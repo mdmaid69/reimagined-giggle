41ce38828161ea084e2869ccf2d76e4e01ebb8df
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:39:29 +0000

    onto shape could attractive basis

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
