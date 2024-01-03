5be5cfb3a061e7e646ce7d70222962266d303170
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:38:32 +0000

    at soon some takeoff same

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def count_elements(lst):
+        return len(lst)
