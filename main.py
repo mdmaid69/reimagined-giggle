00232a50ea55fc42463fe587571021aa18fed5eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:41:14 +0000

    address pressure pain goout dance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def count_words(sentence):
+        return len(sentence.split())
