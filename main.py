60359c57032e2cb6c359797f056099223ab1683f
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:41:12 +0000

    prefer confused extremely condition across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
