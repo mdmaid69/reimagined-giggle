097b4423647b7151dfd4a2e5bcf2bbf215c410a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:12:10 +0000

    cinema funny such contain accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
