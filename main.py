ef251bd753474d4d2778902db170b1bb461334f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:12:08 +0000

    listenfor process shock hole rate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import json
+def convert_to_json(data):
+        return json.dumps(data)
