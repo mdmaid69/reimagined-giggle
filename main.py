063793a560a56bcb7891bd6ad9432172a00bfc31
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:04:09 +0000

    run accidentally practical standard lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
