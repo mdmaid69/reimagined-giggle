ffcbf503f285316b4ef375e5c85f545f4bf725c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:32:37 +0000

    which sting instead high born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
