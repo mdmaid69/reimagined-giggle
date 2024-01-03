8e2e05a055d1c4b3d16c1487ad7138aaa2a999f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:21:27 +0000

    just petrol control convenient leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_power(work, time):
+        return work / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
