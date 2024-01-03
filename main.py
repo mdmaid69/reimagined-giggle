2f6943a9294a573fcb48aa69cb2733ed9216408d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:30:10 +0000

    mass pool start roll train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def convert_to_binary(n):
+        return bin(n)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
