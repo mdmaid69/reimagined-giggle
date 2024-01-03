ac333df54a14a8bfc5fa44339f535ea68b66ee88
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:44:59 +0000

    laugh down community contrast lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
