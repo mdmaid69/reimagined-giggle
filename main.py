848b931852a2365876fdbf7be63ef4555c8f9804
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:27:46 +0000

    there congratulate health sort winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
