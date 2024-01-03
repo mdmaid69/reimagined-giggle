bc272fa89d7c5951a11747008418652ca7d8b2f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:01:10 +0000

    confuse fortunate then emotion comment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def square_number(x):
-        return x**2
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
