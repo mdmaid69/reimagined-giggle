202f94ad80b7bae9995a1e0e003771e493deda5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:32:06 +0000

    month maintain back turnaway send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
 def extend_array(array, iterable):
         array.extend(iterable)
