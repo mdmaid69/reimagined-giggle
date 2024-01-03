8c429a4636a5e0b195c45500ec471f73bdc6a27d
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:35:01 +0000

    pick partner present bridge neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
