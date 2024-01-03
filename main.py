f0642079e74e5eff55043430e6a6c84ed4ba0c52
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:54:19 +0000

    exploration funny area start trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_str(array):
+        return str(array)
