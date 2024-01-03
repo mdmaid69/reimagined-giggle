599f666449d0f749591b0cb782b27873b8261cfa
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:25:14 +0000

    shoot language wherever only nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
