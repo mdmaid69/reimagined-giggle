2ffd1975ee972347b1d8e5d7d329dae6d4c52729
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:15:35 +0000

    improvement breakfast balance fire bill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def square_number(x):
-        return x**2
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
