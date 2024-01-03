38f5c53334dead861255eab47b24a5c29fff09e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:43:18 +0000

    Ss best borrow now breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
