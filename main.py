6088f4b3be91a28b9891ea9a6fc5b1d5b36dcc2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:29:42 +0000

    slice behind wave front bringback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def reverse_list(lst):
+        return lst[::-1]
+def find_min(lst):
+        return min(lst)
