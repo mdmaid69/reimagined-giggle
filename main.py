e9ad440a32c78fe6304074222224c07cf7abb34a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:38:59 +0000

    this method safe tidy getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_list(lst):
-        return sorted(lst)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
