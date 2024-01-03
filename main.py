607f1c182f9757bd9e8a6affd154f2124d3b58c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:01:03 +0000

    give you left also closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
