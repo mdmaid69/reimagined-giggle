c5a3e99a8874bd2433e1edae2a4bf0c9c539c0cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:45:56 +0000

    receive chin shallow failure challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-print(sys.version)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
