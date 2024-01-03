5e5abf39235d2abd50f3959f93b21e5dc5b2798d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:26:07 +0000

    rub string skirt week standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
