a74ae10c76c79dd38b8d289816590c06255355ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:20 +0000

    gentle to knock charge lucky

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+print(random.randint(0, 100))
