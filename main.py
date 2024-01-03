fd6f028c540f8e661b6e0de4cbe9c72ce5ad938b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:07:50 +0000

    tune convince only area clothes

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def clear_array(array):
+        array *= 0
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
