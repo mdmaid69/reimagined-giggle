4a296106b28eeb7fe97d7245590b6050ef3a4f29
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:20:42 +0000

    real keep contact shadow opinion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
