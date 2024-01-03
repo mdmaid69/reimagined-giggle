6cf26d9a1fa41340f43ac9d719236d9a5d63122c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:53:34 +0000

    unable marry view along think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def subtract_numbers(x, y):
+        return x - y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
