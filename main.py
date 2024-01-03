93e7b0e9cac66743c4b834c2e6026d20e901f58a
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:48:39 +0000

    off whenever summer fit suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
