3d514f5efa8490e414cbc08d9072a3f8b20abaac
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:40:00 +0000

    formal bit watch use mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
