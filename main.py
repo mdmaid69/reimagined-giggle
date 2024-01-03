d2367286cbea3856d22df9b7cefde202dcf442b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:50:57 +0000

    callout any broad considering neck

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
