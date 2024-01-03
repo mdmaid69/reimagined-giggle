dbed09888e165e58631cee8e2a9c67583d99a181
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:19:02 +0000

    whenever build represent high plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
