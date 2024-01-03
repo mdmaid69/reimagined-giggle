76021f60bd623145269bfcb54bc1f8408d05b7ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:24:58 +0000

    gentle load police improve whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def add_numbers(a, b):
+        return a + b
