bc657b479c294f9fd57a16a7b70e90f55da4e839
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:04:05 +0000

    comeoff joke ordinary giveup mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def square_number(x):
-        return x**2
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
