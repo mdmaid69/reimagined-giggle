dca561753fe7ee312f96a2a6cd3f5c7c843445e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:57:47 +0000

    sorry giveback match hot plan

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def convert_to_hex(n):
+        return hex(n)
