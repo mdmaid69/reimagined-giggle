bf89f1929ce05e7dbbaec1c2782bba55954efd4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:10:00 +0000

    sight commit never ashamed perfect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
