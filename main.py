05156b5aaffe4bbca189c035151237f98bf5deaa
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:34:39 +0000

    slice clearly kick closed thing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import csv
   def read_csv_file(file_name):
         with open(file_name, "r") as file:
           return list(csv.reader(file))
-import array
-def get_array_as_str(array):
-        return str(array)
