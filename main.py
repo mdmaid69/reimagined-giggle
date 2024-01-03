e96785aa3254a2b6e99558a2e818153d7a58d78d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:25:34 +0000

    law sit several hold rest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
+  def reverse_list(lst):
+        return lst[::-1]
