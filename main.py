92afbe5da1bb82a8c21cdf009f34ce7f03f022b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:17:40 +0000

    illness start nervous takeoff wing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
+  def convert_to_binary(n):
+        return bin(n)
