0e7a219a6ead233f03ca6bfaff9ec25490679967
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:43:10 +0000

    fly visit union bringdown false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
