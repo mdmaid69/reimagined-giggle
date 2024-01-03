96e5a27589c25f75ed6ca3cb787599f0ac9e1352
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:17:07 +0000

    water mood listen around wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
