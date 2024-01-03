99de828201a031b36484a0af66fc29a953dd25df
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:19:00 +0000

    I minute sister uponon line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
