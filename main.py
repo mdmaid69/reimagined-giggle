ae63ca0b33b8d8d8064a6e88ce280e9d67566537
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:18:11 +0000

    send pipe whoever holdup last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
