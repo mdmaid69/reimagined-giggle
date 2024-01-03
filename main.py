9d30b86e2120ec1cd39d4430cddceb6f332c55fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:35:27 +0000

    Cc consider goup will gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
