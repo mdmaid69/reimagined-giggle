49d5ed03bf1422ca6bdd611dc109b6f9eeefbb00
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:49:20 +0000

    tune independent pronunciation reasonable book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
