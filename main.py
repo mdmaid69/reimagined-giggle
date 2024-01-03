3b79af03e4d0fd7c7028c0c9fce87cb3bac114f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:11:27 +0000

    fight why lose occur top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
