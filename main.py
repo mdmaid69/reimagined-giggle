e4d08436eb907de9438ca42dc59d1446e00b0e88
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:54:28 +0000

    poison fear our money spring

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
