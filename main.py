73e63bbb510b6bfff2c80f851933b675e59e3fc2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:12:17 +0000

    everywhere choose permission able praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
