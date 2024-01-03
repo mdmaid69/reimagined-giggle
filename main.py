c984bdad78893c428a4bbe42ad54e31ecfbfc382
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:11:35 +0000

    test middle dear tap once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
