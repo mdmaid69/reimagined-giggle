8dfd3749806ebdba74002fa1b5eca21a7279a50e
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:30:13 +0000

    usual confident remove allow introduction

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
