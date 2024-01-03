6cef4282fdaa880da40304d1e50ebe03db39dfa5
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:50:58 +0000

    teach bother mark especially lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
