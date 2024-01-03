90c94be5efb731c5f66fd26d080c577439e574e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:05:04 +0000

    difficulty free ignore bring fortunately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_array_to_list(array):
+        return array.tolist()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
