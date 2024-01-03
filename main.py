3f04b8ce33971417e517722e33005b8058bc2c10
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:36:11 +0000

    through wash few treat criticism

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import platform
+def get_os_info():
+        return platform.uname()
