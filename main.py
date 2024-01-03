918c2d60c58558a743187ffdcb474b8afd686dfd
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:18:51 +0000

    smooth suckme tax Dd change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import time
+def get_current_time():
+        return time.time()
