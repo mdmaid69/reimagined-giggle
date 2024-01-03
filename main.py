5d19b49bdcd53385ca9294d7a85be0df2c857124
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:16:48 +0000

    speak bridge refuse object none

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
