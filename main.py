a1bbacafaf7a5c26036d132a72561988fcab159e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:52:49 +0000

    battery reply total cost friend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+name = "Python"
+print("Hello,", name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
