0ff9439844f71060cf56317e2324fabdf71c3549
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:29:48 +0000

    ring threaten cut stare sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
