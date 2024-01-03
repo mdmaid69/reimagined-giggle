358ec049b9a1a3e8ecc71768db76e71b300bfff7
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:28:10 +0000

    glad minimum tonight loud wind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def multiply_numbers(x, y):
+        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
