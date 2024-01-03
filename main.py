8529d3a1a62ab37ee71a93cdb3868890af08d999
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:57 +0000

    visit if night improvement tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
