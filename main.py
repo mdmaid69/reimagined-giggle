e7cfb42bcdac595cb6d127163ec6ddec9025b19c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:50:00 +0000

    system responsible precise pure right

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
