126608626fd2bc74c95a86b483669cd1fe55d9c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:21:19 +0000

    them public extra temperature switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
