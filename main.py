97e14e32ccd6390692d3a5757fe94f1bf8899a7b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:42:43 +0000

    point offer appear check pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
