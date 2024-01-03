73d93390b16f277e64d470d6f04249704942b231
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:33:48 +0000

    sale sign description rise neither

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
