95cd70fc9197fe1a3a6a519e9f1057c47221230f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:50:13 +0000

    peace total broad until hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
