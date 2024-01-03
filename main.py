2876ec5c7040a5827ce93967deffc3f01b014145
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:37:20 +0000

    full break age weak brush

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
