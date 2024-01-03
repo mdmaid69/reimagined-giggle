54377963e38633bda642cd82989ae38f0cfca31e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:18:18 +0000

    song scream hope fairly fly

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_set(array):
+        return set(array)
