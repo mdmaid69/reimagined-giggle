abdcbb19383abf949276a09073d91aeecbb6fc69
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:21:40 +0000

    get snow emergency may Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
