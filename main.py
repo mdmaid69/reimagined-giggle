1c384eb2d50d80373049d53ed263ea0f964d0c94
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:45:40 +0000

    tall error whatever wave breakfast

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def convert_to_octal(n):
+        return oct(n)
