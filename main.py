1e48f3be8b792b5ea36ab4a431618ec0e513e613
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:31:07 +0000

    pen start considering afford load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
