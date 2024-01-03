f1955df3e276c226c8edb531f89f331ac329cdd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:30:01 +0000

    week entrance thick bottom character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
