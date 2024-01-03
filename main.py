310769e03ba71c55daa3c66dd627c8277a45a85b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:35:19 +0000

    apologizeapologise ill behave clean criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
