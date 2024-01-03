aced8305635dd87b38728c3160377adffc087d76
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:15:39 +0000

    beginning really sound journey stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
