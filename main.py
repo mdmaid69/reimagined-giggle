5d14f04d125ac782ef856408565e4f19a620ce97
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:34:02 +0000

    break confuse extra takeout runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_min(lst):
-        return min(lst)
+  def delete_file(file_name):
+        os.remove(file_name)
