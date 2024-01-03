08e97faf1b78db499dc2d71381256803d7b2a8c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:10:41 +0000

    here sign breakdown work regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
