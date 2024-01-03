776246ee7c579aa2c50356f4d3a3450e01b073b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:07:03 +0000

    flavour organization Rr care bringback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
