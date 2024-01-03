67ee316e38c1e297226080c3498ad68a58e0e3ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:30:21 +0000

    likely later create cheek order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import sys
+def exit_program():
+        sys.exit()
+def calculate_acceleration(speed, time):
+        return speed / time
