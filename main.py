0c74c7f2e287a32ae69f27fdd2be1c905c2cd84d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:20:04 +0000

    nigga imagination mail convincing description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
