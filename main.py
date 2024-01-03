83fc17c4b446124c01878f1bce8efe0214f272db
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:09:26 +0000

    walk sensible low passage apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print(sum(range(10)))
