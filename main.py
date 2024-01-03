5ae069fcbf47874bf693ce2e08982359dec317b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:48:06 +0000

    stiff skill remark Zz holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
