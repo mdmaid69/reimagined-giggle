a45979ecf94d2f74e2d4b7ff20545fe14125f0c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:39:33 +0000

    arrange to married damage quantity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def divide_numbers(x, y):
+        return x / y
