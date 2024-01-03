b9f2a8775fcb2599056fd30a91e55463b67fdc0d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:55 +0000

    do wind accidental touch already

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
