1ecfa9199c2327f23c81c5e3a5509dc06332542d
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:36:02 +0000

    story turninto level suggest wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
