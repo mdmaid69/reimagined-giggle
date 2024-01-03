9b8faf52a1885d79d5637314770610dc1ef8aecc
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:04:06 +0000

    board mend grand build identify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
