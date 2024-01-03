cb8c30d597fabf9c9aa89e5a6f5ca35583f36134
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:14:39 +0000

    thumb string show breakup aware

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
