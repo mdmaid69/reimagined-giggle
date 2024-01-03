a89d49f73677a304a744df76ec2a055daa8b5f2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:48:06 +0000

    apart cruel flight science desperate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
