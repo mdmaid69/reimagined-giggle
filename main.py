e41f050730706e05711e55fc41cc226ff0403365
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:35:04 +0000

    instruction zero extraordinary worried house

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def get_array_typecode(array):
+        return array.typecode
