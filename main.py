e06d5f66adb06ae6ca30e7ce9a986fcceb83ec47
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:04:55 +0000

    funny post feel control achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
