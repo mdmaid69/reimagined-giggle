4356a1efb8eb203e6b70d780872be9b5843154cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:20:17 +0000

    name workupto table steep Ww

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
