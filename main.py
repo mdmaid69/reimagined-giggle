d0b78bad384eedda659c7b8541b250b81c325959
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:25:04 +0000

    substance school exciting life for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
