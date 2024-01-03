36eb9cb10c2ee440072937995d6ad1ee7eaa26d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:06:33 +0000

    getback danger bush money above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
