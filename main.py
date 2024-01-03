063dce0a6eb36ae0391efc83087f1acbb94a98ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:04:11 +0000

    chase shock impressive sure satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
+import os
+def remove_directory(path):
+        os.rmdir(path)
