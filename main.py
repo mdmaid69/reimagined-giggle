e000c6f75b5f0775ba54d8696a9a4bf73b14aa38
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:40:20 +0000

    approve busy move rate across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def divide_numbers(x, y):
-        return x / y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
