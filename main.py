3a0b40c4b7a3986f2fe94245f07721e55ed3409e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:43:02 +0000

    wave stupid dark rub react

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
