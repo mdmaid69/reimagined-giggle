3d610b50971283996b4d0eac4cf1d04f77781eac
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:51:39 +0000

    so keepaway slice previous finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
