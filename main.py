593784d8fe21361570368cd15c5945aca6bb8832
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:26:43 +0000

    thing desperate programme comedown carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import collections
-def create_counter():
-        return collections.Counter()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
