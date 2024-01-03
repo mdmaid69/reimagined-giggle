11a17146ee54cea31f0e80045eaf3aa729fef106
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:42:19 +0000

    performance question clean regard confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
