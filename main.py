c7974f9919b3eaac2a6090344ccce509ac30327a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:05 +0000

    pull count general hold maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
