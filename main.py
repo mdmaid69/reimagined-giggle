82f67cd2799d57e9b85878284fc27189c6a5857b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:36:41 +0000

    praise involve rubbish noone off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
