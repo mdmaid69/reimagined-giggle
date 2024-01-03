a64b5483d8f82069fa322f5be1dd71351c69ae57
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:21:27 +0000

    have turnout march card know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def is_odd(n):
+        return n % 2 != 0
