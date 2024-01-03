bb6c28988e3adee23f6b04e35c66e494a34a8ab0
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:24:54 +0000

    content similar valuable lot water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
