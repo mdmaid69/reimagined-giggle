2bbbc92bb41a6325fb94c1b699f139e06d6b4dce
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:07:44 +0000

    Ee morning between unfair report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_volume(length, width, height):
+        return length * width * height
