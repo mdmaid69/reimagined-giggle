d9204748f18c18111d8c00363e8f94c081cfede5
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:46:13 +0000

    impatient store believe acceptable intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
