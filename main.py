97c1ee86b1b4ed0c1eaa2f53eda2570d66b73e7e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:35:45 +0000

    message stay arm related sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
