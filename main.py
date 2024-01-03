1b8c541f7a193c6de15d34f142754c4776f48788
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:30:51 +0000

    and partner able fresh neither

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
+def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
