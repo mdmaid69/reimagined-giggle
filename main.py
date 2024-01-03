d2270d3423cd6363463daec76ee4841949815c39
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:47 +0000

    together fear goback(to) art only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
