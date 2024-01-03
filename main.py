84ab35a742e0f05a1922198979c9940831d81f8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:47:23 +0000

    somebody right simple rate uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
