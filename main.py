cfae3668c844d358b15963b12e4aadfd704f7fb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:42:39 +0000

    reply film fact runinto ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
