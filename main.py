f16f455a2bce927111fd998758a321e6b5055e07
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:07:44 +0000

    interrupt cool for common week

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
