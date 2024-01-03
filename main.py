56eac5b7ea6c80af697ada1f1f8d8613194ce7c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:15:23 +0000

    regular proud Ss we joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
