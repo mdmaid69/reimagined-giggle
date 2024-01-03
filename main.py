dc1928c7230c9ed9d784b6ce1e08a22111d80bcd
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:51 +0000

    laugh law none pretty respect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
