75633652b46d8e3d97bea22cf4482d2f5a9b1d23
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:04:09 +0000

    mind join drink question respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
