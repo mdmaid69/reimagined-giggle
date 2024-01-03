01480ec97a48729ea994b322243e79d36c1fa77c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:22:34 +0000

    hole morning drive limit evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
