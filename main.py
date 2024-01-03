aa27a50b38f7d44e94d400e6635431a5caa7fa2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:16:34 +0000

    recent true point something demand

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
