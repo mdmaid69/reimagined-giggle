1c9d7342ad582566dcec780573044ea448eea470
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:09:25 +0000

    belief gradually thanks jump work

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_floor(x):
+        return math.floor(x)
