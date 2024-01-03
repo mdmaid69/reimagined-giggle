5725dd03f9708b8d73698297ca46ed99d4da1cbb
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:28:44 +0000

    open slow level praise imagine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
