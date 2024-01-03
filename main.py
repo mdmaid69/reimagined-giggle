af57a2bc658141b41a7343cead5ed78da60a365e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:56:33 +0000

    exaggerate Qq fetch surprising inside

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
