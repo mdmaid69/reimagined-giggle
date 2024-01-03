0e3df4efb1efaa87fb348561f9efd86cae9e4e59
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:10:37 +0000

    fit mistake single flat cook

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
