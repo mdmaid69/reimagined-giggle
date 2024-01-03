56e89905fbdc81021b547e980b309b1630ea6af6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:15:04 +0000

    OKorokay clear blow for promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_cosine(x):
+        return math.cos(x)
