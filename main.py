3785c31eb44f074141636d4d42e3337df7d4b9ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:44:46 +0000

    room fairly name intelligent loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
