07f56fa368ffbddf655d559a8d4939d6e27d8661
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:35:52 +0000

    point cross so fight pleasant

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
