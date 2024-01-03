af4e82af30793d2bb4f49030b7f73676cad1df44
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:48:51 +0000

    peace tight bread instrument artificial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
