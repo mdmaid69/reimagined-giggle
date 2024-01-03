bc6a39db159b03a172bb38451f5808c804305486
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:56:53 +0000

    how head morning will mark

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import sys
-print(sys.version)
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_power(work, time):
+        return work / time
