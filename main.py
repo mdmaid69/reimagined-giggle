2ed89c2a5d6a32eefaa97ad3350b57881522f536
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:05:21 +0000

    kill also improve doubt case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(x, y):
-        return x + y
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
