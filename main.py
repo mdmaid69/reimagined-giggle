dcb994dbdf8ac66807a4059f4bc0c2e9e8de4798
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:22 +0000

    spray married field thin woman

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
