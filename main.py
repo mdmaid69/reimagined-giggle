0ccbda22e3e8797cc39fd3e80be497f9add58b31
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:18:17 +0000

    telephone callup situation comeover better

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
