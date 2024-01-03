ff351bb0c6ffb855d6bbd48f9ac8a8b192f7b585
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:42:01 +0000

    draw comfort once juice building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_dict():
+        return collections.UserDict()
