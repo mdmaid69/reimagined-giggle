4aea4fb1e8c593efff285194f2904fd517ee102b
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:25:34 +0000

    live old suckme takeoff nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
