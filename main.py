3918af3a97ecee8903dc70d299497566b064e9a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:02:40 +0000

    remain size preparation free clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
