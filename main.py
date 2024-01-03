e7ea74d2aee5387306b74d062b3d4fe0f2b09277
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:09:38 +0000

    underneath pen action skin excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
