d4d9a23ae2300556ebc45d9271c8cbc8b21e76c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:07:49 +0000

    physical entry sort artificial refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
