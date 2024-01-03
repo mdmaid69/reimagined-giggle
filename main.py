c7fe952300dd193aa91b654077862b089ebf12f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:53:38 +0000

    angry reply another centimetre content

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def add_numbers(x, y):
+        return x + y
