1174d412cfb62547ae7c18e6a6458e1699111030
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:27:51 +0000

    force judge formal friend thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
