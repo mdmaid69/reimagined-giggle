177f82719d5edc20d6146d6667c03354c3fc69e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:04:22 +0000

    worse male complain reserve unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
