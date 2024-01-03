f781a87a4321a5e53432aba5895cd38162387e98
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:10:06 +0000

    win along introduction hope disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_current_working_directory():
+        return os.getcwd()
