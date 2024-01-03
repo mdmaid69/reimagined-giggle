641d056bf3a0507794b79e7d7267cf62cbb539df
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:12:34 +0000

    hang recently achieve very form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
