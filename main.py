3dc243466276490931d66c3838039469fced2e56
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:50:19 +0000

    car reduce visit sort able

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
