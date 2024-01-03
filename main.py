a6c8d189bfe2c539756777ec1a2cca78f69fc204
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:43:34 +0000

    often spill cry before know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
