cb96dbcb569c1b5cbf16dfe6f2afe2fd8838b8ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:27:32 +0000

    whole sing ill tear skin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
