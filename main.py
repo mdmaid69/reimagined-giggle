a665bfce58fa232bc165a4441e3d4a9d37abdc31
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:19:25 +0000

    skin situation famous send meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
