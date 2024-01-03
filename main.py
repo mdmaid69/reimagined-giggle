e17e4388f0fe4dbeb33a3a446bdfaca10ffbf311
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:14:22 +0000

    trip other stretch thinkover plate

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def convert_to_octal(n):
+        return oct(n)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
