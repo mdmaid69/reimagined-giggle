584b8ac1233f95ee4d10d931321d08ab7cc0333d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:54:31 +0000

    order smooth tone away responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
