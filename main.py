b02152fb5404d4d3c278cd763df647502f7585a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:11:18 +0000

    fail affect memory connected crime

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
