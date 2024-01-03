241c8d7da903ab1f077744dd6ac7ea1c5cf5dabf
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:09:13 +0000

    Bb cool peace fact calloff

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def convert_to_binary(n):
-        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
