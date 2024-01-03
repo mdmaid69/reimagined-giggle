6d8d9cf2ac399c2b89770c421108d6b23a18c2df
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:02:28 +0000

    turn contact forgive tone trade

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
