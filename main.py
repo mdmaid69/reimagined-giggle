d8b0367cb9621b304fa22fa5e32c2ac46adbca73
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:43:10 +0000

    build recover concerned value touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
