a388419d930e9e32571a48f8952d6315d84cca73
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:20:52 +0000

    friendly passage suggestion shut waste

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def multiply_numbers(x, y):
+        return x * y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
