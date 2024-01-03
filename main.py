6b2ca3025fe9177fe14a94141a70e40534d35ad5
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:52:10 +0000

    cut lot slowly couple worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
