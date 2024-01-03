85fc3d3a06029b6b3ffb70012a2ffd3b9cdcf126
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:05:08 +0000

    save business branch equal they

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def count_elements(lst):
+        return len(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
