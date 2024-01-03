2459ddf31383cb89a8f29509c6c286ee52b2bcfb
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:42:04 +0000

    slide mention debt blind stay

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
