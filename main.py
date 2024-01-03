8d87fbf189cdb787835003ce47b43904fc55e395
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:18:31 +0000

    anyone goodbye about sure trade

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
