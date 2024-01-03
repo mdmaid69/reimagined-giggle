9f3ad3753ef9865c7acb049d7c2b8624b18e2065
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:43:15 +0000

    but woman empty moment suppose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
