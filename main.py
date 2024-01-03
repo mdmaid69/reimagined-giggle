084d283d5bc8d610f866852cb3a34ac376d8493e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:00:13 +0000

    regional objection free loan turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
