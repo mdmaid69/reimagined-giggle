fd3f49ea5275da4b5f7fb1200a120c609aaac7ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:11:48 +0000

    favourite feature minority sensible slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
