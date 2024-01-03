f7b60ba4dcedcfdd2b1b7f5f38dcbf34d19ddb7d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:14:08 +0000

    already radio address public measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
