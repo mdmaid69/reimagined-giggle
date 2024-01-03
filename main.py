2ba6913c835f8085f1bf35555d17c30831c94fb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:14:31 +0000

    unlucky use usually naturally rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
