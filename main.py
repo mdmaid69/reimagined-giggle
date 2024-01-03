3aa3e284a8d0b7fa7d8cbc667a5ad08f3b583317
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:13:21 +0000

    guess violent return thing vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
