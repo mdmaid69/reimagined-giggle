61a248d37bb8a5848f1ca23b7681cf0b4ecb1ae0
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:25:05 +0000

    like become hole yes advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
