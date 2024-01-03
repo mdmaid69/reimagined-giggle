3ba42fca975bcccd8e9b30688d083dc77dc692c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:26:41 +0000

    for either wipe embarrassing model

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
