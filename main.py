cf28c73bc42f2e8bcbdff34e043c42e1a009102a
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:26:00 +0000

    slice please petrol stage past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
