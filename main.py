f0cb41c43205e7e6cb539e12fea1fc65a1cc20e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:09:16 +0000

    morning skin test later shop

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
