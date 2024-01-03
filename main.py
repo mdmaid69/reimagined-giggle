551dd2e5181feb58c4236b7c4d609ed6fbcdf6b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:49:01 +0000

    single female principle further about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
