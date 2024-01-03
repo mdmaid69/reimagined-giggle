ef5a334a411a1be66f77076995e6d2688ae6666c
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:08:16 +0000

    drop instruction tooth fall dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
