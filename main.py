28ba8921bfe2c058c12e7e47e1cb4049918baec8
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:56:51 +0000

    night sting used now attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
