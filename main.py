8c9d99f0dc40f0b591c5dc72f0abed552e3abd98
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:25:57 +0000

    autumn meanwhile might school strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
