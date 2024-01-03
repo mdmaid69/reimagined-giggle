825aca377ce96467d451589636be89a4969390d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:37:06 +0000

    formal short fall have drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import getpass
+def get_username():
+        return getpass.getuser()
