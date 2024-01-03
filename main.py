7976805f3b069b1f39cfdd0c9550075f2b3b9a02
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:35:49 +0000

    far smile respect collect noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_list_from_array(array):
+        return array.tolist()
