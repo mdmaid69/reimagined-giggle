b4ee75273b38b4c7460d95fed11caca19f42042e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:11:21 +0000

    ring switch unkind aim soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
