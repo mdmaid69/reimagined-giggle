db4fb46d1223526d74ae43d79c40d07b6a6bafde
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:27:22 +0000

    considering yourself separate public lay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
