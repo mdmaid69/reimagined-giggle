7164eee912e1ed13263b8b8b39e06caf4f4e83f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:09:34 +0000

    Oo schedule lookaround reliable drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
