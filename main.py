3c65056fd68f4dabe959159fcf5f81e4ab88482a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:37:58 +0000

    discuss tool children rule concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_length(array):
+        return len(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
