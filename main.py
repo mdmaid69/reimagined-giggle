d286c878a325bb4c22f106ecc24bd715be9b96b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:51:16 +0000

    comfort mix willing exhibition soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
