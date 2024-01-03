14d6ce6f580a478552f3a8f7b12fc43bb07eafc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:24:50 +0000

    challenge earth forget welcome communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
