37c782dae671d132a2a6386600e1873a0001d6f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:49:36 +0000

    what deliver extremely start fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
