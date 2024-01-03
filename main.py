51810497069eb9b07eb75713d24865fbcb403661
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:00:31 +0000

    anywayanyhow represent production colour spare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def sort_list(lst):
-        return sorted(lst)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
