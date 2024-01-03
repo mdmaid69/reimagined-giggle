311f07ed17c460c3c19af77d71099c4317a512f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:18 +0000

    content hold noone rise takeback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
