24168039f403c204edb013fc38e136d88f71d482
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:58:49 +0000

    manage step plastic near fresh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
