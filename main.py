c51fc94e2a7b47b1b26b1cb4258392e4a1fd347b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:58:23 +0000

    intend discovery receive turnoff ready

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def add_numbers(a, b):
-        return a + b
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
