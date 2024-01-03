7e225a50d2236a2efd74018fe48a5d1aabeda474
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:32:51 +0000

    knock visit twist distance left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
