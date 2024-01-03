8be19ca2e25d4a53e31b02a8023ba03a40e9b1bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:34 +0000

    cheap can hot whenever cry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
