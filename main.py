8a3f2f168dbc07037d7ac729d216f020dee643ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:39:41 +0000

    advice sorry match similar concerned

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def divide_numbers(x, y):
+        return x / y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
