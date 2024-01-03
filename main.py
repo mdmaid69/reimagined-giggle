1c93c72315955ce35096a3926b29769b9518f453
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:08:19 +0000

    sink guard produce other problem

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
