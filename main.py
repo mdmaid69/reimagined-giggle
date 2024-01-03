73e71ec65d549731186987625e07ea74359bee71
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:22:09 +0000

    angry recover glad shame detail

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_pressure(force, area):
-        return force / area
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
