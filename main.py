ce3f834aaac99bb1d87facdddfdcf59009a2197e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:33:25 +0000

    control activity future trial shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def subtract_numbers(x, y):
-        return x - y
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def reverse_list(lst):
+        return lst[::-1]
