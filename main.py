620dd8af964692662f4110322aad2ae9ebe799fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:40:42 +0000

    maximum chief wrong under trade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_tuple(array):
+        return tuple(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
