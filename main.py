69f035406dacc179a0e096e18322398a87a91ae2
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:56:38 +0000

    use supply intend fierce dinner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
