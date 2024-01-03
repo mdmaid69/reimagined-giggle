a5a137cbc755b416db6366c1e3ee6b5da1bb4a1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:38:22 +0000

    have spare pass quality needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def reverse_string(s):
-        return s[::-1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
