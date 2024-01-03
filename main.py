5f46a64f150e7e956d35d3df593ae2d96e1ba84b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:12:58 +0000

    thirsty check summer dig deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
