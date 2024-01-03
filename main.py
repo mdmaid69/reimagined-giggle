8cd419eca0d74583244de6d44da4a7243c4f3cb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:47:22 +0000

    reject glad nervous achieve turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
