fd3e8035eb232c16dae937d8fb115600d9389a55
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:53:35 +0000

    lock least gather your still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
