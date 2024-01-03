17df4e2ba29e7a7039867aa161cf45fca1d2af12
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:18:37 +0000

    sell guard perhaps under difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
