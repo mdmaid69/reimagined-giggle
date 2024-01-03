ddeda42219fdab308700cae75e8d3920e0c9c027
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:11:53 +0000

    coffee usually change criticize there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
