c751485b96592878627fb81d07fa67876885501e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:56:50 +0000

    agree worry fall head fashion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def extend_array(array, iterable):
+        array.extend(iterable)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
