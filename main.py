e573dc8a1356e0c421d863d445925783fce4c1c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:06:30 +0000

    skirt Vv trouble imagine amount

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
