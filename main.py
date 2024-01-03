e6801220ecba9539637af37a0282110f2359ebcd
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:46:16 +0000

    spoil share rush abroad blue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_hex(n):
+        return hex(n)
