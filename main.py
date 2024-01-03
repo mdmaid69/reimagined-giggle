f0a7174ae4cf50d81a7df5818fbd47ae5de8e805
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:33:34 +0000

    workon little awake sore on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
