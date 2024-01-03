6e2dfea300ecabf2a14b8a69122098c4b3269837
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:32:01 +0000

    pleasant along plain middle bunch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
