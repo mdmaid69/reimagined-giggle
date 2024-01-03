bede689b7084a2fbc090a18515b98af2a6c9f202
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:44:59 +0000

    estimate perfect Hh noisy lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
