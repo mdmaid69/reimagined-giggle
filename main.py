40fe9be189d27eaa94066854c9b4b573752b5835
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:37:02 +0000

    gentle silent interview cut letter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_max(numbers):
+        return max(numbers)
+def greet(name):
+        print(f"Hello, {name}!")
