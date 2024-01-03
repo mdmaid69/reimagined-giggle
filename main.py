fd50bae397d233ffd6a20d81ef55788bc456b460
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:01:18 +0000

    bang catch phone disagree case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
