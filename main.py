e4f6b734e1b4fdefe4580046bd595ae9aabb6379
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:29:11 +0000

    contents concentrate scream nearly abroad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  def sort_list(lst):
+        return sorted(lst)
