bf1b48bd345453294733b4fbc20ffdd0d6d8a607
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:59:56 +0000

    hurry callout century whatever pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
