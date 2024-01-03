2d9b04668377253deffac4d9239372e397e23c2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:28:13 +0000

    emphasize poor sleep pipe dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
