ff60ce691e0db92a8f70972ce3b15cde094bcafc
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:49:41 +0000

    usually point back complicated instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
