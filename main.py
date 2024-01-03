c439afee423c4e54f3f6baa8c5773809e9578a19
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:22:42 +0000

    suffer bottom short generous press

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
