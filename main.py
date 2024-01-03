9f1925c594a08dcf770ebc47ced149f91b6109fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:43:07 +0000

    vote fix press mine comeout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
