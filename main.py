0461dadf696cc91fab0d078eefe6ea1affe808be
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:15:58 +0000

    almost complete confused responsible need

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
