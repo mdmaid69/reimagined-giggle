b0e39ef9bd90d94b57e09677e26d04abf7ee3e77
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:42:45 +0000

    carry breakdown shade side 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
