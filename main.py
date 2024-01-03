cfaa656d713301e17fe9bae0db0ca29b9a06c039
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:07:31 +0000

    breakup pot what indeed door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
