753fd4dcd11daf4730a32d2dcf8cd97cf6bf30af
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:00:36 +0000

    long please gas meanwhile sport

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
