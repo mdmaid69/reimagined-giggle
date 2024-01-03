c4b773be525f55549f3ff7fb5b1583d04302e4e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:59:55 +0000

    cover prefer sorry slowly 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
