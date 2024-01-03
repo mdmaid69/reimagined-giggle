3f70fa7b89c76cbe7704210e400f9dcc840acc6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:56:45 +0000

    blame show congratulate information world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sys
+def print_python_version():
+        print(sys.version)
