46361db9c649e2adf82509d71715d142da63efe4
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:47:06 +0000

    may most detail confuse squeeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
