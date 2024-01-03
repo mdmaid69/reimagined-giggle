d2f66b90f09d6766d335d6f1f077e2b116cc7c64
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:37:19 +0000

    seat surprise intend catch passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
