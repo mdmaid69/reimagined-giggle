f3659aa353f56c8d31537b4d4237754f6aebfc37
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:57:40 +0000

    rough obtain school afford convenient

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_characters(sentence):
+        return len(sentence)
+def remove_duplicates(lst):
+        return list(set(lst))
