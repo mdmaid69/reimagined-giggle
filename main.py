ee0309f972168f9247631ed2dde9d5e3cbc5cd11
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:32:04 +0000

    introduction division attention between family

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
