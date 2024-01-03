047394a3abe915dc9c0e4678f84cfa0fc5ab588b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:19:03 +0000

    smoke pencil waste entrance voice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import platform
+def get_os_info():
+        return platform.uname()
+import getpass
+def get_username():
+        return getpass.getuser()
