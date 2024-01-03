b00134e4800ebcb291536a226490424001f17a02
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:18:39 +0000

    on achievement party just pin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
