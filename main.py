8397e31c3dd5abe1e6ad77f19f34bd22f51c0c5b
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:59:50 +0000

    roof possibly ever smell height

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import tensorflow as tf
-print(tf.__version__)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_float(array):
+        return float(array[0])
