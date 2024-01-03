8231c64eb08816642472e8cb4fa4c561aa1d695a
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:38:36 +0000

    building supply show measure moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
