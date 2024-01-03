e20e4ae357fd3777bd6f024b24ce34eda5aadcec
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:18:37 +0000

    even fun ticket exaggerate Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
