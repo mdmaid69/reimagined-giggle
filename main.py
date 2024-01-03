a37dd725e13f97ebd218358a455f947cb5166144
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:39:29 +0000

    direct family curious communicate may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
