1ecb91466af356afb649fad08394ff0911d3bb3b
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:40:27 +0000

    command connect central change trip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
