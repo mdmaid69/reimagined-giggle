480e667ce62cdfc683b9225f896b0e54c5a394cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:30:17 +0000

    day pool announce in false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
