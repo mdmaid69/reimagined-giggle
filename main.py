6e5e82b219c8a6fde3afb6b9931c1881ce82ae5a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:22:01 +0000

    within consult rest advantage organize

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
