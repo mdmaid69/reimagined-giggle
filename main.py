a78782435728f66bf3961a40df48b221a53f2870
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:29:38 +0000

    thumb advice sign spoil sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
