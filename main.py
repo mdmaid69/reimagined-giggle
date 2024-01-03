7983714f959b63e8ccf682a05d0ac606d15d55ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:04:40 +0000

    fight steam mark border anybody

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_ordered_dict():
+        return collections.OrderedDict()
