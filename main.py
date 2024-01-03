b9d43868146159beb08bdee7ec252852d12942a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:26:23 +0000

    fetch calm right throw seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
