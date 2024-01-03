96ce733397502893006ab87ff0c0019b912ae5c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:49:53 +0000

    morning cruelty bill expensive chase

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(numbers):
+        return max(numbers)
