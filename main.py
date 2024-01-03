4de5ee5abe876ccbeaf5b56c9c7675ac9a7efbaf
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:00:54 +0000

    backwards event light contrast but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
