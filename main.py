a0e1ad6c749809f39b7fd5a4b2280a99b072f613
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:36:24 +0000

    such range reserve copy rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
