c31b9c3371401afca904605f68a5b3b7c54e1b89
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:11:49 +0000

    claim dress clear head urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
