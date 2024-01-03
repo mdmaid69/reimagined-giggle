5461a6bc1d12e9a1ba29147919448b4d9e4418e5
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:37:18 +0000

    blow project left trick with

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
