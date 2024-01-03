0194ef797628490098c30d4dba444ca29e8386af
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:47:16 +0000

    effect finger before payment without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+for i in range(5):
+        print(i)
