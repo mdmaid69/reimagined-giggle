e4c187c4a112f375f1d5c497fd8a12cb6316a43c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:10:31 +0000

    figure guide connect goodbye usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
