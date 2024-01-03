73f2bb833905354fcdbcde04ea5ada5e18844af0
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:24:44 +0000

    scrap all concerned neither unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
