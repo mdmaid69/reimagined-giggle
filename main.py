860f84f0ebd99cbd235759f3843eca254f4c2dfd
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:46:41 +0000

    trick address stop stiff Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
