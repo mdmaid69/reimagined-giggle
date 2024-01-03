7c5c956a4e0688d0a8106d1493b71c3b39ce1c55
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:51:49 +0000

    period upwards centimetre government today

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+print(re.match("h.*o", "hello world"))
