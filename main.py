fb953aaf8cd0b4d22c96cc706da526245aec3390
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:14:14 +0000

    wise mistake behind valuable sign

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_array_as_float(array):
+        return float(array[0])
