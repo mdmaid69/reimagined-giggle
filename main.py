ff8e99aff1f58d3f07af35fcc666b656199cc2ae
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:39:37 +0000

    hole attempt 573entertainment hurt communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+print(json.dumps({"name": "John", "age": 30}))
