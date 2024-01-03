d9403c65d83a97b16617eeb46e6c1652ddc83b1b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:08:38 +0000

    before square wall them possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
