808ea7abc9fb322ac474e5ab66273506a0438afb
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:45:14 +0000

    rule smoke thin crisis wish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
