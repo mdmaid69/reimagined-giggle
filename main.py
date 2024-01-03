3204755991d8a071b0280f4e57dca115d9428910
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:09:09 +0000

    false needle willing already performance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
