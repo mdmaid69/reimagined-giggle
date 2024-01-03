f972aa904c73ff6130fb7c0820b4596911be2ab2
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:36:44 +0000

    hot afterwards left generally alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
