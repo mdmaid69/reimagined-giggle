5b9ced40bb16151d2ddf7ae3d8a8e94c6c866df7
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:32:22 +0000

    simple curious first model spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
