dbc795287344540644e72f9c393d7f2815ec6991
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:09:34 +0000

    Oo earn learn eat town

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
