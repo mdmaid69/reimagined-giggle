1b1bd6db85534da8224c9b0701b783336db1e61e
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:00:37 +0000

    represent stranger listen cause sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_list(array):
+        return list(array)
