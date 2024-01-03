46002b49f208a38edfb378ee5d09a85f3f5c2f2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:07:32 +0000

    slice ago bath claim dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_bytes_from_array(array):
+        return array.tobytes()
