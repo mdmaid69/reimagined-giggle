3aaeddd9a0b1b3db29a00399ee31c3b4c9751bf1
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:21:22 +0000

    change temper love idea perform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+print(sys.version)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
