93a04afe941b1f74a10afbae13dde784c365f30b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:26:03 +0000

    skin object lesson reject uncertain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
