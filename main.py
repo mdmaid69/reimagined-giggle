5420b1b1307cb747e69ea5085c00ad7705fad836
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:53:36 +0000

    hold edge land scene flood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
