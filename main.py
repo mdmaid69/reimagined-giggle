8d137d44e35c7b755b12645dd53d16d9d6c9569a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:51 +0000

    right basic grow tall determined

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_density(mass, volume):
+        return mass / volume
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
