634f71b24dc6d0f72d04cda8997b876af1a884a7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:38:20 +0000

    suggestion office spare card shoot

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+def get_time_since_epoch():
+        return time.time()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
