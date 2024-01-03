8781d73d6da75d645b98c5520e1f8f7e40af29c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:09:49 +0000

    breakout claim like suggest expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
