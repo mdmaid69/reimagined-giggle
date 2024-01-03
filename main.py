e00d9be0efc917c889508d541f6baabf18d3c084
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:57:50 +0000

    talkinto connection lunch threaten disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
