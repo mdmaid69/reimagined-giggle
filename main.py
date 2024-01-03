61acdb7a3fdd2d478775473b7299df7a226ab4b4
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:58 +0000

    swing dinner attempt probably front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
