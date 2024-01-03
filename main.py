bbc380ce05d6981ab2e0207fb3b71819e195b9cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:45:39 +0000

    dear this yet thing purpose

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def square_number(x):
+        return x**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
