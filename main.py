bc3d4fb60c424edef279d12d7b762a79abf9327a
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:34:43 +0000

    OKorokay bill century city cover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
