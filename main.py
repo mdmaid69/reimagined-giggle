17c26ee1fb1061f516774c02dc8684dcd86e324c
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:07:53 +0000

    beyond ocean best block die

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
