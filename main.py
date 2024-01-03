f7c37141d0634ae8888804cae3af7ee02e5a0c24
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:32:19 +0000

    angry quality consult slight whistle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
