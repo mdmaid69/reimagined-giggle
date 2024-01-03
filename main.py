2918687e3b456346bf6dd06e56e48e4f6c0dbd5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:53:54 +0000

    sore no waste shall slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
