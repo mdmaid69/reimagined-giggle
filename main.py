caa27c2f44119312f3ef66cc6621e1804ff8d780
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:00:18 +0000

    less recently able cinema nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
