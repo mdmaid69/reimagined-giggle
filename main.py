86cda4ca8e78cea9671c73937dfc5df4c79357f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:40:57 +0000

    notice getup weekend activity vision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
