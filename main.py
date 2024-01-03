24734a01b0255a1bdb5adbbd4ae79243fd7c88ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:30:50 +0000

    colour dig book bill ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
