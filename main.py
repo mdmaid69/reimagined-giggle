7726766e589a556bf81a80160db35ef3063f254f
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:51:48 +0000

    form breakfast disapprove correct damage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
