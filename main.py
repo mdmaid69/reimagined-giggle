08c79113f82e6a634810e872b55ef3be1c96f380
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:37:37 +0000

    word call active beneath card

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
