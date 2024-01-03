624ef2d5f829737c779eea913cdda0227078a789
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:13:12 +0000

    less sick check calmly interesting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_acceleration(speed, time):
+        return speed / time
