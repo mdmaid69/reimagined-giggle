62409c9e5fb634d8dbfe788ef16d665c94015dd0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:06 +0000

    journey respect choose side woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
