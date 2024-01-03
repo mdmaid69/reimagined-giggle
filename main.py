4e70b8e9d9651358d0d92b53504fd94b97b32e8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:59:39 +0000

    sink machine promise crash test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
