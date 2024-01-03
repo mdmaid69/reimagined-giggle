7acda463b7f128087571974907f631b4292fd4df
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:33:31 +0000

    aim contain idea duty lesson

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
