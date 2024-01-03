537c47e2b40e48c1c7c38c67798edc5a908d1a26
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:33:38 +0000

    tense task history impression dare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
