9003a67315a9de8bd282b38e4b6af2d5e5a1277e
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:44:33 +0000

    switch mistake part dare make

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
