aa64d22175fb34163b29ca93bc0443c1b2e6eb6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:23:56 +0000

    book fly drop commit wheel

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
