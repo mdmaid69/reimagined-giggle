fa5f360c8cc6df733a0d754d0a9bd1ced3841cad
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:23:56 +0000

    drive drawing term exhibition beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
