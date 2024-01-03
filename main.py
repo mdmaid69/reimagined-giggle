9f152484a41b24a09249ca25d2d455bba8f7283c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:11:06 +0000

    study confusing spare Ll blow

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
