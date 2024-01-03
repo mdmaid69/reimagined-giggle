0427497f6e3c9f4f6f305743804a0f40a9485724
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:12:19 +0000

    crisis damage demand language entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
