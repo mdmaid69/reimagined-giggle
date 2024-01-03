3ce2289bd06082f84b53fa69cee3c4efe9ed66c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:07:56 +0000

    time nose dance cold apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
