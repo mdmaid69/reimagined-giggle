805cfdbfe726738d0e33de2e3476784689b1225a
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:15 +0000

    move loose pair arrival only

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
