e36cfb48f50d4e55fa0deecd445f606a938e9173
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:28:31 +0000

    prepare professional aim rent good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
 def calculate_logarithm_base_2(x):
         return math.log2(x)
