9fafe3139974794527c89c53c3b496b33b800344
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:05:20 +0000

    row similar swing hard use

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def calculate_area_rectangle(l, w):
-        return l * w
+print([x**2 for x in range(10)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
