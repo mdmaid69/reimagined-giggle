4086939616a59c2600c939a4dab840b16c11a1cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:17:18 +0000

    approve threaten claim sell shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
