35e62364d22b6e5473687eca9f6e3a3f6dcaef41
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:49:06 +0000

    sail bet hate between Jj

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-print([x**2 for x in range(10)])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def remove_duplicates(lst):
+        return list(set(lst))
+  def reverse_list(lst):
+        return lst[::-1]
