332c8f4d5ae7ce4fc83a71d42a715f8141ee3d07
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:25:38 +0000

    collect form which spring shoot

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
