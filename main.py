52494b477408c6c0cbecb12ec133e27a4000dee9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:27:05 +0000

    thick consider bag fit mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
