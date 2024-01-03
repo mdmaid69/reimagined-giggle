d0a38ac1e39d46686c4a3a2f436235533541b012
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:44:24 +0000

    reliable delicate pipe check count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import time
-print(time.time())
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
