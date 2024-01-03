3d436c9c7342e5bcc971c61a2f51af80d2869912
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:44:56 +0000

    hope shake sorry excuse peace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
