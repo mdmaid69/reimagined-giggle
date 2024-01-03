81e1e264cd02c92af950816cb38da689673eeda7
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:03:51 +0000

    anyone bring direction direct surprising

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
