c6fcd2495145da3e9c5b5710d7f678f53ab54734
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:58:15 +0000

    tonight anybody nature winter past

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
