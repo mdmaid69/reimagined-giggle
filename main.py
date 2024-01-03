a9c8b7573cc8304e3d60b4af8035b21bc4357fab
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:28:47 +0000

    feel shop brave tight wander

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
