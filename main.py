84915b9dbb13bd8714a651d692f97d13d97f206e
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:52:00 +0000

    murder through event coffee regularly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
