0c477a852608ac1674626e9679243a110a7c304c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:57:23 +0000

    trade popular print service force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
