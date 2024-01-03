cef7a15a57cdd41eb8a17bd45b3e76c5501396f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:21:26 +0000

    cut workupto smart slowly attractive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 def count_elements(lst):
         return len(lst)
