e1767bb3ab9d7995689ae4ba124abb913870d536
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:20:23 +0000

    love get game 572enter tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
