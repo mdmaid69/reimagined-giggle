a84c52901e0c30b402da0147450ad422acde6011
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:25:46 +0000

    unit friendly rather stretch art

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
