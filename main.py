6de708f78e43913bffc152da4aca3966a6e5c946
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:59:18 +0000

    apparently bored certainly summer pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-for i in range(10): print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
