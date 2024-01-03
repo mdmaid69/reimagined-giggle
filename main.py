19bfb8a118e90ba7ff725dab08451c3c4a747288
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:39:10 +0000

    ago imagine factory value wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
