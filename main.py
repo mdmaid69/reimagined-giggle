ca77d60ef2e46cdf7d811ae102add7e63579b600
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:01:56 +0000

    callback boat stranger behave excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
