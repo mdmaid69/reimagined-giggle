2c169ec75579676aa122b556ec653dd4e07077b3
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:28:18 +0000

    refuse argument church wet actual

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_density(mass, volume):
+        return mass / volume
+print("Hello, world!")
