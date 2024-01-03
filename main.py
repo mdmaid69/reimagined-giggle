96dcaf6251c68796aa658fe909fe7670e7ec4232
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:57:45 +0000

    mistake authority bring test blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def append_to_array(array, item):
+        array.append(item)
