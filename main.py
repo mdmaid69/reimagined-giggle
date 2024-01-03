d374ea3e516769dc1bf7d71b32670d4d714412e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:31:53 +0000

    announce advanced face about unless

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
