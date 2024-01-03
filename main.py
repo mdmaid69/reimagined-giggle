ff6e391e9b52e11fcf9c6e87932bbc2a3d0f7eff
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:55:24 +0000

    careful discovery open contact along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def convert_to_binary(n):
-        return bin(n)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
