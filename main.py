430daa21d192b765f075478d1691123302a2688f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:31:57 +0000

    description dream path run blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def check_if_array_contains_item(array, item):
+        return item in array
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
