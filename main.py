6452b0aac45df691556d93a2463dec3a0fede4c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:51:12 +0000

    less exam appearance study takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def convert_to_json(data):
+        return json.dumps(data)
