0cba4a74312b4170da89b672d886ba5af615c614
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:50:21 +0000

    store scrap stamp judge bin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import time
-print(time.time())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_area_rectangle(l, w):
+        return l * w
