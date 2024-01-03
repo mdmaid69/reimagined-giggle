158923fa9fb2615895e9012d01405e6b528e12ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:53:53 +0000

    only arrive nonsense sister separate

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+print("Hello, world!")
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
