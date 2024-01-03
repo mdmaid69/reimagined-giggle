f56d13f26664d319ba0d96f5d4105c00f49b51de
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:46:34 +0000

    concerned responsibility entry turn popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(numbers):
+        return min(numbers)
