5b7426768c4b3e033cf586321b209ce8cf5f59f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:04:44 +0000

    block walk without formal spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
