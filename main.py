fb02268f071145bbb0edd97d995e8e0f45914f50
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:54:37 +0000

    correct thinkover well badly tip

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
