bec0fbb162d429728b5ee5ba582fef48674ef2dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:39:34 +0000

    confused herself lunch prefer thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
