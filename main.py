11351e7ec9c5e70aab8033c45d8ffa3c08dde6ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:28:13 +0000

    little used track goout choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def append_to_array(array, item):
+        array.append(item)
+def square_number(x):
+        return x**2
