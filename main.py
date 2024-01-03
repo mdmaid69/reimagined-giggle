fcdc3df5587e49a5b5d20e4219ee745dced16047
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:21:24 +0000

    corner thing cruelty sky thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
