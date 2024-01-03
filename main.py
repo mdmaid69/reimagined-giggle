2537c51f999f43cd5a89bb3d4b02dfe6d09082d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:01:20 +0000

    computer connection aim book bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def count_characters(sentence):
+        return len(sentence)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
