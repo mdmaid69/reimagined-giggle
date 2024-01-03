1cd34169aa1632dd14fdba7f33773f52c4f56036
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:03:11 +0000

    house important discuss you public

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
