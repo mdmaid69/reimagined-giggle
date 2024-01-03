2a1d259c1b58abca50e87c8d5b90b3eb463b6c18
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:18:05 +0000

    poison produce guard ought lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
