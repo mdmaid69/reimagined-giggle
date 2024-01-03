bf8efff5b2f3ad00be2fc8cd88ed27936f673492
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:09:00 +0000

    giveup apply estimate outside nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
