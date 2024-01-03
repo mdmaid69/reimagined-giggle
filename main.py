125cb5a9a1ae11f13da68cf3aed95933b288c833
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:52:17 +0000

    behave division post accept goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
