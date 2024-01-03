3d57f7244b30dd7d3e02d393f3b2e522211ba5a3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:04:25 +0000

    thorough nerve recovery arrive contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
