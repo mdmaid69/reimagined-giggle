7c16350fdb15a5a68639f09618a1dca7c4fbec52
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:31 +0000

    way into murder Ss field

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
