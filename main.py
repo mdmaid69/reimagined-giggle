64bcc8a5055c45eecb13f1deab53f7daea77cf34
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:47:53 +0000

    workout sale performance spread dull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
