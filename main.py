a1dae4fa11060bc695ddda81eec31994e1d056c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:44:16 +0000

    be apart save error connected

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def square_number(x):
+        return x**2
