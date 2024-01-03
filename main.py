2b80a1ab4d5d281a682f861cbdc9ac86eb0182f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:03:05 +0000

    mainly water extraordinary journey point

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
