8010c1eea07a9a2918f62f5637cfa14479cc67ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:31:18 +0000

    whom gather early rescue all

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_min(lst):
+        return min(lst)
