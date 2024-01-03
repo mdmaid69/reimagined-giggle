3de0281a2d67e9970aaca0eb145246f3ea32353d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:32:51 +0000

    tax wise off proof Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def subtract_numbers(x, y):
+        return x - y
