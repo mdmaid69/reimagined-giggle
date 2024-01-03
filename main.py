f24290951a7b2fc4d41590908b91a9a58c3223a2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:31:21 +0000

    slice tape however room start

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_power(work, time):
+        return work / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
