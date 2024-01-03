501710e65451df4d86b5fb634ab3d6692f3b473c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:31:40 +0000

    under onto close slice wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
