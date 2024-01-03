b8f5269cc48d20d6649de86993256e24c461be88
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:07:26 +0000

    somewhere powerful next basis regret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
