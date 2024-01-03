590169746616138f4b4f3faf77de99fcaf3252f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:16:21 +0000

    rain shake mean mad language

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def square_number(x):
+        return x**2
