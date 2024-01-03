d4a28fa42a79948b427b83c322743fc87ca0bf58
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:16:51 +0000

    equipment measure Rr tool description

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
