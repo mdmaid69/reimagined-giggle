0ed4286144e2d8b24b4e2619d9cb738909acd9bb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:34:35 +0000

    brush sky spoil public draw

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def convert_to_binary(n):
+        return bin(n)
+print(sum(range(10)))
