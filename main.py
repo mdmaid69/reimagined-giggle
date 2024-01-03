5f0d5bf4388e4aaa170205d44e4b2a2bf176381e
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:42:33 +0000

    pile cost someone cum process

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(10): print(i)
