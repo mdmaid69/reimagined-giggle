3bfe97dd0fba8062b4b71baa4fbcd2ed913cb042
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:38:24 +0000

    light promise hear holdon positive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_typecode(array):
+        return array.typecode
