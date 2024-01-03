4d0ad14dc46b9d122605d133c41124a12a321c38
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:20:11 +0000

    health react however bill guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(lst):
+        return sum(lst) / len(lst)
