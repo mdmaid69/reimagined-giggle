d1cdab637923ed1c2057c59a18aad3bf73e52941
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:03:13 +0000

    winter favourite familiar 179bite slowly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-x = 10
-y = 20
-print("Sum:", x + y)
+  def cube_number(x):
+        return x**3
+def find_max(lst):
+        return max(lst)
