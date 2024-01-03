afe8231bf46c1e95a6f124af73ad45d66b23e1bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:00:19 +0000

    well Jj state satisfactory strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def reverse_array(array):
+        array.reverse()
