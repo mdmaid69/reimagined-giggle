dca4b1652a31c8b8d15f0846710a919a45656097
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:08:42 +0000

    trust Ii create impatient us

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
