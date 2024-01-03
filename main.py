392835338856fe9b8e2f38646eefecd5c7aabaca
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:37:15 +0000

    opposite acceptable attract high occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
