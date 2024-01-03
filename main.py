34565003bf51814f3e1f957a1e5855a5721147cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:32:37 +0000

    mail interested callout score birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
