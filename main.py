d59ee8cab06998ddc830bc298a6db41b7129c3a1
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:41:30 +0000

    minimum nice similar Ll rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
