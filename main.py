40cb507f4c20f48e2f38e71c08d99ff0db323d9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:50:12 +0000

    disappear lead through central Mm

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
