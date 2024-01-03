eb4a85f37ab6cbd5a05842c834f4c46f9668dd44
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:02:14 +0000

    reserve connection play emphasis nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_item(array, i):
+        return array[i]
