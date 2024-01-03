c01184587835d150b729ef90069c1b5a10ecc4c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:45:47 +0000

    common repair soak peace quality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def reverse_string(s):
-        return s[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
