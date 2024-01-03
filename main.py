2e2ab6eef4b53c8b2e5a5e047513463d8586834c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:27:51 +0000

    record vanish emphasis cool beautiful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
