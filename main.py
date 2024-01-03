e474f2af3f7805150aafd0065b5b5b39018db696
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:41:00 +0000

    popular event head loud spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_list(array):
+        return list(array)
