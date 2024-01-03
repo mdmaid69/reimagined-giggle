11f9ed4f6361fb9895abcca4fcd6129f6ce76d27
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:22:42 +0000

    callat back money happen strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
