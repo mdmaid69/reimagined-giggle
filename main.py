f53908f5519c5467edd6b7a1489903e20b858f61
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:29:36 +0000

    callout another determined transparent never

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import getpass
+def get_username():
+        return getpass.getuser()
