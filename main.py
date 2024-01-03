61259766a3014a9572c2fe97e9aeb76ac5dde802
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:00:00 +0000

    food bear better surface hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
