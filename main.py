ff17d859bb3dfb2f69383691cbcd0b66057a50c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:14:33 +0000

    lookup in sharp day clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
