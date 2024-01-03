7665e3f4ce8b88c978f3bab2edd3f34ad853ed1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:44:26 +0000

    shake satisfied harm breakdown print

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
