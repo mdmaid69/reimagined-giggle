b6ad100f1b42647e39214a485b9034822b76fa4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:07:02 +0000

    whenever border stage die suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
