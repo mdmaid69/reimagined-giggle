c606d01c1652d730e852ade424055ca4f24218a6
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:15:57 +0000

    guard thought bread want shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
