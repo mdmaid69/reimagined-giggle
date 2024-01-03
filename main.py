34ffd8920142742aca33ce79c9dd8cf789079d95
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:10:59 +0000

    seem breathe shout go late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
