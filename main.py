2590db1ea010067e691bab1df5c3c86841c397e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:17:27 +0000

    quickly equipment float name well

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
