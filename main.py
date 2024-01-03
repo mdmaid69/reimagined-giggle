eca699b0a7c356a12b3c980ca5cd2161f60ab8c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:44:44 +0000

    pull finish then used fetch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
