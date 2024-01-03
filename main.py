b0b7dc1467b2a3f68ecbf68462656607092dc4ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:50:26 +0000

    land stranger opposite free look

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
