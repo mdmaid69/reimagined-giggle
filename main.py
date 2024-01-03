9d59f497f3fafd501264c525154583b45f9cdd1c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:45:02 +0000

    tool sign we comment tired

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sys
+def exit_program():
+        sys.exit()
