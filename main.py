fe52160028498db4546c579d3fad3bb947b31e59
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:30:35 +0000

    born cook fasten his mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import os
+def remove_directory(path):
+        os.rmdir(path)
