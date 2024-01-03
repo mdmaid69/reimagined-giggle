81a2b92f71a819a53199cc93d212b7d13f4b9f18
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:38:47 +0000

    pocket wherever mend Nn attractive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
