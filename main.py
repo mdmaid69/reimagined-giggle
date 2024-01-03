6e136236362de260244541a4a2fdff575bb6c45b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:44:11 +0000

    language prize cost plan joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
