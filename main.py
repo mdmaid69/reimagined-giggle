7ba5c9438cc22e36b4e8f534e42842acfc77cf33
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:16:25 +0000

    splash bush patience government private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
