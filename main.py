2ed6f8a0ff94c82b642f58bc37ab0a30efe52f33
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:28:42 +0000

    comfortable kill report shut lift

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
+def convert_to_hex(n):
+        return hex(n)
