d2200dfd3a27f700ecd39936e7407bc3bd1fb8fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:57:41 +0000

    calm makeinto empty mistake challenge

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def square_number(x):
-        return x**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
