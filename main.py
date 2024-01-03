ccd9a5ff2194c89ee55f5f426cb35e0106769e45
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:18:31 +0000

    impressed store test emergency further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
