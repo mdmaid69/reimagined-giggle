f665f8d3e642386366b232a4341504ebc894dfea
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:07:56 +0000

    mood cum sea great bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def insert_into_array(array, i, item):
+        array.insert(i, item)
