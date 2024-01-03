ed6031185daa792ac105de1005e9b9279070a2ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:59:00 +0000

    winter comeback cheap comeround post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def is_even(n):
+        return n % 2 == 0
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
