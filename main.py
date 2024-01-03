4bcb8e3100e21dd5609fa28ec08b818e285c7c78
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:48:08 +0000

    path body wave noise contrast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
