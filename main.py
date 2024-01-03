790a9fd1fdaec00bc334e578ef14d8078b84b1f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:37:44 +0000

    borrow emotion back sick hold

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def remove_duplicates(lst):
+        return list(set(lst))
