60cdbcc353e5a970d67662f595967fe50179b3ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:45:06 +0000

    slow let capital uniform date

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import tensorflow as tf
+print(tf.__version__)
+def find_unique_words(sentence):
+        return set(sentence.split())
