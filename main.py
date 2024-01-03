63ef8fb7661721e598b46b0b68864fbee69bc502
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:44:00 +0000

    Ff Pp ring perfect content

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_work(force, distance):
+        return force * distance
+name = "Python"
+print("Hello,", name)
