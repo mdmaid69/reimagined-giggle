203224b3ad284afea310047eddc0916cda7c98c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:48:28 +0000

    stir dig street flat beautiful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
