35e07727a43acdf5e462f44fad397863799edb68
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:12:34 +0000

    odd once lower excuse wonder

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
