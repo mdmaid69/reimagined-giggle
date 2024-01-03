f849531638f80ca46017bb2628b1ea8eb04df73f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:22 +0000

    middle gather even as crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
