570ca41f1f2ace99ef543d84e42e46869f6f4506
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:54:39 +0000

    winter lunch like praise show

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
