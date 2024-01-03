b9515522f39fd545ad8e69f4521cd95598397431
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:12:21 +0000

    gentle top temper class poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def is_even(n):
+        return n % 2 == 0
