0caff0a31e04b0367ecceea195985f61ed3e3686
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:38:01 +0000

    charge wire advise feature there

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
