ef023263a3b50749d30409c52c19b14d4523e739
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:34:37 +0000

    group interruption remove against pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
