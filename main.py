5dfa28e82ad3c6b3b8d5de479dc3e7cf45c2b9d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:27:37 +0000

    before account begin autumn custom

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
