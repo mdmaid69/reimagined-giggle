da47705ea896ae2d337baaa75b6fb9b16b57230a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:18:18 +0000

    apply people someone order bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+print(re.match("h.*o", "hello world"))
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
