e1ed99a4ed7466ea2248175f2acbbc8735b6694e
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:02:12 +0000

    rely dream skill keepfrom disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
