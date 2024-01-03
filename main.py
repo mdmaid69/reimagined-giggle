6ae717a0006da2b4b7195ac8199fbc87bb2ecf75
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:36:57 +0000

    hand unite prevent push neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
