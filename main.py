737a80ebf7d0cdf1d7d98b0d1da7cf73fef3a331
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:08:16 +0000

    where round rise purpose scale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
