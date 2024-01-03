9e8a93c558cf57fb8220e11df1121d54756858f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:37:24 +0000

    careful heavy brief save just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
