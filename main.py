7962668938b678fba446dba7cfc4678515ea6b4f
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:15 +0000

    shopping nature turnout neither friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_current_working_directory():
+        return os.getcwd()
