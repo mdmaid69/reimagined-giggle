4ff85398501a9bf91db52894a4de6781243aa486
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:22:55 +0000

    sort stretch fine pair supply

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
