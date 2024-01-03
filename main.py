34022ea1526d1a778c78d15916934c4b8bd09474
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:36:12 +0000

    strange method pencil term borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_repr(array):
+        return repr(array)
