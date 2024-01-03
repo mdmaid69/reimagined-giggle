0ef335dee64e227151917d4e003a81c62a3c3b61
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:16:30 +0000

    bet acceptable taste plain available

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def add_numbers(x, y):
+        return x + y
+def is_even(n):
+        return n % 2 == 0
