3a594122d82551d72f4b35a3f8815f0f95d4e3fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:52:49 +0000

    little stranger measure 571enough agreement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_speed(distance, time):
-        return distance / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
