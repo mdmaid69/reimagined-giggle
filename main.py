f5b683ff251f2a94aa5506d6e29cd6f1b40b42f2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:53 +0000

    promise relationship declare order trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
