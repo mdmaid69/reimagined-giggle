c0c71a19dc8270aace1b30c6f86079bf9c4563f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:43:41 +0000

    contact whenever club style around

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_speed(distance, time):
-        return distance / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_int(array):
+        return int(array[0])
