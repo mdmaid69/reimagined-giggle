90ca16e99ca0792e28203634d60c455fe3ee0911
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:30:45 +0000

    please glad company majority calloff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
