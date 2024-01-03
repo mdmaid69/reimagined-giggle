8dfe92c9ceef88c7d4070822f2d5ff9e73a25dbe
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:32:31 +0000

    area uncomfortable copy top prize

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
