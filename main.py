5090bd595e980a1bff8987693b85083832cebc15
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:39:16 +0000

    return Rr moment drop kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
