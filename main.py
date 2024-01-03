7c08a8c85a0dddde38b3ac1b514fd2899ead2d0a
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:00:29 +0000

    convenient sound friend sound firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_as_list(array):
-        return list(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
