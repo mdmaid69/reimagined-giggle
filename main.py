e43eec5f402e2b2c792be7d9ba5dad06eafa330d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:02:06 +0000

    share breakthrough sing our load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
