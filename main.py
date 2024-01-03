7d6e04a507c710b063d0d842658a70768a422115
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:14:24 +0000

    delight real of tidy bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
