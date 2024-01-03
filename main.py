ec787860a8d08b34c71eeecd08657c30992ca1c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:12:56 +0000

    could practical take water there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-print(os.getcwd())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
