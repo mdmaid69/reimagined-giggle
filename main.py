68a9831f1ff7e6d2f0ee32442fe92542813f2f23
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:14:14 +0000

    work holdout build cut outside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def count_characters(sentence):
-        return len(sentence)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_floor(x):
+        return math.floor(x)
