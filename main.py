d4254d5a3ca8fcead73fc24ac025b19f5bd351ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:33:58 +0000

    share soon nowhere or want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def is_even(n):
+        return n % 2 == 0
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
