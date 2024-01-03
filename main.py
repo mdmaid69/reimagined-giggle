22b54ef13750bf42ec9babaaf94d4e15110515fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:13:33 +0000

    understand repeat uncertain correct measure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
