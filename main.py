d6eb6523140b912bfb6042b6356edcc07864f13f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:32:53 +0000

    post there adjective keepaway normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
