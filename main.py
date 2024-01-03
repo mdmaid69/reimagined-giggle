0504e7a611ed249530d0d73068a4b2bd363ff269
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:11:05 +0000

    arrival go gate soon noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
