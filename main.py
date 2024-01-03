487be169fb25c56befc803ef3a9b58e6ce14bd05
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:48 +0000

    behind illness keepoff characteristic pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
