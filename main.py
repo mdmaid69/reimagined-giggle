bacac0c80c96655cc4d47fda816a1fecefec1d14
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:43:22 +0000

    container turnup instrument sting foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_current_directory():
+        return os.getcwd()
