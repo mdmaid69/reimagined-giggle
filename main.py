2ba310f3b173be3bc7ea43521f9744ce5f44f4a0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:01:07 +0000

    possibly holiday neither drop suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
