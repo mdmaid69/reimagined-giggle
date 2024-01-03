161b623de30f05f3bae408247a1f6a2b742b3603
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:16:41 +0000

    gooff market lock now escape

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
