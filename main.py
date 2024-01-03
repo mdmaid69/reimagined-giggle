521617925475db6fcd52ec58ad18836508b02dd9
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:29:23 +0000

    fix over phone collection callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
