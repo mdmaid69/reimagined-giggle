8edb0a54662c38fb2cc27b8de1b1d3c623c9bf93
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:09:15 +0000

    trousers warn mine mouth copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
