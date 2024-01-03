d5c7ac5b328a035f6fccbe4814773a4319a445c5
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:20:41 +0000

    explanation giveback spoil interest scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
