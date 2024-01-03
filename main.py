7f6342a71a4af00cfe11f3c79109f6ca4b171878
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:55:42 +0000

    fall cost evening sky partner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
