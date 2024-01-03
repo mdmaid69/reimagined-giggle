6c9b2c2823270c2c67bd266d0d4e8d80b83ca0df
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:23:50 +0000

    definite throughout disapprove do immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
