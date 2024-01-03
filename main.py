a5f04a0523f8c90ae91f72616143d116d52ebb61
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:37:52 +0000

    Oo estimate poison it anywayanyhow

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import http.client
 conn = http.client.HTTPSConnection("www.python.org")
 conn.request("GET", "/")
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
