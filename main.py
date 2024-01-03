825902dc2e0daa2fd58552262c3709a2206ef1fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:24:32 +0000

    after opposite positive practise concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
