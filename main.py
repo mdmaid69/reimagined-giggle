f3003de0724885fa7a69faece7fe706f5eb975ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:29:16 +0000

    lend alone generous disapprove than

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-def count_words(sentence):
-        return len(sentence.split())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
