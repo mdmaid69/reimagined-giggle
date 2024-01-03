81c2f905e86d8e2739007792b46c302e110db7e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:35:51 +0000

    its risk though plenty all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
