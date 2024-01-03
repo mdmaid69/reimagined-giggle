c6483ea9ca4575925623472210c6c99c287650e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:09:45 +0000

    operation wind move admire reply

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
