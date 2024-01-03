55c2ce9aa93dfd109875cc8f670efe73c124bf95
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:15:41 +0000

    criticize waste use nice go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
