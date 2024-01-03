5ce8a879d70ff5926b4acad9aeddf8db004d3cc1
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:32:47 +0000

    operate majority confident exploration do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
