ccc66139c783bee1fc4af2e15bbee0e1d197fde5
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:13:03 +0000

    bin control skirt sink left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
