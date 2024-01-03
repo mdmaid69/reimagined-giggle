62e14080d13aeab94fdd5386b1a2468eb8c9c9f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:46:24 +0000

    tie punish light may study

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
