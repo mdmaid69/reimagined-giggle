885dfcd2e6185abe226fe1b2ffadae8d470d5319
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:26:00 +0000

    holdon air terrible considering delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
