2e8d2d2b4187ef74481e08f457a8792c308c3bdc
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:09:36 +0000

    play circumstance block bang Vv

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def is_odd(n):
+        return n % 2 != 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
