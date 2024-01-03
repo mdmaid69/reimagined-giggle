c73444ba1bab4a7fcafc56a05ba3b001e4b7600b
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:35:52 +0000

    bank computer loose sure crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
