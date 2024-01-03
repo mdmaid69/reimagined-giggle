f16a4ac00e12ddbcedf9ba1eab242a87d2dd49ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:37:03 +0000

    pick variety snow contact recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
