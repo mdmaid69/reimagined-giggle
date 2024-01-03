7965addde073f7e2d270d60a370cb4f2eb3682f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:01:12 +0000

    live boil turnup prize noise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
