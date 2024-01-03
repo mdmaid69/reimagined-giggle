e9873a3a0ce3526b1a0c3f68c97a44a427ce1e4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:08:46 +0000

    tip truth find bang smart

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
