9db2528dfba3b00b76884c62c3e2fab9e10e7b36
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:12:43 +0000

    wonder whichever noisy guess colour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-for i in range(10): print(i)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
