f75d33b411c6ca25b329bf146b93dddb2ed763b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:22:46 +0000

    switch now kill success detail

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def find_max(numbers):
+        return max(numbers)
+print("Hello, world!")
