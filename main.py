d4bc02548395e000638e2eb1e3fd6dfcf250437b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:43:12 +0000

    trap firm soak start bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
