cb7835b92a8d20e1a03778ccf438946eb3b58b56
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:02:39 +0000

    trial operate straight think part

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def count_elements(lst):
-        return len(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
