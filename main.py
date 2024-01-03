33878208a37b8d894c53ee5bd3684024c57faa7a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:48:03 +0000

    report nature convenient present unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_work(force, distance):
-        return force * distance
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
