6f158af259b0a1c472bc61bdd1ab2b60313c57cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:13:44 +0000

    breakup much contain replace manage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
