e0d75585c9f2560991df034277bb10c27aa934c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:57:37 +0000

    weekend post sad wealth whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
