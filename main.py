a4d5a0d84fad028be37c5d9cb82310aba22a0a40
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:42:47 +0000

    appeal door machine splash issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def is_palindrome(s):
-        return s == s[::-1]
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
