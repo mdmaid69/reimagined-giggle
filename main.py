872bd00232fba4b3cb34676065c44917a793df6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:48:53 +0000

    dish limit programme event fix

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
