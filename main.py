480bf371463222f8db7003aa73b3a1d9ad0f30e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:00:55 +0000

    flat smooth Gg swim surprised

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_unique_words(sentence):
-        return set(sentence.split())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
