d1e726731a89767736670df0f78ef0f8b2ce77ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:45:19 +0000

    loose excuse getoff properly share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
