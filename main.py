78b73ff3c518720cc7e11a46cec2a45fe42198cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:23:43 +0000

    holiday breath loan plastic still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
