a2c5e1e627d8a652faf2859938eb66a36759149d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:01:02 +0000

    pleased common fence not container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
