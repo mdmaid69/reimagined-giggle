4fdcb0dc5ff62fa40ab23398fe9fef31038858ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:19:30 +0000

    beside above behind splash its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import time
+def get_current_time():
+        return time.ctime()
