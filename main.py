dd9ee2510d1c72256a3db5ff670e7ca1bbb74c00
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:53:34 +0000

    begin occasion that deliberately remove

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
