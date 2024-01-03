1bb54b99fecccda7910b6b22557ef2d8e460a139
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:04:04 +0000

    war early violence man occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
