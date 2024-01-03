b24e82111b4c98afd0a8b4b1c1d70523af52c83b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:02:01 +0000

    organize single suffer luck luck

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
