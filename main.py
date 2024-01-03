07ad1c5f58da1ac2a3ef3b9a1492031541fa1b1f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:49:19 +0000

    flavour our against breakout failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
