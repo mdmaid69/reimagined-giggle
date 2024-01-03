26a1186df0da8f1114fd5285f82c720ed1fb88fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:49:39 +0000

    quickly tradition weather floor bottom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
