9ed234de22df35544207c4dd59b480e50c6f6f8a
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:45:29 +0000

    plastic introduce afford for fly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
