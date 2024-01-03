c73288b52bcb623845b9f2e7a42a5f1eba414cfa
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:36:06 +0000

    square firm appeal programme cut

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import datetime
+print(datetime.datetime.now())
