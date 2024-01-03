96e021e48be773da0c59aad92d459b18bfde06ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:50:45 +0000

    borrow only week impressed excuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_list(array):
+        return list(array)
