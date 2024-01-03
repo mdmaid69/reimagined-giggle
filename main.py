d9bbe2ec44f5ce3f8f5a157779fc7e4d25fb45dd
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:55:24 +0000

    568energy rank go completely progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_density(mass, volume):
-        return mass / volume
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
