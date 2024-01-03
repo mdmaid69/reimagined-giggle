3e48e7e43a9105d670dcd863ba6a6da2fa204c7b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:03:16 +0000

    notice hard dull excited anything

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
