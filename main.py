60a65a6530278ece86335b8e3b552f0d34cfbd92
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:05 +0000

    floor yours side away exercise

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-for i in range(10): print(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def reverse_list(lst):
+        return lst[::-1]
