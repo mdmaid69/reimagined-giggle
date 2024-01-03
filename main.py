8ee0a838488424a4b012a95def6fc5bb5e579225
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:14 +0000

    spill cut drop pure full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
