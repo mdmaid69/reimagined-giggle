5a5fca2312b2ea406386c36699c9c426aea8465e
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:53:23 +0000

    sound safety behave branch behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import sys
-def exit_program():
-        sys.exit()
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
