71d67bc10a0739664c257e2697f93fa5048707aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:33:17 +0000

    fear real related dry card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
