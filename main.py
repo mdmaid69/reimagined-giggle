1807af25ddbdfa4f12432dd542d21b03b7de9aea
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:03:41 +0000

    determined forever fall reject tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
