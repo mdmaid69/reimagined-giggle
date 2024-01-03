44917166e7a1cd05b0447ffd6a77641b294e0221
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:53 +0000

    hate area part mess star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
