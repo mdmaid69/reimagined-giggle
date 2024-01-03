1faa1fd1086988bcaa485e20f1c85d6494de4ca2
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:05:46 +0000

    shrink previous godown sensitive even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
