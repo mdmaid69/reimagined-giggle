34266aaea3f433c42defa0cd7f953e8bdf937862
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:41:12 +0000

    possibly sorry serious comeover all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def split_path(path):
+        return os.path.split(path)
