1700c85fbe79c78d298ad13813de3aa66ce00702
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:10:59 +0000

    ring extraordinary closed will interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
