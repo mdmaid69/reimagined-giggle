3f9f1bdc8a48846d196e15d6d4ffa75106b1746d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:34:27 +0000

    argument anybody realize mystery keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
