9621f3b02a922d0ac83637b79b6d8871340ef251
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:13:29 +0000

    bank suggestion she so undo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import array
+def get_array_typecode(array):
+        return array.typecode
+print(sum(range(10)))
