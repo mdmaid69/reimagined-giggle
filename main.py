3a3e7ac7cf885ce0f96e36a41a3a4c3d1f308f80
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:40:27 +0000

    easy unlikely inquire lose support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
