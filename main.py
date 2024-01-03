6b5f2d1304547d56065ed88330f3a3f1552a4b24
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:10:52 +0000

    drive turn Oo weekend scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
