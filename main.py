435f809dcc1ed5221155d3de1d0d6c7ec29e78d4
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:10:14 +0000

    cry coat short project sharp

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
