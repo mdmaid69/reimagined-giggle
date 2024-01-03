7c22704fe4a0c43f34a447186264c8d5f3f07b6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:24:01 +0000

    touch yet curious travel suffer

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
