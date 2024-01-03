91935893320b82280416f7e3ce283d70b896f282
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:45:19 +0000

    office crowded fault land treatment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
