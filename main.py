f2d6a34f30d1f6f1511c1c075370f55c625256b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:33:44 +0000

    reliable content many Pp narrow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_int(array):
-        return int(array[0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
