19b5ead00bf83e65c3d568601b3da4809c74a56e
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:54:41 +0000

    exciting instrument meet plan mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
