0c9827f321b8b1ab0eeca863e9fbccf51ef4dca3
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:33:16 +0000

    avoid characteristic manage branch whenever

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
