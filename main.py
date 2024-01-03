0b83a4182fb6eec5e4e3b05481d7dad5222373fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:16:37 +0000

    house birthday feed shock ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+print(json.dumps({"name": "John", "age": 30}))
