91e12d7c46525aaf1145232b164994946960dee9
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:50:08 +0000

    light official invite safe sometimes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
