9c162e86654db7da2ab7f1d2c9ecdbb770d7d0e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:33:09 +0000

    container warning feed payment dick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
