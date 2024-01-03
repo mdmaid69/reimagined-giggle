26a5f3e1d7a94f40f309551d54b813785a95e6fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:17:02 +0000

    coat point journey word will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
