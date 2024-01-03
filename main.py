8804b1588f32920e82247c4c8e9662ada6887f0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:34:42 +0000

    proof small wish split last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
