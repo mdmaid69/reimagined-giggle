d3353f8ec2652e3bc4d8fac20ee4d636802581e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:23:29 +0000

    load board possible poison post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
