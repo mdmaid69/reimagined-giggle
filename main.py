ec827f4dc96221894fd3956531370aefb4dd8187
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:07:41 +0000

    poor contact comeround hospital minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
