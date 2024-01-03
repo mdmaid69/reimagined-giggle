469532744c646b4014ad11f834c741b8835b46d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:38:40 +0000

    thin hard turndown another turndown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+print(sum(range(10)))
