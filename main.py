d7df822f48ab2ce36575bc34fc5ab5694c2645d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:13 +0000

    up slow return fit tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
