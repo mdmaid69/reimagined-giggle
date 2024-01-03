6ceb9970fdc27ba093944d0e7734a28399d9f738
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:10:58 +0000

    admire mother invitation modern satisfied

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
