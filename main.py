85765307789da1cef95b2e9635f0aea9072f39e9
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:39:04 +0000

    hungry fear wave soon this

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
