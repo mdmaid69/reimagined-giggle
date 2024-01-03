a1545bcfad10c0e4e83b27958608db7051aedd2f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:23:54 +0000

    protection takeback makeinto equal pattern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
