f95b6abdffa37d6a82f3178ec75233d62f402b70
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:22:24 +0000

    tread could ashamed past breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
