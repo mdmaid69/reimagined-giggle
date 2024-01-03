0080cb755c28b4bb8dff1582f3da255dd4fcfea2
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:00:57 +0000

    exist subject deliberate dance sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
