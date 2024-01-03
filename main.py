608b0b2eff4e72737b7196315454b2eb79d05f26
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:12:40 +0000

    right danger tour introduction shine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
