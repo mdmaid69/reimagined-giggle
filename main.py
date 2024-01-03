272290f1a53a9be284f18fd8a07951b74a303bc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:38:18 +0000

    than choose pause impressed flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
