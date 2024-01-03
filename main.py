ccabf697fe94b7ecf1beefad1290fb85820e4013
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:49 +0000

    breakdown crazy contrast charge run

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
