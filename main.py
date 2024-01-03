8488748efcf1ea7b2536089bebc7c14b30f3c854
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:33:35 +0000

    round active frighten waste country

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(lst):
-        return max(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
