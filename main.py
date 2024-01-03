91d8ce6e9ecfb30b88d5b08e00779a63d92057fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:07:23 +0000

    most for return somewhere why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def count_words(sentence):
+        return len(sentence.split())
