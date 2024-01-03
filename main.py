725afdca05783dfcc3fd28e81e9dc7999c59f08f
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:32:30 +0000

    shout condition club medium difficulty

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-print("Hello, world!")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import sys
+def exit_program():
+        sys.exit()
