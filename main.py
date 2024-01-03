0245be5aef11180b5409af0349932a864668a5e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:44:32 +0000

    apply need date sharp great

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
