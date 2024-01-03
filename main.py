f4c58d556a29111562334895651a12efa68ea4e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:30:50 +0000

    arrival beside beyond sister history

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-for i in range(5):
-        print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
