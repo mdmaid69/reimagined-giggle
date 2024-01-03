0fdb252f981b30af59b40b618bdd4d9c6f16f1cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:17:29 +0000

    already pride furniture whatever copy

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
