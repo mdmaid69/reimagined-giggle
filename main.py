0749fd0a323370a8a4cb48bc3c916208fa454ba1
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:11:38 +0000

    odd improve offer information discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
