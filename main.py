476210d7d9088653b7547ec6866c9ea54dbf8866
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:43:20 +0000

    wish bringup all Ll drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
