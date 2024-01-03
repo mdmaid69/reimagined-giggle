a32df4cab7f54b8a053120a56ef27aec44c4b833
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:43:42 +0000

    but result holiday badly consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
