5e1b459a0584a17f2a665afdd50e5c7a1fae95f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:20:59 +0000

    rank support good include tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
