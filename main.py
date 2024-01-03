4dec9985370e7c7328e12bb6d417f4e5b966ad6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:37:21 +0000

    black dark under neither name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
