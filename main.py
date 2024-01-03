5b5ae70ff945675a03cb8c952872dbf0d4ffc1c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:50:55 +0000

    material turninto knock should nothing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def add_numbers(a, b):
+        return a + b
