a32bedf913163bb4182aa82d57f62bbb569e49f8
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:55:56 +0000

    sign inside instead someone skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_acceleration(speed, time):
+        return speed / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
