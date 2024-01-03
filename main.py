e44d547cf65d8bc7211ece4a02d00a438c49ecf2
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:36:48 +0000

    choose to pattern tax her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
