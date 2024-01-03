507a88d99e424e2b42734c6677cb2da585c9ce9f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:58:58 +0000

    confuse Ww survive behaviour dress

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_set(array):
+        return set(array)
