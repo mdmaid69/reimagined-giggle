eebb5c13ed228ad6c73ed4e6e06347d3f881e3d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:38:05 +0000

    skill mysterious job marry busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
