5f9d5c442460591ecee9bb34d3b31756dbdb6feb
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:00:51 +0000

    gradually message child wrong call

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_area_rectangle(l, w):
+        return l * w
+def is_odd(n):
+        return n % 2 != 0
