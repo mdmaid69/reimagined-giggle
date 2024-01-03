4a1e6339aeb3987b50f6d612367673516cead7e4
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:51:18 +0000

    question before century breakfast payment

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
