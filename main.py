f6af3d5b2cb3c642a4295167e21e27a255371fee
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:43:01 +0000

    getout check contain life rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
