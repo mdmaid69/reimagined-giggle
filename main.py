6e21f17493e90afb2650439d709dd9705e90acbe
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:23:09 +0000

    surprising war force comeout congratulations

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import re
+print(re.match("h.*o", "hello world"))
