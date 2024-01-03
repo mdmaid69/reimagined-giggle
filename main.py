528cce702d664b052d72003b67984301e12e7721
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:53:22 +0000

    speed charge copy involve final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
