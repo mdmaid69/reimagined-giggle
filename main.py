f5b5a5d0a01126b106a2def7582b9417609c312f
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:10:21 +0000

    combination song pin except workout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
