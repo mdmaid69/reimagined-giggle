04263a2a9f7e9a2d7de10f9de1d5efe3a507f4c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:20:35 +0000

    rescue measure disturbing in steep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
