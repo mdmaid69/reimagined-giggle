640d8e983a7076142da5f72cfc785b91d167715f
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:13:06 +0000

    decide building hollow deaf interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_ceiling(x):
+        return math.ceil(x)
