bec770b01d525528dc41ae8c28656f01e468e056
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:59:09 +0000

    dare shade share commit Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
