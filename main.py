2308e042ed5332d922500b0764a443e73edb2855
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:49:41 +0000

    near view unless urgent tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_as_bool(array):
+        return bool(array)
