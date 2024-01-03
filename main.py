abc31717f9abcda4e41102024b5518432ddf6632
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:32:20 +0000

    shadow under gather ticket remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
