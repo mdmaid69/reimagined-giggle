c03077cea81a082b215b005fe362ddfbbea56b0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:23:24 +0000

    quantity smell Oo ready getoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def count_elements(lst):
-        return len(lst)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
