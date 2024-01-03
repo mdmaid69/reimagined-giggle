05f052a10ec9718af73f2d059aee50dfb082c612
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:21:54 +0000

    their Dd ourselves port catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_to_octal(n):
+        return oct(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
