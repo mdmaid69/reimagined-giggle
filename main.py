a7301f32f0aaec257d2847c3058bdfa4c4ac6dba
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:48:42 +0000

    thirsty run rain squeeze anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def split_path(path):
+        return os.path.split(path)
