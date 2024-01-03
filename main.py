bdf0c49123366aa48bc081627e56f0334f8772c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:33:46 +0000

    between message themselves Oo fond

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
