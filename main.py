ec9751939340621cb6398d0fc96b30d3bd5268c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:02:02 +0000

    giveup recovery dream recommend comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
