846545bb4249219dd7fc2cb10ee3c90ef3f226d9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:27:23 +0000

    cap stop small soon gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_min(lst):
-        return min(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
