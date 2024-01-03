27393640dc03b5d937d9b10865786cc1d20251b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:19:26 +0000

    strike upstairs over friendly worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import platform
-def get_os_info():
-        return platform.uname()
+import os
+print(os.getcwd())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
