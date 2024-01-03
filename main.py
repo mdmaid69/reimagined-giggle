f9bb9f3f8af79d9bd90dd0025b3509b1c182dc4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:20:29 +0000

    maximum mend safe shock file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_min(lst):
+        return min(lst)
