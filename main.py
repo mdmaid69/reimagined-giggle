8bc13406fc5331906cf764cd944b7c1e7044eb8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:07:24 +0000

    listen equal rent contents ticket

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
