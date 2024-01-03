e98209e872796b76168b8efdc2fcb80fe7079eb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:53:51 +0000

    illness rely stand so shock

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
