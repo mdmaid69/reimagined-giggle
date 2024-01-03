91667906a05c681b9e0406415cd1a86aa9393831
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:41:41 +0000

    join instant age steal unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
