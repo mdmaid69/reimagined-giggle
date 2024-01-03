33df9650c824a757f00b13f7bf043d05d6364734
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:00:25 +0000

    glass cut care shrink subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
