74658201288a2739918b712736d58d4e21bf877c
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:28:52 +0000

    peaceful cry travel recognize interest

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
