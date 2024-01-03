0d69c1271e8afa5223e5c26b54615212c279699f
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:20:23 +0000

    near phone proof gradually all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
