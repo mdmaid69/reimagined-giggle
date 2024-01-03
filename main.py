763bd4c5728a3f008bb4b087495054beecc1acf2
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:25:38 +0000

    run tap fill closely claim

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
