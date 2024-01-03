d604b09ac96b8cc0c1c1e72bb5c8070e0dc7a7e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:27:08 +0000

    ashamed to art callout pair

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
