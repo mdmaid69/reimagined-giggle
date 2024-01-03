9466af1f182e8e1fa788deb8a9148ec7bd63277d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:18:44 +0000

    imagination look 565end pity game

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  def square_number(x):
-        return x**2
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
