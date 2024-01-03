277ac2d09c5427f36921d166ca80a65c64fc48af
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:16:46 +0000

    succeed accident trousers mark vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def print_python_version():
-        return sys.version
+import array
+def clear_array(array):
+        array *= 0
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
