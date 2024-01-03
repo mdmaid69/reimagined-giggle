c88ea4571c6e3df16cde7f654e515275a9d00ae8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:28:51 +0000

    think slice disappointed poison naturally

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_speed(distance, time):
+        return distance / time
