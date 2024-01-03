599182fc824fc6056b3569016fc27f92517da4f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:38:39 +0000

    doubt petrol desperate attract factory

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
