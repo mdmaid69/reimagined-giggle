e3d5c9cf0bc8017ebf25a289d1680c919d4943cb
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:49:07 +0000

    priority possibility limit hot complete

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
