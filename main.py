ca1a0c4797fb499539f4a5a0451640ca52ca374d
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:21:44 +0000

    me tight callat swim him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
