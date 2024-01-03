f708bb940441898dc325827405523949e64e749f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:18:19 +0000

    lower fold earn crisis future

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_set(array):
+        return set(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
