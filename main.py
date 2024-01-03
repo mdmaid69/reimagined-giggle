9bc3122debbc50a89cb3ad9b1031029f97ebc2c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:31:57 +0000

    practise model lookback(on) murder 565end

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
