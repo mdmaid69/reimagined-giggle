7b11b0eed7c8ad0c2a7ec9c50d69cceb69f976ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:32:14 +0000

    high without stone tense place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
