b7228c25ba9a44ba37f11ad1b63cf29386dd4c35
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:17:27 +0000

    direct neither expect avoid interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def add_numbers(a, b):
+        return a + b
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
