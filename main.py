031854760455dba80a4c7a227cbfae14e3571555
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:22:45 +0000

    dead fast goback(to) confusing spare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
