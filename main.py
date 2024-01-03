9dd045d6462234c4ffba2ac1f97a119b64b3102a
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:45:47 +0000

    repair consider regular test bag

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
