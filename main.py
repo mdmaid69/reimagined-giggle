fce34b6d607b04fd3123d1d3edade527956d7b44
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:34:13 +0000

    Pp probably father heavy especially

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import math
+print(math.pi)
