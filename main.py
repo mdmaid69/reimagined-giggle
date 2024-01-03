813fe0592f5981adc032678eb88c8c5530b873df
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:53:26 +0000

    unpleasant fetch offence affect difficult

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
