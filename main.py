d6c2ecc2026882c975e4b646ecd7cc49043ff485
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:47:08 +0000

    negative accommodation movement tip various

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+def is_even(n):
+        return n % 2 == 0
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
