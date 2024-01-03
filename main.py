108b39d6b34074a95a37c82c83cb7321ec5fdf01
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:02:35 +0000

    what Kk force unfair neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
