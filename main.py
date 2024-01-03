5c969b8a21fbf0ceaf0f2b32ca01fa3c9b60f07f
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:52:24 +0000

    pair play calm man area

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
