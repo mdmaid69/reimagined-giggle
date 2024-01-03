5d63f2412dfbe7a3065e5ac36fac10605d8c4db4
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:35:11 +0000

    lot play ring pride bridge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
