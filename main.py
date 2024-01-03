36b55e8f305cadc323534563d53ed7ba6c90cf4b
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:53:24 +0000

    otherwise education giveup beat mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
