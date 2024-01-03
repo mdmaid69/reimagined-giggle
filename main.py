c6b422a396d2fcfbf7d46b7214d9e046138fa673
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:19:45 +0000

    door comfort scene sea direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
 print("Difference:", set(list1) - set(list2))
