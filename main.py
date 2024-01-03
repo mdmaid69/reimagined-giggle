4a1e8ae802bff422de880028343e934927726f8e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:52:15 +0000

    birthday mass human pure bet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
