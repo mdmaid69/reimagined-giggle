5c4dea9fa8d55e2ce4afca787f974f28cced8e9b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:46:22 +0000

    happy whistle bend step excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def reverse_list(lst):
+        return lst[::-1]
