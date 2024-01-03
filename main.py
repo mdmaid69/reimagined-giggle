fad071c9ad338591867b298f106d2f85ef486270
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:53:35 +0000

    common sweep other fear little

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
