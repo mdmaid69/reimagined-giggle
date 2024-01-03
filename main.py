ddf7721315ea10b7b887921787488c18bc2005f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:01:08 +0000

    reduce praise practice realize shame

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
