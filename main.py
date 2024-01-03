038abe82eb2de29c58897ef34ab4531f6da8ddcd
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:57:28 +0000

    play smell too getup busy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
