9080d16f30a24dc93617cf94acb315535d985ed5
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:47:14 +0000

    swim let contents team let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
