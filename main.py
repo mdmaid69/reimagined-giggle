b6d9d6fe5acc080d23805bba8b0371a6070fa6f3
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:34:26 +0000

    public record know Bb ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
