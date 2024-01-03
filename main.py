329837b861c2774520c7eb7a2eec4a3f2e3e0207
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:10:16 +0000

    shout succeed reduce floor hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
