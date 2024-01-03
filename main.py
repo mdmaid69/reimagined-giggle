c9313254cd744ae0391decc2241d7725625ed72b
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:26:24 +0000

    runinto rush hold seat equal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import tensorflow as tf
-print(tf.__version__)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
