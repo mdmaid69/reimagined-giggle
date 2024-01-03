6a2da5fb6a4c158b85be657c4364e1be66f696aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:35:41 +0000

    many children now floor callback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
