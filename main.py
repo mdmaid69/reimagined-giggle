356db48d27e4a148801bccfb604d79947114d3f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:56:26 +0000

    shame yes watch nervous number

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
