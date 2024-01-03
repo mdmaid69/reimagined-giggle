f0236ce7221ee10db199d14e20359c13f834b89f
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:53:59 +0000

    complete ring their handle hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
