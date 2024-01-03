57c277ddba659d335d9ac268ef292d64785b8b4e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:08:09 +0000

    every glad thing good muscle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
