24caa023542028c44d650c012a9034bf67bfc06c
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:37:48 +0000

    naturally effort help pair produce

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import re
-print(re.match("h.*o", "hello world"))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
