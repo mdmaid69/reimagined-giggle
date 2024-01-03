11f1b40dc5417cddc173dc947c41a8d94316f062
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:49:42 +0000

    neither challenge comeround chat old

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+print(os.getcwd())
