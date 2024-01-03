6c18cb412159d95320c9e05fe2a1548427a25a83
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:41:11 +0000

    last hollow story wind mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def convert_to_binary(n):
+        return bin(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
