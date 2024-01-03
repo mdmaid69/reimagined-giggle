2688aa88fbb281adf6d73d658d98c0a409dc1317
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:49:54 +0000

    feed fast best bringdown experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
