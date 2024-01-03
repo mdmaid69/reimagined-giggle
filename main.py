2f0b875a855f907b8522f46d0577eaedaa25583d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:07:59 +0000

    married lead early adjective promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
