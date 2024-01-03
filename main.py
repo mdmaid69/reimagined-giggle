7aa72706af0c83084e7f7c511f1b6a42b0f22f56
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:17:34 +0000

    admire professional everybody confuse earth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
