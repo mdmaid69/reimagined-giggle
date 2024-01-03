182073c18920529167a6c787cc74901ffe703f63
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:29:58 +0000

    treat fairly try reserve display

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
