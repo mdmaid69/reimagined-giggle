a19943a2853171a155809c0045fd9a2a425d7ab2
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:58:22 +0000

    arrangement promise challenge change national

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
