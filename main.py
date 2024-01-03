1ad57336888834fd214991f25c4d644aba2d5d7e
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:43:27 +0000

    nerve everyone whistle translate repeat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_power(work, time):
-        return work / time
+def find_max(numbers):
+        return max(numbers)
+def convert_to_binary(n):
+        return bin(n)
