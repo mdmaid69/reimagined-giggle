966a180ed836de3d01439efc191273f8ba81351d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:06:30 +0000

    wave same fear ring declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
