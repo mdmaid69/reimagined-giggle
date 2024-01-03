44f4b389a395ce1b99ff16713230559aff96364b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:32:09 +0000

    help conclusion outside reach could

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-print(math.pi)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def convert_to_octal(n):
+        return oct(n)
