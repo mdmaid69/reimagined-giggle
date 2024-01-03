beeb0cdb3908dabce74ab85af974a1afda7540bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:26:11 +0000

    failure wild different thin collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
