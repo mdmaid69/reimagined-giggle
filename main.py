61978140a8d55c2110be5b8753aeeec9077c61bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:04:11 +0000

    whisper phone size mail then

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def cube_number(x):
-        return x**3
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
