28ba89c62737eb1de55663b262197b40ae923a75
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:21:50 +0000

    afternoon improvement empty sit culture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-for i in range(5):
-        print(i)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
