c2625328eb7704e6b5a5248c833c62a9de1562ce
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:56:53 +0000

    on party stage carry affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_work(force, distance):
-        return force * distance
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
