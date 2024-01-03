492aa5ef2ff28e032438b55016c2b22aa9203ceb
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:18:35 +0000

    twist exaggerate slice note sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
