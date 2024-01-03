50065639a56668abf468e4654be8ffc95c577cda
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:43:36 +0000

    blame arrive term authority hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
