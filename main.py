0e4415524dbb3f786ebaf11a7af55fa5c3b09c31
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:49:46 +0000

    basically complete relax lower spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
