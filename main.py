cd97dad801ee9fbb969ad771424f09f7bdab75aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:16:20 +0000

    Itself cause Kk uponon if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_volume(length, width, height):
+        return length * width * height
