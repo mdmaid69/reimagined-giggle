b541544eb37e493db592d693d63ba571a93c3dc7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:05:02 +0000

    before top describe sense crash

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
