a1ac4db10fc70422213ff1b54b5e7cf9d4e33a19
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:46:42 +0000

    reaction keen unhappy crisis scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
