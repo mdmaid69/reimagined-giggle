2cc0cb90460e3040b040f8b023e8cd9dd52e8dbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:21:07 +0000

    better reason famous nervous phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
