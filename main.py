887937b13e49910beeae4219b0fc67bc45ffeeb2
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:19:52 +0000

    underneath divide instant doubt try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
