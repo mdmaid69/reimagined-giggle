efc0063cf96ae15513bc54f87c8b16a6e4766c0b
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:42:32 +0000

    hard confused command face deal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
 def calculate_density(mass, volume):
         return mass / volume
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
