476307a31c03d2f935f0a6e590d9dcf933dd5671
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:30:37 +0000

    uponon future match rise whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
