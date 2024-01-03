bd1006218c475290137a09d2fb44d48afe100072
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:26:55 +0000

    organize noisy turnaway hello cruelty

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
+for i in range(10): print(i)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
