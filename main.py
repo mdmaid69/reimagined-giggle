d3dfee64f6204bb4975678809676a9f07f6b8fea
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:46:02 +0000

    minimum food because muscle club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_ceiling(x):
+        return math.ceil(x)
