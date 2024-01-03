dbfe3bf6a70a051b61ade21c894134f00a54afeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:35 +0000

    confuse Ii square smart entrance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
