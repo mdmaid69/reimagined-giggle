6d9592e650eb05ab89914cca7b3fa344718824a9
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:10:07 +0000

    understand worse turnover ignore increase

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
