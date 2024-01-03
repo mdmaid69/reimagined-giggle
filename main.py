91e08ac7b2e56ea12e7c2e69f81d8e7cf5c5e728
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:00:43 +0000

    ear date keepup mystery getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+print(math.pi)
