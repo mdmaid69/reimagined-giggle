354d24fc012d6405e73b1c8cb44bd8a71a6bdf0e
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:08:01 +0000

    flight hold command repair myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
