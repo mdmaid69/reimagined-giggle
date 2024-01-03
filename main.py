dece512f62a5e13bef988b1ab0e15eed4dd6286f
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:20:57 +0000

    Ww free strange anywayanyhow better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
