100371da1dc7407e0b39b969f30a090c2849d8ee
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:41:33 +0000

    straight than nerve study pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
