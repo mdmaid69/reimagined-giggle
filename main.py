05830165263eb30e4e785ddbe6c719d0f0c96977
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:24:14 +0000

    early work word convincing ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
