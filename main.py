7cee0978ce80980763fcfd62329ae51af30db05c
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:23:45 +0000

    brave plain everywhere swing Mm

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def convert_to_hex(n):
+        return hex(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
