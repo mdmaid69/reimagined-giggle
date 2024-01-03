b21e4218d125e1f95ec37d3673b7a46948c17a12
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:23:53 +0000

    height refer bound kill advantage

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-print(sum(range(10)))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
