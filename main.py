69bf2cf6df8e1b4aa6516fd11909a50ed25fe54e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:31:25 +0000

    geton regret woman board clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
