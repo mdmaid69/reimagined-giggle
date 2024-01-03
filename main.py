f398c7142e11e10b7dc4ff6f59db5098f835ba36
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:25:24 +0000

    row criticize shut release break

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def convert_to_binary(n):
+        return bin(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
