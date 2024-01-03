0343ce00fe7a9dcadb52e80a26b8c7c6d9f85145
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:01:45 +0000

    declare teach instruction trousers as

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
