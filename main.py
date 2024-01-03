7461b96e7284dd3a21a9d7ee71abbacaf80d2d5d
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:04:15 +0000

    push nature urgent discovery mean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
