65742d0030fc0ea234b1aa614fec545e9a642ddd
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:17:36 +0000

    unkind bringdown common getaway Ll

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-for i in range(5):
-        print(i)
+def calculate_exponential(x):
+        return math.exp(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
