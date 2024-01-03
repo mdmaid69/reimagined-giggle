dd87de5596a3e6b21956b0bbb311f8d2b6e4cb1e
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:50:03 +0000

    clear shout flood brief Pp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
