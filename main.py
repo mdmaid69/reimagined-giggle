9ec3414dec3575dfb1fc79b8f4d085ff3ddf57fa
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:00:07 +0000

    respect divide variety unit contact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def append_to_array(array, item):
+        array.append(item)
