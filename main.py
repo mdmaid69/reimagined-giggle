9e879990c852406df1e85732d8f3f1de4e893fa8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:37:49 +0000

    secret shut myself settle both

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
