79e58f9121842e051b0bb3b8ac493bd211782e6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:37:23 +0000

    stop town centimetre morning damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
