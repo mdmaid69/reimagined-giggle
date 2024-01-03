d90271e1afa8e0120069e9759d550d755d8cdb0a
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:44:02 +0000

    sail Mm central figure we

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def convert_to_binary(n):
+        return bin(n)
