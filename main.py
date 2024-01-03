89e4167a2d8109fa7eb8ce3bc7d26f04a1dfe893
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:11:20 +0000

    badly before phone play apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def is_odd(n):
+        return n % 2 != 0
