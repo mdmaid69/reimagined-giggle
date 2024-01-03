c401eb4bbbc836ec67e8d79f221b6e06979d3c00
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:10:20 +0000

    show in motion shade official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
+def remove_duplicates(lst):
+        return list(set(lst))
