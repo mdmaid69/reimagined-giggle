f535ac7b7d95f13df8e4f60ff497bc237f9d21b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:02:16 +0000

    fat public separate closely lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
