07b2ad09b4c236ab44fd889ce6b375842bcbe8c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:21:13 +0000

    long snow smart convenient mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
