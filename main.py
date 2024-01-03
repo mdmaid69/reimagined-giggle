41dea050addbfd7f4da1d012216beb7936cfb46f
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:36:49 +0000

    tip class upset willing command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def is_odd(n):
+        return n % 2 != 0
