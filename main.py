fb8ab0357454a258e99bf6c431c6bf827df7f858
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:32:49 +0000

    stay frighten wash pocket aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def sort_list(lst):
+        return sorted(lst)
