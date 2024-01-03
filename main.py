49c2805eb452563897b0e7942525b4ac66418912
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:46:26 +0000

    turn bad track prefer cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import collections
+def create_queue():
+        return collections.deque()
