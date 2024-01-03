a113d6e2fc726ab032fc4bf9a344dc6354ff16ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:29:45 +0000

    ago bang store die holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
