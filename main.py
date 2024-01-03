0bf4a09afaa95b3ffd74adefaf0cd4e7982d947a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:02:37 +0000

    finally appearance wash beyond do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
