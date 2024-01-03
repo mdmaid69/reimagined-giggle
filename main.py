1d4f35c5588008b33f93d82c5bc57d4d54035787
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:03:38 +0000

    convinced possibly seat lost slice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
