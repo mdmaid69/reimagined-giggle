b006167724d7c4260fa7c37c9b533183038a6fc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:46:36 +0000

    land smell frame bang fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
