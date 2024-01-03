d8b1ceaf79256e57c904c591486fc19076ee5025
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:04:06 +0000

    contrast hair side still alive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import platform
+def get_os_info():
+        return platform.uname()
