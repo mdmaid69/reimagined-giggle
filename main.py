d6806f27f1334b6c260a95bfbafdb4b598d7ed26
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:56:09 +0000

    edge away drawing excuse petrol

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def reverse_string(s):
+        return s[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
