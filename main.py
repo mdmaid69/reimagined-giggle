fc2cc933a588a3b5af10cbf4ff6310eee73a98eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:00:07 +0000

    bottom preparation because separate upstairs

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import time
-def get_current_time():
-        return time.time()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
