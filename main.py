15fa73f3fbbe18ed29ec68b80581f113aa38f2ab
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:16:26 +0000

    than before mistake effect software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def get_array_as_set(array):
+        return set(array)
