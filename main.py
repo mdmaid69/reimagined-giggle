41e13535f2d2ab4a5044dbc7bbce597267ae7f2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:56:29 +0000

    set community in know bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
