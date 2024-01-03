16b911de048f0898340a30cec7c7369d810fb1c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:19:23 +0000

    that dark criticize little everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
