c412160994dfcbd502ed7c5da84076ff182e8322
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:03:51 +0000

    combine hit trouble extremely interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
