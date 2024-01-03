2c58cbb9096f99de8abcbe8c4344db6dd022e33b
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:38:27 +0000

    standard visit always sing less

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
