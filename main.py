3f7d8b743c15da6254970b1883e34286cefd02fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:00:44 +0000

    whole poor arrangement storm still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
