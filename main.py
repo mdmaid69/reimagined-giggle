8baaa7117bfd81462180d38cae18c914edc35e53
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:48:01 +0000

    runover division moment vote quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import math
+print(math.pi)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
