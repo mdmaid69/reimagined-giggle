5c1fc08b27afac7f238d3c347c7338291ca67eeb
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:27:35 +0000

    argument hollow keepout mistake bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
