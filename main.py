f04246fb715850e475a432becaf971e1356f719d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:11:04 +0000

    kind whether usual solve of

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
