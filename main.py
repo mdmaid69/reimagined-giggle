55ecd34751c95026aa587ebcff111ca9e1b53652
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:38:52 +0000

    wish vanish snow upstairs door

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
