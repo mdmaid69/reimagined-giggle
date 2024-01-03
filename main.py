bd7990fc2cb6e2ce8241dd34993319b65bcbad68
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:02:48 +0000

    cry law nice base clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
