826204fb0a2d3273a72615a6b0d3a30f5b0abc28
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:52:09 +0000

    do offend behaviour simple introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_words(sentence):
-        return len(sentence.split())
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
