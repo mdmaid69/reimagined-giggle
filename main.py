4b2e7980e2b5c8164cd1e8db7c878c260568486e
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:40:33 +0000

    star shadow polite government bright

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
