d5e2ae9f1a5c1e6b23487298d4f23df9db887c23
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:13:29 +0000

    voice more commit leg test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def remove_duplicates(lst):
+        return list(set(lst))
