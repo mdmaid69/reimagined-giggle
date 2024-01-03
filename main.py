00446e93519f6678b7c8a21929da8730a08f6ef6
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:08:03 +0000

    use order arrangement used cut

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+print(math.pi)
