17bce94e093293af1a16d879bc7ab457c5da04c4
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:37:09 +0000

    somehow against field gap meeting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
