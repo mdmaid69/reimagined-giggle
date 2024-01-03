9dd9df8ccc635195be4bb07bcf6920d26daf9807
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:50:29 +0000

    clever flow discover curious careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
