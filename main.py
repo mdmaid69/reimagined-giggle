3a912d7007d01b24aff9b7f182e5d91b28cff7c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:41:00 +0000

    brave bit nerve travel look

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
