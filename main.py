c4b5c558137d790e141c7cf054fdab46fe755582
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:07:24 +0000

    butter him explanation settle belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
