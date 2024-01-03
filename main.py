4ab4a47c18b882e1da96519d6134e1fb6ca289b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:09:50 +0000

    mistake otherwise stay live part

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_binary(n):
-        return bin(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
