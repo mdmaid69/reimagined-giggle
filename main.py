2eecd721dde1f72deba4fb739a5244e40c989987
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:50:45 +0000

    getup name view ring artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
