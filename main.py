1e536537c92ead8295194277b1073ba6fc39d3f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:22:29 +0000

    limit join sign friend flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
