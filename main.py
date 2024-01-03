5d64a56735e0a5ca662f4c2dabb429ac0c7f5210
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:01:06 +0000

    common formal complain hair damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
