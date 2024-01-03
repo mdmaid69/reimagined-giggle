8774a895d338f1144564ec91efd9f29c9ee14411
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:39:36 +0000

    manage test complain report conclusion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
