6e391b0dac5333c743709777d28c377f6a412578
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:04:50 +0000

    strange opposite form pin treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
