2073a0ede0bf39646fba155304dda6a5abfcb923
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:44:17 +0000

    translate test affect bury concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
