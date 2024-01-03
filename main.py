dd217644c26d513426555bcf22d63fb82de58c73
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:47:17 +0000

    good tie workupto arrive skin

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
