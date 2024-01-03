2ddd06de2ed1639fad0ccd041c7e4a2fbf3fa546
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:14:49 +0000

    this position watch taste bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
