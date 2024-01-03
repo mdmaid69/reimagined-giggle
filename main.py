d4f3a1ca0360419c962724f28742960bbe687a79
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:05:45 +0000

    radio tradition shallow wave conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
