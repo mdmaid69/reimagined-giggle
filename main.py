9257e3d7df3d949d11407fe9f831dd2d34718281
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:10:56 +0000

    crazy make whenever cost emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
