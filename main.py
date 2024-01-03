6cad9f6d2b412f43f28f0a7213beb95dabdbcadf
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:26:08 +0000

    have someone live part eat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
