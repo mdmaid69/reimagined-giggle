b0bfff02f4c74260f6174cad23c7b83ca4275a31
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:31:39 +0000

    fill house against patience easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
