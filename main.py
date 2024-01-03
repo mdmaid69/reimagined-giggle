2cd7f244f01c91ebf9be0be199b5d6a23c5c12b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:03:33 +0000

    save summer this clothes skirt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
