94d9f089af436d3429ed52bdb2793fe0892c6c9c
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:07:30 +0000

    partner computer explain compare until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
