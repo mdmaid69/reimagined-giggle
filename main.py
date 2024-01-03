3ce205840c2443df078fdce0fd5d713df5321108
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:33:46 +0000

    everybody bar similar decision estimate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
