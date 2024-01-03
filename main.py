a76c3fddcfa21e184e4d00da749f425f1f10029a
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:21:43 +0000

    several Tt conversation this congratulations

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
