f12e97eae35bda5268fd429807e17a466bb05db7
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:57:45 +0000

    us handle fond recently brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
