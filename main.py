e1568ab18cd3b63572f6c9dc77b7cfe20e6f8bcf
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:39:03 +0000

    used taste fly hardly proof

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
