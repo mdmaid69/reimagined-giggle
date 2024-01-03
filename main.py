ba1dc5075398e7c521ba6a81e90ae0f3552c986d
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:30:17 +0000

    afraid complete murder worth male

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def sort_list(lst):
+        return sorted(lst)
