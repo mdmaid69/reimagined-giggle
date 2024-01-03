019b8ac5bb16c1a11230b18ff56d0f1c42dea96a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:03:56 +0000

    reject save morning thanks unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_words(sentence):
+        return len(sentence.split())
