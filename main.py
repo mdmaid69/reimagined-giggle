3402d4b1e2f5abab65c606312a8db094155c713a
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:40:00 +0000

    grand still himself confidence usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def find_max(numbers):
-        return max(numbers)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
