998f32236b5946e635f9f78a07fcd1885df064ad
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:53 +0000

    appeal again criticize splash right

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_power(work, time):
-        return work / time
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
