3c7d52774419f4f453b93a768a3419d7247cbdd6
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:58:14 +0000

    tend cruelty male criticism some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import tensorflow as tf
+print(tf.__version__)
