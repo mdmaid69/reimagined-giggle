0f1aaf1ff466b664a27926507cbdabcdbb763854
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:35:39 +0000

    soak content crash light we

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def add_numbers(x, y):
-        return x + y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
