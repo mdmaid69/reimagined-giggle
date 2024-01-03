d09d938f859c78238ec671b0fc2df818aa999fa1
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:40:15 +0000

    gentle deep odd shine Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_work(force, distance):
+        return force * distance
