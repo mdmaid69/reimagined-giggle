b968493e9e7ca482f558cd8f0605c28cbd5e529b
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:10:44 +0000

    rough mistake arrival born bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
