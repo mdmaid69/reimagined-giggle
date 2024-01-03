333773b9f95008dcd2d1a04b9476c26b095de878
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:44:19 +0000

    split city combine uniform responsibility

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
