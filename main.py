0899f59ebf0f8d3b9ddbe00bf343275e85218884
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:03 +0000

    conversation stare dress hole curious

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
