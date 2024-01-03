3d4831fb450b5912f81f73187a0f45e608ef2169
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:40:06 +0000

    voice dress yet total giveaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_rectangle(l, w):
+        return l * w
