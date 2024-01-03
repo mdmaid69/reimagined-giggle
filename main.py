5b8ca29c2a0222ad1bd232630d5d6976d36cf775
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:07:00 +0000

    flight medium music name shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
