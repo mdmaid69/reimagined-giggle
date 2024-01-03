e393c9ed54f96787c163742bb1dc302ad7bc9fc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:52:41 +0000

    book underneath polite separate hour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
