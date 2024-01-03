7db4f118fd8ccdadd93666a47b6adfb1489465d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:09:01 +0000

    split trouble unkind makeup out

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
