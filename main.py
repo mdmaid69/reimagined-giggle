70a471927ca2913b0832eee18d51ae1e6e746cb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:15:16 +0000

    fortunate leg belief square tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
