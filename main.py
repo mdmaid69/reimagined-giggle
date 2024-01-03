a38bd993ee05b67f6fc9b1e214ff0ef6405f6f57
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:51:58 +0000

    deliver friend yes science hurt

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
