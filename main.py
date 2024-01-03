e342c8036bd9792872dd720c2e4af2c8ee724bb8
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:10 +0000

    doctor extraordinary rub file plate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
