0be78bb7767a78817736e65e8475eab9947253c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:47:05 +0000

    fail life nose comment interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
