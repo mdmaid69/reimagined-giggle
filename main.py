b1e4463cfce208801971a45a420b210d9318281b
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:10:16 +0000

    on lookback(on) spring light birthday

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print([x**2 for x in range(10)])
+import math
+print(math.pi)
+def calculate_density(mass, volume):
+        return mass / volume
