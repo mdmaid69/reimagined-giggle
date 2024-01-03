48735561301e852295d6fb4232f08e55fbba4272
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:54:30 +0000

    spin escape seem file come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def find_min(lst):
-        return min(lst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import collections
+def create_queue():
+        return collections.deque()
