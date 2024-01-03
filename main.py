70dcdb40c4d80712f9de66c5fdd14ae542744cd3
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:22:16 +0000

    handle mystery such hate quiet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
