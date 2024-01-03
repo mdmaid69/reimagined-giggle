3f3f85ea4d2e6a23a5c4179d9532f3fd944fe1a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:12:53 +0000

    block male whatever anything health

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
