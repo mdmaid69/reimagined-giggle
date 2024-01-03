5b2b5db01fbf50e3ebad7bf1e1c2dd75a6fd368b
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:29:41 +0000

    development trust break ready back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import tensorflow as tf
+print(tf.__version__)
