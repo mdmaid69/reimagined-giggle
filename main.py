7f863f41cf538a65076628937019a12d1dd7e5eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:22:40 +0000

    Itself food gather safety force

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def convert_to_octal(n):
+        return oct(n)
