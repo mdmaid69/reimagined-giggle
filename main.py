7f8cf7925d5c591e2f2ea360045a0bcc4b2e3776
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:28:54 +0000

    black actually chief refer book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
