9e895cf54f770b90c5792cc7faf099632a18b10f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:35:12 +0000

    improvement fat pupil spill sure

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import random
-print(random.randint(0, 100))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
