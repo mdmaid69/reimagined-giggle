0ad3e296c34e26e4e1b944f5e712b1a93432a41e
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:53:45 +0000

    content catch afford to sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_item(array, i):
-        return array[i]
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
