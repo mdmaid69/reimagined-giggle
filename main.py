03d6f613d372294bf2d1c217ea0cdf6334411e6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:19:29 +0000

    the subject when body meanwhile

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
