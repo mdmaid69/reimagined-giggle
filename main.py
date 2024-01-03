cfd233b72bd8bf02a2c6e1f5756e3f4fb77c8a03
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:14:19 +0000

    situation watch circle content Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
