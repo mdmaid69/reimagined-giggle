8f1e8fb3ad050080d5fd09e68b5f5341ce996276
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:08:28 +0000

    level manner disturb wild pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def square_number(x):
+        return x**2
