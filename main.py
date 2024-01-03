27ba7a6b6b1f20b62d085026894abde24bdd1df0
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:29:08 +0000

    score right keepout better image

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_area_rectangle(l, w):
-        return l * w
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
