b8e22c807fc8e940f5824d41d853fe040b2ed8fd
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:24:38 +0000

    guess hurt protection Ee check

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
