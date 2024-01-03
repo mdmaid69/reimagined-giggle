ea507125c3889f7eff933119eeff9f75649e2f8d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:39:41 +0000

    mouth sign black bright damage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
