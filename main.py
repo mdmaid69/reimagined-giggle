114c6d9ab12826391205e594afaecf9edae342b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:01:56 +0000

    past force town feeling talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
