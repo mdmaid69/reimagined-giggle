75271f8c180e73c7dbcfca93000c3474d846596f
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:54:15 +0000

    turnback finger square rough Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
