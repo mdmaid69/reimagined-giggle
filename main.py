8acbafea8afad045a5f87df02df337150a86735d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:08:44 +0000

    shadow well discovery steer run

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
