e8175b5903426e8e04d3ac3a580f94750693bcab
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:13:32 +0000

    rely keepaway guard flavour visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
