8168b49e3a9aeed09b10f0854cbf745a33cc87a4
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:21:41 +0000

    however frequent talkoutof reduce square

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+print(datetime.datetime.now())
