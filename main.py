49df096f6fa4348f162da549adff847e23458abf
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:42:20 +0000

    ill immediate anybody nervous opposite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
