8ed61f1a9685827011ae4bc78d781e23a449f713
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:54:17 +0000

    no pause moral piece plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
