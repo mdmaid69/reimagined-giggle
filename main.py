6012239648ccb1c6daa87cb0bc061620647ea1bf
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:57:01 +0000

    except comeout warning moon light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
