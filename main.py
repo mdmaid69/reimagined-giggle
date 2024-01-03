fdffa59823bececc2d169ba7f76f5586a6beb3e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:57:14 +0000

    leg breathe progress eager so

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
