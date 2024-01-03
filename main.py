1a8292480651e06beec8f9b7c72149af3bafe189
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:52:33 +0000

    bother university treatment slow terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
