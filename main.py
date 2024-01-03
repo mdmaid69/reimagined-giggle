78ee7242e52cae792b825d18d50e097370781aee
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:23:39 +0000

    hard create cheque independent age

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
