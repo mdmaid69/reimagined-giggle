057afe935a5ff4c4e90f7e5520352749dde9cca3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:14:51 +0000

    forget around pencil petrol cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
