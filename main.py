ec3f59a4ee578168ad3884dbd7aa795861e38c16
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:06:59 +0000

    either attention show family further

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_area(radius):
+        return 3.14 * radius * radius
