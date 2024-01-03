8dec26148abb7e1a8a456714b66385d43bacc1c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:31:27 +0000

    look health copy operation callup

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def is_even(n):
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def is_even(n):
         return n % 2 == 0
-def multiply_numbers(x, y):
-        return x * y
