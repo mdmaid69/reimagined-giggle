f4f9620f00f4ace1bcce711a3e01f29bae81db96
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:41:24 +0000

    equal relief already appear fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
