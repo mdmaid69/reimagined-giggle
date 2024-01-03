8c0ab02f704eed378ca8b2e2cb895fcf772f65d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:22:57 +0000

    secret giveout thick big old

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def is_even(n):
+        return n % 2 == 0
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
