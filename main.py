bdf871fe180ff10d395028f2027abde48fed4c18
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:42:13 +0000

    dig Ww turn downstairs breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
