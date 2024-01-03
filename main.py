72693108d69d550ee3a5da02dc50b7267e63b591
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:26:36 +0000

    anyone best remove everything back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
