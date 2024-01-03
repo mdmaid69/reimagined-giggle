c6af279fd0fa5b726501bf31c4bbdd0e39cdca41
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:52:09 +0000

    disappear season ticket object confident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def count_elements(lst):
+        return len(lst)
