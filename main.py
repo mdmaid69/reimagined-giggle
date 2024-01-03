1ebc76d4f66932703050eaf857aacec4364b767c
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:04:02 +0000

    turn factory soak meanwhile top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
