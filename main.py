53837e9988a78cb6cb01add0bd09854cf0e58ed0
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:09:24 +0000

    piece effort turnout ear memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def count_elements(lst):
+        return len(lst)
