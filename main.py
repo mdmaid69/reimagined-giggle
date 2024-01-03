d2e19b0a6dab50908348586f4b21ebfe80611b4c
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:39:19 +0000

    blue stretch over sad small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
