60f6d92264155750dd1cd896b5c73d23d224b8f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:09:50 +0000

    greeting carry represent vote 570enjoyable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def square_number(x):
+        return x**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
