ef22b9641556e26a7e96c459fd31642b864c72fc
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:50:44 +0000

    bringup me rush wave drop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
