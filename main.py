d8499ddc432e52727207f9377b03c5847ee18a80
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:54:57 +0000

    keepoff school number crash pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
