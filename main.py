01028aeb1f3399da027a10fca57ec034f9ae16ef
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:51:20 +0000

    skill finally load doubt reject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area(radius):
+        return 3.14 * radius * radius
