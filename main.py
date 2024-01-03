a432376d8b9c493e66301eb8a0c34f5ef70e5596
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:14:08 +0000

    much whom wash fasten callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
