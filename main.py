26c2ce49a93f5eb34ee2a910af99b9acb815fd4d
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:41:50 +0000

    mouth truth swimming connected rid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-print(os.getcwd())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
