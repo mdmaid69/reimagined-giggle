ea789d94074827875ce12e0e70c3298a5b3e3c41
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:29:40 +0000

    sometimes dull learn trial involve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
