5dde6e34162aacb600d17f9f1dc69769b9f78973
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:19:31 +0000

    machine gather breath turnover alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
