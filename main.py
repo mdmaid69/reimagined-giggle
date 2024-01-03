b2e8d290d616410f290630d725d85096e4d5a18f
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:55:48 +0000

    Dd family apply dear call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def convert_to_binary(n):
+        return bin(n)
