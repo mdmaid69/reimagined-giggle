7d0be83cc174f41d660262c899cb9323a59d62b5
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:37:10 +0000

    hard authority hope television embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
