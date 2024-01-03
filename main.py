e97521a476ae79b470a29d131df0aa581d86f039
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:40:25 +0000

    comfort injury middle unpleasant become

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_rectangle(l, w):
+        return l * w
