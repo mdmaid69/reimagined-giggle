0e966e69e144f2114f335068c3932793ff4648f1
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:57:26 +0000

    ilovedicks lead obvious modern side

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+print(math.pi)
