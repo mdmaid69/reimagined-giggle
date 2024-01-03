43e34ef6875cb4f76f3d9cefdc6150a0181ee682
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:14:21 +0000

    similar ago after less black

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
