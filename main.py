30e4c0445613c43b4a60d6bf3756f8ac265529af
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:47:05 +0000

    extra possession phone flow sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
