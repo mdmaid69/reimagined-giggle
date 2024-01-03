6a23d73d03c34b8fda35c30772af2a71de548271
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:24:58 +0000

    actual neither reserve smell scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def square_number(x):
+        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
