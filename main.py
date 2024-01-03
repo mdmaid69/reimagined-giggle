b1fe59182f810aff7e1fa597379fcc68a92e236f
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:58:07 +0000

    dark push anybody everyone off

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
