30e31abad3ff4b26408d07a4ec5959f49eeff33a
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:03:02 +0000

    laugh butter ready scrap back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import platform
+def get_os_info():
+        return platform.uname()
