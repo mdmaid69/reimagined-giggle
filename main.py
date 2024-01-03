7768342b032ae501f084b227751c4a6d1c054548
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:51:09 +0000

    567enemy would tend goodbye deep

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
