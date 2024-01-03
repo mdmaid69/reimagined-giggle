c4c5259e1e98fc63a17bf6d6737a7671c7f3e271
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:30:24 +0000

    blow boring level rather shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
