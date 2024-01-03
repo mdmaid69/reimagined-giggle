10ef24ac849a11b19836fccacb4338d3b33dd430
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:46:01 +0000

    kind embarrassed guess argue absolutely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_unique_words(sentence):
+        return set(sentence.split())
