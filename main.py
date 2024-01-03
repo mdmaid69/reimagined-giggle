0bcd7114fefedc07f066201f141c2497bd34105e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:50:44 +0000

    exact wander else cheat main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
