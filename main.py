cf2f1fb4de2c798768c4d18376657e2e3a0be83d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:15:03 +0000

    prove all improve find comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
