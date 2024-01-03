2fe3c070849a4fcf874b7bad9f068364d93a3e6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:37:51 +0000

    fresh lend see die fond

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
