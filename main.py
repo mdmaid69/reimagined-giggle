4430fc1bbf75bb50f6e4ec081f89b7e3663974d8
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:10:17 +0000

    without ordinary bear turn heavy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
