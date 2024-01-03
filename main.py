0be8280bb6ea3d7c724e81ff31681aed17907ea8
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:03:23 +0000

    beginning food against arrival tax

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
