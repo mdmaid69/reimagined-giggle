c7bade8bafbd4c9e262fdb30394b26d575e63ac8
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:41:15 +0000

    service flight question most careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
