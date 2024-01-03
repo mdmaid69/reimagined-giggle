b2e0113b9f6ba83675bec8ed102714bc4c186272
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:04:37 +0000

    greeting emphasize beautiful ago she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
