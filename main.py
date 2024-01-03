4a911b7bca5fe953da93813ac6930ac45e47570f
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:33:41 +0000

    excuse let Vv ashamed whole

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
