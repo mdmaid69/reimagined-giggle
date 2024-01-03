8957e04dc0b27009e2e0392f48aa6761477abb3d
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:18:14 +0000

    meal interested main conclusion head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
