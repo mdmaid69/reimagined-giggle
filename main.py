572f876253be5433b816d652a551dc3fa5b9d92c
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:58:43 +0000

    imagination thought marry spoil limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
