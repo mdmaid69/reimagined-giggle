25c2a1dc560aace46b423c87a7d758df18af06c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:34:25 +0000

    bush comeround holdon once disagree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_force(mass, acceleration):
+        return mass * acceleration
