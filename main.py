c8dc158a277e81c41bcfe8391f610f0b53719cbf
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:34:24 +0000

    direct talkoutof person complicated teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
