2955bb8e1d64bae6851e1dcd8ee46534657de8d2
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:30:42 +0000

    operate date equal lead find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
