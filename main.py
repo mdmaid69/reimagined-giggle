f1342f8e943e7b69d68de28bfd09436ce933397d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:47:39 +0000

    stamp same 569enjoy eager star

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
