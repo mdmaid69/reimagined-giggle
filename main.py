257aca119ecad536c1ff1d3e5b2c60c601ff6da3
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:06:11 +0000

    care classroom reject noise thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
