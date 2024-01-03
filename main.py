0c32be7377bb6f66e9a37974ea94c2fd74c8c759
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:19:39 +0000

    which black thick coffee official

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
