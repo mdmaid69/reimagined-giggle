91ce14eb8cf0dcbd97dcc062164fc2f2aabeb6cc
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:48:09 +0000

    patience case path declare carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_pressure(force, area):
+        return force / area
