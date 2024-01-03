08d9bc71fb88cdbe3e74b8deffea31cb8e403dcd
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:57:09 +0000

    example fond sudden if myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
