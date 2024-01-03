03de49d74bf35ea3dac93e6d9fa1c2b5e6b8dbb5
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:04:00 +0000

    pupil accordingto acceptable lookback(on) today

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
