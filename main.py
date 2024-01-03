5dc8e9d7e9782a537a6b5adcf6b9766911b07055
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:50:51 +0000

    union connected fetch stop duty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def convert_to_binary(n):
+        return bin(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
