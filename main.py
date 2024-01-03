99b416a2dcd848a7df4a4014d91cc0516385adb4
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:04:51 +0000

    lead intend concentration tired bored

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
