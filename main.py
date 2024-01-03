373be9f20f7b322417ebe3efea870101bd324983
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:09:12 +0000

    develop throw exam holiday Ii

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
