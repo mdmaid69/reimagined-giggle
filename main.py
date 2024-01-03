eef84f4edd64a85dab782eceebfd1bc36f678204
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:53:57 +0000

    refuse breathe exactly aim somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
