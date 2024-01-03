7d0154497b9eaf3695612d5e825d9cec83a42687
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:38:48 +0000

    tear fix field character favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
