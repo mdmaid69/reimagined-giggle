2e751d0633b727a2c3feed83c777d8ad06b243f6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:58:19 +0000

    whom Ss over OKorokay gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
 import array
 def get_array_buffer_info(array):
         return array.buffer_info()
