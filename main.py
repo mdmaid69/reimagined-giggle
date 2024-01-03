db92e0938e0dea48c7c729a7ea8b483334e7cffb
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:41:49 +0000

    attract picture march curious 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
