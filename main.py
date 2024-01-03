5b546e9d89622c1d4e683df0d0b3c97175bf4cae
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:37:27 +0000

    bus extra Nn union style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def cube_number(x):
+        return x**3
