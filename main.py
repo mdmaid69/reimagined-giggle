78d60bac989c878229bfadda3bb240ecd2ef1c36
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:41:04 +0000

    job calm flood cheque touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
