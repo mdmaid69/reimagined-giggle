7eed771a2874f6f039572852564a7c3176e4f4d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:32:42 +0000

    size breakup deaf reply practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def count_elements(lst):
+        return len(lst)
