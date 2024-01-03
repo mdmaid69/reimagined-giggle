1c3e4370ba66f256a67b37c0c9532f007f4a8df6
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:11:05 +0000

    tomorrow obtain scream cruel dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
