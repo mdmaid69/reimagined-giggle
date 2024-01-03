34264411a38dea4d2ec338316510f392cc1c6d29
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:04:25 +0000

    weekend accidental stiff your cut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
