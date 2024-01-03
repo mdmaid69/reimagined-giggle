84887b947fc417aa84f8211c5db0c0e20e348e51
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:29:04 +0000

    568energy soon square press development

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
