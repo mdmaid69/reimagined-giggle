440b673c08bd37c7de8cdb225ed9281686aa5e46
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:41:10 +0000

    responsibility science major claim lunch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
