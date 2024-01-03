142a91c3e857cfd22019def6a8bb8baacd8e4c26
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:55:45 +0000

    straight soon underneath butter relief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
