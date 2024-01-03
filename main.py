a13a75185e904840c5397d796f9165106a3a1745
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:04:11 +0000

    ring fast long bush stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
