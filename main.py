2afceaf465ebb2eb137761b95efbcb5ed0bd0dab
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:16:16 +0000

    bored direct add order spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
