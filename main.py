ee1993be4f218b00e99506bf28ab155e98e00d2e
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:24:22 +0000

    whole wave independent pupil offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
