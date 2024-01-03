39c66aea5d6137dfbfaa5c86f1a4e212846029eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:57:36 +0000

    holdout bank yet great minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
