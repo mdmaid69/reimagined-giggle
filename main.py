5964cc6ed0d5759c0222526cebbf807460eae359
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:43:19 +0000

    may peaceful defend afford contents

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import time
-def get_current_time():
-        return time.time()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
