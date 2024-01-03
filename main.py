37caaa169c8f10fbf01b811e4bfac0555a11a60a
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:49:49 +0000

    rest slice tense always opposite

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
