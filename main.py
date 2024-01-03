8fe5bfa5890150737a3a03b8e97c15cbddf510c8
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:46:15 +0000

    previous trade against holdon rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_odd(n):
-        return n % 2 != 0
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
