50379540f93357e058aae0d9705ce2a1e1bc26c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:58:57 +0000

    estimate bet create grow limit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
