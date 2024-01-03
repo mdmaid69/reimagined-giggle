30d8c643a07b6f50b863ae38776eabd3148c7e5e
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:25:21 +0000

    long turn Kk finish suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
