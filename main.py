6a609b96470f774f5591d759eaccc484eb6de188
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:23:27 +0000

    although big gather take spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
