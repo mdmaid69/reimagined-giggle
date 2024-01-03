2d8e13becfc4dddff15bde94c903b114753d3c34
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:33:52 +0000

    precise plan matter male blow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
