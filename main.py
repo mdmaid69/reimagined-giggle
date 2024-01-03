2fc74614716699e796cd76bd64422bcec2e40ea0
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:14:24 +0000

    wherever only statement announce black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
