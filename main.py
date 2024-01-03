492722347f40a83a14a8dade731bfe822d634d2d
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:16:33 +0000

    weather dare disappointed bury apparently

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
