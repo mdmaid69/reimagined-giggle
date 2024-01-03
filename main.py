9c1b431cfa8fea0d52136a8ca2622f74193f98aa
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:38:44 +0000

    at unpleasant entrance sharp rich

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def reverse_array(array):
-        array.reverse()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
