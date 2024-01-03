4064a8186a249c90ca59d063a121699ffb976ea9
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:36:53 +0000

    talkoutof apparently section community main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
