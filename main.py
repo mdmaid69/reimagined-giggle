9da758ae03565cb56b666436ba6555ba0a17a270
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:38:50 +0000

    keepup support probably collection follow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(10): print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
