071d6545c34dc0dbe598a9ec77820f80ee910f1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:33:23 +0000

    burn slide accept it quarter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
