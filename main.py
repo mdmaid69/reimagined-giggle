30082fc0c52cee667ae794e50b4e02611a788757
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:30:11 +0000

    amount odd sea post correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
