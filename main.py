ebbf567af8ae88f33bc527e21cd2107a8a96974d
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:19:41 +0000

    protest hair everything official whisper

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_unique_words(sentence):
+        return set(sentence.split())
