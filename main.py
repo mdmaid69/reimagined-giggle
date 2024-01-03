8beb66ec4adb6f7c0ca0e46e7f51484ecf03aa2a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:08:07 +0000

    ill holiday centre group human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
