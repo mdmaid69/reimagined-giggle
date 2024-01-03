ae4edaa1b88944e933298eb0ef976fe42e2d8a41
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:21:31 +0000

    boat impressive our accidentally risk

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def convert_to_octal(n):
+        return oct(n)
