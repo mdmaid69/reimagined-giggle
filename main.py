4f1d252122d6831418dc93a18160b2e0e90599c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:44:51 +0000

    nigga callout normal copy succeed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
