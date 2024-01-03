aad9c782084efbf18a18a5aa71c1275603975575
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:37:59 +0000

    relative finally help personal safety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
