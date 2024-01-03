255f43e43dc2711d8aa3e67ea4c5e82e3a417840
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:42:38 +0000

    bet edge design tape combination

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
