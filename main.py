3437cf207df93d88796887bcd83fb2927e199d6d
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:41:26 +0000

    interfere acceptable city permission dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
