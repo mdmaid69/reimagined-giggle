c5f7de0454ceab3e6fd954d4a43ddaaa3cc8c538
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:28:15 +0000

    lonely negative Zz honest must

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-print(sum(range(10)))
+print([x**2 for x in range(10)])
+def cube_number(x):
+        return x**3
