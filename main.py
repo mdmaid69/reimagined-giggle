48ad5e748d39daaad3ac18135479c3b6fb7bc2d3
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:14:25 +0000

    takeover treat share freedom various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
