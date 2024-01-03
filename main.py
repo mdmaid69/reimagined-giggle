ebd6b4fc2851bba9e9ea8f6c656df98d38b69fc3
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:23:39 +0000

    need criticize like bottom angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  def convert_to_hex(n):
+        return hex(n)
