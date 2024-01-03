fb425afe18cef37fa52789d5ef3343a4b71aa31f
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:17 +0000

    fit expression praise shoulder equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import getpass
+def get_username():
+        return getpass.getuser()
