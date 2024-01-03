8b55fb69dbaa87e6ba88d4a09ff9b34c2abf75d7
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:58:43 +0000

    mine common uniform wave side

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+print(sum(range(10)))
