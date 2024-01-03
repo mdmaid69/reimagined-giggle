5058b158893a0e88409be7d2337239db0920aa6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:12:18 +0000

    contact twist thankyou than chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def convert_to_hex(n):
+        return hex(n)
