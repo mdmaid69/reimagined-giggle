d16aa7d645d3cbe39a394bcfddbb4639cf2a8048
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:42:20 +0000

    standard past run impossible the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def clear_array(array):
+        array *= 0
