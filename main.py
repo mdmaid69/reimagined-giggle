175cdc4ff7a7eeeca3114c2292b2e0123f43f0e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:08:40 +0000

    shallow activity talk age central

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
