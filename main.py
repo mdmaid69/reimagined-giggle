96d59352f004f13d01608d24f22dfb294a1d6b12
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:23:22 +0000

    relationship because ask like address

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def count_elements(lst):
+        return len(lst)
+  def square_number(x):
+        return x**2
