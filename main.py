679aa560486bd8450e1ecea752f6fb551350e513
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:45 +0000

    standard push false talkinto bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_list():
+        return collections.UserList()
