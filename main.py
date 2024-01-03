ede776deb12911226d0b3dc60663a68e3946249d
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:43:20 +0000

    command lot reject forgive discover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
