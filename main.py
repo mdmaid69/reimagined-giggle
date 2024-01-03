b85ac053a3f161eb79be91c84225b00357eae4c2
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:59:31 +0000

    oil pencil small degree awake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
