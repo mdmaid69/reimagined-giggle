fd8f68f3497cdda422f801a3b4ca3ac99770b4b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:02:30 +0000

    anyone violent final admit would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
