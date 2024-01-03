709b3299d8da2f89f12ace66375fa2c5c332c970
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:05:28 +0000

    compare discuss entry quarter pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
