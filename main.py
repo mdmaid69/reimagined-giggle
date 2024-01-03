54c0f15cf8d86627cdc6f904b27fb463eebc8713
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:26:03 +0000

    unite obvious connection communication hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
