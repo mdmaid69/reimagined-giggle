a68f164e415f430a74bfba5dde84ff82d0b54705
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:36:00 +0000

    whenever night slide cruelty part

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import numpy as np
+print(np.array([1, 2, 3]))
