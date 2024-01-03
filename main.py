172f3604ab87c59d390ed034813ef4dad5f0f15f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:58:50 +0000

    swallow describe sink understand grow

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-for i in range(10): print(i)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
