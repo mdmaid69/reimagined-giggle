3e1e5d5952ee94a989d83fbf62f0b32f107e9ee4
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:06:00 +0000

    operate event go grade centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
