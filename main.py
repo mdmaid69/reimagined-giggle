d3f877838e5ca031e925c4d27b9dd9963f4454e0
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:53:53 +0000

    congratulations exaggerate large Ss computer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
