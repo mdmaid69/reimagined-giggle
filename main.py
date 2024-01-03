b243189fe19c628b44e2b7e222bc4764102e580e
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:57:16 +0000

    getoff ignore die suit imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
