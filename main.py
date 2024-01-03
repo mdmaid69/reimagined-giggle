8f32712078b52790734e6b38e38e6d633283befd
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:46:10 +0000

    line view old scare smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+import tensorflow as tf
+print(tf.__version__)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
