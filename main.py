d5cb788c7f926316cac35656f4791cb01a2fabd3
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:37:43 +0000

    comefrom message wise protest regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
