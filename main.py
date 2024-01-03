7b3e526e9f7f671e0d63910af6644fab5c21d26e
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:55:34 +0000

    off impossible quarter no no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
