d9e9aa32fa66aa25248d6ada75feaa2a0d3f05f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:50:18 +0000

    question terrible getout set bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
