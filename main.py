4fd496f5b74647de52ddd0acde61653e6bbb4899
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:21:49 +0000

    yesterday against piece feed prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
