3339ef7923e326cf09047aafaf800dbe9f5c3d09
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:10:13 +0000

    typical blue confident fine post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
