3c7beff13e67cfedf2750131919c6e423fd3653d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:27:28 +0000

    Itself take couple our group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_item(array, i):
+        return array[i]
+import os
+def remove_directory(path):
+        os.rmdir(path)
