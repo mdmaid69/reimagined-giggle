2dd449d399df75b3cd709bf16967aabc658b58cd
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:33:08 +0000

    terrible offence sometimes life actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
