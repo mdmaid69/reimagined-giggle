bd310056a24f18513672e19e80fbbdce41fde1f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:56:33 +0000

    discuss ought chance entrance invitation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+print(math.pi)
+def calculate_work(force, distance):
+        return force * distance
