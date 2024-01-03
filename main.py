845bb2aca13660dd848d3c18365f6d8e1c2e965d
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:14:52 +0000

    knock shake occasional journey general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
