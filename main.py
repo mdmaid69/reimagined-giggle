af9bf61dc20279719f06e47e52d0a1b44853181d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:15:21 +0000

    average could run object suddenly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
