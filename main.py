bc2a8c80882093c466b0824586c89d3c361a6cab
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:01:27 +0000

    pipe lookafter later need here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def add_numbers(x, y):
+        return x + y
