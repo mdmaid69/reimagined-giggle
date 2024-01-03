2d501c18a0a5e8f8517f6b1aefa40a1c3e735611
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:35:42 +0000

    destroy somewhere thumb guard keepoff

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import datetime
+def get_current_date():
+        return datetime.date.today()
