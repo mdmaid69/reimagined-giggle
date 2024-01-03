bb5efa7428973d435ffdeb96f84f84a9068452a5
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:41:28 +0000

    squeeze damage situation divide long

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def subtract_numbers(x, y):
+        return x - y
