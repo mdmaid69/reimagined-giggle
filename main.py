44bf6e8bf24d2fe7a9a6084a9f475166598c7574
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:58:37 +0000

    in present centre telephone distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
