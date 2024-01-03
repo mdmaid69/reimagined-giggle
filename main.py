6be626e1e5453dca992e0c7aff52506d76c09956
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:09:18 +0000

    confusing free worth sometimes sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  def sort_list(lst):
-        return sorted(lst)
