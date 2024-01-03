09723fd67afbb70ee31b1cc511990df18be82b53
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:15:11 +0000

    that invitation basis attempt lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_str(array):
+        return str(array)
