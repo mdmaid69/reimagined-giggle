d975f9eb6b4bc23688ee5ea5b967c6d5d00a37ec
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:08:53 +0000

    secret fact manage talkinto report

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
