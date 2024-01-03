182ce57d15846bcf10db32fe6ca55d7058e1b9f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:58:38 +0000

    suddenly someone imagine record lunch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
