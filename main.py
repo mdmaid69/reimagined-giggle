b9b8a84339105e2fb898bdf32760cba4994987b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:40:36 +0000

    underneath callat division characteristic many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
