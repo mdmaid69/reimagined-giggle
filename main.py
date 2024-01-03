1dfcc81c7555a3bd14f297996d94f711002e9b6e
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:39:39 +0000

    basic part warning male comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
