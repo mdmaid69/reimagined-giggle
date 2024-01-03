cc186276573c0929aaba05158f2950bbcda924b8
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:24:54 +0000

    doctor collect cost advice desperate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_as_str(array):
+        return str(array)
