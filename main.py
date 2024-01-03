fcacb13333185927147d60f7aa53590433afff25
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:16 +0000

    holdout little left custom cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
