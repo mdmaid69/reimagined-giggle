63f81ef2df42d888676135c1200e1d185d72d17b
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:05:56 +0000

    plan call tone good round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
