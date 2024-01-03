f8be5b0f0a2756ef59155b9ceb493f2ad766ca1d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:36:50 +0000

    exam 179bite office married study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def is_even(n):
+        return n % 2 == 0
