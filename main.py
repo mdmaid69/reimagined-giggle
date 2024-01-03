8ca30cab21ace6fd65d62e7a039228b5b9f73bdd
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:49 +0000

    front address shop sensitive however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
