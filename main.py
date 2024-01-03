71fd1581bea6c404991198a9f38cd9664a22ac38
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:35:01 +0000

    need contact regional once love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def cube_number(x):
+        return x**3
+import array
+def get_array_item(array, i):
+        return array[i]
