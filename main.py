b54d20e142e39d0133bf37bf8e6bb85cedf29ed3
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:18:45 +0000

    colour really picture runover combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
