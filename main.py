3674889279833a5e198880597aa52a0398db481d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:22:48 +0000

    right service fairly party shoulder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
