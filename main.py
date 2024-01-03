938de774779bd394d81ac8ecc337950fb17857d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:38:06 +0000

    interrupt regular near how design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
