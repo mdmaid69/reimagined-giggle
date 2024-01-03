1a0ddaf7461c64bfb2665963a7e193b8f386efca
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:46:54 +0000

    somehow degree neat sick allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
