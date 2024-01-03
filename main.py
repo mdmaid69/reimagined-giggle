2e74d3df96352639ae028aa6d765315dc393a277
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:41:05 +0000

    bank ocean worth become contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
