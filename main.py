4e0f8bf593d47828674c1f63a64252dbc2625154
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:57:16 +0000

    company actual record callat so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
