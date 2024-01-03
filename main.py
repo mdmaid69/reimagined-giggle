27bf3849cdb4dd5562d9b43910428fe272f98ad0
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:41:37 +0000

    news fairly shallow machine already

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
