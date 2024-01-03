9cfe21ed7737104fcce50b97466b9972340de9b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:06:58 +0000

    pull kill you tall temper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
