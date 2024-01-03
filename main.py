a4e1176e8c634d7b967894c50b5ffb002bd46182
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:58:26 +0000

    owe pair goon claim mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
