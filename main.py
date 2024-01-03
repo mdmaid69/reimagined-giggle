d93f782d6464b31862e3456ea5be7ef1601a7a60
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:37:44 +0000

    trade progress not breakdown yes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
