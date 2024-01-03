0f53141b70fb14d937c7f7a7fd266904a877e289
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:24:13 +0000

    backwards real even hang rush

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def count_characters(sentence):
+        return len(sentence)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
