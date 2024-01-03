d7349f7a8a880638d3dc15e6d996524f2ad6c6b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:56:33 +0000

    repair Ee muscle lunch pool

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
