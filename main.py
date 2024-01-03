43e26a7740d75fcfc1be670b50d85ec77ca8d7f7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:00:18 +0000

    kind turninto high whenever belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
