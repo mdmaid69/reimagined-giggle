36532e6f55e19b1a31cb5e9ce6f1ffd2993bd0eb
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:23:22 +0000

    upwards thirsty arrive shut case

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
