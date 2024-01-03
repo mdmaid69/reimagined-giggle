d8a25becee66efb3f487e4fd6642954ddb0ddfe4
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:35:17 +0000

    lead independent fine sea expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_typecode(array):
+        return array.typecode
