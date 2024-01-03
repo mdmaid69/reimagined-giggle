18bacf74e3c0a0bd7aae34aaf7c2e523ade16bbd
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:18:32 +0000

    young content beside music prefer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
