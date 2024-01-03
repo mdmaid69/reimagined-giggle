a0943f38aab0acbb1b6387ade26c13e4e16a8201
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:09:52 +0000

    break success board Zz statement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
