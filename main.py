12861a17e51fc9987164858d71f468f195851a01
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:02:11 +0000

    close anywayanyhow satisfied behind 566end

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
