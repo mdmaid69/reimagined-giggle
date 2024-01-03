6d9a27994d23125604dda190dc84e9eb6703b135
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:05:16 +0000

    up murder apologizeapologise spill attempt

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
