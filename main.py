fa864a93fe1dddc1870e3115d5a21b01b402cb22
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:54:37 +0000

    immediate rather aim mixture before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
