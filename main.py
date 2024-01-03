99d6437e8af8a4f3787745349d0cecdacca95218
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:58:18 +0000

    many office rain certainly politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
