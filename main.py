00b27a51d2623a1fefcafb67a84d97664870fd84
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:52:10 +0000

    too male wide godown indeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def count_words(sentence):
-        return len(sentence.split())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
