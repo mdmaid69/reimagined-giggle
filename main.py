2195c5622d4b0e3274ed7b219c978a1539dc08e6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:05:05 +0000

    break together possession fetch unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
