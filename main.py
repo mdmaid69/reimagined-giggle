922d1d357d38aa032bf86bcbb659f43fc47132f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:26 +0000

    crash complicated development zero today

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def subtract_numbers(x, y):
+        return x - y
