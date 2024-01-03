4f87bef7954d9fcb9eaebaa468af090feb882b46
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:07:04 +0000

    confuse represent though ever pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import platform
-def get_python_version():
-        return platform.python_version()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
