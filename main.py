ea87a04b58420b95fd854e37be2e1eaf9917892a
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:37:11 +0000

    control everywhere wonder Yy breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
