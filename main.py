cb5b8291abb99d6c9b9a3389dc87591469523797
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:43:54 +0000

    impression Itself quit smooth care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
