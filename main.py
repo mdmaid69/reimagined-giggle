21e4fd08fdba262ebc5176d43320383040d1e34e
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:36:35 +0000

    happen unable complete trap pay

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
