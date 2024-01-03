5a1bbc1fee21aad421ae66ff73df48069c244f9d
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:00:05 +0000

    personal summer shine mouth ocean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
