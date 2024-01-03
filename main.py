cd1fbe99a78118b5000ab2528ce85a409b6ecabd
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:06:19 +0000

    brush empty down gift ear

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import re
+print(re.match("h.*o", "hello world"))
+for i in range(10): print(i)
