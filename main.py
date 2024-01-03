a83ae7292328d0daff93741e9f46d8dada74d39f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:21:25 +0000

    company nothing basis object still

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
