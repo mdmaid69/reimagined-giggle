2dd8020fcc8ed4c5a165813a8b43312c86b93232
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:53:11 +0000

    Mm dream left emotion hers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
