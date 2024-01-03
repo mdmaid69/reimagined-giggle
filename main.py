20dd812012f298294d77579a0ebc268f38e5e79a
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:58 +0000

    refer store silent pity opponent

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
