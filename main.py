17e6a170d621e03694ea20d83ea12e4338114414
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:00:21 +0000

    bringback balance slowly friendly other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
