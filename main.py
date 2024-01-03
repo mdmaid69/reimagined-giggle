17c3288e7d2e4910caf3cfbb78f077d4c429b6ea
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:23:39 +0000

    tend address deep world bend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def count_elements(lst):
-        return len(lst)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
