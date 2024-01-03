cdd90bf84af3b7af2b2e5007232130b8a29a96d6
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:54:24 +0000

    easy control holdon swing performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
