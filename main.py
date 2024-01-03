1f5706fe3246971e3eda09b110a3806c71f8dbea
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:12:21 +0000

    would spin stone Jj factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
