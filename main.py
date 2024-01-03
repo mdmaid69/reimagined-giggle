d6886e4e12ab892389b8fb9ad2e1bb4f604e243f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:41:36 +0000

    promise offend takeout or lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
