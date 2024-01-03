8d9cf066087365ed754385c8b065a36721c26749
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:39:33 +0000

    pay look receive some health

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
