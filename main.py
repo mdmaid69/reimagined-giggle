3d4aba1e5f52b9d779a4f10ce162cbd9942846b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:53:01 +0000

    range defeat male unless whenever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
