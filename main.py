96c7beba32d970753baf72ae47ebfefed62c53b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:43:49 +0000

    congratulate accidentally rush lunch collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
