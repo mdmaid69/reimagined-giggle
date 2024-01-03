f75ab352b725656166152c5e5cedd622c1a92866
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:52:22 +0000

    side rain persuade but century

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_work(force, distance):
-        return force * distance
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
