bc16e03f2b4440f8aaf17f89508bbfc732ea5eb4
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:04 +0000

    common life shoot letter getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
