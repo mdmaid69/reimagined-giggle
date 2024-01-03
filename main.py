2fc469ad5af46d7fdb2f726e2ea7c253ad2d6302
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:13:58 +0000

    influence frighten stiff load service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
