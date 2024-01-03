4a6d2e160b1e87d93f0446c16cb779db44da9bd8
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:41:28 +0000

    join fresh holdout squash duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def roll_die():
+        return random.randint(1, 6)
