156e00ec8ccc32445c1dd5b37d74c34d1532821d
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:03:27 +0000

    hard firm majority type flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
