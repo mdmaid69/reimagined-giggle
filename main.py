ab2eac330ca5bbdccf9a91f37fdb1fe5d241fbb7
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:59:46 +0000

    attempt suit disagree area never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
