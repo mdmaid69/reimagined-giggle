c58b3cdfb5bb0040b2819e9c5b4135db172ecbf7
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:50:29 +0000

    reduce performance bush reply hit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
