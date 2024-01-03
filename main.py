a850c1f96d51c173e16cf7d04891a3b355b09cdc
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:45:08 +0000

    better climb evening notice neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
