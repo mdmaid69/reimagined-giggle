45ca05c05c6c4ba0386c5c0a48e7ee12a674a8b6
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:06:38 +0000

    without thorough realize circle container

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
