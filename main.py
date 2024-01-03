e1ded4bdbe49c7c9a02cc67b390c52e071290284
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:53:17 +0000

    rush discover weather sort aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
