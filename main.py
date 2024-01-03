9766f395c5bddfa89d9517ea664663e10bd4e663
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:48:41 +0000

    centre secret some long there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def convert_to_binary(n):
+        return bin(n)
