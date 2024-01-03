7b73afb2de6b83db393a81dff96d5c7704f1ed76
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:36:49 +0000

    wake table desperate match combination

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def count_characters(sentence):
+        return len(sentence)
+def convert_to_binary(n):
+        return bin(n)
