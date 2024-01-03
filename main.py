b7a656b06f854011c131bc5e2f5a60fb0fbe9246
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:29:25 +0000

    skill visit say term squeeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
