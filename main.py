1308d48f9818b56364211ae524da5332dee9d51c
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:11:42 +0000

    aim confidence decision disappear will

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import sys
+print(sys.version)
