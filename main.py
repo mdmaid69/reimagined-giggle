6aaf4370d8a437e589dcbbd948caac315bdad999
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:38:19 +0000

    love tour depend people issue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
