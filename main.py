1a157e36971a4c07dbd41740341a1560c2077672
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:46:52 +0000

    file wish bet why whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
