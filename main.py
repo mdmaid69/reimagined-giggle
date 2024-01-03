1924a25b1b641c45ec0895ce3fac9d70c2ddb598
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:12:01 +0000

    taste it admit criticism draw

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
