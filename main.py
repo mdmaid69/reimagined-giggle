57ad24c570b938692ce6c4079985b38f1adba2c6
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:34:26 +0000

    interruption choice building stamp long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
