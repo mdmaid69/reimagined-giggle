cac3a6ddf30ea15f92d719a616e027b80d754974
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:49:30 +0000

    final possibly schedule help Bb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
