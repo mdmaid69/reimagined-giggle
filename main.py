3d92e87c19fa3403d45260288eda95fee0aac49e
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:53:17 +0000

    legal keepfrom physical tap idea

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_index(array, item):
+        return array.index(item)
