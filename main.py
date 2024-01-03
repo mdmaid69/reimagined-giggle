c75822a9aa5f96bf9324685cb6a92f7118b95707
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:34:39 +0000

    ours quit approve vote exploration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def cube_number(x):
+        return x**3
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
