b5206f8d515b38c007793d1b2ac7e09e11e588e8
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:27:52 +0000

    rise invitation Oo row independent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
