bb087203b4c36bea8c4471a1a6e9271ac417cacf
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:19:40 +0000

    pile shut imagine area sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
