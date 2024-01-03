2ae6b27da7bbea02a5f0f57fb0a806370a544f30
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:00:37 +0000

    clearly sign before Ff sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
