8aa21c98db6d615dcbc6553d6e5d9067b094748a
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:03:58 +0000

    another answer report compare somewhere

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-print(math.pi)
+def get_array_itemsize(array):
+        return array.itemsize
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
