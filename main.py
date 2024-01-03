5a9ff5d5e86f45c17dba8f1bf145cf3510c74d4a
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:02:41 +0000

    idea throw class pile other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
