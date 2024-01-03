6febb281a0109bb827d35f5ebf6b668ce54f0ae6
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:55:09 +0000

    eye film lift step produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
