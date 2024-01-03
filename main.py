412146d95eae2d4f86c60df8f15ae39937c759c0
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:49:05 +0000

    offer hole answer so stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_item(array, i):
-        return array[i]
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def is_odd(n):
+        return n % 2 != 0
