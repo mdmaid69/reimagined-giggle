3dae2706db355a9389ee757f33cdfb9e45e6df36
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:37:59 +0000

    course any open warm centre

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
