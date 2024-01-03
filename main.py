506a195619c15c6f0ef0c225a660c49a72544752
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:52:42 +0000

    smart wild match bush swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_speed(distance, time):
+        return distance / time
+import os
+def remove_directory(path):
+        os.rmdir(path)
