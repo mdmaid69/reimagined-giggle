ae9f98d3b92002db480d89f0c47639b5091056fb
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:28:12 +0000

    proper upwards 565end direct call

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
