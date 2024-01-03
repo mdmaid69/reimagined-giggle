3e3096f4132ee8db0e524bf8d6984a7490638743
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:28:45 +0000

    pen reliable available flat own

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def reverse_string(s):
-        return s[::-1]
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
