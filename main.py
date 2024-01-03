2a57e745b15412518f4d869bef319762058d843d
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:21:04 +0000

    glass programme be hope importance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import collections
-def create_counter():
-        return collections.Counter()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
