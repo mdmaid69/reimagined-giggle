da4f102ac417a2feec828f6136c7c735791cc6f4
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:15:40 +0000

    large suggestion figure effect kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
