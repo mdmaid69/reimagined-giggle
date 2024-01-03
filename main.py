b5bbace44de3b05b9462d45e9132bd12863d14df
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:20:02 +0000

    defend branch wild Hh order

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
