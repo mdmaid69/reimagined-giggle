196b357b5a3af8c0a5cbfd35e5ae8919a9c4bdf0
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:22:03 +0000

    steer university excited pay could

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
