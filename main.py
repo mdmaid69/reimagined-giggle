79114d1219e0ea87151f30953a390cf6416065bc
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:30:13 +0000

    nearly love room date priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
