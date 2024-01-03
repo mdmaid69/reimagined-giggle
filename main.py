45510b993ad55a2e4235752a92d581a528bfee97
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:27:21 +0000

    claim ours compare guilty Ww

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
