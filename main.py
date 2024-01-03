39970692489366376b7ca0c3ad18995f0093be26
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:22:12 +0000

    mainly second mystery include cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
