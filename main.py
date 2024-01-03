5dbbc4e26b13fd2a85ad3454babb14e423467c30
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:08:14 +0000

    circumstance buy scheme Zz term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
