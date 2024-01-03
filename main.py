2daae5dfe0ec6aca68b9e2565d6b7582ff80ea36
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:19:43 +0000

    tread measure improve defeat thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
