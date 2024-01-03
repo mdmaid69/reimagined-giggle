fba18bc34dd0807864518c6d0f85250b80822006
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:19:31 +0000

    aim workon interview scale tonight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
