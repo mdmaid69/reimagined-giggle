b25b9a14d0295e0ce54c7dfd1209b4792f7404de
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:25:15 +0000

    gradually refuse deserve manage dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import datetime
+def get_today_date():
+        return datetime.date.today()
