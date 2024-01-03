77643e26023ffa3acd55cddd0f952b565f1ec17a
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:14:06 +0000

    vote demand shut personal tip

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
