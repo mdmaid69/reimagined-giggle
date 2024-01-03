8b50d1a0a5d9737d19afa73ff9325f974e048016
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:28:48 +0000

    share phone promise break relationship

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
