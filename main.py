4f5f3a348a35f4897c51c85eb38ffade8affe0b2
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:12:28 +0000

    wave previous brave bet only

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
