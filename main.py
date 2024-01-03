30b900d8255a17bdca26f516ada6386eb222de31
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:27:58 +0000

    pile proof frequent rain keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def sort_numbers(numbers):
         return sorted(numbers)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
