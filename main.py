aa7b5ef0e994c33e2c381805c7e104edef87ad6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:15:24 +0000

    getoff effect neat history Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
