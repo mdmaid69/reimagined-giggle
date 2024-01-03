dde7ad5a6dea2648dbe5736a48cde30123d65a23
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:31:00 +0000

    coffee idea else us pain

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
