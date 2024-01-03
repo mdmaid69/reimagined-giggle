a54e25bb6b9864ae34b467fdeba9d9bd364e2e8b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:04:53 +0000

    survive stop crash skill difficulty

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_from_bytes(bytes, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromstring(string)
         return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
