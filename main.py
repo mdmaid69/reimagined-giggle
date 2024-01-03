103c2f30c1eb0966faf4604136c8295174e17bb3
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:09:16 +0000

    me win comfort sound live

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
