9152a6be44af8e7f54641009d131ef8d85eb7cde
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:35:34 +0000

    Qq exact explain wear turnup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
