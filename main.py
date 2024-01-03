c176995aab30f914e140f101bb985bfdf2983a1a
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:35:54 +0000

    expect familiar build them raise

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
