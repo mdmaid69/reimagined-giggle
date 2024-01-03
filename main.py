b80aa61912e16f3c202ee999743fe3eff89bc29f
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:57:27 +0000

    path not mention comeround tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
