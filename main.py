84438f038862a0bbce2746d86b039758c6b8135f
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:52:51 +0000

    shut season develop thumb direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
