578bb39de40ec81cb47b0cb471f5aa0ca9015e52
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:39:36 +0000

    attention clear exaggerate mad whistle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
