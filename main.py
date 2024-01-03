21957d1b34c02bac1da8b8f50c42304979bff2b9
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:34:01 +0000

    stretch news noisy married nearly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
