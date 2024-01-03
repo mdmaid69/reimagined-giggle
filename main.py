3a400a5d45a94ea7bbf6c3857956e615afa271c3
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:15:49 +0000

    odd accident design naturally spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
