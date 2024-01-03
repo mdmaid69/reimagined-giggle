b46fdfb909a4dbe458d53ec98faf5414d66d7ab8
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:54:36 +0000

    key test compete cum aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
