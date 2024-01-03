93830db533f840a1839d48125b6789028242200b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:30:13 +0000

    brush block fear getup by

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
