cb95d024442325fa815ee74e0472dc8a6df3f672
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:09:10 +0000

    his just 566end offence total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
