c7bacd299cbef80b63f589c647fb626d25c7a14e
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:27:53 +0000

    pattern makeup art influence quality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import numpy as np
+print(np.array([1, 2, 3]))
