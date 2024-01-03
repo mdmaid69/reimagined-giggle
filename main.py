099cf98861b48b5146433c4c5daac3cf11737e91
mdmaid69 <robot@alexsyw.me>
2024-01-03 21:16:23 +0000

    worth advanced during damage fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
