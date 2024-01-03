929692d7ab8fb511b4dfa005884e443725ed9901
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:08:01 +0000

    awake increase wild maximum belong

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
