6178fdc392d3d959238751b91210550d726ecbe6
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:38:09 +0000

    along capital top its clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
