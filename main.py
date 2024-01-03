24488b79a9ad2aa8ef285e2f355faa78a9eea8ca
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:22:34 +0000

    chat connect Dd good strength

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
