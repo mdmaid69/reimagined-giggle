b3052100d534d09d3227cf2e737204db21ff3438
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:10:15 +0000

    urgent callout cough hard wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
