a4da7801ab6911aa8f6a3eefaed75d5c2f3c02ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:58:33 +0000

    still she pencil tie avoid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_speed(distance, time):
+        return distance / time
+def add_numbers(x, y):
+        return x + y
