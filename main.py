d946a38c987c989ccf327591b2148d4b7d6a37dc
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:00:57 +0000

    someone makeup rule into turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
