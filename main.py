a93bffe2971ececa0bc418323ff2ac76cae4491b
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:06:19 +0000

    include front immediate private level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
