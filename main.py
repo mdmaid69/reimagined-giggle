51a8a399666a5e9ee8103840680af326606f698b
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:15:41 +0000

    godown welcome admit little occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
