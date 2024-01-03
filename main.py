912972caa20e773e7c017b847030122c667b923c
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:00:15 +0000

    crime quick apart bottom fine

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def subtract_numbers(x, y):
-        return x - y
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
