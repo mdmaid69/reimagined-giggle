dd860500d6f8843280e56d73bcb9e04ac75510be
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:23:44 +0000

    571enough upstairs both delight correction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+name = "Python"
+print("Hello,", name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
