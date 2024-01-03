da1e1d74fbcadfc551cf9f697835f9265345bf50
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:44:10 +0000

    walk careful imagination 573entertainment left

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+for i in range(10): print(i)
