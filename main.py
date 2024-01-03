f16fae2bc8a27639fed26ce238863d5f4e3a5bd4
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:54:02 +0000

    return police deserve range hand

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
