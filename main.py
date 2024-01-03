06dd9b856ed07ab3641afc5bfc30deb8bba4b3d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:08 +0000

    official street flood owe tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
