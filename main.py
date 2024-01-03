386ae2679c299edbd6f00a24f3d6990f08d53cae
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:18:47 +0000

    prepare accommodation offend inside range

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
