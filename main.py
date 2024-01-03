028a116f46c8994aa17555c2a740c61639ae4b3c
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:31:39 +0000

    seem lend no be properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import time
-print(time.time())
+def extend_array(array, iterable):
+        array.extend(iterable)
