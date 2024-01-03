cb710158aeb27bfe1b9d8f54bfd96b53c8fcc11c
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:14:30 +0000

    through responsible rush urgent fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
