9160e4a5a6fc6da5aa952b0641696a1ab9cb94b0
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:22:56 +0000

    increase aim call language try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def divide_numbers(x, y):
-        return x / y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
