55090f487d5981b9d289de445a9b9501b98eecb9
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:38:36 +0000

    refuse complain punish mistake stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
