70672d9fd1847d34807d88fbcb963e9613645954
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:49:28 +0000

    fire punish then complaint professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+print(math.pi)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
