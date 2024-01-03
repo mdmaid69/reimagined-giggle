d067c9e5e4c48a8a17f356693245379e1300c189
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:01:13 +0000

    university write talkinto undo eat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def remove_duplicates(lst):
+        return list(set(lst))
