c7526c5066bbb5e091384cd5acbab4efd1613725
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:22:45 +0000

    split bite door hate conversation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_acceleration(speed, time):
+        return speed / time
