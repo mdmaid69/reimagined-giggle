eff08dda704283c4bf2966780e4aa71b58751f2b
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:23:55 +0000

    cover before maximum base bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
