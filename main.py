a5771876572ce15fc70e7cdf96f1a4c9848e7afd
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:43:24 +0000

    very escape turnout great narrow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
