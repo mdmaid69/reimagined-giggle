39fe718676f5ef716366b84c9f13988f08541376
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:46:44 +0000

    size happen slight medium type

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
