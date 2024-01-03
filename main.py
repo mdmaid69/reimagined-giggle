cf4e0c91d676851fe290bfc545f169067937a4f9
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:19:16 +0000

    heat rule extra steep condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
