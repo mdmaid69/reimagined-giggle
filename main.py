f576bc5c5959fb8df2ccf388bc8877f394c4d364
mdmaid69 <robot@alexsyw.me>
2024-01-03 20:03:23 +0000

    spin summer sick anxious turnoff

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
