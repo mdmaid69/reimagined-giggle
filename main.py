2f64d2e6a7a08b64b7bbb4198937f6cb56a6a0e7
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:05:48 +0000

    stop cover build urgent mean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
