ca2f6d52afb9c895351898ef6de0dfdabf246ce4
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:05:24 +0000

    deaf weekend shoulder lead while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
