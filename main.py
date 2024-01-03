c42076fea3b84259fd4c356a2c5e02821e1a879e
mdmaid69 <robot@alexsyw.me>
2024-01-03 11:51:44 +0000

    wander wet imagine accidental pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
