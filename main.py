2d9b27a21484f2f4ff69f907b4b8098ce045e829
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:02:37 +0000

    comfort attract plate threaten couple

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
