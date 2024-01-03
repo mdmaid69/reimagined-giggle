b7c21493b0e5e614ce493691ca2f0bbad5c686fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:39:25 +0000

    Cc fix carry pattern government

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
