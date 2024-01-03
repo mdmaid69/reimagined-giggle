a784f5a06121a5ab72d2ddd5eda2618b1b39ae6f
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:47:58 +0000

    mixture he recent somewhere thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
