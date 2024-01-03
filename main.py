230af34b4f76fb02fef2fbd96c724ff93c62ed72
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:05:59 +0000

    direction refuse arrival quickly late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
