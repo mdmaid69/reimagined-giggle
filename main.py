10c1cc354893110ca6fb2bd9cde6b212fc5b7adf
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:37:13 +0000

    breakinto sharp personal occur agree

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import time
+print(time.time())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
