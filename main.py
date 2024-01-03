9e1e97c37193e1db62e0c0d67731cd460be2368f
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:10:23 +0000

    die produce breakup decision pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def append_to_array(array, item):
-        array.append(item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
