0dd9c04d1c7a96220cf547e5265bc4c31eeefe6c
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:44:40 +0000

    boring cheek exam valuable find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
 import collections
 def create_priority_queue():
         return collections.deque()
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
