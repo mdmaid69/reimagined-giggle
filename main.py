7753ce348abe0ae1bab67f74abd2688591380881
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:13:40 +0000

    result roof translate without for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
