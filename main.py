5dec12131cdfb21aa75d9ec6aee3325c1bb7e6ed
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:07:26 +0000

    keen regard ordinary excellent convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
