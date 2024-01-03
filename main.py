2fc2031551d50a24c0e1348c4c994f49becddaf5
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:40:36 +0000

    walk respect success meal taste

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
