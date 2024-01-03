6510784093056bf5dd7fefe77804406217e16733
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:52:21 +0000

    considering answer copy shadow stiff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import sys
+def exit_program():
+        sys.exit()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
