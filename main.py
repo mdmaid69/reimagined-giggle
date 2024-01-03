64e80b91360e29a3a4cf3b838d7c89d704576a73
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:33:55 +0000

    hand convincing idea brave birth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
