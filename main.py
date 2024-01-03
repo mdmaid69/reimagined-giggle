2fb99b90a4d6b49ea17f6a8775616baa2324082a
mdmaid69 <robot@alexsyw.me>
2024-01-03 07:00:33 +0000

    quality convinced furniture prize discover

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
