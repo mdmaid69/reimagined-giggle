08747a9b7b65cde68cfdb00a108735caa35f118d
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:05:40 +0000

    fashion check class seat keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
