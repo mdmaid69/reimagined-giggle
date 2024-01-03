68159e463e52bb0bf1f31d5354f2725995fb8950
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:16:13 +0000

    treat delight operate empty round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
