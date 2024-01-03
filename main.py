bd3648d5f601dd7e55d52d31cd11d0b46b85318f
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:46:08 +0000

    people progress action autumn then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_repr(array):
+        return repr(array)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
