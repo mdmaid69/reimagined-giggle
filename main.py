5f37ddf8125b489b02da9c3ebfa505b780f390f0
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:14:37 +0000

    performance formal crowded further try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
