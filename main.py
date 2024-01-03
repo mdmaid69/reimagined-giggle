801b5e737ccbbfd2e12157e1eaf16f25f2598b72
mdmaid69 <robot@alexsyw.me>
2024-01-03 01:02:57 +0000

    child out pleased morning other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def count_words(sentence):
+        return len(sentence.split())
