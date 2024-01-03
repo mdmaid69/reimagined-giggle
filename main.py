86a31bd44fee6b16fc8c63bfcd5ddd554a823936
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:28:29 +0000

    disgusting mind finally neat natural

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def divide_numbers(x, y):
+        return x / y
