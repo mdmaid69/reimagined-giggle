893afab27be9adaa11aecc7348ee422c2ed4fed5
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:35:23 +0000

    force compare easy usual harm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
