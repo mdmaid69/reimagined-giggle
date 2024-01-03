16011be815e34c5b60a86faced350b61f8ec21d0
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:47:47 +0000

    split challenge hot burst meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_list(array):
+        return list(array)
