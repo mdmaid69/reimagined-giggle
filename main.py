4a0507788765941d9de9a729c21881dc6ecf116f
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:30 +0000

    demonstrate rent detail slice value

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
