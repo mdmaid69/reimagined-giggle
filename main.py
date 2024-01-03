72eb6df8986017285d4c08a1bd89f4e347699e05
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:59:20 +0000

    may bunch cover apart colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
