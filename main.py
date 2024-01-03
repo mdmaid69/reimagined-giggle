9b9aa206365d414480bf42ed9b6bbc5401ad3a0c
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:51:22 +0000

    cover human offer length remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
