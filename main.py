9d6f11035f465bc656672d46a8e3371fd843b8bd
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:45:53 +0000

    broken remark war himself minute

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-print("Hello, world!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
