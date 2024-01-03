9436bcb5957f9eb18a076e3403cfa095e5481743
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:33:45 +0000

    represent history friendly plant scale

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
