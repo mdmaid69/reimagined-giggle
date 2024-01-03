d92b46b19b5e07d0cdcfeaeef70eb5ff100d2b78
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:52:11 +0000

    telephone explanation badly body near

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_acceleration(speed, time):
+        return speed / time
