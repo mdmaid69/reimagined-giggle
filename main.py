b8b981b13ff10a49d15ad21aafa019565a9e84d1
mdmaid69 <robot@alexsyw.me>
2024-01-03 10:59:23 +0000

    receive scene party unkind container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
