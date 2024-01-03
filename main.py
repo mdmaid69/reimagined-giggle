45b95354cd8ffee496dd3ea1341be9ab5042e25f
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:17:02 +0000

    near die open put gentle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def cube_number(x):
-        return x**3
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_today_date():
+        return datetime.date.today()
