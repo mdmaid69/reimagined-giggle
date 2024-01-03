1a54a32c90e41f2c144559e89ea7ed2237d561e3
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:44:30 +0000

    temperature nervous thumb drive listenfor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
