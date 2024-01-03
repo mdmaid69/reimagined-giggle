432a7b836c8ce42ac9b26d3be7d96132f45cf162
mdmaid69 <robot@alexsyw.me>
2024-01-03 00:35:10 +0000

    why touch gentle precise decide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def multiply_numbers(x, y):
+        return x * y
+def subtract_numbers(x, y):
+        return x - y
