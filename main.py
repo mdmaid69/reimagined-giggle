102758fce5ce95eda1a9f7f4ba238fcb0f67a9ac
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:23:18 +0000

    it room almost fold summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+print(os.getcwd())
