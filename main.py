7e1a38656cde6355fca2f01258b3311cc17c5a44
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:49:20 +0000

    uncomfortable idea gate extremely upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
