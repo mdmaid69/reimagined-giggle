05f0958a5127942f3aa437e27b22a146d30ce8c1
mdmaid69 <robot@alexsyw.me>
2024-01-03 14:02:26 +0000

    stupid true number task concerned

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_set(array):
+        return set(array)
