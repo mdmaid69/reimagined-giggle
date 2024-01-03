bf417383fc71908d539314d9c1cae70650997cd1
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:45:44 +0000

    agreement kill plastic effect nose

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
