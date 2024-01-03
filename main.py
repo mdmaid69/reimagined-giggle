e24c538303abffc635c7bdd7ad361f91f2e207cf
mdmaid69 <robot@alexsyw.me>
2024-01-03 06:44:20 +0000

    goaway hospital hour fail plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
