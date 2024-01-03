19cf65e351cf2690253e6f69209bf77ba4b810c9
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:44:55 +0000

    present without father right also

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
