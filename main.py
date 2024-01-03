37e0a95fab00e15ae5fd89a55b79dc183df48275
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:44:38 +0000

    very cough pupil unusual flow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def reverse_string(s):
-        return s[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
