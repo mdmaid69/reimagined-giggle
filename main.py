1b270a4204137018a26aa50d333e176c8540b14b
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:00:15 +0000

    reason dick uncomfortable plenty best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
