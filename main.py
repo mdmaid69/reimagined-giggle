0b3baa9aebebde1d11122d1f810486b3bd22700e
mdmaid69 <robot@alexsyw.me>
2024-01-03 05:44:25 +0000

    win dead all leave regional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
