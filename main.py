7625e766b59d23e7170d2fbcf31847783a277f40
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:24:10 +0000

    loud relief book 565end square

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area(radius):
+        return 3.14 * radius * radius
