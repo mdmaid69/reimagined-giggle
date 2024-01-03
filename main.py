56cb39113ac5ce23faf9c3c6ccdf0e80fa38de6a
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:19:39 +0000

    education apply wake muscle surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
