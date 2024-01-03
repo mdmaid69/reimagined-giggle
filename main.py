25c840dbf4021f5dbf26af11a6c7162b4e7e2ad7
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:47:41 +0000

    good visit stir takeout violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
