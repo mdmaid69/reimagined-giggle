afb323f2d6fa27a033139d498a6f495933ca68b7
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:08:28 +0000

    permission lead contact pen angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
