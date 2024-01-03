5b759cf58f9821d2a5a467ea41dba569a667f57b
mdmaid69 <robot@alexsyw.me>
2024-01-03 03:50:55 +0000

    broken reaction cruel uponon accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
