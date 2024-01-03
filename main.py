e2f66e153226bce8ac10683c278055bdb432a1fe
mdmaid69 <robot@alexsyw.me>
2024-01-03 17:35:48 +0000

    foot art their stage justice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
