918476b6746952c78944eaa7e2bfcf0ed7ab8da1
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:11:01 +0000

    plan put gas repair run

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
