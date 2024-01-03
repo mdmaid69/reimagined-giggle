cd24f7c74abd688f3f3d44cf3f921e6cb654d30b
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:59:28 +0000

    black object roof finish fierce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
