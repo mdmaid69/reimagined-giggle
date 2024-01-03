bd28a2ba4e9c375f427eda8cf5d96abb5a9463ba
mdmaid69 <robot@alexsyw.me>
2024-01-03 13:55:21 +0000

    rarely protest Uu mystery night

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def calculate_area_rectangle(l, w):
-        return l * w
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
