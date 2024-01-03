ed949a3e6c9d5bb51cde3ffec230b3efd82cc5e1
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:11:51 +0000

    deliberate cap fortunately second seem

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
