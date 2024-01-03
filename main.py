e078f61a62885cef7e0ae31ae7e97bf1b542fd20
mdmaid69 <robot@alexsyw.me>
2024-01-03 08:47:10 +0000

    sight rarely check increase downstairs

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def is_palindrome(s):
-        return s == s[::-1]
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
