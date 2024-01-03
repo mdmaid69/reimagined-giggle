96091f11775e4e301d8d5631ba1b22dd79421826
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:37:13 +0000

    serve case occur most please

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def convert_to_hex(n):
+        return hex(n)
