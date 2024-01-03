aa6777add3ec3faa91019b9c893a2f995129255d
mdmaid69 <robot@alexsyw.me>
2024-01-03 16:04:11 +0000

    breakout turnout result sting write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
+def remove_duplicates(lst):
+        return list(set(lst))
