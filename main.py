0d7a64eb0c74a2174c23b8ad0fce2da9e5f316f5
mdmaid69 <robot@alexsyw.me>
2024-01-03 12:10:31 +0000

    she suck fasten declare while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_gamma_function(x):
         return math.gamma(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
