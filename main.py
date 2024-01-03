8ce176f01671a7af2e422c2f990e2f3cc22c24ff
mdmaid69 <robot@alexsyw.me>
2024-01-03 04:50:18 +0000

    freedom file turnout might intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
