b135acf6942aee49849a788a096b71204a5bafc0
mdmaid69 <robot@alexsyw.me>
2024-01-03 02:58:27 +0000

    read mainly remember sight thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
