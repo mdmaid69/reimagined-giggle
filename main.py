2a049d37436df4ee7ede75728eb281a1cc266624
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:07:06 +0000

    low sentence chat rain long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_length(array):
+        return len(array)
