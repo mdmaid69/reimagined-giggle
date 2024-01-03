0bd82890a3a628c37081863464b1710f74b2095c
mdmaid69 <robot@alexsyw.me>
2024-01-03 09:59:09 +0000

    doubt city clean care send

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def is_even(n):
-        return n % 2 == 0
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
