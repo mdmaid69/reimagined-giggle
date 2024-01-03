6685ee895c5e8789f686ae048322c57faa749afa
mdmaid69 <robot@alexsyw.me>
2024-01-03 19:25:09 +0000

    hate tool funny escape ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_acceleration(speed, time):
+        return speed / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
