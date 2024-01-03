ca2b2627a96ab7cf68a20bca4e8c5f464f128935
mdmaid69 <robot@alexsyw.me>
2024-01-03 23:54:27 +0000

    hospital especially improvement round create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
