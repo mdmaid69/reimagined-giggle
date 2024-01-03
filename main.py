2c012f78ca4770c2aef73f4673b5c4671581c674
mdmaid69 <robot@alexsyw.me>
2024-01-03 22:26:39 +0000

    myself unless organization escape ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
