5647f1895c6f45c49a0e6a9558b10588faebae53
mdmaid69 <robot@alexsyw.me>
2024-01-03 18:15:20 +0000

    considering various equal you idea

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def convert_to_binary(n):
-        return bin(n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
