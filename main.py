37b2687b8ddc3095c3a536adc6bb7eeedec3c339
mdmaid69 <robot@alexsyw.me>
2024-01-03 15:38:52 +0000

    hear rain finger wind range

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
